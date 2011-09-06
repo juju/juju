c812be718133eb8053a74ade2361fb4a7640644a
Gustavo Niemeyer <gustavo@niemeyer.net>
2011-09-06 18:37:44 -0300

    Added config validation support.

--- formula/config_test.go
+++ formula/config_test.go
@@ -23,6 +23,9 @@ options:
   skill-level:
     description: A number indicating skill.
     type: int
+  agility-ratio:
+    description: A number from 0 to 1 indicating agility.
+    type: float
 `
 
 func repoConfig(name string) (path string) {
@@ -99,4 +102,52 @@ func (s *S) TestParseSample(c *C) {
 			Type:        "int",
 		},
 	)
+}
+
+func (s *S) TestValidate(c *C) {
+	config, err := formula.ParseConfig([]byte(sampleConfig))
+	c.Assert(err, IsNil)
+
+	input := map[string]string{
+		"title": "Helpful Title",
+		"outlook": "Peachy",
+	}
+
+        // This should include an overridden value, a default and a new value.
+	expected := map[string]interface{}{
+		"title": "Helpful Title",
+		"outlook": "Peachy",
+		"username": "admin001",
+	}
+
+	output, err := config.Validate(input)
+	c.Assert(err, IsNil)
+        c.Assert(output, Equals, expected)
+
+	// Check whether float conversion is working.
+	input["agility-ratio"] = "0.5"
+	input["skill-level"] = "7"
+	expected["agility-ratio"] = 0.5
+	expected["skill-level"] = int64(7)
+	output, err = config.Validate(input)
+	c.Assert(err, IsNil)
+        c.Assert(output, Equals, expected)
+
+	// Check whether float errors are caught.
+	input["agility-ratio"] = "foo"
+	output, err = config.Validate(input)
+	c.Assert(err, Matches, `Value for "agility-ratio" is not a float: "foo"`)
+	input["agility-ratio"] = "0.5"
+
+	// Check whether int errors are caught.
+	input["skill-level"] = "foo"
+	output, err = config.Validate(input)
+	c.Assert(err, Matches, `Value for "skill-level" is not an int: "foo"`)
+	input["skill-level"] = "7"
+
+        // Now try to set a value outside the expected.
+        input["bad"] = "value"
+	output, err = config.Validate(input)
+	c.Assert(output, IsNil)
+	c.Assert(err, Matches, `Unknown configuration option: "bad"`)
 }
