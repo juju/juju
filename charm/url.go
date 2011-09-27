6489baa8696ddd01d78346215d94ddaba64d1bc5
Gustavo Niemeyer <gustavo@niemeyer.net>
2011-09-27 11:44:42 -0300

    Actually _add_ the relevant files.. :-(

---
+++ charm/url.go
@@ -0,0 +1,110 @@
+package charm
+
+import (
+	"exp/regexp"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// A charm URL represents charm locations such as:
+//
+//     cs:~joe/oneiric/wordpress
+//     cs:oneiric/wordpress-42
+//     local:oneiric/wordpress
+//
+type URL struct {
+	Name     string
+	Revision int // -1 if unset, 0 is valid
+	Collection
+}
+
+// A charm Collection represents a namespace of charms. The
+// collection precedes the charm name in a charm URL.
+type Collection struct {
+	Schema string
+	User   string
+	Series string
+}
+
+var validUser = regexp.MustCompile("^[a-z0-9][a-zA-Z0-9+.-]+$")
+var validSeries = regexp.MustCompile("^[a-z]+([a-z-]+[a-z])?$")
+var validName = regexp.MustCompile("^[a-z]+([a-z0-9-]+[a-z])?$")
+
+func NewURL(url string) (*URL, os.Error) {
+	u := &URL{}
+	i := strings.Index(url, ":")
+	if i > 0 {
+		u.Schema = url[:i]
+		i++
+	}
+	// cs: or local:
+	if u.Schema != "cs" && u.Schema != "local" {
+		return nil, fmt.Errorf("charm URL has invalid schema: %q", url)
+	}
+	parts := strings.Split(url[i:], "/")
+	if len(parts) < 1 || len(parts) > 3 {
+		return nil, fmt.Errorf("charm URL has invalid form: %q", url)
+	}
+
+	// ~<username>
+	if strings.HasPrefix(parts[0], "~") {
+		if u.Schema == "local" {
+			return nil, fmt.Errorf("local charm URL with user: %q", url)
+		}
+		u.User = parts[0][1:]
+		if !validUser.MatchString(u.User) {
+			return nil, fmt.Errorf("charm URL has invalid user: %q", url)
+		}
+		parts = parts[1:]
+	}
+
+	// <series>
+	if len(parts) < 2 {
+		return nil, fmt.Errorf("charm URL without series: %q", url)
+	}
+	if len(parts) == 2 {
+		u.Series = parts[0]
+		if !validSeries.MatchString(u.Series) {
+			return nil, fmt.Errorf("charm URL has invalid series: %q", url)
+		}
+		parts = parts[1:]
+	}
+
+	// <name>[-<revision>]
+	u.Name = parts[0]
+	u.Revision = -1
+	for i := len(u.Name)-1; i > 0; i-- {
+		c := u.Name[i]
+		if c >= '0' && c <= '9' {
+			continue
+		}
+		if c == '-' && i != len(u.Name)-1 {
+			var err os.Error
+			u.Revision, err = strconv.Atoi(u.Name[i+1:])
+			if err != nil {
+				panic(err) // We just checked it was right.
+			}
+			u.Name = u.Name[:i]
+			break
+		}
+	}
+	if !validName.MatchString(u.Name) {
+		return nil, fmt.Errorf("charm URL has invalid name: %q", url)
+	}
+	return u, nil
+}
+
+func (u *URL) String() string {
+	if u.User != "" {
+		if u.Revision >= 0 {
+			return fmt.Sprintf("%s:~%s/%s/%s-%d", u.Schema, u.User, u.Series, u.Name, u.Revision)
+		}
+		return fmt.Sprintf("%s:~%s/%s/%s", u.Schema, u.User, u.Series, u.Name)
+	}
+	if u.Revision >= 0 {
+		return fmt.Sprintf("%s:%s/%s-%d", u.Schema, u.Series, u.Name, u.Revision)
+	}
+	return fmt.Sprintf("%s:%s/%s", u.Schema, u.Series, u.Name)
+}
---
+++ charm/url_test.go
@@ -0,0 +1,42 @@
+package charm_test
+
+import (
+	. "launchpad.net/gocheck"
+	"launchpad.net/juju/go/charm"
+)
+
+var urlTests = []struct{
+	s, err string
+	url *charm.URL
+}{
+	{"cs:~user/series/name", "", &charm.URL{"name", -1, charm.Collection{"cs", "user", "series"}}},
+	{"cs:~user/series/name-0", "", &charm.URL{"name", 0, charm.Collection{"cs", "user", "series"}}},
+	{"cs:series/name", "", &charm.URL{"name", -1, charm.Collection{"cs", "", "series"}}},
+	{"cs:series/name-42", "", &charm.URL{"name", 42, charm.Collection{"cs", "", "series"}}},
+	{"local:series/name-1", "", &charm.URL{"name", 1, charm.Collection{"local", "", "series"}}},
+	{"local:series/name", "", &charm.URL{"name", -1, charm.Collection{"local", "", "series"}}},
+
+	{"bs:~user/series/name-1", "charm URL has invalid schema: .*", nil},
+	{"cs:~1/series/name-1", "charm URL has invalid user: .*", nil},
+	{"cs:~user/1/name-1", "charm URL has invalid series: .*", nil},
+	{"cs:~user/series/name-1-2", "charm URL has invalid name: .*", nil},
+	{"cs:~user/series/huh/name-1", "charm URL has invalid form: .*", nil},
+	{"cs:~user/name", "charm URL without series: .*", nil},
+	{"cs:name", "charm URL without series: .*", nil},
+	{"local:~user/series/name", "local charm URL with user: .*", nil},
+	{"local:~user/name", "local charm URL with user: .*", nil},
+	{"local:name", "charm URL without series: .*", nil},
+}
+
+func (s *S) TestNewURL(c *C) {
+	for _, t := range urlTests {
+		url, err := charm.NewURL(t.s)
+		bug := Bug("NewURL(%q)", t.s)
+		if t.err != "" {
+			c.Check(err, Matches, t.err, bug)
+		} else {
+			c.Check(url, Equals, t.url, bug)
+			c.Check(t.url.String(), Equals, t.s)
+		}
+	}
+}
