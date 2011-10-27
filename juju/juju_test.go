9586cbc8a532bc24418acf9f5573ddceba8ec9cc
Roger Peppe <roger.peppe@canonical.com>
2011-10-27 17:01:35 +0100

    import gocheck to .

--- juju/juju_test.go
+++ juju/juju_test.go
@@ -1,14 +1,14 @@
 package juju_test
 
 import (
-	C "launchpad.net/gocheck"
+	. "launchpad.net/gocheck"
 	"testing"
 )
 
 func Test(t *testing.T) {
-	C.TestingT(t)
+	TestingT(t)
 }
 
 type suite struct{}
 
-var _ = C.Suite(suite{})
+var _ = Suite(suite{})
