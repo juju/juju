299a4164406444dfc54e18e749805d7217f3886f
Roger Peppe <roger.peppe@canonical.com>
2012-02-10 12:47:56 +0000

    minor changes for review

--- state/open.go
+++ state/open.go
@@ -7,15 +7,17 @@ import (
 	"strings"
 )
 
-// Info encapsulates information about a juju state server
-// or servers and can be used to make a connection to
-// that state.
+// Info encapsulates information about cluster of
+// servers holding juju state and can be used to make a
+// connection to that cluster.
 type Info struct {
-	// Addrs gives the addresses of the zookeeper
+	// Addrs gives the addresses of the Zookeeper
 	// servers for the state. Each address should be in the form
-	// expected by net.Dial (i.e. address:port).
+	// address:port.
 	Addrs []string
 }
+
+const zkTimeout = 15e9
 
 // Open connects to the server described by the given
 // info and returns a new State representing the environment
@@ -24,17 +26,15 @@ func Open(info *Info) (*State, error) {
 	if len(info.Addrs) == 0 {
 		return nil, fmt.Errorf("no zookeeper addresses")
 	}
-	zk, session, err := zookeeper.Dial(strings.Join(info.Addrs, ","), 5e9)
+	zk, session, err := zookeeper.Dial(strings.Join(info.Addrs, ","), zkTimeout)
 	if err != nil {
 		return nil, err
 	}
 	if !(<-session).Ok() {
 		return nil, errors.New("Could not connect to zookeeper")
 	}
-	// throw away subsequent session events.
-	go func() {
-		for _ = range session {
-		}
-	}()
+
+	// TODO decide what to do with session events - currently
+	// we will panic if the session event channel fills up.
 	return &State{zk}, nil
 }
