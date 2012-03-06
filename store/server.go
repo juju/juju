c59fd340153dc4d8bcf561efe615b54875584ed3
Gustavo Niemeyer <gustavo@niemeyer.net>
2012-03-06 01:13:01 -0300

    store: bootstrap juju HTTP API server
    Serves properly /charm-info as required by the client.

---
+++ store/server.go
@@ -0,0 +1,72 @@
+package store
+
+import (
+	"encoding/json"
+	"launchpad.net/juju/go/charm"
+	"launchpad.net/juju/go/log"
+	"net/http"
+)
+
+// Server is an http.Handler that serves the HTTP API of juju
+// so that juju clients can consume published charms.
+type Server struct {
+	store *Store
+	mux   *http.ServeMux
+}
+
+// New returns a new *Server using store.
+func NewServer(store *Store) (*Server, error) {
+	s := &Server{
+		store: store,
+		mux:   http.NewServeMux(),
+	}
+	s.mux.HandleFunc("/charm-info", func(w http.ResponseWriter, r *http.Request) {
+		s.serveInfo(w, r)
+	})
+	return s, nil
+}
+
+// ServeHTTP serves an http request.
+// This method turns *Server into an http.Handler.
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	s.mux.ServeHTTP(w, r)
+}
+
+type responseCharm struct {
+	// These are the fields effectively used by the client as of
+	// this writing.
+	Revision int      `json:"revision"` // Zero is valid. Can't omitempty.
+	Sha256   string   `json:"sha256,omitempty"`
+	Errors   []string `json:"errors,omitempty"`
+	Warnings []string `json:"warnings,omitempty"`
+}
+
+func (s *Server) serveInfo(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	response := map[string]*responseCharm{}
+	for _, url := range r.Form["charms"] {
+		r := &responseCharm{}
+		response[url] = r
+		curl, err := charm.ParseURL(url)
+		var info *CharmInfo
+		if err == nil {
+			info, err = s.store.CharmInfo(curl)
+		}
+		if err == nil {
+			r.Sha256 = info.BundleSha256()
+			r.Revision = info.Revision()
+		} else {
+			r.Errors = append(r.Errors, err.Error())
+		}
+	}
+	data, err := json.Marshal(response)
+	if err == nil {
+		w.Header()["Content-Type"] = []string{"application/json"}
+		_, err = w.Write(data)
+	}
+	if err != nil {
+		log.Printf("can't write content: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+}
---
+++ store/server_test.go
@@ -0,0 +1,54 @@
+package store_test
+
+import (
+	"encoding/json"
+	. "launchpad.net/gocheck"
+	"launchpad.net/juju/go/charm"
+	"launchpad.net/juju/go/store"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+)
+
+func (s *StoreSuite) TestServerCharmInfo(c *C) {
+	u := charm.MustParseURL("cs:oneiric/wordpress")
+	pub, err := s.store.CharmPublisher([]*charm.URL{u}, "some-digest")
+	c.Assert(err, IsNil)
+	err = pub.Publish(&FakeCharmDir{})
+	c.Assert(err, IsNil)
+
+	server, err := store.NewServer(s.store)
+	c.Assert(err, IsNil)
+
+	req, err := http.NewRequest("GET", "/charm-info", nil)
+	c.Assert(err, IsNil)
+	req.Form = url.Values{"charms": []string{u.String()}}
+	rec := httptest.NewRecorder()
+	server.ServeHTTP(rec, req)
+
+	expected := map[string]interface{}{
+		u.String(): map[string]interface{}{
+			"revision": float64(0),
+			"sha256": fakeRevZeroSha,
+		},
+	}
+	obtained := map[string]interface{}{}
+	err = json.NewDecoder(rec.Body).Decode(&obtained)
+	c.Assert(err, IsNil)
+	c.Assert(obtained, DeepEquals, expected)
+
+	// Now check an error condition.
+	req.Form["charms"] = []string{"cs:bad"}
+	rec = httptest.NewRecorder()
+	server.ServeHTTP(rec, req)
+	expected = map[string]interface{}{
+		"cs:bad": map[string]interface{}{
+			"revision": float64(0),
+			"errors": []interface{}{`charm URL without series: "cs:bad"`},
+		},
+	}
+	obtained = map[string]interface{}{}
+	err = json.NewDecoder(rec.Body).Decode(&obtained)
+	c.Assert(err, IsNil)
+	c.Assert(obtained, DeepEquals, expected)
+}
