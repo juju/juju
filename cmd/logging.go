02719955b857ee0edf3dbf166a8cb9ee80fae8ce
William Reade <fwereade@gmail.com>
2012-04-25 15:52:43 +0200

    whoops, add files

---
+++ cmd/logging.go
@@ -0,0 +1,46 @@
+package cmd
+
+import (
+	"io"
+	"launchpad.net/gnuflag"
+	"launchpad.net/juju/go/log"
+	stdlog "log"
+	"os"
+)
+
+// Log supplies the necessary functionality for Commands that wish to set up
+// logging.
+type Log struct {
+	Path    string
+	Verbose bool
+	Debug   bool
+}
+
+// InitFlagSet sets appropriate flags on f.
+func (c *Log) InitFlagSet(f *gnuflag.FlagSet) {
+	f.StringVar(&c.Path, "log-file", "", "path to write log to")
+	f.BoolVar(&c.Verbose, "v", false, "if set, log additional messages")
+	f.BoolVar(&c.Verbose, "verbose", false, "if set, log additional messages")
+	f.BoolVar(&c.Debug, "debug", false, "if set, log debugging messages")
+}
+
+// Start starts logging via ctx.
+func (c *Log) Start(ctx *Context) (err error) {
+	log.Debug = c.Debug
+	var target io.Writer
+	if c.Path != "" {
+		path := ctx.AbsPath(c.Path)
+		target, err = os.OpenFile(path, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
+		if err != nil {
+			return
+		}
+	} else if c.Verbose || c.Debug {
+		target = ctx.Stderr
+	}
+	if target != nil {
+		log.Target = stdlog.New(target, "", stdlog.LstdFlags)
+	} else {
+		log.Target = nil
+	}
+	return
+}
---
+++ cmd/logging_test.go
@@ -0,0 +1,102 @@
+package cmd_test
+
+import (
+	"io/ioutil"
+	"launchpad.net/gnuflag"
+	. "launchpad.net/gocheck"
+	"launchpad.net/juju/go/cmd"
+	"launchpad.net/juju/go/log"
+	"path/filepath"
+)
+
+func saveLog() func() {
+	target, debug := log.Target, log.Debug
+	return func() {
+		log.Target, log.Debug = target, debug
+	}
+}
+
+type LogSuite struct{}
+
+var _ = Suite(&LogSuite{})
+
+func (s *LogSuite) TestInitFlagSet(c *C) {
+	l := &cmd.Log{}
+	f := gnuflag.NewFlagSet("", gnuflag.ContinueOnError)
+	l.InitFlagSet(f)
+
+	err := f.Parse(false, []string{})
+	c.Assert(err, IsNil)
+	c.Assert(l.Path, Equals, "")
+	c.Assert(l.Verbose, Equals, false)
+	c.Assert(l.Debug, Equals, false)
+
+	err = f.Parse(false, []string{"--log-file", "foo", "--verbose", "--debug"})
+	c.Assert(err, IsNil)
+	c.Assert(l.Path, Equals, "foo")
+	c.Assert(l.Verbose, Equals, true)
+	c.Assert(l.Debug, Equals, true)
+}
+
+func (s *LogSuite) TestStart(c *C) {
+	defer saveLog()()
+	for _, t := range []struct {
+		path    string
+		verbose bool
+		debug   bool
+		target  Checker
+	}{
+		{"", true, true, NotNil},
+		{"", true, false, NotNil},
+		{"", false, true, NotNil},
+		{"", false, false, IsNil},
+		{"foo", true, true, NotNil},
+		{"foo", true, false, NotNil},
+		{"foo", false, true, NotNil},
+		{"foo", false, false, NotNil},
+	} {
+		l := &cmd.Log{t.path, t.verbose, t.debug}
+		ctx := dummyContext(c)
+		err := l.Start(ctx)
+		c.Assert(err, IsNil)
+		c.Assert(log.Target, t.target)
+		c.Assert(log.Debug, Equals, t.debug)
+	}
+}
+
+func (s *LogSuite) TestStderr(c *C) {
+	defer saveLog()()
+	l := &cmd.Log{Verbose: true}
+	ctx := dummyContext(c)
+	err := l.Start(ctx)
+	c.Assert(err, IsNil)
+	log.Printf("hello")
+	c.Assert(str(ctx.Stderr), Matches, `.* JUJU hello\n`)
+}
+
+func (s *LogSuite) TestRelPathLog(c *C) {
+	defer saveLog()()
+	l := &cmd.Log{Path: "foo.log"}
+	ctx := dummyContext(c)
+	err := l.Start(ctx)
+	c.Assert(err, IsNil)
+	log.Printf("hello")
+	c.Assert(str(ctx.Stderr), Equals, "")
+	content, err := ioutil.ReadFile(filepath.Join(ctx.Dir, "foo.log"))
+	c.Assert(err, IsNil)
+	c.Assert(string(content), Matches, `.* JUJU hello\n`)
+}
+
+func (s *LogSuite) TestAbsPathLog(c *C) {
+	defer saveLog()()
+	path := filepath.Join(c.MkDir(), "foo.log")
+	l := &cmd.Log{Path: path}
+	ctx := dummyContext(c)
+	err := l.Start(ctx)
+	c.Assert(err, IsNil)
+	log.Printf("hello")
+	c.Assert(str(ctx.Stderr), Equals, "")
+	content, err := ioutil.ReadFile(path)
+	c.Assert(err, IsNil)
+	c.Assert(string(content), Matches, `.* JUJU hello\n`)
+}
