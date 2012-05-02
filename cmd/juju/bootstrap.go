1e6f821099ce6205fcdac8091702e082288e16d7
Roger Peppe <roger.peppe@canonical.com>
2012-05-02 13:53:26 +0100

    go fmt

--- cmd/juju/bootstrap.go
+++ cmd/juju/bootstrap.go
@@ -9,7 +9,7 @@ import (
 // BootstrapCommand is responsible for launching the first machine in a juju
 // environment, and setting up everything necessary to continue working.
 type BootstrapCommand struct {
-	EnvName string
+	EnvName     string
 	UploadTools bool
 }
 
