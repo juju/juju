e9eb64d5fae20985cc5a895f00ea122904034aed
Dave Cheney <david.cheney@canonical.com>
2012-05-09 04:44:22 +1000

    wip

--- cmd/jujud/provisioning.go
+++ cmd/jujud/provisioning.go
@@ -1,10 +1,21 @@
 package main
 
 import (
-	"fmt"
 	"launchpad.net/gnuflag"
 	"launchpad.net/juju/go/cmd"
+	"launchpad.net/juju/go/state"
+	"log"
 )
+
+type machine struct {
+	id string
+}
+
+func (m *machine) loop() {
+	for {
+		
+	}
+}
 
 // ProvisioningAgent is a cmd.Command responsible for running a provisioning agent.
 type ProvisioningAgent struct {
@@ -25,7 +36,27 @@ func (a *ProvisioningAgent) Init(f *gnuflag.FlagSet, args []string) error {
 	return a.Conf.checkArgs(f.Args())
 }
 
+func (a *ProvisioningAgent) openState() (*state.State, error) {
+	return state.Open(&a.Conf.StateInfo)
+}
+
 // Run runs a provisioning agent.
 func (a *ProvisioningAgent) Run(_ *cmd.Context) error {
-	return fmt.Errorf("MachineAgent.Run not implemented")
+	state, err := a.openState()
+	if err != nil {
+		return err
+	}
+	w := state.WatchMachine()
+	defer w.Stop()
+
+	for {
+		event := <- w.Changes
+		switch {
+		case len(event.Added) > 0:
+			log.Printf("Machines added: %v", event.Added)
+		case len(event.Deleted) > 0:
+			log.Printf("Machines deleted: %v", event.Deleted)
+		}
+	}
+	return nil
 }
