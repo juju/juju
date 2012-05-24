6ddf836b5430d056c278ee1da8ffcfed068001dd
Dave Cheney <david.cheney@canonical.com>
2012-05-25 09:06:37 +1000

    Simplify makeBucket logic

--- environs/ec2/storage.go
+++ environs/ec2/storage.go
@@ -13,7 +13,6 @@ import (
 // an ec2.bucket.
 type storage struct {
 	bucketMutex sync.Mutex
-	madeBucket  bool
 	bucket      *s3.Bucket
 }
 
@@ -26,11 +25,7 @@ func (s *storage) makeBucket() error {
 	defer s.bucketMutex.Unlock()
 	// try to make the bucket - PutBucket will succeed if the
 	// bucket already exists.
-	err := s.bucket.PutBucket(s3.Private)
-	if err == nil {
-		s.madeBucket = true
-	}
-	return err
+	return s.bucket.PutBucket(s3.Private)
 }
 
 func (s *storage) Put(file string, r io.Reader, length int64) error {
