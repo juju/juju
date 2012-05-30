849574b4047d400b4d26a29819b136ff0a8ca83a
Dave Cheney <david.cheney@canonical.com>
2012-05-30 16:01:22 +1000

    more func test

--- cmd/jujud/provisioning_test.go
+++ cmd/jujud/provisioning_test.go
@@ -11,7 +11,6 @@ import (
 	"launchpad.net/juju/go/environs/dummy"
 	"launchpad.net/juju/go/log"
 
-	"sync"
 	"time"
 )
 
@@ -78,55 +77,22 @@ func initProvisioningAgent() (*main.ProvisioningAgent, error) {
 	return c, initCmd(c, args)
 }
 
-func (s *ProvisioningSuite) TestStartInstance(c *C) {
-	a, err := initProvisioningAgent()
-	c.Assert(err, IsNil)
-
-	wg := new(sync.WaitGroup)
-	wg.Add(1)
-
+func runPA(c *C, a *main.ProvisioningAgent) chan bool {
+	done := make(chan bool)
 	go func() {
-		defer wg.Done()
-		err = a.Run(nil) // context is unused
+		defer close(done)
+		err := a.Run(nil) // context is unused
 		c.Assert(err, ErrorMatches, ".*ZooKeeper connection closed; ")
 	}()
-
-	op := make(chan dummy.Operation, 1)
-	dummy.Listen(op)
-
-	// place a new machine into the state
-	_, err = s.st.AddMachine()
-	c.Assert(err, IsNil)
-
-	// watch the PA create it
-	select {
-	case o := <-op:
-		c.Assert(o.Kind, Equals, dummy.OpStartInstance)
-	case <-time.After(3 * time.Second):
-		c.Fatalf("ProvisioningAgent did not action AddMachine after 3 second")
-	}
-
-	// close the PA's underlying state, shutting it down.
-	// this races with the PA writing data to the state,
-	// so pause a little.
-	<-time.After(2 * time.Second)
-	a.State.Close()
-
-	wg.Wait()
+	return done
 }
 
-func (s *ProvisioningSuite) TestStopInstance(c *C) {
+// Start and stop one machine, watch the PA.
+func (s *ProvisioningSuite) TestSimple(c *C) {
 	a, err := initProvisioningAgent()
 	c.Assert(err, IsNil)
 
-	wg := new(sync.WaitGroup)
-	wg.Add(1)
-
-	go func() {
-		defer wg.Done()
-		err = a.Run(nil) // context is unused
-		c.Assert(err, ErrorMatches, ".*ZooKeeper connection closed; ")
-	}()
+	done := runPA(c, a)
 
 	op := make(chan dummy.Operation, 1)
 	dummy.Listen(op)
@@ -160,5 +126,61 @@ func (s *ProvisioningSuite) TestStopInstance(c *C) {
 	<-time.After(2 * time.Second)
 	a.State.Close()
 
-	wg.Wait()
+	<-done // blocks until PA exits
+}
+
+// Start and stop lots machines, watch the PA.
+func (s *ProvisioningSuite) TestHard(c *C) {
+	var N = 200
+
+	a, err := initProvisioningAgent()
+	c.Assert(err, IsNil)
+
+	paDone := runPA(c, a)
+
+	op := make(chan dummy.Operation, 1)
+	dummy.Listen(op)
+
+	go func() {
+		for i := 0; i < N; i++ {
+			m, err := s.st.AddMachine()
+			<-time.After(20 * time.Millisecond)
+			c.Assert(err, IsNil)
+			c.Logf("added machine: %d", m.Id())
+			go func() {
+				err := s.st.RemoveMachine(m.Id())
+				c.Assert(err, IsNil)
+				c.Logf("removed machine: %d", m.Id())
+			}()
+		}
+	}()
+
+	var started, stopped int
+
+	for stopped < N {
+		select {
+		case o := <-op:
+			switch o.Kind {
+			case dummy.OpStartInstance:
+				started++
+			case dummy.OpStopInstances:
+				stopped++
+			default:
+				c.Fatalf("dummy reported unknown operation: %v", o)
+			}
+		case <-time.After(10 * time.Second):
+			c.Fatalf("PA stalled")
+		}
+	}
+
+	c.Assert(started, Equals, N)
+	c.Assert(stopped, Equals, N)
+
+	// close the PA's underlying state, shutting it down.
+	// this races with the PA writing data to the state,
+	// so pause a little.
+	<-time.After(2 * time.Second)
+	a.State.Close()
+
+	<-paDone // will block until the PA exits
 }
