a203e3038027dbc27b4ddcf18171fe0c983d7b21
Aram Hăvărneanu <aram@canonical.com>
2012-06-18 23:48:11 +0200

    mstate: added machine.go, forgot to bzr add in in previous commits.

---
+++ mstate/machine.go
@@ -0,0 +1,37 @@
+package mstate
+
+import "launchpad.net/mgo/bson"
+
+// Machine represents the state of a machine.
+type Machine struct {
+	st *State
+	id bson.ObjectId
+}
+
+// machineDoc represents the internal state of a machine in MongoDB.
+type machineDoc struct {
+	Id         bson.ObjectId `bson:"_id"`
+	InstanceId string
+}
+
+// Id returns the machine id.
+func (m *Machine) Id() string {
+	return m.id.Hex()
+}
+
+// InstanceId returns the provider specific machine id for this machine.
+func (m *Machine) InstanceId() (iid string, err error) {
+	mdoc := &machineDoc{}
+	err = m.st.machines.Find(bson.D{{"_id", m.id}}).One(mdoc)
+	if err != nil {
+		return
+	}
+	iid = mdoc.InstanceId
+	return
+}
+
+// SetInstanceId sets the provider specific machine id for this machine.
+func (m *Machine) SetInstanceId(id string) (err error) {
+	err = m.st.machines.Update(bson.D{{"_id", m.id}}, bson.D{{"instanceid", id}})
+	return
+}
