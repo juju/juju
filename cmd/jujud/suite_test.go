626406c542cddeaca89b21956cc7627a52cc5a82
Roger Peppe <roger.peppe@canonical.com>
2012-06-19 17:51:04 +0100

    cmd/jujud: add zkfixture

---
+++ cmd/jujud/suite_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	. "launchpad.net/gocheck"
+	"launchpad.net/gozk/zookeeper"
+	"launchpad.net/juju-core/juju/state"
+	"launchpad.net/juju-core/juju/testing"
+	stdtesting "testing"
+)
+
+var zkAddr string
+
+func TestPackage(t *stdtesting.T) {
+	srv := testing.StartZkServer()
+	defer srv.Destroy()
+	var err error
+	zkAddr, err = srv.Addr()
+	if err != nil {
+		t.Fatalf("could not get ZooKeeper server address: %v", err)
+	}
+	TestingT(t)
+}
+
+type zkFixture struct {
+	zkConn *zookeeper.Conn
+	zkInfo *state.Info
+}
+
+func (f *zkFixture) setup(c *C) {
+	zk, session, err := zookeeper.Dial(zkAddr, 15e9)
+	c.Assert(err, IsNil)
+	event := <-session
+	c.Assert(event.Ok(), Equals, true)
+	c.Assert(event.Type, Equals, zookeeper.EVENT_SESSION)
+	c.Assert(event.State, Equals, zookeeper.STATE_CONNECTED)
+
+	f.zkConn = zk
+	f.zkInfo = &state.Info{
+		Addrs: []string{zkAddr},
+	}
+}
+
+func (f *zkFixture) tearDown() {
+	testing.ZkRemoveTree(f.zkConn, "/")
+	f.zkConn.Close()
+}
