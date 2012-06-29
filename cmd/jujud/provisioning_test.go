331f37656a06597f49dff9ae6572a83885417c14
Dave Cheney <david.cheney@canonical.com>
2012-06-29 16:55:01 +1000

    tweaks

--- cmd/jujud/provisioning_test.go
+++ cmd/jujud/provisioning_test.go
@@ -6,16 +6,11 @@ import (
 	. "launchpad.net/gocheck"
 	"launchpad.net/juju-core/cmd"
 	"launchpad.net/juju-core/environs"
-	"launchpad.net/juju-core/environs/dummy"
-	"launchpad.net/juju-core/state"
 	"launchpad.net/juju-core/testing"
 )
 
 type ProvisioningSuite struct {
 	logging testing.LoggingSuite
-	zkSuite
-	st *state.State
-	op <-chan dummy.Operation
 }
 
 var _ = Suite(&ProvisioningSuite{})
@@ -27,35 +22,9 @@ var veryShortAttempt = environs.AttemptStrategy{
 
 func (s *ProvisioningSuite) SetUpTest(c *C) {
 	s.logging.SetUpTest(c)
-
-	// Create the operations channel with more than enough space
-	// for those tests that don't listen on it.
-	op := make(chan dummy.Operation, 500)
-	dummy.Listen(op)
-	s.op = op
-
-	env, err := environs.NewEnviron(map[string]interface{}{
-		"type":      "dummy",
-		"zookeeper": true,
-		"name":      "testing",
-	})
-	c.Assert(err, IsNil)
-	err = env.Bootstrap(false)
-	c.Assert(err, IsNil)
-
-	s.zkSuite.SetUpTest(c)
-	s.st, err = state.Open(s.zkInfo)
-	c.Assert(err, IsNil)
-
-	// Make sure that zkInfo holds exactly the info we're passing to
-	// service.NewProvisioner,
-	s.zkInfo, err = env.StateInfo()
-	c.Assert(err, IsNil)
 }
 
 func (s *ProvisioningSuite) TearDownTest(c *C) {
-	dummy.Reset()
-	s.zkSuite.TearDownTest()
 	s.logging.TearDownTest(c)
 }
 
