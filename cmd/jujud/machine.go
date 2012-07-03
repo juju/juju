b97b03bb7ed656cac011bfb0ca25c42153933e64
Dave Cheney <david.cheney@canonical.com>
2012-07-04 08:10:03 +1000

    redo branch after testing change

--- cmd/jujud/machine.go
+++ cmd/jujud/machine.go
@@ -4,16 +4,7 @@ import (
 	"fmt"
 	"launchpad.net/gnuflag"
 	"launchpad.net/juju-core/cmd"
-	"launchpad.net/juju-core/container"
-	"launchpad.net/juju-core/log"
-	"launchpad.net/juju-core/state"
-	"launchpad.net/juju-core/state/watcher"
-	"launchpad.net/tomb"
 )
-
-// simpleContainer allows tests to hook into the container
-// deployment logic.
-var simpleContainer = container.Simple
 
 // MachineAgent is a cmd.Command responsible for running a machine agent.
 type MachineAgent struct {
@@ -46,77 +37,5 @@ func (a *MachineAgent) Run(_ *cmd.Context) error {
 	if err != nil {
 		return err
 	}
-	go m.loop()
 	return m.Wait()
-}
-
-// NewMachiner starts a machine agent running.
-// The Machiner dies when it encounters an error.
-func NewMachiner(info *state.Info, machineId int) (m *Machiner, err error) {
-	m = new(Machiner)
-	m.st, err = state.Open(info)
-	if err != nil {
-		return nil, err
-	}
-	m.machine, err = m.st.Machine(machineId)
-	if err != nil {
-		return nil, err
-	}
-	go m.loop()
-	return m, nil
-}
-
-// Machiner represents a running machine agent.
-type Machiner struct {
-	tomb    tomb.Tomb
-	st      *state.State
-	machine *state.Machine
-}
-
-func (m *Machiner) loop() {
-	defer m.tomb.Done()
-	defer m.st.Close()
-	w := m.machine.WatchUnits()
-	defer watcher.Stop(w, &m.tomb)
-
-	// TODO read initial units, check if they're running
-	// and restart them if not. Also track units so
-	// that we don't deploy units that are already running.
-	for {
-		select {
-		case <-m.tomb.Dying():
-			return
-		case change, ok := <-w.Changes():
-			if !ok {
-				m.tomb.Kill(watcher.MustErr(w))
-				return
-			}
-			for _, u := range change.Removed {
-				if u.IsPrincipal() {
-					if err := simpleContainer.Destroy(u); err != nil {
-						log.Printf("cannot destroy unit %s: %v", u.Name(), err)
-					}
-				}
-			}
-			for _, u := range change.Added {
-				if u.IsPrincipal() {
-					if err := simpleContainer.Deploy(u); err != nil {
-						// TODO put unit into a queue to retry the deploy.
-						log.Printf("cannot deploy unit %s: %v", u.Name(), err)
-					}
-				}
-			}
-		}
-	}
-}
-
-// Wait waits until the Machiner has died, and returns the error encountered.
-func (m *Machiner) Wait() error {
-	return m.tomb.Wait()
-}
-
-// Stop terminates the Machiner and returns any error that it encountered.
-func (m *Machiner) Stop() error {
-	m.tomb.Kill(nil)
-	return m.tomb.Wait()
 }
---
+++ cmd/jujud/machiner_test.go
@@ -0,0 +1,156 @@
+package main
+
+import (
+	. "launchpad.net/gocheck"
+	"launchpad.net/juju-core/container"
+	"launchpad.net/juju-core/state"
+	"launchpad.net/juju-core/state/testing"
+	coretesting "launchpad.net/juju-core/testing"
+	"time"
+)
+
+type MachinerSuite struct {
+	coretesting.LoggingSuite
+	testing.StateSuite
+}
+
+var _ = Suite(&MachinerSuite{})
+
+func (s *MachinerSuite) SetUpTest(c *C) {
+	s.LoggingSuite.SetUpTest(c)
+	s.StateSuite.SetUpTest(c)
+}
+
+func (s *MachinerSuite) TearDownTest(c *C) {
+	s.StateSuite.TearDownTest(c)
+	s.LoggingSuite.TearDownTest(c)
+}
+
+func (s *MachinerSuite) TestMachinerStartStop(c *C) {
+	m, err := s.State.AddMachine()
+	c.Assert(err, IsNil)
+
+	p, err := NewMachiner(s.StateInfo(c), m.Id())
+	c.Assert(err, IsNil)
+	c.Assert(p.Stop(), IsNil)
+}
+
+func (s *MachinerSuite) TestMachinerDeployDestroy(c *C) {
+	dcontainer := newDummyContainer()
+	backup := container.Simple
+	container.Simple = dcontainer
+	defer func() {
+		container.Simple = backup
+	}()
+
+	dummyCharm := s.AddTestingCharm(c, "dummy")
+	loggingCharm := s.AddTestingCharm(c, "logging")
+
+	d0, err := s.State.AddService("d0", dummyCharm)
+	c.Assert(err, IsNil)
+	d1, err := s.State.AddService("d1", dummyCharm)
+	c.Assert(err, IsNil)
+	sub0, err := s.State.AddService("sub0", loggingCharm)
+	c.Assert(err, IsNil)
+
+	// Add one unit to start with.
+	ud0, err := d0.AddUnit()
+	c.Assert(err, IsNil)
+	_, err = sub0.AddUnitSubordinateTo(ud0)
+	c.Assert(err, IsNil)
+
+	ud1, err := d1.AddUnit()
+	c.Assert(err, IsNil)
+
+	m0, err := s.State.AddMachine()
+	c.Assert(err, IsNil)
+
+	m1, err := s.State.AddMachine()
+	c.Assert(err, IsNil)
+
+	err = ud0.AssignToMachine(m0)
+	c.Assert(err, IsNil)
+
+	machiner, err := NewMachiner(s.StateInfo(c), m0.Id())
+	c.Assert(err, IsNil)
+
+	tests := []struct {
+		change  func()
+		actions []string
+	}{
+		{
+			func() {},
+			[]string{"+d0/0"},
+		}, {
+			func() {
+				err := ud1.AssignToMachine(m0)
+				c.Assert(err, IsNil)
+			},
+			[]string{"+d1/0"},
+		}, {
+			func() {
+				err := ud0.UnassignFromMachine()
+				c.Assert(err, IsNil)
+			},
+			[]string{"-d0/0"},
+		}, {
+			func() {
+				err := ud1.UnassignFromMachine()
+				c.Assert(err, IsNil)
+			},
+			[]string{"-d1/0"},
+		}, {
+			func() {
+				err := ud0.AssignToMachine(m1)
+				c.Assert(err, IsNil)
+			},
+			nil,
+		},
+	}
+	for i, t := range tests {
+		c.Logf("test %d", i)
+		t.change()
+		for _, a := range t.actions {
+			dcontainer.checkAction(c, a)
+		}
+		dcontainer.checkAction(c, "")
+	}
+
+	err = machiner.Stop()
+	c.Assert(err, IsNil)
+}
+
+type dummyContainer struct {
+	action chan string
+}
+
+func newDummyContainer() *dummyContainer {
+	return &dummyContainer{
+		make(chan string, 5),
+	}
+}
+
+func (d *dummyContainer) Deploy(u *state.Unit) error {
+	d.action <- "+" + u.Name()
+	return nil
+}
+
+func (d *dummyContainer) Destroy(u *state.Unit) error {
+	d.action <- "-" + u.Name()
+	return nil
+}
+
+func (d *dummyContainer) checkAction(c *C, action string) {
+	timeout := 500 * time.Millisecond
+	if action == "" {
+		timeout = 200 * time.Millisecond
+	}
+	select {
+	case a := <-d.action:
+		c.Assert(a, Equals, action)
+	case <-time.After(timeout):
+		if action != "" {
+			c.Fatalf("expected action %v got nothing", action)
+		}
+	}
+}
