064aad7f4a4a1ec7a7329c5c3091f08ebcb11631
Roger Peppe <roger.peppe@canonical.com>
2012-07-18 16:18:40 +0100

    charm: factor out InferURL from InferRepository

--- charm/charm.go
+++ charm/charm.go
@@ -27,23 +27,19 @@ func Read(path string) (Charm, error) {
 	return ReadBundle(path)
 }
 
-// InferRepository returns a charm repository and URL inferred from the provided
-// parameters. charmAlias may hold an exact charm URL, or an alias in a
-// format supported by InferURL.
-func InferRepository(charmAlias, defaultSeries, localRepoPath string) (repo Repository, curl *URL, err error) {
-	if curl, err = InferURL(charmAlias, defaultSeries); err != nil {
-		return
-	}
+// InferRepository returns a charm repository inferred from
+// the provided URL.
+func InferRepository(curl *URL, localRepoPath string) (repo Repository, err error) {
 	switch curl.Schema {
 	case "cs":
 		repo = Store()
 	case "local":
 		if localRepoPath == "" {
-			return nil, nil, errors.New("path to local repository not specified")
+			return nil, errors.New("path to local repository not specified")
 		}
 		repo = &LocalRepository{localRepoPath}
 	default:
-		panic(fmt.Errorf("unknown schema for charm URL %q", curl))
+		return nil, fmt.Errorf("unknown schema for charm URL %q", curl)
 	}
 	return
 }
