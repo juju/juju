381173ea4b310ce27a79e095150887fac83db1b6
Dave Cheney <david.cheney@canonical.com>
2012-07-25 01:11:56 +1000

    wip

--- cmd/juju/status.go
+++ cmd/juju/status.go
@@ -55,17 +55,18 @@ func (c *StatusCommand) Run(ctx *cmd.Context) error {
 		return err
 	}
 
-	result := map[string]interface{}{
-		"machines": nil, // filled out below
-		"services": make(map[string]interface{}),
-	}
+	services, err := fetchAllServices(state)
+	if err != nil { return err }
+
+	result := make(map[string]interface{})
 
 	result["machines"], err = processMachines(machines, instances)
 	if err != nil {
 		return err
 	}
 
-	// TODO(dfc) process services and units
+	result["services"], err = processServices(services, machines)
+	if err != nil { return err }
 
 	if c.out.Name() == "json" {
 		return c.out.Write(ctx, jsonify(result))
@@ -88,7 +89,7 @@ func fetchAllInstances(env environs.Environ) (map[string]environs.Instance, erro
 }
 
 // fetchAllMachines returns a map[int]*state.Machine representing
-// a mapping of machine ids to their respective machine.
+// a mapping of machine ids to machines.
 func fetchAllMachines(st *state.State) (map[int]*state.Machine, error) {
 	v := make(map[int]*state.Machine)
 	machines, err := st.AllMachines()
@@ -97,6 +98,20 @@ func fetchAllMachines(st *state.State) (map[int]*state.Machine, error) {
 	}
 	for _, m := range machines {
 		v[m.Id()] = m
+	}
+	return v, nil
+}
+
+// fetchAllServices returns a map[int]*state.Service representing
+// a mapping of service names to services.
+func fetchAllServices(st *state.State) (map[string]*state.Service, error) {
+	v := make(map[string]*state.Service)
+	services, err := st.AllServices()
+	if err != nil {
+		return nil, err
+	}
+	for _, s := range services {
+		v[m.Id()] = s
 	}
 	return v, nil
 }
@@ -152,6 +167,36 @@ func processMachine(machine *state.Machine, instance environs.Instance) (map[str
 	// TODO(dfc) unit-status
 	return r, nil
 }
+
+// processServices gathers information about services.
+func processMachines(machines map[int]*state.Machine, instances map[string]environs.Instance) (map[int]interface{}, error) {
+	r := make(map[int]interface{})
+	for _, m := range machines {
+		instid, err := m.InstanceId()
+		if err, ok := err.(*state.NoInstanceIdError); ok {
+			r[m.Id()] = map[string]interface{}{
+				"instance-id": "pending",
+			}
+		} else if err != nil {
+			return nil, err
+		} else {
+			instance, ok := instances[instid]
+			if !ok {
+				// Double plus ungood. There is an instance id recorded for this machine in the state,
+				// yet the environ cannot find that id. 
+				return nil, fmt.Errorf("instance %s for machine %d not found", instid, m.Id())
+			}
+			machine, err := processMachine(m, instance)
+			if err != nil {
+				return nil, err
+			}
+			r[m.Id()] = machine
+		}
+	}
+	return r, nil
+}
+
+func processMachine(machine *state.Machine, instance environs.Instance) (map[string]interface{}, error) {
 
 // jsonify converts the keys of the machines map into their string
 // equivalents for compatibility with encoding/json.
