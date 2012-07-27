0fd2fdf2de3df0d07dc313a6c0ff6cc7bb404995
Frank Mueller <themue@gmail.com>
2012-07-27 11:33:00 +0100

    provisioning: added Stop() and test

--- cmd/jujud/provisioning.go
+++ cmd/jujud/provisioning.go
@@ -9,6 +9,7 @@ import (
 	"launchpad.net/juju-core/state"
 	"launchpad.net/juju-core/worker/firewaller"
 	"launchpad.net/juju-core/worker/provisioner"
+	"launchpad.net/tomb"
 
 	// register providers
 	_ "launchpad.net/juju-core/environs/ec2"
@@ -18,7 +19,10 @@ var retryDuration = 10 * time.Second
 
 // ProvisioningAgent is a cmd.Command responsible for running a provisioning agent.
 type ProvisioningAgent struct {
-	Conf AgentConf
+	tomb        tomb.Tomb
+	Conf        AgentConf
+	provisioner *provisioner.Provisioner
+	firewaller  *firewaller.Firewaller
 }
 
 // Info returns usage information for the command.
@@ -35,53 +39,71 @@ func (a *ProvisioningAgent) Init(f *gnuflag.FlagSet, args []string) error {
 	return a.Conf.checkArgs(f.Args())
 }
 
+// Stop stops the provisionig agent by stopping the provisioner
+// and the firewaller.
+func (a *ProvisioningAgent) Stop() error {
+	a.tomb.Kill(nil)
+	return a.tomb.Wait()
+}
+
 // Run run a provisioning agent with a provisioner and a firewaller.
 // If either fails, both will be shutdown and restarted.
-func (a *ProvisioningAgent) Run(_ *cmd.Context) error {
+func (a *ProvisioningAgent) Run(_ *cmd.Context) (err error) {
+	defer a.tomb.Done()
 	for {
-		if err := a.runOnce(); err != nil {
-			time.Sleep(retryDuration)
-			log.Printf("restarting provisioner and firewaller after error: %v", err)
+		err = a.runOnce()
+		if err == nil {
+			// Stop requested by user.
+			return nil
 		}
+		time.Sleep(retryDuration)
+		log.Printf("restarting provisioner and firewaller after error: %v", err)
 	}
 	panic("unreachable")
 }
 
 // runOnce runs a provisioner and firewaller once.
-func (a *ProvisioningAgent) runOnce() (err error) {
+func (a *ProvisioningAgent) runOnce() (stoperr error) {
 	st, err := state.Open(&a.Conf.StateInfo)
 	if err != nil {
 		return err
 	}
+	log.Debugf("provisioning: opened state")
 	defer func() {
 		if e := st.Close(); err != nil {
 			err = e
 		}
+		log.Debugf("provisioning: closed state")
 	}()
 
-	p, err := provisioner.NewProvisioner(st)
+	a.provisioner, err = provisioner.NewProvisioner(st)
 	if err != nil {
 		return err
 	}
+	log.Debugf("provisioning: started provisioner")
 	defer func() {
-		if e := p.Stop(); err != nil {
+		if e := a.provisioner.Stop(); err != nil {
 			err = e
 		}
+		log.Debugf("provisioning: stopped provisioner")
 	}()
 
-	fw, err := firewaller.NewFirewaller(st)
+	a.firewaller, err = firewaller.NewFirewaller(st)
 	if err != nil {
 		return err
 	}
+	log.Debugf("provisioning: started firewaller")
 	defer func() {
-		if e := fw.Stop(); err != nil {
+		if e := a.firewaller.Stop(); err != nil {
 			err = e
 		}
+		log.Debugf("provisioning: stopped firewaller")
 	}()
 
 	select {
-	case <-p.Dying():
-	case <-fw.Dying():
+	case <-a.tomb.Dying():
+	case <-a.provisioner.Dying():
+	case <-a.firewaller.Dying():
 	}
 
 	return
--- cmd/jujud/provisioning_test.go
+++ cmd/jujud/provisioning_test.go
@@ -1,16 +1,107 @@
 package main
 
 import (
+	"fmt"
 	. "launchpad.net/gocheck"
 	"launchpad.net/juju-core/cmd"
-	"launchpad.net/juju-core/testing"
+	"launchpad.net/juju-core/environs"
+	"launchpad.net/juju-core/environs/dummy"
+	"launchpad.net/juju-core/log"
+	"launchpad.net/juju-core/state/testing"
+	coretesting "launchpad.net/juju-core/testing"
+	"sort"
+	"strings"
+	"time"
 )
 
+// hookLogger allows the grabbing of log statements
+// to compare them inside the tests.
+type hookLogger struct {
+	event     chan string
+	oldTarget log.Logger
+}
+
+var logHook *hookLogger
+
+const prefix = "JUJU:DEBUG provisioning: "
+
+func (h *hookLogger) Output(calldepth int, s string) error {
+	err := h.oldTarget.Output(calldepth, s)
+	if strings.HasPrefix(s, prefix) {
+		h.event <- s[len(prefix):]
+	}
+	return err
+}
+
+func setUpLogHook() {
+	logHook = &hookLogger{
+		event:     make(chan string, 100),
+		oldTarget: log.Target,
+	}
+	log.Target = logHook
+}
+
+func tearDownLogHook() {
+	log.Target = logHook.oldTarget
+}
+
+// assertEvents asserts that the expected events are received from
+// the firewaller, in no particular order.
+func assertEvents(c *C, expect []string) {
+	var got []string
+	for _ = range expect {
+		select {
+		case e := <-logHook.event:
+			got = append(got, e)
+		case <-time.After(500 * time.Millisecond):
+			c.Fatalf("expected %q; timed out, got %q", expect, got)
+		}
+	}
+	select {
+	case e := <-logHook.event:
+		got = append(got, e)
+		c.Fatalf("expected %q; too many events %q ", expect, got)
+	case <-time.After(100 * time.Millisecond):
+	}
+	sort.Strings(expect)
+	sort.Strings(got)
+	c.Assert(got, DeepEquals, expect)
+}
+
 type ProvisioningSuite struct {
-	testing.LoggingSuite
+	coretesting.LoggingSuite
+	testing.StateSuite
+	op <-chan dummy.Operation
 }
 
 var _ = Suite(&ProvisioningSuite{})
+
+func (s *ProvisioningSuite) SetUpTest(c *C) {
+	s.LoggingSuite.SetUpTest(c)
+
+	// Create the operations channel with more than enough space
+	// for those tests that don't listen on it.
+	op := make(chan dummy.Operation, 500)
+	dummy.Listen(op)
+	s.op = op
+
+	env, err := environs.NewFromAttrs(map[string]interface{}{
+		"name":            "testing",
+		"type":            "dummy",
+		"zookeeper":       true,
+		"authorized-keys": "i-am-a-key",
+	})
+	c.Assert(err, IsNil)
+	err = env.Bootstrap(false)
+	c.Assert(err, IsNil)
+
+	s.StateSuite.SetUpTest(c)
+}
+
+func (s *ProvisioningSuite) TearDownTest(c *C) {
+	dummy.Reset()
+	s.LoggingSuite.TearDownTest(c)
+}
 
 func (s *ProvisioningSuite) TestParseSuccess(c *C) {
 	create := func() (cmd.Command, *AgentConf) {
@@ -24,4 +115,36 @@ func (s *ProvisioningSuite) TestParseUnknown(c *C) {
 	a := &ProvisioningAgent{}
 	err := ParseAgentCommand(a, []string{"nincompoops"})
 	c.Assert(err, ErrorMatches, `unrecognized args: \["nincompoops"\]`)
+}
+
+func (s *ProvisioningSuite) TestRunStop(c *C) {
+	a := &ProvisioningAgent{
+		Conf: AgentConf{
+			JujuDir:   "/var/lib/juju",
+			StateInfo: *s.StateInfo(c),
+		},
+	}
+
+	setUpLogHook()
+	defer tearDownLogHook()
+
+	go func() {
+		err := a.Run(nil)
+		c.Assert(err, IsNil)
+	}()
+
+	assertEvents(c, []string{
+		fmt.Sprint("opened state"),
+		fmt.Sprint("started provisioner"),
+		fmt.Sprint("started firewaller"),
+	})
+
+	err := a.Stop()
+	c.Assert(err, IsNil)
+
+	assertEvents(c, []string{
+		fmt.Sprint("stopped firewaller"),
+		fmt.Sprint("stopped provisioner"),
+		fmt.Sprint("closed state"),
+	})
 }
