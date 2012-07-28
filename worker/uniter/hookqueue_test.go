f289f02459ab7a8806b9e74b76558ce4556cdc98
William Reade <fwereade@gmail.com>
2012-07-29 00:10:18 +0200

    added MustValidate; more tests

--- worker/uniter/hookqueue_test.go
+++ worker/uniter/hookqueue_test.go
@@ -36,131 +36,131 @@ var hookQueueTests = []hookQueueTest{
 		send{nil, nil},
 	), fullTest(
 		// Joined and changed are both run when unit is first detected.
-		send{msi{"u0": 0}, nil},
-		expect{"joined", "u0", 0, msi{"u0": 0}},
-		expect{"changed", "u0", 0, msi{"u0": 0}},
+		send{msi{"u/0": 0}, nil},
+		expect{"joined", "u/0", 0, msi{"u/0": 0}},
+		expect{"changed", "u/0", 0, msi{"u/0": 0}},
 	), fullTest(
 		// Automatic changed is run with latest settings.
-		send{msi{"u0": 0}, nil},
-		expect{"joined", "u0", 0, msi{"u0": 0}},
-		send{msi{"u0": 7}, nil},
-		expect{"changed", "u0", 7, msi{"u0": 7}},
+		send{msi{"u/0": 0}, nil},
+		expect{"joined", "u/0", 0, msi{"u/0": 0}},
+		send{msi{"u/0": 7}, nil},
+		expect{"changed", "u/0", 7, msi{"u/0": 7}},
 	), fullTest(
 		// Joined is also run with latest settings.
-		send{msi{"u0": 0}, nil},
-		send{msi{"u0": 7}, nil},
-		expect{"joined", "u0", 7, msi{"u0": 7}},
-		expect{"changed", "u0", 7, msi{"u0": 7}},
+		send{msi{"u/0": 0}, nil},
+		send{msi{"u/0": 7}, nil},
+		expect{"joined", "u/0", 7, msi{"u/0": 7}},
+		expect{"changed", "u/0", 7, msi{"u/0": 7}},
 	), fullTest(
 		// Nothing happens if a unit departs before its joined is run.
-		send{msi{"u0": 0}, nil},
-		send{msi{"u0": 7}, nil},
-		send{nil, []string{"u0"}},
+		send{msi{"u/0": 0}, nil},
+		send{msi{"u/0": 7}, nil},
+		send{nil, []string{"u/0"}},
 	), fullTest(
 		// A changed is run after a joined, even if a departed is known.
-		send{msi{"u0": 0}, nil},
-		expect{"joined", "u0", 0, msi{"u0": 0}},
-		send{nil, []string{"u0"}},
-		expect{"changed", "u0", 0, msi{"u0": 0}},
-		expect{"departed", "u0", 0, nil},
+		send{msi{"u/0": 0}, nil},
+		expect{"joined", "u/0", 0, msi{"u/0": 0}},
+		send{nil, []string{"u/0"}},
+		expect{"changed", "u/0", 0, msi{"u/0": 0}},
+		expect{"departed", "u/0", 0, nil},
 	), fullTest(
 		// A departed replaces a changed.
-		send{msi{"u0": 0}, nil},
+		send{msi{"u/0": 0}, nil},
 		advance{2},
-		send{msi{"u0": 7}, nil},
-		send{nil, []string{"u0"}},
-		expect{"departed", "u0", 7, nil},
+		send{msi{"u/0": 7}, nil},
+		send{nil, []string{"u/0"}},
+		expect{"departed", "u/0", 7, nil},
 	), fullTest(
 		// Changed events are ignored if the version has not changed.
-		send{msi{"u0": 0}, nil},
+		send{msi{"u/0": 0}, nil},
 		advance{2},
-		send{msi{"u0": 0}, nil},
+		send{msi{"u/0": 0}, nil},
 	), fullTest(
 		// Multiple changed events are compacted into one.
-		send{msi{"u0": 0}, nil},
+		send{msi{"u/0": 0}, nil},
 		advance{2},
-		send{msi{"u0": 3}, nil},
-		send{msi{"u0": 7}, nil},
-		send{msi{"u0": 79}, nil},
-		expect{"changed", "u0", 79, msi{"u0": 79}},
+		send{msi{"u/0": 3}, nil},
+		send{msi{"u/0": 7}, nil},
+		send{msi{"u/0": 79}, nil},
+		expect{"changed", "u/0", 79, msi{"u/0": 79}},
 	), fullTest(
 		// Multiple changed events are elided.
-		send{msi{"u0": 0}, nil},
+		send{msi{"u/0": 0}, nil},
 		advance{2},
-		send{msi{"u0": 3}, nil},
-		send{msi{"u0": 7}, nil},
-		send{msi{"u0": 79}, nil},
-		expect{"changed", "u0", 79, msi{"u0": 79}},
+		send{msi{"u/0": 3}, nil},
+		send{msi{"u/0": 7}, nil},
+		send{msi{"u/0": 79}, nil},
+		expect{"changed", "u/0", 79, msi{"u/0": 79}},
 	), fullTest(
 		// Latest hooks are run in the original unit order.
-		send{msi{"u0": 0, "u1": 1}, nil},
+		send{msi{"u/0": 0, "u/1": 1}, nil},
 		advance{4},
-		send{msi{"u0": 3}, nil},
-		send{msi{"u1": 7}, nil},
-		send{nil, []string{"u0"}},
-		expect{"departed", "u0", 3, msi{"u1": 7}},
-		expect{"changed", "u1", 7, msi{"u1": 7}},
+		send{msi{"u/0": 3}, nil},
+		send{msi{"u/1": 7}, nil},
+		send{nil, []string{"u/0"}},
+		expect{"departed", "u/0", 3, msi{"u/1": 7}},
+		expect{"changed", "u/1", 7, msi{"u/1": 7}},
 	), fullTest(
 		// Test everything we can think of at the same time.
-		send{msi{"u0": 0, "u1": 0, "u2": 0, "u3": 0, "u4": 0}, nil},
+		send{msi{"u/0": 0, "u/1": 0, "u/2": 0, "u/3": 0, "u/4": 0}, nil},
 		advance{6},
-		// u0, u1, u2 are now up to date; u3, u4 are untouched.
-		send{msi{"u0": 1}, nil},
-		send{msi{"u1": 1, "u2": 1, "u3": 1, "u5": 0}, []string{"u0", "u4"}},
-		send{msi{"u3": 2}, nil},
-		// - Finish off the rest of the initial state, ignoring u4, but using
+		// u/0, u/1, u/2 are now up to date; u/3, u/4 are untouched.
+		send{msi{"u/0": 1}, nil},
+		send{msi{"u/1": 1, "u/2": 1, "u/3": 1, "u/5": 0}, []string{"u/0", "u/4"}},
+		send{msi{"u/3": 2}, nil},
+		// - Finish off the rest of the initial state, ignoring u/4, but using
 		// the latest known settings.
-		expect{"joined", "u3", 2, msi{"u0": 1, "u1": 1, "u2": 1, "u3": 2}},
-		expect{"changed", "u3", 2, msi{"u0": 1, "u1": 1, "u2": 1, "u3": 2}},
-		// - u0 was queued for change by the first RUC, but this change is
+		expect{"joined", "u/3", 2, msi{"u/0": 1, "u/1": 1, "u/2": 1, "u/3": 2}},
+		expect{"changed", "u/3", 2, msi{"u/0": 1, "u/1": 1, "u/2": 1, "u/3": 2}},
+		// - u/0 was queued for change by the first RUC, but this change is
 		// no longer relevant; it's departed in the second RUC, so we run
 		// that hook instead.
-		expect{"departed", "u0", 1, msi{"u1": 1, "u2": 1, "u3": 2}},
-		// - Handle the remaining changes in the second RUC, still ignoring u4.
-		// We do run new changed hooks for u1 and u2, because the latest settings
+		expect{"departed", "u/0", 1, msi{"u/1": 1, "u/2": 1, "u/3": 2}},
+		// - Handle the remaining changes in the second RUC, still ignoring u/4.
+		// We do run new changed hooks for u/1 and u/2, because the latest settings
 		// are newer than those used in their original changed events.
-		expect{"changed", "u1", 1, msi{"u1": 1, "u2": 1, "u3": 2}},
-		expect{"changed", "u2", 1, msi{"u1": 1, "u2": 1, "u3": 2}},
-		expect{"joined", "u5", 0, msi{"u1": 1, "u2": 1, "u3": 2, "u5": 0}},
-		expect{"changed", "u5", 0, msi{"u1": 1, "u2": 1, "u3": 2, "u5": 0}},
-		// - Ignore the third RUC, because the original joined/changed on u3
+		expect{"changed", "u/1", 1, msi{"u/1": 1, "u/2": 1, "u/3": 2}},
+		expect{"changed", "u/2", 1, msi{"u/1": 1, "u/2": 1, "u/3": 2}},
+		expect{"joined", "u/5", 0, msi{"u/1": 1, "u/2": 1, "u/3": 2, "u/5": 0}},
+		expect{"changed", "u/5", 0, msi{"u/1": 1, "u/2": 1, "u/3": 2, "u/5": 0}},
+		// - Ignore the third RUC, because the original joined/changed on u/3
 		// was executed after we got the latest settings version.
 	), reconcileTest(
 		// Check that matching settings versions cause no changes.
-		msi{"u0": 0}, "",
-		send{msi{"u0": 0}, nil},
+		msi{"u/0": 0}, "",
+		send{msi{"u/0": 0}, nil},
 	), reconcileTest(
 		// Check that new settings versions cause appropriate changes.
-		msi{"u0": 0}, "",
-		send{msi{"u0": 1}, nil},
-		expect{"changed", "u0", 1, msi{"u0": 1}},
+		msi{"u/0": 0}, "",
+		send{msi{"u/0": 1}, nil},
+		expect{"changed", "u/0", 1, msi{"u/0": 1}},
 	), reconcileTest(
 		// Check that a just-joined unit gets its changed hook run first.
-		msi{"u0": 0}, "u0",
-		send{msi{"u0": 0}, nil},
-		expect{"changed", "u0", 0, msi{"u0": 0}},
+		msi{"u/0": 0}, "u/0",
+		send{msi{"u/0": 0}, nil},
+		expect{"changed", "u/0", 0, msi{"u/0": 0}},
 	), reconcileTest(
 		// Check that missing units are queued for depart as early as possible.
-		msi{"u0": 0}, "",
-		send{msi{"u1": 0}, nil},
-		expect{"departed", "u0", 0, nil},
-		expect{"joined", "u1", 0, msi{"u1": 0}},
-		expect{"changed", "u1", 0, msi{"u1": 0}},
+		msi{"u/0": 0}, "",
+		send{msi{"u/1": 0}, nil},
+		expect{"departed", "u/0", 0, nil},
+		expect{"joined", "u/1", 0, msi{"u/1": 0}},
+		expect{"changed", "u/1", 0, msi{"u/1": 0}},
 	), reconcileTest(
 		// Double-check that a just-joined unit gets its changed hook run first,
 		// even when it's due to depart.
-		msi{"u0": 0}, "u0",
+		msi{"u/0": 0}, "u/0",
 		send{nil, nil},
-		expect{"changed", "u0", 0, msi{"u0": -1}},
-		expect{"departed", "u0", 0, nil},
+		expect{"changed", "u/0", 0, msi{"u/0": -1}},
+		expect{"departed", "u/0", 0, nil},
 	), reconcileTest(
 		// Check that missing units don't slip in front of required changed hooks.
-		msi{"u0": 0}, "u0",
-		send{msi{"u1": 0}, nil},
-		expect{"changed", "u0", 0, msi{"u0": -1}},
-		expect{"departed", "u0", 0, nil},
-		expect{"joined", "u1", 0, msi{"u1": 0}},
-		expect{"changed", "u1", 0, msi{"u1": 0}},
+		msi{"u/0": 0}, "u/0",
+		send{msi{"u/1": 0}, nil},
+		expect{"changed", "u/0", 0, msi{"u/0": -1}},
+		expect{"departed", "u/0", 0, nil},
+		expect{"joined", "u/1", 0, msi{"u/1": 0}},
+		expect{"changed", "u/1", 0, msi{"u/1": 0}},
 	),
 }
 
--- worker/uniter/relationstate.go
+++ worker/uniter/relationstate.go
@@ -11,9 +11,9 @@ import (
 	"strings"
 )
 
-// RelationState is a snapshot of the state of a relation.
+// RelationState is a filesystem-backed snapshot of the state of a relation.
 type RelationState struct {
-	// Path identifies the directory in which unit state is persisted.
+	// Path identifies the relation directory, in which unit state is persisted.
 	Path string
 
 	// RelationId identifies the relation.
@@ -28,11 +28,18 @@ type RelationState struct {
 	ChangedPending string
 }
 
+// checker validates the format of relation unit state files.
 var checker = schema.StrictFieldMap(
 	schema.Fields{"change-version": schema.Int(), "changed-pending": schema.Bool()},
 	schema.Defaults{"changed-pending": false},
 )
 
+// NewRelationState loads a RelationState from the subdirectory of dirpath named
+// for the supplied RelationId. It must not contain any subdirectories; and, apart
+// from filenames ending in ~, the directory must *only* contain valid relation
+// unit files as written by Commit. Subsequent modification of the directory
+// contents, by any entity other than the returned RelationState instance, will
+// cause undefined behaviour.
 func NewRelationState(dirpath string, relationId int) (*RelationState, error) {
 	path := filepath.Join(dirpath, fmt.Sprintf("%d", relationId))
 	rs := &RelationState{path, relationId, map[string]int{}, ""}
@@ -77,7 +84,11 @@ func NewRelationState(dirpath string, relationId int) (*RelationState, error) {
 	return rs, nil
 }
 
-func LoadRelationStates(dirpath string) (map[int]*RelationState, error) {
+// AllRelationStates loads and returns every RelationState persisted directly
+// inside the directory at the supplied path. It is an error for the directory
+// to contain anything other than valid persisted RelationStates, identified by
+// relation id.
+func AllRelationStates(dirpath string) (map[int]*RelationState, error) {
 	states := map[int]*RelationState{}
 	walker := func(path string, fi os.FileInfo) error {
 		if !fi.IsDir() {
@@ -100,31 +111,69 @@ func LoadRelationStates(dirpath string) (map[int]*RelationState, error) {
 	return states, nil
 }
 
+// MustValidate will panic if the supplied HookInfo does not represent a
+// sane change in the RelationState. This situation indicates that the
+// received stream of HookInfo values, or the RelationState itself,
+// contains corrupt data, and that attempting to continue process
+// execution beyond this point will inevitably do more harm than good.
+func (rs *RelationState) MustValidate(hi HookInfo) {
+	id := hi.RelationId
+	if id != rs.RelationId {
+		panic(fmt.Errorf("cannot store state for relation %d inside relation %d", id, rs.RelationId))
+	}
+	unit, hook, pending := hi.RemoteUnit, hi.HookKind, rs.ChangedPending
+	if pending != "" {
+		if unit != pending || hook != "changed" {
+			panic(fmt.Errorf(`expected a "changed" for %q; got a %q for %q`, pending, hook, unit))
+		}
+	}
+	if _, joined := rs.Members[unit]; joined && hook == "joined" {
+		panic(fmt.Errorf(`invalid "joined": %q has already joined relation %d`, unit, id))
+	} else if !joined && hook != "joined" {
+		panic(fmt.Errorf("invalid %q: %q has not joined relation %d", hook, unit, id))
+	}
+}
+
+// Commit ensures the validity of; stores; and atomically writes to disk,
+// the effect on the RelationState of the successful completion of the
+// hook defined by the supplied HookInfo.
 func (rs *RelationState) Commit(hi HookInfo) error {
-	if hi.RelationId != rs.RelationId {
-		panic("tried to persist hook state to inappropriate relation!")
+	rs.MustValidate(hi)
+	if hi.HookKind == "joined" {
+		rs.ChangedPending = hi.RemoteUnit
+	} else if rs.ChangedPending != "" {
+		rs.ChangedPending = ""
 	}
 	name := strings.Replace(hi.RemoteUnit, "/", "-", -1)
 	path := filepath.Join(rs.Path, name)
+	if hi.HookKind == "departed" {
+		delete(rs.Members, hi.RemoteUnit)
+		return os.Remove(path)
+	}
+	rs.Members[hi.RemoteUnit] = hi.ChangeVersion
 	unit := map[string]interface{}{"change-version": hi.ChangeVersion}
-	if hi.HookKind == "joined" {
+	if rs.ChangedPending != "" {
 		unit["changed-pending"] = true
 	}
 	data, err := goyaml.Marshal(unit)
 	if err != nil {
 		return err
 	}
-	if err = ioutil.WriteFile(path+"~", data, 0777); err != nil {
+	if err = ioutil.WriteFile(path+"~", data, 0600); err != nil {
 		return err
 	}
 	return os.Rename(path+"~", path)
 }
 
+// createWalk walks the supplied directory tree, and calls the supplied function
+// for its children, in the manner of os.Walk. If the directory does not exist,
+// it is created; if the path points to anything other than a directory, an
+// error is returned.
 func createWalk(dirpath string, f func(path string, fi os.FileInfo) error) error {
 	walker := func(path string, fi os.FileInfo, err error) error {
 		if path == dirpath {
 			if os.IsNotExist(err) {
-				return os.Mkdir(dirpath, 0777)
+				return os.Mkdir(dirpath, 0600)
 			} else if !fi.IsDir() {
 				return fmt.Errorf("%s must be a directory", dirpath)
 			}
@@ -137,15 +186,16 @@ func createWalk(dirpath string, f func(path string, fi os.FileInfo) error) error
 	return filepath.Walk(dirpath, walker)
 }
 
-func unitname(fileName string) (string, error) {
-	i := strings.LastIndex(fileName, "-")
+// unitname converts a relation unit filename to a unit name.
+func unitname(filename string) (string, error) {
+	i := strings.LastIndex(filename, "-")
 	if i == -1 {
-		return "", fmt.Errorf("invalid unit filename %q", fileName)
+		return "", fmt.Errorf("invalid unit filename %q", filename)
 	}
-	svcName := fileName[:i]
-	unitId := fileName[i+1:]
+	svcName := filename[:i]
+	unitId := filename[i+1:]
 	if _, err := strconv.Atoi(unitId); err != nil {
-		return "", fmt.Errorf("invalid unit filename %q", fileName)
+		return "", fmt.Errorf("invalid unit filename %q", filename)
 	}
 	return svcName + "/" + unitId, nil
 }
