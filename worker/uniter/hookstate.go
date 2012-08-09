4850ca31ea1c800818d68f43294385c6b87904af
William Reade <fwereade@gmail.com>
2012-08-09 09:28:03 +0200

    finish tests

--- worker/uniter/hookstate.go
+++ worker/uniter/hookstate.go
@@ -2,106 +2,104 @@ package uniter
 
 import (
 	"errors"
+	"fmt"
 	"io/ioutil"
 	"launchpad.net/goyaml"
 	"os"
 )
 
-// HookState stores a hook's execution status.
-type HookState struct {
-	path string
+// HookStatus defines the stages of execution through which a hook passes.
+type HookStatus string
+
+const (
+	StatusStarted   HookStatus = "started"
+	StatusSucceeded HookStatus = "succeeded"
+	StatusCommitted HookStatus = "committed"
+)
+
+// validate will return an error if the HookStatus is not a known value.
+func (hs HookStatus) validate() (err error) {
+	switch hs {
+	case StatusStarted, StatusSucceeded, StatusCommitted:
+	default:
+		err = fmt.Errorf("unknown HookStatus %q!", hs)
+	}
+	return
 }
 
-// Running persists the fact that the Uniter is committed to running
-// the supplied hook.
-func (s *HookState) Started(hi HookInfo) error {
-	return writeDiskHook(s.path, hi, "started")
-}
-
-// Succeeded persists the fact that the Uniter has successfully completed
-// the supplied hook, but that it has not yet reconciled all the state it
-// needs to.
-func (s *HookState) Succeeded(hi HookInfo) error {
-	return writeDiskHook(s.path, hi, "succeeded")
-}
-
-// Committed causes the HookState to entirely forget the supplied hook.
-func (s *HookState) Committed(hi HookInfo) error {
-	return os.Remove(s.path)
-}
-
-// NotSucceeded returns any hook that has been started but has not succeeded.
-// If there is no such hook, it will return NoHook.
-func (s *HookState) NotSucceeded() (HookInfo, error) {
-	return readDiskHook(s.path, "started")
-}
-
-// NotCommitted returns any hook that has succeeded but not been committed.
-// If there is no such hook, it will return NoHook.
-func (s *HookState) NotCommitted() (HookInfo, error) {
-	return readDiskHook(s.path, "succeeded")
-}
-
-// NoHook indicates that no hook exists with the requested characteristics.
-var NoHook = errors.New("no hook")
-
-// diskHook holds the hook information that needs to persist across runs of
-// the unit agent process.
+// diskHook defines the hook state serialization.
 type diskHook struct {
 	RelationId    int
 	HookKind      string
 	RemoteUnit    string
 	ChangeVersion int
 	Members       []string
-	Status        string
+	Status        HookStatus
 }
 
-// writeDiskHook atomically writes the supplied hook information and status.
-func writeDiskHook(path string, hi HookInfo, status string) error {
+// HookState stores and retrieves a hook and its execution status.
+type HookState struct {
+	path string
+}
+
+// NewHookState returns a new hook state that persists to the supplied path.
+func NewHookState(path string) *HookState {
+	return &HookState{path}
+}
+
+// NoHook indicates that no hook has ever been stored.
+var NoHook = errors.New("no hook")
+
+// Get loads the status of, and the HookInfo, that was last Set. If no hook
+// has ever been set, the error will be NoHook.
+func (s *HookState) Get() (hi HookInfo, hs HookStatus, err error) {
+	var data []byte
+	if data, err = ioutil.ReadFile(s.path); err != nil {
+		if os.IsNotExist(err) {
+			err = NoHook
+		}
+		return
+	}
+	var dh diskHook
+	if err = goyaml.Unmarshal(data, &dh); err != nil {
+		return
+	}
+	if dh.HookKind == "" || dh.Status.validate() != nil {
+		err = fmt.Errorf("invalid hook state at %s", s.path)
+	} else {
+		hi = HookInfo{
+			RelationId:    dh.RelationId,
+			HookKind:      dh.HookKind,
+			RemoteUnit:    dh.RemoteUnit,
+			ChangeVersion: dh.ChangeVersion,
+			Members:       map[string]map[string]interface{}{},
+		}
+		for _, m := range dh.Members {
+			hi.Members[m] = nil
+		}
+		hs = dh.Status
+	}
+	return
+}
+
+// Set persists the status of, and information necessary to reconstruct, the
+// supplied hook. It will panic if asked to store invalid data.
+func (s *HookState) Set(hi HookInfo, hs HookStatus) error {
+	if err := hs.validate(); err != nil {
+		panic(err)
+	}
+	if hi.HookKind == "" {
+		panic("empty HookKind!")
+	}
 	dh := diskHook{
 		RelationId:    hi.RelationId,
 		HookKind:      hi.HookKind,
 		RemoteUnit:    hi.RemoteUnit,
 		ChangeVersion: hi.ChangeVersion,
-		Status:        status,
+		Status:        hs,
 	}
 	for m := range hi.Members {
 		dh.Members = append(dh.Members, m)
 	}
-	return atomicWrite(path, dh)
-}
-
-// readDiskHook returns a HookInfo describing a hook persisted with the
-// supplied status. If no such hook exists, NoHook is returned.
-func readDiskHook(path, requireStatus string) (HookInfo, error) {
-	data, err := ioutil.ReadFile(path)
-	if err != nil {
-		if os.IsNotExist(err) {
-			err = NoHook
-		}
-		return HookInfo{}, err
-	}
-	var dh diskHook
-	if err = goyaml.Unmarshal(data, &dh); err != nil {
-		return HookInfo{}, err
-	}
-	if dh.Status == requireStatus {
-		return dh.HookInfo(), nil
-	}
-	return HookInfo{}, NoHook
-}
-
-// HookInfo returns a HookInfo reconstructed from the data stored in dh.
-func (dh diskHook) HookInfo() HookInfo {
-	hi := HookInfo{
-		RelationId:    dh.RelationId,
-		HookKind:      dh.HookKind,
-		RemoteUnit:    dh.RemoteUnit,
-		ChangeVersion: dh.ChangeVersion,
-		Members:       map[string]map[string]interface{}{},
-	}
-	for _, m := range dh.Members {
-		hi.Members[m] = nil
-	}
-	return hi
+	return atomicWrite(s.path, dh)
 }
--- worker/uniter/hookstate_test.go
+++ worker/uniter/hookstate_test.go
@@ -1,13 +1,57 @@
 package uniter_test
 
 import (
+	"io/ioutil"
 	. "launchpad.net/gocheck"
+	"launchpad.net/juju-core/worker/uniter"
+	"path/filepath"
 )
 
 type HookStateSuite struct{}
 
 var _ = Suite(&HookStateSuite{})
 
-func (s *HookStateSuite) TestFails(c *C) {
-	c.Fatalf("rsahiuvfkd")
+func (s *HookStateSuite) TestHookState(c *C) {
+	path := filepath.Join(c.MkDir(), "hook")
+	hs := uniter.NewHookState(path)
+	_, _, err := hs.Get()
+	c.Assert(err, Equals, uniter.NoHook)
+
+	err = ioutil.WriteFile(path, []byte("roflcopter"), 0644)
+	c.Assert(err, IsNil)
+	_, _, err = hs.Get()
+	c.Assert(err, ErrorMatches, "invalid hook state at "+path)
+
+	f := func() { hs.Set(uniter.HookInfo{}, uniter.HookStatus("nonsense")) }
+	c.Assert(f, PanicMatches, `unknown HookStatus "nonsense"!`)
+
+	f = func() { hs.Set(uniter.HookInfo{}, uniter.StatusStarted) }
+	c.Assert(f, PanicMatches, `empty HookKind!`)
+
+	hi := uniter.HookInfo{
+		RelationId:    123,
+		HookKind:      "changed",
+		RemoteUnit:    "abc/999",
+		ChangeVersion: 321,
+		Members: map[string]map[string]interface{}{
+			"abc/999":  {"foo": 1, "bar": 2},
+			"abc/1000": {"baz": 3, "qux": 4},
+		},
+	}
+	err = hs.Set(hi, uniter.StatusStarted)
+	c.Assert(err, IsNil)
+
+	ghi, gst, err := hs.Get()
+	c.Assert(err, IsNil)
+	c.Assert(gst, Equals, uniter.StatusStarted)
+	c.Assert(ghi, DeepEquals, uniter.HookInfo{
+		RelationId:    123,
+		HookKind:      "changed",
+		RemoteUnit:    "abc/999",
+		ChangeVersion: 321,
+		Members: map[string]map[string]interface{}{
+			"abc/999":  nil,
+			"abc/1000": nil,
+		},
+	})
 }
