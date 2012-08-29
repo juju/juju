fa02b244a8a4ef265740edae00b67e8923319061
Dave Cheney <david.cheney@canonical.com>
2012-08-29 15:15:03 +1000

    add logging for open/close port

--- environs/ec2/ec2.go
+++ environs/ec2/ec2.go
@@ -600,6 +600,7 @@ func (inst *instance) OpenPorts(machineId int, ports []state.Port) error {
 	if err != nil {
 		return fmt.Errorf("cannot open ports: %v", err)
 	}
+	log.Printf("environs/ec2: opened %d port(s) in security group %s", len(ports), g.Name)
 	return nil
 }
 
@@ -615,6 +616,7 @@ func (inst *instance) ClosePorts(machineId int, ports []state.Port) error {
 	if err != nil {
 		return fmt.Errorf("cannot close ports: %v", err)
 	}
+	log.Printf("environs/ec2: closed %d port(s) in security group %s", len(ports), g.Name)
 	return nil
 }
 
