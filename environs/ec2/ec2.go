b42f9b1787ab86dd07f28577b6b38426cb095cf9
Dave Cheney <david.cheney@canonical.com>
2012-08-30 09:03:14 +1000

    responding to review feedback

--- environs/ec2/ec2.go
+++ environs/ec2/ec2.go
@@ -600,7 +600,7 @@ func (inst *instance) OpenPorts(machineId int, ports []state.Port) error {
 	if err != nil {
 		return fmt.Errorf("cannot open ports: %v", err)
 	}
-	log.Printf("environs/ec2: opened %d port(s) in security group %s", len(ports), g.Name)
+	log.Printf("environs/ec2: opened ports in security group %s: %v", g.Name, ports)
 	return nil
 }
 
@@ -616,7 +616,7 @@ func (inst *instance) ClosePorts(machineId int, ports []state.Port) error {
 	if err != nil {
 		return fmt.Errorf("cannot close ports: %v", err)
 	}
-	log.Printf("environs/ec2: closed %d port(s) in security group %s", len(ports), g.Name)
+	log.Printf("environs/ec2: closed ports in security group %s: %v", g.Name, ports)
 	return nil
 }
 
