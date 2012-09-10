35ada7d7107bbbdb493bea890cefd8a8a76b2074
William Reade <fwereade@gmail.com>
2012-09-10 15:37:51 +0200

    unit agent command now starts a uniter

--- cmd/jujud/unit_test.go
+++ cmd/jujud/unit_test.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"io/ioutil"
 	. "launchpad.net/gocheck"
 	"launchpad.net/juju-core/cmd"
+	"launchpad.net/juju-core/environs"
+	"launchpad.net/juju-core/juju/testing"
+	"launchpad.net/juju-core/state"
+	"launchpad.net/juju-core/version"
+	"os"
+	"path/filepath"
+	"time"
 )
 
-type UnitSuite struct{}
+type UnitSuite struct {
+	testing.JujuConnSuite
+}
 
 var _ = Suite(&UnitSuite{})
 
@@ -41,4 +51,59 @@ func (s *UnitSuite) TestParseUnknown(c *C) {
 	uc := &UnitAgent{}
 	err := ParseAgentCommand(uc, []string{"--unit-name", "wordpress/1", "thundering typhoons"})
 	c.Assert(err, ErrorMatches, `unrecognized args: \["thundering typhoons"\]`)
+}
+
+func (s *UnitSuite) TestRunStop(c *C) {
+	// Set up state.
+	ch := s.AddTestingCharm(c, "dummy")
+	svc, err := s.Conn.AddService("dummy", ch)
+	c.Assert(err, IsNil)
+	unit, err := svc.AddUnit()
+	c.Assert(err, IsNil)
+
+	// Set up local environment.
+	jujuDir := environs.VarDir
+	vers := version.Current.String()
+	toolsDir := filepath.Join(jujuDir, "tools", vers)
+	err = os.MkdirAll(toolsDir, 0755)
+	c.Assert(err, IsNil)
+	err = ioutil.WriteFile(filepath.Join(toolsDir, "jujuc"), nil, 0644)
+	c.Assert(err, IsNil)
+	toolsLink := filepath.Join(jujuDir, "tools", "unit-dummy-0")
+	err = os.Symlink(vers, toolsLink)
+	c.Assert(err, IsNil)
+
+	// Run a unit agent.
+	a := &UnitAgent{
+		Conf:     AgentConf{jujuDir, *s.StateInfo(c)},
+		UnitName: unit.Name(),
+	}
+	done := make(chan error)
+	go func() {
+		done <- a.Run(nil)
+	}()
+	defer a.Stop()
+	timeout := time.After(2000 * time.Millisecond)
+	for {
+		select {
+		case <-timeout:
+			c.Fatalf("no activity detected")
+		case <-time.After(200 * time.Millisecond):
+			st, info, err := unit.Status()
+			c.Assert(err, IsNil)
+			switch st {
+			case state.UnitPending, state.UnitInstalled:
+				c.Logf("waiting...")
+				continue
+			case state.UnitStarted:
+				c.Logf("started!")
+			default:
+				c.Fatalf("unexpected status %s %s", st, info)
+			}
+		}
+		break
+	}
+	err = a.Stop()
+	c.Assert(err, IsNil)
+	c.Assert(<-done, IsNil)
 }
