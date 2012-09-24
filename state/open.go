e2077a23c887d641da4bcf7eba1b0a9892a3e1b4
Gustavo Niemeyer <gustavo@niemeyer.net>
2012-09-24 18:10:48 -0300

    state,environs/cloudinit: fix bootstrap
    With these changes environments correctly bootstrap again.

--- state/open.go
+++ state/open.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"strings"
+	"time"
 
 	"labix.org/v2/mgo"
 	"labix.org/v2/mgo/txn"
@@ -35,7 +36,7 @@ func Open(info *Info) (*State, error) {
 		return nil, errors.New("no mongo addresses")
 	}
 	if !info.UseSSH {
-		session, err := mgo.Dial(strings.Join(info.Addrs, ","))
+		session, err := mgo.DialWithTimeout(strings.Join(info.Addrs, ","), 10 * time.Minute)
 		if err != nil {
 			return nil, err
 		}
@@ -92,11 +93,11 @@ var indexes = []struct {
 	{"units", []string{"machineid"}},
 }
 
-// The capped collection used for transaction logs defaults to 200MB.
+// The capped collection used for transaction logs defaults to 10MB.
 // It's tweaked in export_test.go to 1MB to avoid the overhead of
-// creating and deleting the large file repeatedly.
+// creating and deleting the large file repeatedly in tests.
 var (
-	logSize      = 200000000
+	logSize      = 10000000
 	logSizeTests = 1000000
 )
 
