fadcb2fc117cb9d69a56e390b9e29a803731cbf7
Dave Cheney <david.cheney@canonical.com>
2012-10-03 14:22:55 +1000

    added set

--- cmd/juju/set.go
+++ cmd/juju/set.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"reflect"
 	"strings"
 
 	"launchpad.net/gnuflag"
 	"launchpad.net/goyaml"
+	"launchpad.net/juju-core/charm"
 	"launchpad.net/juju-core/cmd"
 	"launchpad.net/juju-core/juju"
 )
@@ -43,9 +46,18 @@ func (c *SetCommand) Run(ctx *cmd.Context) error {
 	if err != nil && err != cmd.PathNotSetError {
 		return err
 	}
-	options := make(map[string]interface{})
+	var (
+		unvalidated = make(map[string]string)
+		remove      []string
+	)
 	if len(contents) > 0 {
-		if err := goyaml.Unmarshal(contents, &options); err != nil {
+		if err := goyaml.Unmarshal(contents, &unvalidated); err != nil {
+			return err
+		}
+	}
+	if len(unvalidated) == 0 {
+		unvalidated, remove, err = parse(c.Options)
+		if err != nil {
 			return err
 		}
 	}
@@ -58,11 +70,60 @@ func (c *SetCommand) Run(ctx *cmd.Context) error {
 	if err != nil {
 		return err
 	}
+	charm, _, err := srv.Charm()
+	if err != nil {
+		return err
+	}
+	validated, err := charm.Config().Validate(unvalidated)
+	if err != nil {
+		return err
+	}
+	// Validate will insert into validated 
+	validated = strip(validated, charm.Config())
 	cfg, err := srv.Config()
 	if err != nil {
 		return err
 	}
-	cfg.Update(options)
+	cfg.Update(validated)
+
+	// remove any orphaned keys
+	for _, k := range remove {
+		cfg.Delete(k)
+	}
 	_, err = cfg.Write()
 	return err
+}
+
+// parse parses the option k=v strings into a map of options to be 
+// updated in the config. Keys with empty values are returned separately
+// and should be removed.
+func parse(options []string) (map[string]string, []string, error) {
+	var (
+		m = make(map[string]string)
+		d []string
+	)
+	for _, o := range options {
+		s := strings.Split(o, "=")
+		switch len(s) {
+		case 2:
+			m[s[0]] = s[1]
+		case 1:
+			d = append(d, s[0])
+		default:
+			return nil, nil, fmt.Errorf("invalid option: %q", o)
+		}
+	}
+	return m, d, nil
+}
+
+// strip removes from options any keys whoes values are the charm defaults
+func strip(options map[string]interface{}, config *charm.Config) map[string]interface{} {
+	for k, v := range options {
+		if ch, ok := config.Options[k]; ok {
+			if ch.Default != nil && reflect.DeepEqual(ch.Default, v) {
+				delete(options, k)
+			}
+		}
+	}
+	return options
 }
