4be56f69abf5d52d7a590bcb47daa823cfdafb3a
Roger Peppe <roger.peppe@canonical.com>
2012-10-05 18:47:26 +0100

    trivial: reduce password size

--- trivial/password.go
+++ trivial/password.go
@@ -25,6 +25,10 @@ func RandomBytes(n int) ([]byte, error) {
 // and password that is computationally hard to crack by iterating
 // through possible passwords.
 func PasswordHash(password string) string {
-	h := pbkdf2.Key([]byte(password), salt, 8192, 76, sha512.New)
+	// Generate 18 byte passwords because we know that MongoDB
+	// uses the MD5 sum of the password anyway, so there's
+	// no point in using more bytes. (18 so we don't get base 64
+	// padding characters).
+	h := pbkdf2.Key([]byte(password), salt, 8192, 18, sha512.New)
 	return base64.StdEncoding.EncodeToString(h)
 }
