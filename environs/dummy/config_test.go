c98ac04f284f93ac5f1ced431a6369f753a28624
Frank Mueller <themue@gmail.com>
2012-10-11 14:15:53 +0200

    dummy: added more tests

--- environs/dummy/config_test.go
+++ environs/dummy/config_test.go
@@ -31,7 +31,7 @@ func (*ConfigSuite) TestSecretAttrs(c *C) {
 
 func (*ConfigSuite) TestFirewallMode(c *C) {
 	cfg, err := config.New(map[string]interface{}{
-		"name":            "only", // must match the name in environs_test.go
+		"name":            "only",
 		"type":            "dummy",
 		"state-server":    true,
 		"authorized-keys": "i-am-a-key",
@@ -41,4 +41,52 @@ func (*ConfigSuite) TestFirewallMode(c *C) {
 	c.Assert(err, IsNil)
 	firewallMode := env.Config().FirewallMode()
 	c.Assert(firewallMode, Equals, config.FwInstance)
+
+	cfg, err = config.New(map[string]interface{}{
+		"name":            "only",
+		"type":            "dummy",
+		"state-server":    true,
+		"authorized-keys": "i-am-a-key",
+		"firewall-mode":   "default",
+	})
+	c.Assert(err, IsNil)
+	env, err = environs.New(cfg)
+	c.Assert(err, IsNil)
+	firewallMode = env.Config().FirewallMode()
+	c.Assert(firewallMode, Equals, config.FwInstance)
+
+	cfg, err = config.New(map[string]interface{}{
+		"name":            "only",
+		"type":            "dummy",
+		"state-server":    true,
+		"authorized-keys": "i-am-a-key",
+		"firewall-mode":   "instance",
+	})
+	c.Assert(err, IsNil)
+	env, err = environs.New(cfg)
+	c.Assert(err, IsNil)
+	firewallMode = env.Config().FirewallMode()
+	c.Assert(firewallMode, Equals, config.FwInstance)
+
+	cfg, err = config.New(map[string]interface{}{
+		"name":            "only",
+		"type":            "dummy",
+		"state-server":    true,
+		"authorized-keys": "i-am-a-key",
+		"firewall-mode":   "global",
+	})
+	c.Assert(err, IsNil)
+	env, err = environs.New(cfg)
+	c.Assert(err, IsNil)
+	firewallMode = env.Config().FirewallMode()
+	c.Assert(firewallMode, Equals, config.FwGlobal)
+
+	cfg, err = config.New(map[string]interface{}{
+		"name":            "only",
+		"type":            "dummy",
+		"state-server":    true,
+		"authorized-keys": "i-am-a-key",
+		"firewall-mode":   "invalid",
+	})
+	c.Assert(err, ErrorMatches, `invalid firewall mode in environment configuration: .*`)
 }
