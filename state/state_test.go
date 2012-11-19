511e4c4927d66004032210bc6da61bbd34b0f21d
Roger Peppe <roger.peppe@canonical.com>
2012-11-19 17:54:25 +0000

    state: use testing.RootCert

--- state/state_test.go
+++ state/state_test.go
@@ -399,7 +399,7 @@ func (s *StateSuite) TestEnvironConfig(c *C) {
 		"development":      true,
 		"firewall-mode":    "",
 		"admin-secret":     "",
-		"root-cert":        rootCert,
+		"root-cert":        testing.RootCertPEM,
 		"root-private-key": "",
 	}
 	cfg, err := config.New(initial)
@@ -432,7 +432,7 @@ func (s *StateSuite) TestEnvironConfigWithAdminSecret(c *C) {
 		"default-series":  "precise",
 		"development":     true,
 		"admin-secret":    "foo",
-		"root-cert":       rootCert,
+		"root-cert":       testing.RootCertPEM,
 	}
 	cfg, err := config.New(attrs)
 	c.Assert(err, IsNil)
@@ -793,7 +793,7 @@ func (s *StateSuite) TestInitialize(c *C) {
 		"development":      true,
 		"firewall-mode":    "",
 		"admin-secret":     "",
-		"root-cert":        rootCert,
+		"root-cert":        testing.RootCertPEM,
 		"root-private-key": "",
 	}
 	cfg, err := config.New(m)
@@ -815,7 +815,7 @@ func (s *StateSuite) TestDoubleInitialize(c *C) {
 		"development":      true,
 		"firewall-mode":    "",
 		"admin-secret":     "",
-		"root-cert":        rootCert,
+		"root-cert":        testing.RootCertPEM,
 		"root-private-key": "",
 	}
 	cfg, err := config.New(m)
@@ -836,7 +836,7 @@ func (s *StateSuite) TestDoubleInitialize(c *C) {
 		"development":      false,
 		"firewall-mode":    "",
 		"admin-secret":     "",
-		"root-cert":        rootCert,
+		"root-cert":        testing.RootCertPEM,
 		"root-private-key": "",
 	}
 	cfg, err = config.New(m)
@@ -892,7 +892,7 @@ var watchEnvironConfigTests = []attrs{
 		"type":            "my-type",
 		"name":            "my-name",
 		"authorized-keys": "i-am-a-key",
-		"root-cert":       rootCert,
+		"root-cert":       testing.RootCertPEM,
 	},
 	{
 		// Add an attribute.
@@ -900,7 +900,7 @@ var watchEnvironConfigTests = []attrs{
 		"name":            "my-name",
 		"default-series":  "my-series",
 		"authorized-keys": "i-am-a-key",
-		"root-cert":       rootCert,
+		"root-cert":       testing.RootCertPEM,
 	},
 	{
 		// Set a new attribute value.
@@ -908,7 +908,7 @@ var watchEnvironConfigTests = []attrs{
 		"name":            "my-new-name",
 		"default-series":  "my-series",
 		"authorized-keys": "i-am-a-key",
-		"root-cert":       rootCert,
+		"root-cert":       testing.RootCertPEM,
 	},
 }
 
@@ -970,7 +970,7 @@ func (s *StateSuite) TestWatchEnvironConfigInvalidConfig(c *C) {
 		"type":            "dummy",
 		"name":            "lisboa",
 		"authorized-keys": "i-am-a-key",
-		"root-cert":       rootCert,
+		"root-cert":       testing.RootCertPEM,
 	}
 	cfg1, err := config.New(m)
 	c.Assert(err, IsNil)
@@ -1016,7 +1016,7 @@ func (s *StateSuite) TestWatchEnvironConfigInvalidConfig(c *C) {
 		"type":            "dummy",
 		"name":            "lisboa",
 		"authorized-keys": "new-key",
-		"root-cert":       rootCert,
+		"root-cert":       testing.RootCertPEM,
 	})
 	c.Assert(err, IsNil)
 	err = s.State.SetEnvironConfig(cfg2)
@@ -1184,16 +1184,3 @@ func (s *StateSuite) TestSetAdminPassword(c *C) {
 	err = tryOpenState(info)
 	c.Assert(err, IsNil)
 }
-
-var rootCert = `
------BEGIN CERTIFICATE-----
-MIIBdzCCASOgAwIBAgIBADALBgkqhkiG9w0BAQUwHjENMAsGA1UEChMEanVqdTEN
-MAsGA1UEAxMEcm9vdDAeFw0xMjExMDgxNjIyMzRaFw0xMzExMDgxNjI3MzRaMBwx
-DDAKBgNVBAoTA2htbTEMMAoGA1UEAxMDYW55MFowCwYJKoZIhvcNAQEBA0sAMEgC
-QQCACqz6JPwM7nbxAWub+APpnNB7myckWJ6nnsPKi9SipP1hyhfzkp8RGMJ5Uv7y
-8CSTtJ8kg/ibka1VV8LvP9tnAgMBAAGjUjBQMA4GA1UdDwEB/wQEAwIAsDAdBgNV
-HQ4EFgQU6G1ERaHCgfAv+yoDMFVpDbLOmIQwHwYDVR0jBBgwFoAUP/mfUdwOlHfk
-fR+gLQjslxf64w0wCwYJKoZIhvcNAQEFA0EAbn0MaxWVgGYBomeLYfDdb8vCq/5/
-G/2iCUQCXsVrBparMLFnor/iKOkJB5n3z3rtu70rFt+DpX6L8uBR3LB3+A==
------END CERTIFICATE-----
-`[1:]
