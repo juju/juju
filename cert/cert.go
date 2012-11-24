b8708c85158b74538296099734e2b81a375d1414
Roger Peppe <roger.peppe@canonical.com>
2012-11-24 13:49:55 +0000

    add cert package

---
+++ cert/cert.go
@@ -0,0 +1,117 @@
+package cert
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha1"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"fmt"
+	"math/big"
+	"time"
+)
+
+var KeyBits = 1024
+
+// NewCACert generates a CA certificate/key pair suitable for
+// signing server keys for an environment with the
+// given name.
+func NewCA(envName string, expiry time.Time) (certPEM, keyPEM []byte, err error) {
+	key, err := rsa.GenerateKey(rand.Reader, KeyBits)
+	if err != nil {
+		return nil, nil, err
+	}
+	now := time.Now()
+	template := &x509.Certificate{
+		SerialNumber: new(big.Int),
+		Subject: pkix.Name{
+			// TODO quote the environment name when we start using
+			// Go version 1.1. See Go issue 3791.
+			CommonName:   fmt.Sprintf("juju-generated CA for environment %s", envName),
+			Organization: []string{"juju"},
+		},
+		NotBefore:             now.UTC().Add(-5 * time.Minute),
+		NotAfter:              expiry,
+		SubjectKeyId:          bigIntHash(key.N),
+		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
+		BasicConstraintsValid: true,
+		IsCA:       true,
+		MaxPathLen: 0, // Disallow delegation for now.
+	}
+	certDER, err := x509.CreateCertificate(rand.Reader, template, template, &key.PublicKey, key)
+	if err != nil {
+		return nil, nil, fmt.Errorf("canot create certificate: %v", err)
+	}
+	certPEM = pem.EncodeToMemory(&pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: certDER,
+	})
+	keyPEM = pem.EncodeToMemory(&pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(key),
+	})
+	return certPEM, keyPEM, nil
+}
+
+// NewServer generates a certificate/key pair suitable
+// for using by a server for an environment with the given
+// name.
+func NewServer(envName string, caCertPEM, caKeyPEM []byte, expiry time.Time) (certPEM, keyPEM []byte, err error) {
+	tlsCert, err := tls.X509KeyPair(caCertPEM, caKeyPEM)
+	if err != nil {
+		return nil, nil, err
+	}
+	if len(tlsCert.Certificate) != 1 {
+		return nil, nil, fmt.Errorf("more than one certificate for CA")
+	}
+	caCert, err := x509.ParseCertificate(tlsCert.Certificate[0])
+	if err != nil {
+		return nil, nil, err
+	}
+	if !caCert.BasicConstraintsValid || !caCert.IsCA {
+		return nil, nil, fmt.Errorf("CA certificate is not a valid CA")
+	}
+	caKey, ok := tlsCert.PrivateKey.(*rsa.PrivateKey)
+	if !ok {
+		return nil, nil, fmt.Errorf("CA private key has unexpected type %T", tlsCert.PrivateKey)
+	}
+	key, err := rsa.GenerateKey(rand.Reader, KeyBits)
+	if err != nil {
+		return nil, nil, fmt.Errorf("cannot generate key: %v", err)
+	}
+	now := time.Now()
+	template := &x509.Certificate{
+		SerialNumber: new(big.Int),
+		Subject: pkix.Name{
+			// This won't match host names with dots. The hostname
+			// is hardcoded when connecting to avoid the issue.
+			CommonName:   "*",
+			Organization: []string{"juju"},
+		},
+		NotBefore: now.UTC().Add(-5 * time.Minute),
+		NotAfter:  now.UTC().AddDate(10, 0, 0), // 10 years hence.
+
+		SubjectKeyId: bigIntHash(key.N),
+		KeyUsage:     x509.KeyUsageDataEncipherment,
+	}
+	certDER, err := x509.CreateCertificate(rand.Reader, template, caCert, &key.PublicKey, caKey)
+	if err != nil {
+		return nil, nil, err
+	}
+	certPEM = pem.EncodeToMemory(&pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: certDER,
+	})
+	keyPEM = pem.EncodeToMemory(&pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(key),
+	})
+	return certPEM, keyPEM, nil
+}
+
+func bigIntHash(n *big.Int) []byte {
+	h := sha1.New()
+	h.Write(n.Bytes())
+	return h.Sum(nil)
+}
--- testing/cert.go
+++ testing/cert.go
@@ -2,9 +2,47 @@ package testing
 
 import (
 	"crypto/rsa"
+	"crypto/tls"
 	"crypto/x509"
 	"encoding/pem"
+	"fmt"
 )
+
+func verifyCertificates() error {
+	_, err := tls.X509KeyPair([]byte(CACertPEM), []byte(CAKeyPEM))
+	if err != nil {
+		return fmt.Errorf("bad CA cert key pair: %v", err)
+	}
+	_, err = tls.X509KeyPair([]byte(serverCertPEM), []byte(serverKeyPEM))
+	if err != nil {
+		return fmt.Errorf("bad server cert key pair: %v", err)
+	}
+	caCert, err := trivial.ParseCertificate([]byte(CACertPEM))
+	if err != nil {
+		return err
+	}
+	serverCert, err := trivial.ParseCertificate([]byte(serverCertPEM))
+	if err != nil {
+		return err
+	}
+	pool := x509.NewCertPool()
+	pool.AddCert(caCert)
+	opts := x509.VerifyOptions{
+		DNSName: "anything",
+		Roots: pool,
+	}
+	_, err = serverCert.Verify(opts)
+	if err != nil {
+		return fmt.Errorf("verification failed: %v", err)
+	}
+	return nil
+}
+
+func init() {
+//	if err := verifyCertificates(); err != nil {
+//		panic(err)
+//	}
+}
 
 // CACertPEM and CAKeyPEM make up a CA key pair.
 // CACertX509 and CAKeyRSA hold their parsed equivalents.
@@ -37,7 +75,35 @@ HOzuvYngJpoClGw0ipzJPoNZ2Z/GkdOWGByPeKu/8g==
 -----END RSA PRIVATE KEY-----
 `[1:]
 	CAKeyRSA = mustParseKeyPEM(CAKeyPEM)
+
+	// serverCertPEM holds a certificate siged by the above CA cert.
+	serverCertPEM = `
+-----BEGIN CERTIFICATE-----
+MIIBfDCCASigAwIBAgIBADALBgkqhkiG9w0BAQUwJjENMAsGA1UEChMEanVqdTEV
+MBMGA1UEAxMManVqdSB0ZXN0aW5nMB4XDTEyMTEyNDEzMDY1OVoXDTIyMTEyNDEz
+MTE1OVowGjEMMAoGA1UEChMDaG1tMQowCAYDVQQDEwEqMFkwCwYJKoZIhvcNAQEB
+A0oAMEcCQF9KBtClwqaJuvhRKNNdsxyrdVTfgNhLTf1DX+Z3iBTpvb8fxihC9xQv
+voslONe+wL1MQi8QkjUzex1Z7abC+m8CAwEAAaNSMFAwDgYDVR0PAQH/BAQDAgAQ
+MB0GA1UdDgQWBBRUQs95lLcaqz6iGce/APLVfdw5ZjAfBgNVHSMEGDAWgBRQqPrU
+s3Mlim0tNfp20ruYuj6LTTALBgkqhkiG9w0BAQUDQQBBBuMUIKFpSVjhm1ybbHnC
+BP6lvBILWjb6h7f0hFFQQq2Ks8Hr1cwoRNQQFe06qIb7GFhwu6RoY3BDRPAQbZG5
+-----END CERTIFICATE-----
+`[1:]
+
+	// serverKeyPEM holds the private key for serverCertPEM.
+	serverKeyPEM = `
+-----BEGIN RSA PRIVATE KEY-----
+MIIBOQIBAAJAX0oG0KXCpom6+FEo012zHKt1VN+A2EtN/UNf5neIFOm9vx/GKEL3
+FC++iyU4177AvUxCLxCSNTN7HVntpsL6bwIDAQABAkBP3qifspDZLpC9GqnxoJRE
+76JFJaHFqjkQk6yKbJ5viAUU+rrsKPuU8Sk1oP005QtzofWQKKP8dLZg50dCpDWB
+AiEAjViPqgn2tYt/64xJDUjOK1fMpY/yiK0aEmXFXTctgX8CIQCslXcTO03XeZpD
+0WJDDmEeex2gwAJC2SqtH3XOL3EfEQIgaNWmuJdrRHuTBUGnbRLy13LndmStnnHF
+RJ/3IowqVEECIQCVPzBZdkir1aJdkZ47RR0hwfBuSn3qF2m7i2BSLV7TMQIgR/0Q
+TgZwrr9JK+c8N+/YQ8zMv85a4POQHZnNHrVKeRQ=
+-----END RSA PRIVATE KEY-----
+`[1:]
 )
+
 
 func mustParseCertPEM(pemData string) *x509.Certificate {
 	b, _ := pem.Decode([]byte(pemData))
