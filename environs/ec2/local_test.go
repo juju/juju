c8946af51529e77840759713317de9a4d5711822
Dave Cheney <david.cheney@canonical.com>
2012-11-30 11:24:18 +1100

    fix merge conflict

--- environs/ec2/local_test.go
+++ environs/ec2/local_test.go
@@ -35,8 +35,8 @@ func registerLocalTests() {
 		"access-key":           "x",
 		"secret-key":           "x",
 		"authorized-keys":      "foo",
-		"ca-cert":              testing.CACertPEM,
-		"ca-private-key":       testing.CAKeyPEM,
+		"ca-cert":              testing.CACert,
+		"ca-private-key":       testing.CAKey,
 	}
 
 	Suite(&localServerSuite{
