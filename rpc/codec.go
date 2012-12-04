98446b2fb6b2a72f54494e4168a31684de6ea00a
Roger Peppe <roger.peppe@canonical.com>
2012-12-04 16:31:48 +0000

    rpc: added httpServerCodec

--- rpc/codec.go
+++ rpc/codec.go
@@ -1,8 +1,10 @@
 package rpc
 import (
 	"encoding/json"
+	"fmt"
 	"encoding/xml"
 	"io"
+	"net/http"
 )
 
 type generalServerCodec struct {
@@ -52,11 +54,70 @@ func NewXMLServerCodec(c io.ReadWriter) ServerCodec {
 	}
 }
 
-//
-//// NewHTTPCodec returns a codec which allows
-//// the use of the single HTTP request given in its arguments
-//// as a ServerCodec. URL parameters hold the arguments
-//// to the RPC; the response is written to w in JSON format.
-//func NewHTTPServerCodec(w http.ResponseWriter, r *http.Request) {
-//	
-//}
+type httpServerCodec struct {
+	done bool
+	w http.ResponseWriter
+	req *http.Request
+}
+
+// NewHTTPCodec returns a codec which allows
+// the use of the single HTTP request given in its arguments
+// as a ServerCodec. URL parameters hold the arguments
+// to the RPC (each individually encoded as a JSON value).
+// The response is written to w in JSON format.
+func NewHTTPServerCodec(w http.ResponseWriter, req *http.Request) ServerCodec {
+	return &httpServerCodec{
+		w: w,
+		req: req,
+	}
+}
+
+func (c *httpServerCodec) ReadRequestHeader(req *Request) error {
+	if c.done {
+		return io.EOF
+	}
+	c.done = true
+	req.Path = c.req.URL.Path
+	req.Seq = 0
+	return nil
+}
+
+func (c *httpServerCodec) ReadRequestBody(argp interface{}) error {
+	if err := c.req.ParseForm(); err != nil {
+		return err
+	}
+	// Quick hack: marshal all the parameters into
+	// JSON, then Unmarshal into argp.
+	m := make(map[string]json.RawMessage)
+	for k, vs := range c.req.Form {
+		m[k] = json.RawMessage(vs[0])
+	}
+	data, err := json.Marshal(m)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(data, argp)
+}
+
+func (c *httpServerCodec) WriteResponse(resp *Response, v interface{}) error {
+	type jsonError struct {
+		Error string
+		ErrorPath string
+	}
+	var data []byte
+	c.w.Header().Set("Content-Type", "application/json")
+	if resp.Error != "" {
+		c.w.WriteHeader(http.StatusBadRequest)
+		v = &jsonError{
+			Error: resp.Error,
+			ErrorPath: resp.ErrorPath,
+		}
+	}
+	data, err := json.Marshal(v)
+	if err != nil {
+		return err
+	}
+	c.w.Header().Set("Content-Length", fmt.Sprint(len(data)))
+	_, err = c.w.Write(data)
+	return err
+}
