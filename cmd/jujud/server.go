06bce8fd862a0649bc4f51c907ca3605458926ec
Roger Peppe <roger.peppe@canonical.com>
2012-12-05 17:44:56 +0000

    cmd/jujud: add server stub

---
+++ cmd/jujud/server.go
@@ -0,0 +1,17 @@
+package jujud
+
+// TODO
+//
+//// ServerAgent is a cmd.Command responsible for running
+//// the API server.
+//type ServerAgent struct {
+//	
+//	Conf AgentConf
+//}
+//
+//func (a *ServerAgent) Info() *cmd.Info {
+//	return &cmd.Info{"server", "", "run an API server", ""}
+//}
+//
+//func (a *ServerAgent) Init(f *gnuflag.FlagSet, args []string) error {
+//}
