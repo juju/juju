41e5aca4eed3fa1652a50b2eda562962977978ef
Dave Cheney <david.cheney@canonical.com>
2012-12-11 16:09:03 +1100

    fix race

--- state/watcher/watcher_test.go
+++ state/watcher/watcher_test.go
@@ -375,6 +375,10 @@ func (s *WatcherSuite) TestScale(c *C) {
 
 	c.Logf("Forcing a refresh...")
 	s.w.StartSync()
+	// There is a data race on log.Debug. To avoid this race stop the 
+	// watcher before restoring the value of log.Debug so the watcher
+	// does not observe a stale value of log.Debug.
+	defer s.w.Stop()
 
 	count, err := s.Session.DB("juju").C("test").Count()
 	c.Assert(err, IsNil)
