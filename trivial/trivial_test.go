d59691804fd96ef9e21bb5479f854b4271f3f5cc
Roger Peppe <roger.peppe@canonical.com>
2013-01-21 19:49:21 +0000

    trivial: revert spurious changes

--- trivial/trivial_test.go
+++ trivial/trivial_test.go
@@ -20,7 +20,7 @@ func (trivialSuite) TestAttemptTiming(c *C) {
 	const delta = 0.01e9
 	testAttempt := trivial.AttemptStrategy{
 		Total: 0.25e9,
-		Delay: 0.2e9,
+		Delay: 0.1e9,
 	}
 	want := []time.Duration{0, 0.1e9, 0.2e9, 0.2e9}
 	got := make([]time.Duration, 0, len(want)) // avoid allocation when testing timing
@@ -30,11 +30,11 @@ func (trivialSuite) TestAttemptTiming(c *C) {
 	}
 	got = append(got, time.Now().Sub(t0))
 	c.Assert(got, HasLen, len(want))
-	for i := range got {
+	for i, got := range want {
 		lo := want[i] - delta
 		hi := want[i] + delta
-		if got[i] < lo || got[i] > hi {
-			c.Errorf("attempt %d want %g got %g", i, want[i].Seconds(), got[i].Seconds())
+		if got < lo || got > hi {
+			c.Errorf("attempt %d want %g got %g", i, want[i].Seconds(), got.Seconds())
 		}
 	}
 }
