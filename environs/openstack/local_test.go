dbb8a516843d59d19a9f80c22178c2110906a525
Ian Booth <ian.booth@canonical.com>
2013-01-23 15:56:50 +1000

    Rework test to use new goose infrastructure

--- environs/openstack/local_test.go
+++ environs/openstack/local_test.go
@@ -3,17 +3,9 @@ package openstack_test
 import (
 	. "launchpad.net/gocheck"
 	"launchpad.net/goose/identity"
-	"launchpad.net/goose/testservices/identityservice"
-	"launchpad.net/goose/testservices/novaservice"
-	"launchpad.net/goose/testservices/swiftservice"
+	"launchpad.net/goose/testservices/openstack"
 	"net/http"
 	"net/http/httptest"
-)
-
-const (
-	baseIdentityURL = "/tokens"
-	baseNovaURL     = "/V1/1"
-	baseSwiftURL    = "/object-store"
 )
 
 // Register tests to run against a test Openstack instance (service doubles).
@@ -32,12 +24,9 @@ func registerServiceDoubleTests() {
 type localLiveSuite struct {
 	LiveTests
 	// The following attributes are for using the service doubles.
-	Server         *httptest.Server
-	Mux            *http.ServeMux
-	oldHandler     http.Handler
-	identityDouble *identityservice.UserPass
-	novaDouble     *novaservice.Nova
-	swiftDouble    http.Handler
+	Server     *httptest.Server
+	Mux        *http.ServeMux
+	oldHandler http.Handler
 }
 
 func (s *localLiveSuite) SetUpSuite(c *C) {
@@ -50,34 +39,8 @@ func (s *localLiveSuite) SetUpSuite(c *C) {
 	s.Server.Config.Handler = s.Mux
 
 	s.cred.URL = s.Server.URL
-	// Create the identity service.
-	s.identityDouble = identityservice.NewUserPass()
-	token := s.identityDouble.AddUser(s.cred.User, s.cred.Secrets)
-	s.Mux.Handle(baseIdentityURL, s.identityDouble)
-
-	// Register Swift endpoints with identity service.
-	ep := identityservice.Endpoint{
-		AdminURL:    s.Server.URL + baseSwiftURL,
-		InternalURL: s.Server.URL + baseSwiftURL,
-		PublicURL:   s.Server.URL + baseSwiftURL,
-		Region:      s.cred.Region,
-	}
-	service := identityservice.Service{"swift", "object-store", []identityservice.Endpoint{ep}}
-	s.identityDouble.AddService(service)
-	s.swiftDouble = swiftservice.New("localhost", baseSwiftURL+"/", token)
-	s.Mux.Handle(baseSwiftURL+"/", s.swiftDouble)
-
-	// Register Nova endpoints with identity service.
-	ep = identityservice.Endpoint{
-		AdminURL:    s.Server.URL + baseNovaURL,
-		InternalURL: s.Server.URL + baseNovaURL,
-		PublicURL:   s.Server.URL + baseNovaURL,
-		Region:      s.cred.Region,
-	}
-	service = identityservice.Service{"nova", "compute", []identityservice.Endpoint{ep}}
-	s.identityDouble.AddService(service)
-	s.novaDouble = novaservice.New("localhost", "V1", token, "1")
-	s.novaDouble.SetupHTTP(s.Mux)
+	openstack := openstack.New(s.Server.URL, s.cred.User, s.cred.Secrets, s.LiveTests.cred.Region)
+	openstack.SetupHTTP(s.Mux)
 
 	s.LiveTests.SetUpSuite(c)
 }
