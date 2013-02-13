dda79f76823958e51a1cbefaff0c12e8152144c3
John Arbash Meinel <john@arbash-meinel.com>
2013-02-13 14:59:51 +0400

    Merge the jujuc functionality into the jujud command.
    This means that now we expect symlinks to target the 'jujud' command,
    which will trigger the same behavior. The reason to combine them is
    to just decrease the size of the tools that need to be uploaded.

--- cmd/jujud/main.go
+++ cmd/jujud/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"launchpad.net/juju-core/cmd"
+	"launchpad.net/juju-core/worker/uniter/jujuc"
+	"net/rpc"
 	"os"
+	"path/filepath"
 )
 
 var jujudDoc = `
@@ -10,18 +14,104 @@ juju provides easy, intelligent service orchestration on top of environments
 such as OpenStack, Amazon AWS, or bare metal. jujud is a component of juju.
 
 https://juju.ubuntu.com/
+
+The jujud command can also forward invocations over RPC for execution by the
+juju unit agent. When used in this way, it expects to be called via a symlink
+named for the desired remote command, and expects JUJU_AGENT_SOCKET and
+JUJU_CONTEXT_ID be set in its environment.
 `
 
+func getenv(name string) (string, error) {
+	value := os.Getenv(name)
+	if value == "" {
+		return "", fmt.Errorf("%s not set", name)
+	}
+	return value, nil
+}
+
+func getwd() (string, error) {
+	dir, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+	abs, err := filepath.Abs(dir)
+	if err != nil {
+		return "", err
+	}
+	return abs, nil
+}
+
+// jujuCMain uses JUJU_CONTEXT_ID and JUJU_AGENT_SOCKET to ask a running unit agent
+// to execute a Command on our behalf. Individual commands should be exposed
+// by symlinking the command name to this executable.
+func jujuCMain(commandName string, args []string) (code int, err error) {
+	code = 1
+	contextId, err := getenv("JUJU_CONTEXT_ID")
+	if err != nil {
+		return
+	}
+	dir, err := getwd()
+	if err != nil {
+		return
+	}
+	req := jujuc.Request{
+		ContextId:   contextId,
+		Dir:         dir,
+		CommandName: commandName,
+		Args:        args[1:],
+	}
+	socketPath, err := getenv("JUJU_AGENT_SOCKET")
+	if err != nil {
+		return
+	}
+	client, err := rpc.Dial("unix", socketPath)
+	if err != nil {
+		return
+	}
+	defer client.Close()
+	var resp jujuc.Response
+	err = client.Call("Jujuc.Main", req, &resp)
+	if err != nil {
+		return
+	}
+	os.Stdout.Write(resp.Stdout)
+	os.Stderr.Write(resp.Stderr)
+	return resp.Code, nil
+}
+
 // Main registers subcommands for the jujud executable, and hands over control
-// to the cmd package. This function is not redundant with main, because it
-// provides an entry point for testing with arbitrary command line arguments.
-func Main(args []string) {
+// to the cmd package.
+func jujuDMain(args []string) (code int, err error) {
 	jujud := &cmd.SuperCommand{Name: "jujud", Doc: jujudDoc, Log: &cmd.Log{}}
 	jujud.Register(&BootstrapCommand{})
 	jujud.Register(&MachineAgent{})
 	jujud.Register(&UnitAgent{})
 	jujud.Register(&VersionCommand{})
-	os.Exit(cmd.Main(jujud, cmd.DefaultContext(), args[1:]))
+	code = cmd.Main(jujud, cmd.DefaultContext(), args[1:])
+	return code, nil
+}
+
+// This function is not redundant with main, because it provides an entry point
+// for testing with arbitrary command line arguments.
+
+func Main(args []string) {
+	var code int = 1
+	var err error
+	commandName := filepath.Base(args[0])
+	if commandName == "jujud" {
+		code, err = jujuDMain(args)
+	} else if commandName == "jujuc" {
+		fmt.Fprint(os.Stderr, jujudDoc)
+		code = 2
+		err = fmt.Errorf("jujuc should not be called directly")
+	} else {
+		_ = &cmd.SuperCommand{Name: "jujuc", Doc: jujudDoc, Log: &cmd.Log{}}
+		code, err = jujuCMain(commandName, args)
+	}
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+	}
+	os.Exit(code)
 }
 
 func main() {
