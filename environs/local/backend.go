3fb69e34f05ea9d4b42bedce9e2d1667aa84c0d4
Frank Mueller <themue@gmail.com>
2013-02-15 21:01:10 +0100

    local: added storage backend and tests

---
+++ environs/local/backend.go
@@ -0,0 +1,130 @@
+package local
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"os"
+	"path/filepath"
+	"sort"
+	"strings"
+	"syscall"
+)
+
+// storageBackend provides HTTP access to a defined path.
+type storageBackend struct {
+	environName string
+	uri         string
+	path        string
+}
+
+// ServeHTTP handles the HTTP requests to the container.
+func (s *storageBackend) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case "GET":
+		if strings.HasSuffix(req.URL.Path, "*") {
+			s.handleList(w, req)
+		} else {
+			s.handleGet(w, req)
+		}
+	case "PUT":
+		s.handlePut(w, req)
+	case "DELETE":
+		s.handleDelete(w, req)
+	default:
+		http.Error(w, "method "+req.Method+" is not supported", http.StatusMethodNotAllowed)
+	}
+}
+
+// handleGet returns a storage file to the client.
+func (s *storageBackend) handleGet(w http.ResponseWriter, req *http.Request) {
+	data, err := ioutil.ReadFile(filepath.Join(s.path, req.URL.Path))
+	if err != nil {
+		http.Error(w, fmt.Sprintf("404 %v", err), http.StatusNotFound)
+		return
+	}
+	w.Header().Set("Content-Type", "application/octet-stream")
+	w.Write(data)
+}
+
+// handleList returns the names in the storage to the client.
+func (s *storageBackend) handleList(w http.ResponseWriter, req *http.Request) {
+	fis, err := ioutil.ReadDir(s.path)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("404 %v", err), http.StatusNotFound)
+		return
+	}
+	prefix := req.URL.Path[:len(req.URL.Path)-1]
+	names := []string{}
+	for _, fi := range fis {
+		name := fi.Name()
+		if strings.HasPrefix(name, prefix) {
+			names = append(names, name)
+		}
+	}
+	sort.Strings(names)
+	data := []byte(strings.Join(names, "\n"))
+	w.Header().Set("Content-Type", "application/octet-stream")
+	w.Write(data)
+}
+
+// handlePut stores data from the client in the storage.
+func (s *storageBackend) handlePut(w http.ResponseWriter, req *http.Request) {
+	fp := filepath.Join(s.path, req.URL.Path)
+	out, err := os.Create(fp)
+	defer out.Close()
+	if err != nil {
+		http.Error(w, fmt.Sprintf("403 %v", err), http.StatusForbidden)
+		return
+	}
+	_, err = io.Copy(out, req.Body)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("403 %v", err), http.StatusForbidden)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+}
+
+// handleDelete removes data from the storage.
+func (s *storageBackend) handleDelete(w http.ResponseWriter, req *http.Request) {
+	fp := filepath.Join(s.path, req.URL.Path)
+	if err := os.Remove(fp); err != nil && !os.IsNotExist(err) {
+		http.Error(w, fmt.Sprintf("500 %v", err), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+}
+
+// listen starts an HTTP listener to serve the
+// provider storage.
+func listen(dataPath, environName, ip string, port int) (net.Listener, error) {
+	backend := &storageBackend{
+		environName: environName,
+		uri:         fmt.Sprintf("/%s/", environName),
+		path:        filepath.Join(dataPath, environName),
+	}
+	if err := os.MkdirAll(backend.path, 0777); err != nil {
+		return nil, err
+	}
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", ip, port))
+	if err != nil {
+		return nil, fmt.Errorf("cannot start listener: %v", err)
+	}
+	mux := http.NewServeMux()
+	mux.Handle(backend.uri, http.StripPrefix(backend.uri, backend))
+
+	go http.Serve(listener, mux)
+
+	return listener, nil
+}
+
+// isNotExist checks if a returned error shows that a file 
+// does not exist.
+func isNotExist(err error) bool {
+	if err, ok := err.(*os.PathError); ok && (os.IsNotExist(err.Err) || err.Err == syscall.ENOTDIR) {
+		return true
+	}
+	return false
+}
---
+++ environs/local/backend_test.go
@@ -0,0 +1,244 @@
+package local_test
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	. "launchpad.net/gocheck"
+	"net"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"launchpad.net/juju-core/environs/local"
+)
+
+func TestLocal(t *testing.T) {
+	TestingT(t)
+}
+
+type backendSuite struct {
+	listener net.Listener
+	dataDir  string
+}
+
+var _ = Suite(&backendSuite{})
+
+const environName = "test-environ"
+
+func (s *backendSuite) SetUpSuite(c *C) {
+	var err error
+	s.dataDir = c.MkDir()
+	s.listener, err = local.Listen(s.dataDir, environName, "127.0.0.1", 60006)
+	c.Assert(err, IsNil)
+
+	createTestData(c, s.dataDir)
+}
+
+func (s *backendSuite) TearDownSuite(c *C) {
+	s.listener.Close()
+}
+
+type getTest struct {
+	name    string
+	content string
+	status  int
+}
+
+var getTests = []getTest{
+	{
+		name:    "foo",
+		content: "this is file 'foo'",
+	},
+	{
+		name:    "bar",
+		content: "this is file 'bar'",
+	},
+	{
+		name:    "baz",
+		content: "this is file 'baz'",
+	},
+	{
+		name:    "yadda",
+		content: "this is file 'yadda'",
+	},
+	{
+		name:   "dummy",
+		status: 404,
+	},
+	{
+		name:   "../dummy",
+		status: 404,
+	},
+}
+
+func (s *backendSuite) TestGet(c *C) {
+	// Test retrieving a file from a storage.
+	check := func(gt getTest) {
+		url := fmt.Sprintf("http://localhost:60006/%s/%s", environName, gt.name)
+		resp, err := http.Get(url)
+		if gt.status != 0 {
+			c.Assert(resp.StatusCode, Equals, gt.status)
+			return
+		}
+		c.Assert(err, IsNil)
+		defer resp.Body.Close()
+		var buf bytes.Buffer
+		_, err = buf.ReadFrom(resp.Body)
+		c.Assert(err, IsNil)
+		c.Assert(buf.String(), Equals, gt.content)
+	}
+	for _, gt := range getTests {
+		check(gt)
+	}
+}
+
+type listTest struct {
+	prefix string
+	found  []string
+	status int
+}
+
+var listTests = []listTest{
+	{
+		prefix: "foo",
+		found:  []string{"foo"},
+	},
+	{
+		prefix: "ba",
+		found:  []string{"bar", "baz"},
+	},
+	{
+		prefix: "",
+		found:  []string{"bar", "baz", "foo", "yadda"},
+	},
+	{
+		prefix: "zzz",
+		found:  []string{""},
+	},
+	{
+		prefix: "../",
+		status: 404,
+	},
+}
+
+func (s *backendSuite) TestList(c *C) {
+	// Test listing file of a storage.
+	check := func(lt listTest) {
+		url := fmt.Sprintf("http://localhost:60006/%s/%s*", environName, lt.prefix)
+		resp, err := http.Get(url)
+		if lt.status != 0 {
+			c.Assert(resp.StatusCode, Equals, lt.status)
+			return
+		}
+		c.Assert(err, IsNil)
+		defer resp.Body.Close()
+		var buf bytes.Buffer
+		_, err = buf.ReadFrom(resp.Body)
+		c.Assert(err, IsNil)
+		names := strings.Split(buf.String(), "\n")
+		c.Assert(names, DeepEquals, lt.found)
+	}
+	for _, lt := range listTests {
+		check(lt)
+	}
+}
+
+type putTest struct {
+	name    string
+	content string
+	status  int
+}
+
+var putTests = []putTest{
+	{
+		name:    "porterhouse",
+		content: "this is the sent file 'porterhouse'",
+	},
+	{
+		name:   "../no-way",
+		status: 301,
+	},
+	{
+		name:    "cambridge",
+		content: "this is the sent file 'cambridge'",
+	},
+}
+
+func (s *backendSuite) TestPut(c *C) {
+	// Test sending a file to the storage.
+	check := func(pt putTest) {
+		url := fmt.Sprintf("http://localhost:60006/%s/%s", environName, pt.name)
+		req, err := http.NewRequest("PUT", url, bytes.NewBufferString(pt.content))
+		c.Assert(err, IsNil)
+		req.Header.Set("Content-Type", "application/octet-stream")
+		resp, err := http.DefaultClient.Do(req)
+		if pt.status != 0 {
+			c.Assert(resp.StatusCode, Equals, pt.status)
+			return
+		}
+		c.Assert(err, IsNil)
+
+		fp := filepath.Join(s.dataDir, environName, pt.name)
+		b, err := ioutil.ReadFile(fp)
+		c.Assert(err, IsNil)
+		c.Assert(string(b), Equals, pt.content)
+	}
+	for _, pt := range putTests {
+		check(pt)
+	}
+}
+
+type removeTest struct {
+	name    string
+	content string
+}
+
+var removeTests = []removeTest{
+	{
+		name:    "fox",
+		content: "the quick brown fox jumps over the lazy dog",
+	},
+	{
+		name: "dog",
+	},
+}
+
+func (s *backendSuite) TestRemove(c *C) {
+	// Test removing a file in the storage.
+	check := func(rt removeTest) {
+		fp := filepath.Join(s.dataDir, environName, rt.name)
+		err := ioutil.WriteFile(fp, []byte(rt.content), 0644)
+		c.Assert(err, IsNil)
+
+		url := fmt.Sprintf("http://localhost:60006/%s/%s", environName, rt.name)
+		req, err := http.NewRequest("DELETE", url, nil)
+		c.Assert(err, IsNil)
+		resp, err := http.DefaultClient.Do(req)
+		c.Assert(err, IsNil)
+		c.Assert(resp.StatusCode, Equals, 200)
+
+		_, err = os.Stat(fp)
+		c.Assert(err, ErrorMatches, ".*: no such file or directory")
+	}
+	for _, rt := range removeTests {
+		check(rt)
+	}
+}
+
+func createTestData(c *C, dataDir string) {
+	writeData := func(dir, name, data string) {
+		fn := filepath.Join(dir, name)
+		err := ioutil.WriteFile(fn, []byte(data), 0644)
+		c.Assert(err, IsNil)
+	}
+
+	dir := filepath.Join(dataDir, environName)
+
+	writeData(dir, "foo", "this is file 'foo'")
+	writeData(dir, "bar", "this is file 'bar'")
+	writeData(dir, "baz", "this is file 'baz'")
+	writeData(dir, "yadda", "this is file 'yadda'")
+}
