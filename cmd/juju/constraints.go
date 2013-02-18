2665434b4e95b56c7d497bb70d7490be77708572
William Reade <fwereade@gmail.com>
2013-02-18 01:45:11 +0100

    move constraints parsing into state; add get/set constraints commands

--- cmd/juju/constraints.go
+++ cmd/juju/constraints.go
@@ -2,103 +2,106 @@ package main
 
 import (
 	"fmt"
+	"launchpad.net/gnuflag"
+	"launchpad.net/juju-core/cmd"
+	"launchpad.net/juju-core/juju"
 	"launchpad.net/juju-core/state"
-	"math"
-	"strconv"
-	"strings"
 )
 
-// constraintsValue implements gnuflag.Value for a state.Constraints.
-type constraintsValue struct {
-	c *state.Constraints
+// GetConstraintsCommand shows the constraints for a service or environment.
+type GetConstraintsCommand struct {
+	EnvName     string
+	ServiceName string
+	out         cmd.Output
 }
 
-func (v *constraintsValue) String() string {
-	return v.c.String()
+func (c *GetConstraintsCommand) Info() *cmd.Info {
+	return &cmd.Info{"get-constraints", "", "view constraints", ""}
 }
 
-func (v *constraintsValue) Set(raw string) error {
-	args := strings.Split(strings.TrimSpace(raw), " ")
-	for _, arg := range args {
-		if arg == "" {
-			continue
-		}
-		eq := strings.Index(arg, "=")
-		if eq <= 0 {
-			return fmt.Errorf("malformed constraint %q", arg)
-		}
-		name, str := arg[:eq], arg[eq+1:]
-		var err error
-		switch name {
-		case "cpu-cores":
-			err = v.setCpuCores(str)
-		case "cpu-power":
-			err = v.setCpuPower(str)
-		case "mem":
-			err = v.setMem(str)
-		default:
-			return fmt.Errorf("unknown constraint %q", name)
-		}
-		if err != nil {
-			return fmt.Errorf("bad %q constraint: %v", name, err)
-		}
+func formatConstraints(value interface{}) ([]byte, error) {
+	return []byte(value.(state.Constraints).String()), nil
+}
+
+func (c *GetConstraintsCommand) Init(f *gnuflag.FlagSet, args []string) error {
+	addEnvironFlags(&c.EnvName, f)
+	c.out.AddFlags(f, "constraints", map[string]cmd.Formatter{
+		"constraints": formatConstraints,
+		"yaml":        cmd.FormatYaml,
+		"json":        cmd.FormatJson,
+	})
+	if err := f.Parse(true, args); err != nil {
+		return err
 	}
-	return nil
-}
-
-func (v *constraintsValue) setCpuCores(str string) (err error) {
-	if v.c.CpuCores != nil {
-		return fmt.Errorf("already set")
-	}
-	v.c.CpuCores, err = parseUint64(str)
-	return
-}
-
-func (v *constraintsValue) setCpuPower(str string) (err error) {
-	if v.c.CpuPower != nil {
-		return fmt.Errorf("already set")
-	}
-	v.c.CpuPower, err = parseUint64(str)
-	return
-}
-
-func parseUint64(str string) (*uint64, error) {
-	var value uint64
-	if str != "" {
-		if val, err := strconv.Atoi(str); err != nil || val < 0 {
-			return nil, fmt.Errorf("must be a non-negative integer")
-		} else {
-			value = uint64(val)
+	args = f.Args()
+	if len(args) > 0 {
+		if !state.IsServiceName(args[0]) {
+			return fmt.Errorf("invalid service name %q", args[0])
 		}
+		c.ServiceName, args = args[0], args[1:]
 	}
-	return &value, nil
+	return cmd.CheckEmpty(args)
 }
 
-func (v *constraintsValue) setMem(str string) error {
-	if v.c.Mem != nil {
-		return fmt.Errorf("already set")
+func (c *GetConstraintsCommand) Run(ctx *cmd.Context) (err error) {
+	conn, err := juju.NewConnFromName(c.EnvName)
+	if err != nil {
+		return err
 	}
-	var value uint64
-	if str != "" {
-		mult := 1.0
-		if m, ok := mbSuffixes[str[len(str)-1:]]; ok {
-			str = str[:len(str)-1]
-			mult = m
+	defer conn.Close()
+	var cons state.Constraints
+	if c.ServiceName == "" {
+		cons, err = conn.State.EnvironConstraints()
+	} else {
+		var svc *state.Service
+		if svc, err = conn.State.Service(c.ServiceName); err != nil {
+			return err
 		}
-		val, err := strconv.ParseFloat(str, 64)
-		if err != nil || val < 0 {
-			return fmt.Errorf("must be a non-negative float with optional M/G/T/P suffix")
-		}
-		val *= mult
-		value = uint64(math.Ceil(val))
+		cons, err = svc.Constraints()
 	}
-	v.c.Mem = &value
-	return nil
+	if err != nil {
+		return err
+	}
+	return c.out.Write(ctx, cons)
 }
 
-var mbSuffixes = map[string]float64{
-	"M": 1,
-	"G": 1024,
-	"T": 1024 * 1024,
-	"P": 1024 * 1024 * 1024,
+// SetConstraintsCommand shows the constraints for a service or environment.
+type SetConstraintsCommand struct {
+	EnvName     string
+	ServiceName string
+	Constraints state.Constraints
+}
+
+func (c *SetConstraintsCommand) Info() *cmd.Info {
+	return &cmd.Info{"set-constraints", "[key=[value],...]", "replace constraints", ""}
+}
+
+func (c *SetConstraintsCommand) Init(f *gnuflag.FlagSet, args []string) (err error) {
+	addEnvironFlags(&c.EnvName, f)
+	f.StringVar(&c.ServiceName, "s", "", "set service constraints")
+	f.StringVar(&c.ServiceName, "service", "", "")
+	if err := f.Parse(true, args); err != nil {
+		return err
+	}
+	if c.ServiceName != "" && !state.IsServiceName(c.ServiceName) {
+		return fmt.Errorf("invalid service name %q", c.ServiceName)
+	}
+	c.Constraints, err = state.ParseConstraints(f.Args()...)
+	return err
+}
+
+func (c *SetConstraintsCommand) Run(_ *cmd.Context) (err error) {
+	conn, err := juju.NewConnFromName(c.EnvName)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+	if c.ServiceName == "" {
+		return conn.State.SetEnvironConstraints(c.Constraints)
+	}
+	var svc *state.Service
+	if svc, err = conn.State.Service(c.ServiceName); err != nil {
+		return err
+	}
+	return svc.SetConstraints(c.Constraints)
 }
--- state/constraints.go
+++ state/constraints.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"labix.org/v2/mgo"
 	"labix.org/v2/mgo/txn"
+	"math"
+	"strconv"
 	"strings"
 )
 
@@ -15,16 +17,16 @@ type Constraints struct {
 
 	// CpuCores, if not nil, indicates that a machine must have at least that
 	// number of effective cores available.
-	CpuCores *uint64
+	CpuCores *uint64 `json:"cpu-cores,omitempty" yaml:"cpu-cores,omitempty"`
 
 	// CpuPower, if not nil, indicates that a machine must have at least that
 	// amount of CPU power available, where 100 CpuPower is considered to be
 	// equivalent to 1 Amazon ECU (or, roughly, a single 2007-era Xeon).
-	CpuPower *uint64
+	CpuPower *uint64 `json:"cpu-power,omitempty" yaml:"cpu-power,omitempty"`
 
 	// Mem, if not nil, indicates that a machine must have at least that many
 	// megabytes of RAM.
-	Mem *uint64
+	Mem *uint64 `json:"mem,omitempty" yaml:"mem,omitempty"`
 }
 
 // String expresses a Constraints in the language in which it was specified.
@@ -53,6 +55,105 @@ func uintStr(i uint64) string {
 	return fmt.Sprintf("%d", i)
 }
 
+// ParseConstraints constructs a Constraints from the supplied arguments,
+// each of which must contain only spaces and name=value pairs. If any
+// name is specified more than once, an error is returned.
+func ParseConstraints(args ...string) (Constraints, error) {
+	cons := &Constraints{}
+	for _, arg := range args {
+		raws := strings.Split(strings.TrimSpace(arg), " ")
+		for _, raw := range raws {
+			if err := cons.set(raw); err != nil {
+				return Constraints{}, err
+			}
+		}
+	}
+	return *cons, nil
+}
+
+func (c *Constraints) set(raw string) (err error) {
+	if raw == "" {
+		return nil
+	}
+	eq := strings.Index(raw, "=")
+	if eq <= 0 {
+		return fmt.Errorf("malformed constraint %q", raw)
+	}
+	name, str := raw[:eq], raw[eq+1:]
+	switch name {
+	case "cpu-cores":
+		err = c.setCpuCores(str)
+	case "cpu-power":
+		err = c.setCpuPower(str)
+	case "mem":
+		err = c.setMem(str)
+	default:
+		return fmt.Errorf("unknown constraint %q", name)
+	}
+	if err != nil {
+		return fmt.Errorf("bad %q constraint: %v", name, err)
+	}
+	return nil
+}
+
+func (c *Constraints) setCpuCores(str string) (err error) {
+	if c.CpuCores != nil {
+		return fmt.Errorf("already set")
+	}
+	c.CpuCores, err = parseUint64(str)
+	return
+}
+
+func (c *Constraints) setCpuPower(str string) (err error) {
+	if c.CpuPower != nil {
+		return fmt.Errorf("already set")
+	}
+	c.CpuPower, err = parseUint64(str)
+	return
+}
+
+func (c *Constraints) setMem(str string) error {
+	if c.Mem != nil {
+		return fmt.Errorf("already set")
+	}
+	var value uint64
+	if str != "" {
+		mult := 1.0
+		if m, ok := mbSuffixes[str[len(str)-1:]]; ok {
+			str = str[:len(str)-1]
+			mult = m
+		}
+		val, err := strconv.ParseFloat(str, 64)
+		if err != nil || val < 0 {
+			return fmt.Errorf("must be a non-negative float with optional M/G/T/P suffix")
+		}
+		val *= mult
+		value = uint64(math.Ceil(val))
+	}
+	c.Mem = &value
+	return nil
+}
+
+func parseUint64(str string) (*uint64, error) {
+	var value uint64
+	if str != "" {
+		if val, err := strconv.Atoi(str); err != nil || val < 0 {
+			return nil, fmt.Errorf("must be a non-negative integer")
+		} else {
+			value = uint64(val)
+		}
+	}
+	return &value, nil
+}
+
+var mbSuffixes = map[string]float64{
+	"M": 1,
+	"G": 1024,
+	"T": 1024 * 1024,
+	"P": 1024 * 1024 * 1024,
+}
+
+// constraintsDoc is the mongodb representation of a Constraints.
 type constraintsDoc struct {
 	CpuCores *uint64
 	CpuPower *uint64
