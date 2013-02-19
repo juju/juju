53270007c08e1c1136d8219bb63e316b62150373
Roger Peppe <roger.peppe@canonical.com>
2013-02-19 15:59:51 +0000

    state: fix dial panic

--- state/export_test.go
+++ state/export_test.go
@@ -1,4 +1,5 @@
 package state
+import "time"
 
 type (
 	CharmDoc    charmDoc
@@ -11,6 +12,16 @@ type (
 func (doc *MachineDoc) String() string {
 	m := &Machine{doc: machineDoc(*doc)}
 	return m.String()
+}
+
+var oldDialTimeout = dialTimeout
+
+func SetDialTimeout(d time.Duration) {
+	if d == 0 {
+		dialTimeout = oldDialTimeout
+	} else {
+		dialTimeout = d
+	}
 }
 
 func init() {
