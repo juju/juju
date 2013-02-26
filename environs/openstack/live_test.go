14e272059a07c10dac0a09da97427161d3348765
Ian Booth <ian.booth@canonical.com>
2013-02-26 11:22:29 +1000

    Move a test to a better home

--- environs/openstack/live_test.go
+++ environs/openstack/live_test.go
@@ -7,7 +7,6 @@ import (
 	. "launchpad.net/gocheck"
 	"launchpad.net/goose/client"
 	"launchpad.net/goose/identity"
-	"launchpad.net/goose/nova"
 	"launchpad.net/juju-core/environs"
 	"launchpad.net/juju-core/environs/jujutest"
 	"launchpad.net/juju-core/environs/openstack"
@@ -155,110 +154,4 @@ func (t *LiveTests) TestFindImageBadFlavor(c *C) {
 	c.Assert(ok, Equals, true)
 	c.Assert(imageId, Equals, "")
 	c.Assert(flavorId, Equals, "")
-}
-
-// ported from lp:juju/juju/providers/openstack/tests/test_machine.py
-var addressTests = []struct {
-	summary  string
-	private  []nova.IPAddress
-	public   []nova.IPAddress
-	networks []string
-	expected string
-	failure  error
-}{
-	{
-		summary:  "missing",
-		expected: "",
-		failure:  environs.ErrNoDNSName,
-	},
-	{
-		summary:  "empty",
-		private:  []nova.IPAddress{},
-		networks: []string{"private"},
-		expected: "",
-		failure:  environs.ErrNoDNSName,
-	},
-	{
-		summary:  "private only",
-		private:  []nova.IPAddress{{4, "127.0.0.4"}},
-		networks: []string{"private"},
-		expected: "127.0.0.4",
-		failure:  nil,
-	},
-	{
-		summary:  "private plus (HP cloud)",
-		private:  []nova.IPAddress{{4, "127.0.0.4"}, {4, "8.8.4.4"}},
-		networks: []string{"private"},
-		expected: "8.8.4.4",
-		failure:  nil,
-	},
-	{
-		summary:  "public only",
-		public:   []nova.IPAddress{{4, "8.8.8.8"}},
-		networks: []string{"", "public"},
-		expected: "8.8.8.8",
-		failure:  nil,
-	},
-	{
-		summary:  "public and private",
-		private:  []nova.IPAddress{{4, "127.0.0.4"}},
-		public:   []nova.IPAddress{{4, "8.8.4.4"}},
-		networks: []string{"private", "public"},
-		expected: "8.8.4.4",
-		failure:  nil,
-	},
-	{
-		summary:  "public private plus",
-		private:  []nova.IPAddress{{4, "127.0.0.4"}, {4, "8.8.4.4"}},
-		public:   []nova.IPAddress{{4, "8.8.8.8"}},
-		networks: []string{"private", "public"},
-		expected: "8.8.8.8",
-		failure:  nil,
-	},
-	{
-		summary:  "custom only",
-		private:  []nova.IPAddress{{4, "127.0.0.2"}},
-		networks: []string{"special"},
-		expected: "127.0.0.2",
-		failure:  nil,
-	},
-	{
-		summary:  "custom and public",
-		private:  []nova.IPAddress{{4, "127.0.0.2"}},
-		public:   []nova.IPAddress{{4, "8.8.8.8"}},
-		networks: []string{"special", "public"},
-		expected: "8.8.8.8",
-		failure:  nil,
-	},
-	{
-		summary:  "non-IPv4",
-		private:  []nova.IPAddress{{6, "::dead:beef:f00d"}},
-		networks: []string{"private"},
-		expected: "",
-		failure:  environs.ErrNoDNSName,
-	},
-}
-
-func (s *LiveTests) TestGetServerAddresses(c *C) {
-	for i, t := range addressTests {
-		c.Logf("#%d. %s -> %s (%v)", i, t.summary, t.expected, t.failure)
-		addresses := make(map[string][]nova.IPAddress)
-		if t.private != nil {
-			if len(t.networks) < 1 {
-				addresses["private"] = t.private
-			} else {
-				addresses[t.networks[0]] = t.private
-			}
-		}
-		if t.public != nil {
-			if len(t.networks) < 2 {
-				addresses["public"] = t.public
-			} else {
-				addresses[t.networks[1]] = t.public
-			}
-		}
-		addr, err := openstack.InstanceAddress(addresses)
-		c.Assert(err, Equals, t.failure)
-		c.Assert(addr, Equals, t.expected)
-	}
 }
