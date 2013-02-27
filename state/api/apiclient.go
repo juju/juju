3ac3df10b68a7acc185fcfacd01176b723961330
Brad Crittenden <bac@canonical.com>
2013-02-27 11:23:11 -0400

    Add API client and server for ServiceExpose

--- state/api/apiclient.go
+++ state/api/apiclient.go
@@ -56,6 +56,18 @@ func (c *Client) ServiceGet(service string) (*statecmd.ServiceGetResults, error)
 	return &results, nil
 }
 
+// ServiceExpose changes the juju-managed firewall to expose any ports that
+// were also explicitly marked by units as open.  It returns any errors or
+// nil.
+func (c *Client) ServiceExpose(service string) error {
+	params := statecmd.ServiceExposeParams{ServiceName: service}
+	err := c.st.client.Call("Client", "", "ServiceExpose", params, nil)
+	if err != nil {
+		return clientError(err)
+	}
+	return nil
+}
+
 // EnvironmentInfo holds information about the Juju environment.
 type EnvironmentInfo struct {
 	DefaultSeries string
