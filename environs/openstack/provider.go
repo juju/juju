c58ba41873e2ce5ca4c09bcb831f339709c6973f
Ian Booth <ian.booth@canonical.com>
2013-03-01 14:36:01 +1000

    Add missing code and tests to allow HP Cloud bootstrap

--- environs/openstack/provider.go
+++ environs/openstack/provider.go
@@ -21,6 +21,7 @@ import (
 	"launchpad.net/juju-core/trivial"
 	"launchpad.net/juju-core/version"
 	"net/http"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
@@ -155,6 +156,9 @@ func (p environProvider) PrivateAddress() (string, error) {
 
 func (p environProvider) InstanceId() (state.InstanceId, error) {
 	str, err := fetchInstanceUUID()
+	if err != nil {
+		str, err = fetchInstanceId()
+	}
 	return state.InstanceId(str), err
 }
 
@@ -163,11 +167,15 @@ func (p environProvider) InstanceId() (state.InstanceId, error) {
 // server when needed.
 var metadataHost = "http://169.254.169.254"
 
+// metadataJSON holds the path of the instance's JSON metadata.
+// It is a variable so that tests can change it when needed.
+var metadataJSON = "2012-08-10/meta-data.json"
+
 // fetchMetadata fetches a single atom of data from the openstack instance metadata service.
 // http://docs.amazonwebservices.com/AWSEC2/latest/UserGuide/AESDG-chapter-instancedata.html
-// (the same specs is implemented in OpenStack, hence the reference)
+// (the same specs is implemented in ec2, hence the reference)
 func fetchMetadata(name string) (value string, err error) {
-	uri := fmt.Sprintf("%s/2011-01-01/meta-data/%s", metadataHost, name)
+	uri := fmt.Sprintf("%s/latest/meta-data/%s", metadataHost, name)
 	data, err := retryGet(uri)
 	if err != nil {
 		return "", err
@@ -179,7 +187,7 @@ func fetchMetadata(name string) (value string, err error) {
 // the same thing as the "instance-id" in the ec2-style metadata. This only
 // works on openstack Folsom or later.
 func fetchInstanceUUID() (string, error) {
-	uri := fmt.Sprintf("%s/2012-08-10/meta-data.json", metadataHost)
+	uri := fmt.Sprintf("%s/%s", metadataHost, metadataJSON)
 	data, err := retryGet(uri)
 	if err != nil {
 		return "", err
@@ -194,6 +202,29 @@ func fetchInstanceUUID() (string, error) {
 		return "", fmt.Errorf("no instance UUID found")
 	}
 	return uuid.Uuid, nil
+}
+
+// fetchInstanceId fetches the openstack instance Id, which is derived
+// from the "instance-id" in the ec2-style metadata. The ec2 id contains
+// the numeric instance id encoded as hex with a "i-" prefix.
+// This numeric id is required for older versions of Openstack which do
+// not yet support providing UUID's via the metadata. HP Cloud is one such case.
+// Even though using the numeric id is deprecated in favour of using UUID, where
+// UUID is not yet supported, we need to revert to numeric id.
+func fetchInstanceId() (string, error) {
+	instId, err := fetchMetadata("instance-id")
+	if err != nil {
+		return "", err
+	}
+	if strings.Index(instId, "i-") >= 0 {
+		hex := strings.SplitAfter(instId, "i-")[1]
+		id, err := strconv.ParseInt("0x"+hex, 0, 32)
+		if err != nil {
+			return "", err
+		}
+		instId = fmt.Sprintf("%d", id)
+	}
+	return instId, nil
 }
 
 func retryGet(uri string) (data []byte, err error) {
@@ -910,7 +941,7 @@ func (e *environ) Destroy(ensureInsts []environs.Instance) error {
 }
 
 func (e *environ) AssignmentPolicy() state.AssignmentPolicy {
-	panic("AssignmentPolicy not implemented")
+	return state.AssignUnused
 }
 
 func (e *environ) globalGroupName() string {
