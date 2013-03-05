ca545795f501dfb399ee5388b64ac58f2193df60
Matthew Scott <matthew.scott@canonical.com>
2013-03-05 07:47:21 -0700

    Creating jujuapi package

---
+++ juju/jujuapi/apiconn_test.go
@@ -0,0 +1,47 @@
+package jujuapi
+
+import (
+	. "launchpad.net/gocheck"
+	"launchpad.net/juju-core/environs"
+	"launchpad.net/juju-core/environs/dummy"
+	"launchpad.net/juju-core/juju"
+	coretesting "launchpad.net/juju-core/testing"
+)
+
+type NewAPIConnSuite struct {
+	coretesting.LoggingSuite
+}
+
+var _ = Suite(&NewAPIConnSuite{})
+
+func (cs *NewAPIConnSuite) TearDownTest(c *C) {
+	dummy.Reset()
+	cs.LoggingSuite.TearDownTest(c)
+}
+
+func (*NewAPIConnSuite) TestNewConn(c *C) {
+	attrs := map[string]interface{}{
+		"name":            "erewhemos",
+		"type":            "dummy",
+		"state-server":    true,
+		"authorized-keys": "i-am-a-key",
+		"secret":          "pork",
+		"admin-secret":    "really",
+		"ca-cert":         coretesting.CACert,
+		"ca-private-key":  coretesting.CAKey,
+	}
+	env, err := environs.NewFromAttrs(attrs)
+	c.Assert(err, IsNil)
+	err = environs.Bootstrap(env, false, func(name string, cert, key []byte) error {
+		panic("writeCertAndKey called unexpectedly")
+	})
+	c.Assert(err, IsNil)
+
+	conn, err := juju.NewConn(env)
+	c.Assert(err, IsNil)
+
+	c.Assert(conn.Environ, Equals, env)
+	c.Assert(conn.State, NotNil)
+
+	c.Assert(conn.Close(), IsNil)
+}
