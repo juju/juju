7dcd30f83992acb5addbd00167f35cfeba8cc7c0
Francesco Banconi <francesco.banconi@canonical.com>
2013-03-05 13:59:48 -0500

    Changes as per review.

--- state/annotator.go
+++ state/annotator.go
@@ -15,35 +15,42 @@ type annotator struct {
 }
 
 // SetAnnotation adds a key/value pair to annotations in MongoDB and the annotator.
-func (a annotator) SetAnnotation(key, value string) error {
+func (a *annotator) SetAnnotation(key, value string) error {
 	if strings.Contains(key, ".") {
 		return fmt.Errorf("invalid key %q", key)
-	}
-	ops := []txn.Op{{
-		C:      a.coll,
-		Id:     a.id,
-		Assert: isAliveDoc,
-		Update: D{{"$set", D{{"annotations." + key, value}}}},
-	}}
-	if err := a.st.runner.Run(ops, "", nil); err != nil {
-		return fmt.Errorf("cannot set annotation %q = %q: %v", key, value, onAbort(err, errNotAlive))
 	}
 	if *a.annotations == nil {
 		*a.annotations = make(map[string]string)
 	}
-	(*a.annotations)[key] = value
+	if value == "" {
+		// Delete a key/value pair in MongoDB.
+		ops := []txn.Op{{
+			C:      a.coll,
+			Id:     a.id,
+			Assert: isAliveDoc,
+			Update: D{{"$unset", D{{"annotations." + key, true}}}},
+		}}
+		if err := a.st.runner.Run(ops, "", nil); err != nil {
+			return fmt.Errorf("cannot delete annotation %q: %v", key, onAbort(err, errNotAlive))
+		}
+		delete(*a.annotations, key)
+	} else {
+		// Set a key/value pair in MongoDB.
+		ops := []txn.Op{{
+			C:      a.coll,
+			Id:     a.id,
+			Assert: isAliveDoc,
+			Update: D{{"$set", D{{"annotations." + key, value}}}},
+		}}
+		if err := a.st.runner.Run(ops, "", nil); err != nil {
+			return fmt.Errorf("cannot set annotation %q = %q: %v", key, value, onAbort(err, errNotAlive))
+		}
+		(*a.annotations)[key] = value
+	}
 	return nil
 }
 
 // Annotation returns the annotation value corresponding to the given key.
 func (a annotator) Annotation(key string) string {
 	return (*a.annotations)[key]
-}
-
-// RemoveAnnotation removes the annotation value corresponding to the given key.
-func (a annotator) RemoveAnnotation(key string) error {
-	if _, ok := (*a.annotations)[key]; ok {
-		return a.SetAnnotation(key, "")
-	}
-	return nil
 }
