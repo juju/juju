3d867b96350cb537c0a8747d4a039220c4e00a6b
Brad Crittenden <bac@canonical.com>
2013-03-06 17:13:50 -0500

    More tests, last one fails

--- state/statecmd/deploy_test.go
+++ state/statecmd/deploy_test.go
@@ -15,7 +15,7 @@ type DeployLocalSuite struct {
 	repo          *charm.LocalRepository
 	defaultSeries string
 	seriesPath    string
-	repoPath      string
+	charmUrl      *charm.URL
 }
 
 // Run-time check to ensure DeployLocalSuite implements the Suite
@@ -30,6 +30,9 @@ func (s *DeployLocalSuite) SetUpTest(c *C) {
 	s.seriesPath = filepath.Join(repoPath, s.defaultSeries)
 	err := os.Mkdir(s.seriesPath, 0777)
 	c.Assert(err, IsNil)
+	coretesting.Charms.BundlePath(s.seriesPath, "mysql")
+	s.charmUrl, err = charm.InferURL("local:mysql", s.defaultSeries)
+	c.Assert(err, IsNil)
 }
 
 func (s *DeployLocalSuite) TearDownTest(c *C) {
@@ -42,10 +45,32 @@ func (s *DeployLocalSuite) TestBadCharmUrl(c *C) {
 	c.Assert(err, ErrorMatches, "cannot get latest charm revision: no charms found matching \"local:precise/notarealcharm\"")
 }
 
-func (s *DeployLocalSuite) TestDeployDefaultName(c *C) {
-	coretesting.Charms.BundlePath(s.seriesPath, "mysql")
-	charmurl, err := charm.InferURL("local:mysql", s.defaultSeries)
-	svc, err := statecmd.ServiceDeploy(s.Conn, charmurl, s.repo, false, "", 1, nil, "")
+func (s *DeployLocalSuite) TestDefaultName(c *C) {
+	svc, err := statecmd.ServiceDeploy(s.Conn, s.charmUrl, s.repo, false, "", 1, nil, "")
+	c.Assert(err, IsNil)
+	c.Assert(svc.Name(), Equals, "mysql")
+}
+
+func (s *DeployLocalSuite) TestCustomName(c *C) {
+	svc, err := statecmd.ServiceDeploy(s.Conn, s.charmUrl, s.repo, false, "yoursql", 1, nil, "")
+	c.Assert(err, IsNil)
+	c.Assert(svc.Name(), Equals, "yoursql")
+	units, err := svc.AllUnits()
+	c.Assert(err, IsNil)
+	c.Assert(len(units), Equals, 1)
+}
+
+func (s *DeployLocalSuite) TestSetNumUnits(c *C) {
+	svc, err := statecmd.ServiceDeploy(s.Conn, s.charmUrl, s.repo, false, "", 3, nil, "")
+	c.Assert(err, IsNil)
+	units, err := svc.AllUnits()
+	c.Assert(err, IsNil)
+	c.Assert(len(units), Equals, 3)
+}
+
+func (s *DeployLocalSuite) TestBumpRevision(c *C) {
+	curl := coretesting.Charms.ClonedURL(s.repo.Path, s.defaultSeries, "mysql")
+	svc, err := statecmd.ServiceDeploy(s.Conn, curl, s.repo, true, "", 1, nil, "")
 	c.Assert(err, IsNil)
 	c.Assert(svc.Name(), Equals, "mysql")
 }
