c09092f27babd0569df02b1f2ff97271385ef5e7
Roger Peppe <roger.peppe@canonical.com>
2013-03-06 11:44:27 -0500

    state/api/params: new package

--- state/statecmd/unexpose.go
+++ state/statecmd/unexpose.go
@@ -4,16 +4,12 @@ package statecmd
 
 import (
 	"launchpad.net/juju-core/state"
+	"launchpad.net/juju-core/state/api/params"
 )
-
-// ServiceUnexposeParams stores parameters for making the ServiceUnexpose call.
-type ServiceUnexposeParams struct {
-	ServiceName string
-}
 
 // ServiceUnexpose changes the juju-managed firewall to unexpose any ports that
 // were also explicitly marked by units as open.
-func ServiceUnexpose(state *state.State, args ServiceUnexposeParams) error {
+func ServiceUnexpose(state *state.State, args params.ServiceUnexpose) error {
 	svc, err := state.Service(args.ServiceName)
 	if err != nil {
 		return err
