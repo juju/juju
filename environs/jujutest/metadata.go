1fb2c68a92e5f442d29e6a422091522a2d192c90
John Arbash Meinel <john@arbash-meinel.com>
2013-03-07 15:48:26 -0500

    Start working on a RoundTripper instead of a VirtualFileSystem.

--- environs/jujutest/metadata.go
+++ environs/jujutest/metadata.go
@@ -12,6 +12,12 @@ type VirtualFile struct {
 	fc *FileContent
 }
 
+type BufferCloser struct {
+	*bytes.Buffer
+}
+
+func (bc BufferCloser) Close() error { return nil }
+
 var _ http.File = (*VirtualFile)(nil)
 
 func (f *VirtualFile) Close() error {
@@ -78,3 +84,36 @@ func (fi *VirtualFileInfo) IsDir() bool {
 }
 
 func (fi *VirtualFileInfo) Sys() interface{} { return nil }
+
+
+type VirtualRoundTripper struct {
+	contents []FileContent
+}
+
+var _ http.RoundTripper = (*VirtualRoundTripper)(nil)
+
+func (v *VirtualRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
+	res := &http.Response{Proto: "HTTP/1.0",
+		ProtoMajor: 1,
+		Header: make(http.Header),
+		Close: true,
+	}
+	for _, fc := range v.contents {
+		if fc.Name == req.URL.Path {
+			res.Status = "200 OK"
+			res.StatusCode = http.StatusOK
+			res.ContentLength = int64(len(fc.Content))
+			res.Body = BufferCloser{bytes.NewBufferString(fc.Content)}
+			return res, nil
+		}
+	}
+	res.Status = "404 Not Found"
+	res.StatusCode = http.StatusNotFound
+	res.ContentLength = 0
+	res.Body = BufferCloser{bytes.NewBufferString("")}
+	return res, nil
+}
+
+func NewVirtualRoundTripper(contents []FileContent) *VirtualRoundTripper {
+	return &VirtualRoundTripper{contents}
+}
--- environs/jujutest/metadata_test.go
+++ environs/jujutest/metadata_test.go
@@ -3,6 +3,8 @@ package jujutest
 import (
 	"io/ioutil"
 	. "launchpad.net/gocheck"
+	"net/http"
+	"net/url"
 	"os"
 )
 
@@ -39,4 +41,38 @@ func (s *metadataSuite) TestVFSStat(c *C) {
 	c.Assert(fi, NotNil)
 	c.Assert(fi.Size(), Equals, int64(len("a-content")))
 
+}
+
+func (s *metadataSuite) TestVirtualRoundTripper(c *C) {
+	a_content := "a-content"
+	vrt := NewVirtualRoundTripper([]FileContent{
+		{"a", a_content},
+		{"b", "b-content"},
+	})
+	c.Assert(vrt, NotNil)
+	req := &http.Request{URL: &url.URL{Path: "a"}}
+	resp, err := vrt.RoundTrip(req)
+	c.Assert(err, IsNil)
+	c.Assert(resp, NotNil)
+	content, err := ioutil.ReadAll(resp.Body)
+	c.Assert(string(content), Equals, a_content)
+	c.Assert(resp.ContentLength, Equals, int64(len(a_content)))
+	c.Assert(resp.StatusCode, Equals, http.StatusOK)
+	c.Assert(resp.Status, Equals, "200 OK")
+}
+
+func (s *metadataSuite) TestVirtualRoundTripperMissing(c *C) {
+	vrt := NewVirtualRoundTripper([]FileContent{
+		{"a", "a-content"},
+	})
+	c.Assert(vrt, NotNil)
+	req := &http.Request{URL: &url.URL{Path: "no-such-file"}}
+	resp, err := vrt.RoundTrip(req)
+	c.Assert(err, IsNil)
+	c.Assert(resp, NotNil)
+	content, err := ioutil.ReadAll(resp.Body)
+	c.Assert(string(content), Equals, "")
+	c.Assert(resp.ContentLength, Equals, int64(0))
+	c.Assert(resp.StatusCode, Equals, http.StatusNotFound)
+	c.Assert(resp.Status, Equals, "404 Not Found")
 }
