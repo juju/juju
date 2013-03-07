ee9e40c6794e5045d7aa54e476e586b5334d7954
Benji York <benji.york@canonical.com>
2013-03-07 09:23:15 -0600

    not working yet

--- state/statecmd/resolved_test.go
+++ state/statecmd/resolved_test.go
@@ -3,17 +3,13 @@ package statecmd_test
 import (
 	. "launchpad.net/gocheck"
 	"launchpad.net/juju-core/charm"
-	"launchpad.net/juju-core/environs"
-	"launchpad.net/juju-core/juju"
-	"launchpad.net/juju-core/juju/testing"
 	"launchpad.net/juju-core/state"
 	"launchpad.net/juju-core/state/statecmd"
-	coretesting "launchpad.net/juju-core/testing"
+	"launchpad.net/juju-core/testing"
 )
 
 type ResolvedSuite struct {
-	testing.JujuConnSuite
-	conn *juju.Conn
+        testing.JujuConnSuite
 	repo *charm.LocalRepository
 }
 
@@ -25,36 +21,20 @@ func panicWrite(name string, cert, key []byte) error {
 }
 
 func (s *ResolvedSuite) SetUpTest(c *C) {
-	attrs := map[string]interface{}{
-		"name":            "erewhemos",
-		"type":            "dummy",
-		"state-server":    true,
-		"authorized-keys": "i-am-a-key",
-		"admin-secret":    "deploy-test-secret",
-		"ca-cert":         coretesting.CACert,
-		"ca-private-key":  coretesting.CAKey,
-	}
-	environ, err := environs.NewFromAttrs(attrs)
-	c.Assert(err, IsNil)
-	err = environs.Bootstrap(environ, false, panicWrite)
-	c.Assert(err, IsNil)
-	s.conn, err = juju.NewConn(environ)
-	c.Assert(err, IsNil)
-	s.repo = &charm.LocalRepository{Path: c.MkDir()}
+	s.JujuConnSuite.SetUpTest(c)
 }
 
 func (s *ResolvedSuite) TearDownTest(c *C) {
-	c.Assert(s.conn.Close(), IsNil)
-	s.Conn = nil
+	s.JujuConnSuite.TearDownTest(c)
 }
 
 func (s *ResolvedSuite) TestMarkResolved(c *C) {
-	curl := coretesting.Charms.ClonedURL(s.repo.Path, "series", "riak")
-	sch, err := s.conn.PutCharm(curl, s.repo, false)
+	curl := testing.Charms.ClonedURL(s.repo.Path, "series", "riak")
+	sch, err := s.Conn.PutCharm(curl, s.repo, false)
 	c.Assert(err, IsNil)
-	svc, err := s.conn.State.AddService("testriak", sch)
+	svc, err := s.Conn.State.AddService("testriak", sch)
 	c.Assert(err, IsNil)
-	us, err := s.conn.AddUnits(svc, 1)
+	us, err := s.Conn.AddUnits(svc, 1)
 	c.Assert(err, IsNil)
 	u := us[0]
 
