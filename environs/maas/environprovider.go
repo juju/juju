ca9833511296d97e955b244d56ce31faa6cff539
Jeroen Vermeulen <jtv@canonical.com>
2013-03-07 16:26:55 +0700

    Missing import.

--- environs/maas/environprovider.go
+++ environs/maas/environprovider.go
@@ -4,6 +4,7 @@ import (
 	"launchpad.net/juju-core/environs"
 	"launchpad.net/juju-core/environs/config"
 	"launchpad.net/juju-core/log"
+	"launchpad.net/juju-core/state"
 )
 
 type maasEnvironProvider struct{}
