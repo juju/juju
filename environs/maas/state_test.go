1c6ab9d7ff6ece53e97957dd0be6b56208bac126
Jeroen Vermeulen <jtv@canonical.com>
2013-03-08 11:06:06 +0700

    Compile fixes.

--- environs/maas/state_test.go
+++ environs/maas/state_test.go
@@ -2,6 +2,7 @@ package maas
 
 import (
 	. "launchpad.net/gocheck"
+	"launchpad.net/juju-core/environs"
 )
 
 type StateSuite struct {
@@ -11,6 +12,6 @@ type StateSuite struct {
 var _ = Suite(new(StateSuite))
 
 func (suite *StateSuite) TestLoadStateReturnsNotFoundForMissingFile(c *C) {
-	_, err := suite.loadState()
+	_, err := suite.environ.loadState()
 	c.Check(err, FitsTypeOf, environs.NotFoundError{})
 }
