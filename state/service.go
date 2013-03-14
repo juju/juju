a39ffeb948b28022508f2dcdb539da4d515245ab
William Reade <fwereade@gmail.com>
2013-03-14 09:45:13 +0100

    slightly better checks for service destroy ops

--- state/service.go
+++ state/service.go
@@ -164,12 +164,25 @@ func (s *Service) destroyOps() ([]txn.Op, error) {
 		return append(ops, s.removeOps(hasLastRefs)...), nil
 	}
 	// In all other cases, service removal will be handled as a consequence
-	// of the removal of the last unit or relation referencing it. It is not
-	// simple, and maybe not possible, to build a set of assertions that
-	// correctly handle all possible underlying state changes in terms of
-	// unit and relation count alone, so we fall back to asserting that the
-	// service document has not changed since it was last inspected.
-	notLastRefs := D{{"txn-revno", s.doc.TxnRevno}}
+	// of the removal of the last unit or relation referencing it. If any
+	// relations have been removed, they'll be caught by the operations
+	// collected above; but if any has been added, we need to abort and add
+	// a destroy op for that relation too. In combination, it's enough to
+	// check for count equality: an add/remove will not touch the count, but
+	// will be caught by virtue of being a remove.
+	notLastRefs := D{
+		{"life", Alive},
+		{"relationcount", s.doc.RelationCount},
+	}
+	// With respect to unit count, a changing value doesn't matter, so long
+	// as the count's equality with zero does not change, because all we care
+	// about is that *some* unit is, or is not, keeping the service from
+	// being removed: the difference between 1 unit and 1000 is irrelevant.
+	if s.doc.UnitCount > 0 {
+		notLastRefs = append(notLastRefs, D{{"unitcount", D{{"$gt", 0}}}}...)
+	} else {
+		notLastRefs = append(notLastRefs, D{{"unitcount", 0}}...)
+	}
 	update := D{{"$set", D{{"life", Dying}}}}
 	if removeCount != 0 {
 		decref := D{{"$inc", D{{"relationcount", -removeCount}}}}
