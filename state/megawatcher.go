f8f462867e1e022db8ea0350424881963a6376a2
Roger Peppe <roger.peppe@canonical.com>
2013-03-18 12:47:55 +0000

    wip

--- state/megawatcher.go
+++ state/megawatcher.go
@@ -3,7 +3,9 @@ package state
 import (
 	"container/list"
 	"errors"
+	"fmt"
 	"labix.org/v2/mgo"
+	"launchpad.net/juju-core/log"
 	"launchpad.net/juju-core/state/api/params"
 	"launchpad.net/juju-core/state/watcher"
 	"launchpad.net/tomb"
@@ -333,6 +335,122 @@ func (aw *allWatcher) leave(w *xStateWatcher) {
 			aw.all.decRef(entry, aw.backing.entityIdForInfo(entry.info))
 		}
 		e = next
+	}
+}
+
+// allWatcherStateBacking implements allWatcherBacking by
+// fetching entities from the State.
+type allWatcherStateBacking struct {
+	st *State
+	// collections
+	collectionByName map[string]allWatcherStateCollection
+	collectionByKind map[string]allWatcherStateCollection
+}
+
+// allWatcherStateCollection holds information about a
+// collection watched by an allWatcher and the
+// type of value we use to store entity information
+// for that collection.
+type allWatcherStateCollection struct {
+	c *mgo.Collection
+	// infoSliceType stores the type of a slice of the info type
+	// that we use for this collection.  In Go 1.1 we can change
+	// this to use the type itself, as we'll have reflect.SliceOf.
+	infoSliceType reflect.Type
+}
+
+func newAllWatcherStateBacking(st *State) allWatcherBacking {
+	b := &allWatcherStateBacking{
+		st:               st,
+		collectionByName: make(map[string]allWatcherStateCollection),
+		collectionByKind: make(map[string]allWatcherStateCollection),
+	}
+	collections := []allWatcherStateCollection{{
+		c:             st.machines,
+		infoSliceType: reflect.TypeOf([]params.MachineInfo(nil)),
+	}, {
+		c:             st.units,
+		infoSliceType: reflect.TypeOf([]params.UnitInfo(nil)),
+	}, {
+		c:             st.services,
+		infoSliceType: reflect.TypeOf([]params.ServiceInfo(nil)),
+	}, {
+		c:             st.relations,
+		infoSliceType: reflect.TypeOf([]params.RelationInfo(nil)),
+	}}
+	// Populate the collection maps from the above set of collections.
+	for _, c := range collections {
+		// Create a new instance of the info type so we can
+		// find out its kind.
+		info := reflect.New(c.infoSliceType.Elem()).Interface().(params.EntityInfo)
+		kind := info.EntityKind()
+		if _, ok := b.collectionByKind[kind]; ok {
+			panic(fmt.Errorf("duplicate collection kind %q", kind))
+		}
+		b.collectionByKind[kind] = c
+		if _, ok := b.collectionByName[c.c.Name]; ok {
+			panic(fmt.Errorf("duplicate collection name %q", kind))
+		}
+		b.collectionByName[c.c.Name] = c
+	}
+	return b
+}
+
+// watch watches all the collections.
+func (b *allWatcherStateBacking) watch(in chan<- watcher.Change) {
+	for _, c := range b.collectionByName {
+		b.st.watcher.WatchCollection(c.c.Name, in)
+	}
+}
+
+// watch unwatches all the collections.
+func (b *allWatcherStateBacking) unwatch(in chan<- watcher.Change) {
+	for _, c := range b.collectionByName {
+		b.st.watcher.UnwatchCollection(c.c.Name, in)
+	}
+}
+
+// getAll fetches all items that we want to watch from the state.
+func (b *allWatcherStateBacking) getAll(all *allInfo) error {
+	// TODO(rog) fetch collections concurrently?
+	for _, c := range b.collectionByName {
+		log.Printf("making new slice ptr from %v", c.infoSliceType)
+		infoSlicePtr := reflect.New(c.infoSliceType).Interface()
+		err := c.c.Find(nil).All(infoSlicePtr)
+		if err != nil {
+			return fmt.Errorf("cannot get all %s: %v", c.c.Name, err)
+		}
+		infos := reflect.ValueOf(infoSlicePtr).Elem()
+		for i := 0; i < infos.Len(); i++ {
+			info := infos.Index(i).Addr().Interface().(params.EntityInfo)
+			all.add(b.entityIdForInfo(info), info)
+		}
+	}
+	return nil
+}
+
+func (b *allWatcherStateBacking) fetch(id entityId) (params.EntityInfo, error) {
+	c, ok := b.collectionByName[id.collection]
+	if !ok {
+		log.Printf("collections: %+v", b.collectionByName)
+		panic(fmt.Errorf("unknown collection %q in fetch request", id.collection))
+	}
+	info := reflect.New(c.infoSliceType.Elem()).Interface().(params.EntityInfo)
+	if err := c.c.FindId(id.id).One(info); err != nil {
+		return nil, err
+	}
+	return info, nil
+}
+
+// entityIdForInfo returns the entity id of the given entity document.
+func (b *allWatcherStateBacking) entityIdForInfo(info params.EntityInfo) entityId {
+	c, ok := b.collectionByKind[info.EntityKind()]
+	if !ok {
+		panic(fmt.Errorf("entity with unknown kind %q", info.EntityKind()))
+	}
+	return entityId{
+		collection: c.c.Name,
+		id:         info.EntityId(),
 	}
 }
 
