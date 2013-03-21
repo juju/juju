4fe64bd9db1222d5134abbdedfdd3351ecc2be71
Francesco Banconi <francesco.banconi@canonical.com>
2013-03-21 16:47:35 +0100

    Checkpoint.

--- state/annotator.go
+++ state/annotator.go
@@ -45,86 +45,52 @@ func (a *annotator) SetAnnotations(pairs map[string]string) error {
 			toUpdate["annotations."+key] = value
 		}
 	}
-	for i := 0; i < 3; i++ {
-		ops, err := a.setAnnotationsOps(toInsert, toUpdate, toRemove)
+	// Perform two attempts to update annotations in order to avoid possible
+	// race conditions when the document is missing.
+	for i := 0; i < 2; i++ {
+		op, err := a.setAnnotationsOp(toInsert, toUpdate, toRemove)
 		if err != nil {
 			return err
 		}
-		if len(ops) == 0 {
-			return nil
-		}
-		if err := a.st.runner.Run(ops, "", nil); err == nil {
+		if err := a.st.runner.Run([]txn.Op{op}, "", nil); err == nil {
 			return nil
 		} else if err != txn.ErrAborted {
 			return fmt.Errorf("cannot update annotations on %s: %v", a.globalKey, err)
 		}
-		if hasAnnotations, err := a.hasAnnotations(pairs); err != nil {
-			return err
-		} else if hasAnnotations {
-			return nil
-		}
+	}
+	// Check that the annotator entity was not previously destroyed.
+	if count, err := a.st.annotations.FindId(a.globalKey).Count(); err != nil {
+		return err
+	} else if count == 0 {
+		return fmt.Errorf("%s no longer exists", a.entityName)
 	}
 	return fmt.Errorf("cannot update annotations on %s: %v", a.globalKey, ErrExcessiveContention)
 }
 
-// setAnnotationsOps returns the operations required to insert, update or
+// setAnnotationsOp returns the operation required to insert, update or
 // remove annotations in MongoDB.
-func (a *annotator) setAnnotationsOps(toInsert, toUpdate map[string]string, toRemove map[string]bool) ([]txn.Op, error) {
+func (a *annotator) setAnnotationsOp(toInsert, toUpdate map[string]string, toRemove map[string]bool) (txn.Op, error) {
 	id := a.globalKey
 	coll := a.st.annotations.Name
-	var ops []txn.Op
 	if count, err := a.st.annotations.FindId(id).Count(); err != nil {
-		return nil, err
+		return txn.Op{}, err
 	} else if count == 0 {
 		// The document is missing: no need to remove pairs.
 		// Insert pairs if required.
-		if len(toInsert) == 0 {
-			return ops, nil
-		}
-		insertOp := txn.Op{
+		return txn.Op{
 			C:      coll,
 			Id:     id,
 			Assert: txn.DocMissing,
 			Insert: &annotatorDoc{id, a.entityName, toInsert},
-		}
-		return append(ops, insertOp), nil
+		}, nil
 	}
 	// The document exists.
-	if len(toRemove) != 0 {
-		// Remove pairs.
-		removeOp := txn.Op{
-			C:      coll,
-			Id:     id,
-			Assert: txn.DocExists,
-			Update: D{{"$unset", toRemove}},
-		}
-		ops = append(ops, removeOp)
-	}
-	if len(toUpdate) != 0 {
-		// Insert/update pairs.
-		updateOp := txn.Op{
-			C:      coll,
-			Id:     id,
-			Assert: txn.DocExists,
-			Update: D{{"$set", toUpdate}},
-		}
-		ops = append(ops, updateOp)
-	}
-	return ops, nil
-}
-
-// hasAnnotations checks if the provided annotations already exist.
-func (a *annotator) hasAnnotations(pairs map[string]string) (bool, error) {
-	ann, err := a.Annotations()
-	if err != nil {
-		return false, err
-	}
-	for key, value := range pairs {
-		if ann[key] != value {
-			return false, nil
-		}
-	}
-	return true, nil
+	return txn.Op{
+		C:      coll,
+		Id:     id,
+		Assert: txn.DocExists,
+		Update: D{{"$set", toUpdate}, {"$unset", toRemove}},
+	}, nil
 }
 
 // Annotations returns all the annotations corresponding to an entity.
