21d2f1c19ffbe5e9b5105ca8d9c20000b377d3be
Raphael Badin <raphael.badin@canonical.com>
2013-04-04 14:30:22 +0200

    Use value receiver.

--- environs/maas/environprovider.go
+++ environs/maas/environprovider.go
@@ -15,21 +15,21 @@ var _ environs.EnvironProvider = (*maasEnvironProvider)(nil)
 var providerInstance maasEnvironProvider
 
 func init() {
-	environs.RegisterProvider("maas", &providerInstance)
+	environs.RegisterProvider("maas", maasEnvironProvider{})
 }
 
-func (*maasEnvironProvider) Open(cfg *config.Config) (environs.Environ, error) {
+func (maasEnvironProvider) Open(cfg *config.Config) (environs.Environ, error) {
 	log.Printf("environs/maas: opening environment %q.", cfg.Name())
 	return NewEnviron(cfg)
 }
 
 // BoilerplateConfig is specified in the EnvironProvider interface.
-func (*maasEnvironProvider) BoilerplateConfig() string {
+func (maasEnvironProvider) BoilerplateConfig() string {
 	panic("Not implemented.")
 }
 
 // SecretAttrs is specified in the EnvironProvider interface.
-func (prov *maasEnvironProvider) SecretAttrs(cfg *config.Config) (map[string]interface{}, error) {
+func (prov maasEnvironProvider) SecretAttrs(cfg *config.Config) (map[string]interface{}, error) {
 	secretAttrs := make(map[string]interface{})
 	maasCfg, err := prov.newConfig(cfg)
 	if err != nil {
@@ -40,17 +40,17 @@ func (prov *maasEnvironProvider) SecretAttrs(cfg *config.Config) (map[string]int
 }
 
 // PublicAddress is specified in the EnvironProvider interface.
-func (*maasEnvironProvider) PublicAddress() (string, error) {
+func (maasEnvironProvider) PublicAddress() (string, error) {
 	panic("Not implemented.")
 }
 
 // PrivateAddress is specified in the EnvironProvider interface.
-func (*maasEnvironProvider) PrivateAddress() (string, error) {
+func (maasEnvironProvider) PrivateAddress() (string, error) {
 	panic("Not implemented.")
 }
 
 // InstanceId is specified in the EnvironProvider interface.
-func (*maasEnvironProvider) InstanceId() (state.InstanceId, error) {
+func (maasEnvironProvider) InstanceId() (state.InstanceId, error) {
 	content, err := ioutil.ReadFile(_MAASInstanceIDFilename)
 	if err != nil {
 		return "", err
