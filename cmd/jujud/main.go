23cb69aee0cd1d4300fc104554018eac87987c49
Julian Edwards <julian.edwards@canonical.com>
2013-04-08 11:07:16 +1000

    put the provider import back

--- cmd/jujud/main.go
+++ cmd/jujud/main.go
@@ -13,6 +13,7 @@ import (
 // here, it will register itself with environs.
 import (
 	_ "launchpad.net/juju-core/environs/ec2"
+	_ "launchpad.net/juju-core/environs/maas"
 	_ "launchpad.net/juju-core/environs/openstack"
 )
 
