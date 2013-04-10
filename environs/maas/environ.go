c20de0f03ded5880ba7d2b9e0d898d8caabaf42e
Julian Edwards <julian.edwards@canonical.com>
2013-04-10 12:22:11 +1000

    go fmt yourself

--- environs/maas/environ.go
+++ environs/maas/environ.go
@@ -280,7 +280,7 @@ func (env *maasEnviron) SetConfig(cfg *config.Config) error {
 	env.name = cfg.Name()
 	env.ecfgUnlocked = ecfg
 
-    // We're using v1.0 of the MAAS API.
+	// We're using v1.0 of the MAAS API.
 	authClient, err := gomaasapi.NewAuthenticatedClient(ecfg.MAASServer(), ecfg.MAASOAuth(), "1.0")
 	if err != nil {
 		return err
