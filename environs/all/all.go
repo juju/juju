54de96827ebcfded24edfd4739fc08020aa4ba2a
Raphael Badin <raphael.badin@canonical.com>
2013-04-11 16:11:32 +0200

    Remove comment.

--- environs/all/all.go
+++ environs/all/all.go
@@ -1,8 +1,6 @@
 package all
 
 // Register all the available providers.
-// When we import an environment provider implementation
-// here, it will register itself with environs.
 import (
 	_ "launchpad.net/juju-core/environs/ec2"
 	_ "launchpad.net/juju-core/environs/maas"
