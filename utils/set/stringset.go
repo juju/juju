5c3d29c241e939c775c05f28042ee7284a922d83
Tim Penhey <tim.penhey@canonical.com>
2013-04-12 11:50:16 +1200

    A rudimentary implementation of a string set using map[string]bool

---
+++ utils/set/stringset.go
@@ -0,0 +1,95 @@
+package set
+
+import (
+	"sort"
+)
+
+type StringSet struct {
+	values map[string]bool
+}
+
+func NewStringSet(initial ...string) *StringSet {
+	result := &StringSet{values: make(map[string]bool)}
+	for _, value := range initial {
+		result.Add(value)
+	}
+	return result
+}
+
+// Add puts a value into the set.
+func (s *StringSet) Add(value string) {
+	s.values[value] = true
+}
+
+// Remove takes a value out of the set.  If value wasn't in the set to start
+// with, this method silently succeeds.
+func (s *StringSet) Remove(value string) {
+	delete(s.values, value)
+}
+
+// Contains returns true if the value is in the set, and false otherwise.
+func (s *StringSet) Contains(value string) bool {
+	_, exists := s.values[value]
+	return exists
+}
+
+// Values returns an unordered slice containing all the values in the set.
+func (s *StringSet) Values() []string {
+	result := make([]string, len(s.values))
+	i := 0
+	for key, _ := range s.values {
+		result[i] = key
+		i++
+	}
+	return result
+}
+
+// SortedValues returns an ordered slice containing all the values in the set.
+func (s *StringSet) SortedValues() []string {
+	values := s.Values()
+	sort.Strings(values)
+	return values
+}
+
+// Union returns a new StringSet representing a union of the elments in the
+// method target and the parameter.
+func (s *StringSet) Union(other *StringSet) *StringSet {
+	result := NewStringSet()
+	// Use the internal map rather than going through the friendlier functions
+	// to avoid extra allocation of slices.
+	for value, _ := range s.values {
+		result.values[value] = true
+	}
+	for value, _ := range other.values {
+		result.values[value] = true
+	}
+	return result
+}
+
+// Intersection returns a new StringSet representing a intersection of the elments in the
+// method target and the parameter.
+func (s *StringSet) Intersection(other *StringSet) *StringSet {
+	result := NewStringSet()
+	// Use the internal map rather than going through the friendlier functions
+	// to avoid extra allocation of slices.
+	for value, _ := range s.values {
+		if other.Contains(value) {
+			result.values[value] = true
+		}
+	}
+	return result
+}
+
+// Difference returns a new StringSet representing all the values in the
+// target that are not in the parameter.
+func (s *StringSet) Difference(other *StringSet) *StringSet {
+	result := NewStringSet()
+	// Use the internal map rather than going through the friendlier functions
+	// to avoid extra allocation of slices.
+	for value, _ := range s.values {
+		if !other.Contains(value) {
+			result.values[value] = true
+		}
+	}
+	return result
+}
---
+++ utils/set/stringset_test.go
@@ -0,0 +1,102 @@
+package set_test
+
+import (
+	. "launchpad.net/gocheck"
+	"launchpad.net/juju-core/utils/set"
+	"sort"
+	"testing"
+)
+
+func Test(t *testing.T) {
+	TestingT(t)
+}
+
+type stringSetSuite struct{}
+
+var _ = Suite(stringSetSuite{})
+
+// Helper methods for the tests.
+func AssertEmpty(c *C, s *set.StringSet) {
+	c.Assert(len(s.Values()), Equals, 0)
+}
+
+func AssertValues(c *C, s *set.StringSet, expected ...string) {
+	values := s.Values()
+	sort.Strings(expected)
+	sort.Strings(values)
+	c.Assert(values, DeepEquals, expected)
+}
+
+func AssertSortedValues(c *C, s *set.StringSet, expected ...string) {
+	values := s.Values()
+	sort.Strings(expected)
+	c.Assert(values, DeepEquals, expected)
+}
+
+// Actual tests start here.
+func (stringSetSuite) TestEmpty(c *C) {
+	s := set.NewStringSet()
+	AssertEmpty(c, s)
+}
+
+func (stringSetSuite) TestInitialValues(c *C) {
+	s := set.NewStringSet("foo", "bar", "baz")
+	AssertValues(c, s, "foo", "bar", "baz")
+}
+
+func (stringSetSuite) TestAdd(c *C) {
+	s := set.NewStringSet()
+	s.Add("foo")
+	s.Add("foo")
+	s.Add("bar")
+	AssertValues(c, s, "foo", "bar")
+}
+
+func (stringSetSuite) TestRemove(c *C) {
+	s := set.NewStringSet("foo", "bar")
+	s.Remove("foo")
+	AssertValues(c, s, "bar")
+}
+
+func (stringSetSuite) TestContains(c *C) {
+	s := set.NewStringSet("foo", "bar")
+	c.Assert(s.Contains("foo"), Equals, true)
+	c.Assert(s.Contains("bar"), Equals, true)
+	c.Assert(s.Contains("baz"), Equals, false)
+}
+
+func (stringSetSuite) TestRemoveNonExistant(c *C) {
+	s := set.NewStringSet()
+	s.Remove("foo")
+	AssertEmpty(c, s)
+}
+
+func (stringSetSuite) TestUnion(c *C) {
+	s1 := set.NewStringSet("foo", "bar")
+	s2 := set.NewStringSet("foo", "baz", "bang")
+	union1 := s1.Union(s2)
+	union2 := s2.Union(s1)
+
+	AssertValues(c, union1, "foo", "bar", "baz", "bang")
+	AssertValues(c, union2, "foo", "bar", "baz", "bang")
+}
+
+func (stringSetSuite) TestIntersection(c *C) {
+	s1 := set.NewStringSet("foo", "bar")
+	s2 := set.NewStringSet("foo", "baz", "bang")
+	int1 := s1.Intersection(s2)
+	int2 := s2.Intersection(s1)
+
+	AssertValues(c, int1, "foo")
+	AssertValues(c, int2, "foo")
+}
+
+func (stringSetSuite) TestDifference(c *C) {
+	s1 := set.NewStringSet("foo", "bar")
+	s2 := set.NewStringSet("foo", "baz", "bang")
+	diff1 := s1.Difference(s2)
+	diff2 := s2.Difference(s1)
+
+	AssertValues(c, diff1, "bar")
+	AssertValues(c, diff2, "baz", "bang")
+}
