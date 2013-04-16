8a430f7abd0b1828f679732280a8048739a44029
Tim Penhey <tim.penhey@canonical.com>
2013-04-16 14:55:25 +1200

    Test with existing dirs and files.

--- utils/lockdir/lockdir_test.go
+++ utils/lockdir/lockdir_test.go
@@ -1,9 +1,13 @@
 package lockdir_test
 
 import (
+	"io/ioutil"
+	"os"
+	"path"
+	"testing"
+
 	. "launchpad.net/gocheck"
 	"launchpad.net/juju-core/utils/lockdir"
-	"testing"
 )
 
 func Test(t *testing.T) {
@@ -14,6 +18,8 @@ type lockDirSuite struct{}
 
 var _ = Suite(lockDirSuite{})
 
+// This test also happens to test that locks can get created when the lockDir
+// doesn't exist.
 func (lockDirSuite) TestNamedLockDir(c *C) {
 	validLockName := func(name string) {
 		dir := c.MkDir()
@@ -36,4 +42,24 @@ func (lockDirSuite) TestNamedLockDir(c *C) {
 	invalidLockName("no/slash")
 	invalidLockName("no\\backslash")
 	invalidLockName("no$dollar")
+}
+
+func (lockDirSuite) TestNewLockWithExistingDir(c *C) {
+	dir := c.MkDir()
+	err := os.MkdirAll(dir, 0755)
+	c.Assert(err, IsNil)
+	_, err = lockdir.NewLock(dir, "special")
+	c.Assert(err, IsNil)
+}
+
+func (lockDirSuite) TestNewLockWithExistingFileInPlace(c *C) {
+	dir := c.MkDir()
+	err := os.MkdirAll(dir, 0755)
+	c.Assert(err, IsNil)
+	path := path.Join(dir, "locks")
+	err = ioutil.WriteFile(path, []byte("foo"), 0644)
+	c.Assert(err, IsNil)
+
+	_, err = lockdir.NewLock(path, "special")
+	c.Assert(err, ErrorMatches, `lock dir ".*/locks" exists and is a file not a directory`)
 }
