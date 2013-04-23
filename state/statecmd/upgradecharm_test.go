2f86efb719d6e99f21390c4436c63667f7fd9d89
Matthew Scott <matthew.scott@canonical.com>
2013-04-23 10:39:00 -0600

    API-ified upgrade charm - file add

---
+++ state/statecmd/upgradecharm_test.go
@@ -0,0 +1,61 @@
+package statecmd_test
+
+import (
+	. "launchpad.net/gocheck"
+	"launchpad.net/juju-core/juju/testing"
+	"launchpad.net/juju-core/state"
+	"launchpad.net/juju-core/state/api/params"
+	"launchpad.net/juju-core/state/statecmd"
+	coretest "launchpad.net/juju-core/testing"
+)
+
+type UpgradeCharmSuite struct {
+	testing.JujuConnSuite
+}
+
+var _ = Suite(&UpgradeCharmSuite{})
+
+var serviceUpgradeCharmTests = []struct {
+	about   string
+	charm   string
+	service string
+	force   bool
+	err     string
+}{
+	{
+		about:   "upgrade a charm",
+		charm:   "upgrade1",
+		service: "upgrade",
+		force:   false,
+	},
+	{
+		about:   "force upgrade a charm",
+		charm:   "upgrade1",
+		service: "upgrade",
+		force:   true,
+	},
+}
+
+func (s *UpgradeCharmSuite) TestServiceUpgradeCharm(c *C) {
+	for i, t := range serviceUpgradeCharmTests {
+		charm := s.AddTestingCharm(c, t.charm)
+		svc, err := s.State.AddService(t.service, charm)
+		c.Assert(err, IsNil)
+		c.Assert(svc.Life(), Equals, state.Alive)
+		c.Logf("Svc: %+v", svc)
+		c.Logf("Charm: %+v", charm)
+		c.Logf("test %d. %s", i, t.about)
+		err = statecmd.ServiceUpgradeCharm(s.State, params.ServiceUpgradeCharm{
+			ServiceName: t.service,
+			Force:       t.force,
+			RepoPath:    coretest.Charms.Path,
+		})
+		if t.err != "" {
+			c.Assert(err, ErrorMatches, t.err)
+		} else {
+			c.Assert(err, IsNil)
+		}
+		err = svc.Destroy()
+		c.Assert(err, IsNil)
+	}
+}
