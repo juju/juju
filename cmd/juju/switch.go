33e7673fbe10a5975c7182ab9eff4a9f10e82beb
Tim Penhey <tim.penhey@canonical.com>
2013-04-23 12:45:17 +1200

    Here goes a switch command.

---
+++ cmd/juju/switch.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"sort"
+
+	"launchpad.net/gnuflag"
+	"launchpad.net/juju-core/cmd"
+	"launchpad.net/juju-core/environs"
+	"launchpad.net/juju-core/environs/config"
+)
+
+type SwitchCommand struct {
+	cmd.CommandBase
+	EnvName string
+	List    bool
+}
+
+var switchDoc = `Show or change the default juju environment name.
+
+If no command line parameters are passed, switch will output the current
+environment as defined by the file $JUJU_HOME/current-environment.
+
+If a command line parameter is passed in, that value will is stored in the
+current environment file if it represents a valid environment name as
+specified in the environments.yaml file.
+`
+
+func (c *SwitchCommand) Info() *cmd.Info {
+	return &cmd.Info{
+		Name:    "switch",
+		Args:    "[environment name]",
+		Purpose: "show or change the default juju environment name",
+		Doc:     switchDoc,
+		Aliases: []string{"env"},
+	}
+}
+
+func (c *SwitchCommand) SetFlags(f *gnuflag.FlagSet) {
+	f.BoolVar(&c.List, "l", false, "list the environment names")
+	f.BoolVar(&c.List, "list", false, "")
+}
+
+func (c *SwitchCommand) Init(args []string) (err error) {
+	c.EnvName, err = cmd.ZeroOrOneArgs(args)
+	return
+}
+
+func validEnvironmentName(name string, names []string) bool {
+	for _, n := range names {
+		if name == n {
+			return true
+		}
+	}
+	return false
+}
+
+func (c *SwitchCommand) Run(ctx *cmd.Context) error {
+
+	// Passing through the empty string reads the default environments.yaml file.
+	environments, err := environs.ReadEnvirons("")
+	if err != nil {
+		fmt.Fprintf(ctx.Stderr, "Couldn't read the environment.")
+		return err
+	}
+	names := environments.Names()
+	sort.Strings(names)
+
+	currentEnv := readCurrentEnvironment()
+	if currentEnv == "" {
+		currentEnv = environments.Default
+	}
+
+	if currentEnv == "" {
+		currentEnv = "<not specified>"
+	}
+
+	if c.EnvName == "" || c.EnvName == currentEnv {
+		fmt.Fprintf(ctx.Stdout, "Current environment: %q\n", currentEnv)
+	} else {
+		// Check to make sure that the specified environment
+		if !validEnvironmentName(c.EnvName, names) {
+			return fmt.Errorf("%q is not a name of an existing defined environment", c.EnvName)
+		}
+		currentEnvironment := filepath.Join(config.JujuHome(), CurrentEnvironmentFile)
+		err := ioutil.WriteFile(currentEnvironment, []byte(c.EnvName), 0644)
+		if err != nil {
+			fmt.Fprintf(ctx.Stderr, "Unable to write to the environment file: %q", currentEnvironment)
+			return err
+		}
+		fmt.Fprintf(ctx.Stdout, "Changed default environment from %q to %q\n", currentEnv, c.EnvName)
+	}
+	if c.List {
+		fmt.Fprintf(ctx.Stdout, "\nEnvironments:\n")
+		for _, name := range names {
+			fmt.Fprintf(ctx.Stdout, "\t%s\n", name)
+		}
+	}
+
+	return nil
+}
