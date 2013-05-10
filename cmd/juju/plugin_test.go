39e5ec5be6017b0737c799644d9f6665ee262292
Tim Penhey <tim.penhey@canonical.com>
2013-05-10 16:45:38 -0700

    Isolate the check for plugins from the actual plugins the test runner may have.

--- cmd/juju/plugin_test.go
+++ cmd/juju/plugin_test.go
@@ -1,13 +1,29 @@
 package main
 
 import (
+	"os"
+
 	. "launchpad.net/gocheck"
+	"launchpad.net/juju-core/testing"
 )
 
 type PluginSuite struct {
+	oldPath string
+	home    *testing.FakeHome
 }
 
 var _ = Suite(&PluginSuite{})
+
+func (suite *PluginSuite) SetUpTest(c *C) {
+	suite.oldPath = os.Getenv("PATH")
+	suite.home = testing.MakeEmptyFakeHome(c)
+	os.Setenv("PATH", testing.HomePath())
+}
+
+func (suite *PluginSuite) TearDownTest(c *C) {
+	suite.home.Restore()
+	os.Setenv("PATH", suite.oldPath)
+}
 
 func (*PluginSuite) TestFindPlugins(c *C) {
 	plugins := findPlugins()
