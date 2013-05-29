8914ae7ed970ab16e9e9419b3637fa298036dd6f
Tim Penhey <tim.penhey@canonical.com>
2013-05-30 10:32:32 +1200

    Fix the log tests.

--- cmd/logging_test.go
+++ cmd/logging_test.go
@@ -13,7 +13,6 @@ import (
 )
 
 type LogSuite struct {
-	testing.LoggingSuite
 }
 
 var _ = Suite(&LogSuite{})
@@ -26,55 +25,25 @@ func (s *LogSuite) TestAddFlags(c *C) {
 	err := f.Parse(false, []string{})
 	c.Assert(err, IsNil)
 	c.Assert(l.Path, Equals, "")
-	c.Assert(l.Verbose, Equals, false)
-	c.Assert(l.Debug, Equals, false)
+	c.Assert(l.Config, Equals, "")
 
-	err = f.Parse(false, []string{"--log-file", "foo", "--verbose", "--debug"})
+	err = f.Parse(false, []string{"--log-file", "foo", "--log-config=juju.cmd=INFO;juju.worker.deployer=DEBUG"})
 	c.Assert(err, IsNil)
 	c.Assert(l.Path, Equals, "foo")
-	c.Assert(l.Verbose, Equals, true)
-	c.Assert(l.Debug, Equals, true)
-}
-
-func (s *LogSuite) TestStart(c *C) {
-	for _, t := range []struct {
-		path    string
-		verbose bool
-		debug   bool
-		check   Checker
-	}{
-		{"", true, true, NotNil},
-		{"", true, false, NotNil},
-		{"", false, true, NotNil},
-		{"", false, false, IsNil},
-		{"foo", true, true, NotNil},
-		{"foo", true, false, NotNil},
-		{"foo", false, true, NotNil},
-		{"foo", false, false, NotNil},
-	} {
-		// commands always start with the log target set to its zero value.
-		log.SetTarget(nil)
-
-		l := &cmd.Log{Path: t.path, Verbose: t.verbose, Debug: t.debug}
-		ctx := testing.Context(c)
-		err := l.Start(ctx)
-		c.Assert(err, IsNil)
-		c.Assert(log.Target(), t.check)
-		c.Assert(log.Debug, Equals, t.debug)
-	}
+	c.Assert(l.Config, Equals, "juju.cmd=INFO;juju.worker.deployer=DEBUG")
 }
 
 func (s *LogSuite) TestStderr(c *C) {
-	l := &cmd.Log{Verbose: true}
+	l := &cmd.Log{Config: "<root>=INFO"}
 	ctx := testing.Context(c)
 	err := l.Start(ctx)
 	c.Assert(err, IsNil)
 	log.Infof("hello")
-	c.Assert(bufferString(ctx.Stderr), Matches, `^.* INFO hello\n`)
+	c.Assert(bufferString(ctx.Stderr), Matches, `^.* INFO .* hello\n`)
 }
 
 func (s *LogSuite) TestRelPathLog(c *C) {
-	l := &cmd.Log{Path: "foo.log"}
+	l := &cmd.Log{Path: "foo.log", Config: "<root>=INFO"}
 	ctx := testing.Context(c)
 	err := l.Start(ctx)
 	c.Assert(err, IsNil)
@@ -82,12 +51,12 @@ func (s *LogSuite) TestRelPathLog(c *C) {
 	c.Assert(bufferString(ctx.Stderr), Equals, "")
 	content, err := ioutil.ReadFile(filepath.Join(ctx.Dir, "foo.log"))
 	c.Assert(err, IsNil)
-	c.Assert(string(content), Matches, `^.* INFO hello\n`)
+	c.Assert(string(content), Matches, `^.* INFO .* hello\n`)
 }
 
 func (s *LogSuite) TestAbsPathLog(c *C) {
 	path := filepath.Join(c.MkDir(), "foo.log")
-	l := &cmd.Log{Path: path}
+	l := &cmd.Log{Path: path, Config: "<root>=INFO"}
 	ctx := testing.Context(c)
 	err := l.Start(ctx)
 	c.Assert(err, IsNil)
@@ -95,5 +64,5 @@ func (s *LogSuite) TestAbsPathLog(c *C) {
 	c.Assert(bufferString(ctx.Stderr), Equals, "")
 	content, err := ioutil.ReadFile(path)
 	c.Assert(err, IsNil)
-	c.Assert(string(content), Matches, `^.* INFO hello\n`)
+	c.Assert(string(content), Matches, `^.* INFO .* hello\n`)
 }
