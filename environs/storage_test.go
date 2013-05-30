750c53ab202a6b85263fe60cd81924a648284064
Danilo Šegan <danilo.segan@canonical.com>
2013-05-30 12:13:53 +0200

    Start a VerifyStorage test.

--- environs/storage_test.go
+++ environs/storage_test.go
@@ -4,6 +4,8 @@
 package environs_test
 
 import (
+	"fmt"
+	"io"
 	. "launchpad.net/gocheck"
 	"launchpad.net/juju-core/environs"
 )
@@ -28,4 +30,48 @@ func (s *EmptyStorageSuite) TestList(c *C) {
 	names, err := environs.EmptyStorage.List("anything")
 	c.Assert(names, IsNil)
 	c.Assert(err, IsNil)
+}
+
+
+type MockStorage struct {
+	StorageRequests []string
+}
+
+func (ms *MockStorage) Put(filename string, reader io.Reader, length int64) error {
+	var content []byte
+	_, _ = reader.Read(content)
+	log_message := fmt.Sprintf(
+		"Put('%s', '%s', %d)", filename, content, length)
+	ms.StorageRequests = append(ms.StorageRequests, log_message)
+	return nil
+}
+
+func (ms *MockStorage) Get(name string) (io.ReadCloser, error) {
+	return nil, nil
+	//return nil, &NotFoundError{fmt.Errorf("file %q not found", name)}
+}
+
+func (ms *MockStorage) List(prefix string) ([]string, error) {
+	return nil, nil
+}
+
+func (ms *MockStorage) Remove(file string) error {
+	return nil
+}
+
+func (ms *MockStorage) URL(name string) (string, error) {
+	return "", fmt.Errorf("file %q not found", name)
+}
+
+
+type VerifyStorageSuite struct{}
+
+var _ = Suite(&VerifyStorageSuite{})
+
+func (s *VerifyStorageSuite) TestVerifyStorage(c *C) {
+	storage := MockStorage{}
+	error := environs.VerifyStorage(&storage)
+	c.Assert(error, IsNil)
+	c.Assert(storage.StorageRequests[0], Equals,
+		"Put('bootstrap-verify', '%s', 28)")
 }
