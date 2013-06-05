791937cca2c5e733d1d69d11b670fe6ba4a778df
Dimiter Naydenov <dimiter.naydenov@canonical.com>
2013-06-05 11:47:30 +0200

    Added missing copyright header

--- state/api/common/interfaces.go
+++ state/api/common/interfaces.go
@@ -1,3 +1,6 @@
+// Copyright 2012, 2013 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
 package common
 
 // Caller interface is implemented by the client-facing State object.
