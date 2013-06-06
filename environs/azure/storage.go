4a639e35518ef0e00d52086e5e3cfaecc528771f
Jeroen Vermeulen <jtv@canonical.com>
2013-06-06 12:39:47 +0200

    Skeleton for (future implementation of) Azure provider.

---
+++ environs/azure/storage.go
@@ -0,0 +1,34 @@
+// Copyright 2013 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package azure
+
+type azureStorage struct{}
+
+// azureStorage implements Storage.
+var _ environs.Storage = (*azureStorage)(nil)
+
+// Get is specified in the StorageReader interface.
+func (storage *azureStorage) Get(name string) (io.ReadCloser, error) {
+	panic("unimplemented")
+}
+
+// List is specified in the StorageReader interface.
+func (storage *azureStorage) List(prefix string) ([]string, error) {
+	panic("unimplemented")
+}
+
+// URL is specified in the StorageReader interface.
+func (storage *azureStorage) URL(name string) (string, error) {
+	panic("unimplemented")
+}
+
+// Put is specified in the StorageWriter interface.
+func (storage *azureStorage) Put(name string, r io.Reader, length int64) error {
+	panic("unimplemented")
+}
+
+// Remove is specified in the StorageWriter interface.
+func (storage *azureStorage) Remove(name string) error {
+	panic("unimplemented")
+}
