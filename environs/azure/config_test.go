dbf1b1e460b146db28fd279d2c782a12f2e58321
Jeroen Vermeulen <jtv@canonical.com>
2013-06-12 19:30:05 +0200

    Test skeleton for config.go.

---
+++ environs/azure/config_test.go
@@ -0,0 +1,12 @@
+// Copyright 2013 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package azure
+
+import (
+	. "launchpad.net/gocheck"
+)
+
+type ConfigSuite struct{}
+
+var _ = Suite(new(ConfigSuite))
