3216382e1d47a607061c41c5f245086ad6074f76
Jeroen Vermeulen <jtv@canonical.com>
2013-06-13 16:27:38 +0200

    Basic config test.

--- environs/azure/config_test.go
+++ environs/azure/config_test.go
@@ -5,8 +5,29 @@ package azure
 
 import (
 	. "launchpad.net/gocheck"
+	"launchpad.net/juju-core/environs/config"
+	"launchpad.net/juju-core/testing"
 )
 
 type ConfigSuite struct{}
 
 var _ = Suite(new(ConfigSuite))
+
+// makeConfigMap creates a minimal map of standard configuration items.
+// It's just the bare minimum to produce a configuration object.
+func makeConfigMap() map[string]interface{} {
+	return map[string]interface{}{
+		"name":            "testenv",
+		"type":            "azure",
+		"ca-cert":         testing.CACert,
+		"ca-private-key":  testing.CAKey,
+	}
+}
+
+func (ConfigSuite) TestParsesSettings(c *C) {
+	configMap := makeConfigMap()
+	config, err := config.New(configMap)
+	c.Assert(err, IsNil)
+	c.Assert(config, NotNil)
+	c.Check(config.Name(), Equals, configMap["name"])
+}
