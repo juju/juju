9882edd673ea43d50dd7d6d275c546f282a8dc56
Jeroen Vermeulen <jtv@canonical.com>
2013-06-13 14:56:18 +0200

    Implement basic locking, and add name and ecfg to azureEnviron.

--- environs/azure/environ_test.go
+++ environs/azure/environ_test.go
@@ -5,6 +5,7 @@ package azure
 
 import (
 	. "launchpad.net/gocheck"
+	"sync"
 )
 
 type EnvironSuite struct {
@@ -15,4 +16,22 @@ var _ = Suite(new(EnvironSuite))
 
 func (EnvironSuite) TestExample(c *C) {
 	c.Check("test", Equals, "test")
+}
+
+func (EnvironSuite) TestGetSnapshot(c *C) {
+	original := azureEnviron{name: "this-env", ecfg: new(azureEnvironConfig)}
+	snapshot := original.getSnapshot()
+
+	// The snapshot is identical to the original.
+	c.Check(*snapshot, DeepEquals, original)
+
+	// However, they are distinct objects.
+	c.Check(snapshot, Not(Equals), &original)
+
+	// It's a shallow copy; they still share pointers.
+	c.Check(snapshot.ecfg, Equals, original.ecfg)
+
+	// Neither object is locked at the end of the copy.
+	c.Check(original.Mutex, Equals, sync.Mutex{})
+	c.Check(snapshot.Mutex, Equals, sync.Mutex{})
 }
