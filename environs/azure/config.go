112f78fcb436273448cac2a3312555699f739230
Raphael Badin <raphael.badin@canonical.com>
2013-06-14 13:46:11 +0200

    Add config items for the Azure provider.

--- environs/azure/config.go
+++ environs/azure/config.go
@@ -4,15 +4,25 @@
 package azure
 
 import (
+	"fmt"
+	"io/ioutil"
 	"launchpad.net/juju-core/environs/config"
 	"launchpad.net/juju-core/schema"
 )
 
 var azureConfigChecker = schema.StrictFieldMap(
 	schema.Fields{
-	// TODO: Configuration items defined in this map, as name:type.
+		"management-subscription-id":     schema.String(),
+		"management-certificate-path":    schema.String(),
+		"management-hosted-service-name": schema.String(),
+		"storage-account-name":           schema.String(),
+		"storage-account-key":            schema.String(),
+		"storage-container-name":         schema.String(),
 	},
-	schema.Defaults{},
+	schema.Defaults{
+		"management-hosted-service-name": "",
+		"storage-container-name":         "",
+	},
 )
 
 type azureEnvironConfig struct {
@@ -20,7 +30,29 @@ type azureEnvironConfig struct {
 	attrs map[string]interface{}
 }
 
-// TODO: Configuration getters here.
+func (cfg *azureEnvironConfig) ManagementSubscriptionId() string {
+	return cfg.attrs["management-subscription-id"].(string)
+}
+
+func (cfg *azureEnvironConfig) ManagementCertificatePath() string {
+	return cfg.attrs["management-certificate-path"].(string)
+}
+
+func (cfg *azureEnvironConfig) ManagementHostedServiceName() string {
+	return cfg.attrs["management-hosted-service-name"].(string)
+}
+
+func (cfg *azureEnvironConfig) StorageAccountName() string {
+	return cfg.attrs["storage-account-name"].(string)
+}
+
+func (cfg *azureEnvironConfig) StorageAccountKey() string {
+	return cfg.attrs["storage-account-key"].(string)
+}
+
+func (cfg *azureEnvironConfig) StorageContainerName() string {
+	return cfg.attrs["storage-container-name"].(string)
+}
 
 func (prov azureEnvironProvider) newConfig(cfg *config.Config) (*azureEnvironConfig, error) {
 	validCfg, err := prov.Validate(cfg, nil)
@@ -49,14 +81,37 @@ func (prov azureEnvironProvider) Validate(cfg, oldCfg *config.Config) (*config.C
 	envCfg.Config = cfg
 	envCfg.attrs = v.(map[string]interface{})
 
-	// TODO: Validate settings for individual config items here.
+	// Validate management-certificate-path: must be a path to an existing file.
+	certPath := envCfg.ManagementCertificatePath()
+	_, err = ioutil.ReadFile(certPath)
+	if err != nil {
+		return nil, fmt.Errorf("invalid management-certificate-path: %s", err)
+	}
+	if envCfg.ManagementHostedServiceName() == "" {
+		return nil, fmt.Errorf("environment has no management-hosted-service-name; auto-creation of hosted services is not yet supported")
+	}
+	if envCfg.StorageContainerName() == "" {
+		return nil, fmt.Errorf("environment has no storage-container-name; auto-creation of storage containers is not yet supported")
+	}
 
 	return cfg.Apply(envCfg.attrs)
 }
 
-// BoilerplateConfig is specified in the EnvironProvider interface.
+const boilerplateYAML = `azure:
+  type: azure
+  # http://msdn.microsoft.com/en-us/library/windowsazure
+  # Windows Azure Management info.
+  management-subscription-id: 886413e1-3b8a-5382-9b90-0c9aee199e5d
+  management-certificate-path: /home/me/azure.pem
+  management-hosted-service-name: gwaclbize3r9qh67ro6qbgvm
+  # Windows Azure Storage info.
+  storage-account-name: ghedlkjhw54e
+  storage-account-key: fdjh4sfkg
+  storage-container-name: sdg50984jmsdf
+`
+
 func (prov azureEnvironProvider) BoilerplateConfig() string {
-	panic("unimplemented")
+	return boilerplateYAML
 }
 
 // SecretAttrs is specified in the EnvironProvider interface.
--- environs/azure/config_test.go
+++ environs/azure/config_test.go
@@ -4,18 +4,20 @@
 package azure
 
 import (
+	"io/ioutil"
 	. "launchpad.net/gocheck"
 	"launchpad.net/juju-core/environs/config"
 	"launchpad.net/juju-core/testing"
+	"strings"
 )
 
 type ConfigSuite struct{}
 
 var _ = Suite(new(ConfigSuite))
 
-// makeConfigMap creates a minimal map of standard configuration items.
+// makeBaseConfigMap creates a minimal map of standard configuration items.
 // It's just the bare minimum to produce a configuration object.
-func makeConfigMap() map[string]interface{} {
+func makeBaseConfigMap() map[string]interface{} {
 	return map[string]interface{}{
 		"name":           "testenv",
 		"type":           "azure",
@@ -24,17 +26,35 @@ func makeConfigMap() map[string]interface{} {
 	}
 }
 
-func (ConfigSuite) TestNewParsesSettings(c *C) {
-	attrs := makeConfigMap()
-	config, err := config.New(attrs)
+func makeConfigMap(configMap map[string]interface{}) map[string]interface{} {
+	conf := makeBaseConfigMap()
+	for k, v := range configMap {
+		conf[k] = v
+	}
+	return conf
+}
+
+// createTempFile creates a temporary empty file.  The file will be cleaned
+// up at the end of the test calling this method.
+func createTempFile(c *C) string {
+	file, err := ioutil.TempFile(c.MkDir(), "")
 	c.Assert(err, IsNil)
-	c.Assert(config, NotNil)
-	c.Check(config.Name(), Equals, attrs["name"])
+	filename := file.Name()
+	return filename
 }
 
 func (ConfigSuite) TestValidateAcceptsNilOldConfig(c *C) {
+	managementCertificatePath := createTempFile(c)
+	azureConfig := map[string]interface{}{
+		"management-subscription-id":     "subscription-id",
+		"management-certificate-path":    managementCertificatePath,
+		"management-hosted-service-name": "service-name",
+		"storage-account-name":           "account-name",
+		"storage-account-key":            "account-key",
+		"storage-container-name":         "container-name",
+	}
+	attrs := makeConfigMap(azureConfig)
 	provider := azureEnvironProvider{}
-	attrs := makeConfigMap()
 	config, err := config.New(attrs)
 	c.Assert(err, IsNil)
 	result, err := provider.Validate(config, nil)
@@ -43,8 +63,17 @@ func (ConfigSuite) TestValidateAcceptsNilOldConfig(c *C) {
 }
 
 func (ConfigSuite) TestValidateAcceptsUnchangedConfig(c *C) {
+	managementCertificatePath := createTempFile(c)
+	azureConfig := map[string]interface{}{
+		"management-subscription-id":     "subscription-id",
+		"management-certificate-path":    managementCertificatePath,
+		"management-hosted-service-name": "service-name",
+		"storage-account-name":           "account-name",
+		"storage-account-key":            "account-key",
+		"storage-container-name":         "container-name",
+	}
+	attrs := makeConfigMap(azureConfig)
 	provider := azureEnvironProvider{}
-	attrs := makeConfigMap()
 	oldConfig, err := config.New(attrs)
 	c.Assert(err, IsNil)
 	newConfig, err := config.New(attrs)
@@ -56,13 +85,67 @@ func (ConfigSuite) TestValidateAcceptsUnchangedConfig(c *C) {
 
 func (ConfigSuite) TestValidateChecksConfigChanges(c *C) {
 	provider := azureEnvironProvider{}
-	oldAttrs := makeConfigMap()
+	oldAttrs := makeBaseConfigMap()
 	oldConfig, err := config.New(oldAttrs)
 	c.Assert(err, IsNil)
-	newAttrs := makeConfigMap()
+	newAttrs := makeBaseConfigMap()
 	newAttrs["name"] = "different-name"
 	newConfig, err := config.New(newAttrs)
 	c.Assert(err, IsNil)
 	_, err = provider.Validate(newConfig, oldConfig)
 	c.Check(err, NotNil)
+}
+
+func (ConfigSuite) TestValidateParsesAzureConfig(c *C) {
+	managementSubscriptionId := "subscription-id"
+	managementCertificatePath := createTempFile(c)
+	managementHostedServiceName := "service-name"
+	storageAccountName := "account-name"
+	storageAccountKey := "account-key"
+	storageContainerName := "container-name"
+	azureConfig := map[string]interface{}{
+		"management-subscription-id":     managementSubscriptionId,
+		"management-certificate-path":    managementCertificatePath,
+		"management-hosted-service-name": managementHostedServiceName,
+		"storage-account-name":           storageAccountName,
+		"storage-account-key":            storageAccountKey,
+		"storage-container-name":         storageContainerName,
+	}
+	attrs := makeConfigMap(azureConfig)
+	provider := azureEnvironProvider{}
+	config, err := config.New(attrs)
+	c.Assert(err, IsNil)
+	azConfig, err := provider.newConfig(config)
+	c.Assert(err, IsNil)
+	c.Check(azConfig.Name(), Equals, attrs["name"])
+	c.Check(azConfig.ManagementSubscriptionId(), Equals, managementSubscriptionId)
+	c.Check(azConfig.ManagementCertificatePath(), Equals, managementCertificatePath)
+	c.Check(azConfig.ManagementHostedServiceName(), Equals, managementHostedServiceName)
+	c.Check(azConfig.StorageAccountName(), Equals, storageAccountName)
+	c.Check(azConfig.StorageAccountKey(), Equals, storageAccountKey)
+	c.Check(azConfig.StorageContainerName(), Equals, storageContainerName)
+}
+
+func (ConfigSuite) TestChecksExistingCertFile(c *C) {
+	nonExistingCertPath := "non-existing-cert-file"
+	azureConfig := map[string]interface{}{
+		"management-subscription-id":     "subscription-id",
+		"management-certificate-path":    nonExistingCertPath,
+		"management-hosted-service-name": "service-name",
+		"storage-account-name":           "account-name",
+		"storage-account-key":            "account-key",
+		"storage-container-name":         "container-name",
+	}
+	attrs := makeConfigMap(azureConfig)
+	provider := azureEnvironProvider{}
+	newConfig, err := config.New(attrs)
+	c.Assert(err, IsNil)
+	_, err = provider.Validate(newConfig, nil)
+	c.Check(err, ErrorMatches, ".*"+nonExistingCertPath+": no such file or directory.*")
+}
+
+func (ConfigSuite) TestBoilerplateConfigReturnsAzureConfig(c *C) {
+	provider := azureEnvironProvider{}
+	boilerPlateConfig := provider.BoilerplateConfig()
+	c.Assert(strings.Contains(boilerPlateConfig, "type: azure"), Equals, true)
 }
