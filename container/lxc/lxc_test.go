8276387cf177a8dd81b52acf1a2f668ce71b3eac
Tim Penhey <tim.penhey@canonical.com>
2013-06-17 12:52:44 +1200

    Test for factory creation.

--- container/lxc/lxc_test.go
+++ container/lxc/lxc_test.go
@@ -7,10 +7,22 @@ import (
 	stdtesting "testing"
 
 	. "launchpad.net/gocheck"
+	"launchpad.net/juju-core/container/lxc"
+	"launchpad.net/juju-core/instance"
+	"launchpad.net/juju-core/testing"
 )
 
 func Test(t *stdtesting.T) { TestingT(t) }
 
-type LxcSuite struct{}
+type LxcSuite struct {
+	testing.LoggingSuite
+}
 
 var _ = Suite(&LxcSuite{})
+
+func (s *LxcSuite) TestNewContainer(c *C) {
+	factory := lxc.NewFactory(MockFactory())
+	container, err := factory.NewContainer("2/lxc/0")
+	c.Assert(err, IsNil)
+	c.Assert(container.Id(), Equals, instance.Id("machine-2-lxc-0"))
+}
