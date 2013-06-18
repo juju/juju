24b02f47ce7066628d4f276661240c66e6600738
Tim Penhey <tim.penhey@canonical.com>
2013-06-18 16:04:03 +1200

    Minor refactoring.

--- worker/provisioner/lxc-broker.go
+++ worker/provisioner/lxc-broker.go
@@ -8,8 +8,8 @@ import (
 	"launchpad.net/juju-core/constraints"
 	"launchpad.net/juju-core/container"
 	"launchpad.net/juju-core/container/lxc"
-	"launchpad.net/juju-core/environs"
 	"launchpad.net/juju-core/environs/config"
+	"launchpad.net/juju-core/instance"
 	"launchpad.net/juju-core/state"
 	"launchpad.net/juju-core/state/api"
 	"launchpad.net/loggo"
@@ -17,27 +17,28 @@ import (
 
 var lxcLogger = loggo.GetLogger("juju.provisioner.lxc")
 
-func NewLxcBroker(config *config.Config, tools *state.Tools) Broker {
+func NewLxcBroker(factory golxc.ContainerFactory, config *config.Config, tools *state.Tools) Broker {
 	return &lxcBroker{
-		config: config,
-		tools:  tools,
+		factory: lxc.NewFactory(factory),
+		config:  config,
+		tools:   tools,
 	}
 }
 
 type lxcBroker struct {
-	config *config.Config
-	tools  *state.Tools
+	factory lxc.ContainerFactory
+	config  *config.Config
+	tools   *state.Tools
 }
 
-func (broker *lxcBroker) StartInstance(machineId, machineNonce string, series string, cons constraints.Value, info *state.Info, apiInfo *api.Info) (environs.Instance, error) {
+func (broker *lxcBroker) StartInstance(machineId, machineNonce string, series string, cons constraints.Value, info *state.Info, apiInfo *api.Info) (instance.Instance, error) {
 	lxcLogger.Infof("starting lxc container for machineId: %s", machineId)
 
-	lxcContainer, err := lxc.NewContainer(machineId)
+	lxcContainer, err := broker.factory.NewContainer(machineId)
 	if err != nil {
 		lxcLogger.Errorf("failed to create container: %v", err)
 		return nil, err
 	}
-	// find tools, environ config
 	err = lxcContainer.Create(series, machineNonce, broker.tools, broker.config, info, apiInfo)
 	if err != nil {
 		lxcLogger.Errorf("failed to create container: %v", err)
@@ -47,7 +48,8 @@ func (broker *lxcBroker) StartInstance(machineId, machineNonce string, series st
 		lxcLogger.Errorf("failed to start container: %v", err)
 		return nil, err
 	}
-	return lxcContainer.Instance(), nil
+	// check to make sure it started.
+	return lxcContainer, nil
 }
 
 // StopInstances shuts down the given instances.
@@ -60,14 +62,21 @@ func (broker *lxcBroker) StopInstances(instances []environs.Instance) error {
 			lxcLogger.Warningf("instance is not a container - shouldn't happen")
 			continue
 		}
-		lxcContainer.Stop()
-		lxcContainer.Destroy()
+		if err := lxcContainer.Stop(); err != nil {
+			lxcLogger.Errorf("container did not stop: %v", err)
+			return err
+		}
+		if err := lxcContainer.Destroy(); err != nil {
+			lxcLogger.Errorf("container did not get destroyed: %v", err)
+			return err
+		}
 	}
 	return nil
 }
 
 // AllInstances only returns running containers.
 func (broker *lxcBroker) AllInstances() (result []environs.Instance, err error) {
+	// TODO(thumper): work on some prefix to avoid getting *all* containers.
 	containers, err := golxc.List()
 	if err != nil {
 		lxcLogger.Errorf("failed getting all instances: %v", err)
