b33c4e82e8c14e6de34531ee1e841228173aaf85
Tim Penhey <tim.penhey@canonical.com>
2013-06-19 12:15:14 +1200

    Prefix with Test so they actually get run.

--- testing/file_test.go
+++ testing/file_test.go
@@ -4,6 +4,8 @@
 package testing_test
 
 import (
+	"fmt"
+	"io/ioutil"
 	"path/filepath"
 
 	. "launchpad.net/gocheck"
@@ -14,10 +16,19 @@ type FileSuite struct{}
 
 var _ = Suite(&FileSuite{})
 
-func (s *FileSuite) AssertDirectoryExistsOrNot(c *C) {
+func (s *FileSuite) TestAssertDirectoryExistsOrNot(c *C) {
 	dir := c.MkDir()
 	testing.AssertDirectoryExists(c, dir)
 
 	absentDir := filepath.Join(dir, "foo")
 	testing.AssertDirectoryDoesNotExist(c, absentDir)
+}
+
+func (s *FileSuite) TestAssertNonEmptyFileExists(c *C) {
+	file, err := ioutil.TempFile(c.MkDir(), "")
+	c.Assert(err, IsNil)
+	fmt.Fprintf(file, "something")
+	file.Close()
+
+	testing.AssertNonEmptyFileExists(c, file.Name())
 }
