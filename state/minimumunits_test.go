d52c005560d06e67cafe755c4f0ce369c9777754
Francesco Banconi <francesco.banconi@canonical.com>
2013-06-21 18:08:34 +0200

    First retry test attempt.

--- state/minimumunits_test.go
+++ state/minimumunits_test.go
@@ -53,19 +53,37 @@ var minimumUnitsTests = []struct {
 	},
 }
 
-func (s *MinimumUnitsSuite) TestMinimumUnits(c *C) {
+func (s *MinimumUnitsSuite) TestSetMinimumUnits(c *C) {
 	var err error
+	service := s.service
 	for i, t := range minimumUnitsTests {
 		c.Logf("test %d. %s", i, t.about)
+		// Set up initial minimum units if required.
 		if t.initial > 0 {
-			err = s.service.SetMinimumUnits(t.initial)
+			err = service.SetMinimumUnits(t.initial)
 			c.Assert(err, IsNil)
 		}
-		err = s.service.SetMinimumUnits(t.input)
+		// Insert/update minimum units.
+		err = service.SetMinimumUnits(t.input)
 		c.Assert(err, IsNil)
-		c.Assert(s.service.MinimumUnits(), Equals, t.input)
-		revno, err := state.MinimumUnitsRevno(s.State, s.service.Name())
+		c.Assert(service.MinimumUnits(), Equals, t.input)
+		// Check the document existence and revno.
+		revno, err := state.MinimumUnitsRevno(s.State, service.Name())
 		c.Assert(err, Equals, t.err)
 		c.Assert(revno, Equals, t.revno)
+		// Clean up the existing document.
+		err = service.SetMinimumUnits(0)
+		c.Assert(err, IsNil)
 	}
+}
+
+func (s *UnitSuite) TestDestroyServiceRetry(c *C) {
+	defer state.SetRetryHooks(c, s.State, func() {
+		err := s.service.SetMinimumUnits(1)
+		c.Assert(err, IsNil)
+	}, func() {
+		c.Assert(s.service.MinimumUnits(), Equals, 42)
+	}).Check()
+	err := s.service.SetMinimumUnits(42)
+	c.Assert(err, IsNil)
 }
