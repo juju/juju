6185d121a63e3ce08ef9c65a11ca80629f73319a
Tim Penhey <tim.penhey@canonical.com>
2013-06-24 14:32:38 +1200

    Add skeleton for the local provider.

---
+++ environs/local/environ.go
@@ -0,0 +1,108 @@
+// Copyright 2013 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package local
+
+import (
+	"sync"
+
+	"launchpad.net/juju-core/constraints"
+	"launchpad.net/juju-core/environs"
+	"launchpad.net/juju-core/environs/config"
+	"launchpad.net/juju-core/instance"
+	"launchpad.net/juju-core/state"
+	"launchpad.net/juju-core/state/api"
+)
+
+// localEnviron implements Environ.
+var _ environs.Environ = (*localEnviron)(nil)
+
+type localEnviron struct {
+	// Except where indicated otherwise, all fields in this object should
+	// only be accessed using a lock or a snapshot.
+	sync.Mutex
+
+	// name is immutable; it can be accessed without locking.
+	name string
+}
+
+// Name is specified in the Environ interface.
+func (env *localEnviron) Name() string {
+	return env.name
+}
+
+// Bootstrap is specified in the Environ interface.
+func (env *localEnviron) Bootstrap(cons constraints.Value) error {
+	panic("unimplemented")
+}
+
+// StateInfo is specified in the Environ interface.
+func (env *localEnviron) StateInfo() (*state.Info, *api.Info, error) {
+	panic("unimplemented")
+}
+
+// Config is specified in the Environ interface.
+func (env *localEnviron) Config() *config.Config {
+	snap := env.getSnapshot()
+	return snap.ecfg.Config
+}
+
+// SetConfig is specified in the Environ interface.
+func (env *localEnviron) SetConfig(cfg *config.Config) error {
+	panic("unimplemented")
+}
+
+// StartInstance is specified in the Environ interface.
+func (env *localEnviron) StartInstance(machineId, machineNonce string, series string, cons constraints.Value, info *state.Info, apiInfo *api.Info) (instance.Instance, error) {
+	panic("unimplemented")
+}
+
+// StopInstances is specified in the Environ interface.
+func (env *localEnviron) StopInstances([]instance.Instance) error {
+	panic("unimplemented")
+}
+
+// Instances is specified in the Environ interface.
+func (env *localEnviron) Instances(ids []instance.Id) ([]instance.Instance, error) {
+	panic("unimplemented")
+}
+
+// AllInstances is specified in the Environ interface.
+func (env *localEnviron) AllInstances() ([]instance.Instance, error) {
+	panic("unimplemented")
+}
+
+// Storage is specified in the Environ interface.
+func (env *localEnviron) Storage() environs.Storage {
+	panic("unimplemented")
+}
+
+// PublicStorage is specified in the Environ interface.
+func (env *localEnviron) PublicStorage() environs.StorageReader {
+	panic("unimplemented")
+}
+
+// Destroy is specified in the Environ interface.
+func (env *localEnviron) Destroy(insts []instance.Instance) error {
+	panic("unimplemented")
+}
+
+// OpenPorts is specified in the Environ interface.
+func (env *localEnviron) OpenPorts(ports []instance.Port) error {
+	panic("unimplemented")
+}
+
+// ClosePorts is specified in the Environ interface.
+func (env *localEnviron) ClosePorts(ports []instance.Port) error {
+	panic("unimplemented")
+}
+
+// Ports is specified in the Environ interface.
+func (env *localEnviron) Ports() ([]instance.Port, error) {
+	panic("unimplemented")
+}
+
+// Provider is specified in the Environ interface.
+func (env *localEnviron) Provider() environs.EnvironProvider {
+	panic("unimplemented")
+}
