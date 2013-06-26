629906272cc1809a69de4eeac4df514a8a82093a
John Arbash Meinel <john@arbash-meinel.com>
2013-06-26 16:40:08 +0400

    More upgrader changes

--- state/api/state.go
+++ state/api/state.go
@@ -7,7 +7,6 @@ import (
 	"launchpad.net/juju-core/state/api/machineagent"
 	"launchpad.net/juju-core/state/api/machiner"
 	"launchpad.net/juju-core/state/api/params"
-	"launchpad.net/juju-core/state/api/upgrader"
 )
 
 // Login authenticates as the entity with the given name and password.
@@ -36,9 +35,4 @@ func (st *State) Machiner() *machiner.State {
 // functionality required by the machine agent code.
 func (st *State) MachineAgent() *machineagent.State {
 	return machineagent.NewState(st)
-}
-
-// Upgrader returns access to the Upgrader API
-func (st *State) Upgrader() (*upgrader.Upgrader, error) {
-	return upgrader.New(st), nil
 }
