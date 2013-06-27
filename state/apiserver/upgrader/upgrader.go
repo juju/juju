65347e37378b5b563c2eedb250edf6a327fd9c36
John Arbash Meinel <john@arbash-meinel.com>
2013-06-27 14:50:44 +0400

    Hook up the watch events using the existing EnvironConfigWatcher
    This is what Upgrader does today, though we want something better.
    We at least have something now, which will generate a Changes channel.

--- state/apiserver/upgrader/upgrader.go
+++ state/apiserver/upgrader/upgrader.go
@@ -6,23 +6,37 @@ package upgrader
 import (
 	"launchpad.net/juju-core/state"
 	"launchpad.net/juju-core/state/api/params"
+	"launchpad.net/juju-core/state/apiserver/common"
 )
 
 // UpgraderAPI provides access to the Upgrader API facade.
 type UpgraderAPI struct {
-	st *state.State
+	st        *state.State
+	resources common.ResourceRegistry
 }
 
 // New creates a new client-side UpgraderAPI facade.
-func NewUpgraderAPI(st *state.State) (*UpgraderAPI, error) {
-	return &UpgraderAPI{st: st}, nil
+func NewUpgraderAPI(st *state.State, resources common.ResourceRegistry) (*UpgraderAPI, error) {
+	return &UpgraderAPI{st: st, resources: resources}, nil
 }
+
+const machineTagPrefix = "machine-"
 
 // Start a watcher to track if there is a new version of the API that we want
 // to upgrade to
 func (u *UpgraderAPI) WatchAPIVersion(args params.Agents) (params.EntityWatchResults, error) {
 	result := params.EntityWatchResults{
 		Results: make([]params.EntityWatchResult, len(args.Tags)),
+	}
+	for i, _ := range args.Tags {
+		var err error
+		//if tag[:len(machineTagPrefix)] == machineTagPrefix {
+		//    agent, err := u.st.Machine(state.MachineIdFromTag(tag))
+		//    // TODO: Auth Check for ownership
+		//}
+		envWatcher := u.st.WatchEnvironConfig()
+		result.Results[i].EntityWatcherId = u.resources.Register(envWatcher)
+		result.Results[i].Error = common.ServerError(err)
 	}
 	return result, nil
 }
--- state/apiserver/upgrader/upgrader_test.go
+++ state/apiserver/upgrader/upgrader_test.go
@@ -4,12 +4,16 @@
 package upgrader_test
 
 import (
+	"time"
+
 	. "launchpad.net/gocheck"
+
 	jujutesting "launchpad.net/juju-core/juju/testing"
 	"launchpad.net/juju-core/state"
 	"launchpad.net/juju-core/state/api/params"
 	apitesting "launchpad.net/juju-core/state/apiserver/testing"
 	"launchpad.net/juju-core/state/apiserver/upgrader"
+	"launchpad.net/juju-core/testing/checkers"
 )
 
 type upgraderSuite struct {
@@ -26,6 +30,7 @@ var _ = Suite(&upgraderSuite{})
 
 func (s *upgraderSuite) SetUpTest(c *C) {
 	s.JujuConnSuite.SetUpTest(c)
+	s.resources = make(apitesting.FakeResourceRegistry)
 
 	// Create a machine to work with
 	var err error
@@ -34,8 +39,17 @@ func (s *upgraderSuite) SetUpTest(c *C) {
 	err = s.rawMachine.SetPassword("test-password")
 	c.Assert(err, IsNil)
 
-	s.upgrader, err = upgrader.NewUpgraderAPI(s.State)
+	s.upgrader, err = upgrader.NewUpgraderAPI(s.State, s.resources)
 	c.Assert(err, IsNil)
+}
+
+func (s *upgraderSuite) TearDownTest(c *C) {
+	if s.resources != nil {
+		for _, resource := range s.resources {
+			resource.Stop()
+		}
+	}
+	s.JujuConnSuite.TearDownTest(c)
 }
 
 func (s *upgraderSuite) TestWatchAPIVersionNothing(c *C) {
@@ -52,6 +66,20 @@ func (s *upgraderSuite) TestWatchAPIVersion(c *C) {
 	results, err := s.upgrader.WatchAPIVersion(args)
 	c.Assert(err, IsNil)
 	c.Check(results.Results, HasLen, 1)
-	// Not Implemented Yet
-	//c.Check(results.Results[0].EntityWatchId, Not(Equals), "")
+	c.Check(results.Results[0].EntityWatcherId, Not(Equals), "")
+	resource, ok := s.resources[results.Results[0].EntityWatcherId]
+	c.Check(ok, checkers.IsTrue)
+	defer func() {
+		err := resource.Stop()
+		c.Assert(err, IsNil)
+	}()
+	// Check that the watcher returns an initial event
+	channel := resource.(*state.EnvironConfigWatcher).Changes()
+	// Should use helpers from state/watcher_test.go when generalised
+	select {
+	case _, ok := <-channel:
+		c.Assert(ok, Equals, true)
+	case <-time.After(50 * time.Millisecond):
+		c.Fatal("timeout waiting for entity watcher")
+	}
 }
