d2f071973dcbcd03904cdf671fc617dc7d649b7c
John Arbash Meinel <john@arbash-meinel.com>
2013-06-30 17:12:51 +0400

    Implement a LessThan checker

--- testing/checkers/relop_test.go
+++ testing/checkers/relop_test.go
@@ -22,3 +22,14 @@ func (s *RelopSuite) TestGreaterThan(c *C) {
 	c.Assert(result, IsFalse)
 	c.Assert(msg, Equals, `obtained value string:"Hello" not supported`)
 }
+
+func (s *RelopSuite) TestLessThan(c *C) {
+	c.Assert(42, LessThan, 45)
+	c.Assert(1.0, LessThan, 2.25)
+	c.Assert(42, Not(LessThan), 42)
+	c.Assert(42, Not(LessThan), 10)
+
+	result, msg := LessThan.Check([]interface{}{"Hello", "World"}, nil)
+	c.Assert(result, IsFalse)
+	c.Assert(msg, Equals, `obtained value string:"Hello" not supported`)
+}
