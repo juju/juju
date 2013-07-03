62b8bc003f7521814a64dfefda2385d49a117cd8
Tim Penhey <tim.penhey@canonical.com>
2013-07-03 17:11:04 +1200

    Add local provider to all list.

--- environs/all/all.go
+++ environs/all/all.go
@@ -6,6 +6,7 @@ package all
 // Register all the available providers.
 import (
 	_ "launchpad.net/juju-core/environs/ec2"
+	_ "launchpad.net/juju-core/environs/local"
 	_ "launchpad.net/juju-core/environs/maas"
 	_ "launchpad.net/juju-core/environs/openstack"
 )
