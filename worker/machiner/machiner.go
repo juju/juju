62b5fcdbea837680feebedc35e87a606b43d4479
John Arbash Meinel <john@arbash-meinel.com>
2013-07-08 16:57:55 +0400

    Switch worker/machiner/ to using NotifyWorker.
    Should be a similar cleanup/simplification as we saw with the Cleaner worker.
    This should also get us closer to being API only.

--- worker/machiner/machiner.go
+++ worker/machiner/machiner.go
@@ -5,100 +5,93 @@ package machiner
 
 import (
 	"fmt"
+
+        "launchpad.net/loggo"
+
 	"launchpad.net/juju-core/errors"
-	"launchpad.net/juju-core/log"
 	"launchpad.net/juju-core/state"
+	"launchpad.net/juju-core/state/presence"
 	"launchpad.net/juju-core/state/api/params"
-	"launchpad.net/juju-core/state/watcher"
 	"launchpad.net/juju-core/worker"
-	"launchpad.net/tomb"
 )
+
+var logger = loggo.GetLogger("juju.worker.machiner")
 
 // Machiner is responsible for a machine agent's lifecycle.
 type Machiner struct {
-	tomb tomb.Tomb
-	st   *state.State
-	id   string
+	st *state.State
+	id string
+        pinger *presence.Pinger
+        machine *state.Machine
 }
 
 // NewMachiner returns a Machiner that will wait for the identified machine
 // to become Dying and make it Dead; or until the machine becomes Dead by
 // other means.
-func NewMachiner(st *state.State, id string) *Machiner {
+func NewMachiner(st *state.State, id string) worker.NotifyWorker {
 	mr := &Machiner{st: st, id: id}
-	go func() {
-		defer mr.tomb.Done()
-		mr.tomb.Kill(mr.loop())
-	}()
-	return mr
+	return worker.NewNotifyWorker(mr)
 }
 
 func (mr *Machiner) String() string {
 	return fmt.Sprintf("machiner %s", mr.id)
 }
 
-func (mr *Machiner) Kill() {
-	mr.tomb.Kill(nil)
-}
-
-func (mr *Machiner) Stop() error {
-	mr.tomb.Kill(nil)
-	return mr.tomb.Wait()
-}
-
-func (mr *Machiner) Wait() error {
-	return mr.tomb.Wait()
-}
-
-func (mr *Machiner) loop() error {
+func (mr *Machiner) SetUp() (params.NotifyWatcher, error) {
 	// Find which machine we're responsible for.
 	m, err := mr.st.Machine(mr.id)
 	if errors.IsNotFoundError(err) {
-		return worker.ErrTerminateAgent
+		return nil, worker.ErrTerminateAgent
 	} else if err != nil {
-		return err
+		return nil, err
 	}
+        mr.machine = m
 
 	// Announce our presence to the world.
 	pinger, err := m.SetAgentAlive()
 	if err != nil {
-		return err
+		return nil, err
 	}
-	defer watcher.Stop(pinger, &mr.tomb)
-	log.Debugf("worker/machiner: agent for machine %q is now alive", m)
+        // Now that this is added, TearDown will ensure it is cleaned up
+        mr.pinger = pinger
+	logger.Debugf("agent for machine %q is now alive", m)
 
 	// Mark the machine as started and log it.
 	if err := m.SetStatus(params.StatusStarted, ""); err != nil {
-		return err
+		return nil, err
 	}
-	log.Noticef("worker/machiner: machine %q started", m)
+	logger.Infof("machine %q started", m)
 
 	w := m.Watch()
-	defer watcher.Stop(w, &mr.tomb)
-	for {
-		select {
-		case <-mr.tomb.Dying():
-			return tomb.ErrDying
-		case <-w.Changes():
-			if err := m.Refresh(); errors.IsNotFoundError(err) {
-				return worker.ErrTerminateAgent
-			} else if err != nil {
-				return err
-			}
-			if m.Life() != state.Alive {
-				log.Debugf("worker/machiner: machine %q is now %s", m, m.Life())
-				if err := m.SetStatus(params.StatusStopped, ""); err != nil {
-					return err
-				}
-				// If the machine is Dying, it has no units,
-				// and can be safely set to Dead.
-				if err := m.EnsureDead(); err != nil {
-					return err
-				}
-				log.Noticef("worker/machiner: machine %q shutting down", m)
-				return worker.ErrTerminateAgent
-			}
-		}
-	}
-	panic("unreachable")
+        return w, nil
+}
+
+func (mr *Machiner) Handle() error {
+    if err := mr.machine.Refresh(); errors.IsNotFoundError(err) {
+            return worker.ErrTerminateAgent
+    } else if err != nil {
+            return err
+    }
+    if mr.machine.Life() != state.Alive {
+            logger.Debugf("machine %q is now %s", mr.machine, mr.machine.Life())
+            if err := mr.machine.SetStatus(params.StatusStopped, ""); err != nil {
+                    return err
+            }
+            // If the machine is Dying, it has no units,
+            // and can be safely set to Dead.
+            if err := mr.machine.EnsureDead(); err != nil {
+                    return err
+            }
+            logger.Infof("machine %q shutting down", mr.machine)
+            return worker.ErrTerminateAgent
+    }
+    return nil
+}
+
+func (mr *Machiner) TearDown() error {
+    var err error
+    if mr.pinger != nil {
+        err = mr.pinger.Stop()
+    }
+    return err
 }
