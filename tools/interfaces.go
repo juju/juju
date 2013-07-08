5ccf1aba2ce9eb76aba7887339d1cb93132b9c9e
John Arbash Meinel <john@arbash-meinel.com>
2013-07-08 13:01:10 +0400

    Feedback from Frank Mueller

--- tools/interfaces.go
+++ tools/interfaces.go
@@ -16,9 +16,11 @@ type Tools struct {
 
 // ToolsManager keeps track of a pool of tools
 type ToolsManager interface {
+
 	// ReadTools looks in the current storage to see what tools are
 	// available that match the given Binary version.
 	ReadTools(version version.Binary) (*Tools, error)
+
 	// UnpackTools reads the compressed tarball from the io.Reader and
 	// extracts the tools to be used. tools is used to indicate what exact
 	// version are in the contents of the tarball
