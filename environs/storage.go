9dbc9608f5de3538e38d3b92c824e6e0bc13b409
jtv@canonical.com <>
2013-07-10 12:11:15 +0700

    Utility RemoveAll implementation.

---
+++ environs/storage.go
@@ -0,0 +1,28 @@
+// Copyright 2013 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package environs
+
+import (
+	"fmt"
+)
+
+// RemoveAll is a default implementation for StorageWriter.RemoveAll.
+// Providers may have more efficient implementations, or better error handling,
+// or safeguards against races with other users of the same storage medium.
+// But a simple way to implement RemoveAll would be to delegate to here.
+func RemoveAll(stor Storage) error {
+	files, err := stor.List("")
+	if err != nil {
+		return fmt.Errorf("unable to list files for deletion: %v", err)
+	}
+
+	// Some limited parallellism might be useful in this loop.
+	for _, file := range files {
+		err = stor.Remove(file)
+		if err != nil {
+			break
+		}
+	}
+	return err
+}
