72211c1dd70f8a2d667ab2e7eba323034003b89c
jtv@canonical.com <>
2013-07-15 20:32:03 +0700

    Update MAAS provider to use PatchAttemptStrategies as well.

--- environs/maas/maas_test.go
+++ environs/maas/maas_test.go
@@ -10,6 +10,7 @@ import (
 	. "launchpad.net/gocheck"
 	"launchpad.net/gomaasapi"
 	"launchpad.net/juju-core/environs"
+	envtesting "launchpad.net/juju-core/environs/testing"
 	"launchpad.net/juju-core/testing"
 	"launchpad.net/juju-core/utils"
 )
@@ -22,24 +23,13 @@ type ProviderSuite struct {
 	testing.LoggingSuite
 	environ              *maasEnviron
 	testMAASObject       *gomaasapi.TestMAASObject
-	originalShortAttempt utils.AttemptStrategy
-	originalLongAttempt  utils.AttemptStrategy
+	restoreTimeouts func()
 }
 
 var _ = Suite(&ProviderSuite{})
 
 func (s *ProviderSuite) SetUpSuite(c *C) {
-	s.originalShortAttempt = shortAttempt
-	s.originalLongAttempt = environs.LongAttempt
-
-	// Careful: this must be an assignment ("="), not an
-	// initialization (":=").  We're trying to change a
-	// global variable here.
-	shortAttempt = utils.AttemptStrategy{
-		Total: 100 * time.Millisecond,
-		Delay: 10 * time.Millisecond,
-	}
-	environs.LongAttempt = shortAttempt
+	s.restoreTimeouts = envtesting.PatchAttemptStrategies(&shortAttempt)
 	s.LoggingSuite.SetUpSuite(c)
 	TestMAASObject := gomaasapi.NewTestMAAS("1.0")
 	s.testMAASObject = TestMAASObject
@@ -57,11 +47,6 @@ func (s *ProviderSuite) TearDownTest(c *C) {
 
 func (s *ProviderSuite) TearDownSuite(c *C) {
 	s.testMAASObject.Close()
-
-	// Careful: this must be an assignment ("="), not an
-	// initialization (":=").  We're trying to change a
-	// global variable here.
-	shortAttempt = s.originalShortAttempt
-	environs.LongAttempt = s.originalLongAttempt
+	s.restoreTimeouts()
 	s.LoggingSuite.TearDownSuite(c)
 }
