71524b28e7581164256bab0c0346bef282ca032a
Tim Penhey <tim.penhey@canonical.com>
2013-07-17 17:09:42 +1200

    Mock out the getAddressForInterface function for the tests.

--- environs/local/environprovider_test.go
+++ environs/local/environprovider_test.go
@@ -7,21 +7,25 @@ import (
 	gc "launchpad.net/gocheck"
 	"launchpad.net/loggo"
 
+	"launchpad.net/juju-core/environs/local"
 	"launchpad.net/juju-core/testing"
 )
 
 type baseProviderSuite struct {
 	testing.LoggingSuite
-	home *testing.FakeHome
+	home    *testing.FakeHome
+	restore func()
 }
 
 func (s *baseProviderSuite) SetUpTest(c *gc.C) {
 	s.LoggingSuite.SetUpTest(c)
 	s.home = testing.MakeFakeHomeNoEnvironments(c, "test")
 	loggo.GetLogger("juju.environs.local").SetLogLevel(loggo.TRACE)
+	s.restore = local.MockAddressForInterface()
 }
 
 func (s *baseProviderSuite) TearDownTest(c *gc.C) {
+	s.restore()
 	s.home.Restore()
 	s.LoggingSuite.TearDownTest(c)
 }
