926240f3f4967a5301893b1f77b63f8e019b2b21
Tim Penhey <tim.penhey@canonical.com>
2013-07-22 14:36:41 +1200

    Add a testing helper of finding dependencies of a package.

---
+++ testing/imports.go
@@ -0,0 +1,44 @@
+// Copyright 2013 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package testing
+
+import (
+	"go/build"
+	"path/filepath"
+	"sort"
+	"strings"
+
+	gc "launchpad.net/gocheck"
+)
+
+// FindJujuCoreImports returns a sorted list of juju-core packages that are
+// imported by the packageName parameter.  The resulting list removes the
+// common prefix "launchpad.net/juju-core/" leaving just the short names.
+func FindJujuCoreImports(c *gc.C, packageName string) []string {
+	var imports []string
+
+	for _, root := range build.Default.SrcDirs() {
+		fullpath := filepath.Join(root, packageName)
+		pkg, err := build.ImportDir(fullpath, 0)
+		if err != nil {
+			continue
+		}
+		imports = pkg.Imports
+		break
+	}
+	if imports == nil {
+		c.Logf(packageName + " not found")
+		c.Fail()
+	}
+
+	var result []string
+	const prefix = "launchpad.net/juju-core/"
+	for _, name := range imports {
+		if strings.HasPrefix(name, prefix) {
+			result = append(result, name[len(prefix):])
+		}
+	}
+	sort.Strings(result)
+	return result
+}
