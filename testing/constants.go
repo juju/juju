2328945ed809a130c08041c2bb6eb496dab0c821
Roger Peppe <roger.peppe@canonical.com>
2013-07-23 18:37:37 +0100

    testing: revert ShortWait

--- testing/constants.go
+++ testing/constants.go
@@ -11,7 +11,7 @@ import (
 // ShortWait is a reasonable amount of time to block waiting for something that
 // shouldn't actually happen. (as in, the test suite will *actually* wait this
 // long before continuing)
-const ShortWait = 1 * time.Microsecond
+const ShortWait = 50 * time.Millisecond
 
 // LongWait is used when something should have already happened, or happens
 // quickly, but we want to make sure we just haven't missed it. As in, the test
