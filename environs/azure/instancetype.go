0acf8cedf98bd982822c71800638479a96631d46
jtv@canonical.com <>
2013-07-23 17:54:30 +0700

    Azure machine-type selection.

---
+++ environs/azure/instancetype.go
@@ -0,0 +1,99 @@
+// Copyright 2013 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package azure
+
+import (
+	"fmt"
+	"sort"
+
+	"launchpad.net/gwacl"
+
+	"launchpad.net/juju-core/constraints"
+)
+
+// preferredTypes is a list of machine types, in order of preference so that
+// the first type that matches a set of hardware constraints is also the best
+// (cheapest) fit for those constraints.  Or if your constraint is a maximum
+// price you're willing to pay, your best match is the last type that falls
+// within your threshold price.
+type preferredTypes []*gwacl.RoleSize
+
+// preferredTypes implements sort.Interface.
+var _ sort.Interface = (*preferredTypes)(nil)
+
+// newPreferredTypes creates a preferredTypes based on the given slice of
+// RoleSize objects.  It will hold pointers to the elements of that slice.
+func newPreferredTypes(availableTypes []gwacl.RoleSize) preferredTypes {
+	types := make(preferredTypes, len(availableTypes))
+	for index := range availableTypes {
+		types[index] = &availableTypes[index]
+	}
+	sort.Sort(&types)
+	return types
+}
+
+// Len is specified in sort.Interface.
+func (types *preferredTypes) Len() int {
+	return len(*types)
+}
+
+// Less is specified in sort.Interface.
+func (types *preferredTypes) Less(first, second int) bool {
+	// All we care about for now is cost.  If at some point Azure offers
+	// different tradeoffs for the same price, we may need a tie-breaker.
+	return (*types)[first].Cost < (*types)[second].Cost
+}
+
+// Swap is specified in sort.Interface.
+func (types *preferredTypes) Swap(first, second int) {
+	firstPtr := &(*types)[first]
+	secondPtr := &(*types)[second]
+	*secondPtr, *firstPtr = *firstPtr, *secondPtr
+}
+
+// suffices returns whether the given value is high enough to meet the
+// required value, if any.  If "required" is nil, there is no requirement
+// and so any given value will do.
+//
+// This is a method only to limit namespace pollution.  It ignores the receiver.
+func (*preferredTypes) suffices(given uint64, required *uint64) bool {
+	return required == nil || given >= *required
+}
+
+func (*preferredTypes) isValidArch(arch *string) bool {
+	return arch == nil || *arch == "i386" || *arch == "amd64"
+}
+
+// satisfies returns whether the given machine type is enough to satisfy
+// the given constraints.  (It doesn't matter if it's overkill; all that
+// matters here is whether the machine type is good enough.)
+//
+// This is a method only to limit namespace pollution.  It ignores the receiver.
+func (types *preferredTypes) satisfies(machineType *gwacl.RoleSize, constraint constraints.Value) bool {
+	// gwacl does not model CPU power yet, although Azure does have the
+	// option of a shared core (for ExtraSmall instances).  For now we
+	// just pretend that's a full-fledged core.
+	return (types.suffices(machineType.CpuCores, constraint.CpuCores) &&
+		types.suffices(machineType.Mem, constraint.Mem))
+}
+
+// selectMachineType returns the Azure machine type that best matches the
+// supplied instanceContraint.
+func selectMachineType(availableTypes []gwacl.RoleSize, constraint constraints.Value) (*gwacl.RoleSize, error) {
+	types := newPreferredTypes(availableTypes)
+
+	if !types.isValidArch(constraint.Arch) {
+		return nil, fmt.Errorf("unsupported architecture requested: %q", *constraint.Arch)
+	}
+	if constraint.Container != nil {
+		return nil, fmt.Errorf("container type requested, but not supported in Azure: %v", *constraint.Container)
+	}
+
+	for _, machineType := range types {
+		if types.satisfies(machineType, constraint) {
+			return machineType, nil
+		}
+	}
+	return nil, fmt.Errorf("no machine type matches constraints: %v", constraint)
+}
---
+++ environs/azure/instancetype_test.go
@@ -0,0 +1,212 @@
+// Copyright 2013 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package azure
+
+import (
+	gc "launchpad.net/gocheck"
+	"launchpad.net/gwacl"
+
+	"launchpad.net/juju-core/constraints"
+)
+
+type InstanceTypeSuite struct{}
+
+var _ = gc.Suite(&InstanceTypeSuite{})
+
+func (*InstanceTypeSuite) TestNewPreferredTypesAcceptsNil(c *gc.C) {
+	types := newPreferredTypes(nil)
+
+	c.Check(types, gc.HasLen, 0)
+	c.Check(types.Len(), gc.Equals, 0)
+}
+
+func (*InstanceTypeSuite) TestNewPreferredTypesRepresentsInput(c *gc.C) {
+	availableTypes := []gwacl.RoleSize{{Name: "Humongous", Cost: 123}}
+
+	types := newPreferredTypes(availableTypes)
+
+	c.Assert(types, gc.HasLen, len(availableTypes))
+	c.Check(types[0], gc.Equals, &availableTypes[0])
+	c.Check(types.Len(), gc.Equals, len(availableTypes))
+}
+
+func (*InstanceTypeSuite) TestNewPreferredTypesSortsByCost(c *gc.C) {
+	availableTypes := []gwacl.RoleSize{
+		{Name: "Excessive", Cost: 12},
+		{Name: "Ridiculous", Cost: 99},
+		{Name: "Modest", Cost: 3},
+	}
+
+	types := newPreferredTypes(availableTypes)
+
+	c.Assert(types, gc.HasLen, len(availableTypes))
+	// We end up with machine types sorted by ascending cost.
+	c.Check(types[0].Name, gc.Equals, "Modest")
+	c.Check(types[1].Name, gc.Equals, "Excessive")
+	c.Check(types[2].Name, gc.Equals, "Ridiculous")
+}
+
+func (*InstanceTypeSuite) TestLessComparesCost(c *gc.C) {
+	types := preferredTypes{
+		{Name: "Cheap", Cost: 1},
+		{Name: "Posh", Cost: 200},
+	}
+
+	c.Check(types.Less(0, 1), gc.Equals, true)
+	c.Check(types.Less(1, 0), gc.Equals, false)
+}
+
+func (*InstanceTypeSuite) TestSwapSwitchesEntries(c *gc.C) {
+	types := preferredTypes{
+		{Name: "First"},
+		{Name: "Last"},
+	}
+
+	types.Swap(0, 1)
+
+	c.Check(types[0].Name, gc.Equals, "Last")
+	c.Check(types[1].Name, gc.Equals, "First")
+}
+
+func (*InstanceTypeSuite) TestSwapIsCommutative(c *gc.C) {
+	types := preferredTypes{
+		{Name: "First"},
+		{Name: "Last"},
+	}
+
+	types.Swap(1, 0)
+
+	c.Check(types[0].Name, gc.Equals, "Last")
+	c.Check(types[1].Name, gc.Equals, "First")
+}
+
+func (*InstanceTypeSuite) TestSwapLeavesOtherEntriesIntact(c *gc.C) {
+	types := preferredTypes{
+		{Name: "A"},
+		{Name: "B"},
+		{Name: "C"},
+		{Name: "D"},
+	}
+
+	types.Swap(1, 2)
+
+	c.Check(types[0].Name, gc.Equals, "A")
+	c.Check(types[1].Name, gc.Equals, "C")
+	c.Check(types[2].Name, gc.Equals, "B")
+	c.Check(types[3].Name, gc.Equals, "D")
+}
+
+func (*InstanceTypeSuite) TestSufficesAcceptsNilRequirement(c *gc.C) {
+	types := preferredTypes{}
+	c.Check(types.suffices(0, nil), gc.Equals, true)
+}
+
+func (*InstanceTypeSuite) TestSufficesAcceptsMetRequirement(c *gc.C) {
+	types := preferredTypes{}
+	var expectation uint64 = 100
+	c.Check(types.suffices(expectation+1, &expectation), gc.Equals, true)
+}
+
+func (*InstanceTypeSuite) TestSufficesAcceptsExactRequirement(c *gc.C) {
+	types := preferredTypes{}
+	var expectation uint64 = 100
+	c.Check(types.suffices(expectation+1, &expectation), gc.Equals, true)
+}
+
+func (*InstanceTypeSuite) TestSufficesRejectsUnmetRequirement(c *gc.C) {
+	types := preferredTypes{}
+	var expectation uint64 = 100
+	c.Check(types.suffices(expectation-1, &expectation), gc.Equals, false)
+}
+
+func (*InstanceTypeSuite) TestSatisfiesComparesCPUCores(c *gc.C) {
+	types := preferredTypes{}
+	var desiredCores uint64 = 5
+	constraint := constraints.Value{CpuCores: &desiredCores}
+
+	// A machine with fewer cores than required does not satisfy...
+	machine := gwacl.RoleSize{CpuCores: desiredCores - 1}
+	c.Check(types.satisfies(&machine, constraint), gc.Equals, false)
+	// ...Even if it would, given more cores.
+	machine.CpuCores = desiredCores
+	c.Check(types.satisfies(&machine, constraint), gc.Equals, true)
+}
+
+func (*InstanceTypeSuite) TestSatisfiesComparesMem(c *gc.C) {
+	types := preferredTypes{}
+	var desiredMem uint64 = 37
+	constraint := constraints.Value{Mem: &desiredMem}
+
+	// A machine with less memory than required does not satisfy...
+	machine := gwacl.RoleSize{Mem: desiredMem - 1}
+	c.Check(types.satisfies(&machine, constraint), gc.Equals, false)
+	// ...Even if it would, given more memory.
+	machine.Mem = desiredMem
+	c.Check(types.satisfies(&machine, constraint), gc.Equals, true)
+}
+
+func (*InstanceTypeSuite) TestIsValidArch(c *gc.C) {
+	types := preferredTypes{}
+
+	// No architecture needs to be specified.
+	c.Check(types.isValidArch(nil), gc.Equals, true)
+
+	// Azure supports these architectures...
+	supported := []string{
+		"amd64",
+		"i386",
+	}
+	for _, arch := range supported {
+		c.Log("Checking that %q is supported.", arch)
+		c.Check(types.isValidArch(&arch), gc.Equals, true)
+	}
+
+	// ...But not these.
+	unsupported := []string{
+		"",
+		"axp",
+		"powerpc",
+	}
+	for _, arch := range unsupported {
+		c.Log("Checking that %q is not supported.", arch)
+		c.Check(types.isValidArch(&arch), gc.Equals, false)
+	}
+}
+
+func (*InstanceTypeSuite) TestSelectMachineTypeChecksArch(c *gc.C) {
+	unsupportedArch := "amd32k"
+	constraint := constraints.Value{Arch: &unsupportedArch}
+	_, err := selectMachineType(nil, constraint)
+	c.Assert(err, gc.NotNil)
+	c.Check(err, gc.ErrorMatches, `unsupported architecture requested: "amd32k"`)
+}
+
+func (*InstanceTypeSuite) TestSelectMachineTypeReturnsErrorIfNoMatch(c *gc.C) {
+	var lots uint64 = 1000000000000
+	_, err := selectMachineType(nil, constraints.Value{Mem: &lots})
+	c.Assert(err, gc.NotNil)
+	c.Check(err, gc.ErrorMatches, "no machine type matches constraints: mem=100000*[MGT]")
+}
+
+func (*InstanceTypeSuite) TestSelectMachineTypeReturnsCheapestMatch(c *gc.C) {
+	var desiredCores uint64 = 50
+	availableTypes := []gwacl.RoleSize{
+		// Cheap, but not up to our requirements.
+		{Name: "Panda", CpuCores: desiredCores / 2, Cost: 10},
+		// Exactly what we need, but not the cheapest match.
+		{Name: "LFA", CpuCores: desiredCores, Cost: 200},
+		// Much more power than we need, but actually cheaper.
+		{Name: "Lambo", CpuCores: 2 * desiredCores, Cost: 100},
+		// Way out of our league.
+		{Name: "Veyron", CpuCores: 10 * desiredCores, Cost: 500},
+	}
+
+	choice, err := selectMachineType(availableTypes, constraints.Value{CpuCores: &desiredCores})
+	c.Assert(err, gc.IsNil)
+
+	// Out of these options, selectMachineType picks not the first; not
+	// the cheapest; not the biggest; not the last; but the cheapest type
+	// of machine that meets requirements.
+	c.Check(choice.Name, gc.Equals, "Lambo")
+}
