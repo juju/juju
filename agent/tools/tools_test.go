e70af07587b5bd58a94d9474e1f839ad13262ead
Tim Penhey <tim.penhey@canonical.com>
2013-07-23 17:15:43 +1200

    Add a comment for the package dependency check.

--- agent/tools/tools_test.go
+++ agent/tools/tools_test.go
@@ -30,6 +30,10 @@ func (t *ToolsSuite) SetUpTest(c *gc.C) {
 }
 
 func (t *ToolsSuite) TestPackageDependencies(c *gc.C) {
+	// This test is to ensure we don't bring in dependencies on state, environ
+	// or any of the other bigger packages that'll drag in yet more dependencies.
+	// Only imports that start with "launchpad.net/juju-core" are checked, and the
+	// resulting slice has that prefix removed to keep the output short.
 	c.Assert(testing.FindJujuCoreImports(c, "launchpad.net/juju-core/agent/tools"),
 		gc.DeepEquals,
 		[]string{"utils/set", "version"})
