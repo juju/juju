dc9f1da820af5de9054c27e5288f85bdc987ca2e
Tim Penhey <tim.penhey@canonical.com>
2013-07-24 15:37:07 +1200

    gc prefix for storage_test

--- agent/tools/storage_test.go
+++ agent/tools/storage_test.go
@@ -14,7 +14,7 @@ import (
 	"sort"
 	"strings"
 
-	. "launchpad.net/gocheck"
+	gc "launchpad.net/gocheck"
 
 	"launchpad.net/juju-core/agent/tools"
 	"launchpad.net/juju-core/environs"
@@ -30,9 +30,9 @@ type StorageSuite struct {
 	dataDir string
 }
 
-var _ = Suite(&StorageSuite{})
+var _ = gc.Suite(&StorageSuite{})
 
-func (s *StorageSuite) SetUpTest(c *C) {
+func (s *StorageSuite) SetUpTest(c *gc.C) {
 	s.LoggingSuite.SetUpTest(c)
 	env, err := environs.NewFromAttrs(map[string]interface{}{
 		"name":            "test",
@@ -42,39 +42,39 @@ func (s *StorageSuite) SetUpTest(c *C) {
 		"ca-cert":         testing.CACert,
 		"ca-private-key":  "",
 	})
-	c.Assert(err, IsNil)
+	c.Assert(err, gc.IsNil)
 	s.env = env
 	s.dataDir = c.MkDir()
 }
 
-func (s *StorageSuite) TearDownTest(c *C) {
+func (s *StorageSuite) TearDownTest(c *gc.C) {
 	dummy.Reset()
 	s.LoggingSuite.TearDownTest(c)
 }
 
-func (s *StorageSuite) TestStorageName(c *C) {
+func (s *StorageSuite) TestStorageName(c *gc.C) {
 	vers := version.MustParseBinary("1.2.3-precise-amd64")
 	path := tools.StorageName(vers)
-	c.Assert(path, Equals, "tools/juju-1.2.3-precise-amd64.tgz")
+	c.Assert(path, gc.Equals, "tools/juju-1.2.3-precise-amd64.tgz")
 }
 
-func (s *StorageSuite) TestSetToolPrefix(c *C) {
+func (s *StorageSuite) TestSetToolPrefix(c *gc.C) {
 	vers := version.MustParseBinary("1.2.3-precise-amd64")
 	tools.SetToolPrefix("test_prefix/juju-")
 	path := tools.StorageName(vers)
-	c.Assert(path, Equals, "test_prefix/juju-1.2.3-precise-amd64.tgz")
+	c.Assert(path, gc.Equals, "test_prefix/juju-1.2.3-precise-amd64.tgz")
 	tools.SetToolPrefix(tools.DefaultToolPrefix)
 	path = tools.StorageName(vers)
-	c.Assert(path, Equals, "tools/juju-1.2.3-precise-amd64.tgz")
+	c.Assert(path, gc.Equals, "tools/juju-1.2.3-precise-amd64.tgz")
 }
 
-func (s *StorageSuite) TestReadListEmpty(c *C) {
+func (s *StorageSuite) TestReadListEmpty(c *gc.C) {
 	store := s.env.Storage()
 	_, err := tools.ReadList(store, 2)
-	c.Assert(err, Equals, tools.ErrNoTools)
+	c.Assert(err, gc.Equals, tools.ErrNoTools)
 }
 
-func (s *StorageSuite) TestReadList(c *C) {
+func (s *StorageSuite) TestReadList(c *gc.C) {
 	store := s.env.Storage()
 	v001 := version.MustParseBinary("0.0.1-precise-amd64")
 	t001 := envtesting.UploadFakeToolsVersion(c, store, v001)
@@ -96,46 +96,46 @@ func (s *StorageSuite) TestReadList(c *C) {
 		c.Logf("test %d", i)
 		list, err := tools.ReadList(store, t.majorVersion)
 		if t.list != nil {
-			c.Assert(err, IsNil)
-			c.Assert(list, DeepEquals, t.list)
+			c.Assert(err, gc.IsNil)
+			c.Assert(list, gc.DeepEquals, t.list)
 		} else {
-			c.Assert(err, Equals, tools.ErrNoMatches)
+			c.Assert(err, gc.Equals, tools.ErrNoMatches)
 		}
 	}
 }
 
-func (s *StorageSuite) TestUpload(c *C) {
+func (s *StorageSuite) TestUpload(c *gc.C) {
 	t, err := tools.Upload(s.env.Storage(), nil)
-	c.Assert(err, IsNil)
-	c.Assert(t.Binary, Equals, version.Current)
-	c.Assert(t.URL, Not(Equals), "")
+	c.Assert(err, gc.IsNil)
+	c.Assert(t.Binary, gc.Equals, version.Current)
+	c.Assert(t.URL, gc.Not(gc.Equals), "")
 	dir := downloadTools(c, t)
 	out, err := exec.Command(filepath.Join(dir, "jujud"), "version").CombinedOutput()
-	c.Assert(err, IsNil)
-	c.Assert(string(out), Equals, version.Current.String()+"\n")
+	c.Assert(err, gc.IsNil)
+	c.Assert(string(out), gc.Equals, version.Current.String()+"\n")
 }
 
-func (s *StorageSuite) TestUploadFakeSeries(c *C) {
+func (s *StorageSuite) TestUploadFakeSeries(c *gc.C) {
 	t, err := tools.Upload(s.env.Storage(), nil, "sham", "fake")
-	c.Assert(err, IsNil)
-	c.Assert(t.Binary, Equals, version.Current)
+	c.Assert(err, gc.IsNil)
+	c.Assert(t.Binary, gc.Equals, version.Current)
 	expectRaw := downloadToolsRaw(c, t)
 
 	list, err := tools.ReadList(s.env.Storage(), version.Current.Major)
-	c.Assert(err, IsNil)
-	c.Assert(list, HasLen, 3)
+	c.Assert(err, gc.IsNil)
+	c.Assert(list, gc.HasLen, 3)
 	expectSeries := []string{"fake", "sham", version.CurrentSeries()}
 	sort.Strings(expectSeries)
-	c.Assert(list.Series(), DeepEquals, expectSeries)
+	c.Assert(list.Series(), gc.DeepEquals, expectSeries)
 	for _, t := range list {
 		c.Logf("checking %s", t.URL)
-		c.Assert(t.Number, Equals, version.CurrentNumber())
+		c.Assert(t.Number, gc.Equals, version.CurrentNumber())
 		actualRaw := downloadToolsRaw(c, t)
-		c.Assert(string(actualRaw), Equals, string(expectRaw))
+		c.Assert(string(actualRaw), gc.Equals, string(expectRaw))
 	}
 }
 
-func (s *StorageSuite) TestUploadAndForceVersion(c *C) {
+func (s *StorageSuite) TestUploadAndForceVersion(c *gc.C) {
 	// This test actually tests three things:
 	//   the writing of the FORCE-VERSION file;
 	//   the reading of the FORCE-VERSION file by the version package;
@@ -143,54 +143,54 @@ func (s *StorageSuite) TestUploadAndForceVersion(c *C) {
 	vers := version.Current
 	vers.Patch++
 	t, err := tools.Upload(s.env.Storage(), &vers.Number)
-	c.Assert(err, IsNil)
-	c.Assert(t.Binary, Equals, vers)
+	c.Assert(err, gc.IsNil)
+	c.Assert(t.Binary, gc.Equals, vers)
 }
 
 // Test that the upload procedure fails correctly
 // when the build process fails (because of a bad Go source
 // file in this case).
-func (s *StorageSuite) TestUploadBadBuild(c *C) {
+func (s *StorageSuite) TestUploadBadBuild(c *gc.C) {
 	gopath := c.MkDir()
 	join := append([]string{gopath, "src"}, strings.Split("launchpad.net/juju-core/cmd/broken", "/")...)
 	pkgdir := filepath.Join(join...)
 	err := os.MkdirAll(pkgdir, 0777)
-	c.Assert(err, IsNil)
+	c.Assert(err, gc.IsNil)
 
 	err = ioutil.WriteFile(filepath.Join(pkgdir, "broken.go"), []byte("nope"), 0666)
-	c.Assert(err, IsNil)
+	c.Assert(err, gc.IsNil)
 
 	defer os.Setenv("GOPATH", os.Getenv("GOPATH"))
 	os.Setenv("GOPATH", gopath)
 
 	t, err := tools.Upload(s.env.Storage(), nil)
-	c.Assert(t, IsNil)
-	c.Assert(err, ErrorMatches, `build command "go" failed: exit status 1; can't load package:(.|\n)*`)
+	c.Assert(t, gc.IsNil)
+	c.Assert(err, gc.ErrorMatches, `build command "go" failed: exit status 1; can't load package:(.|\n)*`)
 }
 
 // downloadTools downloads the supplied tools and extracts them into a
 // new directory.
-func downloadTools(c *C, t *tools.Tools) string {
+func downloadTools(c *gc.C, t *tools.Tools) string {
 	resp, err := http.Get(t.URL)
-	c.Assert(err, IsNil)
+	c.Assert(err, gc.IsNil)
 	defer resp.Body.Close()
 	cmd := exec.Command("tar", "xz")
 	cmd.Dir = c.MkDir()
 	cmd.Stdin = resp.Body
 	out, err := cmd.CombinedOutput()
-	c.Assert(err, IsNil, Commentf(string(out)))
+	c.Assert(err, gc.IsNil, gc.Commentf(string(out)))
 	return cmd.Dir
 }
 
 // downloadToolsRaw downloads the supplied tools and returns the raw bytes.
-func downloadToolsRaw(c *C, t *tools.Tools) []byte {
+func downloadToolsRaw(c *gc.C, t *tools.Tools) []byte {
 	resp, err := http.Get(t.URL)
-	c.Assert(err, IsNil)
+	c.Assert(err, gc.IsNil)
 	defer resp.Body.Close()
-	c.Assert(resp.StatusCode, Equals, http.StatusOK)
+	c.Assert(resp.StatusCode, gc.Equals, http.StatusOK)
 	var buf bytes.Buffer
 	_, err = io.Copy(&buf, resp.Body)
-	c.Assert(err, IsNil)
+	c.Assert(err, gc.IsNil)
 	return buf.Bytes()
 }
 
@@ -204,13 +204,13 @@ var setenvTests = []struct {
 	{"zaphod=42", []string{"foo=bar", "arble=", "zaphod=42"}},
 }
 
-func (*StorageSuite) TestSetenv(c *C) {
+func (*StorageSuite) TestSetenv(c *gc.C) {
 	env0 := []string{"foo=bar", "arble="}
 	for i, t := range setenvTests {
 		c.Logf("test %d", i)
 		env := make([]string, len(env0))
 		copy(env, env0)
 		env = tools.Setenv(env, t.set)
-		c.Check(env, DeepEquals, t.expect)
+		c.Check(env, gc.DeepEquals, t.expect)
 	}
 }
