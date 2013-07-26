5c3a471cfa2db3140e0a478b7a898f982c1463f4
Roger Peppe <roger.peppe@canonical.com>
2013-07-26 13:53:37 +0100

    wip

--- worker/upgrader/upgrader_test.go
+++ worker/upgrader/upgrader_test.go
@@ -11,11 +11,12 @@ import (
 	gc "launchpad.net/gocheck"
 
 	"launchpad.net/juju-core/environs"
-	"launchpad.net/juju-core/environs/tools"
+	"launchpad.net/juju-core/agent/tools"
 	"launchpad.net/juju-core/errors"
 	jujutesting "launchpad.net/juju-core/juju/testing"
 	"launchpad.net/juju-core/state"
 	"launchpad.net/juju-core/state/api"
+	apiupgrader "launchpad.net/juju-core/state/api/upgrader"
 	coretesting "launchpad.net/juju-core/testing"
 	jc "launchpad.net/juju-core/testing/checkers"
 	"launchpad.net/juju-core/version"
@@ -27,14 +28,11 @@ func TestPackage(t *stdtesting.T) {
 	coretesting.MgoTestPackage(t)
 }
 
-var _ worker.NotifyWorker = (*upgrader.UpgradeWorker)(nil)
-
 type UpgraderSuite struct {
 	jujutesting.JujuConnSuite
-	//SimpleToolsFixture
 
 	machine *state.Machine
-	apiState   *api.State
+	state  *apiupgrader.State
 }
 
 var _ = gc.Suite(&UpgraderSuite{})
@@ -49,8 +47,8 @@ func (s *UpgraderSuite) SetUpTest(c *gc.C) {
 	err = s.machine.SetPassword("test-password")
 	c.Assert(err, gc.IsNil)
 
-	//s.SimpleToolsFixture.SetUp(c, s.DataDir())
-	s.apiState = s.OpenAPIAs(c, s.machine.Tag(), "test-password")
+	st := s.OpenAPIAs(c, s.machine.Tag(), "test-password")
+	s.state = st.Upgrader()
 }
 
 func (s *UpgraderSuite) TearDownTest(c *gc.C) {
@@ -60,55 +58,46 @@ func (s *UpgraderSuite) TearDownTest(c *gc.C) {
 	s.JujuConnSuite.TearDownTest(c)
 }
 
-func (s *UpgraderSuite) TestUpgraderSetsTools(c *gc.C) {
-	_, err := s.machine.AgentTools()
-	c.Assert(err, jc.Satisfies, errors.IsNotFoundError)
-	upg := upgrader.NewUpgrader(s.apiState, s.machine.Tag(), upgrader.NilToolsManager{})
-	c.Assert(upg.Stop(), gc.IsNil)
-	s.machine.Refresh()
-	ver, err := s.machine.AgentTools()
-	c.Assert(err, gc.IsNil)
-	c.Assert(ver.Binary, gc.Equals, version.Current)
+// primeTools sets up the current version of the tools to vers and
+// makes sure that they're available JujuConnSuite's DataDir.
+func (s *UpgraderSuite) primeTools(c *gc.C, vers version.Binary) *tools.Tools {
+	err := os.RemoveAll(filepath.Join(s.DataDir(), "tools"))
+	c.Assert(err, IsNil)
+	version.Current = vers
+	agentTools := s.uploadTools(c, vers)
+	resp, err := http.Get(agentTools.URL)
+	c.Assert(err, IsNil)
+	defer resp.Body.Close()
+	err = tools.UnpackTools(s.DataDir(), agentTools, resp.Body)
+	c.Assert(err, IsNil)
+	return agentTools
 }
 
-type UpgradeHandlerSuite struct {
-	jujutesting.JujuConnSuite
-	machine *state.Machine
-	apiState   *api.State
-}
-
-func (s *UpgradeHandlerSuite) SetUpTest(c *gc.C) {
-	s.JujuConnSuite.SetUpTest(c)
-
-	// Create a machine to work with
-	var err error
-	s.machine, err = s.State.AddMachine("series", state.JobHostUnits)
-	c.Assert(err, gc.IsNil)
-	err = s.machine.SetPassword("test-password")
-	c.Assert(err, gc.IsNil)
-
-	//s.SimpleToolsFixture.SetUp(c, s.DataDir())
-	s.apiState = s.OpenAPIAs(c, s.machine.Tag(), "test-password")
-}
-
-func (s *UpgradeHandlerSuite) TestUpgraderDoesNothingIfAgentVersionMatches(c *gc.C) {
-	handler := upgrader.NewUpgradeHandler(s.apiState, s.machine.Tag())
-	handler.Handle()
-	c.Assert(handler.DownloadChannel(), gc.IsNil)
-}
-
-func (s *UpgradeHandlerSuite) TestUpgraderGrabsNewTools(c *gc.C) {
-	handler := upgrader.NewUpgradeHandler(s.apiState, s.machine.Tag())
-	handler.Handle()
-}
-
-func uploadTools(storage environs.Storage, c *gc.C, vers version.Binary) *state.Tools {
+// uploadTools uploads fake tools with the given version number
+// to the dummy environment's storage and returns a tools
+// value describing them.
+func (s *UpgraderSuite) uploadTools(c *gc.C, vers version.Binary) *tools.Tools {
 	tgz := coretesting.TarGz(
 		coretesting.NewTarFile("jujud", 0777, "jujud contents "+vers.String()),
 	)
+	storage := s.Conn.Environ.Storage()
 	err := storage.Put(tools.StorageName(vers), bytes.NewReader(tgz), int64(len(tgz)))
+	c.Assert(err, IsNil)
+	url, err := s.Conn.Environ.Storage().URL(tools.StorageName(vers))
+	c.Assert(err, IsNil)
+	return &tools.Tools{URL: url, Version: vers}
+}
+
+func (s *UpgraderSuite) TestUpgraderSetsTools(c *gc.C) {
+	agentTools := s.primeTools(c, version.MustParseBinary("5.3.2-foo-bar"))
+
+	_, err := s.machine.AgentTools()
+	c.Assert(err, jc.Satisfies, errors.IsNotFoundError)
+	
+	u := upgrader.NewUpgrader(s.state, s.DataDir, s.machine.Tag())
+	c.Assert(u.Stop(), gc.IsNil)
+	s.machine.Refresh()
+	gotTools, err := s.machine.AgentTools()
 	c.Assert(err, gc.IsNil)
-	url, err := storage.URL(tools.StorageName(vers))
-	c.Assert(err, gc.IsNil)
-	return &state.Tools{URL: url, Binary: vers}
+	c.Assert(gotTools, gc.DeepEquals, agentTools)
 }
