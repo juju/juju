425d25cd0f8c0c9d659e43744e00fee59bb046fe
Andrew Wilkins <andrew.wilkins@canonical.com>
2013-07-31 20:24:20 +0800

    cmd/juju: confirm destroy-environment

--- cmd/juju/destroyenvironment.go
+++ cmd/juju/destroyenvironment.go
@@ -4,6 +4,11 @@
 package main
 
 import (
+	"fmt"
+	"strings"
+
+	"launchpad.net/gnuflag"
+
 	"launchpad.net/juju-core/cmd"
 	"launchpad.net/juju-core/environs"
 )
@@ -11,6 +16,7 @@ import (
 // DestroyEnvironmentCommand destroys an environment.
 type DestroyEnvironmentCommand struct {
 	EnvCommandBase
+	assumeYes bool
 }
 
 func (c *DestroyEnvironmentCommand) Info() *cmd.Info {
@@ -20,10 +26,37 @@ func (c *DestroyEnvironmentCommand) Info() *cmd.Info {
 	}
 }
 
-func (c *DestroyEnvironmentCommand) Run(_ *cmd.Context) error {
+func (c *DestroyEnvironmentCommand) SetFlags(f *gnuflag.FlagSet) {
+	c.EnvCommandBase.SetFlags(f)
+	f.BoolVar(&c.assumeYes, "y", false, "Do not ask for confirmation")
+	f.BoolVar(&c.assumeYes, "yes", false, "Do not ask for confirmation")
+}
+
+func (c *DestroyEnvironmentCommand) Run(ctx *cmd.Context) error {
 	environ, err := environs.NewFromName(c.EnvName)
 	if err != nil {
 		return err
 	}
+
+	if !c.assumeYes {
+		var answer string
+		fmt.Fprintf(ctx.Stdout, destroyEnvMsg[1:], environ.Name(), environ.Config().Type())
+		_, err = fmt.Fscanln(ctx.Stdin, &answer)
+		if err != nil {
+			return err
+		}
+		answer = strings.ToLower(answer)
+		if answer != "y" && answer != "yes" {
+			fmt.Fprintln(ctx.Stdout, "Environment destruction aborted")
+			return nil
+		}
+	}
+
 	return environ.Destroy(nil)
 }
+
+const destroyEnvMsg = `
+WARNING: this command will destroy the %q environment (type: %s)
+This includes all machines, services, data and other resources.
+
+Continue [y/N]? `
