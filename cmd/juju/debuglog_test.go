a9b413bc16b0e00da24e3bae4f4af349155f0c4e
Andrew Wilkins <andrew.wilkins@canonical.com>
2013-08-02 09:25:20 +0800

    debug-log: refactor tests
    Pull out common assertion logic into helper method.

--- cmd/juju/debuglog_test.go
+++ cmd/juju/debuglog_test.go
@@ -33,20 +33,26 @@ func (c *dummySSHCommand) Run(ctx *cmd.Context) error {
 }
 
 // debug-log is implemented by invoking juju ssh with the correct arguments.
-// This test checks for the expected invocation.
-func (s *DebugLogSuite) TestDebugLogInvokesSSHCommand(c *C) {
+// This test helper checks for the expected invocation.
+func (s *DebugLogSuite) assertDebugLogInvokesSSHCommand(c *C, expected string, args ...string) {
 	defer testing.MakeEmptyFakeHome(c).Restore()
-	debugLogCmd, err := runDebugLog(c)
+	debugLogCmd, err := runDebugLog(c, args...)
 	c.Assert(err, IsNil)
 	debugCmd := debugLogCmd.sshCmd.(*dummySSHCommand)
 	c.Assert(debugCmd.runCalled, Equals, true)
 	c.Assert(debugCmd.Target, Equals, "0")
-	c.Assert([]string{"tail -f /var/log/juju/all-machines.log"}, DeepEquals, debugCmd.Args)
+	c.Assert([]string{expected}, DeepEquals, debugCmd.Args)
+}
 
-	debugLogCmd, err = runDebugLog(c, "--all")
-	c.Assert(err, IsNil)
-	debugCmd = debugLogCmd.sshCmd.(*dummySSHCommand)
-	c.Assert(debugCmd.runCalled, Equals, true)
-	c.Assert(debugCmd.Target, Equals, "0")
-	c.Assert([]string{"tail -n +1 -f /var/log/juju/all-machines.log"}, DeepEquals, debugCmd.Args)
+const logLocation = "/var/log/juju/all-machines.log"
+
+func (s *DebugLogSuite) TestDebugLog(c *C) {
+	const expected = "tail -f " + logLocation
+	s.assertDebugLogInvokesSSHCommand(c, expected)
+}
+
+func (s *DebugLogSuite) TestDebugLogAll(c *C) {
+	const expected = "tail -n +1 -f " + logLocation
+	s.assertDebugLogInvokesSSHCommand(c, expected, "-a")
+	s.assertDebugLogInvokesSSHCommand(c, expected, "--all")
 }
