fad604cb4876c08c600df8f73c8e8f17052204b5
Andrew Wilkins <andrew.wilkins@canonical.com>
2013-08-08 16:55:46 +0800

    Add monitoring-port to TestWordpressEndpoints
    Perhaps this should be moved to a different service,
    since the real wordpress service doesn't have a
    monitoring-port endpoint.

--- state/service_test.go
+++ state/service_test.go
@@ -652,6 +652,18 @@ func (s *ServiceSuite) TestWordpressEndpoints(c *C) {
 		},
 	})
 
+	mpEP, err := wordpress.Endpoint("monitoring-port")
+	c.Assert(err, IsNil)
+	c.Assert(mpEP, DeepEquals, state.Endpoint{
+		ServiceName: "wordpress",
+		Relation: charm.Relation{
+			Interface: "monitoring",
+			Name:      "monitoring-port",
+			Role:      charm.RoleProvider,
+			Scope:     charm.ScopeContainer,
+		},
+	})
+
 	dbEP, err := wordpress.Endpoint("db")
 	c.Assert(err, IsNil)
 	c.Assert(dbEP, DeepEquals, state.Endpoint{
@@ -681,7 +693,7 @@ func (s *ServiceSuite) TestWordpressEndpoints(c *C) {
 
 	eps, err := wordpress.Endpoints()
 	c.Assert(err, IsNil)
-	c.Assert(eps, DeepEquals, []state.Endpoint{cacheEP, dbEP, jiEP, ldEP, urlEP})
+	c.Assert(eps, DeepEquals, []state.Endpoint{cacheEP, dbEP, jiEP, ldEP, mpEP, urlEP})
 }
 
 func (s *ServiceSuite) TestServiceRefresh(c *C) {
