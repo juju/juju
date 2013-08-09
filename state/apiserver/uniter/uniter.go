d205f105fd764339be9ee4b8b6528fb58e4204a8
Dimiter Naydenov <dimiter.naydenov@canonical.com>
2013-08-09 14:01:45 +0200

    (Set)(Private|Public)Address implemented in Uniter API

--- state/apiserver/uniter/uniter.go
+++ state/apiserver/uniter/uniter.go
@@ -6,7 +6,10 @@
 package uniter
 
 import (
+	"fmt"
+
 	"launchpad.net/juju-core/state"
+	"launchpad.net/juju-core/state/api/params"
 	"launchpad.net/juju-core/state/apiserver/common"
 )
 
@@ -17,8 +20,9 @@ type UniterAPI struct {
 	*common.DeadEnsurer
 	*common.AgentEntityWatcher
 
-	st   *state.State
-	auth common.Authorizer
+	st         *state.State
+	auth       common.Authorizer
+	getCanRead common.GetAuthFunc
 }
 
 // NewUniterAPI creates a new instance of the Uniter API.
@@ -36,7 +40,124 @@ func NewUniterAPI(st *state.State, resources *common.Resources, authorizer commo
 		AgentEntityWatcher: common.NewAgentEntityWatcher(st, resources, getCanRead),
 		st:                 st,
 		auth:               authorizer,
+		getCanRead:         getCanRead,
 	}, nil
+}
+
+func (u *UniterAPI) getUnit(tag string) (*state.Unit, error) {
+	entity, err := u.st.FindEntity(tag)
+	if err != nil {
+		return nil, err
+	}
+	unit, ok := entity.(*state.Unit)
+	if !ok {
+		return nil, fmt.Errorf("entity %q is not a unit", tag)
+	}
+	return unit, nil
+}
+
+func (u *UniterAPI) PublicAddress(args params.Entities) (params.StringBoolResults, error) {
+	result := params.StringBoolResults{
+		Results: make([]params.StringBoolResult, len(args.Entities)),
+	}
+	if len(args.Entities) == 0 {
+		return result, nil
+	}
+	canRead, err := u.getCanRead()
+	if err != nil {
+		return params.StringBoolResults{}, err
+	}
+	var unit *state.Unit
+	for i, entity := range args.Entities {
+		err := common.ErrPerm
+		if canRead(entity.Tag) {
+			unit, err = u.getUnit(entity.Tag)
+			if err == nil {
+				address, ok := unit.PublicAddress()
+				result.Results[i].Result = address
+				result.Results[i].Ok = ok
+			}
+		}
+		result.Results[i].Error = common.ServerError(err)
+	}
+	return result, nil
+}
+
+func (u *UniterAPI) SetPublicAddress(args params.SetEntityAddresses) (params.ErrorResults, error) {
+	result := params.ErrorResults{
+		Results: make([]params.ErrorResult, len(args.Entities)),
+	}
+	if len(args.Entities) == 0 {
+		return result, nil
+	}
+	canModify, err := u.getCanRead()
+	if err != nil {
+		return params.ErrorResults{}, err
+	}
+	var unit *state.Unit
+	for i, entity := range args.Entities {
+		err := common.ErrPerm
+		if canModify(entity.Tag) {
+			unit, err = u.getUnit(entity.Tag)
+			if err == nil {
+				err = unit.SetPublicAddress(entity.Address)
+			}
+		}
+		result.Results[i].Error = common.ServerError(err)
+	}
+	return result, nil
+}
+
+func (u *UniterAPI) PrivateAddress(args params.Entities) (params.StringBoolResults, error) {
+	result := params.StringBoolResults{
+		Results: make([]params.StringBoolResult, len(args.Entities)),
+	}
+	if len(args.Entities) == 0 {
+		return result, nil
+	}
+	canRead, err := u.getCanRead()
+	if err != nil {
+		return params.StringBoolResults{}, err
+	}
+	var unit *state.Unit
+	for i, entity := range args.Entities {
+		err := common.ErrPerm
+		if canRead(entity.Tag) {
+			unit, err = u.getUnit(entity.Tag)
+			if err == nil {
+				address, ok := unit.PrivateAddress()
+				result.Results[i].Result = address
+				result.Results[i].Ok = ok
+			}
+		}
+		result.Results[i].Error = common.ServerError(err)
+	}
+	return result, nil
+}
+
+func (u *UniterAPI) SetPrivateAddress(args params.SetEntityAddresses) (params.ErrorResults, error) {
+	result := params.ErrorResults{
+		Results: make([]params.ErrorResult, len(args.Entities)),
+	}
+	if len(args.Entities) == 0 {
+		return result, nil
+	}
+	canModify, err := u.getCanRead()
+	if err != nil {
+		return params.ErrorResults{}, err
+	}
+	var unit *state.Unit
+	for i, entity := range args.Entities {
+		err := common.ErrPerm
+		if canModify(entity.Tag) {
+			unit, err = u.getUnit(entity.Tag)
+			if err == nil {
+				err = unit.SetPrivateAddress(entity.Address)
+			}
+		}
+		result.Results[i].Error = common.ServerError(err)
+	}
+	return result, nil
 }
 
 // TODO(dimitern): Add the other needed API calls.
