985e294da0fee8b9a3e5f2b7807c12b7ad75e808
Tim Penhey <tim.penhey@canonical.com>
2013-09-11 14:25:52 +1200

    Add server end point

---
+++ state/apiserver/logger/logger.go
@@ -0,0 +1,88 @@
+// Copyright 2013 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package logger
+
+import (
+	"launchpad.net/loggo"
+
+	"launchpad.net/juju-core/state"
+	"launchpad.net/juju-core/state/api/params"
+	"launchpad.net/juju-core/state/apiserver/common"
+	"launchpad.net/juju-core/state/watcher"
+)
+
+var logger = loggo.GetLogger("juju.api.logger")
+
+// LoggerAPI defines the methods on the logger API end point.
+type LoggerAPI interface {
+	WatchLoggingConfig(args params.Entities) params.NotifyWatchResults
+	LoggingConfig(args params.Entities) params.StringResults
+}
+
+// NewLoggerAPI creates a new server-side logger API end point.
+func NewLoggerAPI(
+	st *state.State,
+	resources *common.Resources,
+	authorizer common.Authorizer,
+) (LoggerAPI, error) {
+	if !authorizer.AuthMachineAgent() && !authorizer.AuthUnitAgent() {
+		return nil, common.ErrPerm
+	}
+	return &loggerAPI{state: st, resources: resources, authorizer: authorizer}, nil
+}
+
+type loggerAPI struct {
+	state      *state.State
+	resources  *common.Resources
+	authorizer common.Authorizer
+}
+
+var _ LoggerAPI = (*loggerAPI)(nil)
+
+// WatchLoggingConfig starts a watcher to track changes to the logging config.
+// Unfortunately the current infrastruture makes watching parts of the config
+// non-trivial, so currently any change to the config will cause the watcher
+// to notify the client.
+func (api *loggerAPI) WatchLoggingConfig(arg params.Entities) params.NotifyWatchResults {
+	result := make([]params.NotifyWatchResult, len(arg.Entities))
+	for i, entity := range arg.Entities {
+		err := common.ErrPerm
+		if api.authorizer.AuthOwner(entity.Tag) {
+			watch := api.state.WatchForEnvironConfigChanges()
+			// Consume the initial event. Technically, API calls to Watch
+			// 'transmit' the initial event in the Watch response. But
+			// NotifyWatchers have no state to transmit.
+			if _, ok := <-watch.Changes(); ok {
+				result[i].NotifyWatcherId = api.resources.Register(watch)
+				err = nil
+			} else {
+				err = watcher.MustErr(watch)
+			}
+		}
+		result[i].Error = common.ServerError(err)
+	}
+	return params.NotifyWatchResults{result}
+}
+
+// DesiredVersion reports the Agent Version that we want that agent to be running
+func (api *loggerAPI) LoggingConfig(arg params.Entities) params.StringResults {
+	results := make([]params.StringResult, len(arg.Entities))
+	// If someone is stupid enough to call this function with zero entities,
+	// lets punish them by making them wait for us to get the environ config
+	// from state.
+	config, configErr := api.state.EnvironConfig()
+	for i, entity := range arg.Entities {
+		err := common.ErrPerm
+		if api.authorizer.AuthOwner(entity.Tag) {
+			if configErr == nil {
+				results[i].Result = config.LoggingConfig()
+				err = nil
+			} else {
+				err = configErr
+			}
+		}
+		results[i].Error = common.ServerError(err)
+	}
+	return params.StringResults{results}
+}
---
+++ state/apiserver/logger/logger_test.go
@@ -0,0 +1,157 @@
+// Copyright 2013 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package logger_test
+
+import (
+	gc "launchpad.net/gocheck"
+
+	jujutesting "launchpad.net/juju-core/juju/testing"
+	"launchpad.net/juju-core/state"
+	"launchpad.net/juju-core/state/api/params"
+	"launchpad.net/juju-core/state/apiserver/common"
+	"launchpad.net/juju-core/state/apiserver/logger"
+	apiservertesting "launchpad.net/juju-core/state/apiserver/testing"
+	statetesting "launchpad.net/juju-core/state/testing"
+)
+
+type loggerSuite struct {
+	jujutesting.JujuConnSuite
+
+	// These are raw State objects. Use them for setup and assertions, but
+	// should never be touched by the API calls themselves
+	rawMachine *state.Machine
+	logger     logger.LoggerAPI
+	resources  *common.Resources
+	authorizer apiservertesting.FakeAuthorizer
+}
+
+var _ = gc.Suite(&loggerSuite{})
+
+func (s *loggerSuite) SetUpTest(c *gc.C) {
+	s.JujuConnSuite.SetUpTest(c)
+	s.resources = common.NewResources()
+
+	// Create a machine to work with
+	var err error
+	s.rawMachine, err = s.State.AddMachine("series", state.JobHostUnits)
+	c.Assert(err, gc.IsNil)
+	err = s.rawMachine.SetPassword("test-password")
+	c.Assert(err, gc.IsNil)
+
+	// The default auth is as the machine agent
+	s.authorizer = apiservertesting.FakeAuthorizer{
+		Tag:          s.rawMachine.Tag(),
+		LoggedIn:     true,
+		MachineAgent: true,
+	}
+	s.logger, err = logger.NewLoggerAPI(s.State, s.resources, s.authorizer)
+	c.Assert(err, gc.IsNil)
+}
+
+func (s *loggerSuite) TearDownTest(c *gc.C) {
+	s.resources.StopAll()
+	s.JujuConnSuite.TearDownTest(c)
+}
+
+func (s *loggerSuite) TestNewLoggerAPIRefusesNonAgent(c *gc.C) {
+	// We aren't even a machine agent
+	anAuthorizer := s.authorizer
+	anAuthorizer.MachineAgent = false
+	endPoint, err := logger.NewLoggerAPI(s.State, s.resources, anAuthorizer)
+	c.Assert(endPoint, gc.IsNil)
+	c.Assert(err, gc.ErrorMatches, "permission denied")
+}
+
+func (s *loggerSuite) TestNewLoggerAPIAcceptsUnitAgent(c *gc.C) {
+	// We aren't even a machine agent
+	anAuthorizer := s.authorizer
+	anAuthorizer.UnitAgent = true
+	anAuthorizer.MachineAgent = false
+	endPoint, err := logger.NewLoggerAPI(s.State, s.resources, anAuthorizer)
+	c.Assert(err, gc.IsNil)
+	c.Assert(endPoint, gc.NotNil)
+}
+
+func (s *loggerSuite) TestWatchLoggingConfigNothing(c *gc.C) {
+	// Not an error to watch nothing
+	results := s.logger.WatchLoggingConfig(params.Entities{})
+	c.Assert(results.Results, gc.HasLen, 0)
+}
+
+func (s *loggerSuite) setLoggingConfig(c *gc.C, loggingConfig string) {
+	envConfig, err := s.State.EnvironConfig()
+	c.Assert(err, gc.IsNil)
+	newConfig, err := envConfig.Apply(
+		map[string]interface{}{"logging-config": loggingConfig})
+	c.Assert(err, gc.IsNil)
+	err = s.State.SetEnvironConfig(newConfig)
+	c.Assert(err, gc.IsNil)
+	envConfig, err = s.State.EnvironConfig()
+	c.Assert(err, gc.IsNil)
+	c.Assert(envConfig.LoggingConfig(), gc.Equals, loggingConfig)
+}
+
+func (s *loggerSuite) TestWatchLoggingConfig(c *gc.C) {
+	args := params.Entities{
+		Entities: []params.Entity{{Tag: s.rawMachine.Tag()}},
+	}
+	results := s.logger.WatchLoggingConfig(args)
+	c.Assert(results.Results, gc.HasLen, 1)
+	c.Assert(results.Results[0].NotifyWatcherId, gc.Not(gc.Equals), "")
+	c.Assert(results.Results[0].Error, gc.IsNil)
+	resource := s.resources.Get(results.Results[0].NotifyWatcherId)
+	c.Assert(resource, gc.NotNil)
+
+	w := resource.(state.NotifyWatcher)
+	wc := statetesting.NewNotifyWatcherC(c, s.State, w)
+	wc.AssertNoChange()
+
+	s.setLoggingConfig(c, "<root>=WARN;juju.log.test=DEBUG")
+
+	wc.AssertOneChange()
+	statetesting.AssertStop(c, w)
+	wc.AssertClosed()
+}
+
+func (s *loggerSuite) TestWatchLoggingConfigRefusesWrongAgent(c *gc.C) {
+	// We are a machine agent, but not the one we are trying to track
+	args := params.Entities{
+		Entities: []params.Entity{{Tag: "machine-12354"}},
+	}
+	results := s.logger.WatchLoggingConfig(args)
+	// It is not an error to make the request, but the specific item is rejected
+	c.Assert(results.Results, gc.HasLen, 1)
+	c.Assert(results.Results[0].NotifyWatcherId, gc.Equals, "")
+	c.Assert(results.Results[0].Error, gc.DeepEquals, apiservertesting.ErrUnauthorized)
+}
+
+func (s *loggerSuite) TestLoggingConfigForNoone(c *gc.C) {
+	// Not an error to request nothing, dumb, but not an error.
+	results := s.logger.LoggingConfig(params.Entities{})
+	c.Assert(results.Results, gc.HasLen, 0)
+}
+
+func (s *loggerSuite) TestLoggingConfigRefusesWrongAgent(c *gc.C) {
+	args := params.Entities{
+		Entities: []params.Entity{{Tag: "machine-12354"}},
+	}
+	results := s.logger.LoggingConfig(args)
+	c.Assert(results.Results, gc.HasLen, 1)
+	result := results.Results[0]
+	c.Assert(result.Error, gc.DeepEquals, apiservertesting.ErrUnauthorized)
+}
+
+func (s *loggerSuite) TestLoggingConfigForAgent(c *gc.C) {
+	loggingConfig := "<root>=WARN;juju.log.test=DEBUG"
+	s.setLoggingConfig(c, loggingConfig)
+
+	args := params.Entities{
+		Entities: []params.Entity{{Tag: s.rawMachine.Tag()}},
+	}
+	results := s.logger.LoggingConfig(args)
+	c.Assert(results.Results, gc.HasLen, 1)
+	result := results.Results[0]
+	c.Assert(result.Error, gc.IsNil)
+	c.Assert(result.Result, gc.Equals, loggingConfig)
+}
