9eef684c9d4917061516a96a7195586a157a86ac
Roger Peppe <roger.peppe@canonical.com>
2013-09-18 16:23:38 +0100

    environs/configstore: implement write methods

--- environs/configstore/disk.go
+++ environs/configstore/disk.go
@@ -20,10 +20,11 @@ type diskStore struct {
 }
 
 type environInfo struct {
-	User         string
-	Password     string
-	StateServers []string `yaml:"state-servers"`
-	CACert       string   `yaml:"ca-cert"`
+	path string
+	User string
+	Password string
+	StateServers []string	`yaml:"state-servers"`
+	CACert string	`yaml:"ca-cert"`
 }
 
 // NewDisk returns a ConfigStorage implementation that
@@ -42,7 +43,25 @@ func (d *diskStore) envPath(envName string) string {
 	return filepath.Join(d.dir, envName+".yaml")
 }
 
-// EnvironInfo implements environs.ConfigStorage.EnvironInfo.
+// CreateInfo implements environs.ConfigStorage.CreateInfo.
+func (d *diskStore) CreateInfo(envName string) (environs.EnvironInfo, error) {
+	// We create an empty file so that any subsequent CreateInfos
+	// will fail.
+	path := d.envPath(envName)
+	file, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0600)
+	if os.IsExist(err) {
+		return nil, environs.ErrEnvironInfoAlreadyExists
+	}
+	if err != nil {
+		return nil, err
+	}
+	file.Close()
+	return &environInfo{
+		path: path,
+	}, nil
+}
+
+// ReadInfo implements environs.ConfigStorage.ReadInfo.
 func (d *diskStore) ReadInfo(envName string) (environs.EnvironInfo, error) {
 	path := d.envPath(envName)
 	data, err := ioutil.ReadFile(path)
@@ -52,6 +71,9 @@ func (d *diskStore) ReadInfo(envName string) (environs.EnvironInfo, error) {
 		}
 		return nil, err
 	}
+	if len(data) == 0 {
+		return nil, fmt.Errorf("environment in progress XXX better error message needed")
+	}
 	var info environInfo
 	if err := goyaml.Unmarshal(data, &info); err != nil {
 		return nil, fmt.Errorf("error unmarshalling %q: %v", path, err)
@@ -59,16 +81,56 @@ func (d *diskStore) ReadInfo(envName string) (environs.EnvironInfo, error) {
 	return &info, nil
 }
 
+// APICredentials implements environs.EnvironInfo.APICredentials.
 func (info *environInfo) APICredentials() environs.APICredentials {
 	return environs.APICredentials{
-		User:     info.User,
+		User: info.User,
 		Password: info.Password,
 	}
 }
 
+// APIEndpoint implements environs.EnvironInfo.APIEndpoint.
 func (info *environInfo) APIEndpoint() environs.APIEndpoint {
 	return environs.APIEndpoint{
 		Addresses: info.StateServers,
-		CACert:    info.CACert,
+		CACert: info.CACert,
 	}
+}
+
+func (info *environInfo) SetAPIEndpoint(endpoint environs.APIEndpoint) {
+	info.StateServers = endpoint.Addresses
+	info.CACert = endpoint.CACert
+}
+
+func (info *environInfo) SetAPICredentials(creds environs.APICredentials) {
+	info.User = creds.User
+	info.Password = creds.Password
+}
+
+func (info *environInfo) Write() error {
+	data, err := goyaml.Marshal(info)
+	if err != nil {
+		return fmt.Errorf("cannot marshal environment info: %v", err)
+	}
+	// Create a temporary file and rename it, so that the data
+	// changes atomically.
+	parent, _ := filepath.Split(info.path)
+	tmpFile, err := ioutil.TempFile(parent, "")
+	if err != nil {
+		return fmt.Errorf("cannot create temporary file: %v", err)
+	}
+	defer tmpFile.Close()
+	_, err = tmpFile.Write(data)
+	if err != nil {
+		return fmt.Errorf("cannot write temporary file: %v", err)
+	}
+	if err := os.Rename(tmpFile.Name(), info.path); err != nil {
+		os.Remove(tmpFile.Name())
+		return fmt.Errorf("cannot rename new environment info file: %v", err)
+	}
+	return nil
+}
+
+func (info *environInfo) Destroy() error {
+	return os.Remove(info.path)
 }
--- environs/info.go
+++ environs/info.go
@@ -2,6 +2,11 @@
 // Licensed under the AGPLv3, see LICENCE file for details.
 
 package environs
+import (
+	"errors"
+)
+
+var ErrEnvironInfoAlreadyExists = errors.New("environment info already exists")
 
 // APIEndpoint holds information about an API endpoint.
 type APIEndpoint struct {
@@ -29,6 +34,12 @@ type ConfigStorage interface {
 	// If there is no such information, it will
 	// return an errors.NotFound error.
 	ReadInfo(envName string) (EnvironInfo, error)
+
+	// CreateInfo creates some information associated
+	// with the environment with the given name.
+	// It return ErrAlreadyExists if the
+	// information has already been created.
+	CreateInfo(envName string) (EnvironInfo, error)
 }
 
 // EnvironInfo holds information associated with an environment.
@@ -38,4 +49,21 @@ type EnvironInfo interface {
 
 	// APICredentials returns the current API credentials.
 	APICredentials() APICredentials
+
+	// SetAPIEndpoint sets the API endpoint information
+	// currently associated with the environment.
+	SetAPIEndpoint(APIEndpoint)
+
+	// SetAPICreds sets the API credentials currently
+	// associated with the environment.
+	SetAPICredentials(APICredentials)
+
+	// Write writes the current information to persistent storage.
+	// A subsequent call to ConfigStorage.ReadInfo
+	// can retrieve it.
+	Write() error
+
+	// Destroy destroys the information associated with
+	// the environment.
+	Destroy() error
 }
