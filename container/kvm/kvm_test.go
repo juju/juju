14fc802bfec597bfdb24baccd3d088715f9bcf08
Tim Penhey <tim.penhey@canonical.com>
2013-09-26 11:47:22 +1200

    Make the first kvm test pass.

--- container/kvm/kvm_test.go
+++ container/kvm/kvm_test.go
@@ -7,8 +7,8 @@ import (
 	gc "launchpad.net/gocheck"
 
 	"launchpad.net/juju-core/container/kvm"
-	jc "launchpad.net/juju-core/testing/checkers"
-	"launchpad.net/juju-core/testing/testbase"
+	//jc "launchpad.net/juju-core/testing/checkers"
+	//"launchpad.net/juju-core/testing/testbase"
 )
 
 type KVMSuite struct {
@@ -20,7 +20,8 @@ var _ = gc.Suite(&KVMSuite{})
 // TODO: work out how to test the actual kvm implementations.
 
 func (*KVMSuite) TestListInitiallyEmpty(c *gc.C) {
-	manager := kvm.NewContainerManager("test")
+	manager, err := kvm.NewContainerManager("test")
+	c.Assert(err, gc.IsNil)
 	containers, err := manager.ListContainers()
 	c.Assert(err, gc.IsNil)
 	c.Assert(containers, gc.HasLen, 0)
