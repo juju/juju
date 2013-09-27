18e1f484c7567f814e4938ccb3fcbdf5c8e8a8dc
Ian Booth <ian.booth@canonical.com>
2013-09-27 10:23:48 +1000

    Fix tools sources for azure and local provider

--- provider/local/local_test.go
+++ provider/local/local_test.go
@@ -4,13 +4,17 @@
 package local_test
 
 import (
+	"strings"
 	stdtesting "testing"
 
 	gc "launchpad.net/gocheck"
 
 	"launchpad.net/juju-core/environs"
+	envtesting "launchpad.net/juju-core/environs/testing"
+	"launchpad.net/juju-core/environs/tools"
 	"launchpad.net/juju-core/provider"
 	"launchpad.net/juju-core/provider/local"
+	jc "launchpad.net/juju-core/testing/checkers"
 	"launchpad.net/juju-core/testing/testbase"
 )
 
@@ -20,12 +24,31 @@ func TestLocal(t *stdtesting.T) {
 
 type localSuite struct {
 	testbase.LoggingSuite
+	envtesting.ToolsFixture
 }
 
 var _ = gc.Suite(&localSuite{})
+
+func (s *localSuite) SetUpTest(c *gc.C) {
+	s.LoggingSuite.SetUpTest(c)
+	s.ToolsFixture.SetUpTest(c)
+}
 
 func (*localSuite) TestProviderRegistered(c *gc.C) {
 	provider, error := environs.Provider(provider.Local)
 	c.Assert(error, gc.IsNil)
 	c.Assert(provider, gc.DeepEquals, local.Provider)
+}
+
+func (s *localSuite) TestGetToolsMetadataSources(c *gc.C) {
+	testConfig := minimalConfig(c)
+	environ, err := local.Provider.Open(testConfig)
+	c.Assert(err, gc.IsNil)
+
+	sources, err := tools.GetMetadataSources(environ)
+	c.Assert(err, gc.IsNil)
+	c.Assert(len(sources), gc.Equals, 1)
+	url, err := sources[0].URL("")
+	c.Assert(err, gc.IsNil)
+	c.Assert(strings.Contains(url, "/tools"), jc.IsTrue)
 }
