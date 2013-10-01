8f4a38dbc47840f656da98560d96e10217ca9ab4
Roger Peppe <roger.peppe@canonical.com>
2013-10-01 14:11:22 +0100

    environs/ec2: fix some ec2 tests

--- provider/dummy/storage.go
+++ provider/dummy/storage.go
@@ -14,7 +14,6 @@ import (
 	"time"
 
 	"launchpad.net/juju-core/environs/storage"
-	"launchpad.net/juju-core/log"
 	"launchpad.net/juju-core/errors"
 	"launchpad.net/juju-core/utils"
 )
@@ -99,7 +98,6 @@ func (s *storageServer) dataWithDelay(path string) (data []byte, err error) {
 	time.Sleep(delay)
 	s.state.mu.Lock()
 	defer s.state.mu.Unlock()
-	log.Infof("data for path %q; poison: %v", path, s.poisoned)
 	if err := s.poisoned[path]; err != nil {
 		return nil, err
 	}
