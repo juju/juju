3aa6fb64ba181d94867f4cc4f3a75d1f06990a8a
Roger Peppe <roger.peppe@canonical.com>
2013-10-02 21:44:39 +0100

    worker/addressupdater: observer tests

---
+++ juju/testing/utils.go
@@ -0,0 +1,23 @@
+// Copyright 2012, 2013 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package testing
+import (
+	gc "launchpad.net/gocheck"
+
+	"launchpad.net/juju-core/environs/config"
+	"launchpad.net/juju-core/state"
+	coretesting "launchpad.net/juju-core/testing"
+)
+
+// ChangeEnvironConfig applies the given change function
+// to the attributes from st.EnvironConfig and
+// sets the state's environment configuration to the result.
+func ChangeEnvironConfig(c *gc.C, st *state.State, change func(coretesting.Attrs) coretesting.Attrs) {
+	cfg, err := st.EnvironConfig()
+	c.Assert(err, gc.IsNil)
+	newCfg, err := config.New(config.NoDefaults, change(cfg.AllAttrs()))
+	c.Assert(err, gc.IsNil)
+	err = st.SetEnvironConfig(newCfg)
+	c.Assert(err, gc.IsNil)
+}
