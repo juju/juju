feef3030ef17be0443daed7028cbc31f97b5fb01
Roger Peppe <roger.peppe@canonical.com>
2013-10-07 18:30:43 +0100

    agent: more InitializeState testing

--- agent/bootstrap_test.go
+++ agent/bootstrap_test.go
@@ -5,11 +5,13 @@ package agent_test
 import (
 	gc "launchpad.net/gocheck"
 
+	"launchpad.net/juju-core/utils"
 	"launchpad.net/juju-core/agent"
 	"launchpad.net/juju-core/environs/config"
 	"launchpad.net/juju-core/state"
 	"launchpad.net/juju-core/instance"
 	"launchpad.net/juju-core/constraints"
+	jc "launchpad.net/juju-core/testing/checkers"
 	"launchpad.net/juju-core/testing"
 	"launchpad.net/juju-core/version"
 )
@@ -21,14 +23,15 @@ var _ = gc.Suite(&bootstrapSuite{})
 
 func (s *bootstrapSuite) TestInitializeState(c *gc.C) {
 	dataDir := c.MkDir()
-	
+
+	pwHash := utils.PasswordHash(testing.DefaultMongoPassword)
 	cfg, err := agent.NewAgentConfig(agent.AgentConfigParams{
 		DataDir: dataDir,
 		Tag: "machine-0",
 		Nonce: "onceonly",
 		StateAddresses: []string{testing.MgoAddr},
 		CACert: []byte(testing.CACert),
-		Password: testing.DefaultMongoPassword,
+		Password: pwHash,
 	})
 	c.Assert(err, gc.IsNil)
 	expectConstraints := constraints.MustParse("mem=1024M")
@@ -39,15 +42,28 @@ func (s *bootstrapSuite) TestInitializeState(c *gc.C) {
 		InstanceId: "i-bootstrap",
 		Characteristics: expectHW,
 	}
-	attrs := testing.FakeConfig().Delete("admin-secret").Merge(testing.Attrs{
+	envAttrs := testing.FakeConfig().Delete("admin-secret").Merge(testing.Attrs{
 		"agent-version": version.Current.Number.String(),
 	})
-	envcfg, err := config.New(config.NoDefaults, attrs)
+	envCfg, err := config.New(config.NoDefaults, envAttrs)
 	c.Assert(err, gc.IsNil)
 
-	st, m, err := cfg.InitializeState(envcfg, mcfg, state.DialOpts{})
+	st, m, err := cfg.InitializeState(envCfg, mcfg, state.DialOpts{})
 	c.Assert(err, gc.IsNil)
 	defer st.Close()
+
+	// Check that initial admin user has been set up correctly.
+	s.assertCanLogInAsAdmin(c, pwHash)
+	user, err := st.User("admin")
+	c.Assert(err, gc.IsNil)
+	c.Assert(user.PasswordValid(testing.DefaultMongoPassword), jc.IsTrue)
+
+	// Check that environment configuration has been added.
+	newEnvCfg, err := st.EnvironConfig()
+	c.Assert(err, gc.IsNil)
+	c.Assert(newEnvCfg.AllAttrs(), gc.DeepEquals, envCfg.AllAttrs())
+
+	// Check that the bootstrap machine looks correct.
 	c.Assert(m.Id(), gc.Equals, "0")
 	c.Assert(m.Jobs(), gc.DeepEquals, []state.MachineJob{state.JobHostUnits})
 	gotConstraints, err := m.Constraints()
@@ -57,6 +73,31 @@ func (s *bootstrapSuite) TestInitializeState(c *gc.C) {
 	gotHW, err := m.HardwareCharacteristics()
 	c.Assert(err, gc.IsNil)
 	c.Assert(*gotHW, gc.DeepEquals, expectHW)
+
+	// Check that the machine agent's config has been written
+	// and that we can use it to connect to the state.
+	newCfg, err := agent.ReadConf(dataDir, "machine-0")
+	c.Assert(err, gc.IsNil)
+	c.Assert(newCfg.Tag(), gc.Equals, "machine-0")
+	c.Assert(agent.Password(newCfg), gc.Not(gc.Equals), pwHash)
+	c.Assert(agent.Password(newCfg), gc.Not(gc.Equals), testing.DefaultMongoPassword)
+	st1, err := cfg.OpenState()
+	c.Assert(err, gc.IsNil)
+	defer st1.Close()
+}
+
+func (*bootstrapSuite) assertCanLogInAsAdmin(c *gc.C, password string) {
+	info := &state.Info{
+		Addrs: []string{testing.MgoAddr},
+		CACert: []byte(testing.CACert),
+		Tag: "",
+		Password: password,
+	}
+	st, err := state.Open(info, state.DialOpts{})
+	c.Assert(err, gc.IsNil)
+	defer st.Close()
+	_, err = st.Machine("0")
+	c.Assert(err, gc.IsNil)
 }
 
 //test we can log in as admin
