6f6eb1f5a63029f3cc8ab47314575798b45a408b
Curtis Hovey <curtis@canonical.com>
2013-10-08 16:19:02 -0400

    Don't buildout the tree, let "go get" do it.

--- scripts/build-release-tarball/build-release-tarball.bash
+++ scripts/build-release-tarball/build-release-tarball.bash
@@ -1,46 +1,38 @@
 #!/usr/bin/env bash -e
 
-# if someone invokes this with bash 
+# if someone invokes this with bash
 set -e
 
 unset GOPATH
 
-# build release tarball from a bzr branch 
+# build release tarball from a bzr branch
 
 usage() {
 	echo usage: $0 TAG
 	exit 2
 }
 
-# bzr-checkout $SOURCE_URL $TAG $TARGET_DIR
-bzr-checkout() {
-	echo "cloning $1 at revision $2"
-	bzr checkout --lightweight -r $2 $1 ${WORK}/src/$3
-}
-
-test $# -eq 1 ||  usage 
+test $# -eq 1 ||  usage
 TAG=$1
 TMP_DIR=$(mktemp -d)
 mkdir $TMP_DIR/RELEASE
 WORK=$TMP_DIR/RELEASE
 
-# populate top level dirs
-mkdir -p $WORK/src/launchpad.net $WORK/src/labix.org/v2
-
-# Checkout juju manuallly.
-bzr-checkout lp:juju-core $TAG launchpad.net/juju-core
-
-# Fetch dependencies; restore juju-core to the proper revision; set the deps
-# to the proper revision.
+echo "Getting juju-core and all its dependencies."
 GOPATH=$WORK go get -v -d launchpad.net/juju-core/...
+
+echo "Setting juju-core tree to $TAG."
 (cd "${WORK}/src/launchpad.net/juju-core/" && bzr revert -r $TAG)
+
+echo "Updadting juju-core dependencies to the required versions."
 GOPATH=$WORK godeps -u "${WORK}/src/launchpad.net/juju-core/dependencies.tsv"
 
-# smoke test
+# Smoke test
 GOPATH=$WORK go build -v launchpad.net/juju-core/...
 
-# Fetch the version and set the release name.
-VERSION=$(sed -n 's/^const version = "\(.*\)"/\1/p' $WORK/src/launchpad.net/juju-core/version/version.go)
+# Change the generic release to the proper juju-core version.
+VERSION=$(sed -n 's/^const version = "\(.*\)"/\1/p' \
+    $WORK/src/launchpad.net/juju-core/version/version.go)
 mv $WORK $TMP_DIR/juju-core_${VERSION}/
 
 # Tar it up.
