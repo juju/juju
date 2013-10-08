47790f94ac46feb9883c18bb1620266fab9afd3b
Curtis Hovey <curtis@canonical.com>
2013-10-08 14:49:48 -0400

    Create the release name after after the code is retrieved simplify the process.

--- scripts/build-release-tarball/build-release-tarball.bash
+++ scripts/build-release-tarball/build-release-tarball.bash
@@ -18,38 +18,20 @@ bzr-checkout() {
 	bzr checkout --lightweight -r $2 $1 ${WORK}/src/$3
 }
 
-# hg-checkout $SOURCE_URL $TAG $TARGET_DIR
-hg-checkout() {
-	echo "cloning $1 at revision $2"
-	hg clone -q -r $2 $1 ${WORK}/src/$3
-}
-
-# git-checkout $SOURCE_URL $TARGET_DIR
-git-checkout() {
-	echo "cloning $1"
-	git clone -q $1 ${WORK}/src/$2
-}
-
 test $# -eq 1 ||  usage 
 TAG=$1
-WORK=$(mktemp -d)
+TMP_DIR=$(mktemp -d)
+mkdir $TMP_DIR/RELEASE
+WORK=$TMP_DIR/RELEASE
 
 # populate top level dirs
 mkdir -p $WORK/src/launchpad.net $WORK/src/labix.org/v2
 
-# checkout juju (manually, because we're redefining $WORK later on
+# Checkout juju manuallly.
 bzr-checkout lp:juju-core $TAG launchpad.net/juju-core
 
-# fetch the version
-VERSION=$(sed -n 's/^const version = "\(.*\)"/\1/p' $WORK/src/launchpad.net/juju-core/version/version.go)
-
-# fixup paths for tarball
-mkdir $WORK/juju-core_${VERSION}
-mv $WORK/src $WORK/juju-core_${VERSION}/
-WORK=$WORK/juju-core_${VERSION}
-
 # Fetch dependencies; restore juju-core to the proper revision; set the deps
-# to the properrevision.
+# to the proper revision.
 GOPATH=$WORK go get -v -d launchpad.net/juju-core/...
 (cd "${WORK}/src/launchpad.net/juju-core/" && bzr revert -r $TAG)
 GOPATH=$WORK godeps -u "${WORK}/src/launchpad.net/juju-core/dependencies.tsv"
@@ -57,9 +39,13 @@ GOPATH=$WORK godeps -u "${WORK}/src/launchpad.net/juju-core/dependencies.tsv"
 # smoke test
 GOPATH=$WORK go build -v launchpad.net/juju-core/...
 
-# tar it up
+# Fetch the version and set the release name.
+VERSION=$(sed -n 's/^const version = "\(.*\)"/\1/p' $WORK/src/launchpad.net/juju-core/version/version.go)
+mv $WORK $TMP_DIR/juju-core_${VERSION}/
+
+# Tar it up.
 TARFILE=`pwd`/juju-core_${VERSION}.tar.gz
-cd $WORK/..
+cd $TMP_DIR
 tar cfz $TARFILE --exclude .hg --exclude .git --exclude .bzr juju-core_${VERSION}
 
 echo "release tarball: ${TARFILE}"
