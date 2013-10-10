25b08a48af5f242c68f0331ca4e177f51765191e
Nate Finch <nate.finch@canonical.com>
2013-10-10 16:33:07 -0400

    translate the errors returned by the system call in the same way os.Rename does

--- utils/file_windows.go
+++ utils/file_windows.go
@@ -4,6 +4,7 @@
 package utils
 
 import (
+	"os"
 	"syscall"
 )
 
@@ -18,11 +19,14 @@ const (
 func Replace(source, destination string) error {
 	src, err := syscall.UTF16PtrFromString(source)
 	if err != nil {
-		return err
+		return &os.LinkError{"replace", source, destination, err}
 	}
 	dest, err := syscall.UTF16PtrFromString(destination)
 	if err != nil {
-		return err
+		return &os.LinkError{"replace", source, destination, err}
 	}
-	return moveFileEx(src, dest, movefile_replace_existing|movefile_write_through)
+	if err := moveFileEx(src, dest, movefile_replace_existing|movefile_write_through); err != nil {
+		return &os.LinkError{"replace", source, destination, err}
+	}
+	return nil
 }
