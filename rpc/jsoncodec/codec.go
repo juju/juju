5752e99fe22d6380b3ea2e59371d3b187c1d40c9
Roger Peppe <roger.peppe@canonical.com>
2013-10-11 10:56:04 +0100

    state/apiserver: use rpc request notification

--- rpc/jsoncodec/codec.go
+++ rpc/jsoncodec/codec.go
@@ -149,29 +149,47 @@ func (c *Codec) ReadBody(body interface{}, isRequest bool) error {
 	return json.Unmarshal(rawBody, body)
 }
 
+// DumpRequest returns JSON-formatted data representing
+// the RPC message with the given header and body,
+// as it would be written by Codec.WriteMessage.
+// If the body cannot be marshalled as JSON, the data
+// will hold a JSON string describing the error.
+func DumpRequest(hdr *rpc.Header, body interface{}) []byte {
+	var m outMsg
+	m.init(hdr, body)
+	data, err := json.Marshal(&m)
+	if err != nil {
+		return []byte(fmt.Sprintf("%q", "marshal error: %v", err))
+	}
+	return data
+}
+
 func (c *Codec) WriteMessage(hdr *rpc.Header, body interface{}) error {
-	r := &outMsg{
-		RequestId: hdr.RequestId,
-
-		Type:    hdr.Request.Type,
-		Id:      hdr.Request.Id,
-		Request: hdr.Request.Action,
-
-		Error:     hdr.Error,
-		ErrorCode: hdr.ErrorCode,
-	}
-	if hdr.IsRequest() {
-		r.Params = body
-	} else {
-		r.Response = body
-	}
+	var m outMsg
+	m.init(hdr, body)
 	if c.isLogging() {
-		data, err := json.Marshal(r)
+		data, err := json.Marshal(&m)
 		if err != nil {
 			logger.Tracef("-> marshal error: %v", err)
 			return err
 		}
 		logger.Tracef("-> %s", data)
 	}
-	return c.conn.Send(r)
+	return c.conn.Send(&m)
+}
+
+// init fills out the receiving outMsg with information from the given
+// header and body.
+func (m *outMsg) init(hdr *rpc.Header, body interface{}) {
+	m.RequestId = hdr.RequestId
+	m.Type = hdr.Request.Type
+	m.Id = hdr.Request.Id
+	m.Request = hdr.Request.Action
+	m.Error = hdr.Error
+	m.ErrorCode = hdr.ErrorCode
+	if hdr.IsRequest() {
+		m.Params = body
+	} else {
+		m.Response = body
+	}
 }
