70f6fa9e1f2c15c88ebc1acfe909383115f57502
Ian Booth <ian.booth@canonical.com>
2013-10-25 11:45:59 +1000

    Change mirrors path in mirrors index file

--- environs/tools/simplestreams.go
+++ environs/tools/simplestreams.go
@@ -346,7 +346,7 @@ var PublicMirrorsInfo = `{
   "com.ubuntu.juju:released:tools": [
      {
       "datatype": "content-download",
-      "path": "streams/v1/mirrors.json",
+      "path": "streams/v1/tools-mirrors.json",
       "updated": "{{updated}}",
       "format": "mirrors:1.0"
      }
