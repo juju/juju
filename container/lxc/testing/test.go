ff00f761e181c2cfbfc4fadc5c1c5d855f6cd5d7
Matthew Williams <mattyw@me.com>
2013-10-28 13:25:04 +0000

    removed comment about only using the file in tests

--- container/lxc/testing/test.go
+++ container/lxc/testing/test.go
@@ -1,10 +1,6 @@
 // Copyright 2013 Canonical Ltd.
 // Licensed under the AGPLv3, see LICENCE file for details.
 
-// Functions defined in this file should *ONLY* be used for testing.  These
-// functions are exported for testing purposes only, and shouldn't be called
-// from code that isn't in a test file.
-
 package testing
 
 import (
