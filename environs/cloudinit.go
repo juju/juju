b1589578821eee9c4cdba41b49d6c2bec5e6432e
John Arbash Meinel <john@arbash-meinel.com>
2013-11-06 17:48:29 +0400

    More callers

--- environs/cloudinit.go
+++ environs/cloudinit.go
@@ -94,7 +94,7 @@ func FinishMachineConfig(mcfg *cloudinit.MachineConfig, cfg *config.Config, cons
 	if password == "" {
 		return fmt.Errorf("environment configuration has no admin-secret")
 	}
-	passwordHash := utils.CompatPasswordHash(password)
+	passwordHash := utils.UserPasswordHash(password, utils.CompatSalt)
 	mcfg.APIInfo = &api.Info{Password: passwordHash, CACert: caCert}
 	mcfg.StateInfo = &state.Info{Password: passwordHash, CACert: caCert}
 	mcfg.StatePort = cfg.StatePort()
