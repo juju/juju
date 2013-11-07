52c5f68de1f0b530acffdce4f00d04d2dd21b433
John Arbash Meinel <john@arbash-meinel.com>
2013-11-07 12:50:51 +0400

    Support using 'add-unit -n X --to Y'.
    This makes doing scale testing much easier, but is not something
    we want to support in core.

--- juju/conn.go
+++ juju/conn.go
@@ -329,9 +329,9 @@ func (conn *Conn) AddUnits(svc *state.Service, n int, machineIdSpec string) ([]*
 			return nil, fmt.Errorf("cannot add unit %d/%d to service %q: %v", i+1, n, svc.Name(), err)
 		}
 		if machineIdSpec != "" {
-			if n != 1 {
-				return nil, fmt.Errorf("cannot add multiple units of service %q to a single machine", svc.Name())
-			}
+			/// if n != 1 {
+			/// 	return nil, fmt.Errorf("cannot add multiple units of service %q to a single machine", svc.Name())
+			/// }
 			// machineIdSpec may be an existing machine or container, eg 3/lxc/2
 			// or a new container on a machine, eg lxc:3
 			mid := machineIdSpec
