9621c3db9cdb742e91378b1187c47b106e356845
Curtis Hovey <curtis@canonical.com>
2013-11-14 17:04:15 -0500

    Get godeps to pin juju's deps.

--- make-release-tarball.bash
+++ make-release-tarball.bash
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash -e
+#!/bin/bash
 
 # if someone invokes this with bash
 set -e
@@ -29,11 +29,13 @@ GOPATH=$WORK go get -v -d launchpad.net/juju-core/...
 
 echo "Setting juju-core tree to $JUJU_CORE_BRANCH $REVNO."
 (cd "${WORK}/src/launchpad.net/juju-core/" &&
- bzr pull --remember --overwrite $JUJU_CORE_BRANCH &&
- bzr revert -r $REVNO)
+ bzr pull --remember --overwrite -r $REVNO $JUJU_CORE_BRANCH)
 
 echo "Updating juju-core dependencies to the required versions."
-GOPATH=$WORK godeps -u "${WORK}/src/launchpad.net/juju-core/dependencies.tsv"
+GOPATH=$WORK go get -v launchpad.net/godeps
+GODEPS=$WORK/bin/godeps
+GOPATH=$WORK $GODEPS -u "${WORK}/src/launchpad.net/juju-core/dependencies.tsv"
+rm $WORK/bin
 
 # Smoke test
 GOPATH=$WORK go build -v launchpad.net/juju-core/...
