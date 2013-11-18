ade9f7ce7dd68366be46b56d190baf97d62e6440
Curtis Hovey <curtis@canonical.com>
2013-11-18 12:31:42 -0500

    Show the contents of the source tree after godeps should be installed.

--- make-release-tarball.bash
+++ make-release-tarball.bash
@@ -33,6 +33,8 @@ echo "Setting juju-core tree to $JUJU_CORE_BRANCH $REVNO."
 
 echo "Updating juju-core dependencies to the required versions."
 GOPATH=$WORK go get -v launchpad.net/godeps
+echo "$(ls $WORK)"
+echo "$(ls $WORK/bin/)"
 GODEPS=$WORK/bin/godeps
 GOPATH=$WORK $GODEPS -u "${WORK}/src/launchpad.net/juju-core/dependencies.tsv"
 # Remove godeps.
