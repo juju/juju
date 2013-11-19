180d7966b3418219c4905ba580ac53574312eb52
Curtis Hovey <curtis@canonical.com>
2013-11-19 14:02:50 -0500

    Add dep checking to tarball script.

--- make-release-tarball.bash
+++ make-release-tarball.bash
@@ -18,7 +18,20 @@ usage() {
 }
 
 
+check_deps() {
+    echo "Phase 0: Checking requirements."
+    has_deps=1
+    which bzr || has_deps=0
+    which go || has_deps=0
+    if [[ $has_deps == 0 ]]; then
+        echo "Install bzr and golang."
+        exit 2
+    fi
+}
+
+
 test $# -ge 1 ||  usage
+check_deps
 REVNO=$1
 JUJU_CORE_BRANCH=${2:-$DEFAULT_JUJU_CORE}
 TMP_DIR=$(mktemp -d --tmpdir=$(pwd))
