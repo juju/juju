7c64dd409825c7059f809b110b13ed16e5cce814
Daniele Stroppa <daniele.stroppa@joyent.com>
2013-11-21 17:26:19 +0000

    Reverting storage changes

--- provider/joyent/storage.go
+++ provider/joyent/storage.go
@@ -5,21 +5,14 @@ package joyent
 
 import (
 	"io"
-	"sync"
-	"fmt"
 
 	"launchpad.net/juju-core/environs/storage"
 	"launchpad.net/juju-core/utils"
-
-	"launchpad.net/gojoyent/manta"
+	"fmt"
 )
 
 type environStorage struct {
-	sync.Mutex
-	ecfg 			*environConfig
-	madeContainer 	bool
-	containerName 	string
-	manta			*manta.Client
+	ecfg *environConfig
 }
 
 var _ storage.Storage = (*environStorage)(nil)
@@ -31,36 +24,8 @@ func newStorage(ecfg *environConfig) (storage.Storage, error) {
 		manta:        	manta.New(nil)}, nil
 }
 
-// makeContainer makes the environment's control container, the
-// place where bootstrap information and deployed charms
-// are stored. To avoid two round trips on every PUT operation,
-// we do this only once for each environ.
-func (s *environStorage) makeContainer(containerName string) error {
-	s.Lock()
-	defer s.Unlock()
-	if s.madeContainer {
-		return nil
-	}
-	// try to make the container
-	err := s.manta.PutDirectory(containerName)
-	if err == nil {
-		s.madeContainer = true
-	}
-	return err
-}
-
 func (s *environStorage) List(prefix string) ([]string, error) {
-	// use empty opts, i.e. default values
-	// -- might be added in the provider config?
-	contents, err := s.manta.ListDirectory(prefix, manta.ListDirectoryOpts{})
-	if err != nil {
-		return nil, err
-	}
-	var names []string
-	for _, item := range contents {
-		names = append(names, item.Name)
-	}
-	return names, nil
+	return nil, errNotImplemented
 }
 
 func (s *environStorage) URL(name string) (string, error) {
@@ -68,11 +33,7 @@ func (s *environStorage) URL(name string) (string, error) {
 }
 
 func (s *environStorage) Get(name string) (io.ReadCloser, error) {
-	r, err := s.manta.GetObject(s.containerName, name)
-	if err != nil {
-		return nil, err
-	}
-	return r, nil
+	return nil, errNotImplemented
 }
 
 func (s *environStorage) Put(name string, r io.Reader, length int64) error {
@@ -88,50 +49,11 @@ func (s *environStorage) Put(name string, r io.Reader, length int64) error {
 }
 
 func (s *environStorage) Remove(name string) error {
-	err := s.manta.DeleteObject(s.containerName, name)
-	if err != nil {
-		return err
-	}
-	return nil
+	return errNotImplemented
 }
 
 func (s *environStorage) RemoveAll() error {
-	names, err := storage.List(s, "")
-	if err != nil {
-		return err
-	}
-	// Remove all the objects in parallel so that we incur less round-trips.
-	// If we're in danger of having hundreds of objects,
-	// we'll want to change this to limit the number
-	// of concurrent operations.
-	var wg sync.WaitGroup
-	wg.Add(len(names))
-	errc := make(chan error, len(names))
-	for _, name := range names {
-		name := name
-		go func() {
-			defer wg.Done()
-			if err := s.Remove(name); err != nil {
-				errc <- err
-			}
-		}()
-	}
-	wg.Wait()
-	select {
-	case err := <-errc:
-		return fmt.Errorf("cannot delete all provider state: %v", err)
-	default:
-	}
-
-	s.Lock()
-	defer s.Unlock()
-	// Even DeleteContainer fails, it won't harm if we try again - the
-	// operation might have succeeded even if we get an error.
-	s.madeContainer = false
-	if err = s.manta.DeleteDirectory(s.containerName); err != nil {
-		return err
-	}
-	return nil
+	return errNotImplemented
 }
 
 func (s *environStorage) DefaultConsistencyStrategy() utils.AttemptStrategy {
