a9073d1c26c33487ebc050cc542f52a33b46a0b3
Daniele Stroppa <daniele.stroppa@joyent.com>
2013-11-21 18:29:04 +0000

    Manta Storage integration

--- provider/joyent/storage.go
+++ provider/joyent/storage.go
@@ -5,23 +5,62 @@ package joyent
 
 import (
 	"io"
+	"sync"
+	"fmt"
 
 	"launchpad.net/juju-core/environs/storage"
 	"launchpad.net/juju-core/utils"
+
+	"launchpad.net/gojoyent/manta"
 )
 
 type environStorage struct {
-	ecfg *environConfig
+	sync.Mutex
+	ecfg 			*environConfig
+	madeContainer 	bool
+	containerName 	string
+	manta			*manta.Client
 }
 
 var _ storage.Storage = (*environStorage)(nil)
 
 func newStorage(ecfg *environConfig) (storage.Storage, error) {
-	return &environStorage{ecfg}, nil
+	return &environStorage{
+		ecfg:			ecfg,
+		containerName: 	ecfg.controlDir(),
+		manta:        	manta.New(nil)}, nil
+}
+
+// makeContainer makes the environment's control container, the
+// place where bootstrap information and deployed charms
+// are stored. To avoid two round trips on every PUT operation,
+// we do this only once for each environ.
+func (s *environStorage) makeContainer(containerName string) error {
+	s.Lock()
+	defer s.Unlock()
+	if s.madeContainer {
+		return nil
+	}
+	// try to make the container
+	err := s.manta.PutDirectory(containerName)
+	if err == nil {
+		s.madeContainer = true
+	}
+	return err
 }
 
 func (s *environStorage) List(prefix string) ([]string, error) {
-	return nil, errNotImplemented
+	// use empty opts, i.e. default values
+	// -- might be added in the provider config?
+	contents, err := s.manta.ListDirectory(prefix, manta.ListDirectoryOpts{})
+	if err != nil {
+		return nil, err
+	}
+	var names []string
+	for _, item := range contents {
+		names = append(names, item.Name)
+	}
+	return names, nil
 }
 
 func (s *environStorage) URL(name string) (string, error) {
@@ -29,19 +68,70 @@ func (s *environStorage) URL(name string) (string, error) {
 }
 
 func (s *environStorage) Get(name string) (io.ReadCloser, error) {
-	return nil, errNotImplemented
+	r, err := s.manta.GetObject(s.containerName, name)
+	if err != nil {
+		return nil, err
+	}
+	return r, nil
 }
 
 func (s *environStorage) Put(name string, r io.Reader, length int64) error {
-	return errNotImplemented
+	if err := s.makeContainer(s.containerName); err != nil {
+		return fmt.Errorf("cannot make Manta control container: %v", err)
+	}
+	//obj := r.Read()
+	err := s.manta.PutObject(s.containerName, name, r)
+	if err != nil {
+		return fmt.Errorf("cannot write file %q to control container %q: %v", name, s.containerName, err)
+	}
+	return nil
 }
 
 func (s *environStorage) Remove(name string) error {
-	return errNotImplemented
+	err := s.manta.DeleteObject(s.containerName, name)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (s *environStorage) RemoveAll() error {
-	return errNotImplemented
+	names, err := storage.List(s, "")
+	if err != nil {
+		return err
+	}
+	// Remove all the objects in parallel so that we incur less round-trips.
+	// If we're in danger of having hundreds of objects,
+	// we'll want to change this to limit the number
+	// of concurrent operations.
+	var wg sync.WaitGroup
+	wg.Add(len(names))
+	errc := make(chan error, len(names))
+	for _, name := range names {
+		name := name
+		go func() {
+			defer wg.Done()
+			if err := s.Remove(name); err != nil {
+				errc <- err
+			}
+		}()
+	}
+	wg.Wait()
+	select {
+	case err := <-errc:
+		return fmt.Errorf("cannot delete all provider state: %v", err)
+	default:
+	}
+
+	s.Lock()
+	defer s.Unlock()
+	// Even DeleteContainer fails, it won't harm if we try again - the
+	// operation might have succeeded even if we get an error.
+	s.madeContainer = false
+	if err = s.manta.DeleteDirectory(s.containerName); err != nil {
+		return err
+	}
+	return nil
 }
 
 func (s *environStorage) DefaultConsistencyStrategy() utils.AttemptStrategy {
