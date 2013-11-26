e9f2eb789396063ed26374a2368dc7fdaf917bdb
Martin Packman <martin.packman@canonical.com>
2013-11-26 03:31:04 +0000

    Add logic for updating state address in juju-update-bootstrap

--- cmd/plugins/juju-update-bootstrap/updatebootstrap.go
+++ cmd/plugins/juju-update-bootstrap/updatebootstrap.go
@@ -6,18 +6,25 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
+	"strings"
 
 	"launchpad.net/loggo"
 
 	"launchpad.net/juju-core/cmd"
+	"launchpad.net/juju-core/environs"
+	"launchpad.net/juju-core/instance"
 	"launchpad.net/juju-core/juju"
 	_ "launchpad.net/juju-core/provider/all"
+	"launchpad.net/juju-core/state"
+	"launchpad.net/juju-core/utils"
 )
 
 var logger = loggo.GetLogger("juju.plugins.updatebootstrap")
 
 const updateBootstrapDoc = `
-Patches all machines after state server has been restored from backup.
+Patches all machines after state server has been restored from backup, to
+update state server address to new location.
 `
 
 type updateBootstrapCommand struct {
@@ -33,7 +40,81 @@ func (c *updateBootstrapCommand) Info() *cmd.Info {
 }
 
 func (c *updateBootstrapCommand) Run(ctx *cmd.Context) error {
-	logger.Infof("Running update-bootstrap")
+	conn, err := juju.NewConnFromName(c.EnvName)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+	stateAddr, err := GetStateAddress(conn.Environ)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("using state address %v\n", stateAddr)
+	return updateAllMachines(conn, stateAddr)
+}
+
+// GetStateAddress returns the address of one state server
+func GetStateAddress(environ environs.Environ) (string, error) {
+	// XXX: Can easily look up state server address using api instead
+	stateInfo, _, err := environ.StateInfo()
+	if err != nil {
+		return "", err
+	}
+	return strings.Split(stateInfo.Addrs[0], ":")[0], nil
+}
+
+var agentAddressTemplate = `
+set -ex
+cd /var/lib/juju/agents
+for agent in *; do
+	initctl stop jujud-$agent
+	sed -i.old -r "/^(stateaddresses|apiaddresses):/{
+		n
+		s/- .*(:[0-9]+)/- $ADDR\1/
+	}" $agent/agent.conf
+	initctl start jujud-$agent
+done
+sed -i -r 's/^(:syslogtag, startswith, "juju-" @)(.*)(:[0-9]+)$/\1$ADDR\3/' /etc/rsyslog.d/*-juju*.conf
+`
+
+// renderScriptArg generates an ssh script argument to update state addresses
+func renderScriptArg(stateAddr string) string {
+	script := strings.Replace(agentAddressTemplate, "$ADDR", stateAddr, -1)
+	return "sudo sh -c " + utils.ShQuote(script)
+}
+
+// runMachineUpdate connects via ssh to the machine and runs the update script
+func runMachineUpdate(m *state.Machine, sshArg string) error {
+	addr := instance.SelectPublicAddress(m.Addresses())
+	if addr == "" {
+		return fmt.Errorf("no appropriate public address found")
+	}
+	args := []string{
+		"-l", "ubuntu",
+		"-T",
+		"-o", "StrictHostKeyChecking no",
+		"-o", "PasswordAuthentication no",
+		addr,
+		sshArg,
+	}
+	c := exec.Command("ssh", args...)
+	if data, err := c.CombinedOutput(); err != nil {
+		return fmt.Errorf("ssh command failed: %v (%q)", err, data)
+	}
+	return nil
+}
+
+// updateAllMachines finds all machines resets the stored state address
+func updateAllMachines(conn *juju.Conn, stateAddr string) error {
+	// XXX(gz): includes the state server(s) which don't want updating
+	machines, err := conn.State.AllMachines()
+	if err != nil {
+		return err
+	}
+	for _, machine := range machines {
+		logger.Infof("updating machine: %v\n", machine)
+		runMachineUpdate(machine, renderScriptArg(stateAddr))
+	}
 	return nil
 }
 
