a0bae9139fe4f29562d9d0db8ef352e8150628cb
John Arbash Meinel <john@arbash-meinel.com>
2013-11-27 17:52:32 +0400

    In the old 1.16.4 (soon to be 1.16.5) it will be called machine.IsManager again.

--- cmd/plugins/juju-update-bootstrap/updatebootstrap.go
+++ cmd/plugins/juju-update-bootstrap/updatebootstrap.go
@@ -121,7 +121,7 @@ func updateAllMachines(conn *juju.Conn, stateAddr string) error {
 	for _, machine := range machines {
 		// A newly resumed state server requires no updating, and more
 		// than one state server is not yet support by this plugin.
-		if machine.IsStateServer() {
+		if machine.IsManager() {
 			continue
 		}
 		pendingMachineCount += 1
