19d527e2d614de4c48da740f388125d520951fb9
Casey Marshall <cmars@cmarstech.com>
2013-12-06 15:34:16 -0600

    Moved common store config model & parsing to shared package.

---
+++ store/config_test.go
@@ -0,0 +1,51 @@
+// Copyright 2012, 2013 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package store
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	gc "launchpad.net/gocheck"
+
+	"launchpad.net/juju-core/cmd"
+	"launchpad.net/juju-core/testing"
+	"launchpad.net/juju-core/testing/testbase"
+)
+
+type ConfigSuite struct {
+	testbase.LoggingSuite
+}
+
+var _ = gc.Suite(&ConfigSuite{})
+
+const testConfig = `
+mongo-url: localhost:23456
+foo: 1
+bar: false
+`
+
+func (s *ConfigSuite) SetUpSuite(c *gc.C) {
+	s.LoggingSuite.SetUpSuite(c)
+}
+
+func (s *ConfigSuite) TearDownSuite(c *gc.C) {
+	s.LoggingSuite.TearDownSuite(c)
+}
+
+func (s *ConfigSuite) TestReadConfig(c *gc.C) {
+	confDir := c.MkDir()
+	f, err := os.Create(path.Join(confDir, "charmd.conf"))
+	c.Assert(err, gc.IsNil)
+	cfgPath := f.Name()
+	{
+		defer f.Close()
+		fmt.Fprint(f, testConfig)
+	}
+
+	dstr, err := store.ReadConfig(cfgPath)
+	c.Assert(err, gc.IsNil)
+	c.Assert(dstr.MongoUrl, gc.Equals, "localhost:23456")
+}
