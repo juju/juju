6ed6344e0df2772ab042f3176cb7c66584a1b1bc
Ian Booth <ian.booth@canonical.com>
2013-12-10 11:21:32 +1000

    Move deprecated config processing from environs.config to environs

--- environs/config.go
+++ environs/config.go
@@ -68,6 +68,33 @@ func (envs *Environs) Config(name string) (*config.Config, error) {
 	if err := validateEnvironmentKind(attrs); err != nil {
 		return nil, err
 	}
+
+	// The tools url has changed so see if the old one is still in use.
+	if oldToolsURL := attrs["tools-url"]; oldToolsURL != nil && oldToolsURL.(string) != "" {
+		_, newToolsSpecified := attrs["tools-metadata-url"]
+		var msg string
+		if newToolsSpecified {
+			msg = fmt.Sprintf(
+				"Config attribute %q (%v) is deprecated and will be ignored since\n"+
+					"the new tools URL attribute %q has also been used.\n"+
+					"The attribute %q should be removed from your configuration.",
+				"tools-url", oldToolsURL, "tools-metadata-url", "tools-url")
+		} else {
+			msg = fmt.Sprintf(
+				"Config attribute %q (%v) is deprecated.\n"+
+					"The location to find tools is now specified using the %q attribute.\n"+
+					"Your configuration should be updated to set %q as follows\n%v: %v.",
+				"tools-url", oldToolsURL, "tools-metadata-url", "tools-metadata-url", "tools-metadata-url", oldToolsURL)
+			attrs["tools-metadata-url"] = oldToolsURL
+		}
+		logger.Warningf(msg)
+	}
+	// Even if the user has edited their environment yaml to remove the deprecated tools-url value,
+	// we still want it in the config for upgrades.
+	if newToolsURL, ok := attrs["tools-metadata-url"]; ok {
+		attrs["tools-url"] = newToolsURL
+	}
+
 	cfg, err := config.New(config.UseDefaults, attrs)
 	if err != nil {
 		return nil, err
