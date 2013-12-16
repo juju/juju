c1136de310b02930b15b72a86346c6df62bc5977
Tim Penhey <tim.penhey@canonical.com>
2013-12-17 11:21:04 +1300

    machines

--- cmd/juju/run_test.go
+++ cmd/juju/run_test.go
@@ -40,10 +40,38 @@ func (*RunSuite) TestTargetArgParsing(c *gc.C) {
 		message:  "too many args",
 		args:     []string{"--all", "sudo reboot", "oops"},
 		errMatch: `unrecognized args: \["oops"\]`,
+	}, {
+		message:  "command to all machines",
+		args:     []string{"--all", "sudo reboot"},
+		all:      true,
+		commands: "sudo reboot",
+	}, {
+		message:  "all and defined machines",
+		args:     []string{"--all", "--machine=1,2", "sudo reboot"},
+		errMatch: `You cannot specify --all and individual machines`,
+	}, {
+		message:  "command to machines 1, 2, and 1/kvm/0",
+		args:     []string{"--machine=1,2,1/kvm/0", "sudo reboot"},
+		commands: "sudo reboot",
+		machines: []string{"1", "2", "1/kvm/0"},
+	}, {
+		message: "bad machine names",
+		args:    []string{"--machine=foo,machine-2", "sudo reboot"},
+		errMatch: "" +
+			"The following run targets are not valid:\n" +
+			"  \"foo\" is not a valid machine id\n" +
+			"  \"machine-2\" is not a valid machine id",
 	}} {
 		c.Log(fmt.Sprintf("%v: %s", i, test.message))
 		runCmd := &RunCommand{}
 		testing.TestInit(c, runCmd, test.args, test.errMatch)
+		if test.errMatch == "" {
+			c.Check(runCmd.all, gc.Equals, test.all)
+			c.Check(runCmd.machines, gc.DeepEquals, test.machines)
+			c.Check(runCmd.services, gc.DeepEquals, test.services)
+			c.Check(runCmd.units, gc.DeepEquals, test.units)
+			c.Check(runCmd.commands, gc.Equals, test.commands)
+		}
 	}
 }
 
