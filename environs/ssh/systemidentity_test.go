9c5e79968f25785e75f71037306ebd7d2a5a0eaa
Tim Penhey <tim.penhey@canonical.com>
2013-12-19 22:53:02 +1300

    Move the system identity writing over.

--- environs/ssh/systemidentity_test.go
+++ environs/ssh/systemidentity_test.go
@@ -5,12 +5,13 @@ package ssh_test
 
 import (
 	"io/ioutil"
+	"path"
 
 	gc "launchpad.net/gocheck"
 
+	"launchpad.net/juju-core/environs/ssh"
 	jc "launchpad.net/juju-core/testing/checkers"
 	"launchpad.net/juju-core/testing/testbase"
-	"launchpad.net/juju-core/utils/ssh"
 )
 
 type SystemIdentitySuite struct {
@@ -20,12 +21,10 @@ type SystemIdentitySuite struct {
 var _ = gc.Suite(&SystemIdentitySuite{})
 
 func (s *SystemIdentitySuite) TestWrite(c *gc.C) {
-
-	dataDir := c.MkDir()
-	err := ssh.WriteSystemIdentity(dataDir, "private-key")
+	filename := path.Join(c.MkDir(), ssh.SystemIdentity)
+	err := ssh.WriteSystemIdentity(filename, "private-key")
 	c.Assert(err, gc.IsNil)
 
-	filename := ssh.SystemIdentityFilename(dataDir)
 	c.Assert(filename, jc.IsNonEmptyFile)
 
 	content, err := ioutil.ReadFile(filename)
