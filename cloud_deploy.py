18087c61c25d21770e0171eb045e9ffbce032a2e
Aaron Bentley <aaron@canonical.com>
2014-01-15 14:49:25 -0500

    Add cloud_deploy script.

---
+++ cloud_deploy.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+__metaclass__ = type
+
+
+from argparse import ArgumentParser
+import sys
+
+from jujupy import (
+    Environment,
+    until_timeout,
+)
+
+
+def deploy_stack(environment):
+    """"Deploy a test stack in the specified environment.
+
+    :param environment: The name of the desired environment.
+    """
+    env = Environment.from_config(environment)
+    env.bootstrap()
+    try:
+        # wait for status info....
+        env.get_status()
+        env.wait_for_started()
+    finally:
+        env.destroy_environment()
+
+
+def main():
+    parser = ArgumentParser('Test a cloud')
+    parser.add_argument('env', help='The juju environment to test')
+    args = parser.parse_args()
+    try:
+        deploy_stack(args.env)
+    except Exception as e:
+        print e
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
