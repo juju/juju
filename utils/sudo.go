a0453444dc89320f2131bd94c4c2762c64e78d68
Tim Penhey <tim.penhey@canonical.com>
2014-01-16 10:21:31 +1300

    Tests for MkDirForUser.

--- utils/sudo.go
+++ utils/sudo.go
@@ -9,6 +9,10 @@ import (
 	"strconv"
 )
 
+var CheckIfRoot = func() bool {
+	return os.Getuid() == 0
+}
+
 // SudoCallerIds returns the user id and group id of the SUDO caller.
 // If either is unset, it returns zero for both values.
 // An error is returned if the relevant environment variables
@@ -29,4 +33,25 @@ func SudoCallerIds() (uid int, gid int, err error) {
 		return 0, 0, fmt.Errorf("invalid value %q for SUDO_GID", gidStr)
 	}
 	return
+}
+
+// MkDirForUser will call down to os.MkDir and if the user is running as root,
+// the ownership will be changed to the sudo user.  If there is an error
+// getting the SudoCallerIds, the directory is removed and an error returned.
+func MkDirForUser(dir string, perm os.FileMode) error {
+	if err := os.MkdirAll(dir, perm); err != nil {
+		return err
+	}
+	if CheckIfRoot() {
+		uid, gid, err := SudoCallerIds()
+		if err != nil {
+			os.RemoveAll(dir)
+			return err
+		}
+		if err := os.Chown(dir, uid, gid); err != nil {
+			os.RemoveAll(dir)
+			return err
+		}
+	}
+	return nil
 }
--- utils/sudo_test.go
+++ utils/sudo_test.go
@@ -5,9 +5,12 @@ package utils_test
 
 import (
 	"os"
+	"os/user"
+	"path/filepath"
 
 	gc "launchpad.net/gocheck"
 
+	jc "launchpad.net/juju-core/testing/checkers"
 	"launchpad.net/juju-core/testing/testbase"
 	"launchpad.net/juju-core/utils"
 )
@@ -15,6 +18,8 @@ import (
 type sudoSuite struct {
 	testbase.LoggingSuite
 }
+
+var _ = gc.Suite(&sudoSuite{})
 
 func (s *sudoSuite) TestSudoCallerIds(c *gc.C) {
 	s.PatchEnvironment("SUDO_UID", "0")
@@ -55,4 +60,38 @@ func (s *sudoSuite) TestSudoCallerIds(c *gc.C) {
 			c.Assert(gid, gc.Equals, 0)
 		}
 	}
+}
+
+func (s *sudoSuite) TestMkDirForUserAsUser(c *gc.C) {
+	base := c.MkDir()
+	dir := filepath.Join(base, "new-dir")
+	err := utils.MkDirForUser(dir, 0755)
+	c.Assert(err, gc.IsNil)
+	c.Assert(dir, jc.IsDirectory)
+}
+
+func (s *sudoSuite) TestMkDirForUserRoot(c *gc.C) {
+	user, err := user.Current()
+	c.Assert(err, gc.IsNil)
+	s.PatchEnvironment("SUDO_UID", user.Uid)
+	s.PatchEnvironment("SUDO_GID", user.Gid)
+	s.PatchValue(&utils.CheckIfRoot, func() bool { return true })
+
+	base := c.MkDir()
+	dir := filepath.Join(base, "new-dir")
+	err = utils.MkDirForUser(dir, 0755)
+	c.Assert(err, gc.IsNil)
+	c.Assert(dir, jc.IsDirectory)
+}
+
+func (s *sudoSuite) TestMkDirForUserWithError(c *gc.C) {
+	s.PatchEnvironment("SUDO_UID", "omg")
+	s.PatchEnvironment("SUDO_GID", "omg")
+	s.PatchValue(&utils.CheckIfRoot, func() bool { return true })
+
+	base := c.MkDir()
+	dir := filepath.Join(base, "new-dir")
+	err := utils.MkDirForUser(dir, 0755)
+	c.Assert(err, gc.ErrorMatches, `invalid value "omg" for SUDO_UID`)
+	c.Assert(dir, jc.DoesNotExist)
 }
