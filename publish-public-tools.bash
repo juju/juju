42c8ad98e6fd8370eada14a2f3c742bbb9c013aa
Curtis Hovey <curtis@canonical.com>
2014-01-28 17:47:06 -0500

    Allow the jenkins env to control where tools are published.

--- publish-public-tools.bash
+++ publish-public-tools.bash
@@ -40,6 +40,7 @@ check_deps() {
 
 
 publish_to_canonistack() {
+    [[ $JT_IGNORE_CANONISTACK == '1' ]] && return 0
     echo "Phase 1: Publish to canonistack."
     source $JUJU_DIR/canonistacktoolsrc
     cd $JUJU_DIST/tools/releases/
@@ -53,6 +54,7 @@ publish_to_canonistack() {
 
 
 testing_to_canonistack() {
+    [[ $JT_IGNORE_CANONISTACK == '1' ]] && return 0
     echo "Phase 1: Testing to canonistack."
     source $JUJU_DIR/canonistacktoolsrc
     cd $JUJU_DIST/tools/releases/
@@ -63,6 +65,7 @@ testing_to_canonistack() {
 
 
 publish_to_hp() {
+    [[ $JT_IGNORE_HP == '1' ]] && return 0
     echo "Phase 2: Publish to HP Cloud."
     source $JUJU_DIR/hptoolsrc
     cd $JUJU_DIST/tools/releases/
@@ -78,6 +81,7 @@ publish_to_hp() {
 testing_to_hp() {
     # sync-tools cannot place the tools in a testing location, so swift is
     # used.
+    [[ $JT_IGNORE_HP == '1' ]] && return 0
     echo "Phase 2: Testing to HP Cloud."
     source $JUJU_DIR/hptoolsrc
     cd $JUJU_DIST/tools/releases/
@@ -89,6 +93,7 @@ testing_to_hp() {
 
 
 publish_to_aws() {
+    [[ $JT_IGNORE_AWS == '1' ]] && return 0
     echo "Phase 3: Publish to AWS."
     s3cmd -c $JUJU_DIR/s3cfg sync --exclude '*mirror*' \
         ${JUJU_DIST}/tools s3://juju-dist/
@@ -98,6 +103,7 @@ publish_to_aws() {
 testing_to_aws() {
     # this is the same as the publishing command except that the
     # destination is juju-dist/testing/
+    [[ $JT_IGNORE_AWS == '1' ]] && return 0
     echo "Phase 3: Testing to AWS."
     s3cmd -c $JUJU_DIR/s3cfg sync --exclude '*mirror*' \
         ${JUJU_DIST}/tools s3://juju-dist/testing/
@@ -111,6 +117,7 @@ shim_creds() {
 }
 
 publish_to_azure() {
+    [[ $JT_IGNORE_AZURE == '1' ]] && return 0
     echo "Phase 4: Publish to Azure."
     source $JUJU_DIR/azuretoolsrc
     shim_creds
@@ -121,6 +128,7 @@ publish_to_azure() {
 testing_to_azure() {
     # This command is like the publish command expcept tht -container is
     # different.
+    [[ $JT_IGNORE_AZURE == '1' ]] && return 0
     echo "Phase 4: Testing to Azure."
     source $JUJU_DIR/azuretoolsrc
     shim_creds
@@ -130,6 +138,7 @@ testing_to_azure() {
 
 publish_to_streams() {
     [[ -f $JUJU_DIR/streamsrc ]] || return 0
+    [[ $JT_IGNORE_STREAMS == '1' ]] && return 0
     echo "Phase 5: Published to streams.canonica.com."
     source $JUJU_DIR/streamsrc
     rsync -avzh $JUJU_DIST/ $STREAMS_OFFICIAL_DEST
@@ -138,6 +147,7 @@ publish_to_streams() {
 
 testing_to_streams() {
     [[ -f $JUJU_DIR/streamsrc ]] || return 0
+    [[ $JT_IGNORE_STREAMS == '1' ]] && return 0
     echo "Phase 5: Testing to streams.canonica.com."
     source $JUJU_DIR/streamsrc
     rsync -avzh $JUJU_DIST/ $STREAMS_TESTING_DEST
@@ -162,17 +172,17 @@ fi
 
 check_deps
 if [[ $PURPOSE == "RELEASE" ]]; then
+    publish_to_streams
     publish_to_canonistack
     publish_to_hp
     publish_to_aws
     publish_to_azure
-    publish_to_streams
     echo "Release data published to all CPCs."
 else
+    testing_to_streams
     testing_to_canonistack
     testing_to_hp
     testing_to_aws
     testing_to_azure
-    testing_to_streams
     echo "Testing data published to all CPCs."
 fi
