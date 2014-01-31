b59ad2181c11b40c475d3f08dd6ca7480651ae6d
Tim Penhey <tim.penhey@canonical.com>
2014-01-31 16:36:35 +1300

    Don't treat containers specially for apt proxy settings.

--- container/userdata.go
+++ container/userdata.go
@@ -17,8 +17,7 @@ import (
 )
 
 var (
-	logger         = loggo.GetLogger("juju.container")
-	aptHTTPProxyRE = regexp.MustCompile(`(?i)^Acquire::HTTP::Proxy\s+"([^"]+)";$`)
+	logger = loggo.GetLogger("juju.container")
 )
 
 func WriteUserData(machineConfig *cloudinit.MachineConfig, directory string) (string, error) {
@@ -42,33 +41,6 @@ func cloudInitUserData(machineConfig *cloudinit.MachineConfig) ([]byte, error) {
 	err := cloudinit.Configure(machineConfig, cloudConfig)
 	if err != nil {
 		return nil, err
-	}
-
-	// Run apt-config to fetch proxy settings from host. If no proxy
-	// settings are configured, then we don't set up any proxy information
-	// on the container.
-	proxyConfig, err := utils.AptConfigProxy()
-	if err != nil {
-		return nil, err
-	}
-	if proxyConfig != "" {
-		var proxyLines []string
-		for _, line := range strings.Split(proxyConfig, "\n") {
-			line = strings.TrimSpace(line)
-			if len(line) > 0 {
-				if m := aptHTTPProxyRE.FindStringSubmatch(line); m != nil {
-					cloudConfig.SetAptProxy(m[1])
-				} else {
-					proxyLines = append(proxyLines, line)
-				}
-			}
-		}
-		if len(proxyLines) > 0 {
-			cloudConfig.AddFile(
-				"/etc/apt/apt.conf.d/99proxy-extra",
-				strings.Join(proxyLines, "\n"),
-				0644)
-		}
 	}
 
 	// Run ifconfig to get the addresses of the internal container at least
