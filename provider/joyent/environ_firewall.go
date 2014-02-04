34c7132039db4c6f549079b437171edb0a652c1f
Daniele Stroppa <daniele.stroppa@joyent.com>
2014-02-04 17:49:47 +0000

    Instance and Environment firewall implementation

--- provider/joyent/environ_firewall.go
+++ provider/joyent/environ_firewall.go
@@ -4,26 +4,125 @@
 package joyent
 
 import (
+	"fmt"
+	"strings"
+	"strconv"
+
+	"launchpad.net/juju-core/environs/config"
 	"launchpad.net/juju-core/instance"
+
+	"launchpad.net/gojoyent/cloudapi"
 )
 
-// Implementing the methods below (to do something other than return nil) will
-// cause `juju expose` to work when the firewall-mode is "global". If you
-// implement one of them, you should implement them all.
+const(
+	firewallRuleAll 	= "FROM * TO * ALLOW %s PORT %d"
+)
+
+// Helper method to create a firewall rule string for the given port
+func createFirewallRuleAll(port instance.Port) string {
+	return fmt.Sprintf(firewallRuleAll, port.Protocol, port.Number)
+}
+
+// Helper method to check if a firewall rule string already exist
+func ruleExists(rules []cloudapi.FirewallRule, rule string) (bool, string) {
+	for _, r := range rules {
+		if strings.EqualFold(r.Rule, rule) {
+			return true, r.Id
+		}
+	}
+
+	return false, ""
+}
+
+// Helper method to get port from the given firewall rules
+func getPorts(rules []cloudapi.FirewallRule) []instance.Port {
+	ports := make([]instance.Port, len(rules))
+	for _, r := range rules {
+		rule := r.Rule
+		p := strings.ToUpper(rule[strings.Index(rule, "ALLOW") + 6:strings.Index(rule,"PORT")-1])
+		n, _ := strconv.Atoi(rule[strings.LastIndex(rule, " ") + 1:])
+		port := instance.Port{Protocol:p, Number:n}
+		ports = append(ports, port)
+	}
+
+	return ports
+}
 
 func (env *JoyentEnviron) OpenPorts(ports []instance.Port) error {
-	logger.Warningf("pretending to open ports %v for all instances", ports)
-	_ = env.getSnapshot()
+	if env.Config().FirewallMode() != config.FwGlobal {
+		return fmt.Errorf("invalid firewall mode %q for opening ports on environment", env.Config().FirewallMode())
+	}
+
+	fwRules, err := env.compute.cloudapi.ListFirewallRules()
+	if err != nil {
+		return fmt.Errorf("cannot get firewall rules: %v", err)
+	}
+
+	for _, p := range ports {
+		rule := createFirewallRuleAll(p)
+		if e, id := ruleExists(fwRules, rule); e {
+			_, err := env.compute.cloudapi.EnableFirewallRule(id)
+			if err != nil {
+				return fmt.Errorf("couldn't enable rule %s: %v", rule, err)
+			}
+		} else {
+			_, err := env.compute.cloudapi.CreateFirewallRule(cloudapi.CreateFwRuleOpts{
+				Enabled:        true,
+				Rule:           rule,
+			})
+			if err != nil {
+				return fmt.Errorf("couldn't create rule %s: %v", rule, err)
+			}
+		}
+	}
+
+	logger.Infof("ports %v opened in environment", ports)
+
 	return nil
 }
 
 func (env *JoyentEnviron) ClosePorts(ports []instance.Port) error {
-	logger.Warningf("pretending to close ports %v for all instances", ports)
-	_ = env.getSnapshot()
+	if env.Config().FirewallMode() != config.FwGlobal {
+		return fmt.Errorf("invalid firewall mode %q for opening ports on environment", env.Config().FirewallMode())
+	}
+
+	fwRules, err := env.compute.cloudapi.ListFirewallRules()
+	if err != nil {
+		return fmt.Errorf("cannot get firewall rules: %v", err)
+	}
+
+	for _, p := range ports {
+		rule := createFirewallRuleAll(p)
+		if e, id := ruleExists(fwRules, rule); e {
+			_, err := env.compute.cloudapi.DisableFirewallRule(id)
+			if err != nil {
+				return fmt.Errorf("couldn't disable rule %s: %v", rule, err)
+			}
+		} else {
+			_, err := env.compute.cloudapi.CreateFirewallRule(cloudapi.CreateFwRuleOpts{
+				Enabled:        false,
+				Rule:           rule,
+			})
+			if err != nil {
+				return fmt.Errorf("couldn't create rule %s: %v", rule, err)
+			}
+		}
+	}
+
+	logger.Infof("ports %v closed in environment", ports)
+
 	return nil
 }
 
 func (env *JoyentEnviron) Ports() ([]instance.Port, error) {
-	_ = env.getSnapshot()
-	return nil, nil
+	if env.Config().FirewallMode() != config.FwGlobal {
+		return nil, fmt.Errorf("invalid firewall mode %q for opening ports on environment", env.Config().FirewallMode())
+	}
+
+	fwRules, err := env.compute.cloudapi.ListFirewallRules()
+	if err != nil {
+		return nil, fmt.Errorf("cannot get firewall rules: %v", err)
+	}
+
+	return getPorts(fwRules), nil
 }
--- provider/joyent/instance_firewall.go
+++ provider/joyent/instance_firewall.go
@@ -4,23 +4,99 @@
 package joyent
 
 import (
+	"fmt"
+
+	"launchpad.net/juju-core/environs/config"
 	"launchpad.net/juju-core/instance"
+
+	"launchpad.net/gojoyent/cloudapi"
 )
 
-// Implementing the methods below (to do something other than return nil) will
-// cause `juju expose` to work when the firewall-mode is "instance". If you
-// implement one of them, you should implement them all.
+const (
+	firewallRuleVm 		= "FROM * TO vm %s ALLOW %s PORT %d"
+)
+
+// Helper method to create a firewall rule string for the given machine Id and port
+func createFirewallRuleVm(machineId string, port instance.Port) string {
+	return fmt.Sprintf(firewallRuleVm, machineId, port.Protocol, port.Number)
+}
 
 func (inst *joyentInstance) OpenPorts(machineId string, ports []instance.Port) error {
-	logger.Warningf("pretending to open ports %v for instance %q", ports, inst.machine.Id)
+	if inst.env.Config().FirewallMode() != config.FwInstance {
+		return fmt.Errorf("invalid firewall mode %q for opening ports on instance", inst.env.Config().FirewallMode())
+	}
+
+	fwRules, err := inst.env.compute.cloudapi.ListFirewallRules()
+	if err != nil {
+		return fmt.Errorf("cannot get firewall rules: %v", err)
+	}
+
+	for _, p := range ports {
+		rule := createFirewallRuleVm(machineId, p)
+		if e, id := ruleExists(fwRules, rule); e {
+			_, err := inst.env.compute.cloudapi.EnableFirewallRule(id)
+			if err != nil {
+				return fmt.Errorf("couldn't enable rule %s: %v", rule, err)
+			}
+		} else {
+			_, err := inst.env.compute.cloudapi.CreateFirewallRule(cloudapi.CreateFwRuleOpts{
+				Enabled:        true,
+				Rule:           rule,
+			})
+			if err != nil {
+				return fmt.Errorf("couldn't create rule %s: %v", rule, err)
+			}
+		}
+	}
+
+	logger.Infof("ports %v opened for instance %q", ports, machineId)
+
 	return nil
 }
 
 func (inst *joyentInstance) ClosePorts(machineId string, ports []instance.Port) error {
-	logger.Warningf("pretending to close ports %v for instance %q", ports, inst.machine.Id)
+	if inst.env.Config().FirewallMode() != config.FwInstance {
+		return fmt.Errorf("invalid firewall mode %q for opening ports on instance", inst.env.Config().FirewallMode())
+	}
+
+	fwRules, err := inst.env.compute.cloudapi.ListFirewallRules()
+	if err != nil {
+		return fmt.Errorf("cannot get firewall rules: %v", err)
+	}
+
+	for _, p := range ports {
+		rule := createFirewallRuleVm(machineId, p)
+		if e, id := ruleExists(fwRules, rule); e {
+			_, err := inst.env.compute.cloudapi.DisableFirewallRule(id)
+			if err != nil {
+				return fmt.Errorf("couldn't disable rule %s: %v", rule, err)
+			}
+		} else {
+			_, err := inst.env.compute.cloudapi.CreateFirewallRule(cloudapi.CreateFwRuleOpts{
+				Enabled:        false,
+				Rule:           rule,
+			})
+			if err != nil {
+				return fmt.Errorf("couldn't create rule %s: %v", rule, err)
+			}
+		}
+	}
+
+	logger.Infof("ports %v closed for instance %q", ports, machineId)
+
 	return nil
 }
 
 func (inst *joyentInstance) Ports(machineId string) ([]instance.Port, error) {
-	return nil, nil
+	if inst.env.Config().FirewallMode() != config.FwInstance {
+		return nil, fmt.Errorf("invalid firewall mode %q for opening ports on instance", inst.env.Config().FirewallMode())
+	}
+
+	fwRules, err := inst.env.compute.cloudapi.ListMachineFirewallRules(machineId)
+	if err != nil {
+		return nil, fmt.Errorf("cannot get firewall rules: %v", err)
+	}
+
+	return getPorts(fwRules), nil
 }
+
