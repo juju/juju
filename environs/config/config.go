c31b4fce2ffe25467cc264c859c3591445f7c1b4
Jesse Meek <jesse.meek@canonical.com>
2014-02-04 14:15:09 +1300

    environs/config: expand field names
    https://codereview.appspot.com/59520044/

--- environs/config/config.go
+++ environs/config/config.go
@@ -62,10 +62,10 @@ const (
 
 // Config holds an immutable environment configuration.
 type Config struct {
-	// definedAttrs holds the attributes that are defined for Config.
-	// unknownAttrs holds the other attributes that are passed in (aka UnknownAttrs).
+	// defined holds the attributes that are defined for Config.
+	// unknown holds the other attributes that are passed in (aka UnknownAttrs).
 	// the union of these two are AllAttrs
-	definedAttrs, unknownAttrs map[string]interface{}
+	defined, unknown map[string]interface{}
 }
 
 // Defaulting is a value that specifies whether a configuration
@@ -105,13 +105,13 @@ func New(withDefaults Defaulting, attrs map[string]interface{}) (*Config, error)
 	if withDefaults {
 		checker = withDefaultsChecker
 	}
-	definedAttrs, err := checker.Coerce(attrs, nil)
+	defined, err := checker.Coerce(attrs, nil)
 	if err != nil {
 		return nil, err
 	}
 	c := &Config{
-		definedAttrs: definedAttrs.(map[string]interface{}),
-		unknownAttrs: make(map[string]interface{}),
+		defined: defined.(map[string]interface{}),
+		unknown: make(map[string]interface{}),
 	}
 	if withDefaults {
 		if err := c.fillInDefaults(); err != nil {
@@ -128,7 +128,7 @@ func New(withDefaults Defaulting, attrs map[string]interface{}) (*Config, error)
 	// Copy unknown attributes onto the type-specific map.
 	for k, v := range attrs {
 		if _, ok := fields[k]; !ok {
-			c.unknownAttrs[k] = v
+			c.unknown[k] = v
 		}
 	}
 	return c, nil
@@ -153,7 +153,7 @@ func (c *Config) ensureUnitLogging() error {
 	if _, ok := levels["unit"]; !ok {
 		loggingConfig = loggingConfig + ";unit=DEBUG"
 	}
-	c.definedAttrs["logging-config"] = loggingConfig
+	c.defined["logging-config"] = loggingConfig
 	return nil
 }
 
@@ -169,12 +169,12 @@ func (c *Config) fillInDefaults() error {
 	keys := c.asString("authorized-keys")
 	if path != "" || keys == "" {
 		var err error
-		c.definedAttrs["authorized-keys"], err = ReadAuthorizedKeys(path)
+		c.defined["authorized-keys"], err = ReadAuthorizedKeys(path)
 		if err != nil {
 			return err
 		}
 	}
-	delete(c.definedAttrs, "authorized-keys-path")
+	delete(c.defined, "authorized-keys-path")
 
 	// Don't use c.Name() because the name hasn't
 	// been verified yet.
@@ -182,11 +182,11 @@ func (c *Config) fillInDefaults() error {
 	if name == "" {
 		return fmt.Errorf("empty name in environment configuration")
 	}
-	err := maybeReadAttrFromFile(c.definedAttrs, "ca-cert", name+"-cert.pem")
+	err := maybeReadAttrFromFile(c.defined, "ca-cert", name+"-cert.pem")
 	if err != nil {
 		return err
 	}
-	err = maybeReadAttrFromFile(c.definedAttrs, "ca-private-key", name+"-private-key.pem")
+	err = maybeReadAttrFromFile(c.defined, "ca-private-key", name+"-private-key.pem")
 	if err != nil {
 		return err
 	}
@@ -195,7 +195,7 @@ func (c *Config) fillInDefaults() error {
 
 func (c *Config) fillInStringDefault(attr string) {
 	if c.asString(attr) == "" {
-		c.definedAttrs[attr] = defaults[attr]
+		c.defined[attr] = defaults[attr]
 	}
 }
 
@@ -205,19 +205,19 @@ func (c *Config) fillInStringDefault(attr string) {
 func (cfg *Config) processDeprecatedAttributes() {
 	// The tools url has changed so ensure that both old and new values are in the config so that
 	// upgrades work. "tools-url" is the old attribute name.
-	if oldToolsURL := cfg.definedAttrs["tools-url"]; oldToolsURL != nil && oldToolsURL.(string) != "" {
+	if oldToolsURL := cfg.defined["tools-url"]; oldToolsURL != nil && oldToolsURL.(string) != "" {
 		_, newToolsSpecified := cfg.ToolsURL()
 		// Ensure the new attribute name "tools-metadata-url" is set.
 		if !newToolsSpecified {
-			cfg.definedAttrs["tools-metadata-url"] = oldToolsURL
+			cfg.defined["tools-metadata-url"] = oldToolsURL
 		}
 	}
 	// Even if the user has edited their environment yaml to remove the deprecated tools-url value,
 	// we still want it in the config for upgrades.
-	cfg.definedAttrs["tools-url"], _ = cfg.ToolsURL()
+	cfg.defined["tools-url"], _ = cfg.ToolsURL()
 	// Update the provider type from null to manual.
 	if cfg.Type() == "null" {
-		cfg.definedAttrs["type"] = "manual"
+		cfg.defined["type"] = "manual"
 	}
 }
 
@@ -227,20 +227,20 @@ func (cfg *Config) processDeprecatedAttributes() {
 func Validate(cfg, old *Config) error {
 	// Check that we don't have any disallowed fields.
 	for _, attr := range allowedWithDefaultsOnly {
-		if _, ok := cfg.definedAttrs[attr]; ok {
+		if _, ok := cfg.defined[attr]; ok {
 			return fmt.Errorf("attribute %q is not allowed in configuration", attr)
 		}
 	}
 	// Check that mandatory fields are specified.
 	for _, attr := range mandatoryWithoutDefaults {
-		if _, ok := cfg.definedAttrs[attr]; !ok {
+		if _, ok := cfg.defined[attr]; !ok {
 			return fmt.Errorf("%s missing from environment configuration", attr)
 		}
 	}
 
 	// Check that all other fields that have been specified are non-empty,
 	// unless they're allowed to be empty for backward compatibility,
-	for attr, val := range cfg.definedAttrs {
+	for attr, val := range cfg.defined {
 		if !isEmpty(val) {
 			continue
 		}
@@ -255,14 +255,14 @@ func Validate(cfg, old *Config) error {
 
 	// Check that the agent version parses ok if set explicitly; otherwise leave
 	// it alone.
-	if v, ok := cfg.definedAttrs["agent-version"].(string); ok {
+	if v, ok := cfg.defined["agent-version"].(string); ok {
 		if _, err := version.Parse(v); err != nil {
 			return fmt.Errorf("invalid agent version in environment configuration: %q", v)
 		}
 	}
 
 	// If the logging config is set, make sure it is valid.
-	if v, ok := cfg.definedAttrs["logging-config"].(string); ok {
+	if v, ok := cfg.defined["logging-config"].(string); ok {
 		if _, err := loggo.ParseConfigurationString(v); err != nil {
 			return err
 		}
@@ -292,7 +292,7 @@ func Validate(cfg, old *Config) error {
 	// Check the immutable config values.  These can't change
 	if old != nil {
 		for _, attr := range immutableAttributes {
-			if newv, oldv := cfg.definedAttrs[attr], old.definedAttrs[attr]; newv != oldv {
+			if newv, oldv := cfg.defined[attr], old.defined[attr]; newv != oldv {
 				return fmt.Errorf("cannot change %s from %#v to %#v", attr, oldv, newv)
 			}
 		}
@@ -324,22 +324,22 @@ func isEmpty(val interface{}) bool {
 	panic(fmt.Errorf("unexpected type %T in configuration", val))
 }
 
-// maybeReadAttrFromFile sets definedAttrs[attr] to:
+// maybeReadAttrFromFile sets defined[attr] to:
 //
-// 1) The content of the file definedAttrs[attr+"-path"], if that's set
-// 2) The value of definedAttrs[attr] if it is already set.
-// 3) The content of defaultPath if it exists and definedAttrs[attr] is unset
-// 4) Preserves the content of definedAttrs[attr], otherwise
+// 1) The content of the file defined[attr+"-path"], if that's set
+// 2) The value of defined[attr] if it is already set.
+// 3) The content of defaultPath if it exists and defined[attr] is unset
+// 4) Preserves the content of defined[attr], otherwise
 //
-// The definedAttrs[attr+"-path"] key is always deleted.
-func maybeReadAttrFromFile(definedAttrs map[string]interface{}, attr, defaultPath string) error {
+// The defined[attr+"-path"] key is always deleted.
+func maybeReadAttrFromFile(defined map[string]interface{}, attr, defaultPath string) error {
 	pathAttr := attr + "-path"
-	path, _ := definedAttrs[pathAttr].(string)
-	delete(definedAttrs, pathAttr)
+	path, _ := defined[pathAttr].(string)
+	delete(defined, pathAttr)
 	hasPath := path != ""
 	if !hasPath {
 		// No path and attribute is already set; leave it be.
-		if s, _ := definedAttrs[attr].(string); s != "" {
+		if s, _ := defined[attr].(string); s != "" {
 			return nil
 		}
 		path = defaultPath
@@ -363,7 +363,7 @@ func maybeReadAttrFromFile(definedAttrs map[string]interface{}, attr, defaultPat
 	if len(data) == 0 {
 		return fmt.Errorf("file %q is empty", path)
 	}
-	definedAttrs[attr] = string(data)
+	defined[attr] = string(data)
 	return nil
 }
 
@@ -371,16 +371,16 @@ func maybeReadAttrFromFile(definedAttrs map[string]interface{}, attr, defaultPat
 // in once place. It returns the given named attribute as a string,
 // returning "" if it isn't found.
 func (c *Config) asString(name string) string {
-	value, _ := c.definedAttrs[name].(string)
+	value, _ := c.defined[name].(string)
 	return value
 }
 
 // mustString returns the named attribute as an string, panicking if
 // it is not found or is empty.
 func (c *Config) mustString(name string) string {
-	value, _ := c.definedAttrs[name].(string)
+	value, _ := c.defined[name].(string)
 	if value == "" {
-		panic(fmt.Errorf("empty value for %q found in configuration (type %T, val %v)", name, c.definedAttrs[name], c.definedAttrs[name]))
+		panic(fmt.Errorf("empty value for %q found in configuration (type %T, val %v)", name, c.defined[name], c.defined[name]))
 	}
 	return value
 }
@@ -389,7 +389,7 @@ func (c *Config) mustString(name string) string {
 // it is not found or is zero. Zero values should have been
 // diagnosed at Validate time.
 func (c *Config) mustInt(name string) int {
-	value, _ := c.definedAttrs[name].(int)
+	value, _ := c.defined[name].(int)
 	if value == 0 {
 		panic(fmt.Errorf("empty value for %q found in configuration", name))
 	}
@@ -498,13 +498,13 @@ func (c *Config) BootstrapSSHOpts() SSHTimeoutOpts {
 		RetryDelay:     time.Duration(DefaultBootstrapSSHRetryDelay) * time.Second,
 		AddressesDelay: time.Duration(DefaultBootstrapSSHAddressesDelay) * time.Second,
 	}
-	if v, ok := c.definedAttrs["bootstrap-timeout"].(int); ok && v != 0 {
+	if v, ok := c.defined["bootstrap-timeout"].(int); ok && v != 0 {
 		opts.Timeout = time.Duration(v) * time.Second
 	}
-	if v, ok := c.definedAttrs["bootstrap-retry-delay"].(int); ok && v != 0 {
+	if v, ok := c.defined["bootstrap-retry-delay"].(int); ok && v != 0 {
 		opts.RetryDelay = time.Duration(v) * time.Second
 	}
-	if v, ok := c.definedAttrs["bootstrap-addresses-delay"].(int); ok && v != 0 {
+	if v, ok := c.defined["bootstrap-addresses-delay"].(int); ok && v != 0 {
 		opts.AddressesDelay = time.Duration(v) * time.Second
 	}
 	return opts
@@ -513,7 +513,7 @@ func (c *Config) BootstrapSSHOpts() SSHTimeoutOpts {
 // CACert returns the certificate of the CA that signed the state server
 // certificate, in PEM format, and whether the setting is available.
 func (c *Config) CACert() ([]byte, bool) {
-	if s, ok := c.definedAttrs["ca-cert"]; ok {
+	if s, ok := c.defined["ca-cert"]; ok {
 		return []byte(s.(string)), true
 	}
 	return nil, false
@@ -522,7 +522,7 @@ func (c *Config) CACert() ([]byte, bool) {
 // CAPrivateKey returns the private key of the CA that signed the state
 // server certificate, in PEM format, and whether the setting is available.
 func (c *Config) CAPrivateKey() (key []byte, ok bool) {
-	if s, ok := c.definedAttrs["ca-private-key"]; ok && s != "" {
+	if s, ok := c.defined["ca-private-key"]; ok && s != "" {
 		return []byte(s.(string)), true
 	}
 	return nil, false
@@ -531,7 +531,7 @@ func (c *Config) CAPrivateKey() (key []byte, ok bool) {
 // AdminSecret returns the administrator password.
 // It's empty if the password has not been set.
 func (c *Config) AdminSecret() string {
-	if s, ok := c.definedAttrs["admin-secret"]; ok && s != "" {
+	if s, ok := c.defined["admin-secret"]; ok && s != "" {
 		return s.(string)
 	}
 	return ""
@@ -548,7 +548,7 @@ func (c *Config) FirewallMode() string {
 // and whether it has been set. Once an environment is bootstrapped, this
 // must always be valid.
 func (c *Config) AgentVersion() (version.Number, bool) {
-	if v, ok := c.definedAttrs["agent-version"].(string); ok {
+	if v, ok := c.defined["agent-version"].(string); ok {
 		n, err := version.Parse(v)
 		if err != nil {
 			panic(err) // We should have checked it earlier.
@@ -561,7 +561,7 @@ func (c *Config) AgentVersion() (version.Number, bool) {
 // ToolsURL returns the URL that locates the tools tarballs and metadata,
 // and whether it has been set.
 func (c *Config) ToolsURL() (string, bool) {
-	if url, ok := c.definedAttrs["tools-metadata-url"]; ok && url != "" {
+	if url, ok := c.defined["tools-metadata-url"]; ok && url != "" {
 		return url.(string), true
 	}
 	return "", false
@@ -570,7 +570,7 @@ func (c *Config) ToolsURL() (string, bool) {
 // ImageMetadataURL returns the URL at which the metadata used to locate image ids is located,
 // and wether it has been set.
 func (c *Config) ImageMetadataURL() (string, bool) {
-	if url, ok := c.definedAttrs["image-metadata-url"]; ok && url != "" {
+	if url, ok := c.defined["image-metadata-url"]; ok && url != "" {
 		return url.(string), true
 	}
 	return "", false
@@ -578,13 +578,13 @@ func (c *Config) ImageMetadataURL() (string, bool) {
 
 // Development returns whether the environment is in development mode.
 func (c *Config) Development() bool {
-	return c.definedAttrs["development"].(bool)
+	return c.defined["development"].(bool)
 }
 
 // SSLHostnameVerification returns weather the environment has requested
 // SSL hostname verification to be enabled.
 func (c *Config) SSLHostnameVerification() bool {
-	return c.definedAttrs["ssl-hostname-verification"].(bool)
+	return c.defined["ssl-hostname-verification"].(bool)
 }
 
 // LoggingConfig returns the configuration string for the loggers.
@@ -601,7 +601,7 @@ func (c *Config) CharmStoreAuth() (string, bool) {
 // ProvisionerSafeMode reports whether the provisioner should not
 // destroy machines it does not know about.
 func (c *Config) ProvisionerSafeMode() bool {
-	v, _ := c.definedAttrs["provisioner-safe-mode"].(bool)
+	v, _ := c.defined["provisioner-safe-mode"].(bool)
 	return v
 }
 
@@ -609,7 +609,7 @@ func (c *Config) ProvisionerSafeMode() bool {
 // used to identify which image ids to search
 // when starting an instance.
 func (c *Config) ImageStream() string {
-	v, ok := c.definedAttrs["image-stream"].(string)
+	v, ok := c.defined["image-stream"].(string)
 	if ok {
 		return v
 	}
@@ -621,29 +621,29 @@ func (c *Config) ImageStream() string {
 // also be wrong attributes, though. Only the specific environment
 // implementation can tell.
 func (c *Config) UnknownAttrs() map[string]interface{} {
-	unknownAttrs := make(map[string]interface{})
-	for k, v := range c.unknownAttrs {
-		unknownAttrs[k] = v
+	newAttrs := make(map[string]interface{})
+	for k, v := range c.unknown {
+		newAttrs[k] = v
 	}
-	return unknownAttrs
+	return newAttrs
 }
 
 // AllAttrs returns a copy of the raw configuration attributes.
 func (c *Config) AllAttrs() map[string]interface{} {
-	definedAttrs := c.UnknownAttrs()
-	for k, v := range c.definedAttrs {
-		definedAttrs[k] = v
+	allAttrs := c.UnknownAttrs()
+	for k, v := range c.defined {
+		allAttrs[k] = v
 	}
-	return definedAttrs
+	return allAttrs
 }
 
 // Apply returns a new configuration that has the attributes of c plus attrs.
 func (c *Config) Apply(attrs map[string]interface{}) (*Config, error) {
-	definedAttrs := c.AllAttrs()
+	defined := c.AllAttrs()
 	for k, v := range attrs {
-		definedAttrs[k] = v
+		defined[k] = v
 	}
-	return New(NoDefaults, definedAttrs)
+	return New(NoDefaults, defined)
 }
 
 var fields = schema.Fields{
