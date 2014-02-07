a66fccd8df6d211b83f60c539850fd7b4a8f13a9
Roger Peppe <roger.peppe@canonical.com>
2014-02-07 16:32:23 +0000

    state: fix mongo timeout

--- state/open.go
+++ state/open.go
@@ -23,6 +23,8 @@ import (
 	"launchpad.net/juju-core/utils"
 )
 
+const mongoSocketTimeout = 10 * time.Second
+
 // Info encapsulates information about cluster of
 // servers holding juju state and can be used to make a
 // connection to that cluster.
@@ -108,6 +110,7 @@ func Open(info *Info, opts DialOpts) (*State, error) {
 		session.Close()
 		return nil, err
 	}
+	session.SetSocketTimeout(mongoSocketTimeout)
 	return st, nil
 }
 
