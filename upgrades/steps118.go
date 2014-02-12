b6c68eececfeeb736b37ad301dad499214383c65
Tim Penhey <tim.penhey@canonical.com>
2014-02-13 09:52:27 +1300

    Fix the upgrade step.

--- upgrades/steps118.go
+++ upgrades/steps118.go
@@ -5,6 +5,7 @@ package upgrades
 
 import (
 	"fmt"
+
 	"launchpad.net/juju-core/agent"
 	"launchpad.net/juju-core/utils/exec"
 )
@@ -12,7 +13,22 @@ import (
 // stepsFor118 returns upgrade steps to upgrade to a Juju 1.18 deployment.
 func stepsFor118(context Context) []UpgradeStep {
 	return []UpgradeStep{
-	// Nothing yet.
+		// Nothing yet.
+		&upgradeStep{
+			description: `
+				Previously the lock directory was created when the uniter started.
+				This allows serialization of all of the hook execution across units
+				running on a single machine.  This lock directory is now also used
+				but the juju-run command on the host machine.  juju-run also gets
+				a lock on the hook execution fslock prior to execution.  However,
+				the lock directory was owned by root, and the juju-run process was
+				being executed by the ubuntu user, so we need to change the ownership
+				of the lock directory to ubuntu:ubuntu. Also we need to make sure that
+				this directory exists on machines with no units.
+			`,
+			targets: []UpgradeTarget{HostMachine},
+			run:     ensureLockDirExistsAndUbuntuWritable,
+		},
 	}
 }
 
@@ -20,11 +36,8 @@ func stepsFor118(context Context) []UpgradeStep {
 // itself to ubuntu:ubuntu from root:root so the juju-run command run as the
 // ubuntu user is able to get access to the hook execution lock (like the
 // uniter itself does.)
-func ensureLockDirExistsAndUbuntuWritable() {
-	// Pretty sure we want the agent Config in the context...
-	var agentConfig agent.Config // TODO: get from context
-
-	lockDir := path.Join(agentConfig.DataDir(), "locks")
+func ensureLockDirExistsAndUbuntuWritable(context Context) error {
+	lockDir := path.Join(context.AgentConfig().DataDir(), "locks")
 	// We only try to change ownership if there is an ubuntu user
 	// defined, and we determine this by the existance of the home dir.
 	command := fmt.Sprintf(""+
@@ -32,13 +45,13 @@ func ensureLockDirExistsAndUbuntuWritable() {
 		"[ -e /home/ubuntu ] && chown ubuntu:ubuntu %s",
 		lockDir, lockDir)
 	result, err := exec.RunCommands(exec.RunParams{
-		Commands:   command,
-		WorkingDir: ProxyDirectory,
+		Commands: command,
 	})
 	if err != nil {
 		return err
 	}
 	if result.Code != 0 {
-		logger.Errorf("failed writing new proxy values: \n%s\n%s", result.Stdout, result.Stderr)
+		return fmt.Errorf("failed to create lock dir or ownership change: \n%s\n%s", result.Stdout, result.Stderr)
 	}
+	return nil
 }
