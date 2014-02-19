6a70db57766cd7ada6152ea21762bb58c9096fd7
Curtis Hovey <curtis@canonical.com>
2014-02-19 14:45:49 -0500

    Someone added an arch without telling me. not fair!

--- assemble-public-tools.bash
+++ assemble-public-tools.bash
@@ -131,7 +131,8 @@ get_arch() {
     control_file=$1
     arch=$(sed -n 's/^Architecture: \([a-z]\+\)/\1/p' $control_file)
     case "${arch}" in
-        "amd64" | "i386" | "armel" | "armhf" | "arm64" | "ppc64el" )
+        "amd64" | "i386" | "armel" | "armhf" | "arm64" | "ppc64el" | 
+"powerpc" )
             ;;
         *)
             echo "Invalid arch: $arch"
