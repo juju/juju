dcc378532878080b96a610fed7faef5a4190282e
Curtis Hovey <curtis@canonical.com>
2014-02-20 16:31:52 -0500

    Added script that only uploads swift files that are different.

---
+++ swift-sync.bash
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+JUJU_DIST='juju-dist'
+
+
+usage() {
+    echo "usage: $0 PATH FILES"
+    echo "  sync new and changed files to the juju-dist container."
+    echo ""
+    echo "  PATH: the path in the juju-dist container to upload to"
+    echo "  FILES: One or more files to upload."
+    exit 1
+}
+
+
+test $# -gt 1 || usage
+DEST=$1
+shift
+FILES=$@
+
+
+for file in $FILES; do
+    md5=$(md5sum $file | cut -d ' ' -f1)
+    etag=$(swift stat $JUJU_DIST $DEST/$file 2>&1 |
+        grep ETag | sed -r 's,.*: ,,')
+    if [[ $md5 == $etag ]]; then
+        echo "$file is unchanged"
+        continue
+    fi
+    if [[ -z $etag ]]; then
+        echo "$file is new"
+    else
+        echo "$file is different"
+        echo "$md5 == $etag"
+    fi
+    echo "Uploading $JUJU_DIST/$DEST/$file"
+    #swift upload $JUJU_DISTs/$DEST/ $file
+done
