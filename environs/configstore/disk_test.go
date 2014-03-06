a1bacadc3e834c376ef981664e80e17ca7b97cc6
Michael Foord <michael.foord@canonical.com>
2014-03-06 12:06:16 +0000

    Regularise the expression

--- environs/configstore/disk_test.go
+++ environs/configstore/disk_test.go
@@ -110,7 +110,7 @@ func (*diskStoreSuite) TestRead(c *gc.C) {
 		Addresses: []string{"example.com", "kremvax.ru"},
 		CACert:    "first line\nsecond line",
 	})
-	c.Assert(info.Location(), gc.Matches, "*/someenv.jenv")
+	c.Assert(info.Location(), gc.Matches, ".*/someenv.jenv")
 	c.Assert(info.BootstrapConfig(), gc.DeepEquals, map[string]interface{}{
 		"secret": "blah",
 		"arble":  "bletch",
