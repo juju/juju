92cad4e891f848cf6dbc18731f623ff02cbea206
Tim Penhey <tim.penhey@canonical.com>
2014-03-10 20:19:32 +1300

    Move the clone specific stuff to its own file.

--- container/lxc/lxc.go
+++ container/lxc/lxc.go
@@ -5,28 +5,21 @@ package lxc
 
 import (
 	"fmt"
-	"io"
 	"io/ioutil"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
-	"sync"
 	"time"
 
 	"github.com/juju/loggo"
 	"launchpad.net/golxc"
 
 	"launchpad.net/juju-core/agent"
-	coreCloudinit "launchpad.net/juju-core/cloudinit"
 	"launchpad.net/juju-core/container"
 	"launchpad.net/juju-core/environs/cloudinit"
 	"launchpad.net/juju-core/instance"
-	"launchpad.net/juju-core/juju/osenv"
 	"launchpad.net/juju-core/names"
-	"launchpad.net/juju-core/utils"
-	"launchpad.net/juju-core/utils/fslock"
-	"launchpad.net/juju-core/utils/tailer"
 	"launchpad.net/juju-core/version"
 )
 
@@ -116,7 +109,7 @@ func (manager *containerManager) StartContainer(
 	series string,
 	network *container.NetworkConfig,
 ) (instance.Instance, *instance.HardwareCharacteristics, error) {
-
+	start := time.Now()
 	name := names.MachineTag(machineConfig.MachineId)
 	if manager.name != "" {
 		name = fmt.Sprintf("%s-%s", manager.name, name)
@@ -145,9 +138,14 @@ func (manager *containerManager) StartContainer(
 
 	var lxcContainer golxc.Container
 	if manager.createWithClone {
-		var templateContainer golxc.Container
-		if templateContainer, err = manager.ensureCloneTemplate(
-			machineConfig, series, network); err != nil {
+		templateContainer, err := EnsureCloneTemplate(
+			manager.backingFilesystem,
+			series,
+			network,
+			machineConfig.AuthorizedKeys,
+			machineConfig.AptProxySettings,
+		)
+		if err != nil {
 			return nil, nil, err
 		}
 		templateParams := []string{
@@ -160,7 +158,11 @@ func (manager *containerManager) StartContainer(
 			extraCloneArgs = append(extraCloneArgs, "--snapshot")
 		}
 
-		// TODO: fslock on clone template
+		lock, err := AcquireTemplateLock(templateContainer.Name(), "clone")
+		if err != nil {
+			return nil, nil, fmt.Errorf("failed to acquire lock on template: %v", err)
+		}
+		defer lock.Unlock()
 		lxcContainer, err = templateContainer.Clone(name, extraCloneArgs, templateParams)
 		if err != nil {
 			logger.Errorf("lxc container cloning failed: %v", err)
@@ -211,8 +213,19 @@ func (manager *containerManager) StartContainer(
 	hardware := &instance.HardwareCharacteristics{
 		Arch: &arch,
 	}
-	logger.Tracef("container started")
+	logger.Tracef("container %q started: %v", name, time.Now().Sub(start))
 	return &lxcInstance{lxcContainer, name}, hardware, nil
+}
+
+func addToContainerConfig(name string, line string) error {
+	file, err := os.OpenFile(
+		containerConfigFilename(name), os.O_RDWR|os.O_APPEND, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	file.WriteString(line)
+	return nil
 }
 
 func autostartContainer(name string) error {
@@ -229,19 +242,14 @@ func autostartContainer(name string) error {
 		}
 		logger.Tracef("auto-restart link created")
 	} else {
-		file, err := os.OpenFile(
-			containerConfigFilename(name), os.O_RDWR|os.O_APPEND, 0644)
-		if err != nil {
-			return err
-		}
-		defer file.Close()
-		file.WriteString("lxc.start.auto = 1\n")
-		logger.Tracef("Setting auto start to true in lxc config.")
+		logger.Tracef("setting auto start to true in lxc config")
+		return addToContainerConfig(name, "lxc.start.auto = 1\n")
 	}
 	return nil
 }
 
 func (manager *containerManager) StopContainer(instance instance.Instance) error {
+	start := time.Now()
 	name := string(instance.Id())
 	lxcContainer := LxcObjectFactory.New(name)
 	if useRestartDir() {
@@ -256,7 +264,9 @@ func (manager *containerManager) StopContainer(instance instance.Instance) error
 		return err
 	}
 
-	return container.RemoveDirectory(name)
+	err = container.RemoveDirectory(name)
+	logger.Tracef("container %q stopped: %v", name, time.Now().Sub(start))
+	return err
 }
 
 func (manager *containerManager) ListContainers() (result []instance.Instance, err error) {
@@ -353,187 +363,4 @@ func useRestartDir() bool {
 		}
 	}
 	return true
-}
-
-const (
-	templateShutdownUpstartFilename = "/etc/init/juju-template-restart.conf"
-	templateShutdownUpstartScript   = `
-description "Juju lxc template shutdown job"
-author "Juju Team <juju@lists.ubuntu.com>"
-start on stopped cloud-final
-
-script
-  echo "Shutting down"
-  shutdown -h now
-end script
-
-post-stop script
-  echo "Removing restart script"
-  rm ` + templateShutdownUpstartFilename + `
-end script
-`
-)
-
-// templateUserData returns a minimal user data necessary for the template.
-// This should have the authorized keys, base packages, the cloud archive if
-// necessary,  initial apt proxy config, and it should do the apt-get
-// update/upgrade initially.
-func templateUserData(
-	series string, authorizedKeys string, aptProxy osenv.ProxySettings,
-) ([]byte, error) {
-	cloudConfig := coreCloudinit.New()
-	cloudConfig.AddScripts(
-		"set -xe", // ensure we run all the scripts or abort.
-	)
-	cloudinit.MaybeAddCloudArchiveCloudTools(cloudConfig, series)
-	cloudConfig.AddSSHAuthorizedKeys(authorizedKeys)
-	cloudinit.AddAptCommands(aptProxy, cloudConfig)
-	cloudConfig.AddScripts(
-		fmt.Sprintf(
-			"printf '%%s\n' %s > %s",
-			utils.ShQuote(templateShutdownUpstartScript),
-			templateShutdownUpstartFilename,
-		))
-	data, err := cloudConfig.Render()
-	if err != nil {
-		return nil, err
-	}
-	return data, nil
-}
-
-// Make sure a template exists that we can clone from.
-func (manager *containerManager) ensureCloneTemplate(
-	machineConfig *cloudinit.MachineConfig,
-	series string,
-	network *container.NetworkConfig,
-) (golxc.Container, error) {
-	name := fmt.Sprintf("juju-%s-template", series)
-	containerDirectory, err := container.NewDirectory(name)
-	if err != nil {
-		return nil, err
-	}
-
-	logger.Infof("wait for fslock on %v", name)
-	lock, err := fslock.NewLock(
-		filepath.Join(machineConfig.DataDir, "locks"),
-		name)
-	if err != nil {
-		logger.Tracef("failed to create gslock for template: %v", err)
-		return nil, err
-	}
-	err = lock.Lock("manager ensure existence")
-	if err != nil {
-		logger.Tracef("failed to acquire lock for template: %v", err)
-		return nil, err
-	}
-	defer lock.Unlock()
-
-	lxcContainer := LxcObjectFactory.New(name)
-	// Early exit if the container has been constructed before.
-	if lxcContainer.IsConstructed() {
-		logger.Infof("template exists, continuing")
-		return lxcContainer, nil
-	}
-	logger.Infof("template does not exist, creating")
-
-	userData, err := templateUserData(
-		series, machineConfig.AuthorizedKeys, machineConfig.AptProxySettings)
-	if err != nil {
-		logger.Tracef("filed to create tempalte user data for template: %v", err)
-		return nil, err
-	}
-	userDataFilename, err := container.WriteCloudInitFile(containerDirectory, userData)
-	if err != nil {
-		return nil, err
-	}
-
-	configFile, err := writeLxcConfig(network, containerDirectory, manager.logdir)
-	if err != nil {
-		logger.Errorf("failed to write config file: %v", err)
-		return nil, err
-	}
-	templateParams := []string{
-		"--debug",                      // Debug errors in the cloud image
-		"--userdata", userDataFilename, // Our groovey cloud-init
-		"--hostid", name, // Use the container name as the hostid
-		"-r", series,
-	}
-	var extraCreateArgs []string
-	if manager.backingFilesystem == Btrfs {
-		extraCreateArgs = append(extraCreateArgs, "-B", Btrfs)
-	}
-	// Create the container.
-	logger.Tracef("create the container")
-	if err := lxcContainer.Create(configFile, defaultTemplate, extraCreateArgs, templateParams); err != nil {
-		logger.Errorf("lxc container creation failed: %v", err)
-		return nil, err
-	}
-	// Make sure that the mount dir has been created.
-	logger.Tracef("make the mount dir for the shared logs")
-	if err := os.MkdirAll(internalLogDir(name), 0755); err != nil {
-		logger.Tracef("failed to create internal /var/log/juju mount dir: %v", err)
-		return nil, err
-	}
-
-	// Start the lxc container with the appropriate settings for grabbing the
-	// console output and a log file.
-	consoleFile := filepath.Join(containerDirectory, "console.log")
-	lxcContainer.SetLogFile(filepath.Join(containerDirectory, "container.log"), golxc.LogDebug)
-	logger.Tracef("start the container")
-	// We explicitly don't pass through the config file to the container.Start
-	// method as we have passed it through at container creation time.  This
-	// is necessary to get the appropriate rootfs reference without explicitly
-	// setting it ourselves.
-	if err = lxcContainer.Start("", consoleFile); err != nil {
-		logger.Errorf("container failed to start: %v", err)
-		return nil, err
-	}
-	logger.Infof("template container started, now wait for it to stop")
-	// Perhaps we should wait for it to finish, and the question becomes "how
-	// long do we wait for it to complete?"
-
-	console, err := os.Open(consoleFile)
-	if err != nil {
-		// can't listen
-		return nil, err
-	}
-
-	tailWriter := &logTail{}
-	consoleTailer := tailer.NewTailer(console, tailWriter, 0, nil)
-	defer consoleTailer.Stop()
-
-	// We should wait maybe 1 minute between output?
-	// if no output check to see if stopped
-	// If we have no output and still running, something has probably gone wrong
-	for lxcContainer.IsRunning() {
-		if tailWriter.lastTick().Before(time.Now().Add(-5 * time.Minute)) {
-			logger.Infof("not heard anything from the template log for five minutes")
-			return nil, fmt.Errorf("template container did not stop")
-		}
-		time.Sleep(time.Second)
-	}
-
-	return lxcContainer, nil
-}
-
-type logTail struct {
-	tick  time.Time
-	mutex sync.Mutex
-}
-
-var _ io.Writer = (*logTail)(nil)
-
-func (t *logTail) Write(data []byte) (int, error) {
-	logger.Tracef(string(data))
-	t.mutex.Lock()
-	defer t.mutex.Unlock()
-	t.tick = time.Now()
-	return len(data), nil
-}
-
-func (t *logTail) lastTick() time.Time {
-	t.mutex.Lock()
-	defer t.mutex.Unlock()
-	tick := t.tick
-	return tick
 }
