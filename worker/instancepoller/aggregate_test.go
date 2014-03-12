8e9e97d30470b6fbdba127d08dafd2c87e77e8cd
Michael Foord <michael.foord@canonical.com>
2014-03-12 14:28:07 +0000

    Test single request

--- worker/instancepoller/aggregate_test.go
+++ worker/instancepoller/aggregate_test.go
@@ -21,35 +21,45 @@ var _ = gc.Suite(&aggregateSuite{})
 type testInstance struct {
 	instance.Instance
 	addresses []instance.Address
-	id        instance.Id
-	address   instance.Address
+	status    string
 }
 
-func (t *testInstance) Addresses() ([]instance.Address, error) {
+var _ instance.Instance = (*testInstance)(nil)
+
+func (t testInstance) Addresses() ([]instance.Address, error) {
 	return t.addresses, nil
 }
 
-func (t *testInstance) Id() instance.Id {
-	return t.id
+func (t testInstance) Status() string {
+	return t.status
 }
 
 type testInstanceGetter struct {
 	ids     []instance.Id
-	results []instance.Instance
+	results []testInstance
 	err     bool
 }
 
-func (i *testInstanceGetter) Instances(ids []instance.Id) ([]instance.Instance, error) {
+func (i *testInstanceGetter) Instances(ids []instance.Id) (result []instance.Instance, err error) {
 	i.ids = ids
 	if i.err {
 		return nil, fmt.Errorf("Some error")
 	}
-	return i.results, nil
+	for _, inst := range i.results {
+		result = append(result, inst)
+	}
+	return
 }
 
-func (s *aggregateSuite) TestLoop(c *gc.C) {
+func (s *aggregateSuite) TestSingleRequest(c *gc.C) {
 	testGetter := new(testInstanceGetter)
-	testGetter.err = true
+	address1 := instance.NewAddress("127.0.0.1")
+	address2 := instance.NewAddress("192.168.1.1")
+	instance1 := testInstance{
+			status:    "foobar",
+			addresses: []instance.Address{address1, address2},
+		}
+	testGetter.results = []testInstance{instance1}
 	aggregator := newAggregator(testGetter)
 
 	replyChan := make(chan instanceInfoReply)
@@ -59,6 +69,10 @@ func (s *aggregateSuite) TestLoop(c *gc.C) {
 	}
 	aggregator.reqc <- req
 	reply := <-replyChan
-	c.Assert(reply.info, gc.DeepEquals, instanceInfo{})
+	c.Assert(reply.err, gc.IsNil)
+	c.Assert(reply.info, gc.DeepEquals, instanceInfo{
+		status:    "foobar",
+		addresses: []instance.Address{address1, address2},
+	})
 	c.Assert(testGetter.ids, gc.DeepEquals, []instance.Id{instance.Id("foo")})
 }
