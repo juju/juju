33584f429114852d9c4af621c8f131668fbed04c
Roger Peppe <roger.peppe@canonical.com>
2014-03-19 17:22:14 +0000

    state: revert unrelated change

--- state/apiserver/root.go
+++ state/apiserver/root.go
@@ -56,10 +56,6 @@ type srvRoot struct {
 	entity taggedAuthenticator
 }
 
-type SrvRoot struct {
-	srvRoot
-}
-
 // newSrvRoot creates the client's connection representation
 // and starts a ping timeout for the monitoring of this
 // connection.
