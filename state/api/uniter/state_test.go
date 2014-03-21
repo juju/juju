99b39959462892d888f0b69b4e2ac1d6a6e0a903
Roger Peppe <roger.peppe@canonical.com>
2014-03-21 15:11:01 +0000

    wip

--- state/api/uniter/state_test.go
+++ state/api/uniter/state_test.go
@@ -35,18 +35,6 @@ func (s *stateSuite) TearDownTest(c *gc.C) {
 	s.uniterSuite.TearDownTest(c)
 }
 
-func (s *stateSuite) TestAPIAddresses(c *gc.C) {
-	stateAPIAddresses, err := s.State.APIAddressesFromMachines()
-	c.Assert(err, gc.IsNil)
-	addresses, err := s.uniter.APIAddresses()
-	c.Assert(err, gc.IsNil)
-	c.Assert(addresses, gc.DeepEquals, stateAPIAddresses)
-	// testing.AddStateServerMachine creates a machine which does *not*
-	// match the values in the Environ Config, so these don't match
-	apiInfo := s.APIInfo(c)
-	c.Assert(addresses, gc.Not(gc.DeepEquals), apiInfo.Addrs)
-}
-
 func (s *stateSuite) TestProviderType(c *gc.C) {
 	cfg, err := s.State.EnvironConfig()
 	c.Assert(err, gc.IsNil)
