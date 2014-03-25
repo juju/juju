d5e917db359d6a549d4cf5506740dfef2ca704b6
Michael Foord <michael.foord@canonical.com>
2014-03-25 17:46:04 +0000

    Update comment

--- worker/peergrouper/publish.go
+++ worker/peergrouper/publish.go
@@ -19,6 +19,6 @@ func newPublisher(st *state.State) *publisher {
 }
 
 func (pub *publisher) publishAPIServers(apiServers [][]instance.HostPort, instanceIds []instance.Id) error {
-	// TODO(rog) publish API addresses (instance ids?) in environment storage.
+	// TODO(rog) publish instanceIds in environment storage.
 	return pub.st.SetAPIHostPorts(apiServers)
 }
