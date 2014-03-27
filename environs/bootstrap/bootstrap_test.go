7158e0cce86df1b71acdd0e5c51ed7ed34f465a3
Dave Cheney <david.cheney@canonical.com>
2014-03-27 11:34:54 +1100

    bootstrap.SeriesToUpload does not define the order of the series returned. Use jc.SameContent rather than DeepEquals to treat the return value as a set, not a list.

--- environs/bootstrap/bootstrap_test.go
+++ environs/bootstrap/bootstrap_test.go
@@ -9,6 +9,7 @@ import (
 	stdtesting "testing"
 
 	gc "launchpad.net/gocheck"
+	jc "github.com/juju/testing/checkers"
 
 	"launchpad.net/juju-core/constraints"
 	"launchpad.net/juju-core/environs"
@@ -324,11 +325,11 @@ func (s *bootstrapSuite) TestSeriesToUpload(c *gc.C) {
 	s.PatchValue(&version.Current, vers)
 	env := newEnviron("foo", useDefaultKeys, nil)
 	cfg := env.Config()
-	c.Assert(bootstrap.SeriesToUpload(cfg, nil), gc.DeepEquals, []string{"quantal", "precise"})
-	c.Assert(bootstrap.SeriesToUpload(cfg, []string{"quantal"}), gc.DeepEquals, []string{"quantal"})
+	c.Assert(bootstrap.SeriesToUpload(cfg, nil), jc.SameContents, []string{"quantal", "precise"})
+	c.Assert(bootstrap.SeriesToUpload(cfg, []string{"quantal"}), jc.SameContents, []string{"quantal"})
 	env = newEnviron("foo", useDefaultKeys, map[string]interface{}{"default-series": "lucid"})
 	cfg = env.Config()
-	c.Assert(bootstrap.SeriesToUpload(cfg, nil), gc.DeepEquals, []string{"quantal", "precise", "lucid"})
+	c.Assert(bootstrap.SeriesToUpload(cfg, nil), jc.SameContents, []string{"quantal", "precise", "lucid"})
 }
 
 func (s *bootstrapSuite) assertUploadTools(c *gc.C, vers version.Binary, allowRelease bool,
