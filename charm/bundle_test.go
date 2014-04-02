96f4063a95cc1ecee1d84fd89bab9ddf1e827563
Jesse Meek <jesse.meek@canonical.com>
2014-04-02 16:13:07 +1300

    Remove debug line

--- charm/bundle_test.go
+++ charm/bundle_test.go
@@ -306,7 +306,6 @@ func (s *BundleSuite) TestExpandToWithBadLink(c *gc.C) {
 
 func extBundleDirPath(c *gc.C, dirpath string) string {
 	path := filepath.Join(c.MkDir(), "bundle.charm")
-	c.Logf("%#v", fmt.Sprintf("cd %s; zip --fifo --symlinks -r %s .", dirpath, path))
 	cmd := exec.Command("/bin/sh", "-c", fmt.Sprintf("cd %s; zip --fifo --symlinks -r %s .", dirpath, path))
 	output, err := cmd.CombinedOutput()
 	c.Assert(err, gc.IsNil, gc.Commentf("Command output: %s", output))
