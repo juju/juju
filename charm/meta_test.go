329b3f5501649f641cc30ad4f79932b5277f2c0a
Casey Marshall <cmars@cmarstech.com>
2014-04-03 14:50:49 -0500

    Split series tests into valid and invalid. Improve readability.

--- charm/meta_test.go
+++ charm/meta_test.go
@@ -240,17 +240,32 @@ requires:
   innocuous: juju-info`, "")
 }
 
+// dummyMetadata contains a minimally valid charm metadata.yaml
+// for testing valid and invalid series.
+const dummyMetadata = "name: a\nsummary: b\ndescription: c"
+
+// TestSeries ensures that valid series values are parsed correctly when specified
+// in the charm metadata.
 func (s *MetaSuite) TestSeries(c *gc.C) {
-	noSeries := "name: a\nsummary: b\ndescription: c\n"
+	// series not specified
+	meta, err := charm.ReadMeta(strings.NewReader(dummyMetadata))
+	c.Assert(err, gc.IsNil)
+	c.Check(meta.Series, gc.Equals, "")
 
 	for _, seriesName := range []string{"precise", "trusty", "plan9"} {
-		meta, err := charm.ReadMeta(strings.NewReader(noSeries + fmt.Sprintf("series: %s\n", seriesName)))
+		meta, err := charm.ReadMeta(strings.NewReader(
+			fmt.Sprintf("%s\nseries: %s\n", dummyMetadata, seriesName)))
 		c.Assert(err, gc.IsNil)
 		c.Check(meta.Series, gc.Equals, seriesName)
 	}
+}
 
+// TestInvalidSeries ensures that invalid series values cause a parse error
+// when specified in the charm metadata.
+func (s *MetaSuite) TestInvalidSeries(c *gc.C) {
 	for _, seriesName := range []string{"pre-c1se", "pre^cise", "cp/m", "OpenVMS"} {
-		_, err := charm.ReadMeta(strings.NewReader(noSeries + fmt.Sprintf("series: %s\n", seriesName)))
+		_, err := charm.ReadMeta(strings.NewReader(
+			fmt.Sprintf("%s\nseries: %s\n", dummyMetadata, seriesName)))
 		c.Assert(err, gc.NotNil)
 		c.Check(err, gc.ErrorMatches, `charm "a" declares invalid series: .*`)
 	}
