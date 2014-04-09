54d17a787c4072431b0ae5eed071476ef9002240
Roger Peppe <roger.peppe@canonical.com>
2014-04-09 22:06:22 +0100

    lose debugging remnant

--- replicaset/replicaset.go
+++ replicaset/replicaset.go
@@ -248,7 +248,6 @@ func MasterHostPort(session *mgo.Session) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	logger.Infof("MasterHostPort returned %#v", results)
 	return results.PrimaryAddress, nil
 }
 
