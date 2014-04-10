0f71d74665b9508ece7ec224053d1062433bb111
Nate Finch <nate.finch@canonical.com>
2014-04-10 11:25:58 -0400

    add initiate code to peergrouper worker

---
+++ worker/peergrouper/initiate_test.go
@@ -0,0 +1,48 @@
+package peergrouper_test
+
+import (
+	"testing"
+
+	gc "launchpad.net/gocheck"
+
+	coretesting "launchpad.net/juju-core/testing"
+	"launchpad.net/juju-core/testing/testbase"
+	"launchpad.net/juju-core/worker/peergrouper"
+)
+
+func Test(t *testing.T) { gc.TestingT(t) }
+
+type InitiateSuite struct {
+	testbase.LoggingSuite
+}
+
+var _ = gc.Suite(&InitiateSuite{})
+
+// TODO(natefinch) add a test that InitiateMongoServer works when
+// we support upgrading of existing environments.
+
+func (s *InitiateSuite) TestInitiateReplicaSet(c *gc.C) {
+	var err error
+	inst := &coretesting.MgoInstance{Params: []string{"--replSet", "juju"}}
+	err = inst.Start(true)
+	c.Assert(err, gc.IsNil)
+
+	info := inst.DialInfo()
+
+	err = peergrouper.MaybeInitiateMongoServer(peergrouper.InitiateMongoParams{
+		DialInfo:       info,
+		MemberHostPort: inst.Addr(),
+	})
+	c.Assert(err, gc.IsNil)
+
+	// This would return a mgo.QueryError if a ReplicaSet
+	// configuration already existed but we tried to created
+	// one with replicaset.Initiate again.
+	err = peergrouper.MaybeInitiateMongoServer(peergrouper.InitiateMongoParams{
+		DialInfo:       info,
+		MemberHostPort: inst.Addr(),
+	})
+	c.Assert(err, gc.IsNil)
+
+	// TODO test login
+}
