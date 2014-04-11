3835662be539bdcf1d11046989f07fd5bc2f213a
Curtis Hovey <curtis@canonical.com>
2014-04-11 17:02:17 -0400

    Increment juju to 1.18.2

--- version/version.go
+++ version/version.go
@@ -23,7 +23,7 @@ import (
 // The presence and format of this constant is very important.
 // The debian/rules build recipe uses this value for the version
 // number of the release package.
-const version = "1.18.1"
+const version = "1.18.2"
 
 // lsbReleaseFile is the name of the file that is read in order to determine
 // the release version of ubuntu.
