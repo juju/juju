1d7ca435c9619b85a4b9e88e90b166d90bd0d537
Aaron Bentley <aaron.bentley@canonical.com>
2014-04-14 13:28:50 -0400

    Isolate manual-provider differences.

--- deploy_stack.py
+++ deploy_stack.py
@@ -16,10 +16,15 @@ from jujupy import (
 )
 
 
-def deploy_stack(environment, charm_prefix, already_bootstrapped):
-    """"Deploy a Wordpress stack in the specified environment.
+def prepare_environment(environment, already_bootstrapped, machines):
+    """Prepare an environment for deployment.
 
-    :param environment: The name of the desired environment.
+    As well as bootstrapping, this ensures the correct agent version is in
+    use.
+
+    :param environment: The name of the environment to use.
+    :param already_bootstrapped: If true, the environment is already
+        bootstrapped.
     """
     if sys.platform == 'win32':
         # Ensure OpenSSH is never in the path for win tests.
@@ -37,10 +42,17 @@ def deploy_stack(environment, charm_prefix, already_bootstrapped):
     if agent_versions.keys() != [agent_version]:
         print("Current versions: %s" % ', '.join(agent_versions.keys()))
         env.juju('upgrade-juju', '--version', agent_version)
-    if sys.platform == 'win32':
-        # The win client tests only verify the client to the state-server.
-        return
     env.wait_for_version(env.get_matching_agent_version())
+    for machine in machines:
+        env.juju('add-machine', machine)
+    return env
+
+
+def deploy_stack(env, charm_prefix):
+    """"Deploy a Wordpress stack in the specified environment.
+
+    :param environment: The name of the desired environment.
+    """
     env.deploy(charm_prefix + 'wordpress')
     env.deploy(charm_prefix + 'mysql')
     env.juju('add-relation', 'mysql', 'wordpress')
@@ -49,31 +61,9 @@ def deploy_stack(environment, charm_prefix, already_bootstrapped):
     wp_unit_0 = status['services']['wordpress']['units']['wordpress/0']
     check_wordpress(wp_unit_0['public-address'])
 
-def deploy_dummy_stack(environment, charm_prefix, already_bootstrapped):
+def deploy_dummy_stack(env, charm_prefix):
     """"Deploy a dummy stack in the specified environment.
-
-    :param environment: The name of the desired environment.
     """
-    if sys.platform == 'win32':
-        # Ensure OpenSSH is never in the path for win tests.
-        sys.path = [p for p in sys.path if 'OpenSSH' not in p]
-    env = Environment.from_config(environment)
-    if not already_bootstrapped:
-        env.bootstrap()
-    agent_version = env.get_matching_agent_version()
-    status = env.get_status()
-    for ignored in until_timeout(30):
-        agent_versions = env.get_status().get_agent_versions()
-        if 'unknown' not in agent_versions and len(agent_versions) == 1:
-            break
-        status = env.get_status()
-    if agent_versions.keys() != [agent_version]:
-        print("Current versions: %s" % ', '.join(agent_versions.keys()))
-        env.juju('upgrade-juju', '--version', agent_version)
-    if sys.platform == 'win32':
-        # The win client tests only verify the client to the state-server.
-        return
-    env.wait_for_version(env.get_matching_agent_version())
     allowed_chars = string.ascii_uppercase + string.digits
     token=''.join(random.choice(allowed_chars) for n in range(20))
     env.deploy(charm_prefix + 'dummy-source')
@@ -96,17 +86,23 @@ def main():
     parser.add_argument('--already-bootstrapped',
                         help='The environment is already bootstrapped.',
                         action='store_true')
+    parser.add_argument('--machine',
+                        help='A machine to add to the environment.',
+                        action='append')
     parser.add_argument('--dummy', help='Use dummy charms.',
                         action='store_true')
     parser.add_argument('env', help='The environment to deploy on.')
     args = parser.parse_args()
     try:
+        env = prepare_environment(args.env, args.already_bootstrapped,
+                                  args.machine)
+        if sys.platform == 'win32':
+            # The win client tests only verify the client to the state-server.
+            return
         if args.dummy:
-            deploy_dummy_stack(args.env, args.charm_prefix,
-                               args.already_bootstrapped)
+            deploy_dummy_stack(env, args.charm_prefix)
         else:
-            deploy_stack(args.env, args.charm_prefix,
-                         args.already_bootstrapped)
+            deploy_stack(env, args.charm_prefix)
     except Exception as e:
         print('%s (%s)' % (e, type(e).__name__))
         sys.exit(1)
