9954681b06e00d91fa90af025d7df6b86da975fe
Dimiter Naydenov <dimiter.naydenov@canonical.com>
2014-04-17 16:00:10 +0300

    Fixes after merging

--- cmd/jujud/machine.go
+++ cmd/jujud/machine.go
@@ -441,7 +441,7 @@ func (a *MachineAgent) StateWorker() (worker.Worker, error) {
 	}
 
 	st, m, err := openState(agentConfig)
-	if errors.IsUnauthorizedError(err) {
+	if errors.IsUnauthorized(err) {
 		// TODO(axw) remove this when we no longer need
 		// to upgrade from pre-HA-capable environments.
 		logger.Debugf("failed to open state, reattempt after ensuring admin user exists: %v", err)
