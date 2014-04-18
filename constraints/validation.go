992e774f36f00490c682287b053d359bb848c068
Ian Booth <ian.booth@canonical.com>
2014-04-18 23:09:12 +1000

    Rework Validator interface

--- constraints/validation.go
+++ constraints/validation.go
@@ -6,7 +6,6 @@ package constraints
 import (
 	"fmt"
 	"reflect"
-	"strings"
 
 	"launchpad.net/juju-core/utils/set"
 )
@@ -15,6 +14,7 @@ import (
 // used to ensure a constraints value is valid, as well as being able
 // to handle overridden attributes.
 type Validator interface {
+
 	// RegisterConflicts is used to define cross-constraint override behaviour.
 	// The red and blue attribute lists contain attribute names which conflict
 	// with those in the other list.
@@ -23,38 +23,17 @@ type Validator interface {
 	//  when a constraints Value overrides another which specifies a conflicting
 	//   attribute, the attribute in the overridden Value is cleared.
 	RegisterConflicts(reds, blues []string)
+
 	// RegisterUnsupported records attributes which are not supported by a constraints Value.
 	RegisterUnsupported(unsupported []string)
-	// checkConflicts returns an error if the constraints Value contains conflicting attributes.
-	checkConflicts(cons Value) error
-	// checkUnsupported returns an error if the constraints Value contains unsupported attributes.
-	checkUnsupported(cons Value) error
-	// Validate returns an error if the given constraints are not valid.
-	Validate(cons Value) error
-	// Merge merges consB into consA, with any conflicting attributes from consB
-	// overriding those from consA.
-	Merge(consA, consB Value) (Value, error)
-}
 
-type notSupportedError struct {
-	what []string
-}
+	// Validate returns an error if the given constraints are not valid, and also
+	// any unsupported attributes.
+	Validate(cons Value) ([]string, error)
 
-// NewNotSupportedError returns an error signifying that
-// constraint attributes are not supported.
-func NewNotSupportedError(what []string) error {
-	return &notSupportedError{what: what}
-}
-
-func (e *notSupportedError) Error() string {
-	return fmt.Sprintf("unsupported constraints: %s", strings.Join(e.what, ","))
-}
-
-// IsNotSupportedError reports whether the error
-// was created with NewNotSupportedError.
-func IsNotSupportedError(err error) bool {
-	_, ok := err.(*notSupportedError)
-	return ok
+	// Merge merges cons into consFallback, with any conflicting attributes from cons
+	// overriding those from consFallback.
+	Merge(consFallback, cons Value) (Value, error)
 }
 
 // NewValidator returns a new constraints Validator instance.
@@ -84,7 +63,7 @@ func (v *validator) RegisterUnsupported(unsupported []string) {
 	v.unsupported = set.NewStrings(unsupported...)
 }
 
-// checkConflicts is defined on Validator.
+// checkConflicts returns an error if the constraints Value contains conflicting attributes.
 func (v *validator) checkConflicts(cons Value) error {
 	attrNames := cons.attributesWithValues()
 	attrSet := set.NewStrings(attrNames...)
@@ -102,13 +81,9 @@ func (v *validator) checkConflicts(cons Value) error {
 	return nil
 }
 
-// checkUnsupported is defined on Validator.
-func (v *validator) checkUnsupported(cons Value) error {
-	attrNames := cons.hasAny(v.unsupported.Values()...)
-	if len(attrNames) > 0 {
-		return NewNotSupportedError(attrNames)
-	}
-	return nil
+// checkUnsupported returns any unsupported attributes.
+func (v *validator) checkUnsupported(cons Value) []string {
+	return cons.hasAny(v.unsupported.Values()...)
 }
 
 // withFallbacks returns a copy of v with nil values taken from vFallback.
@@ -125,40 +100,35 @@ func withFallbacks(v Value, vFallback Value) Value {
 }
 
 // Validate is defined on Validator.
-func (v *validator) Validate(cons Value) error {
-	// Conflicts are checked first because such errors are fatal.
+func (v *validator) Validate(cons Value) ([]string, error) {
+	unsupported := v.checkUnsupported(cons)
 	if err := v.checkConflicts(cons); err != nil {
-		return err
+		return unsupported, err
 	}
-	// Lastly, check for unsupported attributes.
-	// Callers might choose to ignore such errors since the
-	// constraints value is usable if the unsupported attributes
-	// are simply ignored.
-	return v.checkUnsupported(cons)
+	return unsupported, nil
 }
 
 // Merge is defined on Validator.
-func (v *validator) Merge(consA, consB Value) (Value, error) {
+func (v *validator) Merge(consFallback, cons Value) (Value, error) {
 	// First ensure both constraints are valid. We don't care if there
-	// are constraint attributes that are unsupported - the caller can
-	// either ignore or error as required.
-	if err := v.Validate(consA); err != nil && !IsNotSupportedError(err) {
+	// are constraint attributes that are unsupported.
+	if _, err := v.Validate(consFallback); err != nil {
 		return Value{}, err
 	}
-	if err := v.Validate(consB); err != nil && !IsNotSupportedError(err) {
+	if _, err := v.Validate(cons); err != nil {
 		return Value{}, err
 	}
-	// Gather any attributes from consA which conflict with those on consB.
-	bAttrs := consB.attributesWithValues()
-	var aConflicts []string
-	for _, bAttr := range bAttrs {
-		aConflicts = append(aConflicts, v.conflicts[bAttr].Values()...)
+	// Gather any attributes from consFallback which conflict with those on cons.
+	attrs := cons.attributesWithValues()
+	var fallbackConflicts []string
+	for _, attr := range attrs {
+		fallbackConflicts = append(fallbackConflicts, v.conflicts[attr].Values()...)
 	}
-	// Null out the conflicting consA attribute values because
-	// consB takes priority. We can't error here because we
+	// Null out the conflicting consFallback attribute values because
+	// cons takes priority. We can't error here because we
 	// know that aConflicts contains valid attr names.
-	consAMinusConflicts, _ := consA.without(aConflicts...)
-	// The result is consB with fallbacks coming from any
-	// non conflicting consA attributes.
-	return withFallbacks(consB, consAMinusConflicts), nil
+	consFallbackMinusConflicts, _ := consFallback.without(fallbackConflicts...)
+	// The result is cons with fallbacks coming from any
+	// non conflicting consFallback attributes.
+	return withFallbacks(cons, consFallbackMinusConflicts), nil
 }
