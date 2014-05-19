e83de4b320e4c14b8c1680ad4e0adb5943aafa6e
John Weldon <johnweldon4@gmail.com>
2014-05-19 14:27:24 -0700

    switch from SettingsSuite to ConnSuite. redo dropped changes from botched merge

--- state/action_test.go
+++ state/action_test.go
@@ -1,36 +1,20 @@
-package state
+package state_test
 
 import (
 	gc "launchpad.net/gocheck"
 )
 
 type ActionSuite struct {
-	SettingsSuite
+	ConnSuite
 }
 
 var _ = gc.Suite(&ActionSuite{})
 
-func (s *ActionSuite) SetUpSuite(c *gc.C) {
-	s.SettingsSuite.SetUpSuite(c)
-}
-
-func (s *ActionSuite) TearDownSuite(c *gc.C) {
-	s.SettingsSuite.TearDownSuite(c)
-}
-
-func (s *ActionSuite) SetUpTest(c *gc.C) {
-	s.SettingsSuite.SetUpTest(c)
-}
-
-func (s *ActionSuite) TearDownTest(c *gc.C) {
-	s.SettingsSuite.TearDownTest(c)
-}
-
 func (s *ActionSuite) TokenTest(c *gc.C) {
-	action, err := s.SettingsSuite.state.AddAction("fakeunit/0", "fakeaction", nil)
+	action, err := s.ConnSuite.State.AddAction("fakeunit/0", "fakeaction", nil)
 	c.Assert(err, gc.IsNil)
 
-	action2, err := s.SettingsSuite.state.Action(action.Id())
+	action2, err := s.ConnSuite.State.Action(action.Id())
 	c.Assert(err, gc.IsNil)
 	c.Assert(action.Name(), gc.Equals, action2.Name())
 }
