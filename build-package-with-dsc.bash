d1cd385f441ed44b9b740fbf25e8c068b439157f
Curtis Hovey <curtis@canonical.com>
2014-05-20 19:16:00 -0400

    Organise the steps as functions.

---
+++ build-package-with-dsc.bash
@@ -0,0 +1,184 @@
+#!/bin/bash
+
+# IMPORTANT: The ssh options are always remapped to $@ which preserves
+# their tokenisation.
+
+
+usage() {
+    echo "usage: $0 [<instance-type> <ami> | <[user@]host> '<ssh-optons>'] dsc"
+    echo "  Build binary packages on an ec2 instance or a remote host."
+    echo ""
+    echo "  instance-type: the ec2 instance type, like m1.large."
+    echo "  ami: The ec2 image, it defines the series and arch to build."
+    echo "  user@host: host to build on. 'user@' is optional."
+    echo "      Use localhost to build locally."
+    echo "  ssh-option: a string of all the options to establish a connection."
+    echo "      Like '-o \"ProxyCommand ssh user@10.10.10.10 nc -q0 %h %p\"'."
+    echo "  dsc: The path the the dsc file."
+}
+
+
+create_instance() {
+    echo "Creating an instance to build juju on."
+    if [[ -z $AWS_SECRET_KEY ]]
+    then
+        echo "Source your ec2 credentials to create an instance."
+        exit 1
+    fi
+    export JOB_NAME=${JOB_NAME:-$USER-build-package}
+    INSTANCE_ID=$($SCRIPTS/ec2-run-instance-get-id)
+    $SCRIPTS/ec2-tag-job-instances $INSTANCE_ID
+    set +x
+    echo Starting instance $INSTANCE_ID
+    INSTANCE_NAME=$($SCRIPTS/ec2-get-name $INSTANCE_ID)
+    echo Instance has ip $INSTANCE_NAME
+    sleep 30
+    $SCRIPTS/wait-for-port $INSTANCE_NAME 22
+    # Pause, do not start until cloud-init has updated the apt sources.
+    ssh "$@" $REMOTE_USER@$INSTANCE_NAME  <<EOT
+    set -eux
+    for attempt in \$(seq 10); do
+        if grep -r '^deb .* universe$' /etc/apt/sources.list > /dev/null
+        then
+            break
+        elif [ "\$attempt" == "10" ]; then
+            echo "Universe is not available to install packages from."
+            exit 1
+        fi
+        sleep 10m
+    done
+EOT
+}
+
+
+install_build_deps() {
+    echo "Installing build deps."
+    remote_compliler=$(ssh "$@" $REMOTE_USER@$INSTANCE_NAME  <<EOT
+        if [[ \$(uname -p) =~  .*86|armel|armhf.* ]]; then
+            echo "golang"
+        else
+            echo "gccgo-4.9 gccgo-go"
+        fi
+EOT
+    )
+    juju_compliler=$(echo "$remote_compliler" | tail -1)
+    DEPS="build-essential fakeroot dpkg-dev debhelper $juju_compliler"
+
+    DEP_SCRIPT=$(cat <<EOT
+        sudo apt-add-repository -y ppa:juju/golang;
+        sudo apt-get update;
+        sudo apt-get install -y $DEPS;
+EOT
+    )
+
+    if [[ $INSTANCE_NAME = "localhost" ]]; then
+        eval "$DEP_SCRIPT"
+    else
+        ssh "$@" $REMOTE_USER@$INSTANCE_NAME  <<EOT
+        eval "$DEP_SCRIPT"
+EOT
+    fi
+}
+
+
+upload_source_package_files() {
+    echo "Uploading source package files."
+    source_dir=$(dirname $DSC)
+    source_file_data=$(
+        sed -n '/^Checksums-Sha256/,/^Files/!d; /^ /!d; s,^ ,,; s, ,:,gp' $DSC)
+    source_files="$DSC"
+    echo "Found these files in $source_dir to upload:"
+    for data in $source_file_data
+    do
+        file_sha256=$(echo "$data" | cut -d : -f 1)
+        file_size=$(echo "$data" | cut -d : -f 2)
+        file_name=$(echo "$data" | cut -d : -f 3)
+        echo "$file_name"
+        source_files="$source_files $source_dir/$file_name"
+    done
+    ssh "$@" $REMOTE_USER@$INSTANCE_NAME <<EOT
+        test -d $THERE/juju-build/ || mkdir $THERE/juju-build
+EOT
+    scp "$@" $source_files $REMOTE_USER@$INSTANCE_NAME:$THERE/juju-build/
+}
+
+
+build_binary_packages() {
+    echo "Building binary packages"
+    juju_version=$(basename $DSC .dsc)
+    version=$(echo $juju_version | cut -d _ -f2 | cut -d - -f 1)
+    ssh "$@" $REMOTE_USER@$INSTANCE_NAME <<EOT
+        set -eux
+        cd $THERE/juju-build/
+        dpkg-source -x $juju_version.dsc
+        cd juju-core-$version
+        dpkg-buildpackage -us -uc
+EOT
+}
+
+
+retrieve_binary_packages() {
+    echo "Retrieving built packages."
+    scp "$@" $REMOTE_USER@$INSTANCE_NAME:$THERE/juju-build/*.deb ./
+    EXIT_STATUS=$?
+}
+
+
+cleanup() {
+    echo "Cleaning up remote"
+    if [[ $IS_EPHEMERAL == "true" ]]; then
+        $SCRIPTS/ec2-terminate-job-instances
+    else
+        ssh "$@" $REMOTE_USER@$INSTANCE_NAME  "rm -r $THERE/juju-build"
+    fi
+}
+
+
+if [[ $1 = "--help" ]]; then
+    set +x
+    usage
+    exit
+fi
+
+
+# Do not set -e because this script must cleanup its resources.
+set -ux
+ssh_options='-o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null"'
+EXIT_STATUS=1
+SCRIPTS=${SCRIPTS:-$(dirname $(dirname $0))/juju-ci-tools}
+DSC=$(readlink -f $3)
+
+if [[ $1 = "localhost" ]]; then
+    # Use the local user and working directory to build juju.
+    IS_EPHEMERAL="false"
+    THERE=$(pwd)
+    REMOTE_USER=$USER
+    INSTANCE_NAME=$1
+    eval "set -- $ssh_options"
+elif [[ $1 =~ .*@.* ]]; then
+    # Use the connection information to build juju remotely in ~/juju-build.
+    THERE="~"
+    IS_EPHEMERAL="false"
+    REMOTE_USER=$(echo $1 | cut -d @ -f1)
+    INSTANCE_NAME=$(echo $1 | cut -d @ -f2)
+    eval "set -- $ssh_options $2"
+else
+    # Create an instance to build juju remotely in ~/juju-build.
+    THERE="~"
+    IS_EPHEMERAL="true"
+    REMOTE_USER="ubuntu"
+    export INSTANCE_TYPE=$1
+    export AMI_IMAGE=$2
+    eval "set -- $ssh_options"
+fi
+
+
+if [[ $IS_EPHEMERAL == "true" ]]; then
+    create_instance
+fi
+install_build_deps
+upload_source_package_files
+build_binary_packages
+retrieve_binary_packages
+cleanup
+exit $EXIT_STATUS
