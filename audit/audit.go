afae32a93d73d4de79f8b80d5567737e321c249a
Dave Cheney <david.cheney@canonical.com>
2014-05-20 13:42:30 +1000

    wip

--- audit/audit.go
+++ audit/audit.go
@@ -1,3 +1,6 @@
+// Copyright 2013, 2014 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
 // Package audit records auditable events
 package audit
 
@@ -9,12 +12,12 @@ import (
 	"launchpad.net/juju-core/state"
 )
 
-var auditLogger = loggo.GetLogger("audit")
+var logger = loggo.GetLogger("juju.audit")
 
 // Audit records an auditable event against the user who performed the action
 func Audit(user *state.User, format string, args ...interface{}) {
 	if user == nil {
 		panic("user cannot be nil")
 	}
-	auditLogger.Infof(fmt.Sprintf("user %q: %s", user, format), args...)
+	logger.Logf(loggo.INFO, fmt.Sprintf("user %q: %s", user.Name(), format), args...)
 }
--- audit/audit_test.go
+++ audit/audit_test.go
@@ -1,7 +1,11 @@
+// Copyright 2013, 2014 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
 package audit
 
 import (
 	"testing"
+	"time"
 
 	"github.com/juju/loggo"
 
@@ -20,9 +24,51 @@ var _ = gc.Suite(&auditSuite{})
 
 func (*auditSuite) SetUpTest(c *gc.C) {
 	loggo.ResetLoggers()
+	loggo.ResetWriters()
+	err := loggo.ConfigureLoggers(`<root>=ERROR; juju.audit=INFO`)
+	c.Assert(err, gc.IsNil)
 }
 
-func TestAudit(t *testing.T) {
+// testLogger implements loggo.Writer.
+// The struct members represent the contents of the last line written.
+type testLogger struct {
+	loggo.Level
+	Name      string
+	Filename  string
+	Line      int
+	Timestamp time.Time
+	Message   string
+}
+
+// Write implements loggo.Writer. Each invocation will replace the conents of
+// the fields inside this testLogger implementation with the values logged.
+func (l *testLogger) Write(level loggo.Level, name, filename string, line int, timestamp time.Time, message string) {
+	l.Level = level
+	l.Name = name
+	l.Filename = filename
+	l.Line = line
+	l.Timestamp = timestamp
+	l.Message = message
+}
+
+func (*auditSuite) TestAuditEventWrittenToAuditLogger(c *gc.C) {
+	var w testLogger
+	loggo.ReplaceDefaultWriter(&w)
+
+	// create a new User instance, normally this would come from the state
+	// but as there is no User mock, just create an empty done.
 	var u state.User
 	Audit(&u, "donut eaten, %v donut(s) remain", 7)
+
+	c.Check(w.Level, gc.Equals, loggo.INFO)
+	c.Check(w.Name, gc.Equals, "juju.audit")
+	c.Check(w.Filename, gc.Matches, ".*audit_test.go$") // TODO(dfc) this should be the function which called audit.Audit
+	c.Check(w.Line, gc.Not(gc.Equals), 0)
+	c.Check(w.Timestamp.IsZero(), gc.Equals, false)
+	c.Check(w.Message, gc.Equals, `user "": donut eaten, 7 donut(s) remain`)
+}
+
+func (*auditSuite) TestAuditWithNilUserWillPanic(c *gc.C) {
+	f := func() { Audit(nil, "should never be written") }
+	c.Assert(f, gc.PanicMatches, "user cannot be nil")
 }
