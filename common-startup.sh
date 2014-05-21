c1d0601e83b0c91cd4c342233484427c67ff6010
Curtis Hovey <curtis@canonical.com>
2014-05-21 00:27:49 -0400

    Alway get the juju-core and juju-local package for the series and arch under test.

--- common-startup.sh
+++ common-startup.sh
@@ -1,34 +1,45 @@
 set -eu
 : ${SCRIPTS=$(readlink -f $(dirname $0))}
 export PATH="$SCRIPTS:$PATH"
+
 # For most jobs, this is localhost, so provide it.
 : ${LOCAL_JENKINS_URL=http://localhost:8080}
 export JUJU_HOME=$HOME/cloud-city
 if [ "$ENV" = "manual" ]; then
   source $HOME/cloud-city/ec2rc
 fi
-# Do the deployment for upgrade testing.
 : ${JUJU_REPOSITORY=$HOME/repository}
+export JUJU_REPOSITORY
 
+# Setup workspace and build.
 artifacts_path=$WORKSPACE/artifacts
 export MACHINES=""
 set -x
 rm $WORKSPACE/* -rf
 mkdir -p $artifacts_path
 touch $artifacts_path/empty
+
+# Determine BRANCH, REVNO, and VERSION
 afact='lastSuccessfulBuild/artifact'
-wget -q $LOCAL_JENKINS_URL/job/publish-revision/$afact/new-precise.deb
-# Determine BRANCH and REVNO
 wget -q $LOCAL_JENKINS_URL/job/build-revision/$afact/buildvars.bash
 source buildvars.bash
 echo "Testing $BRANCH $REVNO on $ENV"
-dpkg-deb -x $WORKSPACE/new-precise.deb extracted-bin
+
+# Provide the juju-core and juju-local packages to the test
+RELEASE=$(lsb_release -sr)
+ARCH=$(dpkg --print-architecture)
+juju_local_deb="juju-local_$VERSION-0ubuntu1~$RELEASE.1~juju1_all.deb"
+juju_core_deb="juju-core_$VERSION-0ubuntu1~$RELEASE.1~juju1_$ARCH.deb"
+wget -q $LOCAL_JENKINS_URL/job/publish-revision/$afact/$juju_local_deb
+wget -q $LOCAL_JENKINS_URL/job/publish-revision/$afact/$juju_core_deb
+dpkg-deb -x $WORKSPACE/$juju_core_deb extracted-bin
 export NEW_JUJU_BIN=$(readlink -f $(dirname $(find extracted-bin -name juju)))
+
+# Tear down any resources and data last from a previous test.
 if [ "$ENV" == "manual" ]; then
   ec2-terminate-job-instances
 else
   destroy-environment $ENV
 fi
-export JUJU_REPOSITORY
 jenv=$JUJU_HOME/environments/$ENV.jenv
 if [ -e $jenv ]; then rm $jenv; fi
