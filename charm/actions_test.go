4dbc48342835ceda936784fd240c160377987838
Bodie Solomon <binary132@gmail.com>
2014-05-21 17:07:18 -0400

    All tests are green, ready for MR.

--- charm/actions_test.go
+++ charm/actions_test.go
@@ -21,6 +21,7 @@ func (s *ActionsSuite) TestReadGoodActionsYaml(c *gc.C) {
 		yaml        string
 		actions     *charm.Actions
 	}{
+		// Test 1
 		{
 			"A simple snapshot actions YAML with one parameter.",
 			// YAML
@@ -43,13 +44,79 @@ actionspecs:
 							"description": "The file to write out to.",
 							"type":        "string",
 							"default":     "foo.bz2"}}}}}},
+		// Test 2
+		{
+			"A more complex schema with hyphenated names and multiple parameters.",
+			`
+actionspecs:
+  snapshot:
+    description: Take a snapshot of the database.
+    params:
+      outfile:
+        description: The file to write out to.
+        type: string
+        default: foo.bz2
+      compression-quality:
+        description: The compression quality.
+        type: number
+        minimum: 0
+        maximum: 9
+        exclusiveMaximum: false
+  remote-sync:
+    description: Sync a file to a remote host.
+    params:
+      file:
+        description: The file to send out.
+        type: string
+        format: uri
+        optional: false
+      remote-uri:
+        description: The host to sync to.
+        type: string
+        format: uri
+        optional: false
+      util:
+        description: The util to perform the sync (rsync or scp.)
+        type: string
+        enum: [rsync, scp]
+        default: rsync
+`,
+			// Actions
+			&charm.Actions{map[string]charm.ActionSpec{
+				"snapshot": charm.ActionSpec{
+					Description: "Take a snapshot of the database.",
+					Params: map[string]interface{}{
+						"outfile": map[interface{}]interface{}{
+							"description": "The file to write out to.",
+							"type":        "string",
+							"default":     "foo.bz2"},
+						"compression-quality": map[interface{}]interface{}{
+							"description":      "The compression quality.",
+							"type":             "number",
+							"minimum":          0,
+							"maximum":          9,
+							"exclusiveMaximum": false}}},
+				"remote-sync": charm.ActionSpec{
+					Description: "Sync a file to a remote host.",
+					Params: map[string]interface{}{
+						"file": map[interface{}]interface{}{
+							"description": "The file to send out.",
+							"type":        "string",
+							"format":      "uri",
+							"optional":    false},
+						"remote-uri": map[interface{}]interface{}{
+							"description": "The host to sync to.",
+							"type":        "string",
+							"format":      "uri",
+							"optional":    false},
+						"util": map[interface{}]interface{}{
+							"description": "The util to perform the sync (rsync or scp.)",
+							"type":        "string",
+							"enum":        []interface{}{"rsync", "scp"},
+							"default":     "rsync"}}}}},
+		},
 
-		//{
-		//	"A more complex schema with hyphenated names and multiple parameters.",
-		//	"placeholder",
-		//	nil,
-		//},
-
+		// Test 3
 		{
 			"A schema with an empty \"params\" key, implying no options.",
 			// YAML
@@ -66,7 +133,7 @@ actionspecs:
 					Params:      map[string]interface{}(nil)}}},
 		}}
 
-	// Beginning of actual test
+	// Beginning of testing loop
 	for i, test := range goodActionsYamlTests {
 		c.Logf("test %d: %s", i, test.description)
 		reader := bytes.NewReader([]byte(test.yaml))
@@ -83,6 +150,7 @@ func (s *ActionsSuite) TestReadBadActionsYaml(c *gc.C) {
 		yaml        string
 		actions     *charm.Actions
 	}{
+		// Test 1
 		{
 			"Malformed YAML: missing key in \"outfile\".",
 			// YAML
@@ -99,6 +167,8 @@ actionspecs:
 			// Actions
 			nil,
 		},
+
+		// Test 2
 		{
 			"Malformed JSON-Schema: $schema element misplaced.",
 			// YAML
@@ -116,6 +186,8 @@ actionspecs:
 			// Actions
 			nil,
 		},
+
+		// Test 3
 		{
 			"Malformed Actions: hyphen at beginning of action name.",
 			// YAML
@@ -132,6 +204,8 @@ actionspecs:
 			// Actions
 			nil,
 		},
+
+		// Test 4
 		{
 			"Malformed Actions: hyphen after action name.",
 			// YAML
