4bfa842b08c925fa05ed7efb81932afd1a32b862
John Weldon <johnweldon4@gmail.com>
2014-05-21 22:36:33 -0700

    Comment exported members of actionDoc, and revert commenting out of code per misunderstanding

--- state/action.go
+++ state/action.go
@@ -4,8 +4,13 @@ import ()
 
 type actionDoc struct {
 	Id string `bson:"_id"`
-	//Name    string
-	//Payload map[string]interface{}
+	// Name identifies which action this is.  Actions are defined by charms so
+	// this is necessarily a little vague, but this will tell the Unit which
+	// action to actually perform
+	Name string
+	// Payload is the generic placeholder for parameters passed in to be used
+	// when this action is being performed.
+	Payload map[string]interface{}
 }
 
 // Action represents an instruction to do some "action" and is expected to match
@@ -23,9 +28,9 @@ func newAction(st *State, adoc actionDoc) *Action {
 }
 
 // Name returns the name of the Action
-//func (a *Action) Name() string {
-//	return a.doc.Name
-//}
+func (a *Action) Name() string {
+	return a.doc.Name
+}
 
 // Id returns the id of the Action
 func (a *Action) Id() string {
@@ -35,6 +40,6 @@ func (a *Action) Id() string {
 // Payload will contain a structure representing arguments or parameters to
 // an action, and is expected to be validated by the Unit using the Charm
 // definition of the Action
-//func (a *Action) Payload() map[string]interface{} {
-//	return a.doc.Payload
-//}
+func (a *Action) Payload() map[string]interface{} {
+	return a.doc.Payload
+}
