fc58009710b8eb30539ce9b93a7044fc54993ffc
Francesco Banconi <francesco.banconi@canonical.com>
2014-05-29 12:29:16 +0200

    Add package files.

---
+++ utils/syslog/package_test.go
@@ -0,0 +1,14 @@
+// Copyright 2014 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package syslog_test
+
+import (
+	"testing"
+
+	gc "launchpad.net/gocheck"
+)
+
+func TestPackage(t *testing.T) {
+	gc.TestingT(t)
+}
