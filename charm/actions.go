1ef5e5e205626d3b69307d79ff6b24f55b1d8a6f
Bodie Solomon <binary132@gmail.com>
2014-06-05 11:15:31 -0400

    Added better test cases for map[interface{}] cleaner.

--- charm/actions.go
+++ charm/actions.go
@@ -51,9 +51,16 @@ func ReadActionsYaml(r io.Reader) (*Actions, error) {
 			return nil, fmt.Errorf("bad action name %s", name)
 		}
 
+		// Make sure the names of parameters are acceptable.
+		for paramName, _ := range unmarshaledActions.ActionSpecs[name].Params {
+			if valid := paramNameRule.MatchString(paramName); !valid {
+				return nil, fmt.Errorf("bad param name %s", paramName)
+			}
+		}
+
 		// Clean any map[interface{}]interface{}s out so they don't
 		// cause problems with BSON serialization later.
-		cleanedParams, err := stripBadInterfaces(actionSpec.Params)
+		cleanedParams, err := cleanseStringMap(actionSpec.Params)
 		if err != nil {
 			return nil, fmt.Errorf("invalid params schema for action schema %s: %v", name, err)
 		}
@@ -70,15 +77,59 @@ func ReadActionsYaml(r io.Reader) (*Actions, error) {
 			return nil, fmt.Errorf("invalid params schema for action schema %s: %v", name, err)
 		}
 
-		// Make sure the names of parameters are acceptable.
-		for paramName, _ := range unmarshaledActions.ActionSpecs[name].Params {
-			if valid := paramNameRule.MatchString(paramName); !valid {
-				return nil, fmt.Errorf("bad param name %s", paramName)
-			}
-		}
-
 	}
 	return &unmarshaledActions, nil
+}
+
+func cleanseMap(input map[interface{}]interface{}) (map[string]interface{}, error) {
+	output := make(map[string]interface{})
+
+	for inKey, inValue := range input {
+		typedKey, ok := inKey.(string)
+		if !ok {
+			return nil, errors.New("map keyed with non-string value")
+		}
+		output[typedKey] = inValue
+	}
+
+	return cleanseStringMap(output)
+}
+
+func cleanseSlice(input []interface{}) ([]interface{}, error) {
+	output := make([]interface{}, 0)
+
+	for _, sliceVal := range input {
+		switch typedSliceVal := sliceVal.(type) {
+
+		case map[string]interface{}:
+			newSliceVal, err := cleanseStringMap(typedSliceVal)
+			if err != nil {
+				return nil, err
+			}
+			output = append(output, newSliceVal)
+
+		// Rebuild inner map[interface{}]interface{}s
+		// using recursive technique.
+		case map[interface{}]interface{}:
+			newSliceVal, err := cleanseMap(typedSliceVal)
+			if err != nil {
+				return nil, err
+			}
+			output = append(output, newSliceVal)
+
+		case []interface{}:
+			newSliceVal, err := cleanseSlice(typedSliceVal)
+			if err != nil {
+				return nil, err
+			}
+			output = append(output, newSliceVal)
+
+		// Otherwise, just use the same value
+		default:
+			output = append(output, sliceVal)
+		}
+	}
+	return output, nil
 }
 
 // stripBadInterfaces recurses through the values of a map[string]interface{}
@@ -86,16 +137,15 @@ func ReadActionsYaml(r io.Reader) (*Actions, error) {
 // original are rebuilt as map[string]interface{}s.  If any inner maps have
 // keys which are not of type string, the function will fail with an error.
 // This function does not mutate the original map.
-func stripBadInterfaces(input map[string]interface{}) (map[string]interface{}, error) {
+func cleanseStringMap(input map[string]interface{}) (map[string]interface{}, error) {
 	output := make(map[string]interface{})
 
 	for key, val := range input {
 		switch typedVal := val.(type) {
-
 		// If the value is already a map[string]interface{}, recurse
 		// into it and return the conformed version or error.
 		case map[string]interface{}:
-			newValue, err := stripBadInterfaces(typedVal)
+			newValue, err := cleanseStringMap(typedVal)
 			if err != nil {
 				return nil, err
 			}
@@ -106,78 +156,24 @@ func stripBadInterfaces(input map[string]interface{}) (map[string]interface{}, e
 		// using the typed keys, and same values.  Then recurse on
 		// the new map, returning the conformed version or error.
 		case map[interface{}]interface{}:
-			newInnerMap := make(map[string]interface{})
-			for innerKey, innerValue := range typedVal {
-				switch typedInnerKey := innerKey.(type) {
-				case string:
-					newInnerMap[typedInnerKey] = innerValue
-				default:
-					return nil, errors.New("map keyed with non-string value")
-				}
-			}
-
-			// recurse into the new map and clean it, too;
-			// substitute the new map with the cleaned new map.
-			// add the new item to the new map.
-			newInnerMap, err := stripBadInterfaces(newInnerMap)
+			cleansedVal, err := cleanseMap(typedVal)
 			if err != nil {
-				return nil, err
+				output[key] = cleansedVal
 			}
-			output[key] = newInnerMap
 
 		// If the value is an interface{} slice, step through it and
 		// recursively rebuild any map[interface{}]interface{}s in it.
 		case []interface{}:
-			newSlice := make([]interface{}, 0)
-			for _, sliceVal := range typedVal {
-				switch typedSliceVal := sliceVal.(type) {
-
-				// Make sure inner maps are clean.
-				case map[string]interface{}:
-					newSliceVal, err := stripBadInterfaces(typedSliceVal)
-					if err != nil {
-						return nil, err
-					}
-					newSlice = append(newSlice, newSliceVal)
-
-				// Rebuild inner map[interface{}]interface{}s
-				// using recursive technique.
-				case map[interface{}]interface{}:
-					newInnerMap := make(map[string]interface{})
-
-					// typedSliceVal is a map[interface{}]interface{}
-					for innerKey, innerValue := range typedSliceVal {
-						switch typedInnerKey := innerKey.(type) {
-						case string:
-							newInnerMap[typedInnerKey] = innerValue
-						default:
-							return nil, errors.New("slice's inner map keyed with non-string value")
-						}
-					}
-
-					// recurse into the new map and clean it, too;
-					// substitute the new map with the cleaned new map
-					newInnerMap, err := stripBadInterfaces(newInnerMap)
-					if err != nil {
-						return nil, err
-					}
-					newSlice = append(newSlice, newInnerMap)
-
-				// Otherwise, just use the same value
-				default:
-					newSlice = append(newSlice, sliceVal)
-				}
+			cleansedVal, err := cleanseSlice(typedVal)
+			if err != nil {
+				output[key] = cleansedVal
 			}
 
-			// Now that all items in the slice have been resolved,
-			// return the newly built copy.
-			output[key] = newSlice
-
-		// If the item wasn't a map or a list, we can simply
-		// add it and move on.
+		// If the value is something else, it's fine to use.
 		default:
 			output[key] = val
 		}
 	}
+
 	return output, nil
 }
