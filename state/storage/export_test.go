e5c9b93f6ebb8b56a7f89486e8932392d81e16e7
Ian Booth <ian.booth@canonical.com>
2014-06-06 16:03:59 +1000

    Update to use new base txn struct

---
+++ state/storage/export_test.go
@@ -0,0 +1,9 @@
+// Copyright 2014 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package storage
+
+var (
+	NewResourceCatalog = newResourceCatalog
+	NewResource        = newResource
+)
