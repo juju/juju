3c6f24f6458ea68da7bb9f9a92dfb5f797be394b
Curtis Hovey <curtis@canonical.com>
2014-06-20 15:23:42 -0400

    Remove the token from the previous test.

--- remove-manual-juju.bash
+++ remove-manual-juju.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 JUJU_DIR="/var/lib/juju"
+DUMMY_DIR="/var/run/dummy-sink"
 ips="$@"
 for ip in $ips; do
     ssh -i $JUJU_HOME/staging-juju-rsa ubuntu@$ip <<EOT
@@ -11,6 +12,9 @@ if [[ -n "$(ps ax | grep jujud | grep -v grep)" ]]; then
 fi
 if [[ -d $JUJU_DIR ]]; then
     sudo rm -r $JUJU_DIR
+fi
+if [[ -d $DUMMY_DIR ]]; then
+    sudo rm -r $DUMMY_DIR
 fi
 sudo find /etc/init -name 'juju*' -delete
 EOT
