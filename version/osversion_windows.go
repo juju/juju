0c8625ddea4f3ab7118eaacb83c53da8799c4251
Dave Cheney <dave@cheney.net>
2014-06-30 14:02:56 +1000

    Fix windows version

--- version/osversion_windows.go
+++ version/osversion_windows.go
@@ -4,34 +4,27 @@
 package version
 
 import (
-	"fmt"
-	"regexp"
 	"strings"
 
+	"github.com/juju/errors"
 	"github.com/juju/utils/exec"
 )
 
-func osVersion() string {
-	return getWinVersion()
-}
-
-func getWinVersion() string {
+func osVersion() (string, error) {
 	var com exec.RunParams
 	com.Commands = `(gwmi Win32_OperatingSystem).Name.Split('|')[0]`
-	out, _ := exec.RunCommands(com)
-	if out.Code != 0 {
-		return "unknown"
+	out, err := exec.RunCommands(com)
+	if err != nil || out.Code != 0 {
+		return "unknown", err
 	}
-	serie := strings.TrimSpace(string(out.Stdout))
-	if val, ok := windowsVersions[serie]; ok {
-		return val
+	series := strings.TrimSpace(string(out.Stdout))
+	if val, ok := windowsVersions[series]; ok {
+		return val, err
 	}
 	for key, value := range windowsVersions {
-		reg := regexp.MustCompile(fmt.Sprintf("^%s", key))
-		match := reg.MatchString(serie)
-		if match {
-			return value
+		if strings.HasPrefix(series, key) {
+			return value, nil
 		}
 	}
-	return "unknown"
+	return "unknown", errors.Errorf("unknown series %q", series)
 }
