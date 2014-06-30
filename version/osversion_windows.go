c1a4fd382b4362f785fd955da0684fc73555a760
Dave Cheney <dave@cheney.net>
2014-06-30 11:35:44 +1000

    move windows specific code to windows file

--- version/osversion_windows.go
+++ version/osversion_windows.go
@@ -3,6 +3,35 @@
 
 package version
 
+import (
+	"fmt"
+	"regexp"
+	"strings"
+
+	"github.com/juju/utils/exec"
+)
+
 func osVersion() string {
 	return getWinVersion()
+}
+
+func getWinVersion() string {
+	var com exec.RunParams
+	com.Commands = `(gwmi Win32_OperatingSystem).Name.Split('|')[0]`
+	out, _ := exec.RunCommands(com)
+	if out.Code != 0 {
+		return "unknown"
+	}
+	serie := strings.TrimSpace(string(out.Stdout))
+	if val, ok := windowsVersions[serie]; ok {
+		return val
+	}
+	for key, value := range windowsVersions {
+		reg := regexp.MustCompile(fmt.Sprintf("^%s", key))
+		match := reg.MatchString(serie)
+		if match {
+			return value
+		}
+	}
+	return "unknown"
 }
