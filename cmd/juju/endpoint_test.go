919e650ad1efa06ecc3ef201d5139e59ae3cb208
Frank Mueller <frank.mueller@canonical.com>
2014-07-01 20:20:13 +0200

    Added two more tests for multiple and no endpoints.

--- cmd/juju/endpoint_test.go
+++ cmd/juju/endpoint_test.go
@@ -28,3 +28,49 @@ func (s *EndpointSuite) TestEndpoint(c *gc.C) {
 	info := s.APIInfo(c)
 	c.Assert(output, gc.Equals, fmt.Sprintf("%s\n", info.Addrs[0]))
 }
+
+func (s *EndpointSuite) TestMultipleEndpoints(c *gc.C) {
+	// Run command once to create store in test.
+	_, err := coretesting.RunCommand(c, envcmd.Wrap(&EndpointCommand{}))
+	c.Assert(err, gc.IsNil)
+	s.modifyAddresses(c, []string{"10.0.0.1:17070", "10.0.0.2:17070"})
+	ctx, err := coretesting.RunCommand(c, envcmd.Wrap(&EndpointCommand{}))
+	c.Assert(err, gc.IsNil)
+	c.Assert(ctx.Stderr.(*bytes.Buffer).String(), gc.Equals, "")
+	output := string(ctx.Stdout.(*bytes.Buffer).Bytes())
+	info := s.APIInfo(c)
+	c.Assert(output, gc.Equals, fmt.Sprintf("%s\n", info.Addrs[0]))
+}
+
+func (s *EndpointSuite) TestNoEndpoints(c *gc.C) {
+	// Run command once to create store in test.
+	_, err := coretesting.RunCommand(c, envcmd.Wrap(&EndpointCommand{}))
+	c.Assert(err, gc.IsNil)
+	s.modifyAddresses(c, nil)
+	ctx, err := coretesting.RunCommand(c, envcmd.Wrap(&EndpointCommand{}))
+	c.Assert(err, gc.IsNil)
+	c.Assert(ctx.Stderr.(*bytes.Buffer).String(), gc.Equals, "")
+	output := string(ctx.Stdout.(*bytes.Buffer).Bytes())
+	info := s.APIInfo(c)
+	c.Assert(output, gc.Equals, fmt.Sprintf("%s\n", info.Addrs[0]))
+}
+
+// modifyAddresses adds more endpoint addresses or removes all
+// in case of nil.
+func (s *EndpointSuite) modifyAddresses(c *gc.C, addresses []string) {
+	env, err := s.State.Environment()
+	c.Assert(err, gc.IsNil)
+	info, err := s.ConfigStore.ReadInfo(env.Name())
+	c.Assert(err, gc.IsNil)
+	endpoint := info.APIEndpoint()
+	if len(addresses) == 0 {
+		// Remove all endpoint addresses.
+		endpoint.Addresses = []string{}
+	} else {
+		// Add additional addresses.
+		endpoint.Addresses = append(endpoint.Addresses, addresses...)
+	}
+	info.SetAPIEndpoint(endpoint)
+	err = info.Write()
+	c.Assert(err, gc.IsNil)
+}
