ed1026d3ba665fe481bed079ce9ef9ca3f8edc31
Curtis Hovey <curtis@canonical.com>
2014-07-07 14:50:31 -0400

    Replace 1.18 packaging rules with 1.20. This is a no-op at this time, but allows us to fork stable 1.20 packaging when needed.

--- make-release-tarball.bash
+++ make-release-tarball.bash
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-# if someone invokes this with bash
+# Assemble a source tree using git to checkout the revision,
+# then go to get the package deps, then godeps to pin the versions/
+# Lastly create a release tarball from the tree.
 set -e
 
 unset GOPATH
 unset GOBIN
 
-# build release tarball from a bzr or git branch
-DEFAULT_BZR_JUJU_CORE="lp:juju-core"
 DEFAULT_GIT_JUJU_CORE="https://github.com/juju/juju.git"
+PACKAGE="github.com/juju/juju"
 
 usage() {
-    echo "usage: $0 <BZR_REVNO|GIT_REV> [BZR_BRANCH|GIT_REPO] [MERGE_REF] [MERGE_REPO] [MERGE_REV]"
-    echo "  BZR_REVNO: The juju core bzr revno to build"
+    echo "usage: $0 <GIT_REV> [GIT_REPO] [MERGE_REF] [MERGE_REPO] [MERGE_REV]"
     echo "  GIT_REV: The juju core git revision or branch to build"
-    echo "  BZR_BRANCH: The juju core bzr branch; defaults to $DEFAULT_BZR_JUJU_CORE"
     echo "  GIT_REPO: The juju core git repo; defaults to $DEFAULT_GIT_JUJU_CORE"
     echo "  MERGE_REF: The git branch or tag to merge"
     echo "  MERGE_REPO: The git repo that contains the MERGE_REF"
@@ -44,76 +43,46 @@ MERGE_REPO=""
 MERGE_REF=""
 MERGE_REV=""
 
-
-
-#if echo "$1" | grep -E '^[0-9-]*$'; then
-#    IS_BZR="true"
-#    REVNO=$1
-#    JUJU_CORE_BRANCH=${2:-$DEFAULT_BZR_JUJU_CORE}
-#    PACKAGE="launchpad.net/juju-core"
-#else
-    IS_BZR="false"
-    REVISION=$1
-    JUJU_CORE_REPO=${2:-$DEFAULT_GIT_JUJU_CORE}
-    PACKAGE="github.com/juju/juju"
-    if [[ $# -ge 4 ]]; then
-        MERGE_REF=$3
-        MERGE_REPO=$4
-        if [[ $# -eq 5 ]]; then
-            MERGE_REV=$5
-        fi
+REVISION=$1
+JUJU_CORE_REPO=${2:-$DEFAULT_GIT_JUJU_CORE}
+if [[ $# -ge 4 ]]; then
+    MERGE_REF=$3
+    MERGE_REPO=$4
+    if [[ $# -eq 5 ]]; then
+        MERGE_REV=$5
     fi
-#fi
-
+fi
 
 HERE=$(pwd)
 TMP_DIR=$(mktemp -d --tmpdir=$HERE)
 mkdir $TMP_DIR/RELEASE
 WORK=$TMP_DIR/RELEASE
 
-
-if [[ $IS_BZR == 'true' ]]; then
-    echo "Getting juju core and all its dependencies."
-    GOPATH=$WORK go get -v -d $PACKAGE/... || \
-        GOPATH=$WORK go get -v -d $PACKAGE/... || \
-        GOPATH=$WORK go get -v -d $PACKAGE/...
-    echo "Setting juju core tree to $JUJU_CORE_BRANCH $REVNO."
-    (cd "$WORK/src/$PACKAGE/" &&
-     bzr pull --no-aliases --remember --overwrite -r $REVNO $JUJU_CORE_BRANCH)
-    # Devs moved a package.
-    if [[ $JUJU_CORE_BRANCH == 'lp:juju-core/1.18' ]]; then
-        echo "Moving deps to support 1.18 releases."
-        GOPATH=$WORK go get -v github.com/errgo/errgo
-        rm -rf $WORK/src/github.com/juju/errgo
-    fi
+echo "Getting juju core from $JUJU_CORE_REPO."
+mkdir -p $WORK/src/$PACKAGE
+git clone $JUJU_CORE_REPO $WORK/src/$PACKAGE
+echo "Setting juju core tree to $REVISION."
+cd $WORK/src/$PACKAGE
+if git ls-remote ./  | grep origin/$REVISION; then
+    git checkout origin/$REVISION
 else
-    echo "Getting juju core from $JUJU_CORE_REPO."
-    mkdir -p $WORK/src/$PACKAGE
-    git clone $JUJU_CORE_REPO $WORK/src/$PACKAGE
-    echo "Setting juju core tree to $REVISION."
-    cd $WORK/src/$PACKAGE
-    if git ls-remote ./  | grep origin/$REVISION; then
-        git checkout origin/$REVISION
-    else
-        git checkout $REVISION
-    fi
-    if [[ "$MERGE_REF" != "" && "$MERGE_REPO" != "" ]]; then
-        if [[ "$MERGE_REV" != "" ]]; then
-            echo "Merging $MERGE_REV in $MERGE_REF from $MERGE_REPO into $REVISION"
-            git fetch $MERGE_REPO $MERGE_REF
-            git merge $MERGE_REV
-        else
-            echo "Pulling $MERGE_REF from $MERGE_REPO into $REVISION"
-            git pull $MERGE_REPO $MERGE_REF
-        fi
-    fi
-    echo "Getting juju core's dependencies."
-    GOPATH=$WORK go get -v -d ./... || \
-        GOPATH=$WORK go get -v -d ./... || \
-        GOPATH=$WORK go get -v -d ./... 
-    cd $HERE
+    git checkout $REVISION
 fi
-
+if [[ "$MERGE_REF" != "" && "$MERGE_REPO" != "" ]]; then
+    if [[ "$MERGE_REV" != "" ]]; then
+        echo "Merging $MERGE_REV in $MERGE_REF from $MERGE_REPO into $REVISION"
+        git fetch $MERGE_REPO $MERGE_REF
+        git merge $MERGE_REV
+    else
+        echo "Pulling $MERGE_REF from $MERGE_REPO into $REVISION"
+        git pull $MERGE_REPO $MERGE_REF
+    fi
+fi
+echo "Getting juju core's dependencies."
+GOPATH=$WORK go get -v -d ./... || \
+    GOPATH=$WORK go get -v -d ./... || \
+    GOPATH=$WORK go get -v -d ./... 
+cd $HERE
 
 echo "Updating juju-core dependencies to the required versions."
 GOPATH=$WORK go get -v launchpad.net/godeps
