1447a3497939b41a9bc9d7667c8bc31950d0e042
Menno Smits <menno.smits@canonical.com>
2014-07-08 16:20:54 +1000

    state/apiserver: keep backups in env storage
    Whenever the backup API is called keep the backup tarball in
    environment storage in addition to returning it to the client. Among
    other things, this will be used to support rollback when upgrades
    fail.

--- state/apiserver/backup.go
+++ state/apiserver/backup.go
@@ -13,12 +13,14 @@ import (
 	"path/filepath"
 
 	"github.com/juju/juju/environmentserver/authentication"
+	"github.com/juju/juju/environs"
 	"github.com/juju/juju/state"
 	"github.com/juju/juju/state/api/params"
 	"github.com/juju/juju/state/backup"
 )
 
 var Backup = backup.Backup
+var GetStorage = environs.GetStorage
 
 // backupHandler handles backup requests
 type backupHandler struct {
@@ -44,13 +46,20 @@ func (h *backupHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			h.sendError(w, http.StatusInternalServerError, err.Error())
 			return
 		}
+		if err := uploadBackupToStorage(h.state, file); err != nil {
+			h.sendError(w, http.StatusInternalServerError,
+				"backup storage failed: "+err.Error())
+			return
+		}
+		// uploadBackupToStorage moved the file position to the end so
+		// move it back to the start.
+		file.Seek(0, 0)
 
 		w.Header().Set("Content-Type", "application/octet-stream")
 		filename := filepath.Base(file.Name())
 		w.Header().Set("Content-Disposition",
 			fmt.Sprintf("attachment; filename=\"%s\"", filename))
 		w.Header().Set("Digest", fmt.Sprintf("SHA=%s", sha))
-
 		w.WriteHeader(http.StatusOK)
 		io.Copy(w, file)
 	default:
@@ -58,12 +67,15 @@ func (h *backupHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// doBackup creates a backup and returns an open file handle to the
+// backup archive. The backup file is already deleted when this
+// function returns (space will be returned to the OS once the file is
+// closed).
 func (h *backupHandler) doBackup() (*os.File, string, error) {
 	tempDir, err := ioutil.TempDir("", "jujuBackup")
 	if err != nil {
 		return nil, "", fmt.Errorf("creating backup directory failed: %v", err)
 	}
-
 	defer os.RemoveAll(tempDir)
 
 	info := GetMongoConnectionInfo(h.state)
@@ -74,7 +86,7 @@ func (h *backupHandler) doBackup() (*os.File, string, error) {
 
 	file, err := os.Open(filename)
 	if err != nil {
-		return nil, "", fmt.Errorf("backup failed: missing backup file")
+		return nil, "", fmt.Errorf("backup failed: %v", err)
 	}
 	return file, sha, nil
 }
@@ -89,4 +101,17 @@ func (h *backupHandler) sendError(w http.ResponseWriter, statusCode int, message
 	}
 	w.Write(body)
 	return nil
+}
+
+// uploadBackupToStorage copies a Juju backup file to environment storage.
+var uploadBackupToStorage = func(st *state.State, file *os.File) error {
+	stat, err := file.Stat()
+	if err != nil {
+		return fmt.Errorf("failed to stat backup file: %v", err)
+	}
+	stor, err := GetStorage(st)
+	if err != nil {
+		return fmt.Errorf("failed to open storage: %v", err)
+	}
+	return stor.Put(backup.StorageName(file.Name()), file, stat.Size())
 }
