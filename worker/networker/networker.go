33a55907335dfcc5af6b9fd21532ace11c80cb83
Vladislav Klyachin <vladislav.klyachin@canonical.com>
2014-07-11 01:12:10 +0400

    testing phase 1

--- worker/networker/networker.go
+++ worker/networker/networker.go
@@ -5,48 +5,25 @@ package networker
 
 import (
 	"fmt"
-	"net"
-	"strings"
 
 	"github.com/juju/loggo"
-	"github.com/juju/utils/exec"
 
 	"github.com/juju/juju/agent"
-	"github.com/juju/juju/network"
 	apinetworker "github.com/juju/juju/state/api/networker"
-	//"github.com/juju/juju/state/api/params"
 	"github.com/juju/juju/state/api/watcher"
 	"github.com/juju/juju/worker"
 )
 
 var logger = loggo.GetLogger("juju.networker")
 
-// Patches for testing.
-var (
-	InterfaceIsUp       = interfaceIsUp
-	InterfaceHasAddress = interfaceHasAddress
-	ExecuteCommands     = executeCommands
-)
-
-// Interface for juju internal network
-var InternalInterface = "eth0"
+// Interface and bridge interface for juju internal network
+var InternalInterface string
+var InternalBridge string
 
 // Networker configures network interfaces on the machine, as needed.
 type Networker struct {
 	st  *apinetworker.State
 	tag string
-}
-
-// Internal struct to store information required to setup networks.
-type setupInfo struct {
-	// The name and contents of '/etc/network/interfaces' file.
-	FileInfo
-
-	// The result of MachineNetworkInfo API call.
-	networks []network.Info
-
-	// Generated list of commands to execute.
-	commands []string
 }
 
 // NewNetworker returns a Worker that handles machine networking configuration.
@@ -59,156 +36,73 @@ func NewNetworker(st *apinetworker.State, agentConfig agent.Config) worker.Worke
 }
 
 func (nw *Networker) SetUp() (watcher.NotifyWatcher, error) {
+	var err error
+	s := &configState{}
+
+	// Read network configuration files and revert modifications made by MAAS.
+	if err = s.configFiles.ReadAll(); err != nil {
+		return nil, err
+	}
+	if err = s.configFiles.FixMAAS(); err != nil {
+		return nil, err
+	}
+
+	// Need to configure network interface for juju internal network.
+	InternalInterface = "eth0"
+	if s.configFiles["br0"] != nil {
+		InternalBridge = "br0"
+	} else {
+		InternalBridge = InternalInterface
+	}
+
+	// Verify that internal interface is properly configured and brought up.
+	if !InterfaceIsUp(InternalInterface) {
+		logger.Errorf("interface %q has to be up", InternalInterface)
+		return nil, fmt.Errorf("interface %q has to be up", InternalInterface)
+	}
+	if !InterfaceIsUp(InternalBridge) || !InterfaceHasAddress(InternalBridge) {
+		logger.Errorf("interface %q has to be up", InternalBridge)
+		return nil, fmt.Errorf("interface %q has to be up", InternalBridge)
+	}
+
+	// Add commands to install VLAN module.
+	s.ensureVLANModule()
+
+	// Apply changes.
+	if err = s.apply(); err != nil {
+		return nil, err
+	}
 	return nw.st.WatchInterfaces(nw.tag)
 }
 
-const newSchema = `
-# Source interfaces
-# Please check %s/interfaces.d before changing this file
-# as interfaces may have been defined in %s/interfaces.d
-# NOTE: the primary ethernet device is defined in
-# %s/interfaces.d/eth0.cfg
-# See LP: #1262951
-source %s/interfaces.d/*.cfg
-`
-
-func (s *setupInfo) ensureVLANModule() {
-	commands := []string{
-		`dpkg-query -s vlan || apt-get --option Dpkg::Options::=--force-confold --assume-yes install vlan`,
-		`lsmod | grep -q 8021q || modprobe 8021q`,
-		`grep -q 8021q /etc/modules || echo 8021q >> /etc/modules`,
-		`vconfig set_name_type DEV_PLUS_VID_NO_PAD`,
-	}
-	s.commands = append(s.commands, commands...)
-}
-
-func (s *setupInfo) bringUpInterfaces() {
-	for _, network := range s.networks {
-		name := network.ActualInterfaceName()
-		if name != InternalInterface && !InterfaceIsUp(name) {
-			command := fmt.Sprintf(`ifup %s`, name)
-			s.commands = append(s.commands, command)
-		}
-	}
-}
-
-func interfacePattern(interfaceName string) string {
-	if interfaceName == InternalInterface {
-		return fmt.Sprintf("\nauto %s\nsource %s/%s.config\n", interfaceName, NetworkDir, interfaceName)
-	}
-	return fmt.Sprintf("\nauto %s\niface %s inet dhcp\n", interfaceName, interfaceName)
-}
-
-func (s *setupInfo) interfaceIsConfigured(interfaceName string) bool {
-	substr := interfacePattern(interfaceName)
-	file, ok := s.Files[interfaceName]
-	return ok && strings.Contains(file.Data, substr)
-}
-
-func (s *setupInfo) configureInterfaces() {
-	for _, network := range s.networks {
-		name := network.ActualInterfaceName()
-		if name != InternalInterface {
-			s.configureInterface(name)
-		}
-	}
-}
-
-func (s *setupInfo) configureInterface(interfaceName string) {
-	substr := interfacePattern(interfaceName)
-	if !strings.Contains(s.Files[interfaceName].Data, substr) {
-		s.Files[interfaceName] = &File{
-			FileName: fmt.Sprintf("%s/%s.cfg", IfacesDirName, interfaceName),
-			Data:     substr,
-			Op:       DoWrite,
-		}
-	}
-}
-
-// Execute slice of commands one by one.
-func executeCommands(commands []string) error {
-	for _, command := range commands {
-		result, err := exec.RunCommands(exec.RunParams{
-			Commands:   command,
-			WorkingDir: "/",
-		})
-		if err != nil {
-			err := fmt.Errorf("failed to execute %q: %v", command, err)
-			logger.Errorf("%s", err.Error())
-			return err
-		}
-		if result.Code != 0 {
-			err := fmt.Errorf("command %q failed (code: %d, stdout: %s, stderr: %s)",
-				command, result.Code, result.Stdout, result.Stderr)
-			logger.Errorf("%s", err.Error())
-			return err
-		}
-	}
-	return nil
-}
-
-// Verify that the interface is up.
-func interfaceIsUp(interfaceName string) bool {
-	iface, err := net.InterfaceByName(interfaceName)
-	if err != nil {
-		logger.Errorf("cannot find network interface %q: %v", interfaceName, err)
-		return false
-	}
-	return (iface.Flags & net.FlagUp) != 0
-}
-
-// Verify that the interface has assigned address.
-func interfaceHasAddress(interfaceName string) bool {
-	iface, err := net.InterfaceByName(interfaceName)
-	if err != nil {
-		logger.Errorf("cannot find network interface %q: %v", interfaceName, err)
-		return false
-	}
-	addrs, err := iface.Addrs()
-	if err != nil {
-		logger.Errorf("cannot get addresses for network interface %q: %v", interfaceName, err)
-		return false
-	}
-	return len(addrs) != 0
-}
-
 func (nw *Networker) Handle() error {
-	s := &setupInfo{}
-	err := s.FileInfo.ReadInterfacesFiles()
-	if err != nil {
+	var err error
+	s := &configState{}
+
+	// Read configuration files for managed interfaces.
+	if err = s.configFiles.readManaged(); err != nil {
 		return err
 	}
-	s.networks, err = nw.st.MachineNetworkInfo(nw.tag)
-	logger.Infof("s.networks=%#v", s.networks)
-	if err != nil {
+
+	// Read network info from the state
+	if s.networkInfo, err = nw.st.MachineNetworkInfo(nw.tag); err != nil {
 		logger.Errorf("failed to process network info: %v", err)
 		return err
 	}
 
-	// Verify that internal interface is properly configured and brought up.
-	if !s.interfaceIsConfigured(InternalInterface) || !InterfaceHasAddress(InternalInterface) {
-		if !s.interfaceIsConfigured(InternalInterface) {
-			logger.Errorf("interface %q has to be configured", InternalInterface)
-		}
-		if !InterfaceHasAddress(InternalInterface) {
-			logger.Errorf("interface %q has to be bring up", InternalInterface)
-		}
-		return fmt.Errorf("interface %q has to be configured and bring up", InternalInterface)
-	}
-
-	// Update interface file
-	s.configureInterfaces()
-	err = s.FileInfo.WriteInterfacesFiles()
-	if err != nil {
+	// Down disabled interfaces.
+	s.bringDownInterfaces()
+	if err := s.apply(); err != nil {
 		return err
 	}
 
-	// Generate a list of commands to configure networks.
-	s.ensureVLANModule()
+	// Up configured interfaces.
 	s.bringUpInterfaces()
+	if err := s.apply(); err != nil {
+		return err
+	}
 
-	err = ExecuteCommands(s.commands)
-	return err
+	return nil
 }
 
 func (nw *Networker) TearDown() error {
--- worker/networker/networker_test.go
+++ worker/networker/networker_test.go
@@ -6,6 +6,8 @@ package networker_test
 import (
 	"fmt"
 	"io/ioutil"
+	"strings"
+	"time"
 
 	"github.com/juju/utils"
 	"github.com/juju/utils/set"
@@ -18,6 +20,7 @@ import (
 	"github.com/juju/juju/state"
 	"github.com/juju/juju/state/api"
 	apinetworker "github.com/juju/juju/state/api/networker"
+	coretesting "github.com/juju/juju/testing"
 	"github.com/juju/juju/worker"
 	"github.com/juju/juju/worker/networker"
 )
@@ -32,11 +35,8 @@ type networkerSuite struct {
 
 	st             *api.State
 	networkerState *apinetworker.State
-
-	networkDir          string
-	interfacesFile      string
-	executorCommands    []string
-	executorHasFinished chan struct{}
+	configStates   []*configState
+	executed       chan bool
 }
 
 var _ = gc.Suite(&networkerSuite{})
@@ -123,6 +123,12 @@ func (s *networkerSuite) SetUpTest(c *gc.C) {
 	// Create the networker API facade.
 	s.networkerState = s.st.Networker()
 	c.Assert(s.networkerState, gc.NotNil)
+
+	// Create temporary directory to store interfaces file.
+	s.PatchValue(&networker.ConfigDirName, c.MkDir())
+	s.PatchValue(&networker.ConfigFileName, networker.ConfigDirName+"/interfaces")
+	s.PatchValue(&networker.ConfigSubDirName, networker.ConfigDirName+"/interfaces.d")
+
 }
 
 type mockConfig struct {
@@ -148,54 +154,64 @@ iface lo inet loopback
 auto eth0
 source %s/eth0.config
 
-auto eth1
-iface eth1 inet dhcp
+auto wlan0
+iface wlan0 inet dhcp
+`
+const sampleEth0DotConfigFile = `iface eth0 inet manual
 
-auto eth1.2
-iface eth1.2 inet dhcp
-
-auto eth2
-iface eth2 inet dhcp
+auto br0
+iface br0 inet dhcp
+  bridge_ports eth0
 `
 
-var readyInterfaces = set.NewStrings("eth0", "eth1")
-var interfacesWithAddress = set.NewStrings("eth0", "eth2")
+var readyInterfaces = set.NewStrings("eth0", "br0", "wlan0")
+var interfacesWithAddress = set.NewStrings("br0", "wlan0")
 
-const expectedInterfacesFile = `# This file describes the network interfaces available on your system
+var expectedInterfacesFile = `# This file describes the network interfaces available on your system
 # and how to activate them. For more information see interfaces(5).
 
 # The loopback network interface
 auto lo
 iface lo inet loopback
 
-auto eth0
-source %s/eth0.config
+` + networker.SourceCommentAndCommand
 
-auto eth1
-iface eth1 inet dhcp
+type configState struct {
+	files    networker.ConfigFiles
+	commands []string
+	readyInterfaces []string
+	interfacesWithAddress []string
+}
 
-auto eth1.2
-iface eth1.2 inet dhcp
-
-auto eth2
-iface eth2 inet dhcp
-
-auto eth1.42
-iface eth1.42 inet dhcp
-
-auto eth0.69
-iface eth0.69 inet dhcp
-`
+func executeCommandsHook(c *gc.C, s *networkerSuite, commands []string) error {
+	cs := &configState{}
+	err := cs.files.ReadAll()
+	c.Assert(err, gc.IsNil)
+	cs.commands = append(cs.commands, commands...)
+	// modify state of interfaces
+	for _, cmd := range commands {
+		args := strings.Split(cmd, " ")
+		if len(args) == 2 && args[0] == "ifup" {
+			readyInterfaces.Add(args[1])
+			interfacesWithAddress.Add(args[1])
+		} else if len(args) == 2 && args[0] == "ifdown" {
+			readyInterfaces.Remove(args[1])
+			interfacesWithAddress.Remove(args[1])
+		}
+	}
+	cs.readyInterfaces = readyInterfaces.SortedValues()
+	cs.interfacesWithAddress = interfacesWithAddress.SortedValues()
+	s.configStates = append(s.configStates, cs)
+	s.executed <- true
+	return nil
+}
 
 func (s *networkerSuite) TestNetworker(c *gc.C) {
-	// Create temporary directory to store interfaces file.
-	s.networkDir = c.MkDir()
-	s.PatchValue(&networker.NetworkDir, s.networkDir)
-	s.interfacesFile = s.networkDir + "/interfaces"
-
-	// Create a sample interfaces file
-	interfacesFileContents := fmt.Sprintf(sampleInterfacesFile, s.networkDir)
-	err := ioutil.WriteFile(s.interfacesFile, []byte(interfacesFileContents), 0644)
+	// Create a sample interfaces file (MAAS configuration)
+	interfacesFileContents := fmt.Sprintf(sampleInterfacesFile, networker.ConfigDirName)
+	err := ioutil.WriteFile(networker.ConfigFileName, []byte(interfacesFileContents), 0644)
+	c.Assert(err, gc.IsNil)
+	err = ioutil.WriteFile(networker.ConfigDirName+"/eth0.config", []byte(sampleEth0DotConfigFile), 0644)
 	c.Assert(err, gc.IsNil)
 
 	// Patch the network interface functions
@@ -209,50 +225,91 @@ func (s *networkerSuite) TestNetworker(c *gc.C) {
 		})
 
 	// Path the command executor
-	s.PatchValue(&networker.ExecuteCommands,
-		func(commands []string) error {
-			s.executorCommands = commands
-			close(s.executorHasFinished)
-			return nil
-		})
-	s.executorHasFinished = make(chan struct{})
+	s.configStates = []*configState{}
+	s.PatchValue(&networker.ExecuteCommands, func(commands []string) error { return executeCommandsHook(c, s, commands) })
 
 	// Create and setup networker.
+	s.executed = make(chan bool)
 	nw := networker.NewNetworker(s.networkerState, agentConfig(s.machine.Tag().String()))
 	defer func() { c.Assert(worker.Stop(nw), gc.IsNil) }()
 
-	// Wait until networker setup has finished execute commands.
-	<-s.executorHasFinished
+	executeCount := 0
+loop:
+	for {
+		select {
+		case <-s.executed:
+			executeCount++
+			if executeCount == 3 {
+				break loop
+			}
+		case <-time.After(coretesting.ShortWait):
+			c.Fatalf("command not executed")
+		}
+	}
 
-	// Verify the contents of generated interfaces file.
-	contents, err := ioutil.ReadFile(s.interfacesFile)
-	c.Assert(err, gc.IsNil)
-	expected := fmt.Sprintf(expectedInterfacesFile, s.networkDir)
-	c.Assert(string(contents), gc.Equals, expected)
-
-	// Verify the executed commands
+	// Verify the executed commands from SetUp()
+	expectedConfigFiles := networker.ConfigFiles{
+		"": {
+			FileName: networker.ConfigFileName,
+			Data: fmt.Sprintf(expectedInterfacesFile, networker.ConfigSubDirName,
+				networker.ConfigSubDirName, networker.ConfigSubDirName, networker.ConfigSubDirName),
+		},
+		"br0": {
+			FileName: networker.FileName("br0"),
+			Data: "auto br0\niface br0 inet dhcp\n  bridge_ports eth0\n",
+		},
+		"eth0": {
+			FileName: networker.FileName("eth0"),
+			Data: "auto eth0\niface eth0 inet manual\n",
+		},
+		"wlan0": {
+			FileName: networker.FileName("wlan0"),
+			Data: "auto wlan0\niface wlan0 inet dhcp\n",
+		},
+	}
+	c.Assert(s.configStates[0].files, gc.DeepEquals, expectedConfigFiles)
 	expectedCommands := []string{
 		"dpkg-query -s vlan || apt-get --option Dpkg::Options::=--force-confold --assume-yes install vlan",
 		"lsmod | grep -q 8021q || modprobe 8021q",
 		"grep -q 8021q /etc/modules || echo 8021q >> /etc/modules",
 		"vconfig set_name_type DEV_PLUS_VID_NO_PAD",
-		"ifup eth1.42",
-		"ifup eth0.69",
-		"ifup eth2",
 	}
-	c.Assert(s.executorCommands, gc.DeepEquals, expectedCommands)
-}
+	c.Assert(s.configStates[0].commands, gc.DeepEquals, expectedCommands)
+	c.Assert(s.configStates[0].readyInterfaces, gc.DeepEquals, []string{"br0", "eth0", "wlan0"})
+	c.Assert(s.configStates[0].interfacesWithAddress, gc.DeepEquals, []string{"br0", "wlan0"})
 
-func (s *networkerSuite) TestExecuteCommands(c *gc.C) {
-	commands := []string{
-		"echo start",
-		"sh -c 'echo STDOUT; echo STDERR >&2; exit 123'",
-		"echo end",
-		"exit 111",
+	// Verify the executed commands from Handle()
+	c.Assert(s.configStates[1].files, gc.DeepEquals, expectedConfigFiles)
+	expectedCommands = []string(nil)
+	c.Assert(s.configStates[1].commands, gc.DeepEquals, expectedCommands)
+	c.Assert(s.configStates[1].readyInterfaces, gc.DeepEquals, []string{"br0", "eth0", "wlan0"})
+	c.Assert(s.configStates[1].interfacesWithAddress, gc.DeepEquals, []string{"br0", "wlan0"})
+
+	// Verify the executed commands from Handle()
+	expectedConfigFiles["eth0.69"] = &networker.ConfigFile{
+		FileName: networker.FileName("eth0.69"),
+		Data: "# Managed by Networker, don't change\nauto eth0.69\niface eth0.69 inet dhcp\n\n",
 	}
-	err := networker.ExecuteCommands(commands)
-	expected := "command \"sh -c 'echo STDOUT; echo STDERR >&2; exit 123'\" failed " +
-		"(code: 123, stdout: STDOUT\n, stderr: STDERR\n)"
-	c.Assert(err, gc.NotNil)
-	c.Assert(err.Error(), gc.Equals, expected)
+	expectedConfigFiles["eth1"] = &networker.ConfigFile{
+		FileName: networker.FileName("eth1"),
+		Data: "# Managed by Networker, don't change\nauto eth1\niface eth1 inet dhcp\n\n",
+	}
+	expectedConfigFiles["eth1.42"] = &networker.ConfigFile{
+		FileName: networker.FileName("eth1.42"),
+		Data: "# Managed by Networker, don't change\nauto eth1.42\niface eth1.42 inet dhcp\n\n",
+	}
+	expectedConfigFiles["eth2"] = &networker.ConfigFile{
+		FileName: networker.FileName("eth2"),
+		Data: "# Managed by Networker, don't change\nauto eth2\niface eth2 inet dhcp\n\n",
+	}
+	for k, _ := range s.configStates[2].files {
+		c.Check(s.configStates[2].files[k], gc.DeepEquals, expectedConfigFiles[k])
+	}
+	c.Assert(s.configStates[2].files, gc.DeepEquals, expectedConfigFiles)
+	expectedCommands = []string{"ifup eth0.69", "ifup eth1", "ifup eth1.42", "ifup eth2"}
+	c.Assert(s.configStates[2].commands, gc.DeepEquals, expectedCommands)
+	c.Assert(s.configStates[2].readyInterfaces, gc.DeepEquals,
+		[]string{"br0", "eth0", "eth0.69", "eth1", "eth1.42", "eth2", "wlan0"})
+	c.Assert(s.configStates[2].interfacesWithAddress, gc.DeepEquals,
+		[]string{"br0", "eth0.69", "eth1", "eth1.42", "eth2", "wlan0"})
 }
