b75da10db3aef71ffda25345c2d0633a6db9a2f7
Abel Deuring <abel.deuring@canonical.com>
2014-07-15 16:51:47 +0200

    Get job build numbers from an environment variable 'build_status' that should be specified by ci-director

--- copy_artifacts_to_s3.py
+++ copy_artifacts_to_s3.py
@@ -11,101 +11,74 @@ Required environment variables:
 """
 
 from __future__ import print_function
+from cStringIO import StringIO
 from jenkins import Jenkins
 import os
-import re
 import subprocess
-import sys
 from utility import temp_dir
+import yaml
 
 
 ARCHIVE_BUCKET_URL = 's3://juju-qa-data'
 JENKINS_URL = 'http://juju-ci.vapour.ws:8080'
-BUILD_REVISION_JOB = 'build-revision'
-REVISION_RESULTS_JOB = 'revision-results'
-PARAM_REVISION_BUILD = 'revision_build'
-JOBS = {
-    BUILD_REVISION_JOB: (
-        (re.compile(r'^juju-core_.*\.tar\.gz$'), 'Juju core tarball'),
-        ),
-    'win-client-build-installer': (
-        (re.compile(r'^juju-setup-.*exe$'), 'Windows installer'),
-        ),
-    }
+# Associate a job key as used by ci-director with a Jenkins job name.
+JOBS = (
+    ('build', 'build-revision'),
+    ('build-windows-installer', 'win-client-build-installer'),
+    ('deploy-via-windows', 'win-client-deploy'),
+    ('publication', 'publish-revision'),
+    )
 
 
-def get_build_parameter(build_info, name):
-    for action in build_info.get('actions', []):
-        for parameter in action.get('parameters', []):
-            if parameter['name'] == name:
-                return parameter['value']
+def copy_to_s3(source_path, dest_url):
+    subprocess.check_call([
+        's3cmd', '-c', os.getenv('S3CFG'), '--no-progress', 'put',
+        source_path, dest_url])
 
 
-def revision_build_from_revision_results(juju_ci):
-    """Return the build revision from the parameters of the current build."""
-    build_info = juju_ci.get_build_info(
-        REVISION_RESULTS_JOB, int(os.getenv('BUILD_NUMBER')))
-    revision_build = get_build_parameter(build_info, 'build_number')
-    if revision_build is None:
-        raise ValueError('Main build number not found in build parameters')
-    return int(revision_build)
-
-
-def job_build_number_for_revision_build(juju_ci, job_name, revision_build):
-    """Find the build number for the given job where the parameter
-    revision_build matches the passed value of revision_build.
-    """
-    # The job 'build_revison' itself does not have this parameter.
-    # In this case, we know the required build number already: The
-    # value of revision_build.
-    if job_name == BUILD_REVISION_JOB:
-        return revision_build
-    job_info = juju_ci.get_job_info(job_name)
-    for build_number in [build['number'] for build in job_info['builds']]:
-        build_info = juju_ci.get_build_info(job_name, build_number)
-        found_revision_build = get_build_parameter(
-            build_info, PARAM_REVISION_BUILD)
-        if (found_revision_build is not None and
-            int(found_revision_build) == revision_build):
-            return build_number
-    print("Cannot find a build of %s for revision_build %s" %
-          (job_name, revision_build), file=sys.stderr)
-    return None
-
-
-def copy_job_artifacts(job_name, revision_build, artifact_matchers, juju_ci,
-                        work_dir):
-    build_number = job_build_number_for_revision_build(
-        juju_ci, job_name, revision_build)
-    if build_number is None:
+def copy_job_artifacts(juju_ci, build_number, build_summary, job_name,
+                       work_dir):
+    if 'last_build' not in build_summary:
         return
-    build_info = juju_ci.get_build_info(job_name, build_number)
-    for matcher, name in artifact_matchers:
-        source_url = None
-        for artifact in build_info['artifacts']:
-            filename = artifact['fileName']
-            if matcher.search(filename) is not None:
-                source_url = '%s/artifact/%s' % (build_info['url'], filename)
-                dest_url = '%s/juju-ci/products/build-%s/%s' % (
-                    ARCHIVE_BUCKET_URL, revision_build, filename)
-                local_path = os.path.join(work_dir, filename)
-                break
-        if source_url is None:
-            print(
-                "Cannot find %s in artifacts of %s" % (name, job_name),
-                file=sys.stderr)
+    build_info = juju_ci.get_build_info(job_name, build_summary['last_build'])
+    for artifact in build_info['artifacts']:
+        filename = artifact['fileName']
+        if filename == 'empty':
             continue
-
+        relative_path = artifact['relativePath']
+        source_url = '%s/artifact/%s' % (
+            build_info['url'], relative_path)
+        dest_url = '%s/juju-ci/products/build-%s/%s/%s' % (
+            ARCHIVE_BUCKET_URL, build_number, job_name, relative_path)
+        local_path = os.path.join(work_dir, filename)
         subprocess.check_call(['wget', '-q', '-O', local_path, source_url])
-        subprocess.check_call([
-            's3cmd', '-c', os.getenv('S3CFG'), '--no-progress', 'put',
-            local_path, dest_url])
+        copy_to_s3(local_path, dest_url)
+
+
+def save_build_status(build_status, build_number, work_dir):
+    result_path = os.path.join(work_dir, 'result.yaml')
+    with open(result_path, 'w') as results:
+        results.write(build_status)
+    s3_url = '%s/juju-ci/products/build-%s/result.yaml' % (
+        ARCHIVE_BUCKET_URL, build_number)
+    copy_to_s3(result_path, s3_url)
+
 
 if __name__ == '__main__':
     juju_ci = Jenkins(JENKINS_URL)
+    build_number = int(os.getenv('build_number'))
 
     with temp_dir() as work_dir:
-        revision_build = revision_build_from_revision_results(juju_ci)
-        for job_name, artifact_matchers in JOBS.items():
+        build_status = os.getenv('build_status')
+        save_build_status(build_status, build_number, work_dir)
+        build_status = yaml.load(StringIO(build_status))
+        for job_key, job_name in JOBS:
             copy_job_artifacts(
-                job_name, revision_build, artifact_matchers, juju_ci, work_dir)
+                juju_ci, build_number, build_status[job_key], job_name,
+                work_dir)
+        for test_name, summary in build_status['functional-tests'].items():
+            copy_job_artifacts(
+                juju_ci, build_number, summary, test_name, work_dir)
+        for test_name, summary in build_status['tests'].items():
+            copy_job_artifacts(
+                juju_ci, build_number, summary, test_name, work_dir)
