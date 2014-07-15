a0c751194e8354aec1ba5f4afda5485304e9cd7b
Tim Penhey <tim@penhey.net>
2014-07-15 13:11:27 +1200

    clean up the client

--- environs/manual/provisioner_test.go
+++ environs/manual/provisioner_test.go
@@ -33,9 +33,11 @@ var _ = gc.Suite(&provisionerSuite{})
 func (s *provisionerSuite) getArgs(c *gc.C) manual.ProvisionMachineArgs {
 	hostname, err := os.Hostname()
 	c.Assert(err, gc.IsNil)
+	client := s.APIState.Client()
+	s.AddCleanup(func(*gc.C) { client.Close() })
 	return manual.ProvisionMachineArgs{
 		Host:   hostname,
-		Client: s.APIState.Client(),
+		Client: client,
 	}
 }
 
