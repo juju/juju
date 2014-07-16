542044ce3c09c6df98f76500509834524059252a
John Arbash Meinel <john@arbash-meinel.com>
2014-07-16 16:20:31 +0400

    man, fix one thing, break another... fix another place that I just broke.

--- environs/jujutest/livetests.go
+++ environs/jujutest/livetests.go
@@ -34,6 +34,7 @@ import (
 	"github.com/juju/juju/network"
 	"github.com/juju/juju/provider/dummy"
 	"github.com/juju/juju/state"
+	"github.com/juju/juju/state/api"
 	statetesting "github.com/juju/juju/state/testing"
 	coretesting "github.com/juju/juju/testing"
 	coretools "github.com/juju/juju/tools"
