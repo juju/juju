f9c2524bab7692c31e29f0a39825a027a0756761
Eric Snow <ericsnowcurrently@gmail.com>
2014-07-18 14:33:04 -0600

    Make tarball generation more flexible.

--- state/backup/archive.go
+++ state/backup/archive.go
@@ -15,6 +15,103 @@ import (
 	"strings"
 )
 
+type archive struct {
+	Files       []string
+	StripPrefix string
+}
+
+// Write writes out the archive data for the files/directory-trees.
+func (a *archive) Write(w io.Writer) (err error) {
+	checkClose := func(w io.Closer) {
+		if closeErr := w.Close(); closeErr != nil && err == nil {
+			err = fmt.Errorf("error closing archive file: %v", closeErr)
+		}
+	}
+
+	tarw := tar.NewWriter(w)
+	defer checkClose(tarw)
+
+	for _, ent := range a.Files {
+		if err := a.writeTree(ent, tarw); err != nil {
+			return fmt.Errorf("archive failed: %v", err)
+		}
+	}
+
+	return
+}
+
+func (a *archive) WriteGzipped(w io.Writer) (err error) {
+	checkClose := func(w io.Closer) {
+		if closeErr := w.Close(); closeErr != nil && err == nil {
+			err = fmt.Errorf("error closing archive file: %v", closeErr)
+		}
+	}
+
+	gzw := gzip.NewWriter(w)
+	defer checkClose(gzw)
+
+	return a.Write(gzw)
+}
+
+// writeTree creates an entry for the given file
+// or directory in the given tar archive.
+func (a *archive) writeTree(fileName string, tarw *tar.Writer) error {
+	// Open and inspect the file.
+	f, err := os.Open(fileName)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	fInfo, err := f.Stat()
+	if err != nil {
+		return err
+	}
+	h, err := tar.FileInfoHeader(fInfo, "")
+	if err != nil {
+		return fmt.Errorf("cannot create tar header for %q: %v", fileName, err)
+	}
+	h.Name = filepath.ToSlash(strings.TrimPrefix(fileName, a.StripPrefix))
+
+	// Write out the header.
+	if err := tarw.WriteHeader(h); err != nil {
+		return fmt.Errorf("cannot write header for %q: %v", fileName, err)
+	}
+
+	// Write out the contents.
+	if fInfo.IsDir() {
+		return a.writeDir(fileName, f, tarw)
+	} else {
+		_, err := io.Copy(tarw, f)
+		if err != nil {
+			return fmt.Errorf("failed to write %q: %v", fileName, err)
+		}
+	}
+	return nil
+}
+
+func (a *archive) writeDir(dirname string, f *os.File, tarw *tar.Writer) error {
+	if !strings.HasSuffix(dirname, sep) {
+		dirname += sep
+	}
+	for {
+		names, err := f.Readdirnames(100)
+		if len(names) == 0 && err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			return fmt.Errorf("error reading directory %q: %v", dirname, err)
+		}
+		for _, basename := range names {
+			err := a.writeTree(filepath.Join(dirname, basename), tarw)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
 // tarFiles creates a tar archive at targetPath holding the files listed
 // in fileList. If compress is true, the archive will also be gzip
 // compressed.
@@ -35,73 +132,22 @@ func tarAndHashFiles(fileList []string, targetPath, strip string, compress bool,
 			err = fmt.Errorf("error closing backup file: %v", closeErr)
 		}
 	}
+
+	// Create the file.
 	f, err := os.Create(targetPath)
 	if err != nil {
 		return fmt.Errorf("cannot create backup file %q", targetPath)
 	}
 	defer checkClose(f)
 
+	// Set it to hash the file.
 	w := io.MultiWriter(f, hashw)
 
+	// Write out the archive.
+	ar := archive{fileList, strip}
 	if compress {
-		gzw := gzip.NewWriter(w)
-		defer checkClose(gzw)
-		w = gzw
+		return ar.WriteGzipped(w)
+	} else {
+		return ar.Write(w)
 	}
-
-	tarw := tar.NewWriter(w)
-	defer checkClose(tarw)
-	for _, ent := range fileList {
-		if err := writeContents(ent, strip, tarw); err != nil {
-			return fmt.Errorf("backup failed: %v", err)
-		}
-	}
-	return nil
-}
-
-// writeContents creates an entry for the given file
-// or directory in the given tar archive.
-func writeContents(fileName, strip string, tarw *tar.Writer) error {
-	f, err := os.Open(fileName)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-	fInfo, err := f.Stat()
-	if err != nil {
-		return err
-	}
-	h, err := tar.FileInfoHeader(fInfo, "")
-	if err != nil {
-		return fmt.Errorf("cannot create tar header for %q: %v", fileName, err)
-	}
-	h.Name = filepath.ToSlash(strings.TrimPrefix(fileName, strip))
-	if err := tarw.WriteHeader(h); err != nil {
-		return fmt.Errorf("cannot write header for %q: %v", fileName, err)
-	}
-	if !fInfo.IsDir() {
-		if _, err := io.Copy(tarw, f); err != nil {
-			return fmt.Errorf("failed to write %q: %v", fileName, err)
-		}
-		return nil
-	}
-	if !strings.HasSuffix(fileName, string(os.PathSeparator)) {
-		fileName = fileName + string(os.PathSeparator)
-	}
-
-	for {
-		names, err := f.Readdirnames(100)
-		if len(names) == 0 && err == io.EOF {
-			return nil
-		}
-		if err != nil {
-			return fmt.Errorf("error reading directory %q: %v", fileName, err)
-		}
-		for _, name := range names {
-			if err := writeContents(filepath.Join(fileName, name), strip, tarw); err != nil {
-				return err
-			}
-		}
-	}
-
 }
