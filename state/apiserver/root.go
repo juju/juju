5c4e89902d9b92a13558459987ccf7f09ec4cb69
Casey Marshall <cmars@cmarstech.com>
2014-07-24 14:24:25 +1200

    Move UpgradingRoot to its own file.

--- state/apiserver/root.go
+++ state/apiserver/root.go
@@ -10,7 +10,6 @@ import (
 
 	"github.com/juju/errors"
 	"github.com/juju/names"
-	"github.com/juju/utils/set"
 
 	"github.com/juju/juju/rpc"
 	"github.com/juju/juju/rpc/rpcreflect"
@@ -249,37 +248,6 @@ func (r *AnonRoot) FindMethod(rootName string, version int, methodName string) (
 		Version:    version,
 		Method:     methodName,
 	}
-}
-
-// UpgradingRoot restricts API calls to those supported during an upgrade.
-type UpgradingRoot struct {
-	rpc.MethodFinder
-}
-
-// NewUpgradingRoot returns a new UpgradingRoot.
-func NewUpgradingRoot(finder rpc.MethodFinder) *UpgradingRoot {
-	return &UpgradingRoot{finder}
-}
-
-var inUpgradeError = errors.New("upgrade in progress - Juju functionality is limited")
-
-var allowedMethodsDuringUpgrades = set.NewStrings(
-	"Client.FullStatus",     // for "juju status"
-	"Client.PrivateAddress", // for "juju ssh"
-	"Client.PublicAddress",  // for "juju ssh"
-	"Client.WatchDebugLog",  // for "juju debug-log"
-)
-
-func isMethodAllowedDuringUpgrade(rootName, methodName string) bool {
-	fullName := rootName + "." + methodName
-	return allowedMethodsDuringUpgrades.Contains(fullName)
-}
-
-func (r *UpgradingRoot) FindMethod(rootName string, version int, methodName string) (rpcreflect.MethodCaller, error) {
-	if !isMethodAllowedDuringUpgrade(rootName, methodName) {
-		return nil, inUpgradeError
-	}
-	return r.MethodFinder.FindMethod(rootName, version, methodName)
 }
 
 // AuthMachineAgent returns whether the current client is a machine agent.
---
+++ state/apiserver/upgrading_root.go
@@ -0,0 +1,44 @@
+// Copyright 2014 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package apiserver
+
+import (
+	"errors"
+
+	"github.com/juju/utils/set"
+
+	"github.com/juju/juju/rpc"
+	"github.com/juju/juju/rpc/rpcreflect"
+)
+
+// UpgradingRoot restricts API calls to those supported during an upgrade.
+type UpgradingRoot struct {
+	rpc.MethodFinder
+}
+
+// NewUpgradingRoot returns a new UpgradingRoot.
+func NewUpgradingRoot(finder rpc.MethodFinder) *UpgradingRoot {
+	return &UpgradingRoot{finder}
+}
+
+var inUpgradeError = errors.New("upgrade in progress - Juju functionality is limited")
+
+var allowedMethodsDuringUpgrades = set.NewStrings(
+	"Client.FullStatus",     // for "juju status"
+	"Client.PrivateAddress", // for "juju ssh"
+	"Client.PublicAddress",  // for "juju ssh"
+	"Client.WatchDebugLog",  // for "juju debug-log"
+)
+
+func isMethodAllowedDuringUpgrade(rootName, methodName string) bool {
+	fullName := rootName + "." + methodName
+	return allowedMethodsDuringUpgrades.Contains(fullName)
+}
+
+func (r *UpgradingRoot) FindMethod(rootName string, version int, methodName string) (rpcreflect.MethodCaller, error) {
+	if !isMethodAllowedDuringUpgrade(rootName, methodName) {
+		return nil, inUpgradeError
+	}
+	return r.MethodFinder.FindMethod(rootName, version, methodName)
+}
