934a4540b1f4b96e736465c34a965e07a60f731a
Curtis Hovey <curtis@canonical.com>
2014-07-28 19:29:11 -0400

    Updated script to return a code and reason why a pull rquest can or cannot be tested.

--- check_blockers.py
+++ check_blockers.py
@@ -3,15 +3,59 @@
 from __future__ import print_function
 
 from argparse import ArgumentParser
+import json
+import urllib2
 import sys
 
 
-def get_lp_blockers(args):
+DEVEL = 'juju-core'
+STABLE = 'juju-core/1.20'
+LP_BUGS = (
+    'https://api.launchpad.net/devel/{}'
+    '?ws.op=searchTasks'
+    '&status%3Alist=Triaged&status%3Alist=In+Progress'
+    '&importance%3Alist=Critical'
+    '&tags%3Alist=regression&tags%3Alist=ci&tags_combinator=All'
+    )
+
+
+def get_json(uri):
+    data = urllib2.urlopen(uri).read()
+    if data:
+        return json.loads(data)
     return None
 
 
-def get_reason(blockers, args):
-    return 0, ''
+def get_lp_bugs(args):
+    bugs = {}
+    if args.branch == 'master':
+        target = DEVEL
+    else:
+        target = STABLE
+    uri = LP_BUGS.format(target)
+    batch = get_json(uri)
+    if batch:
+        for bug_data in batch['entries']:
+            bug_id = bug_data['self_link'].split('/')[-1]
+            bugs[bug_id] = bug_data
+    return bugs
+
+
+def get_reason(bugs, args):
+    if not bugs:
+        return 0, 'No blocking bugs'
+    fixes_ids = ['$$fixes-{}$$'.format(bug_id) for bug_id in bugs]
+    uri = 'https://api.github.com/repos/juju/juju/issues/{}/comments'.format(
+        args.pull_request)
+    comments = get_json(uri)
+    if comments:
+        for comment in comments:
+            for fid in fixes_ids:
+                if fid in comment['body']:
+                    return 0, 'Matches {}'.format(fid)
+        else:
+            return 1, 'Does not match {}'.format(fixes_ids)
+    return 1, 'Could not get {} comments from github'.format(args.pull_request)
 
 
 def main():
@@ -19,8 +63,8 @@ def main():
     parser.add_argument('branch', help='The branch to merge into.')
     parser.add_argument('pull_request', help='The pull request to be merged')
     args = parser.parse_args()
-    blockers = get_lp_blockers()
-    code, reason = get_reason(blockers, args)
+    bugs = get_lp_bugs(args)
+    code, reason = get_reason(bugs, args)
     if reason:
         print(reason)
     return code
