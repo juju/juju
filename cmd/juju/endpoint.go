94b6987dc39b7bce8d3c6ed8442915276604794f
Katherine Cox <cox.katherine.e@gmail.com>
2014-07-29 11:15:32 -0500

    Simplified getting the address to return, it's always the first one.

--- cmd/juju/endpoint.go
+++ cmd/juju/endpoint.go
@@ -4,9 +4,6 @@
 package main
 
 import (
-	"bytes"
-	"net"
-
 	"github.com/juju/cmd"
 	"launchpad.net/gnuflag"
 
@@ -22,8 +19,7 @@ type EndpointCommand struct {
 }
 
 const endpointDoc = `
-Returns a list of the API servers formatted as host:port
-Default output format returns an api server per line.
+Returns the address of the current API server formatted as host:port.
 
 Examples:
   $ juju api-endpoints
@@ -35,14 +31,14 @@ func (c *EndpointCommand) Info() *cmd.Info {
 	return &cmd.Info{
 		Name:    "api-endpoints",
 		Args:    "",
-		Purpose: "Print the API server addresses",
+		Purpose: "Print the API server address",
 		Doc:     endpointDoc,
 	}
 }
 
 func (c *EndpointCommand) SetFlags(f *gnuflag.FlagSet) {
 	c.out.AddFlags(f, "smart", cmd.DefaultFormatters)
-	f.BoolVar(&c.refresh, "refresh", false, "connect to the API to ensure up-to-date endpoint locations")
+	f.BoolVar(&c.refresh, "refresh", false, "connect to the API to ensure an up-to-date endpoint location")
 }
 
 // Print out the addresses of the API server endpoints.
@@ -51,72 +47,8 @@ func (c *EndpointCommand) Run(ctx *cmd.Context) error {
 	if err != nil {
 		return err
 	}
-	addresses, err := c.postprocessAddresses(apiendpoint.Addresses)
-	if err != nil {
-		return err
-	}
-	return c.out.Write(ctx, addresses)
-}
-
-// postprocessAddresses orders and filters addresses as wanted.
-func (c *EndpointCommand) postprocessAddresses(addresses []string) ([]string, error) {
-	var out []string
-	for _, addr := range addresses {
-		epIP, err := newEndpointIP(addr)
-		if err != nil {
-			return nil, err
-		}
-		// So far we're only interested in public IPv4 addresses.
-		// Other filters later depend on flags, e.g. IPv6 or private
-		// addresses.
-		if epIP.isPublicV4() {
-			out = append(out, addr)
-		}
-	}
-	return out, nil
-}
-
-var (
-	IPv4StartPrivateA = net.IPv4(10, 0, 0, 0)
-	IPv4EndPrivateA   = net.IPv4(10, 255, 255, 255)
-	IPv4StartPrivateB = net.IPv4(172, 16, 0, 0)
-	IPv4EndPrivateB   = net.IPv4(172, 31, 255, 255)
-	IPv4StartPrivateC = net.IPv4(192, 168, 0, 0)
-	IPv4EndPrivateC   = net.IPv4(192, 168, 255, 255)
-)
-
-// endpointIP is the IP address of an endpoint and provides
-// convenience methods for simpler filtering.
-type endpointIP struct {
-	ip net.IP
-}
-
-// newEndpointIP creates an endpoint IP out of the given address.
-func newEndpointIP(addr string) (*endpointIP, error) {
-	tcpAddr, err := net.ResolveTCPAddr("tcp", addr)
-	if err != nil {
-		return nil, err
-	}
-	return &endpointIP{tcpAddr.IP}, nil
-}
-
-// isPublicV4 returns true if the address is IPv4 and public.
-func (ep *endpointIP) isPublicV4() bool {
-	if ep.ip.To4() == nil {
-		return false
-	}
-	if ep.isInRange(IPv4StartPrivateA, IPv4EndPrivateA) ||
-		ep.isInRange(IPv4StartPrivateB, IPv4EndPrivateB) ||
-		ep.isInRange(IPv4StartPrivateC, IPv4EndPrivateC) {
-		return false
-	}
-	return true
-}
-
-// isInRange checks if the endpoint address is in a given range.
-func (ep *endpointIP) isInRange(start, end net.IP) bool {
-	if bytes.Compare(ep.ip, start) >= 0 && bytes.Compare(ep.ip, end) <= 0 {
-		return true
-	}
-	return false
+	// We rely on the fact that the returned API endpoint always
+	// has the wanted address as first address.
+	address := apiendpoint.Addresses[0]
+	return c.out.Write(ctx, address)
 }
