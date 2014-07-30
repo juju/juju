ce1ac7e28092b70d5aee46393aa8234fcb1c6b17
Curtis Hovey <curtis@canonical.com>
2014-07-30 08:48:46 -0400

    Fix tests broken by uncaffeinated change to rules.

--- test_check_blockers.py
+++ test_check_blockers.py
@@ -4,6 +4,10 @@ from mock import patch
 import check_blockers
 
 
+JUJUBOT_USER = {'login': 'jujubot', 'id': 7779494}
+OTHER_USER = {'login': 'user', 'id': 1}
+
+
 class CheckBlockers(TestCase):
 
     def test_parse_args(self):
@@ -71,7 +75,7 @@ class CheckBlockers(TestCase):
     def test_get_reason_with_blockers_no_match(self):
         args = check_blockers.parse_args(['master', '17'])
         with patch('check_blockers.get_json') as gj:
-            gj.return_value = [{'body': '$$merge$$'}]
+            gj.return_value = [{'body': '$$merge$$', 'user': OTHER_USER}]
             bugs = {'98765': {'self_link': 'https://lp/j/98765'}}
             code, reason = check_blockers.get_reason(bugs, args)
             self.assertEqual(1, code)
@@ -81,9 +85,20 @@ class CheckBlockers(TestCase):
         args = check_blockers.parse_args(['master', '17'])
         with patch('check_blockers.get_json') as gj:
             gj.return_value = [
-                {'body': '$$merge$$'},
-                {'body': 'la la $$fixes-98765$$ ha ha'}]
+                {'body': '$$merge$$', 'user': OTHER_USER},
+                {'body': 'la la $$fixes-98765$$ ha ha', 'user': OTHER_USER}]
             bugs = {'98765': {'self_link': 'https://lp/j/98765'}}
             code, reason = check_blockers.get_reason(bugs, args)
             self.assertEqual(0, code)
             self.assertEqual("Matches $$fixes-98765$$", reason)
+
+    def test_get_reason_with_blockers_with_jujubot_comment(self):
+        args = check_blockers.parse_args(['master', '17'])
+        with patch('check_blockers.get_json') as gj:
+            gj.return_value = [
+                {'body': '$$merge$$', 'user': OTHER_USER},
+                {'body': 'la la $$fixes-98765$$ ha ha', 'user': JUJUBOT_USER}]
+            bugs = {'98765': {'self_link': 'https://lp/j/98765'}}
+            code, reason = check_blockers.get_reason(bugs, args)
+            self.assertEqual(1, code)
+            self.assertEqual("Does not match ['$$fixes-98765$$']", reason)
