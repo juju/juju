57bb51a8ab3b6032f377627a003765aa315a6f51
Dave Cheney <dave@cheney.net>
2014-07-31 16:30:52 +1000

    Make FakeAuthorizer.AuthClient logic the same as apiserver.srvRoot

--- state/apiserver/keymanager/keymanager_test.go
+++ state/apiserver/keymanager/keymanager_test.go
@@ -39,7 +39,6 @@ func (s *keyManagerSuite) SetUpTest(c *gc.C) {
 	s.authoriser = apiservertesting.FakeAuthorizer{
 		Tag:      names.NewUserTag("admin"),
 		LoggedIn: true,
-		Client:   true,
 	}
 	var err error
 	s.keymanager, err = keymanager.NewKeyManagerAPI(s.State, s.resources, s.authoriser)
@@ -62,7 +61,7 @@ func (s *keyManagerSuite) TestNewKeyManagerAPIAcceptsEnvironManager(c *gc.C) {
 
 func (s *keyManagerSuite) TestNewKeyManagerAPIRefusesNonClient(c *gc.C) {
 	anAuthoriser := s.authoriser
-	anAuthoriser.Client = false
+	anAuthoriser.Tag = names.NewUnitTag("mysql/0")
 	endPoint, err := keymanager.NewKeyManagerAPI(s.State, s.resources, anAuthoriser)
 	c.Assert(endPoint, gc.IsNil)
 	c.Assert(err, gc.ErrorMatches, "permission denied")
@@ -70,7 +69,7 @@ func (s *keyManagerSuite) TestNewKeyManagerAPIRefusesNonClient(c *gc.C) {
 
 func (s *keyManagerSuite) TestNewKeyManagerAPIRefusesNonEnvironManager(c *gc.C) {
 	anAuthoriser := s.authoriser
-	anAuthoriser.Client = false
+	anAuthoriser.Tag = names.NewMachineTag("99")
 	anAuthoriser.MachineAgent = true
 	endPoint, err := keymanager.NewKeyManagerAPI(s.State, s.resources, anAuthoriser)
 	c.Assert(endPoint, gc.IsNil)
@@ -139,7 +138,6 @@ func (s *keyManagerSuite) TestAddKeys(c *gc.C) {
 
 func (s *keyManagerSuite) TestAddJujuSystemKey(c *gc.C) {
 	anAuthoriser := s.authoriser
-	anAuthoriser.Client = false
 	anAuthoriser.EnvironManager = true
 	anAuthoriser.Tag = names.NewMachineTag("0")
 	var err error
@@ -167,7 +165,6 @@ func (s *keyManagerSuite) TestAddJujuSystemKey(c *gc.C) {
 
 func (s *keyManagerSuite) TestAddJujuSystemKeyNotMachine(c *gc.C) {
 	anAuthoriser := s.authoriser
-	anAuthoriser.Client = false
 	anAuthoriser.EnvironManager = true
 	anAuthoriser.Tag = names.NewUnitTag("wordpress/0")
 	var err error
--- state/apiserver/testing/fakeauthorizer.go
+++ state/apiserver/testing/fakeauthorizer.go
@@ -16,7 +16,6 @@ type FakeAuthorizer struct {
 	EnvironManager bool
 	MachineAgent   bool
 	UnitAgent      bool
-	Client         bool
 	Entity         state.Entity
 }
 
@@ -33,6 +32,34 @@ func mustParseTag(tag string) names.Tag {
 	return t
 }
 
+/*
+
+// AuthMachineAgent returns whether the current client is a machine agent.
+func (r *srvRoot) AuthMachineAgent() bool {
+        _, isMachine := r.GetAuthTag().(names.MachineTag)
+        return isMachine
+}
+
+// AuthUnitAgent returns whether the current client is a unit agent.
+func (r *srvRoot) AuthUnitAgent() bool {
+        _, isUnit := r.GetAuthTag().(names.UnitTag)
+        return isUnit
+}
+
+// AuthOwner returns whether the authenticated user's tag matches the
+// given entity tag.
+func (r *srvRoot) AuthOwner(tag string) bool {
+        return r.GetAuthTag().String() == tag
+}
+
+// AuthEnvironManager returns whether the authenticated user is a
+// machine with running the ManageEnviron job.
+func (r *srvRoot) AuthEnvironManager() bool {
+        return isMachineWithJob(r.entity, state.JobManageEnviron)
+}
+
+*/
+
 func (fa FakeAuthorizer) AuthEnvironManager() bool {
 	return fa.EnvironManager
 }
@@ -45,8 +72,11 @@ func (fa FakeAuthorizer) AuthUnitAgent() bool {
 	return fa.UnitAgent
 }
 
+// AuthClient returns whether the authenticated entity is a client
+// user.
 func (fa FakeAuthorizer) AuthClient() bool {
-	return fa.Client
+	_, isUser := fa.GetAuthTag().(names.UserTag)
+	return isUser
 }
 
 func (fa FakeAuthorizer) GetAuthTag() names.Tag {
