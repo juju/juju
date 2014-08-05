36836b023a0d433902682edef537a8e18c9a11ac
Gabriel Adrian Samfira <gsamfira@cloudbasesolutions.com>
2014-08-06 01:38:12 +0300

    Replace rsyslog for non-bootstrap agents
    This PR uses a fork of the standard syslog package to stream logs directly to state machine rsyslog daemon
    
    update tls config
    
    * ensureCertificates now adds local IPs to subjectAltName. This allows clients to validate the server when connecting via IP
    * fixed tls config passed to the client

--- worker/rsyslog/worker.go
+++ worker/rsyslog/worker.go
@@ -4,15 +4,21 @@
 package rsyslog
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
+	"io"
+	"net"
 	"os"
 	"os/user"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/juju/errors"
 	"github.com/juju/loggo"
 	"github.com/juju/names"
+	rsyslog "github.com/juju/syslog"
 	"github.com/juju/utils"
 
 	"github.com/juju/juju/agent"
@@ -29,6 +35,7 @@ var logger = loggo.GetLogger("juju.worker.rsyslog")
 var (
 	rsyslogConfDir = "/etc/rsyslog.d"
 	logDir         = agent.DefaultLogDir
+	syslogTargets  = []*rsyslog.Writer{}
 )
 
 // RsyslogMode describes how to configure rsyslog.
@@ -69,7 +76,7 @@ var _ worker.NotifyWatchHandler = (*RsyslogConfigHandler)(nil)
 // on changes. The worker will remove the configuration file
 // on teardown.
 func NewRsyslogConfigWorker(st *apirsyslog.State, mode RsyslogMode, tag names.Tag, namespace string, stateServerAddrs []string) (worker.Worker, error) {
-	if version.Current.OS == version.Windows {
+	if version.Current.OS == version.Windows && mode == RsyslogModeAccumulate {
 		return worker.NewNoOpWorker(), nil
 	}
 	handler, err := newRsyslogConfigHandler(st, mode, tag, namespace, stateServerAddrs)
@@ -140,6 +147,55 @@ func (h *RsyslogConfigHandler) TearDown() error {
 	return nil
 }
 
+func (h *RsyslogConfigHandler) composeTLS(caCert string) (*tls.Config, error) {
+	cert := x509.NewCertPool()
+	ok := cert.AppendCertsFromPEM([]byte(caCert))
+	if !ok {
+		return nil, errors.Errorf("Failed to parse rsyslog root certificate")
+	}
+	return &tls.Config{
+		RootCAs: cert,
+	}, nil
+}
+
+func (h *RsyslogConfigHandler) replaceRemoteLogger(caCert string) error {
+	tlsConf, err := h.composeTLS(caCert)
+	if err != nil {
+		return err
+	}
+
+	var newLoggers []*rsyslog.Writer
+	var wrapLoggers []io.Writer
+	for _, j := range h.syslogConfig.StateServerAddresses {
+		parts := strings.Split(j, ":")
+		target := fmt.Sprintf("%s:%d", parts[0], h.syslogConfig.Port)
+		writer, err := rsyslog.Dial("tcp", target, rsyslog.LOG_DEBUG, "juju-"+h.tag.String(), tlsConf)
+		if err != nil {
+			return err
+		}
+		wrapLoggers = append(wrapLoggers, writer)
+		newLoggers = append(newLoggers, writer)
+	}
+	wapper := io.MultiWriter(wrapLoggers...)
+	writer := loggo.NewSimpleWriter(wapper, &loggo.DefaultFormatter{})
+
+	loggo.RemoveWriter("syslog")
+	err = loggo.RegisterWriter("syslog", writer, loggo.TRACE)
+	if err != nil {
+		return err
+	}
+
+	// Close old targets
+	for _, j := range syslogTargets {
+		if err := j.Close(); err != nil {
+			logger.Warningf("Failed to close syslog writer: %s", err)
+		}
+	}
+	// record new targets
+	syslogTargets = newLoggers
+	return nil
+}
+
 func (h *RsyslogConfigHandler) Handle() error {
 	// TODO(dfc)
 	cfg, err := h.st.GetRsyslogConfig(h.tag.String())
@@ -156,18 +212,22 @@ func (h *RsyslogConfigHandler) Handle() error {
 		if err := writeFileAtomic(h.syslogConfig.CACertPath(), []byte(rsyslogCACert), 0644, 0, 0); err != nil {
 			return errors.Annotate(err, "cannot write CA certificate")
 		}
-	}
-	data, err := h.syslogConfig.Render()
-	if err != nil {
-		return errors.Annotate(err, "failed to render rsyslog configuration file")
-	}
-	if err := writeFileAtomic(h.syslogConfig.ConfigFilePath(), []byte(data), 0644, 0, 0); err != nil {
-		return errors.Annotate(err, "failed to write rsyslog configuration file")
-	}
-	logger.Debugf("Reloading rsyslog configuration")
-	if err := restartRsyslog(); err != nil {
-		logger.Errorf("failed to reload rsyslog configuration")
-		return errors.Annotate(err, "cannot restart rsyslog")
+		if err := h.replaceRemoteLogger(rsyslogCACert); err != nil {
+			return err
+		}
+	} else {
+		data, err := h.syslogConfig.Render()
+		if err != nil {
+			return errors.Annotate(err, "failed to render rsyslog configuration file")
+		}
+		if err := writeFileAtomic(h.syslogConfig.ConfigFilePath(), []byte(data), 0644, 0, 0); err != nil {
+			return errors.Annotate(err, "failed to write rsyslog configuration file")
+		}
+		logger.Debugf("Reloading rsyslog configuration")
+		if err := restartRsyslog(); err != nil {
+			logger.Errorf("failed to reload rsyslog configuration")
+			return errors.Annotate(err, "cannot restart rsyslog")
+		}
 	}
 	// Record config values so we don't try again.
 	// Do this last so we recover from intermittent
@@ -191,6 +251,25 @@ var lookupUser = func(username string) (uid, gid int, err error) {
 		return -1, -1, err
 	}
 	return uid, gid, nil
+}
+
+func localIPS() ([]string, error) {
+	var ips []string
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return nil, err
+	}
+	for _, j := range addrs {
+		ip, _, err := net.ParseCIDR(j.String())
+		if err != nil {
+			return nil, err
+		}
+		if ip.IsLoopback() {
+			continue
+		}
+		ips = append(ips, ip.String())
+	}
+	return ips, nil
 }
 
 // ensureCertificates ensures that a CA certificate,
@@ -221,7 +300,15 @@ func (h *RsyslogConfigHandler) ensureCertificates() error {
 	if err != nil {
 		return err
 	}
-	rsyslogCertPEM, rsyslogKeyPEM, err := cert.NewServer(caCertPEM, caKeyPEM, expiry, nil)
+
+	// Add local IPs to SAN. When connecting via IP address, the client will validate
+	// the server against any IP in the subjectAltName. We add all local ips to make sure
+	// this does not cause an error
+	ips, err := localIPS()
+	if err != nil {
+		return err
+	}
+	rsyslogCertPEM, rsyslogKeyPEM, err := cert.NewServer(caCertPEM, caKeyPEM, expiry, ips)
 	if err != nil {
 		return err
 	}
