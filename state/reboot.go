a98d30d864e21fa40d057a7ad2bcc340f36856ec
Gabriel Adrian Samfira <gsamfira@cloudbasesolutions.com>
2014-08-29 11:28:11 +0300

    Add reboot collection to state

---
+++ state/reboot.go
@@ -0,0 +1,134 @@
+package state
+
+import (
+	"fmt"
+
+	"github.com/juju/errors"
+	// "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"gopkg.in/mgo.v2/txn"
+
+	"github.com/juju/juju/apiserver/params"
+)
+
+var _ RebootFlagSetter = (*Machine)(nil)
+var _ RebootActionGetter = (*Machine)(nil)
+
+// rebootDoc will hold the reboot flag for a machine.
+type rebootDoc struct {
+	Id string `bson:"_id"`
+}
+
+func addRebootDocOps(machineId string) []txn.Op {
+	ops := []txn.Op{{
+		C:      machinesC,
+		Id:     machineId,
+		Assert: notDeadDoc,
+	}, {
+		C:      rebootC,
+		Id:     machineId,
+		Insert: rebootDoc{Id: machineId},
+	}}
+	return ops
+}
+
+func removeRebootDocOps(machineId string) txn.Op {
+	ops := txn.Op{
+		C:      rebootC,
+		Id:     machineId,
+		Remove: true,
+	}
+	return ops
+}
+
+// SetRebootFlag sets the reboot flag of a machine to a boolean value. It will also
+// do a lazy create of a reboot document if needed; i.e. If a document
+// does not exist yet for this machine, it will create it.
+func (m *Machine) SetRebootFlag(flag bool) error {
+	reboot, closer := m.st.getCollection(rebootC)
+	defer closer()
+	buildTxn := func(attempt int) ([]txn.Op, error) {
+		return addRebootDocOps(m.Id()), nil
+	}
+
+	count, err := reboot.FindId(m.Id()).Count()
+	if flag == false {
+		if count == 0 {
+			return nil
+		} else {
+			buildTxn = func(attempt int) ([]txn.Op, error) {
+				op := []txn.Op{
+					removeRebootDocOps(m.Id()),
+				}
+				return op, nil
+			}
+		}
+	}
+
+	// Run the transaction using the state transaction runner.
+	err = m.st.run(buildTxn)
+	if err != nil {
+		return errors.Annotate(err, "Failed to run mongo transaction")
+	}
+	return nil
+}
+
+// GetRebootFlag returns the reboot flag for this machine.
+func (m *Machine) GetRebootFlag() (bool, error) {
+	rebootCol, closer := m.st.getCollection(rebootC)
+	defer closer()
+
+	count, err := rebootCol.FindId(m.Id()).Count()
+	if err != nil {
+		return false, fmt.Errorf("failed to get reboot doc %v: %v", m.Id(), err)
+	}
+	if count == 0 {
+		return false, nil
+	}
+	return true, nil
+}
+
+func (m *Machine) machinesToCareAboutRebootsFor() []string {
+	var possibleIds []string
+	for currentId := m.Id(); currentId != ""; {
+		possibleIds = append(possibleIds, currentId)
+		currentId = ParentId(currentId)
+	}
+	return possibleIds
+}
+
+// ShouldRebootOrShutdown check if the current node should reboot or shutdown
+// If we are a container, and our parent needs to reboot, this should return:
+// ShouldShutdown
+func (m *Machine) ShouldRebootOrShutdown() (params.RebootAction, error) {
+	rebootCol, closer := m.st.getCollection(rebootC)
+	defer closer()
+
+	machines := m.machinesToCareAboutRebootsFor()
+
+	docs := []rebootDoc{}
+	sel := bson.D{{"_id", bson.D{{"$in", machines}}}}
+	if err := rebootCol.Find(sel).All(&docs); err != nil {
+		return params.ShouldDoNothing, errors.Trace(err)
+	}
+
+	iNeedReboot := false
+	for _, val := range docs {
+		if val.Id != m.doc.Id {
+			return params.ShouldShutdown, nil
+		}
+		iNeedReboot = true
+	}
+	if iNeedReboot {
+		return params.ShouldReboot, nil
+	}
+	return params.ShouldDoNothing, nil
+}
+
+type RebootFlagSetter interface {
+	SetRebootFlag(flag bool) error
+}
+
+type RebootActionGetter interface {
+	ShouldRebootOrShutdown() (params.RebootAction, error)
+}
--- state/watcher.go
+++ state/watcher.go
@@ -2049,3 +2049,68 @@ func (w *openedPortsWatcher) merge(ids set.Strings, change watcher.Change) error
 	}
 	return nil
 }
+
+// TODO (gsamfira)
+func (m *Machine) WatchForRebootEvent() (NotifyWatcher, error) {
+	machineIds := m.machinesToCareAboutRebootsFor()
+	machines := set.NewStrings(machineIds...)
+	return newRebootWatcher(m.st, machines), nil
+}
+
+type rebootWatcher struct {
+	commonWatcher
+	machines set.Strings
+	out      chan struct{}
+}
+
+func newRebootWatcher(st *State, machines set.Strings) NotifyWatcher {
+	w := &rebootWatcher{
+		commonWatcher: commonWatcher{st: st},
+		machines:      machines,
+		out:           make(chan struct{}),
+	}
+	go func() {
+		defer w.tomb.Done()
+		defer close(w.out)
+		w.tomb.Kill(w.loop())
+	}()
+	return w
+}
+
+// Changes returns the event channel for the rebootWatcher.
+func (w *rebootWatcher) Changes() <-chan struct{} {
+	return w.out
+}
+
+func (w *rebootWatcher) loop() error {
+	in := make(chan watcher.Change)
+	filter := func(key interface{}) bool {
+		if id, ok := key.(string); ok {
+			if w.machines.Contains(id) {
+				return true
+			}
+		} else {
+			w.tomb.Kill(fmt.Errorf("expected string, got %T", key))
+		}
+		return false
+	}
+	w.st.watcher.WatchCollectionWithFilter(rebootC, in, filter)
+	defer w.st.watcher.UnwatchCollection(rebootC, in)
+	out := w.out
+	for {
+		select {
+		case <-w.tomb.Dying():
+			return tomb.ErrDying
+		case <-w.st.watcher.Dead():
+			return stateWatcherDeadError(w.st.watcher.Err())
+		case ch := <-in:
+			if _, ok := collect(ch, in, w.tomb.Dying()); !ok {
+				return tomb.ErrDying
+			}
+			out = w.out
+		case out <- struct{}{}:
+			out = nil
+
+		}
+	}
+}
