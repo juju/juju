97785cf4ba2947bd8ee623bec6cec0af32b456ef
Andrew Wilkins <axwalk@gmail.com>
2014-09-04 09:35:13 +0800

    api/base/testing: add common FacadeCaller patcher
    And use it in usermanager, uniter and provisioner.

--- api/provisioner/export_test.go
+++ api/provisioner/export_test.go
@@ -4,25 +4,12 @@
 package provisioner
 
 import (
-	"github.com/juju/juju/api/base"
+	"github.com/juju/juju/api/base/testing"
 )
 
 // PatchFacadeCall patches the State's facade such that
 // FacadeCall method calls are diverted to the provided
 // function.
-func PatchFacadeCall(p patcher, st *State, f func(request string, params, response interface{}) error) {
-	p.PatchValue(&st.facade, &facadeWrapper{st.facade, f})
-}
-
-type patcher interface {
-	PatchValue(dest, value interface{})
-}
-
-type facadeWrapper struct {
-	base.FacadeCaller
-	facadeCall func(request string, params, response interface{}) error
-}
-
-func (f *facadeWrapper) FacadeCall(request string, params, response interface{}) error {
-	return f.facadeCall(request, params, response)
+func PatchFacadeCall(p testing.Patcher, st *State, f func(request string, params, response interface{}) error) {
+	testing.PatchFacadeCall(p, &st.facade, f)
 }
--- api/uniter/export_test.go
+++ api/uniter/export_test.go
@@ -2,7 +2,9 @@
 // Licensed under the AGPLv3, see LICENCE file for details.
 package uniter
 
-import "github.com/juju/juju/api/base"
+import (
+	"github.com/juju/juju/api/base/testing"
+)
 
 var NewSettings = newSettings
 
@@ -12,26 +14,8 @@ var NewSettings = newSettings
 // It can also return an error to have the FacadeCall return an error.
 // The function returned by PatchResponses is a cleanup function that returns
 // the client to its original state.
-func PatchUnitResponse(u *Unit, responseFunc func(interface{}) error) func() {
-	orig := u.st.facade
-	u.st.facade = &resultCaller{responseFunc}
-	return func() {
-		u.st.facade = orig
-	}
-}
-
-type resultCaller struct {
-	setResult func(interface{}) error
-}
-
-func (f *resultCaller) FacadeCall(request string, params, response interface{}) error {
-	return f.setResult(response)
-}
-
-func (f *resultCaller) BestAPIVersion() int {
-	return 0
-}
-
-func (f *resultCaller) RawAPICaller() base.APICaller {
-	return nil
+func PatchUnitResponse(p testing.Patcher, u *Unit, responseFunc func(interface{}) error) {
+	testing.PatchFacadeCall(p, &u.st.facade, func(request string, params, response interface{}) error {
+		return responseFunc(response)
+	})
 }
