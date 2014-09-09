7bfd014eec18549ad74613b8d67ad6d51326fc20
Ian Booth <ian.booth@canonical.com>
2014-09-09 14:58:04 +1000

    Fix a manual test

--- environs/manual/bootstrap_test.go
+++ environs/manual/bootstrap_test.go
@@ -71,7 +71,7 @@ func (s *bootstrapSuite) getArgs(c *gc.C) manual.BootstrapArgs {
 		DataDir:       "/var/lib/juju",
 		Environ:       s.env,
 		PossibleTools: toolsList,
-		Series:        coretesting.FakeDefaultSeries,
+		Series:        toolsList[0].Version.Series,
 		HardwareCharacteristics: &instance.HardwareCharacteristics{
 			Arch: &arch,
 		},
