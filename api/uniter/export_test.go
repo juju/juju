235ba9262f4ca1a8516622fab9bd7096d05c4ece
Domas Monkus <domas@monkus.lt>
2014-09-16 11:19:24 +0300

    Added expected request parameter to PatchUnitResponse function.

--- api/uniter/export_test.go
+++ api/uniter/export_test.go
@@ -3,6 +3,8 @@
 package uniter
 
 import (
+	"fmt"
+
 	"github.com/juju/juju/api/base/testing"
 )
 
@@ -11,11 +13,16 @@ var NewSettings = newSettings
 // PatchResponses changes the internal FacadeCaller to one that lets you return
 // canned results. The responseFunc will get the 'response' interface object,
 // and can set attributes of it to fix the response to the caller.
-// It can also return an error to have the FacadeCall return an error.
+// It can also return an error to have the FacadeCall return an error. The expected
+// request is specified using the expectedRequest parameter. If the request name does
+// not match, the function panics.
 // The function returned by PatchResponses is a cleanup function that returns
 // the client to its original state.
-func PatchUnitResponse(p testing.Patcher, u *Unit, responseFunc func(interface{}) error) {
+func PatchUnitResponse(p testing.Patcher, u *Unit, expectedRequest string, responseFunc func(interface{}) error) {
 	testing.PatchFacadeCall(p, &u.st.facade, func(request string, params, response interface{}) error {
+		if request != expectedRequest {
+			panic(fmt.Errorf("unexpected request %q received - expecting %q", request, expectedRequest))
+		}
 		return responseFunc(response)
 	})
 }
