8237e4d2f93080bd3b10aef024213667fce6b956
mattyw <gh@mattyw.net>
2014-09-24 17:18:05 +0100

    added benchmarks for metrics

--- state/bench_test.go
+++ state/bench_test.go
@@ -4,6 +4,8 @@
 package state_test
 
 import (
+	"time"
+
 	gc "gopkg.in/check.v1"
 
 	"github.com/juju/juju/state"
@@ -45,6 +47,68 @@ func (*BenchmarkSuite) BenchmarkAddAndAssignUnit(c *gc.C) {
 		unit, err := svc.AddUnit()
 		c.Assert(err, gc.IsNil)
 		err = s.State.AssignUnit(unit, state.AssignClean)
+		c.Assert(err, gc.IsNil)
+	}
+}
+
+func (*BenchmarkSuite) BenchmarkAddMetrics(c *gc.C) {
+	metricsperBatch := 100
+	batches := 10
+	var s ConnSuite
+	s.SetUpSuite(c)
+	defer s.TearDownSuite(c)
+	s.SetUpTest(c)
+	defer s.TearDownTest(c)
+	now := time.Now()
+	metrics := make([]state.Metric, metricsperBatch)
+	for i, _ := range metrics {
+		metrics[i] = state.Metric{
+			Key:         "metricKey",
+			Value:       "keyValue",
+			Time:        now,
+			Credentials: []byte("creds"),
+		}
+	}
+	charm := s.AddTestingCharm(c, "wordpress")
+	svc := s.AddTestingService(c, "wordpress", charm)
+	unit, err := svc.AddUnit()
+	c.Assert(err, gc.IsNil)
+	serviceCharmURL, _ := svc.CharmURL()
+	err = unit.SetCharmURL(serviceCharmURL)
+	c.Assert(err, gc.IsNil)
+	c.Assert(err, gc.IsNil)
+	c.ResetTimer()
+	for i := 0; i < c.N; i++ {
+		for n := 0; n < batches; n++ {
+			_, err := unit.AddMetrics(now, metrics)
+			c.Assert(err, gc.IsNil)
+		}
+	}
+}
+
+func (*BenchmarkSuite) BenchmarkCleanupMetrics(c *gc.C) {
+	var s ConnSuite
+	s.SetUpSuite(c)
+	defer s.TearDownSuite(c)
+	s.SetUpTest(c)
+	defer s.TearDownTest(c)
+	oldTime := time.Now().Add(-(time.Hour * 25))
+	charm := s.AddTestingCharm(c, "wordpress")
+	svc := s.AddTestingService(c, "wordpress", charm)
+	unit, err := svc.AddUnit()
+	c.Assert(err, gc.IsNil)
+	serviceCharmURL, _ := svc.CharmURL()
+	err = unit.SetCharmURL(serviceCharmURL)
+	c.Assert(err, gc.IsNil)
+	c.ResetTimer()
+	for i := 0; i < c.N; i++ {
+		for i := 0; i < 50; i++ {
+			m, err := unit.AddMetrics(oldTime, []state.Metric{{}})
+			c.Assert(err, gc.IsNil)
+			err = m.SetSent()
+			c.Assert(err, gc.IsNil)
+		}
+		err := s.State.CleanupOldMetrics()
 		c.Assert(err, gc.IsNil)
 	}
 }
