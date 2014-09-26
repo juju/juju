b8629075da0c7abc224760594cc94e387b222d91
Eric Snow <ericsnowcurrently@gmail.com>
2014-09-25 18:31:16 -0600

    Document some constants and eliminate the global vars.

--- state/backups/testing/metadata.go
+++ state/backups/testing/metadata.go
@@ -10,19 +10,22 @@ import (
 )
 
 const (
-	EnvID    = "49db53ac-a42f-4ab2-86e1-0c6fa0fec762"
-	Machine  = "0"
+	// ID is the backups ID used by the metadata helper functions.
+	ID = "49db53ac-a42f-4ab2-86e1-0c6fa0fec762.20140924-010319"
+	// EnvID is the env ID used by metadata helper functions.
+	EnvID = "49db53ac-a42f-4ab2-86e1-0c6fa0fec762"
+	// Machine is the machine ID used by metadata helper functions.
+	Machine = "0"
+	// Hostname is the hostname used by metadata helper functions.
 	Hostname = "main-host"
-	Notes    = ""
-	Size     = 10
+	// Notes is the notes value used by metadata helper functions.
+	Notes = ""
+	// Size is the size used by metadata helper functions.
+	Size = 10
+	// Checksum is the checksum used by metadata helper functions.
 	Checksum = "787b8915389d921fa23fb40e16ae81ea979758bf"
-)
-
-var (
-	ID       = EnvID + ".20140924-010319"
+	// CsFormat is the checksum format used by metadata helper functions.
 	CsFormat = metadata.ChecksumFormat
-	Started  = time.Now().UTC()
-	Finished = Started.Add(time.Minute)
 )
 
 // NewMetadata returns a Metadata to use for testing.
@@ -36,15 +39,17 @@ func NewMetadata() *metadata.Metadata {
 // NewMetadataStarted returns a Metadata to use for testing.
 func NewMetadataStarted(id, notes string) *metadata.Metadata {
 	origin := metadata.NewOrigin(EnvID, Machine, Hostname)
+	started := time.Now().UTC()
 
-	meta := metadata.NewMetadata(*origin, notes, &Started)
+	meta := metadata.NewMetadata(*origin, notes, &started)
 	meta.SetID(id)
 	return meta
 }
 
 // FinishMetadata finishes a metadata with test values.
 func FinishMetadata(meta *metadata.Metadata) {
-	meta.Finish(Size, Checksum, CsFormat, &Finished)
+	finished := meta.Started().Add(time.Minute)
+	meta.Finish(Size, Checksum, CsFormat, &finished)
 }
 
 // UpdateNotes derives a new Metadata with new notes.
