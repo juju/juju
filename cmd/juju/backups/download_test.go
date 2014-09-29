e626166084d15f08753c8ce423a6f44b110b1e31
Eric Snow <ericsnowcurrently@gmail.com>
2014-09-29 09:17:18 -0600

    Add the backups download subcommand.

---
+++ cmd/juju/backups/download_test.go
@@ -0,0 +1,108 @@
+// Copyright 2014 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package backups_test
+
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"github.com/juju/cmd/cmdtesting"
+	"github.com/juju/errors"
+	gc "gopkg.in/check.v1"
+
+	"github.com/juju/juju/apiserver/params"
+	"github.com/juju/juju/cmd/juju/backups"
+	"github.com/juju/juju/testing"
+)
+
+type downloadSuite struct {
+	BaseBackupsSuite
+	subcommand     *backups.DownloadCommand
+	data           string
+	downloadResult params.BackupsDownloadResult
+}
+
+var _ = gc.Suite(&downloadSuite{})
+
+func (s *downloadSuite) SetUpTest(c *gc.C) {
+	s.BaseBackupsSuite.SetUpTest(c)
+	s.subcommand = &backups.DownloadCommand{}
+
+	s.data = "<compressed archive data>"
+	s.downloadResult.ID = s.metaresult.ID
+	s.downloadResult.Archive = ioutil.NopCloser(bytes.NewBufferString(s.data))
+}
+
+func (s *downloadSuite) TearDownTest(c *gc.C) {
+	filename := s.subcommand.ResolveFilename()
+	err := os.Remove(filename)
+	if !os.IsNotExist(err) {
+		c.Check(err, gc.IsNil)
+	}
+
+	s.BaseBackupsSuite.TearDownTest(c)
+}
+
+func (s *downloadSuite) setSuccess() *fakeAPIClient {
+	s.subcommand.ID = s.metaresult.ID
+	client := s.BaseBackupsSuite.setSuccess()
+	client.downloadResult = &s.downloadResult
+	return client
+}
+
+func (s *downloadSuite) checkArchive(c *gc.C, filename string) {
+	archive, err := os.Open(filename)
+	c.Assert(err, gc.IsNil)
+	defer archive.Close()
+
+	data, err := ioutil.ReadAll(archive)
+	c.Check(string(data), gc.Equals, s.data)
+}
+
+func (s *downloadSuite) TestHelp(c *gc.C) {
+	ctx, err := testing.RunCommand(c, s.command, "download", "--help")
+	c.Assert(err, gc.IsNil)
+
+	info := s.subcommand.Info()
+	expected := "(?sm)usage: juju backups download [options] " + info.Args + "$.*"
+	expected = strings.Replace(expected, "[", `\[`, -1)
+	c.Check(testing.Stdout(ctx), gc.Matches, expected)
+	expected = "(?sm).*^purpose: " + info.Purpose + "$.*"
+	c.Check(testing.Stdout(ctx), gc.Matches, expected)
+	expected = "(?sm).*^" + info.Doc + "$.*"
+	c.Check(testing.Stdout(ctx), gc.Matches, expected)
+}
+
+func (s *downloadSuite) TestOkay(c *gc.C) {
+	s.setSuccess()
+	ctx := cmdtesting.Context(c)
+	err := s.subcommand.Run(ctx)
+	c.Check(err, gc.IsNil)
+
+	filename := "juju-backup-" + s.metaresult.ID + ".tar.gz"
+	s.checkStd(c, ctx, filename+"\n", "")
+	s.checkArchive(c, filename)
+}
+
+func (s *downloadSuite) TestFilename(c *gc.C) {
+	s.setSuccess()
+	s.subcommand.Filename = "backup.tar.gz"
+	ctx := cmdtesting.Context(c)
+	err := s.subcommand.Run(ctx)
+	c.Check(err, gc.IsNil)
+
+	filename := "backup.tar.gz"
+	s.checkStd(c, ctx, filename+"\n", "")
+	s.checkArchive(c, filename)
+}
+
+func (s *downloadSuite) TestError(c *gc.C) {
+	s.setFailure("failed!")
+	ctx := cmdtesting.Context(c)
+	err := s.subcommand.Run(ctx)
+
+	c.Check(errors.Cause(err), gc.ErrorMatches, "failed!")
+}
