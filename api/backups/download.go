42e63dd83a25b3c485b4b482dadfcb108bd20ddd
Eric Snow <ericsnowcurrently@gmail.com>
2014-09-30 20:11:04 -0600

    Use HTTP requests in the API client for download.

--- api/backups/download.go
+++ api/backups/download.go
@@ -5,21 +5,55 @@ package backups
 
 import (
 	"bytes"
+	"encoding/json"
 	"io"
 	"io/ioutil"
+	"net/http"
 
 	"github.com/juju/errors"
 
+	"github.com/juju/juju/api/base"
 	"github.com/juju/juju/apiserver/params"
 )
 
 // Download implements the API method.
 func (c *Client) Download(id string) (io.ReadCloser, error) {
-	var result params.BackupsDownloadDirectResult
-	args := params.BackupsDownloadArgs{ID: id}
-	if err := c.facade.FacadeCall("DownloadDirect", args, &result); err != nil {
+	if c.http == nil {
+		msg := "API client does not support direct HTTP requests"
+		return nil, errors.NotImplementedf(msg)
+	}
+
+	// Initialize the HTTP request.
+	req, err := c.http.NewHTTPRequest("GET", "backups")
+	if err != nil {
 		return nil, errors.Trace(err)
 	}
-	archive := ioutil.NopCloser(bytes.NewBuffer(result.Data))
-	return archive, nil
+
+	// Serialize the args.
+	req.Header.Set("Content-Type", "application/json")
+	args := params.BackupsDownloadArgs{
+		ID: id,
+	}
+	data, err := json.Marshal(&args)
+	if err != nil {
+		return nil, errors.Annotate(err, "while serializing args")
+	}
+	req.Body = ioutil.NopCloser(bytes.NewBuffer(data))
+
+	// Send the request.
+	resp, err := c.http.SendHTTPRequest(req)
+	if err != nil {
+		return nil, errors.Annotate(err, "while sending HTTP request")
+	}
+
+	// Handle the response.
+	if resp.StatusCode != http.StatusOK {
+		failure, err := base.HandleHTTPFailure(resp)
+		if err != nil {
+			return nil, errors.Trace(err)
+		}
+		return nil, errors.Trace(failure)
+	}
+
+	return resp.Body, nil
 }
--- api/backups/download_test.go
+++ api/backups/download_test.go
@@ -9,7 +9,6 @@ import (
 	gc "gopkg.in/check.v1"
 
 	"github.com/juju/juju/api/backups"
-	"github.com/juju/juju/apiserver/params"
 )
 
 type downloadSuite struct {
@@ -18,25 +17,9 @@ type downloadSuite struct {
 
 var _ = gc.Suite(&downloadSuite{})
 
-func (s *downloadSuite) TestInfo(c *gc.C) {
-	data := []byte("<compressed archive data>")
-	cleanup := backups.PatchClientFacadeCall(s.client,
-		func(req string, paramsIn interface{}, resp interface{}) error {
-			c.Check(req, gc.Equals, "DownloadDirect")
-
-			c.Assert(paramsIn, gc.FitsTypeOf, params.BackupsDownloadArgs{})
-			p := paramsIn.(params.BackupsDownloadArgs)
-			c.Check(p.ID, gc.Equals, "spam")
-
-			if result, ok := resp.(*params.BackupsDownloadDirectResult); ok {
-				result.Data = data
-			} else {
-				c.Fatalf("wrong output structure")
-			}
-			return nil
-		},
-	)
-	defer cleanup()
+func (s *downloadSuite) TestOkay(c *gc.C) {
+	backups.SetHTTP(s.client,
+		&fakeHTTPCaller{Data: "<compressed archive data>"})
 
 	resultArchive, err := s.client.Download("spam")
 	c.Assert(err, gc.IsNil)
