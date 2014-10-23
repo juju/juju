34a74c7075b345e2df8418371ac77970eee07671
Bodie Solomon <binary132@gmail.com>
2014-10-23 15:48:07 -0400

    action do subcommand
      `juju action do ...`

--- cmd/juju/action/action_test.go
+++ cmd/juju/action/action_test.go
@@ -45,6 +45,7 @@ func (s *ActionCommandSuite) TestHelp(c *gc.C) {
 func (s *ActionCommandSuite) checkHelpSubCommands(c *gc.C, ctx *cmd.Context) {
 	var expectedSubCommmands = [][]string{
 		[]string{"defined", "WIP: show actions defined for a service"},
+		[]string{"do", "WIP: queue an action for execution"},
 		[]string{"fetch", "WIP: show results of an action by UUID"},
 		[]string{"help", "show help on a command or other topic"},
 	}
