f40b5500b1b915fa94e53b76ab7d49595d2e9804
Curtis Hovey <curtis@canonical.com>
2014-10-31 17:13:54 -0400

    Handle each cloud and purpose seperately to set the product_name.

--- generate_mirrors.py
+++ generate_mirrors.py
@@ -15,172 +15,170 @@ PROPOSED = 'proposed'
 DEVEL = 'devel'
 TESTING = 'testing'
 PURPOSES = (RELEASED, PROPOSED, DEVEL, TESTING)
-CPC_MIRROR_LIST = [
-    {
-        "mirror": ("https://us-east.manta.joyent.com/"
-                   "cpcjoyentsupport/public/juju-dist/tools"),
-        "path": "streams/v1/com.ubuntu.juju:released:tools.json",
-        "clouds": [
-            {
-                "region": "eu-ams-1",
-                "endpoint": "https://eu-ams-1.api.joyentcloud.com"
-            },
-            {
-                "region": "us-sw-1",
-                "endpoint": "https://us-sw-1.api.joyentcloud.com"
-            },
-            {
-                "region": "us-east-3",
-                "endpoint": "https://us-east-3.api.joyentcloud.com"
-            },
-            {
-                "region": "us-east-2",
-                "endpoint": "https://us-east-2.api.joyentcloud.com"
-            },
-            {
-                "region": "us-east-1",
-                "endpoint": "https://us-east-1.api.joyentcloud.com"
-            },
-            {
-                "region": "us-west-1",
-                "endpoint": "https://us-west-1.api.joyentcloud.com"
-            }
-        ]
-    },
-    {
-        "mirror": "https://juju-dist.s3.amazonaws.com/tools",
-        "path": "streams/v1/com.ubuntu.juju:released:tools.json",
-        "clouds": [
-            {
-                "endpoint": "https://ec2.us-east-1.amazonaws.com",
-                "region": "us-east-1"
-            },
-            {
-                "endpoint": "https://ec2.us-west-1.amazonaws.com",
-                "region": "us-west-1"
-            },
-            {
-                "endpoint": "https://ec2.us-west-2.amazonaws.com",
-                "region": "us-west-2"
-            },
-            {
-                "endpoint": "https://ec2.eu-west-1.amazonaws.com",
-                "region": "eu-west-1"
-            },
-            {
-                "endpoint": "https://ec2.ap-southeast-1.amazonaws.com",
-                "region": "ap-southeast-1"
-            },
-            {
-                "endpoint": "https://ec2.ap-southeast-2.amazonaws.com",
-                "region": "ap-southeast-2"
-            },
-            {
-                "endpoint": "https://ec2.ap-northeast-1.amazonaws.com",
-                "region": "ap-northeast-1"
-            },
-            {
-                "endpoint": "https://ec2.sa-east-1.amazonaws.com",
-                "region": "sa-east-1"
-            },
-            {
-                "endpoint": "https://ec2.eu-central-1.amazonaws.com",
-                "region": "eu-central-1"
-            }
-        ]
-    },
-    {
-        "mirror": ("https://region-a.geo-1.objects.hpcloudsvc.com/"
-                   "v1/60502529753910/juju-dist/tools"),
-        "path": "streams/v1/com.ubuntu.juju:released:tools.json",
-        "clouds": [
-            {
-                "endpoint": ("https://region-a.geo-1.identity.hpcloudsvc.com:"
-                             "35357/v2.0/"),
-                "region": "region-a.geo-1"
-            },
-            {
-                "endpoint": ("https://region-b.geo-1.identity.hpcloudsvc.com:"
-                             "35357/v2.0/"),
-                "region": "region-b.geo-1"
-            }
-        ]
-    },
-    {
-        "mirror": "https://jujutools.blob.core.windows.net/juju-tools/tools",
-        "path": "streams/v1/com.ubuntu.juju:released:tools.json",
-        "clouds": [
-            {
-                "endpoint": "https://core.windows.net/",
-                "region": "Japan East"
-            },
-            {
-                "endpoint": "https://core.windows.net/",
-                "region": "Japan West"
-            },
-            {
-                "endpoint": "https://core.chinacloudapi.cn/",
-                "region": "China East"
-            },
-            {
-                "endpoint": "https://core.chinacloudapi.cn/",
-                "region": "China North"
-            },
-            {
-                "endpoint": "https://core.windows.net/",
-                "region": "East Asia"
-            },
-            {
-                "endpoint": "https://core.windows.net/",
-                "region": "Southeast Asia"
-            },
-            {
-                "endpoint": "https://core.windows.net/",
-                "region": "North Europe"
-            },
-            {
-                "endpoint": "https://core.windows.net/",
-                "region": "West Europe"
-            },
-            {
-                "endpoint": "https://core.windows.net/",
-                "region": "East US"
-            },
-            {
-                "endpoint": "https://core.windows.net/",
-                "region": "East US 2"
-            },
-            {
-                "endpoint": "https://core.windows.net/",
-                "region": "Central US"
-            },
-            {
-                "endpoint": "https://core.windows.net/",
-                "region": "West US"
-            },
-            {
-                "endpoint": "https://management.core.windows.net/",
-                "region": "North Central US"
-            },
-            {
-                "endpoint": "https://management.core.windows.net/",
-                "region": "South Central US"
-            },
-            {
-                "endpoint": "https://management.core.windows.net/",
-                "region": "Australia Southeast"
-            },
-            {
-                "endpoint": "https://management.core.windows.net/",
-                "region": "Australia East"
-            },
-            {
-                "endpoint": "https://management.core.windows.net/",
-                "region": "Brazil South"
-            }
-        ]
-    }
-]
+AWS_MIRROR = {
+    "mirror": "https://juju-dist.s3.amazonaws.com/tools",
+    "path": "PRODUCT_FILE",
+    "clouds": [
+        {
+            "endpoint": "https://ec2.us-east-1.amazonaws.com",
+            "region": "us-east-1"
+        },
+        {
+            "endpoint": "https://ec2.us-west-1.amazonaws.com",
+            "region": "us-west-1"
+        },
+        {
+            "endpoint": "https://ec2.us-west-2.amazonaws.com",
+            "region": "us-west-2"
+        },
+        {
+            "endpoint": "https://ec2.eu-west-1.amazonaws.com",
+            "region": "eu-west-1"
+        },
+        {
+            "endpoint": "https://ec2.ap-southeast-1.amazonaws.com",
+            "region": "ap-southeast-1"
+        },
+        {
+            "endpoint": "https://ec2.ap-southeast-2.amazonaws.com",
+            "region": "ap-southeast-2"
+        },
+        {
+            "endpoint": "https://ec2.ap-northeast-1.amazonaws.com",
+            "region": "ap-northeast-1"
+        },
+        {
+            "endpoint": "https://ec2.sa-east-1.amazonaws.com",
+            "region": "sa-east-1"
+        },
+        {
+            "endpoint": "https://ec2.eu-central-1.amazonaws.com",
+            "region": "eu-central-1"
+        }
+    ]
+}
+AZURE_MIRROR = {
+    "mirror": "https://jujutools.blob.core.windows.net/juju-tools/tools",
+    "path": "streams/v1/com.ubuntu.juju:%s:tools.json",
+    "clouds": [
+        {
+            "endpoint": "https://core.windows.net/",
+            "region": "Japan East"
+        },
+        {
+            "endpoint": "https://core.windows.net/",
+            "region": "Japan West"
+        },
+        {
+            "endpoint": "https://core.chinacloudapi.cn/",
+            "region": "China East"
+        },
+        {
+            "endpoint": "https://core.chinacloudapi.cn/",
+            "region": "China North"
+        },
+        {
+            "endpoint": "https://core.windows.net/",
+            "region": "East Asia"
+        },
+        {
+            "endpoint": "https://core.windows.net/",
+            "region": "Southeast Asia"
+        },
+        {
+            "endpoint": "https://core.windows.net/",
+            "region": "North Europe"
+        },
+        {
+            "endpoint": "https://core.windows.net/",
+            "region": "West Europe"
+        },
+        {
+            "endpoint": "https://core.windows.net/",
+            "region": "East US"
+        },
+        {
+            "endpoint": "https://core.windows.net/",
+            "region": "East US 2"
+        },
+        {
+            "endpoint": "https://core.windows.net/",
+            "region": "Central US"
+        },
+        {
+            "endpoint": "https://core.windows.net/",
+            "region": "West US"
+        },
+        {
+            "endpoint": "https://management.core.windows.net/",
+            "region": "North Central US"
+        },
+        {
+            "endpoint": "https://management.core.windows.net/",
+            "region": "South Central US"
+        },
+        {
+            "endpoint": "https://management.core.windows.net/",
+            "region": "Australia Southeast"
+        },
+        {
+            "endpoint": "https://management.core.windows.net/",
+            "region": "Australia East"
+        },
+        {
+            "endpoint": "https://management.core.windows.net/",
+            "region": "Brazil South"
+        }
+    ]
+}
+HP_MIRROR = {
+    "mirror": ("https://region-a.geo-1.objects.hpcloudsvc.com/"
+               "v1/60502529753910/juju-dist/tools"),
+    "path": "streams/v1/com.ubuntu.juju:released:tools.json",
+    "clouds": [
+        {
+            "endpoint": ("https://region-a.geo-1.identity.hpcloudsvc.com:"
+                         "35357/v2.0/"),
+            "region": "region-a.geo-1"
+        },
+        {
+            "endpoint": ("https://region-b.geo-1.identity.hpcloudsvc.com:"
+                         "35357/v2.0/"),
+            "region": "region-b.geo-1"
+        }
+    ]
+}
+JOYENT_MIRROR = {
+    "mirror": ("https://us-east.manta.joyent.com/"
+               "cpcjoyentsupport/public/juju-dist/tools"),
+    "path": "streams/v1/com.ubuntu.juju:released:tools.json",
+    "clouds": [
+        {
+            "region": "eu-ams-1",
+            "endpoint": "https://eu-ams-1.api.joyentcloud.com"
+        },
+        {
+            "region": "us-sw-1",
+            "endpoint": "https://us-sw-1.api.joyentcloud.com"
+        },
+        {
+            "region": "us-east-3",
+            "endpoint": "https://us-east-3.api.joyentcloud.com"
+        },
+        {
+            "region": "us-east-2",
+            "endpoint": "https://us-east-2.api.joyentcloud.com"
+        },
+        {
+            "region": "us-east-1",
+            "endpoint": "https://us-east-1.api.joyentcloud.com"
+        },
+        {
+            "region": "us-west-1",
+            "endpoint": "https://us-west-1.api.joyentcloud.com"
+        }
+    ]
+}
 
 
 def generate_mirrors_file(updated, streams_path,
@@ -220,9 +218,21 @@ def generate_cpc_mirrors_file(updated, streams_path,
     }
     for purpose in PURPOSES:
         product_name = "com.ubuntu.juju:%s:tools" % purpose
+        product_path = "streams/v1/%s.json" % product_name
         if verbose:
-            print("Adding %s at %s to mirrors.json" % (product_name, updated))
-        mirrors['mirrors'][product_name] = list(CPC_MIRROR_LIST)
+            print(
+                "Adding %s at %s to cpc-mirrors.json" %
+                (product_path, updated))
+        aws_mirror = dict(AWS_MIRROR)
+        aws_mirror['path'] = product_path
+        azure_mirror = dict(AZURE_MIRROR)
+        azure_mirror['path'] = product_path
+        hp_mirror = dict(HP_MIRROR)
+        hp_mirror['path'] = product_path
+        joyent_mirror = dict(JOYENT_MIRROR)
+        joyent_mirror['path'] = product_path
+        mirrors['mirrors'][product_name] = [
+            aws_mirror, azure_mirror, hp_mirror, joyent_mirror]
     data = json.dumps(mirrors)
     file_path = '%s/cpc-mirrors.json' % streams_path
     if not dry_run:
--- tests/test_generate_mirrors.py
+++ tests/test_generate_mirrors.py
@@ -28,12 +28,12 @@ class GenerateMirrors(TestCase):
         expected_produts = sorted(
             'com.ubuntu.juju:%s:tools' % p for p in PURPOSES)
         self.assertEqual(expected_produts, sorted(data['mirrors'].keys()))
-        first_mirror = data['mirrors']['com.ubuntu.juju:released:tools'][0]
-        self.assertEqual('content-download', first_mirror['datatype'])
-        self.assertEqual('mirrors:1.0', first_mirror['format'])
-        self.assertEqual('streams/v1/cpc-mirrors.json', first_mirror['path'])
+        first_purpose = data['mirrors']['com.ubuntu.juju:released:tools'][0]
+        self.assertEqual('content-download', first_purpose['datatype'])
+        self.assertEqual('mirrors:1.0', first_purpose['format'])
+        self.assertEqual('streams/v1/cpc-mirrors.json', first_purpose['path'])
         expected_updated = updated.strftime('%Y%m%d')
-        self.assertEqual(expected_updated, first_mirror['updated'])
+        self.assertEqual(expected_updated, first_purpose['updated'])
 
     def test_cpc_generate_mirrors_file(self):
         updated = datetime.datetime.utcnow()
@@ -51,6 +51,22 @@ class GenerateMirrors(TestCase):
         expected_produts.extend(['format', 'updated'])
         self.assertEqual(expected_produts, sorted(data['mirrors'].keys()))
         expected_updated = updated.strftime('%a, %d %b %Y %H:%M:%S -0000')
+        self.assertEqual(expected_updated, data['mirrors']['updated'])
+        proposed_mirrors = data['mirrors']['com.ubuntu.juju:proposed:tools']
         self.assertEqual(
-            expected_updated,
-            data['mirrors']['updated'])
+            'streams/v1/com.ubuntu.juju:proposed:tools.json',
+            proposed_mirrors[0]['path'])
+        self.assertEqual(
+            'https://juju-dist.s3.amazonaws.com/tools',
+            proposed_mirrors[0]['mirror'])
+        self.assertEqual(
+            'https://jujutools.blob.core.windows.net/juju-tools/tools',
+            proposed_mirrors[1]['mirror'])
+        self.assertEqual(
+            ("https://region-a.geo-1.objects.hpcloudsvc.com/"
+             "v1/60502529753910/juju-dist/tools"),
+            proposed_mirrors[2]['mirror'])
+        self.assertEqual(
+            ("https://us-east.manta.joyent.com/"
+             "cpcjoyentsupport/public/juju-dist/tools"),
+            proposed_mirrors[3]['mirror'])
