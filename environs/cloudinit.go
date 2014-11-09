3d0af6d0babf34d5e500157ad9d89ffe7a227eac
John Weldon <johnweldon4@gmail.com>
2014-11-08 17:26:46 -0800

    Fixes go vet errors:
        audit/audit.go:30: constant 3 not a string in call to Logf
        environs/cloudinit.go:165: arg cfg.NumaCtlPreference() for printf verb %q of wrong type: bool

--- environs/cloudinit.go
+++ environs/cloudinit.go
@@ -162,7 +162,7 @@ func FinishMachineConfig(mcfg *cloudinit.MachineConfig, cfg *config.Config) (err
 	if isStateMachineConfig(mcfg) {
 		// Add NUMACTL preference. Needed to work for both bootstrap and high availability
 		// Only makes sense for state server
-		logger.Debugf("Setting numa ctl preference to %q", cfg.NumaCtlPreference())
+		logger.Debugf("Setting numa ctl preference to %t", cfg.NumaCtlPreference())
 		// Unfortunately, AgentEnvironment can only take strings as values
 		mcfg.AgentEnvironment[agent.NumaCtlPreference] = fmt.Sprintf("%v", cfg.NumaCtlPreference())
 	}
