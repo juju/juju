a2b3d11777d649e58af384f0d5762826eea1d5ce
Dave Cheney <dave@cheney.net>
2014-11-12 07:36:51 +1100

    Rename test to follow source.

--- juju_test.go
+++ juju_test.go
@@ -3,6 +3,24 @@
 
 package juju
 
+import (
+	jc "github.com/juju/testing/checkers"
+	gc "gopkg.in/check.v1"
+)
+
+type ConstantsSuite struct{}
+
+var _ = gc.Suite(&ConstantsSuite{})
+
+func (s *ConstantsSuite) TestAnyJobNeedsState(c *gc.C) {
+	c.Assert(AnyJobNeedsState(), jc.IsFalse)
+	c.Assert(AnyJobNeedsState(JobHostUnits), jc.IsFalse)
+	c.Assert(AnyJobNeedsState(JobManageNetworking), jc.IsFalse)
+	c.Assert(AnyJobNeedsState(JobManageStateDeprecated), jc.IsFalse)
+	c.Assert(AnyJobNeedsState(JobManageEnviron), jc.IsTrue)
+	c.Assert(AnyJobNeedsState(JobHostUnits, JobManageEnviron), jc.IsTrue)
+}
+
 var (
 	_ EntityInfo = (*MachineInfo)(nil)
 	_ EntityInfo = (*ServiceInfo)(nil)
