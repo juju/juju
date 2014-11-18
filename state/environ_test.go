500ace893fe7edf6a6d962a184138246cb960a43
Menno Smits <menno.smits@canonical.com>
2014-11-19 10:15:21 +1300

    state: use jc.IsNil throughout EnvironSuite

--- state/environ_test.go
+++ state/environ_test.go
@@ -22,7 +22,7 @@ var _ = gc.Suite(&EnvironSuite{})
 
 func (s *EnvironSuite) TestEnvironment(c *gc.C) {
 	env, err := s.State.Environment()
-	c.Assert(err, gc.IsNil)
+	c.Assert(err, jc.IsNil)
 
 	expectedTag := names.NewEnvironTag(env.UUID())
 	c.Assert(env.Tag(), gc.Equals, expectedTag)
@@ -41,7 +41,7 @@ func (s *EnvironSuite) TestNewEnvironment(c *gc.C) {
 	owner := names.NewUserTag("test@remote")
 
 	env, st, err := s.State.NewEnvironment(cfg, owner)
-	c.Assert(err, gc.IsNil)
+	c.Assert(err, jc.IsNil)
 	defer st.Close()
 
 	envTag := names.NewEnvironTag(uuid)
@@ -58,28 +58,28 @@ func (s *EnvironSuite) TestNewEnvironment(c *gc.C) {
 	// Since the environ tag for the State connection is different,
 	// asking for this environment through FindEntity returns a not found error.
 	env, err = s.State.GetEnvironment(envTag)
-	c.Assert(err, gc.IsNil)
+	c.Assert(err, jc.IsNil)
 	assertEnvMatches(env)
 
 	env, err = st.Environment()
-	c.Assert(err, gc.IsNil)
+	c.Assert(err, jc.IsNil)
 	assertEnvMatches(env)
 
 	_, err = s.State.FindEntity(envTag)
 	c.Assert(err, jc.Satisfies, errors.IsNotFound)
 
 	entity, err := st.FindEntity(envTag)
-	c.Assert(err, gc.IsNil)
+	c.Assert(err, jc.IsNil)
 	c.Assert(entity.Tag(), gc.Equals, envTag)
 
 	// Ensure the environment is functional by adding a machine
 	_, err = st.AddMachine("quantal", state.JobManageEnviron)
-	c.Assert(err, gc.IsNil)
+	c.Assert(err, jc.IsNil)
 }
 
 func (s *EnvironSuite) TestStateServerEnvironment(c *gc.C) {
 	env, err := s.State.StateServerEnvironment()
-	c.Assert(err, gc.IsNil)
+	c.Assert(err, jc.IsNil)
 
 	expectedTag := names.NewEnvironTag(env.UUID())
 	c.Assert(env.Tag(), gc.Equals, expectedTag)
@@ -99,7 +99,7 @@ func (s *EnvironSuite) TestStateServerEnvironmentAccessibleFromOtherEnvironments
 	defer st.Close()
 
 	env, err := st.StateServerEnvironment()
-	c.Assert(err, gc.IsNil)
+	c.Assert(err, jc.IsNil)
 	c.Assert(env.Tag(), gc.Equals, s.envTag)
 	c.Assert(env.Name(), gc.Equals, "testenv")
 	c.Assert(env.Owner(), gc.Equals, s.owner)
@@ -114,15 +114,15 @@ func (s *EnvironSuite) TestStateServerEnvironmentAccessibleFromOtherEnvironments
 // environment config for testing.
 func (s *EnvironSuite) createTestEnvConfig(c *gc.C) (*config.Config, string) {
 	uuid, err := utils.NewUUID()
-	c.Assert(err, gc.IsNil)
+	c.Assert(err, jc.IsNil)
 	uuidStr := uuid.String()
 
 	cfg, err := s.State.EnvironConfig()
-	c.Assert(err, gc.IsNil)
+	c.Assert(err, jc.IsNil)
 	cfg, err = cfg.Apply(map[string]interface{}{
 		"name": "testing",
 		"uuid": uuidStr,
 	})
-	c.Assert(err, gc.IsNil)
+	c.Assert(err, jc.IsNil)
 	return cfg, uuidStr
 }
