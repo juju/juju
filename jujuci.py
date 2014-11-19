544221d3843361d4f425f922af356948d1d49c67
Curtis Hovey <curtis@canonical.com>
2014-11-19 15:14:45 -0500

    added list_files().

--- jujuci.py
+++ jujuci.py
@@ -1,5 +1,6 @@
 """Access Juju CI artifacts and data."""
 
+import fnmatch
 import json
 import urllib2
 
@@ -10,3 +11,12 @@ def get_build_data(jenkins_url, job_name, build='lastSuccessfulBuild'):
         '%s/job/%s/%s/api/json' % (jenkins_url, job_name, build))
     build_data = json.load(build_data)
     return build_data
+
+
+def list_files(build_data, glob='*'):
+    found = []
+    for artifact in build_data['artifacts']:
+        file_name = artifact['fileName']
+        if fnmatch.fnmatch(file_name, glob):
+            found.append(file_name)
+    return found
--- test_jujuci.py
+++ test_jujuci.py
@@ -3,84 +3,101 @@ from mock import patch
 from StringIO import StringIO
 from unittest import TestCase
 
-from jujuci import get_build_data
+from jujuci import (
+    get_build_data,
+    list_files,
+)
 
 
-def make_build_json():
-    return json.dumps({
-        "actions": [],
-        "buildable": True,
-        "builds": [
+def make_build_data():
+    return {
+        "actions": [
             {
-                "number": 2090,
-                "url": "http://juju-ci.vapour.ws:8080/job/build-revision/2090/"
+                "parameters": [
+                    {
+                        "name": "branch",
+                        "value": "gitbranch:master:github.com/juju/juju"
+                    },
+                    {
+                        "name": "revision",
+                        "value": "3c53cf578ef100ba5368661224de4af5da72ee74"
+                    }
+                ]
             },
             {
-                "number": 2089,
-                "url": "http://juju-ci.vapour.ws:8080/job/build-revision/2089/"
-            }
+                "causes": [
+                    {
+                        "shortDescription": "Started by user admin",
+                        "userName": "admin"
+                    }
+                ]
+            },
+            {},
+            {}
         ],
-        "color": "blue",
-        "concurrentBuild": False,
-        "description": "tags: subjects=release",
-        "displayName": "build-revision",
-        "downstreamProjects": [],
-        "firstBuild": {
-            "number": 2089,
-            "url": "http://juju-ci.vapour.ws:8080/job/build-revision/2089/"
-        },
-        "healthReport": [
+        "artifacts": [
             {
-                "description": "Build stability: No recent builds failed.",
-                "iconUrl": "health-80plus.png",
-                "score": 100
+                "displayPath": "buildvars.bash",
+                "fileName": "buildvars.bash",
+                "relativePath": "buildvars.bash"
+            },
+            {
+                "displayPath": "buildvars.json",
+                "fileName": "buildvars.json",
+                "relativePath": "buildvars.json"
+            },
+            {
+                "displayPath": "juju-core_1.22-alpha1.tar.gz",
+                "fileName": "juju-core_1.22-alpha1.tar.gz",
+                "relativePath": "juju-core_1.22-alpha1.tar.gz"
             }
         ],
-        "inQueue": False,
-        "lastBuild": {
-            "number": 2090,
-            "url": "http://juju-ci.vapour.ws:8080/job/build-revision/2090/"
+        "building": False,
+        "builtOn": "",
+        "changeSet": {
+            "items": [],
+            "kind": None
         },
-        "lastCompletedBuild": {
-            "number": 2090,
-            "url": "http://juju-ci.vapour.ws:8080/job/build-revision/2090/"
-        },
-        "lastFailedBuild": None,
-        "lastStableBuild": {
-            "number": 2090,
-            "url": "http://juju-ci.vapour.ws:8080/job/build-revision/2090/"
-        },
-        "lastSuccessfulBuild": {
-            "number": 2090,
-            "url": "http://juju-ci.vapour.ws:8080/job/build-revision/2090/"
-        },
-        "lastUnstableBuild": None,
-        "lastUnsuccessfulBuild": None,
-        "name": "build-revision",
-        "nextBuildNumber": 2091,
-        "property": [],
-        "queueItem": None,
-        "scm": {},
-        "upstreamProjects": [],
-        "url": "http://juju-ci.vapour.ws:8080/job/build-revision/"
-    })
+        "culprits": [],
+        "description": "gitbranch:master:github.com/juju/juju 3c53cf57",
+        "duration": 142986,
+        "fullDisplayName": "build-revision #2102",
+        "id": "2014-11-19_07-35-02",
+        "keepLog": False,
+        "number": 2102,
+        "result": "SUCCESS",
+        "timestamp": 1416382502379,
+        "url": "http://juju-ci.vapour.ws:8080/job/build-revision/2102/"
+    }
 
 
 class JujuCITestCase(TestCase):
 
     def test_get_build_data(self):
-        expected_json = make_build_json()
-        json_io = StringIO(expected_json)
+        expected_data = make_build_data()
+        json_io = StringIO(json.dumps(expected_data))
         with patch('urllib2.urlopen', return_value=json_io) as mock:
             build_data = get_build_data('http://foo:8080', 'bar', '1234')
         mock.assert_called_once(['http://foo:8080/job/bar/1234/api/json'])
-        expected_data = json.loads(expected_json)
         self.assertEqual(expected_data, build_data)
 
     def test_get_build_data_with_default_build(self):
-        expected_json = make_build_json()
-        json_io = StringIO(expected_json)
+        expected_data = make_build_data()
+        json_io = StringIO(json.dumps(expected_data))
         with patch('urllib2.urlopen', return_value=json_io) as mock:
             get_build_data('http://foo:8080', 'bar')
         mock.assert_called_once(
             ['http://foo:8080/job/bar/lastSuccessfulBuild/api/json'])
+
+    def test_list_files_all(self):
+        expected_data = make_build_data()
+        files = list_files(expected_data)
+        self.assertEqual(
+            ['buildvars.bash', 'buildvars.json',
+             'juju-core_1.22-alpha1.tar.gz'],
+            sorted(files))
+
+    def test_list_files_glob_tarball(self):
+        expected_data = make_build_data()
+        files = list_files(expected_data, '*.tar.gz')
+        self.assertEqual(['juju-core_1.22-alpha1.tar.gz'], files)
