d4671b1c7118254ea0306c919366b2629bd40e9b
Andrew Wilkins <axwalk@gmail.com>
2014-11-20 15:05:16 +0800

    worker/diskformatter: moved package doc

--- worker/diskformatter/doc.go
+++
@@ -1,8 +0,0 @@
-// Copyright 2014 Canonical Ltd.
-// Licensed under the AGPLv3, see LICENCE file for details.
-
-// Package diskformatter defines a worker that watches for block devices
-// attached to datastores owned by the unit that runs this worker, and
-// creates filesystems on them as necessary. Each unit agent runs this
-// worker.
-package diskformatter
