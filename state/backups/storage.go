f86e7cb044fd78998770dd872a518c6449bb3f02
Eric Snow <ericsnowcurrently@gmail.com>
2014-11-21 12:39:00 -0700

    Update the package doc comment.

--- state/backups/storage.go
+++ state/backups/storage.go
@@ -19,43 +19,6 @@ import (
 	"github.com/juju/juju/version"
 )
 
-/*
-Backups are not a part of juju state nor of normal state operations.
-However, they certainly are tightly coupled with state (the very
-subject of backups).  This puts backups in an odd position,
-particularly with regard to the storage of backup metadata and
-archives.  As a result, here are a couple concerns worth mentioning.
-
-First, as noted above backup is about state but not a part of state.
-So exposing backup-related methods on State would imply the wrong
-thing.  Thus the backup functionality here in the state package (not
-state/backups) is exposed as functions to which you pass a state
-object.
-
-Second, backup creates an archive file containing a dump of state's
-mongo DB.  Storing backup metadata/archives in mongo is thus a
-somewhat circular proposition that has the potential to cause
-problems.  That may need further attention.
-
-Note that state (and juju as a whole) currently does not have a
-persistence layer abstraction to facilitate separating different
-persistence needs and implementations.  As a consequence, state's
-data, whether about how an environment should look or about existing
-resources within an environment, is dumped essentially straight into
-State's mongo connection.  The code in the state package does not
-make any distinction between the two (nor does the package clearly
-distinguish between state-related abstractions and state-related
-data).
-
-Backup adds yet another category, merely taking advantage of
-State's DB.  In the interest of making the distinction clear, the
-code that directly interacts with State and its DB) lives in this
-file.  As mentioned previously, the functionality here is exposed
-through functions that take State, rather than as methods on State.
-Furthermore, the bulk of the backup-related code, which does not need
-direct interaction with State, lives in the state/backups package.
-*/
-
 // backupIDTimstamp is used to format the timestamp from a backup
 // metadata into a string. The result is used as the first half of the
 // corresponding backup ID.
