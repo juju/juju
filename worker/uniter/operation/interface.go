72391ad3478e2ad7cc014aa70e2d91cbaa87008c
William Reade <fwereade@gmail.com>
2014-11-24 23:11:33 +0100

    add some interface docs

--- worker/uniter/operation/interface.go
+++ worker/uniter/operation/interface.go
@@ -15,6 +15,8 @@ import (
 
 var logger = loggo.GetLogger("juju.worker.uniter.operation")
 
+// Operation encapsulates the stages of the various things the uniter can do,
+// and the state changes that need to be recorded as they happen.
 type Operation interface {
 	String() string
 	Prepare(state State) (*State, error)
@@ -22,12 +24,15 @@ type Operation interface {
 	Commit(state State) (*State, error)
 }
 
+// Executor records and exposes uniter state, and applies suitable changes as
+// operations are run or skipped.
 type Executor interface {
 	State() State
 	Run(Operation) error
 	Skip(Operation) error
 }
 
+// Factory creates operations.
 type Factory interface {
 	NewDeploy(charmURL *corecharm.URL, kind Kind) (Operation, error)
 	NewHook(hookInfo hook.Info) (Operation, error)
@@ -35,35 +40,50 @@ type Factory interface {
 	NewCommands(commands string, relationId int, remoteUnitName string, sendResponse CommandResponseFunc) (Operation, error)
 }
 
+// CommandResponseFunc is for marshalling command responses back to the source
+// of the original request.
 type CommandResponseFunc func(*utilexec.ExecResponse, error)
 
-// Callbacks...
+// Callbacks exposes all the uniter code that's required by the various operations.
+// It's far from cohesive, and fundamentally represents inappropriate coupling, so
+// it's a prime candidate for future refactoring.
 type Callbacks interface {
 
-	// AcquireExecutionLock...
+	// AcquireExecutionLock acquires the machine-level execution lock, and
+	// returns a func that must be called to unlock it. It's used by all the
+	// operations that execute external code.
 	AcquireExecutionLock(message string) (unlock func(), err error)
 
-	// GetRunner...
+	// GetRunner returns a runner for the supplied context; it primarily exists
+	// to allow operations to be tested without shelling out to run actual
+	// external code. It's used by all the operations that execute external
+	// code.
 	GetRunner(ctx context.Context) context.Runner
 
-	// PrepareHook and CommitHook exist so that I can defer worrying about how
-	// to untangle Uniter.relationers from everything else.
+	// PrepareHook and CommitHook exist so that we can defer worrying about how
+	// to untangle Uniter.relationers from everything else. They're only used by
+	// RunHook operations.
 	PrepareHook(info hook.Info) (name string, err error)
 	CommitHook(info hook.Info) error
 
-	// NotifyHook* exist so that I can defer worrying about how to untangle the
-	// callbacks inserted for uniter_test.
+	// NotifyHook* exist so that we can defer worrying about how to untangle the
+	// callbacks inserted for uniter_test. They're only used by RunHook operations.
 	NotifyHookCompleted(string, context.Context)
 	NotifyHookFailed(string, context.Context)
 
 	// FailAction marks the supplied action failed. It exists so we can test
-	// the operation package without involving the API.
+	// the operation package without involving the API. It's only used by
+	// RunActions operations.
 	FailAction(actionId, message string) error
 
 	// GetArchiveInfo is used to find out how to download a charm archive. It
 	// exists so we can test the operation package without involving the API.
+	// It's only used by Deploy operations.
 	GetArchiveInfo(charmURL *corecharm.URL) (charm.BundleInfo, error)
 
-	// SetCurrentCharm...
+	// SetCurrentCharm records intent to deploy a given charm. It must be called
+	// *before* recording local state referencing that charm, to ensure there's
+	// no path by which the state server can legitimately garbage collect that
+	// charm or the service's settings for it. It's only used by Deploy operations.
 	SetCurrentCharm(charmURL *corecharm.URL) error
 }
