e1579b4be0ac8d4b9d257c4a7941b3b994bdd99a
Michael Foord <michael.foord@canonical.com>
2014-11-25 15:36:19 +0000

    Strip out unneeded code

--- state/subnets.go
+++ state/subnets.go
@@ -4,12 +4,9 @@
 package state
 
 import (
-	"fmt"
-
 	"github.com/juju/errors"
 	"github.com/juju/juju/network"
 	jujutxn "github.com/juju/txn"
-	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/txn"
 )
 
@@ -55,64 +52,23 @@ func (s *Subnet) Life() Life {
 	return s.doc.Life
 }
 
-func (s *Subnet) Refresh() error {
-	subnets, closer := s.st.getCollection(subnetsC)
-	defer closer()
-
-	err := subnets.FindId(s.doc.DocID).One(&s.doc)
-	if err == mgo.ErrNotFound {
-		return errors.NotFoundf("subnet %v", s)
-	}
-	if err != nil {
-		return fmt.Errorf("cannot refresh subnet %v: %v", s, err)
-	}
-	return nil
-}
-
 func (s *Subnet) Destroy() (err error) {
 	defer errors.DeferredAnnotatef(&err, "cannot destroy subnet %v", s)
+	if s.doc.Life == Dying {
+		return jujutxn.ErrNoOperations
+	}
+
 	defer func() {
 		if err == nil {
 			// This is a white lie; the document might actually be removed.
 			s.doc.Life = Dying
 		}
 	}()
-	sub := &Subnet{st: s.st, doc: s.doc}
-	buildTxn := func(attempt int) ([]txn.Op, error) {
-		if attempt > 0 {
-			if err := sub.Refresh(); errors.IsNotFound(err) {
-				return nil, jujutxn.ErrNoOperations
-			} else if err != nil {
-				return nil, err
-			}
-		}
-		switch ops, err := sub.destroyOps(); err {
-		case errRefresh:
-		case errAlreadyDying:
-			return nil, jujutxn.ErrNoOperations
-		case nil:
-			return ops, nil
-		default:
-			return nil, err
-		}
-		return nil, jujutxn.ErrTransientFailure
-	}
-	return s.st.run(buildTxn)
-}
 
-func (s *Subnet) destroyOps() ([]txn.Op, error) {
-	// XXX we also need to destroy related addresses
-	if s.doc.Life == Dying {
-		return nil, errAlreadyDying
-	}
 	ops := []txn.Op{{
 		C:      subnetsC,
 		Id:     s.doc.DocID,
 		Remove: true,
 	}}
-	return ops, nil
-}
-
-func (s *Subnet) globalKey() string {
-	return "sn#" + s.doc.CIDR
+	return s.st.runTransaction(ops)
 }
