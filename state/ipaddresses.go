a5461ee5fbc62bd9cb641e1a5be8af1f40da21b5
Michael Foord <michael.foord@canonical.com>
2014-12-01 11:50:13 +0000

    Updated comments for methods

--- state/ipaddresses.go
+++ state/ipaddresses.go
@@ -45,31 +45,43 @@ type ipaddressDoc struct {
 	State       AddressState  `bson:",omitempty"`
 }
 
+// SubnetId returns the ID of the subnet the IP address is associated with. If
+// the address is not associated with a subnet this returns "".
 func (i *IPAddress) SubnetId() string {
 	return i.doc.SubnetId
 }
 
+// MachineId returns the ID of the machine the IP address is associated with. If
+// the address is not associated with a machine this returns "".
 func (i *IPAddress) MachineId() string {
 	return i.doc.MachineId
 }
 
+// InterfaceId returns the ID of the network interface the IP address is
+// associated with. If the address is not associated with a netowrk interface
+// this returns "".
 func (i *IPAddress) InterfaceId() string {
 	return i.doc.InterfaceId
 }
 
+// Value returns the IP address.
 func (i *IPAddress) Value() string {
 	return i.doc.Value
 }
 
+// Type returns the type of the IP address. The IP address will have a type of
+// IPv4, IPv6 or hostname.
 func (i *IPAddress) Type() network.AddressType {
 	return i.doc.Type
 }
 
+// Scope returns the scope of the IP address. If the scope is not set this
+// returns "".
 func (i *IPAddress) Scope() network.Scope {
 	return i.doc.Scope
 }
 
-// Remove removes a dead subnet. If the subnet is not dead it returns an error.
+// Remove removes a no-longer need IP address.
 func (i *IPAddress) Remove() (err error) {
 	defer errors.DeferredAnnotatef(&err, "cannot remove IP address %v", i)
 	ops := []txn.Op{{
