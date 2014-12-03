93f9abab590f70f76b4329e159d53284380fb59c
Eric Snow <ericsnowcurrently@gmail.com>
2014-12-02 18:22:02 -0700

    Use errors.Trace.

--- state/utils/fakes_test.go
+++ state/utils/fakes_test.go
@@ -4,6 +4,8 @@
 package utils_test
 
 import (
+	"github.com/juju/errors"
+
 	"github.com/juju/juju/environs"
 	"github.com/juju/juju/instance"
 	"github.com/juju/juju/provider/common"
@@ -23,11 +25,11 @@ type fakeZonedEnv struct {
 
 func (e *fakeZonedEnv) AvailabilityZones() ([]common.AvailabilityZone, error) {
 	e.calls = append(e.calls, "AvailabilityZones")
-	return e.zones, e.err
+	return e.zones, errors.Trace(e.err)
 }
 
 func (e *fakeZonedEnv) InstanceAvailabilityZoneNames(ids []instance.Id) ([]string, error) {
 	e.calls = append(e.calls, "InstanceAvailabilityZoneNames")
 	e.idsArg = ids
-	return e.instZones, e.err
+	return e.instZones, errors.Trace(e.err)
 }
