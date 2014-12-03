ee28ca5c9c9acda1d2bf2661b559f19040b95920
Eric Snow <ericsnowcurrently@gmail.com>
2014-12-02 17:49:57 -0700

    Retry if the session drops.

--- state/backups/db.go
+++ state/backups/db.go
@@ -75,6 +75,11 @@ func NewDBInfo(mgoInfo *mongo.MongoInfo, session DBSession) (*DBInfo, error) {
 
 func getBackupTargetDatabases(session DBSession) (set.Strings, error) {
 	dbNames, err := session.DatabaseNames()
+	if err == io.EOF {
+		// The connection dropped (probably) so try again.
+		session = session.Refresh()
+		dbNames, err = session.DatabaseNames()
+	}
 	if err != nil {
 		return nil, errors.Annotate(err, "unable to get DB names")
 	}
