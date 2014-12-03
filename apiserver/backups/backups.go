f0198558a3da45fc4f0b9a82dd92de0cdece32fd
Eric Snow <ericsnowcurrently@gmail.com>
2014-12-03 13:18:48 -0700

    extractResourceValues -> extractResourceValue.

--- apiserver/backups/backups.go
+++ apiserver/backups/backups.go
@@ -36,39 +36,43 @@ func NewAPI(st *state.State, resources *common.Resources, authorizer common.Auth
 	}
 
 	// Get the backup paths.
-	values, err := extractResourceValues(resources, "dataDir", "logDir", "machineID")
+	dataDir, err := extractResourceValue(resources, "dataDir")
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+	logsDir, err := extractResourceValue(resources, "logDir")
 	if err != nil {
 		return nil, errors.Trace(err)
 	}
 	paths := backups.Paths{
-		DataDir: values["dataDir"],
-		LogsDir: values["logDir"],
+		DataDir: dataDir,
+		LogsDir: logsDir,
 	}
 
 	// Build the API.
+	machineID, err := extractResourceValue(resources, "machineID")
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
 	b := API{
 		st:        st,
 		paths:     &paths,
-		machineID: values["machineID"],
+		machineID: machineID,
 	}
 	return &b, nil
 }
 
-func extractResourceValues(resources *common.Resources, keys ...string) (map[string]string, error) {
-	resourceValues := make(map[string]string)
-	for _, key := range keys {
-		res := resources.Get(key)
-		strRes, ok := res.(common.StringResource)
-		if !ok {
-			if res == nil {
-				strRes = ""
-			} else {
-				return nil, errors.Errorf("invalid %s resource: %v", key, res)
-			}
+func extractResourceValue(resources *common.Resources, key string) (string, error) {
+	res := resources.Get(key)
+	strRes, ok := res.(common.StringResource)
+	if !ok {
+		if res == nil {
+			strRes = ""
+		} else {
+			return "", errors.Errorf("invalid %s resource: %v", key, res)
 		}
-		resourceValues[key] = strRes.String()
 	}
-	return resourceValues, nil
+	return strRes.String(), nil
 }
 
 var newBackups = func(st *state.State) (backups.Backups, io.Closer) {
