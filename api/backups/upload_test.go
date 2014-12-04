c188d1dfe80179abb30c24f20b99c1b864413f03
Eric Snow <ericsnowcurrently@gmail.com>
2014-12-04 16:11:58 -0700

    Make the API upload tests more thorough.

--- api/backups/upload_test.go
+++ api/backups/upload_test.go
@@ -40,6 +40,7 @@ func (s *uploadSuite) TestSuccess(c *gc.C) {
 	c.Assert(err, jc.ErrorIsNil)
 
 	c.Check(id, gc.Equals, "<a new backup ID>")
+	s.FakeClient.CheckCalledReader(c, "backups", archive, &meta, "juju-backup.tar.gz", "SendHTTPRequestReader")
 }
 
 func (s *uploadSuite) TestFunctional(c *gc.C) {
@@ -49,9 +50,25 @@ func (s *uploadSuite) TestFunctional(c *gc.C) {
 	meta := apiserverbackups.ResultFromMetadata(s.Meta)
 	meta.ID = ""
 	meta.Stored = time.Time{}
+	meta.Size = int64(len(data))
 
 	id, err := s.client.Upload(archive, meta)
 	c.Assert(err, jc.ErrorIsNil)
 
 	c.Check(id, gc.Matches, `[-\d]+\.[-0-9a-f]+`)
+
+	// Check the stored contents.
+	stored, err := s.client.Download(id)
+	c.Assert(err, jc.ErrorIsNil)
+	defer archive.Close()
+	storedData, err := ioutil.ReadAll(stored)
+	c.Assert(err, jc.ErrorIsNil)
+	c.Check(string(storedData), gc.Equals, data)
+
+	// Check the stored metadata.
+	storedMeta, err := s.client.Info(id)
+	c.Assert(err, jc.ErrorIsNil)
+	meta.ID = id
+	meta.Stored = storedMeta.Stored
+	c.Check(storedMeta, gc.DeepEquals, &meta)
 }
