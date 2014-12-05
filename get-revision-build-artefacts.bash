f72257f791f8098726e32a093e828e19ab383c9a
Curtis Hovey <curtis@canonical.com>
2014-12-05 13:26:44 -0500

    use --skip-existing with s3cmd because conflicting console logs prevent us from getting all the assets for release.

--- get-revision-build-artefacts.bash
+++ get-revision-build-artefacts.bash
@@ -10,6 +10,6 @@ JOBS="build-revision win-client-build-installer"
 
 for job in $JOBS; do
     artefacts=$(echo "$ALL_FILES" | sed -r "/$job\/build-/!d; s,^.*s3:,s3:,")
-    s3cmd -c $JUJU_HOME/juju-qa.s3cfg get $artefacts ./
+    s3cmd -c $JUJU_HOME/juju-qa.s3cfg get --skip-existing $artefacts ./
 done
 
