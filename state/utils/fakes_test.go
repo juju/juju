d2358fc21c8a72b8794755d2e79a52acbb8c096e
Eric Snow <ericsnowcurrently@gmail.com>
2014-12-05 16:40:40 -0700

    Add some doc comments.

--- state/utils/fakes_test.go
+++ state/utils/fakes_test.go
@@ -23,11 +23,13 @@ type fakeZonedEnv struct {
 	idsArg []instance.Id
 }
 
+// AvailabilityZones implements ZonedEnviron.
 func (e *fakeZonedEnv) AvailabilityZones() ([]common.AvailabilityZone, error) {
 	e.calls = append(e.calls, "AvailabilityZones")
 	return e.zones, errors.Trace(e.err)
 }
 
+// InstanceAvailabilityZoneNames implements ZonedEnviron.
 func (e *fakeZonedEnv) InstanceAvailabilityZoneNames(ids []instance.Id) ([]string, error) {
 	e.calls = append(e.calls, "InstanceAvailabilityZoneNames")
 	e.idsArg = ids
