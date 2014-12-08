793dc02bc5f34e18e670414636d578d90df5a42a
Aaron Bentley <aaron.bentley@canonical.com>
2014-12-08 12:28:00 -0500

    Reduce repetition, add hashbang and eux.

--- run-reliability-test.bash
+++ run-reliability-test.bash
@@ -1,13 +1,19 @@
+#!/usr/bin/bash
+set -eux
+: ${SCRIPTS=$(readlink -f $(dirname $0))}
 new_juju=$(find $new_juju_dir -name juju)
 export JUJU_HOME=$HOME/cloud-city
-$HOME/juju-ci-tools/write_industrial_test_metadata.py $new_juju_dir/buildvars.json $environment metadata.json
+$SCRIPTS/write_industrial_test_metadata.py $new_juju_dir/buildvars.json \
+  $environment metadata.json
 build_id=${JOB_NAME}-${BUILD_NUMBER}
-s3cmd -c ~/cloud-city/juju-qa.s3cfg put metadata.json s3://juju-qa-data/industrial-test/${build_id}-metadata.json
+s3cfg=$JUJU_HOME/juju-qa.s3cfg
+s3base=s3://juju-qa-data/industrial-test/${build_id}
+s3cmd -c $s3cfg put metadata.json $s3base-metadata.json
 if [ "$new_agent_url" != "" ]; then
   extra_args="--new-agent-url $new_agent_url"
 else
   extra_args=""
 fi
-$HOME/juju-ci-tools/industrial_test.py $environment $new_juju $suite --attempts $attempts \
-    --json-file results.json $extra_args
-s3cmd -c ~/cloud-city/juju-qa.s3cfg put results.json s3://juju-qa-data/industrial-test/${build_id}-results.json
+$SCRIPTS/industrial_test.py $environment $new_juju $suite --attempts \
+  $attempts --json-file results.json $extra_args
+s3cmd -c $s3cfg put results.json $s3base-results.json
