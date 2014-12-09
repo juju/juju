f9b746bc49de741b9de869d946bdc1a5000a812f
Curtis Hovey <curtis@canonical.com>
2014-12-09 11:12:36 -0500

    Added main win-client command test.

--- tests/test_crossbuild.py
+++ tests/test_crossbuild.py
@@ -4,15 +4,20 @@ from unittest import TestCase
 from crossbuild import (
     main,
 )
-from utils import temp_dir
 
 
 class CrossBuildTestCase(TestCase):
 
     def test_main_setup(self):
-        with temp_dir() as base_dir:
-            with patch('crossbuild.setup_cross_building') as mock:
-                main(['-d', '-v', 'setup', '--build-dir', base_dir])
+        with patch('crossbuild.setup_cross_building') as mock:
+            main(['-d', '-v', 'setup', '--build-dir', './foo'])
         args, kwargs = mock.call_args
-        self.assertEqual((base_dir, ), args)
+        self.assertEqual(('./foo', ), args)
         self.assertEqual({'dry_run': True, 'verbose': True}, kwargs)
+
+    def test_main_win_client(self):
+        with patch('crossbuild.build_win_client') as mock:
+            main(['win-client', '--build-dir', './foo', 'bar.1.2.3.tar.gz'])
+        args, kwargs = mock.call_args
+        self.assertEqual(('bar.1.2.3.tar.gz', './foo'), args)
+        self.assertEqual({'dry_run': False, 'verbose': False}, kwargs)
