6eea16e1af14ebfc7fb601766c1a409bf6df7161
Curtis Hovey <curtis@canonical.com>
2014-12-09 11:09:01 -0500

    Added test for main setup command.

--- tests/test_crossbuild.py
+++ tests/test_crossbuild.py
@@ -1,7 +1,18 @@
 from mock import patch
 from unittest import TestCase
 
+from crossbuild import (
+    main,
+)
+from utils import temp_dir
+
 
 class CrossBuildTestCase(TestCase):
 
-    pass
+    def test_main_setup(self):
+        with temp_dir() as base_dir:
+            with patch('crossbuild.setup_cross_building') as mock:
+                main(['-d', '-v', 'setup', '--build-dir', base_dir])
+        args, kwargs = mock.call_args
+        self.assertEqual((base_dir, ), args)
+        self.assertEqual({'dry_run': True, 'verbose': True}, kwargs)
