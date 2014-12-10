c71435a41c191fd11a6e3ba2a94b1dd021a11cdb
Eric Snow <ericsnowcurrently@gmail.com>
2014-12-09 18:18:42 -0700

    Set the instance on the test machine.

--- api/uniter/uniter_test.go
+++ api/uniter/uniter_test.go
@@ -63,6 +63,8 @@ func (s *uniterSuite) setUpTest(c *gc.C, addStateServer bool) {
 func (s *uniterSuite) addMachineServiceCharmAndUnit(c *gc.C, serviceName string) (*state.Machine, *state.Service, *state.Charm, *state.Unit) {
 	machine, err := s.State.AddMachine("quantal", state.JobHostUnits)
 	c.Assert(err, jc.ErrorIsNil)
+	err = machine.SetProvisioned("spam", "fake_nonce", nil)
+	c.Assert(err, jc.ErrorIsNil)
 	charm := s.AddTestingCharm(c, serviceName)
 	service := s.AddTestingService(c, serviceName, charm)
 	unit, err := service.AddUnit()
