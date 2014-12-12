a647522c7e48fc20b46a8bb2393efc977d29fb50
Eric Snow <ericsnowcurrently@gmail.com>
2014-12-12 10:33:38 -0700

    Add TODO comment.

--- provider/gce/environ_network.go
+++ provider/gce/environ_network.go
@@ -8,6 +8,8 @@ import (
 	"github.com/juju/juju/network"
 )
 
+// TODO(ericsnow) Fold this back into environ.go if neither ends up too big.
+
 // SupportNetworks returns whether the environment has support to
 // specify networks for services and machines.
 func (env *environ) SupportNetworks() bool {
