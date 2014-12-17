4d32df46d663a627da56828fce38dd6602f120e3
Eric Snow <ericsnowcurrently@gmail.com>
2014-12-17 11:35:09 -0700

    provider/gce: Add Arches to instance type definitions.

--- provider/gce/instancetypes.go
+++ provider/gce/instancetypes.go
@@ -7,9 +7,14 @@ import (
 	"fmt"
 
 	"github.com/juju/juju/environs/instances"
+	"github.com/juju/juju/juju/arch"
 )
 
-var vtype = "kvm" // Google *container* engine uses docker.
+var (
+	vtype = "kvm" // FYI, Google *container* engine uses docker.
+
+	arches = []string{arch.AMD64}
+)
 
 // Instance types are not associated with disks in GCE, so we do not
 // set RootDisk.
@@ -18,30 +23,35 @@ var vtype = "kvm" // Google *container* engine uses docker.
 var allInstanceTypes = []instances.InstanceType{
 	{ // Standard machine types
 		Name:     "n1-standard-1",
+		Arches:   arches,
 		CpuCores: 1,
 		CpuPower: instances.CpuPower(275),
 		Mem:      3750,
 		VirtType: &vtype,
 	}, {
 		Name:     "n1-standard-2",
+		Arches:   arches,
 		CpuCores: 2,
 		CpuPower: instances.CpuPower(550),
 		Mem:      7500,
 		VirtType: &vtype,
 	}, {
 		Name:     "n1-standard-4",
+		Arches:   arches,
 		CpuCores: 4,
 		CpuPower: instances.CpuPower(1100),
 		Mem:      15000,
 		VirtType: &vtype,
 	}, {
 		Name:     "n1-standard-8",
+		Arches:   arches,
 		CpuCores: 8,
 		CpuPower: instances.CpuPower(2200),
 		Mem:      30000,
 		VirtType: &vtype,
 	}, {
 		Name:     "n1-standard-16",
+		Arches:   arches,
 		CpuCores: 16,
 		CpuPower: instances.CpuPower(4400),
 		Mem:      60000,
@@ -50,24 +60,28 @@ var allInstanceTypes = []instances.InstanceType{
 
 	{ // High memory machine types
 		Name:     "n1-highmem-2",
+		Arches:   arches,
 		CpuCores: 2,
 		CpuPower: instances.CpuPower(550),
 		Mem:      13000,
 		VirtType: &vtype,
 	}, {
 		Name:     "n1-highmem-4",
+		Arches:   arches,
 		CpuCores: 4,
 		CpuPower: instances.CpuPower(1100),
 		Mem:      26000,
 		VirtType: &vtype,
 	}, {
 		Name:     "n1-highmem-8",
+		Arches:   arches,
 		CpuCores: 8,
 		CpuPower: instances.CpuPower(2200),
 		Mem:      52000,
 		VirtType: &vtype,
 	}, {
 		Name:     "n1-highmem-16",
+		Arches:   arches,
 		CpuCores: 16,
 		CpuPower: instances.CpuPower(4400),
 		Mem:      104000,
@@ -76,24 +90,28 @@ var allInstanceTypes = []instances.InstanceType{
 
 	{ // High CPU machine types
 		Name:     "n1-highcpu-2",
+		Arches:   arches,
 		CpuCores: 2,
 		CpuPower: instances.CpuPower(550),
 		Mem:      1800,
 		VirtType: &vtype,
 	}, {
 		Name:     "n1-highcpu-4",
+		Arches:   arches,
 		CpuCores: 4,
 		CpuPower: instances.CpuPower(1100),
 		Mem:      3600,
 		VirtType: &vtype,
 	}, {
 		Name:     "n1-highcpu-8",
+		Arches:   arches,
 		CpuCores: 8,
 		CpuPower: instances.CpuPower(2200),
 		Mem:      7200,
 		VirtType: &vtype,
 	}, {
 		Name:     "n1-highcpu-16",
+		Arches:   arches,
 		CpuCores: 16,
 		CpuPower: instances.CpuPower(4400),
 		Mem:      14400,
@@ -102,12 +120,14 @@ var allInstanceTypes = []instances.InstanceType{
 
 	{ // Micro and small machine types
 		Name:     "f1-micro",
+		Arches:   arches,
 		CpuCores: 1,
 		CpuPower: instances.CpuPower(0),
 		Mem:      600,
 		VirtType: &vtype,
 	}, {
 		Name:     "g1-small",
+		Arches:   arches,
 		CpuCores: 1,
 		CpuPower: instances.CpuPower(138),
 		Mem:      1700,
