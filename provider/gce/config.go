42d7cf6b28fc32ee3fc7ec7838966752e26698a8
Eric Snow <ericsnowcurrently@gmail.com>
2014-12-19 13:51:58 -0700

    provider/gce: Clarify a doc comment.

--- provider/gce/config.go
+++ provider/gce/config.go
@@ -157,7 +157,8 @@ func validateConfig(cfg, old *config.Config) (*environConfig, error) {
 		return nil, errors.Trace(handleInvalidField(err))
 	}
 
-	// TODO(ericsnow) Follow up with someone on this.
+	// TODO(ericsnow) Follow up with someone on if it is appropriate
+	// to call Apply here.
 	cfg, err = ecfg.Config.Apply(ecfg.attrs)
 	if err != nil {
 		return nil, errors.Trace(err)
