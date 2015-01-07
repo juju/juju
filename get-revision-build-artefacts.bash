9e77e40856b35065e482e8695c86591c1fe82c05
Curtis Hovey <curtis@canonical.com>
2015-01-07 17:21:51 -0500

    Update get-revision-build-artefacts.bash to get the win and osx files from the new jobs.

--- get-revision-build-artefacts.bash
+++ get-revision-build-artefacts.bash
@@ -6,10 +6,11 @@ CONTAINER_BASE="juju-qa-data/juju-ci/products/version-$REVISION_BUILD"
 
 ALL_FILES=$(s3cmd -c $JUJU_HOME/juju-qa.s3cfg ls -r s3://$CONTAINER_BASE/*)
 
-JOBS="build-revision win-client-build-installer"
+JOBS="build-revision build-osx-client build-win-agent build-win-client"
 
 for job in $JOBS; do
     artefacts=$(echo "$ALL_FILES" | sed -r "/$job\/build-/!d; s,^.*s3:,s3:,")
     s3cmd -c $JUJU_HOME/juju-qa.s3cfg get --skip-existing $artefacts ./
+    rm consoleText
 done
 
