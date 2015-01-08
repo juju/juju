5362e10eb45407f85dc1c7c9c56dca1c20fb9390
Eric Snow <ericsnowcurrently@gmail.com>
2015-01-07 22:33:41 -0700

    provider/gce: Split auth out into own file.

--- provider/gce/gceapi/gce.go
+++ provider/gce/gceapi/gce.go
@@ -5,14 +5,10 @@ package gceapi
 
 import (
 	"fmt"
-	"net/http"
-	"net/mail"
 	"path"
 	"strings"
 	"time"
 
-	"code.google.com/p/goauth2/oauth"
-	"code.google.com/p/goauth2/oauth/jwt"
 	"code.google.com/p/google-api-go-client/compute/v1"
 	"github.com/juju/errors"
 	"github.com/juju/loggo"
@@ -23,13 +19,6 @@ import (
 )
 
 const (
-	driverScopes = "https://www.googleapis.com/auth/compute " +
-		"https://www.googleapis.com/auth/devstorage.full_control"
-
-	tokenURL = "https://accounts.google.com/o/oauth2/token"
-
-	authURL = "https://accounts.google.com/o/oauth2/auth"
-
 	partialMachineType = "zones/%s/machineTypes/%s"
 
 	diskTypeScratch    = "SCRATCH"
@@ -78,71 +67,6 @@ var (
 		Delay: 1 * time.Second,
 	}
 )
-
-type Auth struct {
-	ClientID    string
-	ClientEmail string
-	PrivateKey  []byte
-}
-
-func (ga Auth) Validate() error {
-	if ga.ClientID == "" {
-		return &config.InvalidConfigValue{Key: OSEnvClientID}
-	}
-	if ga.ClientEmail == "" {
-		return &config.InvalidConfigValue{Key: OSEnvClientEmail}
-	} else if _, err := mail.ParseAddress(ga.ClientEmail); err != nil {
-		err = errors.Trace(err)
-		return &config.InvalidConfigValue{OSEnvClientEmail, ga.ClientEmail, err}
-	}
-	if len(ga.PrivateKey) == 0 {
-		return &config.InvalidConfigValue{Key: OSEnvPrivateKey}
-	}
-	return nil
-}
-
-func (ga Auth) newTransport() (*oauth.Transport, error) {
-	token, err := newToken(ga, driverScopes)
-	if err != nil {
-		return nil, errors.Trace(err)
-	}
-
-	transport := oauth.Transport{
-		Config: &oauth.Config{
-			ClientId: ga.ClientID,
-			Scope:    driverScopes,
-			TokenURL: tokenURL,
-			AuthURL:  authURL,
-		},
-		Token: token,
-	}
-	return &transport, nil
-}
-
-var newToken = func(auth Auth, scopes string) (*oauth.Token, error) {
-	jtok := jwt.NewToken(auth.ClientEmail, scopes, auth.PrivateKey)
-	jtok.ClaimSet.Aud = tokenURL
-
-	token, err := jtok.Assert(&http.Client{})
-	if err != nil {
-		msg := "retrieving auth token for %s"
-		return nil, errors.Annotatef(err, msg, auth.ClientEmail)
-	}
-	return token, nil
-}
-
-func (ga Auth) newConnection() (*compute.Service, error) {
-	transport, err := ga.newTransport()
-	if err != nil {
-		return nil, errors.Trace(err)
-	}
-	service, err := newService(transport)
-	return service, errors.Trace(err)
-}
-
-var newService = func(transport *oauth.Transport) (*compute.Service, error) {
-	return compute.New(transport.Client())
-}
 
 type Connection struct {
 	raw *compute.Service
