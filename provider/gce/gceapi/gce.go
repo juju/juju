c071a79d5ffef82135cdca65890de8ed9c108736
Eric Snow <ericsnowcurrently@gmail.com>
2015-01-07 23:40:54 -0700

    provider/gce: Split network code into own file.

--- provider/gce/gceapi/gce.go
+++ provider/gce/gceapi/gce.go
@@ -8,15 +8,9 @@ import (
 
 	"code.google.com/p/google-api-go-client/compute/v1"
 	"github.com/juju/loggo"
-
-	"github.com/juju/juju/network"
 )
 
 const (
-	networkDefaultName       = "default"
-	networkPathRoot          = "global/networks/"
-	networkAccessOneToOneNAT = "ONE_TO_ONE_NAT"
-
 	StatusDone         = "DONE"
 	StatusDown         = "DOWN"
 	StatusPending      = "PENDING"
@@ -45,57 +39,4 @@ func zoneName(value interface{}) string {
 		// TODO(ericsnow) Fail?
 		return ""
 	}
-}
-
-type NetworkSpec struct {
-	Name string
-	// TODO(ericsnow) support a CIDR for internal IP addr range?
-}
-
-func (ns *NetworkSpec) path() string {
-	name := ns.Name
-	if name == "" {
-		name = networkDefaultName
-	}
-	return networkPathRoot + name
-}
-
-func (ns *NetworkSpec) newInterface(name string) *compute.NetworkInterface {
-	var access []*compute.AccessConfig
-	if name != "" {
-		// This interface has an internet connection.
-		access = append(access, &compute.AccessConfig{
-			Name: name,
-			Type: networkAccessOneToOneNAT, // the default
-			// NatIP (only set if using a reserved public IP)
-		})
-		// TODO(ericsnow) Will we need to support more access configs?
-	}
-	return &compute.NetworkInterface{
-		Network:       ns.path(),
-		AccessConfigs: access,
-	}
-}
-
-// firewallSpec expands a port range set in to compute.FirewallAllowed
-// and returns a compute.Firewall for the provided name.
-func firewallSpec(name string, ps network.PortSet) *compute.Firewall {
-	firewall := compute.Firewall{
-		// Allowed is set below.
-		// Description is not set.
-		Name: name,
-		// Network: (defaults to global)
-		// SourceTags is not set.
-		TargetTags:   []string{name},
-		SourceRanges: []string{"0.0.0.0/0"},
-	}
-
-	for _, protocol := range ps.Protocols() {
-		allowed := compute.FirewallAllowed{
-			IPProtocol: protocol,
-			Ports:      ps.PortStrings(protocol),
-		}
-		firewall.Allowed = append(firewall.Allowed, &allowed)
-	}
-	return &firewall
 }
