edbf6a52facab76329773221af02d41bed905eba
Wayne Witzel III <wayne@riotousliving.com>
2015-01-09 13:00:04 -0500

    provider/gce: added newAttached tests.

--- provider/gce/google/disk_test.go
+++ provider/gce/google/disk_test.go
@@ -4,6 +4,7 @@
 package google_test
 
 import (
+	"code.google.com/p/google-api-go-client/compute/v1"
 	jc "github.com/juju/testing/checkers"
 	gc "gopkg.in/check.v1"
 
@@ -43,15 +44,73 @@ func (s *diskSuite) TestDiskSpecSizeGBMin(c *gc.C) {
 	c.Check(size, gc.Equals, int64(0))
 }
 
+type attachedInfo struct {
+	attached *compute.AttachedDisk
+	diskType string
+	diskMode string
+}
+
+func (s *diskSuite) checkAttached(c *gc.C, aInfo attachedInfo) {
+	c.Check(aInfo.attached.Type, gc.Equals, aInfo.diskType)
+	c.Check(aInfo.attached.Boot, gc.Equals, s.DiskSpec.Boot)
+	c.Check(aInfo.attached.Mode, gc.Equals, aInfo.diskMode)
+	c.Check(aInfo.attached.AutoDelete, gc.Equals, s.DiskSpec.AutoDelete)
+	c.Check(aInfo.attached.Interface, gc.Equals, "")
+	c.Check(aInfo.attached.DeviceName, gc.Equals, "")
+}
+
 func (s *diskSuite) TestDiskSpecNewAttached(c *gc.C) {
 	attached := google.NewAttached(s.DiskSpec)
 
-	c.Check(attached.Type, gc.Equals, "PERSISTENT")
-	c.Check(attached.Boot, jc.IsTrue)
-	c.Check(attached.Mode, gc.Equals, "READ_WRITE")
-	c.Check(attached.AutoDelete, jc.IsTrue)
-	c.Check(attached.Interface, gc.Equals, "")
-	c.Check(attached.DeviceName, gc.Equals, "")
+	s.checkAttached(c, attachedInfo{
+		attached: attached,
+		diskType: "PERSISTENT",
+		diskMode: "READ_WRITE",
+	})
+}
+
+func (s *diskSuite) TestDiskSpecNewAttachedBootFalse(c *gc.C) {
+	s.DiskSpec.Boot = false
+	attached := google.NewAttached(s.DiskSpec)
+
+	s.checkAttached(c, attachedInfo{
+		attached: attached,
+		diskType: "PERSISTENT",
+		diskMode: "READ_WRITE",
+	})
+}
+
+func (s *diskSuite) TestDiskSpecNewAttachedAutoDeleteFalse(c *gc.C) {
+	s.DiskSpec.AutoDelete = false
+	attached := google.NewAttached(s.DiskSpec)
+
+	s.checkAttached(c, attachedInfo{
+		attached: attached,
+		diskType: "PERSISTENT",
+		diskMode: "READ_WRITE",
+	})
+}
+
+func (s *diskSuite) TestDiskSpecNewAttachedScratch(c *gc.C) {
+	s.DiskSpec.Scratch = true
+	attached := google.NewAttached(s.DiskSpec)
+
+	s.checkAttached(c, attachedInfo{
+		attached: attached,
+		diskType: "SCRATCH",
+		diskMode: "READ_WRITE",
+	})
+}
+
+func (s *diskSuite) TestDiskSpecNewAttachedReadOnly(c *gc.C) {
+	s.DiskSpec.Readonly = true
+	attached := google.NewAttached(s.DiskSpec)
+
+	s.checkAttached(c, attachedInfo{
+		attached: attached,
+		diskType: "PERSISTENT",
+		diskMode: "READ_ONLY",
+	})
 }
 
 func (s *diskSuite) TestRootDisk(c *gc.C) {
