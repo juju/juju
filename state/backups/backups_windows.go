db742250eaa41b39c09d0ed47311eed4cda54ba6
Horacio Duran <horacio.duran@canonical.com>
2015-01-14 22:05:23 -0300

    Correced as per davidcheney comments

--- state/backups/backups_windows.go
+++ state/backups/backups_windows.go
@@ -1,13 +1,15 @@
-// Copyright 2014 Canonical Ltd.
+// Copyright 2015 Canonical Ltd.
 // Licensed under the AGPLv3, see LICENCE file for details.
 
 package backups
 
 import (
+	"github.com/juju/errors"
+
 	"github.com/juju/juju/apiserver/params"
 )
 
 // Restore satisfies the Backups interface on windows.
 func (b *backups) Restore(backupId string, args params.RestoreArgs) error {
-	return nil
+	return errors.Errorf("backups not supported under windows")
 }
