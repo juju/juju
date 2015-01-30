f83a63021760289fd0a95b0ee0823fd72dfeee60
Curtis Hovey <curtis@canonical.com>
2015-01-30 17:52:36 -0500

    Added script to run osx client test.

---
+++ run-osx-client.bash
@@ -0,0 +1,50 @@
+#!/bin/bash
+# This script presumes ~/ci and ~/.juju is setup on the remote machine.
+set -eu
+SCRIPTS=$(readlink -f $(dirname $0))
+JUJU_HOME=${JUJU_HOME:-$(dirname $SCRIPTS)/cloud-city}
+
+SSH_OPTIONS="-i $JUJU_HOME/staging-juju-rsa \
+    -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
+
+
+usage() {
+    echo "usage: $0 user@host"
+    echo "  user@host: The user and host to ssh to."
+    exit 1
+}
+
+
+test $# -eq 1 || usage
+USER_AT_HOST="$1"
+
+set -x
+$SCRIPTS/jujuci.py get build-revision 'buildvars.bash' ./
+source ./buildvars.bash
+rev=${REVNO-$(echo $REVISION_ID | head -c8)}
+echo "Testing $BRANCH $rev"
+
+TARFILE=$($SCRIPTS/jujuci.py get build-osx-client 'juju-*-osx.tar.gz' ./)
+echo "Downloaded $TARFILE"
+
+scp $SSH_OPTIONS \
+    $SCRIPTS/deploy_stack.py $SCRIPTS/jujupy.py $SCRIPTS/jujuconfig.py \
+    $SCRIPTS/substrate.py $SCRIPTS/utility.py $TARFILE \
+    $USER_AT_HOST:~/ci/
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+ssh $SSH_OPTIONS $USER_AT_HOST <<"EOT"
+#!/bin/bash
+set -ux
+set +e
+juju destroy-environment --force -y test-osx-client
+python ci/deploy_stack.py test-osx-client
+EXIT_STATUS=$?
+juju destroy-environment --force -y test-osx-client
+exit $EXIT_STATUS
+EOT
+EXIT_STATUS=$?
+
+exit $EXIT_STATUS
