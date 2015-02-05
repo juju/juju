56791c971eedc2cff6f40b91965c140e24206651
Curtis Hovey <curtis@canonical.com>
2015-02-05 18:04:58 -0500

    added test for old_machines_stuck with provisioning.

--- tests/test_joyent.py
+++ tests/test_joyent.py
@@ -12,6 +12,23 @@ from joyent import (
 )
 
 
+def make_machine(state='running', hours=2):
+    then = datetime.utcnow() - timedelta(hours=hours)
+    return {
+        'id': 'id',
+        'state': state,
+        'created': then.strftime(ISO_8601_FORMAT)}
+
+
+def fake_list_machines(machine):
+    def list_machines(id=None):
+        if id:
+            return machine
+        else:
+            return [machine]
+    return list_machines
+
+
 class JoyentTestCase(TestCase):
 
     def test_parse_args(self):
@@ -41,21 +58,10 @@ class ClientTestCase(TestCase):
         self.assertTrue(client.verbose)
 
     def test_delete_old_machines(self):
-        then = datetime.utcnow() - timedelta(hours=2)
-        machine = {
-            'id': 'id',
-            'state': 'stopped',
-            'created': then.strftime(ISO_8601_FORMAT)}
-
-        def fake_list_machines(id=None):
-            if id:
-                return machine
-            else:
-                return [machine]
-
+        machine = make_machine('stopped')
         client = Client('sdc_url', 'account', 'key_id', './key', pause=0)
         with patch.object(client, '_list_machines',
-                          side_effect=fake_list_machines) as lm_mock:
+                          side_effect=fake_list_machines(machine)) as lm_mock:
             with patch.object(client, 'stop_machine') as sm_mock:
                 with patch.object(client, 'delete_machine') as dm_mock:
                     with patch.object(client, 'request_deletion') as rd_mock:
@@ -65,3 +71,16 @@ class ClientTestCase(TestCase):
         sm_mock.assert_called_once_with('id')
         dm_mock.assert_called_once_with('id')
         rd_mock.assert_called_once_with([], 'foo@bar')
+
+    def test_delete_old_machines_stuck_provisioning(self):
+        machine = make_machine('provisioning')
+        client = Client('sdc_url', 'account', 'key_id', './key', pause=0)
+        with patch.object(client, '_list_machines',
+                          side_effect=fake_list_machines(machine)):
+            with patch.object(client, 'stop_machine') as sm_mock:
+                with patch.object(client, 'delete_machine') as dm_mock:
+                    with patch.object(client, 'request_deletion') as rd_mock:
+                        client.delete_old_machines(1, 'foo@bar')
+        self.assertEqual(0, sm_mock.call_count)
+        self.assertEqual(0, dm_mock.call_count)
+        rd_mock.assert_called_once_with([machine], 'foo@bar')
