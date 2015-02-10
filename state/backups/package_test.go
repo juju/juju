4b923797862d36736eb34af8e72e5f76fb5a8323
Eric Snow <ericsnowcurrently@gmail.com>
2015-02-10 13:42:14 -0700

    Do not skip all backups tests on non-linux.

--- state/backups/package_test.go
+++ state/backups/package_test.go
@@ -7,12 +7,8 @@ import (
 	stdtesting "testing"
 
 	"github.com/juju/juju/testing"
-	"github.com/juju/juju/version"
 )
 
 func Test(t *stdtesting.T) {
-	if version.Current.OS != version.Ubuntu {
-		t.Skip("backups are a state-server-only feature, hence Ubuntu-only")
-	}
 	testing.MgoTestPackage(t)
 }
