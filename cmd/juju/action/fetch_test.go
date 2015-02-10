f414678dc55f0a060eb9faf94ba2be6d6e1108ee
Bodie Solomon <binary132@gmail.com>
2015-02-10 00:46:28 -0500

    Clean up `juju action fetch` tests a bit

--- cmd/juju/action/fetch_test.go
+++ cmd/juju/action/fetch_test.go
@@ -50,10 +50,10 @@ func (s *FetchSuite) TestInit(c *gc.C) {
 	}}
 
 	for i, t := range tests {
-		s.subcommand = &action.FetchCommand{}
+
 		c.Logf("test %d: it should %s: juju actions fetch %s", i,
 			t.should, strings.Join(t.args, " "))
-		err := testing.InitCommand(s.subcommand, t.args)
+		err := testing.InitCommand(&action.FetchCommand{}, t.args)
 		if t.expectError != "" {
 			c.Check(err, gc.ErrorMatches, t.expectError)
 		}
@@ -62,37 +62,37 @@ func (s *FetchSuite) TestInit(c *gc.C) {
 
 func (s *FetchSuite) TestRun(c *gc.C) {
 	tests := []struct {
-		should         string
-		withTags       params.FindTagsResults
-		withResults    []params.ActionResult
-		withAPIError   string
-		expectedErr    string
-		expectedOutput string
+		should          string
+		withTags        params.FindTagsResults
+		withAPIResponse []params.ActionResult
+		withAPIError    string
+		expectedErr     string
+		expectedOutput  string
 	}{{
 		should:       "pass api error through properly",
 		withAPIError: "api call error",
 		expectedErr:  "api call error",
 	}, {
-		should:      "fail with no results",
-		withTags:    tagsForIdPrefix(validActionId),
-		withResults: []params.ActionResult{},
-		expectedErr: `actions for identifier "` + validActionId + `" not found`,
+		should:          "fail with no results",
+		withTags:        tagsForIdPrefix(validActionId),
+		withAPIResponse: []params.ActionResult{},
+		expectedErr:     `actions for identifier "` + validActionId + `" not found`,
 	}, {
-		should:      "error correctly with multiple results",
-		withTags:    tagsForIdPrefix(validActionId, validActionTagString),
-		withResults: []params.ActionResult{{}, {}},
-		expectedErr: "too many results for action " + validActionId,
+		should:          "error correctly with multiple results",
+		withTags:        tagsForIdPrefix(validActionId, validActionTagString),
+		withAPIResponse: []params.ActionResult{{}, {}},
+		expectedErr:     "too many results for action " + validActionId,
 	}, {
 		should:   "pass through an error from the API server",
 		withTags: tagsForIdPrefix(validActionId, validActionTagString),
-		withResults: []params.ActionResult{{
+		withAPIResponse: []params.ActionResult{{
 			Error: common.ServerError(errors.New("an apiserver error")),
 		}},
 		expectedErr: "an apiserver error",
 	}, {
 		should:   "pretty-print action output",
 		withTags: tagsForIdPrefix(validActionId, validActionTagString),
-		withResults: []params.ActionResult{{
+		withAPIResponse: []params.ActionResult{{
 			Status:  "complete",
 			Message: "oh dear",
 			Output: map[string]interface{}{
@@ -104,21 +104,21 @@ func (s *FetchSuite) TestRun(c *gc.C) {
 			Started:   time.Date(2015, time.February, 14, 8, 15, 0, 0, time.UTC),
 			Completed: time.Date(2015, time.February, 14, 8, 15, 30, 0, time.UTC),
 		}},
-		expectedOutput: "" +
-			"message: oh dear\n" +
-			"results:\n" +
-			"  foo:\n" +
-			"    bar: baz\n" +
-			"status: complete\n" +
-			"timing:\n" +
-			"  completed: 2015-02-14 08:15:30 \\+0000 UTC\n" +
-			"  enqueued: 2015-02-14 08:13:00 \\+0000 UTC\n" +
-			"  started: 2015-02-14 08:15:00 \\+0000 UTC\n" +
-			"",
+		expectedOutput: `
+message: oh dear
+results:
+  foo:
+    bar: baz
+status: complete
+timing:
+  completed: 2015-02-14 08:15:30 +0000 UTC
+  enqueued: 2015-02-14 08:13:00 +0000 UTC
+  started: 2015-02-14 08:15:00 +0000 UTC
+`[1:],
 	}, {
 		should:   "pretty-print action output with no completed time",
 		withTags: tagsForIdPrefix(validActionId, validActionTagString),
-		withResults: []params.ActionResult{{
+		withAPIResponse: []params.ActionResult{{
 			Status: "pending",
 			Output: map[string]interface{}{
 				"foo": map[string]interface{}{
@@ -128,19 +128,19 @@ func (s *FetchSuite) TestRun(c *gc.C) {
 			Enqueued: time.Date(2015, time.February, 14, 8, 13, 0, 0, time.UTC),
 			Started:  time.Date(2015, time.February, 14, 8, 15, 0, 0, time.UTC),
 		}},
-		expectedOutput: "" +
-			"results:\n" +
-			"  foo:\n" +
-			"    bar: baz\n" +
-			"status: pending\n" +
-			"timing:\n" +
-			"  enqueued: 2015-02-14 08:13:00 \\+0000 UTC\n" +
-			"  started: 2015-02-14 08:15:00 \\+0000 UTC\n" +
-			"",
+		expectedOutput: `
+results:
+  foo:
+    bar: baz
+status: pending
+timing:
+  enqueued: 2015-02-14 08:13:00 +0000 UTC
+  started: 2015-02-14 08:15:00 +0000 UTC
+`[1:],
 	}, {
 		should:   "pretty-print action output with no enqueued time",
 		withTags: tagsForIdPrefix(validActionId, validActionTagString),
-		withResults: []params.ActionResult{{
+		withAPIResponse: []params.ActionResult{{
 			Status: "pending",
 			Output: map[string]interface{}{
 				"foo": map[string]interface{}{
@@ -150,19 +150,19 @@ func (s *FetchSuite) TestRun(c *gc.C) {
 			Completed: time.Date(2015, time.February, 14, 8, 15, 30, 0, time.UTC),
 			Started:   time.Date(2015, time.February, 14, 8, 15, 0, 0, time.UTC),
 		}},
-		expectedOutput: "" +
-			"results:\n" +
-			"  foo:\n" +
-			"    bar: baz\n" +
-			"status: pending\n" +
-			"timing:\n" +
-			"  completed: 2015-02-14 08:15:30 \\+0000 UTC\n" +
-			"  started: 2015-02-14 08:15:00 \\+0000 UTC\n" +
-			"",
+		expectedOutput: `
+results:
+  foo:
+    bar: baz
+status: pending
+timing:
+  completed: 2015-02-14 08:15:30 +0000 UTC
+  started: 2015-02-14 08:15:00 +0000 UTC
+`[1:],
 	}, {
 		should:   "pretty-print action output with no started time",
 		withTags: tagsForIdPrefix(validActionId, validActionTagString),
-		withResults: []params.ActionResult{{
+		withAPIResponse: []params.ActionResult{{
 			Status: "pending",
 			Output: map[string]interface{}{
 				"foo": map[string]interface{}{
@@ -172,38 +172,47 @@ func (s *FetchSuite) TestRun(c *gc.C) {
 			Enqueued:  time.Date(2015, time.February, 14, 8, 13, 0, 0, time.UTC),
 			Completed: time.Date(2015, time.February, 14, 8, 15, 30, 0, time.UTC),
 		}},
-		expectedOutput: "" +
-			"results:\n" +
-			"  foo:\n" +
-			"    bar: baz\n" +
-			"status: pending\n" +
-			"timing:\n" +
-			"  completed: 2015-02-14 08:15:30 \\+0000 UTC\n" +
-			"  enqueued: 2015-02-14 08:13:00 \\+0000 UTC\n" +
-			"",
+		expectedOutput: `
+results:
+  foo:
+    bar: baz
+status: pending
+timing:
+  completed: 2015-02-14 08:15:30 +0000 UTC
+  enqueued: 2015-02-14 08:13:00 +0000 UTC
+`[1:],
 	}}
 
 	for i, t := range tests {
-		func() { // for the defer of restoring patch function
-			client := &fakeAPIClient{
-				actionTagMatches: t.withTags,
-				actionResults:    t.withResults,
-			}
-			if t.withAPIError != "" {
-				client.apiErr = errors.New(t.withAPIError)
-			}
-			defer s.BaseActionSuite.patchAPIClient(client)()
-
-			s.subcommand = &action.FetchCommand{}
-			c.Logf("test %d: it should %s", i, t.should)
-
-			ctx, err := testing.RunCommand(c, s.subcommand, validActionId)
-			if t.expectedErr != "" || t.withAPIError != "" {
-				c.Check(err, gc.ErrorMatches, t.expectedErr)
-			} else {
-				c.Assert(err, gc.IsNil)
-				c.Check(ctx.Stdout.(*bytes.Buffer).String(), gc.Matches, t.expectedOutput)
-			}
-		}()
+		c.Logf("test %d: should %s", i, t.should)
+		testRunHelper(
+			c, s,
+			makeFakeClient(t.withTags, t.withAPIResponse, t.withAPIError),
+			t.expectedErr,
+			t.expectedOutput,
+		)
 	}
+}
+
+func testRunHelper(c *gc.C, s *FetchSuite, client *fakeAPIClient, expectedErr, expectedOutput string) {
+	unpatch := s.BaseActionSuite.patchAPIClient(client)
+	defer unpatch()
+	ctx, err := testing.RunCommand(c, &action.FetchCommand{}, validActionId)
+	if expectedErr != "" {
+		c.Check(err, gc.ErrorMatches, expectedErr)
+	} else {
+		c.Assert(err, gc.IsNil)
+		c.Check(ctx.Stdout.(*bytes.Buffer).String(), gc.Equals, expectedOutput)
+	}
+}
+
+func makeFakeClient(tags params.FindTagsResults, response []params.ActionResult, errStr string) *fakeAPIClient {
+	client := &fakeAPIClient{
+		actionTagMatches: tags,
+		actionResults:    response,
+	}
+	if errStr != "" {
+		client.apiErr = errors.New(errStr)
+	}
+	return client
 }
