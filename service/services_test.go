f92a45ab8aa0100c2dcf972684f21a5cc327aebd
Eric Snow <ericsnowcurrently@gmail.com>
2015-02-17 16:35:29 -0700

    Add test for Services.InitSystem.

--- service/services_test.go
+++ service/services_test.go
@@ -8,24 +8,39 @@ import (
 	gc "gopkg.in/check.v1"
 
 	"github.com/juju/juju/service"
+	"github.com/juju/juju/service/initsystems"
 )
 
 var _ = gc.Suite(&servicesSuite{})
 
 type servicesSuite struct {
 	service.BaseSuite
+
+	init     *initsystems.Stub
+	services *service.Services
 }
 
 func (s *servicesSuite) SetUpTest(c *gc.C) {
 	s.BaseSuite.SetUpTest(c)
+
+	s.init = initsystems.NewStub()
+	s.services = service.NewServices(c.MkDir(), s.init)
 }
 
 // TODO(ericsnow) Write the tests.
 
 func (s *servicesSuite) TestDiscoverServices(c *gc.C) {
+	// TODO(ericsnow) Write the test?
 }
 
 func (s *servicesSuite) TestInitSystem(c *gc.C) {
+	// Our choice of init system name here is not significant.
+	expected := service.InitSystemUpstart
+	s.init.Returns.Name = expected
+
+	name := s.services.InitSystem()
+
+	c.Check(name, gc.Equals, expected)
 }
 
 func (s *servicesSuite) TestList(c *gc.C) {
