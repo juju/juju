1deb0af493bb925e8a5d6b14bb381829eb789830
Eric Snow <ericsnowcurrently@gmail.com>
2015-02-18 00:15:47 -0700

    Add remaining agent tests.

--- service/agent_test.go
+++ service/agent_test.go
@@ -10,6 +10,7 @@ import (
 	gc "gopkg.in/check.v1"
 
 	"github.com/juju/juju/service"
+	"github.com/juju/juju/service/initsystems"
 )
 
 var _ = gc.Suite(&agentSuite{})
@@ -61,14 +62,58 @@ func (s *agentSuite) TestNewAgentServiceSpec(c *gc.C) {
 	c.Check(spec.Name(), gc.Equals, name)
 }
 
-func (s *agentSuite) TestAgentServiceSpecName(c *gc.C) {
-}
-
 func (s *agentSuite) TestAgentServiceSpecToolsDir(c *gc.C) {
+	name := "jujud-unit-wordpress-0"
+	tag, err := names.ParseTag(name[6:])
+	c.Assert(err, jc.ErrorIsNil)
+	spec, err := service.NewAgentServiceSpec(tag, s.Paths, service.InitSystemUpstart)
+	c.Assert(err, jc.ErrorIsNil)
+
+	c.Check(spec.ToolsDir(), gc.Equals, "/var/lib/juju/tools/unit-wordpress-0")
 }
 
 func (s *agentSuite) TestAgentServiceSpecConf(c *gc.C) {
+	name := "jujud-unit-wordpress-0"
+	tag, err := names.ParseTag(name[6:])
+	c.Assert(err, jc.ErrorIsNil)
+	spec, err := service.NewAgentServiceSpec(tag, s.Paths, service.InitSystemUpstart)
+	c.Assert(err, jc.ErrorIsNil)
+	conf := spec.Conf()
+
+	c.Check(conf, jc.DeepEquals, service.Conf{Conf: initsystems.Conf{
+		Desc: "juju agent for unit wordpress/0",
+		Cmd:  `"/var/lib/juju/tools/unit-wordpress-0/jujud" unit --data-dir "/var/lib/juju" --unit-name "wordpress/0" --debug`,
+		Out:  "/var/log/juju/unit-wordpress-0.log",
+	}})
+}
+
+func (s *agentSuite) TestAgentServiceSpecConfWindows(c *gc.C) {
+	name := "jujud-unit-wordpress-0"
+	tag, err := names.ParseTag(name[6:])
+	c.Assert(err, jc.ErrorIsNil)
+	spec, err := service.NewAgentServiceSpec(tag, s.Paths, service.InitSystemWindows)
+	c.Assert(err, jc.ErrorIsNil)
+	conf := spec.Conf()
+
+	c.Check(conf, jc.DeepEquals, service.Conf{Conf: initsystems.Conf{
+		Desc: "juju agent for unit wordpress/0",
+		Cmd:  `"\var\lib\juju\tools\unit-wordpress-0\jujud.exe" unit --data-dir "\var\lib\juju" --unit-name "wordpress/0" --debug`,
+	}})
 }
 
 func (s *agentSuite) TestNewAgentService(c *gc.C) {
+	expName := "jujud-unit-wordpress-0"
+	tag, err := names.ParseTag(expName[6:])
+	c.Assert(err, jc.ErrorIsNil)
+	svc, err := service.NewAgentService(tag, s.Paths, nil, s.services)
+	c.Assert(err, jc.ErrorIsNil)
+	name := svc.Name()
+	conf := svc.Conf()
+
+	c.Check(name, gc.Equals, expName)
+	c.Check(conf, jc.DeepEquals, service.Conf{Conf: initsystems.Conf{
+		Desc: "juju agent for unit wordpress/0",
+		Cmd:  `"/var/lib/juju/tools/unit-wordpress-0/jujud" unit --data-dir "/var/lib/juju" --unit-name "wordpress/0" --debug`,
+		Out:  "/var/log/juju/unit-wordpress-0.log",
+	}})
 }
