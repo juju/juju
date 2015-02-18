9ad52e987f08dbd1074637f0a0df6b9b10731af6
Eric Snow <ericsnowcurrently@gmail.com>
2015-02-17 23:51:22 -0700

    Add tests for ListAgents and NewAgentServiceSpec.

--- service/agent_test.go
+++ service/agent_test.go
@@ -5,8 +5,8 @@ package service_test
 
 import (
 	//"github.com/juju/errors"
-	//"github.com/juju/names"
-	//jc "github.com/juju/testing/checkers"
+	"github.com/juju/names"
+	jc "github.com/juju/testing/checkers"
 	gc "gopkg.in/check.v1"
 
 	"github.com/juju/juju/service"
@@ -28,12 +28,37 @@ func (s *agentSuite) SetUpTest(c *gc.C) {
 }
 
 func (s *agentSuite) TestListAgents(c *gc.C) {
+	svcNames := []string{
+		"jujud-machine-0",
+		"juju-mongod",
+		"jujud-unit-wordpress-0",
+	}
+	for _, name := range svcNames {
+		s.SetManaged(name, s.services)
+		s.Init.Returns.Names = append(s.Init.Returns.Names, name)
+	}
+	s.Init.Returns.CheckPassed = true
+
+	tags, err := service.ListAgents(s.services)
+	c.Assert(err, jc.ErrorIsNil)
+
+	var expected []names.Tag
+	for _, name := range []string{"machine-0", "unit-wordpress-0"} {
+		tag, err := names.ParseTag(name)
+		c.Assert(err, jc.ErrorIsNil)
+		expected = append(expected, tag)
+	}
+	c.Check(tags, jc.SameContents, expected)
 }
 
 func (s *agentSuite) TestNewAgentServiceSpec(c *gc.C) {
-}
+	name := "jujud-unit-wordpress-0"
+	tag, err := names.ParseTag(name[6:])
+	c.Assert(err, jc.ErrorIsNil)
+	spec, err := service.NewAgentServiceSpec(tag, s.Paths, service.InitSystemUpstart)
+	c.Assert(err, jc.ErrorIsNil)
 
-func (s *agentSuite) TestDiscoverAgentServiceSpec(c *gc.C) {
+	c.Check(spec.Name(), gc.Equals, name)
 }
 
 func (s *agentSuite) TestAgentServiceSpecName(c *gc.C) {
