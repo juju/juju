5e85aceed7aa9bc354474a7d2a7b98474a70acd6
Dimiter Naydenov <dimiter.naydenov@canonical.com>
2015-02-20 20:50:49 +0200

    PrepareContainerInterfaceInfo server-side provisioner API added

---
+++ apiserver/provisioner/export_test.go
@@ -0,0 +1,10 @@
+// Copyright 2015 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package provisioner
+
+var (
+	AllocateAddrTo = &allocateAddrTo
+	SetAddrsTo     = &setAddrsTo
+	SetAddrState   = &setAddrState
+)
