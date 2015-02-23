b2f6434b205ec10e514cee05764d56fe3ad89388
Aaron Bentley <aaron.bentley@canonical.com>
2015-02-23 14:58:49 -0500

    Add --old-stable parameter to run-reliability-test.

--- run-reliability-test.bash
+++ run-reliability-test.bash
@@ -2,6 +2,8 @@
 set -eu
 : ${SCRIPTS=$(readlink -f $(dirname $0))}
 new_juju=$(find $new_juju_dir -name juju)
+old_stable_juju=$(find $old_stable_juju_dir -name juju)
+
 export JUJU_HOME=$HOME/cloud-city
 build_id=${JOB_NAME}-${BUILD_NUMBER}
 s3cfg=$JUJU_HOME/juju-qa.s3cfg
@@ -16,5 +18,6 @@ $SCRIPTS/write_industrial_test_metadata.py $new_juju_dir/buildvars.json \
   $environment metadata.json
 s3cmd -c $s3cfg put metadata.json $s3base-metadata.json
 timeout -sINT -k 10m 1d $SCRIPTS/industrial_test.py $environment $new_juju \
-  $suite --attempts $attempts --json-file results.json $extra_args
+  --old-stable $old_stable_juju $suite --attempts $attempts \
+  --json-file results.json $extra_args
 s3cmd -c $s3cfg put results.json $s3base-results.json
