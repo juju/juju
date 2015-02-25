930d90809c2e68ffa724da9bac5e8c9efaaa8452
anastasia <anastasia.macmood@canonical.com>
2015-02-26 00:03:43 +1000

    Changed to cater for most cases including with floating points vs IPs but excluding negative numbers.

--- cmd/juju/statusnaturalsort.go
+++ cmd/juju/statusnaturalsort.go
@@ -4,12 +4,10 @@
 package main
 
 import (
-	"fmt"
-	"regexp"
 	"strconv"
+	"strings"
+	"unicode"
 )
-
-var splitRegexp = regexp.MustCompile("^(?P<prefix>.*?)(?P<number>\\d+)$")
 
 type naturally []string
 
@@ -24,28 +22,106 @@ func (n naturally) Swap(a, b int) {
 // Less sorts by non-numeric prefix and numeric suffix
 // when one exists.
 func (n naturally) Less(a, b int) bool {
-	aPrefix, aNumber := splitAtNumber(n[a])
-	bPrefix, bNumber := splitAtNumber(n[b])
-	if aPrefix == bPrefix {
-		return aNumber < bNumber
-	}
-	return n[a] < n[b]
+	return n.examineElement(n[a], n[b])
 }
 
-// splitAtNumber splits given string into prefix and numeric suffix.
-// If no numeric suffix exists, full original string is returned as
-// prefix with -1 as a suffix.
-func splitAtNumber(str string) (string, int) {
-	prefix := splitRegexp.ReplaceAllString(str, "$prefix")
-	number := splitRegexp.ReplaceAllString(str, "$number")
+// Recursion: no characters are ignored, not even white spaces and leading 0s
+func (n naturally) examineElement(a, b string) bool {
+	// CHECK IF SHOULD JUMP OUT OF RECURSION
+	if done, answer := n.isOneStringFinished(a, b); done {
+		return answer
+	}
 
-	if prefix == number {
-		// no number suffix exists
-		return str, -1
+	if compared, outcome := n.comparedNumbers(a, b); compared {
+		return outcome
 	}
-	new_s, err := strconv.Atoi(number)
-	if err != nil {
-		panic(fmt.Sprintf("parsing number %v", err)) // should never happen
+
+	// no numbers are encountered
+	if a[0] == b[0] {
+		return n.examineElement(a[1:], b[1:])
 	}
-	return prefix, new_s
+	return a[0] < b[0]
+}
+
+func (n naturally) comparedNumbers(a, b string) (compared, result bool) {
+	aIsNumber, aEnd, aFloat := n.extractNumber(a)
+	bIsNumber, bEnd, bFloat := n.extractNumber(b)
+
+	// dealing with 2 numbers
+	if aIsNumber && bIsNumber {
+		// if numbers are equal then recurse
+		if aFloat == bFloat {
+			return true, n.examineElement(aEnd, bEnd)
+		}
+		return true, aFloat < bFloat
+	}
+	if aIsNumber {
+		// number go before letter in alphanumeric ordering
+		return true, true
+	}
+	if bIsNumber {
+		// number go before letter in alphanumeric ordering
+		return true, false
+	}
+	return false, false
+}
+
+func (n naturally) isOneStringFinished(a, b string) (finished, result bool) {
+	// check a length
+	if len(a) == 0 {
+		// matters if b has something in it
+		if len(b) > 0 {
+			return true, true
+		}
+		return true, false
+	}
+	// check b length
+	if len(b) == 0 {
+		// matters if a has something in it
+		if len(a) > 0 {
+			return true, false
+		}
+		return true, true
+	}
+	return false, true
+}
+
+func (n naturally) extractNumber(original string) (found bool, suffix string, number float64) {
+	if unicode.IsDigit(rune(original[0])) {
+		found = true
+		end := strings.IndexFunc(original, func(r rune) bool {
+			return !unicode.IsDigit(r)
+		})
+		// Handle float vs IP
+		if end > -1 && end < len(original) {
+			//look forward is this a float, get its decimal?
+			// otherwise treat as dot separated numbers, for eg. IPs
+			if rune(original[end]) == rune("."[0]) {
+				//could be a float
+				maybeEnd := strings.IndexFunc(original[end+1:], func(r rune) bool {
+					return !unicode.IsDigit(r)
+				})
+				if maybeEnd == -1 {
+					// the whole string is a float
+					end = -1
+				} else if maybeEnd < len(original[end:]) {
+					if rune(original[maybeEnd+end+1]) != rune("."[0]) {
+						// just this is a float followed by other characters
+						end = end + maybeEnd + 1
+					}
+				}
+			}
+		}
+		convert := original
+		if end > -1 {
+			convert = original[:end]
+			suffix = original[end:]
+		}
+		var err error
+		number, err = strconv.ParseFloat(convert, 64)
+		if err != nil {
+			panic(err)
+		}
+	}
+	return found, suffix, number
 }
--- cmd/juju/statusnaturalsort_test.go
+++ cmd/juju/statusnaturalsort_test.go
@@ -33,7 +33,7 @@ func (s *naturalSortSuite) TestNaturallyAlpha(c *gc.C) {
 	)
 }
 
-func (s *naturalSortSuite) TestNaturallyAlphanumeric(c *gc.C) {
+func (s *naturalSortSuite) TestNaturallyAlphaNumeric(c *gc.C) {
 	s.assertNaturallySort(
 		c,
 		[]string{"a1", "a10", "a100", "a11"},
@@ -41,7 +41,7 @@ func (s *naturalSortSuite) TestNaturallyAlphanumeric(c *gc.C) {
 	)
 }
 
-func (s *naturalSortSuite) TestNaturallyAlphanumericSpecial(c *gc.C) {
+func (s *naturalSortSuite) TestNaturallySpecial(c *gc.C) {
 	s.assertNaturallySort(
 		c,
 		[]string{"a1", "a10", "a100", "a1/1", "1a"},
@@ -54,6 +54,38 @@ func (s *naturalSortSuite) TestNaturallyTagLike(c *gc.C) {
 		c,
 		[]string{"a1/1", "a1/11", "a1/2", "a1/7", "a1/100"},
 		[]string{"a1/1", "a1/2", "a1/7", "a1/11", "a1/100"},
+	)
+}
+
+func (s *naturalSortSuite) TestNaturallyMixed(c *gc.C) {
+	s.assertNaturallySort(
+		c,
+		[]string{"a1a", "a", "a1", "a1b", "a0", "a20", "a2", "a10"},
+		[]string{"a", "a0", "a1", "a1a", "a1b", "a2", "a10", "a20"},
+	)
+}
+
+func (s *naturalSortSuite) TestNaturallySeveralNumericParts(c *gc.C) {
+	s.assertNaturallySort(
+		c,
+		[]string{"x2-y08", "x2-g8", "x8-y8", "x2-y7"},
+		[]string{"x2-g8", "x2-y7", "x2-y08", "x8-y8"},
+	)
+}
+
+func (s *naturalSortSuite) TestNaturallyDecimalFractions(c *gc.C) {
+	s.assertNaturallySort(
+		c,
+		[]string{"1.002", "1.3", "1.001", "1.010", "1.02", "1.1"},
+		[]string{"1.001", "1.002", "1.010", "1.02", "1.1", "1.3"},
+	)
+}
+
+func (s *naturalSortSuite) TestNaturallyIPs(c *gc.C) {
+	s.assertNaturallySort(
+		c,
+		[]string{"100.001.010.123", "001.001.010.123", "001.002.010.123"},
+		[]string{"001.001.010.123", "001.002.010.123", "100.001.010.123"},
 	)
 }
 
