a0d9fafa2844cf3f896d9e4012db9416cc5d3ba7
William Reade <fwereade@gmail.com>
2015-02-25 14:08:30 +0100

    extract interfaces to own file; slightly clearer field name/use in tracker

---
+++ worker/leadership/interface.go
@@ -0,0 +1,29 @@
+// Copyright 2015 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package leadership
+
+import (
+	"github.com/juju/juju/worker"
+)
+
+// Ticket is used to communicate leadership status to Tracker clients.
+type Ticket interface {
+	Wait() bool
+}
+
+// Tracker allows clients to discover current leadership status by attempting to
+// claim it for themselves.
+type Tracker interface {
+
+	// ClaimLeader will return a Ticket which, when Wait()ed for, will return
+	// true if leadership is guaranteed for at least the tracker's duration from
+	// the time the ticket was issued.
+	ClaimLeader() Ticket
+}
+
+// TrackerWorker embeds the Tracker and worker.Worker interfaces.
+type TrackerWorker interface {
+	worker.Worker
+	Tracker
+}
--- worker/leadership/tracker.go
+++ worker/leadership/tracker.go
@@ -12,39 +12,33 @@ import (
 	"launchpad.net/tomb"
 
 	"github.com/juju/juju/leadership"
-	"github.com/juju/juju/worker"
 )
 
 var logger = loggo.GetLogger("juju.worker.leadership")
 
-// Ticket is used to communicate leadership status to Tracker clients.
-type Ticket interface {
-	Wait() bool
+// ticket is used with tracker to communicate leadership status back to a client.
+type ticket struct {
+	ch      chan bool
+	success bool
 }
 
-// Tracker allows clients to discover current leadership status by attempting to
-// claim it for themselves.
-type Tracker interface {
-
-	// ClaimLeader will return a Ticket which, when Wait()ed for, will return
-	// true if leadership is guaranteed for at least the tracker's duration from
-	// the time the ticket was issued.
-	ClaimLeader() Ticket
+// Wait is part of the Ticket interface.
+func (t *ticket) Wait() bool {
+	if <-t.ch {
+		t.success = true
+	}
+	return t.success
 }
 
-// TrackerWorker embeds the Tracker and worker.Worker interfaces.
-type TrackerWorker interface {
-	worker.Worker
-	Tracker
-}
-
+// tracker implements TrackerWorker.
 type tracker struct {
-	tomb        tomb.Tomb
-	leadership  leadership.LeadershipManager
-	unitName    string
-	serviceName string
-	duration    time.Duration
-	isMinion    bool
+	tomb            tomb.Tomb
+	leadership      leadership.LeadershipManager
+	unitName        string
+	serviceName     string
+	leaseDuration   time.Duration
+	overlapDuration time.Duration
+	isMinion        bool
 
 	claimLease   chan struct{}
 	renewLease   <-chan time.Time
@@ -53,17 +47,20 @@ type tracker struct {
 
 // NewTrackerWorker returns a TrackerWorker that attempts to claim and retain
 // service leadership for the supplied unit. It will claim leadership for twice
-// the supplied duration, and once it has it it will renew leadership every time
-// the duration elapses.
+// the supplied duration, and once it's leader it will renew leadership every
+// time the duration elapses.
+// Thus, successful leadership claims on the resulting Tracker will guarantee
+// leadership for the duration supplied here.
 func NewTrackerWorker(tag names.UnitTag, leadership leadership.LeadershipManager, duration time.Duration) TrackerWorker {
 	unitName := tag.Id()
 	serviceName, _ := names.UnitService(unitName)
 	t := &tracker{
-		unitName:     unitName,
-		serviceName:  serviceName,
-		leadership:   leadership,
-		duration:     duration * 2,
-		claimTickets: make(chan chan bool),
+		unitName:        unitName,
+		serviceName:     serviceName,
+		leadership:      leadership,
+		leaseDuration:   duration * 2,
+		overlapDuration: -duration,
+		claimTickets:    make(chan chan bool),
 	}
 	go func() {
 		defer t.tomb.Done()
@@ -121,8 +118,8 @@ func (t *tracker) loop() error {
 // latest known reality.
 func (t *tracker) refresh() error {
 	logger.Infof("checking leadership...")
-	untilTime := time.Now().Add(t.duration)
-	err := t.leadership.ClaimLeadership(t.serviceName, t.unitName, t.duration)
+	untilTime := time.Now().Add(t.leaseDuration)
+	err := t.leadership.ClaimLeadership(t.serviceName, t.unitName, t.leaseDuration)
 	switch {
 	case err == nil:
 		t.setLeader(untilTime)
@@ -134,17 +131,17 @@ func (t *tracker) refresh() error {
 	return nil
 }
 
-// setLeader arranges for lease renewal.
+// setLeader arranges for lease renewal .
 func (t *tracker) setLeader(untilTime time.Time) {
 	logger.Infof("leadership confirmed until %s", untilTime)
-	renewTime := untilTime.Add(-(t.duration / 2))
+	renewTime := untilTime.Add(t.overlapDuration)
 	logger.Infof("will renew at %s", renewTime)
 	t.isMinion = false
 	t.claimLease = nil
 	t.renewLease = time.After(renewTime.Sub(time.Now()))
 }
 
-// setLeader arranges for lease acquisition when there's an opportunity.
+// setMinion arranges for lease acquisition when there's an opportunity.
 func (t *tracker) setMinion() {
 	logger.Infof("leadership denied")
 	t.isMinion = true
@@ -202,18 +199,4 @@ func (t *tracker) confirm(ticket chan bool) error {
 	case ticket <- true:
 	}
 	return nil
-}
-
-// ticket is used with Tracker to communicate leadership status back to a client.
-type ticket struct {
-	ch      chan bool
-	success bool
-}
-
-// Wait is part of the Ticket interface.
-func (t *ticket) Wait() bool {
-	if <-t.ch {
-		t.success = true
-	}
-	return t.success
 }
