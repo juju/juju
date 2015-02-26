ffff4f301960daf6f3916c9c0f0e1df2adcdc6cd
Eric Snow <ericsnowcurrently@gmail.com>
2015-02-26 12:20:07 -0700

    Include the executable in return values of the commands type methods.

--- service/systemd/cmdline.go
+++ service/systemd/cmdline.go
@@ -13,67 +13,75 @@ import (
 
 const executable = "/bin/systemctl"
 
-type commands struct{}
+type commands struct {
+	binary string
+}
 
-// TODO(ericsnow) Always prepend `executable` to commands?
+func (c commands) resolve(args string) string {
+	binary := c.binary
+	if binary == "" {
+		binary = executable
+	}
+	return binary + " " + args
+}
 
-func (commands) listAll() string {
+func (c commands) listAll() string {
 	// We can't just use the same command as listRunning (with an extra
 	// "--all" because it misses some inactive units.
 	args := `list-unit-files --no-legend --no-page -t service` +
 		` | grep -o -P '^\w[\S]*(?=\.service)'`
-	return args
+	return c.resolve(args)
 }
 
-func (commands) listRunning() string {
+func (c commands) listRunning() string {
 	args := `--no-legend --no-page -t service` +
 		` | grep -o -P '^\w[\S]*(?=\.service)'`
-	return args
+	return c.resolve(args)
 }
 
-func (commands) activeStatus(name string) string {
+func (c commands) activeStatus(name string) string {
 	args := fmt.Sprintf("is-active %s.service || exit 0", name)
-	return args
+	return c.resolve(args)
 }
 
-func (commands) loadStatus(name string) string {
+func (c commands) loadStatus(name string) string {
 	args := fmt.Sprintf("is-enabled %s.service || exit 0", name)
-	return args
+	return c.resolve(args)
 }
 
-func (commands) start(name string) string {
+func (c commands) start(name string) string {
 	args := fmt.Sprintf("start %s.service", name)
-	return args
+	return c.resolve(args)
 }
 
-func (commands) stop(name string) string {
+func (c commands) stop(name string) string {
 	args := fmt.Sprintf("stop %s.service", name)
-	return args
+	return c.resolve(args)
 }
 
-func (commands) link(name, dirname string) string {
+func (c commands) link(name, dirname string) string {
 	args := fmt.Sprintf("link %s/%s.service", dirname, name)
-	return args
+	return c.resolve(args)
 }
 
-func (commands) enable(name string) string {
+func (c commands) enable(name string) string {
 	args := fmt.Sprintf("enable %s.service", name)
-	return args
+	return c.resolve(args)
 }
 
-func (commands) disable(name string) string {
+func (c commands) disable(name string) string {
 	args := fmt.Sprintf("disable %s.service", name)
-	return args
+	return c.resolve(args)
 }
 
-func (commands) conf(name string) string {
+func (c commands) conf(name string) string {
 	args := fmt.Sprintf("cat %s.service", name)
-	return args
+	return c.resolve(args)
 }
 
-func (commands) writeConf(name, dirname, data string) string {
-	args := fmt.Sprintf("cat >> %s/%s.service << 'EOF'\n%sEOF", dirname, name, data)
-	return args
+func (c commands) writeFile(name, dirname string, data []byte) string {
+	cmd := fmt.Sprintf("cat >> %s/%s.service << 'EOF'\n%sEOF", dirname, name, data)
+	return cmd
 }
 
 // Cmdline exposes the core operations of interacting with systemd units.
@@ -85,9 +93,9 @@ type Cmdline struct {
 
 // ListAll returns the names of all enabled systemd units.
 func (cl Cmdline) ListAll() ([]string, error) {
-	args := cl.commands.listAll()
+	cmd := cl.commands.listAll()
 
-	out, err := cl.runCommand(args)
+	out, err := cl.runCommand(cmd)
 	if err != nil {
 		return nil, errors.Trace(err)
 	}
@@ -101,7 +109,7 @@ func (cl Cmdline) ListAll() ([]string, error) {
 
 func (Cmdline) runCommand(cmd string) (string, error) {
 	resp, err := runCommands(exec.RunParams{
-		Commands: executable + " " + cmd,
+		Commands: cmd,
 	})
 	if err != nil {
 		return "", errors.Trace(err)
