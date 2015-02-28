dcf68cc1d64dff59d912ef1fab1557ce04b06fa9
Curtis Hovey <curtis@canonical.com>
2015-02-27 22:15:25 -0500

    Added tests for delete_unused_disks().

--- test_winazure.py
+++ test_winazure.py
@@ -2,11 +2,16 @@ from datetime import (
     datetime,
     timedelta,
 )
-from mock import patch
+from mock import (
+    Mock,
+    patch,
+)
 from unittest import TestCase
 
 from winazure import (
+    delete_unused_disks,
     main,
+    ServiceManagementService,
 )
 
 
@@ -40,3 +45,24 @@ class WinAzureTestCase(TestCase):
         sms_mock.assert_called_once_with('secret', 'cert.pem')
         l_mock.assert_called_once_with(
             'sms', glob='juju-deploy*', old_age=2, verbose=True, dry_run=True)
+
+    def test_delete_unused_disks(self):
+        sms = ServiceManagementService('secret', 'cert.pem')
+        disk1 = Mock()
+        disk1.name = 'disk1'
+        disk1.attached_to = None
+        disk2 = Mock()
+        disk2.name = 'disk2'
+        disk2.attached_to.hosted_service_name = ''
+        disk3 = Mock()
+        disk3.name = 'disk3'
+        disk3.attached_to.hosted_service_name = 'hs3'
+        disks = [disk1, disk2, disk3]
+        with patch.object(sms, 'list_disks', autospec=True,
+                          return_value=disks) as ld_mock:
+            with patch.object(sms, 'delete_disk', autospec=True) as dd_mock:
+                delete_unused_disks(sms, dry_run=False, verbose=False)
+        ld_mock.assert_called_once_with()
+        dd_mock.assert_any_call('disk1', delete_vhd=True)
+        dd_mock.assert_any_call('disk2', delete_vhd=True)
+        self.assertEqual(2, dd_mock.call_count)
