b2c8adf2ddaa9271eb6aa513e0002713196b183f
Eric Snow <ericsnowcurrently@gmail.com>
2015-03-04 16:59:55 -0700

    Improve the local discovery approach.

--- service/discovery_test.go
+++ service/discovery_test.go
@@ -24,6 +24,7 @@ const unknownExecutable = "/sbin/unknown/init/system"
 type discoveryTest struct {
 	os       version.OSType
 	series   string
+	exec     string
 	expected string
 }
 
@@ -44,6 +45,10 @@ func (dt discoveryTest) goos() string {
 }
 
 func (dt discoveryTest) executable(c *gc.C) string {
+	if dt.exec != "" {
+		return dt.exec
+	}
+
 	switch dt.expected {
 	case service.InitSystemUpstart:
 		return "/sbin/upstart"
@@ -59,9 +64,13 @@ func (dt discoveryTest) executable(c *gc.C) string {
 	}
 }
 
+func (dt discoveryTest) log(c *gc.C) {
+	c.Logf("testing {%q, %q, %q}...", dt.os, dt.series, dt.exec)
+}
+
 func (dt discoveryTest) disableLocalDiscovery(c *gc.C, s *discoverySuite) {
 	service.PatchGOOS(s, "<another OS>")
-	service.PatchPid1File(c, s, unknownExecutable)
+	service.PatchPid1File(c, s, unknownExecutable, "")
 }
 
 func (dt discoveryTest) disableVersionDiscovery(s *discoverySuite) {
@@ -70,9 +79,10 @@ func (dt discoveryTest) disableVersionDiscovery(s *discoverySuite) {
 	})
 }
 
-func (dt discoveryTest) setLocal(c *gc.C, s *discoverySuite) {
+func (dt discoveryTest) setLocal(c *gc.C, s *discoverySuite) string {
 	service.PatchGOOS(s, dt.goos())
-	service.PatchPid1File(c, s, dt.executable(c))
+	verText := "..." + dt.expected + "..."
+	return service.PatchPid1File(c, s, dt.executable(c), verText)
 }
 
 func (dt discoveryTest) setVersion(s *discoverySuite) version.Binary {
@@ -193,9 +203,25 @@ func (s *discoverySuite) TestDiscoverServiceLocalHost(c *gc.C) {
 	test.checkService(c, svc, err, s.name, s.conf)
 }
 
-func (s *discoverySuite) TestDiscoverServiceVersionLocal(c *gc.C) {
+func (s *discoverySuite) TestDiscoverServiceGeneric(c *gc.C) {
+	test := discoveryTest{
+		os:       version.Ubuntu,
+		series:   "trusty",
+		exec:     "/sbin/init",
+		expected: service.InitSystemUpstart,
+	}
+
+	test.setLocal(c, s)
+	test.disableVersionDiscovery(s)
+
+	svc, err := service.DiscoverService(s.name, s.conf)
+
+	test.checkService(c, svc, err, s.name, s.conf)
+}
+
+func (s *discoverySuite) TestDiscoverServiceLocalOnly(c *gc.C) {
 	for _, test := range discoveryTests {
-		c.Logf("testing {%q, %q}...", test.os, test.series)
+		test.log(c)
 
 		test.setLocal(c, s)
 		test.disableVersionDiscovery(s)
@@ -208,7 +234,7 @@ func (s *discoverySuite) TestDiscoverServiceVersionLocal(c *gc.C) {
 
 func (s *discoverySuite) TestDiscoverServiceVersionFallback(c *gc.C) {
 	for _, test := range discoveryTests {
-		c.Logf("testing {%q, %q}...", test.os, test.series)
+		test.log(c)
 
 		test.disableLocalDiscovery(c, s)
 		test.setVersion(s)
@@ -221,7 +247,7 @@ func (s *discoverySuite) TestDiscoverServiceVersionFallback(c *gc.C) {
 
 func (s *discoverySuite) TestVersionInitSystem(c *gc.C) {
 	for _, test := range discoveryTests {
-		c.Logf("testing {%q, %q}...", test.os, test.series)
+		test.log(c)
 
 		vers := test.setVersion(s)
 
--- service/export_test.go
+++ service/export_test.go
@@ -5,7 +5,9 @@ package service
 
 import (
 	"io/ioutil"
+	"os"
 	"path/filepath"
+	"runtime"
 
 	jc "github.com/juju/testing/checkers"
 	gc "gopkg.in/check.v1"
@@ -25,11 +27,26 @@ func PatchGOOS(patcher patcher, os string) {
 	patcher.PatchValue(&runtimeOS, os)
 }
 
-func PatchPid1File(c *gc.C, patcher patcher, executable string) {
+func PatchPid1File(c *gc.C, patcher patcher, executable, verText string) string {
 	dirname := c.MkDir()
+
+	exeSuffix := ".sh"
+	if runtime.GOOS == "windows" {
+		executable = filepath.FromSlash(executable)
+		exeSuffix = ".bat"
+	}
+	exeName := filepath.Join(dirname, executable) + exeSuffix
+	if verText != "" {
+		err := os.MkdirAll(filepath.Dir(exeName), 0755)
+		c.Assert(err, jc.ErrorIsNil)
+		err = ioutil.WriteFile(exeName, []byte("#!/usr/bin/env bash\necho "+verText), 0755)
+		c.Assert(err, jc.ErrorIsNil)
+	}
+
 	filename := filepath.Join(dirname, "pid1cmdline")
-	err := ioutil.WriteFile(filename, []byte(executable), 0644)
+	err := ioutil.WriteFile(filename, []byte(exeName), 0644)
 	c.Assert(err, jc.ErrorIsNil)
 
 	patcher.PatchValue(&pid1File, filename)
+	return exeName
 }
