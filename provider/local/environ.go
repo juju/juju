008c80331e4e1455b353142ceea83c4aa3d64e76
Eric Snow <ericsnowcurrently@gmail.com>
2015-03-04 10:13:05 -0700

    Log errors from stopping/removing services.

--- provider/local/environ.go
+++ provider/local/environ.go
@@ -515,13 +515,20 @@ func (env *localEnviron) Destroy() error {
 			}
 		}
 	}
-	// Stop the mongo database and machine agent. It's possible that the
-	// service doesn't exist or is not running, so don't check the error.
-	mongoRemoveService(env.config.namespace())
+	// Stop the mongo database and machine agent. We log any errors but
+	// do not fail, so that remaining "destroy" steps will still happen.
+	err = mongoRemoveService(env.config.namespace())
+	if err != nil && !errors.IsNotFound(err) {
+		logger.Errorf("while stopping mongod: %v", err)
+	}
 	svc, err := discoverService(env.machineAgentServiceName())
 	if err == nil {
-		svc.Stop()
-		svc.Remove()
+		if err := svc.Stop(); err != nil {
+			logger.Errorf("while stopping machine agent: %v", err)
+		}
+		if err := svc.Remove(); err != nil {
+			logger.Errorf("while disabling machine agent: %v", err)
+		}
 	}
 
 	// Finally, remove the data-dir.
