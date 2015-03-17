51823136d1a7e4c7056d0b4d02e77a6fc21930b8
Michael Foord <michael.foord@canonical.com>
2015-03-17 10:22:47 +0000

    Skeleton test

--- worker/addresser/worker_test.go
+++ worker/addresser/worker_test.go
@@ -5,6 +5,7 @@ package addresser_test
 
 import (
 	"github.com/juju/juju/juju/testing"
+	jc "github.com/juju/testing/checkers"
 	gc "gopkg.in/check.v1"
 )
 
@@ -12,4 +13,8 @@ var _ = gc.Suite(&workerSuite{})
 
 type workerSuite struct {
 	testing.JujuConnSuite
+}
+
+func (s *workerSuite) TestSomething(c *gc.C) {
+	c.Assert(false, jc.IsTrue)
 }
