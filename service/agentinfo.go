07d7cf37e3df68d4c182f079bf0362c060499cd3
Eric Snow <ericsnowcurrently@gmail.com>
2015-03-17 14:34:04 -0600

    Add doc comments.

--- service/agentinfo.go
+++ service/agentinfo.go
@@ -12,10 +12,12 @@ import (
 	"github.com/juju/juju/agent/tools"
 )
 
-// TODO(ericsnow) Move to the agent package.
+// TODO(ericsnow) Move this file to the agent package.
 
+// AgentKind identifies the kind of agent.
 type AgentKind string
 
+// These are the agent kinds in juju.
 const (
 	AgentKindMachine AgentKind = "machine"
 	AgentKindUnit    AgentKind = "unit"
@@ -26,15 +28,25 @@ var idOptions = map[AgentKind]string{
 	AgentKindUnit:    "--unit-name",
 }
 
+// AgentInfo holds commonly used information about a juju agent.
 type AgentInfo struct {
-	Kind    AgentKind
-	ID      string
-	DataDir string
-	LogDir  string
-
 	name string
+
+	// ID is the string that identifies the agent uniquely within
+	// a juju environment.
+	ID string
+
+	// Kind is the kind of agent.
+	Kind AgentKind
+
+	// DataDir is the path to the agent's data dir.
+	DataDir string
+
+	// LogDir is the path to the agent's log dir.
+	LogDir string
 }
 
+// NewAgentInfo composes a new AgentInfo for the given essentials.
 func NewAgentInfo(kind AgentKind, id, dataDir, logDir string) AgentInfo {
 	name := fmt.Sprintf("%s-%s", kind, strings.Replace(id, "/", "-", -1))
 
@@ -49,14 +61,17 @@ func NewAgentInfo(kind AgentKind, id, dataDir, logDir string) AgentInfo {
 	return info
 }
 
+// NewMachineAgentInfo returns a new AgentInfo for a machine agent.
 func NewMachineAgentInfo(id, dataDir, logDir string) AgentInfo {
 	return NewAgentInfo(AgentKindMachine, id, dataDir, logDir)
 }
 
+// NewUnitAgentInfo returns a new AgentInfo for a unit agent.
 func NewUnitAgentInfo(id, dataDir, logDir string) AgentInfo {
 	return NewAgentInfo(AgentKindUnit, id, dataDir, logDir)
 }
 
+// ToolsDir returns the path to the agent's tools dir.
 func (ai AgentInfo) ToolsDir(renderer shell.Renderer) string {
 	return renderer.FromSlash(tools.ToolsDir(ai.DataDir, ai.name))
 }
