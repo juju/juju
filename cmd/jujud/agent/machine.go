e43956e92a6f37a92f45587695e67e0a5dff66ca
Andrew Wilkins <axwalk@gmail.com>
2015-03-18 14:00:46 +0800

    cmd/jujud/agent: disable environ storage

--- cmd/jujud/agent/machine.go
+++ cmd/jujud/agent/machine.go
@@ -668,7 +668,10 @@ func (a *MachineAgent) postUpgradeAPIWorker(
 			storageDir := filepath.Join(agentConfig.DataDir(), "storage")
 			return newStorageWorker(storageDir, api, api), nil
 		})
-		if isEnvironManager {
+		// TODO(axw) enable environ storage provisioner when it
+		// ignores non-dynamic storage. Until then, we have a
+		// race condition.
+		if isEnvironManager && false {
 			runner.StartWorker("storageprovisioner-environ", func() (worker.Worker, error) {
 				api := st.StorageProvisioner(agentConfig.Environment())
 				return newStorageWorker("", api, api), nil
--- cmd/jujud/agent/machine_test.go
+++ cmd/jujud/agent/machine_test.go
@@ -1251,7 +1251,7 @@ func (s *MachineSuite) TestMachineAgentRunsEnvironStorageWorker(c *gc.C) {
 	s.SetFeatureFlags(feature.Storage)
 	featureflag.SetFlagsFromEnvironment(osenv.JujuFeatureFlagEnvKey)
 	// TODO(wallyworld) - worker is currently disabled even with feature flag
-	s.testMachineAgentRunsEnvironStorageWorkers(c, true, coretesting.LongWait)
+	s.testMachineAgentRunsEnvironStorageWorkers(c, false, coretesting.LongWait)
 }
 
 func (s *MachineSuite) testMachineAgentRunsEnvironStorageWorkers(c *gc.C, shouldRun bool, timeout time.Duration) {
