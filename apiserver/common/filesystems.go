417e64596e6db4e6b94cee9990b83ae31c0af257
Andrew Wilkins <axwalk@gmail.com>
2015-03-19 18:20:55 +0800

    storageprovisioner: FilesystemParams

--- apiserver/common/filesystems.go
+++ apiserver/common/filesystems.go
@@ -4,10 +4,47 @@
 package common
 
 import (
+	"fmt"
+
 	"github.com/juju/errors"
 	"github.com/juju/juju/apiserver/params"
 	"github.com/juju/juju/state"
+	"github.com/juju/juju/storage/poolmanager"
 )
+
+type filesystemAlreadyProvisionedError struct {
+	error
+}
+
+// IsFilesystemAlreadyProvisioned returns true if the specified error
+// is caused by a filesystem already being provisioned.
+func IsFilesystemAlreadyProvisioned(err error) bool {
+	_, ok := err.(*filesystemAlreadyProvisionedError)
+	return ok
+}
+
+// FilesystemParams returns the parameters for creating the given filesystem.
+func FilesystemParams(v state.Filesystem, poolManager poolmanager.PoolManager) (params.FilesystemParams, error) {
+	stateFilesystemParams, ok := v.Params()
+	if !ok {
+		err := &filesystemAlreadyProvisionedError{fmt.Errorf(
+			"filesystem %q is already provisioned", v.Tag().Id(),
+		)}
+		return params.FilesystemParams{}, err
+	}
+
+	providerType, cfg, err := StoragePoolConfig(stateFilesystemParams.Pool, poolManager)
+	if err != nil {
+		return params.FilesystemParams{}, errors.Trace(err)
+	}
+	return params.FilesystemParams{
+		v.Tag().String(),
+		stateFilesystemParams.Size,
+		string(providerType),
+		cfg.Attrs(),
+		nil, // attachment params set by the caller
+	}, nil
+}
 
 // FilesystemFromState converts a state.Filesystem to params.Filesystem.
 func FilesystemFromState(v state.Filesystem) (params.Filesystem, error) {
