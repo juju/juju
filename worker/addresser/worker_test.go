167f63e492770af061e2d1913819ea2f2abd0f3b
Michael Foord <michael.foord@canonical.com>
2015-03-19 17:02:45 +0000

    Initial test

--- worker/addresser/worker_test.go
+++ worker/addresser/worker_test.go
@@ -4,11 +4,15 @@
 package addresser_test
 
 import (
+	stdtesting "testing"
+
 	jc "github.com/juju/testing/checkers"
 	gc "gopkg.in/check.v1"
 
 	"github.com/juju/juju/juju/testing"
 	coretesting "github.com/juju/juju/testing"
+	"github.com/juju/juju/worker"
+	"github.com/juju/juju/worker/addresser"
 )
 
 func TestPackage(t *stdtesting.T) {
@@ -21,6 +25,11 @@ type workerSuite struct {
 	testing.JujuConnSuite
 }
 
-func (s *workerSuite) TestSomething(c *gc.C) {
+func (s *workerSuite) TestWorker(c *gc.C) {
+	s.State.StartSync()
+	w := addresser.NewWorker(s.State)
+	defer func() {
+		c.Assert(worker.Stop(w), gc.IsNil)
+	}()
 	c.Assert(false, jc.IsTrue)
 }
