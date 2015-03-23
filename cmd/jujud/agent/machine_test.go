772fab67d956ccc3fd23df98c02a87b27bbf1b29
Andrew Wilkins <axwalk@gmail.com>
2015-03-23 11:22:47 +0800

    cmd/jujud/agent: enable env storage provisioner

--- cmd/jujud/agent/machine_test.go
+++ cmd/jujud/agent/machine_test.go
@@ -1253,8 +1253,7 @@ func (s *MachineSuite) testMachineAgentRunsMachineStorageWorker(c *gc.C, shouldR
 func (s *MachineSuite) TestMachineAgentRunsEnvironStorageWorker(c *gc.C) {
 	s.SetFeatureFlags(feature.Storage)
 	featureflag.SetFlagsFromEnvironment(osenv.JujuFeatureFlagEnvKey)
-	// TODO(wallyworld) - worker is currently disabled even with feature flag
-	s.testMachineAgentRunsEnvironStorageWorkers(c, false, coretesting.LongWait)
+	s.testMachineAgentRunsEnvironStorageWorkers(c, true, coretesting.LongWait)
 }
 
 func (s *MachineSuite) testMachineAgentRunsEnvironStorageWorkers(c *gc.C, shouldRun bool, timeout time.Duration) {
