08db63a6ae2d1ad018d7b239544200925e2cb537
James Tunnicliffe <james.tunnicliffe@nanosheep.org>
2015-03-23 14:17:43 +0000

    Added create command

--- cmd/juju/space/space.go
+++ cmd/juju/space/space.go
@@ -6,15 +6,17 @@ package space
 import (
 	"github.com/juju/cmd"
 	"github.com/juju/loggo"
+
+	"github.com/juju/juju/cmd/envcmd"
 )
 
-var logger = loggo.GetLogger("juju.cmd.juju.spaces")
+var logger = loggo.GetLogger("juju.cmd.spaces")
 
 const commandDoc = `
 "juju spaces" provides commands to interact with Juju network spaces.
 `
 
-// NewSuperCommand creates the environment supercommand and registers the
+// NewSuperCommand creates the "space" supercommand and registers the
 // subcommands that it supports.
 func NewSuperCommand() cmd.Command {
 	spaceCmd := cmd.NewSuperCommand(cmd.SuperCommandParams{
@@ -23,6 +25,13 @@ func NewSuperCommand() cmd.Command {
 		UsagePrefix: "juju",
 		Purpose:     "manage network spaces",
 	})
+	spaceCmd.Register(envcmd.Wrap(&CreateCommand{}))
 
 	return spaceCmd
+}
+
+// SpaceCommandBase is a helper base structure that has a method to get the
+// space managing client.
+type SpaceCommandBase struct {
+	envcmd.EnvCommandBase
 }
--- cmd/juju/space/space_test.go
+++ cmd/juju/space/space_test.go
@@ -15,6 +15,7 @@ import (
 )
 
 var expectedSubCommmandNames = []string{
+	"create",
 	"help",
 }
 
