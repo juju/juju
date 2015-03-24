3dac9a08815c4eb6ab631d3b3a53718e4ceee147
Michael Foord <michael.foord@canonical.com>
2015-03-24 16:58:54 +0000

    Retry failed releasing

--- worker/addresser/worker.go
+++ worker/addresser/worker.go
@@ -4,8 +4,11 @@
 package addresser
 
 import (
+	"time"
+
 	"github.com/juju/errors"
 	"github.com/juju/loggo"
+	"github.com/juju/utils"
 
 	apiWatcher "github.com/juju/juju/api/watcher"
 	"github.com/juju/juju/environs"
@@ -17,6 +20,11 @@ import (
 )
 
 var logger = loggo.GetLogger("juju.worker.addresser")
+
+var shortAttempt = utils.AttemptStrategy{
+	Total: 5 * time.Second,
+	Delay: 300 * time.Millisecond,
+}
 
 type releaser interface {
 	// ReleaseAddress has the same signature as the same method in the
@@ -103,15 +111,18 @@ func (a *addresserHandler) releaseIPAddress(addr *state.IPAddress) (err error) {
 	if err != nil {
 		return errors.Annotatef(err, "cannot get machine %q instance ID", addr.MachineId())
 	}
-	err = a.releaser.ReleaseAddress(instId, network.Id(addr.SubnetId()), addr.Address())
-	if err != nil {
-		// Don't remove the address from state so we
-		// can retry releasing the address later.
-		logger.Warningf("cannot release address %q: %v (will retry)", addr.Value(), err)
-		return errors.Trace(err)
-	}
 
-	return nil
+	netId := network.Id(addr.SubnetId())
+	for attempt := shortAttempt.Start(); attempt.Next(); {
+		err = a.releaser.ReleaseAddress(instId, netId, addr.Address())
+		if err == nil {
+			return nil
+		}
+	}
+	// Don't remove the address from state so we
+	// can retry releasing the address later.
+	logger.Warningf("cannot release address %q: %v (will retry)", addr.Value(), err)
+	return errors.Trace(err)
 }
 
 // SetUp is part of the StringsWorker interface.
