0c717748ab5f471d848d1b2c8b1d1837a537e9a9
Martin Packman <martin.packman@canonical.com>
2015-03-30 18:47:08 +0000

    Fix git gate arg tests given added restriction

--- test_git_gate.py
+++ test_git_gate.py
@@ -6,20 +6,23 @@ import git_gate
 
 class TestParseArgs(unittest.TestCase):
 
-    def test_project_only(self):
-        args = git_gate.parse_args(["--project", "git.testing/project"])
+    def test_project_and_url(self):
+        args = git_gate.parse_args(
+            ["--project", "git.testing/project",
+             "--project-url", "https://git.testing/project"])
         self.assertEqual(args.project, "git.testing/project")
+        self.assertEqual(args.project_url, "https://git.testing/project")
 
     def test_project_and_ref(self):
         args = git_gate.parse_args(
-            ["--project", "git.testing/project",
+            ["--project", "git.testing/project", "--go-get-all",
              "--project-ref", "v1"])
         self.assertEqual(args.project, "git.testing/project")
         self.assertEqual(args.project_ref, "v1")
 
     def test_merging_other(self):
         args = git_gate.parse_args(
-            ["--project", "git.testing/project",
+            ["--project", "git.testing/project", "--go-get-all",
              "--merge-url", "git.testing/proposed"])
         self.assertEqual(args.project, "git.testing/project")
         self.assertEqual(args.merge_url, "git.testing/proposed")
@@ -27,7 +30,7 @@ class TestParseArgs(unittest.TestCase):
 
     def test_merging_other_ref(self):
         args = git_gate.parse_args(
-            ["--project", "git.testing/project",
+            ["--project", "git.testing/project", "--go-get-all",
              "--merge-url", "git.testing/proposed", "--merge-ref", "feature"])
         self.assertEqual(args.project, "git.testing/project")
         self.assertEqual(args.merge_url, "git.testing/proposed")
@@ -36,8 +39,10 @@ class TestParseArgs(unittest.TestCase):
     def test_project_with_deps(self):
         args = git_gate.parse_args(
             ["--project", "git.testing/project",
+             "--project-url", "https://git.testing/project",
              "--dependencies", "git.testing/a", "git.testing/b"])
         self.assertEqual(args.project, "git.testing/project")
+        self.assertEqual(args.project_url, "https://git.testing/project")
         self.assertEqual(args.dependencies, ["git.testing/a", "git.testing/b"])
         self.assertEqual(args.go_get_all, False)
 
