f751d36c50a250b2b6330dacebff2f10f98e5a1b
bogdanteleaga <bteleaga@cloudbasesolutions.com>
2015-04-14 10:18:07 +0300

    Fixed some tests

--- worker/provisioner/container_initialisation_test.go
+++ worker/provisioner/container_initialisation_test.go
@@ -277,7 +277,7 @@ func (s *ContainerSetupSuite) TestContainerManagerConfigName(c *gc.C) {
 	expect("any-old-thing")
 }
 
-func (s *ContainerSetupSuite) assertContainerInitialised(c *gc.C, ctype instance.ContainerType, packages []string) {
+func (s *ContainerSetupSuite) assertContainerInitialised(c *gc.C, ctype instance.ContainerType, packages [][]string) {
 	// A noop worker callback.
 	startProvisionerWorker := func(runner worker.Runner, containerType instance.ContainerType,
 		pr *apiprovisioner.State, cfg agent.Config, broker environs.InstanceBroker,
@@ -313,23 +313,29 @@ func (s *ContainerSetupSuite) assertContainerInitialised(c *gc.C, ctype instance
 		c.Assert(s.fakeLXCNet, jc.DoesNotExist)
 	}
 
-	cmd := <-s.aptCmdChan
-	c.Assert(cmd.Env[len(cmd.Env)-1], gc.Equals, "DEBIAN_FRONTEND=noninteractive")
-	expected := []string{
-		"apt-get", "--option=Dpkg::Options::=--force-confold",
-		"--option=Dpkg::options::=--force-unsafe-io", "--assume-yes", "--quiet",
-		"install"}
-	expected = append(expected, packages...)
-	c.Assert(cmd.Args, gc.DeepEquals, expected)
+	for _, pack := range packages {
+		cmd := <-s.aptCmdChan
+		c.Assert(cmd.Env[len(cmd.Env)-1], gc.Equals, "DEBIAN_FRONTEND=noninteractive")
+		expected := []string{
+			"apt-get", "--option=Dpkg::Options::=--force-confold",
+			"--option=Dpkg::options::=--force-unsafe-io", "--assume-yes", "--quiet",
+			"install"}
+		expected = append(expected, pack...)
+		c.Assert(cmd.Args, gc.DeepEquals, expected)
+	}
 }
 
 func (s *ContainerSetupSuite) TestContainerInitialised(c *gc.C) {
 	for _, test := range []struct {
 		ctype    instance.ContainerType
-		packages []string
+		packages [][]string
 	}{
-		{instance.LXC, []string{"--target-release", "precise-updates/cloud-tools", "lxc", "cloud-image-utils"}},
-		{instance.KVM, []string{"uvtool-libvirt", "uvtool"}},
+		{instance.LXC, [][]string{
+			[]string{"--target-release", "precise-updates/cloud-tools", "lxc"},
+			[]string{"--target-release", "precise-updates/cloud-tools", "cloud-image-utils"}}},
+		{instance.KVM, [][]string{
+			[]string{"uvtool-libvirt"},
+			[]string{"uvtool"}}},
 	} {
 		s.assertContainerInitialised(c, test.ctype, test.packages)
 	}
