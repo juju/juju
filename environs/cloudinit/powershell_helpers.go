ca1d9ec7e16c58d20ec5339d5472b1eb485ee1f1
Tim Penhey <tim@penhey.net>
2015-04-15 03:10:50 +1200

    Move and tweak comment.

--- environs/cloudinit/powershell_helpers.go
+++ environs/cloudinit/powershell_helpers.go
@@ -2,12 +2,14 @@
 // Copyright 2014, 2015 Cloudbase Solutions
 // Copyright 2012 Aaron Jensen
 //
-// This file borrowed some code from https://bitbucket.org/splatteredbits/carbon.
-// This external source was licensed under apache-2.0 which compatible with AGPLv3.
-// Because it's compatible we can and have licensed this derived work under AGPLv3.
-// The original license for the external source can be found inside Apache-License.txt
-//
 // Licensed under the AGPLv3, see LICENCE file for details.
+//
+// This file borrowed some code from https://bitbucket.org/splatteredbits/carbon
+// (see Source/Security/Privilege.cs). This external source is licensed under
+// Apache-2.0 license which is compatible with AGPLv3 license. Because it's
+// compatible we can and have licensed this derived work under AGPLv3. The original
+// Apache-2.0 license for the external source can be found inside Apache-License.txt.
+// Copyright statement of the external source: Copyright 2012 Aaron Jensen
 
 package cloudinit
 
