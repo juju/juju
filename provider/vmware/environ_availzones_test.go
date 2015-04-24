f900a310236130ca810359d63116573d75748eee
Sergey Matyukevich <s.matyukevich@gmail.com>
2015-04-24 09:56:01 -0600

    Add the generic vmware (vSphere) provider.

---
+++ provider/vmware/environ_availzones_test.go
@@ -0,0 +1,48 @@
+// Copyright 2015 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package vmware_test
+
+import (
+	jc "github.com/juju/testing/checkers"
+	gc "gopkg.in/check.v1"
+
+	"github.com/juju/juju/instance"
+	"github.com/juju/juju/provider/common"
+	"github.com/juju/juju/provider/vmware"
+)
+
+type environAvailzonesSuite struct {
+	vmware.BaseSuite
+}
+
+var _ = gc.Suite(&environAvailzonesSuite{})
+
+func (s *environAvailzonesSuite) SetUpTest(c *gc.C) {
+	s.BaseSuite.SetUpTest(c)
+}
+
+func (s *environAvailzonesSuite) TestAvailabilityZones(c *gc.C) {
+	client := vmware.ExposeEnvFakeClient(s.Env)
+	s.FakeAvailabilityZones(client, "z1", "z2")
+	zones, err := s.Env.AvailabilityZones()
+
+	c.Assert(err, jc.ErrorIsNil)
+	c.Assert(len(zones), gc.Equals, 2)
+	c.Assert(zones[0].Name(), gc.Equals, "z1")
+	c.Assert(zones[1].Name(), gc.Equals, "z2")
+}
+
+func (s *environAvailzonesSuite) TestInstanceAvailabilityZoneNames(c *gc.C) {
+	client := vmware.ExposeEnvFakeClient(s.Env)
+	client.SetPropertyProxyHandler("FakeDatacenter", vmware.RetrieveDatacenterProperties)
+	vmName := common.MachineFullName(s.Env, "1")
+	s.FakeInstancesWithResourcePool(client, vmware.InstRp{Inst: vmName, Rp: "rp1"})
+	s.FakeAvailabilityZonesWithResourcePool(client, vmware.ZoneRp{Zone: "z1", Rp: "rp1"}, vmware.ZoneRp{Zone: "z2", Rp: "rp2"})
+
+	zones, err := s.Env.InstanceAvailabilityZoneNames([]instance.Id{instance.Id(vmName)})
+
+	c.Assert(err, jc.ErrorIsNil)
+	c.Assert(len(zones), gc.Equals, 1)
+	c.Assert(zones[0], gc.Equals, "z1")
+}
---
+++ provider/vmware/provider_test.go
@@ -0,0 +1,64 @@
+// Copyright 2015 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package vmware_test
+
+import (
+	jc "github.com/juju/testing/checkers"
+	gc "gopkg.in/check.v1"
+
+	"github.com/juju/juju/environs"
+	envtesting "github.com/juju/juju/environs/testing"
+	"github.com/juju/juju/provider/vmware"
+)
+
+type providerSuite struct {
+	vmware.BaseSuite
+
+	provider environs.EnvironProvider
+}
+
+var _ = gc.Suite(&providerSuite{})
+
+func (s *providerSuite) SetUpTest(c *gc.C) {
+	s.BaseSuite.SetUpTest(c)
+
+	var err error
+	s.provider, err = environs.Provider("vmware")
+	c.Check(err, jc.ErrorIsNil)
+}
+
+func (s *providerSuite) TestRegistered(c *gc.C) {
+	c.Assert(s.provider, gc.Equals, vmware.Provider)
+}
+
+func (s *providerSuite) TestOpen(c *gc.C) {
+	env, err := s.provider.Open(s.Config)
+	c.Check(err, jc.ErrorIsNil)
+
+	envConfig := env.Config()
+	c.Assert(envConfig.Name(), gc.Equals, "testenv")
+}
+
+func (s *providerSuite) TestPrepareForBootstrap(c *gc.C) {
+	env, err := s.provider.PrepareForBootstrap(envtesting.BootstrapContext(c), s.Config)
+	c.Check(err, jc.ErrorIsNil)
+	c.Check(env, gc.NotNil)
+}
+
+func (s *providerSuite) TestValidate(c *gc.C) {
+	validCfg, err := s.provider.Validate(s.Config, nil)
+	c.Check(err, jc.ErrorIsNil)
+
+	validAttrs := validCfg.AllAttrs()
+	c.Assert(s.Config.AllAttrs(), gc.DeepEquals, validAttrs)
+}
+
+func (s *providerSuite) TestSecretAttrs(c *gc.C) {
+	obtainedAttrs, err := s.provider.SecretAttrs(s.Config)
+	c.Check(err, jc.ErrorIsNil)
+
+	expectedAttrs := map[string]string{"password": "password1"}
+	c.Assert(obtainedAttrs, gc.DeepEquals, expectedAttrs)
+
+}
