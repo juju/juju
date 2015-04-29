a286cb8def68ac6446101d37bbfea089d59346e2
John George <john.george@canonical.com>
2015-04-28 22:00:56 -0700

    Rename the chaos monkey charm directory, move most setup to the start action, add show-logs and show-monkey-ids actions.

--- trusty/cmonkey/hooks/cp_chaos_monkey.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -eux
-chaos_dir="$(config-get chaos-dir)"
-[ -d ${chaos_dir} ] && mv ${chaos_dir} ${chaos_dir}.$$
-cp -a ${CHARM_DIR}/chaos-monkey ${chaos_dir}
-mkdir -p ${chaos_dir}/log
-chown -R ubuntu:ubuntu ${chaos_dir}
