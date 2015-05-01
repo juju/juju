4b7faaff2ac7298158850e610294fbdf986d4280
Ian Booth <ian.booth@canonical.com>
2015-05-01 11:12:53 +1000

    Add option to set the CLI compatible version and use with juju status

--- feature/flags.go
+++ feature/flags.go
@@ -38,10 +38,6 @@ const LegacyUpstart = "legacy-upstart"
 // MongoDB instead of to all-machines.log using rsyslog.
 const DbLog = "db-log"
 
-// NewStatus is the name of the feature to enable the new
-// juju status output.
-const NewStatus = "new-status"
-
 // CloudSigma enables the CloudSigma provider.
 const CloudSigma = "cloudsigma"
 
