e4f1129ccfa45071a4b9993c599c42a9dd15611b
Eric Snow <ericsnowcurrently@gmail.com>
2015-05-06 18:58:45 -0600

    Drop dead code.

--- environs/cloudinit/cloudinit.go
+++ environs/cloudinit/cloudinit.go
@@ -30,7 +30,6 @@ import (
 	"github.com/juju/juju/instance"
 	"github.com/juju/juju/mongo"
 	"github.com/juju/juju/service"
-	"github.com/juju/juju/service/common"
 	"github.com/juju/juju/state/multiwatcher"
 	coretools "github.com/juju/juju/tools"
 	"github.com/juju/juju/version"
@@ -285,32 +284,6 @@ func (cfg *MachineConfig) machineAgentCommands() ([]string, string, error) {
 		return nil, "", errors.Annotatef(err, "cannot make cloud-init init script for the %s agent", name)
 	}
 	return cmds, toolsDir, nil
-}
-
-func (cfg *MachineConfig) initService() (service.Service, string, error) {
-	conf, toolsDir := service.MachineAgentConf(
-		cfg.MachineId,
-		cfg.DataDir,
-		cfg.LogDir,
-		strings.ToLower(cfg.Tools.Version.OS.String()),
-	)
-
-	name := cfg.MachineAgentServiceName
-	initSystem, ok := cfg.initSystem()
-	if !ok {
-		return nil, "", errors.New("could not identify init system")
-	}
-	logger.Debugf("using init system %q for machine agent script", initSystem)
-	svc, err := newService(name, conf, initSystem)
-	return svc, toolsDir, errors.Trace(err)
-}
-
-func (cfg *MachineConfig) initSystem() (string, bool) {
-	return service.VersionInitSystem(cfg.Tools.Version)
-}
-
-var newService = func(name string, conf common.Conf, initSystem string) (service.Service, error) {
-	return service.NewService(name, conf, initSystem)
 }
 
 func (cfg *MachineConfig) dataFile(name string) string {
