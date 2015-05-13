bdb68010a5a6c2748dfc22c58a13d88cc2383fb9
Tim Penhey <tim@penhey.net>
2015-05-14 11:46:22 +1200

    Add Update and UpdateId to the collection and use UpdateId to update the timestamps.

--- state/envuser.go
+++ state/envuser.go
@@ -76,14 +76,13 @@ func (e *EnvironmentUser) LastConnection() *time.Time {
 
 // UpdateLastConnection updates the last connection time of the environment user.
 func (e *EnvironmentUser) UpdateLastConnection() error {
+	envUsers, closer := e.st.getCollection(envUsersC)
+	defer closer()
+
 	timestamp := nowToTheSecond()
-	ops := []txn.Op{{
-		C:      envUsersC,
-		Id:     e.ID(),
-		Assert: txn.DocExists,
-		Update: bson.D{{"$set", bson.D{{"lastconnection", timestamp}}}},
-	}}
-	if err := e.st.runTransaction(ops); err != nil {
+	update := bson.D{{"$set", bson.D{{"lastconnection", timestamp}}}}
+
+	if err := envUsers.UpdateId(e.ID(), update); err != nil {
 		return errors.Annotatef(err, "cannot update last connection timestamp for envuser %q", e.ID())
 	}
 
