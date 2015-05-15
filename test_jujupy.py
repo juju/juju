7a1986354649a995690327c7c1d308643f7c3b6c
John George <john.george@canonical.com>
2015-05-15 12:45:42 -0700

    Drive-by lint.

--- test_jujupy.py
+++ test_jujupy.py
@@ -237,9 +237,9 @@ class TestEnvJujuClient(ClientTest):
             client = EnvJujuClient(env, None, None)
             with patch.object(client.env, 'joyent', lambda: True):
                 client.bootstrap()
-            mock.assert_called_once_with(client,
-                'bootstrap', ('--constraints', 'mem=2G cpu-cores=1'), False,
-                juju_home=None)
+            mock.assert_called_once_with(
+                client, 'bootstrap', ('--constraints', 'mem=2G cpu-cores=1'),
+                False, juju_home=None)
 
     def test_bootstrap_non_sudo(self):
         env = Environment('foo', '')
