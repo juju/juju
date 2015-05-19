42f75283a942bf25aced0c84dde3e7e122d0dde9
Curtis Hovey <curtis@canonical.com>
2015-05-19 14:48:37 -0400

    Always clear the queue.

--- manage-build-revision.bash
+++ manage-build-revision.bash
@@ -17,13 +17,15 @@ CREDS="$JENKINS_USER:$JENKINS_PASSWORD"
 JOB="http://$CREDS@juju-ci.vapour.ws:8080/job/build-revision"
 
 
+# Empty the "C" queue. There can be only 1 or 0 jobs in it.
+for job in $(atq -q c | cut  -f 1); do
+    atrm $job
+done
+
 if [[ $ACTION == 'enable' ]]; then
     curl --data enable "$JOB/enable"
 else
     curl --data disable "$JOB/disable"
-    for job in $(atq -q c | cut  -f 1); do
-        atrm $job
-    done
     echo "Build-revision will be enabled in $HOURS hour"
     echo curl --data enable "$JOB/enable" | at -q c now +$HOURS hours
 fi
