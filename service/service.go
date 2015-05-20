f2e8687d54e2bd31905b5429a457fa55b3812d08
Eric Snow <ericsnowcurrently@gmail.com>
2015-05-20 12:55:55 -0600

    Clarify a comment.

--- service/service.go
+++ service/service.go
@@ -109,8 +109,10 @@ func NewService(name string, conf common.Conf, initSystem string) (Service, erro
 	case InitSystemUpstart:
 		return upstart.NewService(name, conf), nil
 	case InitSystemSystemd:
-		// TODO(ericsnow) dataDir should come from the series or be passed in.
-		// For now it just needs to be non-windows.
+		// TODO(ericsnow) lp:1457122
+		// dataDir should come from the series or be passed in.
+		// Until the proper fix is in place, we use paths.DataDir here
+		// with a non-windows series (e.g. trusty).
 		dataDir, err := paths.DataDir("trusty")
 		if err != nil {
 			return nil, errors.Trace(err)
