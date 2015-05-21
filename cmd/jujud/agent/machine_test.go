ad1089b8fa625ee3a951e9cdffe22085b4dabf43
Cheryl Jennings <cheryl.jennings@canonical.com>
2015-05-21 13:51:57 -0500

    Address review comments
    Added closure of the channel.

--- cmd/jujud/agent/machine_test.go
+++ cmd/jujud/agent/machine_test.go
@@ -1655,6 +1655,7 @@ func (s *MachineSuite) TestMachineAgentAPIWorkerErrorClosesAPI(c *gc.C) {
 	s.AgentSuite.PatchValue(&reportClosedAPI, func(st io.Closer) {
 		select {
 		case closedAPI <- st:
+			close(closedAPI)
 		default:
 		}
 	})
