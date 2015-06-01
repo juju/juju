007d87f3644d1ec3eb6aaae952b467aa799485d0
John George <john.george@canonical.com>
2015-06-01 09:55:28 -0700

    Remove run_chaos_monkey.py changes from this branch.

--- run_chaos_monkey.py
+++ run_chaos_monkey.py
@@ -29,7 +29,6 @@ class MonkeyRunner:
         self.service = service
         self.health_checker = health_checker
         self.client = client
-        self.monkey_ids = []
 
     def deploy_chaos_monkey(self):
         """Juju deploy chaos-monkey and add a relation.
@@ -44,24 +43,6 @@ class MonkeyRunner:
         logging.debug('Waiting for services to start.')
         self.client.wait_for_started()
 
-    def unleash_once(self):
-        status = self.client.get_status()
-        try:
-            units = status.get_subordinate_units(self.service)
-        except KeyError as err:
-            raise KeyError('Service not found: {}'.format(err))
-        for unit in units:
-            unit_name = unit.keys().pop()
-            logging.info('Starting the chaos monkey on: {}'.format(unit_name))
-            action_out = self.client.get_juju_output(
-                'action do', unit_name, 'start', 'mode=single')
-            if not action_out.startswith('Action queued with id'):
-                raise Exception(
-                    'Unexpected output from "juju action do": {}'.format(
-                        action_out))
-            logging.info(action_out)
-            self.monkey_ids.append(action_out.split().pop())
-
 
 def get_args(argv=None):
     parser = ArgumentParser()
@@ -75,14 +56,10 @@ def get_args(argv=None):
 
 
 def main():
-    configure_logging(logging.INFO)
+    configure_logging(logging.DEBUG)
     args = get_args()
     monkey_runner = MonkeyRunner.from_config(args)
     monkey_runner.deploy_chaos_monkey()
-    # Juju add-relation returns before a subordinate is listed in the status
-    # output. This means that additional logic is needed to wait for the
-    # subordinate before trying to unleash a monkey.
-    # monkey_runner.unleash_once()
 
 if __name__ == '__main__':
     main()
