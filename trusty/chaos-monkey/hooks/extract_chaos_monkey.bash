77af6558abe69ae98ab003008a008a9cf7a1675b
Seman Said <seman.said@canonical.com>
2015-06-15 08:26:53 -0700

    Moved the source URL to the configuration file.

--- trusty/chaos-monkey/hooks/extract_chaos_monkey.bash
+++ trusty/chaos-monkey/hooks/extract_chaos_monkey.bash
@@ -1,9 +1,9 @@
 #!/bin/bash -eux
 cd ${CHARM_DIR}
-wget https://github.com/juju/chaos-monkey/archive/master.tar.gz \
-    --output-document chaos-monkey.tar.gz
+chaos_source="$(config-get chaos-source)"
+wget ${chaos_source} --output-document chaos-monkey.tar.gz
 # Delete any existing Chaos Monkey directory.
 [[ -d chaos-monkey ]] && rm -rf chaos-monkey
-# Extract and rename chaos monkey-master to chaos-monkey
+# Extract and rename chaos-monkey-master to chaos-monkey
 [[ -f chaos-monkey.tar.gz ]] && tar -zxf chaos-monkey.tar.gz && \
     mv chaos-monkey-master chaos-monkey
