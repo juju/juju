552a76c17928e1417da3d3fe15f8765d8635f8a4
Curtis Hovey <curtis@canonical.com>
2015-06-16 13:00:07 -0400

    Added main test for assess_recovery.py ha.

--- test_assess_recovery.py
+++ test_assess_recovery.py
@@ -1,7 +1,17 @@
+from contextlib import contextmanager
+from mock import patch
 from unittest import TestCase
 
 from assess_recovery import (
+    main,
     parse_args,
+)
+from jujupy import (
+    EnvJujuClient,
+    SimpleEnvironment,
+)
+from utility import (
+    setup_test_logging,
 )
 
 
@@ -41,3 +51,70 @@ class TestParseArgs(TestCase):
         self.assertIs(args.temp_env_name, None)
         args = parse_args(['foo', 'bar', 'baz', 'qux'])
         self.assertEqual(args.temp_env_name, 'qux')
+
+
+@contextmanager
+def make_mocked_client(name, status_error=None):
+    client = EnvJujuClient(SimpleEnvironment(
+        name, {'type': 'paas'}), '1.23', 'path')
+    with patch.object(client, 'wait_for_ha', autospec=True):
+        with patch.object(client, 'get_status', autospec=True,
+                          side_effect=status_error):
+            with patch.object(client,
+                              'destroy_environment', autospec=True):
+                yield client
+
+
+@patch('assess_recovery.dump_env_logs', autospec=True)
+@patch('assess_recovery.parse_new_state_server_from_error', autospec=True,
+       return_value='new_host')
+@patch('assess_recovery.wait_for_state_server_to_shutdown', autospec=True)
+@patch('assess_recovery.delete_instance', autospec=True)
+@patch('assess_recovery.deploy_stack', autospec=True, return_value='i_id')
+@patch('assess_recovery.get_machine_dns_name', autospec=True,
+       return_value='host')
+@patch('subprocess.check_output', autospec=True)
+@patch('subprocess.check_call', autospec=True)
+@patch('sys.stdout', autospec=True)
+class TestMain(TestCase):
+
+    def setUp(self):
+        setup_test_logging(self)
+
+    def test_ha(self, so_mock, cc_mock, co_mock,
+                dns_mock, ds_mock, di_mock, ws_mock, ns_mock, dl_mock):
+        with make_mocked_client('foo') as client:
+            with patch('assess_recovery.make_client', autospec=True,
+                       return_value=client) as mc_mock:
+                main(['./', 'foo', 'log_dir',
+                      '--ha', '--charm-prefix', 'prefix'])
+                mc_mock.assert_called_once_with('./', False, 'foo', None)
+                client.wait_for_ha.assert_called_once_with()
+                client.get_status.assert_called_once_with(600)
+                client.destroy_environment.assert_called_once_with()
+        dns_mock.assert_called_once_with(client, 0)
+        ds_mock.assert_called_once_with(client, 'prefix')
+        di_mock.assert_called_once_with(client, 'i_id')
+        ws_mock.assert_called_once_with('host', client, 'i_id')
+        dl_mock.assert_called_once_with(client, None, 'log_dir')
+        self.assertEqual(0, ns_mock.call_count)
+
+    def test_ha_error(self, so_mock, cc_mock, co_mock,
+                      dns_mock, ds_mock, di_mock, ws_mock, ns_mock, dl_mock):
+        error = Exception()
+        with make_mocked_client('foo', status_error=error) as client:
+            with patch('assess_recovery.make_client', autospec=True,
+                       return_value=client) as mc_mock:
+                with self.assertRaises(SystemExit):
+                    main(['./', 'foo', 'log_dir',
+                          '--ha', '--charm-prefix', 'prefix'])
+                    mc_mock.assert_called_once_with('./', False, 'foo', None)
+                    client.wait_for_ha.assert_called_once_with()
+                    client.get_status.assert_called_once_with(600)
+                    client.destroy_environment.assert_called_once_with()
+        dns_mock.assert_called_once_with(client, 0)
+        ds_mock.assert_called_once_with(client, 'prefix')
+        di_mock.assert_called_once_with(client, 'i_id')
+        ws_mock.assert_called_once_with('host', client, 'i_id')
+        ns_mock.assert_called_once_with(error)
+        dl_mock.assert_called_once_with(client, 'new_host', 'log_dir')
