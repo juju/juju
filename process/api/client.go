3ae886aa119daf377b5261ef72f2c5f98920b952
Eric Snow <ericsnowcurrently@gmail.com>
2015-06-18 11:32:12 -0600

    Add missing doc comments.

--- process/api/client.go
+++ process/api/client.go
@@ -9,24 +9,31 @@ import (
 	"github.com/juju/juju/process"
 )
 
+// Client provides methods for interacting with the Juju API relative
+// to workload processes.
 type Client struct {
 }
 
+// NewClient builds a new workload process API client.
 func NewClient() (*Client, error) {
 	// TODO(ericsnow) finish
 	return &Client{}, errors.Errorf("not finished")
 }
 
+// List gets the list of defined workload processes from Juju
+// via the API.
 func (c *Client) List() ([]string, error) {
 	// TODO(ericsnow) finish
 	return nil, errors.Errorf("not finished")
 }
 
+// Get gets the info for the specified workload processes via the API.
 func (c *Client) Get(ids ...string) ([]*process.Info, error) {
 	// TODO(ericsnow) finish
 	return nil, errors.Errorf("not finished")
 }
 
+// Set pushes the provided process info up to Juju state via the API.
 func (c *Client) Set(procs ...*process.Info) error {
 	// TODO(ericsnow) finish
 	return errors.Errorf("not finished")
