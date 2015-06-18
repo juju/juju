d13368107456d420f9f75c56cac9b6a1ad9b51e0
Eric Snow <ericsnowcurrently@gmail.com>
2015-06-18 12:14:36 -0600

    Add a doc comment explaining the purpose of the component/all package.

--- component/all/all.go
+++ component/all/all.go
@@ -1,6 +1,13 @@
 // Copyright 2015 Canonical Ltd.
 // Licensed under the AGPLv3, see LICENCE file for details.
 
+// The all package facilitates the registration of Juju components into
+// the relevant machinery. It is intended as the one place in Juju where
+// the components (horizontal design layers) and the machinery
+// (vertical/architectural layers) intersect. This approach helps
+// alleviate interdependence between the components and the machinery.
+//
+// This is done in an independent package to avoid circular imports.
 package all
 
 import (
