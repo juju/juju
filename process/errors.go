1d659e645637f72be0998a0f8af36ff0def20429
Eric Snow <ericsnowcurrently@gmail.com>
2015-06-22 12:24:30 -0600

    Clean up validationErr.

--- process/errors.go
+++ process/errors.go
@@ -7,13 +7,26 @@ import (
 	"github.com/juju/errors"
 )
 
+type invalidChecker interface {
+	// Return true if not valid.
+	IsInvalid() bool
+}
+
 // validationErr represents an error signifying an object with an invalid value.
 type validationErr struct {
 	*errors.Err
 }
 
+// IsInvalid implements invalidChecker.
+func (err validationErr) IsInvalid() bool {
+	return true
+}
+
 // IsInvalid returns whether the given error indicates an invalid value.
 func IsInvalid(e error) bool {
-	_, ok := e.(validationErr)
-	return ok
+	inv, ok := e.(invalidChecker)
+	if !ok {
+		return false
+	}
+	return inv.IsInvalid()
 }
