3d72658726bd109122aa92eac66a9fa2a7034c9c
Eric Snow <ericsnowcurrently@gmail.com>
2015-06-25 12:55:10 -0600

    Implement tests for ProcessDefinitions.

--- state/processes_test.go
+++ state/processes_test.go
@@ -14,6 +14,7 @@ import (
 	"gopkg.in/juju/charm.v5"
 
 	"github.com/juju/juju/process"
+	"github.com/juju/juju/state"
 	"github.com/juju/juju/testing"
 )
 
@@ -63,7 +64,57 @@ type processDefinitionsSuite struct {
 	baseProcessesSuite
 }
 
-func (s *processDefinitionsSuite) TestEnsureDefined(c *gc.C) {
+func (s *processDefinitionsSuite) TestEnsureDefinedOkay(c *gc.C) {
+	definitions := s.newDefinitions("docker", "procA", "procB")
+	pd := state.ProcessDefinitions{Persist: s.persist}
+	err := pd.EnsureDefined(definitions...)
+	c.Assert(err, jc.ErrorIsNil)
+
+	s.stub.CheckCallNames(c, "EnsureDefinitions")
+	s.persist.checkDefinitions(c, definitions)
+}
+
+func (s *processDefinitionsSuite) TestEnsureDefinedNoOp(c *gc.C) {
+	pd := state.ProcessDefinitions{Persist: s.persist}
+	err := pd.EnsureDefined()
+	c.Assert(err, jc.ErrorIsNil)
+
+	s.stub.CheckCallNames(c, "EnsureDefinitions")
+	c.Check(s.persist.definitions, gc.HasLen, 0)
+}
+
+func (s *processDefinitionsSuite) TestEnsureDefinedBadDefinition(c *gc.C) {
+	definitions := s.newDefinitions("docker", "procA", "procB")
+	definitions = append(definitions, charm.Process{})
+	pd := state.ProcessDefinitions{Persist: s.persist}
+	err := pd.EnsureDefined(definitions...)
+
+	c.Check(err, jc.Satisfies, errors.IsNotValid)
+}
+
+func (s *processDefinitionsSuite) TestEnsureDefinedMatched(c *gc.C) {
+	same := charm.Process{Name: "procA", Type: "docker"}
+	s.persist.setDefinitions(&same)
+
+	definitions := s.newDefinitions("docker", "procA", "procB")
+	pd := state.ProcessDefinitions{Persist: s.persist}
+	err := pd.EnsureDefined(definitions...)
+	c.Assert(err, jc.ErrorIsNil)
+
+	s.persist.checkDefinitions(c, definitions)
+}
+
+func (s *processDefinitionsSuite) TestEnsureDefinedMismatched(c *gc.C) {
+	same := charm.Process{Name: "procA", Type: "docker"}
+	different := charm.Process{Name: "procB", Type: "kvm"}
+	s.persist.setDefinitions(&same, &different)
+
+	definitions := s.newDefinitions("docker", "procA", "procB", "procC")
+	definitions = append(definitions, same)
+	pd := state.ProcessDefinitions{Persist: s.persist}
+	err := pd.EnsureDefined(definitions...)
+
+	c.Check(err, jc.Satisfies, errors.IsNotValid)
 }
 
 type unitProcessesSuite struct {
