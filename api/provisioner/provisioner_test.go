1cfa6ff644d34540e5cd51cdfe2647d167192614
Michael Foord <michael.foord@canonical.com>
2015-06-26 12:25:06 +0100

    Yet another test fix

--- api/provisioner/provisioner_test.go
+++ api/provisioner/provisioner_test.go
@@ -833,9 +833,12 @@ func (s *provisionerSuite) TestPrepareContainerInterfaceInfo(c *gc.C) {
 	container, err := s.State.AddMachineInsideMachine(template, s.machine.Id(), instance.LXC)
 	c.Assert(err, jc.ErrorIsNil)
 
+	ifaceInfo, err := s.provisioner.PrepareContainerInterfaceInfo(container.MachineTag())
+	c.Assert(err, jc.ErrorIsNil)
+	c.Assert(ifaceInfo, gc.HasLen, 1)
+
 	expectInfo := []network.InterfaceInfo{{
 		DeviceIndex:      0,
-		MACAddress:       "aa:bb:cc:dd:ee:f0",
 		CIDR:             "0.10.0.0/24",
 		NetworkName:      "juju-private",
 		ProviderId:       "dummy-eth0",
@@ -852,11 +855,10 @@ func (s *provisionerSuite) TestPrepareContainerInterfaceInfo(c *gc.C) {
 		GatewayAddress: network.NewAddress("0.10.0.2"),
 		ExtraConfig:    nil,
 	}}
-	ifaceInfo, err := s.provisioner.PrepareContainerInterfaceInfo(container.MachineTag())
-	c.Assert(err, jc.ErrorIsNil)
-	c.Assert(ifaceInfo, gc.HasLen, 1)
 	c.Assert(ifaceInfo[0].Address, gc.Not(gc.DeepEquals), network.Address{})
+	c.Assert(ifaceInfo[0].MACAddress, gc.Not(gc.DeepEquals), "")
 	expectInfo[0].Address = ifaceInfo[0].Address
+	expectInfo[0].MACAddress = ifaceInfo[0].MACAddress
 	c.Assert(ifaceInfo, jc.DeepEquals, expectInfo)
 }
 
