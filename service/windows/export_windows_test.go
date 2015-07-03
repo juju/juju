9d0450689631af0c39fb45fa7fe59efda2175c36
bogdanteleaga <bteleaga@cloudbasesolutions.com>
2015-07-03 17:06:37 +0200

    Ensure service handles are closed

--- service/windows/export_windows_test.go
+++ service/windows/export_windows_test.go
@@ -16,7 +16,7 @@ var (
 
 func PatchMgrConnect(patcher patcher, stub *testing.Stub) *StubMgr {
 	conn := &StubMgr{Stub: stub}
-	patcher.PatchValue(&newConn, func() (mgrInterface, error) { return conn, nil })
+	patcher.PatchValue(&newConn, func() (windowsManager, error) { return conn, nil })
 	return conn
 }
 
