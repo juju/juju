f699a6ecf9e02a7acf33c330bdbe711b6611155d
Dimiter Naydenov <dimiter.naydenov@canonical.com>
2015-07-03 11:43:06 +0300

    Updated the help text for subnet and subnet add commands

--- cmd/juju/subnet/add.go
+++ cmd/juju/subnet/add.go
@@ -29,10 +29,10 @@ is supported on a wider variety of clouds (where SDN features are not
 available, e.g. MAAS). The subnet will be associated with the given
 existing Juju network space.
 
-Subnets can be referenced by either their CIDR or provider-specific ID.
-If CIDR is used an multiple subnets have the same CIDR, an error will
-be returned, including the list of possible provider IDs uniquely
-identifying each subnet.
+Subnets can be referenced by either their CIDR or ProviderId (if the
+provider supports it). If CIDR is used an multiple subnets have the
+same CIDR, an error will be returned, including the list of possible
+provider IDs uniquely identifying each subnet.
 
 Any availablility zones associated with the added subnet are automatically
 discovered using the cloud API (if supported). If this is not possible,
