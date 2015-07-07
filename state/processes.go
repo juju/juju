b54f1ecbdfdb5a789214cd28d82646ab0b869cf1
Eric Snow <ericsnowcurrently@gmail.com>
2015-07-07 10:04:06 -0600

    Expand some doc comments.

--- state/processes.go
+++ state/processes.go
@@ -16,15 +16,24 @@ import (
 // UnitProcesses exposes high-level interaction with workload processes
 // for a unit.
 type UnitProcesses interface {
-	// Register registers a workload process in state.
+	// Register registers a workload process in state. If a process is
+	// already registered for the same (unit, proc name, plugin ID)
+	// then the request will fail. The unit must also be "alive".
 	Register(info process.Info) error
-	// SetStatus sets the raw status of a workload process.
+	// SetStatus sets the raw status of a workload process. The process
+	// ID is in the format provided by process.Info.ID()
+	// ("<proc name>/<plugin ID>"). If the process is not in state then
+	// the request will fail.
 	SetStatus(id string, status process.Status) error
 	// List builds the list of workload processes registered for
 	// the given unit and IDs. If no IDs are provided then all
-	// registered processes for the unit are returned.
+	// registered processes for the unit are returned. In the case that
+	// IDs are provided, any that are not in state are ignored and only
+	// the found ones are returned. It is up to the caller to
+	// extrapolate the list of missing IDs.
 	List(ids ...string) ([]process.Info, error)
-	// Unregister marks the identified process as unregistered.
+	// Unregister marks the identified process as unregistered. If the
+	// given ID is not in state then the request will fail.
 	Unregister(id string) error
 }
 
