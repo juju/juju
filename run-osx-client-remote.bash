4237912a90bb85e0d4920934c06e8c8ebce6d180
Aaron Bentley <aaron.bentley@canonical.com>
2015-07-09 15:38:56 -0400

    Switch to deploy_job, reduce number of ENV vars used.

--- run-osx-client-remote.bash
+++ run-osx-client-remote.bash
@@ -1,21 +1,17 @@
 #!/bin/bash
 set -eu
-RELEASE_SCRIPTS=$HOME/juju-release-tools
-SCRIPTS=$HOME/juju-ci-tools
+export PATH=$HOME/juju-ci-tools:$PATH
 WORKSPACE=$(pwd)
 JUJU_HOME=$HOME/.juju
 source $HOME/.bashrc
 source $HOME/cloud-city/juju-qa.jujuci
 set -x
 
-$SCRIPTS/jujuci.py setup-workspace --clean-env testing-osx-client $WORKSPACE
+jujuci.py setup-workspace --clean-env testing-osx-client $WORKSPACE
 ~/Bin/juju destroy-environment --force -y testing-osx-client || true
-TARFILE=$($SCRIPTS/jujuci.py get build-osx-client 'juju-*-osx.tar.gz' ./)
+TARFILE=$(jujuci.py get build-osx-client 'juju-*-osx.tar.gz' ./)
 echo "Downloaded $TARFILE"
 tar -xf ./$TARFILE -C $WORKSPACE
 
-export PATH=$WORKSPACE/juju-bin:$PATH
-$SCRIPTS/deploy_stack.py testing-osx-client
-EXIT_STATUS=$?
-juju destroy-environment -y testing-osx-client || true
-exit $EXIT_STATUS
+deploy_job.py testing-osx-client artifacts testing-osx-client \
+  --new-juju-bin $WORKSPACE/juju-bin
