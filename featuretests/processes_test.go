9808df93952610c3b69acb1d86e668e02f878bb1
Eric Snow <ericsnowcurrently@gmail.com>
2015-07-10 20:50:54 -0600

    Add a testing helper.

--- featuretests/processes_test.go
+++ featuretests/processes_test.go
@@ -4,10 +4,13 @@
 package featuretests
 
 import (
+	"github.com/juju/names"
+	jc "github.com/juju/testing/checkers"
 	gc "gopkg.in/check.v1"
 
 	"github.com/juju/juju/component/all"
 	jujutesting "github.com/juju/juju/juju/testing"
+	"github.com/juju/juju/state"
 )
 
 func initProcessesSuites() {
@@ -21,14 +24,31 @@ func initProcessesSuites() {
 }
 
 type processesBaseSuite struct {
-	jujutesting.RepoSuite
+	jujutesting.JujuConnSuite
+}
+
+func (s *processesBaseSuite) addUnit(c *gc.C, charmName, serviceName string) (names.CharmTag, *state.Unit) {
+	ch := s.AddTestingCharm(c, charmName)
+
+	svc := s.AddTestingService(c, serviceName, ch)
+	unit, err := svc.AddUnit()
+	c.Assert(err, jc.ErrorIsNil)
+	err = s.State.AssignUnit(unit, state.AssignCleanEmpty)
+	c.Assert(err, jc.ErrorIsNil)
+
+	charmTag := ch.Tag().(names.CharmTag)
+	return charmTag, unit
 }
 
 type processesHookContextSuite struct {
 	processesBaseSuite
 }
 
+// TODO(ericsnow) Add a test specifically for each supported plugin
+// (e.g. docker)?
+
 func (s *processesHookContextSuite) TestRegister(c *gc.C) {
+	//s.addUnit(c, "proc-hooks", "a-service")
 	// TODO(ericsnow) Finish!
 	c.Skip("not finished")
 }
