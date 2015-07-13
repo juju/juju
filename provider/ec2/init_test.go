a254d152b3fc7c6cc162d5dd25a97f92037c7f6d
anastasia <anastasia.macmood@canonical.com>
2015-07-13 11:45:10 +1000

    Fixed ec2 test that validates that cloud local storage was registered as image metadata data source.

--- provider/ec2/init_test.go
+++ provider/ec2/init_test.go
@@ -7,8 +7,6 @@ import (
 	jc "github.com/juju/testing/checkers"
 	gc "gopkg.in/check.v1"
 
-	"github.com/juju/juju/environs"
-	"github.com/juju/juju/environs/config"
 	"github.com/juju/juju/provider/ec2"
 	"github.com/juju/juju/storage"
 	"github.com/juju/juju/storage/provider/registry"
@@ -34,41 +32,4 @@ func (*providerSuite) TestSupportedProviders(c *gc.C) {
 		ok := registry.IsProviderSupported("ec2", providerType)
 		c.Assert(ok, jc.IsTrue)
 	}
-}
-
-type initSuite struct {
-	testing.BaseSuite
-}
-
-var _ = gc.Suite(&initSuite{})
-
-func makeEnviron(c *gc.C) environs.Environ {
-	attrs := testing.FakeConfig().Merge(testing.Attrs{
-		"type":           "ec2",
-		"control-bucket": "x",
-		"access-key":     "ko",
-		"secret-key":     "ko",
-	})
-	cfg, err := config.New(config.NoDefaults, attrs)
-	c.Assert(err, jc.ErrorIsNil)
-	e, err := environs.New(cfg)
-	c.Assert(err, jc.ErrorIsNil)
-	return e
-}
-
-func (s *initSuite) TestImageMetadataDatasourceAdded(c *gc.C) {
-	env := makeEnviron(c)
-	dss, err := environs.ImageMetadataSources(env)
-	c.Assert(err, jc.ErrorIsNil)
-
-	expected := "cloud local storage"
-	found := false
-	for i, ds := range dss {
-		c.Logf("datasource %d: %+v", i, ds)
-		if ds.Description() == expected {
-			found = true
-			break
-		}
-	}
-	c.Assert(found, jc.IsTrue)
 }
