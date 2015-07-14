9894d83efd352e716b9b7f6834a3fa7244ce3ea5
William Reade <fwereade@gmail.com>
2015-07-14 21:11:33 +0200

    replace leadership.LeadershipManager with .Claimer and .Checker interfaces; building again, no tests addressed

--- state/leadership/token.go
+++ state/leadership/token.go
@@ -4,15 +4,21 @@
 package leadership
 
 import (
+	"github.com/juju/errors"
 	"gopkg.in/mgo.v2/txn"
 )
 
-// token implements Token.
+// token implements leadership.Token.
 type token struct {
 	op txn.Op
 }
 
-// AssertOps is part of the Token interface.
-func (t token) AssertOps() []txn.Op {
-	return []txn.Op{t.op}
+// Read is part of the leadership.Token interface.
+func (t token) Read(out interface{}) error {
+	outPtr, ok := out.(*[]txn.Op)
+	if !ok {
+		return errors.New("expected pointer to []txn.Op")
+	}
+	*outPtr = []txn.Op{t.op}
+	return nil
 }
