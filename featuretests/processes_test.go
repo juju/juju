ff0cc7332972a1008d473a9f63161b0bd89c8dda
Eric Snow <ericsnowcurrently@gmail.com>
2015-07-14 13:40:52 -0600

    Use the new testing helpers.

--- featuretests/processes_test.go
+++ featuretests/processes_test.go
@@ -4,7 +4,8 @@
 package featuretests
 
 import (
-	"github.com/juju/names"
+	"strings"
+
 	jc "github.com/juju/testing/checkers"
 	gc "gopkg.in/check.v1"
 	"gopkg.in/juju/charm.v5"
@@ -12,7 +13,7 @@ import (
 	"github.com/juju/juju/component/all"
 	jujutesting "github.com/juju/juju/juju/testing"
 	"github.com/juju/juju/process"
-	"github.com/juju/juju/state"
+	coretesting "github.com/juju/juju/testing"
 )
 
 func initProcessesSuites() {
@@ -27,19 +28,21 @@ func initProcessesSuites() {
 
 type processesBaseSuite struct {
 	jujutesting.JujuConnSuite
+
+	env  *procsEnviron
+	unit *procsUnit
 }
 
-func (s *processesBaseSuite) addUnit(c *gc.C, charmName, serviceName string) (names.CharmTag, *state.Unit) {
-	ch := s.AddTestingCharm(c, charmName)
+func (s *processesBaseSuite) SetUpSuite(c *gc.C) {
+	s.JujuConnSuite.SetUpSuite(c)
 
-	svc := s.AddTestingService(c, serviceName, ch)
-	unit, err := svc.AddUnit()
-	c.Assert(err, jc.ErrorIsNil)
-	err = s.State.AssignUnit(unit, state.AssignCleanEmpty)
-	c.Assert(err, jc.ErrorIsNil)
+	s.env = newProcsEnv(c, s, "") // TODO(ericsnow) switch to "local"...
+}
 
-	charmTag := ch.Tag().(names.CharmTag)
-	return charmTag, unit
+func (s *processesBaseSuite) TearDownSuite(c *gc.C) {
+	s.env.destroy(c)
+
+	s.JujuConnSuite.TearDownSuite(c)
 }
 
 type processesHookContextSuite struct {
@@ -50,25 +53,14 @@ func (s *processesHookContextSuite) TestHookLifecycle(c *gc.C) {
 	// start: info, launch, info
 	// config-changed: info, set-status, info
 	// stop: info, destroy, info
-	// TODO(ericsnow) At the moment, only launch happens...
 
-	unitTag := names.NewUnitTag("a-service/0")
-	s.checkState(c, unitTag, nil)
-	s.checkPluginLog(c, []string{
-		"...",
-	})
-
-	s.prepPlugin(c, "myproc", "xyz123", "running")
-	s.checkPluginLog(c, []string{
-		"...",
-	})
+	svc := s.env.addService(c, "proc-hooks", "a-service")
 
 	// Add/start the unit.
 
-	_, unit := s.addUnit(c, "proc-hooks", "a-service")
-	c.Assert(unit.UnitTag(), gc.Equals, unitTag)
+	unit := svc.deploy(c, "myproc", "xyz123", "running")
 
-	s.checkState(c, unitTag, []process.Info{{
+	unit.checkState(c, []process.Info{{
 		Process: charm.Process{
 			Name: "myproc",
 			Type: "myplugin",
@@ -80,14 +72,15 @@ func (s *processesHookContextSuite) TestHookLifecycle(c *gc.C) {
 			},
 		},
 	}})
-	s.checkPluginLog(c, []string{
+	unit.checkPluginLog(c, []string{
 		"...",
 	})
 
 	// Change the config.
-	s.setConfig(c, unitTag, "status", "okay")
 
-	s.checkState(c, unitTag, []process.Info{{
+	unit.setStatus(c, "okay")
+
+	unit.checkState(c, []process.Info{{
 		Process: charm.Process{
 			Name: "myproc",
 			Type: "myplugin",
@@ -99,15 +92,16 @@ func (s *processesHookContextSuite) TestHookLifecycle(c *gc.C) {
 			},
 		},
 	}})
-	s.checkPluginLog(c, []string{
+	unit.checkPluginLog(c, []string{
 		"...",
 	})
 
 	// Stop the unit.
-	s.destroyUnit(c, unitTag)
 
-	s.checkState(c, unitTag, nil)
-	s.checkPluginLog(c, []string{
+	unit.destroy(c)
+
+	unit.checkState(c, nil)
+	unit.checkPluginLog(c, []string{
 		"...",
 	})
 }
@@ -162,4 +156,97 @@ type processesCmdJujuSuite struct {
 func (s *processesCmdJujuSuite) TestStatus(c *gc.C) {
 	// TODO(ericsnow) Finish!
 	c.Skip("not finished")
+}
+
+type procsEnviron struct {
+	base coretesting.Environ
+}
+
+func newProcsEnv(c *gc.C, s *processesBaseSuite, envName string) *procsEnviron {
+	base := jujutesting.NewEnv(&s.JujuConnSuite)
+	if envName != "" {
+		base = coretesting.NewEnv(c, envName)
+	}
+	return &procsEnviron{base: base}
+}
+
+func (env *procsEnviron) addService(c *gc.C, charmName, serviceName string) *procsService {
+	svc := env.base.AddService(c, charmName, serviceName)
+	return &procsService{base: svc}
+}
+
+func (env *procsEnviron) destroy(c *gc.C) {
+	env.base.Destroy(c)
+}
+
+type procsService struct {
+	base coretesting.Service
+}
+
+func (svc *procsService) setConfig(c *gc.C, settings map[string]string) {
+	svc.base.SetConfig(c, settings)
+}
+
+func (svc *procsService) deploy(c *gc.C, procName, pluginID, status string) *procsUnit {
+	settings := map[string]string{
+		"plugin-name":   procName,
+		"plugin-id":     pluginID,
+		"plugin-status": status,
+	}
+	svc.setConfig(c, settings)
+
+	unit := svc.base.Deploy(c)
+	return &procsUnit{base: unit, svc: svc}
+}
+
+type procsUnit struct {
+	base coretesting.Unit
+	svc  *procsService
+}
+
+func (u *procsUnit) setConfig(c *gc.C, settings map[string]string) {
+	u.base.SetConfig(c, settings)
+}
+
+func (u *procsUnit) runAction(c *gc.C, name string, args map[string]interface{}) map[string]string {
+	rawResults := u.base.RunAction(c, name, args)
+	delete(rawResults, "status")
+	results := make(map[string]string, len(rawResults))
+	for k, v := range rawResults {
+		results[k] = v.(string)
+	}
+	return results
+}
+
+func (u *procsUnit) destroy(c *gc.C) {
+	u.base.Destroy(c)
+}
+
+func (u *procsUnit) setStatus(c *gc.C, status string) {
+	settings := map[string]string{"plugin-status": status}
+	u.setConfig(c, settings)
+}
+
+func (u *procsUnit) prepPlugin(c *gc.C, procName, pluginID, status string) {
+	args := map[string]interface{}{
+		"name":   procName,
+		"id":     pluginID,
+		"status": status,
+	}
+	u.runAction(c, "plugin-prep", args)
+}
+
+func (u *procsUnit) checkState(c *gc.C, expected []process.Info) {
+	procs := u.base.Procs(c)
+	c.Check(procs, jc.DeepEquals, expected)
+}
+
+func (u *procsUnit) checkPluginLog(c *gc.C, expected []string) {
+	results := u.runAction(c, "plugin-dump", nil)
+	c.Assert(results, gc.HasLen, 1)
+	output, ok := results["output"]
+	c.Assert(ok, jc.IsTrue)
+	lines := strings.Split(output, "\n")
+
+	c.Check(lines, jc.DeepEquals, expected)
 }
