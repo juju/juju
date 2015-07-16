c3c1b9c6a1d13f38cd50762b451ca19eb0a05ff2
Aaron Bentley <aaron.bentley@canonical.com>
2015-07-16 15:23:22 -0400

    Update for more artifact support.

--- run_deploy_job_wr.py
+++ run_deploy_job_wr.py
@@ -7,16 +7,29 @@ from tempfile import NamedTemporaryFile
 
 
 def main():
+    revision_build = os.environ['revision_build']
+    job_name = os.environ['JOB_NAME']
+    build_number = os.environ['BUILD_NUMBER']
+    prefix='juju-ci/products/version-{}/{}/build-{}'.format(
+        revision_build, job_name, build_number)
+    s3_config = join(os.environ['HOME'], 'cloud-city/juju-qa.s3cfg')
     command = [
         '$HOME/juju-ci-tools/run-deploy-job-remote.bash',
-        os.environ['revision_build'],
-        os.environ['JOB_NAME'],
+        revision_build,
+        job_name,
         ]
     command.extend(sys.argv[2:])
     with NamedTemporaryFile() as config_file:
-        json.dump({'command': command, 'install': {}}, config_file)
+        json.dump({
+            'command': command, 'install': {},
+            'artifacts': {'artifacts': ['*']},
+            'bucket': 'juju-qa-data',
+            }, config_file)
         config_file.flush()
-        subprocess.check_call(['workspace-run', config_file.name, sys.argv[1]])
+        subprocess.check_call([
+            'workspace-run', config_file.name, sys.argv[1], prefix,
+            '--s3-config', s3_config,
+            ])
 
 if __name__ == '__main__':
     main()
