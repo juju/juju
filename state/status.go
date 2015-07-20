be8556dbf25ae2386d123e72c82f60425f1fa4ed
Menno Smits <menno.smits@canonical.com>
2015-07-20 17:14:31 +1200

    state: undo band-aid fix for LP #1474606
    multiEnvRunner now does the right thing.

--- state/status.go
+++ state/status.go
@@ -552,20 +552,11 @@ func createStatusOp(st *State, globalKey string, doc statusDoc) txn.Op {
 // updateStatusOp returns the operations needed to update the given
 // status document associated with the given globalKey.
 func updateStatusOp(st *State, globalKey string, doc statusDoc) txn.Op {
-	// never $set the actual statusDoc received as it might wipe envuuid field
-	// that is set automatically upon insertion.
-	// See #1474606.
-	updateFields := bson.D{
-		{"status", doc.Status},
-		{"statusinfo", doc.StatusInfo},
-		{"statusdata", doc.StatusData},
-		{"updated", doc.Updated},
-	}
 	return txn.Op{
 		C:      statusesC,
 		Id:     st.docID(globalKey),
 		Assert: txn.DocExists,
-		Update: bson.D{{"$set", updateFields}},
+		Update: bson.D{{"$set", &doc}},
 	}
 }
 
