8c879b0327cb31a49e0c79157c2dd90b827440d6
Aaron Bentley <aaron.bentley@canonical.com>
2015-07-20 16:06:34 -0400

    Cleanup.

--- jenkins_build_package.py
+++ jenkins_build_package.py
@@ -2,26 +2,29 @@
 from glob import glob
 import json
 import os
-from os.path import join
+from os.path import (
+    dirname,
+    join,
+    )
 import sys
 import subprocess
 from tempfile import NamedTemporaryFile
 
 
 def main():
-    os.environ['PATH'] = '{}:{}'.format(
-        os.environ['SCRIPTS'], os.environ['PATH'])
+    scripts = dirname(__file__)
+    os.environ['PATH'] = '{}:{}'.format(scripts, os.environ['PATH'])
     revision_build = os.environ['revision_build']
     job_name = os.environ['JOB_NAME']
     build_number = os.environ['BUILD_NUMBER']
     workspace = os.environ['WORKSPACE']
+    s3_config = join(os.environ['JUJU_HOME'], 'juju-qa.s3cfg')
     host = sys.argv[1]
-    s3_config = join(os.environ['HOME'], 'cloud-city/juju-qa.s3cfg')
 
     subprocess.check_call(['jujuci.py', '-v', 'setup-workspace', workspace])
 
-    arch = subprocess.check_output(['ssh', host, 'dpkg',
-                                    '--print-architecture']).rstrip()
+    arch = subprocess.check_output(
+        ['ssh', host, 'dpkg', '--print-architecture']).rstrip()
     series = subprocess.check_output(
         ['ssh', host, 'lsb_release', '-sc']).rstrip()
     release = subprocess.check_output(
@@ -38,11 +41,12 @@ def main():
          '*orig.tar.gz', workspace])
     packages.extend(glob('*.orig.tar.gz'))
     command = [
-        'mv', 'packages/*', '.', ';', 'pwd', ';', 'ls', '-l', ';'
+        'mv', 'packages/*', '.', ';',
         '/home/ubuntu/juju-release-tools/build_package.py', '-v', 'binary',
         dsc_file, '$(pwd)', series, arch]
     prefix = 'juju-ci/products/version-{}/{}/build-{}'.format(
         revision_build, job_name, build_number)
+    private_key = join(os.environ['JUJU_HOME'], 'staging-juju-rsa')
     with NamedTemporaryFile() as config_file:
         json.dump({
             'command': command,
@@ -52,13 +56,8 @@ def main():
             }, config_file)
         config_file.flush()
         subprocess.check_call([
-            join(os.environ['HOME'], 'workspace-runner', 'workspace-run'),
-            '-v',
-            config_file.name,
-            host,
-            prefix,
-            '--s3-config', s3_config,
-            '-i', join(os.environ['JUJU_HOME'], 'staging-juju-rsa'),
+            'workspace-run', '-v', config_file.name, host, prefix,
+            '--s3-config', s3_config, '-i', private_key,
             ])
 
 
