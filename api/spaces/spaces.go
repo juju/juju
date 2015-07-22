7fdf0933ef59a7c1357448292738f6d624efeae2
James Tunnicliffe <james.tunnicliffe@nanosheep.org>
2015-07-22 14:55:02 +0100

    CreateSpace added.

--- api/spaces/spaces.go
+++ api/spaces/spaces.go
@@ -5,8 +5,10 @@ package spaces
 
 import (
 	"github.com/juju/loggo"
+	"github.com/juju/names"
 
 	"github.com/juju/juju/api/base"
+	"github.com/juju/juju/apiserver/params"
 )
 
 var logger = loggo.GetLogger("juju.api.spaces")
@@ -27,4 +29,30 @@ func NewAPI(caller base.APICaller) *API {
 	return &API{
 		facade: facadeCaller,
 	}
+}
+
+func makeAddSpeceParams(name string, subnetIds []string, public bool) params.AddSpaceParams {
+	spaceTag := names.NewSpaceTag(name).String()
+	subnetTags := []string{}
+
+	for _, s := range subnetIds {
+		subnetTags = append(subnetTags, names.NewSubnetTag(s).String())
+	}
+
+	return params.AddSpaceParams{
+		SpaceTag:   spaceTag,
+		SubnetTags: subnetTags,
+		Public:     public,
+	}
+}
+
+// CreateSpace creates a new Juju network space, associating the
+// specified subnets with it (optional; can be empty).
+func (api *API) CreateSpace(name string, subnetIds []string, public bool) (params.ErrorResults, error) {
+	var response params.ErrorResults
+	params := params.AddSpacesParams{
+		Spaces: []params.AddSpaceParams{makeAddSpeceParams(name, subnetIds, public)},
+	}
+	err := api.facade.FacadeCall("CreateSpaces", params, &response)
+	return response, err
 }
--- api/spaces/spaces_test.go
+++ api/spaces/spaces_test.go
@@ -4,18 +4,52 @@
 package spaces_test
 
 import (
+	"errors"
+	"fmt"
+
 	gc "gopkg.in/check.v1"
 
+	"github.com/juju/juju/api/base"
 	apitesting "github.com/juju/juju/api/base/testing"
 	"github.com/juju/juju/api/spaces"
+	"github.com/juju/juju/apiserver/params"
 	coretesting "github.com/juju/juju/testing"
+	"github.com/juju/names"
 )
 
 type SpacesSuite struct {
 	coretesting.BaseSuite
+
+	called    int
+	apiCaller base.APICaller
+	api       *spaces.API
 }
 
 var _ = gc.Suite(&SpacesSuite{})
+
+func (s *SpacesSuite) SetUpSuite(c *gc.C) {
+	s.BaseSuite.SetUpSuite(c)
+}
+
+func (s *SpacesSuite) TearDownSuite(c *gc.C) {
+	s.BaseSuite.TearDownSuite(c)
+}
+
+func (s *SpacesSuite) SetUpTest(c *gc.C) {
+	s.BaseSuite.SetUpTest(c)
+}
+
+func (s *SpacesSuite) TearDownTest(c *gc.C) {
+	s.BaseSuite.TearDownTest(c)
+}
+
+func (s *SpacesSuite) init(c *gc.C, args *apitesting.CheckArgs, err error) {
+	s.called = 0
+	s.apiCaller = apitesting.CheckingAPICaller(c, args, &s.called, err)
+	s.api = spaces.NewAPI(s.apiCaller)
+	c.Check(s.api, gc.NotNil)
+	c.Check(s.called, gc.Equals, 0)
+}
 
 func (s *SpacesSuite) TestNewAPISuccess(c *gc.C) {
 	var called int
@@ -28,4 +62,59 @@ func (s *SpacesSuite) TestNewAPISuccess(c *gc.C) {
 func (s *SpacesSuite) TestNewAPIWithNilCaller(c *gc.C) {
 	panicFunc := func() { spaces.NewAPI(nil) }
 	c.Assert(panicFunc, gc.PanicMatches, "caller is nil")
+}
+
+func makeArgs(name string, subnets []string) (string, []string, apitesting.CheckArgs) {
+	spaceTag := names.NewSpaceTag(name).String()
+	subnetTags := []string{}
+
+	for _, s := range subnets {
+		subnetTags = append(subnetTags, names.NewSubnetTag(s).String())
+	}
+
+	expectArgs := params.AddSpacesParams{
+		Spaces: []params.AddSpaceParams{
+			params.AddSpaceParams{
+				SpaceTag:   spaceTag,
+				SubnetTags: subnetTags,
+			}}}
+
+	expectResults := params.ErrorResults{}
+
+	args := apitesting.CheckArgs{
+		Facade:  "Spaces",
+		Method:  "CreateSpaces",
+		Args:    expectArgs,
+		Results: expectResults,
+	}
+
+	return name, subnets, args
+}
+
+func (s *SpacesSuite) testCreateSpace(c *gc.C, name string, subnets []string) {
+	_, _, args := makeArgs(name, subnets)
+	s.init(c, &args, nil)
+	results, err := s.api.CreateSpace(name, subnets, false)
+	c.Assert(s.called, gc.Equals, 1)
+	c.Assert(err, gc.IsNil)
+	c.Assert(results, gc.DeepEquals, args.Results)
+}
+
+func (s *SpacesSuite) TestCreateSpace(c *gc.C) {
+	name := "foo"
+	subnets := []string{}
+	for count := 0; count < 300; count += 11 {
+		newSubnet := fmt.Sprintf("1.%d.%d.0/24", uint8(count>>8), uint8(count))
+		subnets = append(subnets, newSubnet)
+		s.testCreateSpace(c, name, subnets)
+	}
+}
+
+func (s *SpacesSuite) TestCreateSpaceFails(c *gc.C) {
+	name, subnets, args := makeArgs("foo", []string{"1.1.1.0/24"})
+	s.init(c, &args, errors.New("bang"))
+	results, err := s.api.CreateSpace(name, subnets, false)
+	c.Check(s.called, gc.Equals, 1)
+	c.Assert(err, gc.ErrorMatches, "bang")
+	c.Assert(results, gc.DeepEquals, args.Results)
 }
