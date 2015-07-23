b42b493064fc81f3cc94d8b4d77b5e1f1017abe5
bogdanteleaga <bteleaga@cloudbasesolutions.com>
2015-07-23 18:59:43 +0200

    provider/azure: Implement windows compatibility

--- provider/azure/userdata.go
+++ provider/azure/userdata.go
@@ -12,6 +12,17 @@ import (
 	"github.com/juju/juju/version"
 )
 
+const (
+	// The userdata on windows will arrive as CustomData.bin
+	// We need to execute that as a powershell script and then remove it
+	bootstrapUserdataScript = `#ps1_sysnative
+mv C:\AzureData\CustomData.bin C:\AzureData\CustomData.ps1
+& C:\AzureData\CustomData.ps1
+rm C:\AzureData\CustomData.ps1
+`
+	bootstrapUserdataScriptFilename = "juju-userdata.ps1"
+)
+
 type AzureRenderer struct{}
 
 func (AzureRenderer) EncodeUserdata(udata []byte, vers version.OSType) ([]byte, error) {
@@ -19,7 +30,7 @@ func (AzureRenderer) EncodeUserdata(udata []byte, vers version.OSType) ([]byte, 
 	case version.Ubuntu, version.CentOS:
 		return renderers.ToBase64(utils.Gzip(udata)), nil
 	case version.Windows:
-		return renderers.WinEmbedInScript(udata), nil
+		return renderers.ToBase64(renderers.WinEmbedInScript(udata)), nil
 	default:
 		return nil, errors.Errorf("Cannot encode userdata for OS: %s", vers)
 	}
