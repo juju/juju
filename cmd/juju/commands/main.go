397bb7449dfb0fd38fc0566c27b6ed072dcfd7c3
Cheryl Jennings <cheryl.jennings@canonical.com>
2015-07-24 10:55:22 -0500

    Add juju system command top level aliases
    To match the spec for the JES CLI work, a
    couple top level aliases need to be
    added for some systems command.

--- cmd/juju/commands/main.go
+++ cmd/juju/commands/main.go
@@ -214,6 +214,9 @@ func registerCommands(r commandRegistry, ctx *cmd.Context) {
 		r.Register(system.NewSuperCommand())
 		r.RegisterSuperAlias("systems", "system", "list", nil)
 		r.RegisterSuperAlias("environments", "system", "environments", nil)
+		r.RegisterSuperAlias("login", "system", "login", nil)
+		r.RegisterSuperAlias("create-environment", "system", "create-environment", nil)
+		r.RegisterSuperAlias("create-env", "system", "create-env", nil)
 	}
 }
 
