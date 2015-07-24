1931de2d0a70d9ecc01837d4908bb4b04c045877
Frank Mueller <mue@tideland.biz>
2015-07-24 21:55:36 +0200

    Modified worker for API usage; not yet compiling nor tested.

--- worker/addresser/worker.go
+++ worker/addresser/worker.go
@@ -15,21 +15,13 @@ import (
 	"github.com/juju/juju/environs/config"
 	"github.com/juju/juju/instance"
 	"github.com/juju/juju/network"
-	"github.com/juju/juju/provider/common"
 	"github.com/juju/juju/worker"
 )
 
 var logger = loggo.GetLogger("juju.worker.addresser")
 
-type releaser interface {
-	// ReleaseAddress has the same signature as the same method in the
-	// environs.Networking interface.
-	ReleaseAddress(instance.Id, network.Id, network.Address, string) error
-}
-
 type addresserHandler struct {
-	api      *apiaddresser.API
-	releaser releaser
+	api *apiaddresser.API
 }
 
 // NewWorker returns a worker that keeps track of
@@ -43,22 +35,11 @@ func NewWorker(api *apiaddresser.API) (worker.Worker, error) {
 	if err != nil {
 		return nil, errors.Trace(err)
 	}
-	// If netEnviron is nil the worker will start but won't do anything as
-	// no IP addresses will be created or destroyed.
-	netEnviron, _ := environs.SupportsNetworking(environ)
-	aw := newWorkerWithReleaser(api, netEnviron)
-	return aw, nil
-}
-
-// newWorkerWithReleaser creates a worker with the addresser handler.
-// It's an own helper function to be exported for tests.
-func newWorkerWithReleaser(api *apiaddresser.API, releaser releaser) worker.Worker {
-	a := &addresserHandler{
+	ah := &addresserHandler{
 		api:      api,
-		releaser: releaser,
 	}
-	w := worker.NewStringsWorker(a)
-	return w
+	w := worker.NewStringsWorker(ah)
+	return aw, nil
 }
 
 // Handle is part of the StringsWorker interface.
@@ -82,46 +63,30 @@ func (a *addresserHandler) Handle(watcherTags []string) error {
 			return errors.Annotate(err, "cannot retrieve IP addresses")
 		}
 	}
-	toBeRemoved := []*apiaddresser.IPAddress
+	toBeReleased := []*apiaddresser.IPAddress
 	for i, ipAddress := range ipAddresses {
 		tag := tags[i]
 		if ipAddress == nil {
-			logger.Debugf("IP address %v was removed; skipping", tag)
+			logger.Debugf("IP address %v already has been removed; skipping", tag)
 			continue
 		}
 		if ipAddress.Life() != state.Dead {
-			logger.Debugf("IP address %v is not Dead (life %q); skipping", tag, ipAddress.Life())
+			logger.Debugf("IP address %v is not dead (life %q); skipping", tag, ipAddress.Life())
 			continue
 		}
-		if err := a.releaseIPAddress(addr); err != nil {
-			return errors.Annotatef(err, "cannot release IP address %v", tag)
-		}
-		logger.Debugf("IP address %v released", tag)
-		toBeRemoved = append(toBeRemoved, ipAddress)
+		toBeReleased = append(toBeReleased, ipAddress)
 	}
-	// Finally remove the release ones.
-	if err := a.api.Remove(toBeRemoved...); err != nil {
+	// Release the IP addresses.
+	if err := a.api.ReleaseIPAddresses(toBeReleased...); err != nil {
+		return errors.Annotate(err, "cannot release all IP addresses")
+	}
+	logger.Debugf("IP address %v released", tag)
+	// Finally remove the released ones.
+	if err := a.api.Remove(toBeReleased...); err != nil {
 		return errors.Annotate(err, "cannot remove all released IP addresses")
 	}
 	logger.Debugf("removed released IP addresses")
 	return nil
-}
-
-func (a *addresserHandler) releaseIPAddress(ipAddress *apiaddresser.IPAddress) (err error) {
-	defer errors.DeferredAnnotatef(&err, "failed to release address %v", addr.Value())
-	logger.Debugf("attempting to release dead address %#v", addr.Value())
-
-	subnetId := network.Id(addr.SubnetId())
-	for attempt := common.ShortAttempt.Start(); attempt.Next(); {
-		err = a.releaser.ReleaseAddress(addr.InstanceId(), subnetId, addr.Address(), addr.MACAddress())
-		if err == nil {
-			return nil
-		}
-	}
-	// Don't remove the address from state so we
-	// can retry releasing the address later.
-	logger.Warningf("cannot release address %q: %v (will retry)", addr.Value(), err)
-	return errors.Trace(err)
 }
 
 // SetUp is part of the StringsWorker interface.
