1bc74cae25e7344ee3f57038ef20304cf93d2d6f
Andrew Wilkins <axwalk@gmail.com>
2015-07-29 12:42:07 +0800

    charmresources: add ParseResourcePath

--- charmresources/resourcepath.go
+++ charmresources/resourcepath.go
@@ -5,6 +5,8 @@ package charmresources
 
 import (
 	"path"
+	"regexp"
+	"strings"
 
 	"github.com/juju/errors"
 )
@@ -38,9 +40,46 @@ func ResourcePath(params ResourceAttributes) (string, error) {
 	return resPath, nil
 }
 
+var segmentSnippet = "[^/]+"
+var resourcePathRe = regexp.MustCompile(
+	"^/" +
+		"(" + segmentSnippet + ")/" + // type
+		"(?:org/(" + segmentSnippet + ")/)?" + // org
+		"(?:u/(" + segmentSnippet + ")/)?" + // user
+		"(?:c/(" + segmentSnippet + ")/)?" + // stream
+		"(?:s/(" + segmentSnippet + ")/)?" + // series
+		"(" + segmentSnippet + ")" + // path-name
+		"(?:/(" + segmentSnippet + "))?" + // revision
+		"$",
+)
+
+// ParseResourcePath parses a resource path into its constituent
+// ResourceAttributes.
+func ParseResourcePath(resourcePath string) (ResourceAttributes, error) {
+	var attrs ResourceAttributes
+	submatch := resourcePathRe.FindStringSubmatch(resourcePath)
+	if submatch == nil {
+		return attrs, errors.Errorf("invalid resource path %q", resourcePath)
+	}
+	attrs.Type = submatch[1]
+	attrs.Org = submatch[2]
+	attrs.User = submatch[3]
+	attrs.Stream = submatch[4]
+	attrs.Series = submatch[5]
+	attrs.PathName = submatch[6]
+	attrs.Revision = submatch[7]
+	if err := validatePathParameters(attrs); err != nil {
+		return attrs, errors.Trace(err)
+	}
+	return attrs, nil
+}
+
 func validatePathParameters(params ResourceAttributes) error {
 	if params.PathName == "" {
 		return errors.New("resource path name cannot be empty")
+	}
+	if strings.Contains(params.PathName, "/") {
+		return errors.New(`resource path name cannot contain "/"`)
 	}
 	if params.User != "" && params.Org != "" {
 		return errors.New("both user and org cannot be specified together")
