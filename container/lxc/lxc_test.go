c8aaf24cf25ce82668be5bb658a7b08a600d1780
Cheryl Jennings <cheryl.jennings@canonical.com>
2015-07-29 11:47:22 -0500

    Add clarifying comment to noproxy test.

--- container/lxc/lxc_test.go
+++ container/lxc/lxc_test.go
@@ -1262,6 +1262,8 @@ func (s *LxcSuite) TestWgetEnvironmentUsesNoProxy(c *gc.C) {
 	})
 	_, closer, err := lxc.WgetEnvironment(fakeCert)
 	if err == nil {
+		// If the WgetEnvironment call was successful, we need to clean
+		// up the temporary directory it created.
 		defer closer()
 	}
 	c.Assert(err, jc.ErrorIsNil)
