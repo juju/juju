b7ba4fc5cb6ad0405644ab52a449991bb2e8ee66
Cheryl Jennings <cheryl.jennings@canonical.com>
2015-07-31 15:23:54 -0500

    Check for NotImplemented err with new charms endpoint
    When deploying a charm to an older state server, the
    call to charms.IsMetered may fail with NotImplemented.
    Warn the user, but don't return an error from the
    deploy.

--- cmd/juju/commands/deploy_test.go
+++ cmd/juju/commands/deploy_test.go
@@ -13,6 +13,7 @@ import (
 	"strings"
 
 	"github.com/juju/errors"
+	"github.com/juju/persistent-cookiejar"
 	jujutesting "github.com/juju/testing"
 	jc "github.com/juju/testing/checkers"
 	"github.com/juju/utils"
@@ -26,6 +27,7 @@ import (
 	"gopkg.in/macaroon-bakery.v0/bakerytest"
 
 	"github.com/juju/juju/api"
+	"github.com/juju/juju/apiserver/params"
 	"github.com/juju/juju/cmd/envcmd"
 	"github.com/juju/juju/cmd/juju/service"
 	"github.com/juju/juju/constraints"
@@ -654,4 +656,21 @@ func (s *DeploySuite) TestAddMetricCredentialsHttp(c *gc.C) {
 	c.Assert(handler.registrationCalls, gc.HasLen, 1)
 	c.Assert(handler.registrationCalls[0].CharmURL, gc.DeepEquals, "local:quantal/metered-1")
 	c.Assert(handler.registrationCalls[0].ServiceName, gc.DeepEquals, "metered")
+}
+
+func (s *DeploySuite) TestDeployCharmsEndpointNotImplemented(c *gc.C) {
+
+	s.PatchValue(&registerMeteredCharm, func(r string, s *api.State, j *cookiejar.Jar, c string, sv, e string) error {
+		return &params.Error{"IsMetered", params.CodeNotImplemented}
+	})
+
+	testcharms.Repo.ClonedDirPath(s.SeriesPath, "dummy")
+	ctx, err := coretesting.RunCommand(c, envcmd.Wrap(&DeployCommand{}), "local:dummy")
+	c.Assert(err, jc.ErrorIsNil)
+
+	c.Assert(coretesting.Stdout(ctx), gc.Equals, "")
+	output := strings.Split(coretesting.Stderr(ctx), "\n")
+	c.Check(output[0], gc.Matches, `Added charm ".*" to the environment.`)
+	c.Check(output[1], gc.Equals, "Current state server version does not support charm metering.")
+
 }
