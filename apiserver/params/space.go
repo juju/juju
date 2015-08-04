866d3047082e33b0a0c12ba61bf53349968c827d
Michael Foord <michael.foord@canonical.com>
2015-08-04 16:49:32 +0300

    Skeleton parameter for SpaceList

---
+++ apiserver/params/space.go
@@ -0,0 +1,13 @@
+// Copyright 2015 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package params
+
+//  .
+type SpaceListResults struct {
+	Results []SpaceListResult
+	Error   *Error
+}
+
+type SpaceListResult struct {
+}
