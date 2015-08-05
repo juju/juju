40d08f9f0b26b66ae7da3062763ef94a03716c2e
Curtis Hovey <curtis@canonical.com>
2015-08-05 13:04:31 -0400

    Added tests for copy_packages,

--- tests/test_lp_copy_packages.py
+++ tests/test_lp_copy_packages.py
@@ -5,6 +5,7 @@ from mock import (
 from unittest import TestCase
 
 from lp_copy_packages import (
+    copy_packages,
     get_archives,
     get_args,
     Launchpad,
@@ -57,3 +58,25 @@ class LPCopyPackagesTestCase(TestCase):
         from_archive, to_archive = get_archives(lp, 'stable')
         from_team_mock.getPPAByName.assert_called_with_any(name='proposed')
         from_team_mock.getPPAByName.assert_called_with(name='stable')
+
+    def test_copy_packages(self):
+        lp = Mock()
+        from_archive = Mock(getPublishedSources=Mock())
+        from_archive.getPublishedSources.return_value = [
+            Mock(source_package_version='1.2.3~0',
+                 source_package_name='juju-core'),
+            Mock(source_package_version='1.2.1~0',
+                 source_package_name='juju-core')]
+        to_archive = Mock(copyPackage=Mock())
+        with patch('lp_copy_packages.get_archives', autospec=True,
+                   return_value=(from_archive, to_archive)) as ga_mock:
+            return_code = copy_packages(lp, '1.2.3', 'proposed', dry_run=False)
+        self.assertEqual(0, return_code)
+        ga_mock.assert_called_with(lp, 'proposed')
+        from_archive.getPublishedSources.assert_called_with(
+            status='Published', source_name='juju-core')
+        self.assertEqual(1, to_archive.copyPackage.call_count)
+        to_archive.copyPackage.assert_called_with(
+            from_archive=from_archive, source_name='juju-core',
+            version='1.2.3~0', to_pocket='Release',
+            include_binaries=True, unembargo=True)
