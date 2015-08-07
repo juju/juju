0da7ffb742bdf8a6108635e2fd8c80fe63582cf6
William Reade <fwereade@gmail.com>
2015-08-07 19:17:47 +0200

    fix build

--- cmd/jujud/agent/unit.go
+++ cmd/jujud/agent/unit.go
@@ -17,11 +17,7 @@ import (
 	"launchpad.net/tomb"
 
 	"github.com/juju/juju/agent"
-<<<<<<< HEAD
-	"github.com/juju/juju/api/leadership"
-=======
 	"github.com/juju/juju/cmd/jujud/agent/unit"
->>>>>>> dropped filter manifold (not a shared resource); added uniter manifold; uniter now gets leadership tracker as a resource from manifold, does not create its own; moved APIOpen funcs from cmd/jujud/agent to worker/agent; s/Api/API/ in some field names for consistency
 	cmdutil "github.com/juju/juju/cmd/jujud/util"
 	"github.com/juju/juju/network"
 	"github.com/juju/juju/version"
@@ -134,122 +130,22 @@ func (a *UnitAgent) Run(ctx *cmd.Context) error {
 	return err
 }
 
-func (a *UnitAgent) APIWorkers() (_ worker.Worker, err error) {
-<<<<<<< HEAD
-	agentConfig := a.CurrentConfig()
-	dataDir := agentConfig.DataDir()
-	hookLock, err := cmdutil.HookExecutionLock(dataDir)
-	if err != nil {
-		return nil, err
-	}
-	st, entity, err := OpenAPIState(agentConfig, a)
-	if err != nil {
-		return nil, err
-	}
-	defer func() {
-		// TODO(fwereade): this is not properly tested. Old tests were both
-		// incomplete (missing a fail case) and evil (dependent on injecting
-		// an error in a patched-out upgrader API that shouldn't even be
-		// used at this level)... so I just deleted them. Not a major worry:
-		// this whole method will become redundant once we switch to the
-		// dependency engine (and specifically use worker/apicaller to
-		// connect).
-		if err != nil {
-			if err := st.Close(); err != nil {
-				logger.Errorf("while closing API: %v", err)
-			}
-		}
-	}()
+// APIWorkers returns a dependency.Engine running the unit agent's responsibilities.
+func (a *UnitAgent) APIWorkers() (worker.Worker, error) {
+	manifolds := unit.Manifolds(unit.ManifoldsConfig{
+		Agent:               a,
+		LogSource:           a.bufferedLogs,
+		LeadershipGuarantee: 30 * time.Second,
+	})
 
-	// Ensure that the environment uuid is stored in the agent config.
-	// Luckily the API has it recorded for us after we connect.
-	if agentConfig.Environment().Id() == "" {
-		err := a.ChangeConfig(func(setter agent.ConfigSetter) error {
-			environTag, err := st.EnvironTag()
-			if err != nil {
-				return errors.Annotate(err, "no environment uuid set on api")
-			}
-
-			return setter.Migrate(agent.MigrateParams{
-				Environment: environTag,
-			})
-		})
-		if err != nil {
-			logger.Warningf("unable to save environment uuid: %v", err)
-			// Not really fatal, just annoying.
-=======
-	manifolds := unit.Manifolds(a, a.bufferedLogs)
 	engine := dependency.NewEngine(cmdutil.IsFatal, 3*time.Second, 10*time.Millisecond)
 	if err := dependency.Install(engine, manifolds); err != nil {
 		if err := worker.Stop(engine); err != nil {
 			logger.Errorf("while stopping engine with bad manifolds: %v", err)
->>>>>>> dropped filter manifold (not a shared resource); added uniter manifold; uniter now gets leadership tracker as a resource from manifold, does not create its own; moved APIOpen funcs from cmd/jujud/agent to worker/agent; s/Api/API/ in some field names for consistency
 		}
 		return nil, err
 	}
-<<<<<<< HEAD
-
-	unitTag, err := names.ParseUnitTag(entity.Tag())
-	if err != nil {
-		return nil, errors.Trace(err)
-	}
-	runner := worker.NewRunner(cmdutil.ConnectionIsFatal(logger, st), cmdutil.MoreImportant)
-
-	// start proxyupdater first to ensure proxy settings are correct
-	runner.StartWorker("proxyupdater", func() (worker.Worker, error) {
-		return proxyupdater.New(st.Environment(), false), nil
-	})
-	if feature.IsDbLogEnabled() {
-		runner.StartWorker("logsender", func() (worker.Worker, error) {
-			return logsender.New(a.bufferedLogs, agentConfig.APIInfo()), nil
-		})
-	}
-	runner.StartWorker("upgrader", func() (worker.Worker, error) {
-		return upgrader.NewAgentUpgrader(
-			st.Upgrader(),
-			agentConfig,
-			agentConfig.UpgradedToVersion(),
-			func() bool { return false },
-			a.initialAgentUpgradeCheckComplete,
-		), nil
-	})
-	runner.StartWorker("logger", func() (worker.Worker, error) {
-		return workerlogger.NewLogger(st.Logger(), agentConfig), nil
-	})
-	runner.StartWorker("uniter", func() (worker.Worker, error) {
-		uniterFacade, err := st.Uniter()
-		if err != nil {
-			return nil, errors.Trace(err)
-		}
-		uniterParams := uniter.UniterParams{
-			uniterFacade,
-			unitTag,
-			leadership.NewClient(st),
-			dataDir,
-			hookLock,
-			uniter.NewMetricsTimerChooser(),
-			uniter.NewUpdateStatusTimer(),
-			operation.NewExecutor,
-		}
-		return uniter.NewUniter(&uniterParams), nil
-	})
-
-	runner.StartWorker("apiaddressupdater", func() (worker.Worker, error) {
-		uniterFacade, err := st.Uniter()
-		if err != nil {
-			return nil, errors.Trace(err)
-		}
-		return apiaddressupdater.NewAPIAddressUpdater(uniterFacade, a), nil
-	})
-	if !featureflag.Enabled(feature.DisableRsyslog) {
-		runner.StartWorker("rsyslog", func() (worker.Worker, error) {
-			return cmdutil.NewRsyslogConfigWorker(st.Rsyslog(), agentConfig, rsyslog.RsyslogModeForwarding)
-		})
-	}
-	return cmdutil.NewCloseWorker(logger, runner, st), nil
-=======
 	return engine, nil
->>>>>>> dropped filter manifold (not a shared resource); added uniter manifold; uniter now gets leadership tracker as a resource from manifold, does not create its own; moved APIOpen funcs from cmd/jujud/agent to worker/agent; s/Api/API/ in some field names for consistency
 }
 
 func (a *UnitAgent) Tag() names.Tag {
