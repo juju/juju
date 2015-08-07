d88d12b0d4c1ad32f87257361126f2b4cf633632
bogdanteleaga <bteleaga@cloudbasesolutions.com>
2015-08-07 18:20:15 +0200

    Added support for sending userdata on windows as a (small-sized) script

--- cloudconfig/providerinit/providerinit.go
+++ cloudconfig/providerinit/providerinit.go
@@ -7,12 +7,17 @@
 package providerinit
 
 import (
+	"encoding/base64"
+	"errors"
+	"fmt"
+
 	"github.com/juju/loggo"
 	"github.com/juju/utils"
 
 	"github.com/juju/juju/cloudconfig"
 	"github.com/juju/juju/cloudconfig/cloudinit"
 	"github.com/juju/juju/cloudconfig/instancecfg"
+	"github.com/juju/juju/version"
 )
 
 var logger = loggo.GetLogger("juju.cloudconfig.providerinit")
@@ -55,10 +60,40 @@ func ComposeUserData(icfg *instancecfg.InstanceConfig, cloudcfg cloudinit.CloudC
 	if err != nil {
 		return nil, err
 	}
+	operatingSystem, err := version.GetOSFromSeries(icfg.Series)
+	if err != nil {
+		return nil, err
+	}
+	switch operatingSystem {
+	case version.Ubuntu, version.CentOS:
+		return gzippedUserdata(cloudcfg)
+	case version.Windows:
+		return encodedUserdata(cloudcfg)
+	default:
+		return nil, errors.New(fmt.Sprintf("Cannot compose userdata for os %s", operatingSystem))
+	}
+}
+
+// gzippedUserdata returns the rendered userdata in a gzipped format
+func gzippedUserdata(cloudcfg cloudinit.CloudConfig) ([]byte, error) {
 	data, err := cloudcfg.RenderYAML()
 	logger.Tracef("Generated cloud init:\n%s", string(data))
 	if err != nil {
 		return nil, err
 	}
 	return utils.Gzip(data), nil
+}
+
+// encodedUserdata for now is used on windows and it retuns a powershell script
+// which has the userdata embedded as base64(gzip(userdata))
+// We need this because most cloud provider do not accept gzipped userdata on
+// windows and they have size limitations
+func encodedUserdata(cloudcfg cloudinit.CloudConfig) ([]byte, error) {
+	zippedData, err := gzippedUserdata(cloudcfg)
+	if err != nil {
+		return nil, err
+	}
+
+	base64Data := base64.StdEncoding.EncodeToString(zippedData)
+	return []byte(fmt.Sprintf(cloudconfig.UserdataScript, base64Data)), nil
 }
