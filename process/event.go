b12cd408bba68abb036ecbc5503c802673a1b6fa
Eric Snow <ericsnowcurrently@gmail.com>
2015-08-10 10:05:50 -0600

    Add the plugin to events.

--- process/event.go
+++ process/event.go
@@ -3,11 +3,28 @@
 
 package process
 
+import (
+	"gopkg.in/juju/charm.v5"
+)
+
+// The kinds of events.
 const (
 	EventKindNoop      = ""
 	EventKindTracked   = "tracked"
 	EventKindUntracked = "untracked"
 )
+
+// Plugin represents the functionality of a workload process plugin.
+type Plugin interface {
+	// Launch runs the plugin's "launch" command, passing the provided
+	// process definition. The output is converted to a Details.
+	Launch(definition charm.Process) (Details, error)
+	// Destroy runs the plugin's "destroy" command for the given ID.
+	Destroy(id string) error
+	// Status runs the plugin's "status" command. The output is
+	// converted to a PluginStatus.
+	Status(id string) (PluginStatus, error)
+}
 
 // Event describes something that happened for a workload process.
 type Event struct {
@@ -15,10 +32,6 @@ type Event struct {
 	Kind string
 	// ID identifies the workload process, relative to the current unit.
 	ID string
-}
-
-// AddEvents adds the provided events to the channel.
-func AddEvents(ch chan []Event, events ...Event) {
-	// TODO(ericsnow) Validate the events?
-	ch <- events
+	// Plugin is the plugin to use for this event.
+	Plugin Plugin
 }
