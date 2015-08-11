d087b38b20d09cfba576d35cbc0f4f67e209a9ea
Eric Snow <ericsnowcurrently@gmail.com>
2015-08-11 12:33:03 -0600

    Adjust the testing stubs.

--- worker/testing/runner.go
+++ worker/testing/runner.go
@@ -6,17 +6,27 @@ package testing
 import (
 	"github.com/juju/errors"
 	"github.com/juju/testing"
+	jc "github.com/juju/testing/checkers"
+	gc "gopkg.in/check.v1"
 
 	"github.com/juju/juju/worker"
 )
 
 var _ worker.Runner = (*StubRunner)(nil)
 
+var runnerMethodNames = []string{
+	"StartWorker",
+	"StopWorker",
+}
+
 // StubRunner is a testing stub for worker.Runner.
 type StubRunner struct {
 	worker.Worker
 	// Stub is the underlying testing stub.
 	Stub *testing.Stub
+	// CallWhenStarted indicates that the newWorker func should be
+	// called when StartWorker is called.
+	CallWhenStarted bool
 }
 
 // NewStubRunner returns a new StubRunner.
@@ -27,6 +37,40 @@ func NewStubRunner(stub *testing.Stub) *StubRunner {
 	}
 }
 
+func (r *StubRunner) validMethodName(funcName string) bool {
+	for _, knownName := range runnerMethodNames {
+		if funcName == knownName {
+			return true
+		}
+	}
+	return false
+}
+
+func (r *StubRunner) checkCallIDs(c *gc.C, methName string, skipMismatch bool, expected []string) {
+	var ids []string
+	for _, call := range r.Stub.Calls() {
+		if !r.validMethodName(call.FuncName) {
+			c.Logf("invalid called func name %q (must be one of %#v)", call.FuncName, runnerMethodNames)
+			c.FailNow()
+		}
+		if methName != "" {
+			if skipMismatch && call.FuncName != methName {
+				continue
+			}
+			c.Check(call.FuncName, gc.Equals, methName)
+		}
+		ids = append(ids, call.Args[0].(string))
+	}
+	c.Check(ids, jc.DeepEquals, expected)
+}
+
+// CheckCallIDs verifies that the worker IDs in all calls match the
+// provided ones. If a method name is provided as well then all calls must
+// have that method name.
+func (r *StubRunner) CheckCallIDs(c *gc.C, methName string, expected ...string) {
+	r.checkCallIDs(c, methName, false, expected)
+}
+
 // StartWorker implements worker.Runner.
 func (r *StubRunner) StartWorker(id string, newWorker func() (worker.Worker, error)) error {
 	r.Stub.AddCall("StartWorker", id, newWorker)
@@ -34,7 +78,12 @@ func (r *StubRunner) StartWorker(id string, newWorker func() (worker.Worker, err
 		return errors.Trace(err)
 	}
 
-	// Do nothing.
+	if r.CallWhenStarted {
+		// TODO(ericsnow) Save the workers?
+		if _, err := newWorker(); err != nil {
+			return errors.Trace(err)
+		}
+	}
 	return nil
 }
 
