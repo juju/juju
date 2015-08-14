61bae648102a24d6e2c3c6c345151a5585c22765
Nate Finch <nate.finch@gmail.com>
2015-08-13 22:03:32 -0400

    rename unreg to untrack

--- process/api/server/hookcontext_test.go
+++ process/api/server/hookcontext_test.go
@@ -388,13 +388,13 @@ func (suite) TestSetProcessStatus(c *gc.C) {
 	c.Assert(res, gc.DeepEquals, expected)
 }
 
-func (suite) TestUnregisterProcesses(c *gc.C) {
+func (suite) TestUntrack(c *gc.C) {
 	st := &FakeState{}
 	a := HookContextAPI{st}
-	args := api.UnregisterProcessesArgs{
+	args := api.UntrackArgs{
 		IDs: []string{"fooID/bar"},
 	}
-	res, err := a.UnregisterProcesses(args)
+	res, err := a.Untrack(args)
 	c.Assert(err, jc.ErrorIsNil)
 
 	c.Assert(st.id, gc.Equals, "fooID/bar")
