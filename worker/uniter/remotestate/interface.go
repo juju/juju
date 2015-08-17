f3fda60ccb45219a0ff3624a272860d18edb1b9e
Andrew Wilkins <axwalk@gmail.com>
2015-08-17 15:49:00 +0200

    worker/uniter/remotestate: add package

---
+++ worker/uniter/remotestate/interface.go
@@ -0,0 +1,10 @@
+package remotestate
+
+type Watcher interface {
+	// RemoteStateChanged returns a channel which is signalled
+	// whenever the remote state is changed.
+	RemoteStateChanged() <-chan struct{}
+
+	// Snapshot returns the current snapshot of the remote state.
+	Snapshot() Snapshot
+}
