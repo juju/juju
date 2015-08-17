54d4ff4d39d6be86635d93fee9d78fadcbcd678e
Curtis Hovey <curtis@canonical.com>
2015-08-17 12:59:34 -0400

    Updated docstrings.

--- publish_streams.py
+++ publish_streams.py
@@ -27,23 +27,34 @@ CPCS = {
 
 
 def get_remote_file(url):
+    """Return the content of a remote file."""
     response = urllib2.urlopen(url)
     content = response.read()
     return content
 
 
 def diff_files(local, remote):
+    """Return the difference of a local and a remote file.
+
+    :return: a tuple of identical (True, None) or different (False, str).
+    """
     with open(local, 'r') as f:
         local_lines = f.read().splitlines()
     remote_lines = get_remote_file(remote).splitlines()
     diff_gen = difflib.unified_diff(local_lines, remote_lines, local, remote)
-    diff = '\n'.join([l for l in diff_gen])
+    diff = '\n'.join(list(diff_gen))
     if diff:
         return False, diff
     return True, None
 
 
 def verify_metadata(location, remote_stream, verbose=False):
+    """Verify all the streams metadata in a cloud matches the local metadata.
+
+
+    This verifies all the streams, not a single stream, to ensure the cloud
+    has exactly the same metadata as the local instance.
+    """
     local_metadata = os.path.join(location, 'tools', 'streams', 'v1')
     remote_metadata = '{}/tools/streams/v1'.format(remote_stream)
     if verbose:
@@ -64,6 +75,7 @@ def verify_metadata(location, remote_stream, verbose=False):
 
 def publish(stream, location, cloud,
             remote_root=None, dry_run=False, verbose=False):
+    """Publish a stream to a cloud and verify it."""
     if remote_root:
         remote_stream = '{}/{}'.format(CPCS[cloud], remote_root)
     else:
@@ -75,13 +87,11 @@ def parse_args(argv=None):
     """Return the argument parser for this program."""
     parser = ArgumentParser("Publish streams to a cloud.")
     parser.add_argument(
-        '-d', '--dry-run', action="store_true", default=False,
-        help='Do not make changes.')
+        '-d', '--dry-run', action="store_true", help='Do not make changes.')
     parser.add_argument(
-        '-v', '--verbose', action="store_true", default=False,
-        help='Increase verbosity.')
+        '-v', '--verbose', action="store_true", help='Increase verbosity.')
     parser.add_argument(
-        '-r', '--remote-root', default=None,
+        '-r', '--remote-root',
         help='An alternate root to publish to such as testing or weekly.')
     parser.add_argument(
         'stream', help='The agent-stream to publish.',
@@ -96,7 +106,7 @@ def parse_args(argv=None):
 
 
 def main(argv):
-    """Manage agent-archive in the archive."""
+    """Publish streams to a cloud."""
     args = parse_args(argv)
     try:
         publish(
@@ -104,7 +114,7 @@ def main(argv):
             remote_root=args.remote_root, dry_run=args.dry_run,
             verbose=args.verbose)
     except Exception as e:
-        print(e)
+        print('{}: {}'.format(e.__class__.__name__, e))
         if args.verbose:
             traceback.print_tb(sys.exc_info()[2])
         return 2
