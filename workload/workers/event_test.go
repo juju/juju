e3a3987d2bd42941702bcc2c40b369fab7e640b3
Eric Snow <ericsnowcurrently@gmail.com>
2015-08-17 17:55:42 -0600

    Fold NewWorker into eventsManifold.

--- workload/workers/event_test.go
+++ workload/workers/event_test.go
@@ -4,12 +4,15 @@
 package workers_test
 
 import (
+	"time"
+
 	"github.com/juju/errors"
 	gitjujutesting "github.com/juju/testing"
 	jc "github.com/juju/testing/checkers"
 	gc "gopkg.in/check.v1"
 
 	"github.com/juju/juju/testing"
+	"github.com/juju/juju/worker/dependency"
 	workertesting "github.com/juju/juju/worker/testing"
 	"github.com/juju/juju/workload"
 	"github.com/juju/juju/workload/context"
@@ -21,7 +24,7 @@ type eventHandlerSuite struct {
 
 	stub      *gitjujutesting.Stub
 	runner    *workertesting.StubRunner
-	apiClient context.APIClient // TODO(ericsnow) Use a stub.
+	apiClient *stubAPIClient
 }
 
 var _ = gc.Suite(&eventHandlerSuite{})
@@ -31,6 +34,7 @@ func (s *eventHandlerSuite) SetUpTest(c *gc.C) {
 
 	s.stub = &gitjujutesting.Stub{}
 	s.runner = workertesting.NewStubRunner(s.stub)
+	s.apiClient = &stubAPIClient{stub: s.stub}
 }
 
 func (s *eventHandlerSuite) handler(events []workload.Event, apiClient context.APIClient, runner workers.Runner) error {
@@ -79,21 +83,30 @@ func (s *eventHandlerSuite) TestAddEvents(c *gc.C) {
 	c.Check(got, jc.DeepEquals, [][]workload.Event{events})
 }
 
-func (s *eventHandlerSuite) TestNewWorker(c *gc.C) {
+func (s *eventHandlerSuite) TestManifolds(c *gc.C) {
 	events := []workload.Event{{
 		Kind: workload.EventKindTracked,
 		ID:   "spam/eggs",
 	}}
+	engine, err := dependency.NewEngine(dependency.EngineConfig{
+		IsFatal:       func(error) bool { return false },
+		MoreImportant: func(_ error, worst error) error { return worst },
+		ErrorDelay:    3 * time.Second,
+		BounceDelay:   10 * time.Second,
+	})
+	c.Assert(err, jc.ErrorIsNil)
+
 	eh := workers.NewEventHandlers()
 	eh.Init(s.apiClient, s.runner)
 	eh.RegisterHandler(s.handler)
-	w, err := eh.NewWorker()
+	manifolds := eh.Manifolds()
+	err = dependency.Install(engine, manifolds)
 	c.Assert(err, jc.ErrorIsNil)
 
 	eh.AddEvents(events...)
 
-	w.Kill()
-	err = w.Wait()
+	engine.Kill()
+	err = engine.Wait()
 	c.Assert(err, jc.ErrorIsNil)
 	eh.Close()
 
@@ -102,10 +115,25 @@ func (s *eventHandlerSuite) TestNewWorker(c *gc.C) {
 		unhandled = append(unhandled, event)
 	}
 	c.Check(unhandled, gc.HasLen, 0)
-	s.stub.CheckCallNames(c, "handler")
-	c.Check(s.stub.Calls()[0].Args[0], gc.DeepEquals, events)
-	c.Check(s.stub.Calls()[0].Args[1], gc.DeepEquals, s.apiClient)
-	runner, running := workers.ExposeRunner(s.stub.Calls()[0].Args[2].(workers.Runner))
+	s.stub.CheckCallNames(c, "List", "handler", "handler")
+	c.Check(s.stub.Calls()[1].Args[0], gc.HasLen, 0)
+	c.Check(s.stub.Calls()[2].Args[0], gc.DeepEquals, events)
+	c.Check(s.stub.Calls()[2].Args[1], gc.DeepEquals, s.apiClient)
+	runner, running := workers.ExposeRunner(s.stub.Calls()[2].Args[2].(workers.Runner))
 	c.Check(runner, jc.DeepEquals, s.runner)
 	c.Check(running.Values(), gc.HasLen, 0)
+}
+
+type stubAPIClient struct {
+	context.APIClient
+	stub *gitjujutesting.Stub
+}
+
+func (c *stubAPIClient) List(ids ...string) ([]workload.Info, error) {
+	c.stub.AddCall("List", ids)
+	if err := c.stub.NextErr(); err != nil {
+		return nil, errors.Trace(err)
+	}
+
+	return nil, nil
 }
