591249d0af9fe63773d344db2c2dbad423352d5e
Eric Snow <ericsnowcurrently@gmail.com>
2015-08-17 16:21:19 -0600

    Add a logger.

--- component/all/workload.go
+++ component/all/workload.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/juju/cmd"
 	"github.com/juju/errors"
+	"github.com/juju/loggo"
 	"github.com/juju/names"
 	"gopkg.in/juju/charm.v5"
 
@@ -31,6 +32,8 @@ import (
 	"github.com/juju/juju/workload/status"
 	"github.com/juju/juju/workload/workers"
 )
+
+var workloadsLogger = loggo.GetLogger("component.all.workload")
 
 type workloads struct{}
 
