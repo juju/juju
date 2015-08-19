89642b328c82c4ddcf44fadec274276a438af8ea
Eric Snow <ericsnowcurrently@gmail.com>
2015-08-19 08:56:51 -0600

    Move process.Plugin to own file.

--- workload/event.go
+++ workload/event.go
@@ -3,28 +3,12 @@
 
 package workload
 
-import (
-	"gopkg.in/juju/charm.v5"
-)
-
 // The kinds of events.
 const (
 	EventKindNoop      = ""
 	EventKindTracked   = "tracked"
 	EventKindUntracked = "untracked"
 )
-
-// Plugin represents the functionality of a workload plugin.
-type Plugin interface {
-	// Launch runs the plugin's "launch" command, passing the provided
-	// workload definition. The output is converted to a Details.
-	Launch(definition charm.Workload) (Details, error)
-	// Destroy runs the plugin's "destroy" command for the given ID.
-	Destroy(id string) error
-	// Status runs the plugin's "status" command. The output is
-	// converted to a PluginStatus.
-	Status(id string) (PluginStatus, error)
-}
 
 // Event describes something that happened for a workload.
 type Event struct {
