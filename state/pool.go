2f98166bf878e004e05d7a2e08bc457f4bb7e0f2
Menno Smits <menno.smits@canonical.com>
2015-08-20 16:13:11 +1200

    state: exported StatePool
    statePool was just being used by the AllEnvWatcher plumbing but will
    soon be used with the API server too.
    
    Also expanded testing and added a short-circuit for the state server
    environment case.

---
+++ state/pool.go
@@ -0,0 +1,67 @@
+// Copyright 2015 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package state
+
+import (
+	"sync"
+
+	"github.com/juju/errors"
+	"github.com/juju/names"
+)
+
+// NewStatePool returns a new StatePool instance. It takes a State for
+// the state server environment.
+func NewStatePool(ssSt *State) *StatePool {
+	return &StatePool{
+		ssSt: ssSt,
+		pool: make(map[string]*State),
+	}
+}
+
+// StatePool is a simple cache of State instances for multiple environments.
+type StatePool struct {
+	ssSt *State
+	// mu protects pool
+	mu   sync.Mutex
+	pool map[string]*State
+}
+
+// Get returns a State for a given environment from the pool, creating
+// one if required.
+func (p *StatePool) Get(envUUID string) (*State, error) {
+	if envUUID == p.ssSt.EnvironUUID() {
+		return p.ssSt, nil
+	}
+
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	st, ok := p.pool[envUUID]
+	if ok {
+		return st, nil
+	}
+
+	st, err := p.ssSt.ForEnviron(names.NewEnvironTag(envUUID))
+	if err != nil {
+		return nil, errors.Annotatef(err, "failed to create state for environment %v", envUUID)
+	}
+	p.pool[envUUID] = st
+	return st, nil
+}
+
+// Close closes all State instances in the pool.
+func (p *StatePool) Close() error {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	var lastErr error
+	for _, st := range p.pool {
+		err := st.Close()
+		if err != nil {
+			lastErr = err
+		}
+	}
+	p.pool = make(map[string]*State)
+	return errors.Annotate(lastErr, "at least one error closing a state")
+}
