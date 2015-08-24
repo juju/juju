c6b7f24bfc64afa9d83fa2224d7e3f62dd920d41
Francesco Banconi <francesco.banconi@canonical.com>
2015-08-24 12:06:28 +0200

    Improve GetBundleChanges API tests.

--- apiserver/client/bundles_test.go
+++ apiserver/client/bundles_test.go
@@ -4,38 +4,37 @@
 package client_test
 
 import (
-	"github.com/juju/bundlechanges"
 	jc "github.com/juju/testing/checkers"
 	gc "gopkg.in/check.v1"
+
+	"github.com/juju/juju/apiserver/params"
 )
 
-type bundlesSuite struct {
-	baseSuite
-}
-
-var _ = gc.Suite(&bundlesSuite{})
-
-func (s *bundlesSuite) TestGetBundleChangesBundleContentError(c *gc.C) {
-	changes, errors, err := s.APIState.Client().GetBundleChanges(":")
+func (s *serverSuite) TestGetBundleChangesBundleContentError(c *gc.C) {
+	args := params.GetBundleChangesParams{
+		BundleDataYAML: ":",
+	}
+	r, err := s.client.GetBundleChanges(args)
 	c.Assert(err, gc.ErrorMatches, `cannot read bundle YAML: cannot unmarshal bundle data: YAML error: did not find expected key`)
-	c.Assert(changes, gc.IsNil)
-	c.Assert(errors, gc.IsNil)
+	c.Assert(r, gc.DeepEquals, params.GetBundleChangesResults{})
 }
 
-func (s *bundlesSuite) TestGetBundleChangesBundleVerificationErrors(c *gc.C) {
-	yaml := `
-        services:
-            django:
-                charm: django
-                to: [1]
-            haproxy:
-                charm: 42
-                num_units: -1
-    `
-	changes, errors, err := s.APIState.Client().GetBundleChanges(yaml)
+func (s *serverSuite) TestGetBundleChangesBundleVerificationErrors(c *gc.C) {
+	args := params.GetBundleChangesParams{
+		BundleDataYAML: `
+            services:
+                django:
+                    charm: django
+                    to: [1]
+                haproxy:
+                    charm: 42
+                    num_units: -1
+        `,
+	}
+	r, err := s.client.GetBundleChanges(args)
 	c.Assert(err, jc.ErrorIsNil)
-	c.Assert(changes, gc.IsNil)
-	c.Assert(errors, jc.SameContents, []string{
+	c.Assert(r.Changes, gc.IsNil)
+	c.Assert(r.Errors, jc.SameContents, []string{
 		`placement "1" refers to a machine not defined in this bundle`,
 		`too many units specified in unit placement for service "django"`,
 		`invalid charm URL in service "haproxy": charm URL has invalid charm name: "42"`,
@@ -43,22 +42,24 @@ func (s *bundlesSuite) TestGetBundleChangesBundleVerificationErrors(c *gc.C) {
 	})
 }
 
-func (s *bundlesSuite) TestGetBundleChangesSuccess(c *gc.C) {
-	yaml := `
-        services:
-            django:
-                charm: django
-                options:
-                    debug: true
-            haproxy:
-                charm: cs:trusty/haproxy-42
-        relations:
-            - - django:web
-              - haproxy:web
-    `
-	changes, errors, err := s.APIState.Client().GetBundleChanges(yaml)
+func (s *serverSuite) TestGetBundleChangesSuccess(c *gc.C) {
+	args := params.GetBundleChangesParams{
+		BundleDataYAML: `
+            services:
+                django:
+                    charm: django
+                    options:
+                        debug: true
+                haproxy:
+                    charm: cs:trusty/haproxy-42
+            relations:
+                - - django:web
+                  - haproxy:web
+        `,
+	}
+	r, err := s.client.GetBundleChanges(args)
 	c.Assert(err, jc.ErrorIsNil)
-	c.Assert(changes, jc.DeepEquals, []*bundlechanges.Change{{
+	c.Assert(r.Changes, jc.DeepEquals, []*params.BundleChangesChange{{
 		Id:     "addCharm-0",
 		Method: "addCharm",
 		Args:   []interface{}{"django"},
@@ -82,5 +83,5 @@ func (s *bundlesSuite) TestGetBundleChangesSuccess(c *gc.C) {
 		Args:     []interface{}{"$addService-1:web", "$addService-3:web"},
 		Requires: []string{"addService-1", "addService-3"},
 	}})
-	c.Assert(errors, gc.IsNil)
+	c.Assert(r.Errors, gc.IsNil)
 }
