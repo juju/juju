4d16f1751bcd8bca2532887321fb38d45c173883
Curtis Hovey <curtis@canonical.com>
2015-08-26 17:41:25 -0400

    Remove mock.

--- tests/test_upload_packages.py
+++ tests/test_upload_packages.py
@@ -67,20 +67,19 @@ class UploadPackageTestCase(TestCase):
         self.assertEqual('foo_source.changes', file_name)
 
     @patch('subprocess.check_call', autospec=True)
-    @patch('upload_packages.get_changes', autospec=True)
-    def test_upload_package_uploaded(self, gc_mock, cc_mock):
-        gc_mock.return_value = (
-            'juju-core', '1.24.5-0ubuntu1~14.04.1~juju1', 'foo_source.changes')
+    def test_upload_package_uploaded(self, cc_mock):
         archive = Mock(getPublishedSources=Mock())
         archive.getPublishedSources.return_value = [
             Mock(source_package_version='1.24.5-0ubuntu1~14.04.1~juju1',
                  source_package_name='juju-core')]
         with temp_dir() as package_dir:
+            changes_path = os.path.join(package_dir, 'foo_source.changes')
+            with open(changes_path, 'w') as changes_file:
+                changes_file.write(CHANGES_DATA)
             result = upload_package(
                 'ppa:bar/baz', archive, package_dir, dry_run=False)
         self.assertFalse(result)
         self.assertEqual(0, cc_mock.call_count)
-        gc_mock.assert_called_once_with(package_dir)
         archive.getPublishedSources.assert_called_once_with(
             source_name='juju-core', version='1.24.5-0ubuntu1~14.04.1~juju1')
 
