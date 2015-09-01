740d2eb514f7102c8a123a9002ef0a0b147ccb8a
Roger Peppe <rogpeppe@gmail.com>
2015-09-01 16:55:23 -0500

    apiserver: create macaroon and bakery service

--- environs/config/config_test.go
+++ environs/config/config_test.go
@@ -19,7 +19,7 @@ import (
 	gc "gopkg.in/check.v1"
 	"gopkg.in/juju/charmrepo.v1"
 	"gopkg.in/juju/environschema.v1"
-	"gopkg.in/macaroon-bakery.v0/bakery"
+	"gopkg.in/macaroon-bakery.v1/bakery"
 
 	"github.com/juju/juju/cert"
 	"github.com/juju/juju/environs/config"
