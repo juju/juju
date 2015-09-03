d16465f9623e19a8b997f11eea22ff3e4824a49c
Eric Snow <ericsnowcurrently@gmail.com>
2015-09-03 11:00:08 -0600

    Only use an executable plugin if it is named "testing-plugin".

--- workload/plugin/plugin.go
+++ workload/plugin/plugin.go
@@ -15,8 +15,31 @@ import (
 
 var logger = loggo.GetLogger("juju.workload.plugin")
 
+// We special-case a specific name for the sake of the feature tests.
+const testingPluginName = "testing-plugin"
+
 // Find returns the plugin for the given name.
+//
+// If the plugin is not found then errors.NotFound is returned.
 func Find(name, dataDir string) (workload.Plugin, error) {
+	if name == testingPluginName {
+		return find(name, dataDir)
+	}
+
+	plugin, err := findBuiltin(name)
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+	return plugin, nil
+}
+
+// find returns the plugin for the given name. First it looks for an
+// executable plugin and then it falls back to one of the built-in
+// plugins. Favoring executable plugins allows charms to maintain
+// control over the plugin's behavior.
+//
+// If the plugin is not found then errors.NotFound is returned.
+func find(name, dataDir string) (workload.Plugin, error) {
 	plugin, err := executable.FindPlugin(name, dataDir)
 	if errors.IsNotFound(err) {
 		plugin, err := findBuiltin(name)
