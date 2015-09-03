64aaa71ec8b0629f3b91982ec04444f4b532790a
Eric Snow <ericsnowcurrently@gmail.com>
2015-09-03 11:00:49 -0600

    Add some explanation about the pros and cons of supporting plugins.

--- workload/plugin/plugin.go
+++ workload/plugin/plugin.go
@@ -3,6 +3,44 @@
 
 // Package plugin contains the code that interfaces with plugins for workload
 // technologies such as Docker, Rocket, or systemd.
+//
+// Workload plugins are either executable files located on $PATH or
+// libraries compiled into the jujud binary. Executable plugins
+// have precedence. This allows charms to override built-in plugins,
+// thus maintaining control of the functionality of the plugin. Doing
+// so is desirable in a few situations which correlate closely with the
+// overall benefits of plugins.
+//
+// In general, support for executable plugins here has the same benefits
+// as the use of plugins in other parts of Juju; it allows charmers to:
+//
+// * guarantee that a plugin will not change during a Juju upgrade
+// * use an updated version of a plugin before it is updated in Juju
+// * use an updated version of a plugin without upgrading Juju
+// * use a custom plugin before it is incorporated into Juju
+// * use a custom plugin that might not be appropriate for Juju
+// * use a proprietary plugin that should not be released publicly
+// * create a plugin without needing to modify the Juju code base
+//
+// The possible down-side to the plugin approach is that a charm could
+// use a plugin which performs poorly or causes failures. In turn that
+// could be misinterpreted as a problem with Juju, thus inaccurately
+// reflecting poorly on Juju itself. This is a concern that cannot be
+// dismissed lightly.
+//
+// This isn't much different from the situation with plugin commands at
+// the Juju CLI or even with charms themselves. However, with charms the
+// distinction between "official" and other charms is more obvious and
+// charms in the charm store are rigorously vetted. Clear rules about
+// plugins used by charms in the charm store would help mitigate that
+// concern. So would tighter control over how such plugins are
+// distributed and updated (e.g. apt, bundled with charm).
+//
+// There is also a specific concern with charms that override built-in
+// plugins, with similar consequences to the perception of Juju: such
+// charms will not benefit from fixes and improvements to the plugin
+// in newer Juju releases. The solution is much the same as with
+// plugins in general.
 package plugin
 
 import (
