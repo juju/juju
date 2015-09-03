e939d48ab1a428f66234b1e1e3bfc5a91a1a29d1
Seman Said <seman.said@canonical.com>
2015-09-02 22:08:52 -0700

    Get OSX client from S3.

--- run-client-server-test-remote.bash
+++ run-client-server-test-remote.bash
@@ -16,13 +16,14 @@ client="$2"
 agent_arg="$3"
 agent_arg_value="$4"
 
-log_dir=$HOME/log
+# Create ssh home.
 ssh_home="/tmp/sshhome"
 mkdir -p $ssh_home/.ssh
 cp $JUJU_HOME/staging-juju-rsa $ssh_home/.ssh/id_rsa
 cp $JUJU_HOME/staging-juju-rsa.pub $ssh_home/.ssh/id_rsa.pub
 export HOME=$ssh_home
 
+# Extract the client and the server.
 mkdir server
 mkdir client
 tar zxf $server -C server
@@ -30,7 +31,8 @@ tar zxf $client -C client
 server_juju=$(find server -name juju)
 client_juju=$(find client -name juju)
 
+mkdir logs
 env=compatibility-control
 ~/Bin/juju destroy-environment --force -y $env || true
 $SCRIPT/assess_heterogeneous_control.py $server_juju $client_juju \
-    test-osx-client-server $env $log_dir $agent_arg $agent_arg_value
+    test-reliability-aws $env logs $agent_arg $agent_arg_value
--- run-client-server-test.bash
+++ run-client-server-test.bash
@@ -4,16 +4,17 @@ SCRIPTS=$(readlink -f $(dirname $0))
 export PATH=$HOME/workspace-runner:$PATH
 
 usage() {
-    echo "usage: $0 user@host server client agent-arg remote-script"
+    echo "usage: $0 user@host server client agent-arg agent-value remote-script"
     exit 1
 }
-test $# -eq 5 || usage
+test $# -eq 6 || usage
 
 user_at_host="$1"
 server="$2"
 client="$3"
 agent_arg="$4"
-remote_script="$5"
+agent_value="$5"
+remote_script="$6"
 
 cat > temp-config.yaml <<EOT
 install:
@@ -23,7 +24,7 @@ install:
         - "$client"
 command: [remote/$remote_script,
           "remote/$(basename $server)", "remote/$(basename $client)",
-          "$agent_arg"]
+          "$agent_arg", "$agent_value"]
 EOT
 
 workspace-run temp-config.yaml $user_at_host
