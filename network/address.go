06ff699370472a270f579224f05c66fb1d199259
Michael Foord <michael.foord@canonical.com>
2015-09-04 12:31:43 +0100

    Comment update

--- network/address.go
+++ network/address.go
@@ -203,7 +203,9 @@ func deriveScope(addr Address) Scope {
 	return addr.Scope
 }
 
-// ExactMatchScope
+// ExactMatchScope checks if an address exactly matches any of the specified
+// scopes. An address will not match if globalPreferIPv6 is set and it isn't an
+// IPv6 address.
 func ExactMatchScope(addr Address, addrScopes ...Scope) bool {
 	if globalPreferIPv6 && addr.Type != IPv6Address {
 		return false
