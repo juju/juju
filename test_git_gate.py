15d24154f61444f1441acdec5e16642e348caeab
Martin Packman <martin.packman@canonical.com>
2015-09-10 10:56:04 +0000

    Add testing for go_test() function

--- test_git_gate.py
+++ test_git_gate.py
@@ -1,3 +1,5 @@
+import argparse
+import mock
 import subprocess
 import unittest
 
@@ -23,17 +25,18 @@ class TestParseArgs(unittest.TestCase):
     def test_merging_other(self):
         args = git_gate.parse_args(
             ["--project", "git.testing/project", "--go-get-all",
-             "--merge-url", "git.testing/proposed"])
+             "--merge-url", "https://git.testing/proposed"])
         self.assertEqual(args.project, "git.testing/project")
-        self.assertEqual(args.merge_url, "git.testing/proposed")
+        self.assertEqual(args.merge_url, "https://git.testing/proposed")
         self.assertEqual(args.merge_ref, "HEAD")
 
     def test_merging_other_ref(self):
         args = git_gate.parse_args(
             ["--project", "git.testing/project", "--go-get-all",
-             "--merge-url", "git.testing/proposed", "--merge-ref", "feature"])
+             "--merge-url", "https://git.testing/proposed",
+             "--merge-ref", "feature"])
         self.assertEqual(args.project, "git.testing/project")
-        self.assertEqual(args.merge_url, "git.testing/proposed")
+        self.assertEqual(args.merge_url, "https://git.testing/proposed")
         self.assertEqual(args.merge_ref, "feature")
 
     def test_project_with_deps(self):
@@ -73,3 +76,138 @@ class TestSubcommandError(unittest.TestCase):
         proc_error = subprocess.CalledProcessError(1, ["git"])
         err = git_gate.SubcommandError("git", "clone", proc_error)
         self.assertEqual(str(err), "Subprocess git clone failed with code 1")
+
+
+class TestGoTest(unittest.TestCase):
+
+    maxDiff = None
+
+    def setUp(self):
+        # Patch out and record actions run as part of go_test()
+        self.actions = []
+        self.patch_action("git_gate.print_now", lambda s: ("print", s))
+        self.patch_action("git_gate.SubcommandRunner.__call__",
+                          lambda self, *args: (self.command,) + args)
+        self.patch_action("os.chdir", lambda d: ("chdir", d))
+        # Verify go commands run with GOPATH overridden
+        real_runner = git_gate.SubcommandRunner
+        def _check(command, environ=None):
+            if command in ("go", "godeps"):
+                self.assertIsInstance(environ, dict)
+                self.assertEquals(environ.get("GOPATH"), "/tmp/fake")
+            return real_runner(command, environ)
+        patcher = mock.patch("git_gate.SubcommandRunner", side_effect=_check)
+        patcher.start()
+        self.addCleanup(patcher.stop)
+
+    def patch_action(self, target, action):
+        def _record(*args, **kwargs):
+            self.actions.append(action(*args, **kwargs))
+        patcher = mock.patch(target, _record)
+        patcher.start()
+        self.addCleanup(patcher.stop)
+
+    @staticmethod
+    def make_args(project, **kwargs):
+        return argparse.Namespace(
+            project=project,
+            project_url=kwargs.get("project_url"),
+            project_ref=kwargs.get("project_ref"),
+            merge_url=kwargs.get("merge_url"),
+            merge_ref=kwargs.get("merge_ref"),
+            go_get_all=kwargs.get("go_get_all"),
+            dependencies=kwargs.get("dependencies", []),
+            tsv_path=kwargs.get("tsv_path"),
+        )
+
+    def test_get_test(self):
+        args = self.make_args("git.testing/project", go_get_all=True)
+        git_gate.go_test(args, "/tmp/fake")
+        self.assertEqual(self.actions, [
+            ('print', 'Getting git.testing/project and dependencies using go'),
+            ('go', 'get', '-v', '-d', '-t', 'git.testing/project/...'),
+            ('chdir', '/tmp/fake/src/git.testing/project'),
+            ('go', 'build', 'git.testing/project/...'),
+            ('go', 'test', 'git.testing/project/...')
+        ])
+
+    def test_get_merge_test(self):
+        args = self.make_args("git.testing/project", go_get_all=True,
+                              merge_url="https://git.testing/proposed",
+                              merge_ref="HEAD")
+        git_gate.go_test(args, "/tmp/fake")
+        self.assertEqual(self.actions, [
+            ('print', 'Getting git.testing/project and dependencies using go'),
+            ('go', 'get', '-v', '-d', '-t', 'git.testing/project/...'),
+            ('chdir', '/tmp/fake/src/git.testing/project'),
+            ('print', 'Merging https://git.testing/proposed ref HEAD'),
+            ('git', 'fetch', 'https://git.testing/proposed', 'HEAD'),
+            ('git', 'merge', '--no-ff', '-m', 'Merged HEAD', 'FETCH_HEAD'),
+            ('go', 'build', 'git.testing/project/...'),
+            ('go', 'test', 'git.testing/project/...')
+        ])
+
+    def test_get_merge_other_test(self):
+        args = self.make_args("git.testing/project", go_get_all=True,
+                              project_url="https://git.testing/project",
+                              project_ref="v1",
+                              merge_url="https://git.testing/proposed",
+                              merge_ref="feature")
+        git_gate.go_test(args, "/tmp/fake")
+        self.assertEqual(self.actions, [
+            ('print', 'Cloning git.testing/project from'
+             ' https://git.testing/project'),
+            ('git', 'clone', 'https://git.testing/project',
+             '/tmp/fake/src/git.testing/project'),
+            ('print', 'Getting git.testing/project and dependencies using go'),
+            ('go', 'get', '-v', '-d', '-t', 'git.testing/project/...'),
+            ('chdir', '/tmp/fake/src/git.testing/project'),
+            ('print', 'Switching repository to v1'),
+            ('git', 'checkout', 'v1'),
+            ('print', 'Merging https://git.testing/proposed ref feature'),
+            ('git', 'fetch', 'https://git.testing/proposed', 'feature'),
+            ('git', 'merge', '--no-ff', '-m', 'Merged feature', 'FETCH_HEAD'),
+            ('go', 'build', 'git.testing/project/...'),
+            ('go', 'test', 'git.testing/project/...')
+        ])
+
+    def test_deps_test(self):
+        args = self.make_args("git.testing/project",
+                              project_url="https://git.testing/project",
+                              dependencies=["git.testing/a", "git.testing/b"])
+        git_gate.go_test(args, "/tmp/fake")
+        self.assertEqual(self.actions, [
+            ('print', 'Cloning git.testing/project from'
+             ' https://git.testing/project'),
+            ('git', 'clone', 'https://git.testing/project',
+             '/tmp/fake/src/git.testing/project'),
+            ('chdir', '/tmp/fake/src/git.testing/project'),
+            ('print', 'Getting git.testing/a and dependencies using go'),
+            ('go', 'get', '-v', '-d', 'git.testing/a'),
+            ('print', 'Getting git.testing/b and dependencies using go'),
+            ('go', 'get', '-v', '-d', 'git.testing/b'),
+            ('go', 'build', 'git.testing/project/...'),
+            ('go', 'test', 'git.testing/project/...')
+        ])
+
+    def test_tsv_test(self):
+        args = self.make_args("git.testing/project",
+                              project_url="https://git.testing/project",
+                              tsv_path="git.testing/project/dependencies.tsv")
+        git_gate.go_test(args, "/tmp/fake")
+        self.assertEqual(self.actions, [
+            ('print', 'Getting and installing godeps'),
+            ('go', 'get', '-v', '-d', 'launchpad.net/godeps/...'),
+            ('go', 'install', 'launchpad.net/godeps/...'),
+            ('print', 'Cloning git.testing/project from'
+             ' https://git.testing/project'),
+            ('git', 'clone', 'https://git.testing/project',
+             '/tmp/fake/src/git.testing/project'),
+            ('chdir', '/tmp/fake/src/git.testing/project'),
+            ('print', 'Getting dependencies using godeps from'
+             ' /tmp/fake/src/git.testing/project/dependencies.tsv'),
+            ('/tmp/fake/bin/godeps', '-u',
+             '/tmp/fake/src/git.testing/project/dependencies.tsv'),
+            ('go', 'build', 'git.testing/project/...'),
+            ('go', 'test', 'git.testing/project/...')
+        ])
