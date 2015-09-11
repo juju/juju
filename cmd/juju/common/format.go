568723c36bbd2ed84674e5c7d961a0c1246af789
Cheryl Jennings <cheryl.jennings@canonical.com>
2015-09-11 14:43:16 -0500

    LP 1486254: Convert nested YAML maps
    Moved the conform() function from cmd/juju/action into the
    common directory for commands so that the system create-env
    command can also use it to convert nested maps into the proper
    map[string]interface{} format.

--- cmd/juju/common/format.go
+++ cmd/juju/common/format.go
@@ -3,7 +3,11 @@
 
 package common
 
-import "time"
+import (
+	"time"
+
+	"github.com/juju/errors"
+)
 
 // FormatTime returns a string with the local time formatted
 // in an arbitrary format used for status or and localized tz
@@ -20,4 +24,49 @@ func FormatTime(t *time.Time, formatISO bool) string {
 	}
 	// Otherwise use local time.
 	return t.Local().Format("02 Jan 2006 15:04:05Z07:00")
+}
+
+// ConformYAML ensures all keys of any nested maps are strings.  This is
+// necessary because YAML unmarshals map[interface{}]interface{} in nested
+// maps, which cannot be serialized by bson. Also, handle []interface{}.
+// cf. gopkg.in/juju/charm.v4/actions.go cleanse
+func ConformYAML(input interface{}) (interface{}, error) {
+	switch typedInput := input.(type) {
+
+	case map[string]interface{}:
+		newMap := make(map[string]interface{})
+		for key, value := range typedInput {
+			newValue, err := ConformYAML(value)
+			if err != nil {
+				return nil, err
+			}
+			newMap[key] = newValue
+		}
+		return newMap, nil
+
+	case map[interface{}]interface{}:
+		newMap := make(map[string]interface{})
+		for key, value := range typedInput {
+			typedKey, ok := key.(string)
+			if !ok {
+				return nil, errors.New("map keyed with non-string value")
+			}
+			newMap[typedKey] = value
+		}
+		return ConformYAML(newMap)
+
+	case []interface{}:
+		newSlice := make([]interface{}, len(typedInput))
+		for i, sliceValue := range typedInput {
+			newSliceValue, err := ConformYAML(sliceValue)
+			if err != nil {
+				return nil, errors.New("map keyed with non-string value")
+			}
+			newSlice[i] = newSliceValue
+		}
+		return newSlice, nil
+
+	default:
+		return input, nil
+	}
 }
