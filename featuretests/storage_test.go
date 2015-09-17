f23050ebf1f126b2883357f0e83790dfeb4b686f
Andrew Wilkins <axwalk@gmail.com>
2015-09-17 16:19:54 +0800

    featuretests: update storage test

--- featuretests/storage_test.go
+++ featuretests/storage_test.go
@@ -402,8 +402,8 @@ func (s *cmdStorageSuite) TestListVolumeTabularFilterMatch(c *gc.C) {
 	createUnitWithStorage(c, &s.JujuConnSuite, testPool)
 	context := runVolumeList(c, "0")
 	expected := `
-MACHINE  UNIT             STORAGE  DEVICE  VOLUME  ID  SIZE  STATE    MESSAGE
-0        storage-block/0  data/0           0/0               pending  
+MACHINE  UNIT             STORAGE  ID   PROVIDER-ID  DEVICE  SIZE  STATE    MESSAGE
+0        storage-block/0  data/0   0/0                             pending  
 
 `[1:]
 	c.Assert(testing.Stdout(context), gc.Equals, expected)
