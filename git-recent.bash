ac4f55352c2e633c5207bd3d1430366c4aac8d29
Curtis Hovey <curtis@canonical.com>
2015-09-18 12:18:15 -0500

    use PRIORITIES.

--- git-recent.bash
+++ git-recent.bash
@@ -10,4 +10,4 @@ RECENT=$(git for-each-ref \
     --format='gitbranch:%(refname:short):github.com/juju/juju' \
     --sort -committerdate --count 11 refs/remotes/origin/ | \
     sed -e '/HEAD/d; s,origin/,,' | sort --reverse | tr -s '\n' ' ')
-echo "$@ $RECENT"
+echo "$PRIORITIES $RECENT"
