0bc35e48d48a4e61420e902af5c17fea6159ab11
Aaron Bentley <aaron.bentley@canonical.com>
2015-09-23 12:24:25 -0400

    Support --region in assess_bootstrap.

--- assess_bootstrap.py
+++ assess_bootstrap.py
@@ -7,20 +7,27 @@ import os.path
 from jujupy import (
     EnvJujuClient,
     SimpleEnvironment,
+    temp_bootstrap_env,
     until_timeout,
     )
 from utility import scoped_environ
 
 
-def assess_bootstrap(juju, env, debug):
+def assess_bootstrap(juju, env, debug, region):
     with scoped_environ():
         juju_bin = os.path.dirname(os.path.abspath(juju))
         os.environ['PATH'] = '{}:{}'.format(juju_bin, os.environ['PATH'])
         client = EnvJujuClient.by_version(SimpleEnvironment.from_config(env),
                                           juju, debug)
-    client.destroy_environment()
+    if region is not None:
+        client.env.config['region'] = region
+    with temp_bootstrap_env(client.juju_home, client):
+        client.destroy_environment()
+        try:
+            client.bootstrap()
+        except:
+            client.destroy_environment()
     try:
-        client.bootstrap()
         for ignored in until_timeout(30):
             client.get_status(1)
         print('Environment successfully bootstrapped.')
@@ -34,6 +41,7 @@ def parse_args(argv=None):
     parser.add_argument('env', help="The environment to test with.")
     parser.add_argument('--debug', action="store_true", default=False,
                         help='Use --debug juju logging.')
+    parser.add_argument('--region', help='Override environment region.')
     return parser.parse_args(argv)
 
 
