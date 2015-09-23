7552d0ed8a0dba4232a659aeb707ad18d330c037
Aaron Bentley <aaron.bentley@canonical.com>
2015-09-23 12:58:09 -0400

    Test new args.

--- tests/test_assess_bootstrap.py
+++ tests/test_assess_bootstrap.py
@@ -1,16 +1,25 @@
-from assess_bootstrap import parse_args
-
+from argparse import Namespace
 from unittest import TestCase
+
+from assess_bootstrap import parse_args
 
 
 class TestParseArgs(TestCase):
 
     def test_parse_args(self):
         args = parse_args(['foo', 'bar'])
-        self.assertEqual(args.juju, 'foo')
-        self.assertEqual(args.env, 'bar')
-        self.assertEqual(args.debug, False)
+        self.assertEqual(args, Namespace(
+            juju='foo', env='bar', debug=False, region=None,
+            temp_env_name=None))
 
     def test_parse_args_debug(self):
         args = parse_args(['foo', 'bar', '--debug'])
         self.assertEqual(args.debug, True)
+
+    def test_parse_args_region(self):
+        args = parse_args(['foo', 'bar', '--region', 'foo'])
+        self.assertEqual(args.region, 'foo')
+
+    def test_parse_args_temp_env_name(self):
+        args = parse_args(['foo', 'bar', 'foo'])
+        self.assertEqual(args.temp_env_name, 'foo')
