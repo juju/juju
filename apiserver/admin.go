juju/juju

commit 5cec732d258e28e0c886ae64d631bf92dd45b4e2

apiserver: don't Kill presence if mongo is dead
When mongo is unavailable/unreachable, don't attempt
to "kill" the agent presence. Presence is informational,
and so updating it should not block connection termination
when Mongo is unreachable.

Fixes https://bugs.launchpad.net/juju-core/+bug/1497229

apiserver/admin.go
@@ -310,6 +310,7 @@ func checkForValidMachineAgent(entity state.Entity, req params.LoginRequest) err
 // machinePinger wraps a presence.Pinger.
 type machinePinger struct {
 	*presence.Pinger
+	mongoUnavailable *bool
 }
 
 // Stop implements Pinger.Stop() as Pinger.Kill(), needed at
@@ -318,7 +319,15 @@ func (p *machinePinger) Stop() error {
 	if err := p.Pinger.Stop(); err != nil {
 		return err
 	}
-	return p.Pinger.Kill()
+	if !*p.mongoUnavailable {
+		// Kill marks the agent as not-present. If the
+		// Mongo server is known to be unavailable, then
+		// we do not perform this operation; the agent
+		// will naturally become "not present" when its
+		// presence expires.
+		return p.Pinger.Kill()
+	}
+	return nil
 }
 
 func startPingerIfAgent(root *apiHandler, entity state.Entity) error {
@@ -336,7 +345,7 @@ func startPingerIfAgent(root *apiHandler, entity state.Entity) error {
 		return err
 	}
 
-	root.getResources().Register(&machinePinger{pinger})
+	root.getResources().Register(&machinePinger{pinger, root.mongoUnavailable})
 	action := func() {
 		if err := root.getRpcConn().Close(); err != nil {
 			logger.Errorf("error closing the RPC connection: %v", err)

apiserver/apiserver.go
@@ -48,6 +48,7 @@ type Server struct {
 	limiter           utils.Limiter
 	validator         LoginValidator
 	adminApiFactories map[int]adminApiFactory
+	mongoUnavailable  bool
 
 	mu          sync.Mutex // protects the fields that follow
 	environUUID string
@@ -310,6 +311,11 @@ func (srv *Server) run(lis net.Listener) {
 	srv.wg.Add(1)
 	go func() {
 		err := srv.mongoPinger()
+		// Before killing the tomb, inform the API handlers that
+		// Mongo is unavailable. API handlers can use this to decide
+		// not to perform non-critical Mongo-related operations when
+		// tearing down.
+		srv.mongoUnavailable = true
 		srv.tomb.Kill(err)
 		srv.wg.Done()
 	}()

apiserver/root.go
@@ -42,10 +42,11 @@ type objectKey struct {
 // after it has logged in. It contains an rpc.MethodFinder which it
 // uses to dispatch Api calls appropriately.
 type apiHandler struct {
-	state     *state.State
-	rpcConn   *rpc.Conn
-	resources *common.Resources
-	entity    state.Entity
+	state            *state.State
+	rpcConn          *rpc.Conn
+	resources        *common.Resources
+	entity           state.Entity
+	mongoUnavailable *bool
 	// An empty envUUID means that the user has logged in through the
 	// root of the API server rather than the /environment/:env-uuid/api
 	// path, logins processed with v2 or later will only offer the
@@ -58,10 +59,11 @@ var _ = (*apiHandler)(nil)
 // newApiHandler returns a new apiHandler.
 func newApiHandler(srv *Server, st *state.State, rpcConn *rpc.Conn, reqNotifier *requestNotifier, envUUID string) (*apiHandler, error) {
 	r := &apiHandler{
-		state:     st,
-		resources: common.NewResources(),
-		rpcConn:   rpcConn,
-		envUUID:   envUUID,
+		state:            st,
+		resources:        common.NewResources(),
+		rpcConn:          rpcConn,
+		envUUID:          envUUID,
+		mongoUnavailable: &srv.mongoUnavailable,
 	}
 	if err := r.resources.RegisterNamed("machineID", common.StringResource(srv.tag.Id())); err != nil {
 		return nil, errors.Trace(err)

commit 2f32cdfbd06dd7b3156c26ca9aa471bb0a544220

apiserver: fix data race in shutdown
Fixes LP 1500283.

Fixes data race in any test using mongo/state which was introduced in 5cec732d.

To make sure the reference to mongoUnavailable is visible to all threads we need
to use an atomic operation (or a lock). There is no operation that acts on a bool
in the standard library, so we use atomic.LoadUint32.

apiserver/admin.go
@@ -5,6 +5,7 @@ package apiserver
 
 import (
 	"sync"
+	"sync/atomic"
 	"time"
 
 	"github.com/juju/errors"
@@ -310,7 +311,7 @@ func checkForValidMachineAgent(entity state.Entity, req params.LoginRequest) err
 // machinePinger wraps a presence.Pinger.
 type machinePinger struct {
 	*presence.Pinger
-	mongoUnavailable *bool
+	mongoUnavailable *uint32
 }
 
 // Stop implements Pinger.Stop() as Pinger.Kill(), needed at
@@ -319,7 +320,7 @@ func (p *machinePinger) Stop() error {
 	if err := p.Pinger.Stop(); err != nil {
 		return err
 	}
-	if *p.mongoUnavailable {
+	if atomic.LoadUint32(p.mongoUnavailable) > 0 {
 		// Kill marks the agent as not-present. If the
 		// Mongo server is known to be unavailable, then
 		// we do not perform this operation; the agent

apiserver/apiserver.go
@@ -48,7 +48,7 @@ type Server struct {
 	limiter           utils.Limiter
 	validator         LoginValidator
 	adminApiFactories map[int]adminApiFactory
-	mongoUnavailable  bool
+	mongoUnavailable  uint32 // non zero if mongoUnavailable
 
 	mu          sync.Mutex // protects the fields that follow
 	environUUID string
@@ -315,7 +315,7 @@ func (srv *Server) run(lis net.Listener) {
 		// Mongo is unavailable. API handlers can use this to decide
 		// not to perform non-critical Mongo-related operations when
 		// tearing down.
-		srv.mongoUnavailable = true
+		atomic.AddUint32(&srv.mongoUnavailable, 1)
 		srv.tomb.Kill(err)
 		srv.wg.Done()
 	}()

apiserver/root.go
@@ -46,7 +46,7 @@ type apiHandler struct {
 	rpcConn          *rpc.Conn
 	resources        *common.Resources
 	entity           state.Entity
-	mongoUnavailable *bool
+	mongoUnavailable *uint32
 	// An empty envUUID means that the user has logged in through the
 	// root of the API server rather than the /environment/:env-uuid/api
 	// path, logins processed with v2 or later will only offer the