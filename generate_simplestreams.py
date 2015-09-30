f46a457eb0fa679d9b5699b1fe15eca0a9b883ec
Aaron Bentley <aaron.bentley@canonical.com>
2015-09-30 09:57:57 -0400

    Support juju-style index/index2.

--- generate_simplestreams.py
+++ generate_simplestreams.py
@@ -63,11 +63,37 @@ def items2content_trees(itemslist, exdata):
     return ctrees
 
 
-def write_streams(out_d, trees, updated, sign):
+class FileNamer:
+
     streamdir = 'streams/v1'
+
+    @classmethod
+    def get_index_path(cls):
+        return "%s/%s" % (cls.streamdir, 'index.json')
+
+    @classmethod
+    def get_content_path(cls, content_id):
+        return "%s/%s.json" % (cls.streamdir, content_id)
+
+
+class JujuFileNamer(FileNamer):
+
+    @classmethod
+    def get_index_path(cls):
+        return "%s/%s" % (cls.streamdir, 'index2.json')
+
+    @classmethod
+    def get_content_path(cls, content_id):
+        return "%s/%s.json" % (cls.streamdir, content_id.replace(':', '-'))
+
+
+def write_streams(out_d, trees, updated, namer=None):
+    if namer is None:
+        namer = FileNamer
+
     index = {"index": {}, 'format': 'index:1.0', 'updated': updated}
 
-    to_write = [("%s/%s" % (streamdir, 'index.json'), index,)]
+    to_write = [(namer.get_index_path(), index,)]
 
     not_copied_up = ['content_id']
     for content_id in trees:
@@ -75,8 +101,8 @@ def write_streams(out_d, trees, updated, sign):
                                sticky=['path', 'sha256', 'md5', 'size'])
         content = trees[content_id]
         index['index'][content_id] = {
-            'path': "%s/%s.json" % (streamdir, content_id),
             'products': list(content['products'].keys()),
+            'path': namer.get_content_path(content_id),
         }
         for k in util.stringitems(content):
             if k not in not_copied_up:
@@ -84,17 +110,13 @@ def write_streams(out_d, trees, updated, sign):
 
         to_write.append((index['index'][content_id]['path'], content,))
 
+    out_filenames = []
     for (outfile, data) in to_write:
         filef = os.path.join(out_d, outfile)
         util.mkdir_p(os.path.dirname(filef))
-        with open(filef, "w") as fp:
-            sys.stderr.write("writing %s\n" % filef)
-            fp.write(json.dumps(data, indent=2, sort_keys=True) + "\n")
-
-        if sign:
-            sys.stderr.write("signing %s\n" % filef)
-            toolutil.signjson_file(filef)
-
+        json_dump(data, filef)
+        out_filenames.append(filef)
+    return out_filenames
 
 def parse_args(argv=None):
     parser = ArgumentParser()
@@ -106,29 +128,39 @@ def parse_args(argv=None):
     return parser.parse_args(argv)
 
 
+def json_dump(data, filename):
+    with open(filename, "w") as fp:
+        sys.stderr.write("writing %s\n" % filename)
+        fp.write(json.dumps(data, indent=2, sort_keys=True) + "\n")
+
+
+def write_release_index(out_d):
+    in_path = os.path.join(out_d, JujuFileNamer.get_index_path())
+    with open(in_path) as in_file:
+        full_index = json.load(in_file)
+    full_index['index'] = dict(
+        (k, v) for k, v in list(full_index['index'].items())
+        if k == 'com.ubuntu.juju:released:tools')
+    out_path = os.path.join(out_d, FileNamer.get_index_path())
+    json_dump(full_index, out_path)
+    return out_path
+
+
 def main():
     args = parse_args()
-    items = [
-        ('com.ubuntu.juju:released:tools',
-         'com.ubuntu.juju:14.04:ppc64el',
-         '20150928',
-         '1.24-beta6-trusty-ppc64el', {
-             'arch': 'ppc64el',
-             'ftype': 'tar.gz',
-             'path': 'releases/juju-1.24-beta6-trusty-ppc64el.tgz',
-             'release': 'trusty',
-             'sha256': ('a5ee6a753eef008418992e3105857ea69be78e9565ab1'
-                        'f428203d2855b7f5522'),
-             'size': 15114526,
-             'version': '1.24-beta6',
-             }),
-    ]
     items = read_items_file(args.items_file)
     updated = util.timestamp()
 
     data = {'updated': updated, 'datatype': 'content-download'}
     trees = items2content_trees(items, data)
-    write_streams(args.out_d, trees, updated, False)
+    out_filenames = write_streams(args.out_d, trees, updated, JujuFileNamer)
+    out_filenames.append(write_release_index(args.out_d))
+    if False:
+        for filef in out_filenames:
+            sys.stderr.write("signing %s\n" % filef)
+            #toolutil.signjson_file(filef)
+
+
 
 
 if __name__ == '__main__':
