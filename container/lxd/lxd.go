0040223c793f611a66563473d5a76e6e40897a57
Eric Snow <ericsnowcurrently@gmail.com>
2015-10-01 12:53:20 -0600

    Clean up the LXD container manager code.

--- container/lxd/lxd.go
+++ container/lxd/lxd.go
@@ -16,127 +16,58 @@ import (
 	"github.com/juju/juju/instance"
 )
 
-// XXX: should we allow managing containers on other hosts? this is
+// TODO(ericsnow) Move this check to a test suite.
+var _ container.Manager = (*containerManager)(nil)
+
+// TODO: should we allow managing containers on other hosts? this is
 // functionality LXD gives us and from discussion juju would use eventually for
 // the local provider, so the APIs probably need to be changed to pass extra
 // args around. I'm punting for now.
+
+// containerManager is an implementation of container.Manager for LXD.
 type containerManager struct {
-	name string
+	namespace string
+	remote    string
 	// A cached client.
 	client *lxd.Client
 }
 
-// containerManager implements container.Manager.
-var _ container.Manager = (*containerManager)(nil)
+// NewContainerManager builds a new instance of container.Manager and
+// returns that new manager.
+func NewContainerManager(conf container.ManagerConfig) (container.Manager, error) {
+	namespace := conf.PopValue(container.ConfigName)
+	if namespace == "" {
+		return nil, errors.Errorf("namespace is required")
+	}
 
-func (manager *containerManager) connect() (*lxd.Client, error) {
+	remote := "" // localhost over unix socket
+	client, err := connect(remote)
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+
+	manager := &containerManager{
+		namespace: namespace,
+		remote:    remote,
+		client:    client,
+	}
+	return manager, nil
+}
+
+func connect(remote string) (*lxd.Client, error) {
 	// TODO: this is going to write the config in the user's home
 	// directory, which is (probably) not what we want long term. You can
 	// set where the config goes via the LXD API (although it is a bit
 	// obtuse), but I'm not sure what path we should use.
 	cfg, err := lxd.LoadConfig()
 	if err != nil {
-		return nil, err
+		return nil, errors.Trace(err)
 	}
 
-	// "" == localhost over unix socket
-	return lxd.NewClient(cfg, "")
+	return lxd.NewClient(cfg, remote)
 }
 
-func NewContainerManager(conf container.ManagerConfig) (container.Manager, error) {
-	name := conf.PopValue(container.ConfigName)
-	if name == "" {
-		return nil, errors.Errorf("name is required")
-	}
-
-	return &containerManager{name: name}, nil
-}
-
-func (manager *containerManager) CreateContainer(
-	instanceConfig *instancecfg.InstanceConfig,
-	series string,
-	networkConfig *container.NetworkConfig,
-	storageConfig *container.StorageConfig,
-) (inst instance.Instance, _ *instance.HardwareCharacteristics, err error) {
-	if manager.client == nil {
-		manager.client, err = manager.connect()
-		if err != nil {
-			return
-		}
-	}
-
-	name := names.NewMachineTag(instanceConfig.MachineId).String()
-	if manager.name != "" {
-		name = fmt.Sprintf("%s-%s", manager.name, name)
-	}
-
-	// TODO: FIXME: XXX: don't hardcode ubuntu
-	init, err := manager.client.Init(name, "", "ubuntu", nil, false)
-	if err != nil {
-		return
-	}
-
-	// Init is an async operation, since the tar -xvf (or whatever) might
-	// take a while; the result is an LXD operation id, which we can just
-	// wait on until it is finished.
-	if err = manager.client.WaitForSuccess(init.Operation); err != nil {
-		return
-	}
-
-	start, err := manager.client.Action(name, shared.Start, -1, false)
-	if err != nil {
-		// Try to clean up, but just do it async (i.e. don't
-		// WaitForSuccess) since we can't do much if this fails...
-		manager.client.Delete(name)
-		return
-	}
-
-	if err = manager.client.WaitForSuccess(start.Operation); err != nil {
-		return
-	}
-
-	inst = &lxdInstance{name, manager.client}
-	return
-}
-
-func (manager *containerManager) DestroyContainer(id instance.Id) error {
-	if manager.client == nil {
-		var err error
-		manager.client, err = manager.connect()
-		if err != nil {
-			return err
-		}
-	}
-
-	resp, err := manager.client.Delete(string(id))
-	if err != nil {
-		return err
-	}
-
-	return manager.client.WaitForSuccess(resp.Operation)
-}
-
-func (manager *containerManager) ListContainers() (result []instance.Instance, err error) {
-	result = []instance.Instance{}
-	if manager.client == nil {
-		manager.client, err = manager.connect()
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	containers, err := manager.client.ListContainers()
-	if err != nil {
-		return nil, err
-	}
-
-	for _, container := range containers {
-		result = append(result, &lxdInstance{container.State.Name, manager.client})
-	}
-
-	return result, nil
-}
-
+// IsInitialized implements container.Manager.
 func (manager *containerManager) IsInitialized() bool {
 	if manager.client != nil {
 		return true
@@ -144,7 +75,88 @@ func (manager *containerManager) IsInitialized() bool {
 
 	// NewClient does a roundtrip to the server to make sure it understands
 	// the versions, so all we need to do is connect above and we're done.
-	var err error
-	manager.client, err = manager.connect()
-	return err == nil
+	client, err := connect(manager.remote)
+	if err != nil {
+		return false
+	}
+
+	manager.client = client
+	return true
+}
+
+// ListContainers implements container.Manager.
+func (manager *containerManager) ListContainers() ([]instance.Instance, error) {
+	var result []instance.Instance
+
+	infos, err := manager.client.ListContainers()
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+
+	for _, info := range infos {
+		inst := &lxdInstance{
+			info.State.Name,
+			manager.client,
+		}
+		result = append(result, inst)
+	}
+
+	return result, nil
+}
+
+// CreateContainer implements container.Manager.
+func (manager *containerManager) CreateContainer(
+	instanceConfig *instancecfg.InstanceConfig,
+	series string,
+	networkConfig *container.NetworkConfig,
+	storageConfig *container.StorageConfig,
+) (instance.Instance, *instance.HardwareCharacteristics, error) {
+	name := names.NewMachineTag(instanceConfig.MachineId).String()
+	if manager.namespace != "" {
+		name = fmt.Sprintf("%s-%s", manager.namespace, name)
+	}
+	// TODO: FIXME: XXX: don't hardcode ubuntu
+	imageAlias := "ubuntu"
+	var profiles *[]string
+	ephemeral := false
+	resp, err := manager.client.Init(name, manager.remote, imageAlias, profiles, ephemeral)
+	if err != nil {
+		return nil, nil, errors.Trace(err)
+	}
+
+	// Init is an async operation, since the tar -xvf (or whatever) might
+	// take a while; the result is an LXD operation id, which we can just
+	// wait on until it is finished.
+	if err = manager.client.WaitForSuccess(resp.Operation); err != nil {
+		return nil, nil, errors.Trace(err)
+	}
+
+	resp, err = manager.client.Action(name, shared.Start, -1, false)
+	if err != nil {
+		// Try to clean up, but just do it async (i.e. don't
+		// WaitForSuccess) since we can't do much if this fails...
+		manager.client.Delete(name)
+		return nil, nil, errors.Trace(err)
+	}
+
+	if err = manager.client.WaitForSuccess(resp.Operation); err != nil {
+		return nil, nil, errors.Trace(err)
+	}
+
+	inst := &lxdInstance{
+		id:     name,
+		client: manager.client,
+	}
+	var hwc *instance.HardwareCharacteristics
+	return inst, hwc, nil
+}
+
+// DestroyContainer implements container.Manager.
+func (manager *containerManager) DestroyContainer(id instance.Id) error {
+	resp, err := manager.client.Delete(string(id))
+	if err != nil {
+		return errors.Trace(err)
+	}
+
+	return manager.client.WaitForSuccess(resp.Operation)
 }
