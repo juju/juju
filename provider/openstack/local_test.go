b0bd3d341039b0979e26ce046de145ed05c05ac7
anastasia <anastasia.macmood@canonical.com>
2015-10-02 00:12:30 +1000

    Added setting of default values.

--- provider/openstack/local_test.go
+++ provider/openstack/local_test.go
@@ -908,7 +908,7 @@ func (s *localServerSuite) TestFindImageBadDefaultImage(c *gc.C) {
 
 	// An error occurs if no suitable image is found.
 	_, err := openstack.FindInstanceSpec(env, "saucy", "amd64", "mem=1G")
-	c.Assert(err, gc.ErrorMatches, `no "saucy" images in some-region with arches \[amd64\]`)
+	c.Assert(err, gc.ErrorMatches, `image metadata for series \[saucy\], architectures \[amd64\] not found`)
 }
 
 func (s *localServerSuite) TestConstraintsValidator(c *gc.C) {
