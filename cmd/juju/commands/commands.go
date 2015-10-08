bb9d949b7f4f77eff3e11c8db06252cfb4812f7a
Eric Snow <ericsnowcurrently@gmail.com>
2015-10-08 11:44:16 -0600

    Add a way for components to register juju sub-commands.

---
+++ cmd/juju/commands/commands.go
@@ -0,0 +1,70 @@
+// Copyright 2015 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package commands
+
+import (
+	"github.com/juju/cmd"
+
+	"github.com/juju/juju/cmd/envcmd"
+)
+
+// TODO(ericsnow) Replace all this with a better registry mechanism,
+// likely over in the cmd repo.
+
+type commandRegistryItem struct {
+	newCommand    func() cmd.Command
+	newEnvCommand func() envcmd.EnvironCommand
+	//aliases []alias
+	//deprecated bool
+	//featureFlags []string
+}
+
+var registeredCommands []commandRegistryItem
+
+func registerCommand(item commandRegistryItem) {
+	registeredCommands = append(registeredCommands, item)
+}
+
+// RegisterCommand adds the provided func to the set of those that will
+// be called when the juju command runs. Each returned command will be
+// registered with the "juju" supercommand.
+func RegisterCommand(newCommand func() cmd.Command) {
+	item := commandRegistryItem{
+		newCommand: newCommand,
+	}
+	registerCommand(item)
+}
+
+// RegisterCommand adds the provided func to the set of those that will
+// be called when the juju command runs. Each returned command will be
+// wrapped in envCmdWrapper, which is what gets registered with the
+// "juju" supercommand.
+func RegisterEnvCommand(newCommand func() envcmd.EnvironCommand) {
+	item := commandRegistryItem{
+		newEnvCommand: newCommand,
+	}
+	registerCommand(item)
+}
+
+func (cri commandRegistryItem) command(ctx *cmd.Context) cmd.Command {
+	var command cmd.Command
+
+	switch {
+	case cri.newCommand != nil:
+		command = cri.newCommand()
+	case cri.newCommand != nil:
+		envCommand := cri.newEnvCommand()
+		command = envCmdWrapper{
+			Command: envcmd.Wrap(envCommand),
+			ctx:     ctx,
+		}
+	}
+
+	return command
+}
+
+func (cri commandRegistryItem) apply(r commandRegistry, ctx *cmd.Context) {
+	command := cri.command(ctx)
+	r.Register(command)
+}
