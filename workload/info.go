120cb846f2dd69fbbec753b2ad16aa0518641c3d
Eric Snow <ericsnowcurrently@gmail.com>
2015-10-08 18:36:08 -0600

    Add Payload filtering.

--- workload/info.go
+++ workload/info.go
@@ -119,3 +119,62 @@ func (info Info) AsPayload() Payload {
 		Tags:   tags,
 	}
 }
+
+// Filter applies the provided predicates to the payloads and returns
+// only those that matched.
+func Filter(payloads []Payload, predicates ...func(Payload) bool) []Payload {
+	var results []Payload
+	for _, payload := range payloads {
+		if matched := filterOne(payload, predicates); matched {
+			results = append(results, payload)
+		}
+	}
+	return results
+}
+
+func filterOne(payload Payload, predicates []func(Payload) bool) bool {
+	if len(predicates) == 0 {
+		return true
+	}
+
+	for _, pred := range predicates {
+		if matched := pred(payload); matched {
+			return true
+		}
+	}
+	return false
+}
+
+// TODO(ericsnow) ParseEntityFilters is mostly something that can be generalized...
+
+// BuildPredicatesFor converts the provided patterns into predicates
+// that may be passed to Filter.
+func BuildPredicatesFor(patterns []string) ([]func(Payload) bool, error) {
+	var predicates []func(Payload) bool
+	for _, pattern := range patterns {
+		predicates = append(predicates, func(payload Payload) bool {
+			switch {
+			case strings.ToLower(payload.Name) == strings.ToLower(pattern):
+				return true
+			case strings.ToLower(payload.Type) == strings.ToLower(pattern):
+				return true
+			case strings.ToLower(payload.ID) == strings.ToLower(pattern):
+				return true
+			case strings.ToLower(payload.Status) == strings.ToLower(pattern):
+				return true
+			case strings.ToLower(payload.Unit) == strings.ToLower(pattern):
+				return true
+			case strings.ToLower(payload.Machine) == strings.ToLower(pattern):
+				return true
+			default:
+				for _, tag := range payload.Tags {
+					if strings.ToLower(tag) == strings.ToLower(pattern) {
+						return true
+					}
+				}
+			}
+			return false
+		})
+	}
+	return predicates, nil
+}
