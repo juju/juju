f214b1d56df4cc4ac794de1b98efe9013defa8c5
Eric Snow <ericsnowcurrently@gmail.com>
2015-10-13 11:39:26 -0600

    Fix the docker import in tests.

--- workload/plugin/docker/client_info_test.go
+++ workload/plugin/docker/client_info_test.go
@@ -11,7 +11,7 @@ import (
 	jc "github.com/juju/testing/checkers"
 	gc "gopkg.in/check.v1"
 
-	"github.com/juju/juju-process-docker/docker"
+	"github.com/juju/juju/workload/plugin/docker"
 )
 
 var _ = gc.Suite(&infoSuite{})
