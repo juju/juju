183a1e80a02ebdc55bb83e86b4fcb3c813626dd8
Seman Said <seman.said@canonical.com>
2015-10-19 16:12:30 -0700

    Removed PURPOSE concept from the sign_metadata script.

--- sign_metadata.py
+++ sign_metadata.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 from argparse import ArgumentParser
 from os import listdir
 from os.path import (
@@ -11,30 +13,24 @@ from tempfile import NamedTemporaryFile
 from lprelease import run
 
 
-def sign_metadata(signing_key, dest_dist, purpose, juju_dist=None,
-                  signing_passphrase_file=None):
+def sign_metadata(signing_key, meta_dir, signing_passphrase_file=None):
     key_option, gpg_options = get_gpg_options(
         signing_key, signing_passphrase_file)
-    dists = [dest_dist]
-    if purpose in ('released', 'weekly', 'testing'):
-        dists.append(juju_dist)
-    for dist in dists:
-        meta_dir, meta_files = get_meta_files(dist)
-        for meta_file in meta_files:
-            with NamedTemporaryFile() as temp_file:
-                meta_file_path = join(meta_dir, meta_file)
-                update_file_content(meta_file_path, temp_file.name)
-                meta_basename = basename(meta_file).replace('.json', '.sjson')
-                output_file = join(meta_dir, meta_basename)
-                ensure_no_file(output_file)
-                cmd = 'gpg {} --clearsign {} -o {} {}'.format(
-                    gpg_options, key_option, output_file, temp_file.name)
-                run(cmd.split())
-                output_file = join(meta_dir, '{}.gpg'.format(meta_file))
-                ensure_no_file(output_file)
-                cmd = 'gpg {} --detach-sign {} -o {} {}'.format(
-                    gpg_options, key_option, output_file, temp_file.name)
-                run(cmd.split())
+    for meta_file in get_meta_files(meta_dir):
+        with NamedTemporaryFile() as temp_file:
+            meta_file_path = join(meta_dir, meta_file)
+            update_file_content(meta_file_path, temp_file.name)
+            meta_basename = basename(meta_file).replace('.json', '.sjson')
+            output_file = join(meta_dir, meta_basename)
+            ensure_no_file(output_file)
+            cmd = 'gpg {} --clearsign {} -o {} {}'.format(
+                gpg_options, key_option, output_file, temp_file.name)
+            run(cmd.split())
+            output_file = join(meta_dir, '{}.gpg'.format(meta_file))
+            ensure_no_file(output_file)
+            cmd = 'gpg {} --detach-sign {} -o {} {}'.format(
+                gpg_options, key_option, output_file, meta_file_path)
+            run(cmd.split())
 
 
 def update_file_content(mete_file, dst_file):
@@ -52,10 +48,11 @@ def get_gpg_options(signing_key, signing_passphrase_file=None):
     return key_option, gpg_options
 
 
-def get_meta_files(dist):
-    meta_dir = join(dist, 'tools', 'streams', 'v1')
-    meta_files = listdir(meta_dir)
-    return meta_dir, filter(lambda x: x.endswith(".json"),  meta_files)
+def get_meta_files(meta_dir):
+    meta_files = filter(lambda x: x.endswith(".json"),  listdir(meta_dir))
+    if not meta_files:
+        print('Warning! no meta files found in {}'.format(meta_dir))
+    return meta_files
 
 
 def ensure_no_file(file_path):
@@ -65,17 +62,14 @@ def ensure_no_file(file_path):
 
 def parse_args(argv=None):
     parser = ArgumentParser("Sign streams' meta files.")
-    parser.add_argument(
-        'purpose', help='Purpose: devel, proposed, released,  testing, weekly',
-        choices=['devel', 'proposed', 'released',  'testing', 'weekly'])
-    parser.add_argument('stream_dir', help='Stream directory.')
+    parser.add_argument('metadata_dir', help='Metadata directory.')
     parser.add_argument('signing_key', help='Name to sign with.')
     parser.add_argument(
         '-p', '--signing-passphrase-file', help='Signing passphrase file path.')
     args = parser.parse_args(argv)
-    if not isdir(args.stream_dir):
+    if not isdir(args.metadata_dir):
         parser.error(
-            'Invalid stream directory path {}'.format(args.stream_dir))
+            'Invalid metadata directory path {}'.format(args.metadata_dir))
     if (args.signing_passphrase_file and
             not isfile(args.signing_passphrase_file)):
         parser.error(
@@ -84,15 +78,7 @@ def parse_args(argv=None):
     return args
 
 
-def main(args):
-    juju_dist = join(args.stream_dir, 'juju-dist')
-    dest_dist = join(args.stream_dir, 'juju-dist', args.purpose)
-    if args.purpose == 'released':
-        dest_dist = join(args.stream_dir, 'juju-dist')
-    sign_metadata(args.signing_key, dest_dist, args.purpose, juju_dist,
-                  args.signing_passphrase_file)
-
-
 if __name__ == '__main__':
     args = parse_args()
-    main(args)
+    sign_metadata(args.signing_key, args.metadata_dir,
+                  args.signing_passphrase_file)
--- tests/test_sign_metadata.py
+++ tests/test_sign_metadata.py
@@ -1,6 +1,5 @@
 from argparse import Namespace
 from contextlib import contextmanager
-from os import makedirs
 from os.path import(
     join,
     isfile,
@@ -18,7 +17,6 @@ from mock import (
 from sign_metadata import (
     get_gpg_options,
     get_meta_files,
-    main,
     sign_metadata,
     parse_args,
     update_file_content,
@@ -48,18 +46,16 @@ class TestSignMetaData(TestCase):
          })
 
     def test_get_meta_files(self):
-        with temp_dir() as dest_dist:
-            dir_path = join(dest_dist, 'tools', 'streams', 'v1')
-            makedirs(dir_path)
-            file1 = join(dir_path, "index.json")
-            file2 = join(dir_path, "proposed-tools.json")
-            file3 = join(dir_path, "random.txt")
+        with temp_dir() as meta_dir:
+            file1 = join(meta_dir, "index.json")
+            file2 = join(meta_dir, "proposed-tools.json")
+            file3 = join(meta_dir, "random.txt")
             open(file1, 'w').close()
             open(file2, 'w').close()
             open(file3, 'w').close()
-            meta_files = get_meta_files(dest_dist)
+            meta_files = get_meta_files(meta_dir)
             self.assertItemsEqual(
-                meta_files, (dir_path, ['index.json', 'proposed-tools.json']))
+                meta_files, ['index.json', 'proposed-tools.json'])
 
     def test_gpg_options(self):
         output = get_gpg_options('thedude@example.com')
@@ -84,60 +80,47 @@ class TestSignMetaData(TestCase):
     def test_sign_metadata(self):
         with patch('sign_metadata.run', autospec=True,
                    side_effect=self.my_gpg) as smr:
-            with temp_dir() as stream_dir:
-                dest_dist = join(stream_dir, 'juju-dist', 'devel')
-                meta_dir = join(dest_dist, 'tools', 'streams', 'v1')
-                makedirs(meta_dir)
+            with temp_dir() as meta_dir:
                 meta_file = join(meta_dir, 'index.json')
                 write_file(meta_file, self.content)
                 with NamedTemporaryFile() as temp_file:
                     with patch('sign_metadata.NamedTemporaryFile',
                                autospec=True, return_value=temp_file) as ntf:
-                        sign_metadata('thedude@example.com', dest_dist, 'devel')
+                        sign_metadata('thedude@example.com', meta_dir)
                         self.verify_signed_content(meta_dir)
-        self.verify_calls(smr.mock_calls, meta_file, temp_file.name)
-        ntf.assert_called_once_with()
-
-    def test_sign_metadata_weekly(self):
-        with patch('sign_metadata.run', autospec=True,
-                   side_effect=self.my_gpg) as smr:
-            with temp_dir() as stream_dir:
-                dest_dist = join(stream_dir, 'juju-dist', 'weekly')
-                juju_dist = join(stream_dir, 'juju-dist')
-                dest_dir = join(dest_dist, 'tools', 'streams', 'v1')
-                dist_dir = join(juju_dist, 'tools', 'streams', 'v1')
-                makedirs(dest_dir)
-                makedirs(dist_dir)
-                dest_meta_file = join(dest_dir, 'index.json')
-                juju_dist_file = join(dist_dir, 'index.json')
-                write_file(dest_meta_file, self.content)
-                write_file(juju_dist_file, self.content)
-                with NamedTemporaryFile() as temp_file:
-                    with NamedTemporaryFile() as temp_file2:
-                        with patch('sign_metadata.NamedTemporaryFile',
-                                   autospec=True,
-                                   side_effect=[temp_file, temp_file2]):
-                            sign_metadata('thedude@example.com', dest_dist,
-                                          'weekly', juju_dist=juju_dist)
-                            self.verify_signed_content(dest_dir)
-                            self.verify_signed_content(dist_dir)
-        dest_signed_file = dest_meta_file.replace('.json', '.sjson')
-        dest_gpg_file = '{}.gpg'.format(dest_meta_file)
-        juju_signed_file = juju_dist_file.replace('.json', '.sjson')
-        juju_gpg_file = '{}.gpg'.format(juju_dist_file)
+        signed_file = meta_file.replace('.json', '.sjson')
+        gpg_file = '{}.gpg'.format(meta_file)
         calls = [
             call(['gpg', '--no-tty', '--clearsign', '--default-key',
-                  'thedude@example.com', '-o',  dest_signed_file,
-                  temp_file.name]),
+                  'thedude@example.com', '-o', signed_file, temp_file.name]),
             call(['gpg', '--no-tty', '--detach-sign', '--default-key',
-                  'thedude@example.com', '-o', dest_gpg_file, temp_file.name]),
-            call(['gpg', '--no-tty', '--clearsign', '--default-key',
-                  'thedude@example.com', '-o', juju_signed_file,
-                  temp_file2.name]),
-            call(['gpg', '--no-tty', '--detach-sign', '--default-key',
-                  'thedude@example.com', '-o', juju_gpg_file,
-                  temp_file2.name])]
+                  'thedude@example.com', '-o', gpg_file, meta_file])]
         self.assertEqual(smr.mock_calls, calls)
+        ntf.assert_called_once_with()
+
+    def test_sign_metadata_signing_passphrase_file(self):
+        with patch('sign_metadata.run', autospec=True,
+                   side_effect=self.my_gpg) as smr:
+            with temp_dir() as meta_dir:
+                meta_file = join(meta_dir, 'index.json')
+                write_file(meta_file, self.content)
+                with NamedTemporaryFile() as temp_file:
+                    with patch('sign_metadata.NamedTemporaryFile',
+                               autospec=True, return_value=temp_file) as ntf:
+                        sign_metadata(
+                            'thedude@example.com', meta_dir, 'passphrase_file')
+                        self.verify_signed_content(meta_dir)
+        signed_file = meta_file.replace('.json', '.sjson')
+        gpg_file = '{}.gpg'.format(meta_file)
+        calls = [
+            call(['gpg', '--no-use-agent', '--no-tty', '--passphrase-file',
+                  'passphrase_file', '--clearsign', '--default-key',
+                  'thedude@example.com', '-o', signed_file, temp_file.name]),
+            call(['gpg', '--no-use-agent', '--no-tty', '--passphrase-file',
+                  'passphrase_file', '--detach-sign', '--default-key',
+                  'thedude@example.com', '-o', gpg_file, meta_file])]
+        self.assertEqual(smr.mock_calls, calls)
+        ntf.assert_called_once_with()
 
     def verify_signed_content(self, meta_dir):
         file_path = join(meta_dir, 'index.sjson')
@@ -148,16 +131,6 @@ class TestSignMetaData(TestCase):
         self.assertTrue(
             isfile(join(meta_dir, 'index.json.gpg')))
 
-    def verify_calls(self, mock_calls, meta_file, temp_file):
-        signed_file = meta_file.replace('.json', '.sjson')
-        gpg_file = '{}.gpg'.format(meta_file)
-        calls = [
-            call(['gpg', '--no-tty', '--clearsign', '--default-key',
-                  'thedude@example.com', '-o', signed_file, temp_file]),
-            call(['gpg', '--no-tty', '--detach-sign', '--default-key',
-                  'thedude@example.com', '-o', gpg_file, temp_file])]
-        self.assertEqual(mock_calls, calls)
-
     def gpg_header(self):
         return '-----BEGIN PGP SIGNED MESSAGE-----\nHash: SHA1\n'
 
@@ -165,82 +138,51 @@ class TestSignMetaData(TestCase):
         return ('\n-----BEGIN PGP SIGNATURE-----\nblah blah\n'
                 '-----END PGP SIGNATURE-----')
 
-    def my_gpg(self, arg):
-        output_file = arg[6]
-        input_file = arg[7]
-        if '--clearsign' in arg:
+    def my_gpg(self, args):
+        output_file = args[6]
+        input_file = args[7]
+        if '--passphrase-file' in args:
+            output_file = args[9]
+            input_file = args[10]
+        if '--clearsign' in args:
             with open(input_file) as i:
                 with open(output_file, 'w') as o:
                     content = i.read()
                     o.write('{}{}{}'.format(
                         self.gpg_header(), content, self.gpg_footer()))
-        if '--detach-sign' in arg:
+        if '--detach-sign' in args:
             open(output_file, 'w').close()
 
     def test_parse_args_default(self):
-        with temp_dir() as stream_dir:
-            args = parse_args(['devel', stream_dir, 's_key'])
+        with temp_dir() as metadata_dir:
+            args = parse_args([metadata_dir, 's_key'])
         self.assertEqual(args, Namespace(
-            purpose='devel', signing_key='s_key', signing_passphrase_file=None,
-            stream_dir=stream_dir))
+            signing_key='s_key', signing_passphrase_file=None,
+            metadata_dir=metadata_dir))
 
-    def test_parse_args_set_options(self):
-        with temp_dir() as stream_dir:
+    def test_parse_args_signing_passphrase_file(self):
+        with temp_dir() as metadata_dir:
             with NamedTemporaryFile() as pass_file:
-                args = parse_args(['devel', stream_dir, 's_key',
+                args = parse_args([metadata_dir, 's_key',
                                    '--signing-passphrase-file', pass_file.name])
         self.assertEqual(args, Namespace(
-            purpose='devel', signing_key='s_key',
-            signing_passphrase_file=pass_file.name, stream_dir=stream_dir))
+            signing_key='s_key', signing_passphrase_file=pass_file.name,
+            metadata_dir=metadata_dir))
 
     def test_parse_args_error(self):
         with parse_error(self) as stderr:
-            parse_args()
-        self.assertIn(
-            "choose from 'devel', 'proposed', 'released', 'testing', 'weekly'",
-            stderr.getvalue())
-
-        with parse_error(self) as stderr:
-            parse_args(['proposed'])
+            parse_args([])
         self.assertIn("error: too few arguments", stderr.getvalue())
 
         with parse_error(self) as stderr:
-            parse_args(['proposed', 'stream_dir', 'signing_key'])
-        self.assertIn("Invalid stream directory path", stderr.getvalue())
+            parse_args(['metadata_dir', 'signing_key'])
+        self.assertIn("Invalid metadata directory path", stderr.getvalue())
 
         with parse_error(self) as stderr:
-            with temp_dir() as stream_dir:
-                parse_args(['devel', stream_dir, 's_key',
-                            '--signing-passphrase-file', 'no/file/'])
+            with temp_dir() as metadata_dir:
+                parse_args([metadata_dir, 's_key', '--signing-passphrase-file',
+                            'fake/file/'])
         self.assertIn("Invalid passphrase file path ", stderr.getvalue())
-
-    def test_main(self):
-        with patch('sign_metadata.sign_metadata', autospec=True) as sm:
-            with temp_dir() as stream_dir:
-                with NamedTemporaryFile() as pass_file:
-                    args = Namespace(
-                        purpose='devel', signing_key='s_key',
-                        signing_passphrase_file=pass_file.name,
-                        stream_dir=stream_dir)
-                    main(args)
-        dest_dist = join(stream_dir, 'juju-dist', 'devel')
-        juju_dist = join(stream_dir, 'juju-dist')
-        sm.assert_called_once_with(
-            's_key', dest_dist, 'devel', juju_dist,  pass_file.name)
-
-    def test_main_released(self):
-        with patch('sign_metadata.sign_metadata', autospec=True) as sm:
-            with temp_dir() as stream_dir:
-                with NamedTemporaryFile() as pass_file:
-                    args = Namespace(
-                        purpose='released', signing_key='s_key',
-                        signing_passphrase_file=pass_file.name,
-                        stream_dir=stream_dir)
-                    main(args)
-        dest_dist = join(stream_dir, 'juju-dist')
-        juju_dist = join(stream_dir, 'juju-dist')
-        sm.assert_called_once_with(
-            's_key', dest_dist, 'released', juju_dist,  pass_file.name)
 
 
 @contextmanager
