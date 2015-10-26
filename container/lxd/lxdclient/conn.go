b36423bc51b9ce4c8829fc005251f0ba94a00311
Eric Snow <ericsnowcurrently@gmail.com>
2015-10-26 13:12:58 -0600

    Always apply the config before connecting.

--- container/lxd/lxdclient/conn.go
+++ container/lxd/lxdclient/conn.go
@@ -17,6 +17,10 @@ type Client struct {
 // Connect opens an API connection to LXD and returns a high-level
 // Client wrapper around that connection.
 func Connect(cfg Config) (*Client, error) {
+	if err := cfg.Apply(); err != nil {
+		return nil, errors.Trace(err)
+	}
+
 	raw, err := newRawClient(cfg.Remote)
 	if err != nil {
 		return nil, errors.Trace(err)
