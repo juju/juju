680c0baaac86236ec9b0f851c1a240c070a46ae4
Eric Snow <ericsnowcurrently@gmail.com>
2015-10-28 16:53:34 -0600

    Use Certificate.Name.

--- container/lxd/lxdclient/conn.go
+++ container/lxd/lxdclient/conn.go
@@ -6,7 +6,6 @@ package lxdclient
 import (
 	"github.com/juju/errors"
 	"github.com/lxc/lxd"
-	"github.com/lxc/lxd/shared"
 )
 
 // Client is a high-level wrapper around the LXD API client.
@@ -76,9 +75,7 @@ func prepareRemote(cfg Config, newCert Cert) error {
 		return errors.Trace(err)
 	}
 
-	name, _ := shared.SplitExt(cfg.Filename) // TODO(ericsnow) Is this right?
-	name = "juju-" + name
-	if err := client.AddCert(newCert, name); err != nil {
+	if err := client.AddCert(newCert); err != nil {
 		return errors.Trace(err)
 	}
 
