c1ac2f5e056b8d3927c912fccbc4761433b1aba8
Wayne Witzel III <wayne@riotousliving.com>
2015-10-28 17:41:24 -0400

    added profile methods to lxdclient

--- container/lxd/lxdclient/conn.go
+++ container/lxd/lxdclient/conn.go
@@ -14,6 +14,7 @@ import (
 type Client struct {
 	*serverConfigClient
 	*certClient
+	*profileClient
 
 	raw       rawClientWrapper
 	namespace string
@@ -38,6 +39,7 @@ func Connect(cfg Config) (*Client, error) {
 	conn := &Client{
 		serverConfigClient: &serverConfigClient{raw},
 		certClient:         &certClient{raw},
+		profileClient:      &profileClient{raw},
 
 		raw:       raw,
 		namespace: cfg.Namespace,
