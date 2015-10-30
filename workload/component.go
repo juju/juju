656de8146c018fde9e6b49e5e235c8bea60c8cb4
Eric Snow <ericsnowcurrently@gmail.com>
2015-10-30 12:30:41 -0600

    Rename workload.go to component.go.

--- workload/component.go
+++ workload/component.go
@@ -8,15 +8,3 @@ package workload
 
 // ComponentName is the name of the Juju component for workload management.
 const ComponentName = "payloads"
-
-// Result is a struct that ties an error to a workload ID.
-type Result struct {
-	// ID is the ID of the workload that this result applies to.
-	ID string
-	// Payload holds the info about the workload, if available.
-	Payload *FullPayloadInfo
-	// NotFound indicates that the workload was not found in Juju.
-	NotFound bool
-	// Error is the error associated with this result (if any).
-	Error error
-}
