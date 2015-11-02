84495226d2ea26aecda0f5d1f0a4d47a4e5a5311
Eric Snow <ericsnowcurrently@gmail.com>
2015-11-02 11:40:20 -0700

    Fix the error message in the not-found case.

--- payload/api/internal/client/unitfacade.go
+++ payload/api/internal/client/unitfacade.go
@@ -115,7 +115,7 @@ func (c UnitFacadeClient) lookUp(fullIDs []string) ([]string, error) {
 
 	var ids []string
 	for _, result := range results {
-		if result.Error != nil && !result.NotFound {
+		if result.Error != nil {
 			// TODO(ericsnow) Do not short-circuit?
 			return nil, errors.Annotate(result.Error, "while looking up IDs")
 		}
