20e71f407ad9bdbc1341509d78e0974327fb2224
Eric Snow <ericsnowcurrently@gmail.com>
2015-11-03 19:28:00 -0700

    Make fixAddr() more strict.

--- container/lxd/lxdclient/addserver.go
+++ container/lxd/lxdclient/addserver.go
@@ -7,23 +7,17 @@ import (
 	"fmt"
 	"net"
 	"net/url"
+	"strconv"
 	"strings"
 
+	"github.com/juju/errors"
 	"github.com/lxc/lxd"
 	"github.com/lxc/lxd/shared"
 )
 
-// TODO(ericsnow) This file should be removed as soon as possible.
-// It is based on https://github.com/lxc/lxd/blob/master/lxc/remote.go
-// and the code there should instead be cleaned up and exported so we
-// could use it here.
-
-// TODO(ericsnow) Address licensing.
-
 // addServer adds the given remote info to the provided config.
-// The implementation is derived from:
+// The implementation is based loosely on:
 //  https://github.com/lxc/lxd/blob/master/lxc/remote.go
-// Note that we've removed some validation code (we don't need it).
 func addServer(config *lxd.Config, server string, addr string) error {
 	addr, err := fixAddr(addr, shared.PathExists)
 	if err != nil {
@@ -41,80 +35,180 @@ func addServer(config *lxd.Config, server string, addr string) error {
 	return nil
 }
 
-// fixAddr is the portion of the original addServer function that fixes
-// up the provided addr to be a fully qualified URL.
 func fixAddr(addr string, pathExists func(string) bool) (string, error) {
-	if addr == "" {
+	if addr == "" || addr == "unix://" {
 		// TODO(ericsnow) Return lxd.LocalRemote.Addr?
 		return addr, nil
 	}
 
+	if strings.HasPrefix(addr, ":") {
+		parts := strings.SplitN(addr, "/", 2)
+		if net.ParseIP(parts[0]) != nil {
+			addr = fmt.Sprintf("[%s]", parts[0])
+			if len(parts) == 2 {
+				addr = "/" + parts[1]
+			}
+		}
+	}
+
+	if strings.HasPrefix(addr, "//") {
+		addr = addr[2:]
+	} else if strings.HasPrefix(addr, "unix://") {
+		addr = addr[len("unix://"):]
+	}
+
 	parsedURL, err := url.Parse(addr)
 	if err != nil {
-		return "", err
+		return "", errors.Trace(err)
 	}
+	if parsedURL.RawQuery != "" {
+		return "", errors.NewNotValid(nil, fmt.Sprintf("URL queries not supported (got %q)", addr))
+	}
+	if parsedURL.Fragment != "" {
+		return "", errors.NewNotValid(nil, fmt.Sprintf("URL fragments not supported (got %q)", addr))
+	}
+	if parsedURL.Opaque != "" {
+		if strings.Contains(parsedURL.Scheme, ".") {
+			addr, err := fixAddr("https://"+addr, pathExists)
+			if err != nil {
+				return "", errors.Trace(err)
+			}
+			return addr, nil
+		}
+		return "", errors.NewNotValid(nil, fmt.Sprintf("opaque URLs not supported (got %q)", addr))
+	}
+
 	remoteURL := url.URL{
 		Scheme: parsedURL.Scheme,
 		Host:   parsedURL.Host,
-		//Path:   parsedURL.Path,
+		Path:   strings.TrimRight(parsedURL.Path, "/"),
 	}
 
 	// Fix the scheme.
-	switch remoteURL.Scheme {
-	case "https":
-	case "unix":
-	case "":
-		// TODO(ericsnow) Decided based on whether or not Host is set?
-		remoteURL.Scheme = "https"
-		// TODO(ericsnow) Use remoteURL.Path/.Opaque here instead of addr.
-		if addr[0] == '/' || pathExists(addr) {
-			remoteURL.Scheme = "unix"
-		}
-	default:
-		// TODO(ericsnow) Fail by default?
-		remoteURL.Scheme = "https"
+	remoteURL.Scheme = fixScheme(remoteURL, pathExists)
+	if err := validateScheme(remoteURL); err != nil {
+		return "", errors.Trace(err)
 	}
 
 	// Fix the host.
-	if remoteURL.Host == "" {
-		// TODO(ericsnow) Instead, make use of Path and Opaque below.
-		remoteURL.Host = addr
-	}
-	host, port, err := net.SplitHostPort(remoteURL.Host)
-	if err != nil {
-		port = shared.DefaultPort
-	} else {
-		remoteURL.Host = host
-	}
-
-	// Special-case "unix".
 	if remoteURL.Scheme == "unix" {
-		// TODO(ericsnow) All this could be done earlier.
-		// TODO(ericsnow) Use Path and Opaque instead of addr.
-		if addr[0:5] == "unix:" {
-			if len(addr) > 6 && addr[0:7] == "unix://" {
-				if len(addr) > 8 {
-					remoteURL.Host = addr[8:]
-				} else {
-					remoteURL.Host = ""
-				}
-			} else {
-				remoteURL.Host = addr[6:]
-			}
+		if remoteURL.Host != "" {
+			return "", errors.NewNotValid(nil, fmt.Sprintf("invalid unix socket URL (got %q)", addr))
 		}
-		port = ""
+		remoteURL.Host = remoteURL.Path
+		remoteURL.Path = ""
+	} else {
+		if remoteURL.Host == "" {
+			addr = fmt.Sprintf("%s://%s%s", remoteURL.Scheme, remoteURL.Host, remoteURL.Path)
+			addr, err := fixAddr(addr, pathExists)
+			if err != nil {
+				return "", errors.Trace(err)
+			}
+			return addr, nil
+		}
+		remoteURL.Host = fixHost(remoteURL.Host, shared.DefaultPort)
+		if err := validateHost(remoteURL); err != nil {
+			return "", errors.Trace(err)
+		}
 	}
 
-	// Handle IPv6 hosts.
-	if strings.Contains(remoteURL.Host, ":") && !strings.HasPrefix(remoteURL.Host, "[") {
-		remoteURL.Host = fmt.Sprintf("[%s]", remoteURL.Host)
-	}
-
-	// Add the port, if applicable.
-	if port != "" {
-		remoteURL.Host += ":" + port
+	// Do some final validation.
+	if remoteURL.Scheme == "unix" {
+		if !strings.HasPrefix(remoteURL.Host, "/") {
+			return "", errors.NewNotValid(nil, fmt.Sprintf("relative unix socket paths not supported (got %q)", addr))
+		}
+		if !pathExists(remoteURL.Host) {
+			return "", errors.NewNotValid(nil, fmt.Sprintf("unix socket file %q not found", remoteURL.Host))
+		}
 	}
 
 	// TODO(ericsnow) Use remoteUrl.String()
-	return fmt.Sprintf("%s://%s", remoteURL.Scheme, remoteURL.Host), nil
+	return fmt.Sprintf("%s://%s%s", remoteURL.Scheme, remoteURL.Host, remoteURL.Path), nil
+}
+
+func fixScheme(url url.URL, pathExists func(string) bool) string {
+	switch url.Scheme {
+	case "unix":
+		return url.Scheme
+	case "https":
+		return url.Scheme
+	case "http":
+		return "https"
+	case "":
+		if url.Host != "" {
+			return "https"
+		}
+		if strings.HasPrefix(url.Path, "/") {
+			return "unix"
+		}
+		if pathExists(url.Path) {
+			return "unix"
+		}
+		return "https"
+	default:
+		return url.Scheme
+	}
+}
+
+func validateScheme(url url.URL) error {
+	switch url.Scheme {
+	case "unix":
+	case "https":
+	case "http":
+	default:
+		return errors.NewNotValid(nil, fmt.Sprintf("unsupported URL scheme %q", url.Scheme))
+	}
+	return nil
+}
+
+func fixHost(host, defaultPort string) string {
+	// Handle IPv6 hosts.
+	if strings.Count(host, ":") > 1 {
+		if !strings.HasPrefix(host, "[") {
+			return fmt.Sprintf("[%s]:%s", host, defaultPort)
+		} else if !strings.Contains(host, "]:") {
+			return host + ":" + defaultPort
+		}
+		return host
+	}
+
+	// Handle ports.
+	if !strings.Contains(host, ":") {
+		return host + ":" + defaultPort
+	}
+
+	return host
+}
+
+func validateHost(url url.URL) error {
+	if url.Host == "" {
+		return errors.NewNotValid(nil, "URL missing host")
+	}
+
+	host, port, err := net.SplitHostPort(url.Host)
+	if err != nil {
+		return errors.NewNotValid(err, "")
+	}
+
+	// Check the host.
+	if net.ParseIP(host) == nil {
+		if err := validateDomainName(host); err != nil {
+			return errors.Trace(err)
+		}
+	}
+
+	// Check the port.
+	if p, err := strconv.Atoi(port); err != nil {
+		return errors.NewNotValid(err, fmt.Sprintf("invalid port in host %q", url.Host))
+	} else if p <= 0 || p > 0xFFFF {
+		return errors.NewNotValid(err, fmt.Sprintf("invalid port in host %q", url.Host))
+	}
+
+	return nil
+}
+
+func validateDomainName(fqdn string) error {
+	// TODO(ericsnow) Do checks for a valid domain name.
+
+	return nil
 }
