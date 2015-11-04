25acaea2d082d205706ba9ebd06772d6bde493d3
Eric Snow <ericsnowcurrently@gmail.com>
2015-11-03 19:28:00 -0700

    Make fixAddr() more strict.

--- container/lxd/lxdclient/addserver_test.go
+++ container/lxd/lxdclient/addserver_test.go
@@ -30,26 +30,59 @@ var typicalAddrTests = []addrTest{{
 	addr:     "",
 	expected: "",
 }, {
-	addr:     "https://x.y.z",
-	expected: "https://x.y.z:8443",
+	addr:     "unix://",
+	expected: "unix://",
 }, {
-	addr:     "https://x.y.z/",
-	expected: "https://x.y.z:8443",
+	addr:     "a.b.c",
+	exists:   false,
+	expected: "https://a.b.c:8443",
 }, {
-	addr:     "https://x.y.z:1234",
-	expected: "https://x.y.z:1234",
+	addr:     "https://a.b.c",
+	expected: "https://a.b.c:8443",
 }, {
-	addr:     "http://x.y.z",
-	expected: "https://x.y.z:8443",
+	addr:     "https://a.b.c/",
+	expected: "https://a.b.c:8443",
+}, {
+	addr:     "a.b.c:1234",
+	exists:   false,
+	expected: "https://a.b.c:1234",
+}, {
+	addr:     "https://a.b.c:1234",
+	expected: "https://a.b.c:1234",
+}, {
+	addr:     "https://a.b.c:1234/",
+	expected: "https://a.b.c:1234",
+}, {
+	addr:     "a.b.c/x/y/z",
+	exists:   false,
+	expected: "https://a.b.c:8443/x/y/z",
+}, {
+	addr:     "https://a.b.c/x/y/z",
+	expected: "https://a.b.c:8443/x/y/z",
+}, {
+	addr:     "https://a.b.c:1234/x/y/z",
+	expected: "https://a.b.c:1234/x/y/z",
+}, {
+	addr:     "http://a.b.c",
+	expected: "https://a.b.c:8443",
 }, {
 	addr:     "1.2.3.4",
 	expected: "https://1.2.3.4:8443",
 }, {
+	addr:     "1.2.3.4:1234",
+	expected: "https://1.2.3.4:1234",
+}, {
 	addr:     "https://1.2.3.4",
 	expected: "https://1.2.3.4:8443",
 }, {
+	addr:     "127.0.0.1",
+	expected: "https://127.0.0.1:8443",
+}, {
 	addr:     "2001:db8::ff00:42:8329",
 	expected: "https://[2001:db8::ff00:42:8329]:8443",
+}, {
+	addr:     "[2001:db8::ff00:42:8329]:1234",
+	expected: "https://[2001:db8::ff00:42:8329]:1234",
 }, {
 	addr:     "https://2001:db8::ff00:42:8329",
 	expected: "https://[2001:db8::ff00:42:8329]:8443",
@@ -57,27 +90,24 @@ var typicalAddrTests = []addrTest{{
 	addr:     "https://[2001:db8::ff00:42:8329]:1234",
 	expected: "https://[2001:db8::ff00:42:8329]:1234",
 }, {
-	addr:     "unix://",
-	expected: "unix://",
+	addr:     "::1",
+	expected: "https://[::1]:8443",
 }, {
-	// TODO(ericsnow) Expect 3 slashes?
 	addr:     "unix:///x/y/z",
-	expected: "unix://x/y/z",
-}, {
-	// TODO(ericsnow) Expect 3 slashes?
-	addr:     "unix:/x/y/z",
-	expected: "unix://x/y/z",
-}, {
-	addr:     "/x/y/z",
+	exists:   true,
 	expected: "unix:///x/y/z",
 }, {
-	addr:     "x/y/z",
+	addr:     "unix:/x/y/z",
 	exists:   true,
-	expected: "unix://x/y/z",
+	expected: "unix:///x/y/z",
 }, {
-	addr:     "x.y.z",
+	addr:     "/x/y/z",
+	exists:   true,
+	expected: "unix:///x/y/z",
+}, {
+	addr:     "a.b.c",
 	exists:   false,
-	expected: "https://x.y.z:8443",
+	expected: "https://a.b.c:8443",
 }}
 
 func (s *fixAddrSuite) TestFixAddrTypical(c *gc.C) {
@@ -94,10 +124,47 @@ func (s *fixAddrSuite) TestFixAddrTypical(c *gc.C) {
 	}
 }
 
+// TODO(ericsnow) Add failure tests for bad domain names
+// and IP addresses (v4/v6).
+
 var failureAddrTests = []addrTest{{
 	// a malformed URL
-	addr: ":x.y.z",
+	addr: ":a.b.c",
 	err:  `.*`,
+}, {
+	addr: "https://a.b.c:xyz",
+	err:  `.*invalid port.*`,
+}, {
+	addr: "https://a.b.c:0",
+	err:  `.*invalid port.*`,
+}, {
+	addr: "https://a.b.c:99999",
+	err:  `.*invalid port.*`,
+}, {
+	addr: "spam://a.b.c",
+	err:  `.*unsupported URL scheme.*`,
+}, {
+	addr: "https://a.b.c?d=e",
+	err:  `.*URL queries not supported.*`,
+}, {
+	addr: "https://a.b.c#d",
+	err:  `.*URL fragments not supported.*`,
+}, {
+	addr:   "/x/y/z",
+	exists: false,
+	err:    `.*unix socket file .* not found.*`,
+}, {
+	addr:   "x/y/z",
+	exists: true,
+	err:    `.*relative unix socket paths not supported \(got "x/y/z"\).*`,
+}, {
+	addr:   "//x/y/z",
+	exists: true,
+	err:    `.*relative unix socket paths not supported \(got "x/y/z"\).*`,
+}, {
+	addr:   "unix://x/y/z",
+	exists: true,
+	err:    `.*relative unix socket paths not supported \(got "x/y/z"\).*`,
 }}
 
 func (s *fixAddrSuite) TestFixAddrFailures(c *gc.C) {
@@ -108,72 +175,5 @@ func (s *fixAddrSuite) TestFixAddrFailures(c *gc.C) {
 		_, err := fixAddr(test.addr, test.pathExists)
 
 		c.Check(err, gc.ErrorMatches, test.err)
-	}
-}
-
-var weirdAddrTests = []addrTest{{
-	addr:     "https://x.y.z/:1234",
-	expected: "https://x.y.z:8443",
-}, {
-	addr:     "https://x.y.z/a/b/c",
-	expected: "https://x.y.z:8443",
-}, {
-	addr:     "https://x.y.z/a/b/c",
-	expected: "https://x.y.z:8443",
-}, {
-	addr:     "http://x.y.z/a/b/c",
-	expected: "https://x.y.z:8443",
-}, {
-	addr:     "http://x.y.z/a/b/c:1234",
-	expected: "https://x.y.z:8443",
-}, {
-	addr:     "spam://x.y.z/a/b/c",
-	expected: "https://x.y.z:8443",
-}, {
-	addr:     "2001:db8::ff00:42:8329/a/b/c",
-	expected: "https://[2001:db8::ff00:42:8329/a/b/c]:8443",
-}, {
-	addr:     "https://2001:db8::ff00:42:8329/a/b/c",
-	expected: "https://[2001:db8::ff00:42:8329]:8443",
-}, {
-	addr:     "https://[2001:db8::ff00:42:8329]/a/b/c",
-	expected: "https://[2001:db8::ff00:42:8329]:8443",
-}, {
-	addr:     "//a.b.c/x/y/z",
-	expected: "unix://a.b.c",
-}, {
-	addr:     "a.b.c/x/y/z",
-	exists:   true,
-	expected: "unix://a.b.c/x/y/z",
-}, {
-	addr:     "x.y.z/a/b/c",
-	exists:   false,
-	expected: "https://x.y.z/a/b/c:8443",
-}, {
-	addr:     "xyz/a/b/c",
-	exists:   false,
-	expected: "https://xyz/a/b/c:8443",
-}, {
-	addr:     "unix:/",
-	expected: "unix://",
-}, {
-	addr:     "unix://x/y/z",
-	expected: "unix:///y/z",
-}, {
-	addr:     "unix:/x/y/z:8443",
-	expected: "unix://[x/y/z:8443]",
-}}
-
-func (s *fixAddrSuite) TestFixAddrWeird(c *gc.C) {
-	for i, test := range weirdAddrTests {
-		c.Logf("test %d: checking %q (exists: %v)", i, test.addr, test.exists)
-		c.Assert(test.err, gc.Equals, "")
-
-		fixed, err := fixAddr(test.addr, test.pathExists)
-
-		if !c.Check(err, jc.ErrorIsNil) {
-			continue
-		}
-		c.Check(fixed, gc.Equals, test.expected)
 	}
 }
