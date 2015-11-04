20d55839bebb93c5a3a275b3b95124956fd70c30
anastasia <anastasia.macmood@canonical.com>
2015-11-04 18:07:06 +1000

    show-saas cli

--- apiserver/crossmodel/crossmodel_test.go
+++ apiserver/crossmodel/crossmodel_test.go
@@ -23,6 +23,12 @@ type crossmodelSuite struct {
 
 var _ = gc.Suite(&crossmodelSuite{})
 
+func (s *crossmodelSuite) SetUpTest(c *gc.C) {
+	s.baseCrossmodelSuite.SetUpTest(c)
+
+	crossmodel.TempPlaceholder = make(map[names.ServiceTag]crossmodel.Offer)
+}
+
 func (s *crossmodelSuite) TestOffer(c *gc.C) {
 	serviceName := names.NewServiceTag("test")
 	one := params.CrossModelOffer{serviceName.String(), nil, "", nil}
@@ -167,4 +173,40 @@ func (s *crossmodelSuite) TestListOffersFoundMultiple(c *gc.C) {
 			{Result: params.RemoteServiceInfo{Service: tag2.String(), Endpoints: []params.RemoteEndpoint{}}},
 		}})
 	s.assertCalls(c, searchCall)
+}
+
+func (s *crossmodelSuite) TestShow(c *gc.C) {
+	tag := names.NewServiceTag("test")
+	url := "local:/u/fred/prod/hosted-db2"
+	anOffer := crossmodel.NewOffer(tag, nil, url, nil)
+
+	crossmodel.TempPlaceholder[tag] = anOffer
+
+	found, err := s.api.Show(params.SAASSearchFilter{URL: url})
+	c.Assert(err, jc.ErrorIsNil)
+	c.Assert(found, gc.DeepEquals, params.SAASDetailsResult{Service: tag.String()})
+}
+
+func (s *crossmodelSuite) TestShowNotFound(c *gc.C) {
+	url := "local:/u/fred/prod/hosted-db2"
+
+	found, err := s.api.Show(params.SAASSearchFilter{URL: url})
+	c.Assert(err, gc.ErrorMatches, `endpoints with url "local:/u/fred/prod/hosted-db2" not found`)
+	c.Assert(found, gc.DeepEquals, params.SAASDetailsResult{})
+}
+
+func (s *crossmodelSuite) TestShowFoundMultiple(c *gc.C) {
+	url := "local:/u/fred/prod/hosted-db2"
+
+	tag := names.NewServiceTag("test")
+	anOffer := crossmodel.NewOffer(tag, nil, url, nil)
+	tag2 := names.NewServiceTag("testAgain")
+	anOffer2 := crossmodel.NewOffer(tag2, nil, url, nil)
+
+	crossmodel.TempPlaceholder[tag] = anOffer
+	crossmodel.TempPlaceholder[tag2] = anOffer2
+
+	found, err := s.api.Show(params.SAASSearchFilter{URL: url})
+	c.Assert(err, gc.ErrorMatches, `expected to find one result for url "local:/u/fred/prod/hosted-db2" but found 2`)
+	c.Assert(found, gc.DeepEquals, params.SAASDetailsResult{})
 }
