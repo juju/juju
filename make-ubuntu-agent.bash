ce9b10f42792cba65b73054e85511c953f5399a1
Aaron Bentley <aaron.bentley@canonical.com>
2015-11-06 16:00:26 -0500

    Implement making agents.

---
+++ make-ubuntu-agent.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -eux
+RELEASE=$1
+SERIES=$2
+ARCH=$3
+revision_build=$4
+RELEASE_TOOLS=$(readlink -f $(dirname $0))
+jujud=$(find -path ./juju-build-$SERIES-amd64/juju-core-*/bin/jujud -type f)
+version=$(echo $jujud|sed -r "s/.\/juju-build-$SERIES-amd64\/juju-core-\
+(.*)\/bin\/jujud/\1/")
+tarfile=juju-$version-$SERIES-$ARCH.tgz
+tar -czvf $tarfile  -C $(dirname $jujud) jujud
+$RELEASE_TOOLS/make_stanza.py ubuntu $RELEASE $SERIES $ARCH $version \
+  $revision_build $tarfile
