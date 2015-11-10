0baa740ad7d61c5604a946c0796863fcd0bb13fe
Aaron Bentley <aaron.bentley@canonical.com>
2015-11-10 09:42:00 -0500

    Update make-stanzas.bash to use --json.

--- make-stanzas.bash
+++ make-stanzas.bash
@@ -3,6 +3,7 @@ set -eux
 revision_build=$1
 VERSION=$2
 final_path=$3
-sstream-query $HOME/streams/juju-dist/testing/tools/streams/v1/index.json version=$VERSION > revision-build-$revision_build.repr
-ssquery_json.py revision-build-$revision_build.repr revision-build-$revision_build.json
-set_stream.py revision-build-$revision_build.json $final_path $revision_build --update-path
+index=$HOME/streams/juju-dist/testing/tools/streams/v1/index.json
+temp_path=revision-build-$revision_build.json
+sstream-query --json $index version=$VERSION > $temp_path
+set_stream.py $temp_path $final_path $revision_build --update-path
