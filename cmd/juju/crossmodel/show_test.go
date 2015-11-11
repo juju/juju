5d0259068abfb972b36d96c4ab6ef2cbc60c9c27
anastasia <anastasia.macmood@canonical.com>
2015-11-11 17:29:02 +1000

    Added truncation for long desc.

--- cmd/juju/crossmodel/show_test.go
+++ cmd/juju/crossmodel/show_test.go
@@ -140,6 +140,34 @@ hosted-db2  IBM DB2 Express Server Edition is an entry level database systemIBM 
 	)
 }
 
+func (s *showSuite) TestShowTabularExactly100Desc(c *gc.C) {
+	s.mockAPI.desc = s.mockAPI.desc + s.mockAPI.desc[:36]
+	s.assertShow(
+		c,
+		[]string{"local:/u/fred/prod/db2", "--format", "tabular"},
+		`
+SERVICE     DESCRIPTION                                                                                           RELATION  INTERFACE  ROLE
+hosted-db2  IBM DB2 Express Server Edition is an entry level database systemIBM DB2 Express Server Edition is an  db2       hhtp       role
+                                                                                                                  log       http       role
+
+`[1:],
+	)
+}
+
+func (s *showSuite) TestShowTabularMoreThan100Desc(c *gc.C) {
+	s.mockAPI.desc = s.mockAPI.desc + s.mockAPI.desc
+	s.assertShow(
+		c,
+		[]string{"local:/u/fred/prod/db2", "--format", "tabular"},
+		`
+SERVICE     DESCRIPTION                                                                                           RELATION  INTERFACE  ROLE
+hosted-db2  IBM DB2 Express Server Edition is an entry level database systemIBM DB2 Express Server Edition is...  db2       hhtp       role
+                                                                                                                  log       http       role
+
+`[1:],
+	)
+}
+
 func (s *showSuite) assertShow(c *gc.C, args []string, expected string) {
 	context, err := s.runShow(c, args...)
 	c.Assert(err, jc.ErrorIsNil)
