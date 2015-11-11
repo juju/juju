08533f73939812ae86e1fa4cec836621af50633e
Aaron Bentley <aaron.bentley@canonical.com>
2015-11-11 12:29:57 -0500

    Convert to argument-accepting script.

--- assemble-parallel-streams.bash
+++ assemble-parallel-streams.bash
@@ -3,16 +3,10 @@ set -eu
 export JUJU_HOME=$HOME/cloud-city
 source $JUJU_HOME/juju-qa.jujuci
 set -x
-# Prepare for generating in a Jenkins workspace
-NEW_STREAMS=$WORKSPACE/new-streams
-TESTING=$NEW_STREAMS/testing
-AGENT_JSON=$NEW_STREAMS/testing-stanzas
-PATH="$HOME/juju-release-tools:$HOME/juju-ci-tools:$PATH"
-jujuci.py setup-workspace $WORKSPACE
-mkdir -p $TESTING
-mkdir -p $AGENT_JSON
-cp $HOME/new-streams/testing-stanzas/*.json $AGENT_JSON/
-rm $AGENT_JSON/revision-build-$revision_build-paths.json || true
+revision_build=$1
+WORKSPACE=$2
+AGENT_JSON=$3
+TESTING=$4
 
 # General-case begins
 AGENT_JOBS="build-win-agent build-centos build-binary-precise-amd64 \
@@ -21,6 +15,7 @@ AGENT_JOBS="build-win-agent build-centos build-binary-precise-amd64 \
    build-binary-wily-amd64"
 WS_JSON=$WORKSPACE/ws-json
 WS_AGENTS=$WORKSPACE/agent/revision-build-$revision_build
+VERSION=$(jujuci.py get-build-vars $revision_build --version)
 mkdir $WS_JSON
 mkdir -p $WS_AGENTS
 for job in $AGENT_JOBS; do
@@ -32,7 +27,6 @@ set_stream.py $AGENT_JSON/release.json \
 cp -r $(dirname $WS_AGENTS) $TESTING/agent/
 cp $WS_JSON/*.json $AGENT_JSON/
 json2streams --juju-format $AGENT_JSON/* $TESTING
-VERSION=$(jujuci.py get-build-vars $revision_build --version)
 sstream-query $TESTING/streams/v1/index2.json \
   content_id="com.ubuntu.juju:revision-build-$revision_build:tools" \
   version=$VERSION --output-format="%(sha256)s  %(item_url)s" |sort|uniq > \
