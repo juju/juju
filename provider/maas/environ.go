b1b4829202394ff2a035aab671582a30977b7b15
Michael Foord <michael.foord@canonical.com>
2015-11-11 09:46:45 +0000

    Implement SupportsSpaces for maas 1.9

--- provider/maas/environ.go
+++ provider/maas/environ.go
@@ -281,7 +281,11 @@ func (env *maasEnviron) SupportedArchitectures() ([]string, error) {
 
 // SupportsSpaces is specified on environs.Networking.
 func (env *maasEnviron) SupportsSpaces() (bool, error) {
-	return false, errors.NotSupportedf("spaces")
+	caps, err := env.getCapabilities()
+	if err != nil {
+		return false, errors.Annotatef(err, "getCapabilities failed")
+	}
+	return caps.Contains(capSpaces), nil
 }
 
 // SupportsAddressAllocation is specified on environs.Networking.
@@ -574,6 +578,7 @@ const (
 	capNetworksManagement = "networks-management"
 	capStaticIPAddresses  = "static-ipaddresses"
 	capDevices            = "devices-management"
+	capSpaces             = "network-management"
 )
 
 // getCapabilities asks the MAAS server for its capabilities, if
