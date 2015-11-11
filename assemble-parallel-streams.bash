5c8eb2f87a8a27affc7a97236bd5fe6cc404bc1c
Aaron Bentley <aaron.bentley@canonical.com>
2015-11-11 10:12:56 -0500

    Reorganize to support writing to real testing location.

--- assemble-parallel-streams.bash
+++ assemble-parallel-streams.bash
@@ -3,31 +3,34 @@ set -eu
 export JUJU_HOME=$HOME/cloud-city
 source $JUJU_HOME/juju-qa.jujuci
 set -x
-PATH="$HOME/juju-release-tools:$HOME/juju-ci-tools:$PATH"
+NEW_STREAMS=$WORKSPACE/new-streams
+TESTING=$NEW_STREAMS/testing
+AGENT_JSON=$NEW_STREAMS/testing-stanzas
+mkdir -p $AGENT_JSON
 jujuci.py setup-workspace $WORKSPACE
-AGENTS=$WORKSPACE/agent/revision-build-$revision_build
-mkdir -p $AGENTS
-AGENT_JSON=$WORKSPACE/agent-json
-mkdir $AGENT_JSON
-agent_jobs="build-win-agent build-centos build-binary-precise-amd64 \
+cp $HOME/new-streams/testing-stanzas/*.json $AGENT_JSON/
+rm $AGENT_JSON/revision-build-$revision_build-paths.json || true
+
+PATH="$HOME/juju-release-tools:$HOME/juju-ci-tools:$PATH"
+AGENT_JOBS="build-win-agent build-centos build-binary-precise-amd64 \
    build-binary-trusty-amd64 build-binary-trusty-i386 \
    build-binary-trusty-ppc64el build-binary-vivid-amd64  \
    build-binary-wily-amd64"
-for job in $agent_jobs; do
-  jujuci.py get -b lastBuild $job '*.tgz' $AGENTS
-  jujuci.py get -b lastBuild $job '*.json' $AGENT_JSON
+WS_JSON=$WORKSPACE/ws-json
+WS_AGENTS=$WORKSPACE/agent/revision-build-$revision_build
+mkdir $WS_JSON
+mkdir -p $WS_AGENTS
+for job in $AGENT_JOBS; do
+  jujuci.py get -b lastBuild $job '*.tgz' $WS_AGENTS
+  jujuci.py get -b lastBuild $job '*.json' $WS_JSON
 done
-cp $HOME/new-streams/testing-stanzas/*.json $AGENT_JSON/
-rm $AGENT_JSON/revision-build-$revision_build-paths.json || true
-set_stream.py $HOME/new-streams/testing-stanzas/release.json \
-  $AGENT_JSON/release-$revision_build.json $revision_build
-new_streams=$WORKSPACE/new-streams
-testing=$new_streams/testing
-mkdir -p $testing
-cp -r $(dirname $AGENTS) $testing/agent/
-json2streams --juju-format $AGENT_JSON/* $testing
+set_stream.py $AGENT_JSON/release.json \
+  $WS_JSON/release-$revision_build.json $revision_build
+mkdir -p $TESTING
+cp -r $(dirname $WS_AGENTS) $TESTING/agent/
+json2streams --juju-format $AGENT_JSON/* $TESTING
 VERSION=$(jujuci.py get-build-vars $revision_build --version)
-sstream-query $testing/streams/v1/index2.json \
+sstream-query $TESTING/streams/v1/index2.json \
   content_id="com.ubuntu.juju:revision-build-$revision_build:tools" \
   version=$VERSION --output-format="%(sha256)s  %(item_url)s" |sort|uniq > \
   sha256sums
