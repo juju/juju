8dc59db253476ab016daa7ede438d060d079dc13
Martin Packman <martin.packman@canonical.com>
2015-11-12 18:29:04 +0000

    Attempt two at making the manual cleanup script sane

--- remove-manual-juju.bash
+++ remove-manual-juju.bash
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-JUJU_DIR="/var/lib/juju"
-DUMMY_DIR="/var/run/dummy-sink"
 ips="$@"
 for ip in $ips; do
     ssh -i $JUJU_HOME/staging-juju-rsa ubuntu@$ip <<"EOT"
 #!/bin/bash
 set -ux
-if [[ -n "$(ps ax | grep jujud | grep -v grep)" ]]; then
-    sudo touch /var/lib/juju/uninstall-agent
+JUJU_DIR="/var/lib/juju"
+DUMMY_DIR="/var/run/dummy-sink"
+if [[ -n "$(ps -C jujud --no-headers)" ]]; then
+    sudo touch $JUJU_DIR/uninstall-agent
     sudo killall -SIGABRT jujud
-    sudo killall -9 mongod
 fi
+sudo killall -9 mongod || true
 if [[ -d $JUJU_DIR ]]; then
     sudo rm -r $JUJU_DIR
 fi
