9632e717ee12ec71e1f0d4fdbc24416bcd7d8314
Menno Smits <menno.smits@canonical.com>
2015-11-16 13:49:12 +1300

    cmd/jujud/agent/unit: removed APIInfo gate
    This gate is no longer in use. The problem it was dealing with is now
    handled in another way.

--- cmd/jujud/agent/unit/manifolds.go
+++ cmd/jujud/agent/unit/manifolds.go
@@ -13,7 +13,6 @@ import (
 	"github.com/juju/juju/worker/apicaller"
 	"github.com/juju/juju/worker/dependency"
 	"github.com/juju/juju/worker/fortress"
-	"github.com/juju/juju/worker/gate"
 	"github.com/juju/juju/worker/leadership"
 	"github.com/juju/juju/worker/logger"
 	"github.com/juju/juju/worker/logsender"
@@ -69,16 +68,8 @@ func Manifolds(config ManifoldsConfig) dependency.Manifolds {
 		// how this works when we consolidate the agents; might be best to
 		// handle the auth changes server-side..?
 		APICallerName: apicaller.Manifold(apicaller.ManifoldConfig{
-			AgentName:       AgentName,
-			APIInfoGateName: APIInfoGateName,
+			AgentName: AgentName,
 		}),
-
-		// This manifold is used to coordinate between the api caller and the
-		// log sender, which share the API credentials that the API caller may
-		// update. To avoid surprising races, the log sender waits for the api
-		// caller to unblock this, indicating that any password dance has been
-		// completed and the log-sender can now connect without confusion.
-		APIInfoGateName: gate.Manifold(),
 
 		// The log sender is a leaf worker that sends log messages to some
 		// API server, when configured so to do. We should only need one of
@@ -200,7 +191,6 @@ const (
 	AgentName                = "agent"
 	APIAdddressUpdaterName   = "api-address-updater"
 	APICallerName            = "api-caller"
-	APIInfoGateName          = "api-info-gate"
 	LeadershipTrackerName    = "leadership-tracker"
 	LoggingConfigUpdaterName = "logging-config-updater"
 	LogSenderName            = "log-sender"
