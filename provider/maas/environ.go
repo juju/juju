29b560f40dfa2a2145d5ce1c54148be511b07d2c
Andrew McDermott <andrew.mcdermott@canonical.com>
2015-11-17 19:16:09 +0000

    provider/maas: make network bridge script work for MAAS 1.9
    This is a forward port of PR #3780 from 1.25.
    
    The previous rendering of /etc/network/interfaces would result in a
    broken bootstrap on MAAS 1.9 when the interface was set to DHCP. This
    change reworks the script to replace interface names and bridge names
    in-situ and no longer reorders the existing content; the ifup/down
    scripts failed to parse the (previously) modified file.
    
    Fixes [LP:#1512371](https://bugs.launchpad.net/juju-core/+bug/1512371)
    
    (cherry picked from commit c2511d450e9f8ba07606b622f6dd126b651f2706)
    (cherry picked from commit e7fc23963fee92fef6b57cc3b4ea3521766d627e)

--- provider/maas/environ.go
+++ provider/maas/environ.go
@@ -1264,38 +1264,22 @@ ifup -v {{.Bridge}}
 // setupJujuNetworking returns a string representing the script to run
 // in order to prepare the Juju-specific networking config on a node.
 func setupJujuNetworking() (string, error) {
-	modifyConfigScript, err := renderEtcNetworkInterfacesScript("/etc/network/interfaces", instancecfg.DefaultBridgeName)
-	if err != nil {
-		return "", err
-	}
 	parsedTemplate := template.Must(
-		template.New("BridgeConfig").Parse(bridgeConfigTemplate),
+		template.New("BridgeConfig").Parse(bridgeScriptMain),
 	)
 	var buf bytes.Buffer
-	err = parsedTemplate.Execute(&buf, map[string]interface{}{
+	err := parsedTemplate.Execute(&buf, map[string]interface{}{
 		"Config": "/etc/network/interfaces",
 		"Bridge": instancecfg.DefaultBridgeName,
-		"Script": modifyConfigScript,
 	})
 	if err != nil {
 		return "", errors.Annotate(err, "bridge config template error")
 	}
-	return buf.String(), nil
+	return bridgeScriptBase + buf.String(), nil
 }
 
-func renderEtcNetworkInterfacesScript(config, bridge string) (string, error) {
-	parsedTemplate := template.Must(
-		template.New("ModifyConfigScript").Parse(modifyEtcNetworkInterfaces),
-	)
-	var buf bytes.Buffer
-	err := parsedTemplate.Execute(&buf, map[string]interface{}{
-		"Config": config,
-		"Bridge": bridge,
-	})
-	if err != nil {
-		return "", errors.Annotate(err, "modify /etc/network/interfaces script template error")
-	}
-	return buf.String(), nil
+func renderEtcNetworkInterfacesScript() (string, error) {
+	return setupJujuNetworking()
 }
 
 // newCloudinitConfig creates a cloudinit.Config structure
