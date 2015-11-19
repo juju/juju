202e739f0eebadf68be6fdc6171737b6b108589d
Tim Penhey <tim@penhey.net>
2015-11-20 10:57:28 +1300

    Use a type for the function signature, and fix a race in Open.

--- cmd/envcmd/apiopener.go
+++ cmd/envcmd/apiopener.go
@@ -14,6 +14,8 @@ import (
 
 var ErrConnTimedOut = errors.New("open connection timed out")
 
+type OpenFunc func(string) (api.Connection, error)
+
 // Opener provides a way to open a connection to the Juju
 // API Server through the named connection.
 type Opener interface {
@@ -21,12 +23,12 @@ type Opener interface {
 }
 
 type passthroughOpener struct {
-	fn func(string) (api.Connection, error)
+	fn OpenFunc
 }
 
 // NewPassthroughOpener returns an instance that will just call the opener
 // function when Open is called.
-func NewPassthroughOpener(opener func(string) (api.Connection, error)) Opener {
+func NewPassthroughOpener(opener OpenFunc) Opener {
 	return &passthroughOpener{fn: opener}
 }
 
@@ -35,7 +37,7 @@ func (p *passthroughOpener) Open(name string) (api.Connection, error) {
 }
 
 type timeoutOpener struct {
-	fn      func(string) (api.Connection, error)
+	fn      OpenFunc
 	clock   clock.Clock
 	timeout time.Duration
 }
@@ -43,10 +45,7 @@ type timeoutOpener struct {
 // NewTimeoutOpener will call the opener function when Open is called, but if
 // the function does not return by the specified timeout, ErrConnTimeOut is
 // returned.
-func NewTimeoutOpener(
-	opener func(string) (api.Connection, error),
-	clock clock.Clock,
-	timeout time.Duration) Opener {
+func NewTimeoutOpener(opener OpenFunc, clock clock.Clock, timeout time.Duration) Opener {
 	return &timeoutOpener{
 		fn:      opener,
 		clock:   clock,
@@ -59,29 +58,21 @@ func (t *timeoutOpener) Open(name string) (api.Connection, error) {
 	// not go get blocked trying to send down the channel.
 	apic := make(chan api.Connection, 1)
 	errc := make(chan error, 1)
-	timedOut := make(chan struct{})
 	go func() {
 		api, dialErr := t.fn(name)
-
-		// Check to see if we have timed out before we block forever.
-		// Well, we take a certain block forever on timeout, to a very
-		// small race around blocking forever.
-		select {
-		case <-timedOut:
-			// If we have a valid api, close it.
-			if api != nil {
-				api.Close()
-			}
-			return
-		default:
-			// continue on
-		}
 
 		if dialErr != nil {
 			errc <- dialErr
 			return
 		}
-		apic <- api
+
+		select {
+		case apic <- api:
+			// sent fine
+		default:
+			// couldn't send, was blocked by the dummy value, must have timed out.
+			api.Close()
+		}
 	}()
 
 	var apiRoot api.Connection
@@ -90,7 +81,18 @@ func (t *timeoutOpener) Open(name string) (api.Connection, error) {
 		return nil, err
 	case apiRoot = <-apic:
 	case <-t.clock.After(t.timeout):
-		close(timedOut)
+		select {
+		case apic <- nil:
+			// Fill up the buffer on the apic to indicate to the other goroutine
+			// that we have timed out.
+		case apiRoot = <-apic:
+			// We hit that weird edge case where we have both timed out and
+			// returned a viable apiRoot at exactly the same time, and the other
+			// goroutine managed to send back the apiRoot before we pushed the
+			// dummy value.  If this is the case, then we are good, return the
+			// apiRoot
+			return apiRoot, nil
+		}
 		return nil, ErrConnTimedOut
 	}
 
