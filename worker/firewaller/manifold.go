e42e9ac1ae1644f7234b01afbca797ac51f9f2c3
William Reade <fwereade@gmail.com>
2015-11-23 15:33:31 +0100

    add firewaller manifold

--- worker/firewaller/manifold.go
+++ worker/firewaller/manifold.go
@@ -1,22 +1,22 @@
 // Copyright 2015 Canonical Ltd.
 // Licensed under the AGPLv3, see LICENCE file for details.
 
-package cleaner
+package firewaller
 
 import (
 	"github.com/juju/errors"
 
 	"github.com/juju/juju/api/base"
-	"github.com/juju/juju/api/cleaner"
+	"github.com/juju/juju/api/firewaller"
 	"github.com/juju/juju/worker"
 	"github.com/juju/juju/worker/dependency"
 	"github.com/juju/juju/worker/util"
 )
 
-// ManifoldConfig describes the resources used by the cleanup worker.
+// ManifoldConfig describes the resources used by the firewaller worker.
 type ManifoldConfig util.ApiManifoldConfig
 
-// Manifold returns a Manifold that encapsulates the cleanup worker.
+// Manifold returns a Manifold that encapsulates the firewaller worker.
 func Manifold(config ManifoldConfig) dependency.Manifold {
 	return util.ApiManifold(
 		util.ApiManifoldConfig(config),
@@ -24,10 +24,10 @@ func Manifold(config ManifoldConfig) dependency.Manifold {
 	)
 }
 
-// manifoldStart creates a cleaner worker, given a base.APICaller.
+// manifoldStart creates a firewaller worker, given a base.APICaller.
 func manifoldStart(apiCaller base.APICaller) (worker.Worker, error) {
-	api := cleaner.NewAPI(apiCaller)
-	w, err := NewCleaner(api)
+	api := firewaller.NewState(apiCaller)
+	w, err := NewFirewaller(api)
 	if err != nil {
 		return nil, errors.Trace(err)
 	}
