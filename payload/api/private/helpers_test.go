05a15f183f7279b2e347f91cae2ef05100f9b583
Eric Snow <ericsnowcurrently@gmail.com>
2015-11-23 12:54:30 -0700

    Fix error conversion tests.

--- payload/api/private/helpers_test.go
+++ payload/api/private/helpers_test.go
@@ -136,11 +136,12 @@ func (internalHelpersSuite) TestAPI2ResultError(c *gc.C) {
 	})
 	c.Assert(err, jc.ErrorIsNil)
 
+	c.Check(result.Error.Error(), gc.Equals, failure.Error())
 	c.Check(result, jc.DeepEquals, payload.Result{
 		ID:       id,
 		Payload:  nil,
 		NotFound: false,
-		Error:    failure,
+		Error:    result.Error, // The actual error is checked above.
 	})
 }
 
@@ -157,11 +158,13 @@ func (internalHelpersSuite) TestAPI2ResultNotFound(c *gc.C) {
 	})
 	c.Assert(err, jc.ErrorIsNil)
 
+	c.Check(result.Error.Error(), gc.Equals, notFound.Error())
+	c.Check(result.Error, jc.Satisfies, errors.IsNotFound)
 	c.Check(result, jc.DeepEquals, payload.Result{
 		ID:       id,
 		Payload:  nil,
 		NotFound: false,
-		Error:    notFound,
+		Error:    result.Error, // The actual error is checked above.
 	})
 }
 
