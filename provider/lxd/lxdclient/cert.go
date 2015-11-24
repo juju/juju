48de29e9927ecead066f357da6dbc8539ba9ce69
Eric Snow <ericsnowcurrently@gmail.com>
2015-11-24 14:30:56 -0700

    Check for nil in Cert.isZero().

--- provider/lxd/lxdclient/cert.go
+++ provider/lxd/lxdclient/cert.go
@@ -65,7 +65,10 @@ func (cert Cert) WithDefaults() (Cert, error) {
 	return cert, nil
 }
 
-func (cert Cert) isZero() bool {
+func (cert *Cert) isZero() bool {
+	if cert == nil {
+		return true
+	}
 	return len(cert.CertPEM) == 0 && len(cert.KeyPEM) == 0
 }
 
