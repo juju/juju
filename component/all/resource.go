b1c2004fbe805ea8b129cbb80ead4c76d889efb5
Eric Snow <ericsnowcurrently@gmail.com>
2015-11-25 14:34:11 -0700

    Stub out the component registration.

---
+++ component/all/resource.go
@@ -0,0 +1,14 @@
+// Copyright 2015 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package all
+
+type resources struct{}
+
+func (c payloads) registerForServer() error {
+	return nil
+}
+
+func (c payloads) registerForClient() error {
+	return nil
+}
