29c2adb453978c28c89b72e0cc5ea3beb329b327
Eric Snow <ericsnowcurrently@gmail.com>
2015-11-25 10:57:45 -0700

    Add a public API client with a ListSpecs method.

--- resource/api/client/client.go
+++ resource/api/client/client.go
@@ -5,6 +5,28 @@ package client
 
 import (
 	"github.com/juju/loggo"
+	"io"
 )
 
 var logger = loggo.GetLogger("juju.resource.api.client")
+
+type facadeCaller interface {
+	FacadeCall(request string, params, response interface{}) error
+}
+
+type rawAPI interface {
+	facadeCaller
+	io.Closer
+}
+
+// Client is the public client for the resources API facade.
+type Client struct {
+	*specClient
+}
+
+// NewClient returns a new Client for the given raw API caller.
+func NewClient(raw rawAPI) *Client {
+	return &Client{
+		specClient: &specClient{raw},
+	}
+}
