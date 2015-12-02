16e1daa6021a5e4b9c371b2e405e1d66f450880e
Eric Snow <ericsnowcurrently@gmail.com>
2015-12-02 14:34:08 -0700

    Add API2SpecsResult.

--- resource/api/helpers.go
+++ resource/api/helpers.go
@@ -3,11 +3,14 @@
 
 package api
 
+// TODO(ericsnow) Eliminate the dependence on apiserver if possible.
+
 import (
 	"github.com/juju/errors"
 	"github.com/juju/names"
 	charmresource "gopkg.in/juju/charm.v6-unstable/resource"
 
+	"github.com/juju/juju/apiserver/common"
 	"github.com/juju/juju/resource"
 )
 
@@ -72,4 +75,30 @@ func API2ResourceSpec(apiSpec ResourceSpec) (resource.Spec, error) {
 		return spec, errors.Trace(err)
 	}
 	return spec, nil
+}
+
+// API2SpecsResult converts a ResourceSpecsResult into a resource.SpecsResult.
+func API2SpecsResult(service string, apiResult ResourceSpecsResult) (resource.SpecsResult, error) {
+	result := resource.SpecsResult{
+		Service: service,
+	}
+
+	result.Error, _ = common.RestoreError(apiResult.Error)
+
+	var failure error
+	for _, apiSpec := range apiResult.Specs {
+		spec, err := API2ResourceSpec(apiSpec)
+		if err != nil {
+			// This could happen if the server is misbehaving
+			// or non-conforming.
+			if result.Error == nil {
+				result.Error = errors.Annotate(err, "got bad data from server")
+				failure = result.Error
+			}
+			// TODO(ericsnow) Set an empty spec?
+		}
+		result.Specs = append(result.Specs, spec)
+	}
+
+	return result, failure
 }
