3f8307076b894e7f68bc31cad2aca5b454ed04c8
James Tunnicliffe <james.tunnicliffe@nanosheep.org>
2015-12-04 19:45:19 +0000

    deploy --bind wired up through to API server (which doesn't do anything yet)

--- cmd/juju/commands/deploy_test.go
+++ cmd/juju/commands/deploy_test.go
@@ -1015,49 +1015,49 @@ const bindErrPrefix = "--bind must be in the form '[<relation-name>]@<space> [[<
 
 func (s *ParseBindSuite) TestBindParseEmpty(c *gc.C) {
 	deploy := &DeployCommand{BindToSpaces: ""}
-	bindings, err := deploy.parseBind()
+	err := deploy.parseBind()
 	c.Assert(err, jc.ErrorIsNil)
-	c.Assert(bindings, gc.IsNil)
+	c.Assert(deploy.Bindings, gc.IsNil)
 }
 
 func (s *ParseBindSuite) TestBindParseOK(c *gc.C) {
 	deploy := &DeployCommand{BindToSpaces: "foo@a bar@b"}
-	bindings, err := deploy.parseBind()
+	err := deploy.parseBind()
 	c.Assert(err, jc.ErrorIsNil)
-	c.Assert(bindings, jc.DeepEquals, []binding{{"foo", "a"}, {"bar", "b"}})
+	c.Assert(deploy.Bindings, jc.DeepEquals, map[string]string{"foo": "a", "bar": "b"})
 }
 
 func (s *ParseBindSuite) TestBindParseNoRelation(c *gc.C) {
 	deploy := &DeployCommand{BindToSpaces: "@default"}
-	bindings, err := deploy.parseBind()
+	err := deploy.parseBind()
 	c.Assert(err, jc.ErrorIsNil)
-	c.Assert(bindings, jc.DeepEquals, []binding{{"", "default"}})
+	c.Assert(deploy.Bindings, jc.DeepEquals, map[string]string{"": "default"})
 }
 
 func (s *ParseBindSuite) TestBindParseNoAt(c *gc.C) {
 	deploy := &DeployCommand{BindToSpaces: "foo"}
-	bindings, err := deploy.parseBind()
+	err := deploy.parseBind()
 	c.Assert(err.Error(), gc.Equals, bindErrPrefix+"Could not find '@'.")
-	c.Assert(bindings, gc.IsNil)
+	c.Assert(deploy.Bindings, gc.IsNil)
 }
 
 func (s *ParseBindSuite) TestBindParseBadList(c *gc.C) {
 	deploy := &DeployCommand{BindToSpaces: "foo@bar@baz"}
-	bindings, err := deploy.parseBind()
+	err := deploy.parseBind()
 	c.Assert(err.Error(), gc.Equals, bindErrPrefix+"Found multiple @ in binding. Did you forget to space-separate the binding list?")
-	c.Assert(bindings, gc.IsNil)
+	c.Assert(deploy.Bindings, gc.IsNil)
 }
 
 func (s *ParseBindSuite) TestBindParseDoc(c *gc.C) {
 	deploy := &DeployCommand{BindToSpaces: "rel1@space1 rel2@space2 @space3"}
-	bindings, err := deploy.parseBind()
+	err := deploy.parseBind()
 	c.Assert(err, jc.ErrorIsNil)
-	c.Assert(bindings, jc.DeepEquals, []binding{{"rel1", "space1"}, {"rel2", "space2"}, {"", "space3"}})
+	c.Assert(deploy.Bindings, jc.DeepEquals, map[string]string{"rel1": "space1", "rel2": "space2", "": "space3"})
 }
 
 func (s *ParseBindSuite) TestBindParseBadSpace(c *gc.C) {
 	deploy := &DeployCommand{BindToSpaces: "rel1@spa#ce1"}
-	bindings, err := deploy.parseBind()
+	err := deploy.parseBind()
 	c.Assert(err.Error(), gc.Equals, bindErrPrefix+"Space name invalid.")
-	c.Assert(bindings, gc.IsNil)
+	c.Assert(deploy.Bindings, gc.IsNil)
 }
