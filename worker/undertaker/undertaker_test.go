b4507fd79e3c814de8345799a31ca56de8a45d96
waigani <waigani@gmail.com>
2015-12-15 16:26:26 -0800

    worker/undertaker: fix race
    Fix a race condition in the undertaker unit test.

--- worker/undertaker/undertaker_test.go
+++ worker/undertaker/undertaker_test.go
@@ -4,6 +4,8 @@
 package undertaker_test
 
 import (
+	"sync"
+	"sync/atomic"
 	"time"
 
 	jc "github.com/juju/testing/checkers"
@@ -21,66 +23,95 @@ type undertakerSuite struct {
 
 var _ = gc.Suite(&undertakerSuite{})
 
+type clock struct {
+
+	// advanceDurationAfterNow is the duration to advance the clock after the
+	// next call to Now().
+	advanceDurationAfterNow *int64
+
+	*testing.Clock
+}
+
+func (c clock) Now() time.Time {
+	now := c.Clock.Now()
+	d := atomic.LoadInt64(c.advanceDurationAfterNow)
+	if d != int64(0) {
+		c.Clock.Advance(time.Duration(d))
+		atomic.SwapInt64(c.advanceDurationAfterNow, int64(0))
+	}
+
+	return now
+}
+
+func (c clock) advanceAfterNextNow(d time.Duration) {
+	atomic.SwapInt64(c.advanceDurationAfterNow, int64(d))
+}
+
 func (s *undertakerSuite) TestAPICalls(c *gc.C) {
 	client := &mockClient{
-		calls: make(chan string, 1),
+		calls: make(chan string),
 		mockEnviron: clientEnviron{
 			Life: state.Dying,
 			UUID: utils.MustNewUUID().String(),
 			HasMachinesAndServices: true,
 		},
 		watcher: &mockEnvironResourceWatcher{
-			events: make(chan struct{}, 1),
+			events: make(chan struct{}),
 		},
 	}
+
 	startTime := time.Date(2015, time.September, 1, 17, 2, 1, 0, time.UTC)
-	mClock := testing.NewClock(startTime)
+	mClock := &clock{
+		Clock: testing.NewClock(startTime),
+		advanceDurationAfterNow: new(int64),
+	}
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		for _, test := range []struct {
+			call     string
+			callback func()
+		}{{
+			call: "EnvironInfo",
+		}, {
+			call: "ProcessDyingEnviron",
+			callback: func() {
+				c.Check(client.mockEnviron.Life, gc.Equals, state.Dying)
+				c.Check(client.mockEnviron.TimeOfDeath, gc.IsNil)
+				client.mockEnviron.HasMachinesAndServices = false
+				client.watcher.(*mockEnvironResourceWatcher).events <- struct{}{}
+				mClock.advanceAfterNextNow(undertaker.RIPTime)
+			}}, {
+			call: "ProcessDyingEnviron",
+			callback: func() {
+				c.Check(client.mockEnviron.Life, gc.Equals, state.Dead)
+				c.Check(client.mockEnviron.TimeOfDeath, gc.NotNil)
+			}}, {
+			call: "RemoveEnviron",
+			callback: func() {
+				oneDayLater := startTime.Add(undertaker.RIPTime)
+				c.Check(mClock.Now().Equal(oneDayLater), jc.IsTrue)
+				c.Check(client.mockEnviron.Removed, gc.Equals, true)
+			}},
+		} {
+			select {
+			case call := <-client.calls:
+				c.Check(call, gc.Equals, test.call)
+				if test.callback != nil {
+					test.callback()
+				}
+			case <-time.After(testing.LongWait):
+				c.Fatalf("timed out waiting for API call: %q", test.call)
+			}
+		}
+	}()
+
 	worker := undertaker.NewUndertaker(client, mClock)
 	defer worker.Kill()
 
-	c.Assert(client.mockEnviron.TimeOfDeath, gc.IsNil)
-	for _, test := range []struct {
-		call     string
-		callback func()
-	}{{
-		call: "EnvironInfo",
-		callback: func() {
-			client.watcher.(*mockEnvironResourceWatcher).events <- struct{}{}
-		},
-	}, {
-		call: "ProcessDyingEnviron",
-		callback: func() {
-
-			c.Assert(client.mockEnviron.Life, gc.Equals, state.Dying)
-			c.Assert(client.mockEnviron.TimeOfDeath, gc.IsNil)
-			client.mockEnviron.HasMachinesAndServices = false
-			client.watcher.(*mockEnvironResourceWatcher).events <- struct{}{}
-
-		}}, {
-		call: "ProcessDyingEnviron",
-		callback: func() {
-			c.Assert(client.mockEnviron.Life, gc.Equals, state.Dead)
-			c.Assert(client.mockEnviron.TimeOfDeath, gc.NotNil)
-
-			mClock.Advance(undertaker.RIPTime)
-		}}, {
-		call: "RemoveEnviron",
-		callback: func() {
-			oneDayLater := startTime.Add(undertaker.RIPTime)
-			c.Assert(mClock.Now().Equal(oneDayLater), jc.IsTrue)
-			c.Assert(client.mockEnviron.Removed, gc.Equals, true)
-		}},
-	} {
-		select {
-		case call := <-client.calls:
-			c.Assert(call, gc.Equals, test.call)
-			if test.callback != nil {
-				test.callback()
-			}
-		case <-time.After(testing.LongWait):
-			c.Fatalf("timed out waiting for API call: %q", test.call)
-		}
-	}
+	wg.Wait()
 
 	select {
 	case call := <-client.calls:
@@ -103,39 +134,50 @@ func (s *undertakerSuite) TestRemoveEnvironDocsNotCalledForStateServer(c *gc.C) 
 		watcher: mockWatcher,
 	}
 	startTime := time.Date(2015, time.September, 1, 17, 2, 1, 0, time.UTC)
-	mClock := testing.NewClock(startTime)
+	mClock := &clock{
+		Clock: testing.NewClock(startTime),
+		advanceDurationAfterNow: new(int64),
+	}
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		for _, test := range []struct {
+			call     string
+			callback func()
+		}{{
+			call: "EnvironInfo",
+			callback: func() {
+				mockWatcher.events <- struct{}{}
+			},
+		}, {
+			call: "ProcessDyingEnviron",
+			callback: func() {
+				c.Assert(client.mockEnviron.Life, gc.Equals, state.Dead)
+				c.Assert(client.mockEnviron.TimeOfDeath, gc.NotNil)
+
+				mClock.advanceAfterNextNow(undertaker.RIPTime)
+			},
+		},
+		} {
+			select {
+			case call := <-client.calls:
+				c.Assert(call, gc.Equals, test.call)
+				if test.callback != nil {
+					test.callback()
+				}
+			case <-time.After(testing.LongWait):
+				c.Fatalf("timed out waiting for API call: %q", test.call)
+			}
+		}
+	}()
+
 	worker := undertaker.NewUndertaker(client, mClock)
 	defer worker.Kill()
 
-	c.Assert(client.mockEnviron.TimeOfDeath, gc.IsNil)
-	for _, test := range []struct {
-		call     string
-		callback func()
-	}{{
-		call: "EnvironInfo",
-		callback: func() {
-			mockWatcher.events <- struct{}{}
-		},
-	}, {
-		call: "ProcessDyingEnviron",
-		callback: func() {
-			c.Assert(client.mockEnviron.Life, gc.Equals, state.Dead)
-			c.Assert(client.mockEnviron.TimeOfDeath, gc.NotNil)
-
-			mClock.Advance(undertaker.RIPTime)
-		},
-	},
-	} {
-		select {
-		case call := <-client.calls:
-			c.Assert(call, gc.Equals, test.call)
-			if test.callback != nil {
-				test.callback()
-			}
-		case <-time.After(testing.LongWait):
-			c.Fatalf("timed out waiting for API call: %q", test.call)
-		}
-	}
+	wg.Wait()
 
 	select {
 	case call := <-client.calls:
@@ -160,38 +202,46 @@ func (s *undertakerSuite) TestRemoveEnvironOnRebootCalled(c *gc.C) {
 		},
 	}
 
-	worker := undertaker.NewUndertaker(client, mClock)
-	defer worker.Kill()
+	wg := sync.WaitGroup{}
+	wg.Add(1)
 
 	// We expect RemoveEnviron not to be called, as we have to wait another
 	// 12hrs.
-	for _, test := range []struct {
-		call     string
-		callback func()
-	}{{
-		call: "EnvironInfo",
-		callback: func() {
-			// As environ was set to dead 12hrs earlier, assert that the
-			// undertaker picks up where it left off and RemoveEnviron
-			// is called 12hrs later.
-			mClock.Advance(12 * time.Hour)
-		},
-	}, {
-		call: "RemoveEnviron",
-		callback: func() {
-			c.Assert(client.mockEnviron.Removed, gc.Equals, true)
-		}},
-	} {
-		select {
-		case call := <-client.calls:
-			c.Assert(call, gc.Equals, test.call)
-			if test.callback != nil {
-				test.callback()
+	go func() {
+		defer wg.Done()
+		for _, test := range []struct {
+			call     string
+			callback func()
+		}{{
+			call: "EnvironInfo",
+			callback: func() {
+				// As environ was set to dead 12hrs earlier, assert that the
+				// undertaker picks up where it left off and RemoveEnviron
+				// is called 12hrs later.
+				mClock.Advance(12 * time.Hour)
+			},
+		}, {
+			call: "RemoveEnviron",
+			callback: func() {
+				c.Assert(client.mockEnviron.Removed, gc.Equals, true)
+			}},
+		} {
+			select {
+			case call := <-client.calls:
+				c.Assert(call, gc.Equals, test.call)
+				if test.callback != nil {
+					test.callback()
+				}
+			case <-time.After(testing.LongWait):
+				c.Fatalf("timed out waiting for API call: %q", test.call)
 			}
-		case <-time.After(testing.LongWait):
-			c.Fatalf("timed out waiting for API call: %q", test.call)
 		}
-	}
+	}()
+
+	worker := undertaker.NewUndertaker(client, mClock)
+	defer worker.Kill()
+
+	wg.Wait()
 
 	select {
 	case call := <-client.calls:
