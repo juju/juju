98eb118113d840b12c38b582b2e6d51429ddedf3
Menno Smits <menno.smits@canonical.com>
2015-12-21 22:07:47 +1300

    Base the logger worker on PostUpgradeManifoldConfig
    ... instead of AgentApiManifold. This way the manifold can be used
    both with the unit and machine agents. Under the unit agent the
    upgrade wait is skipped (util.UpgradeWaitNotRequired).

--- cmd/jujud/agent/unit/manifolds.go
+++ cmd/jujud/agent/unit/manifolds.go
@@ -25,6 +25,7 @@ import (
 	"github.com/juju/juju/worker/rsyslog"
 	"github.com/juju/juju/worker/uniter"
 	"github.com/juju/juju/worker/upgrader"
+	"github.com/juju/juju/worker/util"
 )
 
 // ManifoldsConfig allows specialisation of the result of Manifolds.
@@ -92,8 +93,9 @@ func Manifolds(config ManifoldsConfig) dependency.Manifolds {
 		// according to changes in environment config. We should only need
 		// one of these in a consolidated agent.
 		LoggingConfigUpdaterName: logger.Manifold(logger.ManifoldConfig{
-			AgentName:     AgentName,
-			APICallerName: APICallerName,
+			AgentName:         AgentName,
+			APICallerName:     APICallerName,
+			UpgradeWaiterName: util.UpgradeWaitNotRequired,
 		}),
 
 		// The api address updater is a leaf worker that rewrites agent config
