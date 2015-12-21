c7879750f2161c8c45a2d1abb8dbeffe62cb8976
Andrew Wilkins <axwalk@gmail.com>
2015-12-21 10:02:40 +1000

    upgrades: remove ugrade steps before 1.26
    We will be requiring users to upgrade from
    1.25 to 1.26 (2.0), so there is no need for
    the older upgrade steps anymore.

--- upgrades/upgrade_test.go
+++ upgrades/upgrade_test.go
@@ -668,13 +668,13 @@ func (s *upgradeSuite) TestUpgradeOperationsOrdered(c *gc.C) {
 func (s *upgradeSuite) TestStateUpgradeOperationsVersions(c *gc.C) {
 	versions := extractUpgradeVersions(c, (*upgrades.StateUpgradeOperations)())
 	c.Assert(versions, gc.DeepEquals, []string{
-		"1.18.0", "1.21.0", "1.22.0", "1.23.0", "1.24.0", "1.24.4", "1.25.0", "1.26.0",
+		"1.26.0",
 	})
 }
 
 func (s *upgradeSuite) TestUpgradeOperationsVersions(c *gc.C) {
 	versions := extractUpgradeVersions(c, (*upgrades.UpgradeOperations)())
-	c.Assert(versions, gc.DeepEquals, []string{"1.18.0", "1.22.0", "1.23.0", "1.24.0", "1.25.0", "1.26.0"})
+	c.Assert(versions, gc.DeepEquals, []string{"1.26.0"})
 }
 
 func extractUpgradeVersions(c *gc.C, ops []upgrades.Operation) []string {
