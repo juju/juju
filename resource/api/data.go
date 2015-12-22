36479976c318259acc630f7dbc3d8dfcfeb9f199
Eric Snow <ericsnowcurrently@gmail.com>
2015-12-22 12:23:18 -0700

    Add API data and helpers for listing resources.

--- resource/api/data.go
+++ resource/api/data.go
@@ -3,13 +3,81 @@
 
 package api
 
+// TODO(ericsnow) Eliminate the dependence on apiserver if possible.
+
 import (
 	"time"
+
+	"github.com/juju/errors"
+	"github.com/juju/names"
+
+	"github.com/juju/juju/apiserver/common"
+	"github.com/juju/juju/apiserver/params"
 )
+
+// ListResourcesArgs are the arguments for the ListResources endpoint.
+type ListResourcesArgs params.Entities
+
+// NewListResourcesArgs returns the arguments for the ListResources endpoint.
+func NewListResourcesArgs(services ...string) (ListResourcesArgs, error) {
+	var args ListResourcesArgs
+	for _, service := range services {
+		if !names.IsValidService(service) {
+			return args, errors.Errorf("invalid service %q", service)
+		}
+
+		args.Entities = append(args.Entities, params.Entity{
+			Tag: names.NewServiceTag(service).String(),
+		})
+	}
+	return args, nil
+}
+
+// ResourcesResults holds the resources that result
+// from a bulk API call.
+type ResourcesResults struct {
+	// Results is the list of resource results.
+	Results []ResourcesResult
+}
+
+// ResourcesResult holds the resources that result from an API call
+// for a single service.
+type ResourcesResult struct {
+	params.ErrorResult
+
+	// Resources is the list of resources for the service.
+	Resources []Resource
+}
+
+// NewResourcesResult produces a ResourcesResult for the given service
+// tag. The corresponding service ID is also returned. If any error
+// results, it is stored in the Error field of the result.
+func NewResourcesResult(tagStr string) (ResourcesResult, string) {
+	var result ResourcesResult
+
+	serviceID, err := ServiceTag2ID(tagStr)
+	if err != nil {
+		result.Error = &params.Error{
+			Message: err.Error(),
+			Code:    params.CodeBadRequest,
+		}
+		return result, ""
+	}
+
+	return result, serviceID
+}
+
+// SetResultError sets the error on the result.
+func SetResultError(result *ResourcesResult, err error) {
+	result.Error = common.ServerError(err)
+}
 
 // Resource contains info about a Resource.
 type Resource struct {
 	CharmResource
+
+	// Charm identifies the resource's charm.
+	Charm names.CharmTag
 
 	// Username is the ID of the user that added the revision
 	// to the model (whether implicitly or explicitly).
--- resource/api/helpers_test.go
+++ resource/api/helpers_test.go
@@ -6,11 +6,13 @@ package api_test
 import (
 	"time"
 
+	"github.com/juju/errors"
 	"github.com/juju/testing"
 	jc "github.com/juju/testing/checkers"
 	gc "gopkg.in/check.v1"
 	charmresource "gopkg.in/juju/charm.v6-unstable/resource"
 
+	"github.com/juju/juju/apiserver/params"
 	"github.com/juju/juju/resource"
 	"github.com/juju/juju/resource/api"
 )
@@ -65,6 +67,108 @@ func (helpersSuite) TestResource2API(c *gc.C) {
 		Username:  "a-user",
 		Timestamp: now,
 	})
+}
+
+func (helpersSuite) TestAPIResult2ResourcesOkay(c *gc.C) {
+	fp, err := charmresource.NewFingerprint([]byte(fingerprint))
+	c.Assert(err, jc.ErrorIsNil)
+	now := time.Now()
+	expected := resource.Resource{
+		Resource: charmresource.Resource{
+			Meta: charmresource.Meta{
+				Name:    "spam",
+				Type:    charmresource.TypeFile,
+				Path:    "spam.tgz",
+				Comment: "you need it",
+			},
+			Origin:      charmresource.OriginUpload,
+			Revision:    1,
+			Fingerprint: fp,
+		},
+		Username:  "a-user",
+		Timestamp: now,
+	}
+	err = expected.Validate()
+	c.Assert(err, jc.ErrorIsNil)
+	apiRes := api.Resource{
+		CharmResource: api.CharmResource{
+			Name:        "spam",
+			Type:        "file",
+			Path:        "spam.tgz",
+			Comment:     "you need it",
+			Origin:      "upload",
+			Revision:    1,
+			Fingerprint: []byte(fingerprint),
+		},
+		Username:  "a-user",
+		Timestamp: now,
+	}
+
+	resources, err := api.APIResult2Resources(api.ResourcesResult{
+		Resources: []api.Resource{
+			apiRes,
+		},
+	})
+	c.Assert(err, jc.ErrorIsNil)
+
+	c.Check(resources, jc.DeepEquals, []resource.Resource{
+		expected,
+	})
+}
+
+func (helpersSuite) TestAPIResult2ResourcesFailure(c *gc.C) {
+	apiRes := api.Resource{
+		CharmResource: api.CharmResource{
+			Name:        "spam",
+			Type:        "file",
+			Path:        "spam.tgz",
+			Origin:      "upload",
+			Revision:    1,
+			Fingerprint: []byte(fingerprint),
+		},
+	}
+	failure := errors.New("<failure>")
+
+	_, err := api.APIResult2Resources(api.ResourcesResult{
+		ErrorResult: params.ErrorResult{
+			Error: &params.Error{
+				Message: failure.Error(),
+			},
+		},
+		Resources: []api.Resource{
+			apiRes,
+		},
+	})
+
+	c.Check(err, gc.ErrorMatches, "<failure>")
+	c.Check(errors.Cause(err), gc.Not(gc.Equals), failure)
+}
+
+func (helpersSuite) TestAPIResult2ResourcesNotFound(c *gc.C) {
+	apiRes := api.Resource{
+		CharmResource: api.CharmResource{
+			Name:        "spam",
+			Type:        "file",
+			Path:        "spam.tgz",
+			Origin:      "upload",
+			Revision:    1,
+			Fingerprint: []byte(fingerprint),
+		},
+	}
+
+	_, err := api.APIResult2Resources(api.ResourcesResult{
+		ErrorResult: params.ErrorResult{
+			Error: &params.Error{
+				Message: `service "a-service" not found`,
+				Code:    params.CodeNotFound,
+			},
+		},
+		Resources: []api.Resource{
+			apiRes,
+		},
+	})
+
+	c.Check(err, jc.Satisfies, errors.IsNotFound)
 }
 
 func (helpersSuite) TestAPI2Resource(c *gc.C) {
