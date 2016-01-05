0ec2cd0ec6bb08db51104cda472ff2257cf9e0cd
Dimiter Naydenov <dimiter.naydenov@canonical.com>
2016-01-05 17:56:44 +0200

    Extra whitespace dropped

--- provider/maas/interfaces_test.go
+++ provider/maas/interfaces_test.go
@@ -29,223 +29,223 @@ func (s *interfacesSuite) TestParseInterfacesBadJSON(c *gc.C) {
 func (s *interfacesSuite) TestParseInterfacesExampleJSON(c *gc.C) {
 
 	const exampleJSON = `
-    [
-        {
-            "name": "eth0", 
-            "links": [
-                {
-                    "subnet": {
-                        "dns_servers": [], 
-                        "name": "pxe", 
-                        "space": "default", 
-                        "vlan": {
-                            "name": "untagged", 
-                            "vid": 0, 
-                            "mtu": 1500, 
-                            "fabric": "managed", 
-                            "id": 5001, 
-                            "resource_uri": "/MAAS/api/1.0/vlans/5001/"
-                        }, 
-                        "gateway_ip": "10.20.19.2", 
-                        "cidr": "10.20.19.0/24", 
-                        "id": 3, 
-                        "resource_uri": "/MAAS/api/1.0/subnets/3/"
-                    }, 
-                    "ip_address": "10.20.19.103", 
-                    "id": 436, 
-                    "mode": "static"
-                }
-            ], 
-            "tags": [], 
-            "vlan": {
-                "name": "untagged", 
-                "vid": 0, 
-                "mtu": 1500, 
-                "fabric": "managed", 
-                "id": 5001, 
-                "resource_uri": "/MAAS/api/1.0/vlans/5001/"
-            }, 
-            "enabled": true, 
-            "id": 91, 
-            "discovered": [
-                {
-                    "subnet": {
-                        "dns_servers": [], 
-                        "name": "pxe", 
-                        "space": "default", 
-                        "vlan": {
-                            "name": "untagged", 
-                            "vid": 0, 
-                            "mtu": 1500, 
-                            "fabric": "managed", 
-                            "id": 5001, 
-                            "resource_uri": "/MAAS/api/1.0/vlans/5001/"
-                        }, 
-                        "gateway_ip": "10.20.19.2", 
-                        "cidr": "10.20.19.0/24", 
-                        "id": 3, 
-                        "resource_uri": "/MAAS/api/1.0/subnets/3/"
-                    }, 
-                    "ip_address": "10.20.19.20"
-                }
-            ], 
-            "mac_address": "52:54:00:70:9b:fe", 
-            "parents": [], 
-            "effective_mtu": 1500, 
-            "params": {}, 
-            "type": "physical", 
-            "children": [
-                "eth0.100", 
-                "eth0.250", 
-                "eth0.50"
-            ], 
-            "resource_uri": "/MAAS/api/1.0/nodes/node-18489434-9eb0-11e5-bdef-00163e40c3b6/interfaces/91/"
-        }, 
-        {
-            "name": "eth0.50", 
-            "links": [
-                {
-                    "subnet": {
-                        "dns_servers": [], 
-                        "name": "admin", 
-                        "space": "admin", 
-                        "vlan": {
-                            "name": "admin", 
-                            "vid": 50, 
-                            "mtu": 1500, 
-                            "fabric": "managed", 
-                            "id": 5004, 
-                            "resource_uri": "/MAAS/api/1.0/vlans/5004/"
-                        }, 
-                        "gateway_ip": "10.50.19.2", 
-                        "cidr": "10.50.19.0/24", 
-                        "id": 5, 
-                        "resource_uri": "/MAAS/api/1.0/subnets/5/"
-                    }, 
-                    "ip_address": "10.50.19.103", 
-                    "id": 517, 
-                    "mode": "static"
-                }
-            ], 
-            "tags": [], 
-            "vlan": {
-                "name": "admin", 
-                "vid": 50, 
-                "mtu": 1500, 
-                "fabric": "managed", 
-                "id": 5004, 
-                "resource_uri": "/MAAS/api/1.0/vlans/5004/"
-            }, 
-            "enabled": true, 
-            "id": 150, 
-            "discovered": null, 
-            "mac_address": "52:54:00:70:9b:fe", 
-            "parents": [
-                "eth0"
-            ], 
-            "effective_mtu": 1500, 
-            "params": {}, 
-            "type": "vlan", 
-            "children": [], 
-            "resource_uri": "/MAAS/api/1.0/nodes/node-18489434-9eb0-11e5-bdef-00163e40c3b6/interfaces/150/"
-        }, 
-        {
-            "name": "eth0.100", 
-            "links": [
-                {
-                    "subnet": {
-                        "dns_servers": [], 
-                        "name": "public", 
-                        "space": "public", 
-                        "vlan": {
-                            "name": "public", 
-                            "vid": 100, 
-                            "mtu": 1500, 
-                            "fabric": "managed", 
-                            "id": 5005, 
-                            "resource_uri": "/MAAS/api/1.0/vlans/5005/"
-                        }, 
-                        "gateway_ip": "10.100.19.2", 
-                        "cidr": "10.100.19.0/24", 
-                        "id": 6, 
-                        "resource_uri": "/MAAS/api/1.0/subnets/6/"
-                    }, 
-                    "ip_address": "10.100.19.103", 
-                    "id": 519, 
-                    "mode": "static"
-                }
-            ], 
-            "tags": [], 
-            "vlan": {
-                "name": "public", 
-                "vid": 100, 
-                "mtu": 1500, 
-                "fabric": "managed", 
-                "id": 5005, 
-                "resource_uri": "/MAAS/api/1.0/vlans/5005/"
-            }, 
-            "enabled": true, 
-            "id": 151, 
-            "discovered": null, 
-            "mac_address": "52:54:00:70:9b:fe", 
-            "parents": [
-                "eth0"
-            ], 
-            "effective_mtu": 1500, 
-            "params": {}, 
-            "type": "vlan", 
-            "children": [], 
-            "resource_uri": "/MAAS/api/1.0/nodes/node-18489434-9eb0-11e5-bdef-00163e40c3b6/interfaces/151/"
-        }, 
-        {
-            "name": "eth0.250", 
-            "links": [
-                {
-                    "subnet": {
-                        "dns_servers": [], 
-                        "name": "storage", 
-                        "space": "storage", 
-                        "vlan": {
-                            "name": "storage", 
-                            "vid": 250, 
-                            "mtu": 1500, 
-                            "fabric": "managed", 
-                            "id": 5008, 
-                            "resource_uri": "/MAAS/api/1.0/vlans/5008/"
-                        }, 
-                        "gateway_ip": "10.250.19.2", 
-                        "cidr": "10.250.19.0/24", 
-                        "id": 8, 
-                        "resource_uri": "/MAAS/api/1.0/subnets/8/"
-                    }, 
-                    "ip_address": "10.250.19.103", 
-                    "id": 523, 
-                    "mode": "static"
-                }
-            ], 
-            "tags": [], 
-            "vlan": {
-                "name": "storage", 
-                "vid": 250, 
-                "mtu": 1500, 
-                "fabric": "managed", 
-                "id": 5008, 
-                "resource_uri": "/MAAS/api/1.0/vlans/5008/"
-            }, 
-            "enabled": true, 
-            "id": 152, 
-            "discovered": null, 
-            "mac_address": "52:54:00:70:9b:fe", 
-            "parents": [
-                "eth0"
-            ], 
-            "effective_mtu": 1500, 
-            "params": {}, 
-            "type": "vlan", 
-            "children": [], 
-            "resource_uri": "/MAAS/api/1.0/nodes/node-18489434-9eb0-11e5-bdef-00163e40c3b6/interfaces/152/"
-        }
-    ]`
+	[
+		{
+			"name": "eth0",
+			"links": [
+				{
+					"subnet": {
+						"dns_servers": [],
+						"name": "pxe",
+						"space": "default",
+						"vlan": {
+							"name": "untagged",
+							"vid": 0,
+							"mtu": 1500,
+							"fabric": "managed",
+							"id": 5001,
+							"resource_uri": "/MAAS/api/1.0/vlans/5001/"
+						},
+						"gateway_ip": "10.20.19.2",
+						"cidr": "10.20.19.0/24",
+						"id": 3,
+						"resource_uri": "/MAAS/api/1.0/subnets/3/"
+					},
+					"ip_address": "10.20.19.103",
+					"id": 436,
+					"mode": "static"
+				}
+			],
+			"tags": [],
+			"vlan": {
+				"name": "untagged",
+				"vid": 0,
+				"mtu": 1500,
+				"fabric": "managed",
+				"id": 5001,
+				"resource_uri": "/MAAS/api/1.0/vlans/5001/"
+			},
+			"enabled": true,
+			"id": 91,
+			"discovered": [
+				{
+					"subnet": {
+						"dns_servers": [],
+						"name": "pxe",
+						"space": "default",
+						"vlan": {
+							"name": "untagged",
+							"vid": 0,
+							"mtu": 1500,
+							"fabric": "managed",
+							"id": 5001,
+							"resource_uri": "/MAAS/api/1.0/vlans/5001/"
+						},
+						"gateway_ip": "10.20.19.2",
+						"cidr": "10.20.19.0/24",
+						"id": 3,
+						"resource_uri": "/MAAS/api/1.0/subnets/3/"
+					},
+					"ip_address": "10.20.19.20"
+				}
+			],
+			"mac_address": "52:54:00:70:9b:fe",
+			"parents": [],
+			"effective_mtu": 1500,
+			"params": {},
+			"type": "physical",
+			"children": [
+				"eth0.100",
+				"eth0.250",
+				"eth0.50"
+			],
+			"resource_uri": "/MAAS/api/1.0/nodes/node-18489434-9eb0-11e5-bdef-00163e40c3b6/interfaces/91/"
+		},
+		{
+			"name": "eth0.50",
+			"links": [
+				{
+					"subnet": {
+						"dns_servers": [],
+						"name": "admin",
+						"space": "admin",
+						"vlan": {
+							"name": "admin",
+							"vid": 50,
+							"mtu": 1500,
+							"fabric": "managed",
+							"id": 5004,
+							"resource_uri": "/MAAS/api/1.0/vlans/5004/"
+						},
+						"gateway_ip": "10.50.19.2",
+						"cidr": "10.50.19.0/24",
+						"id": 5,
+						"resource_uri": "/MAAS/api/1.0/subnets/5/"
+					},
+					"ip_address": "10.50.19.103",
+					"id": 517,
+					"mode": "static"
+				}
+			],
+			"tags": [],
+			"vlan": {
+				"name": "admin",
+				"vid": 50,
+				"mtu": 1500,
+				"fabric": "managed",
+				"id": 5004,
+				"resource_uri": "/MAAS/api/1.0/vlans/5004/"
+			},
+			"enabled": true,
+			"id": 150,
+			"discovered": null,
+			"mac_address": "52:54:00:70:9b:fe",
+			"parents": [
+				"eth0"
+			],
+			"effective_mtu": 1500,
+			"params": {},
+			"type": "vlan",
+			"children": [],
+			"resource_uri": "/MAAS/api/1.0/nodes/node-18489434-9eb0-11e5-bdef-00163e40c3b6/interfaces/150/"
+		},
+		{
+			"name": "eth0.100",
+			"links": [
+				{
+					"subnet": {
+						"dns_servers": [],
+						"name": "public",
+						"space": "public",
+						"vlan": {
+							"name": "public",
+							"vid": 100,
+							"mtu": 1500,
+							"fabric": "managed",
+							"id": 5005,
+							"resource_uri": "/MAAS/api/1.0/vlans/5005/"
+						},
+						"gateway_ip": "10.100.19.2",
+						"cidr": "10.100.19.0/24",
+						"id": 6,
+						"resource_uri": "/MAAS/api/1.0/subnets/6/"
+					},
+					"ip_address": "10.100.19.103",
+					"id": 519,
+					"mode": "static"
+				}
+			],
+			"tags": [],
+			"vlan": {
+				"name": "public",
+				"vid": 100,
+				"mtu": 1500,
+				"fabric": "managed",
+				"id": 5005,
+				"resource_uri": "/MAAS/api/1.0/vlans/5005/"
+			},
+			"enabled": true,
+			"id": 151,
+			"discovered": null,
+			"mac_address": "52:54:00:70:9b:fe",
+			"parents": [
+				"eth0"
+			],
+			"effective_mtu": 1500,
+			"params": {},
+			"type": "vlan",
+			"children": [],
+			"resource_uri": "/MAAS/api/1.0/nodes/node-18489434-9eb0-11e5-bdef-00163e40c3b6/interfaces/151/"
+		},
+		{
+			"name": "eth0.250",
+			"links": [
+				{
+					"subnet": {
+						"dns_servers": [],
+						"name": "storage",
+						"space": "storage",
+						"vlan": {
+							"name": "storage",
+							"vid": 250,
+							"mtu": 1500,
+							"fabric": "managed",
+							"id": 5008,
+							"resource_uri": "/MAAS/api/1.0/vlans/5008/"
+						},
+						"gateway_ip": "10.250.19.2",
+						"cidr": "10.250.19.0/24",
+						"id": 8,
+						"resource_uri": "/MAAS/api/1.0/subnets/8/"
+					},
+					"ip_address": "10.250.19.103",
+					"id": 523,
+					"mode": "static"
+				}
+			],
+			"tags": [],
+			"vlan": {
+				"name": "storage",
+				"vid": 250,
+				"mtu": 1500,
+				"fabric": "managed",
+				"id": 5008,
+				"resource_uri": "/MAAS/api/1.0/vlans/5008/"
+			},
+			"enabled": true,
+			"id": 152,
+			"discovered": null,
+			"mac_address": "52:54:00:70:9b:fe",
+			"parents": [
+				"eth0"
+			],
+			"effective_mtu": 1500,
+			"params": {},
+			"type": "vlan",
+			"children": [],
+			"resource_uri": "/MAAS/api/1.0/nodes/node-18489434-9eb0-11e5-bdef-00163e40c3b6/interfaces/152/"
+		}
+	]`
 
 	vlan0 := maasVLAN{
 		ID:          5001,
