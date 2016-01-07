aaa8bdc0708c7cb52f14831dca16a42c2ee95b45
Andrew McDermott <andrew.mcdermott@canonical.com>
2016-01-07 14:29:07 +0000

    Revert "Merge pull request #3877 from dimitern/lp-1520199-maas-1.25-follow-up"
    This reverts commit b9c3c1dcbe364226deaa8ca2e800d98d96f3e60d, reversing
    changes made to a0101e743e84c6b10fb984cbff884230dae12e4f.

--- provider/maas/environ.go
+++ provider/maas/environ.go
@@ -116,7 +116,7 @@ func reserveIPAddressOnDevice(devices gomaasapi.MAASObject, deviceId string, add
 			// We only need the first address, but we're logging all we got.
 			firstAddress = network.NewAddress(value)
 		}
-		logger.Debugf("reserved address %q for device %q", value, deviceId)
+		logger.Debugf("reserved address %q for device %q", value)
 	}
 	return firstAddress, nil
 }
