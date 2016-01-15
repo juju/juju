2c0133b050166251bdfaabeefa15872581a6bf53
Eric Snow <ericsnowcurrently@gmail.com>
2016-01-15 15:22:27 -0700

    Add SerializeCharmResource() and Serialized.DeserializeCharm().

--- resource/serialization.go
+++ resource/serialization.go
@@ -31,6 +31,47 @@ type Serialized struct {
 // Serialize converts the given resource into a serialized
 // equivalent. No validation is performed.
 func Serialize(res Resource) Serialized {
+	chSerialized := SerializeCharmResource(res.Resource)
+	return Serialized{
+		Name:    chSerialized.Name,
+		Type:    chSerialized.Type,
+		Path:    chSerialized.Path,
+		Comment: chSerialized.Comment,
+
+		Origin:      chSerialized.Origin,
+		Revision:    chSerialized.Revision,
+		Fingerprint: chSerialized.Fingerprint,
+		Size:        chSerialized.Size,
+
+		Username:  res.Username,
+		Timestamp: res.Timestamp,
+	}
+}
+
+// Deserialize converts the serialized resource back into a Resource.
+// "placeholder" resources are treated appropriately.
+func (s Serialized) Deserialize() (Resource, error) {
+	chRes, err := s.DeserializeCharm()
+	if err != nil {
+		return Resource{}, errors.Trace(err)
+	}
+	res := Resource{
+		Resource:  chRes,
+		Username:  s.Username,
+		Timestamp: s.Timestamp,
+	}
+
+	if err := res.Validate(); err != nil {
+		return res, errors.Trace(err)
+	}
+	return res, nil
+}
+
+// TODO(ericsnow) Move these to the charm repo.
+
+// SerializeCharmResource converts the charm resource info into
+// the uniform serialized format.
+func SerializeCharmResource(res resource.Resource) Serialized {
 	return Serialized{
 		Name:    res.Name,
 		Type:    res.Type.String(),
@@ -41,28 +82,12 @@ func Serialize(res Resource) Serialized {
 		Revision:    res.Revision,
 		Fingerprint: res.Fingerprint.Bytes(),
 		Size:        res.Size,
-
-		Username:  res.Username,
-		Timestamp: res.Timestamp,
 	}
 }
 
-// Deserialize converts the serialized resource back into a Resource.
-// "placeholder" resources are treated appropriately.
-func (s Serialized) Deserialize() (Resource, error) {
-	res, err := s.deserialize()
-	if err != nil {
-		return res, errors.Trace(err)
-	}
-
-	if err := res.Validate(); err != nil {
-		return res, errors.Trace(err)
-	}
-	return res, nil
-}
-
-func (sr Serialized) deserialize() (Resource, error) {
-	var res Resource
+// DeserializeCharm converts the serialized resource into a charm.Resource.
+func (sr Serialized) DeserializeCharm() (resource.Resource, error) {
+	var res resource.Resource
 
 	resType, err := resource.ParseType(sr.Type)
 	if err != nil {
@@ -74,8 +99,8 @@ func (sr Serialized) deserialize() (Resource, error) {
 		return res, errors.Trace(err)
 	}
 
-	// The fingerprint is the only "placeholder" field we have to
-	// treat specially.
+	// The fingerprint is the only field where we have to special-case
+	// the zero value.
 	var fp resource.Fingerprint
 	if len(sr.Fingerprint) != 0 {
 		fp, err = resource.NewFingerprint(sr.Fingerprint)
@@ -84,21 +109,17 @@ func (sr Serialized) deserialize() (Resource, error) {
 		}
 	}
 
-	res = Resource{
-		Resource: resource.Resource{
-			Meta: resource.Meta{
-				Name:    sr.Name,
-				Type:    resType,
-				Path:    sr.Path,
-				Comment: sr.Comment,
-			},
-			Origin:      origin,
-			Revision:    sr.Revision,
-			Fingerprint: fp,
-			Size:        sr.Size,
+	res = resource.Resource{
+		Meta: resource.Meta{
+			Name:    sr.Name,
+			Type:    resType,
+			Path:    sr.Path,
+			Comment: sr.Comment,
 		},
-		Username:  sr.Username,
-		Timestamp: sr.Timestamp,
+		Origin:      origin,
+		Revision:    sr.Revision,
+		Fingerprint: fp,
+		Size:        sr.Size,
 	}
 
 	return res, nil
