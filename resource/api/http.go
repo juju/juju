4ba8bd83b2c384765d46a1372bdf899874e39efc
Eric Snow <ericsnowcurrently@gmail.com>
2016-01-19 15:18:32 -0700

    Add doc comments for HTTP helper code.

--- resource/api/http.go
+++ resource/api/http.go
@@ -118,18 +118,39 @@ func ExtractUploadRequest(req *http.Request) (service, name string, size int64, 
 	return service, name, size, fp, nil
 }
 
+// NewHTTPDownloadRequest creates a new HTTP download request
+// for the given resource.
+//
+// Intended for use on the client side.
+func NewHTTPDownloadRequest(resourceName string) (*http.Request, error) {
+	return http.NewRequest("GET", "/resources/"+resourceName, nil)
+}
+
+// ExtractDownloadRequest pulls the download request info out of the
+// given HTTP request.
+//
+// Intended for use on the server side.
+func ExtractDownloadRequest(req *http.Request) string {
+	return req.URL.Query().Get(":resource")
+}
+
+// UpdateDownloadResponse sets the appropriate headers in the response
+// to an HTTP download request.
+//
+// Intended for use on the server side.
 func UpdateDownloadResponse(resp http.ResponseWriter, resource resource.Resource) {
 	resp.Header().Set("Content-Type", ContentTypeRaw)
 	resp.Header().Set("Content-Length", fmt.Sprint(resource.Size))
 	resp.Header().Set("Content-Sha384", resource.Fingerprint.String())
 }
 
-func NewHTTPDownloadRequest(resourceName string) (*http.Request, error) {
-	return http.NewRequest("GET", "/resources/"+resourceName, nil)
-}
+// ExtractDownloadResponse pulls the download size and checksum
+// from the HTTP response.
+func ExtractDownloadResponse(resp *http.Response) (int64, charmresource.Fingerprint, error) {
+	var fp charmresource.Fingerprint
 
-func ExtractDownloadRequest(req *http.Request) string {
-	return req.URL.Query().Get(":resource")
+	// TODO(ericsnow) Finish!
+	return 0, fp, errors.New("not finished")
 }
 
 // TODO(ericsnow) These are copied from apiserver/httpcontext.go...
