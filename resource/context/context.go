e34e0c73ced2c06921c1b85a380e37471bef3af7
Eric Snow <ericsnowcurrently@gmail.com>
2016-01-19 17:32:46 -0700

    Re-organize around top-level operations.

--- resource/context/context.go
+++ resource/context/context.go
@@ -38,6 +38,9 @@ func NewContextAPI(apiClient APIClient, dataDir string) *Context {
 	return &Context{
 		apiClient: apiClient,
 		dataDir:   dataDir,
+
+		createResourceFile: createResourceFile,
+		writeResource:      writeResource,
 	}
 }
 
@@ -45,6 +48,9 @@ func NewContextAPI(apiClient APIClient, dataDir string) *Context {
 type Context struct {
 	apiClient APIClient
 	dataDir   string
+
+	createResourceFile func(path string) (io.WriteCloser, error)
+	writeResource      func(io.Writer, io.Reader) (int64, charmresource.Fingerprint, error)
 }
 
 // DownloadResource downloads the named resource and returns the path
@@ -53,31 +59,13 @@ type Context struct {
 //
 // Note that the downloaded file is checked for correctness.
 func (c *Context) DownloadResource(name string) (string, error) {
+	// TODO(katco): Potential race-condition: two commands running at
+	// once.
 	// TODO(katco): Check to see if we have latest version
 
-	checksumWriter := charmresource.NewFingerprintHash()
-
-	resourceInfo, resourceReader, err := c.apiClient.GetResource(name)
+	resourcePath, _, err := c.downloadResource(name)
 	if err != nil {
 		return "", errors.Trace(err)
-	}
-	defer resourceReader.Close()
-
-	hashingReader := hash.NewHashingReader(resourceReader, checksumWriter)
-	sizingReader := utils.NewSizingReader(hashingReader)
-
-	resourcePath := resolveResourcePath(c.unitDataDirPath, resourceInfo)
-	if err := downloadAndWriteResourceToDisk(resourcePath, sizingReader); err != nil {
-		return "", errors.Trace(err)
-	}
-
-	size := sizingReader.Size()
-	if size != resourceInfo.Size {
-		return "", errors.Errorf("resource size does not match expected (%d != %d)", size, resourceInfo.Size)
-	}
-	fp := checksumWriter.Fingerprint()
-	if bytes.Equal(fp.Bytes(), resourceInfo.Fingerprint.Bytes()) == false {
-		return "", errors.Errorf("resource fingerprint does not match expected (%q != %q)", fp, resourceInfo.Fingerprint)
 	}
 
 	return resourcePath, nil
@@ -88,29 +76,94 @@ func (c *Context) Flush() error {
 	return nil
 }
 
+// downloadResource downloads the named resource to the provided path.
+func (c *Context) downloadResource(name string) (string, resource.Resource, error) {
+	// TODO(ericsnow) This needs to be atomic?
+	// (e.g. write to separate dir and move the dir into place)
+
+	info, resourceReader, err := c.apiClient.GetResource(name)
+	if err != nil {
+		return "", resource.Resource{}, errors.Trace(err)
+	}
+	defer resourceReader.Close()
+
+	resourcePath := resolveResourcePath(c.dataDir, info)
+
+	target, err := c.createResourceFile(resourcePath)
+	if err != nil {
+		return "", resource.Resource{}, errors.Trace(err)
+	}
+	defer target.Close()
+
+	content := resourceContent{
+		data:        resourceReader,
+		size:        info.Size,
+		fingerprint: info.Fingerprint,
+	}
+	size, fp, err := c.writeResource(target, content.data)
+	if err != nil {
+		return "", resource.Resource{}, errors.Trace(err)
+	}
+
+	if err := content.verify(size, fp); err != nil {
+		return "", resource.Resource{}, errors.Trace(err)
+	}
+
+	return resourcePath, info, nil
+}
+
+// resourceContent holds a reader for the content of a resource along
+// with details about that content.
+type resourceContent struct {
+	data        io.Reader
+	size        int64
+	fingerprint charmresource.Fingerprint
+}
+
+// verify ensures that the actual resource content details match
+// the expected ones.
+func (c resourceContent) verify(size int64, fp charmresource.Fingerprint) error {
+	if size != c.size {
+		return errors.Errorf("resource size does not match expected (%d != %d)", size, c.size)
+	}
+	if !bytes.Equal(fp.Bytes(), c.fingerprint.Bytes()) {
+		return errors.Errorf("resource fingerprint does not match expected (%q != %q)", fp, c.fingerprint)
+	}
+	return nil
+}
+
 // resolveResourcePath returns the full path to the resource.
 func resolveResourcePath(unitPath string, resourceInfo resource.Resource) string {
 	return filepath.Join(unitPath, resourceInfo.Name, resourceInfo.Path)
 }
 
-// downloadAndWriteResourceToDisk stores the provided resource content
-// to disk at the given path.
-func downloadAndWriteResourceToDisk(resourcePath string, resourceReader io.Reader) error {
-	// TODO(ericsnow) This needs to be atomic?
-	// (e.g. write to separate dir and move the dir into place)
-	// TODO(katco): Potential race-condition: two commands running at
-	// once.
-	if err := os.MkdirAll(filepath.Dir(resourcePath), 0755); err != nil {
-		return errors.Annotate(err, "could not create resource dir")
+// createResourceFile creates the file into which a resource's content
+// should be written.
+func createResourceFile(path string) (io.WriteCloser, error) {
+	if err := os.MkdirAll(filepath.Dir(path), 0755); err != nil {
+		return nil, errors.Annotate(err, "could not create resource dir")
 	}
-	resourceHandle, err := os.Create(resourcePath)
+	target, err := os.Create(path)
 	if err != nil {
-		return errors.Annotate(err, "could not create new file for resource")
+		return nil, errors.Annotate(err, "could not create new file for resource")
+	}
+	// TODO(ericsnow) chmod 0644?
+	return target, nil
+}
+
+// writeResource writes the provided (source) resource content
+// to the target. The size and fingerprint are returned.
+func writeResource(target io.Writer, source io.Reader) (size int64, fp charmresource.Fingerprint, err error) {
+	checksumWriter := charmresource.NewFingerprintHash()
+	hashingReader := hash.NewHashingReader(source, checksumWriter)
+	sizingReader := utils.NewSizingReader(hashingReader)
+	source = sizingReader
+
+	if _, err := io.Copy(target, source); err != nil {
+		return size, fp, errors.Annotate(err, "could not write resource to file")
 	}
 
-	if _, err := io.Copy(resourceHandle, resourceReader); err != nil {
-		return errors.Annotate(err, "could not write resource to file")
-	}
-
-	return nil
+	size = sizingReader.Size()
+	fp = checksumWriter.Fingerprint()
+	return size, fp, nil
 }
