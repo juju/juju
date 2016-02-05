0b3c34a224223bc47fa40ce5f44c2267763c38ae
Tim Penhey <tim@penhey.net>
2016-02-05 15:19:58 +1300

    Add a test that checks the read only calls actually exist.

--- apiserver/read_only_calls_test.go
+++ apiserver/read_only_calls_test.go
@@ -4,14 +4,44 @@
 package apiserver
 
 import (
+	"strings"
+
 	jc "github.com/juju/testing/checkers"
 	gc "gopkg.in/check.v1"
+
+	"github.com/juju/juju/apiserver/common"
 )
 
 type readOnlyCallsSuite struct {
 }
 
 var _ = gc.Suite(&readOnlyCallsSuite{})
+
+func (*readOnlyCallsSuite) TestReadOnlyCallsExist(c *gc.C) {
+	// Iterate through the list of readOnlyCalls and make sure
+	// that the facades are reachable.
+	facades := common.Facades.List()
+
+	maxVersion := map[string]int{}
+	for _, facade := range facades {
+		version := 0
+		for _, ver := range facade.Versions {
+			if ver > version {
+				version = ver
+			}
+		}
+		maxVersion[facade.Name] = version
+	}
+
+	for _, name := range readOnlyCalls.Values() {
+		parts := strings.Split(name, ".")
+		facade, method := parts[0], parts[1]
+		version := maxVersion[facade]
+
+		_, _, err := lookupMethod(facade, version, method)
+		c.Check(err, jc.ErrorIsNil)
+	}
+}
 
 func (*readOnlyCallsSuite) TestReadOnlyCall(c *gc.C) {
 	for _, test := range []struct {
