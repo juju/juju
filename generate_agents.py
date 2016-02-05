60642059c79062324052098509c6710d72a3efc8
Aaron Bentley <aaron.bentley@canonical.com>
2016-02-05 15:54:36 -0500

    Remove as much subprocess as possible.

--- generate_agents.py
+++ generate_agents.py
@@ -1,54 +1,65 @@
 #!/usr/bin/env python
 from __future__ import print_function
 
-from argparse import ArgumentParser
+from argparse import (
+    ArgumentParser,
+    Namespace,
+    )
+from datetime import datetime
 import glob
 import os
 import shutil
 import subprocess
+import sys
+from urlparse import (
+    urlsplit,
+    urlunsplit,
+    )
+
+from agent_archive import get_agents
 
 
-# These are the archives that are search for matching releases.
-UBUNTU_ARCH="http://archive.ubuntu.com/ubuntu/pool/universe/j/juju-core/"
-ARM_ARCH="http://ports.ubuntu.com/pool/universe/j/juju-core/"
-PUBLIC_ARCHIVES=[UBUNTU_ARCH, ARM_ARCH]
+# These are the archives that are searched for matching releases.
+UBUNTU_ARCH = "http://archive.ubuntu.com/ubuntu/pool/universe/j/juju-core/"
+ARM_ARCH = "http://ports.ubuntu.com/pool/universe/j/juju-core/"
+PUBLIC_ARCHIVES = [UBUNTU_ARCH, ARM_ARCH]
 
 
-def retrieve_packages(release, upatch, archives, dest_debs, juju_dir,
-                      script_dir):
-    # Retrieve the $RELEASE packages that contain jujud,
-    # or copy a locally built package.
+def retrieve_packages(release, upatch, archives, dest_debs, s3_config):
+    # Retrieve the packages that contain a jujud for this version.
     print("Retrieving juju-core packages from archives")
-    print(subprocess.check_output(["date", "+%Y-%m-%dT%H:%M:%S"]))
+    print(datetime.now().replace(microsecond=0).isoformat())
     os.chdir(dest_debs)
     for archive in archives:
-        proc = subprocess.Popen(
-            ['sed', '-e', 's,//.*@,//,'], stdin=subprocess.PIPE,
-            stdout=subprocess.PIPE)
-        stdout, stderrdata = proc.communicate(archive)
-        safe_archive = stdout.rstrip('\n')
+        scheme, netloc, path, query, fragment = urlsplit(archive)
+        # Strip username / password
+        netloc = netloc.rsplit('@')[-1]
+        safe_archive = urlunsplit((scheme, netloc, path, query, fragment))
         print("checking {} for {}".format(safe_archive, release))
         subprocess.check_call([
             'lftp', '-c', 'mirror', '-I',
             "juju-core_{}*.{}~juj*.deb".format(release, upatch),
             archive])
-    if os.path.isdir(os.path.join(dest_debs, 'juju-core')):
-        found = subprocess.check_output([
-            'find', os.path.join(dest_debs, 'juju-core/'), '-name',
-            "*deb"]).rstrip('\n')
-        if found != "":
-            debs = glob.glob(os.path.join(dest_debs, 'juju-core', '*deb'))
-            for deb in debs:
-                shutil.move(deb, './')
-        shutil.rmtree(os.path.join(dest_debs, 'juju-core'))
-    if os.path.exists(os.path.join(juju_dir, 'juju-qa.s3cfg')):
+    juju_core_dir = os.path.join(dest_debs, 'juju-core')
+    if os.path.isdir(juju_core_dir):
+        debs = glob.glob(os.path.join(juju_core_dir, '*deb'))
+        for deb in debs:
+            shutil.move(deb, './')
+        shutil.rmtree(juju_core_dir)
+    if os.path.exists(s3_config):
         print(
             'checking s3://juju-qa-data/agent-archive for'
             ' {}.'.format(release))
-        subprocess.check_call([
-            os.path.join(script_dir, 'agent_archive.py'), '--config',
-            os.path.join(juju_dir, 'juju-qa.s3cfg'), 'get', release,
-               dest_debs])
+        args = Namespace(
+            version=release, destination=dest_debs, config=s3_config,
+            dry_run=False, verbose=False)
+        try:
+            get_agents(args)
+        except subprocess.CalledProcessError as e:
+            print()
+            sys.stderr.write(e.output)
+            print("FAILED: get_agents() failed.")
+            sys.exit(1)
 
 
 def parse_args():
@@ -73,19 +84,18 @@ def list_ppas(juju_home):
 
 def main():
     args = parse_args()
-    dest_debs=os.path.join(args.destination, 'debs')
+    dest_debs = os.path.join(args.destination, 'debs')
     juju_dir = os.environ.get(
         'JUJU_HOME', os.path.join(os.environ.get('HOME'), '.juju'))
+    s3_config = os.path.join(juju_dir, 'juju-qa.s3cfg')
     archives = list_ppas(juju_dir)
     if archives is None:
         print("Only public archives will be searched.")
         archives = PUBLIC_ARCHIVES
     else:
         print("Searching the build archives.")
-    archives.extend(list_ppas(juju_dir))
-    script_dir = os.path.dirname(__file__)
     retrieve_packages(args.release, args.upatch, archives, dest_debs,
-                      juju_dir, script_dir)
+                      s3_config)
 
 
 if __name__ == '__main__':
