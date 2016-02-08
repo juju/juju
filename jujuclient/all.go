e1797abb3fe38b9c1aa685fa0ae060dd2e522366
anastasia <anastasia.macmood@canonical.com>
2016-02-09 09:52:00 +1000

    Added TODOs for follow up.

--- jujuclient/all.go
+++ jujuclient/all.go
@@ -6,11 +6,15 @@
 
 package jujuclient
 
+// TODO(anastasiamac 2016-02-08) all store methods should hold a file lock -
+// If we don't lock, we're going to end up corrupting files.
 type store struct {
 }
 
 // DefaultControllerStore returns files-based controller store
 // rooted at JujuHome.
+// TODO (anastasiamac 2016-02-08) aim to remove this
+// and instead inject a Cache into commands that need to use it.
 var DefaultControllerStore = func() (ControllerStore, error) {
 	return &store{}, nil
 }
