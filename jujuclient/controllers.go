e7b47a5dd7548ea893cdb492e61dbdf14f136c6e
anastasia <anastasia.macmood@canonical.com>
2016-02-09 18:10:43 +1000

    Use file lock. Copied from existing cache.yaml support code which is heading out.

--- jujuclient/controllers.go
+++ jujuclient/controllers.go
@@ -7,15 +7,28 @@ import (
 	"github.com/juju/errors"
 )
 
+const controllersLockName = "controllers.lock"
+
 // AllControllers implements ControllersGetter.AllControllers.
 // This implementation gets all controllers defined in the controllers file.
 func (f *store) AllControllers() (map[string]ControllerDetails, error) {
+	lock, err := acquireEnvironmentLock(controllersLockName, "read-all-controllers")
+	if err != nil {
+		return nil, errors.Annotate(err, "cannot read all controllers")
+	}
+	defer unlockEnvironmentLock(lock)
 	return ReadControllersFile(JujuControllersPath())
 }
 
 // ControllerByName implements ControllersGetter.ControllerByName.
 func (f *store) ControllerByName(name string) (*ControllerDetails, error) {
-	controllers, err := f.AllControllers()
+	lock, err := acquireEnvironmentLock(controllersLockName, "read-controller-by-name")
+	if err != nil {
+		return nil, errors.Annotatef(err, "cannot read controller %v", name)
+	}
+	defer unlockEnvironmentLock(lock)
+
+	controllers, err := ReadControllersFile(JujuControllersPath())
 	if err != nil {
 		return nil, errors.Trace(err)
 	}
@@ -31,7 +44,14 @@ func (f *store) UpdateController(name string, one ControllerDetails) error {
 	if err := ValidateControllerDetails(name, one); err != nil {
 		return err
 	}
-	all, err := f.AllControllers()
+
+	lock, err := acquireEnvironmentLock(controllersLockName, "update-controller")
+	if err != nil {
+		return errors.Annotatef(err, "cannot update controller %v", name)
+	}
+	defer unlockEnvironmentLock(lock)
+
+	all, err := ReadControllersFile(JujuControllersPath())
 	if err != nil {
 		return errors.Annotate(err, "cannot get controllers")
 	}
@@ -47,7 +67,13 @@ func (f *store) UpdateController(name string, one ControllerDetails) error {
 // RemoveController implements ControllersRemover.RemoveController
 // Once controllers collection is updated, controllers file is written.
 func (f *store) RemoveController(name string) error {
-	all, err := f.AllControllers()
+	lock, err := acquireEnvironmentLock(controllersLockName, "remove-controller")
+	if err != nil {
+		return errors.Annotatef(err, "cannot remove controller %v", name)
+	}
+	defer unlockEnvironmentLock(lock)
+
+	all, err := ReadControllersFile(JujuControllersPath())
 	if err != nil {
 		return errors.Annotate(err, "cannot get controllers")
 	}
