9884bba1db37918304bee48cfae8942e49f4b47f
Nate Finch <nate.finch@gmail.com>
2016-02-10 15:54:53 -0500

    fix merge problem

--- resource/cmd/formatter.go
+++ resource/cmd/formatter.go
@@ -46,7 +46,7 @@ func FormatCharmResource(res charmresource.Resource) FormattedCharmResource {
 		Name:        res.Name,
 		Type:        res.Type.String(),
 		Path:        res.Path,
-		Comment:       res.Comment,
+		Description: res.Description,
 		Revision:    res.Revision,
 		Origin:      res.Origin.String(),
 		Fingerprint: res.Fingerprint.String(), // ...the hex string.
