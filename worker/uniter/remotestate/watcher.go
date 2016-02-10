e240607752986cddfa70b3a9a43d112cf10203ae
Eric Snow <ericsnowcurrently@gmail.com>
2016-02-10 12:49:02 -0700

    Add the CharmModified() method to the uniter service model.

--- worker/uniter/remotestate/watcher.go
+++ worker/uniter/remotestate/watcher.go
@@ -478,9 +478,14 @@ func (w *RemoteStateWatcher) serviceChanged() error {
 	if err != nil {
 		return errors.Trace(err)
 	}
+	touched, err := w.service.CharmModified()
+	if err != nil {
+		return errors.Trace(err)
+	}
 	w.mu.Lock()
 	w.current.CharmURL = url
 	w.current.ForceCharmUpgrade = force
+	w.current.CharmModified = touched
 	w.mu.Unlock()
 	return nil
 }
