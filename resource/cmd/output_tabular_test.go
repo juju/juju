b269bfe9bbffdd722271e05726e959b3a78597d9
Nate Finch <nate.finch@gmail.com>
2016-02-12 10:35:26 -0500

    put back test that shouldn't have been deleted

--- resource/cmd/output_tabular_test.go
+++ resource/cmd/output_tabular_test.go
@@ -213,3 +213,9 @@ openjdk2 charmstore  8
 website2 Bill User   2012-12-12T12:12
 `[1:])
 }
+
+func (s *SvcTabularSuite) TestFormatCharmTabularBadValue(c *gc.C) {
+	bogus := "should have been something else"
+	_, err := FormatSvcTabular(bogus)
+	c.Check(err, gc.ErrorMatches, `unexpected type for data: string`)
+}
