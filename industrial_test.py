95cbdebd2c8dcd8cc4fc8fed0949e551ada0389d
Aaron Bentley <aaron.bentley@canonical.com>
2016-02-16 16:57:16 -0500

    Support new restore method.

--- industrial_test.py
+++ industrial_test.py
@@ -771,8 +771,11 @@ class BackupRestoreAttempt(SteppedStageAttempt):
             yield results
             wait_for_state_server_to_shutdown(host, client, instance_id)
             yield results
-            with client.juju_async('restore', (backup_file,)):
+            client = client.get_restore_client(
+                client.env.environment + '-restore')
+            with client.restore_backup_async(backup_file):
                 yield results
+            yield results
         finally:
             os.unlink(backup_file)
         with wait_for_started(client):
