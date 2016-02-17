275bc2816cbe8b60447ec81e898118b6284998b1
Eric Snow <ericsnowcurrently@gmail.com>
2016-02-16 18:40:10 -0700

    Add fakeUnit.CharmURL().

--- resource/state/resource_test.go
+++ resource/state/resource_test.go
@@ -14,6 +14,7 @@ import (
 	"github.com/juju/testing"
 	jc "github.com/juju/testing/checkers"
 	gc "gopkg.in/check.v1"
+	"gopkg.in/juju/charm.v6-unstable"
 	charmresource "gopkg.in/juju/charm.v6-unstable/resource"
 	"gopkg.in/mgo.v2/txn"
 
@@ -659,4 +660,8 @@ func newUnit(stub *testing.Stub, name string) *resourcetesting.StubUnit {
 		ReturnName:        name,
 		ReturnServiceName: strings.Split(name, "/")[0],
 	}
+}
+
+func (f fakeUnit) CharmURL() (*charm.URL, bool) {
+	return nil, false
 }
