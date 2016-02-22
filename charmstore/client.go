069126b854e75d586acf8fc3a4e5102d8d774199
Eric Snow <ericsnowcurrently@gmail.com>
2016-02-22 10:52:32 -0700

    Drop TestingClient.

--- charmstore/client.go
+++ charmstore/client.go
@@ -12,6 +12,13 @@ import (
 
 // Client exposes the functionality of the charm store, as provided
 // by github.com/juju/charmrepo/csclient.Client.
+//
+// Note that the following csclient.Client methods are used as well,
+// but only in tests:
+//  - Put(path string, val interface{}) error
+//  - UploadCharm(id *charm.URL, ch charm.Charm) (*charm.URL, error)
+//  - UploadCharmWithRevision(id *charm.URL, ch charm.Charm, promulgatedRevision int) error
+//  - UploadBundleWithRevision()
 type Client interface {
 	// TODO(ericsnow) Replace use of Get with use of more specific API methods?
 
@@ -35,35 +42,4 @@ type Client interface {
 	// is ignored. If the identified resource is not in the charm store
 	// then errors.NotFound is returned.
 	GetResource(cURL *charm.URL, resourceName string, revision int) (io.ReadCloser, error)
-}
-
-// TestingClient expands Client with methods needed during testing.
-type TestingClient interface {
-	Client
-
-	// Put makes a PUT request to the given path in the charm store. The
-	// path must have a leading slash, but must not include the host
-	// name or version prefix. The given value is marshaled as JSON to
-	// use as the request body.
-	Put(path string, val interface{}) error
-
-	// UploadCharm uploads the given charm to the charm store with the
-	// given id, which must not specify a revision. The accepted charm
-	// implementations are charm.CharmDir and charm.CharmArchive.
-	//
-	// UploadCharm returns the id that the charm has been given in the
-	// store - this will be the same as id except the revision.
-	UploadCharm(id *charm.URL, ch charm.Charm) (*charm.URL, error)
-
-	// UploadCharmWithRevision uploads the given charm to the given id
-	// in the charm store, which must contain a revision. If
-	// promulgatedRevision is not -1, it specifies that the charm should
-	// be marked as promulgated with that revision.
-	UploadCharmWithRevision(id *charm.URL, ch charm.Charm, promulgatedRevision int) error
-
-	// UploadBundleWithRevision uploads the given bundle to the given id
-	// in the charm store, which must contain a revision. If
-	// promulgatedRevision is not -1, it specifies that the charm should
-	// be marked as promulgated with that revision.
-	UploadBundleWithRevision()
 }
