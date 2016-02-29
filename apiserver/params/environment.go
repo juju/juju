869fb080339a27b76134803a9248d85be25eb5c4
Cheryl Jennings <cheryl.jennings@canonical.com>
2016-02-29 12:05:20 -0600

    Add json tags for SetEnvironAgentVersion struct

--- apiserver/params/environment.go
+++ apiserver/params/environment.go
@@ -50,8 +50,8 @@ type ModifyEnvironUser struct {
 // SetEnvironAgentVersion contains the arguments for
 // SetEnvironAgentVersion client API call.
 type SetEnvironAgentVersion struct {
-	Version             version.Number
-	MajorUpgradeAllowed bool
+	Version             version.Number `json:"version"`
+	MajorUpgradeAllowed bool           `json:"majorupgradeallowed"`
 }
 
 // EnvUserInfo holds information on a user.
