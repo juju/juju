fe45885f549b877bf3ad59d08b551acab1ae03f0
Tim Penhey <tim@penhey.net>
2016-03-02 14:24:34 +1300

    Add a test to make sure we don't get package dependency creep.

--- core/description/package_test.go
+++ core/description/package_test.go
@@ -6,7 +6,10 @@ package description
 import (
 	"testing"
 
+	jc "github.com/juju/testing/checkers"
 	gc "gopkg.in/check.v1"
+
+	coretesting "github.com/juju/juju/testing"
 )
 
 // Useful test constants.
@@ -17,4 +20,15 @@ const gig uint64 = 1024
 // None of the tests in this package require mongo.
 func TestPackage(t *testing.T) {
 	gc.TestingT(t)
+}
+
+type ImportTest struct{}
+
+var _ = gc.Suite(&ImportTest{})
+
+func (*ImportTest) TestImports(c *gc.C) {
+	found := coretesting.FindJujuCoreImports(c, "github.com/juju/juju/core/description")
+
+	// This package only uses 'version' from the main repo.
+	c.Assert(found, jc.SameContents, []string{"version"})
 }
