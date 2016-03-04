b7bc179ea08aaa3385978c13afbcdf638fe00d37
Curtis Hovey <curtis@canonical.com>
2016-03-04 16:58:10 -0500

    Hush stderr.

--- tests/test_assess_recovery.py
+++ tests/test_assess_recovery.py
@@ -92,6 +92,7 @@ def make_mocked_client(name, status_error=None):
     return client
 
 
+@patch('deploy_stack.dump_juju_timings', autospec=True)
 @patch('deploy_stack.dump_env_logs_known_hosts', autospec=True)
 @patch('assess_recovery.parse_new_state_server_from_error', autospec=True,
        return_value='new_host')
@@ -107,7 +108,8 @@ def make_mocked_client(name, status_error=None):
 class TestMain(FakeHomeTestCase):
 
     def test_ha(self, so_mock, cc_mock, co_mock,
-                dns_mock, gc_mock, ds_mock, di_mock, ws_mock, ns_mock, dl_mock):
+                dns_mock, gc_mock, ds_mock, di_mock, ws_mock,
+                ns_mock, dl_mock, djt_mock):
         client = make_mocked_client('foo')
         with patch('assess_recovery.make_client_from_args', autospec=True,
                    return_value=client) as mc_mock:
@@ -131,7 +133,7 @@ class TestMain(FakeHomeTestCase):
 
     def test_ha_error(self, so_mock, cc_mock, co_mock,
                       dns_mock, gc_mock, ds_mock, di_mock,
-                      ws_mock, ns_mock, dl_mock):
+                      ws_mock, ns_mock, dl_mock, djt_mock):
         error = Exception()
         client = make_mocked_client('foo', status_error=error)
         with patch('assess_recovery.make_client_from_args', autospec=True,
@@ -157,7 +159,7 @@ class TestMain(FakeHomeTestCase):
 
     def test_destroy_on_boot_error(self, so_mock, cc_mock, co_mock,
                                    dns_mock, gc_mock, ds_mock, di_mock,
-                                   ws_mock, ns_mock, dl_mock):
+                                   ws_mock, ns_mock, dl_mock, djt_mock):
         client = make_mocked_client('foo')
         with patch('assess_recovery.make_client', autospec=True,
                    return_value=client):
