28b842de701d2f594c3802056ed95732265f3814
Eric Snow <ericsnowcurrently@gmail.com>
2016-03-10 11:31:10 -0700

    Move UnitResources to unit.go.

--- resource/unit.go
+++ resource/unit.go
@@ -4,6 +4,7 @@
 package resource
 
 import (
+	"github.com/juju/names"
 	"gopkg.in/juju/charm.v6-unstable"
 )
 
@@ -19,4 +20,13 @@ type Unit interface {
 
 	// CharmURL returns the unit's charm URL.
 	CharmURL() (*charm.URL, bool)
+}
+
+// UnitResources conains the list of resources used by a unit.
+type UnitResources struct {
+	// Tag is the tag of the unit.
+	Tag names.UnitTag
+
+	// Resources are the resource versions currently in use by this unit.
+	Resources []Resource
 }
