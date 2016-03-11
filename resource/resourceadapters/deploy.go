86c88285b30b0a698f39865975b862e45203be2b
Eric Snow <ericsnowcurrently@gmail.com>
2016-03-11 14:06:52 -0700

    Pass the charm store auth macaroon through the resources-related CLI code.

--- resource/resourceadapters/deploy.go
+++ resource/resourceadapters/deploy.go
@@ -7,6 +7,7 @@ import (
 	"github.com/juju/errors"
 	"gopkg.in/juju/charm.v6-unstable"
 	charmresource "gopkg.in/juju/charm.v6-unstable/resource"
+	"gopkg.in/macaroon.v1"
 
 	"github.com/juju/juju/api"
 	"github.com/juju/juju/resource/api/client"
@@ -16,18 +17,19 @@ import (
 // DeployResources uploads the bytes for the given files to the server and
 // creates pending resource metadata for the all resource mentioned in the
 // metadata. It returns a map of resource name to pending resource IDs.
-func DeployResources(serviceID string, cURL *charm.URL, files map[string]string, resources map[string]charmresource.Meta, conn api.Connection) (ids map[string]string, err error) {
+func DeployResources(serviceID string, cURL *charm.URL, csMac *macaroon.Macaroon, files map[string]string, resources map[string]charmresource.Meta, conn api.Connection) (ids map[string]string, err error) {
 	client, err := newAPIClient(conn)
 	if err != nil {
 		return nil, errors.Trace(err)
 	}
 
 	ids, err = cmd.DeployResources(cmd.DeployResourcesArgs{
-		ServiceID:     serviceID,
-		CharmURL:      cURL,
-		Specified:     files,
-		ResourcesMeta: resources,
-		Client:        &deployClient{client},
+		ServiceID:          serviceID,
+		CharmURL:           cURL,
+		CharmStoreMacaroon: csMac,
+		Specified:          files,
+		ResourcesMeta:      resources,
+		Client:             &deployClient{client},
 	})
 	if err != nil {
 		return nil, errors.Trace(err)
@@ -40,10 +42,11 @@ type deployClient struct {
 }
 
 // AddPendingResources adds pending metadata for store-based resources.
-func (cl *deployClient) AddPendingResources(serviceID string, cURL *charm.URL, resources []charmresource.Resource) ([]string, error) {
+func (cl *deployClient) AddPendingResources(serviceID string, cURL *charm.URL, csMac *macaroon.Macaroon, resources []charmresource.Resource) ([]string, error) {
 	return cl.Client.AddPendingResources(client.AddPendingResourcesArgs{
-		ServiceID: serviceID,
-		CharmURL:  cURL,
-		Resources: resources,
+		ServiceID:          serviceID,
+		CharmURL:           cURL,
+		CharmStoreMacaroon: csMac,
+		Resources:          resources,
 	})
 }
