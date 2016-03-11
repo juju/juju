2b410df0f202565ee90d033547d9948c245f4f68
Dave Cheney <dave@cheney.net>
2016-03-11 11:05:59 +1100

    resource/api/private/server: rename LegacyHTTPHandler to UnitResourceHandler

--- apiserver/resource.go
+++ apiserver/resource.go
@@ -11,7 +11,6 @@ import (
 	"github.com/juju/errors"
 	"github.com/juju/names"
 
-	internalserver "github.com/juju/juju/resource/api/private/server"
 	"github.com/juju/juju/resource/api/server"
 	"github.com/juju/juju/resource/resourceadapters"
 	"github.com/juju/juju/state"
@@ -68,6 +67,6 @@ func newUnitResourceHandler(httpCtxt httpContext) http.Handler {
 	extractor := resourceadapters.HTTPDownloadRequestExtractor{
 		Connector: &resourcesHandlerDeps{httpCtxt},
 	}
-	deps := internalserver.NewLegacyHTTPHandlerDeps(extractor)
-	return internalserver.NewLegacyHTTPHandler(deps)
+	deps := server.NewUnitResourceHandlerDeps(extractor)
+	return server.NewUnitResourceHandler(deps)
 }
