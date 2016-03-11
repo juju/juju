076a3f3473c62b748a92b32ae9aad6dad42762a2
Eric Snow <ericsnowcurrently@gmail.com>
2016-03-11 11:01:25 -0700

    Pass the charm URL to DeployResources().

--- resource/resourceadapters/deploy.go
+++ resource/resourceadapters/deploy.go
@@ -15,13 +15,12 @@ import (
 // DeployResources uploads the bytes for the given files to the server and
 // creates pending resource metadata for the all resource mentioned in the
 // metadata. It returns a map of resource name to pending resource IDs.
-func DeployResources(serviceID string, files map[string]string, resources map[string]charmresource.Meta, conn api.Connection) (ids map[string]string, err error) {
+func DeployResources(serviceID string, cURL *charm.URL, files map[string]string, resources map[string]charmresource.Meta, conn api.Connection) (ids map[string]string, err error) {
 	client, err := newAPIClient(conn)
 	if err != nil {
 		return nil, errors.Trace(err)
 	}
 
-	var cURL *charm.URL
 	ids, err = cmd.DeployResources(cmd.DeployResourcesArgs{
 		ServiceID:     serviceID,
 		CharmURL:      cURL,
