2bf185da9f11568e86dd4a2f800bfe1441ec51da
John Arbash Meinel <john@arbash-meinel.com>
2016-03-23 17:34:04 +0400

    Add direct tests of EnsureImageExists
    We weren't able to directly put lxd.Client.CopyImage into an interface
    because it needs the exact lxd.Client object. But if we wrap it a bit
    deeper we can actually do some testing on the logic inside of
    EnsureImageExists. It is a Stub (mock) sort of test which is fairly tied
    to the specifics, but we really do want to test things like "what do we
    do if we fail to connect to a Remote", and "what if the remote doesn't
    have the Alias".
    
    This doesn't change any functionality (that will be coming in later
    patches), but does make it possible to test things.

--- tools/lxdclient/client_image.go
+++ tools/lxdclient/client_image.go
@@ -11,17 +11,57 @@ import (
 	"github.com/juju/errors"
 	"github.com/juju/loggo"
 	"github.com/lxc/lxd"
-	"github.com/lxc/lxd/shared"
 
 	"github.com/juju/juju/utils/stringforwarder"
 )
 
 type rawImageClient interface {
-	ListAliases() (shared.ImageAliases, error)
+	GetAlias(string) string
+}
+
+type remoteClient interface {
+	URL() string
+	GetAlias(name string) string
+	// This is like lxd.Client.CopyImage() but simplified and allows us to
+	// inject a testing double.
+	CopyImage(imageTarget string, dest rawImageClient, aliases []string, callback func(string)) error
 }
 
 type imageClient struct {
-	raw rawImageClient
+	raw             rawImageClient
+	connectToSource func(Remote) (remoteClient, error)
+}
+
+type rawWrapper struct {
+	*lxd.Client
+}
+
+func (r rawWrapper) URL() string {
+	return r.Client.BaseURL
+}
+
+func (r rawWrapper) CopyImage(imageTarget string, dest rawImageClient, aliases []string, callback func(string)) error {
+	rawDest, ok := dest.(*lxd.Client)
+	if !ok {
+		return errors.Errorf("can only copy images to a real lxd.Client instance")
+	}
+	return r.Client.CopyImage(
+		imageTarget,
+		rawDest,
+		false,   // copy_aliases
+		aliases, // create these aliases
+		false,   // make the image public
+		true,    // autoUpdate,
+		callback,
+	)
+}
+
+func connectToRaw(remote Remote) (remoteClient, error) {
+	raw, err := newRawClient(remote)
+	if err != nil {
+		return nil, err
+	}
+	return rawWrapper{raw}, nil
 }
 
 // progressContext takes progress messages from LXD and just writes them to
@@ -41,10 +81,18 @@ func (p *progressContext) copyProgress(progress string) {
 	}
 }
 
+// EnsureImageExists makes sure we have a local image so we can launch a
+// container.
+// @param series: OS series (trusty, precise, etc)
+// @param architecture: (TODO) The architecture of the image we want to use
+// @param trustLocal: (TODO) check if we already have an image with the right alias.
+// Setting this to False means we will always check the remote sources and only
+// launch the newest version.
+// @param sources: a list of Remotes that we will look in for the image.
+// @param copyProgressHandler: a callback function. If we have to download an
+// image, we will call this with messages indicating how much of the download
+// we have completed (and where we are downloading it from).
 func (i *imageClient) EnsureImageExists(series string, sources []Remote, copyProgressHandler func(string)) error {
-	// TODO(jam) We should add Architecture in this information as well
-	// TODO(jam) We should also update this for multiple locations to copy
-	// from
 	// TODO(jam) Find a way to test this, even though lxd.Client can't
 	// really be stubbed out because CopyImage takes one directly and pokes
 	// at private methods so we can't easily tweak it.
@@ -54,26 +102,16 @@ func (i *imageClient) EnsureImageExists(series string, sources []Remote, copyPro
 	// non-state machines to only trust the alias that is set on the state
 	// machines.
 	// if IgnoreLocalAliases {}
-	aliases, err := i.raw.ListAliases()
-	if err != nil {
-		return err
+	target := i.raw.GetAlias(name)
+	if target != "" {
+		// GetAlias returns "" if the alias is not found, else it
+		// returns the Target of the alias (the hash)
+		return nil
 	}
 
-	for _, alias := range aliases {
-		if alias.Description == name {
-			logger.Infof("found cached image %q = %s",
-				alias.Description, alias.Target)
-			return nil
-		}
-	}
-
-	client, ok := i.raw.(*lxd.Client)
-	if !ok {
-		return errors.Errorf("can only copy images to a real lxd.Client instance")
-	}
 	var lastErr error
 	for _, remote := range sources {
-		source, err := newRawClient(remote)
+		source, err := i.connectToSource(remote)
 		if err != nil {
 			logger.Infof("failed to connect to %q: %s", remote.Host, err)
 			lastErr = err
@@ -88,13 +126,13 @@ func (i *imageClient) EnsureImageExists(series string, sources []Remote, copyPro
 		// naming scheme, and only fall back for synchronization.
 		target := source.GetAlias(series)
 		if target == "" {
-			logger.Infof("no image for %s found in %s", name, source.BaseURL)
+			logger.Infof("no image for %s found in %s", name, source.URL())
 			// TODO(jam) Add a test that we skip sources that don't
 			// have what we are looking for
 			continue
 		}
 		logger.Infof("found image from %s for %s = %s",
-			source.BaseURL, series, target)
+			source.URL(), series, target)
 		forwarder := stringforwarder.NewStringForwarder(copyProgressHandler)
 		defer func() {
 			dropCount := forwarder.Stop()
@@ -103,12 +141,10 @@ func (i *imageClient) EnsureImageExists(series string, sources []Remote, copyPro
 		adapter := &progressContext{
 			logger:  logger,
 			level:   loggo.INFO,
-			context: fmt.Sprintf("copying image for %s from %s: %%s", name, source.BaseURL),
+			context: fmt.Sprintf("copying image for %s from %s: %%s", name, source.URL()),
 			forward: forwarder.Receive,
 		}
-		err = source.CopyImage(
-			target, client, false, []string{name}, false,
-			true, adapter.copyProgress)
+		err = source.CopyImage(target, i.raw, []string{name}, adapter.copyProgress)
 		if err != nil {
 			// TODO(jam) Should this be fatal? Or just set lastErr
 			// and then continue on?
@@ -122,5 +158,7 @@ func (i *imageClient) EnsureImageExists(series string, sources []Remote, copyPro
 
 // A common place to compute image names (aliases) based on the series
 func (i imageClient) ImageNameForSeries(series string) string {
-	return "ubuntu-" + series
+	// TODO(jam) Do we need 'ubuntu' in there? We only need it if "series"
+	// would collide, but all our supported series are disjoint
+	return fmt.Sprintf("ubuntu-%s", series)
 }
---
+++ tools/lxdclient/client_image_test.go
@@ -0,0 +1,301 @@
+// Copyright 2015 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+// +build go1.3
+
+package lxdclient
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/juju/errors"
+	jc "github.com/juju/testing/checkers"
+	// lxdshared "github.com/lxc/lxd/shared"
+	"github.com/juju/testing"
+	gc "gopkg.in/check.v1"
+)
+
+type imageSuite struct {
+	testing.IsolationSuite
+	Stub              *testing.Stub
+	remoteWithTrusty  *stubRemoteClient
+	remoteWithNothing *stubRemoteClient
+}
+
+var _ = gc.Suite(&imageSuite{})
+
+func (s *imageSuite) SetUpTest(c *gc.C) {
+	s.IsolationSuite.SetUpTest(c)
+	s.Stub = &testing.Stub{}
+	s.remoteWithTrusty = &stubRemoteClient{
+		stub: s.Stub,
+		url:  "https://match",
+		aliases: map[string]string{
+			"trusty": "deadbeef",
+		},
+	}
+	s.remoteWithNothing = &stubRemoteClient{
+		stub:    s.Stub,
+		url:     "https://missing",
+		aliases: nil,
+	}
+}
+
+type stubRemoteClient struct {
+	stub    *testing.Stub
+	url     string
+	aliases map[string]string
+}
+
+var _ remoteClient = (*stubRemoteClient)(nil)
+
+func (s *stubRemoteClient) URL() string {
+	// Note we don't log calls to URL because they are not interesting, and
+	// are generally just used for logging, etc.
+	// s.stub.AddCall("URL")
+	return s.url
+}
+
+func (s *stubRemoteClient) GetAlias(alias string) string {
+	s.stub.AddCall("GetAlias", alias)
+	if err := s.stub.NextErr(); err != nil {
+		// GetAlias can't return an Err, but if we get an error, we'll
+		// just treat that as a miss on the Alias lookup.
+		return ""
+	}
+	return s.aliases[alias]
+}
+
+func (s *stubRemoteClient) CopyImage(imageTarget string, dest rawImageClient, aliases []string, callback func(string)) error {
+	// We don't include the destination or the callback because they aren't
+	// objects we can easily assert against.
+	s.stub.AddCall("CopyImage", imageTarget, aliases)
+	if err := s.stub.NextErr(); err != nil {
+		return err
+	}
+	// This is to make this CopyImage act a bit like a real CopyImage. it
+	// gives some Progress callbacks, and then sets the alias in the
+	// target.
+	if callback != nil {
+		// The real one gives progress every 1%
+		for i := 10; i <= 100; i += 10 {
+			callback(fmt.Sprintf("%d%%", i))
+			time.Sleep(1 * time.Microsecond)
+		}
+	}
+	if stubDest, ok := dest.(*stubClient); ok {
+		if stubDest.Aliases == nil {
+			stubDest.Aliases = make(map[string]string)
+		}
+		for _, alias := range aliases {
+			stubDest.Aliases[alias] = imageTarget
+		}
+	}
+	return nil
+}
+
+func (s *stubRemoteClient) AsRemote() Remote {
+	return Remote{
+		Host:     s.url,
+		Protocol: SimplestreamsProtocol,
+	}
+}
+
+type stubConnector struct {
+	stub          *testing.Stub
+	remoteClients map[string]remoteClient
+}
+
+func MakeConnector(stub *testing.Stub, remotes ...remoteClient) *stubConnector {
+	remoteMap := make(map[string]remoteClient)
+	for _, remote := range remotes {
+		remoteMap[remote.URL()] = remote
+	}
+	return &stubConnector{
+		stub:          stub,
+		remoteClients: remoteMap,
+	}
+}
+
+func (s *stubConnector) connectToSource(remote Remote) (remoteClient, error) {
+	s.stub.AddCall("connectToSource", remote.Host)
+	if err := s.stub.NextErr(); err != nil {
+		return nil, err
+	}
+	return s.remoteClients[remote.Host], nil
+}
+
+func (s *imageSuite) TestEnsureImageExistsAlreadyPresent(c *gc.C) {
+	raw := &stubClient{
+		stub: s.Stub,
+		Aliases: map[string]string{
+			"ubuntu-trusty": "dead-beef",
+		},
+	}
+	client := &imageClient{
+		raw: raw,
+	}
+	err := client.EnsureImageExists("trusty", nil, nil)
+	c.Assert(err, jc.ErrorIsNil)
+	s.Stub.CheckCall(c, 0, "GetAlias", "ubuntu-trusty")
+}
+
+func (s *imageSuite) TestEnsureImageExistsFirstRemote(c *gc.C) {
+	connector := MakeConnector(s.Stub, s.remoteWithTrusty)
+	raw := &stubClient{
+		stub: s.Stub,
+		// We don't have the image locally
+		Aliases: nil,
+	}
+	client := &imageClient{
+		raw:             raw,
+		connectToSource: connector.connectToSource,
+	}
+	remotes := []Remote{s.remoteWithTrusty.AsRemote()}
+	s.Stub.ResetCalls()
+	err := client.EnsureImageExists("trusty", remotes, nil)
+	c.Assert(err, jc.ErrorIsNil)
+	// We didn't find it locally
+	s.Stub.CheckCalls(c, []testing.StubCall{
+		{ // Check if we already have 'ubuntu-trusty' locally
+			FuncName: "GetAlias",
+			Args:     []interface{}{"ubuntu-trusty"},
+		},
+		{ // We didn't so connect to the first remote
+			FuncName: "connectToSource",
+			Args:     []interface{}{"https://match"},
+		},
+		{ // And check if it has trusty (which it should)
+			FuncName: "GetAlias",
+			Args:     []interface{}{"trusty"},
+		},
+		{ // So Copy the Image
+			FuncName: "CopyImage",
+			Args:     []interface{}{"deadbeef", []string{"ubuntu-trusty"}},
+		},
+	})
+	// We've updated the aliases
+	c.Assert(raw.Aliases, gc.DeepEquals, map[string]string{
+		"ubuntu-trusty": "deadbeef",
+	})
+}
+
+func (s *imageSuite) TestEnsureImageExistsUnableToConnect(c *gc.C) {
+	connector := MakeConnector(s.Stub, s.remoteWithTrusty)
+	raw := &stubClient{
+		stub: s.Stub,
+		// We don't have the image locally
+		Aliases: nil,
+	}
+	client := &imageClient{
+		raw:             raw,
+		connectToSource: connector.connectToSource,
+	}
+	badRemote := Remote{
+		Host:     "https://nosuch-remote.invalid",
+		Protocol: SimplestreamsProtocol,
+	}
+	s.Stub.ResetCalls()
+	s.Stub.SetErrors(nil, errors.Errorf("unable-to-connect"))
+	remotes := []Remote{badRemote, s.remoteWithTrusty.AsRemote()}
+	err := client.EnsureImageExists("trusty", remotes, nil)
+	c.Assert(err, jc.ErrorIsNil)
+	// We didn't find it locally
+	s.Stub.CheckCalls(c, []testing.StubCall{
+		{ // Check if we already have 'ubuntu-trusty' locally
+			FuncName: "GetAlias",
+			Args:     []interface{}{"ubuntu-trusty"},
+		},
+		{ // We didn't so connect to the first remote
+			FuncName: "connectToSource",
+			Args:     []interface{}{"https://nosuch-remote.invalid"},
+		},
+		{ // Connect failed to first, so connect to second and copy
+			FuncName: "connectToSource",
+			Args:     []interface{}{"https://match"},
+		},
+		{ // And check if it has trusty (which it should)
+			FuncName: "GetAlias",
+			Args:     []interface{}{"trusty"},
+		},
+		{ // So Copy the Image
+			FuncName: "CopyImage",
+			Args:     []interface{}{"deadbeef", []string{"ubuntu-trusty"}},
+		},
+	})
+	// We've updated the aliases
+	c.Assert(raw.Aliases, gc.DeepEquals, map[string]string{
+		"ubuntu-trusty": "deadbeef",
+	})
+}
+
+func (s *imageSuite) TestEnsureImageExistsNotPresentInFirstRemote(c *gc.C) {
+	connector := MakeConnector(s.Stub, s.remoteWithNothing, s.remoteWithTrusty)
+	raw := &stubClient{
+		stub: s.Stub,
+		// We don't have the image locally
+		Aliases: nil,
+	}
+	client := &imageClient{
+		raw:             raw,
+		connectToSource: connector.connectToSource,
+	}
+	s.Stub.ResetCalls()
+	remotes := []Remote{s.remoteWithNothing.AsRemote(), s.remoteWithTrusty.AsRemote()}
+	err := client.EnsureImageExists("trusty", remotes, nil)
+	c.Assert(err, jc.ErrorIsNil)
+	// We didn't find it locally
+	s.Stub.CheckCalls(c, []testing.StubCall{
+		{ // Check if we already have 'ubuntu-trusty' locally
+			FuncName: "GetAlias",
+			Args:     []interface{}{"ubuntu-trusty"},
+		},
+		{ // We didn't so connect to the first remote
+			FuncName: "connectToSource",
+			Args:     []interface{}{s.remoteWithNothing.URL()},
+		},
+		{ // Lookup the Alias
+			FuncName: "GetAlias",
+			Args:     []interface{}{"trusty"},
+		},
+		{ // It wasn't found, so connect to second and look there
+			FuncName: "connectToSource",
+			Args:     []interface{}{s.remoteWithTrusty.URL()},
+		},
+		{ // And check if it has trusty (which it should)
+			FuncName: "GetAlias",
+			Args:     []interface{}{"trusty"},
+		},
+		{ // So Copy the Image
+			FuncName: "CopyImage",
+			Args:     []interface{}{"deadbeef", []string{"ubuntu-trusty"}},
+		},
+	})
+	// We've updated the aliases
+	c.Assert(raw.Aliases, gc.DeepEquals, map[string]string{
+		"ubuntu-trusty": "deadbeef",
+	})
+}
+
+func (s *imageSuite) TestEnsureImageExistsCallbackIncludesSourceURL(c *gc.C) {
+	var calls []string
+	callback := func(message string) {
+		calls = append(calls, message)
+	}
+	connector := MakeConnector(s.Stub, s.remoteWithTrusty)
+	raw := &stubClient{
+		stub: s.Stub,
+		// We don't have the image locally
+		Aliases: nil,
+	}
+	client := &imageClient{
+		raw:             raw,
+		connectToSource: connector.connectToSource,
+	}
+	remotes := []Remote{s.remoteWithTrusty.AsRemote()}
+	err := client.EnsureImageExists("trusty", remotes, callback)
+	c.Assert(err, jc.ErrorIsNil)
+	c.Check(len(calls), jc.GreaterThan, 0)
+	c.Check(calls[0], gc.Matches, "copying image for ubuntu-trusty from https://match: \\d+%")
+}
