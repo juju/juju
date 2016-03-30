632da61c0203debd2e3b27adad3c48eb48f41a63
Dave Cheney <dave@cheney.net>
2016-03-30 12:58:05 +1100

    provider/vsphere: fix copy mutex by value error
    The pattern `env = env.getSnapshot()` was broken as it would return a
    shallow copy of the environ value, including its lock. However all the
    fields referenced by pointers would point to the same instnace, but with
    the lock no longer protecting access.
    
    The `env = env.getSnapshot()` pattern is identical to declaring the
    method on on `env`, not `*env`, but the pattern hid this incorrect
    behaviour until a recent improvement to go vet spotted it.

--- provider/vsphere/environ.go
+++ provider/vsphere/environ.go
@@ -77,17 +77,12 @@ func (env *environ) SetConfig(cfg *config.Config) error {
 	return nil
 }
 
-// getSnapshot returns a copy of the environment. This is useful for
-// ensuring the env you are using does not get changed by other code
-// while you are using it.
-func (env *environ) getSnapshot() *environ {
-	e := *env
-	return &e
-}
-
 // Config returns the configuration data with which the env was created.
 func (env *environ) Config() *config.Config {
-	return env.getSnapshot().ecfg.Config
+	env.lock.Lock()
+	cfg := env.ecfg.Config
+	env.lock.Unlock()
+	return cfg
 }
 
 //this variable is exported, because it has to be rewritten in external unit tests
