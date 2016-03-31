de35bb7df6f07770c7f13f88faf7e47e37dcd39b
Michael Foord <michael.foord@canonical.com>
2016-03-31 16:22:47 +0100

    Test for SupportedArchitectures in MAAS 2 error case

--- provider/maas/maas2_environ_whitebox_test.go
+++ provider/maas/maas2_environ_whitebox_test.go
@@ -4,6 +4,10 @@
 package maas
 
 import (
+	"net/http"
+
+	"github.com/juju/errors"
+	"github.com/juju/gomaasapi"
 	jc "github.com/juju/testing/checkers"
 	gc "gopkg.in/check.v1"
 
@@ -11,22 +15,62 @@ import (
 	coretesting "github.com/juju/juju/testing"
 )
 
+type fakeController struct {
+	gomaasapi.Controller
+}
+
+func (fakeController) BootResources() ([]gomaasapi.BootResource, error) {
+	return nil, errors.New("Something terrible!")
+}
+
 type maas2EnvironSuite struct {
 	controllerSuite
 }
 
 var _ = gc.Suite(&maas2EnvironSuite{})
 
-func (suite *maas2EnvironSuite) TestNewEnvironWithController(c *gc.C) {
+func makeEnviron(c *gc.C) *maasEnviron {
 	testAttrs := coretesting.Attrs{}
 	for k, v := range maasEnvAttrs {
 		testAttrs[k] = v
 	}
-	testAttrs["maas-server"] = suite.testServer.Server.URL
+	testAttrs["maas-server"] = "http://any-old-junk.invalid/"
 	attrs := coretesting.FakeConfig().Merge(testAttrs)
 	cfg, err := config.New(config.NoDefaults, attrs)
 	c.Assert(err, jc.ErrorIsNil)
 	env, err := NewEnviron(cfg)
 	c.Assert(err, jc.ErrorIsNil)
 	c.Assert(env, gc.NotNil)
+	return env
+}
+
+func (suite *maas2EnvironSuite) TestNewEnvironWithController(c *gc.C) {
+	testServer := gomaasapi.NewSimpleServer()
+	testServer.AddResponse("/api/2.0/version/", http.StatusOK, maas2VersionResponse)
+	testServer.Start()
+	defer testServer.Close()
+	testAttrs := coretesting.Attrs{}
+	for k, v := range maasEnvAttrs {
+		testAttrs[k] = v
+	}
+	testAttrs["maas-server"] = testServer.Server.URL
+	attrs := coretesting.FakeConfig().Merge(testAttrs)
+	cfg, err := config.New(config.NoDefaults, attrs)
+	c.Assert(err, jc.ErrorIsNil)
+	env, err := NewEnviron(cfg)
+	c.Assert(err, jc.ErrorIsNil)
+	c.Assert(env, gc.NotNil)
+}
+
+func (suite *maas2EnvironSuite) TestSupportedArchitectures(c *gc.C) {
+}
+
+func (suite *maas2EnvironSuite) TestSupportedArchitecturesError(c *gc.C) {
+	mockGetController := func(maasServer, apiKey string) (gomaasapi.Controller, error) {
+		return fakeController{}, nil
+	}
+	suite.PatchValue(&GetMAAS2Controller, mockGetController)
+	env := makeEnviron(c)
+	_, err := env.SupportedArchitectures()
+	c.Assert(err, gc.ErrorMatches, "Something terrible!")
 }
