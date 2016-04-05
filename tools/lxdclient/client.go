902e5a060ea711ac8c9c3ea253420c77acae8d7c
Tycho Andersen <tycho.andersen@canonical.com>
2016-04-05 14:24:40 -0600

    lxd provider: check if lxdbr0 is configured
    ...and if it's not, give a better error message.
    
    Also add a bit about configuration of lxdbr0 to the install tips for lxd.
    
    Signed-off-by: Tycho Andersen <tycho.andersen@canonical.com>

--- tools/lxdclient/client.go
+++ tools/lxdclient/client.go
@@ -7,9 +7,11 @@ package lxdclient
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net"
 	"net/url"
 	"os"
+	"strconv"
 	"strings"
 	"syscall"
 
@@ -21,6 +23,8 @@ import (
 )
 
 var logger = loggo.GetLogger("juju.tools.lxdclient")
+
+const LXDBridgeFile = "/etc/default/lxd-bridge"
 
 // Client is a high-level wrapper around the LXD API client.
 type Client struct {
@@ -114,7 +118,86 @@ func newRawClient(remote Remote) (*lxd.Client, error) {
 		}
 		return nil, errors.Trace(err)
 	}
+
+	/* If this is the LXD provider on the localhost, let's do an extra
+	 * check to make sure that lxdbr0 is configured.
+	 */
+	if remote.ID() == remoteIDForLocal {
+		profile, err := client.ProfileConfig("default")
+		if err != nil {
+			return nil, errors.Trace(err)
+		}
+
+		/* If the default profile doesn't have eth0 in it, then the
+		 * user has messed with it, so let's just use whatever they set up.
+		 *
+		 * Otherwise, if it looks like it's pointing at our lxdbr0,
+		 * let's check and make sure that is configured.
+		 */
+		eth0, ok := profile.Devices["eth0"]
+		if ok && eth0["type"] == "nic" && eth0["nictype"] == "bridged" && eth0["parent"] == "lxdbr0" {
+			conf, err := ioutil.ReadFile(LXDBridgeFile)
+			if err != nil {
+				if !os.IsNotExist(err) {
+					return nil, errors.Trace(err)
+				} else {
+					return nil, bridgeConfigError("lxdbr0 configured but no config file found at " + LXDBridgeFile)
+				}
+			}
+
+			if err = checkLXDBridgeConfiguration(string(conf)); err != nil {
+				return nil, errors.Trace(err)
+			}
+		}
+	}
+
 	return client, nil
+}
+
+func bridgeConfigError(err string) error {
+	return errors.Errorf(`%s
+It looks like your lxdbr0 has not yet been configured. Please configure it via:
+
+	sudo dpkg-reconfigure -p medium lxd
+
+and then bootstrap again.`, err)
+}
+
+func checkLXDBridgeConfiguration(conf string) error {
+	foundSubnetConfig := false
+	for _, line := range strings.Split(conf, "\n") {
+		if strings.HasPrefix(line, "USE_LXD_BRIDGE=") {
+			b, err := strconv.ParseBool(strings.Trim(line[len("USE_LXD_BRIDGE="):], " \""))
+			if err != nil {
+				logger.Warningf("couldn't parse bool, skipping USE_LXD_BRIDGE check: %s", err)
+				continue
+			}
+
+			if !b {
+				return bridgeConfigError("lxdbr0 not enabled but required")
+			}
+		} else if strings.HasPrefix(line, "LXD_BRIDGE=") {
+			name := strings.Trim(line[len("LXD_BRIDGE="):], " \"")
+			/* If we're here, we want lxdbr0 to be configured
+			 * because the default profile that juju will use says
+			 * lxdbr0. So let's fail if it doesn't.
+			 */
+			if name != "lxdbr0" {
+				return bridgeConfigError(fmt.Sprintf(LXDBridgeFile+" has a bridge named %s, not lxdbr0", name))
+			}
+		} else if strings.HasPrefix(line, "LXD_IPV4_ADDR=") || strings.HasPrefix(line, "LXD_IPV6_ADDR=") {
+			contents := strings.Trim(line[len("LXD_IPVN_ADDR="):], " \"")
+			if len(contents) > 0 {
+				foundSubnetConfig = true
+			}
+		}
+	}
+
+	if !foundSubnetConfig {
+		return bridgeConfigError("lxdbr0 has no ipv4 or ipv6 subnet enabled")
+	}
+
+	return nil
 }
 
 func hoistLocalConnectErr(err error) error {
@@ -145,6 +228,7 @@ func hoistLocalConnectErr(err error) error {
 
 	configureText := `
 Please configure LXD by running:
+	$ sudo dpkg-reconfigure -p medium lxd
 	$ newgrp lxd
 	$ lxd init
 `
@@ -152,6 +236,7 @@ Please configure LXD by running:
 	installText := `
 Please install LXD by running:
 	$ sudo apt-get install lxd
+	$ sudo dpkg-reconfigure -p medium lxd
 and then configure it with:
 	$ newgrp lxd
 	$ lxd init
