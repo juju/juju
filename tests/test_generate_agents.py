b64a13b166227c41cf37c2e3e71c7e16e6d6da45
Curtis Hovey <curtis@canonical.com>
2016-04-07 12:18:40 -0400

    Added 2 of 3 needed tests.

--- tests/test_generate_agents.py
+++ tests/test_generate_agents.py
@@ -2,6 +2,8 @@ import os
 from unittest import TestCase
 
 from generate_agents import (
+    make_centos_agent,
+    make_windows_agent,
     move_debs,
     NoDebsFound,
     )
@@ -40,3 +42,47 @@ class TestMoveDebs(TestCase):
             open(os.path.join(parent, 'foo.deb'), 'w').close()
             with self.assertRaisesRegexp(NoDebsFound, 'No deb files found.'):
                 move_debs(dest_debs)
+
+
+class TestMakeCentosAgent(TestCase):
+
+    def test_make_centos_agent(self):
+        with temp_dir() as workspace:
+            dest_debs = os.path.join(workspace, 'debs')
+            agent_dir = os.path.join(dest_debs, 'agent', '1.25.5')
+            os.makedirs(agent_dir)
+            stanzas = os.path.join(workspace, 'stanzas')
+            os.mkdir(stanzas)
+            agent = os.path.join(dest_debs, 'juju-1.25.5-centos7-amd64.tgz')
+            with open(agent, 'w') as dummy_file:
+                dummy_file.write('centos7 agent')
+            make_centos_agent(dest_debs, 'proposed', '1.25.5')
+            agent_path = os.path.join(
+                workspace, 'debs', 'agent', '1.25.5',
+                'juju-1.25.5-centos7-amd64.tgz')
+            self.assertTrue(os.path.exists(agent_path))
+            stanza_path = os.path.join(
+                workspace, 'debs', 'proposed-1.25.5-centos.json')
+            self.assertTrue(os.path.exists(stanza_path))
+
+
+class TestMakeWindowsAgent(TestCase):
+
+    def test_make_windows_agent(self):
+        with temp_dir() as workspace:
+            dest_debs = os.path.join(workspace, 'debs')
+            agent_dir = os.path.join(dest_debs, 'agent', '1.25.5')
+            os.makedirs(agent_dir)
+            stanzas = os.path.join(workspace, 'stanzas')
+            os.mkdir(stanzas)
+            agent = os.path.join(dest_debs, 'juju-1.25.5-win2012-amd64.tgz')
+            with open(agent, 'w') as dummy_file:
+                dummy_file.write('windows agent')
+            make_windows_agent(dest_debs, 'proposed', '1.25.5')
+            agent_path = os.path.join(
+                workspace, 'debs', 'agent', '1.25.5',
+                'juju-1.25.5-windows-amd64.tgz')
+            self.assertTrue(os.path.exists(agent_path))
+            stanza_path = os.path.join(
+                workspace, 'debs', 'proposed-1.25.5-windows.json')
+            self.assertTrue(os.path.exists(stanza_path))
