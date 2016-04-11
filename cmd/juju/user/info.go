5c3b9bb537cf7823dc58766c58b90684c11cab85
Reed O'Brien <reed@reedobrien.com>
2016-04-11 14:15:41 -0700

    Update helptext per Doc Team recommendations
    See https://bugs.launchpad.net/juju-core/+bug/1557143 for details.

--- cmd/juju/user/info.go
+++ cmd/juju/user/info.go
@@ -50,7 +50,7 @@ type infoCommandBase struct {
 }
 
 func (c *infoCommandBase) SetFlags(f *gnuflag.FlagSet) {
-	f.BoolVar(&c.exactTime, "exact-time", false, "use full timestamp precision")
+	f.BoolVar(&c.exactTime, "exact-time", false, "Use full timestamp for connection times")
 }
 
 func NewShowUserCommand() cmd.Command {
