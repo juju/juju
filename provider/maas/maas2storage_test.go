31136d9bf8a04ea6e839d4b91e6a7bde16243af3
Christian Muirhead <christian.muirhead@canonical.com>
2016-04-11 09:54:05 +0100

    Simplify tests
    Don't get the controller out with awkward casting, hang on to the
    original reference.

--- provider/maas/maas2storage_test.go
+++ provider/maas/maas2storage_test.go
@@ -53,20 +53,21 @@ func (s *maas2StorageSuite) TestGetReadFails(c *gc.C) {
 }
 
 func (s *maas2StorageSuite) TestGetSuccess(c *gc.C) {
-	storage := s.makeStorage(c, &fakeController{
+	controller := &fakeController{
 		files: []gomaasapi.File{
 			&fakeFile{
 				contents: []byte("The man in the high castle"),
 			},
 		},
-	})
+	}
+	storage := s.makeStorage(c, controller)
 	reader, err := storage.Get("grasshopper.avi")
 	c.Assert(err, jc.ErrorIsNil)
 	defer reader.Close()
 	result, err := ioutil.ReadAll(reader)
 	c.Assert(err, jc.ErrorIsNil)
 	c.Assert(result, gc.DeepEquals, []byte("The man in the high castle"))
-	c.Assert(getController(c, storage).getFileFilename, gc.Equals, "prefix-grasshopper.avi")
+	c.Assert(controller.getFileFilename, gc.Equals, "prefix-grasshopper.avi")
 }
 
 func (s *maas2StorageSuite) TestListError(c *gc.C) {
@@ -78,16 +79,17 @@ func (s *maas2StorageSuite) TestListError(c *gc.C) {
 }
 
 func (s *maas2StorageSuite) TestListSuccess(c *gc.C) {
-	storage := s.makeStorage(c, &fakeController{
+	controller := &fakeController{
 		files: []gomaasapi.File{
 			&fakeFile{name: "prefix-julianna"},
 			&fakeFile{name: "prefix-frank"},
 		},
-	})
+	}
+	storage := s.makeStorage(c, controller)
 	result, err := storage.List("grasshopper")
 	c.Assert(err, jc.ErrorIsNil)
-	c.Assert(result, gc.DeepEquals, []string{"frank", "julianna"})
-	c.Assert(getController(c, storage).filesPrefix, gc.Equals, "prefix-grasshopper")
+	c.Assert(result, jc.DeepEquals, []string{"frank", "julianna"})
+	c.Assert(controller.filesPrefix, gc.Equals, "prefix-grasshopper")
 }
 
 func (s *maas2StorageSuite) TestURLError(c *gc.C) {
@@ -99,13 +101,15 @@ func (s *maas2StorageSuite) TestURLError(c *gc.C) {
 }
 
 func (s *maas2StorageSuite) TestURLSuccess(c *gc.C) {
-	storage := s.makeStorage(c, &fakeController{
+	controller := &fakeController{
 		files: []gomaasapi.File{
 			&fakeFile{url: "heavy lies"},
 		},
-	})
+	}
+	storage := s.makeStorage(c, controller)
 	result, err := storage.URL("grasshopper.avi")
 	c.Assert(err, jc.ErrorIsNil)
 	c.Assert(result, gc.Equals, "heavy lies")
-	c.Assert(getController(c, storage).getFileFilename, gc.Equals, "prefix-grasshopper.avi")
+	c.Assert(controller.getFileFilename, gc.Equals, "prefix-grasshopper.avi")
+}
 }
