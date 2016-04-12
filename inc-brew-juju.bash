6b53f68946947080d90aef685ddd063a1ef46b63
Curtis Hovey <curtis@canonical.com>
2016-04-12 17:01:02 -0400

    Update brew build and vet rules per homebrew-core requirements.

--- inc-brew-juju.bash
+++ inc-brew-juju.bash
@@ -15,7 +15,7 @@ TARBALL=$(basename $URI)
 VERSION=$(basename $TARBALL .tar.gz | cut -d _ -f 2)
 cd ~/Downloads
 curl -L -o ~/Downloads/$TARBALL $URI
-SHASUM=$(shasum ~/Downloads/$TARBALL | cut -d ' ' -f 1)
+SHASUM=$(shasum -a 256 ~/Downloads/$TARBALL | cut -d ' ' -f 1)
 echo "$TARBALL is $SHASUM"
 
 cd /usr/local/Library/Formula
@@ -27,21 +27,22 @@ git checkout -b juju-$VERSION
 if [[ $PURPOSE == 'stable' ]]; then
     # The stable block is indented 2 spaces.
     FORMULA=$(cat juju.rb |
-        sed -e "s,^  url '.*juju-core.*.tar.gz',  url '$URI',;" |
-        sed -e "s,^  sha1 '.*',  sha1 '$SHASUM',;")
-    MESSAGE="Update juju stable $VERSION."
+        sed -e "s,^  url \".*juju-core.*.tar.gz\",  url \"$URI\",;" |
+        sed -e "s,^  sha256 \".*\",  sha256 \"$SHASUM\",;")
     OPT=""
 else
     # The development block is indented 4 spaces.
     FORMULA=$(cat juju.rb |
-        sed -e "/devel do/,/bottle do/ s,^    url [\'\"].*juju-core.*.tar.gz[\'\"],    url '$URI',;" |
-        sed -e "/devel do/,/bottle do/s,^    sha1 [\'\"].*[\'\"],    sha1 '$SHASUM',;")
-    MESSAGE="Update juju devel $VERSION."
+        sed -e "/devel do/,/bottle do/ s,^    url [\'\"].*juju-core.*.tar.gz[\'\"],    url \"$URI\",;" |
+        sed -e "/devel do/,/bottle do/s,^    sha256 [\'\"].*[\'\"],    sha256 \"$SHASUM\",;")
     OPT="--devel"
 fi
 echo "$FORMULA" > juju.rb
-git commit -m "$MESSAGE" juju.rb
+brew audit juju
+brew audit --online --strict juju
+git commit -m "juju $VERSION" juju.rb
 brew install $OPT juju
 juju --version
+
 echo "You can push this to github (fork, sinzui/homebrew)."
 echo "  git push fork juju-$VERSION"
