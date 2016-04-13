b25d47b2793d65e6e9dfd0a8a07e0b16711d719d
Tycho Andersen <tycho.andersen@canonical.com>
2016-04-13 12:39:52 -0400

    Use the default network config when no juju-specific config is provided
    73a85aaf creates network profiles based on network configuration provided
    to the container type when network spaces are present (e.g. in maas, etc.).
    However, in some cases (notably, AWS, GCE), there is no network
    configuration provided to the container type code, and thus the profile
    gets created with no network devices.
    
    Instead, let's fall back to the default network configuration in LXD when
    nothing is explicitly provided by juju.
    
    Signed-off-by: Tycho Andersen <tycho.andersen@canonical.com>

--- container/lxd/lxd.go
+++ container/lxd/lxd.go
@@ -125,11 +125,14 @@ func (manager *containerManager) CreateContainer(
 		"boot.autostart": "true",
 	}
 
-	networkProfile := fmt.Sprintf("%s-network", name)
+	networkProfile := "default"
+	if len(networkConfig.Interfaces) > 0 {
+		networkProfile = fmt.Sprintf("%s-network", name)
 
-	err = manager.createNetworkProfile(networkProfile, networkConfig)
-	if err != nil {
-		return
+		err = manager.createNetworkProfile(networkProfile, networkConfig)
+		if err != nil {
+			return
+		}
 	}
 
 	spec := lxdclient.InstanceSpec{
