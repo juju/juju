840d055981bd9ac00f58c8028acd4d37ac05828f
Reed O'Brien <reed@reedobrien.com>
2016-04-14 22:09:57 -0700

    Correction as noted by thumper
    Refs https://bugs.launchpad.net/juju-core/+bug/1554705

--- cmd/juju/commands/list_sshkeys.go
+++ cmd/juju/commands/list_sshkeys.go
@@ -23,7 +23,7 @@ created unit.
 This command will display a list of all the keys currently used by Juju in
 the current model (or the model specified, if the '-m' option is used).
 By default a minimal list is returned, showing only the fingerprint of
-each key and its text identifier. By using the '-full' option, the entire
+each key and its text identifier. By using the '--full' option, the entire
 key may be displayed.
 
 Examples:
