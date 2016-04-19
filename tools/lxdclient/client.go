521a9fd15af83a19c4727e669e86a1642a8a6914
Tycho Andersen <tycho.andersen@canonical.com>
2016-04-19 11:42:32 -0600

    lxd: better version checking against lxd versions
    Signed-off-by: Tycho Andersen <tycho.andersen@canonical.com>

--- tools/lxdclient/client.go
+++ tools/lxdclient/client.go
@@ -67,6 +67,42 @@ func Connect(cfg Config) (*Client, error) {
 
 var lxdNewClientFromInfo = lxd.NewClientFromInfo
 
+func isSupportedLxdVersion(version string) bool {
+	var major, minor, micro int
+	var err error
+
+	versionParts := strings.Split(version, ".")
+	if len(versionParts) < 3 {
+		return false
+	}
+
+	major, err = strconv.Atoi(versionParts[0])
+	if err != nil {
+		return false
+	}
+
+	minor, err = strconv.Atoi(versionParts[1])
+	if err != nil {
+		return false
+	}
+
+	micro, err = strconv.Atoi(versionParts[2])
+	if err != nil {
+		return false
+	}
+
+	if major < 2 {
+		return false
+	}
+
+	/* disallow 2.0.0.rc4 and friends */
+	if major == 2 && minor == 0 && micro == 0 && len(versionParts) > 3 {
+		return false
+	}
+
+	return true
+}
+
 // newRawClient connects to the LXD host that is defined in Config.
 func newRawClient(remote Remote) (*lxd.Client, error) {
 	host := remote.Host
@@ -124,12 +160,7 @@ func newRawClient(remote Remote) (*lxd.Client, error) {
 		return nil, errors.Trace(err)
 	}
 
-	versionParts := strings.Split(status.Environment.ServerVersion, ".")
-	if len(versionParts) != 3 ||
-		versionParts[0] < "2" ||
-		versionParts[1] < "0" ||
-		versionParts[1] < "0" {
-
+	if !isSupportedLxdVersion(status.Environment.ServerVersion) {
 		return nil, errors.Errorf("lxd version %s, juju needs at least 2.0.0", status.Environment.ServerVersion)
 	}
 
