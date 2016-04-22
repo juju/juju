82fc6034e410a1bde6d8d78c6ed8be83b8d70e3b
Menno Smits <menno.smits@canonical.com>
2016-04-22 15:13:59 +1200

    worker/hostkeyreporter: Add worker & manifold
    The hostkeyreporter is a shortlived worker which send the machine's
    SSH host keys to the controller for storage.

---
+++ worker/hostkeyreporter/shim.go
@@ -0,0 +1,23 @@
+// Copyright 2016 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package hostkeyreporter
+
+import (
+	"github.com/juju/errors"
+	"github.com/juju/juju/api/base"
+	apihostkeyreporter "github.com/juju/juju/api/hostkeyreporter"
+	"github.com/juju/juju/worker"
+)
+
+func NewFacade(apiCaller base.APICaller) (Facade, error) {
+	return apihostkeyreporter.NewFacade(apiCaller), nil
+}
+
+func NewWorker(config Config) (worker.Worker, error) {
+	worker, err := New(config)
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+	return worker, nil
+}
---
+++ worker/hostkeyreporter/worker_test.go
@@ -0,0 +1,110 @@
+// Copyright 2016 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package hostkeyreporter_test
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	jujutesting "github.com/juju/testing"
+	jc "github.com/juju/testing/checkers"
+	gc "gopkg.in/check.v1"
+
+	"github.com/juju/juju/worker/dependency"
+	"github.com/juju/juju/worker/hostkeyreporter"
+	"github.com/juju/juju/worker/workertest"
+)
+
+type Suite struct {
+	jujutesting.IsolationSuite
+
+	dir    string
+	stub   *jujutesting.Stub
+	facade *stubFacade
+	config hostkeyreporter.Config
+}
+
+var _ = gc.Suite(&Suite{})
+
+func (s *Suite) SetUpTest(c *gc.C) {
+	s.IsolationSuite.SetUpTest(c)
+
+	// Generate some dummy key files
+	s.dir = c.MkDir()
+	sshDir := filepath.Join(s.dir, "etc", "ssh")
+	err := os.MkdirAll(sshDir, 0755)
+	c.Assert(err, jc.ErrorIsNil)
+	writeKey := func(keyType string) {
+		baseName := fmt.Sprintf("ssh_host_%s_key.pub", keyType)
+		fileName := filepath.Join(sshDir, baseName)
+		err := ioutil.WriteFile(fileName, []byte(keyType), 0644)
+		c.Assert(err, jc.ErrorIsNil)
+	}
+	writeKey("dsa")
+	writeKey("rsa")
+	writeKey("ecdsa")
+
+	s.stub = new(jujutesting.Stub)
+	s.facade = newStubFacade(s.stub)
+	s.config = hostkeyreporter.Config{
+		Facade:    s.facade,
+		MachineId: "42",
+		RootDir:   s.dir,
+	}
+}
+
+func (s *Suite) TestInvalidConfig(c *gc.C) {
+	s.config.MachineId = ""
+	_, err := hostkeyreporter.New(s.config)
+	c.Check(err, gc.ErrorMatches, "empty MachineId .+")
+	c.Check(s.stub.Calls(), gc.HasLen, 0)
+}
+
+func (s *Suite) TestNoKeys(c *gc.C) {
+	// Pass an empty directory so the keys created in setup won't be
+	// there.
+	s.config.RootDir = c.MkDir()
+
+	w, err := hostkeyreporter.New(s.config)
+	c.Assert(err, jc.ErrorIsNil)
+	err = workertest.CheckKilled(c, w)
+	c.Check(err, gc.ErrorMatches, "no SSH host keys found")
+}
+
+func (s *Suite) TestReportKeysError(c *gc.C) {
+	s.facade.reportErr = errors.New("blam")
+	w, err := hostkeyreporter.New(s.config)
+	c.Assert(err, jc.ErrorIsNil)
+	err = workertest.CheckKilled(c, w)
+	c.Check(err, gc.ErrorMatches, "blam")
+}
+
+func (s *Suite) TestSuccess(c *gc.C) {
+	w, err := hostkeyreporter.New(s.config)
+	c.Assert(err, jc.ErrorIsNil)
+	err = workertest.CheckKilled(c, w)
+	c.Check(err, gc.Equals, dependency.ErrUninstall)
+	s.stub.CheckCalls(c, []jujutesting.StubCall{{
+		"ReportKeys", []interface{}{"42", []string{"dsa", "ecdsa", "rsa"}},
+	}})
+}
+
+func newStubFacade(stub *jujutesting.Stub) *stubFacade {
+	return &stubFacade{
+		stub: stub,
+	}
+}
+
+type stubFacade struct {
+	stub      *jujutesting.Stub
+	reportErr error
+}
+
+func (c *stubFacade) ReportKeys(machineId string, publicKeys []string) error {
+	c.stub.AddCall("ReportKeys", machineId, publicKeys)
+	return c.reportErr
+}
