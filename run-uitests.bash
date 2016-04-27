283d2411dbb69a1c962ee8e3b74f48dc3d0e03f7
Curtis Hovey <curtis@canonical.com>
2016-04-26 20:15:41 -0400

    Savepoint for building a tainted juju.

--- run-uitests.bash
+++ run-uitests.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eu
 export USER=jenkins
-git commit -a -m "Increment juju to 2.0-beta7"
+export SCRIPTS=$HOME/juju-ci-tools
 export RELEASE_TOOLS=$HOME/juju-release-tools
 export JUJU_UITEST=$HOME/eco-repos/src/github.com/CanonicalLtd/juju-uitest
 export CLOUD_CITY=$HOME/cloud-city
@@ -15,9 +15,11 @@ export PATH=/usr/lib/go-1.6/bin:$PATH
 revision_build=${1:-$revision_build}
 JOB_NAME=${2:-juju-with-uitest}
 WORKSPACE=${3:-$WORKSPACE}
+if [[ -z $WORKSPACE || $WORKSPACE == './' || $WORKSPACE == './' ]]; then
+    echo "Set a sane WORKSPACE: [$WORKSPACE] is not safe."
+    exit 1
+fi
 WORKSPACE=$(readlink -f $WORKSPACE)
-
-
 
 $SCRIPTS/jujuci.py -v setup-workspace $WORKSPACE
 export TMPDIR="$WORKSPACE/tmp"
@@ -25,17 +27,30 @@ mkdir $TMPDIR
 
 source $($SCRIPTS/s3ci.py get --config $S3_CONFIG $revision_build build-revision buildvars.bash)
 
-GUI_URL=$(sstream-query http://streams.canonical.com/juju/gui/streams/v1/index.json --output-format="%(item_url)s" | head -1)
-GUI_ARCHIVE=$(basename $GUI_URL)
-wget $GUI_URL
-
-# JUJU_ARCHIVE=$($SCRIPTS/s3ci.py get --config $S3_CONFIG $revision_build build-revision juju-core_.*.tar.gz)
-# tar -xf $JUJU_ARCHIVE
-# JUJU_DIR=$(basename $JUJU_ARCHIVE .tar.gz)
-# export GOPATH=$WORKSPACE/$JUJU_DIR
-# JUJU_BRANCH=$GOPATH/src/github.com/juju/juju
+# Get the pristine tarfile and unpack it to build a testing juju.
+JUJU_ARCHIVE=$($SCRIPTS/s3ci.py get --config $S3_CONFIG $revision_build build-revision juju-core_.*.tar.gz)
+tar -xf $JUJU_ARCHIVE
+JUJU_DIR=$(basename $JUJU_ARCHIVE .tar.gz)
+export GOPATH=$WORKSPACE/$JUJU_DIR
+JUJU_PACKAGE=$GOPATH/src/github.com/juju/juju
+CHARM_PACKAGE=$GOPATH/src/github.com/juju/charm
+# Add the charm package to the tree.
+#cp -r $HOME/eco-repos/src/github.com/juju/charm $CHARM_PACKAGE
+go get github.com/juju/charm/...
 
 # ^ Fails because uitest requires a git tree to make changes to.
+
+# If tree must be tampered with, then let it be us who do it to ensure
+# we understand what is not being tested.
+CSCLIENT="$GOPATH/src/gopkg.in/juju/charmrepo.v2-unstable/csclient/csclient.go"
+SERVER_URL="s,\"https://api.jujucharms.com/charmstore\",\"$STORE_URL\","
+sed -i -e "$SERVER_URL" $CSCLIENT
+for PACKAGE in $JUJU_PACKAGE $CHARM_PACKAGE; do
+    cd $PACKAGE
+    go install ./...
+done
+cd $WORKSPACE
+ls $GOPATH/bin
 
 # SHORT_REVISION=$(echo $REVISION_ID|head -c7)
 # echo Building $BRANCH revision $SHORT_REVISION
@@ -46,6 +61,10 @@ wget $GUI_URL
 
 # ^ Fails because sees uncommitted changes made by the purge of
 #   undocumented deps and non-free files.
+
+GUI_URL=$(sstream-query http://streams.canonical.com/juju/gui/streams/v1/index.json --output-format="%(item_url)s" | head -1)
+GUI_ARCHIVE=$(basename $GUI_URL)
+wget $GUI_URL
 
 export JUJU_DATA=$CLOUD_CITY/jes-homes/$JOB_NAME
 export JUJU_HOME=$JUJU_DATA
@@ -77,18 +96,20 @@ SUITE="not TestStorefront"
 echo "devenv/bin/uitest --driver phantom \
     -c google \
     --gui-archive $WORKSPACE/$GUI_ARCHIVE \
-    --juju-branch $REVISION_ID \
+    --gopath $GOPATH \
     --credentials <SECRET> \
     --admin <SECRET> \
     --url $STORE_URL" \
     "$SUITE"
+#    --juju-branch $REVISION_ID \
 set +x
 devenv/bin/uitest --driver phantom \
     -c google \
     --gui-archive $WORKSPACE/$GUI_ARCHIVE \
-    --juju-branch $REVISION_ID \
+    --gopath $GOPATH \
     --credentials $STORE_CREDENTIALS \
     --admin $STORE_ADMIN \
     --url $STORE_URL \
+    --failfast \
     "$SUITE"
 
