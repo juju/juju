5f5c8903d45cfce59a90ac97f180676fe2d51fa6
Katherine Cox-Buday <cox.katherine.e@gmail.com>
2016-05-04 21:51:48 -0500

    Removed dead code.

--- cmd/jujud/reboot/reboot_test.go
+++ cmd/jujud/reboot/reboot_test.go
@@ -13,16 +13,7 @@ import (
 // Hook up gocheck into the "go test" runner.
 func Test(t *testing.T) { gc.TestingT(t) }
 
-type RebootSuite struct {
-	// jujutesting.JujuConnSuite
-
-	// acfg    agent.Config
-	// mgoInst testing.MgoInstance
-	// st      api.Connection
-
-	// tmpDir           string
-	// rebootScriptName string
-}
+type RebootSuite struct{}
 
 var _ = gc.Suite(&RebootSuite{})
 
@@ -61,48 +52,3 @@ func (s *RebootSuite) TestExecuteReboot_ExecuteBuiltShutdownCmd(c *gc.C) {
 	c.Assert(err, gc.NotNil)
 	c.Check(err, gc.ErrorMatches, "foo")
 }
-
-// func (s *RebootSuite) SetUpTest(c *gc.C) {
-// 	if testing.GOVERSION < 1.3 {
-// 		c.Skip("skipping test, lxd requires Go 1.3 or later")
-// 	}
-
-// 	s.JujuConnSuite.SetUpTest(c)
-// 	testing.PatchExecutableAsEchoArgs(c, s, rebootBin)
-// 	s.PatchEnvironment("TEMP", c.MkDir())
-
-// 	s.tmpDir = c.MkDir()
-// 	s.rebootScriptName = "juju-reboot-script"
-// 	s.PatchValue(tmpFile, func() (*os.File, error) {
-// 		script := s.rebootScript(c)
-// 		return os.Create(script)
-// 	})
-
-// 	s.mgoInst.EnableAuth = true
-// 	err := s.mgoInst.Start(coretesting.Certs)
-// 	c.Assert(err, jc.ErrorIsNil)
-
-// 	configParams := agent.AgentConfigParams{
-// 		Paths:             agent.Paths{DataDir: c.MkDir()},
-// 		Tag:               names.NewMachineTag("0"),
-// 		UpgradedToVersion: jujuversion.Current,
-// 		StateAddresses:    []string{s.mgoInst.Addr()},
-// 		CACert:            coretesting.CACert,
-// 		Password:          "fake",
-// 		Model:             s.State.ModelTag(),
-// 		MongoVersion:      mongo.Mongo24,
-// 	}
-// 	s.st, _ = s.OpenAPIAsNewMachine(c)
-
-// 	s.acfg, err = agent.NewAgentConfig(configParams)
-// 	c.Assert(err, jc.ErrorIsNil)
-// }
-
-// func (s *RebootSuite) TearDownTest(c *gc.C) {
-// 	s.mgoInst.Destroy()
-// 	s.JujuConnSuite.TearDownTest(c)
-// }
-
-// func (s *RebootSuite) rebootScript(c *gc.C) string {
-// 	return filepath.Join(s.tmpDir, s.rebootScriptName)
-// }
