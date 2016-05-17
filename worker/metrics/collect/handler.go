9a41e93dca197ec02927b5753537fff99a970ff9
Ales Stimec <ales.stimec@canonical.com>
2016-05-17 12:26:11 +0200

    metrics/collect: Read valid metrics each time the handler is called rather than once on startup.

--- worker/metrics/collect/handler.go
+++ worker/metrics/collect/handler.go
@@ -10,16 +10,18 @@ import (
 
 	"github.com/juju/errors"
 	"github.com/juju/names"
-	corecharm "gopkg.in/juju/charm.v6-unstable"
 
+	"github.com/juju/juju/agent"
+	"github.com/juju/juju/worker/fortress"
 	"github.com/juju/juju/worker/metrics/spool"
+	"github.com/juju/juju/worker/uniter"
 )
 
 // handlerConfig stores configuration values for the socketListener.
 type handlerConfig struct {
+	charmdir       fortress.Guest
+	agent          agent.Agent
 	unitTag        names.UnitTag
-	charmURL       *corecharm.URL
-	validMetrics   map[string]corecharm.Metric
 	metricsFactory spool.MetricFactory
 	runner         *hookRunner
 }
@@ -35,6 +37,13 @@ type handler struct {
 // Handle triggers the collect-metrics hook and writes collected metrics
 // to the specified connection.
 func (l *handler) Handle(c net.Conn) (err error) {
+	err = l.config.charmdir.Visit(func() error {
+		return l.do(c)
+	}, nil)
+	return errors.Trace(err)
+}
+
+func (l *handler) do(c net.Conn) (err error) {
 	defer func() {
 		if err != nil {
 			fmt.Fprintf(c, "%v\n", err.Error())
@@ -43,14 +52,21 @@ func (l *handler) Handle(c net.Conn) (err error) {
 		}
 		c.Close()
 	}()
+
+	paths := uniter.NewWorkerPaths(l.config.agent.CurrentConfig().DataDir(), l.config.unitTag, "metrics-collect")
+	charmURL, validMetrics, err := readCharm(l.config.unitTag, paths)
+	if err != nil {
+		return errors.Trace(err)
+	}
+
 	// TODO(fwereade): 2016-03-17 lp:1558657
 	err = c.SetDeadline(time.Now().Add(spool.DefaultTimeout))
 	if err != nil {
 		return errors.Annotate(err, "failed to set the deadline")
 	}
 	recorder, err := l.config.metricsFactory.Recorder(
-		l.config.validMetrics,
-		l.config.charmURL.String(),
+		validMetrics,
+		charmURL.String(),
 		l.config.unitTag.String(),
 	)
 	if err != nil {
