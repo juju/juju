9b71ede6e80f0397faf94237b3517b47812c6915
William Reade <fwereade@gmail.com>
2016-05-19 13:25:59 +0200

    update engine-tracking infrastructure in agent tests

--- cmd/jujud/agent/util_test.go
+++ cmd/jujud/agent/util_test.go
@@ -4,14 +4,16 @@
 package agent
 
 import (
+	"fmt"
 	"sync"
 
 	"github.com/juju/errors"
-	jc "github.com/juju/testing/checkers"
 	"github.com/juju/utils/set"
 	gc "gopkg.in/check.v1"
 
+	"github.com/juju/juju/cmd/jujud/agent/machine"
 	"github.com/juju/juju/cmd/jujud/agent/model"
+	"github.com/juju/juju/cmd/jujud/agent/unit"
 	coretesting "github.com/juju/juju/testing"
 	"github.com/juju/juju/worker"
 	"github.com/juju/juju/worker/dependency"
@@ -66,99 +68,153 @@ var (
 	// at least two 5s ticks to pass, and should expect rare
 	// circumstances to take even longer.
 	ReallyLongWait = coretesting.LongWait * 3
+
+	alwaysUnitWorkers = []string{
+		"api-caller",
+		"api-config-watcher",
+		"...",
+	}
+	aliveUnitWorkers = []string{
+		"...",
+	}
+	migratingUnitWorkers = []string{
+		"...",
+	}
+
+	alwaysMachineWorkers = []string{
+		"api-caller",
+		"api-config-watcher",
+		"...",
+	}
+	aliveMachineWorkers = []string{
+		"...",
+	}
+	migratingMachineWorkers = []string{
+		"...",
+	}
 )
 
-// modelMatchFunc returns a func that will return whether the current
-// set of workers running for the supplied model matches those supplied;
-// and will log what it saw in some detail.
-func modelMatchFunc(c *gc.C, tracker *modelTracker, workers []string) func(string) bool {
+type ModelManifoldsFunc func(config model.ManifoldsConfig) dependency.Manifolds
+
+func TrackModels(c *gc.C, tracker *engineTracker, inner ModelManifoldsFunc) ModelManifoldsFunc {
+	return func(config model.ManifoldsConfig) dependency.Manifolds {
+		raw := inner(config)
+		id := config.Agent.CurrentConfig().Model().Id()
+		if err := tracker.Install(raw, id); err != nil {
+			c.Errorf("cannot install tracker: %v", err)
+		}
+		return raw
+	}
+}
+
+type MachineManifoldsFunc func(config machine.ManifoldsConfig) dependency.Manifolds
+
+func TrackMachines(c *gc.C, tracker *engineTracker, inner MachineManifoldsFunc) MachineManifoldsFunc {
+	return func(config machine.ManifoldsConfig) dependency.Manifolds {
+		raw := inner(config)
+		id := config.Agent.CurrentConfig().Tag().String()
+		if err := tracker.Install(raw, id); err != nil {
+			c.Errorf("cannot install tracker: %v", err)
+		}
+		return raw
+	}
+}
+
+type UnitManifoldsFunc func(config unit.ManifoldsConfig) dependency.Manifolds
+
+func TrackUnits(c *gc.C, tracker *engineTracker, inner UnitManifoldsFunc) UnitManifoldsFunc {
+	return func(config unit.ManifoldsConfig) dependency.Manifolds {
+		raw := inner(config)
+		id := config.Agent.CurrentConfig().Tag().String()
+		if err := tracker.Install(raw, id); err != nil {
+			c.Errorf("cannot install tracker: %v", err)
+		}
+		return raw
+	}
+}
+
+// EngineMatchFunc returns a func that accepts an identifier for a
+// single engine manager by the tracker; that will return true if the
+// workers running in that engine match the supplied workers.
+func EngineMatchFunc(c *gc.C, tracker *engineTracker, workers []string) func(string) bool {
 	expect := set.NewStrings(workers...)
-	return func(uuid string) bool {
-		actual := tracker.Workers(uuid)
-		c.Logf("\n%s: has workers %v", uuid, actual.SortedValues())
+	return func(id string) bool {
+		actual := tracker.Workers(id)
+		c.Logf("\n%s: has workers %v", id, actual.SortedValues())
 		extras := actual.Difference(expect)
 		missed := expect.Difference(actual)
 		if len(extras) == 0 && len(missed) == 0 {
 			return true
 		}
-		c.Logf("%s: waiting for %v", uuid, missed.SortedValues())
-		c.Logf("%s: unexpected %v", uuid, extras.SortedValues())
+		c.Logf("%s: waiting for %v", id, missed.SortedValues())
+		c.Logf("%s: unexpected %v", id, extras.SortedValues())
 		return false
 	}
 }
 
-// newModelTracker creates a type whose Manifolds method can
-// be patched over modelManifolds, and whose Workers method
-// will tell you what workers are currently running stably
-// within the requested model's dependency engine.
-func newModelTracker(c *gc.C) *modelTracker {
-	return &modelTracker{
-		c:       c,
+// NewEngineTracker creates a type that can Install itself into a
+// Manifolds map, and expose recent snapshots of running Workers.
+func NewEngineTracker() *engineTracker {
+	return &engineTracker{
 		current: make(map[string]set.Strings),
 	}
 }
 
-type modelTracker struct {
-	c       *gc.C
+type engineTracker struct {
 	mu      sync.Mutex
 	current map[string]set.Strings
 }
 
-func (tracker *modelTracker) Workers(model string) set.Strings {
+func (tracker *engineTracker) Workers(id string) set.Strings {
 	tracker.mu.Lock()
 	defer tracker.mu.Unlock()
-	return tracker.current[model]
+	return tracker.current[id]
 }
 
-func (tracker *modelTracker) Manifolds(config model.ManifoldsConfig) dependency.Manifolds {
+func (tracker *engineTracker) Install(raw dependency.Manifolds, id string) error {
 	const trackerName = "TEST-TRACKER"
-	raw := model.Manifolds(config)
-	uuid := config.Agent.CurrentConfig().Model().Id()
-
 	names := make([]string, 0, len(raw))
 	for name := range raw {
 		if name == trackerName {
-			tracker.c.Errorf("manifold tracker used repeatedly")
-			return raw
-		} else {
-			names = append(names, name)
+			return errors.New("manifold tracker used repeatedly")
 		}
+		names = append(names, name)
 	}
 
 	tracker.mu.Lock()
 	defer tracker.mu.Unlock()
-	if _, exists := tracker.current[uuid]; exists {
-		tracker.c.Errorf("model %s started repeatedly", uuid)
-		return raw
+	if _, exists := tracker.current[id]; exists {
+		return errors.Errorf("manifolds for %s created repeatedly", id)
 	}
-
-	raw[trackerName] = tracker.manifold(uuid, names)
-	return raw
+	raw[trackerName] = dependency.Manifold{
+		Inputs: names,
+		Start:  tracker.startFunc(id, names),
+	}
+	return nil
 }
 
-func (tracker *modelTracker) manifold(uuid string, names []string) dependency.Manifold {
-	return dependency.Manifold{
-		Inputs: names,
-		Start: func(context dependency.Context) (worker.Worker, error) {
-			seen := set.NewStrings()
-			for _, name := range names {
-				err := context.Get(name, nil)
-				if errors.Cause(err) == dependency.ErrMissing {
-					continue
-				}
-				if tracker.c.Check(err, jc.ErrorIsNil) {
-					seen.Add(name)
-				}
-			}
-			select {
-			case <-context.Abort():
-				// don't bother to report if it's about to change
+func (tracker *engineTracker) startFunc(id string, names []string) dependency.StartFunc {
+	return func(context dependency.Context) (worker.Worker, error) {
+		seen := set.NewStrings()
+		for _, name := range names {
+			err := context.Get(name, nil)
+			switch errors.Cause(err) {
+			case nil:
+			case dependency.ErrMissing:
+				continue
 			default:
-				tracker.mu.Lock()
-				defer tracker.mu.Unlock()
-				tracker.current[uuid] = seen
+				name = fmt.Sprintf("%s [%v]", name, err)
 			}
-			return nil, dependency.ErrMissing
-		},
+			seen.Add(name)
+		}
+		select {
+		case <-context.Abort():
+			// don't bother to report if it's about to change
+		default:
+			tracker.mu.Lock()
+			defer tracker.mu.Unlock()
+			tracker.current[id] = seen
+		}
+		return nil, dependency.ErrMissing
 	}
 }
