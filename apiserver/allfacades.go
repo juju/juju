bebf7520e6de633e8d9c5ee2ea0710feba51a36d
anastasia <anastasia.macmood@canonical.com>
2016-05-23 13:01:34 +1000

    Import apiserver.leadership too to ensure best facade version is used.

--- apiserver/allfacades.go
+++ apiserver/allfacades.go
@@ -27,6 +27,7 @@ import (
 	_ "github.com/juju/juju/apiserver/instancepoller"
 	_ "github.com/juju/juju/apiserver/keymanager"
 	_ "github.com/juju/juju/apiserver/keyupdater"
+	_ "github.com/juju/juju/apiserver/leadership"
 	_ "github.com/juju/juju/apiserver/logger"
 	_ "github.com/juju/juju/apiserver/machine"
 	_ "github.com/juju/juju/apiserver/machinemanager"
