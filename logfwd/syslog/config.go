326aa845dede04f650f282bf61599e94f458c469
Eric Snow <ericsnowcurrently@gmail.com>
2016-05-24 12:55:46 -0600

    Add logfwd/syslog.RawConfig.

---
+++ logfwd/syslog/config.go
@@ -0,0 +1,133 @@
+// Copyright 2016 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package syslog
+
+import (
+	"fmt"
+	"net"
+	"regexp"
+	"strings"
+
+	"github.com/juju/errors"
+
+	"github.com/juju/juju/cert"
+	"github.com/juju/juju/network"
+)
+
+const (
+	// TLSPort is the TCP port used for syslog-over-TLS.
+	TLSPort = 6514
+)
+
+// RawConfig holds the raw configuration data for a connection to a
+// syslog forwarding target.
+type RawConfig struct {
+	// Host is the host-port of the syslog host. The format is:
+	//
+	//   [domain-or-ip-addr] or [domain-or-ip-addr][:port]
+	//
+	// If the port is not set then the default TLS port (6514) will
+	// be used.
+	Host string
+
+	// TODO(ericsnow) Add ExpectedServerCert?
+
+	// CACert is the CA cert PEM to use for the client cert.
+	ClientCACert string
+
+	// ClientCert is the TLS certificate (x.509, PEM-encoded) to use
+	// when connecting.
+	ClientCert string
+
+	// ClientCert is the TLS private key (x.509, PEM-encoded) to use
+	// when connecting.
+	ClientKey string
+}
+
+// Validate ensures that the config is currently valid.
+func (cfg RawConfig) Validate() error {
+	if err := cfg.validateHost(); err != nil {
+		return errors.Trace(err)
+	}
+
+	if err := cfg.validateSSL(); err != nil {
+		return errors.Trace(err)
+	}
+
+	return nil
+}
+
+func (cfg RawConfig) validateHost() error {
+	if cfg.Host == "" {
+		return errors.NewNotValid(nil, "syslog forwarding config missing host")
+	}
+
+	hostport, err := parseHost(cfg.Host)
+	if err != nil {
+		return errors.NewNotValid(err, "syslog forwarding config has bad host")
+	}
+	if hostport.Type == network.HostName && hostport.Value == "" {
+		return errors.NewNotValid(nil, "syslog forwarding config host missing hostname")
+	}
+
+	return nil
+}
+
+// TODO(ericsnow) network.ParseHostPort() should do this for us...
+
+var hostRE = regexp.MustCompile(`^.*:\d+$`)
+
+func parseHost(host string) (*network.HostPort, error) {
+	if _, _, err := net.SplitHostPort(host); err != nil {
+		if hostRE.MatchString(host) {
+			return nil, errors.Trace(err)
+		}
+		host = fmt.Sprintf("%s:%d", host, TLSPort)
+	}
+
+	hostport, err := network.ParseHostPort(host)
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+
+	if hostport.Type == network.HostName {
+		// network.ParseHostPort() *should* do this for us, but currently does not.
+		// TODO(ericsnow) This needs better criteria.
+		if strings.ContainsAny(hostport.Value, "#") {
+			return nil, errors.Errorf("invalid domain name %q", hostport.Value)
+		}
+	}
+
+	return hostport, nil
+}
+
+// TODO(ericsnow) Split up validateSSL() to make it more follow-able?
+
+func (cfg RawConfig) validateSSL() error {
+	if cfg.ClientCert == "" {
+		return errors.NewNotValid(nil, "syslog forwarding config missing client cert")
+	}
+
+	if cfg.ClientKey == "" {
+		return errors.NewNotValid(nil, "syslog forwarding config missing client SSL key")
+	}
+
+	if _, _, err := cert.ParseCertAndKey(cfg.ClientCert, cfg.ClientKey); err != nil {
+		if _, err := cert.ParseCert(cfg.ClientCert); err != nil {
+			return errors.NewNotValid(err, "syslog forwarding config has invalid SSL certificate")
+		}
+		return errors.NewNotValid(err, "syslog forwarding config has invalid client key or key does not match certificate")
+	}
+
+	if cfg.ClientCACert == "" {
+		return errors.NewNotValid(nil, "syslog forwarding config missing SSL CA cert")
+	}
+	if _, err := cert.ParseCert(cfg.ClientCACert); err != nil {
+		return errors.NewNotValid(err, "syslog forwarding config has invalid CA certificate")
+	}
+
+	// TODO(ericsnow) Also call cert.Verify() to ensure the CA cert matches?
+
+	return nil
+}
---
+++ logfwd/syslog/config_test.go
@@ -0,0 +1,347 @@
+// Copyright 2016 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package syslog_test
+
+import (
+	"github.com/juju/errors"
+	"github.com/juju/testing"
+	jc "github.com/juju/testing/checkers"
+	gc "gopkg.in/check.v1"
+
+	"github.com/juju/juju/logfwd/syslog"
+)
+
+type ConfigSuite struct {
+	testing.IsolationSuite
+}
+
+var _ = gc.Suite(&ConfigSuite{})
+
+func (s *ConfigSuite) TestRawValidateFull(c *gc.C) {
+	cfg := syslog.RawConfig{
+		Host:         "a.b.c:9876",
+		ClientCACert: validCACert,
+		ClientCert:   validCert,
+		ClientKey:    validKey,
+	}
+
+	err := cfg.Validate()
+
+	c.Check(err, jc.ErrorIsNil)
+}
+
+func (s *ConfigSuite) TestRawValidateWithoutPort(c *gc.C) {
+	cfg := syslog.RawConfig{
+		Host:         "a.b.c",
+		ClientCACert: validCACert,
+		ClientCert:   validCert,
+		ClientKey:    validKey,
+	}
+
+	err := cfg.Validate()
+
+	c.Check(err, jc.ErrorIsNil)
+}
+
+func (s *ConfigSuite) TestRawValidateZeroValue(c *gc.C) {
+	var cfg syslog.RawConfig
+
+	err := cfg.Validate()
+
+	c.Check(err, jc.Satisfies, errors.IsNotValid)
+}
+
+func (s *ConfigSuite) TestRawValidateMissingHost(c *gc.C) {
+	cfg := syslog.RawConfig{
+		Host:         "",
+		ClientCACert: validCACert,
+		ClientCert:   validCert,
+		ClientKey:    validKey,
+	}
+
+	err := cfg.Validate()
+
+	c.Check(err, jc.Satisfies, errors.IsNotValid)
+	c.Check(err, gc.ErrorMatches, `syslog forwarding config missing host`)
+}
+
+func (s *ConfigSuite) TestRawValidateMissingHostname(c *gc.C) {
+	cfg := syslog.RawConfig{
+		Host:         ":9876",
+		ClientCACert: validCACert,
+		ClientCert:   validCert,
+		ClientKey:    validKey,
+	}
+
+	err := cfg.Validate()
+
+	c.Check(err, jc.Satisfies, errors.IsNotValid)
+	c.Check(err, gc.ErrorMatches, `syslog forwarding config host missing hostname`)
+}
+
+func (s *ConfigSuite) TestRawValidateBadHostname(c *gc.C) {
+	cfg := syslog.RawConfig{
+		Host:         "###:9876",
+		ClientCACert: validCACert,
+		ClientCert:   validCert,
+		ClientKey:    validKey,
+	}
+
+	err := cfg.Validate()
+
+	c.Check(err, jc.Satisfies, errors.IsNotValid)
+	c.Check(err, gc.ErrorMatches, `syslog forwarding config has bad host: invalid domain name "###"`)
+}
+
+func (s *ConfigSuite) TestRawValidateMissingPort(c *gc.C) {
+	cfg := syslog.RawConfig{
+		Host:         "a.b.c:",
+		ClientCACert: validCACert,
+		ClientCert:   validCert,
+		ClientKey:    validKey,
+	}
+
+	err := cfg.Validate()
+
+	c.Check(err, jc.Satisfies, errors.IsNotValid)
+	c.Check(err, gc.ErrorMatches, `syslog forwarding config has bad host: cannot parse "a.b.c:" port: strconv.ParseInt: parsing "": invalid syntax`)
+}
+
+func (s *ConfigSuite) TestRawValidateBadPort(c *gc.C) {
+	cfg := syslog.RawConfig{
+		Host:         "a.b.c:xyz",
+		ClientCACert: validCACert,
+		ClientCert:   validCert,
+		ClientKey:    validKey,
+	}
+
+	err := cfg.Validate()
+
+	c.Check(err, jc.Satisfies, errors.IsNotValid)
+	c.Check(err, gc.ErrorMatches, `syslog forwarding config has bad host: cannot parse "a.b.c:xyz" port: strconv.ParseInt: parsing "xyz": invalid syntax`)
+}
+
+func (s *ConfigSuite) TestRawValidateMissingCACert(c *gc.C) {
+	cfg := syslog.RawConfig{
+		Host:         "a.b.c:9876",
+		ClientCACert: "",
+		ClientCert:   validCert,
+		ClientKey:    validKey,
+	}
+
+	err := cfg.Validate()
+
+	c.Check(err, jc.Satisfies, errors.IsNotValid)
+	c.Check(err, gc.ErrorMatches, `syslog forwarding config missing SSL CA cert`)
+}
+
+func (s *ConfigSuite) TestRawValidateBadCACert(c *gc.C) {
+	cfg := syslog.RawConfig{
+		Host:         "a.b.c:9876",
+		ClientCACert: invalidCACert,
+		ClientCert:   validCert,
+		ClientKey:    validKey,
+	}
+
+	err := cfg.Validate()
+
+	c.Check(err, jc.Satisfies, errors.IsNotValid)
+	c.Check(err, gc.ErrorMatches, `syslog forwarding config has invalid CA certificate: asn1: syntax error: data truncated`)
+}
+
+func (s *ConfigSuite) TestRawValidateBadCACertFormat(c *gc.C) {
+	cfg := syslog.RawConfig{
+		Host:         "a.b.c:9876",
+		ClientCACert: "abc",
+		ClientCert:   validCert,
+		ClientKey:    validKey,
+	}
+
+	err := cfg.Validate()
+
+	c.Check(err, jc.Satisfies, errors.IsNotValid)
+	c.Check(err, gc.ErrorMatches, `syslog forwarding config has invalid CA certificate: no certificates found`)
+}
+
+func (s *ConfigSuite) TestRawValidateMissingCert(c *gc.C) {
+	cfg := syslog.RawConfig{
+		Host:         "a.b.c:9876",
+		ClientCACert: validCACert,
+		ClientCert:   "",
+		ClientKey:    validKey,
+	}
+
+	err := cfg.Validate()
+
+	c.Check(err, jc.Satisfies, errors.IsNotValid)
+	c.Check(err, gc.ErrorMatches, `syslog forwarding config missing client cert`)
+}
+
+func (s *ConfigSuite) TestRawValidateBadCert(c *gc.C) {
+	cfg := syslog.RawConfig{
+		Host:         "a.b.c:9876",
+		ClientCACert: validCACert,
+		ClientCert:   invalidCert,
+		ClientKey:    validKey,
+	}
+
+	err := cfg.Validate()
+
+	c.Check(err, jc.Satisfies, errors.IsNotValid)
+	c.Check(err, gc.ErrorMatches, `syslog forwarding config has invalid SSL certificate: asn1: syntax error: data truncated`)
+}
+
+func (s *ConfigSuite) TestRawValidateBadCertFormat(c *gc.C) {
+	cfg := syslog.RawConfig{
+		Host:         "a.b.c:9876",
+		ClientCACert: validCACert,
+		ClientCert:   "abc",
+		ClientKey:    validKey,
+	}
+
+	err := cfg.Validate()
+
+	c.Check(err, jc.Satisfies, errors.IsNotValid)
+	c.Check(err, gc.ErrorMatches, `syslog forwarding config has invalid SSL certificate: no certificates found`)
+}
+
+func (s *ConfigSuite) TestRawValidateMissingKey(c *gc.C) {
+	cfg := syslog.RawConfig{
+		Host:         "a.b.c:9876",
+		ClientCACert: validCACert,
+		ClientCert:   validCert,
+		ClientKey:    "",
+	}
+
+	err := cfg.Validate()
+
+	c.Check(err, jc.Satisfies, errors.IsNotValid)
+	c.Check(err, gc.ErrorMatches, `syslog forwarding config missing client SSL key`)
+}
+
+func (s *ConfigSuite) TestRawValidateBadKey(c *gc.C) {
+	cfg := syslog.RawConfig{
+		Host:         "a.b.c:9876",
+		ClientCACert: validCACert,
+		ClientCert:   validCert,
+		ClientKey:    invalidKey,
+	}
+
+	err := cfg.Validate()
+
+	c.Check(err, jc.Satisfies, errors.IsNotValid)
+	c.Check(err, gc.ErrorMatches, `syslog forwarding config has invalid client key or key does not match certificate: crypto/tls: failed to parse private key`)
+}
+
+func (s *ConfigSuite) TestRawValidateBadKeyFormat(c *gc.C) {
+	cfg := syslog.RawConfig{
+		Host:         "a.b.c:9876",
+		ClientCACert: validCACert,
+		ClientCert:   validCert,
+		ClientKey:    "abc",
+	}
+
+	err := cfg.Validate()
+
+	c.Check(err, jc.Satisfies, errors.IsNotValid)
+	c.Check(err, gc.ErrorMatches, `syslog forwarding config has invalid client key or key does not match certificate: crypto/tls: failed to find any PEM data in key input`)
+}
+
+func (s *ConfigSuite) TestRawValidateCertKeyMismatch(c *gc.C) {
+	cfg := syslog.RawConfig{
+		Host:         "a.b.c:9876",
+		ClientCACert: validCACert,
+		ClientCert:   validCert,
+		ClientKey:    validKey2,
+	}
+
+	err := cfg.Validate()
+
+	c.Check(err, jc.Satisfies, errors.IsNotValid)
+	c.Check(err, gc.ErrorMatches, `syslog forwarding config has invalid client key or key does not match certificate: crypto/tls: private key does not match public key`)
+}
+
+var validCACert = `
+-----BEGIN CERTIFICATE-----
+MIIBjTCCATmgAwIBAgIBADALBgkqhkiG9w0BAQUwHjENMAsGA1UEChMEanVqdTEN
+MAsGA1UEAxMEcm9vdDAeFw0xMjExMDkxNjQxMjlaFw0yMjExMDkxNjQ2MjlaMB4x
+DTALBgNVBAoTBGp1anUxDTALBgNVBAMTBHJvb3QwWjALBgkqhkiG9w0BAQEDSwAw
+SAJBAIW7CbHFJivvV9V6mO8AGzJS9lqjUf6MdEPsdF6wx2Cpzr/lSFIggCwRA138
+9MuFxflxb/3U8Nq+rd8rVtTgFMECAwEAAaNmMGQwDgYDVR0PAQH/BAQDAgCkMBIG
+A1UdEwEB/wQIMAYBAf8CAQEwHQYDVR0OBBYEFJafrxqByMN9BwGfcmuF0Lw/1QII
+MB8GA1UdIwQYMBaAFJafrxqByMN9BwGfcmuF0Lw/1QIIMAsGCSqGSIb3DQEBBQNB
+AHq3vqNhxya3s33DlQfSj9whsnqM0Nm+u8mBX/T76TF5rV7+B33XmYzSyfA3yBi/
+zHaUR/dbHuiNTO+KXs3/+Y4=
+-----END CERTIFICATE-----
+`[1:]
+
+var validCert = `
+-----BEGIN CERTIFICATE-----
+MIIBjDCCATigAwIBAgIBADALBgkqhkiG9w0BAQUwHjENMAsGA1UEChMEanVqdTEN
+MAsGA1UEAxMEcm9vdDAeFw0xMjExMDkxNjQwMjhaFw0yMjExMDkxNjQ1MjhaMB4x
+DTALBgNVBAoTBGp1anUxDTALBgNVBAMTBHJvb3QwWTALBgkqhkiG9w0BAQEDSgAw
+RwJAduA1Gnb2VJLxNGfG4St0Qy48Y3q5Z5HheGtTGmti/FjlvQvScCFGCnJG7fKA
+Knd7ia3vWg7lxYkIvMPVP88LAQIDAQABo2YwZDAOBgNVHQ8BAf8EBAMCAKQwEgYD
+VR0TAQH/BAgwBgEB/wIBATAdBgNVHQ4EFgQUlvKX8vwp0o+VdhdhoA9O6KlOm00w
+HwYDVR0jBBgwFoAUlvKX8vwp0o+VdhdhoA9O6KlOm00wCwYJKoZIhvcNAQEFA0EA
+LlNpevtFr8gngjAFFAO/FXc7KiZcCrA5rBfb/rEy297lIqmKt5++aVbLEPyxCIFC
+r71Sj63TUTFWtRZAxvn9qQ==
+-----END CERTIFICATE-----
+`[1:]
+
+var validKey = `
+-----BEGIN RSA PRIVATE KEY-----
+MIIBOQIBAAJAduA1Gnb2VJLxNGfG4St0Qy48Y3q5Z5HheGtTGmti/FjlvQvScCFG
+CnJG7fKAKnd7ia3vWg7lxYkIvMPVP88LAQIDAQABAkEAsFOdMSYn+AcF1M/iBfjo
+uQWJ+Zz+CgwuvumjGNsUtmwxjA+hh0fCn0Ah2nAt4Ma81vKOKOdQ8W6bapvsVDH0
+6QIhAJOkLmEKm4H5POQV7qunRbRsLbft/n/SHlOBz165WFvPAiEAzh9fMf70std1
+sVCHJRQWKK+vw3oaEvPKvkPiV5ui0C8CIGNsvybuo8ald5IKCw5huRlFeIxSo36k
+m3OVCXc6zfwVAiBnTUe7WcivPNZqOC6TAZ8dYvdWo4Ifz3jjpEfymjid1wIgBIJv
+ERPyv2NQqIFQZIyzUP7LVRIWfpFFOo9/Ww/7s5Y=
+-----END RSA PRIVATE KEY-----
+`[1:]
+
+var validCert2 = `
+-----BEGIN CERTIFICATE-----
+MIIBjTCCATmgAwIBAgIBADALBgkqhkiG9w0BAQUwHjENMAsGA1UEChMEanVqdTEN
+MAsGA1UEAxMEcm9vdDAeFw0xMjExMDkxNjQxMDhaFw0yMjExMDkxNjQ2MDhaMB4x
+DTALBgNVBAoTBGp1anUxDTALBgNVBAMTBHJvb3QwWjALBgkqhkiG9w0BAQEDSwAw
+SAJBAJkSWRrr81y8pY4dbNgt+8miSKg4z6glp2KO2NnxxAhyyNtQHKvC+fJALJj+
+C2NhuvOv9xImxOl3Hg8fFPCXCtcCAwEAAaNmMGQwDgYDVR0PAQH/BAQDAgCkMBIG
+A1UdEwEB/wQIMAYBAf8CAQEwHQYDVR0OBBYEFOsX/ZCqKzWCAaTTVcWsWKT5Msow
+MB8GA1UdIwQYMBaAFOsX/ZCqKzWCAaTTVcWsWKT5MsowMAsGCSqGSIb3DQEBBQNB
+AAVV57jetEzJQnjgBzhvx/UwauFn78jGhXfV5BrQmxIb4SF4DgSCFstPwUQOAr8h
+XXzJqBQH92KYmp+y3YXDoMQ=
+-----END CERTIFICATE-----
+`[1:]
+
+var validKey2 = `
+-----BEGIN RSA PRIVATE KEY-----
+MIIBOQIBAAJBAJkSWRrr81y8pY4dbNgt+8miSKg4z6glp2KO2NnxxAhyyNtQHKvC
++fJALJj+C2NhuvOv9xImxOl3Hg8fFPCXCtcCAwEAAQJATQNzO11NQvJS5U6eraFt
+FgSFQ8XZjILtVWQDbJv8AjdbEgKMHEy33icsAKIUAx8jL9kjq6K9kTdAKXZi9grF
+UQIhAPD7jccIDUVm785E5eR9eisq0+xpgUIa24Jkn8cAlst5AiEAopxVFl1auer3
+GP2In3pjdL4ydzU/gcRcYisoJqwHpM8CIHtqmaXBPeq5WT9ukb5/dL3+5SJCtmxA
+jQMuvZWRe6khAiBvMztYtPSDKXRbCZ4xeQ+kWSDHtok8Y5zNoTeu4nvDrwIgb3Al
+fikzPveC5g6S6OvEQmyDz59tYBubm2XHgvxqww0=
+-----END RSA PRIVATE KEY-----
+`[1:]
+
+var invalidCACert = `
+-----BEGIN CERTIFICATE-----
+MIIBOgIBAAJAZabKgKInuOxj5vDWLwHHQtK3/45KB+32D15w94Nt83BmuGxo90lw
+-----END CERTIFICATE-----
+`[1:]
+
+var invalidCert = `
+-----BEGIN CERTIFICATE-----
+MIIBOgIBAAJAZabKgKInuOxj5vDWLwHHQtK3/45KB+32D15w94Nt83BmuGxo90lw
+-----END CERTIFICATE-----
+`[1:]
+
+var invalidKey = `
+-----BEGIN RSA PRIVATE KEY-----
+MIIBOgIBAAJAZabKgKInuOxj5vDWLwHHQtK3/45KB+32D15w94Nt83BmuGxo90lw
+-----END RSA PRIVATE KEY-----
+`[1:]
