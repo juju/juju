ba0d8f7b78a2c9459347f96cc753e1fd77bb8797
Tim Penhey <tim@penhey.net>
2016-06-01 10:20:43 +1200

    Don't run state or state/backup tests on non-Ubuntu machines.

--- state/package_test.go
+++ state/package_test.go
@@ -6,9 +6,16 @@ package state_test
 import (
 	stdtesting "testing"
 
+	"github.com/juju/utils/os"
+
 	coretesting "github.com/juju/juju/testing"
 )
 
 func TestPackage(t *stdtesting.T) {
-	coretesting.MgoTestPackage(t)
+	// At this stage, Juju only supports running the apiservers and database
+	// on Ubuntu. If we end up officially supporting CentOS, then we should
+	// make sure we run the tests there.
+	if os.HostOS() == os.Ubuntu {
+		coretesting.MgoTestPackage(t)
+	}
 }
