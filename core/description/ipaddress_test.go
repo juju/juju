f53493289e8ab7a2e5bbeb248f52a7b116cb846b
Michael Foord <michael.foord@canonical.com>
2016-05-31 12:24:32 +0100

    Test fixes

--- core/description/ipaddress_test.go
+++ core/description/ipaddress_test.go
@@ -17,50 +17,56 @@ var _ = gc.Suite(&IPAddressSerializationSuite{})
 
 func (s *IPAddressSerializationSuite) SetUpTest(c *gc.C) {
 	s.SliceSerializationSuite.SetUpTest(c)
-	s.importName = "addresss"
-	s.sliceName = "addresss"
+	s.importName = "addresses"
+	s.sliceName = "addresses"
 	s.importFunc = func(m map[string]interface{}) (interface{}, error) {
-		return importIPAddresss(m)
+		return importIPAddresses(m)
 	}
 	s.testFields = func(m map[string]interface{}) {
-		m["addresss"] = []interface{}{}
+		m["addresses"] = []interface{}{}
 	}
 }
 
 func (s *IPAddressSerializationSuite) TestNewIPAddress(c *gc.C) {
 	args := IPAddressArgs{
-		CIDR:              "10.0.0.0/24",
-		ProviderId:        "magic",
-		VLANTag:           64,
-		SpaceName:         "foo",
-		AvailabilityZone:  "bar",
-		AllocatableIPHigh: "10.0.0.255",
-		AllocatableIPLow:  "10.0.0.0",
+		SubnetCIDR:       "10.0.0.0/24",
+		ProviderID:       "magic",
+		DeviceName:       "foo",
+		MachineID:        "bar",
+		ConfigMethod:     "static",
+		Value:            "10.0.0.4",
+		DNSServers:       []string{"10.1.0.1", "10.2.0.1"},
+		DNSSearchDomains: []string{"bam", "mam"},
+		GatewayAddress:   "10.0.0.1",
 	}
 	address := newIPAddress(args)
-	c.Assert(address.CIDR(), gc.Equals, args.CIDR)
-	c.Assert(address.ProviderId(), gc.Equals, args.ProviderId)
-	c.Assert(address.VLANTag(), gc.Equals, args.VLANTag)
-	c.Assert(address.SpaceName(), gc.Equals, args.SpaceName)
-	c.Assert(address.AvailabilityZone(), gc.Equals, args.AvailabilityZone)
-	c.Assert(address.AllocatableIPHigh(), gc.Equals, args.AllocatableIPHigh)
-	c.Assert(address.AllocatableIPLow(), gc.Equals, args.AllocatableIPLow)
+	c.Assert(address.SubnetCIDR(), gc.Equals, args.SubnetCIDR)
+	c.Assert(address.ProviderID(), gc.Equals, args.ProviderID)
+	c.Assert(address.DeviceName(), gc.Equals, args.DeviceName)
+	c.Assert(address.MachineID(), gc.Equals, args.MachineID)
+	c.Assert(address.ConfigMethod(), gc.Equals, args.ConfigMethod)
+	c.Assert(address.Value(), gc.Equals, args.Value)
+	c.Assert(address.DNSServers(), gc.Equals, args.DNSServers)
+	c.Assert(address.DNSSearchDomains(), gc.Equals, args.DNSSearchDomains)
+	c.Assert(address.GatewayAddress(), gc.Equals, args.GatewayAddress)
 }
 
 func (s *IPAddressSerializationSuite) TestParsingSerializedData(c *gc.C) {
-	initial := addresss{
+	initial := ipaddresses{
 		Version: 1,
-		IPAddresss_: []*address{
+		IPAddresses_: []*ipaddress{
 			newIPAddress(IPAddressArgs{
-				CIDR:              "10.0.0.0/24",
-				ProviderId:        "magic",
-				VLANTag:           64,
-				SpaceName:         "foo",
-				AvailabilityZone:  "bar",
-				AllocatableIPHigh: "10.0.0.255",
-				AllocatableIPLow:  "10.0.0.0",
+				SubnetCIDR:       "10.0.0.0/24",
+				ProviderID:       "magic",
+				DeviceName:       "foo",
+				MachineID:        "bar",
+				ConfigMethod:     "static",
+				Value:            "10.0.0.4",
+				DNSServers:       []string{"10.1.0.1", "10.2.0.1"},
+				DNSSearchDomains: []string{"bam", "mam"},
+				GatewayAddress:   "10.0.0.1",
 			}),
-			newIPAddress(IPAddressArgs{CIDR: "10.0.1.0/24"}),
+			newIPAddress(IPAddressArgs{Value: "10.0.0.5"}),
 		},
 	}
 
@@ -71,8 +77,8 @@ func (s *IPAddressSerializationSuite) TestParsingSerializedData(c *gc.C) {
 	err = yaml.Unmarshal(bytes, &source)
 	c.Assert(err, jc.ErrorIsNil)
 
-	addresss, err := importIPAddresss(source)
+	addresses, err := importIPAddresses(source)
 	c.Assert(err, jc.ErrorIsNil)
 
-	c.Assert(addresss, jc.DeepEquals, initial.IPAddresss_)
+	c.Assert(addresses, jc.DeepEquals, initial.IPAddresses_)
 }
