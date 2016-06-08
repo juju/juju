c6d778702d77ae1730adc9901e53158d3a577868
anastasia <anastasia.macmood@canonical.com>
2016-06-08 22:15:24 +1000

    Added test to ensure that we fallthrough to trying to login with old password when we get certain errors.

--- worker/apicaller/open_test.go
+++ worker/apicaller/open_test.go
@@ -6,11 +6,13 @@ package apicaller
 import (
 	"fmt"
 
+	jc "github.com/juju/testing/checkers"
 	"github.com/juju/utils"
 	gc "gopkg.in/check.v1"
 
 	"github.com/juju/juju/agent"
 	"github.com/juju/juju/api"
+	"github.com/juju/juju/apiserver/common"
 	"github.com/juju/juju/apiserver/params"
 	"github.com/juju/juju/state/multiwatcher"
 	coretesting "github.com/juju/juju/testing"
@@ -85,6 +87,31 @@ func (s *OpenAPIStateSuite) TestOpenAPIStateWaitsProvisionedGivesUp(c *gc.C) {
 	// +1 because we always attempt at least once outside the attempt strategy
 	// (twice if the API server initially returns CodeUnauthorized.)
 	c.Assert(called, gc.Equals, checkProvisionedStrategy.Min+1)
+}
+
+func (s *OpenAPIStateSuite) TestOpenAPIStateUsesOldPwd(c *gc.C) {
+	currentPwd := "current"
+	oldPwd := "old"
+
+	var apiError error
+	s.PatchValue(&apiOpen, func(info *api.Info, opts api.DialOpts) (api.Connection, error) {
+		if info.Password != oldPwd {
+			return nil, apiError
+		}
+		return nil, nil
+	})
+
+	// If we receive these errors, we should try to login using old password
+	fallthroughErrors := []error{common.ErrBadCreds, &params.Error{Code: params.CodeUnauthorized}}
+	info := &api.Info{Password: currentPwd}
+
+	for i, errFallthrough := range fallthroughErrors {
+		c.Logf("test %d", i)
+		apiError = errFallthrough
+		_, usedOldPassword, err := openAPIStateUsingInfo(info, oldPwd)
+		c.Assert(err, jc.ErrorIsNil)
+		c.Assert(usedOldPassword, jc.IsTrue)
+	}
 }
 
 type fakeAgent struct {
