cb5ee490ea7ecad3599d6db4bcf1c4e0f9d83c82
Menno Smits <menno.smits@canonical.com>
2016-06-15 14:44:17 +1200

    api/migrationminion: Remove unnecessary interface
    It wasn't really being used.

--- api/migrationminion/client.go
+++ api/migrationminion/client.go
@@ -12,17 +12,8 @@ import (
 	"github.com/juju/juju/watcher"
 )
 
-// Client describes the client side API for the MigrationMinion facade
-// (used by the migration minion worker).
-type Client interface {
-	// Watch returns a watcher which reports when the status changes
-	// for the migration for the model associated with the API
-	// connection.
-	Watch() (watcher.MigrationStatusWatcher, error)
-}
-
 // NewClient returns a new Client based on an existing API connection.
-func NewClient(caller base.APICaller) Client {
+func NewClient(caller base.APICaller) *client {
 	return &client{base.NewFacadeCaller(caller, "MigrationMinion")}
 }
 
@@ -31,7 +22,8 @@ type client struct {
 	caller base.FacadeCaller
 }
 
-// Watch implements Client.
+// Watch returns a watcher which reports when the status changes for
+// the migration for the model associated with the API connection.
 func (c *client) Watch() (watcher.MigrationStatusWatcher, error) {
 	var result params.NotifyWatchResult
 	err := c.caller.FacadeCall("Watch", nil, &result)
