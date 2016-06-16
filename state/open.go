24dfe5281971b905329856c53da32ce353747bfc
Ian Booth <ian.booth@canonical.com>
2016-06-16 16:53:21 +1000

    Add validation todo

--- state/open.go
+++ state/open.go
@@ -158,6 +158,8 @@ func (p InitializeParams) Validate() error {
 	if p.Cloud.Type == "" {
 		return errors.NotValidf("empty Cloud")
 	}
+	// TODO(wallyworld) - TODO check that the cloud has a non-empty list of auth types
+	//
 	if _, err := validateCloudRegion(p.Cloud, p.ControllerModelArgs.CloudRegion); err != nil {
 		return errors.Annotate(err, "validating controller model cloud region")
 	}
