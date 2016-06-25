0bfacc77e1a92e63b42e36d171042316a610d9ac
Eric Snow <ericsnowcurrently@gmail.com>
2016-06-24 18:28:48 -0600

    Add documentation for several packages.

---
+++ standards/rfc5424/doc.go
@@ -0,0 +1,7 @@
+// Copyright 2016 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+// The rfc5424 package holds an implementation of a RFC 5424 syslog
+// client along with types and functions that facilitate complying
+// with the RFC.
+package rfc5424
