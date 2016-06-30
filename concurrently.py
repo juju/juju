4a5bdd13b3c257fa243c1f5300572219bbd95952
Curtis Hovey <curtis@canonical.com>
2016-07-01 00:20:56 +0100

    Use context manager to manage procs.

--- concurrently.py
+++ concurrently.py
@@ -3,9 +3,9 @@
 from __future__ import print_function
 
 from argparse import ArgumentParser
+from contextlib import contextmanager
 import subprocess
 import sys
-import time
 import traceback
 
 __metaclass__ = type
@@ -18,75 +18,51 @@ class Task:
         self.command = self.commandline.split()
         self.out_log_name = '{}-out.log'.format(self.name)
         self.err_log_name = '{}-err.log'.format(self.name)
-        self.proc = None
         self.returncode = None
-        self._stdout = None
-        self._stderr = None
-        self._done = False
 
+    @contextmanager
     def run(self):
-        """Run a task in a subprocess and log std out and err."""
-        self._open_logs()
-        self.proc = subprocess.Popen(
-            self.command, stdout=self._stdout, stderr=self._stderr)
-
-    def _open_logs(self):
-        """Open the tasks out and err logs for appending."""
-        self._stdout = open(self.out_log_name, 'ab')
-        self._stderr = open(self.err_log_name, 'ab')
-
-    def _close_logs(self):
-        """Close the tasks out and err logs."""
-        self._stdout.close()
-        self._stderr.close()
-
-    def is_done(self):
-        """Is the process complete.
-
-        This call implicitly closes the std out and error logs.
-        """
-        if self.proc and self.proc.poll() is not None:
-            self._close_logs()
-            self.returncode = self.proc.returncode
-            self._done = True
-        return self._done
-
-    def is_success(self):
-        """Is the process successful."""
-        return self.returncode == 0
+        """Yield the running proc, then wait to set the returncode."""
+        with open(self.out_log_name, 'ab') as out_log:
+            with open(self.err_log_name, 'ab') as err_log:
+                try:
+                    proc = subprocess.Popen(
+                        self.command, stdout=out_log, stderr=err_log)
+                    yield proc
+                finally:
+                    self.returncode = proc.wait()
 
 
-class TaskManager:
+def run_all(tasks):
+    """Run all tasks in the list.
 
-    def __init__(self, tasks, concurrency=8):
-        self.backlog = tasks
-        self.concurrency = concurrency
-        self.running = []
-        self.complete = []
+    The list is a queue that will be emptied.
+    """
+    try:
+        task = tasks.pop()
+    except IndexError:
+        return
+    with task.run():
+        run_all(tasks)
 
-    def run(self):
-        """Exec tasks in parallel."""
-        while True:
-            while self.backlog and len(self.running) < self.concurrency:
-                task = self.backlog.pop()
-                self.running.append(task)
-                print(task.commandline)
-                task.run()
-            for task in self.running:
-                if task.is_done():
-                    self.running.remove(task)
-                    self.complete.append(task)
-            if not self.backlog and not self.running:
-                break
-            else:
-                time.sleep(0.05)
 
-    def get_returncode(self):
-        """Return the sum of all return codes."""
-        return sum([t.returncode for t in self.complete])
+def summarise_tasks(tasks, verbose=False):
+    """Return of sum of tasks returncodes."""
+    returncode = sum([t.returncode for t in tasks])
+    if verbose:
+        if returncode == 0:
+            print('SUCCESS')
+        else:
+            print('FAIL')
+            for task in tasks:
+                if task.returncode != 0:
+                    print('  {} failed with {}'.format(
+                          task.name, task.returncode))
+    return returncode
 
 
 def parse_args(argv=None):
+    """Return the parsed args for this program."""
     parser = ArgumentParser(
         description="Run many processes concurrently.")
     parser.add_argument(
@@ -100,20 +76,19 @@ def parse_args(argv=None):
 
 def main(argv=None):
     """Run go test against the content of a tarfile."""
-    returncode = 0
+    returncode = 254
     args = parse_args(argv)
     tasks = [Task(t) for t in args.tasks]
     try:
         if args.verbose:
             names = [t.name for t in tasks]
             print('Running these tasks {}'.format(names))
-        task_manager = TaskManager(tasks)
-        task_manager.run()
-        returncode = task_manager.get_returncode()
+        run_all(list(tasks))
+        returncode = summarise_tasks(tasks, verbose=args.verbose)
     except Exception as e:
         print(str(e))
         print(traceback.print_exc())
-        return 2
+        return 253
     return returncode
 
 
