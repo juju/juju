7fc56da7a743ffd234f83632e1aa54c55f9ef432
Ian Booth <ian.booth@canonical.com>
2016-07-01 23:50:28 +1000

    Add credential detection to MAAS

--- provider/maas/credentials.go
+++ provider/maas/credentials.go
@@ -4,7 +4,14 @@
 package maas
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+
 	"github.com/juju/errors"
+	"github.com/juju/utils"
+
 	"github.com/juju/juju/cloud"
 )
 
@@ -26,6 +33,34 @@ func (environProviderCredentials) CredentialSchemas() map[cloud.AuthType]cloud.C
 
 // DetectCredentials is part of the environs.ProviderCredentials interface.
 func (environProviderCredentials) DetectCredentials() (*cloud.CloudCredential, error) {
-	// TODO(axw) find out where the MAAS CLI stores credentials.
-	return nil, errors.NotFoundf("credentials")
+	// MAAS stores credentials in a json file: ~/.maasrc
+	// {"Server": "http://<ip>/MAAS", "OAuth": "<key>"}
+	maasrc := filepath.Join(utils.Home(), ".maasrc")
+	fileBytes, err := ioutil.ReadFile(maasrc)
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+
+	details := make(map[string]interface{})
+	err = json.Unmarshal(fileBytes, &details)
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+	oauthKey := details["OAuth"]
+	if oauthKey == "" {
+		return nil, errors.New("MAAS credentials require a value for OAuth token")
+	}
+	cred := cloud.NewCredential(cloud.OAuth1AuthType, map[string]string{
+		"maas-oauth": fmt.Sprintf("%v", oauthKey),
+	})
+	server, ok := details["Server"]
+	if server == "" || !ok {
+		server = "unspecified server"
+	}
+	cred.Label = fmt.Sprintf("MAAS credential for %s", server)
+
+	return &cloud.CloudCredential{
+		AuthCredentials: map[string]cloud.Credential{
+			"default": cred,
+		}}, nil
 }
