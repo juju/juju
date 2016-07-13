39411ef61586787be903fa0b10d5b5b2ed15c778
Andrew Wilkins <axwalk@gmail.com>
2016-07-13 20:31:17 +0800

    storage: move DeviceLink sorting to diskmanager
    SortBlockDevices was changed to alter the contents
    of BlockDevices, whereas its intended and documented
    purpose is to reorder elements in a slice.
    
    Only the diskmanager cares about the order of contents
    of BlockDevices. The sorting of block device contents
    has been relocated there.

--- storage/path_test.go
+++ storage/path_test.go
@@ -37,24 +37,3 @@ func testBlockDevicePath(c *gc.C, dev storage.BlockDevice, expect string) {
 	c.Assert(err, jc.ErrorIsNil)
 	c.Assert(path, jc.SamePath, expect)
 }
-
-func (s *BlockDevicePathSuite) TestSortBlockDevices(c *gc.C) {
-	devices := []storage.BlockDevice{{
-		DeviceName:  "sdb",
-		DeviceLinks: []string{"by-b", "by-a"},
-	}, {
-		DeviceName:  "sda",
-		DeviceLinks: []string{"by-c", "by-d"},
-	}}
-	storage.SortBlockDevices(devices)
-
-	expected := []storage.BlockDevice{{
-		DeviceName:  "sda",
-		DeviceLinks: []string{"by-c", "by-d"},
-	}, {
-		DeviceName:  "sdb",
-		DeviceLinks: []string{"by-a", "by-b"},
-	}}
-
-	c.Assert(devices, jc.DeepEquals, expected)
-}
