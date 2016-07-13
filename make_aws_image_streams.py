e7698d8ac9fcf7e148bc11abefb76c79acad6e0d
Aaron Bentley <aaron.bentley@canonical.com>
2016-07-13 11:25:03 -0400

    Get make_aws_items under test.

--- make_aws_image_streams.py
+++ make_aws_image_streams.py
@@ -227,10 +227,9 @@ def write_juju_streams(out_d, trees, updated, sticky):
     return out_filenames
 
 
-def make_aws_items(all_credentials):
+def make_aws_items(all_credentials, now):
     credentials = make_aws_credentials(all_credentials['aws'])
     china_credentials = make_aws_credentials(all_credentials['aws-china'])
-    now = datetime.utcnow()
     return [make_item(i, now) for i in
             iter_centos_images(credentials, china_credentials)]
 
@@ -239,7 +238,8 @@ def main():
     streams, creds_filename = get_parameters()
     with open(creds_filename) as creds_file:
         all_credentials = yaml.safe_load(creds_file)['credentials']
-    items = make_aws_items(all_credentials)
+    now = datetime.utcnow()
+    items = make_aws_items(all_credentials, now)
     write_item_streams(items, streams)
 
 
