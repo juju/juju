d9d777bd9954f3a3b742764f134cbe17bc01e08a
Christopher Lee <chris.lee@canonical.com>
2016-07-21 09:41:39 +1200

    Initial collectino of mongodb stats.

--- assess_perf_test_simple.py
+++ assess_perf_test_simple.py
@@ -4,7 +4,9 @@
 from __future__ import print_function
 
 import argparse
+from collections import namedtuple
 from contextlib import contextmanager
+from datetime import datetime
 from jinja2 import Template
 import logging
 import os
@@ -24,6 +26,32 @@ from utility import (
 
 
 __metaclass__ = type
+
+
+class TimingData:
+
+    strf_format = '%F %H:%M:%S'
+
+    def __init__(self, start, end):
+        self._start = start
+        self._end = end
+        self._seconds = int((end - start).total_seconds())
+
+    @property
+    def start(self):
+        return self._start.strftime(self.strf_format)
+
+    @property
+    def end(self):
+        return self._end.strftime(self.strf_format)
+
+    @property
+    def seconds(self):
+        return self._seconds
+
+DeployDetails = namedtuple(
+    'DeployDetails', ['name', 'applications', 'timings'])
+
 
 collectd_config = dedent("""\
 # Global
@@ -146,40 +174,21 @@ LoadPlugin users
 log = logging.getLogger("assess_perf_test_simple")
 
 
-@contextmanager
-def _timer(log_details_name):
-    try:
-        start = time.time()
-        yield
-    finally:
-        end = time.time()
-        print('TIME {}: {} seconds'.format(
-            log_details_name,
-            end-start))
-
-
 def assess_perf_test_simple(bs_manager, upload_tools):
-    bs_start = time.time()
+    bs_start = datetime.now()
     # Make sure you check the responsiveness of 'juju status'
     with bs_manager.booted_context(upload_tools):
         try:
             client = bs_manager.client
             admin_client = client.get_admin_client()
             admin_client.wait_for_started()
-            bs_end = time.time()
-            bootstrap_time = bs_end - bs_start
-            print('TIME bootstrap: {}'.format(bootstrap_time))
+            bs_end = datetime.now()
+            bootstrap_timing = TimingData(bs_start, bs_end)
 
             setup_system_monitoring(admin_client)
 
-            deploy_start = time.time()
-            assess_deployment_perf(client)
-            deploy_end = time.time()
-            deploy_time = deploy_end - deploy_start
+            deploy_details = assess_deployment_perf(client)
         finally:
-            # Grab the performance data files from the client(s)
-            # make dir in logs dir for the results files.
-            # grab the results files to a known place.
             results_dir = os.path.join(
                 os.path.abspath(bs_manager.log_dir), 'performance_results/')
             os.makedirs(results_dir)
@@ -188,23 +197,49 @@ def assess_perf_test_simple(bs_manager, upload_tools):
                 ('--', '-r', '0:/var/lib/collectd/rrd/localhost/*',
                  results_dir)
             )
-            timings = dict(bootstrap=bootstrap_time, deploys=[deploy_time])
-            generate_reports(results_dir, timings)
+
+            admin_client.juju(
+                'scp', ('0:/tmp/mongodb-stats.log', results_dir)
+            )
+
+            deployments = dict(
+                bootstrap=bootstrap_timing, deploys=[deploy_details])
+            generate_reports(results_dir, deployments)
 
 
-def generate_reports(results_dir, timings):
+def generate_reports(results_dir, deployments):
     """Generate reports and graphs from run results."""
     cpu_image = generate_cpu_graph_image(results_dir)
     memory_image = generate_memory_graph_image(results_dir)
     swap_image = generate_swap_graph_image(results_dir)
     network_image = generate_network_graph_image(results_dir)
+    mongo_image = generate_mongo_graph_image(results_dir)
 
     details = dict(
         cpu_graph=cpu_image,
         memory_graph=memory_image,
         swap_graph=swap_image,
         network_graph=network_image,
-        timings=timings,
+        mongo_graph=mongo_image,
+        deployments=deployments,
+        log_message_chunks=[
+            dict(
+                timeframe='02:40:27 - 2:40:47',
+                message="<br/>".join([
+                    "2016-07-19 02:40:27 INFO juju.apiserver request_notifier.go:78 [16] user-admin@local API connection terminated after 244.262266ms",
+                    "2016-07-19 02:40:27 INFO juju.apiserver request_notifier.go:68 [18] API connection from 10.194.140.1:52650",
+                    ". . .",
+                    "2016-07-19 02:40:41 INFO juju.apiserver request_notifier.go:68 [20] API connection from 10.194.140.1:52692",
+                    "2016-07-19 02:40:41 INFO juju.apiserver request_notifier.go:78 [20] user-admin@local API connection terminated after 37.649557ms"])),
+            dict(
+                timeframe='02:40:47 - 2:41:07',
+                message="<br/>".join([
+                    "2016-47-19 02:40:48 INFO juju.apiserver request_notifier.go:78 [1486] user-admin@local API connection terminated after 37.836893ms",
+                    "2016-07-19 02:40:49 INFO juju.apiserver request_notifier.go:68 [1487] API connection from 10.194.140.1:35212",
+                    ". . . ",
+                    "2016-07-19 02:41:05 INFO juju.apiserver.metricsender metricsender.go:74 nothing to send",
+                    "2016-07-19 02:41:05 INFO juju.apiserver.metricsender metricsender.go:114 metrics collection summary: sent:0 unsent:0 (0 sent metrics stored)"]))
+        ]
     )
 
     create_html_report(results_dir, details)
@@ -270,6 +305,16 @@ def generate_network_graph_image(results_dir):
 def create_network_report_graph(rrd_dir, output_dir):
     return create_report_graph(
         rrd_dir, output_dir, 'network', _rrd_network_graph)
+
+
+def generate_mongo_graph_image(results_dir):
+    return generate_graph_image(
+        results_dir, 'aggregation-cpu-average', create_mongodb_report_graph)
+
+
+def create_mongodb_report_graph(rrd_dir, output_dir):
+    return create_report_graph(
+        rrd_dir, output_dir, 'mongodb', _rrd_mongdb_graph)
 
 
 def _rrd_network_graph(start, end, rrd_path, output_file):
@@ -373,6 +418,65 @@ def _rrd_memory_graph(start, end, rrd_path, output_file):
         subprocess.check_call(['sh', script_path])
 
 
+def _rrd_mongdb_graph(start, end, rrd_path, output_file):
+    script_text = dedent("""\
+    rrdtool graph {output_file} -a PNG --full-size-mode \
+    -s '{start}' -e '{end}' -w '800' -h '600' \
+    '-n' \
+    'DEFAULT:0:Bitstream Vera Sans' \
+    '-v' \
+    'Count' \
+    '-r' \
+    '-u' \
+    '100' \
+    '-t' \
+    'Mongo Statistics' \
+    'DEF:idle_avg={rrd_path}/cpu-idle.rrd:value:AVERAGE' \
+    'CDEF:idle_nnl=idle_avg,UN,0,idle_avg,IF' \
+    'DEF:wait_avg={rrd_path}/cpu-wait.rrd:value:AVERAGE' \
+    'CDEF:wait_nnl=wait_avg,UN,0,wait_avg,IF' \
+    'DEF:nice_avg={rrd_path}/cpu-nice.rrd:value:AVERAGE' \
+    'CDEF:nice_nnl=nice_avg,UN,0,nice_avg,IF' \
+    'DEF:user_avg={rrd_path}/cpu-user.rrd:value:AVERAGE' \
+    'CDEF:user_nnl=user_avg,UN,0,user_avg,IF' \
+    'DEF:system_avg={rrd_path}/cpu-system.rrd:value:AVERAGE' \
+    'CDEF:system_nnl=system_avg,UN,0,system_avg,IF' \
+    'DEF:softirq_avg={rrd_path}/cpu-softirq.rrd:value:AVERAGE' \
+    'CDEF:softirq_nnl=softirq_avg,UN,0,softirq_avg,IF' \
+    'DEF:interrupt_avg={rrd_path}/cpu-interrupt.rrd:value:AVERAGE' \
+    'CDEF:interrupt_nnl=interrupt_avg,UN,0,interrupt_avg,IF' \
+    'DEF:steal_avg={rrd_path}/cpu-steal.rrd:value:AVERAGE' \
+    'CDEF:steal_nnl=steal_avg,UN,0,steal_avg,IF' \
+    'CDEF:steal_stk=steal_nnl' \
+    'CDEF:interrupt_stk=interrupt_nnl,steal_stk,+' \
+    'CDEF:softirq_stk=softirq_nnl,interrupt_stk,+' \
+    'CDEF:system_stk=system_nnl,softirq_stk,+' \
+    'CDEF:user_stk=user_nnl,system_stk,+' \
+    'CDEF:nice_stk=nice_nnl,user_stk,+' \
+    'CDEF:wait_stk=wait_nnl,nice_stk,+' \
+    'CDEF:idle_stk=idle_nnl,wait_stk,+' \
+    'AREA:idle_stk#ffffff' \
+    'LINE1:idle_stk#ffffff: insert' \
+    'AREA:wait_stk#ffebbf' \
+    'LINE1:wait_stk#ffb000: query' \
+    'AREA:nice_stk#bff7bf' \
+    'LINE1:nice_stk#00e000: update' \
+    'AREA:user_stk#bfbfff' \
+    'LINE1:user_stk#0000ff: delete'
+    """.format(
+        output_file=output_file,
+        rrd_path=rrd_path,
+        start=start,
+        end=end
+    ))
+
+    with temp_dir() as temp:
+        script_path = os.path.abspath(os.path.join(temp, 'mongo.sh'))
+        with open(script_path, 'wt') as f:
+            f.write(script_text)
+        subprocess.check_call(['sh', script_path])
+
+
 def _rrd_cpu_graph(start, end, rrd_path, output_file):
     script_text = dedent("""\
     rrdtool graph {output_file} -a PNG --full-size-mode \
@@ -455,15 +559,31 @@ def get_duration_points(rrd_file):
 
 
 def assess_deployment_perf(client):
-    with _timer('deploying bundle'):
-        # This is where multiple services are started either at the same time
-        # or one after the other etc.
-        # start timearoo
-        # deploy some bundle, test one for now.
-        client.deploy('cs:bundle/wiki-simple-0')
-        client.wait_for_started()
-        # ensure the service is responding.
-        # Collect data results.
+    # This is where multiple services are started either at the same time
+    # or one after the other etc.
+    deploy_start = datetime.now()
+
+    client.deploy('cs:bundle/wiki-simple-0')
+    client.wait_for_started()
+
+    deploy_end = datetime.now()
+    deploy_timing = TimingData(deploy_start, deploy_end)
+
+    # get details like unit count etc.
+    client_details = get_client_details(client)
+
+    # ensure the service is responding.
+    # Collect data results.
+    return DeployDetails(
+        'cs:bundle/wiki-simple-0', client_details, deploy_timing)
+
+
+def get_client_details(client):
+    status = client.get_status()
+    units = dict()
+    for name in status.get_applications().keys():
+        units[name] = status.get_service_unit_count(name)
+    return units
 
 
 def setup_system_monitoring(admin_client):
@@ -481,6 +601,34 @@ def setup_system_monitoring(admin_client):
         ('0', 'sudo cp /tmp/collectd.config /etc/collectd/collectd.conf'))
     admin_client.juju('ssh', ('0', 'sudo /etc/init.d/collectd restart'))
 
+    script_contents = dedent("""\
+    password=`sudo grep oldpassword \
+      /var/lib/juju/agents/machine-*/agent.conf  | cut -d" " -f2`
+    mongostat --host=127.0.0.1:37017 --authenticationDatabase admin --ssl \
+      --sslAllowInvalidCertificates --username \"admin\" \
+      --password \"\$password\" --noheaders 5 > /tmp/mongodb-stats.log
+    """)
+    admin_client.juju(
+        'ssh',
+        ('0', '--', 'echo "{}" > /tmp/runner.sh'.format(script_contents)))
+
+    commands = [
+        'sudo echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/testing multiverse" | sudo tee /etc/apt/sources.list.d/mongo-org.list',
+        'sudo apt-get update',
+        'sudo apt-get install --yes --allow-unauthenticated mongodb-org-tools daemon',
+        'sudo chmod +x /tmp/runner.sh',
+        'daemon /tmp/runner.sh',
+    ]
+    full_command = " && ".join(commands)
+    admin_client.juju('ssh', ('0', '--', full_command))
+
+    # create rrd database.
+    # values for insert, query, update, delete. (then maybe vsize, res?
+    # netin/netout)
+    # for line in stat_json:
+    #     convert line to epoch seconds
+
+
 
 def parse_args(argv):
     """Parse all arguments."""
@@ -494,8 +642,24 @@ def main(argv=None):
     configure_logging(args.verbose)
     bs_manager = BootstrapManager.from_args(args)
     assess_perf_test_simple(bs_manager, args.upload_tools)
-    # generate_reports(
-    #     '/tmp/example_collection/', dict(bootstrap=123, deploys=['321']))
+
+    # # deploy
+    # deploy = TimingData(
+    #     '\'cs:bundle/wiki-simple-0\' #1',
+    #     start=datetime(2016, 7, 19, 2, 40, 27, 855692),
+    #     end=datetime(2016, 7, 19, 2, 51, 51, 910747))
+    # deploy2 = TimingData(
+    #     '(another) \'cs:bundle/wiki-simple-0\' #2',
+    #     start=datetime(2016, 7, 19, 2, 51, 59, 910747),
+    #     end=datetime(2016, 7, 19, 3, 2, 30, 910747))
+    # # bootstrap:
+    # bootstrap = TimingData(
+    #     'Bootstrap',
+    #     start=datetime(2016, 7, 19, 2, 33, 41, 5351),
+    #     end=datetime(2016, 7, 19, 2, 39, 45, 16832))
+    # timings = dict(bootstrap=bootstrap, deploys=[deploy, deploy2])
+    # generate_reports('/tmp/example_collection/', timings)
+
     return 0
 
 
