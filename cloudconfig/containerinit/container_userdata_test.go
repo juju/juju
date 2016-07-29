e3df10988462568536a4ac785f9a8387a989f0e9
James Tunnicliffe <james.tunnicliffe@nanosheep.org>
2016-07-29 14:34:30 +0100

    Generate /etc/network/interfaces for a container with the gateway staying on the correct card.

--- cloudconfig/containerinit/container_userdata_test.go
+++ cloudconfig/containerinit/container_userdata_test.go
@@ -42,6 +42,38 @@ type UserDataSuite struct {
 
 var _ = gc.Suite(&UserDataSuite{})
 
+func configToUserData(config string) string {
+	userData := `#cloud-config
+bootcmd:
+- install -D -m 644 /dev/null '%[1]s'
+- |-
+  printf '%%s\n' '`
+
+	for _, line := range strings.Split(strings.TrimSuffix(config, "\n"), "\n") {
+		if line != "" {
+			userData += "  " + line + "\n"
+		} else {
+			userData += "\n"
+		}
+	}
+
+	userData += `  ' > '%[1]s'
+runcmd:
+- |-
+  if [ -f %[1]s ]; then
+      ifdown -a
+      sleep 1.5
+      if ifup -a --interfaces=%[1]s; then
+          cp %[2]s %[2]s-orig
+          cp %[1]s %[2]s
+      else
+          ifup -a
+      fi
+  fi
+`
+	return userData
+}
+
 func (s *UserDataSuite) SetUpTest(c *gc.C) {
 	s.BaseSuite.SetUpTest(c)
 	s.networkInterfacesFile = filepath.Join(c.MkDir(), "juju-interfaces")
@@ -57,15 +89,12 @@ func (s *UserDataSuite) SetUpTest(c *gc.C) {
 		GatewayAddress:   network.NewAddress("0.1.2.1"),
 		MACAddress:       "aa:bb:cc:dd:ee:f0",
 	}, {
-		InterfaceName:    "eth1",
-		CIDR:             "0.1.2.0/24",
-		ConfigType:       network.ConfigStatic,
-		NoAutoStart:      false,
-		Address:          network.NewAddress("0.1.2.4"),
-		DNSServers:       network.NewAddresses("ns1.invalid", "ns2.invalid"),
-		DNSSearchDomains: []string{"foo", "bar"},
-		GatewayAddress:   network.NewAddress("0.1.2.1"),
-		MACAddress:       "aa:bb:cc:dd:ee:f0",
+		InterfaceName: "eth1",
+		CIDR:          "0.1.2.0/24",
+		ConfigType:    network.ConfigStatic,
+		NoAutoStart:   false,
+		Address:       network.NewAddress("0.1.2.4"),
+		MACAddress:    "aa:bb:cc:dd:ee:f0",
 	}, {
 		InterfaceName: "eth2",
 		ConfigType:    network.ConfigDHCP,
@@ -99,44 +128,7 @@ iface eth3 inet dhcp
 
 iface eth4 inet manual
 `
-	s.expectedSampleUserData = `
-#cloud-config
-bootcmd:
-- install -D -m 644 /dev/null '%[1]s'
-- |-
-  printf '%%s\n' '
-  auto eth0 eth1 eth3 lo
-
-  iface lo inet loopback
-    dns-nameservers ns1.invalid ns2.invalid
-    dns-search bar foo
-
-  iface eth0 inet static
-    address 0.1.2.3/24
-    gateway 0.1.2.1
-
-  iface eth1 inet static
-    address 0.1.2.4/24
-
-  iface eth2 inet dhcp
-
-  iface eth3 inet dhcp
-
-  iface eth4 inet manual
-  ' > '%[1]s'
-runcmd:
-- |-
-  if [ -f %[1]s ]; then
-      ifdown -a
-      sleep 1.5
-      if ifup -a --interfaces=%[1]s; then
-          cp %[2]s %[2]s-orig
-          cp %[1]s %[2]s
-      else
-          ifup -a
-      fi
-  fi
-`[1:]
+	s.expectedSampleUserData = configToUserData(s.expectedSampleConfig)
 
 	s.expectedFallbackConfig = `
 auto eth0 lo
@@ -145,31 +137,7 @@ iface lo inet loopback
 
 iface eth0 inet dhcp
 `
-	s.expectedFallbackUserData = `
-#cloud-config
-bootcmd:
-- install -D -m 644 /dev/null '%[1]s'
-- |-
-  printf '%%s\n' '
-  auto eth0 lo
-
-  iface lo inet loopback
-
-  iface eth0 inet dhcp
-  ' > '%[1]s'
-runcmd:
-- |-
-  if [ -f %[1]s ]; then
-      ifdown -a
-      sleep 1.5
-      if ifup -a --interfaces=%[1]s; then
-          cp %[2]s %[2]s-orig
-          cp %[1]s %[2]s
-      else
-          ifup -a
-      fi
-  fi
-`[1:]
+	s.expectedFallbackUserData = configToUserData(s.expectedFallbackConfig)
 
 	s.PatchValue(containerinit.NetworkInterfacesFile, s.networkInterfacesFile)
 	s.PatchValue(containerinit.SystemNetworkInterfacesFile, s.systemNetworkInterfacesFile)
@@ -282,4 +250,79 @@ func assertUserData(c *gc.C, cloudConf cloudinit.CloudConfig, expected string) {
 	} else {
 		c.Assert(out["bootcmd"], gc.IsNil)
 	}
+}
+
+func (s *UserDataSuite) TestDualNicBug1602054(c *gc.C) {
+	networkConfig := container.NetworkConfig{
+		NetworkType: "bridge",
+		Device:      "lxdbr0", MTU: 0,
+		Interfaces: []network.InterfaceInfo{
+			{
+				DeviceIndex:         0,
+				MACAddress:          "00:16:3e:29:de:c5",
+				CIDR:                "192.168.200.0/24",
+				ProviderId:          "33",
+				ProviderSubnetId:    "3",
+				ProviderSpaceId:     "",
+				ProviderVLANId:      "5001",
+				ProviderAddressId:   "216",
+				AvailabilityZones:   []string(nil),
+				VLANTag:             0,
+				InterfaceName:       "eth0",
+				ParentInterfaceName: "br-eth0",
+				InterfaceType:       "ethernet",
+				Disabled:            false,
+				NoAutoStart:         false,
+				ConfigType:          "static",
+				Address:             network.NewScopedAddress("192.168.200.1", network.ScopeCloudLocal),
+				DNSServers: []network.Address{
+					network.NewScopedAddress("192.168.1.2", network.ScopeCloudLocal),
+				},
+				MTU:              1500,
+				DNSSearchDomains: []string{"maas"},
+				GatewayAddress:   network.Address{},
+			}, {
+				DeviceIndex:         0,
+				MACAddress:          "00:16:3e:43:10:2b",
+				CIDR:                "192.168.1.0/24",
+				ProviderId:          "34",
+				ProviderSubnetId:    "1",
+				ProviderSpaceId:     "",
+				ProviderVLANId:      "0",
+				ProviderAddressId:   "218",
+				AvailabilityZones:   []string(nil),
+				VLANTag:             0,
+				InterfaceName:       "eth1",
+				ParentInterfaceName: "br-eth1",
+				InterfaceType:       "ethernet",
+				Disabled:            false,
+				NoAutoStart:         false,
+				ConfigType:          "static",
+				Address:             network.NewScopedAddress("192.168.200.102", network.ScopeCloudLocal),
+				DNSServers:          []network.Address{},
+				MTU:                 1500,
+				DNSSearchDomains:    []string(nil),
+				GatewayAddress:      network.NewScopedAddress("192.168.1.1", network.ScopeCloudLocal),
+			}},
+	}
+
+	data, err := containerinit.GenerateNetworkConfig(&networkConfig)
+	c.Assert(err, jc.ErrorIsNil)
+	c.Assert(data, gc.NotNil)
+
+	expected := `
+auto eth0 eth1 lo
+
+iface lo inet loopback
+  dns-nameservers 192.168.1.2
+  dns-search maas
+
+iface eth0 inet static
+  address 192.168.200.1/24
+
+iface eth1 inet static
+  address 192.168.200.102/24
+  gateway 192.168.1.1
+`
+	c.Assert(data, gc.Equals, expected)
 }
