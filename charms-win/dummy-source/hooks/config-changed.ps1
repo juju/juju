6ae4445e427a18e2f729c32ead4b5f339468ba27
Curtis Hovey <curtis@hovey.name>
2016-08-01 13:58:38 -0400

    Fixed token var.

--- charms-win/dummy-source/hooks/config-changed.ps1
+++ charms-win/dummy-source/hooks/config-changed.ps1
@@ -4,4 +4,4 @@ foreach ($relation_id in $relations.split()) {
 	relation-set -r $relation_id token="$token"
 }
 juju-log.exe "Token is $token"
-status-set.exe "active" "Token is $current_token"
+status-set.exe "active" "Token is $token"
