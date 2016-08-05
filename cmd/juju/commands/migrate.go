901e1d36951e7c27000e22d458c10a8e4edcdc07
Menno Smits <menno.smits@canonical.com>
2016-08-05 12:29:09 +1200

    cmd/juju/commands: Have migrate command refresh models
    The "juju migrate" command now refreshes models from the selected
    controller if the model to be migrated can't be found in the local
    config. The ModelUUIDs helper is now used to look up the model UUID as
    this already has the required refresh logic.
    
    Fixes LP 1607457

--- cmd/juju/commands/migrate.go
+++ cmd/juju/commands/migrate.go
@@ -87,10 +87,11 @@ func (c *migrateCommand) Init(args []string) error {
 func (c *migrateCommand) getMigrationSpec() (*controller.ModelMigrationSpec, error) {
 	store := c.ClientStore()
 
-	modelInfo, err := store.ModelByName(c.ControllerName(), c.model)
+	modelUUIDs, err := c.ModelUUIDs([]string{c.model})
 	if err != nil {
 		return nil, err
 	}
+	modelUUID := modelUUIDs[0]
 
 	controllerInfo, err := store.ControllerByName(c.targetController)
 	if err != nil {
@@ -103,7 +104,7 @@ func (c *migrateCommand) getMigrationSpec() (*controller.ModelMigrationSpec, err
 	}
 
 	return &controller.ModelMigrationSpec{
-		ModelUUID:            modelInfo.ModelUUID,
+		ModelUUID:            modelUUID,
 		TargetControllerUUID: controllerInfo.ControllerUUID,
 		TargetAddrs:          controllerInfo.APIEndpoints,
 		TargetCACert:         controllerInfo.CACert,
