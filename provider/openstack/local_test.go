62b398dc7f341cc68006460939670627ef919f93
anastasia <anastasia.macmood@canonical.com>
2016-08-19 13:59:21 +1000

    Test improvement - use host arch.

--- provider/openstack/local_test.go
+++ provider/openstack/local_test.go
@@ -1005,7 +1005,9 @@ func (s *localServerSuite) TestConstraintsMerge(c *gc.C) {
 	consB := constraints.MustParse("instance-type=m1.small")
 	cons, err := validator.Merge(consA, consB)
 	c.Assert(err, jc.ErrorIsNil)
-	c.Assert(cons, gc.DeepEquals, constraints.MustParse("arch=amd64 instance-type=m1.small"))
+	hostArch := arch.HostArch()
+	consString := fmt.Sprintf("arch=%v instance-type=m1.small", hostArch)
+	c.Assert(cons, gc.DeepEquals, constraints.MustParse(consString))
 }
 
 func (s *localServerSuite) TestFindImageInstanceConstraint(c *gc.C) {
