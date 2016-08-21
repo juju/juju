27488e6d00758c33d429b162a4c7097d224670c5
Christian Muirhead <christian.muirhead@canonical.com>
2016-08-21 23:04:42 +0100

    Implemented body of machine undertaker worker
    No tests yet.

--- worker/machineundertaker/undertaker.go
+++ worker/machineundertaker/undertaker.go
@@ -28,13 +28,14 @@ type Facade interface {
 // MachineUndertaker is responsible for doing any provider-level
 // cleanup needed and then removing the machine.
 type Undertaker struct {
-	api Facade
-	env environs.Environ
+	api           Facade
+	envNetworking environs.Networking
 }
 
 func NewWorker(api Facade, env environs.Environ) (worker.Worker, error) {
+	envNetworking, _ := environs.SupportsNetworking(env)
 	w, err := watcher.NewNotifyWorker(watcher.NotifyConfig{
-		Handler: &Undertaker{api: api, env: env},
+		Handler: &Undertaker{api: api, envNetworking: envNetworking},
 	})
 	if err != nil {
 		return nil, errors.Trace(err)
@@ -49,7 +50,54 @@ func (u *Undertaker) SetUp() (watcher.NotifyWatcher, error) {
 
 func (u *Undertaker) Handle(<-chan struct{}) error {
 	removals, err := u.api.AllMachineRemovals()
-	logger.Infof("handling removals of %v, %v", removals, err)
+	if err != nil {
+		// Should this be a fatal error instead?
+		logger.Errorf("couldn't get machine removals, %s", err)
+		return nil
+	}
+	logger.Debugf("handling removals: %v", removals)
+	// TODO(babbageclunk): shuffle the removals so if there's a
+	// problem with one others can still get past.
+	for _, machine := range removals {
+		err := u.maybeReleaseAddresses(machine)
+		if err != nil {
+			logger.Errorf("couldn't release addresses for %s: %s", machine, err)
+			continue
+		}
+		err = u.api.CompleteRemoval(machine)
+		if err != nil {
+			logger.Errorf("couldn't complete removal for %s: %s", machine, err)
+		} else {
+			logger.Debugf("completed removal: %s", machine)
+		}
+	}
+	return nil
+}
+
+func (u *Undertaker) maybeReleaseAddresses(machine names.MachineTag) error {
+	if u.envNetworking == nil {
+		// This environ doesn't support releasing addresses.
+		return nil
+	}
+	if !names.IsContainerMachine(machine.Id()) {
+		// Only containers need their addresses releasing.
+		return nil
+	}
+	interfaceInfos, err := u.api.GetProviderInterfaceInfo(machine)
+	if err != nil {
+		return errors.Trace(err)
+	}
+	if len(interfaceInfos) == 0 {
+		logger.Debugf("%s has no addresses to release", machine)
+		return nil
+	}
+	err = u.envNetworking.ReleaseContainerAddresses(interfaceInfos)
+	// Some providers say they support networking but don't
+	// actually support container addressing; don't freak out
+	// about those.
+	if err != nil && !errors.IsNotSupported(err) {
+		return errors.Trace(err)
+	}
 	return nil
 }
 
