37aafa087c4ef0f0e19f59ae929f10581836a759
Tim Penhey <tim@penhey.net>
2016-08-24 11:26:09 +1200

    initial delete

--- worker/uniter/charm/export_test.go
+++ worker/uniter/charm/export_test.go
@@ -8,16 +8,6 @@ func GitDeployerDataPath(d Deployer) string {
 	return d.(*gitDeployer).dataPath
 }
 
-// exported so we can get the deployer current git repo from tests.
-func GitDeployerCurrent(d Deployer) *GitDir {
-	return d.(*gitDeployer).current
-}
-
-func IsGitDeployer(d Deployer) bool {
-	_, ok := d.(*gitDeployer)
-	return ok
-}
-
 func IsManifestDeployer(d Deployer) bool {
 	_, ok := d.(*manifestDeployer)
 	return ok
