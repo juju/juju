dea55c167aeba132e17906f8ac52aca331a6caaf
Menno Smits <menno.smits@canonical.com>
2016-08-24 14:58:11 +1200

    migration: Check machine tools match model
    Ensure that an upgrade isn't in progress by checking that all machines
    in the model have the same tools version as what's set for the model.

--- migration/precheck.go
+++ migration/precheck.go
@@ -5,22 +5,58 @@ package migration
 
 import (
 	"github.com/juju/errors"
+	"github.com/juju/version"
+
+	"github.com/juju/juju/tools"
 )
 
-// SourcePrecheckBackend defines the things required to
+// SourcePrecheckBackend defines the interface to query Juju's state
+// for migration prechecks.
 type SourcePrecheckBackend interface {
 	NeedsCleanup() (bool, error)
+	AgentVersion() (version.Number, error)
+	AllMachines() ([]PrecheckMachine, error)
+}
+
+// PrecheckMachine describes state interface for a machine needed by
+// migration prechecks.
+type PrecheckMachine interface {
+	Id() string
+	AgentTools() (*tools.Tools, error)
 }
 
 // SourcePrecheck checks the state of the source controller to make
-// sure that the preconditions for model migration are met.
+// sure that the preconditions for model migration are met. The
+// backend provided must be for the model to be migrated.
 func SourcePrecheck(backend SourcePrecheckBackend) error {
 	cleanupNeeded, err := backend.NeedsCleanup()
 	if err != nil {
 		return errors.Annotate(err, "checking cleanups")
 	}
 	if cleanupNeeded {
-		return errors.New("precheck failed: cleanup needed")
+		return errors.New("cleanup needed")
 	}
+
+	modelVersion, err := backend.AgentVersion()
+	if err != nil {
+		return errors.Annotate(err, "retrieving model version")
+	}
+
+	machines, err := backend.AllMachines()
+	if err != nil {
+		return errors.Annotate(err, "retrieving machines")
+	}
+	for _, machine := range machines {
+		tools, err := machine.AgentTools()
+		if err != nil {
+			return errors.Annotatef(err, "retrieving tools for machine %s", machine.Id())
+		}
+		machineVersion := tools.Version.Number
+		if machineVersion != modelVersion {
+			return errors.Errorf("machine %s tools don't match model (%s != %s)",
+				machine.Id(), machineVersion, modelVersion)
+		}
+	}
+
 	return nil
 }
--- migration/precheck_test.go
+++ migration/precheck_test.go
@@ -6,10 +6,12 @@ package migration_test
 import (
 	"github.com/juju/errors"
 	jc "github.com/juju/testing/checkers"
+	"github.com/juju/version"
 	gc "gopkg.in/check.v1"
 
 	"github.com/juju/juju/migration"
 	"github.com/juju/juju/testing"
+	"github.com/juju/juju/tools"
 )
 
 type SourcePrecheckSuite struct {
@@ -19,32 +21,90 @@ type SourcePrecheckSuite struct {
 var _ = gc.Suite(&SourcePrecheckSuite{})
 
 func (*SourcePrecheckSuite) TestCleanups(c *gc.C) {
-	backend := &fakePrecheckBackend{}
+	backend := &fakeBackend{}
 	err := migration.SourcePrecheck(backend)
 	c.Assert(err, jc.ErrorIsNil)
 }
 
 func (*SourcePrecheckSuite) TestCleanupsError(c *gc.C) {
-	backend := &fakePrecheckBackend{
-		cleanupError: errors.New("boom"),
+	backend := &fakeBackend{
+		cleanupErr: errors.New("boom"),
 	}
 	err := migration.SourcePrecheck(backend)
 	c.Assert(err, gc.ErrorMatches, "checking cleanups: boom")
 }
 
 func (*SourcePrecheckSuite) TestCleanupsNeeded(c *gc.C) {
-	backend := &fakePrecheckBackend{
+	backend := &fakeBackend{
 		cleanupNeeded: true,
 	}
 	err := migration.SourcePrecheck(backend)
-	c.Assert(err, gc.ErrorMatches, "precheck failed: cleanup needed")
+	c.Assert(err, gc.ErrorMatches, "cleanup needed")
 }
 
-type fakePrecheckBackend struct {
+func (*SourcePrecheckSuite) TestAgentVersionError(c *gc.C) {
+	backend := &fakeBackend{
+		agentVersionErr: errors.New("boom"),
+	}
+	err := migration.SourcePrecheck(backend)
+	c.Assert(err, gc.ErrorMatches, "retrieving model version: boom")
+}
+
+func (*SourcePrecheckSuite) TestMachineVersionsMatch(c *gc.C) {
+	backend := &fakeBackend{
+		machines: []migration.PrecheckMachine{
+			&machine{"0", version.MustParseBinary("1.2.3-trusty-amd64")},
+			&machine{"1", version.MustParseBinary("1.2.3-xenial-amd64")},
+		},
+	}
+	err := migration.SourcePrecheck(backend)
+	c.Assert(err, jc.ErrorIsNil)
+}
+
+func (*SourcePrecheckSuite) TestMachineVersionsDontMatch(c *gc.C) {
+	backend := &fakeBackend{
+		machines: []migration.PrecheckMachine{
+			&machine{"0", version.MustParseBinary("1.2.3-trusty-amd64")},
+			&machine{"1", version.MustParseBinary("1.3.1-xenial-amd64")},
+		},
+	}
+	err := migration.SourcePrecheck(backend)
+	c.Assert(err, gc.ErrorMatches, `machine 1 tools don't match model \(1.3.1 != 1.2.3\)`)
+}
+
+type fakeBackend struct {
 	cleanupNeeded bool
-	cleanupError  error
+	cleanupErr    error
+
+	agentVersionErr error
+
+	machines       []migration.PrecheckMachine
+	allMachinesErr error
 }
 
-func (b *fakePrecheckBackend) NeedsCleanup() (bool, error) {
-	return b.cleanupNeeded, b.cleanupError
+func (b *fakeBackend) NeedsCleanup() (bool, error) {
+	return b.cleanupNeeded, b.cleanupErr
+}
+
+func (b *fakeBackend) AgentVersion() (version.Number, error) {
+	return version.MustParse("1.2.3"), b.agentVersionErr
+}
+
+func (b *fakeBackend) AllMachines() ([]migration.PrecheckMachine, error) {
+	return b.machines, b.allMachinesErr
+}
+
+type machine struct {
+	id      string
+	version version.Binary
+}
+
+func (m *machine) Id() string {
+	return m.id
+}
+
+func (m *machine) AgentTools() (*tools.Tools, error) {
+	return &tools.Tools{
+		Version: m.version,
+	}, nil
 }
