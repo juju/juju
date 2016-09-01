e62a28432be03ceb044565e38ea6938c293fc00c
Christopher Lee <chris.lee@canonical.com>
2016-09-01 17:33:24 +1200

    Sep. out graphing details. Refine rrd handling.

--- generate_perfscale_results.py
+++ generate_perfscale_results.py
@@ -14,7 +14,6 @@ import time
 
 import rrdtool
 from jinja2 import Template
-from fixtures import EnvironmentVariable
 
 from deploy_stack import (
     BootstrapManager,
@@ -23,6 +22,7 @@ from logbreakdown import (
     _render_ds_string,
     breakdown_log_by_timeframes,
 )
+import perf_graphing
 from utility import (
     add_basic_testing_arguments,
     configure_logging,
@@ -30,12 +30,6 @@ from utility import (
 
 
 __metaclass__ = type
-
-
-class GraphDefaults:
-    height = '600'
-    width = '800'
-    font = 'DEFAULT:0:Bitstream Vera Sans'
 
 
 class TimingData:
@@ -70,24 +64,13 @@ def assess_perf_test_simple(bs_manager, upload_tools):
     # XXX
 
     bs_start = datetime.utcnow()
-    # Make sure you check the responsiveness of 'juju status'
     with bs_manager.booted_context(upload_tools):
         client = bs_manager.client
         admin_client = client.get_admin_client()
         admin_client.wait_for_started()
         bs_end = datetime.utcnow()
         try:
-            # Work around mysql charm wanting 80% memory.
-            if client.env.get_cloud() == 'lxd':
-                constraint_cmd = [
-                    'lxc',
-                    'profile',
-                    'set',
-                    'juju-{}'.format(client.env.environment),
-                    'limits.memory',
-                    '2GB'
-                ]
-                subprocess.check_output(constraint_cmd)
+            apply_any_workarounds(client)
             bootstrap_timing = TimingData(bs_start, bs_end)
 
             setup_system_monitoring(admin_client)
@@ -127,6 +110,20 @@ def assess_perf_test_simple(bs_manager, upload_tools):
         'machine-0.log.gz')
 
     generate_reports(controller_log_file, results_dir, deployments)
+
+
+def apply_any_workarounds(client):
+    # Work around mysql charm wanting 80% memory.
+    if client.env.get_cloud() == 'lxd':
+        constraint_cmd = [
+            'lxc',
+            'profile',
+            'set',
+            'juju-{}'.format(client.env.environment),
+            'limits.memory',
+            '2GB'
+        ]
+        subprocess.check_output(constraint_cmd)
 
 
 def generate_reports(controller_log, results_dir, deployments):
@@ -219,17 +216,17 @@ def create_report_graph(rrd_dir, output_dir, name, generator):
 
 def generate_cpu_graph_image(results_dir):
     return generate_graph_image(
-        results_dir, 'aggregation-cpu-average', 'cpu', _rrd_cpu_graph)
+        results_dir, 'aggregation-cpu-average', 'cpu', perf_graphing.cpu_graph)
 
 
 def generate_memory_graph_image(results_dir):
     return generate_graph_image(
-        results_dir, 'memory', 'memory', _rrd_memory_graph)
+        results_dir, 'memory', 'memory', perf_graphing.memory_graph)
 
 
 def generate_network_graph_image(results_dir):
     return generate_graph_image(
-        results_dir, 'interface-eth0', 'network', _rrd_network_graph)
+        results_dir, 'interface-eth0', 'network', perf_graphing.network_graph)
 
 
 def generate_mongo_graph_image(results_dir):
@@ -239,7 +236,7 @@ def generate_mongo_graph_image(results_dir):
         log.error('Failed to create the MongoDB RRD file.')
         return None
     return generate_graph_image(
-        results_dir, 'mongodb', 'mongodb', _rrd_mongdb_graph)
+        results_dir, 'mongodb', 'mongodb', perf_graphing.mongdb_graph)
 
 
 def create_mongodb_rrd_file(results_dir, destination_dir):
@@ -296,192 +293,44 @@ def get_mongodb_stat_data(log_file):
     return data_lines[0][0], data_lines[-1][0], data_lines
 
 
-def _rrd_network_graph(start, end, rrd_path, output_file):
-    with EnvironmentVariable('TZ', 'UTC'):
-        # This could prob be made to take: start, end, y-axis, title, data.
-        rrdtool.graph(
-            output_file,
-            '--start', str(start),
-            '--end', str(end),
-            '--full-size-mode',  # test
-            '-w', GraphDefaults.width,
-            '-h', GraphDefaults.height,
-            '-n', GraphDefaults.font,
-            '-v', 'Bytes',
-            '--alt-autoscale-max',  # test
-            '-t', 'Network Usage',
-            'DEF:rx_avg={}/if_packets.rrd:rx:AVERAGE'.format(rrd_path),
-            'DEF:tx_avg={}/if_packets.rrd:tx:AVERAGE'.format(rrd_path),
-            'CDEF:rx_nnl=rx_avg,UN,0,rx_avg,IF',
-            'CDEF:tx_nnl=tx_avg,UN,0,tx_avg,IF',
-            'CDEF:rx_stk=rx_nnl',
-            'CDEF:tx_stk=tx_nnl',
-            'LINE2:rx_stk#bff7bf: RX',
-            'LINE2:tx_stk#ffb000: TX')
-
-
-def _rrd_memory_graph(start, end, rrd_path, output_file):
-    with EnvironmentVariable('TZ', 'UTC'):
-        rrdtool.graph(
-            output_file,
-            '--start', str(start),
-            '--end', str(end),
-            '--full-size-mode',  # test
-            '-w', GraphDefaults.width,
-            '-h', GraphDefaults.height,
-            '-n', GraphDefaults.font,
-            '-v', 'Memory',
-            '--alt-autoscale-max',  # test
-            '-t', 'Memory Usage',
-            'DEF:free_avg={}/memory-free.rrd:value:AVERAGE'.format(rrd_path),
-            'CDEF:free_nnl=free_avg,UN,0,free_avg,IF',
-            'DEF:used_avg={}/memory-used.rrd:value:AVERAGE'.format(rrd_path),
-            'CDEF:used_nnl=used_avg,UN,0,used_avg,IF',
-            'DEF:buffered_avg={}/memory-buffered.rrd:value:AVERAGE'.format(
-                rrd_path),
-            'CDEF:buffered_nnl=buffered_avg,UN,0,buffered_avg,IF',
-            'DEF:cached_avg={}/memory-cached.rrd:value:AVERAGE'.format(
-                rrd_path),
-            'CDEF:cached_nnl=cached_avg,UN,0,cached_avg,IF',
-            'CDEF:free_stk=free_nnl',
-            'CDEF:used_stk=used_nnl',
-            'AREA:free_stk#ffffff',
-            'LINE1:free_stk#ffffff: free',
-            'AREA:used_stk#ffebbf',
-            'LINE1:used_stk#ffb000: used')
-
-
-def _rrd_mongdb_graph(start, end, rrd_path, output_file):
-    with EnvironmentVariable('TZ', 'UTC'):
-        rrdtool.graph(
-            output_file,
-            '--start', str(start),
-            '--end', str(end),
-            '--full-size-mode',
-            '-w', '800',
-            '-h', '600',
-            '-n', 'DEFAULT:0:Bitstream Vera Sans',
-            '-v', 'Queries',
-            '--alt-autoscale-max',
-            '-t', 'MongoDB Actions',
-            'DEF:insert_max={rrd}:insert:MAX'.format(
-                rrd=os.path.join(rrd_path, 'mongodb.rrd')),
-            'DEF:query_max={rrd}:query:MAX'.format(
-                rrd=os.path.join(rrd_path, 'mongodb.rrd')),
-            'DEF:update_max={rrd}:update:MAX'.format(
-                rrd=os.path.join(rrd_path, 'mongodb.rrd')),
-            'DEF:delete_max={rrd}:delete:MAX'.format(
-                rrd=os.path.join(rrd_path, 'mongodb.rrd')),
-            'CDEF:insert_nnl=insert_max,UN,0,insert_max,IF',
-            'CDEF:query_nnl=query_max,UN,0,query_max,IF',
-            'CDEF:update_nnl=update_max,UN,0,update_max,IF',
-            'CDEF:delete_nnl=delete_max,UN,0,delete_max,IF',
-            'CDEF:delete_stk=delete_nnl',
-            'CDEF:update_stk=update_nnl',
-            'CDEF:query_stk=query_nnl',
-            'CDEF:insert_stk=insert_nnl',
-            'AREA:insert_stk#bff7bf80',
-            'LINE1:insert_stk#00E000: insert',
-            'AREA:query_stk#bfbfff80',
-            'LINE1:query_stk#0000FF: query',
-            'AREA:update_stk#ffebbf80',
-            'LINE1:update_stk#FFB000: update',
-            'AREA:delete_stk#ffbfbf80',
-            'LINE1:delete_stk#FF0000: delete')
-
-
-def _rrd_cpu_graph(start, end, rrd_path, output_file):
-    with EnvironmentVariable('TZ', 'UTC'):
-        rrdtool.graph(
-            output_file,
-            '--start', str(start),
-            '--end', str(end),
-            '--full-size-mode',  # test
-            '-w', GraphDefaults.width,
-            '-h', GraphDefaults.height,
-            '-n', GraphDefaults.font,
-            '-v', 'Jiffies',
-            '--alt-autoscale-max',  # test
-            '-t', 'CPU Average',
-            '-u', '100',
-            '-r',               # ?
-            'DEF:idle_avg={}/cpu-idle.rrd:value:AVERAGE'.format(rrd_path),
-            'CDEF:idle_nnl=idle_avg,UN,0,idle_avg,IF',
-            'DEF:wait_avg={}/cpu-wait.rrd:value:AVERAGE'.format(rrd_path),
-            'CDEF:wait_nnl=wait_avg,UN,0,wait_avg,IF',
-            'DEF:nice_avg={}/cpu-nice.rrd:value:AVERAGE'.format(rrd_path),
-            'CDEF:nice_nnl=nice_avg,UN,0,nice_avg,IF',
-            'DEF:user_avg={}/cpu-user.rrd:value:AVERAGE'.format(rrd_path),
-            'CDEF:user_nnl=user_avg,UN,0,user_avg,IF',
-            'DEF:system_avg={}/cpu-system.rrd:value:AVERAGE'.format(rrd_path),
-            'CDEF:system_nnl=system_avg,UN,0,system_avg,IF',
-            'DEF:softirq_avg={}/cpu-softirq.rrd:value:AVERAGE'.format(
-                rrd_path),
-            'CDEF:softirq_nnl=softirq_avg,UN,0,softirq_avg,IF',
-            'DEF:interrupt_avg={}/cpu-interrupt.rrd:value:AVERAGE'.format(
-                rrd_path),
-            'CDEF:interrupt_nnl=interrupt_avg,UN,0,interrupt_avg,IF',
-            'DEF:steal_avg={}/cpu-steal.rrd:value:AVERAGE'.format(rrd_path),
-            'CDEF:steal_nnl=steal_avg,UN,0,steal_avg,IF',
-            'CDEF:steal_stk=steal_nnl',
-            'CDEF:interrupt_stk=interrupt_nnl,steal_stk,+',
-            'CDEF:softirq_stk=softirq_nnl,interrupt_stk,+',
-            'CDEF:system_stk=system_nnl,softirq_stk,+',
-            'CDEF:user_stk=user_nnl,system_stk,+',
-            'CDEF:nice_stk=nice_nnl,user_stk,+',
-            'CDEF:wait_stk=wait_nnl,nice_stk,+',
-            'CDEF:idle_stk=idle_nnl,wait_stk,+',
-            'AREA:idle_stk#ffffff',
-            'LINE1:idle_stk#ffffff: Idle',
-            'AREA:wait_stk#ffebbf',
-            'LINE1:wait_stk#ffb000: Wait',
-            'AREA:nice_stk#bff7bf',
-            'LINE1:nice_stk#00e000: Nice',
-            'AREA:user_stk#bfbfff',
-            'LINE1:user_stk#0000ff: User',
-            'AREA:system_stk#ffbfbf',
-            'LINE1:system_stk#ff0000: system',
-            'AREA:softirq_stk#ffbfff',
-            'LINE1:softirq_stk#ff00ff: Softirq',
-            'AREA:interrupt_stk#e7bfe7',
-            'LINE1:interrupt_stk#a000a0: Interrupt',
-            'AREA:steal_stk#bfbfbf',
-            'LINE1:steal_stk#000000: Steal')
-
-
 def get_duration_points(rrd_file):
-    start = subprocess.check_output(['rrdtool', 'first', rrd_file]).strip()
-    end = subprocess.check_output(['rrdtool', 'last', rrd_file]).strip()
+    start = rrdtool.first(rrd_file)
+    end = rrdtool.last(rrd_file)
 
-    # This probably stems from a misunderstanding on my part.
-    shitty_command = 'rrdtool fetch {file} AVERAGE --start {start} --end {end} | tail --lines=+3 | grep -v "\-nan" | head -n1'.format(
-        file=rrd_file,
-        start=start,
-        end=end)
-    actual_start = subprocess.check_output(shitty_command, shell=True)
+    command = [
+        'rrdtool', 'fetch', rrd_file, 'AVERAGE',
+        '--start', str(start), '--end', str(end)]
+    output = subprocess.check_output(command)
 
-    return actual_start.split(':')[0], end
+    actual_start = find_actual_start(output)
+
+    return actual_start, end
 
 
-def assess_deployment_perf(client):
+def find_actual_start(fetch_output):
+    # Gets a start timestamp this isn't a NaN.
+    for line in fetch_output.splitlines():
+        try:
+            timestamp, value = line.split(':', 1)
+            if value != ' -nan':
+                return timestamp
+        except ValueError:
+            pass
+
+
+def assess_deployment_perf(client, bundle_name='cs:ubuntu'):
     # This is where multiple services are started either at the same time
     # or one after the other etc.
     deploy_start = datetime.utcnow()
 
-    # bundle_name = 'cs:~landscape/bundle/landscape-scalable'
-    # bundle_name = 'cs:bundle/wiki-simple-0'
-    bundle_name = 'cs:ubuntu'
     client.deploy(bundle_name)
     client.wait_for_started()
 
     deploy_end = datetime.utcnow()
     deploy_timing = TimingData(deploy_start, deploy_end)
 
-    # get details like unit count etc.
     client_details = get_client_details(client)
 
-    # ensure the service is responding.
-    # Collect data results.
     return DeployDetails(bundle_name, client_details, deploy_timing)
 
 
---
+++ perf_graphing.py
@@ -0,0 +1,163 @@
+"""Generate graphs for system statistics."""
+
+from fixtures import EnvironmentVariable
+import os
+import rrdtool
+
+
+class GraphDefaults:
+    height = '600'
+    width = '800'
+    font = 'DEFAULT:0:Bitstream Vera Sans'
+
+
+def network_graph(start, end, rrd_path, output_file):
+    with EnvironmentVariable('TZ', 'UTC'):
+        rrdtool.graph(
+            output_file,
+            '--start', str(start),
+            '--end', str(end),
+            '--full-size-mode',
+            '-w', GraphDefaults.width,
+            '-h', GraphDefaults.height,
+            '-n', GraphDefaults.font,
+            '-v', 'Bytes',
+            '--alt-autoscale-max',
+            '-t', 'Network Usage',
+            'DEF:rx_avg={}/if_packets.rrd:rx:AVERAGE'.format(rrd_path),
+            'DEF:tx_avg={}/if_packets.rrd:tx:AVERAGE'.format(rrd_path),
+            'CDEF:rx_nnl=rx_avg,UN,0,rx_avg,IF',
+            'CDEF:tx_nnl=tx_avg,UN,0,tx_avg,IF',
+            'CDEF:rx_stk=rx_nnl',
+            'CDEF:tx_stk=tx_nnl',
+            'LINE2:rx_stk#bff7bf: RX',
+            'LINE2:tx_stk#ffb000: TX')
+
+
+def memory_graph(start, end, rrd_path, output_file):
+    with EnvironmentVariable('TZ', 'UTC'):
+        rrdtool.graph(
+            output_file,
+            '--start', str(start),
+            '--end', str(end),
+            '--full-size-mode',
+            '-w', GraphDefaults.width,
+            '-h', GraphDefaults.height,
+            '-n', GraphDefaults.font,
+            '-v', 'Memory',
+            '--alt-autoscale-max',
+            '-t', 'Memory Usage',
+            'DEF:free_avg={}/memory-free.rrd:value:AVERAGE'.format(rrd_path),
+            'CDEF:free_nnl=free_avg,UN,0,free_avg,IF',
+            'DEF:used_avg={}/memory-used.rrd:value:AVERAGE'.format(rrd_path),
+            'CDEF:used_nnl=used_avg,UN,0,used_avg,IF',
+            'DEF:buffered_avg={}/memory-buffered.rrd:value:AVERAGE'.format(
+                rrd_path),
+            'CDEF:buffered_nnl=buffered_avg,UN,0,buffered_avg,IF',
+            'DEF:cached_avg={}/memory-cached.rrd:value:AVERAGE'.format(
+                rrd_path),
+            'CDEF:cached_nnl=cached_avg,UN,0,cached_avg,IF',
+            'CDEF:free_stk=free_nnl',
+            'CDEF:used_stk=used_nnl',
+            'AREA:free_stk#ffffff',
+            'LINE1:free_stk#ffffff: free',
+            'AREA:used_stk#ffebbf',
+            'LINE1:used_stk#ffb000: used')
+
+
+def mongdb_graph(start, end, rrd_path, output_file):
+    with EnvironmentVariable('TZ', 'UTC'):
+        rrdtool.graph(
+            output_file,
+            '--start', str(start),
+            '--end', str(end),
+            '--full-size-mode',
+            '-w', '800',
+            '-h', '600',
+            '-n', 'DEFAULT:0:Bitstream Vera Sans',
+            '-v', 'Queries',
+            '--alt-autoscale-max',
+            '-t', 'MongoDB Actions',
+            'DEF:insert_max={rrd}:insert:MAX'.format(
+                rrd=os.path.join(rrd_path, 'mongodb.rrd')),
+            'DEF:query_max={rrd}:query:MAX'.format(
+                rrd=os.path.join(rrd_path, 'mongodb.rrd')),
+            'DEF:update_max={rrd}:update:MAX'.format(
+                rrd=os.path.join(rrd_path, 'mongodb.rrd')),
+            'DEF:delete_max={rrd}:delete:MAX'.format(
+                rrd=os.path.join(rrd_path, 'mongodb.rrd')),
+            'CDEF:insert_nnl=insert_max,UN,0,insert_max,IF',
+            'CDEF:query_nnl=query_max,UN,0,query_max,IF',
+            'CDEF:update_nnl=update_max,UN,0,update_max,IF',
+            'CDEF:delete_nnl=delete_max,UN,0,delete_max,IF',
+            'CDEF:delete_stk=delete_nnl',
+            'CDEF:update_stk=update_nnl',
+            'CDEF:query_stk=query_nnl',
+            'CDEF:insert_stk=insert_nnl',
+            'AREA:insert_stk#bff7bf80',
+            'LINE1:insert_stk#00E000: insert',
+            'AREA:query_stk#bfbfff80',
+            'LINE1:query_stk#0000FF: query',
+            'AREA:update_stk#ffebbf80',
+            'LINE1:update_stk#FFB000: update',
+            'AREA:delete_stk#ffbfbf80',
+            'LINE1:delete_stk#FF0000: delete')
+
+
+def cpu_graph(start, end, rrd_path, output_file):
+    with EnvironmentVariable('TZ', 'UTC'):
+        rrdtool.graph(
+            output_file,
+            '--start', str(start),
+            '--end', str(end),
+            '--full-size-mode',
+            '-w', GraphDefaults.width,
+            '-h', GraphDefaults.height,
+            '-n', GraphDefaults.font,
+            '-v', 'Jiffies',
+            '--alt-autoscale-max',
+            '-t', 'CPU Average',
+            '-u', '100',
+            '-r',
+            'DEF:idle_avg={}/cpu-idle.rrd:value:AVERAGE'.format(rrd_path),
+            'CDEF:idle_nnl=idle_avg,UN,0,idle_avg,IF',
+            'DEF:wait_avg={}/cpu-wait.rrd:value:AVERAGE'.format(rrd_path),
+            'CDEF:wait_nnl=wait_avg,UN,0,wait_avg,IF',
+            'DEF:nice_avg={}/cpu-nice.rrd:value:AVERAGE'.format(rrd_path),
+            'CDEF:nice_nnl=nice_avg,UN,0,nice_avg,IF',
+            'DEF:user_avg={}/cpu-user.rrd:value:AVERAGE'.format(rrd_path),
+            'CDEF:user_nnl=user_avg,UN,0,user_avg,IF',
+            'DEF:system_avg={}/cpu-system.rrd:value:AVERAGE'.format(rrd_path),
+            'CDEF:system_nnl=system_avg,UN,0,system_avg,IF',
+            'DEF:softirq_avg={}/cpu-softirq.rrd:value:AVERAGE'.format(
+                rrd_path),
+            'CDEF:softirq_nnl=softirq_avg,UN,0,softirq_avg,IF',
+            'DEF:interrupt_avg={}/cpu-interrupt.rrd:value:AVERAGE'.format(
+                rrd_path),
+            'CDEF:interrupt_nnl=interrupt_avg,UN,0,interrupt_avg,IF',
+            'DEF:steal_avg={}/cpu-steal.rrd:value:AVERAGE'.format(rrd_path),
+            'CDEF:steal_nnl=steal_avg,UN,0,steal_avg,IF',
+            'CDEF:steal_stk=steal_nnl',
+            'CDEF:interrupt_stk=interrupt_nnl,steal_stk,+',
+            'CDEF:softirq_stk=softirq_nnl,interrupt_stk,+',
+            'CDEF:system_stk=system_nnl,softirq_stk,+',
+            'CDEF:user_stk=user_nnl,system_stk,+',
+            'CDEF:nice_stk=nice_nnl,user_stk,+',
+            'CDEF:wait_stk=wait_nnl,nice_stk,+',
+            'CDEF:idle_stk=idle_nnl,wait_stk,+',
+            'AREA:idle_stk#ffffff',
+            'LINE1:idle_stk#ffffff: Idle',
+            'AREA:wait_stk#ffebbf',
+            'LINE1:wait_stk#ffb000: Wait',
+            'AREA:nice_stk#bff7bf',
+            'LINE1:nice_stk#00e000: Nice',
+            'AREA:user_stk#bfbfff',
+            'LINE1:user_stk#0000ff: User',
+            'AREA:system_stk#ffbfbf',
+            'LINE1:system_stk#ff0000: system',
+            'AREA:softirq_stk#ffbfff',
+            'LINE1:softirq_stk#ff00ff: Softirq',
+            'AREA:interrupt_stk#e7bfe7',
+            'LINE1:interrupt_stk#a000a0: Interrupt',
+            'AREA:steal_stk#bfbfbf',
+            'LINE1:steal_stk#000000: Steal')
