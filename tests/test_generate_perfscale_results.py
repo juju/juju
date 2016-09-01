2393b11cc64a21271b61452fd528165fe8ea5de2
Christopher Lee <chris.lee@canonical.com>
2016-09-01 17:57:45 +1200

    Failing test to cover multidata in rrd file.

--- tests/test_generate_perfscale_results.py
+++ tests/test_generate_perfscale_results.py
@@ -27,9 +27,22 @@ class TestFindActualStart(TestCase):
     1468554282: 1.7661269333e+08
     1468554285: 1.7819374933e+08""")
 
+    example_multivalue_output = dedent("""\
+                             value1    value2
+
+    1472708601: -nan -nan
+    1472708604: -nan -nan
+    1472708607: -nan -nan
+    1472708610: -nan -nan
+    1472708613: 7.5466666667e+02 5.8166666667e+02
+    1472708616: 2.5555555556e+02 1.9833333333e+02
+    1472708619: 1.3333333333e+01 1.1555555556e+01
+    1472708622: 2.7444444444e+01 2.6222222222e+01""")
+
     def test_doesnt_choke_on_non_timestamp_lines(self):
         try:
             gpr.find_actual_start(self.example_output)
+            gpr.find_actual_start(self.example_multivalue_output)
         except Exception:
             self.fail('Unexpected exception raised.')
 
@@ -37,6 +50,10 @@ class TestFindActualStart(TestCase):
         self.assertEqual(
             gpr.find_actual_start(self.example_output),
             '1468554276')
+
+        self.assertEqual(
+            gpr.find_actual_start(self.example_multivalue_output),
+            '1472708613')
 
 
 class TestParseArgs(TestCase):
