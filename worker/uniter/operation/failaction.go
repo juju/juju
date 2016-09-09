bb88c457f648dfa836ea67185fdfc41a9a67f765
Michael Foord <michael.foord@canonical.com>
2016-09-09 12:23:03 +0100

    Skeleton fail action operation (file added)

---
+++ worker/uniter/operation/failaction.go
@@ -0,0 +1,60 @@
+// Copyright 2014 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package operation
+
+import (
+	"fmt"
+)
+
+type failAction struct {
+	actionId string
+	message  string
+
+	callbacks Callbacks
+
+	name string
+
+	RequiresMachineLock
+}
+
+// String is part of the Operation interface.
+func (fa *failAction) String() string {
+	return fmt.Sprintf("fail action %s", fa.actionId)
+}
+
+// Prepare is part of the Operation interface.
+func (fa *failAction) Prepare(state State) (*State, error) {
+	return stateChange{
+		Kind:     RunAction,
+		Step:     Pending,
+		ActionId: &fa.actionId,
+		Hook:     state.Hook,
+	}.apply(state), nil
+}
+
+// Execute is part of the Operation interface.
+func (fa *failAction) Execute(state State) (*State, error) {
+	message := fmt.Sprintf("running action %s", fa.name)
+
+	if err := fa.callbacks.SetExecutingStatus(message); err != nil {
+		return nil, err
+	}
+
+	return stateChange{
+		Kind:     RunAction,
+		Step:     Done,
+		ActionId: &fa.actionId,
+		Hook:     state.Hook,
+	}.apply(state), nil
+}
+
+// Commit preserves the recorded hook, and returns a neutral state.
+// Commit is part of the Operation interface.
+func (fa *failAction) Commit(state State) (*State, error) {
+	return stateChange{
+		Kind: continuationKind(state),
+		Step: Pending,
+		Hook: state.Hook,
+	}.apply(state), nil
+}
