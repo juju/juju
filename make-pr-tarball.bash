ce7d85ed69c1c7c5e4ad14197f752d9b4c3e0ba4
Nicholas Skaggs <nicholas.skaggs@canonical.com>
2016-09-14 17:39:04 -0400

    Add breakouts and common script points; add echos to original script

--- make-pr-tarball.bash
+++ make-pr-tarball.bash
@@ -20,23 +20,8 @@ usage() {
     exit 1
 }
 
-
-check_deps() {
-    echo "Phase 0: Checking requirements."
-    has_deps=1
-    which bzr || has_deps=0
-    which git || has_deps=0
-    which hg || has_deps=0
-    which go || has_deps=0
-    if [[ $has_deps == 0 ]]; then
-        echo "Install bzr, hg, git, and golang."
-        exit 2
-    fi
-}
-
-
-test $# -ge 1 ||  usage
-check_deps
+# check build dependencies
+. "$SCRIPT_DIR/check-source-depends.bash"
 
 GIT_PR=$1
 JUJU_CORE_REPO=${2:-$DEFAULT_GIT_JUJU_CORE}
@@ -56,63 +41,8 @@ git fetch --tags $JUJU_CORE_REPO +refs/pull/$GIT_PR/merge:refs/remotes/origin-pu
 MERGE_COMMIT=$(git rev-parse refs/remotes/origin-pull/pull/$GIT_PR/merge^{commit})
 git checkout -f $MERGE_COMMIT
 
-echo "Getting and updating juju-core dependencies to the required versions."
-GOPATH=$WORK go get -v github.com/rogpeppe/godeps
-GODEPS=$WORK/bin/godeps
-if [[ ! -f $GODEPS ]]; then
-    echo "! Could not install godeps."
-    exit 1
-fi
-GOPATH=$WORK $GODEPS -u "$WORKPACKAGE/dependencies.tsv"
-
-# Remove godeps, and non-free data
-echo "Removing godeps and non-free data."
-rm -rf $WORK/src/github.com/rogpeppe/godeps
-rm -rf $WORK/src/github.com/kisielk
-rm -rf $WORK/src/code.google.com/p/go.net/html/charset/testdata/
-rm -f $WORK/src/code.google.com/p/go.net/html/charset/*test.go
-rm -rf $WORK/src/golang.org/x/net/html/charset/testdata/
-rm -f $WORK/src/golang.org/x/net/html/charset/*test.go
-rm -rf $WORK/src/github.com/prometheus/procfs/fixtures
-
-# Remove backup files that confuse lintian.
-echo "Removing backup files"
-find $WORK/src/ -type f -name *.go.orig -delete
-
-# Validate the go src tree against dependencies.tsv
-echo "Validating dependencies.tsv"
-$SCRIPT_DIR/check_dependencies.py --delete-unknown --ignore $PACKAGE \
-    "$WORKPACKAGE/dependencies.tsv" "$WORK/src"
-
-# Apply patches against the whole source tree from the juju project
-echo "Applying Patches"
-if [[ -d "$WORKPACKAGE/patches" ]]; then
-    $SCRIPT_DIR/apply_patches.py "$WORKPACKAGE/patches" "$WORK/src"
-fi
-
-# Run juju's fmt and vet script on the source after finding the right version
-echo "Running format and checking build"
-if [[ $(lsb_release -sc) == "trusty" ]]; then
-    CHECKSCRIPT=./scripts/verify.bash
-    if [[ ! -f $WORKPACKAGE/scripts/verify.bash ]]; then
-        CHECKSCRIPT=./scripts/pre-push.bash
-    fi
-    (cd $WORKPACKAGE && GOPATH=$WORK $CHECKSCRIPT)
-fi
-
-# Remove binaries and build artefacts
-echo "Removing binaries and build artifacts"
-rm -r $WORK/bin
-if [[ -d $WORK/pkg ]]; then
-    rm -r $WORK/pkg
-fi
-
-echo "Rename to proper release version"
-VERSION=$(sed -n 's/^const version = "\(.*\)"/\1/p' \
-    $WORKPACKAGE/version/version.go)
-
-# Change the generic release to the proper juju-core version.
-mv $WORK $TMP_DIR/juju-core_${VERSION}/
+# Build juju in directory
+. "$SCRIPT_DIR/build-juju-source"
 
 # Tar it up.
 echo "Creating build tarball"
--- make-release-tarball.bash
+++ make-release-tarball.bash
@@ -23,19 +23,8 @@ usage() {
     exit 1
 }
 
-
-check_deps() {
-    echo "Phase 0: Checking requirements."
-    has_deps=1
-    which bzr || has_deps=0
-    which git || has_deps=0
-    which hg || has_deps=0
-    which go || has_deps=0
-    if [[ $has_deps == 0 ]]; then
-        echo "Install bzr, hg, git, and golang."
-        exit 2
-    fi
-}
+# check build dependencies
+. "$SCRIPT_DIR/check-source-depends.bash"
 
 
 test $# -ge 1 ||  usage
@@ -83,59 +72,13 @@ if [[ "$MERGE_REF" != "" && "$MERGE_REPO" != "" ]]; then
 fi
 cd $HERE
 
-echo "Getting and updating juju-core dependencies to the required versions."
-GOPATH=$WORK go get -v github.com/rogpeppe/godeps
-GODEPS=$WORK/bin/godeps
-if [[ ! -f $GODEPS ]]; then
-    echo "! Could not install godeps."
-    exit 1
-fi
-GOPATH=$WORK $GODEPS -u "$WORKPACKAGE/dependencies.tsv"
-
-# Remove godeps, and non-free data
-rm -rf $WORK/src/github.com/rogpeppe/godeps
-rm -rf $WORK/src/github.com/kisielk
-rm -rf $WORK/src/code.google.com/p/go.net/html/charset/testdata/
-rm -f $WORK/src/code.google.com/p/go.net/html/charset/*test.go
-rm -rf $WORK/src/golang.org/x/net/html/charset/testdata/
-rm -f $WORK/src/golang.org/x/net/html/charset/*test.go
-rm -rf $WORK/src/github.com/prometheus/procfs/fixtures
-# Remove backup files that confuse lintian.
-find $WORK/src/ -type f -name *.go.orig -delete
-
-# Validate the go src tree against dependencies.tsv
-$SCRIPT_DIR/check_dependencies.py --delete-unknown --ignore $PACKAGE \
-    "$WORKPACKAGE/dependencies.tsv" "$WORK/src"
-
-# Apply patches against the whole source tree from the juju project
-if [[ -d "$WORKPACKAGE/patches" ]]; then
-    $SCRIPT_DIR/apply_patches.py "$WORKPACKAGE/patches" "$WORK/src"
-fi
-
-# Run juju's fmt and vet script on the source after finding the right version
-if [[ $(lsb_release -sc) == "trusty" ]]; then
-    CHECKSCRIPT=./scripts/verify.bash
-    if [[ ! -f $WORKPACKAGE/scripts/verify.bash ]]; then
-        CHECKSCRIPT=./scripts/pre-push.bash
-    fi
-    (cd $WORKPACKAGE && GOPATH=$WORK $CHECKSCRIPT)
-fi
-
-# Remove binaries and build artefacts
-rm -r $WORK/bin
-if [[ -d $WORK/pkg ]]; then
-    rm -r $WORK/pkg
-fi
-
-VERSION=$(sed -n 's/^const version = "\(.*\)"/\1/p' \
-    $WORKPACKAGE/version/version.go)
-
-# Change the generic release to the proper juju-core version.
-mv $WORK $TMP_DIR/juju-core_${VERSION}/
+# Build juju in directory
+. "$SCRIPT_DIR/build-juju-source"
 
 # Tar it up.
+echo "Creating build tarball"
 TARFILE=$(pwd)/juju-core_${VERSION}.tar.gz
 cd $TMP_DIR
 tar cfz $TARFILE --exclude .hg --exclude .git --exclude .bzr juju-core_${VERSION}
 
-echo "release tarball: $TARFILE"
+echo "Successfully created tarball: $TARFILE"
