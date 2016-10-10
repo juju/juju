4e7a42bc711adf15cde9c64399c28cdd0b060f28
Michael Foord <michael.foord@canonical.com>
2016-10-10 16:56:05 +0100

    Another missing BootstrapMessage

--- provider/common/mock_test.go
+++ provider/common/mock_test.go
@@ -45,6 +45,11 @@ func (env *mockEnviron) Storage() storage.Storage {
 func (env *mockEnviron) AllInstances() ([]instance.Instance, error) {
 	return env.allInstances()
 }
+
+func (env *mockEnviron) BootstrapMessage() string {
+	return ""
+}
+
 func (env *mockEnviron) StartInstance(args environs.StartInstanceParams) (*environs.StartInstanceResult, error) {
 	inst, hw, networkInfo, err := env.startInstance(
 		args.Placement,
