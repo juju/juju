81663c3960c7d7e6e5dda06327e085203ddfd850
Ian Booth <ian.booth@canonical.com>
2016-10-18 09:54:23 +1000

    Add new aws us-east-2 region

--- cmd/juju/cloud/regions_test.go
+++ cmd/juju/cloud/regions_test.go
@@ -36,6 +36,7 @@ func (s *regionsSuite) TestListRegions(c *gc.C) {
 	out := testing.Stdout(ctx)
 	c.Assert(out, jc.DeepEquals, `
 us-east-1
+us-east-2
 us-west-1
 us-west-2
 eu-west-1
@@ -57,6 +58,8 @@ func (s *regionsSuite) TestListRegionsYaml(c *gc.C) {
 	c.Assert(out, jc.DeepEquals, `
 us-east-1:
   endpoint: https://ec2.us-east-1.amazonaws.com
+us-east-2:
+  endpoint: https://ec2.us-east-2.amazonaws.com
 us-west-1:
   endpoint: https://ec2.us-west-1.amazonaws.com
 us-west-2:
