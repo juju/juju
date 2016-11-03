402a900a5d402b026fc89fe1a331e9b0f4eaf271
Curtis Hovey <curtis@canonical.com>
2016-11-03 12:01:25 -0400

    Added script that allows a slave to update itself.

---
+++ update-jenkins-slave.bash
@@ -0,0 +1,64 @@
+#!/bin/bash
+set -eux
+
+
+update_branch() {
+    # Branch or pull a branch.
+    local_branch=$1
+    local_dir="$(basename $local_branch | cut -d ':' -f2)"
+    local_path="$HOME/$local_dir"
+    if [[ -d $local_path ]]; then
+        bzr pull -d $local_path
+    else
+        bzr branch $local_branch $local_path
+    fi
+}
+
+
+get_os() {
+    # Get the to OS name: ubuntu, darwin, linux, unknown.
+    set +x
+    local_uname=$(uname -a)
+    if echo "$local_uname" | grep -qi "ubuntu"; then
+        echo "ubuntu"
+    elif echo "$local_uname" | grep -qi "darwin"; then
+        echo "darwin"
+    elif echo "$local_uname" | grep -qi "linux"; then
+        # Probably CentOS.
+        echo "linux"
+    else
+        echo "unknown"
+    fi
+    set -x
+}
+
+
+# This works when the slave was setup by the jenkins-juju-ci subordinate
+# charm, or when a person installed the keys in .ssh by links to
+# cloud-city.
+bzr --no-aliases launchpad-login juju-qa-bot
+
+echo "Updating branches"
+update_branch lp:workspace-runner
+update_branch lp:juju-release-tools
+update_branch lp:juju-ci-tools
+update_branch lp:juju-ci-tools/repository
+update_branch lp:~juju-qa/+junk/cloud-city
+
+echo "Updating permissions"
+chown -R jenkins $HOME/cloud-city
+chmod -R go-w $HOME/cloud-city
+chmod 700 $HOME/cloud-city
+chmod 700 $HOME/cloud-city/gnupg
+chmod 600 $HOME/cloud-city/staging-juju-rsa
+
+echo "Updating dependencies from branches"
+OS=$(get_os)
+if [[ $OS == "ubuntu" ]]; then
+    make -C $HOME/juju-ci-tools install-deps
+    make -C $HOME/workspace-runner install
+elif [[ $OS == "darwin" ]]; then
+    $HOME/juju-ci-tools/pipdeps.py install
+fi
+
+echo "$HOSTNAME update complete"
