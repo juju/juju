9f4eb616d41da0311d233d7d44ae94777741ba6d
Reed O'Brien <reed@reedobrien.com>
2016-11-08 15:14:37 -0800

    cloud-server image caching to local disk and tests
    This updates the Sync method to fetch one image from simplestreams and
    cache/update a local copy. This functionality was previously provided
    by uvtool subclassing a mirorring library from the python simplestreams
    package.
    
    Refs: remove-uvtool-dependency

--- container/kvm/libvirt.go
+++ container/kvm/libvirt.go
@@ -42,24 +42,6 @@ func run(command string, args ...string) (output string, err error) {
 	return output, err
 }
 
-// SyncImages updates the local cached images by reading the simplestreams
-// data and downloading the cloud images to the uvtool pool (used by libvirt).
-func SyncImages(series, arch, source string) error {
-
-	args := []string{
-		"sync",
-		fmt.Sprintf("arch=%s", arch),
-		fmt.Sprintf("release=%s", series),
-	}
-
-	if source != "" {
-		args = append(args, fmt.Sprintf("--source=%s", source))
-	}
-
-	_, err := run("uvt-simplestreams-libvirt", args...)
-	return err
-}
-
 type CreateMachineParams struct {
 	Hostname      string
 	Series        string
