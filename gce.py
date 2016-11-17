5ac041a170d620286faf5c6e8dde4928307b5b60
Curtis Hovey <curtis@canonical.com>
2016-11-17 11:56:10 -0500

    Revised tag and destroy rules.

--- gce.py
+++ gce.py
@@ -12,17 +12,17 @@ import logging
 import os
 import sys
 
+from dateutil import parser as date_parser
 import pytz
 
 __metaclass__ = type
 
 
-DEFAULT_RESOURCE_PREFIX = 'Default-'
+PERMANENT = 'permanent'
 OLD_MACHINE_AGE = 6
 
 
-# The azure lib is very chatty even at the info level. This logger
-# strictly reports the activity of this script.
+# This logger strictly reports the activity of this script.
 log = logging.getLogger("gce")
 handler = logging.StreamHandler(sys.stderr)
 handler.setFormatter(logging.Formatter(
@@ -31,117 +31,43 @@ handler.setFormatter(logging.Formatter(
 log.addHandler(handler)
 
 
-class ResourceGroupDetails:
+def is_permanent(node):
+    """Return True of the node is permanent."""
+    # the tags keys only exists if there are tags.
+    tags = node.extra.get('tags', [])
+    return PERMANENT in tags
 
-    def __init__(self, client, group, storage_accounts=None, vms=None,
-                 addresses=None, networks=None):
-        self.client = client
-        self.is_loaded = False
-        self.group = group
-        self.storage_accounts = storage_accounts
-        self.vms = vms
-        self.addresses = addresses
-        self.networks = networks
 
-    def __eq__(self, other):
-        # Testing is the common case for checking equality.
-        return (
-            type(other) == type(self) and
-            self.client == other.client and
-            self.is_loaded is other.is_loaded and
-            self.group is other.group and
-            self.storage_accounts == other.storage_accounts and
-            self.vms == other.vms and
-            self.addresses == other.addresses and
-            self.networks == other.networks)
-
-    @property
-    def name(self):
-        return self.group.name
-
-    def load_details(self):
-        self.storage_accounts = list(
-            self.client.storage.storage_accounts.list_by_resource_group(
-                self.name))
-        self.vms = list(
-            self.client.compute.virtual_machines.list(self.name))
-        self.addresses = list(
-            self.client.network.public_ip_addresses.list(self.name))
-        self.networks = list(
-            self.client.network.virtual_networks.list(self.name))
-        self.is_loaded = True
-
-    def print_out(self, recursive=False):
-        print(self.name)
-        if recursive:
-            for vm in self.vms:
-                print('    VM {} {}'.format(vm.name, vm.vm_id))
-            for address in self.addresses:
-                print('    {} {}'.format(address.name, address.ip_address))
-            for network in self.networks:
-                print('    Network {}'.format(network.name))
-            for storage_account in self.storage_accounts:
-                print('    Storage {} {}'.format(
-                    storage_account.name, storage_account.creation_time))
-
-    def is_old(self, now, old_age):
-        """Return True if the resource group is old.
-
-        There are ambiguous cases where a resource groups is not completely
-        deleted. We know from logs that networks are are often that last
-        resource to be deleted and may be delete quick enough for the test.
-        A resource group with just a network is considered to be old.
-
-        :param now: The datetime object that is the basis for old age.
-        :param old_age: The age of the resource group to must be.
-        """
-        if old_age == 0:
-            # In the case of O hours old, the caller is stating any resource
-            # group that exists is old.
-            return True
+def is_young(node, old_age, now=None):
+    """Return True if the node is young."""
+    if not now:
+        now = datetime.now(pytz.utc)
+    young = True
+    # The value is not guaranteed, but is always present in running instances.
+    created = node.extra.get('creationTimestamp')
+    if created:
+        creation_time = date_parser.parse(created)
+        age = now - creation_time
+        hours = age.total_seconds() // 3600
+        log.debug('{} is {} old'.format(node.name, hours))
         ago = timedelta(hours=old_age)
-        # Contrary to Juju claims, the Azure RM provider always creates storage
-        # and that one resource has a creation_time.
-        if self.storage_accounts:
-            # Azure allows many storage accounts per resource group, but Juju
-            # only creates one.
-            creation_time = self.storage_accounts[0].creation_time
-            age = now - creation_time
-            if age > ago:
-                hours_old = (age.total_seconds() // 3600)
-                log.debug('{} is {} hours old:'.format(self.name, hours_old))
-                log.debug('  {}'.format(creation_time))
-                return True
-        elif (self.networks and not
-                all([self.vms, self.addresses, self.storage_accounts])):
-            # There is a network, but no vms, storage or public addresses.
-            # Networks can take a long time to delete and are often
-            # left behind when Juju cannot complete a delete in time.
-            log.debug('{} only has a network, likely a failed delete'.format(
-                      self.name))
-            # https://bugs.launchpad.net/juju-ci-tools/+bug/1613767
-            # return True
-        return False
-
-    def delete(self):
-        """Delete the resource group and all subordinate resources.
-
-        Returns a AzureOperationPoller.
-        """
-        return self.client.resource.resource_groups.delete(self.name)
-
-    def delete_vm(self, vm):
-        """Delete the VirtualMachine.
-
-        Returns a AzureOperationPoller.
-        """
-        return self.client.compute.virtual_machines.delete(self.name, vm.name)
+        if age > ago:
+            young = False
+    return young
 
 
-def list_resources(client, glob='*', print_out=False):
-    """Return a list of ResourceGroupDetails.
+def get_client(sa_email, pem_path, project_id):
+    """Delay imports and activation of GCE client as needed."""
+    import libcloud
+    gce = libcloud.compute.providers.get_driver(
+        libcloud.compute.types.Provider.GCE)
+    return gce(sa_email, pem_path, project=project_id)
 
-    Use print_out=True to print a listing of resources.
+
+def list_instances(client, glob='*', print_out=False):
+    """Return a list of cloud Nodes.
+
+    Use print_out=True to print a listing of nodes.
 
     :param client: The GCE client.
     :param glob: The glob to find matching resource groups to delete.
@@ -163,97 +89,46 @@ def list_resources(client, glob='*', print_out=False):
     return nodes
 
 
-def delete_resources(client, glob='*', old_age=OLD_MACHINE_AGE, now=None):
-    """Delete old resource groups and return the number deleted.
+def delete_instances(client, name_id, old_age=OLD_MACHINE_AGE, dry_run=False):
+    """Delete a node instance.
 
-    :param client: The GCE client.
-    :param glob: The glob to find matching resource groups to delete.
-    :param old_age: The age of the resource group to delete.
-    :param now: The datetime object that is the basis for old age.
+    :param name_id: A glob to match the gce name or Juju instance-id.
+    :param old_age: The minimum age to delete.
+    :param dry_run: Do not make changes when True.
     """
-    if not now:
-        now = datetime.now(pytz.utc)
-    resources = list_resources(client, glob=glob)
-    pollers = []
-    deleted_count = 0
-    for rgd in resources:
-        name = rgd.name
-        if not rgd.is_old(now, old_age):
+    nodes = list_instances(client, glob=name_id)
+    deletable = []
+    for node in nodes:
+        if is_permanent(node):
+            log.debug('Skipping {} because it is permanent'.format(node.name))
             continue
-        log.debug('Deleting {}'.format(name))
-        if not client.read_only:
-            poller = rgd.delete()
-            deleted_count += 1
-            if poller:
-                pollers.append((name, poller))
-            else:
-                # Deleting a group created using the old API might not return
-                # a poller! Or maybe the resource was deleting already.
-                log.debug(
-                    'poller is None for {}.delete(). Already deleted?'.format(
-                        name))
-    for name, poller in pollers:
-        log.debug('Waiting for {} to be deleted'.format(name))
-        # It is an error to ask for a poller's result() when it is done.
-        # Calling result() makes the poller wait for done, but the result
-        # of a delete operation is None.
-        if not poller.done():
-            poller.result()
-    return deleted_count
-
-
-def find_vm_instance(resources, name_id, resource_group):
-    """Return a tuple of matching ResourceGroupDetails and VirtualMachine.
-
-    Juju 1.x shows the machine's id as the instance_id.
-    Juju 2.x shows the machine's name in the resource group as the instance_id.
-
-    :param resources: A iterator of ResourceGroupDetails.
-    :param name_id: The name or id of a VM instance to find.
-    :param resource_group: The optional name of the resource group the
-        VM belongs to.
-    :return: A tuple of matching ResourceGroupDetails and VirtualMachine
-    """
-    for rgd in resources:
-        for vm in rgd.vms:
-            if (rgd.name == resource_group and vm.name == name_id or
-                    vm.vm_id == name_id):
-                return rgd, vm
-    return None, None
-
-
-def delete_instance(client, name_id, resource_group=None):
-    """Delete a VM instance.
-
-    When resource_group is provided, VM name is used to locate the VM.
-    Otherwise, all resource groups are searched for a matching VM id.
-
-    :param name_id: The name or id of a VM instance.
-    :param resource_group: The optional name of the resource group the
-        VM belongs to.
-    """
-    if resource_group:
-        glob = resource_group
-    else:
-        glob = '*'
-    resources = list_resources(client, glob=glob)
-    rgd, vm = find_vm_instance(resources, name_id, resource_group)
-    if vm:
-        log.debug('Found {} {}'.format(rgd.name, vm.name))
-        if not client.read_only:
-            poller = rgd.delete_vm(vm)
-            log.debug('Waiting for {} to be deleted'.format(vm.name))
-            if not poller.done():
-                poller.result()
-    else:
-        group_names = ', '.join([g.name for g in resources])
+        if is_young(node, old_age):
+            log.debug('Skipping {} because it is young:'.format(node.name))
+            continue
+        deletable.append(node)
+    if not deletable:
         raise ValueError(
-            'The vm name {} was not found in {}'.format(name_id, group_names))
+            'The no machines match {} that are older than {}'.format(
+                name_id, old_age))
+    for node in deletable:
+        deleted_count = 0
+        node_name = node.name
+        log.debug('Deleting {}'.format(node_name))
+        if not dry_run:
+            # Do not pass destroy_boot_disk=True unless the node has a special
+            # boot disk that is not set to autodestroy.
+            success = client.destroy_node(node)
+            if success:
+                log.debug('Deleted {}'.format(node_name))
+                deleted_count += 1
+            else:
+                log.error('Cannot delete {}'.format(node_name))
+    return deleted_count
 
 
 def parse_args(argv):
     """Return the argument parser for this program."""
-    parser = ArgumentParser(description='Query and manage azure.')
+    parser = ArgumentParser(description='Query and manage GCE.')
     parser.add_argument(
         '-d', '--dry-run', action='store_true', default=False,
         help='Do not make changes.')
@@ -268,7 +143,7 @@ def parse_args(argv):
         default=os.environ.get('GCE_SA_EMAIL'))
     parser.add_argument(
         '--pem-path',
-        help=("The client id to make requests with. "
+        help=("The path to the PEM file or a json file with PEM data. "
               "Environment: $GCE_PEM_PATH."),
         default=os.environ.get('GCE_PEM_PATH'))
     parser.add_argument(
@@ -278,28 +153,19 @@ def parse_args(argv):
         default=os.environ.get('GCE_PROJECT_ID'))
     subparsers = parser.add_subparsers(help='sub-command help', dest="command")
     ls_parser = subparsers.add_parser(
-        'list-resources', help='List resource groups.')
-    ls_parser.add_argument(
-        '-r', '--recursive', default=False, action='store_true',
-        help='Show resources with a resources group.')
+        'list-instances', help='List vm instances.')
     ls_parser.add_argument(
         'filter', default='*', nargs='?',
         help='A glob pattern to match services to.')
-    dr_parser = subparsers.add_parser(
-        'delete-resources',
+    di_parser = subparsers.add_parser(
+        'delete-instances',
         help='delete old resource groups and their vm, networks, etc.')
-    dr_parser.add_argument(
+    di_parser.add_argument(
         '-o', '--old-age', default=OLD_MACHINE_AGE, type=int,
         help='Set old machine age to n hours.')
-    dr_parser.add_argument(
-        'filter', default='*', nargs='?',
-        help='A glob pattern to select resource groups to delete.')
-    di_parser = subparsers.add_parser('delete-instance', help='Delete a vm.')
     di_parser.add_argument(
-        'name_id', help='The name or id of an instance (name needs group).')
-    di_parser.add_argument(
-        'resource_group', default=None, nargs='?',
-        help='The resource-group name of the machine name.')
+        'filter',
+        help='A glob pattern to select gce name or juju instance-id')
     args = parser.parse_args(argv[1:])
     if not all(
             [args.sa_email, args.pem_path, args.project_id]):
@@ -308,26 +174,17 @@ def parse_args(argv):
     return args
 
 
-def get_client(sa_email, pem_path, project_id):
-    """Delay imports and activation of GCE client until needed."""
-    import libcloud
-    gce = libcloud.compute.providers.get_driver(
-        libcloud.compute.types.Provider.GCE)
-    return gce(sa_email, pem_path, project=project_id)
-
-
 def main(argv):
     args = parse_args(argv)
     log.setLevel(args.verbose)
     client = get_client(args.sa_email, args.pem_path, args.project_id)
     try:
-        if args.command == 'list-resources':
-            list_resources(client, glob=args.filter, print_out=True)
-        elif args.command == 'delete-resources':
-            delete_resources(client, glob=args.filter, old_age=args.old_age)
-        elif args.command == 'delete-instance':
-            delete_instance(
-                client, args.name_id, resource_group=args.resource_group)
+        if args.command == 'list-instances':
+            list_instances(client, glob=args.filter, print_out=True)
+        elif args.command == 'delete-instances':
+            delete_instances(
+                client, args.filter,
+                old_age=args.old_age, dry_run=args.dry_run)
     except Exception as e:
         print(e)
         return 1
