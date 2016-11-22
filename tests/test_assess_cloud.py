688a4ef67d092b29b13bc36c5d9a38a093b8bd23
Aaron Bentley <aaron.bentley@canonical.com>
2016-11-22 12:46:42 -0500

    Test client_from_args.

--- tests/test_assess_cloud.py
+++ tests/test_assess_cloud.py
@@ -1,9 +1,23 @@
+from argparse import Namespace
 
-from mock import call, Mock, patch
-
-from assess_cloud import assess_cloud_combined
+from mock import (
+    call,
+    patch,
+    )
+from assess_cloud import (
+    assess_cloud_combined,
+    client_from_args,
+    )
 from deploy_stack import BootstrapManager
-from fakejuju import fake_juju_client
+from fakejuju import (
+    FakeBackend,
+    fake_juju_client,
+    )
+from jujupy import (
+    EnvJujuClient,
+    Juju2Backend,
+    temp_yaml_file,
+    )
 from tests import (
     FakeHomeTestCase,
     observable_temp_file,
@@ -40,6 +54,40 @@ class TestAssessCloudCombined(FakeHomeTestCase):
             self.backend_call(client, 'remove-unit', 'ubuntu/0', 'foo:foo'),
             self.backend_call(
                 client, 'destroy-controller',
-                ('foo', '-y', '--destroy-all-models'),
-                 timeout=600),
+                ('foo', '-y', '--destroy-all-models'), timeout=600),
             ], any_order=True)
+
+
+class TestClientFromArgs(FakeHomeTestCase):
+
+    def test_client_from_args(self):
+        with temp_yaml_file({}) as example_clouds:
+            args = Namespace(
+                juju_bin='/usr/bin/juju', example_clouds=example_clouds,
+                cloud='mycloud', region=None, debug=False, deadline=None)
+            with patch.object(EnvJujuClient.config_class,
+                              'from_cloud_region') as fcr_mock:
+                with patch.object(EnvJujuClient, 'get_version',
+                                  return_value='2.0.x'):
+                    client = client_from_args(args)
+        fcr_mock.assert_called_once_with('mycloud', None, {}, {},
+                                         self.juju_home)
+        self.assertIs(type(client), EnvJujuClient)
+        self.assertIs(type(client._backend), Juju2Backend)
+        self.assertEqual(client.version, '2.0.x')
+        self.assertIs(client.env, fcr_mock.return_value)
+
+    def test_client_from_args_fake(self):
+        with temp_yaml_file({}) as example_clouds:
+            args = Namespace(
+                juju_bin='FAKE', example_clouds=example_clouds,
+                cloud='mycloud', region=None, debug=False, deadline=None)
+            with patch.object(EnvJujuClient.config_class,
+                              'from_cloud_region') as fcr_mock:
+                client = client_from_args(args)
+        fcr_mock.assert_called_once_with('mycloud', None, {}, {},
+                                         self.juju_home)
+        self.assertIs(type(client), EnvJujuClient)
+        self.assertIs(type(client._backend), FakeBackend)
+        self.assertEqual(client.version, '2.0.0')
+        self.assertIs(client.env, fcr_mock.return_value)
