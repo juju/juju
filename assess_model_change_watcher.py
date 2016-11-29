d29c28bb51acb388e3ea855dfa1fcc3c4fc0ff74
Aaron Bentley <aaron.bentley@canonical.com>
2016-11-29 18:07:40 -0500

    Use Juju 2 compatible deploy.

--- assess_model_change_watcher.py
+++ assess_model_change_watcher.py
@@ -10,6 +10,7 @@ import sys
 from deploy_stack import (
     BootstrapManager,
     )
+from jujucharm import local_charm_path
 from utility import (
     add_basic_testing_arguments,
     configure_logging,
@@ -22,9 +23,11 @@ __metaclass__ = type
 log = logging.getLogger("assess_model_change_watcher")
 
 
-def assess_model_change_watcher(client):
+def assess_model_change_watcher(client, charm_series):
     # Deploy charms, there are several under ./repository
-    client.deploy('dummy-source')
+    charm = local_charm_path(charm='dummy-source', juju_ver=client.version,
+                             series=charm_series, platform='ubuntu')
+    client.deploy(charm)
     # Wait for the deployment to finish.
     client.wait_for_started()
     token = "hello"
@@ -43,7 +46,7 @@ def main(argv=None):
     configure_logging(args.verbose)
     bs_manager = BootstrapManager.from_args(args)
     with bs_manager.booted_context(args.upload_tools):
-        assess_model_change_watcher(bs_manager.client)
+        assess_model_change_watcher(bs_manager.client, bs_manager.series)
     return 0
 
 
