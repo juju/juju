598c3460351f7e20339136f5dee6210406f32d4a
Curtis Hovey <curtis@canonical.com>
2016-12-06 00:21:08 +0100

    Added a script to upgrade a remote maas.

--- upgrade-maas-micro.bash
+++ upgrade-maas-micro.bash
@@ -5,19 +5,34 @@
 # invalidate tests.
 
 set -eu
+USER_AT_ADDRESS=$1
 
-POLICY=$(apt-cache policy maas | egrep '(Installed|Candidate)')
-INSTALLED=$(echo "$POLICY" | grep Installed | cut -d ' ' -f 4)
-CANDIDATE=$(echo "$POLICY" | grep Candidate | cut -d ' ' -f 4)
-INSTALLED_MINOR=$(echo "$INSTALLED" | cut -d '.' -f 1,2)
-CANDIDATE_MINOR=$(echo "$CANDIDATE" | cut -d '.' -f 1,2)
 
-if [[ "$INSTALLED_MINOR" != "$CANDIDATE_MINOR" ]]; then
-    echo "Refusing to upgrade from $INSTALLED_MINOR to $CANDIDATE_MINOR"
-    exit 0
-fi
+upgrade_maas() {
+    set -eu
+    POLICY=$(apt-cache policy maas | egrep '(Installed|Candidate)')
+    INSTALLED=$(echo "$POLICY" | grep Installed | cut -d ' ' -f 4)
+    CANDIDATE=$(echo "$POLICY" | grep Candidate | cut -d ' ' -f 4)
+    INSTALLED_MINOR=$(echo "$INSTALLED" | cut -d '.' -f 1,2)
+    CANDIDATE_MINOR=$(echo "$CANDIDATE" | cut -d '.' -f 1,2)
 
-if [[ "$INSTALLED" != "$CANDIDATE" ]]; then
-    echo "Upgrading from $INSTALLED_MINOR to $CANDIDATE_MINOR"
-    sudo apt-get install -y maas=$CANDIDATE
-fi
+    echo "INSTALLED: $INSTALLED"
+    echo "CANDIDATE: $CANDIDATE"
+    echo "INSTALLED_MINOR: $INSTALLED_MINOR"
+    echo "CANDIDATE_MINOR: $CANDIDATE_MINOR"
+
+    if [[ "$INSTALLED_MINOR" != "$CANDIDATE_MINOR" ]]; then
+        echo "Refusing to upgrade from $INSTALLED_MINOR to $CANDIDATE_MINOR"
+        exit 0
+    fi
+
+    if [[ "$INSTALLED" != "$CANDIDATE" ]]; then
+        echo "Upgrading from $INSTALLED_MINOR to $CANDIDATE_MINOR"
+        sudo apt-get install -y maas=$CANDIDATE
+    else
+        echo "No upgrade available."
+    fi
+}
+
+
+ssh $USER_AT_ADDRESS "$(typeset -f upgrade_maas); upgrade_maas"
