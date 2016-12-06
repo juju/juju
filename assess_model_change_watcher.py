c8064c8e0f01d05d36146a53d48938c48500a841
Seman Said <seman.said@canonical.com>
2016-12-05 20:54:17 -0800

    Updated makefile.

--- assess_model_change_watcher.py
+++ assess_model_change_watcher.py
@@ -11,7 +11,6 @@ from juju.client import watcher
 
 from deploy_stack import (
     BootstrapManager,
-    get_random_string,
     until_timeout,
     )
 from jujucharm import local_charm_path
@@ -23,8 +22,6 @@ from utility import (
 
 
 __metaclass__ = type
-
-
 log = logging.getLogger("assess_model_change_watcher")
 TOKEN = "1234asdf"
 
@@ -51,27 +48,28 @@ def is_in_dict(key, value, items):
 
 async def listen_to_watcher(event_found, conn, future):
     logging.info("Starting to listen for the watcher.")
-    allwatcher = watcher.AllWatcher()
-    allwatcher.connect(conn)
+    all_watcher = watcher.AllWatcher()
+    all_watcher.connect(conn)
     for _ in until_timeout(120):
         logging.info("Listening for events...")
-        change = await allwatcher.Next()
+        change = await all_watcher.Next()
         for delta in change.deltas:
             logging.info("Event received: {}".format(str(delta.deltas)))
             if event_found(delta.deltas) is True:
-                await allwatcher.Stop()
+                await all_watcher.Stop()
                 await conn.close()
                 logging.info("Event found: {}".format(str(delta.deltas)))
                 future.set_result(True)
                 return
-    await allwatcher.Stop()
+
+    await all_watcher.Stop()
     await conn.close()
     logging.warning("Event not found.")
     future.set_result(False)
 
 
 def run_listener(client, event, juju_bin):
-    logging.info("Running the watcher")
+    logging.info("Running listener.")
     loop = asyncio.get_event_loop()
     future = asyncio.Future()
 
@@ -83,31 +81,30 @@ def run_listener(client, event, juju_bin):
     logging.info("Connected to the current model.")
 
     asyncio.ensure_future(listen_to_watcher(event, conn, future))
-    return loop, future, conn
+    return loop, future
 
 
 def assess_model_change_watcher(client, charm_series, juju_bin):
-    logging.info("Deploying a charm.")
-    # charm = local_charm_path(
-    #     charm='dummy-source', juju_ver=client.version, series=charm_series,
-    #     platform='ubuntu')
-    # client.deploy(charm)
-    # client.wait_for_started()
+    charm = local_charm_path(
+        charm='dummy-source', juju_ver=client.version, series=charm_series,
+        platform='ubuntu')
+    client.deploy(charm)
+    client.wait_for_started()
 
-    loop, future, conn = run_listener(client, is_config_change_in_event, juju_bin)
+    loop, future = run_listener(client, is_config_change_in_event, juju_bin)
 
-    #logging.info("Making config change.")
-    #client.set_config('dummy-source', {'token': TOKEN})
+    logging.info("Making config change.")
+    client.set_config('dummy-source', {'token': TOKEN})
 
     loop.run_until_complete(future)
     result = future.result()
     if result is not True:
-        raise JujuAssertionError("Config change was not sent.")
+        raise JujuAssertionError("Config change event was not sent.")
     loop.close()
 
 
 def parse_args(argv):
-    parser = argparse.ArgumentParser(description="Model change watcher")
+    parser = argparse.ArgumentParser(description="Assess config change.")
     add_basic_testing_arguments(parser)
     return parser.parse_args(argv)
 
@@ -117,22 +114,10 @@ def main(argv=None):
     configure_logging(args.verbose)
     bs_manager = BootstrapManager.from_args(args)
     with bs_manager.booted_context(args.upload_tools):
-        assess_model_change_watcher(bs_manager.client, bs_manager.series,
-                                    args.juju_bin)
+        assess_model_change_watcher(
+            bs_manager.client, bs_manager.series, args.juju_bin)
     return 0
 
 
-
-def testing_ONLY():
-    class Object(object):
-        pass
-    c = Object()
-    c.env = Object()
-    args=parse_args(None)
-    configure_logging(args.verbose)
-    c.env.juju_home = '/home/me/cloud-city'
-    assess_model_change_watcher(c, None, '/usr/bin/juju')
-
 if __name__ == '__main__':
-    testing_ONLY()
-    #sys.exit(main())
+    sys.exit(main())
