0f7db7e15e8de11b72d55f4bc73dffc68f73f44c
Christian Muirhead <christian.muirhead@canonical.com>
2016-12-14 23:50:39 +1300

    Close state leaks from the NewResourceOpener code
    This code path is a bit trickier to close than the other handler.

--- resource/resourceadapters/apiserver.go
+++ resource/resourceadapters/apiserver.go
@@ -47,14 +47,15 @@ func NewUploadHandler(args apihttp.NewHandlerArgs) http.Handler {
 			if err != nil {
 				return nil, nil, nil, errors.Trace(err)
 			}
-			resources, err := st.Resources()
-			if err != nil {
-				return nil, nil, nil, errors.Trace(err)
-			}
-
 			closer := func() error {
 				return args.Release(st)
 			}
+			resources, err := st.Resources()
+			if err != nil {
+				closer()
+				return nil, nil, nil, errors.Trace(err)
+			}
+
 			return resources, closer, entity.Tag(), nil
 		},
 	)
@@ -64,6 +65,7 @@ func NewUploadHandler(args apihttp.NewHandlerArgs) http.Handler {
 func NewDownloadHandler(args apihttp.NewHandlerArgs) http.Handler {
 	extractor := &httpDownloadRequestExtractor{
 		connect: args.Connect,
+		release: args.Release,
 	}
 	deps := internalserver.NewLegacyHTTPHandlerDeps(extractor)
 	return internalserver.NewLegacyHTTPHandler(deps)
@@ -78,15 +80,27 @@ type stateConnector interface {
 // handle a resource download HTTP request.
 type httpDownloadRequestExtractor struct {
 	connect func(*http.Request) (*corestate.State, corestate.Entity, error)
+	release func(*corestate.State) error
 }
 
 // NewResourceOpener returns a new resource.Opener for the given
 // HTTP request.
-func (ex *httpDownloadRequestExtractor) NewResourceOpener(req *http.Request) (resource.Opener, error) {
+func (ex *httpDownloadRequestExtractor) NewResourceOpener(req *http.Request) (opener resource.Opener, err error) {
 	st, ent, err := ex.connect(req)
 	if err != nil {
 		return nil, errors.Trace(err)
 	}
+
+	closer := func() error {
+		return ex.release(st)
+	}
+
+	defer func() {
+		if err != nil {
+			closer()
+		}
+	}()
+
 	unit, ok := ent.(*corestate.Unit)
 	if !ok {
 		logger.Errorf("unexpected type: %T", ent)
@@ -98,11 +112,12 @@ func (ex *httpDownloadRequestExtractor) NewResourceOpener(req *http.Request) (re
 		return nil, errors.Trace(err)
 	}
 
-	opener := &resourceOpener{
+	opener = &resourceOpener{
 		st:     st,
 		res:    resources,
 		userID: unit.Tag(),
 		unit:   unit,
+		closer: closer,
 	}
 	return opener, nil
 }
