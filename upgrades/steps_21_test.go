ea2ef942ceae94bd5770ee767e5edc5072f888ac
Menno Smits <menno.smits@canonical.com>
2016-12-15 16:11:12 +1300

    Upgrade step to add "attempt" to migration docs
    Migration docs now have an integer "attempt" column. The upgrade step
    populates these by parsing the doc _id.

---
+++ upgrades/steps_21_test.go
@@ -0,0 +1,33 @@
+// Copyright 2016 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package upgrades_test
+
+import (
+	jc "github.com/juju/testing/checkers"
+	"github.com/juju/version"
+	gc "gopkg.in/check.v1"
+
+	"github.com/juju/juju/testing"
+	"github.com/juju/juju/upgrades"
+)
+
+var v210 = version.MustParse("2.1.0")
+
+type steps21Suite struct {
+	testing.BaseSuite
+}
+
+var _ = gc.Suite(&steps21Suite{})
+
+func (s *steps21Suite) TestDropOldLogIndex(c *gc.C) {
+	step := findStateStep(c, v210, "drop old log index")
+	// Logic for step itself is tested in state package.
+	c.Assert(step.Targets(), jc.DeepEquals, []upgrades.Target{upgrades.DatabaseMaster})
+}
+
+func (s *steps21Suite) TestAddMigrationAttempt(c *gc.C) {
+	step := findStateStep(c, v210, "add attempt to migration docs")
+	// Logic for step itself is tested in state package.
+	c.Assert(step.Targets(), jc.DeepEquals, []upgrades.Target{upgrades.DatabaseMaster})
+}
