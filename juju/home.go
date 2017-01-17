f31995a0891d3a948f59eef7912defca1fe27297
José Pekkarinen <jose.pekkarinen@canonical.com>
2017-01-17 20:39:55 +0200

    Add logic to autogenerate an x509 certificate during client config phase. This will service as the default certificate for the LXD provider.

--- juju/home.go
+++ juju/home.go
@@ -4,8 +4,13 @@
 package juju
 
 import (
+	"os"
+	"time"
+
 	"github.com/juju/errors"
+	"github.com/juju/utils"
 	"github.com/juju/utils/ssh"
+	"github.com/juju/utils/cert"
 	"gopkg.in/juju/charmrepo.v2-unstable"
 
 	"github.com/juju/juju/juju/osenv"
@@ -22,6 +27,33 @@ func InitJujuXDGDataHome() error {
 	charmrepo.CacheDir = osenv.JujuXDGDataHomePath("charmcache")
 	if err := ssh.LoadClientKeys(osenv.JujuXDGDataHomePath("ssh")); err != nil {
 		return errors.Annotate(err, "cannot load ssh client keys")
+	}
+	if err := InitJujuX509Certificate(); err != nil{
+		return errors.Annotate(err, "cannot create x509 certificate")
+	}
+	return nil
+}
+
+// InitJujuX509Certificate initializes a default x509 certificate to be stored
+// in the config folder this certificate can be used by any provider that requires
+// encryption through this kind of certificates, for instance, LXD.
+func InitJujuX509Certificate() (error) {
+	expiry := time.Now().UTC().AddDate(10, 0, 0)
+	uuid, _ := utils.NewUUID()
+	certdir := osenv.JujuXDGDataHomePath("x509")
+	if certdir != "" {
+		err := os.MkdirAll(certdir, 0700)
+		if err == nil {
+			pem, key, err := cert.NewCA("client", uuid.String(), expiry, 2048)
+			if err != nil {
+				return errors.Annotate(err, "failed to create new certificate")
+			}
+			public, err := cert.ComputePublicKey(pem)
+			if err != nil {
+				return errors.Annotate(err, "failed to compute x509 public key")
+			}
+			return cert.StoreDefaultX509Cert(certdir, pem, key, public)
+		}
 	}
 	return nil
 }
