76cd938c9f579f02e43f2e29759d925783df98e5
John Arbash Meinel <john@arbash-meinel.com>
2017-01-23 12:26:25 +0400

    (broken) very close to validating real ssh keys.
    adding a front-end script to test the validation against live SSH servers.
    Messing up something about how we're passing the data around, need to fix that.

--- network/ssh/reachable.go
+++ network/ssh/reachable.go
@@ -4,22 +4,74 @@
 package ssh
 
 import (
+	"bytes"
 	"net"
 	"time"
 
-	"golang.org/x/crypto/ssh"
 	"github.com/juju/errors"
 	"github.com/juju/juju/network"
+	"golang.org/x/crypto/ssh"
 
 	"github.com/juju/loggo"
 )
-
 
 var logger = loggo.GetLogger("juju.network.ssh")
 
 // Dialer defines a Dial() method matching the signature of net.Dial().
 type Dialer interface {
 	Dial(network, address string) (net.Conn, error)
+}
+
+// hostKeyChecker checks if this host matches one of allowed public keys
+// it uses the golang/x/crypto/ssh/HostKeyCallback to find the host keys on a
+// given connection.
+type hostKeyChecker struct {
+	acceptablePublicKeys []ssh.PublicKey
+	hostPort network.HostPort
+	// validHostPortChan will get hostPort passed if this host matches the
+	// acceptablePublicKeys
+	validHostPortChan chan network.HostPort
+	// stop will be polled for whether we should stop trying to do any work
+	stop <-chan struct{}
+	// keyProcessed lets the setup function know when we're done
+	keyProcessed chan struct{}
+}
+
+func (h *hostKeyChecker) hostKeyCallback(hostname string, remote net.Addr, key ssh.PublicKey) error {
+	// Note: we don't do any advanced checking of the PublicKey, like whether
+	// the key is revoked or expired. All we care about is whether it matches
+	// the public keys that we consider acceptable
+	logger.Infof("Checking %q at %v, with key %q", hostname, remote, ssh.MarshalAuthorizedKey(key))
+	defer func() {
+		select {
+		case h.keyProcessed <- struct{}{}:
+		case <-h.stop:
+		}
+	}()
+
+	for i, accepted := range h.acceptablePublicKeys {
+		// TODO(jam): 2016-01-23 The ssh test suite uses Marshal and byte
+		// comparision to determine a match. Is that really the best way?
+		if h == nil {
+			logger.Warningf("why is h nil?", i)
+		}
+		if accepted == nil {
+			logger.Warningf("why is key %d nil?", i)
+		}
+		if bytes.Equal(accepted.Marshal(), key.Marshal()) {
+			logger.Debugf("found match: %q %v", hostname, remote)
+			select {
+			case <-h.stop:
+				logger.Debugf("stopped after finding a valid host: %q, %v %v", hostname, remote, h.hostPort)
+				return nil
+			case h.validHostPortChan <- h.hostPort:
+				logger.Debugf("found valid host for: %q, %v %v", hostname, remote, h.hostPort)
+				return nil
+			}
+		}
+	}
+	logger.Debugf("not the host key we were looking for %q %v", hostname, remote)
+	return errors.Errorf("do not proceed")
 }
 
 // ReachableHostPort dials the entries in the given hostPorts, in parallel,
@@ -35,31 +87,84 @@ type Dialer interface {
 func ReachableHostPort(hostPorts []network.HostPort, publicKeys []string, dialer Dialer, timeout time.Duration) (network.HostPort, error) {
 	uniqueHPs := network.UniqueHostPorts(hostPorts)
 	successful := make(chan network.HostPort, 1)
-	done := make(chan struct{}, 0)
+	stop := make(chan struct{}, 0)
+	finished := make(chan struct{}, len(uniqueHPs))
 
+	pubKeys := make([]ssh.PublicKey, len(publicKeys))
+	for _, pubKey := range publicKeys {
+		// key, comment, options, rest, err
+		sshKey, _, _, _, err := ssh.ParseAuthorizedKey([]byte(pubKey))
+		if err != nil {
+			logger.Warningf("unable to handle public key: %q\n", pubKey)
+			continue
+		}
+		logger.Debugf("pub key: %q %v", pubKey, sshKey)
+		pubKeys = append(pubKeys, sshKey)
+	}
 	for _, hostPort := range uniqueHPs {
-		go func(hp network.HostPort) {
-			conn, err := dialer.Dial("tcp", hp.NetAddr())
-			if err == nil {
-				conn.Close()
-				select {
-				case successful <- hp:
-					return
-				case <-done:
-					return
-				}
+		go func(hostPort network.HostPort) {
+			checker := &hostKeyChecker{
+				acceptablePublicKeys: pubKeys,
+				hostPort: hostPort,
+				validHostPortChan: successful,
+				stop: stop,
+				keyProcessed: make(chan struct{}, 1),
 			}
+			sshconfig := &ssh.ClientConfig{
+				// User ?
+				// Auth ?
+				HostKeyCallback: checker.hostKeyCallback,
+			}
+			addr := hostPort.NetAddr()
+			logger.Debugf("dialing %q", addr)
+			conn, err := dialer.Dial("tcp", addr)
+			if err != nil {
+				// TODO: Tracef
+				logger.Debugf("dial %q failed with: %v", addr, err)
+				select {
+					case <-finished:
+					case <-stop:
+					}
+					return
+			}
+			logger.Debugf("NewClientConn for %q", addr)
+			clientConn, chans, requests, err := ssh.NewClientConn(conn, addr, sshconfig)
+			if err != nil {
+				logger.Debugf("NewClientConn %q failed with: %v", addr, err)
+				select {
+				case <-finished:
+				case <-stop:
+				}
+				return
+			}
+			logger.Debugf("Discarding requests: %q", addr)
+			go ssh.DiscardRequests(requests)
+			go func() {
+				for ch := range chans {
+					ch.Reject(ssh.ResourceShortage, "no channels allowed")
+				}
+			}()
+			select {
+			case <-checker.keyProcessed:
+			case <-stop:
+			}
+			clientConn.Close()
 		}(hostPort)
 	}
 
-	select {
-	case result := <-successful:
-		logger.Infof("dialed %q successfully", result)
-		close(done)
-		return result, nil
+	for finishedCount := 0; finishedCount < len(uniqueHPs); {
+		select {
+		case result := <-successful:
+			logger.Infof("dialed %q successfully", result)
+			close(stop)
+			return result, nil
 
-	case <-time.After(timeout):
-		close(done)
-		return network.HostPort{}, errors.Errorf("cannot connect to any address: %v", hostPorts)
+		case <-finished:
+			finishedCount++
+		case <-time.After(timeout):
+			break
+		}
 	}
+	close(stop)
+	return network.HostPort{}, errors.Errorf("cannot connect to any address: %v", hostPorts)
 }
--- network/ssh/reachable_test.go
+++ network/ssh/reachable_test.go
@@ -7,7 +7,7 @@ import (
 	"net"
 	"time"
 
-	_	"github.com/juju/errors"
+	_ "github.com/juju/errors"
 	jc "github.com/juju/testing/checkers"
 	gc "gopkg.in/check.v1"
 
@@ -22,36 +22,56 @@ type SSHReachableHostPortSuite struct {
 
 var _ = gc.Suite(&SSHReachableHostPortSuite{})
 
-func (s *SSHReachableHostPortSuite) TestAllUnreachable(c *gc.C) {
-	dialer := &net.Dialer{Timeout: 50 * time.Millisecond}
-	unreachableHPs := closedTCPHostPorts(c, 10)
-	timeout := 100 * time.Millisecond
+// sshKey1 generated with `ssh-keygen -b 256 -C test-only -t ecdsa -f test-key`
+var sshKey1 = `-----BEGIN EC PRIVATE KEY-----
+MHcCAQEEILhuaRN6CI4h85SjOFV2+SU1uslRirsyyhGdsVmkKaC2oAoGCCqGSM49
+AwEHoUQDQgAESKoQ2r2l3hdXf9K+j+KsTwpTHNWMdd7gsl0tgy+77DYbz7DUDml1
+vIBDwimK29kn9WpPU8WSW23ZFPLk53mNTw==
+-----END EC PRIVATE KEY-----
+`
 
-	best, err := ssh.ReachableHostPort(unreachableHPs, nil, dialer, timeout)
+var sshPub1 = "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEiqENq9pd4XV3/Svo/irE8KUxzVjHXe4LJdLYMvu+w2G8+w1A5pdbyAQ8IpitvZJ/VqT1PFkltt2RTy5Od5jU8= test-only"
+
+// sshKey2 generated with `ssh-keygen -b 256 -C test-only -t ed25519 -f test-key`
+var sshKey2 = `-----BEGIN OPENSSH PRIVATE KEY-----
+b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAMwAAAAtzc2gtZW
+QyNTUxOQAAACANk3iR1VrTsEfIyQDXkrZajtOIwmKBdz+hAN90VXdxOQAAAJDQ4EH60OBB
++gAAAAtzc2gtZWQyNTUxOQAAACANk3iR1VrTsEfIyQDXkrZajtOIwmKBdz+hAN90VXdxOQ
+AAAEB0Vb6XYd1aFm1dl+37KgqgEeZDuFRlSHjeHrXEDFP4Iw2TeJHVWtOwR8jJANeStlqO
+04jCYoF3P6EA33RVd3E5AAAACXRlc3Qtb25seQECAwQ=
+-----END OPENSSH PRIVATE KEY-----
+`
+
+var sshPub2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA2TeJHVWtOwR8jJANeStlqO04jCYoF3P6EA33RVd3E5 test-only"
+
+var searchTimeout = 300 * time.Millisecond
+var dialTimeout = 100 * time.Millisecond
+
+func (s *SSHReachableHostPortSuite) TestAllUnreachable(c *gc.C) {
+	dialer := &net.Dialer{Timeout: dialTimeout}
+	unreachableHPs := closedTCPHostPorts(c, 10)
+	best, err := ssh.ReachableHostPort(unreachableHPs, nil, dialer, searchTimeout)
 	c.Check(err, gc.ErrorMatches, "cannot connect to any address: .*")
 	c.Check(best, gc.Equals, network.HostPort{})
 }
 
 func (s *SSHReachableHostPortSuite) TestReachableInvalidPublicKey(c *gc.C) {
 	hostPorts := []network.HostPort{
-		testSSHServer(c, "wrong public-key"),
+		// We use Key2, but are looking for Pub1
+		testSSHServer(c, sshKey2),
 	}
-	timeout := 300 * time.Millisecond
-
-	dialer := &net.Dialer{Timeout: 100 * time.Millisecond}
-	best, err := ssh.ReachableHostPort(hostPorts, []string{"public-key"}, dialer, timeout)
+	dialer := &net.Dialer{Timeout: dialTimeout}
+	best, err := ssh.ReachableHostPort(hostPorts, []string{sshPub1}, dialer, searchTimeout)
 	c.Check(err, gc.ErrorMatches, "cannot connect to any address: .*")
 	c.Check(best, gc.Equals, network.HostPort{})
 }
 
 func (s *SSHReachableHostPortSuite) TestReachableValidPublicKey(c *gc.C) {
 	hostPorts := []network.HostPort{
-		testSSHServer(c, "public-key"),
+		testSSHServer(c, sshKey1),
 	}
-	timeout := 300 * time.Millisecond
-
-	dialer := &net.Dialer{Timeout: 100 * time.Millisecond}
-	best, err := ssh.ReachableHostPort(hostPorts, []string{"public-key"}, dialer, timeout)
+	dialer := &net.Dialer{Timeout: dialTimeout}
+	best, err := ssh.ReachableHostPort(hostPorts, []string{sshPub1}, dialer, searchTimeout)
 	c.Check(err, jc.ErrorIsNil)
 	c.Check(best, gc.Equals, hostPorts[0])
 }
@@ -63,12 +83,11 @@ func (s *SSHReachableHostPortSuite) TestReachableMixedPublicKeys(c *gc.C) {
 	hostPorts := []network.HostPort{
 		fakeHostPort,
 		testTCPServer(c),
-		testSSHServer(c, "wrong public-key"),
-		testSSHServer(c, "public-key"),
+		testSSHServer(c, sshKey2),
+		testSSHServer(c, sshKey1),
 	}
-	timeout := 300 * time.Millisecond
-	dialer := &net.Dialer{Timeout: 100 * time.Millisecond}
-	best, err := ssh.ReachableHostPort(hostPorts, []string{"public-key"}, dialer, timeout)
+	dialer := &net.Dialer{Timeout: dialTimeout}
+	best, err := ssh.ReachableHostPort(hostPorts, []string{sshPub1}, dialer, searchTimeout)
 	c.Check(best, gc.Equals, network.HostPort{})
 	c.Check(err, jc.ErrorIsNil)
 	c.Check(best, jc.DeepEquals, hostPorts[3])
@@ -80,10 +99,8 @@ func (s *SSHReachableHostPortSuite) TestReachableNoPublicKeysPassed(c *gc.C) {
 		fakeHostPort,
 		testTCPServer(c),
 	}
-	timeout := 300 * time.Millisecond
-
-	dialer := &net.Dialer{Timeout: 100 * time.Millisecond}
-	best, err := ssh.ReachableHostPort(hostPorts, nil, dialer, timeout)
+	dialer := &net.Dialer{Timeout: dialTimeout}
+	best, err := ssh.ReachableHostPort(hostPorts, nil, dialer, searchTimeout)
 	c.Check(err, jc.ErrorIsNil)
 	c.Check(best, jc.DeepEquals, hostPorts[1]) // the only real listener
 }
@@ -94,15 +111,21 @@ func (s *SSHReachableHostPortSuite) TestReachableNoPublicKeysAvailable(c *gc.C) 
 		fakeHostPort,
 		testTCPServer(c),
 	}
-	timeout := 300 * time.Millisecond
-
-	dialer := &net.Dialer{Timeout: 100 * time.Millisecond}
-	best, err := ssh.ReachableHostPort(hostPorts, []string{"public-key"}, dialer, timeout)
+	dialer := &net.Dialer{Timeout: dialTimeout}
+	best, err := ssh.ReachableHostPort(hostPorts, []string{sshPub1}, dialer, searchTimeout)
 	c.Check(err, gc.ErrorMatches, "cannot connect to any address: .*")
 	c.Check(best, gc.Equals, network.HostPort{})
 }
 
-const maxTCPPort = 65535
+func (s *SSHReachableHostPortSuite) TestMultiplePublicKeys(c *gc.C) {
+	hostPorts := []network.HostPort{
+		testSSHServer(c, sshKey2),
+	}
+	dialer := &net.Dialer{Timeout: dialTimeout}
+	best, err := ssh.ReachableHostPort(hostPorts, []string{sshPub1, sshPub2}, dialer, searchTimeout)
+	c.Check(err, jc.ErrorIsNil)
+	c.Check(best, gc.Equals, hostPorts[0])
+}
 
 // closedTCPHostPorts opens and then immediately closes a bunch of ports and
 // saves their port numbers so we're unlikely to find a real listener at that
@@ -145,7 +168,7 @@ func testTCPServer(c *gc.C) network.HostPort {
 }
 
 // testSSHServer will listen on the socket and respond with the appropriate
-// public key information and then die. 
+// public key information and then die.
 func testSSHServer(c *gc.C, publicKey string) network.HostPort {
 	listener, err := net.Listen("tcp", "127.0.0.1:0")
 	c.Assert(err, jc.ErrorIsNil)
