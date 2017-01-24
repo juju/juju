4eed80e0f7094e208319f986b47b373caae93843
Christopher Lee <chris.lee@canonical.com>
2017-01-24 13:42:44 +1300

    Shift versioned ModelClients to have a split for 2.0/2.1 to ease test splits.

--- jujupy/version_client.py
+++ jujupy/version_client.py
@@ -128,7 +128,12 @@ class ModelClient2_1(ModelClient):
     REGION_ENDPOINT_PROMPT = 'Enter the API endpoint url for the region:'
 
 
-class ModelClientRC(ModelClient2_1):
+class ModelClient2_0(ModelClient2_1):
+    """Client for Juju 2.0"""
+    pass
+
+
+class ModelClientRC(ModelClient2_0):
 
     def get_bootstrap_args(
             self, upload_tools, config_filename, bootstrap_series=None,
@@ -674,7 +679,9 @@ def get_client_class(version):
         raise VersionNotTestedError(version)
     elif re.match('^2\.0-rc[1-3]', version):
         client_class = ModelClientRC
-    elif re.match('^2\.[0-1][.-]', version):
+    elif re.match('^2\.0[.-]', version):
+        client_class = ModelClient2_0
+    elif re.match('^2\.1[.-]', version):
         client_class = ModelClient2_1
     else:
         client_class = ModelClient
