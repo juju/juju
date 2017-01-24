09e3580b044af7ef3873cc454a4c8791c9f498bf
John Arbash Meinel <john@arbash-meinel.com>
2017-01-24 20:16:25 +0400

    Write a helper to start an SSH server-side connection.

--- network/ssh/reachable_test.go
+++ network/ssh/reachable_test.go
@@ -13,6 +13,7 @@ import (
 
 	"github.com/juju/juju/network"
 	"github.com/juju/juju/network/ssh"
+	sshtesting "github.com/juju/juju/network/ssh/testing"
 	coretesting "github.com/juju/juju/testing"
 )
 
@@ -21,28 +22,6 @@ type SSHReachableHostPortSuite struct {
 }
 
 var _ = gc.Suite(&SSHReachableHostPortSuite{})
-
-// sshKey1 generated with `ssh-keygen -b 256 -C test-only -t ecdsa -f test-key`
-var sshKey1 = `-----BEGIN EC PRIVATE KEY-----
-MHcCAQEEILhuaRN6CI4h85SjOFV2+SU1uslRirsyyhGdsVmkKaC2oAoGCCqGSM49
-AwEHoUQDQgAESKoQ2r2l3hdXf9K+j+KsTwpTHNWMdd7gsl0tgy+77DYbz7DUDml1
-vIBDwimK29kn9WpPU8WSW23ZFPLk53mNTw==
------END EC PRIVATE KEY-----
-`
-
-var sshPub1 = "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEiqENq9pd4XV3/Svo/irE8KUxzVjHXe4LJdLYMvu+w2G8+w1A5pdbyAQ8IpitvZJ/VqT1PFkltt2RTy5Od5jU8= test-only"
-
-// sshKey2 generated with `ssh-keygen -b 256 -C test-only -t ed25519 -f test-key`
-var sshKey2 = `-----BEGIN OPENSSH PRIVATE KEY-----
-b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAMwAAAAtzc2gtZW
-QyNTUxOQAAACANk3iR1VrTsEfIyQDXkrZajtOIwmKBdz+hAN90VXdxOQAAAJDQ4EH60OBB
-+gAAAAtzc2gtZWQyNTUxOQAAACANk3iR1VrTsEfIyQDXkrZajtOIwmKBdz+hAN90VXdxOQ
-AAAEB0Vb6XYd1aFm1dl+37KgqgEeZDuFRlSHjeHrXEDFP4Iw2TeJHVWtOwR8jJANeStlqO
-04jCYoF3P6EA33RVd3E5AAAACXRlc3Qtb25seQECAwQ=
------END OPENSSH PRIVATE KEY-----
-`
-
-var sshPub2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA2TeJHVWtOwR8jJANeStlqO04jCYoF3P6EA33RVd3E5 test-only"
 
 var searchTimeout = 300 * time.Millisecond
 var dialTimeout = 100 * time.Millisecond
@@ -58,20 +37,20 @@ func (s *SSHReachableHostPortSuite) TestAllUnreachable(c *gc.C) {
 func (s *SSHReachableHostPortSuite) TestReachableInvalidPublicKey(c *gc.C) {
 	hostPorts := []network.HostPort{
 		// We use Key2, but are looking for Pub1
-		testSSHServer(c, sshKey2),
+		testSSHServer(c, s, sshtesting.SSHKey2),
 	}
 	dialer := &net.Dialer{Timeout: dialTimeout}
-	best, err := ssh.ReachableHostPort(hostPorts, []string{sshPub1}, dialer, searchTimeout)
+	best, err := ssh.ReachableHostPort(hostPorts, []string{sshtesting.SSHPub1}, dialer, searchTimeout)
 	c.Check(err, gc.ErrorMatches, "cannot connect to any address: .*")
 	c.Check(best, gc.Equals, network.HostPort{})
 }
 
 func (s *SSHReachableHostPortSuite) TestReachableValidPublicKey(c *gc.C) {
 	hostPorts := []network.HostPort{
-		testSSHServer(c, sshKey1),
+		testSSHServer(c, s, sshtesting.SSHKey1),
 	}
 	dialer := &net.Dialer{Timeout: dialTimeout}
-	best, err := ssh.ReachableHostPort(hostPorts, []string{sshPub1}, dialer, searchTimeout)
+	best, err := ssh.ReachableHostPort(hostPorts, []string{sshtesting.SSHPub1}, dialer, searchTimeout)
 	c.Check(err, jc.ErrorIsNil)
 	c.Check(best, gc.Equals, hostPorts[0])
 }
@@ -82,12 +61,12 @@ func (s *SSHReachableHostPortSuite) TestReachableMixedPublicKeys(c *gc.C) {
 	fakeHostPort := closedTCPHostPorts(c, 1)[0]
 	hostPorts := []network.HostPort{
 		fakeHostPort,
-		testTCPServer(c),
-		testSSHServer(c, sshKey2),
-		testSSHServer(c, sshKey1),
+		testTCPServer(c, s),
+		testSSHServer(c, s, sshtesting.SSHKey2),
+		testSSHServer(c, s, sshtesting.SSHKey1),
 	}
 	dialer := &net.Dialer{Timeout: dialTimeout}
-	best, err := ssh.ReachableHostPort(hostPorts, []string{sshPub1}, dialer, searchTimeout)
+	best, err := ssh.ReachableHostPort(hostPorts, []string{sshtesting.SSHPub1}, dialer, searchTimeout)
 	c.Check(best, gc.Equals, network.HostPort{})
 	c.Check(err, jc.ErrorIsNil)
 	c.Check(best, jc.DeepEquals, hostPorts[3])
@@ -97,7 +76,7 @@ func (s *SSHReachableHostPortSuite) TestReachableNoPublicKeysPassed(c *gc.C) {
 	fakeHostPort := closedTCPHostPorts(c, 1)[0]
 	hostPorts := []network.HostPort{
 		fakeHostPort,
-		testTCPServer(c),
+		testTCPServer(c, s),
 	}
 	dialer := &net.Dialer{Timeout: dialTimeout}
 	best, err := ssh.ReachableHostPort(hostPorts, nil, dialer, searchTimeout)
@@ -109,20 +88,20 @@ func (s *SSHReachableHostPortSuite) TestReachableNoPublicKeysAvailable(c *gc.C) 
 	fakeHostPort := closedTCPHostPorts(c, 1)[0]
 	hostPorts := []network.HostPort{
 		fakeHostPort,
-		testTCPServer(c),
+		testTCPServer(c, s),
 	}
 	dialer := &net.Dialer{Timeout: dialTimeout}
-	best, err := ssh.ReachableHostPort(hostPorts, []string{sshPub1}, dialer, searchTimeout)
+	best, err := ssh.ReachableHostPort(hostPorts, []string{sshtesting.SSHPub1}, dialer, searchTimeout)
 	c.Check(err, gc.ErrorMatches, "cannot connect to any address: .*")
 	c.Check(best, gc.Equals, network.HostPort{})
 }
 
 func (s *SSHReachableHostPortSuite) TestMultiplePublicKeys(c *gc.C) {
 	hostPorts := []network.HostPort{
-		testSSHServer(c, sshKey2),
+		testSSHServer(c, s, sshtesting.SSHKey2),
 	}
 	dialer := &net.Dialer{Timeout: dialTimeout}
-	best, err := ssh.ReachableHostPort(hostPorts, []string{sshPub1, sshPub2}, dialer, searchTimeout)
+	best, err := ssh.ReachableHostPort(hostPorts, []string{sshtesting.SSHPub1, sshtesting.SSHPub2}, dialer, searchTimeout)
 	c.Check(err, jc.ErrorIsNil)
 	c.Check(best, gc.Equals, hostPorts[0])
 }
@@ -145,8 +124,12 @@ func closedTCPHostPorts(c *gc.C, count int) []network.HostPort {
 	return ports
 }
 
+type Cleaner interface {
+	AddCleanup(cleanup func(*gc.C))
+}
+
 // testTCPServer only listens on the socket, but doesn't speak SSH
-func testTCPServer(c *gc.C) network.HostPort {
+func testTCPServer(c *gc.C, cleaner Cleaner) network.HostPort {
 	listener, err := net.Listen("tcp", "127.0.0.1:0")
 	c.Assert(err, jc.ErrorIsNil)
 
@@ -155,37 +138,53 @@ func testTCPServer(c *gc.C) network.HostPort {
 	c.Assert(err, jc.ErrorIsNil)
 	c.Logf("listening on %q", hostPort)
 
+	shutdown := make(chan struct{}, 0)
+
 	go func() {
-		conn, _ := listener.Accept()
-		if conn != nil {
-			c.Logf("accepted connection on %q from %s", hostPort, conn.RemoteAddr())
-			conn.Close()
+		for {
+			select {
+			case <-shutdown:
+				// no more listening
+				c.Logf("shutting down %s", listenAddress)
+				listener.Close()
+				return
+			default:
+			}
+			// Don't get hung on Accept, set a deadline
+			if tcpListener, ok := listener.(*net.TCPListener); ok {
+				tcpListener.SetDeadline(time.Now().Add(1 * time.Second))
+			}
+			tcpConn, err := listener.Accept()
+			if err != nil {
+				if netErr, ok := err.(net.Error); ok {
+					if netErr.Timeout() {
+						// Try again, so we reevaluate if we need to shut down
+						continue
+					}
+				}
+			}
+			if err != nil {
+				c.Logf("failed to accept connection on %s: %v", listenAddress, err)
+				continue
+			}
+			if tcpConn != nil {
+				c.Logf("accepted connection on %q from %s", hostPort, tcpConn.RemoteAddr())
+				tcpConn.Close()
+			}
 		}
-		listener.Close()
 	}()
+	cleaner.AddCleanup(func(*gc.C) { close(shutdown) })
 
 	return *hostPort
 }
 
 // testSSHServer will listen on the socket and respond with the appropriate
 // public key information and then die.
-func testSSHServer(c *gc.C, publicKey string) network.HostPort {
-	listener, err := net.Listen("tcp", "127.0.0.1:0")
+func testSSHServer(c *gc.C, cleaner Cleaner, privateKey string) network.HostPort {
+	address, shutdown := sshtesting.CreateSSHServer(c, privateKey)
+	hostPort, err := network.ParseHostPort(address)
 	c.Assert(err, jc.ErrorIsNil)
-
-	listenAddress := listener.Addr().String()
-	hostPort, err := network.ParseHostPort(listenAddress)
-	c.Assert(err, jc.ErrorIsNil)
-	c.Logf("listening on %q", hostPort)
-
-	go func() {
-		conn, _ := listener.Accept()
-		if conn != nil {
-			c.Logf("accepted connection on %q from %s", hostPort, conn.RemoteAddr())
-			conn.Close()
-		}
-		listener.Close()
-	}()
+	cleaner.AddCleanup(func(*gc.C) { close(shutdown) })
 
 	return *hostPort
 }
---
+++ network/ssh/testing/sshserver.go
@@ -0,0 +1,100 @@
+// Copyright 2017 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package testing
+
+import (
+	"net"
+	"time"
+
+	jc "github.com/juju/testing/checkers"
+	"golang.org/x/crypto/ssh"
+	gc "gopkg.in/check.v1"
+)
+
+// SSHKey1 generated with `ssh-keygen -b 256 -C test-only -t ecdsa -f test-key`
+var SSHKey1 = `-----BEGIN EC PRIVATE KEY-----
+MHcCAQEEILhuaRN6CI4h85SjOFV2+SU1uslRirsyyhGdsVmkKaC2oAoGCCqGSM49
+AwEHoUQDQgAESKoQ2r2l3hdXf9K+j+KsTwpTHNWMdd7gsl0tgy+77DYbz7DUDml1
+vIBDwimK29kn9WpPU8WSW23ZFPLk53mNTw==
+-----END EC PRIVATE KEY-----
+`
+
+var SSHPub1 = "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEiqENq9pd4XV3/Svo/irE8KUxzVjHXe4LJdLYMvu+w2G8+w1A5pdbyAQ8IpitvZJ/VqT1PFkltt2RTy5Od5jU8= test-only"
+
+// SSHKey2 generated with `ssh-keygen -b 256 -C test-only -t ed25519 -f test-key`
+var SSHKey2 = `-----BEGIN OPENSSH PRIVATE KEY-----
+b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAMwAAAAtzc2gtZW
+QyNTUxOQAAACANk3iR1VrTsEfIyQDXkrZajtOIwmKBdz+hAN90VXdxOQAAAJDQ4EH60OBB
++gAAAAtzc2gtZWQyNTUxOQAAACANk3iR1VrTsEfIyQDXkrZajtOIwmKBdz+hAN90VXdxOQ
+AAAEB0Vb6XYd1aFm1dl+37KgqgEeZDuFRlSHjeHrXEDFP4Iw2TeJHVWtOwR8jJANeStlqO
+04jCYoF3P6EA33RVd3E5AAAACXRlc3Qtb25seQECAwQ=
+-----END OPENSSH PRIVATE KEY-----
+`
+
+var SSHPub2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA2TeJHVWtOwR8jJANeStlqO04jCYoF3P6EA33RVd3E5 test-only"
+
+// CreateSSHServer launches an SSH server that will use the described private
+// key to allow SSH connections. Note that it explicitly doesn't actually
+// support any Auth mechanisms, so nobody can complete connections, but it will
+// do Key exchange to set up the encrypted conversation.
+// We return the address where the SSH service is listening, and a channel
+// callers must close when they want the service to stop.
+func CreateSSHServer(c *gc.C, privateKeys ...string) (string, chan struct{}) {
+	serverConf := &ssh.ServerConfig{
+	// Note that we don't set up a PasswordCallback or 'NoClientAuth:
+	// true'. Right now we just don't allow actual logins.
+	}
+	for _, privateStr := range privateKeys {
+		privateKey, err := ssh.ParsePrivateKey([]byte(privateStr))
+		c.Assert(err, jc.ErrorIsNil)
+		serverConf.AddHostKey(privateKey)
+	}
+	// We explicitly listen on IPv4 loopback instead of 'localhost'
+	listener, err := net.Listen("tcp", "127.0.0.1:0")
+	c.Assert(err, jc.ErrorIsNil)
+	localAddress := listener.Addr().String()
+
+	shutdown := make(chan struct{}, 0)
+
+	go func() {
+		for {
+			select {
+			case <-shutdown:
+				// no more listening
+				c.Logf("shutting down %s", localAddress)
+				listener.Close()
+				return
+			default:
+			}
+			// Don't get hung on Accept, set a deadline
+			if tcpListener, ok := listener.(*net.TCPListener); ok {
+				tcpListener.SetDeadline(time.Now().Add(1 * time.Second))
+			}
+			tcpConn, err := listener.Accept()
+			if err != nil {
+				if netErr, ok := err.(net.Error); ok {
+					if netErr.Timeout() {
+						// Try again, so we reevaluate if we need to shut down
+						continue
+					}
+				}
+			}
+			if err != nil {
+				c.Logf("failed to accept connection on %s: %v", localAddress, err)
+				continue
+			}
+			remoteAddress := tcpConn.RemoteAddr().String()
+			c.Logf("accepted connection on %s from %s", localAddress, remoteAddress)
+			sshConn, _, _, err := ssh.NewServerConn(tcpConn, serverConf)
+			if err != nil {
+				// TODO: some errors are expected, as we don't support Auth
+				c.Logf("error initiating ssh connection for %s: %v", remoteAddress, err)
+			} else {
+				// We don't expect to get here, but if we do, make sure we close the connection.
+				sshConn.Close()
+			}
+		}
+	}()
+	return localAddress, shutdown
+}
