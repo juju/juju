fcd3ba42759a96adc5a3d9bdd72dac9751d168e6
Christopher Lee <chris.lee@canonical.com>
2017-01-30 21:08:19 +1300

    Initial work for collecting pprof profiles during a test run.

---
+++ pprof_collector.py
@@ -0,0 +1,166 @@
+"""Collector of pprof details using juju introspection."""
+
+from __future__ import print_function
+
+from functools import partial
+import logging
+import os
+import requests
+
+from utility import get_unit_ipaddress
+
+__metaclass__ = type
+
+log = logging.getLogger("pprof_collector")
+
+
+class NoopCollector:
+
+    def __init__(self, client, machine_id):
+        """Internal collector for pprof profiles, not intended to be used
+        directly
+
+        Does not actually install or collect data. Used as a stand in when
+        collection is disabled.
+
+        :param client: jujupy.ModelClient object to install the pprof software
+          with.
+        :machine_id: ID of the juju machine with which to install pprof
+          software/charm.
+        """
+        pass
+
+    def collect_profile(self, filepath, seconds):
+        """Collect `seconds` worth of CPU profile."""
+        pass
+
+    def collect_heap(self, filepath, seconds):
+        """Collect `seconds` worth of heap profile."""
+        pass
+
+    def collect_goroutines(self, filepath, seconds):
+        """Collect `seconds` worth of goroutines profile."""
+        pass
+
+
+class ActiveCollector(NoopCollector):
+
+    def __init__(self, client, machine_id):
+        """Internal collector for pprof profiles, not intended to be used
+        directly.
+
+        Installs required  on `machine_id` and enable collection of
+        pprof details.
+
+        For instance to install on the controller machine pass in
+        client=client.get_contoller_client() and machine_id='0'
+
+        :param client: jujupy.ModelClient object to install the pprof software
+          with.
+        :machine_id: ID of the juju machine with which to install pprof
+          software/charm.
+        """
+        introspection_ip = install_introspection_charm(
+            client, machine_id)
+        self._get_url_func = partial(
+            get_profile_url, introspection_ip, machine_id)
+
+    def _collect_profile(self, profile, filepath, seconds):
+        get_profile_reading(self._get_url_func(profile, seconds), filepath)
+
+    def collect_profile(self, filepath, seconds):
+        """Collect `seconds` worth of CPU profile."""
+        self._collect_profile('profile', filepath, seconds)
+
+    def collect_heap(self, filepath, seconds):
+        """Collect `seconds` worth of heap profile."""
+        self._collect_profile('heap', filepath, seconds)
+
+    def collect_goroutines(self, filepath, seconds):
+        """Collect `seconds` worth of goroutines profile."""
+        self._collect_profile('goroutines', filepath, seconds)
+
+
+def get_profile_url(ipaddress, machine_id, profile_name, seconds):
+    profile_url = 'agents/machine-{}/debug/pprof/{}'.format(
+        machine_id, profile_name)
+    return 'http://{}:19090/{}?seconds={}'.format(
+        ipaddress, profile_url, seconds)
+
+
+def get_profile_reading(url, filepath):
+    res = requests.get(url)
+    with open(filepath, 'wb') as f:
+        f.write(res.content)
+
+
+def install_introspection_charm(client, machine_id):
+    client.deploy(_get_introspection_charm_url(), to=machine_id)
+    client.wait_for_started()
+    client.wait_for_workloads()
+    client.juju('expose', 'juju-introspection')
+
+    return get_unit_ipaddress(client, 'juju-introspection/0')
+
+
+def _get_introspection_charm_url():
+    return 'cs:~axwalk/juju-introspection'
+
+
+class PPROFCollector:
+
+    def __init__(self, client, machine_id, logs_dir, active=False):
+        """Collector of pprof profiles from a machine.
+
+        Defaults to being non-active meaning that any attempt to collect a
+        profile will be a no-op.
+        Setting active (either at creation or at any point during the lifespan
+        of this object) will enable collection of pprof profiles from the
+        `machine_id`.
+        (Note. first time going active will result in the introspection charm
+        being deployed to the `machine_id.)
+
+        """
+        self._active_collector = None
+        self._noop_collector = None
+        self._active = active
+
+        self._cpu_profile_path = os.path.join(logs_dir, 'cpu_profile')
+        os.makedirs(self._cpu_profile_path)
+        self._heap_profile_path = os.path.join(logs_dir, 'heap_profile')
+        os.makedirs(self._heap_profile_path)
+        self._goroutines_path = os.path.join(logs_dir, 'goroutines_profile')
+        os.makedirs(self._goroutines_path)
+
+        # Store in case we need to activate a collector at a later date.
+        self._client = client
+        self._machine_id = machine_id
+
+        if self._active:
+            self.set_active()
+        else:
+            self.unset_active()
+
+    def set_active(self):
+        if self._active_collector is None:
+            self._active_collector = ActiveCollector(
+                self._client, self._machine_id)
+        self._collector = self._active_collector
+
+    def unset_active(self):
+        if self._noop_collector is None:
+            self._noop_collector = NoopCollector(
+                self._client, self._machine_id)
+        self._collector = self._noop_collector
+
+    def collect_profile(self, seconds=5):
+        """Collect `seconds` worth of CPU profile."""
+        self._collector.collect_profile(self._cpu_profile_path, seconds)
+
+    def collect_heap(self, seconds=5):
+        """Collect `seconds` worth of heap profile."""
+        self._collector.collect_heap(self._heap_profile_path, seconds)
+
+    def collect_goroutines(self, seconds=5):
+        """Collect `seconds` worth of goroutines profile."""
+        self._collector.collect_goroutines(self._goroutines_path, seconds)
---
+++ tests/test_pprof_collector.py
@@ -0,0 +1,199 @@
+"""Tests for assess_perf_test_simple module."""
+
+from contextlib import contextmanager
+import os
+from mock import call, patch, Mock, mock_open
+
+import pprof_collector as pc
+from tests import TestCase
+
+
+class TestActiveCollector(TestCase):
+
+    def test_installs_introspection_charm(self):
+        client = Mock()
+        with patch.object(
+                pc, 'install_introspection_charm', autospec=True) as iic:
+            pc.ActiveCollector(client, 'test')
+        iic.assert_called_once_with(client, 'test')
+
+    def test_creates_personalised_url_func(self):
+        client = Mock()
+        with patch.object(
+                pc, 'install_introspection_charm',
+                return_value='123.123.123.123',
+                autospec=True):
+            with patch.object(pc, 'partial', autospec=True) as pp:
+                pc.ActiveCollector(client, 'test')
+
+        pp.assert_called_once_with(
+            pc.get_profile_url, '123.123.123.123', 'test')
+
+    def test__collect_profile_uses_personalised_url_path(self):
+        client = Mock()
+        expected_url = pc.get_profile_url(
+            '123.123.123.123', 'test', 'test_profile', '12')
+        with patch.object(
+                pc, 'install_introspection_charm',
+                return_value='123.123.123.123',
+                autospec=True):
+            with patch.object(pc, 'get_profile_reading', autospec=True) as gpr:
+                collector = pc.ActiveCollector(client, 'test')
+                collector._collect_profile('test_profile', '/tmp/test', '12')
+        gpr.assert_called_once_with(expected_url, '/tmp/test')
+
+    @contextmanager
+    def mocked_collector(self, machine_id='test'):
+        """Create an ActiveCollector with patched _collect_profile to test
+        collect_* methods.
+        """
+        client = Mock()
+        with patch.object(
+                pc, 'install_introspection_charm',
+                return_value='123.123.123.123',
+                autospec=True):
+            collector = pc.ActiveCollector(client, machine_id)
+            with patch.object(collector, '_collect_profile'):
+                yield collector
+
+    def test_collect_profile(self):
+        with self.mocked_collector() as mc:
+            mc.collect_profile('/path/profile', 42)
+            mc._collect_profile.assert_called_once_with(
+                'profile', '/path/profile', 42)
+
+    def test_collect_heap(self):
+        with self.mocked_collector() as mc:
+            mc.collect_heap('/path/heap', 42)
+            mc._collect_profile.assert_called_once_with(
+                'heap', '/path/heap', 42)
+
+    def test_collect_goroutines(self):
+        with self.mocked_collector() as mc:
+            mc.collect_goroutines('/path/goroutines', 42)
+            mc._collect_profile.assert_called_once_with(
+                'goroutines', '/path/goroutines', 42)
+
+
+class TestHelperFunctions(TestCase):
+
+    def test_get_profile_url(self):
+        url = pc.get_profile_url('1.1.1.1', 'test', 'test_profile', 123)
+        self.assertEqual(
+            url,
+            'http://1.1.1.1:19090/agents/machine-test/debug/pprof/test_profile'
+            '?seconds=123'
+        )
+
+    def test_get_profile_reading(self):
+        m = mock_open()
+        open_patch = '{}.open'.format(pc.__name__)
+        mock_response = Mock()
+        mock_response.content = 'This is some content.'
+        with patch.object(pc, 'requests', autospect=True) as pr:
+            pr.get.return_value = mock_response
+            with patch(open_patch, m, create=True):
+                pc.get_profile_reading('test/url', '/tmp/testing/url')
+                m.assert_called_once_with('/tmp/testing/url', 'wb')
+                handle = m()
+                handle.write.assert_called_once_with('This is some content.')
+
+    def test_installs_introspection_charm(self):
+        client = Mock()
+        with patch.object(pc, 'get_unit_ipaddress', autospec=True) as p_pui:
+            pc.install_introspection_charm(client, 'test')
+
+        client.deploy.assert_called_once_with(
+            'cs:~axwalk/juju-introspection', to='test')
+        client.wait_for_started.assert_called_once_with()
+        client.wait_for_workloads.assert_called_once_with()
+        client.juju.assert_called_once_with('expose', 'juju-introspection')
+        p_pui.assert_called_once_with(client, 'juju-introspection/0')
+
+
+class TestPPROFCollector(TestCase):
+
+    def test_creates_profile_directories(self):
+        client = Mock()
+        log_dir = '/test/logs/dir'
+        with patch.object(pc.os, 'makedirs', autospec=True) as p_md:
+            pc.PPROFCollector(client, 'test', log_dir)
+
+        self.assertListEqual(
+            p_md.call_args_list,
+            [
+                call(os.path.join(log_dir, 'cpu_profile')),
+                call(os.path.join(log_dir, 'heap_profile')),
+                call(os.path.join(log_dir, 'goroutines_profile'))
+            ]
+        )
+
+    def test_defaults_to_non_active_collector(self):
+        client = Mock()
+        log_dir = '/test/logs/dir'
+        with patch.object(pc.os, 'makedirs', autospec=True):
+            collector = pc.PPROFCollector(client, 'test', log_dir)
+
+            self.assertTrue(type(collector._collector), pc.NoopCollector)
+            self.assertIsNone(collector._active_collector)
+
+    def test_creates_active_collector(self):
+        client = Mock()
+        log_dir = '/test/logs/dir'
+        with patch.object(pc.os, 'makedirs', autospec=True):
+            with patch.object(
+                    pc, 'install_introspection_charm',
+                    autospec=True):
+                collector = pc.PPROFCollector(
+                    client, 'test', log_dir, active=True)
+                self.assertTrue(type(collector._collector), pc.ActiveCollector)
+                self.assertIsNone(collector._noop_collector)
+
+    def test_initiates_active_collector_after_creation(self):
+        client = Mock()
+        log_dir = '/test/logs/dir'
+        with patch.object(pc.os, 'makedirs', autospec=True):
+            collector = pc.PPROFCollector(client, 'test', log_dir)
+            self.assertTrue(type(collector._collector), pc.NoopCollector)
+            with patch.object(
+                    pc, 'install_introspection_charm',
+                    autospec=True):
+                self.assertIsNone(collector._active_collector)
+                collector.set_active()
+                self.assertTrue(type(collector._collector), pc.NoopCollector)
+
+    def test_collect_profile(selfm):
+        client = Mock()
+        log_dir = '/test/logs/dir'
+        profile_log = os.path.join(log_dir, 'cpu_profile')
+        with patch.object(pc.os, 'makedirs', autospec=True):
+            with patch.object(pc, 'NoopCollector', autospec=True):
+                collector = pc.PPROFCollector(client, 'test', log_dir)
+                collector.collect_profile()
+        collector._collector.collect_profile.assert_called_once_with(
+            profile_log, 5
+        )
+
+    def test_collect_heap(selfm):
+        client = Mock()
+        log_dir = '/test/logs/dir'
+        profile_log = os.path.join(log_dir, 'heap_profile')
+        with patch.object(pc.os, 'makedirs', autospec=True):
+            with patch.object(pc, 'NoopCollector', autospec=True):
+                collector = pc.PPROFCollector(client, 'test', log_dir)
+                collector.collect_heap()
+        collector._collector.collect_heap.assert_called_once_with(
+            profile_log, 5
+        )
+
+    def test_collect_goroutines(selfm):
+        client = Mock()
+        log_dir = '/test/logs/dir'
+        profile_log = os.path.join(log_dir, 'goroutines_profile')
+        with patch.object(pc.os, 'makedirs', autospec=True):
+            with patch.object(pc, 'NoopCollector', autospec=True):
+                collector = pc.PPROFCollector(client, 'test', log_dir)
+                collector.collect_goroutines()
+        collector._collector.collect_goroutines.assert_called_once_with(
+            profile_log, 5
+        )
