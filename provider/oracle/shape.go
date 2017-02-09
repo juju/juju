9cab89c2fd681ea4e41c48245583ad83b0113ba0
Giulitti Salvatore <sgiulitti@cloudbasesolutions.com>
2017-02-09 12:35:47 +0200

    Fix spelling issues
    Signed-off-by: Giulitti Salvatore <sgiulitti@cloudbasesolutions.com>

--- provider/oracle/shape.go
+++ provider/oracle/shape.go
@@ -17,8 +17,8 @@ type shape struct {
 	ram uint64
 }
 
-// findShape searches through the oracle api and finds the best shape that is complaint with
-// the bootstrap constraints based on ram and cpu
+// find shape searches through the oracle api and finds the best shape
+// that is complaint with the bootstrap constraints containing the ram and cpu
 func findShape(shapes []response.Shape, cons constraints.Value) (*shape, error) {
 	if shapes == nil {
 		return nil, errors.NotValidf("cannot search through nil shape")
@@ -32,26 +32,26 @@ func findShape(shapes []response.Shape, cons constraints.Value) (*shape, error) 
 		n, min, cpus, bestMin uint64
 		bestFit               *shape
 	)
-
-	// take the best decision on the shape based on the cpu core number
-	// and if the we found two cpu elements with the same cpu core number
-	// we will take the more closest one on the ram/mem value
+	// take te best decision on the shape based on the cpu core number
+	// if we found two shapes with the same cpu
+	// we'll take the most the most closest ram value
+	// based on the constraints mem value
 	for key, val := range shapes {
 		fmt.Println(val)
-		// because of the oracle api having the cpu core number being a float
-		// we should cast it here avoiding extra casting between the code
+		// because the oracle api has the cpu core number being a float
+		// we cast it here avoiding extra casting between the code
 		cpus = uint64(val.Cpus)
 
-		// compute the minimum distrance between two cpus
+		// compute the minimum distance between two cpus<Paste>
 		if *cons.CpuCores < cpus {
 			min = cpus - *cons.CpuCores
 		} else {
 			min = *cons.CpuCores - cpus
 		}
 
-		// if we started, this means we don't have
-		// any distrance already checked
-		// and add the current one as if it's the best one
+		// if we just started, this means we don't have any
+		// computed distance checked and
+		// we should add this one instead
 		if key == 0 {
 			n = min
 			bestFit = &shape{
@@ -65,10 +65,11 @@ func findShape(shapes []response.Shape, cons constraints.Value) (*shape, error) 
 			bestFit.name = val.Name
 			n = min
 			// if we have the same number of cores
-			// we must check the memory count
+			// we must check the memory/ram count
 		} else if min == n {
-			// compute the difference between the memory from each point
-			// and previously point
+			// compute the difference between
+			// the memory from each previous
+			// value and current one
 			if *cons.Mem < val.Ram {
 				min = val.Ram - *cons.Mem
 			} else {
@@ -80,9 +81,9 @@ func findShape(shapes []response.Shape, cons constraints.Value) (*shape, error) 
 				bestMin = *cons.Mem - bestFit.ram
 			}
 
-			// if the previously point is greater than
-			// the current memory point this means the current one is
-			// more appropiate for the selection
+			// if the previous memory value is greater
+			// than the current one this means the current
+			// one is more appropriate for selection
 			if bestMin > min {
 				bestFit.cpus = cpus
 				bestFit.ram = val.Ram
@@ -93,7 +94,7 @@ func findShape(shapes []response.Shape, cons constraints.Value) (*shape, error) 
 
 	if bestFit == nil {
 		return bestFit, errors.NotFoundf(
-			"cannot find any shape that is complaint with the constraint provider",
+			"cannot find any shape that is compliant with the constraint provider",
 		)
 	}
 
