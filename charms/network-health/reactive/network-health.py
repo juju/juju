dec1fd34c8ba9f8f1268a185fba9eac9caf1a550
Michael Skalka <michael.skalka@canonical.com>
2017-02-17 15:28:32 -0500

    reverted install script

--- charms/network-health/reactive/network-health.py
+++ charms/network-health/reactive/network-health.py
@@ -4,10 +4,4 @@ from charms.reactive import when, when_not, set_state
 
 @when_not('network-health.installed')
 def install_network_health():
-    try:
-        import charmhelpers  # noqa
-    except ImportError:
-        import subprocess
-        subprocess.check_call(['apt-get', 'install', '-y', 'python-pip'])
-        subprocess.check_call(['pip', 'install', 'charmhelpers'])
     set_state('network-health.installed')
