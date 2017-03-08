0b89b580a3eaccb83fd28377904929a36fd30f0c
mattyw <gh@mattyw.net>
2017-03-08 11:18:22 -0600

    cmd/support: Myriad changes

--- cmd/juju/romulus/sla/sla_test.go
+++ cmd/juju/romulus/sla/sla_test.go
@@ -88,6 +88,17 @@ func (s supportCommandSuite) TestSupportCommand(c *gc.C) {
 				"personal:10",
 			},
 		}},
+	}, {
+		about: "invalid level",
+		level: "invalid",
+		apiCalls: []testing.StubCall{{
+			FuncName: "Authorize",
+			Args: []interface{}{
+				s.State.ModelUUID(),
+				"invalid",
+			},
+		}},
+		err: `SLA level "invalid" not valid`,
 	},
 	}
 	for i, test := range tests {
