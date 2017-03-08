6048fb51d2a54d4c689138962e9a6d7495033404
Curtis Hovey <curtis@canonical.com>
2017-03-08 17:33:39 -0600

    Remove Errant to= arg from assess_endpoint_bindings.

--- assess_endpoint_bindings.py
+++ assess_endpoint_bindings.py
@@ -281,7 +281,7 @@ def machine_spaces_for_bundle(bundle):
 
 
 def bootstrap_and_test(bootstrap_manager, bundle_path, machine):
-    with bootstrap_manager.booted_context(False, to=machine, no_gui=True):
+    with bootstrap_manager.booted_context(False, no_gui=True):
         client = bootstrap_manager.client
         log.info("Deploying bundle.")
         client.deploy(bundle_path)
