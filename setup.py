a5df2cc20199735855391f03d44fe6c0426fd4c4
Aaron Bentley <aaron.bentley@canonical.com>
2017-03-13 09:15:32 -0400

    Update version to 0.1.1 in honour of python 3 compatibility.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='jujupy',
-    version='0.1.0',
+    version='0.1.1',
     description='A library for driving the Juju client.',
     packages=['jujupy'],
     install_requires=[
