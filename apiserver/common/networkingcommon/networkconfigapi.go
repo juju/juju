b84963d0c3f33bf7cb29f4a88bfaac81f8e56600
Witold Krecicki <witold.krecicki@canonical.com>
2017-03-15 11:50:02 +0100

    Move NetworkConfigAPI out of MachinerAPI

---
+++ apiserver/common/networkingcommon/networkconfigapi.go
@@ -0,0 +1,183 @@
+// Copyright 2017 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+// The networkconfigapi package implements the network config parts
+// common to machiner and provisioner interface
+
+package networkingcommon
+
+import (
+	"gopkg.in/juju/names.v2"
+	"github.com/juju/errors"
+	"github.com/juju/juju/apiserver/common"
+	"github.com/juju/juju/state/stateenvirons"
+	"github.com/juju/juju/apiserver/params"
+	"github.com/juju/juju/state"
+)
+
+type NetworkConfigAPI struct {
+	st		*state.State
+	getCanModify	common.GetAuthFunc
+}
+
+func NewNetworkConfigAPI(st *state.State, getCanModify common.GetAuthFunc) *NetworkConfigAPI {
+	return &NetworkConfigAPI{
+		st:		st,
+		getCanModify:	getCanModify,
+	}
+}
+
+func (api *NetworkConfigAPI) getMachine(tag names.Tag) (*state.Machine, error) {
+	entity, err := api.st.FindEntity(tag)
+	if err != nil {
+		return nil, err
+	}
+	return entity.(*state.Machine), nil
+}
+
+func (api *NetworkConfigAPI) getOneMachineProviderNetworkConfig(m *state.Machine) ([]params.NetworkConfig, error) {
+	instId, err := m.InstanceId()
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+
+	netEnviron, err := NetworkingEnvironFromModelConfig(
+		stateenvirons.EnvironConfigGetter{api.st},
+	)
+	if errors.IsNotSupported(err) {
+		logger.Infof("provider network config not supported: %v", err)
+		return nil, nil
+	} else if err != nil {
+		return nil, errors.Annotate(err, "cannot get provider network config")
+	}
+
+	interfaceInfos, err := netEnviron.NetworkInterfaces(instId)
+	if err != nil {
+		return nil, errors.Annotatef(err, "cannot get network interfaces of %q", instId)
+	}
+	if len(interfaceInfos) == 0 {
+		logger.Infof("not updating provider network config: no interfaces returned")
+		return nil, nil
+	}
+
+	providerConfig := NetworkConfigFromInterfaceInfo(interfaceInfos)
+	logger.Tracef("provider network config instance %q: %+v", instId, providerConfig)
+
+	return providerConfig, nil
+}
+
+func (api *NetworkConfigAPI) setOneMachineNetworkConfig(m *state.Machine, networkConfig []params.NetworkConfig) error {
+	devicesArgs, devicesAddrs := NetworkConfigsToStateArgs(networkConfig)
+
+	logger.Debugf("setting devices: %+v", devicesArgs)
+	if err := m.SetParentLinkLayerDevicesBeforeTheirChildren(devicesArgs); err != nil {
+		return errors.Trace(err)
+	}
+
+	logger.Debugf("setting addresses: %+v", devicesAddrs)
+	if err := m.SetDevicesAddressesIdempotently(devicesAddrs); err != nil {
+		return errors.Trace(err)
+	}
+
+	logger.Debugf("updated machine %q network config", m.Id())
+	return nil
+}
+
+func (api *NetworkConfigAPI) getMachineForSettingNetworkConfig(machineTag string) (*state.Machine, error) {
+	canModify, err := api.getCanModify()
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+
+	tag, err := names.ParseMachineTag(machineTag)
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+	if !canModify(tag) {
+		return nil, errors.Trace(common.ErrPerm)
+	}
+
+	m, err := api.getMachine(tag)
+	if errors.IsNotFound(err) {
+		return nil, errors.Trace(common.ErrPerm)
+	} else if err != nil {
+		return nil, errors.Trace(err)
+	}
+
+	if m.IsContainer() {
+		logger.Warningf("not updating network config for container %q", m.Id())
+	}
+
+	return m, nil
+}
+
+
+func (api *NetworkConfigAPI) SetObservedNetworkConfig(args params.SetMachineNetworkConfig) error {
+	m, err := api.getMachineForSettingNetworkConfig(args.Tag)
+	if err != nil {
+		return errors.Trace(err)
+	}
+	if m.IsContainer() {
+		return nil
+	}
+	observedConfig := args.Config
+	logger.Tracef("observed network config of machine %q: %+v", m.Id(), observedConfig)
+	if len(observedConfig) == 0 {
+		logger.Infof("not updating machine %q network config: no observed network config found", m.Id())
+		return nil
+	}
+
+	providerConfig, err := api.getOneMachineProviderNetworkConfig(m)
+	if errors.IsNotProvisioned(err) {
+		logger.Infof("not updating machine %q network config: %v", m.Id(), err)
+		return nil
+	}
+	if err != nil {
+		return errors.Trace(err)
+	}
+	finalConfig := observedConfig
+	if len(providerConfig) != 0 {
+		finalConfig = MergeProviderAndObservedNetworkConfigs(providerConfig, observedConfig)
+		logger.Tracef("merged observed and provider network config for machine %q: %+v", m.Id(), finalConfig)
+	}
+
+	return api.setOneMachineNetworkConfig(m, finalConfig)
+}
+
+
+
+
+
+func (api *NetworkConfigAPI) SetProviderNetworkConfig(args params.Entities) (params.ErrorResults, error) {
+	result := params.ErrorResults{
+		Results: make([]params.ErrorResult, len(args.Entities)),
+	}
+
+	for i, arg := range args.Entities {
+		m, err := api.getMachineForSettingNetworkConfig(arg.Tag)
+		if err != nil {
+			result.Results[i].Error = common.ServerError(err)
+			continue
+		}
+
+		if m.IsContainer() {
+			continue
+		}
+
+		providerConfig, err := api.getOneMachineProviderNetworkConfig(m)
+		if err != nil {
+			result.Results[i].Error = common.ServerError(err)
+			continue
+		} else if len(providerConfig) == 0 {
+			continue
+		}
+
+		logger.Tracef("provider network config for %q: %+v", m.Id(), providerConfig)
+
+		if err := api.setOneMachineNetworkConfig(m, providerConfig); err != nil {
+			result.Results[i].Error = common.ServerError(err)
+			continue
+		}
+	}
+	return result, nil
+}
---
+++ apiserver/common/networkingcommon/networkconfigapi_test.go
@@ -0,0 +1,137 @@
+// Copyright 2017 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package networkingcommon_test
+
+import (
+	jc "github.com/juju/testing/checkers"
+	gc "gopkg.in/check.v1"
+	"github.com/juju/juju/apiserver/common"
+	"github.com/juju/juju/apiserver/common/networkingcommon"
+	"github.com/juju/juju/apiserver/params"
+	apiservertesting "github.com/juju/juju/apiserver/testing"
+	"github.com/juju/juju/state"
+	"github.com/juju/juju/juju/testing"
+)
+
+type networkconfigSuite struct {
+	testing.JujuConnSuite
+
+	machine		*state.Machine
+	resources 	*common.Resources
+	networkconfig	*networkingcommon.NetworkConfigAPI
+}
+
+func (s *networkconfigSuite) SetUpTest(c *gc.C) {
+	s.JujuConnSuite.SetUpTest(c)
+	var err error
+	s.machine, err = s.State.AddMachine("quantal", state.JobHostUnits)
+	c.Assert(err, jc.ErrorIsNil)
+}
+
+func (s *networkconfigSuite) TestSetObservedNetworkConfig(c *gc.C) {
+	c.Skip("dimitern: Test disabled until dummy provider is fixed properly")
+	devices, err := s.machine.AllLinkLayerDevices()
+	c.Assert(err, jc.ErrorIsNil)
+	c.Assert(devices, gc.HasLen, 0)
+
+	err = s.machine.SetInstanceInfo("i-foo", "FAKE_NONCE", nil, nil, nil, nil, nil)
+	c.Assert(err, jc.ErrorIsNil)
+
+	observedConfig := []params.NetworkConfig{{
+		InterfaceName: "lo",
+		InterfaceType: "loopback",
+		CIDR:          "127.0.0.0/8",
+		Address:       "127.0.0.1",
+	}, {
+		InterfaceName: "eth0",
+		InterfaceType: "ethernet",
+		MACAddress:    "aa:bb:cc:dd:ee:f0",
+		CIDR:          "0.10.0.0/24",
+		Address:       "0.10.0.2",
+	}, {
+		InterfaceName: "eth1",
+		InterfaceType: "ethernet",
+		MACAddress:    "aa:bb:cc:dd:ee:f1",
+		CIDR:          "0.20.0.0/24",
+		Address:       "0.20.0.2",
+	}}
+	args := params.SetMachineNetworkConfig{
+		Tag:    s.machine.Tag().String(),
+		Config: observedConfig,
+	}
+
+	err = s.networkconfig.SetObservedNetworkConfig(args)
+	c.Assert(err, jc.ErrorIsNil)
+
+	devices, err = s.machine.AllLinkLayerDevices()
+	c.Assert(err, jc.ErrorIsNil)
+	c.Assert(devices, gc.HasLen, 3)
+
+	for _, device := range devices {
+		c.Check(device.Name(), gc.Matches, `(lo|eth0|eth1)`)
+		c.Check(string(device.Type()), gc.Matches, `(loopback|ethernet)`)
+		c.Check(device.MACAddress(), gc.Matches, `(|aa:bb:cc:dd:ee:f0|aa:bb:cc:dd:ee:f1)`)
+	}
+}
+
+func (s *networkconfigSuite) TestSetObservedNetworkConfigPermissions(c *gc.C) {
+	args := params.SetMachineNetworkConfig{
+		Tag:    "machine-0",
+		Config: nil,
+	}
+
+	err := s.networkconfig.SetObservedNetworkConfig(args)
+	c.Assert(err, gc.ErrorMatches, "permission denied")
+}
+
+func (s *networkconfigSuite) TestSetProviderNetworkConfig(c *gc.C) {
+	c.Skip("dimitern: Test disabled until dummy provider is fixed properly")
+	devices, err := s.machine.AllLinkLayerDevices()
+	c.Assert(err, jc.ErrorIsNil)
+	c.Assert(devices, gc.HasLen, 0)
+
+	err = s.machine.SetInstanceInfo("i-foo", "FAKE_NONCE", nil, nil, nil, nil, nil)
+	c.Assert(err, jc.ErrorIsNil)
+
+	args := params.Entities{Entities: []params.Entity{
+		{Tag: s.machine.Tag().String()},
+	}}
+
+	result, err := s.networkconfig.SetProviderNetworkConfig(args)
+	c.Assert(err, jc.ErrorIsNil)
+	c.Assert(result, gc.DeepEquals, params.ErrorResults{
+		Results: []params.ErrorResult{{nil}},
+	})
+
+	devices, err = s.machine.AllLinkLayerDevices()
+	c.Assert(err, jc.ErrorIsNil)
+	c.Assert(devices, gc.HasLen, 3)
+
+	for _, device := range devices {
+		c.Check(device.Name(), gc.Matches, `eth[0-2]`)
+		c.Check(string(device.Type()), gc.Equals, "ethernet")
+		c.Check(device.MACAddress(), gc.Matches, `aa:bb:cc:dd:ee:f[0-2]`)
+		addrs, err := device.Addresses()
+		c.Check(err, jc.ErrorIsNil)
+		c.Check(addrs, gc.HasLen, 1)
+	}
+}
+
+func (s *networkconfigSuite) TestSetProviderNetworkConfigPermissions(c *gc.C) {
+	args := params.Entities{Entities: []params.Entity{
+		{Tag: "machine-1"},
+		{Tag: "machine-0"},
+		{Tag: "machine-42"},
+	}}
+
+	result, err := s.networkconfig.SetProviderNetworkConfig(args)
+	c.Assert(err, jc.ErrorIsNil)
+	c.Assert(result, gc.DeepEquals, params.ErrorResults{
+		Results: []params.ErrorResult{
+			{Error: apiservertesting.NotProvisionedError(s.machine.Id())},
+			{Error: apiservertesting.ErrUnauthorized},
+			{Error: apiservertesting.ErrUnauthorized},
+		},
+	})
+}
