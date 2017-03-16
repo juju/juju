fad21992f01fb7fcb8302fc520f67497248640d6
Roger Peppe <rogpeppe@gmail.com>
2017-03-16 17:46:35 +0000

    state: fix flaky tests
    dumpSuite.TestDumpAll and TestNoModelDocs were
    failing because the state workers are now started asynchronously but
    this tests was relying on their side-effects (creating
    collections) taking place immediately.
    
    Other tests relying on setting the state clock were
    also failing the race detector for similar reasons.
    
    Fixes https://bugs.launchpad.net/juju/+bug/1673544, https://bugs.launchpad.net/juju/+bug/1673552 and https://bugs.launchpad.net/juju/+bug/1673554.

--- state/state.go
+++ state/state.go
@@ -2155,13 +2155,14 @@ func tagForGlobalKey(key string) (string, bool) {
 // to set the internal clock for the State instance. It is named such
 // that it should be obvious if it is ever called from a non-test package.
 func (st *State) SetClockForTesting(clock clock.Clock) error {
-	st.clock = clock
 	// Need to restart the lease workers so they get the new clock.
+	// Stop them first so they don't try to use it when we're setting it.
 	st.workers.Kill()
 	err := st.workers.Wait()
 	if err != nil {
 		return errors.Trace(err)
 	}
+	st.clock = clock
 	err = st.start(st.controllerTag)
 	if err != nil {
 		return errors.Trace(err)
