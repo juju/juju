c4434363d5f0a9e8c89b9e1b012a638c73c4058d
Menno Smits <menno.smits@canonical.com>
2017-03-20 12:30:10 +1300

    apiserver: Register ResourcesUnitContext facade

--- apiserver/allfacades.go
+++ apiserver/allfacades.go
@@ -63,6 +63,7 @@ import (
 	_ "github.com/juju/juju/apiserver/remotefirewaller"
 	_ "github.com/juju/juju/apiserver/remoterelations"
 	_ "github.com/juju/juju/apiserver/resources"
+	_ "github.com/juju/juju/apiserver/resourceshookcontext"
 	_ "github.com/juju/juju/apiserver/resumer"
 	_ "github.com/juju/juju/apiserver/retrystrategy"
 	_ "github.com/juju/juju/apiserver/singular"
