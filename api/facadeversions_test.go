758c5de48581f6b128ae50810303dad32a94c17d
Menno Smits <menno.smits@canonical.com>
2017-03-24 12:49:04 +1300

    api: Use apiserver.AllFacades instead of global registry
    The global registry is gone now.

--- api/facadeversions_test.go
+++ api/facadeversions_test.go
@@ -9,7 +9,7 @@ import (
 	gc "gopkg.in/check.v1"
 
 	"github.com/juju/juju/api"
-	"github.com/juju/juju/apiserver/common"
+	"github.com/juju/juju/apiserver"
 	"github.com/juju/juju/feature"
 	coretesting "github.com/juju/juju/testing"
 )
@@ -35,7 +35,7 @@ func (s *facadeVersionSuite) TestFacadeVersionsMatchServerVersions(c *gc.C) {
 		// All versions should now be non-zero.
 		c.Check(version, jc.GreaterThan, 0)
 	}
-	allServerFacades := common.Facades.List()
+	allServerFacades := apiserver.AllFacades().List()
 	serverFacadeNames := set.NewStrings()
 	serverFacadeBestVersions := make(map[string]int, len(allServerFacades))
 	for _, facade := range allServerFacades {
