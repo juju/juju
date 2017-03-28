37f660d9c6827dca4e5aa45b3787e95d5967830f
Aaron Bentley <aaron.bentley@canonical.com>
2017-03-28 16:45:57 -0400

    Skip precise.

--- update-jenkins-slave.bash
+++ update-jenkins-slave.bash
@@ -71,7 +71,7 @@ echo "Updating dependencies from branches"
 if [[ $OS == "ubuntu" ]]; then
     make -C $HOME/juju-ci-tools install-deps
     make -C $HOME/workspace-runner install
-    if ! (lsb_release -c|grep trusty); then
+    if (lsb_release -c|grep -v -E 'trusty|precise'); then
         make -C $HOME/hammer-time develop
     fi
 elif [[ $OS == "darwin" ]]; then
