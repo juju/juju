491cc418282c104b076b8d4f5774e5e0ebe96658
Giulitti Salvatore <sgiulitti@cloudbasesolutions.com>
2017-04-03 14:20:11 +0300

    Open ports and GlobalIngressRules unit tests.
    Signed-off-by: Giulitti Salvatore <sgiulitti@cloudbasesolutions.com>

--- provider/oracle/network/firewall_test.go
+++ provider/oracle/network/firewall_test.go
@@ -4,14 +4,13 @@
 package network_test
 
 import (
-	"fmt"
+	"errors"
 
 	"github.com/juju/go-oracle-cloud/api"
-	"github.com/juju/go-oracle-cloud/common"
-	"github.com/juju/go-oracle-cloud/response"
 	gc "gopkg.in/check.v1"
 
 	"github.com/juju/juju/environs/config"
+	jujunetwork "github.com/juju/juju/network"
 	"github.com/juju/juju/provider/oracle/network"
 	"github.com/juju/juju/testing"
 )
@@ -40,31 +39,138 @@ func (u *firewallSuite) TestNewFirewall(c *gc.C) {
 
 func (u *firewallSuite) TestGlobalIngressRules(c *gc.C) {
 	cfg := &fakeEnvironConfig{cfg: testing.ModelConfig(c)}
-	cli := &FakeFirewallAPI{
-		FakeComposer: FakeComposer{
-			compose: "some-test-string",
-		},
-		FakeRules: FakeRules{
-			All: response.AllSecRules{
-				Result: []response.SecRule{
-					response.SecRule{
-						Action:      common.SecRulePermit,
-						Application: "/Compute-acme/jack.jones@example.com/video_streaming_udp",
-						Name:        "/Compute-acme/jack.jones@example.com/es_to_videoservers_stream",
-						Dst_list:    "seclist:/Compute-acme/jack.jones@example.com/allowed_video_servers",
-						Src_list:    "seciplist:/Compute-acme/jack.jones@example.com/es_iplist",
-						Uri:         "https://api-z999.compute.us0.oraclecloud.com/secrule/Compute-acme/jack.jones@example.com/es_to_videoservers_stream",
-					},
-				},
-			},
-		},
-		//TODO
-	}
 
-	firewall := network.NewFirewall(cfg, cli)
+	firewall := network.NewFirewall(cfg, DefaultFakeFirewallAPI)
 	c.Assert(firewall, gc.NotNil)
 
 	rule, err := firewall.GlobalIngressRules()
 	c.Assert(err, gc.IsNil)
-	fmt.Println(rule)
+	c.Assert(rule, gc.NotNil)
+}
+
+func (u *firewallSuite) TestGlobalIngressRulesWithErrors(c *gc.C) {
+	cfg := &fakeEnvironConfig{cfg: testing.ModelConfig(c)}
+
+	for _, fake := range []*FakeFirewallAPI{
+		&FakeFirewallAPI{
+			FakeComposer: FakeComposer{
+				compose: "/Compute-acme/jack.jones@example.com/allowed_video_servers",
+			},
+			FakeRules: FakeRules{AllErr: errors.New("FakeRulesError")},
+		},
+		&FakeFirewallAPI{
+			FakeComposer: FakeComposer{
+				compose: "/Compute-acme/jack.jones@example.com/allowed_video_servers",
+			},
+			FakeApplication: FakeApplication{
+				AllErr: errors.New("FakeApplicationError"),
+			},
+		},
+		&FakeFirewallAPI{
+			FakeComposer: FakeComposer{
+				compose: "/Compute-acme/jack.jones@example.com/allowed_video_servers",
+			},
+			FakeApplication: FakeApplication{
+				DefaultErr: errors.New("FakeApplicationError"),
+			},
+		},
+		&FakeFirewallAPI{
+			FakeComposer: FakeComposer{
+				compose: "/Compute-acme/jack.jones@example.com/allowed_video_servers",
+			},
+			FakeSecIp: FakeSecIp{AllErr: errors.New("FakeSecIpError")},
+		},
+	} {
+
+		firewall := network.NewFirewall(cfg, fake)
+		c.Assert(firewall, gc.NotNil)
+
+		rule, err := firewall.GlobalIngressRules()
+		c.Assert(err, gc.NotNil)
+		c.Assert(rule, gc.IsNil)
+	}
+
+}
+
+func (u *firewallSuite) TestOpenPorts(c *gc.C) {
+	fakeConfig := testing.CustomModelConfig(c, testing.Attrs{
+		"firewall-mode": config.FwGlobal,
+	})
+	cfg := &fakeEnvironConfig{cfg: fakeConfig}
+
+	firewall := network.NewFirewall(cfg, DefaultFakeFirewallAPI)
+	c.Assert(firewall, gc.NotNil)
+
+	err := firewall.OpenPorts([]jujunetwork.IngressRule{})
+	c.Assert(err, gc.IsNil)
+
+}
+
+func (u *firewallSuite) TestOpenPortsWithErrors(c *gc.C) {
+	fakeConfig := testing.CustomModelConfig(c, testing.Attrs{
+		"firewall-mode": config.FwGlobal,
+	})
+	cfg := &fakeEnvironConfig{cfg: fakeConfig}
+
+	for _, fake := range []*FakeFirewallAPI{
+		&FakeFirewallAPI{
+			FakeComposer: FakeComposer{
+				compose: "/Compute-acme/jack.jones@example.com/allowed_video_servers",
+			},
+			FakeRules: FakeRules{AllErr: errors.New("FakeRulesError")},
+		},
+		&FakeFirewallAPI{
+			FakeComposer: FakeComposer{
+				compose: "/Compute-acme/jack.jones@example.com/allowed_video_servers",
+			},
+			FakeApplication: FakeApplication{
+				AllErr: errors.New("FakeApplicationError"),
+			},
+		},
+		&FakeFirewallAPI{
+			FakeComposer: FakeComposer{
+				compose: "/Compute-acme/jack.jones@example.com/allowed_video_servers",
+			},
+			FakeApplication: FakeApplication{
+				DefaultErr: errors.New("FakeApplicationError"),
+			},
+		},
+		&FakeFirewallAPI{
+			FakeComposer: FakeComposer{
+				compose: "/Compute-acme/jack.jones@example.com/allowed_video_servers",
+			},
+			FakeSecIp: FakeSecIp{AllErr: errors.New("FakeSecIpError")},
+		},
+		&FakeFirewallAPI{
+			FakeComposer: FakeComposer{
+				compose: "/Compute-acme/jack.jones@example.com/allowed_video_servers",
+			},
+			FakeSecList: FakeSecList{
+				SecListErr: errors.New("FakeSecListErr"),
+			},
+		},
+		&FakeFirewallAPI{
+			FakeComposer: FakeComposer{
+				compose: "/Compute-acme/jack.jones@example.com/allowed_video_servers",
+			},
+			FakeSecList: FakeSecList{
+				SecListErr: api.ErrNotFound{},
+				CreateErr:  errors.New("FakeSecListErr"),
+			},
+		},
+	} {
+		firewall := network.NewFirewall(cfg, fake)
+		c.Assert(firewall, gc.NotNil)
+
+		err := firewall.OpenPorts([]jujunetwork.IngressRule{})
+		c.Assert(err, gc.NotNil)
+	}
+
+	// test with error in firewall config
+	cfg = &fakeEnvironConfig{cfg: testing.ModelConfig(c)}
+	firewall := network.NewFirewall(cfg, DefaultFakeFirewallAPI)
+	c.Assert(firewall, gc.NotNil)
+
+	err := firewall.OpenPorts([]jujunetwork.IngressRule{})
+	c.Assert(err, gc.NotNil)
 }
