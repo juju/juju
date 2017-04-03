340eab77768706cfd486ed2e2f65d2bd1c2f2a57
Curtis Hovey <curtis@canonical.com>
2017-04-03 10:22:56 -0400

    Updated scripts to install and setup a jenkins slave on hardware.

--- add-remote-machine.bash
+++ add-remote-machine.bash
@@ -18,7 +18,7 @@ check_access() {
     echo "Checking $USER_AT_HOST can access $name at $url..."
     set +e
     result=$(ssh -i $SSH_KEY $USER_AT_HOST \
-        curl --connect-timeout 5 --silent --head $url || echo "fail")
+        curl --connect-timeout 5 --silent -k --head $url || echo "fail")
     set -e
     result=$(echo "$result" | tail -1)
     if [[ $result == "fail" ]]; then
@@ -30,55 +30,34 @@ check_access() {
 }
 
 
-check_url_access() {
-    option=$1
-    option_url=$(juju get-env -e $ENV $option | sed -e 's,/tools$,,')
-    if [[ -n "$option_url" ]]; then
-        check_access $option $option_url
-    else
-        echo "! You must verify that $USER_AT_HOST can access $option"
-    fi
-}
-
-
 if [[ "$1" == "--dry-run" ]]; then
     DRY_RUN="true"
     shift
 else
     DRY_RUN="false"
 fi
-ENV=$1
+MODEL=$1
 SSH_KEY=$2
 USER_AT_HOST=$3
 NETWORK_ACCESS="true"
 
-private_dns_name=$(juju ssh -e $ENV 0 'echo "$(hostname).$(dnsdomainname)"')
-public_dns_name=$(juju status -e $ENV | sed -r '/dns-name/!d; 1,1!d; s/.*: (.*)/\1/')
-public_ip=$(dig "$public_dns_name" | sed -r '/^;/d; /IN/!d; s/.*A (.*)/\1/;')
-api_port=$(juju get-env -e $ENV api-port)
-echo "State-server public address: $public_ip $public_dns_name"
-echo "State-server private address: $private_dns_name"
-check_access "state-server" http://$public_ip:$api_port
-check_url_access image-metadata-url
-check_url_access tools-metadata-url
-
-echo "Checking $USER_AT_HOST can access the cloud provider's storage"
-provider=$(juju get-env -e $ENV "type")
-if [[ $provider == "ec2" ]]; then
-    control_bucket=$(juju get-env -e $ENV control-bucket)
-    check_access "s3" http://s3.amazon.com/$control_bucket
-else
-    echo "! You must verify that $USER_AT_HOST can access the cloud storage."
-fi
-
+private_dns_name=$(
+    juju ssh -m controller 0 'echo "$(hostname).$(dnsdomainname)"')
+public_ip=$(
+    juju status -m controller --format yaml 0 |
+    sed -e '/dns-name/!d; s/.*: //')
+api_port=$(juju controller-config api-port)
+echo "Controller public address: $public_ip"
+echo "Controller private address: $private_dns_name"
+check_access "controller" https://$public_ip:$api_port
 
 if [[ $DRY_RUN == "true" || $NETWORK_ACCESS == 'false' ]]; then
     exit
 fi
 
-echo "Adding state-server's private dns name to $USER_AT_HOST:/etc/hosts"
+echo "Adding controllers's private dns name to $USER_AT_HOST:/etc/hosts"
 ssh -i $SSH_KEY $USER_AT_HOST \
     "echo '$public_ip $private_dns_name' | sudo tee -a /etc/hosts"
 
-echo "Adding $USER_AT_HOST to $ENV"
-juju --show-log add-machine -e $ENV ssh:$USER_AT_HOST
+echo "Adding $USER_AT_HOST to $MODEL"
+juju --show-log add-machine -m $MODEL ssh:$USER_AT_HOST
