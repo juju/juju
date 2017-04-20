6e068ff4118087f98988de7c51a459915558c973
Christian Muirhead <christian.muirhead@canonical.com>
2017-04-20 19:52:45 +1200

    Decouple GCE firewall name from source CIDRs
    We can't update the firewall name when we change the source CIDRs in it,
    because GCE rejects the update (since the name doesn't match the URL).
    That means we can't use hashes of the CIDRs as the name - when we change the
    CIDRs then we'll no longer be able to find the matching rule since it
    won't have the name we expect based on the CIDRs.
    
    Instead, assign random names to the firewall rules as they're created,
    and ensure we preserve them in the RuleSet so that we can send them back
    to GCE when updating an existing rule.
    
    We still use hashing of the CIDRs to match input rules against existing
    rules to determine whether to update or create a rule.
    
    UNTESTED

--- provider/gce/google/conn_network.go
+++ provider/gce/google/conn_network.go
@@ -13,45 +13,35 @@ import (
 	"github.com/juju/juju/network"
 )
 
+// FirewallRules collects the firewall rules for the given name
+// (within the Connection's project) and returns them as a RuleSet. If
+// no rules match the name the RuleSet will be empty and no error is
+// returned.
+func (gce Connection) firewallRules(fwname string) (RuleSet, error) {
+	firewalls, err := gce.raw.GetFirewalls(gce.projectID, fwname)
+	if errors.IsNotFound(err) {
+		return make(RuleSet), nil
+	}
+	if err != nil {
+		return nil, errors.Annotate(err, "while getting firewall rules from GCE")
+	}
+
+	return NewRulesetFromFirewalls(firewalls...)
+}
+
 // IngressRules build a list of all open port ranges for a given firewall name
 // (within the Connection's project) and returns it. If the firewall
 // does not exist then the list will be empty and no error is returned.
 func (gce Connection) IngressRules(fwname string) ([]network.IngressRule, error) {
-	firewalls, err := gce.raw.GetFirewalls(gce.projectID, fwname)
-	if errors.IsNotFound(err) {
-		return nil, nil
-	}
+	ruleset, err := gce.firewallRules(fwname)
 	if err != nil {
-		return nil, errors.Annotate(err, "while getting ports from GCE")
+		return nil, errors.Trace(err)
 	}
-
-	var rules []network.IngressRule
-	for _, firewall := range firewalls {
-		sourceRanges := firewall.SourceRanges
-		if len(sourceRanges) == 0 {
-			sourceRanges = []string{"0.0.0.0/0"}
-		}
-		var ranges []network.PortRange
-		for _, allowed := range firewall.Allowed {
-			for _, portRangeStr := range allowed.Ports {
-				portRange, err := network.ParsePortRange(portRangeStr)
-				if err != nil {
-					return rules, errors.Annotate(err, "bad ports from GCE")
-				}
-				portRange.Protocol = allowed.IPProtocol
-				ranges = append(ranges, portRange)
-			}
-		}
-
-		collapsed := network.CombinePortRanges(ranges...)
-		for _, portRange := range collapsed {
-			rule, _ := network.NewIngressRule(portRange.Protocol, portRange.FromPort, portRange.ToPort, sourceRanges...)
-			rules = append(rules, rule)
-		}
-	}
-
-	return rules, nil
+	return ruleset.toIngressRules()
 }
+
+// FirewallNamer generates a name for a firewall.
+type FirewallNamer func(Firewall) (string, error)
 
 // OpenPorts sends a request to the GCE API to open the provided port
 // ranges on the named firewall. If the firewall does not exist yet it
@@ -65,41 +55,40 @@ func (gce Connection) OpenPorts(target string, rules ...network.IngressRule) err
 	}
 
 	// First gather the current ingress rules.
-	currentRules, err := gce.IngressRules(target)
+	currentRuleSet, err := gce.firewallRules(target)
 	if err != nil {
 		return errors.Trace(err)
 	}
-	currentRuleSet := newRuleSet(currentRules...)
-
-	// Get the rules for the target from the current rules.
-	currentFirewallRules := currentRuleSet.getFirewallRules(target)
-
 	// From the input rules, compose the firewall specs we want to add.
-	inputRuleSet := newRuleSet(rules...)
-	inputFirewallRules := inputRuleSet.getFirewallRules(target)
+	inputRuleSet := NewRuleSetFromRules(rules...)
 
 	// For each input rule, either create a new firewall or update
 	// an existing one depending on what exists already.
-	var sortedNames []string
-	for name := range inputFirewallRules {
-		sortedNames = append(sortedNames, name)
+	// The rules are keyed by a hash of the source CIDRs.
+	var sortedKeys []string
+	for key := range inputRuleSet {
+		sortedKeys = append(sortedKeys, key)
 	}
-	sort.Strings(sortedNames)
+	sort.Strings(sortedKeys)
 
-	for _, name := range sortedNames {
-		portsByProtocol := inputFirewallRules[name]
-		inputCidrs := inputRuleSet.getCIDRs(name)
+	// Get the rules by sorted key for deterministic testing.
+	for _, key := range sortedKeys {
+		inputFirewall := inputRuleSet[key]
 
 		// First check to see if there's any existing firewall with the same ports as what we want.
-		existingFirewallName, ok := matchProtocolPorts(currentFirewallRules, portsByProtocol)
+		existingFirewall, ok := currentRuleSet.matchProtocolPorts(inputFirewall.AllowedPorts)
 		if !ok {
 			// If not, look for any existing firewall with the same source CIDRs.
-			existingFirewallName, ok = matchSourceCIDRs(currentFirewallRules, currentRuleSet, inputCidrs)
+			existingFirewall, ok = currentRuleSet.matchSourceCIDRs(inputFirewall.SourceCIDRs)
 		}
 
 		if !ok {
 			// Create a new firewall.
-			spec := firewallSpec(name, target, inputCidrs, portsByProtocol)
+			name, err := generateName(target, inputFirewall)
+			if err != nil {
+				return errors.Trace(err)
+			}
+			spec := firewallSpec(name, target, inputFirewall.SourceCIDRs, inputFirewall.AllowedPorts)
 			if err := gce.raw.AddFirewall(gce.projectID, spec); err != nil {
 				return errors.Annotatef(err, "opening port(s) %+v", rules)
 			}
@@ -111,45 +100,27 @@ func (gce Connection) OpenPorts(target string, rules ...network.IngressRule) err
 		// the existing firewall.
 
 		// Merge the ports.
-		existingFirewallRules := currentFirewallRules[existingFirewallName]
-		portsByProtocol = existingFirewallRules.union(portsByProtocol)
+		allowedPorts := existingFirewall.AllowedPorts.union(inputFirewall.AllowedPorts)
 
 		// Merge the CIDRs
-		cidrs := set.NewStrings(currentRuleSet.getCIDRs(existingFirewallName)...)
-		inputCidrs = cidrs.Union(set.NewStrings(inputCidrs...)).SortedValues()
+		cidrs := set.NewStrings(existingFirewall.SourceCIDRs...)
+		combinedCIDRs := cidrs.Union(set.NewStrings(inputFirewall.SourceCIDRs...)).SortedValues()
 
 		// Copy new firewall details into required firewall spec.
-		spec := firewallSpec(existingFirewallName, target, inputCidrs, portsByProtocol)
-		if err := gce.raw.UpdateFirewall(gce.projectID, existingFirewallName, spec); err != nil {
+		spec := firewallSpec(existingFirewall.Name, target, combinedCIDRs, allowedPorts)
+		if err := gce.raw.UpdateFirewall(gce.projectID, existingFirewall.Name, spec); err != nil {
 			return errors.Annotatef(err, "opening port(s) %+v", rules)
 		}
 	}
 	return nil
 }
 
-// matchProtocolPorts returns the matching firewall name and true if any of the current
-// firewall ports (ignoring CIDR values) matches the ports to match.
-func matchProtocolPorts(currentPorts map[string]protocolPorts, match protocolPorts) (string, bool) {
-	for fwname, current := range currentPorts {
-		if current.String() == match.String() {
-			return fwname, true
-		}
+func generateName(target string, firewall *Firewall) (string, error) {
+	suffix, err := randomSuffix(firewall)
+	if err != nil {
+		return "", errors.Trace(err)
 	}
-	return "", false
-}
-
-// matchSourceCIDRs returns the matching firewall name and true if any of the current firewall rules
-// has the same CIDR source values as those specified.
-func matchSourceCIDRs(currentPorts map[string]protocolPorts, currentRuleSet RuleSet, matchCIDRs []string) (string, bool) {
-	matchCidrSet := set.NewStrings(matchCIDRs...)
-	for fwname := range currentPorts {
-		currentCidrs := currentRuleSet.getCIDRs(fwname)
-		diff := matchCidrSet.Difference(set.NewStrings(currentCidrs...))
-		if diff.IsEmpty() {
-			return fwname, true
-		}
-	}
-	return "", false
+	return target + suffix, nil
 }
 
 // ClosePorts sends a request to the GCE API to close the provided port
@@ -160,50 +131,42 @@ func matchSourceCIDRs(currentPorts map[string]protocolPorts, currentRuleSet Rule
 // closed or the request fails.
 func (gce Connection) ClosePorts(target string, rules ...network.IngressRule) error {
 	// First gather the current ingress rules.
-	currentRules, err := gce.IngressRules(target)
+	currentRuleSet, err := gce.firewallRules(target)
 	if err != nil {
 		return errors.Trace(err)
 	}
-	currentRuleSet := newRuleSet(currentRules...)
-
-	// Get the rules for the target from the current rules.
-	currentFirewallRules := currentRuleSet.getFirewallRules(target)
 
 	// From the input rules, compose the firewall specs we want to add.
-	inputRuleSet := newRuleSet(rules...)
-	inputFirewallRules := inputRuleSet.getFirewallRules(target)
+	inputRuleSet := NewRuleSetFromRules(rules...)
 
-	// For each input rule, either create a new firewall or update
-	// an existing one depending on what exists already.
-	for name, portsByProtocol := range inputFirewallRules {
-		inputCidrs := inputRuleSet.getCIDRs(name)
-
-		existingFirewallName, allPortsMatch := matchProtocolPorts(currentFirewallRules, portsByProtocol)
+	// For each input firewall, find an existing firewall including it
+	// and update or remove it.
+	for _, inputFirewall := range inputRuleSet {
+		existingFirewall, allPortsMatch := currentRuleSet.matchProtocolPorts(inputFirewall.AllowedPorts)
 		if allPortsMatch {
 			// All the ports match so it may be that just a CIDR needs to be removed.
-			cidrs := set.NewStrings(currentRuleSet.getCIDRs(existingFirewallName)...)
-			remainingCidrs := cidrs.Difference(set.NewStrings(inputCidrs...)).SortedValues()
+			cidrs := set.NewStrings(existingFirewall.SourceCIDRs...)
+			remainingCidrs := cidrs.Difference(set.NewStrings(inputFirewall.SourceCIDRs...)).SortedValues()
 
 			// If all CIDRs are also to be removed, we can delete the firewall.
 			if len(remainingCidrs) == 0 {
 				// Delete a firewall.
 				// TODO(ericsnow) Handle case where firewall does not exist.
-				if err := gce.raw.RemoveFirewall(gce.projectID, existingFirewallName); err != nil {
+				if err := gce.raw.RemoveFirewall(gce.projectID, existingFirewall.Name); err != nil {
 					return errors.Annotatef(err, "closing port(s) %+v", rules)
 				}
 				continue
 			}
 
 			// Update the existing firewall with the remaining CIDRs.
-			portsByProtocol = currentFirewallRules[existingFirewallName]
-			spec := firewallSpec(name, target, remainingCidrs, portsByProtocol)
-			if err := gce.raw.UpdateFirewall(gce.projectID, existingFirewallName, spec); err != nil {
+			spec := firewallSpec(existingFirewall.Name, target, remainingCidrs, existingFirewall.AllowedPorts)
+			if err := gce.raw.UpdateFirewall(gce.projectID, existingFirewall.Name, spec); err != nil {
 				return errors.Annotatef(err, "closing port(s) %+v", rules)
 			}
 			continue
 		}
 
-		existingFirewallName, sourceCIDRMatch := matchSourceCIDRs(currentFirewallRules, currentRuleSet, inputCidrs)
+		existingFirewall, sourceCIDRMatch := currentRuleSet.matchSourceCIDRs(inputFirewall.SourceCIDRs)
 		if !sourceCIDRMatch {
 			// We already know ports don't match, so if CIDRs don't match either, we either
 			// have a partial match or no match.
@@ -215,12 +178,11 @@ func (gce Connection) ClosePorts(target string, rules ...network.IngressRule) er
 		}
 
 		// Delete the ports to close.
-		existingFirewallRules := currentFirewallRules[existingFirewallName]
-		portsByProtocol = existingFirewallRules.remove(portsByProtocol)
+		remainingPorts := existingFirewall.AllowedPorts.remove(inputFirewall.AllowedPorts)
 
 		// Copy new firewall details into required firewall spec.
-		spec := firewallSpec(name, target, inputCidrs, portsByProtocol)
-		if err := gce.raw.UpdateFirewall(gce.projectID, existingFirewallName, spec); err != nil {
+		spec := firewallSpec(existingFirewall.Name, target, existingFirewall.SourceCIDRs, remainingPorts)
+		if err := gce.raw.UpdateFirewall(gce.projectID, existingFirewall.Name, spec); err != nil {
 			return errors.Annotatef(err, "closing port(s) %+v", rules)
 		}
 	}
--- provider/gce/google/ruleset.go
+++ provider/gce/google/ruleset.go
@@ -6,43 +6,138 @@ package google
 import (
 	"crypto/sha256"
 	"fmt"
+	"math/rand"
 	"sort"
 	"strings"
+
+	"github.com/juju/errors"
+	"google.golang.org/api/compute/v1"
 
 	"github.com/juju/juju/network"
 )
 
-// RuleSet is used to manipulate port ranges
-// for a collection of IngressRules.
-type RuleSet struct {
-	rules []network.IngressRule
+// RuleSet is used to manipulate port ranges for a collection of
+// IngressRules. Each key is the identifier for a set of source CIDRs
+// that are allowed for a set of port ranges.
+type RuleSet map[string]*Firewall
 
-	// firewallCidrs is filled in each time the
-	// firewall rules are determined from
-	// the ingress rules.
-	firewallCidrs map[string][]string
-}
-
-func newRuleSet(rules ...network.IngressRule) RuleSet {
-	var result RuleSet
-	result.rules = make([]network.IngressRule, len(rules))
-	result.firewallCidrs = make(map[string][]string)
-	copy(result.rules, rules)
+func NewRuleSetFromRules(rules ...network.IngressRule) RuleSet {
+	result := make(RuleSet)
+	for _, rule := range rules {
+		result.addRule(rule)
+	}
 	return result
 }
 
-func (rs RuleSet) getCIDRs(name string) []string {
-	return rs.firewallCidrs[name]
+func (rs RuleSet) addRule(rule network.IngressRule) {
+	key := sourcecidrs(rule.SourceCIDRs).key()
+	fw, ok := rs[key]
+	if !ok {
+		fw = &Firewall{
+			SourceCIDRs:  rule.SourceCIDRs,
+			AllowedPorts: make(protocolPorts),
+		}
+		rs[key] = fw
+	}
+	ports := fw.AllowedPorts
+	ports[rule.Protocol] = append(ports[rule.Protocol], rule.PortRange)
 }
 
-// sourcecidrs is used to calculate a unique firewall
-// name suffix for a collection of cidrs.
+func NewRulesetFromFirewalls(firewalls ...*compute.Firewall) (RuleSet, error) {
+	result := make(RuleSet)
+	for _, firewall := range firewalls {
+		err := result.addFirewall(firewall)
+		if err != nil {
+			return result, errors.Trace(err)
+		}
+	}
+	return result, nil
+}
+
+func (rs RuleSet) addFirewall(firewall *compute.Firewall) error {
+	key := sourcecidrs(firewall.SourceRanges).key()
+	if len(firewall.TargetTags) != 1 {
+		return errors.Errorf(
+			"firewall rule %q has %d targets (expected 1): %#v",
+			firewall.Name,
+			len(firewall.TargetTags),
+			firewall.TargetTags,
+		)
+	}
+	sourceRanges := firewall.SourceRanges
+	if len(sourceRanges) == 0 {
+		sourceRanges = []string{"0.0.0.0/0"}
+	}
+	result := &Firewall{
+		Name:         firewall.Name,
+		Target:       firewall.TargetTags[0],
+		SourceCIDRs:  sourceRanges,
+		AllowedPorts: make(protocolPorts),
+	}
+	for _, allowed := range firewall.Allowed {
+		ranges := make([]network.PortRange, len(allowed.Ports))
+		for i, rangeStr := range allowed.Ports {
+			portRange, err := network.ParsePortRange(rangeStr)
+			if err != nil {
+				return errors.Trace(err)
+			}
+			portRange.Protocol = allowed.IPProtocol
+			ranges[i] = portRange
+		}
+		result.AllowedPorts[allowed.IPProtocol] = network.CombinePortRanges(ranges...)
+	}
+	if other, ok := rs[key]; ok {
+		return errors.Errorf(
+			"duplicate firewall rules found matching CIDRs %#v: %q and %q",
+			firewall.SourceRanges,
+			firewall.Name,
+			other.Name,
+		)
+	}
+	rs[key] = result
+	return nil
+}
+
+func (rs RuleSet) matchProtocolPorts(ports protocolPorts) (*Firewall, bool) {
+	for _, firewall := range rs {
+		if firewall.AllowedPorts.String() == ports.String() {
+			return firewall, true
+		}
+	}
+	return nil, false
+}
+
+func (rs RuleSet) matchSourceCIDRs(cidrs []string) (*Firewall, bool) {
+	result, ok := rs[sourcecidrs(cidrs).key()]
+	return result, ok
+}
+
+// ToIngressRules converts this set of firewall rules to the ingress
+// rules used elsewhere in Juju. This conversion throws away the rule
+// name information, so these ingress rules can't be directly related
+// back to the firewall rules they came from (except by matching
+// source CIDRs and ports).
+func (rs RuleSet) toIngressRules() ([]network.IngressRule, error) {
+	var results []network.IngressRule
+	for _, firewall := range rs {
+		for _, portRanges := range firewall.AllowedPorts {
+			for _, p := range portRanges {
+				rule, err := network.NewIngressRule(p.Protocol, p.FromPort, p.ToPort, firewall.SourceCIDRs...)
+				if err != nil {
+					return nil, errors.Trace(err)
+				}
+				results = append(results, rule)
+			}
+		}
+	}
+	return results, nil
+}
+
+// sourcecidrs is used to calculate a unique key for a collection of
+// cidrs.
 type sourcecidrs []string
 
-func (s sourcecidrs) nameSuffix() string {
-	if len(s) == 0 || len(s) == 1 && s[0] == "0.0.0.0/0" {
-		return ""
-	}
+func (s sourcecidrs) key() string {
 	src := strings.Join(s, ",")
 	hash := sha256.New()
 	hash.Write([]byte(src))
@@ -50,41 +145,32 @@ func (s sourcecidrs) nameSuffix() string {
 	return hashStr[:6]
 }
 
+// Firewall represents a GCE firewall - if it was constructed from a
+// set of ingress rules the name and target information won't be
+// populated.
+type Firewall struct {
+	Name         string
+	Target       string
+	SourceCIDRs  []string
+	AllowedPorts protocolPorts
+}
+
 // protocolPorts maps a protocol eg "tcp" to a collection of
 // port ranges for that protocol.
 type protocolPorts map[string][]network.PortRange
 
-// getFirewallRules returns a map  of "firewallname" to ports to open
-// fot that firewall, based on the ingress rules in the ruleset.
-func (rs RuleSet) getFirewallRules(namePrefix string) map[string]protocolPorts {
-	result := make(map[string]protocolPorts)
-	for _, rule := range rs.rules {
-
-		// We make a unique firewall name based on a has of the CIDRs.
-		// For backwards compatibility, open rules for "0.0.0.0/0"
-		// do not use any hash in the name.
-		fwname := namePrefix
-		suffix := sourcecidrs(rule.SourceCIDRs).nameSuffix()
-		if suffix != "" {
-			fwname = fwname + "-" + suffix
-		}
-		cidrs := rule.SourceCIDRs
-		if len(cidrs) == 0 {
-			cidrs = []string{"0.0.0.0/0"}
-		}
-		rs.firewallCidrs[fwname] = cidrs
-
-		// Add the current port ranges to the firewall name.
-		protocolPortsForName, ok := result[fwname]
-		if !ok {
-			protocolPortsForName = make(protocolPorts)
-			result[fwname] = protocolPortsForName
-		}
-		portsForProtocol := protocolPortsForName[rule.Protocol]
-		portsForProtocol = append(portsForProtocol, rule.PortRange)
-		protocolPortsForName[rule.Protocol] = portsForProtocol
+func randomSuffix(f *Firewall) (string, error) {
+	// For backwards compatibility, open rules for "0.0.0.0/0"
+	// do not use any suffix in the name.
+	if len(f.SourceCIDRs) == 0 || len(f.SourceCIDRs) == 1 && f.SourceCIDRs[0] == "0.0.0.0/0" {
+		return "", nil
 	}
-	return result
+	data := make([]byte, 4)
+	_, err := rand.Read(data)
+	if err != nil {
+		return "", errors.Trace(err)
+	}
+	return fmt.Sprintf("-%x", data), nil
 }
 
 func (pp protocolPorts) String() string {
