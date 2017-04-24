0d749c7c021250b7a41b7c03d7e0b8ff6389cce6
Nicholas Skaggs <nicholas.skaggs@canonical.com>
2017-04-24 17:37:20 -0400

    Remove hg and bzr dependency check

--- check-source-depends.bash
+++ check-source-depends.bash
@@ -1,12 +1,10 @@
 check_deps() {
     echo "Phase 0: Checking requirements."
     has_deps=1
-    which bzr || has_deps=0
     which git || has_deps=0
-    which hg || has_deps=0
     which go || has_deps=0
     if [[ $has_deps == 0 ]]; then
-        echo "Install bzr, hg, git, and golang."
+        echo "Install git and golang."
         exit 2
     fi
 }
