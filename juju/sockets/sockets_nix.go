145a1d3048843004590baaf0417547bac90662d9
Ian Booth <ian.booth@canonical.com>
2017-05-25 09:07:23 +1000

    Security fix for bug 1682411 - create socket path with restricted permissions

--- juju/sockets/sockets_nix.go
+++ juju/sockets/sockets_nix.go
@@ -7,9 +7,11 @@
 package sockets
 
 import (
+	"io/ioutil"
 	"net"
 	"net/rpc"
 	"os"
+	"path/filepath"
 
 	"github.com/juju/errors"
 )
@@ -23,6 +25,32 @@ func Listen(socketPath string) (net.Listener, error) {
 	if err := os.Remove(socketPath); err != nil {
 		logger.Tracef("ignoring error on removing %q: %v", socketPath, err)
 	}
-	listener, err := net.Listen("unix", socketPath)
-	return listener, errors.Trace(err)
+	// We first create the socket in a temporary directory as a subdirectory of
+	// the target dir so we know we can get the permissions correct and still
+	// rename the socket into the correct place.
+	// ioutil.TempDir creates the temporary directory as 0700 so it starts with
+	// the right perms as well.
+	socketDir, socketName := filepath.Split(socketPath)
+	// socketName here is just the prefix for the temporary dir name,
+	// so it won't collide
+	tempdir, err := ioutil.TempDir(socketDir, socketName)
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+	defer os.RemoveAll(tempdir)
+	tempSocketPath := filepath.Join(tempdir, socketName)
+	listener, err := net.Listen("unix", tempSocketPath)
+	if err != nil {
+		logger.Errorf("failed to listen on unix:%s: %v", socketPath, err)
+		return nil, errors.Trace(err)
+	}
+	if err := os.Chmod(tempSocketPath, 0700); err != nil {
+		listener.Close()
+		return nil, errors.Trace(err)
+	}
+	if err := os.Rename(tempSocketPath, socketPath); err != nil {
+		listener.Close()
+		return nil, errors.Trace(err)
+	}
+	return listener, nil
 }
