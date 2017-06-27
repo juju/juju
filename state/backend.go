da9dd38c9ea225b5414bcd7ccbf4f07bef062313
Joel Sing <joel.sing@canonical.com>
2017-06-28 08:39:19 +1000

    state: tidy up modelBackend and provide modelUUID.
    - Move doc comments to the interface, instead of having them on
      the implementation.
    
    - s/ID/id/ for local variables and arguments.
    
    - s/id/ID/ and s/uuid/UUID/ in comments.
    
    - Provide modelUUID as a way of getting the model UUID, needed
      for upcoming changes.

--- state/backend.go
+++ state/backend.go
@@ -12,39 +12,45 @@ import (
 // modelBackend collects together some useful internal state methods for
 // accessing mongo and mapping local and global ids to one another.
 type modelBackend interface {
+	// docID generates a globally unique ID value
+	// where the model UUID is prefixed to the
+	// localID.
 	docID(string) string
+
+	// localID returns the local ID value by stripping
+	// off the model UUID prefix if it is there.
 	localID(string) string
+
+	// strictLocalID returns the local ID value by removing the
+	// model UUID prefix. If there is no prefix matching the
+	// State's model, an error is returned.
 	strictLocalID(string) (string, error)
+
 	db() Database
+	modelUUID() string
 	txnLogWatcher() *watcher.Watcher
 }
 
-// docID generates a globally unique id value
-// where the model uuid is prefixed to the
-// localID.
 func (st *State) docID(localID string) string {
 	return ensureModelUUID(st.ModelUUID(), localID)
 }
 
-// localID returns the local id value by stripping
-// off the model uuid prefix if it is there.
-func (st *State) localID(ID string) string {
-	modelUUID, localID, ok := splitDocID(ID)
+func (st *State) localID(id string) string {
+	modelUUID, localID, ok := splitDocID(id)
 	if !ok || modelUUID != st.ModelUUID() {
-		return ID
+		return id
 	}
 	return localID
 }
 
-// strictLocalID returns the local id value by removing the
-// model UUID prefix.
-//
-// If there is no prefix matching the State's model, an error is
-// returned.
-func (st *State) strictLocalID(ID string) (string, error) {
-	modelUUID, localID, ok := splitDocID(ID)
+func (st *State) strictLocalID(id string) (string, error) {
+	modelUUID, localID, ok := splitDocID(id)
 	if !ok || modelUUID != st.ModelUUID() {
-		return "", errors.Errorf("unexpected id: %#v", ID)
+		return "", errors.Errorf("unexpected id: %#v", id)
 	}
 	return localID, nil
+}
+
+func (st *State) modelUUID() string {
+	return st.ModelUUID()
 }
