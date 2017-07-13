d44d66ea7b738ed62bdf7e9343f9d498485e350d
Burton Swan <burton.swan@canonical.com>
2017-07-13 23:14:10 +1000

    Add stdout check to remove-application with persistent storage unit attached.

--- acceptancetests/assess_persistent_storage.py
+++ acceptancetests/assess_persistent_storage.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python
-"""Testing Juju's persistent storage feature.
-
-   Full Juju CLI will be printed to the stdout to ease the efforts
-   in understanding the code, investigating and reproducing bugs, especially
-   for those who are not familiar with jujupy framework.
-"""
+"""Testing Juju's persistent storage feature."""
 
 from __future__ import print_function
 
@@ -13,6 +8,7 @@ import sys
 import time
 import yaml
 import json
+import ipdb
 import logging
 import argparse
 import subprocess
@@ -33,19 +29,20 @@ from utility import (
 __metaclass__ = type
 log = logging.getLogger("assess_persistent_storage")
 
-def assess_charm_deploy_and_remove_single_storage(client):
-    """This function tests charm deployment and removal with single
-       persistent storage unit.
+def assess_charm_deploy_single_block_and_filesystem_storage(client):
+    """This function tests charm deployment with a single filesystem
+       storage unit and a single persistent block device storage unit.
 
        Steps taken to the test:
        - Deploy dummy-storage charm with a single block storage unit (ebs)
          and a single filesystem storage unit (rootfs).
        - Check charm status once the deployment is done.
-       - Check charm storage list once the deployment is done.
-       - Remove the deployed charm, the output should indicate that the
-         rootfs storage unit will be removed, ebs storage unit will be
-         detached.
-       - Check storage list again to ensure ebs storage units really exists.
+           > Application status should be active.
+       - Check charm storage units once the deployment is done.
+           > Total number of storage units should be 2.
+           > Name of storage units should be in align with charm config.
+           > Properties of storage units should be as defined.
+               - Storage Type, Persistent Setting and Pool.
 
        :param client: ModelClient object to deploy the charm on.
     """
@@ -56,35 +53,28 @@ def assess_charm_deploy_and_remove_single_storage(client):
     log.info(
         '{} is going to be deployed with 1 ebs block storage unit and '\
         '1 rootfs filesystem storage unit.'.format(charm_name))
-    log.info(
-        'juju deploy ${JUJU_REPOSITORY}/charms/dummy-storage '\
-        '--storage single-blk=ebs --storage single-fs=rootfs')
-    # client.deploy() doesn't support repeat parameters,
-    # although juju deploy supports it
-    #client.deploy(
-    #    charm_path,
-    #    storage='single-blk=ebs',
-    #    storage='single-fs=rootfs')
+
+    # Run juju deploy dummy-storage --storage single-blk=ebs --storage single-fs=rootfs
     client.get_juju_output(
-        'deploy', '/home/noname/work/Canonical/workbench/repository/charms/dummy-storage',
+        'deploy', charm_path,
         '--storage', 'single-blk=ebs',
         '--storage', 'single-fs=rootfs',
         include_e=False)
     client.wait_for_started()
     client.wait_for_workloads()
 
-    log.info('juju status --format json')
+    # Run juju status --format json
     status_output = json.loads(
         client.get_juju_output('status', '--format', 'json', include_e=False))
     app_status = status_output['applications'][charm_name]['application-status']['current']
 
     if app_status != 'active':
-        log.error(
-            'Charm {} is not functioning properly, test terminated.'.format(
-            charm_name))
-        sys.exit(1)
+        raise JujuAssertionError(
+            'App status is incorrect. '\
+            'Found: {}\nExpected: {}\ntest terminated.'.format(
+            app_status, 'active'))
     else:
-        log.info('juju storage --format json')
+        # Run juju storage --format json
         storage_output = json.loads(
             client.get_juju_output('storage', '--format', 'json', include_e=False))
 
@@ -120,28 +110,119 @@ def assess_charm_deploy_and_remove_single_storage(client):
                 log.error(
                     'Name mismatch on Single block device storage, test terminated.')
                 sys.exit(1)
+            else:
+                log.info('Check name and total number of storage unit: PASSED.')
 
-        # check type, persistent setting and volume of single block storage unit
+        # check type, persistent setting and pool of single block storage unit
         storage_type = storage_output['storage'][single_blk_id]['kind']
         persistent_setting = storage_output['storage'][single_blk_id]['persistent']
         pool_storage = storage_output['volumes']['0']['storage']
         pool_setting = storage_output['volumes']['0']['pool']
+
         if storage_type != 'block':
             log.error(
-                'Incorrect type for single block device storage detected \
-                - {}, test terminated.'.format(storage_type))
+                'Incorrect type for single block device storage detected '\
+                '- {}, test terminated.'.format(storage_type))
             sys.exit(1)
-        elif persistent_setting != 'true':
+        # Be careful, the type of persistent_setting's value is boolean.
+        # Using != to clearly highlight it here.
+        elif persistent_setting != True:
             log.error(
-                'Incorrect value for persistent setting \
-                - {}, test terminated.'.format(persistent_setting))
+                'Incorrect value for persistent setting on single block device '\
+                'storage - {}, test terminated.'.format(persistent_setting))
             sys.exit(1)
         elif (pool_storage != single_blk_id) and (pool_setting != 'ebs'):
-            log.error('Incorrect volumes detected \
-            - {} with {}, test terminated.'.format(
+            log.error('Incorrect volumes detected '\
+            '- {} with {}, test terminated.'.format(
             pool_storage, pool_setting))
             sys.exit(1)
+        else:
+            log.info('Check properties of single block device storage unit: PASSED.')
 
+        # check type, persistent setting and pool of single filesystem storage unit
+        storage_type = storage_output['storage'][single_fs_id]['kind']
+        persistent_setting = storage_output['storage'][single_fs_id]['persistent']
+        pool_storage = storage_output['filesystems']['0/0']['storage']
+        pool_setting = storage_output['filesystems']['0/0']['pool']
+
+        if storage_type != 'filesystem':
+            log.error(
+                'Incorrect type for single filesystem storage detected '\
+                '- {}, test terminated.'.format(storage_type))
+            sys.exit(1)
+        elif persistent_setting != False:
+            log.error(
+                'Incorrect value for persistent setting on single filesystem '\
+                'storage - {}, test terminated.'.format(persistent_setting))
+            sys.exit(1)
+        elif (pool_storage != single_fs_id) and (pool_setting != 'rootfs'):
+            log.error('Incorrect filesystems detected '\
+            '- {} with {}, test terminated.'.format(
+            pool_storage, pool_setting))
+            sys.exit(1)
+        else:
+            log.info('Check properties of single filesystem storage unit: PASSED.')
+    return (single_fs_id, single_blk_id)
+
+
+def assess_charm_removal_single_block_and_filesystem_storage(client):
+    """This function tests charm removal while a single filesystem storage
+       and a single persistent block device storage attached.
+
+       Steps taken to the test:
+       - Run assess_charm_deploy_single_block_and_filesystem_storage(client)
+       - Remove dummy-storage charm while a single block storage unit (ebs)
+         and a single filesystem storage unit (rootfs) attached.
+           > The output should states that there is a persistent storage unit.
+           > The application should be removed successfully.
+       - Check charm storage units once the removal is done.
+           > The filesystem storage unit (rootfs)should be removed successfully.
+           > The block device storage unit (ebs) should remain and detached,
+
+       :param client: ModelClient object to deploy the charm on.
+    """
+
+    charm_name = 'dummy-storage'
+    single_fs_id, single_blk_id = \
+        assess_charm_deploy_single_block_and_filesystem_storage(client)
+    # Run juju remove-application dummy-storage
+    app_removal_output = client.get_juju_output(
+        'remove-application', charm_name, '--show-log', include_e=False, merge_stderr=True)
+    ipdb.set_trace()
+    # pre-set the result to FAILED
+    remove_app_output_check = 'FAILED'
+    remove_single_fs_output_check = 'FAILED'
+    detach_single_blk_output_check = 'FAILED'
+
+    for line in app_removal_output.split('\n'):
+        if line.find('will remove unit {}'.format(charm_name)) != -1:
+            log.info(line)
+            remove_app_output_check = 'PASSED'
+        elif line.find('will remove storage {}'.format(single_fs_id)) != -1:
+            log.info(line)
+            remove_single_fs_output_check = 'PASSED'
+        elif line.find('will detach storage {}'.format(single_blk_id)) != -1:
+            log.info(line)
+            detach_single_blk_output_check = 'PASSED'
+
+    if remove_app_output_check != 'PASSED':
+        raise JujuAssertionError(
+            'Missing application name in remove-application stdout.')
+    else:
+        log.info('Remove Application output check: {}'.format(
+            remove_app_output_check))
+    if remove_single_fs_output_check != 'PASSED':
+        raise JujuAssertionError(
+            'Missing single filesystem id in remove-application stdout.')
+    else:
+        log.info('Remove single filesystem storage output check: {}'.format(
+            remove_single_fs_output_check))
+    if detach_single_blk_output_check != 'PASSED':
+        raise JujuAssertionError(
+            'Missing single block device id in remove-application stdout.')
+    else:
+        log.info('Detach single block device storage output check: {}'.format(
+            detach_single_blk_output_check))
 
 
 def assess_storage_remove_single_storage(client, storage_type):
@@ -278,18 +359,16 @@ def parse_args(argv):
 
 
 def assess_persistent_storage(client):
-    # Based on the test spec, persistent storage need to be tested on both
-    # LXD and AWS and ebs volume is unavailable on LXD, a switcher is
-    # required here to decide which test should be run.
+    # PR7635 landed later today, brings in persistent storage support on lxd
+    # The storage type for persistent storage on lxd is 'lxd'
+    # https://github.com/juju/juju/pull/7635
+
     environment = os.getenv('ENV', default='parallel-aws')
     if environment == 'parallel-lxd':
-        assess_storage_remove_single_storage(
-            client, storage_type='single-fs=rootfs')
-    elif environment == 'parallel-aws':
-        assess_charm_deploy_and_remove_single_storage(client)
-        #assess_storage_remove_single_storage(
-        #    client, storage_type='single-fs=ebs,10G')
-    #assess_storage_remove_multi_fs(client)
+        log.error('TODO: Add persistent storage test on lxd.')
+        sys.exit(1)
+    else:
+        assess_charm_removal_single_block_and_filesystem_storage(client)
 
 
 def main(argv=None):
@@ -297,11 +376,9 @@ def main(argv=None):
     configure_logging(args.verbose)
     bs_manager = BootstrapManager.from_args(args)
     with bs_manager.booted_context(args.upload_tools):
-        #assess_persistent_storage(bs_manager.client)
-        assess_charm_deploy_and_remove_single_storage(bs_manager.client)
+        assess_persistent_storage(bs_manager.client)
     return 0
 
 
 if __name__ == '__main__':
     sys.exit(main())
-
