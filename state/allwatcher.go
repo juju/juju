23f8b9e2f1622f3f3d025f55548f18460c6bcaf7
Menno Smits <menno.smits@canonical.com>
2017-07-28 13:22:09 +1200

    state: allwatcher now reflects instance status changes
    Due to a silly bug (and missing test coverage), instance status
    changes were never reflected in the output from the allwatcher.
    
    Fixes https://bugs.launchpad.net/juju/+bug/1695335

--- state/allwatcher.go
+++ state/allwatcher.go
@@ -1138,6 +1138,7 @@ func backingEntityIdForGlobalKey(modelUUID, key string) (multiwatcher.EntityId, 
 	id := key[2:]
 	switch key[0] {
 	case 'm':
+		id = strings.TrimSuffix(id, "#instance")
 		return (&multiwatcher.MachineInfo{
 			ModelUUID: modelUUID,
 			Id:        id,
