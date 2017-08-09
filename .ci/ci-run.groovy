b86c268ddd02170f49bc1b7fd2417ea7a68d6430
Christopher Lee <veebers@gmail.com>
2017-08-09 14:04:15 +1200

    Initial dry run.

---
+++ .ci/ci-run.groovy
@@ -0,0 +1,35 @@
+// Pipeline configuration for a Juju CI Run
+// Intended to be run when a feature branch is merged.
+// Builds binaries and runs functional (and unit) tests.
+
+node('juju-core-slave') {
+    // setup working directory and script dir paths (for after scm checkout.)
+    work_dir = "${pwd(tmp: true)}/${env.BUILD_NUMBER}"
+    // juju_build_dir = "${work_dir}/build/"
+    // juju_checkout_dir = "$build_base_dir/src/github.com/juju/juju"
+
+    stage('Build') {
+        def arch = "amd64"  // as we'll parallise this stuff.
+        parallel(
+            'amd64': {
+                node('build-slave') {
+                    sh "snap install go --classic && apt-get install snapcraft -y"
+                    dir(${pwd(tmp: true)}) {
+                        checkout scm
+                        withEnv(['PATH+SNAP=/snap/bin']) {
+                            sh "go version"
+                            sh "snapcraft build && snapcraft prime"
+                            // Also, artifact the snap itself
+                            stash includes: "./prime/bin/juju*", name: "${arch}-binaries"
+                        }
+                    }
+                }
+            },
+        )
+    }
+
+    stage('Test') {
+        unstash 'amd64-binaries'
+        print(sh(script: "ls -larth", returnStdout: true))
+    }
+}
