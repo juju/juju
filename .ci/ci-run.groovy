85e43c1b6c74dd7d86904f904bb0bad72eab9cfa
Christopher Lee <veebers@gmail.com>
2017-08-09 16:33:17 +1200

    Attempt to clean up, use function and re-use generated binaries.

--- .ci/ci-run.groovy
+++ .ci/ci-run.groovy
@@ -7,33 +7,113 @@ node('juju-core-slave') {
     work_dir = "${pwd(tmp: true)}/${env.BUILD_NUMBER}"
     // juju_build_dir = "${work_dir}/build/"
     // juju_checkout_dir = "$build_base_dir/src/github.com/juju/juju"
+    amd64_juju_data = "${pwd(tmp: true)}/amd64-juju"
+
+    def build_binaries(arch) {
+        def binary_stash_name = "${arch}-binaries"
+        sh("""
+        sudo snap install go --classic \
+        && sudo apt-get install snapcraft -y
+        """)
+
+        dir("${pwd(tmp: true)}") {
+            checkout scm
+            withEnv(['PATH+SNAP=/snap/bin']) {
+                sh "go version"
+                sh "snapcraft build && snapcraft prime"
+                // Also, artifact the snap itself
+                dir('./prime/bin/') {
+                    stash includes: "**/prime/bin/juju*", name: "${binary_stash_name}"
+                }
+            }
+        }
+
+        return binary_stash_name
+    }
 
     stage('Build') {
-        def arch = "amd64"  // as we'll parallise this stuff.
         parallel(
             'amd64': {
                 node('build-slave') {
-                    sh("""
-                    sudo snap install go --classic \
-                    && sudo apt-get install snapcraft -y
-                    """)
+                    // sh("""
+                    // sudo snap install go --classic \
+                    // && sudo apt-get install snapcraft -y
+                    // """)
 
-                    dir(${pwd(tmp: true)}) {
-                        checkout scm
-                        withEnv(['PATH+SNAP=/snap/bin']) {
-                            sh "go version"
-                            sh "snapcraft build && snapcraft prime"
-                            // Also, artifact the snap itself
-                            stash includes: "./prime/bin/juju*", name: "${arch}-binaries"
-                        }
-                    }
+                    // dir("${pwd(tmp: true)}") {
+                    //     checkout scm
+                    //     withEnv(['PATH+SNAP=/snap/bin']) {
+                    //         sh "go version"
+                    //         sh "snapcraft build && snapcraft prime"
+                    //         // Also, artifact the snap itself
+                    //         dir('./prime/bin/') {
+                    //             stash includes: "**/prime/bin/juju*", name: "${arch}-binaries"
+                    //         }
+                    //     }
+                    // }
+                    binary_stash_name = build_binaries('amd64')
                 }
             },
         )
     }
 
     stage('Test') {
-        unstash 'amd64-binaries'
-        print(sh(script: "ls -larth", returnStdout: true))
+        parallel(
+            'amd64': {
+                binary_path = pwd(tmp: true)
+                dir(binary_path) {
+                    unstash 'amd64-binaries'
+                    parallel(
+                        'log-rotation': {
+                            juju_data = pwd(tmp:true)
+                            dir(juju_data) {
+                                sh "mkdir _logs"
+                                withEnv(['JUJU_DATA=${juju_data}']) {
+                                    sh "${binary_path}/juju version"
+                                    // scripts/blah logs/ ...
+                                    // artifact logs/ ...
+                                }
+                            }
+                        },
+                        'model-migration': {
+                            juju_data = pwd(tmp:true)
+                            dir(juju_data) {
+                                sh "mkdir _logs"
+                                withEnv(['JUJU_DATA=${juju_data}']) {
+                                    sh "${binary_path}/juju version"
+                                    // scripts/blah logs/ ...
+                                    // artifact logs/ ...
+                                }
+                            }
+                        }
+                    )
+                } // dir binary path.
+            }
+        )
     }
 }
+
+// // Get binaries
+// // Bootstrap a controller with a known name for many clouds
+// //   using --existing.
+// // Can you parallel within a parallel?
+// cwd = pwd(tmp: true)
+// dir(cwd) {
+//     unstash 'amd64-binaries'
+//     dir(amd64_juju_data) {
+//         parallel(
+//             'lxd': {
+//                 withEnv(['JUJU_DATA=${amd64_juju_data']) {
+//                     // This would be a bootstrap . . .
+//                     sh(script: "${cwd}/juju version")
+//                 }
+//             },
+//             'aws': {
+//                 withEnv(['JUJU_DATA=${amd64_juju_data']) {
+//                     // This would be a bootstrap . . .
+//                     sh(script: "${cwd}/juju version")
+//                 }
+//             }
+//         )
+//     }
+// }
