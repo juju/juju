558b7720ecbe70eca7891b2503d0eead40b4232a
Christopher Lee <veebers@gmail.com>
2017-08-14 14:55:11 +1200

    Have some archives and runs working now. Still rough.

--- .ci/ci-run.groovy
+++ .ci/ci-run.groovy
@@ -5,68 +5,127 @@
 node('juju-core-slave') {
     // setup working directory and script dir paths (for after scm checkout.)
     work_dir = "${pwd(tmp: true)}/${env.BUILD_NUMBER}"
-    // juju_build_dir = "${work_dir}/build/"
-    // juju_checkout_dir = "$build_base_dir/src/github.com/juju/juju"
     amd64_juju_data = "${pwd(tmp: true)}/amd64-juju"
 
     stage('Build') {
+        checkout scm
+        stash includes: "acceptancetests/**/*", name: "acceptancetests"
+        // A re-run of the binary build should be fine as the stuff won't have
+        // been cleaned up? Or should we clean up properly etc.
         parallel(
             'amd64': {
                 node('build-slave') {
-                    // sh("""
-                    // sudo snap install go --classic \
-                    // && sudo apt-get install snapcraft -y
-                    // """)
-
-                    // dir("${pwd(tmp: true)}") {
-                    //     checkout scm
-                    //     withEnv(['PATH+SNAP=/snap/bin']) {
-                    //         sh "go version"
-                    //         sh "snapcraft build && snapcraft prime"
-                    //         // Also, artifact the snap itself
-                    //         dir('./prime/bin/') {
-                    //             stash includes: "**/prime/bin/juju*", name: "${arch}-binaries"
-                    //         }
-                    //     }
-                    // }
                     binary_stash_name = build_binaries('amd64')
                 }
             },
+            // 's390x': {
+            //     node('s390x-slave') {
+            //         binary_stash_name = build_binaries('s390x')
+            //     }
+            // }
         )
     }
 
     stage('Test') {
         parallel(
             'amd64': {
-                binary_path = pwd(tmp: true)
-                dir(binary_path) {
-                    unstash 'amd64-binaries'
-                    parallel(
-                        'log-rotation': {
-                            juju_data = pwd(tmp:true)
+
+                parallel(
+                    'log-rotation': {
+                        node('feature-slave-a') {
+                            working_path = "${pwd()}/amd64"
+                            bin_path = "${working_path}/bin"
+                            logs_path = "${working_path}/logs/log-rotation"
+                            juju_data_base = "${working_path}/juju_data"
+                            scripts_path = "${working_path}/acceptancetests"
+                            cloud_city = "${working_path}/cloud-city"
+                            env='parallel-lxd'
+                            job_name='log-rotation-ci-run'
+
+                            dir(working_path) {
+                                unstash 'acceptancetests'
+                                // safe create some directories
+                                dir(bin_path) {}
+                                dir(logs_path) {
+                                    // This is because workspaces are re-used?
+                                    // Do they survive a log archiving?
+                                    sh "rm -fr ./*"
+                                }
+
+                                get_cloud_city()
+                            }
+
+                            dir(bin_path) {
+                                unstash 'amd64-binaries'
+                            }
+
+                            juju_data = "${juju_data_base}/log-rotation"
                             dir(juju_data) {
-                                sh "mkdir _logs"
                                 withEnv(['JUJU_DATA=${juju_data}']) {
-                                    sh "${binary_path}/juju version"
                                     // scripts/blah logs/ ...
-                                    // artifact logs/ ...
+                                    withEnv(["JUJU_HOME=${cloud_city}", "JUJU_REPOSITORY=${scripts_path}/repository"]) {
+                                        ret_code = sh(
+                                            script: "${scripts_path}/assess_log_rotation.py ${env} ${bin_path}/juju ${logs_path} ${job_name} machine --series xenial",
+                                            returnStatus: true)
+
+                                    }
+
+                                    if(ret_code != 0) {
+                                        error "log-rotation test failed."
+                                    }
                                 }
                             }
-                        },
-                        'model-migration': {
-                            juju_data = pwd(tmp:true)
-                            dir(juju_data) {
-                                sh "mkdir _logs"
-                                withEnv(['JUJU_DATA=${juju_data}']) {
-                                    sh "${binary_path}/juju version"
-                                    // scripts/blah logs/ ...
-                                    // artifact logs/ ...
-                                }
-                            }
-                        }
-                    )
-                } // dir binary path.
-            }
+                            archiveArtifacts(
+                                artifacts: "amd64/logs/log-rotation/",
+                                fingerprint: true)
+
+                    },
+                    // 'model-migration': {
+                    //         node('feature-slave-b') {
+                    //             working_path = "${pwd()}/amd64"
+                    //             bin_path = "${working_path}/bin"
+                    //             logs_path = "${working_path}/logs"
+                    //             juju_data_base = "${working_path}/juju_data"
+                    //             scripts_path = "${working_path}/acceptancetests"
+
+                    //             dir(working_path) {
+                    //                 unstash 'acceptancetests'
+                    //                 // safe create some directories
+                    //                 dir(bin_path) {}
+                    //                 dir(logs_path) {
+                    //                     sh "rm -fr ./*"
+                    //                 }
+
+                    //                 get_cloud_city()
+                    //             }
+
+                    //             dir(bin_path) {
+                    //                 unstash 'amd64-binaries'
+                    //             }
+
+                    //             juju_data = "${juju_data_base}/model-migration"
+                    //             dir(juju_data) {
+                    //                 withEnv(['JUJU_DATA=${juju_data}']) {
+
+                    //                     sh "${bin_path}/juju version"
+                    //                     // scripts/blah logs/ ...
+                    //                     // artifact logs/ ...
+                    //                 }
+                    //             }
+                    //     }
+                    // }
+                )
+            },// amd64
+            // 's390x': {
+            //     node('s390x-slave') {
+            //         binary_parh = "${pwd()}/juju_binary"
+            //         dir(binary_path) {
+            //             unstash 's390x-binaries'
+            //             sh "${binary_path}/juju version"
+            //         }
+            //     }
+
+            // }
         )
     }
 }
@@ -78,14 +137,15 @@ def build_binaries(arch) {
     && sudo apt-get install snapcraft -y
     """)
 
-    dir("${pwd(tmp: true)}") {
+    //dir("${pwd(tmp: true)}") {
+    dir("${pwd()}/_build") {
         checkout scm
         withEnv(['PATH+SNAP=/snap/bin']) {
             sh "go version"
             sh "snapcraft build && snapcraft prime"
             // Also, artifact the snap itself
             dir('./prime/bin/') {
-                stash includes: "**/prime/bin/juju*", name: "${binary_stash_name}"
+                stash includes: "juju*", name: "${binary_stash_name}"
             }
         }
     }
@@ -93,27 +153,17 @@ def build_binaries(arch) {
     return binary_stash_name
 }
 
-// // Get binaries
-// // Bootstrap a controller with a known name for many clouds
-// //   using --existing.
-// // Can you parallel within a parallel?
-// cwd = pwd(tmp: true)
-// dir(cwd) {
-//     unstash 'amd64-binaries'
-//     dir(amd64_juju_data) {
-//         parallel(
-//             'lxd': {
-//                 withEnv(['JUJU_DATA=${amd64_juju_data']) {
-//                     // This would be a bootstrap . . .
-//                     sh(script: "${cwd}/juju version")
-//                 }
-//             },
-//             'aws': {
-//                 withEnv(['JUJU_DATA=${amd64_juju_data']) {
-//                     // This would be a bootstrap . . .
-//                     sh(script: "${cwd}/juju version")
-//                 }
-//             }
-//         )
-//     }
-// }
+def get_cloud_city() {
+    // Checkout cloud city into the current directory
+    checkout(
+        changelog: false,
+        poll: false,
+        scm: [
+            $class: 'GitSCM',
+            branches: [[name: '*/master']],
+            doGenerateSubmoduleConfigurations: false,
+            extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'cloud-city']],
+            submoduleCfg: [],
+            userRemoteConfigs: [[url: 'git+ssh://juju-qa-bot@git.launchpad.net/~juju-qa/+git/cloud-city']]]
+    )
+}
