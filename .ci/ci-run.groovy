61ef59a9d3b36a25a2b045a4a655d654098b7a3c
Christopher Lee <veebers@gmail.com>
2017-08-15 15:28:48 +1200

    Working example with 2x tests running.

--- .ci/ci-run.groovy
+++ .ci/ci-run.groovy
@@ -8,6 +8,7 @@ node('juju-core-slave') {
     amd64_juju_data = "${pwd(tmp: true)}/amd64-juju"
 
     stage('Build') {
+        // Grab source and save test scripts for later.
         checkout scm
         stash includes: "acceptancetests/**/*", name: "acceptancetests"
         // A re-run of the binary build should be fine as the stuff won't have
@@ -29,91 +30,39 @@ node('juju-core-slave') {
     stage('Test') {
         parallel(
             'amd64': {
+                def working_path = "${pwd()}"
+                def arch = 'amd64'
 
                 parallel(
-                    'log-rotation': {
+                    'log-rotation-machine': {
                         node('feature-slave-a') {
-                            working_path = "${pwd()}/amd64"
-                            bin_path = "${working_path}/bin"
-                            logs_path = "${working_path}/logs/log-rotation"
-                            juju_data_base = "${working_path}/juju_data"
-                            scripts_path = "${working_path}/acceptancetests"
-                            cloud_city = "${working_path}/cloud-city"
-                            env='parallel-lxd'
-                            job_name='log-rotation-ci-run'
-
-                            dir(working_path) {
-                                unstash 'acceptancetests'
-                                // safe create some directories
-                                dir(bin_path) {}
-                                dir(logs_path) {
-                                    // This is because workspaces are re-used?
-                                    // Do they survive a log archiving?
-                                    sh "rm -fr ./*"
-                                }
-
-                                get_cloud_city()
+                            timeout(time: 25, unit: 'MINUTES') {
+                                run_simple_test(
+                                    'log-rotation',
+                                    'parallel-lxd',
+                                    working_path,
+                                    arch,
+                                    'assess_log_rotation.py',
+                                    'machine --series xenial',
+                                )
                             }
-
-                            dir(bin_path) {
-                                unstash 'amd64-binaries'
-                            }
-
-                            juju_data = "${juju_data_base}/log-rotation"
-                            dir(juju_data) {
-                                withEnv(['JUJU_DATA=${juju_data}']) {
-                                    // scripts/blah logs/ ...
-                                    withEnv(["JUJU_HOME=${cloud_city}", "JUJU_REPOSITORY=${scripts_path}/repository"]) {
-                                        ret_code = sh(
-                                            script: "${scripts_path}/assess_log_rotation.py ${env} ${bin_path}/juju ${logs_path} ${job_name} machine --series xenial",
-                                            returnStatus: true)
-
-                                    }
-
-                                    if(ret_code != 0) {
-                                        error "log-rotation test failed."
-                                    }
-                                }
-                            }
-                            archiveArtifacts(
-                                artifacts: "amd64/logs/log-rotation/",
-                                fingerprint: true)
+                        }
 
                     },
-                    // 'model-migration': {
-                    //         node('feature-slave-b') {
-                    //             working_path = "${pwd()}/amd64"
-                    //             bin_path = "${working_path}/bin"
-                    //             logs_path = "${working_path}/logs"
-                    //             juju_data_base = "${working_path}/juju_data"
-                    //             scripts_path = "${working_path}/acceptancetests"
-
-                    //             dir(working_path) {
-                    //                 unstash 'acceptancetests'
-                    //                 // safe create some directories
-                    //                 dir(bin_path) {}
-                    //                 dir(logs_path) {
-                    //                     sh "rm -fr ./*"
-                    //                 }
-
-                    //                 get_cloud_city()
-                    //             }
-
-                    //             dir(bin_path) {
-                    //                 unstash 'amd64-binaries'
-                    //             }
-
-                    //             juju_data = "${juju_data_base}/model-migration"
-                    //             dir(juju_data) {
-                    //                 withEnv(['JUJU_DATA=${juju_data}']) {
-
-                    //                     sh "${bin_path}/juju version"
-                    //                     // scripts/blah logs/ ...
-                    //                     // artifact logs/ ...
-                    //                 }
-                    //             }
-                    //     }
-                    // }
+                    'model-migration': {
+                        node('feature-slave-b') {
+                            timeout(time: 50, unit: 'MINUTES') {
+                                run_simple_test(
+                                    'model-migration',
+                                    'parallel-abc',
+                                    working_path,
+                                    arch,
+                                    'assess_model_migration.py',
+                                    '--timeout 2700',
+                                )
+                            }
+                        }
+                    }
                 )
             },// amd64
             // 's390x': {
@@ -130,7 +79,10 @@ node('juju-core-slave') {
     }
 }
 
+
 def build_binaries(arch) {
+    // Note: this will have issues if multiple runs attempt to run this step
+    // on the same machine.
     def binary_stash_name = "${arch}-binaries"
     sh("""
     sudo snap install go --classic \
@@ -153,6 +105,7 @@ def build_binaries(arch) {
     return binary_stash_name
 }
 
+
 def get_cloud_city() {
     // Checkout cloud city into the current directory
     checkout(
@@ -166,4 +119,82 @@ def get_cloud_city() {
             submoduleCfg: [],
             userRemoteConfigs: [[url: 'git+ssh://juju-qa-bot@git.launchpad.net/~juju-qa/+git/cloud-city']]]
     )
+}
+
+
+def prepare_simple_test(working_path, test_name, arch) {
+    // This is a little gross, we can make it better I'm sure.
+    def bin_path = "${working_path}/${arch}/bin"
+    def juju_data_base = "${working_path}/${arch}/juju_data"
+    def juju_data = "${juju_data_base}/${arch}/${test_name}"
+    def scripts_path = "${working_path}/${arch}/acceptancetests"
+    def cloud_city = "${working_path}/${arch}/cloud-city"
+    def base_logs_path = "${working_path}/${arch}/logs"
+    def logs_collection = "${arch}/logs/${test_name}"
+    def logs_path = "${working_path}/${logs_collection}"
+
+    dir(bin_path) {
+        if(! fileExists('.binary-prepared')) {
+            unstash "${arch}-binaries"
+            sh 'touch .binary-prepared'
+        }
+    }
+
+    dir(juju_data_base) {}
+    dir(working_path) {
+        get_cloud_city()
+
+        if(! fileExists('.acceptancetests-prepared')) {
+            unstash 'acceptancetests'
+            sh 'touch .acceptancetests-prepared'
+        }
+    }
+    dir(logs_path) {
+        // This is unique to the named test (not shared on a node.)
+        sh "rm -fr ./* || true"
+        sh "touch .log-${test_name}"
+    }
+
+    return [
+        bin_path: bin_path,
+        juju_data_base: juju_data_base,
+        juju_data: juju_data,
+        scripts_path: scripts_path,
+        cloud_city: cloud_city,
+        base_logs_path: base_logs_path,
+        logs_path: logs_path,
+        logs_collection: logs_collection,
+    ]
+}
+
+
+def run_simple_test(test_name, env, working_path, arch, test_script, extra_args) {
+    def run_paths = prepare_simple_test(
+        working_path, test_name, arch)
+    def job_name = "${test_name}-ci-run"
+
+    try {
+        dir(run_paths['juju_data']) {
+            withEnv(["JUJU_DATA=${run_paths['juju_data']}"]) {
+                withEnv([
+                    "JUJU_HOME=${run_paths['cloud_city']}",
+                    "JUJU_REPOSITORY=${run_paths['scripts_path']}/repository"
+                ]) {
+                    def ret_code = sh(
+                        script: "${run_paths['scripts_path']}/${test_script} ${env} ${run_paths['bin_path']}/juju ${run_paths['logs_path']} ${job_name} ${extra_args}",
+                        returnStatus: true)
+
+                    if(ret_code != 0) {
+                        error "${test_name} test failed."
+                    }
+
+                }
+            }
+        }
+    } finally {
+        // when this fails it sets currentBuild.result to failed.
+        archiveArtifacts(
+            artifacts: run_paths['logs_collection'],
+            fingerprint: true)
+    }
 }
