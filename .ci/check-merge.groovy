f91c4fe35f17b16571a301d0b8513677a66b997f
Nicholas Skaggs <nicholas.skaggs@canonical.com>
2017-10-13 12:24:32 -0400

    drop ppa from check-merge groovy

--- .ci/check-merge.groovy
+++ .ci/check-merge.groovy
@@ -109,7 +109,7 @@ node('juju-core-slave-b') {
                         withEnv(["JUJU_HOME=${cloud_city}"]){
                             sh("""
                             . $JUJU_HOME/juju-qa.jujuci && . $JUJU_HOME/ec2rc >2 /dev/null && \\
-                            ${scripts_dir}/run-unit-tests c4.4xlarge $xenial_ami --local "$tarfile" --use-tmpfs --use-ppa ppa:juju/golang --force-archive
+                            ${scripts_dir}/run-unit-tests c4.4xlarge $xenial_ami --local "$tarfile" --use-tmpfs --force-archive
                             """)
                         }
                     } catch(e) {
