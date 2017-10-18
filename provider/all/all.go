d1868c4d036a28c272c7d27baa36481061734acb
John Arbash Meinel <john@arbash-meinel.com>
2017-10-18 15:24:23 +0400

    We don't need conditionally compile the LXD provider

--- provider/all/all.go
+++ provider/all/all.go
@@ -10,6 +10,7 @@ import (
 	_ "github.com/juju/juju/provider/ec2"
 	_ "github.com/juju/juju/provider/gce"
 	_ "github.com/juju/juju/provider/joyent"
+	_ "github.com/juju/juju/provider/lxd"
 	_ "github.com/juju/juju/provider/maas"
 	_ "github.com/juju/juju/provider/manual"
 	_ "github.com/juju/juju/provider/openstack"
