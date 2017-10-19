80c249e28396c1bdf01e0fa790da4d2a00376971
anastasia <anastasia.macmood@canonical.com>
2017-10-19 14:48:10 +1000

    Hosted models may error individually and skip.

--- cmd/juju/controller/kill.go
+++ cmd/juju/controller/kill.go
@@ -189,6 +189,7 @@ func (c *killCommand) DirectDestroyRemaining(ctx *cmd.Context, api destroyContro
 			// Only model name is guaranteed to be set in the result
 			// when an error is returned.
 			ctx.Infof("Could not kill %s directly: %v", model.Name, model.Error)
+			continue
 		}
 		ctx.Infof("Killing %s/%s directly", model.Owner.Id(), model.Name)
 		cfg, err := config.New(config.NoDefaults, model.Config)
