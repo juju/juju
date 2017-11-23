ee0d7e6969cfc80aad0579d0a4e29cc14c9a0f4f
Christian Muirhead <christian.muirhead@canonical.com>
2017-11-24 10:01:22 +1300

    Added AuditLogFile (which implements AuditLog)

--- core/auditlog/auditlog.go
+++ core/auditlog/auditlog.go
@@ -4,9 +4,15 @@ package auditlog
 
 import (
 	"encoding/hex"
+	"io"
 	"math/rand"
+	"os"
+	"path/filepath"
 
 	"github.com/juju/errors"
+	"github.com/juju/utils"
+	"gopkg.in/natefinch/lumberjack.v2"
+	"gopkg.in/yaml.v2"
 )
 
 // Call represents a high-level juju command from the juju client (or
@@ -47,6 +53,11 @@ type MethodArgs struct {
 	Method  string
 	Version int
 	Args    string
+}
+
+type Record struct {
+	Call   Call         `yaml:"call"`
+	Method FacadeMethod `yaml:"method"`
 }
 
 // AuditLog represents something that can store calls and methods
@@ -95,4 +106,70 @@ func newConnectionID() string {
 	buf := make([]byte, 8)
 	rand.Read(buf) // Can't fail
 	return hex.EncodeToString(buf)
+}
+
+type AuditLogFile struct {
+	fileLogger io.WriteCloser
+}
+
+// NewLogFileSink returns an audit entry sink which writes
+// to an audit.log file in the specified directory.
+func NewLogFile(logDir string) *AuditLogFile {
+	logPath := filepath.Join(logDir, "audit-log.yaml")
+	if err := primeLogFile(logPath); err != nil {
+		// This isn't a fatal error so log and continue if priming
+		// fails.
+		logger.Errorf("Unable to prime %s (proceeding anyway): %v", logPath, err)
+	}
+
+	return &AuditLogFile{
+		fileLogger: &lumberjack.Logger{
+			Filename:   logPath,
+			MaxSize:    300, // MB
+			MaxBackups: 10,
+			Compress:   true,
+		},
+	}
+}
+
+func (a *AuditLogFile) AddCall(c Call) error {
+	return errors.Trace(a.addRecord(Record{Call: c}))
+}
+
+func (a *AuditLogFile) AddMethod(m FacadeMethod) error {
+	return errors.Trace(a.addRecord(Record{Method: m}))
+}
+
+func (a *AuditLogFile) Close() error {
+	return errors.Trace(a.fileLogger.Close())
+}
+
+const documentStart = "---\n"
+
+func (a *AuditLogFile) addRecord(r Record) error {
+	bytes, err := yaml.Marshal(r)
+	if err != nil {
+		return errors.Trace(err)
+	}
+	// Combining the start and document together in one write to
+	// prevent lumberjack from rolling the file between them.
+	withStart := make([]byte, 0, len(documentStart)+len(bytes))
+	withStart = append(withStart, []byte(documentStart)...)
+	withStart = append(withStart, bytes...)
+	_, err = a.fileLogger.Write(withStart)
+	return errors.Trace(err)
+}
+
+// primeLogFile ensures the logsink log file is created with the
+// correct mode and ownership.
+func primeLogFile(path string) error {
+	f, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY, 0600)
+	if err != nil {
+		return errors.Trace(err)
+	}
+	if err := f.Close(); err != nil {
+		return errors.Trace(err)
+	}
+	err = utils.ChownPath(path, "syslog")
+	return errors.Trace(err)
 }
