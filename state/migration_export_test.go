fa8c7434c04d7b3eb4d55a350e92c4c7ec259f84
Ian Booth <ian.booth@canonical.com>
2017-12-15 11:05:51 +1000

    Add application type to export

--- state/migration_export_test.go
+++ state/migration_export_test.go
@@ -407,6 +407,7 @@ func (s *MigrationExportSuite) assertMigrateApplications(c *gc.C, cons constrain
 	exported := applications[0]
 	c.Assert(exported.Name(), gc.Equals, application.Name())
 	c.Assert(exported.Tag(), gc.Equals, application.ApplicationTag())
+	c.Assert(exported.Type(), gc.Equals, string(s.Model.Type()))
 	c.Assert(exported.Series(), gc.Equals, application.Series())
 	c.Assert(exported.Annotations(), jc.DeepEquals, testAnnotations)
 
