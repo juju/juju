2ab470c701060f84d84dffb4eb7ab56b6e3c0786
Ian Booth <ian.booth@canonical.com>
2017-12-15 13:56:24 +1000

    Add support for migrating applications with passwords

--- state/migration_internal_test.go
+++ state/migration_internal_test.go
@@ -366,8 +366,6 @@ func (s *MigrationSuite) TestApplicationDocFields(c *gc.C) {
 		// RelationCount is handled by the number of times the application name
 		// appears in relation endpoints.
 		"RelationCount",
-		// TODO(caas) - add to export/import
-		"PasswordHash",
 	)
 	migrated := set.NewStrings(
 		"Name",
@@ -380,6 +378,7 @@ func (s *MigrationSuite) TestApplicationDocFields(c *gc.C) {
 		"Exposed",
 		"MinUnits",
 		"MetricCredentials",
+		"PasswordHash",
 	)
 	s.AssertExportedFields(c, applicationDoc{}, migrated.Union(ignored))
 }
