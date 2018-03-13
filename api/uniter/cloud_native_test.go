6da9307899a65e803ba685e1642d7c36c8b5c8e8
Joseph Phillips <joe.phillips@canonical.com>
2018-03-13 12:07:07 +0100

    Adds API client method for cloud spec retrieval. Includes some formatting cleanup and redundant test removal.

---
+++ api/uniter/cloud_native_test.go
@@ -0,0 +1,56 @@
+package uniter_test
+
+import (
+	jc "github.com/juju/testing/checkers"
+	gc "gopkg.in/check.v1"
+	"gopkg.in/juju/environschema.v1"
+
+	"github.com/juju/juju/apiserver/facades/client/application"
+)
+
+type cloudNativeUniterSuite struct {
+	uniterSuite
+}
+
+var _ = gc.Suite(&cloudNativeUniterSuite{})
+
+func (s *cloudNativeUniterSuite) SetUpTest(c *gc.C) {
+	s.uniterSuite.SetUpTest(c)
+
+	// Ensure the application is not trusted prior to tests.
+	s.setApplicationTrust(c, false)
+}
+
+// setApplicationTrust updates the configuration for the application unit to
+// allow or deny access for cloud spec retrieval.
+func (s *cloudNativeUniterSuite) setApplicationTrust(c *gc.C, trusted bool) {
+	conf := map[string]interface{}{application.TrustConfigOptionName: trusted}
+	fields := map[string]environschema.Attr{application.TrustConfigOptionName: {Type: environschema.Tbool}}
+	defaults := map[string]interface{}{application.TrustConfigOptionName: false}
+	err := s.wordpressApplication.UpdateApplicationConfig(conf, nil, fields, defaults)
+	c.Assert(err, jc.ErrorIsNil)
+
+	newConf, err := s.wordpressApplication.ApplicationConfig()
+	c.Assert(err, jc.ErrorIsNil)
+	c.Assert(newConf.GetBool(application.TrustConfigOptionName, false), gc.Equals, trusted)
+}
+
+func (s *cloudNativeUniterSuite) TestCloudSpecErrorWhenUnauthorized(c *gc.C) {
+	result, err := s.uniter.CloudSpec()
+	c.Check(err, gc.ErrorMatches, "permission denied")
+	c.Check(result, gc.IsNil)
+}
+
+func (s *cloudNativeUniterSuite) TestGetCloudSpecReturnsSpecWhenTrusted(c *gc.C) {
+	s.setApplicationTrust(c, true)
+
+	result, err := s.uniter.CloudSpec()
+	c.Assert(err, jc.ErrorIsNil)
+	c.Check(result.Name, gc.Equals, "dummy")
+
+	exp := map[string]string{
+		"username": "dummy",
+		"password": "secret",
+	}
+	c.Check(result.Credential.Attributes, gc.DeepEquals, exp)
+}
