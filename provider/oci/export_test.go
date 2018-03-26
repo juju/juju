a15fbf240e07d9f93a8d0cc05aa3e627b90b709f
Gabriel Adrian Samfira <gsamfira@cloudbasesolutions.com>
2018-03-26 17:52:43 +0300

    Parse provider images
    This adds provider image parsing, and instance size selection.

---
+++ provider/oci/export_test.go
@@ -0,0 +1,10 @@
+package oci
+
+var (
+	InstanceTypes     = instanceTypes
+	RefreshImageCache = refreshImageCache
+	FindInstanceSpec  = findInstanceSpec
+	GetImageType      = getImageType
+	ShapeSpecs        = shapeSpecs
+	SetImageCache     = setImageCache
+)
