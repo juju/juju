943493f8dfd4e50ef37eef10970defbb9b99d7af
Joseph Phillips <joe.phillips@canonical.com>
2018-04-04 10:09:37 +0200

    Adds shims for patched methods. Adds mock for shims generated with "mockgen -package systemd -destination service/systemd/shims_mock.go github.com/juju/juju/service/systemd ShimFileOps,ShimExec".

---
+++ service/systemd/shims.go
@@ -0,0 +1,17 @@
+package systemd
+
+import (
+	"os"
+
+	"github.com/juju/utils/exec"
+)
+
+type ShimFileOps interface {
+	RemoveAll(name string) error
+	MkdirAll(dirname string) error
+	CreateFile(filename string, data []byte, perm os.FileMode) error
+}
+
+type ShimExec interface {
+	RunCommand(args exec.RunParams) (*exec.ExecResponse, error)
+}
