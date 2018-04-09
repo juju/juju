9704901fbacf1db218a25d14d9d8cd14cfe0de10
Joseph Phillips <joe.phillips@canonical.com>
2018-04-09 16:50:12 +0200

    Adds go:generate directives to produce mocks automatically.

--- service/systemd/shims.go
+++ service/systemd/shims.go
@@ -6,6 +6,11 @@ import (
 	"github.com/juju/utils/exec"
 )
 
+// Interfaces exposing call surfaces
+// To regenerate the mock for these interfaces,
+// run "go generate" from the package directory.
+//go:generate mockgen -package systemd -destination shims_mock.go github.com/juju/juju/service/systemd ShimFileOps,ShimExec
+
 type ShimFileOps interface {
 	RemoveAll(name string) error
 	MkdirAll(dirname string) error
