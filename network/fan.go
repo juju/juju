de272b6262957f5bd7e5d8f1f34aa52c3d499a82
externalreality <eric.jones@canonical.com>
2018-04-16 06:16:21 -0400

    Overlay segment calculation should not panic on IPv6.

--- network/fan.go
+++ network/fan.go
@@ -70,6 +70,9 @@ func CalculateOverlaySegment(underlayCIDR string, fan FanConfigEntry) (*net.IPNe
 		newOverlaySize := overlaySize + (subnetSize - underlaySize)
 		fanSize := uint(underlaySize - overlaySize)
 		newFanIP := underlayNet.IP.To4()
+		if newFanIP == nil {
+			return nil, errors.New("fan address in not an IPv4 address.")
+		}
 		for i := 0; i < 4; i++ {
 			newFanIP[i] &^= fan.Underlay.Mask[i]
 		}
