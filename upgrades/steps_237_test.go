7ba51f6d459aee3cfd309887abfd0137e0aaa3a1
John Arbash Meinel <john@arbash-meinel.com>
2018-04-22 17:05:01 +0400

    Re-run the upgrade step going to 2.3.7.
    Arguably the 2.3.7 step should be removing it from docs that shouldn't
    have it.

---
+++ upgrades/steps_237_test.go
@@ -0,0 +1,27 @@
+// Copyright 2018 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package upgrades_test
+
+import (
+	jc "github.com/juju/testing/checkers"
+	"github.com/juju/version"
+	gc "gopkg.in/check.v1"
+
+	"github.com/juju/juju/testing"
+	"github.com/juju/juju/upgrades"
+)
+
+var v237 = version.MustParse("2.3.7")
+
+type steps237Suite struct {
+	testing.BaseSuite
+}
+
+var _ = gc.Suite(&steps237Suite{})
+
+func (s *steps236Suite) TestEnsureContainerImageStreamDefault237(c *gc.C) {
+	step := findStateStep(c, v237, "ensure container-image-stream config defaults to released")
+	// Logic for step itself is tested in state package.
+	c.Assert(step.Targets(), jc.DeepEquals, []upgrades.Target{upgrades.DatabaseMaster})
+}
