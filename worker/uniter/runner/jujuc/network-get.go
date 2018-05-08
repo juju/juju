ff5889a2c6878998e82f7343c47776deb27e0cfa
externalreality <eric.jones@canonical.com>
2018-05-08 12:23:38 -0400

    Add test for hostname resolution on output.

--- worker/uniter/runner/jujuc/network-get.go
+++ worker/uniter/runner/jujuc/network-get.go
@@ -35,6 +35,12 @@ type NetworkGetCommand struct {
 	out cmd.Output
 }
 
+// Type alias for Domain resolution function for testing
+type resolver = func(host string) (addrs []string, err error)
+
+var lookupFunc = net.LookupHost
+var LookupHost = &lookupFunc
+
 func NewNetworkGetCommand(ctx Context) (_ cmd.Command, err error) {
 	cmd := &NetworkGetCommand{ctx: ctx}
 	cmd.relationIdProxy, err = NewRelationIdValue(ctx, &cmd.RelationId)
@@ -121,7 +127,7 @@ func (c *NetworkGetCommand) Run(ctx *cmd.Context) error {
 		return errors.Trace(ni.Error)
 	}
 
-	ni = resolveNetworkInfoAddresses(ni)
+	ni = resolveNetworkInfoAddresses(ni, *LookupHost)
 
 	// If no specific attributes asked for,
 	// print everything we know.
@@ -167,23 +173,24 @@ func (c *NetworkGetCommand) Run(ctx *cmd.Context) error {
 	return c.out.Write(ctx, keyValues)
 }
 
-// This addresses the immediate problem of
+// TODO(externalreality) This addresses the immediate problem of
 // https://bugs.launchpad.net/juju/+bug/1721368, but the hostname can populate
 // both the egress subnet cidr and the ingress addreses. These too should be
 // resolved. In addition these values probably should not be stored as hostnames
 // but rather the IP, that is, it might be better to do the resolution on input
 // rather than output (network-get) as we do here.
-func resolveNetworkInfoAddresses(networkInfoResult params.NetworkInfoResult) params.NetworkInfoResult {
+func resolveNetworkInfoAddresses(networkInfoResult params.NetworkInfoResult, lookupHost resolver) params.NetworkInfoResult {
 	logger.Debugf("Resolving Addresses")
 	for i, networkInfo := range networkInfoResult.Info {
 		for j, interfaceAddress := range networkInfo.Addresses {
 			if ip := net.ParseIP(interfaceAddress.Address); ip != nil {
 				continue
 			}
-			resolvedAddress, err := net.LookupHost(interfaceAddress.Address)
+			resolvedAddress, err := lookupHost(interfaceAddress.Address)
 			if err != nil {
 				logger.Warningf("The address %q is neither an IP address or a resolvable hostname", interfaceAddress.Address)
 			} else {
+				networkInfoResult.Info[i].Addresses[j].Hostname = interfaceAddress.Address
 				networkInfoResult.Info[i].Addresses[j].Address = resolvedAddress[0]
 			}
 		}
