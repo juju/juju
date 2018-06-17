9ccac96e78130885e8bdecb03dc1237c801f2d2b
externalreality <eric.jones@canonical.com>
2018-06-17 15:57:01 -0400

    Ordering fix.

--- state/migration_internal_test.go
+++ state/migration_internal_test.go
@@ -33,9 +33,9 @@ func (s *MigrationSuite) TestKnownCollections(c *gc.C) {
 
 		// machine
 		instanceDataC,
+		machineUpgradeSeriesLocksC,
 		machinesC,
 		openedPortsC,
-		machineUpgradeSeriesLocksC,
 
 		// application / unit
 		applicationsC,
