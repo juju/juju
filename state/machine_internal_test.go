e3cf33de0b1cea91dc2c5dada463d49e84eefc9e
externalreality <eric.jones@canonical.com>
2018-06-20 13:30:21 -0400

    change assertions.

--- state/machine_internal_test.go
+++ state/machine_internal_test.go
@@ -4,6 +4,7 @@
 package state
 
 import (
+	jc "github.com/juju/testing/checkers"
 	gc "gopkg.in/check.v1"
 	"gopkg.in/mgo.v2/bson"
 	"gopkg.in/mgo.v2/txn"
@@ -17,13 +18,15 @@ var _ = gc.Suite(&MachineInternalSuite{})
 func (s *MachineInternalSuite) TestCreateUpgradePrepareLockTxnAssertsMachineAlive(c *gc.C) {
 	arbitraryId := "1"
 	arbitraryData := &upgradeSeriesLock{}
+	var found bool
 	for _, op := range createUpgradeSeriesPrepareLockTxnOps(arbitraryId, arbitraryData) {
 		assertVal, ok := op.Assert.(bson.D)
 		if op.C == machinesC && ok && assertVal.Map()["life"] == Alive {
-			c.SucceedNow()
+			found = true
+			break
 		}
 	}
-	c.Fatal("Transaction does not assert that machines are of status Alive")
+	c.Assert(found, jc.IsTrue, gc.Commentf("Transaction does not assert that machines are of status Alive"))
 }
 
 func (s *MachineInternalSuite) TestCreateUpgradePrepareLockTxnAssertsDocDoesNOTExist(c *gc.C) {
@@ -50,11 +53,12 @@ func (s *MachineInternalSuite) TestRemoveUpgradePrepareLockTxnAssertsDocExists(c
 }
 
 func assertConstainsOP(c *gc.C, expectedOp txn.Op, actualOps []txn.Op) {
+	var found bool
 	for _, actualOp := range actualOps {
 		if actualOp == expectedOp {
-			c.SucceedNow()
+			found = true
+			break
 		}
 	}
-	c.Fatalf("expected %#v to contain %#v", actualOps, expectedOp)
-	return
+	c.Assert(found, jc.IsTrue, gc.Commentf("expected %#v to contain %#v", actualOps, expectedOp))
 }
