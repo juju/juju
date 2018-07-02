3056cc1c6514778377e391f90761a5290b85b2aa
vinu2003 <me.vinob@gmail.com>
2018-07-02 13:52:26 +1000

    Initial datastructures changes for bundle export

--- apiserver/facades/client/bundle/bundle.go
+++ apiserver/facades/client/bundle/bundle.go
@@ -8,9 +8,12 @@ import (
 	"strings"
 
 	"github.com/juju/bundlechanges"
+	"github.com/juju/description"
 	"github.com/juju/errors"
 	"github.com/juju/loggo"
+	"github.com/juju/os/series"
 	"gopkg.in/juju/charm.v6"
+	"gopkg.in/juju/names.v2"
 
 	"github.com/juju/juju/apiserver/common"
 	"github.com/juju/juju/apiserver/facade"
@@ -39,11 +42,16 @@ type Bundle interface {
 	// GetChanges returns the list of changes required to deploy the given
 	// bundle data.
 	GetChanges(params.BundleChangesParams) (params.BundleChangesResults, error)
+
+	// ExportBundle exports the model configuration as bundle.
+	ExportBundle(params.ExportBundleParams) (params.BundleData, error)
 }
 
 // bundleAPI implements the Bundle interface and is the concrete implementation
 // of the API end point.
-type bundleAPI struct{}
+type bundleAPI struct {
+	state common.ModelManagerBackend
+}
 
 // GetChanges returns the list of changes required to deploy the given bundle
 // data. The changes are sorted by requirements, so that they can be applied in
@@ -95,4 +103,62 @@ func (b *bundleAPI) GetChanges(args params.BundleChangesParams) (params.BundleCh
 		}
 	}
 	return results, nil
+}
+
+// ExportBundle exports the model configuration specified in the argument as bundle.
+func (b bundleAPI) ExportBundle(args params.ExportBundleParams) (params.BundleData, error) {
+	result := params.BundleData{}
+
+	tag, err := names.ParseModelTag(args.ModelTag)
+	if err != nil {
+		return result, errors.Annotate(err, "model-tag cannot be parsed.")
+	}
+
+	st, releaseSt, err := b.state.GetBackend(tag.Id())
+	if err != nil {
+		return result, errors.Trace(err)
+	}
+	defer releaseSt()
+
+	var model description.Model
+	model, err = st.Export()
+	if err != nil {
+		return result, errors.Trace(err)
+	}
+
+	// Fill in the BundleApplicationSpec of BundleData.
+	app := make(map[string]*params.BundleApplicationSpec)
+
+	for _, application := range model.Applications() {
+		app[application.Name()].Charm = application.CharmURL()
+		app[application.Name()].Series = application.Series()
+		app[application.Name()].NumUnits = len(application.Units())
+		//app[application.Name()].To              = application.To
+		app[application.Name()].Expose = application.Exposed()
+		app[application.Name()].Options = application.CharmConfig()
+		app[application.Name()].Annotations = application.Annotations()
+
+		app[application.Name()].EndpointBindings = application.EndpointBindings()
+	}
+
+	// Fill in the BundleMachineSpec of BundleData.
+	mac := make(map[string]*params.BundleMachineSpec)
+
+	for _, machine := range model.Machines() {
+		constraints := []string{"arch=" + machine.Constraints().Architecture(),
+			"cpu-cores=" + string(machine.Constraints().CpuCores()),
+			"cpu-power=" + string(machine.Constraints().CpuPower()),
+			"mem=" + string(machine.Constraints().Memory()),
+			"root-disk=" + string(machine.Constraints().RootDisk())}
+		mac[machine.Id()].Constraints = strings.Join(constraints, " ")
+		mac[machine.Id()].Annotations = machine.Annotations()
+		mac[machine.Id()].Series = machine.Series()
+	}
+
+	result = params.BundleData{
+		Applications: app,
+		Machines:     mac,
+		Series:       series.LatestLts(),
+	}
+	return result, nil
 }
