b624269aa180a6e5aacd4110d19c902b6a6ccab6
externalreality <ecjones@protonmail.com>
2018-08-08 18:10:43 -0500

    Remove status types entirely.

--- core/model/upgradeseries.go
+++ core/model/upgradeseries.go
@@ -21,62 +21,6 @@ const (
 	UpgradeSeriesError           UpgradeSeriesStatus = "error"
 )
 
-// TODO (manadart 2018-08-07) Everything below here to be discarded.
-
-// ToOldStatus is a shim between the new and old status representations.
-// It exists to bootstrap that transition. Unit tests omitted by design.
-func (s UpgradeSeriesStatus) ToOldStatus() (UpgradeSeriesStatusType, MachineSeriesUpgradeStatus) {
-	switch s {
-	case UpgradeSeriesNotStarted:
-		return PrepareStatus, MachineSeriesUpgradeNotStarted
-	case UpgradeSeriesPrepareStarted:
-		return PrepareStatus, MachineSeriesUpgradeStarted
-	case UpgradeSeriesPrepareMachine:
-		return PrepareStatus, MachineSeriesUpgradeAgentsStopped
-	case UpgradeSeriesPrepareComplete:
-		return PrepareStatus, MachineSeriesUpgradeComplete
-	case UpgradeSeriesCompleteStarted:
-		return CompleteStatus, MachineSeriesUpgradeStarted
-	case UpgradeSeriesComplete:
-		return CompleteStatus, MachineSeriesUpgradeComplete
-	default:
-		return "", ""
-	}
-}
-
-func FromOldUpgradeSeriesStatus(prepare, complete MachineSeriesUpgradeStatus) (UpgradeSeriesStatus, error) {
-	switch prepare {
-	case MachineSeriesUpgradeNotStarted:
-		return UpgradeSeriesNotStarted, nil
-	case MachineSeriesUpgradeStarted:
-		return UpgradeSeriesPrepareStarted, nil
-	case MachineSeriesUpgradeAgentsStopped:
-		return UpgradeSeriesPrepareMachine, nil
-	}
-
-	// Arriving here, prepare is complete, so we check the complete status.
-	switch complete {
-	case MachineSeriesUpgradeNotStarted:
-		return UpgradeSeriesPrepareComplete, nil
-	case MachineSeriesUpgradeStarted:
-		return UpgradeSeriesCompleteStarted, nil
-	case MachineSeriesUpgradeComplete:
-		return UpgradeSeriesComplete, nil
-	}
-
-	return "", errors.Errorf("unable to determine status from old combination: %q/%q", prepare, complete)
-}
-
-// The Statuses, at least for units, appy to both the "Prepare" and "Complete"
-// phases of a managed series upgrade. This type can be used to distinguish
-// between those phases when working with the state of an upgraded.
-type UpgradeSeriesStatusType string
-
-const (
-	PrepareStatus  UpgradeSeriesStatusType = "Prepare"
-	CompleteStatus UpgradeSeriesStatusType = "Complete"
-)
-
 //MachineSeriesUpgradeStatus is the current status a machine series upgrade
 type MachineSeriesUpgradeStatus string
 
@@ -103,7 +47,7 @@ const (
 func ValidateUnitSeriesUpgradeStatus(series string) (UnitSeriesUpgradeStatus, error) {
 	unCheckedStatus := UnitSeriesUpgradeStatus(series)
 	switch unCheckedStatus {
-	case NotStarted, PrepareStarted, UnitErrored, PrepareCompleted:
+	case NotStarted, PrepareStarted, PrepareCompleted, CompleteStarted, Completed, UnitErrored:
 		return unCheckedStatus, nil
 	}
 
