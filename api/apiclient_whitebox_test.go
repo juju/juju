1b76e90c76e276ee57ef2b3c4b8702680dfd5947
John Arbash Meinel <john@arbash-meinel.com>
2018-08-13 16:57:18 +0400

    Trying to see what happens when we cancel a Dial.
    However, the test currently doesn't seem to respect any of:
     Timeout
     DialTimeout
     Context.Cancel
    
    So we still need to investigate to understand what is going on.

---
+++ api/apiclient_whitebox_test.go
@@ -0,0 +1,56 @@
+// Copyright 2014 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package api
+
+import (
+	"context"
+	"net"
+	"time"
+
+	jc "github.com/juju/testing/checkers"
+	gc "gopkg.in/check.v1"
+
+	jtesting "github.com/juju/juju/testing"
+	"github.com/juju/testing"
+)
+
+type apiclientWhiteboxSuite struct {
+	testing.IsolationSuite
+}
+
+var _ = gc.Suite(&apiclientWhiteboxSuite{})
+
+func (s *apiclientWhiteboxSuite) TestDialWebsocketMultiTimeout(c *gc.C) {
+	ctx := context.TODO()
+	ctx, cancel := context.WithCancel(ctx)
+	started := make(chan struct{})
+	go func() {
+		select {
+		case <-started:
+		case <-time.After(jtesting.LongWait):
+			c.Fatalf("timed out waiting %s for started", jtesting.LongWait)
+		}
+		<-time.After(10 * time.Millisecond)
+		if cancel != nil {
+			c.Logf("cancelling")
+			cancel()
+		}
+	}()
+	listen, err := net.Listen("tcp4", ":0")
+	c.Assert(err, jc.ErrorIsNil)
+	defer listen.Close()
+	addr := listen.Addr().String()
+	c.Logf("listening at: %s", addr)
+	// Note that we Listen, but we never Accept
+	close(started)
+	info := &Info{
+		Addrs: []string{addr},
+	}
+	opts := DialOpts{
+		Timeout:     10 * time.Millisecond,
+		DialTimeout: 5 * time.Millisecond,
+	}
+	_, err = dialAPI(ctx, info, opts)
+	c.Assert(err, jc.ErrorIsNil)
+}
