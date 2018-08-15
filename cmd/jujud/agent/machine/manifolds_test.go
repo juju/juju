3c35a08f00487f133861c7762fcac13b4c91f556
externalreality <ecjones@protonmail.com>
2018-08-14 20:15:56 -0500

    Remove unwanted manfold flags and fix tests.

--- cmd/jujud/agent/machine/manifolds_test.go
+++ cmd/jujud/agent/machine/manifolds_test.go
@@ -14,6 +14,7 @@ import (
 
 	"github.com/juju/juju/cmd/jujud/agent/agenttest"
 	"github.com/juju/juju/cmd/jujud/agent/machine"
+	"github.com/juju/juju/feature"
 	"github.com/juju/juju/testing"
 	"github.com/juju/juju/worker/gate"
 )
@@ -23,6 +24,11 @@ type ManifoldsSuite struct {
 }
 
 var _ = gc.Suite(&ManifoldsSuite{})
+
+func (s *ManifoldsSuite) SetUpTest(c *gc.C) {
+	s.SetInitialFeatureFlags(feature.UpgradeSeries)
+	s.BaseSuite.SetUpTest(c)
+}
 
 func (*ManifoldsSuite) TestStartFuncs(c *gc.C) {
 	manifolds := machine.Manifolds(machine.ManifoldsConfig{
@@ -98,7 +104,6 @@ func (*ManifoldsSuite) TestManifoldNames(c *gc.C) {
 		"upgrade-check-flag",
 		"upgrade-check-gate",
 		"upgrade-series",
-		"upgrade-series-enabled",
 		"upgrade-steps-flag",
 		"upgrade-steps-gate",
 		"upgrade-steps-runner",
@@ -743,10 +748,14 @@ var expectedMachineManifoldsWithDependencies = map[string][]string{
 	"upgrade-series": {
 		"agent",
 		"api-caller",
-		"upgrade-series-enabled",
+		"api-config-watcher",
+		"migration-fortress",
+		"migration-inactive-flag",
+		"upgrade-check-flag",
+		"upgrade-check-gate",
+		"upgrade-steps-flag",
+		"upgrade-steps-gate",
 	},
-
-	"upgrade-series-enabled": {},
 
 	"upgrade-steps-flag": {"upgrade-steps-gate"},
 
