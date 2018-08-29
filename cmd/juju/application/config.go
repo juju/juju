3255c003b00558df8ac675788dc671dc063f2038
anastasia <anastasia.macmood@canonical.com>
2018-08-29 11:52:30 +1000

    Remove newline after value when output is for a single setting.

--- cmd/juju/application/config.go
+++ cmd/juju/application/config.go
@@ -345,7 +345,9 @@ func (c *configCommand) getConfig(client configCommandAPI, ctx *cmd.Context) err
 		if !ok || v == nil {
 			v = ""
 		}
-		fmt.Fprintln(ctx.Stdout, v)
+		// TODO (anastasiamac 2018-08-29) We want to have a new line after here (fmt.Fprintln).
+		// However, it will break all existing scripts and should be done as part of Juju 3.x work.
+		fmt.Fprint(ctx.Stdout, v)
 		return nil
 	}
 
