28ad01d831b364d17d775127d00610dda8e23b43
anastasia <anastasia.macmood@canonical.com>
2018-08-29 15:38:36 +1000

    check contents, ignore order.

--- state/credentialmodels_test.go
+++ state/credentialmodels_test.go
@@ -84,7 +84,7 @@ func (s *CredentialModelsSuite) TestCredentialModelsAndOwnerAccessMany(c *gc.C) 
 
 	out, err := s.State.CredentialModelsAndOwnerAccess(s.credentialTag)
 	c.Assert(err, jc.ErrorIsNil)
-	c.Assert(out, gc.DeepEquals, []state.CredentialOwnerModelAccess{
+	c.Assert(out, jc.SameContents, []state.CredentialOwnerModelAccess{
 		{ModelName: "abcmodel", OwnerAccess: permission.AdminAccess, ModelUUID: s.abcModelTag.Id()},
 		{ModelName: "xyzmodel", OwnerAccess: permission.AdminAccess, ModelUUID: xyzModelTag.Id()},
 	})
