9ae5cf47ac4d8ce05563b89076fbf0558d83782c
John Arbash Meinel <john@arbash-meinel.com>
2018-08-30 15:49:19 +0400

    Give a Warning if an external network isn't in use
    If someone tries to "expose" something, but there isn't an external
    network, we treat it as a no-op but log a warning. Note that this
    typically happens with Bundles as they default to exporting things they
    think are important.

--- provider/vsphere/instance.go
+++ provider/vsphere/instance.go
@@ -89,6 +89,7 @@ func (inst *environInstance) changeIngressRules(insert bool, rules []network.Ing
 		// Open/Close port without an externalNetwork defined is treated as a no-op.
 		// We don't firewall the internal network, and without an external network we don't have any iptables rules
 		// to define.
+		logger.Warningf("ingress rules changing without an external network defined, no changes will be made")
 		return nil
 	}
 	addresses, client, err := inst.getInstanceConfigurator()
