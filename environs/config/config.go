3311fd74a3018cdd2209e34f5d3f0dadac2e4144
Joseph Phillips <joe.phillips@canonical.com>
2018-09-27 10:20:33 +0200

    Errors are traced in environ config.

--- environs/config/config.go
+++ environs/config/config.go
@@ -365,7 +365,7 @@ func New(withDefaults Defaulting, attrs map[string]interface{}) (*Config, error)
 	}
 	defined, err := checker.Coerce(attrs, nil)
 	if err != nil {
-		return nil, err
+		return nil, errors.Trace(err)
 	}
 
 	c := &Config{
@@ -373,12 +373,12 @@ func New(withDefaults Defaulting, attrs map[string]interface{}) (*Config, error)
 		unknown: make(map[string]interface{}),
 	}
 	if err := c.ensureUnitLogging(); err != nil {
-		return nil, err
+		return nil, errors.Trace(err)
 	}
 
 	// no old config to compare against
 	if err := Validate(c, nil); err != nil {
-		return nil, err
+		return nil, errors.Trace(err)
 	}
 	// Copy unknown attributes onto the type-specific map.
 	for k, v := range attrs {
