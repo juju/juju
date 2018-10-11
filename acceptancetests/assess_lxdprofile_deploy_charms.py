93bca4eef6857b83aa0b5161f1ee8cdd9bb4979c
Simon Richardson <stickupkid@gmail.com>
2018-10-11 17:52:12 +0100

    LXDProfile acceptance test verify
    This will fail until we remove the feature flag

--- acceptancetests/assess_lxdprofile_deploy_charms.py
+++ acceptancetests/assess_lxdprofile_deploy_charms.py
@@ -35,6 +35,35 @@ def setup(client, series):
     _, primary = client.deploy(charm_sink, series=series)
     client.wait_for(primary)
 
+def assess_juju_lxdprofile_machine(client, args):
+    """ Tests juju status
+
+    Verify with juju status
+    """
+    lxdprofile_machine_verify(client, "juju-{}-lxd-profile-0".format(client.model_name))
+
+def lxdprofile_machine_verify(client, profilename):
+    """ Checks the status output is the same as profilename
+
+    :param client: Juju client
+    :param profilename: LXD Profile name to expect in the output
+    :return: None
+    :raises JujuAssertionError: if profilename is not appropriately shown.
+    """
+    status = client.get_status()
+    machine_info = dict(status.iter_machines())
+
+    machine_lxdprofile = machine_info["0"]["lxd-profiles"]
+    log.info(
+        "profile name {}, machine lxd profile {}".format(profilename, machine_lxdprofile))
+
+    if profilename not in machine_lxdprofile:
+        raise JujuAssertionError(
+            "LXD profile in juju status for machine-0 is not {}, per juju".format(
+                profilename))
+
+    log.info("juju status 0 {} succeeded".format(profilename))
+
 def parse_args(argv):
     parser = argparse.ArgumentParser(description="Test juju lxd profile deploy.")
     add_basic_testing_arguments(parser)
@@ -45,7 +74,8 @@ def main(argv=None):
     configure_logging(args.verbose)
     bs_manager = BootstrapManager.from_args(args)
     with bs_manager.booted_context(args.upload_tools):
-        setup(bs_manager.client, "bionic")
+        setup(bs_manager.client, args.series)
+        assess_juju_lxdprofile_machine(bs_manager.client, args)
     return 0
 
 if __name__ == '__main__':
