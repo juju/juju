3b1762a0c4c6176c921ebebdf4faf4a13fa7d264
Jeff Pihach <jeff.pihach@canonical.com>
2018-10-16 17:21:20 -0600

    Add BundleURL to the apiserver bundlechanges endpoint.

--- apiserver/params/params.go
+++ apiserver/params/params.go
@@ -1107,6 +1107,7 @@ type BundleChangesParams struct {
 	// BundleDataYAML is the YAML-encoded charm bundle data
 	// (see "github.com/juju/charm.BundleData").
 	BundleDataYAML string `json:"yaml"`
+	BundleURL      string
 }
 
 // BundleChangesResults holds results of the Bundle.GetChanges call.
