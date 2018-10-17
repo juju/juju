bc6046f4f7222e647c2b480bcd9243f7474c06fb
Christian Muirhead <christian.muirhead@canonical.com>
2018-10-17 17:41:35 +1300

    Get all handlers registered before httpserver starts
    Having api-server and raft-transport (which register handlers) depend on
    the http-server so that they can get the mux and authenticator means
    that there's a time between when http-server starts and those workers
    start where we respond with 404s to valid API requests.
    
    Instead, create the mux and authenticator in their own
    worker (http-server-args, for want of a better name), have api-server
    and raft-transport depend on that, then have http-server depend on both
    of those so that it doesn't start until they've registered handlers.
    
    This required tweaking raft-transport so that it registered the handlers
    before returning the worker. Also needed to move the ifFullyUpgraded
    gate to the raft worker, because otherwise there's a dep cycle from
    raft-transport -> upgrader -> api-caller (implicitly) -> http-server ->
    raft-transport. Moving it's fine because the raft transport doesn't need
    anything that could be changed in an upgrade step (although raft-worker
    does).
    
    Tests not updated yet - committing this so I can apply to 2.5 and test
    upgrading.

--- cmd/jujud/agent/machine/manifolds.go
+++ cmd/jujud/agent/machine/manifolds.go
@@ -57,6 +57,7 @@ import (
 	"github.com/juju/juju/worker/globalclockupdater"
 	"github.com/juju/juju/worker/hostkeyreporter"
 	"github.com/juju/juju/worker/httpserver"
+	"github.com/juju/juju/worker/httpserverargs"
 	"github.com/juju/juju/worker/identityfilewriter"
 	leasemanager "github.com/juju/juju/worker/lease/manifold"
 	"github.com/juju/juju/worker/logger"
@@ -701,23 +702,29 @@ func Manifolds(config ManifoldsConfig) dependency.Manifolds {
 			},
 		))),
 
-		httpServerName: httpserver.Manifold(httpserver.ManifoldConfig{
-			AgentName:             agentName,
-			CertWatcherName:       certificateWatcherName,
+		httpServerArgsName: httpserverargs.Manifold(httpserverargs.ManifoldConfig{
 			ClockName:             clockName,
 			StateName:             stateName,
-			PrometheusRegisterer:  config.PrometheusRegisterer,
-			NewTLSConfig:          httpserver.NewTLSConfig,
-			NewStateAuthenticator: httpserver.NewStateAuthenticator,
-			NewWorker:             httpserver.NewWorkerShim,
+			NewStateAuthenticator: httpserverargs.NewStateAuthenticator,
+		}),
+
+		httpServerName: httpserver.Manifold(httpserver.ManifoldConfig{
+			AgentName:            agentName,
+			CertWatcherName:      certificateWatcherName,
+			StateName:            stateName,
+			MuxName:              httpServerArgsName,
+			StartAfter:           []string{apiServerName, raftTransportName},
+			PrometheusRegisterer: config.PrometheusRegisterer,
+			NewTLSConfig:         httpserver.NewTLSConfig,
+			NewWorker:            httpserver.NewWorkerShim,
 		}),
 
 		apiServerName: apiserver.Manifold(apiserver.ManifoldConfig{
 			AgentName:                         agentName,
-			AuthenticatorName:                 httpServerName,
+			AuthenticatorName:                 httpServerArgsName,
 			ClockName:                         clockName,
 			StateName:                         stateName,
-			MuxName:                           httpServerName,
+			MuxName:                           httpServerArgsName,
 			LeaseManagerName:                  leaseManagerName,
 			UpgradeGateName:                   upgradeStepsGateName,
 			RestoreStatusName:                 restoreWatcherName,
@@ -772,25 +779,25 @@ func Manifolds(config ManifoldsConfig) dependency.Manifolds {
 
 		// All the other raft workers hang off the raft transport, so
 		// it's the only one that needs to be gated by the enabled flag.
-		raftTransportName: ifRaftEnabled(ifFullyUpgraded(rafttransport.Manifold(rafttransport.ManifoldConfig{
+		raftTransportName: ifRaftEnabled(rafttransport.Manifold(rafttransport.ManifoldConfig{
 			ClockName:         clockName,
 			AgentName:         agentName,
-			AuthenticatorName: httpServerName,
+			AuthenticatorName: httpServerArgsName,
 			HubName:           centralHubName,
-			MuxName:           httpServerName,
+			MuxName:           httpServerArgsName,
 			DialConn:          rafttransport.DialConn,
 			NewWorker:         rafttransport.NewWorker,
 			Path:              "/raft",
-		}))),
+		})),
 
-		raftName: raft.Manifold(raft.ManifoldConfig{
+		raftName: ifFullyUpgraded(raft.Manifold(raft.ManifoldConfig{
 			ClockName:     clockName,
 			AgentName:     agentName,
 			TransportName: raftTransportName,
 			FSM:           leaseFSM,
 			Logger:        loggo.GetLogger("juju.worker.raft"),
 			NewWorker:     raft.NewWorker,
-		}),
+		})),
 
 		raftFlagName: raftflag.Manifold(raftflag.ManifoldConfig{
 			RaftName:  raftName,
@@ -971,8 +978,9 @@ const (
 	upgradeSeriesEnabledName = "upgrade-series-enabled"
 	upgradeSeriesWorkerName  = "upgrade-series"
 
-	httpServerName = "http-server"
-	apiServerName  = "api-server"
+	httpServerName     = "http-server"
+	httpServerArgsName = "http-server-args"
+	apiServerName      = "api-server"
 
 	raftTransportName = "raft-transport"
 	raftName          = "raft"
