1f66d0133f34905edf3d590d11afe6f5eea77558
Christian Muirhead <christian.muirhead@canonical.com>
2018-10-18 17:58:12 +1300

    Unit tests for new http-server-args worker/manifold
    And updated the tests for http-server.

--- worker/httpserver/manifold_test.go
+++ worker/httpserver/manifold_test.go
@@ -7,12 +7,10 @@ import (
 	"crypto/tls"
 	"net"
 	"net/http"
-	"time"
 
 	"github.com/juju/errors"
 	"github.com/juju/testing"
 	jc "github.com/juju/testing/checkers"
-	"github.com/juju/utils/clock"
 	gc "gopkg.in/check.v1"
 	"gopkg.in/juju/worker.v1"
 	"gopkg.in/juju/worker.v1/dependency"
@@ -20,7 +18,6 @@ import (
 	"gopkg.in/juju/worker.v1/workertest"
 
 	"github.com/juju/juju/apiserver/apiserverhttp"
-	"github.com/juju/juju/apiserver/httpcontext"
 	"github.com/juju/juju/state"
 	"github.com/juju/juju/worker/httpserver"
 )
@@ -28,14 +25,15 @@ import (
 type ManifoldSuite struct {
 	testing.IsolationSuite
 
+	config               httpserver.ManifoldConfig
 	manifold             dependency.Manifold
 	context              dependency.Context
 	agent                *mockAgent
-	clock                *testing.Clock
 	state                stubStateTracker
+	mux                  *apiserverhttp.Mux
+	raftEnabled          *mockFlag
 	prometheusRegisterer stubPrometheusRegisterer
 	certWatcher          stubCertWatcher
-	authenticator        mockLocalMacaroonAuthenticator
 	tlsConfig            *tls.Config
 
 	stub testing.Stub
@@ -47,50 +45,44 @@ func (s *ManifoldSuite) SetUpTest(c *gc.C) {
 	s.IsolationSuite.SetUpTest(c)
 
 	s.agent = &mockAgent{}
-	s.clock = testing.NewClock(time.Time{})
 	s.state = stubStateTracker{}
+	s.mux = &apiserverhttp.Mux{}
+	s.raftEnabled = &mockFlag{set: true}
 	s.prometheusRegisterer = stubPrometheusRegisterer{}
 	s.certWatcher = stubCertWatcher{}
 	s.tlsConfig = &tls.Config{}
 	s.stub.ResetCalls()
 
 	s.context = s.newContext(nil)
-	s.manifold = httpserver.Manifold(httpserver.ManifoldConfig{
-		AgentName:             "agent",
-		CertWatcherName:       "cert-watcher",
-		ClockName:             "clock",
-		StateName:             "state",
-		PrometheusRegisterer:  &s.prometheusRegisterer,
-		NewStateAuthenticator: s.newStateAuthenticator,
-		NewTLSConfig:          s.newTLSConfig,
-		NewWorker:             s.newWorker,
-	})
+	s.config = httpserver.ManifoldConfig{
+		AgentName:            "agent",
+		CertWatcherName:      "cert-watcher",
+		StateName:            "state",
+		MuxName:              "mux",
+		APIServerName:        "api-server",
+		RaftTransportName:    "raft-transport",
+		RaftEnabledName:      "raft-enabled",
+		PrometheusRegisterer: &s.prometheusRegisterer,
+		NewTLSConfig:         s.newTLSConfig,
+		NewWorker:            s.newWorker,
+	}
+	s.manifold = httpserver.Manifold(s.config)
 }
 
 func (s *ManifoldSuite) newContext(overlay map[string]interface{}) dependency.Context {
 	resources := map[string]interface{}{
-		"agent":        s.agent,
-		"cert-watcher": s.certWatcher.get,
-		"clock":        s.clock,
-		"state":        &s.state,
+		"agent":          s.agent,
+		"cert-watcher":   s.certWatcher.get,
+		"state":          &s.state,
+		"mux":            s.mux,
+		"raft-enabled":   s.raftEnabled,
+		"raft-transport": nil,
+		"api-server":     nil,
 	}
 	for k, v := range overlay {
 		resources[k] = v
 	}
 	return dt.StubContext(nil, resources)
-}
-
-func (s *ManifoldSuite) newStateAuthenticator(
-	statePool *state.StatePool,
-	mux *apiserverhttp.Mux,
-	clock clock.Clock,
-	abort <-chan struct{},
-) (httpcontext.LocalMacaroonAuthenticator, error) {
-	s.stub.MethodCall(s, "NewStateAuthenticator", statePool, mux, clock, abort)
-	if err := s.stub.NextErr(); err != nil {
-		return nil, err
-	}
-	return &s.authenticator, nil
 }
 
 func (s *ManifoldSuite) newTLSConfig(
@@ -124,7 +116,13 @@ func (s *ManifoldSuite) newWorker(config httpserver.Config) (worker.Worker, erro
 }
 
 var expectedInputs = []string{
-	"agent", "cert-watcher", "clock", "state",
+	"agent",
+	"cert-watcher",
+	"state",
+	"mux",
+	"raft-enabled",
+	"raft-transport",
+	"api-server",
 }
 
 func (s *ManifoldSuite) TestInputs(c *gc.C) {
@@ -145,8 +143,8 @@ func (s *ManifoldSuite) TestStart(c *gc.C) {
 	w := s.startWorkerClean(c)
 	workertest.CleanKill(c, w)
 
-	s.stub.CheckCallNames(c, "NewTLSConfig", "NewStateAuthenticator", "NewWorker")
-	newWorkerArgs := s.stub.Calls()[2].Args
+	s.stub.CheckCallNames(c, "NewTLSConfig", "NewWorker")
+	newWorkerArgs := s.stub.Calls()[1].Args
 	c.Assert(newWorkerArgs, gc.HasLen, 1)
 	c.Assert(newWorkerArgs[0], gc.FitsTypeOf, httpserver.Config{})
 	config := newWorkerArgs[0].(httpserver.Config)
@@ -167,25 +165,85 @@ func (s *ManifoldSuite) TestStart(c *gc.C) {
 		PrometheusRegisterer: &s.prometheusRegisterer,
 		TLSConfig:            s.tlsConfig,
 		AutocertHandler:      autocertHandler,
-		Mux:                  apiserverhttp.NewMux(),
+		Mux:                  s.mux,
 	})
+}
+
+func (s *ManifoldSuite) TestValidate(c *gc.C) {
+	type test struct {
+		f      func(*httpserver.ManifoldConfig)
+		expect string
+	}
+	tests := []test{{
+		func(cfg *httpserver.ManifoldConfig) { cfg.AgentName = "" },
+		"empty AgentName not valid",
+	}, {
+		func(cfg *httpserver.ManifoldConfig) { cfg.CertWatcherName = "" },
+		"empty CertWatcherName not valid",
+	}, {
+		func(cfg *httpserver.ManifoldConfig) { cfg.StateName = "" },
+		"empty StateName not valid",
+	}, {
+		func(cfg *httpserver.ManifoldConfig) { cfg.MuxName = "" },
+		"empty MuxName not valid",
+	}, {
+		func(cfg *httpserver.ManifoldConfig) { cfg.RaftTransportName = "" },
+		"empty RaftTransportName not valid",
+	}, {
+		func(cfg *httpserver.ManifoldConfig) { cfg.APIServerName = "" },
+		"empty APIServerName not valid",
+	}, {
+		func(cfg *httpserver.ManifoldConfig) { cfg.RaftEnabledName = "" },
+		"empty RaftEnabledName not valid",
+	}, {
+		func(cfg *httpserver.ManifoldConfig) { cfg.PrometheusRegisterer = nil },
+		"nil PrometheusRegisterer not valid",
+	}, {
+		func(cfg *httpserver.ManifoldConfig) { cfg.NewTLSConfig = nil },
+		"nil NewTLSConfig not valid",
+	}, {
+		func(cfg *httpserver.ManifoldConfig) { cfg.NewWorker = nil },
+		"nil NewWorker not valid",
+	}}
+	for i, test := range tests {
+		c.Logf("test #%d (%s)", i, test.expect)
+		config := s.config
+		test.f(&config)
+		manifold := httpserver.Manifold(config)
+		w, err := manifold.Start(s.context)
+		workertest.CheckNilOrKill(c, w)
+		c.Check(err, gc.ErrorMatches, test.expect)
+	}
+}
+
+func (s *ManifoldSuite) TestStartWithRaftDisabled(c *gc.C) {
+	s.raftEnabled.set = false
+	s.context = s.newContext(map[string]interface{}{
+		"raft-transport": dependency.ErrMissing,
+	})
+	// If raft is disabled raft-transport isn't needed to start
+	// up.
+	w := s.startWorkerClean(c)
+	workertest.CleanKill(c, w)
 }
 
 func (s *ManifoldSuite) TestStartNoAutocert(c *gc.C) {
 	s.manifold = httpserver.Manifold(httpserver.ManifoldConfig{
-		AgentName:             "agent",
-		CertWatcherName:       "cert-watcher",
-		ClockName:             "clock",
-		StateName:             "state",
-		PrometheusRegisterer:  &s.prometheusRegisterer,
-		NewStateAuthenticator: s.newStateAuthenticator,
-		NewTLSConfig:          s.newTLSConfigNoHandler,
-		NewWorker:             s.newWorker,
+		AgentName:            "agent",
+		CertWatcherName:      "cert-watcher",
+		StateName:            "state",
+		MuxName:              "mux",
+		APIServerName:        "api-server",
+		RaftTransportName:    "raft-transport",
+		RaftEnabledName:      "raft-enabled",
+		PrometheusRegisterer: &s.prometheusRegisterer,
+		NewTLSConfig:         s.newTLSConfigNoHandler,
+		NewWorker:            s.newWorker,
 	})
 	w := s.startWorkerClean(c)
 	defer workertest.CleanKill(c, w)
-	s.stub.CheckCallNames(c, "NewTLSConfig", "NewStateAuthenticator", "NewWorker")
-	newWorkerArgs := s.stub.Calls()[2].Args
+	s.stub.CheckCallNames(c, "NewTLSConfig", "NewWorker")
+	newWorkerArgs := s.stub.Calls()[1].Args
 	c.Assert(newWorkerArgs, gc.HasLen, 1)
 	c.Assert(newWorkerArgs[0], gc.FitsTypeOf, httpserver.Config{})
 	config := newWorkerArgs[0].(httpserver.Config)
@@ -204,30 +262,6 @@ func (s *ManifoldSuite) TestStopWorkerClosesState(c *gc.C) {
 	s.state.CheckCallNames(c, "Use", "Done")
 }
 
-func (s *ManifoldSuite) TestMuxOutput(c *gc.C) {
-	w := s.startWorkerClean(c)
-	defer workertest.CleanKill(c, w)
-
-	var mux *apiserverhttp.Mux
-	err := s.manifold.Output(w, &mux)
-	c.Assert(err, jc.ErrorIsNil)
-	c.Assert(mux, gc.NotNil)
-}
-
-func (s *ManifoldSuite) TestAuthenticatorOutput(c *gc.C) {
-	w := s.startWorkerClean(c)
-	defer workertest.CleanKill(c, w)
-
-	var auth1 httpcontext.Authenticator
-	var auth2 httpcontext.LocalMacaroonAuthenticator
-	for _, out := range []interface{}{&auth1, &auth2} {
-		err := s.manifold.Output(w, out)
-		c.Assert(err, jc.ErrorIsNil)
-	}
-	c.Assert(auth1, gc.NotNil)
-	c.Assert(auth1, gc.Equals, auth2)
-}
-
 func (s *ManifoldSuite) startWorkerClean(c *gc.C) worker.Worker {
 	w, err := s.manifold.Start(s.context)
 	c.Assert(err, jc.ErrorIsNil)
@@ -240,3 +274,11 @@ type mockHandler struct{}
 func (*mockHandler) ServeHTTP(http.ResponseWriter, *http.Request) {}
 
 var autocertHandler = &mockHandler{}
+
+type mockFlag struct {
+	set bool
+}
+
+func (f *mockFlag) Check() bool {
+	return f.set
+}
