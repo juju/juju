1e2e85b0105559a1d7c3b850488ad9585b36efeb
externalreality <ecjones@protonmail.com>
2018-10-18 09:47:18 -0500

    Only mark unseen messages as seen.

--- state/machine_upgradeseries.go
+++ state/machine_upgradeseries.go
@@ -521,7 +521,7 @@ func (m *Machine) GetUpgradeSeriesMessages() ([]string, bool, error) {
 	for _, unseenMessage := range unseenMessages {
 		messages = append(messages, unseenMessage.Message)
 	}
-	err = m.SetUpgradeSeriesMessagesAsSeen(lock.Messages)
+	err = m.SetUpgradeSeriesMessagesAsSeen(unseenMessages)
 	if err != nil {
 		return nil, false, errors.Trace(err)
 	}
@@ -537,17 +537,18 @@ func (m *Machine) GetUpgradeSeriesMessages() ([]string, bool, error) {
 }
 
 // SetUpgradeSeriesMessagesAsSeen marks a given upgrade series messages as
-// having been seen by a client of the API. This method is exported since only
-// the client of this method can determine when a message has been "seen" and
-// thus the decision must be made ad the APIServer level as whether to call this
-// method or not (as apposed to immediately calling this method when messages
-// are queried for sending).
+// having been seen by a client of the API. The method we use to determine have
+// that the message has actually been seen is that a client has made a call to
+// fetch the message.
 func (m *Machine) SetUpgradeSeriesMessagesAsSeen(messages []UpgradeSeriesMessage) error {
 	buildTxn := func(attempt int) ([]txn.Op, error) {
 		if attempt > 0 {
 			if err := m.Refresh(); err != nil {
 				return nil, errors.Trace(err)
 			}
+		}
+		if len(messages) == 0 {
+			return nil, jujutxn.ErrNoOperations
 		}
 		if err := m.isStillAlive(); err != nil {
 			return nil, errors.Trace(err)
