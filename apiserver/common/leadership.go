560c145b772b08eb49ffd302b76170288809486b
Joseph Phillips <joe.phillips@canonical.com>
2018-10-19 11:31:15 +0200

    Add leadership API server to common.

---
+++ apiserver/common/leadership.go
@@ -0,0 +1,113 @@
+// Copyright 2018 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package common
+
+import (
+	"github.com/juju/errors"
+	"gopkg.in/juju/names.v2"
+
+	"github.com/juju/juju/apiserver/facade"
+	"github.com/juju/juju/apiserver/params"
+	"github.com/juju/juju/core/leadership"
+	"github.com/juju/juju/permission"
+)
+
+// API exposes leadership pinning and unpinning functionality for remote use.
+type LeadershipAPI interface {
+	PinLeadership(params params.PinLeadershipBulkParams) (params.ErrorResults, error)
+	UnpinLeadership(params params.PinLeadershipBulkParams) (params.ErrorResults, error)
+}
+
+// NewFacade creates and returns a new leadership API.
+// This signature is suitable for facade registration.
+func NewFacade(ctx facade.Context) (LeadershipAPI, error) {
+	st := ctx.State()
+	model, err := st.Model()
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+	pinner, err := ctx.LeadershipPinner(model.UUID())
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+	return NewLeadershipAPI(model.ModelTag(), pinner, ctx.Auth())
+}
+
+// NewAPI creates and returns a new leadership API from the input tag,
+// Pinner implementation and facade Authorizer.
+func NewLeadershipAPI(
+	modelTag names.ModelTag, pinner leadership.Pinner, authorizer facade.Authorizer,
+) (LeadershipAPI, error) {
+	return &leadershipAPI{
+		modelTag:   modelTag,
+		pinner:     pinner,
+		authorizer: authorizer,
+	}, nil
+}
+
+type leadershipAPI struct {
+	modelTag   names.ModelTag
+	pinner     leadership.Pinner
+	authorizer facade.Authorizer
+}
+
+// PinLeadership (API) pins the leadership for applications indicated in the
+// input arguments.
+func (a *leadershipAPI) PinLeadership(param params.PinLeadershipBulkParams) (params.ErrorResults, error) {
+	entity := a.authorizer.GetAuthTag()
+	if err := a.checkAccess(entity, permission.WriteAccess); err != nil {
+		return params.ErrorResults{}, err
+	}
+
+	return a.pinOps(a.pinner.PinLeadership, param, entity), nil
+}
+
+// PinLeadership (API) unpins the leadership for applications indicated in the
+// input arguments.
+func (a *leadershipAPI) UnpinLeadership(param params.PinLeadershipBulkParams) (params.ErrorResults, error) {
+	entity := a.authorizer.GetAuthTag()
+	if err := a.checkAccess(entity, permission.WriteAccess); err != nil {
+		return params.ErrorResults{}, err
+	}
+
+	return a.pinOps(a.pinner.UnpinLeadership, param, entity), nil
+}
+
+// TODO (manadart 2018-10-19): Querying for pins,
+// which will use permission.WriteAccess
+
+func (a *leadershipAPI) pinOps(
+	op func(string, names.Tag) error, param params.PinLeadershipBulkParams, entity names.Tag,
+) params.ErrorResults {
+	results := make([]params.ErrorResult, len(param.Params))
+	for i, p := range param.Params {
+		appTag, err := names.ParseApplicationTag(p.ApplicationTag)
+		if err != nil {
+			results[i] = params.ErrorResult{Error: ServerError(err)}
+			continue
+		}
+		if err = op(appTag.Id(), entity); err != nil {
+			results[i] = params.ErrorResult{Error: ServerError(err)}
+		}
+	}
+	return params.ErrorResults{Results: results}
+}
+
+func (a *leadershipAPI) checkAccess(entity names.Tag, access permission.Access) error {
+	switch entity.Kind() {
+	case names.MachineTagKind, names.ApplicationTagKind, names.ModelTagKind:
+		return nil
+	case names.UserTagKind:
+		canAccess, err := a.authorizer.HasPermission(access, a.modelTag)
+		if err != nil {
+			return errors.Trace(err)
+		}
+		if !canAccess {
+			return ErrPerm
+		}
+		return nil
+	default:
+		return ErrPerm
+	}
+}
---
+++ apiserver/common/leadership_test.go
@@ -0,0 +1,138 @@
+// Copyright 2018 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package common_test
+
+import (
+	"errors"
+
+	"github.com/golang/mock/gomock"
+	jc "github.com/juju/testing/checkers"
+	"github.com/juju/utils"
+	gc "gopkg.in/check.v1"
+	"gopkg.in/juju/names.v2"
+
+	"github.com/juju/juju/apiserver/common"
+	"github.com/juju/juju/apiserver/params"
+	apiservertesting "github.com/juju/juju/apiserver/testing"
+	"github.com/juju/juju/core/leadership/mocks"
+	coretesting "github.com/juju/juju/testing"
+)
+
+type LeadershipSuite struct {
+	coretesting.BaseSuite
+
+	pinner *mocks.MockPinner
+	tag    names.Tag
+	api    common.LeadershipAPI
+}
+
+var _ = gc.Suite(&LeadershipSuite{})
+
+func (s *LeadershipSuite) SetUpTest(c *gc.C) {
+	s.BaseSuite.SetUpTest(c)
+	s.tag = nil
+}
+
+func (s *LeadershipSuite) TestPinSuccess(c *gc.C) {
+	defer s.setup(c).Finish()
+
+	s.pinner.EXPECT().PinLeadership("redis", s.tag).Return(nil)
+
+	arg := params.PinLeadershipBulkParams{
+		Params: []params.PinLeadershipParams{{
+			ApplicationTag: "application-redis",
+		}},
+	}
+	res, err := s.api.PinLeadership(arg)
+	c.Assert(err, jc.ErrorIsNil)
+
+	expected := params.ErrorResults{Results: []params.ErrorResult{{}}}
+	c.Check(res, gc.DeepEquals, expected)
+}
+
+func (s *LeadershipSuite) TestPinError(c *gc.C) {
+	defer s.setup(c).Finish()
+
+	s.pinner.EXPECT().PinLeadership("redis", s.tag).Return(errors.New("boom"))
+
+	arg := params.PinLeadershipBulkParams{
+		Params: []params.PinLeadershipParams{{
+			ApplicationTag: "application-redis",
+		}},
+	}
+	res, err := s.api.PinLeadership(arg)
+	c.Assert(err, jc.ErrorIsNil)
+
+	results := res.Results
+	c.Assert(results, gc.HasLen, 1)
+	c.Check(results[0].Error, gc.ErrorMatches, "boom")
+}
+
+func (s *LeadershipSuite) TestUnpinSuccess(c *gc.C) {
+	defer s.setup(c).Finish()
+
+	s.pinner.EXPECT().UnpinLeadership("redis", s.tag).Return(nil)
+
+	arg := params.PinLeadershipBulkParams{
+		Params: []params.PinLeadershipParams{{
+			ApplicationTag: "application-redis",
+		}},
+	}
+	res, err := s.api.UnpinLeadership(arg)
+	c.Assert(err, jc.ErrorIsNil)
+
+	expected := params.ErrorResults{Results: []params.ErrorResult{{}}}
+	c.Check(res, gc.DeepEquals, expected)
+}
+
+func (s *LeadershipSuite) TestUnpinError(c *gc.C) {
+	defer s.setup(c).Finish()
+
+	s.pinner.EXPECT().UnpinLeadership("redis", s.tag).Return(errors.New("boom"))
+
+	arg := params.PinLeadershipBulkParams{
+		Params: []params.PinLeadershipParams{{
+			ApplicationTag: "application-redis",
+		}},
+	}
+	res, err := s.api.UnpinLeadership(arg)
+	c.Assert(err, jc.ErrorIsNil)
+
+	results := res.Results
+	c.Assert(results, gc.HasLen, 1)
+	c.Check(results[0].Error, gc.ErrorMatches, "boom")
+}
+
+func (s *LeadershipSuite) TestPermissionDenied(c *gc.C) {
+	s.tag = names.NewUserTag("some-random-cat")
+	defer s.setup(c).Finish()
+
+	arg := params.PinLeadershipBulkParams{
+		Params: []params.PinLeadershipParams{{
+			ApplicationTag: "application-redis",
+		}},
+	}
+	_, err := s.api.UnpinLeadership(arg)
+	c.Assert(err, gc.ErrorMatches, "permission denied")
+}
+
+func (s *LeadershipSuite) setup(c *gc.C) *gomock.Controller {
+	ctrl := gomock.NewController(c)
+
+	s.pinner = mocks.NewMockPinner(ctrl)
+
+	if s.tag == nil {
+		s.tag = names.NewMachineTag("0")
+	}
+
+	var err error
+	s.api, err = common.NewLeadershipAPI(
+		names.NewModelTag(utils.MustNewUUID().String()),
+		s.pinner,
+		&apiservertesting.FakeAuthorizer{Tag: s.tag},
+	)
+	c.Assert(err, jc.ErrorIsNil)
+
+	return ctrl
+}
