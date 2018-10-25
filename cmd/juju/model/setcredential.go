7b5056bd97c87aa11ab3a033531860a337c72d6a
Peter Matulis <peter.matulis@canonical.com>
2018-10-25 14:55:36 +0000

    replace link with relate; mention 'show-controller'

--- cmd/juju/model/setcredential.go
+++ cmd/juju/model/setcredential.go
@@ -61,7 +61,7 @@ func (c *modelCredentialCommand) Info() *cmd.Info {
 	return &cmd.Info{
 		Name:    "set-credential",
 		Args:    "<cloud name> <credential name>",
-		Purpose: "Links a remote credential to a model.",
+		Purpose: "Relates a remote credential to a model.",
 		Doc:     modelCredentialDoc,
 	}
 }
@@ -172,22 +172,29 @@ func (c *modelCredentialCommand) findCredentialLocally(ctx *cmd.Context) (*cloud
 }
 
 const modelCredentialDoc = `
-This command links a credential cached on a controller to a specific model. It
-does not change/update the contents of an existing active credential. See
+This command relates a credential cached on a controller to a specific model.
+It does not change/update the contents of an existing active credential. See
 command ` + "`update-credential`" + ` for that.
 
 The credential specified may exist locally (on the client), remotely (on the
-controller), or both. The command will error out if the credential is neither
-remotely nor locally stored.
+controller), or both. The command will error out if the credential is stored
+neither remotely nor locally.
 
-When remote, the credential will be linked to the specified model.
+When remote, the credential will be related to the specified model.
 
 When local and not remote, the credential will first be uploaded to the
-controller and then linked.
+controller and then related.
+
+This command does not affect an existing relation between the specified
+credential and another model. If the credential is already related to a model
+this operation will result in that credential being related to two models.
+
+Use the ` + "`show-credential`" + ` command to see how remote credentials are related
+to models.
 
 Examples:
 
-For cloud 'aws', link remote credential 'bob' to model 'trinity':
+For cloud 'aws', relate remote credential 'bob' to model 'trinity':
 
     juju set-credential -m trinity aws bob
 
