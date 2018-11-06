74780c4875c0b95b7099ab1d801ee7cbbd30ac47
Anastasia Macmood <anastasia.macmood@canonical.com>
2018-11-07 09:45:03 +1000

    invalidate credential should take in params structure.

--- apiserver/facades/agent/credentialvalidator/credentialvalidator_test.go
+++ apiserver/facades/agent/credentialvalidator/credentialvalidator_test.go
@@ -86,7 +86,7 @@ func (s *CredentialValidatorSuite) TestWatchCredentialInvalidTag(c *gc.C) {
 }
 
 func (s *CredentialValidatorSuite) TestInvalidateModelCredential(c *gc.C) {
-	result, err := s.api.InvalidateModelCredential("not again")
+	result, err := s.api.InvalidateModelCredential(params.InvalidateCredentialArg{"not again"})
 	c.Assert(err, jc.ErrorIsNil)
 	c.Assert(result, gc.DeepEquals, params.ErrorResult{})
 	s.backend.CheckCalls(c, []testing.StubCall{
@@ -97,7 +97,7 @@ func (s *CredentialValidatorSuite) TestInvalidateModelCredential(c *gc.C) {
 func (s *CredentialValidatorSuite) TestInvalidateModelCredentialError(c *gc.C) {
 	expected := errors.New("boom")
 	s.backend.SetErrors(expected)
-	result, err := s.api.InvalidateModelCredential("not again")
+	result, err := s.api.InvalidateModelCredential(params.InvalidateCredentialArg{"not again"})
 	c.Assert(err, jc.ErrorIsNil)
 	c.Assert(result, gc.DeepEquals, params.ErrorResult{Error: common.ServerError(expected)})
 	s.backend.CheckCalls(c, []testing.StubCall{
