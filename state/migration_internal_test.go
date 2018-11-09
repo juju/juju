9efa3487131380a87e5e48b2ad48d3c0085bc8c6
Simon Richardson <stickupkid@gmail.com>
2018-11-09 17:05:40 +0000

    Remove the old migration data from the machine doc
    The following removes the old transient data from the machine doc,
    so that it's put in the right place: InstanceCharmProfileData.

--- state/migration_internal_test.go
+++ state/migration_internal_test.go
@@ -317,10 +317,6 @@ func (s *MigrationSuite) TestMachineDocFields(c *gc.C) {
 		// Ignored at this stage, could be an issue if mongo 3.0 isn't
 		// available.
 		"StopMongoUntilVersion",
-		// These items are transient data, so no need to save for model migrations.
-		"UpgradeCharmProfileApplication",
-		"UpgradeCharmProfileCharmURL",
-		"UpgradeCharmProfileComplete",
 	)
 	migrated := set.NewStrings(
 		"Addresses",
