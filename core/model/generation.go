287b6d0a2c9b364ca32549edb3a0f729707e589b
Joseph Phillips <joe.phillips@canonical.com>
2018-12-07 15:33:44 +0100

    Adds copyright headers for new generation files.

--- core/model/generation.go
+++ core/model/generation.go
@@ -1,3 +1,6 @@
+// Copyright 2018 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
 package model
 
 // GenerationVersion indicates a generation to use for model config.
