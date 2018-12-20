f505b7e3a3a7e0b273c7f2fbdca691f9daf1c4e8
Simon Richardson <stickupkid@gmail.com>
2018-12-20 12:30:57 +0000

    Deploy webscale charm bundle

--- acceptancetests/assess_deploy_webscale.py
+++ acceptancetests/assess_deploy_webscale.py
@@ -21,6 +21,7 @@ from utility import (
     add_basic_testing_arguments,
     configure_logging,
     JujuAssertionError,
+    get_current_model,
 )
 
 from jujucharm import (
@@ -32,9 +33,29 @@ __metaclass__ = type
 
 log = logging.getLogger("assess_deploy_webscale")
 
+def deploy_bundle(client, charm_bundle):
+    """Deploy the given charm bundle
+
+    :param client: Jujupy ModelClient object
+    """
+    model_name = "webscale"
+    current_model = client.add_model(model_name)
+    current_model.deploy(
+        charm=charm_bundle,
+    )
+    current_model.juju(current_model._show_status, ('--format', 'tabular'))
+    current_model.wait_for_workloads(timeout=3600)
+    current_model.juju(current_model._show_status, ('--format', 'tabular'))
+
+    current_model.destroy_model()
+
 def parse_args(argv):
     """Parse all arguments."""
     parser = argparse.ArgumentParser(description="Webscale charm deployment CI test")
+    parser.add_argument(
+        '--charm-bundle',
+        help="Override the charm bundle to deploy",
+    )
     add_basic_testing_arguments(parser, existing=False)
     return parser.parse_args(argv)
 
@@ -44,6 +65,7 @@ def main(argv=None):
     bs_manager = BootstrapManager.from_args(args)
     with bs_manager.booted_context(args.upload_tools):
         client = bs_manager.client
+        deploy_bundle(client, charm_bundle=args.charm_bundle)
     return 0
 
 if __name__ == '__main__':
