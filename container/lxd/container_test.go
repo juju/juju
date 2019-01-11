916e8faaa824c99dbbed293d26a11a0a05126b09
Joseph Phillips <joe.phillips@canonical.com>
2019-01-11 05:59:55 +0100

    Regenerate LXD mock and update limits.memeroy to us MiB over MB.

--- container/lxd/container_test.go
+++ container/lxd/container_test.go
@@ -48,11 +48,11 @@ func (s *containerSuite) TestContainerCPUs(c *gc.C) {
 func (s *containerSuite) TestContainerMem(c *gc.C) {
 	container := lxd.Container{}
 
-	container.Config = map[string]string{"limits.memory": "1MB"}
-	c.Check(container.Mem(), gc.Equals, uint(1))
+	container.Config = map[string]string{"limits.memory": "1MiB"}
+	c.Check(int(container.Mem()), gc.Equals, 1)
 
-	container.Config = map[string]string{"limits.memory": "2GB"}
-	c.Check(container.Mem(), gc.Equals, uint(2048))
+	container.Config = map[string]string{"limits.memory": "2GiB"}
+	c.Check(int(container.Mem()), gc.Equals, 2048)
 }
 
 func (s *containerSuite) TestContainerAddDiskNoDevices(c *gc.C) {
