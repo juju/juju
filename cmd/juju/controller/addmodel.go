c8131e2d7abf6f4516f5887c3b57affb1eeeee8d
Heather Lanigan <heather.lanigan@canonical.com>
2019-01-24 17:44:46 -0500

    put addition of modelGeneration to modeldetails behind feature flag

--- cmd/juju/controller/addmodel.go
+++ cmd/juju/controller/addmodel.go
@@ -12,6 +12,7 @@ import (
 	"github.com/juju/cmd"
 	"github.com/juju/errors"
 	"github.com/juju/gnuflag"
+	"github.com/juju/utils/featureflag"
 	"gopkg.in/juju/names.v2"
 
 	"github.com/juju/juju/api"
@@ -27,6 +28,7 @@ import (
 	coremodel "github.com/juju/juju/core/model"
 	"github.com/juju/juju/environs"
 	"github.com/juju/juju/environs/config"
+	"github.com/juju/juju/feature"
 	"github.com/juju/juju/jujuclient"
 )
 
@@ -246,12 +248,15 @@ func (c *addModelCommand) Run(ctx *cmd.Context) error {
 	messageFormat := "Added '%s' model"
 	messageArgs := []interface{}{c.Name}
 
+	details := jujuclient.ModelDetails{
+		ModelUUID: model.UUID,
+		ModelType: model.Type,
+	}
+	if featureflag.Enabled(feature.Generations) {
+		details.ModelGeneration = coremodel.GenerationCurrent // This is the default generation.
+	}
 	if modelOwner == accountDetails.User {
-		if err := store.UpdateModel(controllerName, c.Name, jujuclient.ModelDetails{
-			ModelUUID:       model.UUID,
-			ModelType:       model.Type,
-			ModelGeneration: coremodel.GenerationCurrent, // This is the default generation.
-		}); err != nil {
+		if err := store.UpdateModel(controllerName, c.Name, details); err != nil {
 			return errors.Trace(err)
 		}
 		if !c.noSwitch {
