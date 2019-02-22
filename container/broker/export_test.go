3cca06cf64ae6a6e2f212a1a2d8d5708f0119fd8
Heather Lanigan <heather.lanigan@canonical.com>
2019-02-22 09:44:20 -0500

    Refactor the container brokers from the provisioner into a new package

---
+++ container/broker/export_test.go
@@ -0,0 +1,9 @@
+// Copyright 2019 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package broker
+
+var (
+	ResolvConfFiles       = &resolvConfFiles
+	CombinedCloudInitData = combinedCloudInitData
+)
