5ea5c5ed0e6f910f3b381734c7fc66c35ece7ae6
Simon Richardson <stickupkid@gmail.com>
2019-02-27 16:55:08 +0100

    Adding machine facade calls

--- api/instancemutater/machine.go
+++ api/instancemutater/machine.go
@@ -6,6 +6,7 @@ package instancemutater
 import (
 	"fmt"
 
+	"github.com/juju/errors"
 	"gopkg.in/juju/names.v2"
 
 	"github.com/juju/juju/api/base"
@@ -23,18 +24,56 @@ type Machine struct {
 	life params.Life
 }
 
+// CharmProfiles returns the CharmProfiles for the machine
 func (m *Machine) CharmProfiles() ([]string, error) {
-	return nil, nil
+	var result params.StringsResult
+	args := params.Entities{
+		Entities: []params.Entity{{Tag: m.tag.String()}},
+	}
+	err := m.facade.FacadeCall("CharmProfiles", args, &result)
+	if err != nil {
+		return nil, err
+	}
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return result.Result, nil
 }
 
+// SetUpgradeCharmProfileComplete allows setting the charm profile complete
+// status message for a given unit.
 func (m *Machine) SetUpgradeCharmProfileComplete(unitName string, message string) error {
+	var results params.ErrorResults
+	args := params.SetProfileUpgradeCompleteArgs{
+		Args: []params.SetProfileUpgradeCompleteArg{
+			{
+				Entity:   params.Entity{Tag: m.tag.String()},
+				UnitName: unitName,
+				Message:  message,
+			},
+		},
+	}
+	err := m.facade.FacadeCall("SetUpgradeCharmProfileComplete", args, &results)
+	if err != nil {
+		return err
+	}
+	if len(results.Results) != 1 {
+		return errors.Errorf("expected 1 result, got %d", len(results.Results))
+	}
+	result := results.Results[0]
+	if result.Error != nil {
+		return result.Error
+	}
 	return nil
 }
 
+// Tag returns the current machine tag
 func (m *Machine) Tag() names.MachineTag {
 	return m.tag
 }
 
+// WatchUnits returns a watcher.StringsWatcher for watching units of a given
+// machine.
 func (m *Machine) WatchUnits() (watcher.StringsWatcher, error) {
 	var results params.StringsWatchResults
 	args := params.Entities{
