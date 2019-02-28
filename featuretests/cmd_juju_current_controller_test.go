80794629b9c3dae0ec3e7d71d8d475b04ecb68c7
Anastasia Macmood <anastasia.macmood@canonical.com>
2019-02-28 08:56:29 +0100

    organise imports

--- featuretests/cmd_juju_current_controller_test.go
+++ featuretests/cmd_juju_current_controller_test.go
@@ -4,13 +4,13 @@
 package featuretests
 
 import (
-	gc "gopkg.in/check.v1"
 	"regexp"
 
 	"github.com/juju/cmd"
 	"github.com/juju/cmd/cmdtesting"
 	"github.com/juju/loggo"
 	jc "github.com/juju/testing/checkers"
+	gc "gopkg.in/check.v1"
 
 	"github.com/juju/juju/cmd/juju/commands"
 	jujutesting "github.com/juju/juju/juju/testing"
