1943fac2cc84485c7d29c45796db1c4f42dc1cd1
Heather Lanigan <heather.lanigan@canonical.com>
2019-03-13 16:41:48 -0400

    Add StringsWatcher and setup for testing them.

---
+++ core/cache/stringswatcher_test.go
@@ -0,0 +1,77 @@
+// Copyright 2018 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package cache_test
+
+import (
+	"time"
+
+	jc "github.com/juju/testing/checkers"
+	gc "gopkg.in/check.v1"
+
+	"github.com/juju/juju/core/cache"
+	"github.com/juju/juju/testing"
+)
+
+func NewStringsWatcherC(c *gc.C, watcher cache.StringsWatcher) StringsWatcherC {
+	return StringsWatcherC{
+		C:       c,
+		Watcher: watcher,
+	}
+}
+
+type StringsWatcherC struct {
+	*gc.C
+	Watcher cache.StringsWatcher
+}
+
+// AssertOneChange fails if no change is sent before a long time has passed; or
+// if, subsequent to that, any further change is sent before a short time has
+// passed.
+func (c StringsWatcherC) AssertOneChange() {
+	select {
+	case _, ok := <-c.Watcher.Changes():
+		c.Assert(ok, jc.IsTrue)
+	case <-time.After(testing.LongWait):
+		c.Fatalf("watcher did not send change")
+	}
+	c.AssertNoChange()
+}
+
+// AssertNoChange fails if it manages to read a value from Changes before a
+// short time has passed.
+func (c StringsWatcherC) AssertNoChange() {
+	select {
+	case _, ok := <-c.Watcher.Changes():
+		if ok {
+			c.Fatalf("watcher sent unexpected change")
+		}
+		c.Fatalf("watcher changes channel closed")
+	case <-time.After(testing.ShortWait):
+	}
+}
+
+// AssertStops Kills the watcher and asserts (1) that Wait completes without
+// error before a long time has passed; and (2) that Changes channel is closed.
+func (c StringsWatcherC) AssertStops() {
+	c.Watcher.Kill()
+	wait := make(chan error)
+	go func() {
+		wait <- c.Watcher.Wait()
+	}()
+	select {
+	case <-time.After(testing.LongWait):
+		c.Fatalf("watcher never stopped")
+	case err := <-wait:
+		c.Assert(err, jc.ErrorIsNil)
+	}
+
+	select {
+	case _, ok := <-c.Watcher.Changes():
+		if ok {
+			c.Fatalf("watcher sent unexpected change")
+		}
+	default:
+		c.Fatalf("channel not closed")
+	}
+}
--- core/cache/watcher.go
+++ core/cache/watcher.go
@@ -144,3 +144,76 @@ func (w *ConfigWatcher) configChanged(topic string, value interface{}) {
 	}
 	w.notify()
 }
+
+// StringsWatcher will return what has changed.
+type StringsWatcher interface {
+	Watcher
+	Changes() <-chan struct{}
+}
+
+type stringsWatcherBase struct {
+	tomb    tomb.Tomb
+	changes chan []string
+	// We can't send down a closed channel, so protect the sending
+	// with a mutex and bool. Since you can't really even ask a channel
+	// if it is closed.
+	closed bool
+	mu     sync.Mutex
+}
+
+func newStringsWatcherBase() *stringsWatcherBase {
+	// We use a single entry buffered channel for the changes.
+	// This allows the config changed handler to send a value when there
+	// is a change, but if that value hasn't been consumed before the
+	// next change, the second change is discarded.
+	ch := make(chan []string, 1)
+
+	// Send initial event down the channel. We know that this will
+	// execute immediately because it is a buffered channel.
+	ch <- []string{}
+
+	return &stringsWatcherBase{changes: ch}
+}
+
+// Changes is part of the core watcher definition.
+// The changes channel is never closed.
+func (w *stringsWatcherBase) Changes() <-chan []string {
+	return w.changes
+}
+
+// Kill is part of the worker.Worker interface.
+func (w *stringsWatcherBase) Kill() {
+	w.mu.Lock()
+	w.closed = true
+	close(w.changes)
+	w.mu.Unlock()
+	w.tomb.Kill(nil)
+}
+
+// Wait is part of the worker.Worker interface.
+func (w *stringsWatcherBase) Wait() error {
+	return w.tomb.Wait()
+}
+
+// Stop is currently required by the Resources wrapper in the apiserver.
+func (w *stringsWatcherBase) Stop() error {
+	w.Kill()
+	return w.Wait()
+}
+
+func (w *stringsWatcherBase) notify(values []string) {
+	w.mu.Lock()
+
+	if w.closed {
+		w.mu.Unlock()
+		return
+	}
+
+	select {
+	case w.changes <- values:
+	default:
+		// Already a pending change, so do nothing.
+	}
+
+	w.mu.Unlock()
+}
