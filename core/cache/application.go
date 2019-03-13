5053d395102fdcaa9e9c6bcfebdb9c2caa94c3a3
Joseph Phillips <joe.phillips@canonical.com>
2019-03-13 11:32:05 +0100

    Adds field-based watcher for cached applications.

--- core/cache/application.go
+++ core/cache/application.go
@@ -9,6 +9,11 @@ import (
 	"github.com/juju/pubsub"
 )
 
+const (
+	applicationConfigChange = "application-config-change"
+	applicationFieldChange  = "application-field-change"
+)
+
 func newApplication(metrics *ControllerGauges, hub *pubsub.SimpleHub) *Application {
 	m := &Application{
 		metrics: metrics,
@@ -28,18 +33,78 @@ type Application struct {
 	configHash string
 }
 
-func (m *Application) setDetails(details ApplicationChange) {
-	m.mu.Lock()
-	defer m.mu.Unlock()
-	m.details = details
+func (a *Application) WatchFields(comparitors ...func(*ApplicationDelta) bool) *applicationFieldWatcher {
+	w := newApplicationFieldWatcher(comparitors)
+
+	unsub := a.hub.Subscribe(a.topic(applicationFieldChange), w.detailsChange)
+
+	w.tomb.Go(func() error {
+		<-w.tomb.Dying()
+		unsub()
+		return nil
+	})
+
+	return w
+}
+
+// TODO (manadart 2018-03-13) Should we change this up and down the call stack
+// so that pointers are passed instead of copies?
+func (a *Application) setDetails(details ApplicationChange) {
+	a.mu.Lock()
+	defer a.mu.Unlock()
+
+	delta := &ApplicationDelta{old: a.details, new: details}
+	a.details = details
 
 	configHash, err := hash(details.Config)
 	if err != nil {
 		logger.Errorf("invariant error - application config should be yaml serializable and hashable, %v", err)
 		configHash = ""
 	}
-	if configHash != m.configHash {
-		m.configHash = configHash
+	if configHash != a.configHash {
+		a.configHash = configHash
 		// TODO: publish config change...
+	}
+
+	a.hub.Publish(a.topic(applicationFieldChange), delta)
+}
+
+// topic prefixes the input string with the application name.
+func (a *Application) topic(suffix string) string {
+	return a.details.Name + ":" + suffix
+}
+
+type applicationFieldWatcher struct {
+	*notifyWatcherBase
+
+	comparitors []func(*ApplicationDelta) bool
+}
+
+func newApplicationFieldWatcher(comparitors []func(*ApplicationDelta) bool) *applicationFieldWatcher {
+	return &applicationFieldWatcher{
+		notifyWatcherBase: newNotifyWatcherBase(),
+
+		comparitors: comparitors,
+	}
+}
+
+func (w *applicationFieldWatcher) detailsChange(topic string, value interface{}) {
+	delta, ok := value.(*ApplicationDelta)
+	if !ok {
+		logger.Errorf("programming error, value not of type *ApplicationChange")
+	}
+
+	// If no comparitors were specified, notify for any change.
+	if len(w.comparitors) == 0 {
+		w.notify()
+		return
+	}
+
+	// Otherwise notify if any of the articles we are interested in change.
+	for _, c := range w.comparitors {
+		if c(delta) {
+			w.notify()
+			return
+		}
 	}
 }
--- core/cache/application_test.go
+++ core/cache/application_test.go
@@ -4,6 +4,7 @@ package cache_test
 
 import (
 	gc "gopkg.in/check.v1"
+	"gopkg.in/juju/worker.v1/workertest"
 
 	"github.com/juju/juju/core/cache"
 	"github.com/juju/juju/core/constraints"
@@ -19,6 +20,65 @@ var _ = gc.Suite(&ApplicationSuite{})
 
 func (s *ApplicationSuite) SetUpTest(c *gc.C) {
 	s.entitySuite.SetUpTest(c)
+}
+
+func (s *ApplicationSuite) TestFieldWatcherStops(c *gc.C) {
+	m := s.newApplication(appChange)
+	w := m.WatchFields()
+	wc := NewNotifyWatcherC(c, w)
+	// Sends initial event.
+	wc.AssertOneChange()
+	wc.AssertStops()
+}
+
+func (s *ApplicationSuite) TestFieldWatcherAnyChange(c *gc.C) {
+	m := s.newApplication(appChange)
+	w := m.WatchFields()
+	defer workertest.CleanKill(c, w)
+
+	wc := NewNotifyWatcherC(c, w)
+	// Sends initial event.
+	wc.AssertOneChange()
+
+	change := appChange
+	change.Config = map[string]interface{}{
+		"key": "changed",
+	}
+	m.SetDetails(change)
+	wc.AssertOneChange()
+}
+
+func (s *ApplicationSuite) TestFieldWatcherSpecificChange(c *gc.C) {
+	m := s.newApplication(appChange)
+	w := m.WatchFields(cache.ApplicationCharmURLChanged, cache.ApplicationExposedChanged)
+	defer workertest.CleanKill(c, w)
+
+	wc := NewNotifyWatcherC(c, w)
+	// Sends initial event.
+	wc.AssertOneChange()
+
+	change := appChange
+
+	// Not a change we are watching for.
+	change.Config = map[string]interface{}{
+		"key": "changed",
+	}
+	m.SetDetails(change)
+	wc.AssertNoChange()
+
+	change.CharmURL = "www.new-charm-url.com"
+	m.SetDetails(change)
+	wc.AssertOneChange()
+
+	change.Exposed = true
+	m.SetDetails(change)
+	wc.AssertOneChange()
+}
+
+func (s *ApplicationSuite) newApplication(details cache.ApplicationChange) *cache.Application {
+	a := cache.NewApplication(s.gauges, s.hub)
+	a.SetDetails(details)
+	return a
 }
 
 var appChange = cache.ApplicationChange{
