50ed693437db6d5a30433a8d3192f68878723bad
Kelvin Liu <kelvin.liu@canonical.com>
2019-03-15 20:51:41 +1100

    refactor assess_caas_deploy_charms.py to support the new k8s infra

--- acceptancetests/jujupy/k8s_provider/__init__.py
+++ acceptancetests/jujupy/k8s_provider/__init__.py
@@ -1,1 +1,19 @@
+# This file is part of JujuPy, a library for driving the Juju CLI.
+# Copyright 2013-2019 Canonical Ltd.
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the Lesser GNU General Public License version 3, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranties of MERCHANTABILITY,
+# SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR PURPOSE.  See the Lesser
+# GNU General Public License for more details.
+#
+# You should have received a copy of the Lesser GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Functionality for handling installed or other juju binaries
+# (including paths etc.)
+
 from . import KubernetesCore, MicroK8s  # noqa
