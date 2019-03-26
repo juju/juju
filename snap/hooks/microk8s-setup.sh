07b3575623928d188a69c9ea3b6b3988aea2b5ed
Christopher Lee <veebers@gmail.com>
2019-03-27 10:42:27 +1300

    Add oci image precaching to snap
    Adds hooks for install and refresh (upgrade etc.) that will pull the operator
    images if microk8s is installed.

--- snap/hooks/microk8s-setup.sh
+++ snap/hooks/microk8s-setup.sh
@@ -1,31 +1,31 @@
-#!/usr/bin/env bash
+#!/bin/sh -eu
 
-set -eu
-
-# maybe announce which trigger this is
-env
+PATH=/snap/bin:$PATH
 
 if ! which microk8s.kubectl; then
     echo "microk8s is not installed."
     exit 0
 fi
 
-# get this version of juju
+container_cmd=microk8s.ctr
+image_arg=images
+if ! which $container_cmd; then
+    container_cmd=microk8s.docker
+    image_arg=image
+    if ! which $container_cmd; then
+        echo "Neither docker or ctr image control found."
+        exit 0
+    fi
+fi
+
 juju_version=$(/snap/bin/juju version | rev | cut -d- -f3- | rev)
-docker_image=jujusolutions/jujud-operator:$juju_version
-mongo_image=""
+oci_image="docker.io/jujusolutions/jujud-operator:$juju_version"
+mongo_image="docker.io/jujusolutions/juju-db:4.0"
 
-echo "Going to cache images: ${docker_image} and $mongo_image"
+echo "Going to cache images: $oci_image and $mongo_image."
 
-# Do I need sudo here? do I even have the right perms for this?
-echo "Pulling: ${docker_image}"
-microk8s.docker image pull ${docker_image}
+echo "Pulling: $oci_image."
+$container_cmd $image_arg pull $oci_image
 
-echo "Pulling: ${mongo_image}"
-microk8s.docker image pull ${mongo_image}
-
-echo "Available images:"
-microk8s.docker image ls
-
-echo "Pre-loading microk8s cloud"
-microk8s.config | /snap/bin/juju add-k8s microK8s
+echo "Pulling: $mongo_image."
+$container_cmd $image_arg pull $mongo_image
