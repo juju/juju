8ea3c8e41e32829bb65a6a8aae0620ec2789a0d3
Joseph Phillips <joe.phillips@canonical.com>
2019-04-04 09:41:38 +0200

    Renames switchGenerationSuite to checkoutSuite

--- cmd/juju/model/checkout_test.go
+++ cmd/juju/model/checkout_test.go
@@ -15,23 +15,23 @@ import (
 	coremodel "github.com/juju/juju/core/model"
 )
 
-type switchGenerationSuite struct {
+type checkoutSuite struct {
 	generationBaseSuite
 }
 
-var _ = gc.Suite(&switchGenerationSuite{})
+var _ = gc.Suite(&checkoutSuite{})
 
-func (s *switchGenerationSuite) TestInit(c *gc.C) {
+func (s *checkoutSuite) TestInit(c *gc.C) {
 	err := s.runInit(s.branchName)
 	c.Assert(err, jc.ErrorIsNil)
 }
 
-func (s *switchGenerationSuite) TestInitFail(c *gc.C) {
+func (s *checkoutSuite) TestInitFail(c *gc.C) {
 	err := s.runInit()
 	c.Assert(err, gc.ErrorMatches, "must specify a branch name to switch to")
 }
 
-func (s *switchGenerationSuite) TestRunCommandCurrent(c *gc.C) {
+func (s *checkoutSuite) TestRunCommandCurrent(c *gc.C) {
 	ctx, err := s.runCommand(c, nil, coremodel.GenerationMaster)
 	c.Assert(err, jc.ErrorIsNil)
 	c.Assert(cmdtesting.Stdout(ctx), gc.Equals, "target generation set to \"master\"\n")
@@ -42,7 +42,7 @@ func (s *switchGenerationSuite) TestRunCommandCurrent(c *gc.C) {
 	c.Assert(details.ModelGeneration, gc.Equals, coremodel.GenerationMaster)
 }
 
-func (s *switchGenerationSuite) TestRunCommandNextGenExists(c *gc.C) {
+func (s *checkoutSuite) TestRunCommandNextGenExists(c *gc.C) {
 	ctrl, api := setUpSwitchMocks(c)
 	defer ctrl.Finish()
 
@@ -58,7 +58,7 @@ func (s *switchGenerationSuite) TestRunCommandNextGenExists(c *gc.C) {
 	c.Assert(details.ModelGeneration, gc.Equals, s.branchName)
 }
 
-func (s *switchGenerationSuite) TestRunCommandNextNoGenError(c *gc.C) {
+func (s *checkoutSuite) TestRunCommandNextNoGenError(c *gc.C) {
 	ctrl, api := setUpSwitchMocks(c)
 	defer ctrl.Finish()
 
@@ -68,11 +68,11 @@ func (s *switchGenerationSuite) TestRunCommandNextNoGenError(c *gc.C) {
 	c.Assert(err, gc.ErrorMatches, `this model has no active branch "`+s.branchName+`"`)
 }
 
-func (s *switchGenerationSuite) runInit(args ...string) error {
+func (s *checkoutSuite) runInit(args ...string) error {
 	return cmdtesting.InitCommand(model.NewSwitchGenerationCommandForTest(nil, s.store), args)
 }
 
-func (s *switchGenerationSuite) runCommand(c *gc.C, api model.SwitchGenerationCommandAPI, args ...string) (*cmd.Context, error) {
+func (s *checkoutSuite) runCommand(c *gc.C, api model.SwitchGenerationCommandAPI, args ...string) (*cmd.Context, error) {
 	return cmdtesting.RunCommand(c, model.NewSwitchGenerationCommandForTest(api, s.store), args...)
 }
 
