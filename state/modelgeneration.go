675039c67b5c5a9baca6f732868ad99999430bd4
Joseph Phillips <joe.phillips@canonical.com>
2019-04-11 12:15:00 +0200

    Adds Branches method to state for retrieving all active branches.

--- state/modelgeneration.go
+++ state/modelgeneration.go
@@ -526,6 +526,29 @@ func insertGenerationTxnOps(id, branchName, userName string, now *time.Time) []t
 	}
 }
 
+// Branches returns all "in-flight" branches for the model.
+func (m *Model) Branches() ([]*Generation, error) {
+	b, err := m.st.Branches()
+	return b, errors.Trace(err)
+}
+
+// Branches returns all "in-flight" branches.
+func (st *State) Branches() ([]*Generation, error) {
+	col, closer := st.db().GetCollection(generationsC)
+	defer closer()
+
+	var docs []generationDoc
+	if err := col.Find(bson.M{"completed": 0}).All(&docs); err != nil {
+		return nil, errors.Trace(err)
+	}
+
+	branches := make([]*Generation, len(docs))
+	for i, d := range docs {
+		branches[i] = newGeneration(st, &d)
+	}
+	return branches, nil
+}
+
 // Branch retrieves the generation with the the input branch name from the
 // collection of not-yet-completed generations.
 func (m *Model) Branch(name string) (*Generation, error) {
@@ -547,9 +570,8 @@ func (st *State) getBranchDoc(name string) (*generationDoc, error) {
 	col, closer := st.db().GetCollection(generationsC)
 	defer closer()
 
-	var err error
 	doc := &generationDoc{}
-	err = col.Find(bson.M{
+	err := col.Find(bson.M{
 		"name":      name,
 		"completed": 0,
 	}).One(doc)
