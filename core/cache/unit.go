6adba511811bc294a014bfafb1cae049fb2dda63
Heather Lanigan <heather.lanigan@canonical.com>
2019-04-17 15:25:06 -0400

    Improvements to MachineAppLXDProfileWatcher, move setup of initial data to init() called by newMachineAppLXDProfileWatcher() after subscribing to events.
    Split modelUnitLXDProfileChange event into modelUnitLXDProfileAdd
    and modelUnitLXDProfileRemove to process separately.

--- core/cache/unit.go
+++ core/cache/unit.go
@@ -79,8 +79,8 @@ func (u *Unit) setDetails(details UnitChange) {
 
 	machineChange := u.details.MachineId != details.MachineId
 	u.details = details
-	if machineChange {
-		u.hub.Publish(u.modelTopic(modelUnitLXDProfileChange), u)
+	if machineChange || u.details.Subordinate {
+		u.hub.Publish(u.modelTopic(modelUnitLXDProfileAdd), u)
 	}
 
 	// TODO (manadart 2019-02-11): Maintain hash and publish changes.
