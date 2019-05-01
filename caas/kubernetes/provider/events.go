008022e7c34141b4a0309f29a1a51515d6839663
Kelvin Liu <kelvin.liu@canonical.com>
2019-05-01 18:46:06 +1000

    Rebuild lock file

--- caas/kubernetes/provider/events.go
+++ caas/kubernetes/provider/events.go
@@ -6,7 +6,7 @@ package provider
 import (
 	"github.com/juju/errors"
 	core "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/apis/meta/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/fields"
 )
 
