2734a57fa0fb5fa397c8562e977b0a16b4198d2f
Anastasia Macmood <anastasia.macmood@canonical.com>
2019-05-29 11:53:25 +1000

    organise imports

--- state/modelsummaries_test.go
+++ state/modelsummaries_test.go
@@ -5,7 +5,6 @@ package state_test
 
 import (
 	"fmt"
-	"github.com/juju/juju/cloud"
 	"sort"
 	"time"
 
@@ -15,6 +14,7 @@ import (
 	"gopkg.in/juju/names.v2"
 	"gopkg.in/mgo.v2/bson"
 
+	"github.com/juju/juju/cloud"
 	"github.com/juju/juju/core/status"
 	"github.com/juju/juju/instance"
 	"github.com/juju/juju/permission"
