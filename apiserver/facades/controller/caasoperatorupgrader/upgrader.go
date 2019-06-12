a16199bed2134f0a023d8d50d073417cb2fe547a
Kelvin Liu <kelvin.liu@canonical.com>
2019-06-12 16:46:19 +1000

    Add a new facade OperatorVersion to fetch operator version for a specified application

--- apiserver/facades/controller/caasoperatorupgrader/upgrader.go
+++ apiserver/facades/controller/caasoperatorupgrader/upgrader.go
@@ -48,6 +48,33 @@ func NewCAASOperatorUpgraderAPI(
 	}, nil
 }
 
+// OperatorVersion returns operator version for specified application.
+func (api *API) OperatorVersion(args params.Entities) (params.VersionResults, error) {
+	result := make([]params.VersionResult, len(args.Entities))
+	for i, entity := range args.Entities {
+		tag, err := names.ParseTag(entity.Tag)
+		if err != nil {
+			result[i].Error = common.ServerError(common.ErrPerm)
+			continue
+		}
+		err = common.ErrPerm
+		if api.auth.AuthOwner(tag) {
+			*result[i].Version, err = api.broker.OperatorVersion(tagToAppName(tag))
+		}
+		result[i].Error = common.ServerError(err)
+	}
+	return params.VersionResults{Results: result}, nil
+}
+
+func tagToAppName(t names.Tag) string {
+	appName := t.Id()
+	// Machines representing controllers really mean the controller operator.
+	if t.Kind() == names.MachineTagKind {
+		appName = bootstrap.ControllerModelName
+	}
+	return appName
+}
+
 // UpgradeOperator upgrades the operator for the specified agents.
 func (api *API) UpgradeOperator(arg params.KubernetesUpgradeArg) (params.ErrorResult, error) {
 	serverErr := func(err error) params.ErrorResult {
@@ -60,12 +87,7 @@ func (api *API) UpgradeOperator(arg params.KubernetesUpgradeArg) (params.ErrorRe
 	if !api.auth.AuthOwner(tag) {
 		return serverErr(common.ErrPerm), nil
 	}
-	appName := tag.Id()
-
-	// Machines representing controllers really mean the controller operator.
-	if tag.Kind() == names.MachineTagKind {
-		appName = bootstrap.ControllerModelName
-	}
+	appName := tagToAppName(tag)
 	logger.Debugf("upgrading caas app %v", appName)
 	err = api.broker.Upgrade(appName, arg.Version)
 	if err != nil {
