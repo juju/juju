0dd2183bdea183bfd33cfcbbc54d2e310f38a661
Joseph Phillips <joe.phillips@canonical.com>
2019-06-20 13:53:24 +0200

    Adds ConfigSettings method to cached unit for retrieving charm config.

--- core/cache/unit.go
+++ core/cache/unit.go
@@ -6,7 +6,12 @@ package cache
 import (
 	"fmt"
 
+	"github.com/juju/collections/set"
+	"github.com/juju/juju/core/settings"
+
 	"github.com/juju/errors"
+	"gopkg.in/juju/charm.v6"
+
 	"github.com/juju/juju/core/network"
 )
 
@@ -64,6 +69,55 @@ func (u *Unit) CharmURL() string {
 // Ports returns the exposed ports for the unit.
 func (u *Unit) Ports() []network.Port {
 	return u.details.Ports
+}
+
+// Config settings returns the effective charm configuration for this unit
+// taking into account whether it is tracking a model branch.
+func (u *Unit) ConfigSettings() (charm.Settings, error) {
+	appName := u.details.Application
+	app, err := u.model.Application(appName)
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+	cfg := app.Config()
+	if cfg == nil {
+		cfg = make(map[string]interface{})
+	}
+
+	// Apply any branch-based deltas to the master settings
+	var deltas settings.ItemChanges
+	for _, b := range u.model.Branches() {
+		if units := b.AssignedUnits()[appName]; len(units) > 0 {
+			if set.NewStrings(units...).Contains(u.details.Name) {
+				deltas = b.AppConfig(appName)
+				break
+			}
+		}
+	}
+
+	for _, delta := range deltas {
+		switch {
+		case delta.IsAddition(), delta.IsModification():
+			cfg[delta.Key] = delta.NewValue
+		case delta.IsDeletion():
+			delete(cfg, delta.Key)
+		}
+	}
+
+	// Fill in any empty values with charm defaults.
+	ch, err := u.model.Charm(u.details.CharmURL)
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+	charmDefaults := ch.DefaultConfig()
+
+	for k, v := range charmDefaults {
+		if _, ok := cfg[k]; !ok {
+			cfg[k] = v
+		}
+	}
+
+	return cfg, nil
 }
 
 // WatchConfigSettings returns a new watcher that will notify when the
