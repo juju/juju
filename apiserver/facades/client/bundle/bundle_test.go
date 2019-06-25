0b979718766958197d1035a601e6e36be389f6e8
Achilleas Anagnostopoulos <achilleas.anagnostopoulos@canonical.com>
2019-06-25 10:21:11 +0100

    Include ACL in export-bundle output
    Additionally, this commit also updates the bundle exporting code to
    suppress the offer list if the cmr-aware-bundles feature flag is not
    set. This ensures that older clients connecting to a newer controller
    will not be getting the offer list till all cmr-related changes have
    landed.

--- apiserver/facades/client/bundle/bundle_test.go
+++ apiserver/facades/client/bundle/bundle_test.go
@@ -15,6 +15,7 @@ import (
 	"github.com/juju/juju/apiserver/facades/client/bundle"
 	"github.com/juju/juju/apiserver/params"
 	apiservertesting "github.com/juju/juju/apiserver/testing"
+	"github.com/juju/juju/feature"
 	coretesting "github.com/juju/juju/testing"
 )
 
@@ -542,6 +543,7 @@ applications:
 }
 
 func (s *bundleSuite) TestExportBundleWithApplicationOffers(c *gc.C) {
+	s.SetFeatureFlags(feature.CMRAwareBundles)
 	s.st.model = description.NewModel(description.ModelArgs{Owner: names.NewUserTag("magic"),
 		Config: map[string]interface{}{
 			"name": "awesome",
@@ -560,7 +562,12 @@ func (s *bundleSuite) TestExportBundleWithApplicationOffers(c *gc.C) {
 	_ = app.AddOffer(description.ApplicationOfferArgs{
 		OfferName: "my-offer",
 		Endpoints: []string{"endpoint-1", "endpoint-2"},
+		ACL: map[string]string{
+			"admin": "admin",
+			"foo":   "consume",
+		},
 	})
+
 	_ = app.AddOffer(description.ApplicationOfferArgs{
 		OfferName: "my-other-offer",
 		Endpoints: []string{"endpoint-1", "endpoint-2"},
@@ -585,6 +592,9 @@ applications:
         endpoints:
         - endpoint-1
         - endpoint-2
+        acl:
+          admin: admin
+          foo: consume
       my-other-offer:
         endpoints:
         - endpoint-1
