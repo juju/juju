d68d9d41ea6b03fcb689d1d5cb6692209fb3b264
Achilleas Anagnostopoulos <achilleas.anagnostopoulos@canonical.com>
2019-06-27 18:11:58 +0100

    Process grant-offer-access changes when deploying bundles

--- cmd/juju/application/bundle.go
+++ cmd/juju/application/bundle.go
@@ -129,6 +129,7 @@ type bundleDeploySpec struct {
 	bundleStorage       map[string]map[string]storage.Constraints
 	bundleDevices       map[string]map[string]devices.Constraints
 
+	targetModelName string
 	targetModelUUID string
 	controllerName  string
 	accountUser     string
@@ -242,7 +243,8 @@ type bundleHandler struct {
 	// bundle.
 	warnedLXC bool
 
-	// The UUID of the model where the bundle is about to be deployed.
+	// The name and UUID of the model where the bundle is about to be deployed.
+	targetModelName string
 	targetModelUUID string
 
 	// Controller name required for consuming a offer when deploying a bundle.
@@ -276,6 +278,7 @@ func makeBundleHandler(spec bundleDeploySpec) *bundleHandler {
 		macaroons:     make(map[*charm.URL]*macaroon.Macaroon),
 		channels:      make(map[*charm.URL]csparams.Channel),
 
+		targetModelName: spec.targetModelName,
 		targetModelUUID: spec.targetModelUUID,
 		controllerName:  spec.controllerName,
 		accountUser:     spec.accountUser,
@@ -388,14 +391,15 @@ func (h *bundleHandler) getChanges() error {
 		return errors.Trace(err)
 	}
 
-	// Filter create offer changes if the feature flag is not enabled. We
+	// Filter cmr-related changes if the feature flag is not enabled. We
 	// need to do it here rather in handleChanges() as the bundle handler
 	// will also iterate the list to print out a changelog.
 	if !featureflag.Enabled(feature.CMRAwareBundles) {
 		var filtered []bundlechanges.Change
 		for _, ch := range changes {
 			if ch.Method() == "createOffer" ||
-				ch.Method() == "consumeOffer" {
+				ch.Method() == "consumeOffer" ||
+				ch.Method() == "grantOfferAccess" {
 				continue
 			}
 
@@ -459,6 +463,8 @@ func (h *bundleHandler) handleChanges() error {
 			err = h.createOffer(change)
 		case *bundlechanges.ConsumeOfferChange:
 			err = h.consumeOffer(change)
+		case *bundlechanges.GrantOfferAccessChange:
+			err = h.grantOfferAccess(change)
 		default:
 			return errors.Errorf("unknown change type: %T", change)
 		}
@@ -1154,6 +1160,22 @@ func (h *bundleHandler) consumeOffer(change *bundlechanges.ConsumeOfferChange) e
 	return nil
 }
 
+// grantOfferAccess grants access to an offer.
+func (h *bundleHandler) grantOfferAccess(change *bundlechanges.GrantOfferAccessChange) error {
+	if h.dryRun {
+		return nil
+	}
+
+	p := change.Params
+
+	offerURL := fmt.Sprintf("%s.%s", h.targetModelName, p.Offer)
+	if err := h.api.GrantOffer(p.User, p.Access, offerURL); err != nil && !isUserAlreadyHasAccessErr(err) {
+
+		return errors.Annotatef(err, "cannot grant %s access to user %s on offer %s", p.Access, p.User, offerURL)
+	}
+	return nil
+}
+
 // applicationsForMachineChange returns the names of the applications for which an
 // "addMachine" change is required, as adding machines is required to place
 // units, and units belong to applications.
@@ -1757,4 +1779,12 @@ func applicationConfigValue(key string, valueMap interface{}) (interface{}, erro
 func applicationRequiresTrust(appSpec *charm.ApplicationSpec) bool {
 	optRequiresTrust := appSpec.Options != nil && appSpec.Options["trust"] == true
 	return appSpec.RequiresTrust || optRequiresTrust
+}
+
+// isUserAlreadyHasAccessErr returns true if err indicates that the user
+// already has access to an offer. Unfortunately, the server does not set a
+// status code for this error so we need to fall back to a hacky string
+// comparison to be compatible with older controllers.
+func isUserAlreadyHasAccessErr(err error) bool {
+	return err != nil && strings.Contains(err.Error(), "user already has")
 }
