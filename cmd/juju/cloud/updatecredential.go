f0606ccb4aaee8dec02e129709dbcbaa01224ce2
Anastasia Macmood <anastasia.macmood@canonical.com>
2019-07-15 11:45:05 +1000

    Organise imports.

--- cmd/juju/cloud/updatecredential.go
+++ cmd/juju/cloud/updatecredential.go
@@ -5,7 +5,6 @@ package cloud
 
 import (
 	"fmt"
-	"github.com/juju/juju/environs"
 	"io/ioutil"
 
 	"github.com/juju/cmd"
@@ -19,6 +18,7 @@ import (
 	jujucmd "github.com/juju/juju/cmd"
 	"github.com/juju/juju/cmd/juju/common"
 	"github.com/juju/juju/cmd/modelcmd"
+	"github.com/juju/juju/environs"
 	"github.com/juju/juju/jujuclient"
 )
 
