12e428763e8530facaa15e2f933f25e7b9aec107
Kelvin Liu <kelvin.liu@canonical.com>
2019-07-16 15:16:43 +1000

    Set resource UID as precondtion in DeleteOptions for all RBAC single resourece deletion

--- caas/kubernetes/provider/storage_test.go
+++ caas/kubernetes/provider/storage_test.go
@@ -103,9 +103,9 @@ func (s *storageSuite) TestDestroyVolumes(c *gc.C) {
 				Spec: core.PersistentVolumeSpec{
 					ClaimRef: &core.ObjectReference{Namespace: "test", Name: "vol-1-pvc"},
 				}}, nil),
-		s.mockPersistentVolumeClaims.EXPECT().Delete("vol-1-pvc", s.deleteOptions(v1.DeletePropagationForeground)).Times(1).
+		s.mockPersistentVolumeClaims.EXPECT().Delete("vol-1-pvc", s.deleteOptions(v1.DeletePropagationForeground, nil)).Times(1).
 			Return(s.k8sNotFoundError()),
-		s.mockPersistentVolumes.EXPECT().Delete("vol-1", s.deleteOptions(v1.DeletePropagationForeground)).Times(1).
+		s.mockPersistentVolumes.EXPECT().Delete("vol-1", s.deleteOptions(v1.DeletePropagationForeground, nil)).Times(1).
 			Return(nil),
 	)
 
@@ -128,9 +128,9 @@ func (s *storageSuite) TestDestroyVolumesNotFoundIgnored(c *gc.C) {
 				Spec: core.PersistentVolumeSpec{
 					ClaimRef: &core.ObjectReference{Namespace: "test", Name: "vol-1-pvc"},
 				}}, nil),
-		s.mockPersistentVolumeClaims.EXPECT().Delete("vol-1-pvc", s.deleteOptions(v1.DeletePropagationForeground)).Times(1).
+		s.mockPersistentVolumeClaims.EXPECT().Delete("vol-1-pvc", s.deleteOptions(v1.DeletePropagationForeground, nil)).Times(1).
 			Return(s.k8sNotFoundError()),
-		s.mockPersistentVolumes.EXPECT().Delete("vol-1", s.deleteOptions(v1.DeletePropagationForeground)).Times(1).
+		s.mockPersistentVolumes.EXPECT().Delete("vol-1", s.deleteOptions(v1.DeletePropagationForeground, nil)).Times(1).
 			Return(s.k8sNotFoundError()),
 	)
 
