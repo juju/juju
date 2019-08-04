71e6f7950e6b90724395f798a99ce4e379e0449f
Tim Penhey <tim@penhey.net>
2019-08-05 08:11:46 +1200

    Remove old watcher code.

--- state/watcher/export_test.go
+++ state/watcher/export_test.go
@@ -3,22 +3,12 @@
 
 package watcher
 
-import (
-	"gopkg.in/mgo.v2"
-
-	"github.com/juju/juju/mongo"
-)
-
 const (
 	TxnWatcherStarting   = txnWatcherStarting
 	TxnWatcherSyncErr    = txnWatcherSyncErr
 	TxnWatcherCollection = txnWatcherCollection
 	TxnWatcherShortWait  = txnWatcherShortWait
 )
-
-func NewTestWatcher(changelog *mgo.Collection, iteratorFunc func() mongo.Iterator) *Watcher {
-	return newWatcher(changelog, iteratorFunc)
-}
 
 func NewTestHubWatcher(hub HubSource, clock Clock, modelUUID string, logger Logger) (*HubWatcher, <-chan struct{}) {
 	return newHubWatcher(hub, clock, modelUUID, logger)
