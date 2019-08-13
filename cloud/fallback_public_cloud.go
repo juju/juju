0136a5ade41dc93134b0e295796f9b858b8ec240
Ian Booth <ian.booth@canonical.com>
2019-08-14 09:00:51 +1000

    Add new Azure South Africa North and West regions

--- cloud/fallback_public_cloud.go
+++ cloud/fallback_public_cloud.go
@@ -221,7 +221,15 @@ clouds:
       francecentral:
         endpoint: https://management.azure.com
         storage-endpoint: https://core.windows.net
-        identity-endpoint: https://graph.windows.net       
+        identity-endpoint: https://graph.windows.net
+      southafricanorth:
+        endpoint: https://management.azure.com
+        storage-endpoint: https://core.windows.net
+        identity-endpoint: https://graph.windows.net
+      southafricawest:
+        endpoint: https://management.azure.com
+        storage-endpoint: https://core.windows.net
+        identity-endpoint: https://graph.windows.net
   azure-china:
     type: azure
     description: Microsoft Azure China
@@ -327,5 +335,4 @@ clouds:
         endpoint: https://compute.em2.oraclecloud.com
       em3:
         endpoint: https://compute.em3.oraclecloud.com
-
 `
