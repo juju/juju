6a87a51ed0f51226cdd11ee052c28634fc64216f
John Arbash Meinel <john@arbash-meinel.com>
2019-08-26 09:29:28 +0400

    Get a test FAIL if the package doesn't build.
    It seems that when running `go test ./...`
    if a subdirectory fails to build, but we have no tests in
    that directory the failure we get is:
    ```
    ./schemadocs.go:294:10: undefined: strings.ReplaceAll
    ```
    
    However there isn't anything that clearly indicates a failure. By adding
    a dummy Test function we get:
    ```
    ./schemadocs.go:294:10: undefined: strings.ReplaceAll
    FAIL    github.com/juju/juju/scripts/schemadocs [build failed]
    ```
    
    Which at least means we can find the issue in the test run output.

---
+++ scripts/schemadocs/schema_test.go
@@ -0,0 +1,12 @@
+// Copyright 2019 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package main_test
+
+import (
+	"testing"
+)
+
+// TestPackage just ensures that this package will build
+func TestPackage(t *testing.T) {
+}
