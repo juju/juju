fba0847396fc49e2a94736b31d98924343f83668
Anastasia Macmood <anastasia.macmood@canonical.com>
2019-09-04 11:53:32 +1000

    Add cloud adds a default region when needed server-side and multi-cloud feature flag no longer needed.

--- apiserver/facades/client/cloud/cloud.go
+++ apiserver/facades/client/cloud/cloud.go
@@ -23,7 +23,6 @@ import (
 	"github.com/juju/juju/cloud"
 	"github.com/juju/juju/environs"
 	environscontext "github.com/juju/juju/environs/context"
-	"github.com/juju/juju/feature"
 	"github.com/juju/juju/permission"
 	"github.com/juju/juju/state"
 )
@@ -1002,16 +1001,6 @@ func (api *CloudAPI) AddCloud(cloudArgs params.AddCloudArgs) error {
 	} else if !isAdmin {
 		return common.ServerError(common.ErrPerm)
 	}
-	cfg, err := api.backend.ControllerConfig()
-	if err != nil {
-		return errors.Trace(err)
-	}
-	if !cfg.Features().Contains(feature.MultiCloud) {
-		if cloudArgs.Cloud.Type != string(provider.K8s_ProviderType) {
-			return errors.Errorf(
-				"feature flag %q needs to be enabled to add a %q cloud", feature.MultiCloud, cloudArgs.Cloud.Type)
-		}
-	}
 
 	if cloudArgs.Cloud.Type != string(provider.K8s_ProviderType) {
 		// All non-k8s cloud need to go through whitelist.
@@ -1030,7 +1019,14 @@ func (api *CloudAPI) AddCloud(cloudArgs params.AddCloudArgs) error {
 			logger.Infof("force adding cloud %q of type %q to controller bootstrapped on cloud type %q", cloudArgs.Name, cloudArgs.Cloud.Type, controllerCloud.Type)
 		}
 	}
-	err = api.backend.AddCloud(common.CloudFromParams(cloudArgs.Name, cloudArgs.Cloud), api.apiUser.Name())
+
+	aCloud := common.CloudFromParams(cloudArgs.Name, cloudArgs.Cloud)
+	// All clouds must have at least one 'default' region, lp#1819409.
+	if len(aCloud.Regions) == 0 {
+		aCloud.Regions = []cloud.Region{{Name: "default"}}
+	}
+
+	err = api.backend.AddCloud(aCloud, api.apiUser.Name())
 	return errors.Trace(err)
 }
 
