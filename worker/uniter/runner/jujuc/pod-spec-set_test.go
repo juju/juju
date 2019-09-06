aaddfc0a142fb74eaaa3ef9ec47a7eb5f4d770e8
Kelvin Liu <kelvin.liu@canonical.com>
2019-09-06 14:38:49 +1000

    Make podspec-set cmd support --k8s-resources;

--- worker/uniter/runner/jujuc/pod-spec-set_test.go
+++ worker/uniter/runner/jujuc/pod-spec-set_test.go
@@ -22,10 +22,37 @@ type ContainerspecSetSuite struct {
 
 var _ = gc.Suite(&ContainerspecSetSuite{})
 
-var containerSpecYaml = `
+var (
+	containerSpecYaml = `
 containerspec:
   foo: bar
 `[1:]
+
+	k8sResourcesYaml = `
+kubernetesResources:
+  pod:
+    restartPolicy: OnFailure
+    activeDeadlineSeconds: 10
+    terminationGracePeriodSeconds: 20
+    securityContext:
+      runAsNonRoot: true
+      supplementalGroups: [1,2]
+    priority: 30
+    readinessGates:
+      - conditionType: PodScheduled
+    dnsPolicy: ClusterFirstWithHostNet
+  secrets:
+    - name: build-robot-secret
+      annotations:
+          kubernetes.io/service-account.name: build-robot
+      type: kubernetes.io/service-account-token
+      stringData:
+          config.yaml: |-
+              apiUrl: "https://my.api.com/api/v1"
+              username: fred
+              password: shhhh
+`[1:]
+)
 
 var containerSpecSetInitTests = []struct {
 	args []string
@@ -52,7 +79,7 @@ func (s *ContainerspecSetSuite) TestHelp(c *gc.C) {
 	code := cmd.Main(jujuc.NewJujucCommandWrappedForTest(com), ctx, []string{"--help"})
 	c.Assert(code, gc.Equals, 0)
 	expectedHelp := "" +
-		"Usage: pod-spec-set [options] --file <pod spec file>\n" +
+		"Usage: pod-spec-set [options] --file <pod spec file> [--k8s-resources <k8s pod spec file>]\n" +
 		"\n" +
 		"Summary:\n" +
 		"set pod spec information\n" +
@@ -60,6 +87,8 @@ func (s *ContainerspecSetSuite) TestHelp(c *gc.C) {
 		"Options:\n" +
 		"--file  (= -)\n" +
 		"    file containing pod spec\n" +
+		"--k8s-resources  (= )\n" +
+		"    file containing k8s pod spec\n" +
 		"\n" +
 		"Details:\n" +
 		"Sets configuration data to use for a pod.\n" +
@@ -84,25 +113,37 @@ func (s *ContainerspecSetSuite) TestContainerSpecSetNoData(c *gc.C) {
 }
 
 func (s *ContainerspecSetSuite) TestContainerSpecSet(c *gc.C) {
-	s.assertContainerSpecSet(c, "specfile.yaml")
+	s.assertContainerSpecSet(c, "specfile.yaml", false)
 }
 
 func (s *ContainerspecSetSuite) TestContainerSpecSetStdIn(c *gc.C) {
-	s.assertContainerSpecSet(c, "-")
+	s.assertContainerSpecSet(c, "-", false)
 }
 
-func (s *ContainerspecSetSuite) assertContainerSpecSet(c *gc.C, filename string) {
+func (s *ContainerspecSetSuite) TestContainerSpecSetWithK8sResource(c *gc.C) {
+	s.assertContainerSpecSet(c, "specfile.yaml", true)
+}
+
+func (s *ContainerspecSetSuite) TestContainerSpecSetStdInWithK8sResource(c *gc.C) {
+	s.assertContainerSpecSet(c, "-", true)
+}
+
+func (s *ContainerspecSetSuite) assertContainerSpecSet(c *gc.C, filename string, withK8sResource bool) {
 	hctx := s.GetHookContext(c, -1, "")
-	com, args, ctx := s.initCommand(c, hctx, containerSpecYaml, filename)
+	com, args, ctx := s.initCommand(c, hctx, containerSpecYaml, filename, withK8sResource)
 	code := cmd.Main(jujuc.NewJujucCommandWrappedForTest(com), ctx, args)
 	c.Check(code, gc.Equals, 0)
 	c.Assert(bufferString(ctx.Stderr), gc.Equals, "")
 	c.Assert(bufferString(ctx.Stdout), gc.Equals, "")
-	c.Assert(hctx.info.ContainerSpec, gc.Equals, containerSpecYaml)
+	expectedSpecYaml := containerSpecYaml
+	if withK8sResource {
+		expectedSpecYaml += k8sResourcesYaml
+	}
+	c.Assert(hctx.info.ContainerSpec, gc.Equals, expectedSpecYaml)
 }
 
 func (s *ContainerspecSetSuite) initCommand(
-	c *gc.C, hctx jujuc.Context, yaml string, filename string,
+	c *gc.C, hctx jujuc.Context, yaml string, filename string, withK8sResource bool,
 ) (cmd.Command, []string, *cmd.Context) {
 	com, err := jujuc.NewCommand(hctx, "pod-spec-set")
 	c.Assert(err, jc.ErrorIsNil)
@@ -113,9 +154,16 @@ func (s *ContainerspecSetSuite) initCommand(
 		ctx.Stdin = bytes.NewBufferString(yaml)
 	} else if filename != "" {
 		filename = filepath.Join(c.MkDir(), filename)
-		args = append(args, "--file", filename)
 		err := ioutil.WriteFile(filename, []byte(yaml), 0644)
 		c.Assert(err, jc.ErrorIsNil)
+		args = append(args, "--file", filename)
+	}
+	if withK8sResource {
+		k8sResourceFileName := "k8sresources.yaml"
+		k8sResourceFileName = filepath.Join(c.MkDir(), k8sResourceFileName)
+		err := ioutil.WriteFile(k8sResourceFileName, []byte(k8sResourcesYaml), 0644)
+		c.Assert(err, jc.ErrorIsNil)
+		args = append(args, "--k8s-resources", k8sResourceFileName)
 	}
 	return jujuc.NewJujucCommandWrappedForTest(com), args, ctx
 }
