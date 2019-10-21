4638b98bb94ef31756d8ac9719a0840ff410e470
Kelvin Liu <kelvin.liu@canonical.com>
2019-10-21 17:34:34 +1100

    Ensure UUID for caas credentials;

--- cmd/juju/caas/credential.go
+++ cmd/juju/caas/credential.go
@@ -2,3 +2,48 @@
 // Licensed under the AGPLv3, see LICENCE file for details.
 
 package caas
+
+import (
+	"github.com/juju/errors"
+	"github.com/juju/utils"
+
+	jujucloud "github.com/juju/juju/cloud"
+)
+
+const caasCrendentialLabelKeyName = "juju-caas-credential-uuid"
+
+func ensureCredentialLabel(
+	credentialName string,
+	existingCredential, newcredential jujucloud.Credential,
+) (_ *jujucloud.Credential, err error) {
+
+	newAttr := newcredential.Attributes()
+	if newAttr == nil {
+		return nil, errors.NotValidf("empty credential %q", credentialName)
+	}
+
+	var credUUID string
+	if existingCredential.Attributes() != nil {
+		credUUID = existingCredential.Attributes()[caasCrendentialLabelKeyName]
+	}
+	if credUUID == "" {
+		uuid, err := utils.NewUUID()
+		if err != nil {
+			return nil, errors.Trace(err)
+		}
+		credUUID = uuid.String()
+	}
+
+	newAttr[caasCrendentialLabelKeyName] = credUUID
+
+	cred := jujucloud.NewNamedCredential(
+		credentialName, newcredential.AuthType(), newAttr, newcredential.Revoked,
+	)
+	return &cred, nil
+}
+
+func getExistingCredential() (jujucloud.Credential, error) {
+	// TODO: return existing  local/remote credential!!!!!
+	// how to handle conflict between local and remote cred??????????
+	return jujucloud.Credential{}, nil
+}
