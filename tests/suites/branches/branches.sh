2678a0172ec2c47170a47e79c5860f6535f04ad7
Simon Richardson <stickupkid@gmail.com>
2019-10-25 15:11:09 +0100

    Adding integration tests for branches
    The following adds a full integration test for the branches code. We
    can be more confident in changes to the code and ensure we don't break
    anything in the future.

---
+++ tests/suites/branches/branches.sh
@@ -0,0 +1,44 @@
+run_branch() {
+    # Echo out to ensure nice output to the test suite.
+    echo
+
+    # The following ensures that a bootstrap juju exists
+    file="${TEST_DIR}/test-branches.txt"
+    ensure "branches" "${file}"
+
+    juju branch | grep -q 'Active branch is "master"'
+
+    juju deploy redis
+
+    wait_for "redis" ".applications | select(.redis | .units | .[\"redis/0\"] | .[\"juju-status\"] | .current == \"idle\") | keys[0]"
+
+    set -ux
+
+    juju add-branch test-branch
+    juju branch | grep -q 'Active branch is "test-branch"'
+
+    juju config redis password=pass --branch test-branch
+    juju config redis password --branch test-branch | grep -q "pass"
+    juju config redis password --branch master | wc -c | grep -q "0"
+
+    juju commit test-branch | grep -q 'Active branch set to "master"'
+    juju config redis password | grep -q "pass"
+
+    # Clean up!
+    destroy_model "branches"
+}
+
+test_branch() {
+    if [ -n "$(skip 'test_branch')" ]; then
+        echo "==> SKIP: Asked to skip branch tests"
+        return
+    fi
+
+    (
+        set_verbosity
+
+        cd .. || exit
+
+        run "run_branch"
+    )
+}
