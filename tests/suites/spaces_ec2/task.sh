f3e3d13b18678de33c5129567f484e7324f8e49d
Achilleas Anagnostopoulos <achilleas.anagnostopoulos@canonical.com>
2019-11-05 17:13:42 +0000

    Add ec2 integration tests for spaces

---
+++ tests/suites/spaces_ec2/task.sh
@@ -0,0 +1,37 @@
+test_spaces_ec2() {
+    if [ "$(skip 'test_spaces_ec2')" ]; then
+        echo "==> TEST SKIPPED: space tests (EC2)"
+        return
+    fi
+
+    set_verbosity
+
+    echo "==> Checking for dependencies"
+    check_dependencies juju aws
+
+    echo "==> Checking for EC2 prerequisites"
+    check_prerequisites_for_space_tests
+
+    file="${TEST_DIR}/test-spaces-ec2.txt"
+
+    bootstrap "test-spaces-ec2" "${file}"
+
+    test_upgrade_charm_with_bind
+    test_juju_bind
+
+    destroy_controller "test-spaces-ec2"
+}
+
+check_prerequisites_for_space_tests() {
+  match_count=$(aws ec2 describe-subnets --filters Name=cidr-block,Values=172.31.254.0/24 | jq '.Subnets | length')
+  if [[ "$match_count" != "1" ]]; then
+	  echo $(red "To run these tests you need to create a subnet with name \"isolated\" and CIDR \"172.31.254/24\"")
+	  exit 1
+  fi
+  	
+  if_count=$(aws ec2 describe-network-interfaces --filters Name=tag:nic-type,Values=hotpluggable | jq '.NetworkInterfaces[] | select(.["PrivateIpAddress"] | startswith("172.31.254.")) | .NetworkInterfaceId')
+  if [[ -z "$if_count" ]]; then
+	  echo $(red "To run these tests you need to create a network interface on the isolated subnet (CIDR \"172.31.254/24\") and tag it with \"nic-type:hotpluggable\"")
+	  exit 1
+  fi
+}
---
+++ tests/suites/spaces_ec2/util.sh
@@ -0,0 +1,79 @@
+# add_multi_nic_machine()
+#
+# Create a new machine, wait for it to boot and hotplug a pre-allocated
+# network interface which has been tagged: "nic-type: hotpluggable".
+#
+# Then, patch the netplan settings for the new interface, apply the new plan,
+# restart the machine agent and wait for juju to detect the new interface 
+# before returning.
+add_multi_nic_machine() {
+  juju add-machine
+  juju_machine_id=$(juju show-machine --format json | jq -r '.["machines"] | keys[0]')
+  echo "[+] waiting for machine ${juju_machine_id} to start..."
+  
+  wait_for_machine_agent_status "$juju_machine_id" "started"
+
+  # Hotplug the second network device to the machine
+  echo "[+] hotplugging second NIC to machine ${juju_machine_id}..."
+  aws ec2 attach-network-interface --device-index 1 \
+  	--network-interface-id $(aws ec2 describe-network-interfaces --filters Name=tag:nic-type,Values=hotpluggable | jq -r '.NetworkInterfaces[0].NetworkInterfaceId') \
+  	--instance-id $(juju show-machine --format json | jq -r ".[\"machines\"] | .[\"${juju_machine_id}\"] | .[\"instance-id\"]")
+  
+  # Add an entry to netplan and apply it so the second interface comes online
+  echo "[+] updating netplan and restarting machine agent"
+  juju ssh ${juju_machine_id} 'sudo sh -c "echo \"            gateway4: `ip route | grep default | cut -d\" \" -f3`\n        ens6:\n            dhcp4: true\n\" >> /etc/netplan/50-cloud-init.yaml"'
+  juju ssh ${juju_machine_id} 'sudo netplan apply'
+  juju ssh ${juju_machine_id} 'sudo systemctl restart jujud-machine-*'
+  
+  # Wait for the interface to be detected by juju
+  echo "[+] waiting for juju to detect added NIC"
+  wait_for_machine_netif_count "$juju_machine_id" "3"
+}
+
+# assert_net_iface_for_endpoint_matches(app_name, endpoint_name, exp_if_name)
+#
+# Verify that the (non-fan) network adapter assigned to the specified endpoint
+# matches the provided value.
+assert_net_iface_for_endpoint_matches() {
+    local app_name endpoint_name exp_if_name
+
+    app_name=${1}
+    endpoint_name=${2}
+    exp_if_name=${3}
+
+    got_if=$(juju run -a ${app_name} "network-get ${endpoint_name}" | grep "interfacename: ens" | awk '{print $2}')
+    if [[ "$got_if" != "$exp_if_name" ]]; then
+	    echo $(red "Expected network interface for ${app_name}:${endpoint_name} to be ${exp_if_name}; got ${got_if}")
+	    exit 1
+    fi
+}
+
+# assert_endpoint_binding_matches(app_name, endpoint_name, exp_space_name)
+#
+# Verify that show-application shows that the specified endpoint is bound to
+# the provided space name.
+assert_endpoint_binding_matches() {
+    local app_name endpoint_name exp_space_name
+
+    app_name=${1}
+    endpoint_name=${2}
+    exp_space_name=${3}
+
+    got=$(juju show-application ${app_name} --format json | jq -r ".[\"${app_name}\"] | .[\"endpoint-bindings\"] | .[\"${endpoint_name}\"]")
+    if [[ "$got" != "$exp_space_name" ]]; then
+	    echo $(red "Expected endpoint \"${endpoint_name}\" in juju show-application ${app_name} to be ${exp_space_name}; got ${got}")
+	    exit 1
+    fi
+}
+
+# get_unit_index(app_name)
+#
+# Lookup and return the unit index for app_name.
+get_unit_index() {
+    local app_name
+
+    app_name=${1}
+
+    index=$(juju status | grep "${app_name}/" | cut -d' ' -f1 | cut -d'/' -f2 | cut -d'*' -f1)
+    echo $index
+}
