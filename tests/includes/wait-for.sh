ce103d6f4ad9145904bc7c749f82b7e3cb8c241b
Achilleas Anagnostopoulos <achilleas.anagnostopoulos@canonical.com>
2019-11-06 11:48:11 +0000

    Add more wait-for-XXX helpers and indent poll output

--- tests/includes/wait-for.sh
+++ tests/includes/wait-for.sh
@@ -12,10 +12,13 @@ wait_for() {
     name=${1}
     query=${2}
 
-    # shellcheck disable=SC2143
+    attempt=0
+    # shellcheck disable=SC2046,SC2143
     until [ "$(juju status --format=json 2> /dev/null | jq "${query}" | grep "${name}")" ]; do
-        juju status --relations
+        echo "[+] (attempt ${attempt}) polling status"
+        juju status --relations 2>&1 | sed 's/^/    | /g'
         sleep 5
+	let attempt=attempt+1
     done
 }
 
@@ -49,4 +52,52 @@ agent_status() {
     unit=$2
 
     echo ".applications[\"$app\"].units[\"$app/$unit\"][\"juju-status\"]"
+}
+
+# wait_for_machine_agent_status blocks until the machine agent for the specified
+# machine instance ID reports the requested status.
+#
+# ```
+# wait_for_machine_agent_status <instance-id> <status>
+#
+# example:
+# wait_for_machine_agent_status "i-1234" "started"
+# ```
+wait_for_machine_agent_status() {
+    local inst_id status
+
+    inst_id=${1}
+    status=${2}
+
+    attempt=0
+    until [ $(juju show-machine --format json | jq -r ".[\"machines\"] | .[\"${inst_id}\"] | .[\"juju-status\"] | .[\"current\"]" | grep "${status}") ]; do
+        echo "[+] (attempt ${attempt}) polling status"
+        juju machines | grep "$inst_id" 2>&1 | sed 's/^/    | /g'
+        sleep 5
+	let attempt=attempt+1
+    done
+}
+
+# wait_for_machine_netif_count blocks until the number of detected network
+# interfaces for the requested machine instance ID becomes equal to the desired
+# value.
+#
+# ```
+# wait_for_machine_netif_count <instance-id> <count>
+#
+# example:
+# wait_for_machine_netif_count "i-1234" "42"
+# ```
+wait_for_machine_netif_count() {
+    local inst_id count
+
+    inst_id=${1}
+    count=${2}
+
+    attempt=0
+    until [ $(juju show-machine --format json | jq -r ".[\"machines\"] | .[\"${inst_id}\"] | .[\"network-interfaces\"] | length" | grep "${count}") ]; do
+	echo "[+] (attempt ${attempt}) network interface count for instance ${inst_id} = "$(juju show-machine --format json | jq -r ".[\"machines\"] | .[\"${inst_id}\"] | .[\"network-interfaces\"] | length")
+        sleep 5
+	let attempt=attempt+1
+    done
 }
