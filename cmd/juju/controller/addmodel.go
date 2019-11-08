f1f36af340fd964a45cde0ea13d10695e7d16e18
Simon Richardson <stickupkid@gmail.com>
2019-11-08 12:25:11 +0000

    Ensure we handle no model names better
    The following handles no model names better, by helping the user move
    forward, rather than dead ending them.
    
    As a driveby I also cleaned up the issue around whitespace.

--- cmd/juju/controller/addmodel.go
+++ cmd/juju/controller/addmodel.go
@@ -66,25 +66,25 @@ type addModelCommand struct {
 const addModelHelpDoc = `
 Adding a model is typically done in order to run a specific workload.
 
-To add a model, you must specify a model name. Model names can be duplicated 
-across controllers but must be unique per user for any given controller. 
-In other words, Alice and Bob can each have their own model called "secret" but 
+To add a model, you must specify a model name. Model names can be duplicated
+across controllers but must be unique per user for any given controller.
+In other words, Alice and Bob can each have their own model called "secret" but
 Alice can have only one model called "secret" in a controller.
-Model names may only contain lowercase letters, digits and hyphens, and 
+Model names may only contain lowercase letters, digits and hyphens, and
 may not start with a hyphen.
 
 To add a model, Juju requires a credential:
 
     * if you have a default (or just one) credential defined at client
      (i.e. in credentials.yaml), then juju will use that;
-    * if you have no default (and multiple) credentials defined at the client, 
+    * if you have no default (and multiple) credentials defined at the client,
      then you must specify one using --credential;
-    * as the admin user you can omit the credential, 
+    * as the admin user you can omit the credential,
      and the credential used to bootstrap will be used.
 
-To add a credential for add-model, use one of the "juju add-credential" or 
-"juju autoload-credentials" commands. These will add credentials 
-to the Juju client, which "juju add-model" will upload to the controller 
+To add a credential for add-model, use one of the "juju add-credential" or
+"juju autoload-credentials" commands. These will add credentials
+to the Juju client, which "juju add-model" will upload to the controller
 as necessary.
 
 You may also supply model-specific configuration as well as a
@@ -94,7 +94,7 @@ are the ones used to create any future resources within the model.
 If no cloud/region is specified, then the model will be deployed to
 the same cloud/region as the controller model. If a region is specified
 without a cloud qualifier, then it is assumed to be in the same cloud
-as the controller model. 
+as the controller model.
 
 Examples:
 
@@ -124,7 +124,7 @@ func (c *addModelCommand) SetFlags(f *gnuflag.FlagSet) {
 
 func (c *addModelCommand) Init(args []string) error {
 	if len(args) == 0 {
-		return errors.New("model name is required")
+		return common.MissingModelNameError("add-model")
 	}
 	c.Name, args = args[0], args[1:]
 
