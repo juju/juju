f54f1c4cf82323ff22852de5a5678f8cde777eaa
Achilleas Anagnostopoulos <achilleas.anagnostopoulos@canonical.com>
2019-11-13 12:45:05 +0000

    Wire instancepoller to invoke WatchModelMachineStartTimes
    This change allows the instancepoller to be notified when the machine
    agent restarts so it can eagerly inspect the provider instance
    information to discover changes to shadow IPs.
    
    This can happen for instance on EC2 if a machine instance is stopped and
    then re-started.
    
    A side-effect of the current way of doing things is that if the machine
    was placed in the long polling group, we wouldn't detect shadow IP
    changes until the machine gets polled again (~10-15min). This detection
    delay prevens us from SSHing into rebooted machines and also seems to
    block `juju upgrade-controller` until the new IP is detected.

--- apiserver/common/modelmachineswatcher.go
+++ apiserver/common/modelmachineswatcher.go
@@ -5,6 +5,7 @@ package common
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/juju/juju/apiserver/facade"
 	"github.com/juju/juju/apiserver/params"
@@ -40,6 +41,27 @@ func (e *ModelMachinesWatcher) WatchModelMachines() (params.StringsWatchResult, 
 		return result, ErrPerm
 	}
 	watch := e.st.WatchModelMachines()
+	// Consume the initial event and forward it to the result.
+	if changes, ok := <-watch.Changes(); ok {
+		result.StringsWatcherId = e.resources.Register(watch)
+		result.Changes = changes
+	} else {
+		err := watcher.EnsureErr(watch)
+		return result, fmt.Errorf("cannot obtain initial model machines: %v", err)
+	}
+	return result, nil
+}
+
+// WatchModelMachineStartTimes watches the non-container machines in the model
+// for changes to the Life or AgentStartTime fields and reports them as a batch
+// after the specified quiesceInterval time has passed without seeing any new
+// change events.
+func (e *ModelMachinesWatcher) WatchModelMachineStartTimes() (params.StringsWatchResult, error) {
+	result := params.StringsWatchResult{}
+	if !e.authorizer.AuthController() {
+		return result, ErrPerm
+	}
+	watch := e.st.WatchModelMachineStartTimes(500 * time.Millisecond)
 	// Consume the initial event and forward it to the result.
 	if changes, ok := <-watch.Changes(); ok {
 		result.StringsWatcherId = e.resources.Register(watch)
