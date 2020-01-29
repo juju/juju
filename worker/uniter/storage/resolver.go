0474258aeace8b339ec91a695c3830be3a66cdb9
Heather Lanigan <heather.lanigan@canonical.com>
2020-01-29 17:03:10 -0500

    Rework storage resolver for CAAS based on now calling install hook.
    Ensure that the start hook is run for CAAS units with storage.
    Add tests for CAAS units, wait for started to attach storage.

--- worker/uniter/storage/resolver.go
+++ worker/uniter/storage/resolver.go
@@ -65,32 +65,51 @@ func (s *storageResolver) NextOp(
 		return nil, errors.Trace(err)
 	}
 
-	// Only IAAS models need to first run the install hook.
-	// For CAAS models, storage is specified in the pod config
-	// and mounted as the pod in started.
-	blockedWaitingForInstall := !localState.Installed && s.modelType == model.IAAS
-
+	// The decision making below with regard to when to run the storage hooks for
+	// the first time after a charm is deployed is applicable only to IAAS models.
+	// For IAAS models, we do not run install hook before any storage provisioned
+	// with the deployment of the unit is available. The presumption is that IAAS
+	// charms may need storage on which to install their workloads.
+	//
+	// For CAAS models, it's different because we need to create the pod before
+	// any storage is provisioned. See in-line explanation below.
+	//
+	// TODO(juju3) - allow storage hooks to run after install for IAAS models
+	// This will make IAAS and CAAS behaviour the same, and charms should be
+	// resilient to resources such as storage not being ready at any given time.
 	var runStorageHooks bool
 	switch {
 	case localState.Kind == operation.Continue:
 		// There's nothing in progress.
-		fallthrough
-	case blockedWaitingForInstall && localState.Kind == operation.RunHook && localState.Step == operation.Queued:
-		// The install operation completed, and there's an install
-		// hook queued. Run storage-attached hooks first.
-		fallthrough
-	case s.modelType == model.CAAS:
-		// CAAS models skip the uniter Install operation, but do run the
-		// install hook. No need to wait for the install hook to be run
-		// as storage is mounted when a pod is started.  This will keep
-		// behavior consistent with before CAAS units run an install hook,
 		runStorageHooks = true
+	case localState.Kind == operation.RunHook && localState.Step == operation.Queued:
+		// There's a hook queued.
+		switch s.modelType {
+		case model.CAAS:
+			// For CAAS models, we wait until after the start hook has run before
+			// running storage-attached hooks since storage is provisioned only
+			// after the pod has been created.
+			//
+			// NB we must be careful here. The initial hook sequence is
+			// install->leader-elected->config-changed->started
+			// This chain is activated by each preceding hook queuing the next.
+			// If we allow a storage-attached hook to run before the start hook, we
+			// will potentially overwrite the state to initiate the next hook.
+			// So we need to get to at least started. This means that any charm logic
+			// that needs storage cannot be in install or start and if in config-changed,
+			// needs to be deferred until storage is available.
+			runStorageHooks = localState.Started
+		case model.IAAS:
+			// For IAAS models, we run storage-attached hooks before install.
+			runStorageHooks = !localState.Installed
+		}
 	}
 	if !runStorageHooks {
 		return nil, resolver.ErrNoOperation
 	}
 
-	if !localState.Installed && s.storage.Pending() == 0 {
+	// This message is only interesting for IAAS models.
+	if s.modelType == model.IAAS && !localState.Installed && s.storage.Pending() == 0 {
 		logger.Infof("initial storage attachments ready")
 	}
 
@@ -103,11 +122,13 @@ func (s *storageResolver) NextOp(
 	}
 	if s.storage.Pending() > 0 {
 		logger.Debugf("still pending %v", s.storage.pending.SortedValues())
-		if blockedWaitingForInstall {
-			// We only wait for pending storage before
-			// the install hook runs; we should not block
-			// other hooks from running while storage is
-			// being provisioned.
+		// For IAAS models, storage hooks are run before install.
+		// If the install hook has not yet run and there's still
+		// pending storage, we wait. We don't wait after the
+		// install hook has run as we should not block other
+		// hooks from running while new storage added post install
+		// is being provisioned.
+		if !localState.Installed && s.modelType == model.IAAS {
 			return nil, resolver.ErrWaiting
 		}
 	}
