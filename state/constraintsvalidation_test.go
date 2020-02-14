bd79e64c48105b4619e1ac9ad5ba2987bd534fd8
Anastasia Macmood <anastasia.macmood@canonical.com>
2020-02-14 12:29:55 +1000

    revert change in favor of fix on develop

--- state/constraintsvalidation_test.go
+++ state/constraintsvalidation_test.go
@@ -7,11 +7,12 @@ import (
 	"regexp"
 
 	"github.com/juju/errors"
-	"github.com/juju/juju/core/constraints"
-	"github.com/juju/juju/state"
 	jc "github.com/juju/testing/checkers"
 	gc "gopkg.in/check.v1"
 	"gopkg.in/mgo.v2/bson"
+
+	"github.com/juju/juju/core/constraints"
+	"github.com/juju/juju/state"
 )
 
 type applicationConstraintsSuite struct {
@@ -329,14 +330,15 @@ func (s *applicationConstraintsSuite) TestConstraintsOpsForSpaceNameChange(c *gc
 	c.Assert(err, jc.ErrorIsNil)
 	c.Assert(ops, gc.HasLen, 2)
 
-	var opsSpacesCombined []string
+	var obtainedConstraintSpaces []string
 	for _, op := range ops {
 		found := op.Update.(bson.D).Map()["$set"].(state.ConstraintsDoc).Spaces
-		opsSpacesCombined = append(opsSpacesCombined, *found...)
+		obtainedConstraintSpaces = append(obtainedConstraintSpaces, *found...)
 
 	}
 	expectedSpace := []string{to, "alpha"}
 	negExpectedSpace := []string{negatedTo, "alpha"}
-	combinedExpected := append(expectedSpace, negExpectedSpace...)
-	c.Assert(combinedExpected, jc.SameContents, opsSpacesCombined)
+	expectedConstraintSpaces := append(expectedSpace, negExpectedSpace...)
+
+	c.Assert(obtainedConstraintSpaces, jc.SameContents, expectedConstraintSpaces)
 }
