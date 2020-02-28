38ca42eba1ec1c49fd4d423a2c968c62cad2ea7e
Achilleas Anagnostopoulos <achilleas.anagnostopoulos@canonical.com>
2020-02-28 15:25:09 +0000

    Add upgrade step to pre-populate reboot monitor state for deployed units
    This upgrade step prevents already deployed unit agents from assuming a
    reboot has occured when they get restarted after the upgrade to 2.8
    since the reboot monitor's flag files will be missing. To avoid this,
    the upgrade step runs on each unit-hosting machine, introspects the
    active unit agent deployments by peeking into the juju data dir and
    pre-populates the reboot-handled flag files for them.

--- upgrades/steps_28.go
+++ upgrades/steps_28.go
@@ -3,7 +3,14 @@
 
 package upgrades
 
-// stateStepsFor272 returns upgrade steps for Juju 2.8.0.
+import (
+	"github.com/juju/errors"
+	"github.com/juju/juju/service"
+	"github.com/juju/juju/worker/common/reboot"
+	"gopkg.in/juju/names.v3"
+)
+
+// stateStepsFor28 returns upgrade steps for Juju 2.8.0.
 func stateStepsFor28() []Step {
 	return []Step{
 		&upgradeStep{
@@ -14,4 +21,46 @@ func stateStepsFor28() []Step {
 			},
 		},
 	}
+}
+
+// stepsFor28 returns upgrade steps for Juju 2.8.0.
+func stepsFor28() []Step {
+	return []Step{
+		// This step pre-populates the reboot-handled flag for all
+		// running units so they do not accidentally trigger a start
+		// hook once they get restarted after the upgrade is complete.
+		&upgradeStep{
+			description: "ensure currently running units do not fire start hooks thinking a reboot has occurred",
+			targets:     []Target{HostMachine},
+			run:         prepopulateRebootHandledFlagsForDeployedUnits,
+		},
+	}
+}
+
+func prepopulateRebootHandledFlagsForDeployedUnits(ctx Context) error {
+	// Lookup the names of all unit agents installed on this machine.
+	agentConf := ctx.AgentConfig()
+	_, unitNames, _, err := service.FindAgents(agentConf.DataDir())
+	if err != nil {
+		return errors.Annotate(err, "looking up unit agents")
+	}
+
+	// Pre-populate reboot-handled flag files.
+	monitor := reboot.NewMonitor(agentConf.TransientDataDir())
+	for _, unitName := range unitNames {
+		// This should never fail as it is already validated by the
+		// FindAgents call. However, since this is an upgrade step
+		// it's fine to be a bit extra paranoid.
+		tag, err := names.ParseTag(unitName)
+		if err != nil {
+			return errors.Annotatef(err, "unable to parse unit agent tag %q", unitName)
+		}
+
+		// Querying the reboot monitor will set up the flag for this
+		// unit if it doesn't already exist.
+		if _, err = monitor.Query(tag); err != nil {
+			return errors.Annotatef(err, "querying reboot monitor for %q", unitName)
+		}
+	}
+	return nil
 }
