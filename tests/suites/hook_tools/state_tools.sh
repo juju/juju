e075cb23ec2004fb41796dbadd455b28d7f52dcb
Heather Lanigan <heather.lanigan@canonical.com>
2020-03-25 08:47:56 -0400

    Add run_state_set_clash_uniter_state
    Writing uniter internal data will not change state created by the
    unit's charm.  This test is added after a bug was found during
    testing, of a prior pr, where writing the uniter internal state
    deleted the unit's state.

--- tests/suites/hook_tools/state_tools.sh
+++ tests/suites/hook_tools/state_tools.sh
@@ -22,6 +22,34 @@ run_state_delete_get_set() {
     destroy_model "${model_name}"
 }
 
+# The unit's state set via the charm, is sharing a doc with the uniter's state.
+# They use the same method in state to update the db doc.  For the uniter's
+# state, SetState in the uniter api is used.  For the unit's state, Commit is
+# used.  Ensure that data set via Commit, is not overwritten by SetState.
+run_state_set_clash_uniter_state() {
+    echo
+
+    model_name="test-state_set_clash_uniter_state"
+    file="${TEST_DIR}/${model_name}.txt"
+
+    ensure "${model_name}" "${file}"
+
+    juju deploy cs:~jameinel/ubuntu-lite-7
+    wait_for "ubuntu-lite" "$(idle_condition "ubuntu-lite")"
+
+    juju run --unit ubuntu-lite/0 'state-get | grep -q "{}"'
+    juju run --unit ubuntu-lite/0 'state-set one=two'
+    juju run --unit ubuntu-lite/0 'state-get | grep -q "one: two"'
+
+    # force a hook
+    juju run --unit ubuntu-lite/0 hooks/update-status
+
+    # verify charm set values
+    juju run --unit ubuntu-lite/0 'state-get | grep -q "one: two"'
+
+    destroy_model "${model_name}"
+}
+
 test_state_hook_tools() {
     if [ "$(skip 'test_state_hook_tools')" ]; then
         echo "==> TEST SKIPPED: state hook tools"
@@ -34,5 +62,6 @@ test_state_hook_tools() {
         cd .. || exit
 
         run "run_state_delete_get_set"
+        run "run_state_set_clash_uniter_state"
     )
 }
