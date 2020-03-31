c18116e410d5012a5f92bb1d0dde8f75f7853a9b
Simon Richardson <stickupkid@gmail.com>
2020-03-31 10:35:55 +0100

    Move the space discovery code to spaces
    The previous space discovery code was at the wrong abstraction layer,
    discovery is a property of a controller not in the persistence layer.
    
    So it's natural that we move the old state code into a better location,
    this code does that.

--- state/spaces.go
+++ state/spaces.go
@@ -4,8 +4,12 @@
 package state
 
 import (
+	"fmt"
+	"net"
 	"strconv"
+	"strings"
 
+	"github.com/juju/collections/set"
 	"github.com/juju/errors"
 	"gopkg.in/juju/names.v3"
 	"gopkg.in/mgo.v2"
@@ -13,6 +17,7 @@ import (
 	"gopkg.in/mgo.v2/txn"
 
 	"github.com/juju/juju/core/network"
+	jujunetwork "github.com/juju/juju/network"
 )
 
 // Space represents the state of a juju network space.
@@ -393,4 +398,144 @@ func (st *State) createDefaultSpaceOp() txn.Op {
 			IsPublic: true,
 		},
 	}
+}
+
+func (st *State) getModelSubnets() (set.Strings, error) {
+	subnets, err := st.AllSubnets()
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+	modelSubnetIds := make(set.Strings)
+	for _, subnet := range subnets {
+		modelSubnetIds.Add(string(subnet.ProviderId()))
+	}
+	return modelSubnetIds, nil
+}
+
+// SaveProviderSubnets loads subnets into state.
+// Currently it does not delete removed subnets.
+func (st *State) SaveProviderSubnets(subnets []network.SubnetInfo, spaceID string) error {
+	modelSubnetIds, err := st.getModelSubnets()
+	if err != nil {
+		return errors.Trace(err)
+	}
+
+	for _, subnet := range subnets {
+		ip, _, err := net.ParseCIDR(subnet.CIDR)
+		if err != nil {
+			return errors.Trace(err)
+		}
+		if ip.IsInterfaceLocalMulticast() || ip.IsLinkLocalMulticast() || ip.IsLinkLocalUnicast() {
+			continue
+		}
+
+		subnet.SpaceID = spaceID
+		if modelSubnetIds.Contains(string(subnet.ProviderId)) {
+			err = st.SubnetUpdate(subnet)
+		} else {
+			_, err = st.AddSubnet(subnet)
+		}
+
+		if err != nil {
+			return errors.Trace(err)
+		}
+	}
+
+	// We process FAN subnets separately for clarity.
+	m, err := st.Model()
+	if err != nil {
+		return errors.Trace(err)
+	}
+	cfg, err := m.ModelConfig()
+	if err != nil {
+		return errors.Trace(err)
+	}
+	fans, err := cfg.FanConfig()
+	if err != nil {
+		return errors.Trace(err)
+	}
+	if len(fans) == 0 {
+		return nil
+	}
+
+	for _, subnet := range subnets {
+		for _, fan := range fans {
+			_, subnetNet, err := net.ParseCIDR(subnet.CIDR)
+			if err != nil {
+				return errors.Trace(err)
+			}
+			subnetWithDashes := strings.Replace(strings.Replace(subnetNet.String(), ".", "-", -1), "/", "-", -1)
+			id := fmt.Sprintf("%s-%s-%s", subnet.ProviderId, network.InFan, subnetWithDashes)
+			if modelSubnetIds.Contains(id) {
+				continue
+			}
+			if subnetNet.IP.To4() == nil {
+				logger.Debugf("%s address is not an IPv4 address.", subnetNet.IP)
+				continue
+			}
+			overlaySegment, err := jujunetwork.CalculateOverlaySegment(subnet.CIDR, fan)
+			if err != nil {
+				return errors.Trace(err)
+			}
+			if overlaySegment != nil {
+				subnet.ProviderId = network.Id(id)
+				subnet.SpaceID = spaceID
+				subnet.SetFan(subnet.CIDR, fan.Overlay.String())
+				subnet.CIDR = overlaySegment.String()
+
+				_, err := st.AddSubnet(subnet)
+				if err != nil {
+					return errors.Trace(err)
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
+// SaveProviderSpaces loads providerSpaces into state.
+// Currently it does not delete removed spaces.
+func (st *State) SaveProviderSpaces(providerSpaces []network.SpaceInfo) error {
+	stateSpaces, err := st.AllSpaces()
+	if err != nil {
+		return errors.Trace(err)
+	}
+	modelSpaceMap := make(map[network.Id]*Space)
+	spaceNames := make(set.Strings)
+	for _, space := range stateSpaces {
+		modelSpaceMap[space.ProviderId()] = space
+		spaceNames.Add(space.Name())
+	}
+
+	// TODO(mfoord): we need to delete spaces and subnets that no longer
+	// exist, so long as they're not in use.
+	for _, spaceInfo := range providerSpaces {
+		// Check if the space is already in state,
+		// in which case we know its name.
+		stateSpace, ok := modelSpaceMap[spaceInfo.ProviderId]
+		var spaceId string
+		if ok {
+			spaceId = stateSpace.Id()
+		} else {
+			// The space is new, we need to create a valid name for it in state.
+			// Convert the name into a valid name that is not already in use.
+			spaceName := network.ConvertSpaceName(string(spaceInfo.Name), spaceNames)
+
+			logger.Debugf("Adding space %s from provider %s", spaceName, string(spaceInfo.ProviderId))
+			space, err := st.AddSpace(spaceName, spaceInfo.ProviderId, []string{}, false)
+			if err != nil {
+				return errors.Trace(err)
+			}
+
+			spaceNames.Add(spaceName)
+			spaceId = space.Id()
+		}
+
+		err = st.SaveProviderSubnets(spaceInfo.Subnets, spaceId)
+		if err != nil {
+			return errors.Trace(err)
+		}
+	}
+	return nil
 }
--- state/spaces_test.go
+++ state/spaces_test.go
@@ -13,6 +13,7 @@ import (
 	gc "gopkg.in/check.v1"
 
 	"github.com/juju/juju/core/network"
+	corenetwork "github.com/juju/juju/core/network"
 	"github.com/juju/juju/state"
 )
 
@@ -681,4 +682,328 @@ func (s *SpacesSuite) TestSpaceToNetworkSpace(c *gc.C) {
 	sort.Slice(expSpaceInfo.Subnets, func(l, r int) bool { return expSpaceInfo.Subnets[l].CIDR < expSpaceInfo.Subnets[r].CIDR })
 
 	c.Assert(spaceInfo, gc.DeepEquals, expSpaceInfo)
+}
+
+type SpacesDiscoverySuite struct {
+	ConnSuite
+}
+
+var _ = gc.Suite(&SpacesDiscoverySuite{})
+
+var twoSubnets = []network.SubnetInfo{
+	{
+		ProviderId:        "1",
+		AvailabilityZones: []string{"1", "2"},
+		CIDR:              "10.0.0.1/24",
+	},
+	{
+		ProviderId:        "2",
+		AvailabilityZones: []string{"3", "4"},
+		CIDR:              "10.100.30.1/24",
+	},
+}
+
+var twoSubnetsAndIgnored = []network.SubnetInfo{
+	{
+		ProviderId:        "1",
+		AvailabilityZones: []string{"1", "2"},
+		CIDR:              "10.0.0.1/24",
+	},
+	{
+		ProviderId:        "2",
+		AvailabilityZones: []string{"3", "4"},
+		CIDR:              "10.100.30.1/24",
+	},
+	// Interface-local multicast:
+	{
+		ProviderId:        "foo",
+		AvailabilityZones: []string{"bar", "baz"},
+		CIDR:              "ff51:dead:beef::/48",
+	},
+	// Link-local multicast:
+	{
+		ProviderId:        "moo",
+		AvailabilityZones: []string{"bar", "baz"},
+		CIDR:              "ff32:dead:beef::/48",
+	},
+	// IPv6 link-local unicast:
+	{
+		ProviderId:        "baa",
+		AvailabilityZones: []string{"bar", "baz"},
+		CIDR:              "fe80:dead:beef::/48",
+	},
+	// IPv4 link-local unicast:
+	{
+		ProviderId:        "maa",
+		AvailabilityZones: []string{"bar", "baz"},
+		CIDR:              "169.254.13.0/24",
+	},
+}
+
+var anotherTwoSubnets = []network.SubnetInfo{
+	{
+		ProviderId:        "3",
+		AvailabilityZones: []string{"5", "6"},
+		CIDR:              "10.101.0.1/24",
+	},
+	{
+		ProviderId:        "4",
+		AvailabilityZones: []string{"7", "8"},
+		CIDR:              "10.105.0.1/24",
+	},
+}
+
+var fourSubnets = []network.SubnetInfo{
+	{
+		ProviderId:        "1",
+		AvailabilityZones: []string{"1", "2"},
+		CIDR:              "10.0.0.1/24",
+	},
+	{
+		ProviderId:        "2",
+		AvailabilityZones: []string{"3", "4"},
+		CIDR:              "10.100.30.1/24",
+	},
+	{
+		ProviderId:        "3",
+		AvailabilityZones: []string{"5", "6"},
+		CIDR:              "10.101.0.1/24",
+	},
+	{
+		ProviderId:        "4",
+		AvailabilityZones: []string{"7", "8"},
+		CIDR:              "10.105.0.1/24",
+	},
+}
+
+var spaceOne = []network.SpaceInfo{
+	{
+		Name:       "space1",
+		ProviderId: "1",
+		Subnets:    twoSubnets,
+	},
+}
+var spaceOneAndIgnored = []network.SpaceInfo{
+	{
+		Name:       "space1",
+		ProviderId: "1",
+		Subnets:    twoSubnetsAndIgnored,
+	},
+}
+var spaceTwo = []network.SpaceInfo{
+	{
+		Name:       "space2",
+		ProviderId: "2",
+		Subnets:    anotherTwoSubnets,
+	},
+}
+
+var twoSpaces = []network.SpaceInfo{spaceOne[0], spaceTwo[0]}
+
+var twoSubnetsAfterFAN = []network.SubnetInfo{
+	{
+		ProviderId:        "1",
+		AvailabilityZones: []string{"1", "2"},
+		CIDR:              "10.0.0.1/24",
+	},
+	{
+		ProviderId:        "2",
+		AvailabilityZones: []string{"3", "4"},
+		CIDR:              "10.100.30.1/24",
+	},
+	{
+		ProviderId:        corenetwork.Id(fmt.Sprintf("2-%s-10-100-30-0-24", corenetwork.InFan)),
+		AvailabilityZones: []string{"3", "4"},
+		CIDR:              "253.30.0.0/16",
+	},
+}
+
+var spaceOneAfterFAN = []network.SpaceInfo{
+	{
+		Name:       "space1",
+		ProviderId: "1",
+		Subnets:    twoSubnetsAfterFAN,
+	},
+}
+
+func checkSubnetsEqual(c *gc.C, subnets []*state.Subnet, subnetInfos []network.SubnetInfo) {
+	c.Assert(len(subnetInfos), gc.Equals, len(subnets))
+	for i, subnetInfo := range subnetInfos {
+		subnet := subnets[i]
+		c.Check(subnetInfo.CIDR, gc.Equals, subnet.CIDR())
+		c.Check(subnetInfo.AvailabilityZones, gc.DeepEquals, subnet.AvailabilityZones())
+		c.Check(subnetInfo.ProviderId, gc.Equals, subnet.ProviderId())
+		c.Check(subnetInfo.ProviderNetworkId, gc.Equals, subnet.ProviderNetworkId())
+		c.Check(subnetInfo.VLANTag, gc.Equals, subnet.VLANTag())
+	}
+}
+
+func checkSpacesEqual(c *gc.C, spaces []*state.Space, spaceInfos []network.SpaceInfo) {
+	// Filter out the default space for comparisons.
+	filtered := spaces[:0]
+	for _, s := range spaces {
+		if s.Name() != network.AlphaSpaceName {
+			filtered = append(filtered, s)
+		}
+	}
+
+	c.Assert(len(spaceInfos), gc.Equals, len(filtered))
+	for i, spaceInfo := range spaceInfos {
+		space := filtered[i]
+		c.Check(string(spaceInfo.Name), gc.Equals, space.Name())
+		c.Check(spaceInfo.ProviderId, gc.Equals, space.ProviderId())
+		subnets, err := space.Subnets()
+		c.Assert(err, jc.ErrorIsNil)
+		checkSubnetsEqual(c, subnets, spaceInfo.Subnets)
+	}
+}
+
+func (s *SpacesDiscoverySuite) TestSaveProviderSubnets(c *gc.C) {
+	err := s.State.SaveProviderSubnets(twoSubnets, "")
+	c.Check(err, jc.ErrorIsNil)
+
+	subnets, err := s.State.AllSubnets()
+	c.Assert(err, jc.ErrorIsNil)
+	checkSubnetsEqual(c, subnets, twoSubnets)
+}
+
+// TODO(wpk) 2017-05-24 this test will have to be rewritten when we support removing spaces/subnets in discovery.
+func (s *SpacesDiscoverySuite) TestSaveProviderSubnetsOnlyAddsSubnets(c *gc.C) {
+	err := s.State.SaveProviderSubnets(twoSubnets, "")
+	c.Check(err, jc.ErrorIsNil)
+
+	err = s.State.SaveProviderSubnets(anotherTwoSubnets, "")
+	c.Check(err, jc.ErrorIsNil)
+
+	subnets, err := s.State.AllSubnets()
+	c.Assert(err, jc.ErrorIsNil)
+	checkSubnetsEqual(c, subnets, fourSubnets)
+}
+
+func (s *SpacesDiscoverySuite) TestSaveProviderSubnetsUpdatesSubnets(c *gc.C) {
+	err := s.State.SaveProviderSubnets(twoSubnets, "")
+	c.Check(err, jc.ErrorIsNil)
+
+	err = s.State.SaveProviderSpaces(spaceOne)
+	c.Assert(err, jc.ErrorIsNil)
+
+	subnets, err := s.State.AllSubnets()
+	c.Assert(err, jc.ErrorIsNil)
+	twoSubnetsWithSpace := twoSubnets
+	twoSubnetsWithSpace[0].ProviderSpaceId = spaceOne[0].ProviderId
+	twoSubnetsWithSpace[1].ProviderSpaceId = spaceOne[0].ProviderId
+	checkSubnetsEqual(c, subnets, twoSubnetsWithSpace)
+}
+
+func (s *SpacesDiscoverySuite) TestSaveProviderSubnetsOnlyIdempotent(c *gc.C) {
+	err := s.State.SaveProviderSubnets(twoSubnets, "")
+	c.Check(err, jc.ErrorIsNil)
+
+	subnets1, err := s.State.AllSubnets()
+	c.Assert(err, jc.ErrorIsNil)
+
+	err = s.State.SaveProviderSubnets(twoSubnets, "")
+	c.Check(err, jc.ErrorIsNil)
+
+	subnets2, err := s.State.AllSubnets()
+	c.Assert(err, jc.ErrorIsNil)
+	c.Check(subnets1, jc.DeepEquals, subnets2)
+}
+
+func (s *SpacesDiscoverySuite) TestSaveProviderSubnetsWithFAN(c *gc.C) {
+	err := s.Model.UpdateModelConfig(map[string]interface{}{"fan-config": "10.100.0.0/16=253.0.0.0/8"}, nil)
+	c.Assert(err, jc.ErrorIsNil)
+
+	err = s.State.SaveProviderSubnets(twoSubnets, "")
+	c.Check(err, jc.ErrorIsNil)
+
+	subnets, err := s.State.AllSubnets()
+	c.Assert(err, jc.ErrorIsNil)
+
+	checkSubnetsEqual(c, subnets, twoSubnetsAfterFAN)
+}
+
+func (s *SpacesDiscoverySuite) TestSaveProviderSubnetsIgnoredWithFAN(c *gc.C) {
+	// This is just a test configuration. This configuration may be
+	// considered invalid in the future. Here we show that this
+	// configuration is ignored.
+	err := s.Model.UpdateModelConfig(
+		map[string]interface{}{"fan-config": "fe80:dead:beef::/48=fe80:dead:beef::/24"}, nil)
+	c.Assert(err, jc.ErrorIsNil)
+
+	err = s.State.SaveProviderSubnets(twoSubnetsAndIgnored, "")
+	c.Check(err, jc.ErrorIsNil)
+
+	subnets, err := s.State.AllSubnets()
+	c.Assert(err, jc.ErrorIsNil)
+
+	checkSubnetsEqual(c, subnets, twoSubnets)
+}
+
+func (s *SpacesDiscoverySuite) TestSaveProviderSubnetsIgnored(c *gc.C) {
+	err := s.State.SaveProviderSubnets(twoSubnetsAndIgnored, "")
+	c.Check(err, jc.ErrorIsNil)
+
+	subnets, err := s.State.AllSubnets()
+	c.Assert(err, jc.ErrorIsNil)
+
+	checkSubnetsEqual(c, subnets, twoSubnets)
+}
+
+func (s *SpacesDiscoverySuite) TestSaveProviderSpaces(c *gc.C) {
+	err := s.State.SaveProviderSpaces(spaceOne)
+	c.Check(err, jc.ErrorIsNil)
+
+	spaces, err := s.State.AllSpaces()
+	c.Assert(err, jc.ErrorIsNil)
+	checkSpacesEqual(c, spaces, spaceOne)
+}
+
+// TODO(wpk) 2017-05-24 this test will have to be rewritten when we support removing spaces/subnets in discovery.
+func (s *SpacesDiscoverySuite) TestSaveProviderSpacesAddsSpaces(c *gc.C) {
+	err := s.State.SaveProviderSpaces(spaceOne)
+	c.Check(err, jc.ErrorIsNil)
+
+	err = s.State.SaveProviderSpaces(spaceTwo)
+	c.Check(err, jc.ErrorIsNil)
+
+	spaces, err := s.State.AllSpaces()
+	c.Assert(err, jc.ErrorIsNil)
+	checkSpacesEqual(c, spaces, twoSpaces)
+}
+
+func (s *SpacesDiscoverySuite) TestSaveProviderSpacesIdempotent(c *gc.C) {
+	err := s.State.SaveProviderSpaces(twoSpaces)
+	c.Check(err, jc.ErrorIsNil)
+
+	spaces1, err := s.State.AllSpaces()
+	c.Assert(err, jc.ErrorIsNil)
+
+	err = s.State.SaveProviderSpaces(twoSpaces)
+	c.Check(err, jc.ErrorIsNil)
+
+	spaces2, err := s.State.AllSpaces()
+	c.Assert(err, jc.ErrorIsNil)
+	c.Check(spaces1, gc.DeepEquals, spaces2)
+}
+
+func (s *SpacesDiscoverySuite) TestSaveProviderSpacesWithFAN(c *gc.C) {
+	err := s.Model.UpdateModelConfig(map[string]interface{}{"fan-config": "10.100.0.0/16=253.0.0.0/8"}, nil)
+	c.Assert(err, jc.ErrorIsNil)
+
+	err = s.State.SaveProviderSpaces(spaceOne)
+	c.Check(err, jc.ErrorIsNil)
+
+	spaces, err := s.State.AllSpaces()
+	c.Assert(err, jc.ErrorIsNil)
+	checkSpacesEqual(c, spaces, spaceOneAfterFAN)
+}
+
+func (s *SpacesDiscoverySuite) TestReloadSpacesIgnored(c *gc.C) {
+	err := s.State.SaveProviderSpaces(spaceOneAndIgnored)
+	c.Check(err, jc.ErrorIsNil)
+
+	spaces, err := s.State.AllSpaces()
+	c.Assert(err, jc.ErrorIsNil)
+	checkSpacesEqual(c, spaces, spaceOne)
 }
