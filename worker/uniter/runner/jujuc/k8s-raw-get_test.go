6ee37e8f9a5765421a5f489d31e897524316bd62
Kelvin Liu <kelvin.liu@canonical.com>
2020-04-06 23:11:30 -0700

    Add tests for k8s-raw-get and k8s-raw-set commands;

---
+++ worker/uniter/runner/jujuc/k8s-raw-get_test.go
@@ -0,0 +1,70 @@
+// Copyright 2019 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package jujuc_test
+
+import (
+	"github.com/juju/cmd"
+	"github.com/juju/cmd/cmdtesting"
+	jc "github.com/juju/testing/checkers"
+	gc "gopkg.in/check.v1"
+
+	"github.com/juju/juju/worker/uniter/runner/jujuc"
+)
+
+type rawK8sSpecGetSuite struct {
+	ContextSuite
+}
+
+var _ = gc.Suite(&rawK8sSpecGetSuite{})
+
+var rawK8sSpecGetInitTests = []struct {
+	args []string
+	err  string
+}{
+	{[]string{"extra"}, `unrecognized args: \["extra"\]`},
+}
+
+func (s *rawK8sSpecGetSuite) TestRawK8sSpecGetInit(c *gc.C) {
+	for i, t := range rawK8sSpecGetInitTests {
+		c.Logf("test %d: %#v", i, t.args)
+		hctx := s.GetHookContext(c, -1, "")
+		com, err := jujuc.NewCommand(hctx, "k8s-raw-get")
+		c.Assert(err, jc.ErrorIsNil)
+		cmdtesting.TestInit(c, jujuc.NewJujucCommandWrappedForTest(com), t.args, t.err)
+	}
+}
+
+func (s *rawK8sSpecGetSuite) TestHelp(c *gc.C) {
+	hctx := s.GetHookContext(c, -1, "")
+	com, err := jujuc.NewCommand(hctx, "k8s-raw-get")
+	c.Assert(err, jc.ErrorIsNil)
+	ctx := cmdtesting.Context(c)
+	code := cmd.Main(jujuc.NewJujucCommandWrappedForTest(com), ctx, []string{"--help"})
+	c.Assert(code, gc.Equals, 0)
+	expectedHelp := `
+Usage: k8s-raw-get
+
+Summary:
+get k8s raw spec information
+
+Details:
+Gets configuration data used to set up k8s resources.
+`[1:]
+
+	c.Assert(bufferString(ctx.Stdout), gc.Equals, expectedHelp)
+	c.Assert(bufferString(ctx.Stderr), gc.Equals, "")
+}
+
+func (s *rawK8sSpecGetSuite) TestRawK8sSpecGet(c *gc.C) {
+	hctx := s.GetHookContext(c, -1, "")
+	hctx.info.RawK8sSpec = "k8sspec"
+	com, err := jujuc.NewCommand(hctx, "k8s-raw-get")
+	c.Assert(err, jc.ErrorIsNil)
+	ctx := cmdtesting.Context(c)
+
+	code := cmd.Main(jujuc.NewJujucCommandWrappedForTest(com), ctx, nil)
+	c.Check(code, gc.Equals, 0)
+	c.Assert(bufferString(ctx.Stderr), gc.Equals, "")
+	c.Assert(bufferString(ctx.Stdout), gc.Equals, "k8sspec")
+}
