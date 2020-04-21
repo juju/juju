51c5843a55e016f9effc5b5c237a33426cb5c90a
Harry Pidcock <harry.pidcock@canonical.com>
2020-04-21 15:53:06 +1000

    Fix PR feedback + Update CONTRIBUTING.md

--- apiserver/apiserver.go
+++ apiserver/apiserver.go
@@ -457,6 +457,21 @@ func (srv *Server) getAgentToken() error {
 	return nil
 }
 
+// loggoWrapper is an io.Writer() that forwards the messages to a loggo.Logger.
+// Unfortunately http takes a concrete stdlib log.Logger struct, and not an
+// interface, so we can't just proxy all of the log levels without inspecting
+// the string content. For now, we just want to get the messages into the log
+// file.
+type loggoWrapper struct {
+	logger loggo.Logger
+	level  loggo.Level
+}
+
+func (w *loggoWrapper) Write(content []byte) (int, error) {
+	w.logger.Logf(w.level, "%s", string(content))
+	return len(content), nil
+}
+
 // logsinkMetricsCollectorWrapper defines a wrapper for exposing the essentials
 // for the logsink api handler to interact with the metrics collector.
 type logsinkMetricsCollectorWrapper struct {
