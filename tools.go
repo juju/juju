3e3aef392ede1a499497b38e097f1cedf30ac17d
Harry Pidcock <harry.pidcock@canonical.com>
2020-04-23 08:34:46 +1000

    Fix tools.go build tag.

--- tools.go
+++ tools.go
@@ -1,9 +1,9 @@
 // Copyright 2020 Canonical Ltd.
 // Licensed under the AGPLv3, see LICENCE file for details.
 
-package juju
-
 // +build tools
+
+package juju
 
 import (
 	// github.com/golang/mock/mockgen for generating mocks
