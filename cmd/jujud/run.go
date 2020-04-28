b29042f15fd2c46a6e49562252df89c9b2515dfd
Harry Pidcock <harry.pidcock@canonical.com>
2020-04-28 13:23:25 +1000

    Support multiple command arguments for juju-run
    - juju-run handles multiple arguments correctly
    - juju-run now avoid positional argument for unit-name
    - fixes issues with commands that look like a unit name/id
    - fix resolver loop crashing with empty commands

--- cmd/jujud/run.go
+++ cmd/jujud/run.go
@@ -20,6 +20,7 @@ import (
 	"github.com/juju/names/v4"
 	jujuos "github.com/juju/os"
 	"github.com/juju/os/series"
+	"github.com/juju/utils"
 	"github.com/juju/utils/exec"
 	"gopkg.in/yaml.v2"
 
@@ -37,6 +38,7 @@ type RunCommand struct {
 	cmd.CommandBase
 	dataDir               string
 	MachineLock           machinelock.Lock
+	unitName              string
 	unit                  names.UnitTag
 	commands              string
 	showHelp              bool
@@ -56,11 +58,23 @@ unit-name can be either the unit tag:
 or the unit id:
  i.e.  ubuntu/0
 
+unit-name can be specified by the -u argument.
+If -u is passed, unit-name cannot be passed as a positional argument.
+
 If --no-context is specified, the <unit-name> positional
-argument is not needed.
+argument or -u argument is not needed.
 
 If the there's one and only one unit on this host, <unit-name>
 is automatically inferred and the positional argument is not needed.
+If -u is passed an empty string, this behaviour is also observed.
+
+Examples:
+	juju-run app/0 hostname -f
+	juju-run --no-context -- hostname -f
+	juju-run "hostname -f"
+	juju-run -u "" -- hostname -f
+	juju-run -u app/0 "hostname -f"
+	juju-run -u app/0 -- hostname -f
 
 The commands are executed with '/bin/bash -s', and the output returned.
 `
@@ -69,7 +83,7 @@ The commands are executed with '/bin/bash -s', and the output returned.
 func (c *RunCommand) Info() *cmd.Info {
 	return jujucmd.Info(&cmd.Info{
 		Name:    "juju-run",
-		Args:    "[<unit-name>] <commands>",
+		Args:    "[-u] [<unit-name>] <commands>",
 		Purpose: "run commands in a unit's hook context",
 		Doc:     runCommandDoc,
 	})
@@ -83,40 +97,13 @@ func (c *RunCommand) SetFlags(f *gnuflag.FlagSet) {
 	f.StringVar(&c.remoteApplicationName, "remote-app", "", "run the commands for a specific remote application in a relation context on a unit")
 	f.BoolVar(&c.operator, "operator", false, "run the commands on the operator instead of the workload. Only supported on k8s workload charms")
 	f.BoolVar(&c.forceRemoteUnit, "force-remote-unit", false, "run the commands for a specific relation context, bypassing the remote unit check")
+	f.StringVar(&c.unitName, "u", "-", "explicit unit-name, all other arguments are commands. if -u is passed an empty string, unit-name is inferred from state")
 }
 
 func (c *RunCommand) Init(args []string) error {
 	// make sure we aren't in an existing hook context
 	if contextId, err := getenv("JUJU_CONTEXT_ID"); err == nil && contextId != "" {
 		return fmt.Errorf("juju-run cannot be called from within a hook, have context %q", contextId)
-	}
-	if !c.noContext {
-		if len(args) < 1 {
-			return fmt.Errorf("missing unit-name")
-		}
-		var unitName = args[0]
-		// If the command line param is a unit id (like application/2) we need to
-		// change it to the unit tag as that is the format of the agent directory
-		// on disk (unit-application-2).
-		if names.IsValidUnit(unitName) {
-			c.unit = names.NewUnitTag(unitName)
-			args = args[1:]
-		} else {
-			var err error
-			c.unit, err = names.ParseUnitTag(unitName)
-			if err == nil {
-				args = args[1:]
-			} else {
-				// If arg[0] is neither a unit name not unit tag, perhaps
-				// we are running where there's only one unit, in which case
-				// we can safely use that.
-				var err2 error
-				c.unit, err2 = c.maybeGetUnitTag()
-				if err2 != nil {
-					return errors.Trace(err)
-				}
-			}
-		}
 	}
 	// if remoteApplication wasn't set but remoteUnit was, infer the remoteApplication
 	if c.remoteApplicationName == "" && c.remoteUnitName != "" {
@@ -130,11 +117,68 @@ func (c *RunCommand) Init(args []string) error {
 		}
 		c.remoteApplicationName = appName
 	}
+	if c.noContext && c.unitName != "-" {
+		return fmt.Errorf("-no-context cannot be passed with an explicit unit-name (-u %q)", c.unitName)
+	} else if !c.noContext {
+		if c.unitName == "-" {
+			if len(args) < 1 {
+				return fmt.Errorf("missing unit-name")
+			}
+			var unitName = args[0]
+			// If the command line param is a unit id (like application/2) we need to
+			// change it to the unit tag as that is the format of the agent directory
+			// on disk (unit-application-2).
+			if names.IsValidUnit(unitName) {
+				c.unit = names.NewUnitTag(unitName)
+				args = args[1:]
+			} else {
+				var err error
+				c.unit, err = names.ParseUnitTag(unitName)
+				if err == nil {
+					args = args[1:]
+				} else {
+					// If arg[0] is neither a unit name not unit tag, perhaps
+					// we are running where there's only one unit, in which case
+					// we can safely use that.
+					var err2 error
+					c.unit, err2 = c.maybeGetUnitTag()
+					if err2 != nil {
+						return errors.Trace(err)
+					}
+				}
+			}
+		} else if c.unitName == "" {
+			var err error
+			c.unit, err = c.maybeGetUnitTag()
+			if err != nil {
+				return errors.Trace(err)
+			}
+		} else {
+			if names.IsValidUnit(c.unitName) {
+				c.unit = names.NewUnitTag(c.unitName)
+			} else {
+				var err error
+				c.unit, err = names.ParseUnitTag(c.unitName)
+				if err != nil {
+					return errors.Trace(err)
+				}
+			}
+		}
+	}
 	if len(args) < 1 {
 		return fmt.Errorf("missing commands")
 	}
-	c.commands, args = args[0], args[1:]
-	return cmd.CheckEmpty(args)
+	if len(args) == 1 {
+		// If just one argument is specified, we don't pass it through
+		// utils.CommandString in case it contains multiple arguments
+		// (e.g. juju-run -u "sudo whatever"). Passing it through
+		// utils.CommandString would quote the string, which the backend
+		// does not expect.
+		c.commands = args[0]
+	} else {
+		c.commands = utils.CommandString(args...)
+	}
+	return nil
 }
 
 // maybeGetUnitTag looks at the contents of the agents directory
