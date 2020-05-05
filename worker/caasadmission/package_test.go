96b64a66f4afb4ea485b41147ef39028e8ba15c0
Harry Pidcock <harry.pidcock@canonical.com>
2020-05-06 09:53:42 +1000

    Fix 1869959 - Admission worker has no side effects
    Tell k8s that the admission worker's webhook has no side effects
    so that dry runs work.

---
+++ worker/caasadmission/package_test.go
@@ -0,0 +1,14 @@
+// Copyright 2020 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package caasadmission_test
+
+import (
+	"testing"
+
+	gc "gopkg.in/check.v1"
+)
+
+func Test(t *testing.T) {
+	gc.TestingT(t)
+}
