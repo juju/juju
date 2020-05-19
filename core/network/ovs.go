fa996eb6de38f5f579252dcc1b868db744427eb9
Achilleas Anagnostopoulos <achilleas.anagnostopoulos@canonical.com>
2020-05-19 14:37:17 +0100

    Move OVS bridge-detection code to core/network and export it
    We need to filter out OVS-managed bridges in the network (the legacy
    package; not the core/network one) so the detection code had to be
    extracted from the container package and moved to core/network.

---
+++ core/network/ovs.go
@@ -0,0 +1,45 @@
+// Copyright 2020 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package network
+
+import (
+	"os/exec"
+	"strings"
+
+	"github.com/juju/collections/set"
+	"github.com/juju/errors"
+)
+
+// Overridden by tests
+var getCommandOutput = func(cmd *exec.Cmd) ([]byte, error) { return cmd.Output() }
+
+// OvsManagedBridges returns a filtered version of ifaceList that only contains
+// bridge interfaces managed by openvswitch.
+func OvsManagedBridges(ifaceList InterfaceInfos) (InterfaceInfos, error) {
+	if _, err := exec.LookPath("ovs-vsctl"); err != nil {
+		// ovs tools not installed; nothing to do
+		if execErr, isExecErr := err.(*exec.Error); isExecErr && execErr.Unwrap() == exec.ErrNotFound {
+			return nil, nil
+		}
+
+		return nil, errors.Annotate(err, "ovsManagedBridges: looking for ovs-vsctl")
+	}
+
+	// Query list of ovs-managed device names
+	res, err := getCommandOutput(exec.Command("ovs-vsctl", "list-br"))
+	if err != nil {
+		return nil, errors.Annotate(err, "querying ovs-managed bridges via ovs-vsctl")
+	}
+
+	ovsBridges := set.NewStrings()
+	for _, iface := range strings.Split(string(res), "\n") {
+		if iface = strings.TrimSpace(iface); iface != "" {
+			ovsBridges.Add(iface)
+		}
+	}
+
+	return ifaceList.Filter(func(iface InterfaceInfo) bool {
+		return ovsBridges.Contains(iface.InterfaceName)
+	}), nil
+}
