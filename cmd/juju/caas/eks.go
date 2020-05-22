fd83551da0098f3e4aeeb3f960578d8bf1765615
Kelvin Liu <kelvin.liu@canonical.com>
2020-05-22 17:41:51 +1000

    Implement --eks for add-k8s command;

---
+++ cmd/juju/caas/eks.go
@@ -0,0 +1,128 @@
+// Copyright 2020 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package caas
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/juju/cmd"
+	"github.com/juju/errors"
+
+	"github.com/juju/juju/caas"
+	"github.com/juju/juju/caas/kubernetes/clientconfig"
+	"github.com/juju/juju/cmd/juju/interact"
+)
+
+type eks struct {
+	tool string
+	CommandRunner
+}
+
+func newEKSCluster() k8sCluster {
+	return &eks{
+		tool:          "eksctl",
+		CommandRunner: &defaultRunner{},
+	}
+}
+
+func (e *eks) cloud() string {
+	return caas.K8sCloudEC2
+}
+
+func (e *eks) ensureExecutable() error {
+	cmd := []string{"which", e.tool}
+	err := collapseRunError(runCommand(e, cmd, ""))
+	errAnnotationMessage := fmt.Sprintf("%q not found. Please install %q (see: https://eksctl.io/introduction/#installation, login, and try again", e.tool, e.tool)
+	if err != nil {
+		return errors.Errorf(errAnnotationMessage)
+	}
+
+	// check that we are logged in, there is no way to provide login details to a separate command.
+	cmd = []string{e.tool, "get", "cluster", "-v", "5"}
+	logger.Criticalf("cmd -> %v", cmd)
+	err = collapseRunError(runCommand(e, cmd, ""))
+	logger.Criticalf("cmd -> %v, err -> %#v", cmd, err)
+	if err != nil {
+		return errors.Errorf("please ensure the account has been setup and re-run this command")
+	}
+	return nil
+}
+
+func (e *eks) getKubeConfig(p *clusterParams) (io.ReadCloser, string, error) {
+	kubeconfig := clientconfig.GetKubeConfigPath()
+	cmd := []string{
+		e.tool, "utils", "write-kubeconfig",
+		"--cluster", p.name,
+		"--kubeconfig", kubeconfig,
+	}
+	if len(p.region) > 0 {
+		cmd = append(cmd, "--region", p.region)
+	}
+
+	err := collapseRunError(runCommand(e, cmd, kubeconfig))
+	if err != nil {
+		return nil, "", errors.Trace(err)
+	}
+	reader, err := os.Open(kubeconfig)
+	return reader, p.name, err
+}
+
+func (e *eks) interactiveParams(ctx *cmd.Context, p *clusterParams) (*clusterParams, error) {
+	errout := interact.NewErrWriter(ctx.Stdout)
+	pollster := interact.New(ctx.Stdin, ctx.Stdout, errout)
+
+	if len(p.region) == 0 {
+		var err error
+		if p.region, err = pollster.Enter("region"); err != nil {
+			return nil, errors.Trace(err)
+		}
+	}
+	if len(p.name) == 0 {
+		clusters, err := e.listClusters(p.region)
+		if err != nil {
+			return nil, errors.Trace(err)
+		}
+
+		if p.name, err = pollster.Select(interact.List{
+			Singular: "cluster",
+			Plural:   "Available clusters",
+			Options:  clusters,
+			Default:  clusters[0],
+		}); err != nil {
+			return nil, errors.Trace(err)
+		}
+	}
+	return p, nil
+}
+
+type eksDetail struct {
+	Name   string `json:"name"`
+	Region string `json:"region"`
+}
+
+func (e *eks) listClusters(region string) (clusters []string, err error) {
+	cmd := []string{
+		e.tool, "get", "cluster", "--region", region, "-o", "json",
+	}
+	result, err := runCommand(e, cmd, "")
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+	if result.Code != 0 {
+		return nil, errors.New(string(result.Stderr))
+	}
+
+	var eksClusters []eksDetail
+	if err := json.Unmarshal(result.Stdout, &eksClusters); err != nil {
+		return nil, errors.Trace(err)
+	}
+
+	for _, item := range eksClusters {
+		clusters = append(clusters, item.Name)
+	}
+	return clusters, nil
+}
