a6d2efa0cb91b7ee2570bff523dd3dff44758ec9
Heather Lanigan <heather.lanigan@canonical.com>
2020-06-04 14:47:28 -0400

    Update uniter facade to version 16.
    Adds NewLXDProfileAPIv2: LXDProfileRequired, LXDProfileName and
    WatchInstanceData.

--- apiserver/allfacades.go
+++ apiserver/allfacades.go
@@ -337,7 +337,8 @@ func AllFacades() *facade.Registry {
 	reg("Uniter", 12, uniter.NewUniterAPIV12)
 	reg("Uniter", 13, uniter.NewUniterAPIV13)
 	reg("Uniter", 14, uniter.NewUniterAPIV14)
-	reg("Uniter", 15, uniter.NewUniterAPI)
+	reg("Uniter", 15, uniter.NewUniterAPIV15)
+	reg("Uniter", 16, uniter.NewUniterAPI)
 
 	reg("Upgrader", 1, upgrader.NewUpgraderFacade)
 	reg("UpgradeSeries", 1, upgradeseries.NewAPI)
