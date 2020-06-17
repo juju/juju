60b99ac721ed992fcebeeccf823b57a6b4b1edc7
Thomas Miller <thomas@tlm.id.au>
2020-06-18 09:32:55 +1000

    Adds support for host pid support to pod spec.
    See https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.18/#podspec-v1-core

--- caas/kubernetes/provider/specs/types.go
+++ caas/kubernetes/provider/specs/types.go
@@ -88,6 +88,7 @@ type PodSpec struct {
 	ReadinessGates                []core.PodReadinessGate  `json:"readinessGates,omitempty" yaml:"readinessGates,omitempty"`
 	DNSPolicy                     core.DNSPolicy           `json:"dnsPolicy,omitempty" yaml:"dnsPolicy,omitempty"`
 	HostNetwork                   bool                     `json:"hostNetwork,omitempty" yaml:"hostNetwork,omitempty"`
+	HostPID                       bool                     `json:"hostPID,omitempty" yaml:"hostPID,omitempty"`
 }
 
 // IsEmpty checks if PodSpec is empty or not.
