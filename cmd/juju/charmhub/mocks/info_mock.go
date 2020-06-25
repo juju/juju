86adb4ee85de9daa55385876dd7eeade293c82dd
Heather Lanigan <heather.lanigan@canonical.com>
2020-06-25 15:56:24 -0400

    Add output to info.
    The charmhub api Info call is now available.  Use it to display output.
    Data received is currently in alpha form.  There are a few TODOs as a
    result.

--- cmd/juju/charmhub/mocks/info_mock.go
+++ cmd/juju/charmhub/mocks/info_mock.go
@@ -6,6 +6,7 @@ package mocks
 
 import (
 	gomock "github.com/golang/mock/gomock"
+	charmhub "github.com/juju/juju/api/charmhub"
 	reflect "reflect"
 )
 
@@ -44,4 +45,19 @@ func (m *MockInfoCommandAPI) Close() error {
 func (mr *MockInfoCommandAPIMockRecorder) Close() *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Close", reflect.TypeOf((*MockInfoCommandAPI)(nil).Close))
+}
+
+// Info mocks base method
+func (m *MockInfoCommandAPI) Info(arg0 string) (charmhub.InfoResponse, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "Info", arg0)
+	ret0, _ := ret[0].(charmhub.InfoResponse)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// Info indicates an expected call of Info
+func (mr *MockInfoCommandAPIMockRecorder) Info(arg0 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Info", reflect.TypeOf((*MockInfoCommandAPI)(nil).Info), arg0)
 }
