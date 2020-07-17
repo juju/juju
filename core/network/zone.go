ebf254dd63962e7ded754018b0f81393a47b09e7
Joseph Phillips <joe.phillips@canonical.com>
2020-07-17 09:09:01 +0200

    Adds AvailabilityZone interface type to core/network.
    This includes a typed collection with a Validate method that will
    replace ValidateAvailabilityZone in provider/common. We should be able
    to validate using any materialised list of zones, without including a
    dependency on an environ.

---
+++ core/network/zone.go
@@ -0,0 +1,30 @@
+// Copyright 2020 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package network
+
+import "github.com/juju/errors"
+
+// AvailabilityZone describes the common methods
+// for general interaction with an AZ.
+type AvailabilityZone interface {
+	// Name returns the name of the availability zone.
+	Name() string
+
+	// Available reports whether the availability zone is currently available.
+	Available() bool
+}
+
+type AvailabilityZones []AvailabilityZone
+
+func (a AvailabilityZones) Validate(zoneName string) error {
+	for _, az := range a {
+		if az.Name() == zoneName {
+			if az.Available() {
+				return nil
+			}
+			return errors.Errorf("zone %q is unavailable", zoneName)
+		}
+	}
+	return errors.NotValidf("availability zone %q", zoneName)
+}
---
+++ core/network/zone_test.go
@@ -0,0 +1,49 @@
+// Copyright 2020 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package network
+
+import (
+	"github.com/juju/errors"
+	jujutesting "github.com/juju/testing"
+	jc "github.com/juju/testing/checkers"
+	gc "gopkg.in/check.v1"
+)
+
+type zoneSuite struct {
+	jujutesting.IsolationSuite
+
+	zones AvailabilityZones
+}
+
+var _ = gc.Suite(&zoneSuite{})
+
+func (s *zoneSuite) SetUpTest(c *gc.C) {
+	s.zones = AvailabilityZones{
+		&az{name: "zone1", available: true},
+		&az{name: "zone2"},
+	}
+
+	s.IsolationSuite.SetUpTest(c)
+}
+
+func (s *zoneSuite) TestAvailabilityZones(c *gc.C) {
+	c.Assert(s.zones.Validate("zone1"), jc.ErrorIsNil)
+	c.Assert(s.zones.Validate("zone2"), gc.ErrorMatches, `zone "zone2" is unavailable`)
+	c.Assert(s.zones.Validate("zone3"), jc.Satisfies, errors.IsNotValid)
+}
+
+type az struct {
+	name      string
+	available bool
+}
+
+var _ = AvailabilityZone(&az{})
+
+func (a *az) Name() string {
+	return a.name
+}
+
+func (a *az) Available() bool {
+	return a.available
+}
