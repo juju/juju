bcf2bfab1ad8394247a94296cb5b3252ee8333af
Simon Richardson <stickupkid@gmail.com>
2020-07-20 17:20:00 +0100

    Add comments about what the OUTPUT function does
    The output function is a bit cryptic, so I've added some comments to
    help understand that.
    
    The output function aims to solve all the outputing issues from running
    commands against juju.

--- tests/includes/output.sh
+++ tests/includes/output.sh
@@ -9,12 +9,19 @@ OUTPUT() {
     fi
 
     while read data; do
+        # If there is no output, just dump straight to stdout.
         if [ -z "${output}" ]; then
             echo "${data}"
+        # If there is an output, but we're not in verbose mode, just append to
+        # the output.
         elif [ "${VERBOSE}" -le 1 ]; then
             echo "${data}" >> "${output}"
+        # If we are in verbose mode, but we're an empty line, send to stdout
+        # and also tee it to the output.
         elif echo "${data}" | grep -q "^\s*$"; then
             echo "${data}" | tee -a "${output}"
+        # Finally, we have content and we're in verbose mode. Send the data to
+        # the output and then format it for stdout.
         else
             echo "${data}" | tee -a "${output}" | sed 's/^/    | /g'
         fi
