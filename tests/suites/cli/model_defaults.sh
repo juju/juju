914d2de279569dba67b087e3e2a7d9faf46ef7be
Simon Richardson <stickupkid@gmail.com>
2020-07-21 16:42:57 +0100

    Adding integration tests
    These are very simple ones for now, but we can always add more as we go.

---
+++ tests/suites/cli/model_defaults.sh
@@ -0,0 +1,39 @@
+run_model_defaults_isomorphic() {
+  echo
+
+  FILE=$(mktemp)
+
+  juju model-defaults --format=yaml | juju model-defaults -
+}
+
+run_model_defaults_cloudinit_userdata() {
+  echo
+
+  FILE=$(mktemp)
+
+  cat << EOF > "${FILE}"
+cloudinit-userdata: |
+  packages:
+    - shellcheck
+EOF
+
+  juju model-defaults "${FILE}"
+  juju model-defaults cloudinit-userdata --format=yaml | grep -q "default: \"\""
+  juju model-defaults cloudinit-userdata --format=yaml  | grep -q "shellcheck"
+}
+
+test_model_defaults() {
+  if [ "$(skip 'test_model_defaults')" ]; then
+    echo "==> TEST SKIPPED: model defaults"
+    return
+  fi
+
+  (
+    set_verbosity
+
+    cd .. || exit
+
+    run "run_model_defaults_isomorphic"
+    run "run_model_defaults_cloudinit_userdata"
+  )
+}
