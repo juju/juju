626485fae61cb03cc51fd5d72cf893561c862013
Joseph Phillips <joe.phillips@canonical.com>
2020-07-22 11:57:12 +0200

    Replaces direct usage of state methods with a model operation for updating link-layer devices sourced from the machine agent.
    This allows future deletion of the convoluted state logic used for this
    purpose.

--- apiserver/common/networkingcommon/networkconfigapi.go
+++ apiserver/common/networkingcommon/networkconfigapi.go
@@ -8,10 +8,13 @@ package networkingcommon
 
 import (
 	"net"
+	"strings"
 
+	"github.com/juju/collections/set"
 	"github.com/juju/errors"
 	"github.com/juju/loggo"
 	"github.com/juju/names/v4"
+	"gopkg.in/mgo.v2/txn"
 
 	"github.com/juju/juju/apiserver/common"
 	"github.com/juju/juju/apiserver/params"
@@ -33,9 +36,10 @@ func NewNetworkConfigAPI(st *state.State, getCanModify common.GetAuthFunc) *Netw
 	}
 }
 
-// SetObservedNetworkConfig reads the network config for the machine identified
-// by the input args. This config is merged with the new network config supplied
-// in the same args and updated if it has changed.
+// SetObservedNetworkConfig reads the network config for the machine
+// identified by the input args.
+// This config is merged with the new network config supplied in the
+// same args and updated if it has changed.
 func (api *NetworkConfigAPI) SetObservedNetworkConfig(args params.SetMachineNetworkConfig) error {
 	m, err := api.getMachineForSettingNetworkConfig(args.Tag)
 	if err != nil {
@@ -54,12 +58,18 @@ func (api *NetworkConfigAPI) SetObservedNetworkConfig(args params.SetMachineNetw
 		return errors.Trace(err)
 	}
 
-	ifaces := params.InterfaceInfoFromNetworkConfig(mergedConfig)
-	return api.setLinkLayerDevicesAndAddresses(m, ifaces)
+	devs := params.InterfaceInfoFromNetworkConfig(mergedConfig)
+	if err = devs.Validate(); err != nil {
+		return errors.Trace(err)
+	}
+
+	return errors.Trace(api.st.ApplyOperation(newUpdateMachineLinkLayerOp(&machineShim{m}, devs)))
 }
 
-// fixUpFanSubnets takes network config and updates FAN subnets with proper CIDR, providerId and providerSubnetId.
-// The method how fan overlay is cut into segments is described in network/fan.go.
+// fixUpFanSubnets takes network config and updates Fan subnets
+// with proper CIDR, providerId and providerSubnetId.
+// See network/fan.go for more detail on how Fan overlays
+// are divided into segments.
 func (api *NetworkConfigAPI) fixUpFanSubnets(networkConfig []params.NetworkConfig) ([]params.NetworkConfig, error) {
 	subnets, err := api.st.AllSubnets()
 	if err != nil {
@@ -125,21 +135,213 @@ func (api *NetworkConfigAPI) getMachine(tag names.MachineTag) (*state.Machine, e
 	return entity.(*state.Machine), nil
 }
 
-func (api *NetworkConfigAPI) setLinkLayerDevicesAndAddresses(
-	m *state.Machine, interfaceInfos network.InterfaceInfos,
-) error {
-	devicesArgs, devicesAddrs := NetworkInterfacesToStateArgs(interfaceInfos)
+// mergeMachineLinkLayerOp is a model operation used to merge incoming
+// provider-sourced network configuration with existing data for a single
+// machine/host/container.
+type updateMachineLinkLayerOp struct {
+	*MachineLinkLayerOp
 
-	logger.Debugf("setting devices: %+v", devicesArgs)
-	if err := m.SetParentLinkLayerDevicesBeforeTheirChildren(devicesArgs); err != nil {
-		return errors.Trace(err)
+	// removalCandidates are devices that exist in state, but since have not
+	// been observed by the instance-poller or machine agent.
+	// We check that these can be deleted after processing all devices.
+	removalCandidates []LinkLayerDevice
+
+	// observedParentIDs are the IDs of link-layer devices that are parents of
+	// children that we are *not* deleting, thus preventing such parents from
+	// being deleted.
+	observedParentIDs set.Strings
+}
+
+func newUpdateMachineLinkLayerOp(
+	machine LinkLayerMachine, incoming network.InterfaceInfos,
+) *updateMachineLinkLayerOp {
+	return &updateMachineLinkLayerOp{
+		MachineLinkLayerOp: NewMachineLinkLayerOp(machine, incoming),
+	}
+}
+
+// Build (state.ModelOperation) returns the transaction operations used to
+// merge incoming provider link-layer data with that in state.
+func (o *updateMachineLinkLayerOp) Build(_ int) ([]txn.Op, error) {
+	if err := o.PopulateExistingDevices(); err != nil {
+		return nil, errors.Trace(err)
 	}
 
-	logger.Debugf("setting addresses: %+v", devicesAddrs)
-	if err := m.SetDevicesAddresses(devicesAddrs...); err != nil {
-		return errors.Trace(err)
+	if err := o.PopulateExistingAddresses(); err != nil {
+		return nil, errors.Trace(err)
 	}
 
-	logger.Debugf("updated machine %q network config", m.Id())
-	return nil
+	// TODO: Consider walking the hierarchy here (maybe) and removing incoming
+	// child devices that don't have a parent either incoming also, or residing
+	// in state. This would prevent us having to check whether removal
+	// candidates have an incoming first-time child - however unlikely this is.
+
+	// InterfaceInfo validation is now called before this model Op is
+	// validated, but the logic does not yet replicate validation of parents as
+	// is currently done is state.
+
+	var ops []txn.Op
+	for _, existingDev := range o.ExistingDevices() {
+		devOps, err := o.processExistingDevice(existingDev)
+		if err != nil {
+			return nil, errors.Trace(err)
+		}
+		ops = append(ops, devOps...)
+	}
+
+	addOps, err := o.processNewDevices()
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+	ops = append(ops, addOps...)
+
+	ops = append(ops, o.processRemovalCandidates()...)
+
+	return ops, nil
+}
+
+func (o *updateMachineLinkLayerOp) processExistingDevice(dev LinkLayerDevice) ([]txn.Op, error) {
+	incomingDev := o.MatchingIncoming(dev)
+
+	if incomingDev == nil {
+		ops, err := o.processExistingDeviceNotObserved(dev)
+		return ops, errors.Trace(err)
+	}
+
+	// At this point we know that we are dealing with a device that is both in
+	// state and observed once again on the machine.
+	// We want to ensure that if it has a parent device,
+	// then that device can not be deleted.
+	parentID := dev.ParentID()
+	if parentID != "" {
+		o.observedParentIDs.Add(parentID)
+	}
+
+	ops := dev.UpdateOps(networkDeviceToStateArgs(*incomingDev))
+
+	incomingAddrs := o.MatchingIncomingAddrs(dev.MACAddress())
+
+	for _, addr := range o.DeviceAddresses(dev) {
+		existingAddrOps, err := o.processExistingDeviceAddress(dev, addr, incomingAddrs)
+		if err != nil {
+			return nil, errors.Trace(err)
+		}
+		ops = append(ops, existingAddrOps...)
+	}
+
+	newAddrOps, err := o.processExistingDeviceNewAddresses(dev, incomingAddrs)
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+	return append(ops, newAddrOps...), nil
+}
+
+// processExistingDeviceNotObserved returns transaction operations for
+// processing a device we have in state, but that the machine agent no
+// longer observes locally.
+// The device itself is not marked for deletion now, but for later processing
+// to ensure it is not a parent of other observed devices.
+func (o *updateMachineLinkLayerOp) processExistingDeviceNotObserved(dev LinkLayerDevice) ([]txn.Op, error) {
+	addrs := o.DeviceAddresses(dev)
+
+	var ops []txn.Op
+	var removing int
+	for _, addr := range addrs {
+		// If the machine is the authority for this address,
+		// we can delete it; otherwise leave it alone.
+		if addr.Origin() == network.OriginMachine {
+			logger.Debugf("removing address %q from device %q", addr.Value(), dev.Name())
+			ops = append(ops, addr.RemoveOps()...)
+			removing++
+		}
+	}
+
+	// If the device is having all of its addresses removed and is not under
+	// the authority of the provider, add it as a candidate for removal.
+	// If the device has been relinquished by the provider, the instance-poller
+	// will have removed the provider ID - see the instance-poller API facade
+	// logic.
+	if removing == len(addrs) && dev.ProviderID() == "" {
+		o.removalCandidates = append(o.removalCandidates, dev)
+	}
+
+	return ops, nil
+}
+
+func (o *updateMachineLinkLayerOp) processExistingDeviceAddress(
+	dev LinkLayerDevice, addr LinkLayerAddress, incomingAddrs []state.LinkLayerDeviceAddress,
+) ([]txn.Op, error) {
+	addrValue := addr.Value()
+
+	// If one of the incoming addresses matches the existing one,
+	// update it.
+	for _, incomingAddr := range incomingAddrs {
+		if strings.HasPrefix(incomingAddr.CIDRAddress, addrValue) {
+			o.MarkAddrProcessed(dev.MACAddress(), incomingAddr.CIDRAddress)
+			ops, err := addr.UpdateOps(incomingAddr)
+			return ops, errors.Trace(err)
+		}
+	}
+
+	// Otherwise if we are the authority, delete it.
+	if addr.Origin() == network.OriginMachine {
+		logger.Debugf("removing address %q from device %q", addrValue, addr.DeviceName())
+		return addr.RemoveOps(), nil
+	}
+
+	return nil, nil
+}
+
+// processExistingDeviceNewAddresses interrogates the list of incoming
+// addresses and adds any that were not processed as already existing.
+func (o *updateMachineLinkLayerOp) processExistingDeviceNewAddresses(
+	dev LinkLayerDevice, incomingAddrs []state.LinkLayerDeviceAddress,
+) ([]txn.Op, error) {
+	var ops []txn.Op
+	for _, addr := range incomingAddrs {
+		if !o.IsAddrProcessed(dev.MACAddress(), addr.CIDRAddress) {
+			addOps, err := dev.AddAddressOps(addr)
+			if err != nil {
+				return nil, errors.Trace(err)
+			}
+			ops = append(ops, addOps...)
+		}
+	}
+	return ops, nil
+}
+
+// processNewDevices handles incoming devices that
+// did not match any we already have in state.
+func (o *updateMachineLinkLayerOp) processNewDevices() ([]txn.Op, error) {
+	var ops []txn.Op
+	for _, dev := range o.Incoming() {
+		if !o.IsDevProcessed(dev) {
+			logger.Debugf("adding new device %q (%s) with addresses %v",
+				dev.InterfaceName, dev.MACAddress, dev.Addresses)
+
+			addOps, err := o.machine.AddLinkLayerDeviceOps(
+				networkDeviceToStateArgs(dev), o.MatchingIncomingAddrs(dev.MACAddress)...)
+			if err != nil {
+				return nil, errors.Trace(err)
+			}
+			ops = append(ops, addOps...)
+		}
+	}
+	return ops, nil
+}
+
+// processRemovalCandidates returns transaction operations for
+// removing unobserved devices that it is safe to delete.
+// A device is considered safe to delete if it has no children,
+// or if all of its children are also candidates for deletion.
+// Any device considered here will already have ops generated
+// for removing its addresses.
+func (o *updateMachineLinkLayerOp) processRemovalCandidates() []txn.Op {
+	var ops []txn.Op
+	for _, dev := range o.removalCandidates {
+		if !o.observedParentIDs.Contains(dev.ID()) {
+			ops = append(ops, dev.RemoveOps()...)
+		}
+	}
+	return ops
 }
