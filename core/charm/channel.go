80eaaf20eff1c8f6c48078ce94608e52e762f931
Simon Richardson <stickupkid@gmail.com>
2020-07-28 10:10:44 +0100

    Improve the channel documentation
    The snap channel documentation is good enough to understand, so much so
    we can derive the channel implementation.
    
    https://snapcraft.io/docs/channels

--- core/charm/channel.go
+++ core/charm/channel.go
@@ -37,6 +37,21 @@ func isRisk(potential string) bool {
 }
 
 // Channel identifies and describes completely a store channel.
+//
+// A channel consists of, and is subdivided by, tracks, risk-levels and
+// branches:
+//  - Tracks enable snap developers to publish multiple supported releases of
+//    their application under the same snap name.
+//  - Risk-levels represent a progressive potential trade-off between stability
+//    and new features.
+//  - Branches are _optional_ and hold temporary releases intended to help with
+//    bug-fixing.
+//
+// The complete channel name can be structured as three distinct parts separated
+// by slashes:
+//
+//    <track>/<risk>/<branch>
+//
 type Channel struct {
 	Track  string
 	Risk   Risk
