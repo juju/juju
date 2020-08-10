95ee05660bed548ea84c3743ab29cb055134e477
Achilleas Anagnostopoulos <achilleas.anagnostopoulos@canonical.com>
2020-08-10 15:41:37 +0100

    Sync contents of remove juju script in juju tree to the one installed

--- scripts/removejujuservices.bash
+++ scripts/removejujuservices.bash
@@ -10,7 +10,11 @@ for path_to_unit in $(ls /etc/systemd/system/juju*); do
   systemctl stop "$unit"
   systemctl disable "$unit"
   systemctl daemon-reload
+  rm -f "$path_to_unit"
 done
+
+echo "removing /var/lib/juju/tools/*"
+rm -rf /var/lib/juju/tools/*
 
 echo "removing /var/lib/juju/db/*"
 rm -rf /var/lib/juju/db/*
@@ -20,3 +24,9 @@ rm -rf /var/lib/juju/raft/*
 
 echo "removing /var/run/juju/*"
 rm -rf /var/run/juju/*
+
+has_juju_db_snap=$(snap info juju-db | grep installed:)
+if [ ! -z "$has_juju_db_snap" ]; then
+  echo "removing juju-db snap and any persisted database data"
+  snap remove --purge juju-db
+fi
