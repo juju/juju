a42c6c5f1903e3a303edba4b90af98e5a29ac888
Simon Richardson <stickupkid@gmail.com>
2020-08-27 10:07:00 +0100

    Ensure that the tests compile
    There are no neew tests here, just fixes to ensure that we have a
    existing tests pass.
    
    Originally I spent time trying to wire up mocks for this, but it was a
    rabbit hole of work, one that doesn't justify the cause.

--- apiserver/facades/client/modelmanager/package_test.go
+++ apiserver/facades/client/modelmanager/package_test.go
@@ -9,6 +9,8 @@ import (
 	"github.com/juju/juju/testing"
 )
 
+//go:generate go run github.com/golang/mock/mockgen -package mocks -destination mocks/state_mock.go github.com/juju/juju/apiserver/facades/client/modelmanager StatePool,State,Model,Machine
+
 func TestAll(t *stdtesting.T) {
 	testing.MgoTestPackage(t)
 }
