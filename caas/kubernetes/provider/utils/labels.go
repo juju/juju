94f2157b8ed82b5bc3be08b933a60990a6ef0584
John Arbash Meinel <john@arbash-meinel.com>
2020-09-11 07:03:52 +0400

    gofmt fixes for tabs vs spaces.

--- caas/kubernetes/provider/utils/labels.go
+++ caas/kubernetes/provider/utils/labels.go
@@ -41,9 +41,9 @@ func LabelsForApp(name string) map[string]string {
 
 // LabelForKeyValue returns a Kubernetes label set for the supplied key value.
 func LabelForKeyValue(key, value string) k8slabels.Set {
-       return k8slabels.Set{
-               key: value,
-       }
+	return k8slabels.Set{
+		key: value,
+	}
 }
 
 // LabelsForModel returns the labels that should be on a k8s object for a given
