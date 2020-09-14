3a440c847e6a28dac1140d1e29aa03083dba1e6c
Achilleas Anagnostopoulos <achilleas.anagnostopoulos@canonical.com>
2020-09-14 11:22:13 +0100

    Implement client for ExposeInfo API call

--- api/firewaller/application.go
+++ api/firewaller/application.go
@@ -64,3 +64,32 @@ func (s *Application) IsExposed() (bool, error) {
 	}
 	return result.Result, nil
 }
+
+// ExposeInfo returns a flag to indicate whether an application is exposed
+// as well as any endpoint-specific expose settings (if present).
+func (s *Application) ExposeInfo() (bool, map[string]params.ExposedEndpoint, error) {
+	if s.st.BestAPIVersion() < 6 {
+		// ExposeInfo() was introduced in FirewallerAPIV6.
+		return false, nil, errors.NotImplementedf("ExposeInfo() (need V6+)")
+	}
+
+	var results params.ExposeInfoResults
+	args := params.Entities{
+		Entities: []params.Entity{{Tag: s.tag.String()}},
+	}
+	err := s.st.facade.FacadeCall("GetExposeInfo", args, &results)
+	if err != nil {
+		return false, nil, err
+	}
+	if len(results.Results) != 1 {
+		return false, nil, fmt.Errorf("expected 1 result, got %d", len(results.Results))
+	}
+	result := results.Results[0]
+	if result.Error != nil {
+		if params.IsCodeNotFound(result.Error) {
+			return false, nil, errors.NewNotFound(result.Error, "")
+		}
+		return false, nil, result.Error
+	}
+	return result.Exposed, result.ExposedEndpoints, nil
+}
