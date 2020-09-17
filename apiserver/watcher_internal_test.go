3c41b441541ec43a0796bf91fca723f49a0a748e
Simon Richardson <stickupkid@gmail.com>
2020-09-17 14:18:40 +0100

    Application status shouldn't use model cache
    This fix is very subtle, because it highlights a problem with how the
    all watcher is consumed by different clients (model cache, pylibjuju,
    jslibjuju).
    
    When you connect to the allwatcher you're given a series of deltas and
    is expected that the client consumes that as verbatim. There is a
    problem though, with the insert of the previous derviation status it
    causes problems where it's impossible to know what the status of an
    application is right now.
    
    At the application delta is dispatched once at the start of signing on
    to the allwatcher, you get what it was at the time of sign on, which is
    inconsisent.
    
    With the changes to getting the application status from the model cache
    then depends if the units have primed the model cache in time to get
    that information.
    
    If we assume the diagram below we can see that depending on when you
    join the allwatcher defines what application status.
    
    If you're AllWatcher (A, B, C) you'll never see that the unit status is
    blocked.
    
    In order for pylibjuju the *only* way to get the right information is to
    do it in pylibjuju.
    
    ```
     ALLWATCHER  ALLWATCHER  ALLWATCHER
         (A)         (B)         (C)
          +           +           +
          |           |           |
          |           |           |
          |           |           |
          |           |           |
          |           |           |
    ---+--+-----------+-+---------+------+-----------> TIME
       ^                ^                ^
       |                |                |
       |                |                |
       |                |                |
       +                +                +
    
    APP STATUS      UNIT STATUS      UNIT STATUS
     (UNSET)         (WAITING)        (BLOCKED)
    ```

--- apiserver/watcher_internal_test.go
+++ apiserver/watcher_internal_test.go
@@ -4,13 +4,9 @@
 package apiserver
 
 import (
-	"time"
-
-	"github.com/juju/worker/v2/workertest"
 	gc "gopkg.in/check.v1"
 
 	"github.com/juju/juju/apiserver/params"
-	"github.com/juju/juju/core/cache"
 	"github.com/juju/juju/core/life"
 	"github.com/juju/juju/core/multiwatcher"
 	"github.com/juju/juju/core/status"
@@ -39,87 +35,6 @@ func (s *allWatcherSuite) TestTranslateApplicationWithStatus(c *gc.C) {
 		Life:      life.Alive,
 		Status: multiwatcher.StatusInfo{
 			Current: status.Active,
-		},
-	}
-	output := w.translateApplication(input)
-	c.Assert(output, jc.DeepEquals, &params.ApplicationInfo{
-		ModelUUID: input.ModelUUID,
-		Name:      input.Name,
-		CharmURL:  input.CharmURL,
-		Life:      input.Life,
-		Status: params.StatusInfo{
-			Current: status.Active,
-		},
-	})
-}
-
-func (s *allWatcherSuite) setupCache(c *gc.C) *cache.Controller {
-	changes := make(chan interface{})
-	handled := make(chan interface{})
-	notify := func(evt interface{}) {
-		c.Logf("%#v", evt)
-		select {
-		case handled <- evt:
-		case <-time.After(testing.LongWait):
-			c.Fatalf("handled notify not retrieved")
-		}
-	}
-	sendEvent := func(event interface{}) {
-		select {
-		case changes <- event:
-		case <-time.After(testing.LongWait):
-			c.Fatal("cache did not accept event")
-		}
-		select {
-		case <-handled:
-		case <-time.After(testing.LongWait):
-			c.Fatal("cache did not handle event")
-		}
-	}
-
-	controller, err := cache.NewController(cache.ControllerConfig{
-		Changes: changes,
-		Notify:  notify,
-	})
-	c.Assert(err, jc.ErrorIsNil)
-	s.AddCleanup(func(c *gc.C) { workertest.CleanKill(c, controller) })
-
-	sendEvent(cache.ModelChange{
-		ModelUUID: testing.ModelTag.Id(),
-		// Defaults for everything else.
-	})
-	sendEvent(cache.ApplicationChange{
-		ModelUUID: testing.ModelTag.Id(),
-		Name:      "test-app",
-		Status: status.StatusInfo{
-			Status: status.Unset,
-		},
-		// Defaults for everything else.
-	})
-	sendEvent(cache.UnitChange{
-		ModelUUID:   testing.ModelTag.Id(),
-		Name:        "test-app/0",
-		Application: "test-app",
-		WorkloadStatus: status.StatusInfo{
-			Status: status.Active,
-		},
-		// Defaults for everything else.
-	})
-
-	return controller
-}
-
-func (s *allWatcherSuite) TestTranslateApplicationStatusUnset(c *gc.C) {
-	controller := s.setupCache(c)
-	w := s.watcher()
-	w.controller = controller
-	input := &multiwatcher.ApplicationInfo{
-		ModelUUID: testing.ModelTag.Id(),
-		Name:      "test-app",
-		CharmURL:  "test-app",
-		Life:      life.Alive,
-		Status: multiwatcher.StatusInfo{
-			Current: status.Unset,
 		},
 	}
 	output := w.translateApplication(input)
