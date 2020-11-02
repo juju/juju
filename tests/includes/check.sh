df8212a9d7b691e6ee1cc484349a396218f7440b
Simon Richardson <stickupkid@gmail.com>
2020-11-02 14:37:55 +0000

    Juju plugin check
    If using snaps then juju-metadata won't exist, instead it will have sort
    of forwarding from juju to juju metadata and so the which check won't
    work.
    
    Interestingly I'm surprised this even works with plugins as it expects
    things to be on a given path.

--- tests/includes/check.sh
+++ tests/includes/check.sh
@@ -15,6 +15,23 @@ check_dependencies() {
     fi
 }
 
+check_juju_dependencies() {
+    local dep missing
+    missing=""
+
+    for dep in "$@"; do
+        if ! juju "$dep" >/dev/null 2>&1; then
+            [ "$missing" ] && missing="$missing $dep" || missing="$dep"
+        fi
+    done
+
+    if [ "$missing" ]; then
+        echo "Missing dependencies: $missing" >&2
+        echo ""
+        exit 1
+    fi
+}
+
 check_not_contains() {
     local input value chk
 
