02e749b96c0fa7b09803cb39e678df36d924c538
Heather Lanigan <heather.lanigan@canonical.com>
2020-11-04 16:32:57 -0500

    When using an already deployed aws controller, the provider shows up as ec2.

--- tests/includes/juju.sh
+++ tests/includes/juju.sh
@@ -46,6 +46,9 @@ bootstrap() {
         "aws")
             provider="aws"
             ;;
+        "ec2")
+            provider="aws"
+            ;;
         "localhost")
             provider="lxd"
             ;;
