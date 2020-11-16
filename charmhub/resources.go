24624b08a97041589831c73f73f0498e634f7539
Heather Lanigan <heather.lanigan@canonical.com>
2020-11-16 15:10:34 -0500

    Add ResourceRevision for use in info and refresh.
    A recent addition to the charmhub api for resources.  Including fields
    to include the data with the info response.

---
+++ charmhub/resources.go
@@ -0,0 +1,16 @@
+// Copyright 2020 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package charmhub
+
+var resourceFilter = []string{
+	"download.hash-sha256",
+	"download.hash-sha3-384",
+	"download.hash-sha384",
+	"download.hash-sha512",
+	"download.size",
+	"download.url",
+	"name",
+	"revision",
+	"type",
+}
