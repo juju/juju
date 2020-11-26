95fd6ffba49f56c6489c77c6193638c2d3a0a73d
Joseph Phillips <joe.phillips@canonical.com>
2020-11-26 17:10:46 +0100

    Includes client version in API login tests. Fixes upgrade tests. Includes missing legacy labels function CAAS upgrade broker.

--- featuretests/agent_caasoperator_test.go
+++ featuretests/agent_caasoperator_test.go
@@ -60,10 +60,10 @@ func (s *CAASOperatorSuite) SetUpTest(c *gc.C) {
 
 	s.PatchValue(&provider.NewK8sClients, k8stesting.NoopFakeK8sClients)
 	// Set up a CAAS model to replace the IAAS one.
-	// Ensure an older major version is used to prevent an upgrade
+	// Ensure major version 1 is used to prevent an upgrade
 	// from being attempted.
 	modelVers := jujuversion.Current
-	modelVers.Major--
+	modelVers.Major = 1
 	extraAttrs := coretesting.Attrs{
 		"agent-version": modelVers.String(),
 	}
