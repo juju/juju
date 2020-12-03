37ba9933f44bd326f0d514c0e4c558e033429ddf
Simon Richardson <stickupkid@gmail.com>
2020-12-03 11:58:16 +0000

    Omit empty on the charm origin type
    This needs an upgrade step, which will follow.

--- state/charm.go
+++ state/charm.go
@@ -69,7 +69,7 @@ type Platform struct {
 // changing.
 type CharmOrigin struct {
 	Source   string    `bson:"source"`
-	Type     string    `bson:"type"`
+	Type     string    `bson:"type,omitempty"`
 	ID       string    `bson:"id"`
 	Hash     string    `bson:"hash"`
 	Revision *int      `bson:"revision,omitempty"`
