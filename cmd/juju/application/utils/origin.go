dc413d06bd9db8109da193f9da4bee7b6fdddb23
Simon Richardson <stickupkid@gmail.com>
2021-01-19 11:00:37 +0000

    Ensure we pass an arch for charmstore charms
    The following ensures that we at least attempt to handle missing
    architectual origins in charm-store charms. This is not mandatory and
    shouldn't reflect what actually is installed 100%, instead should be an
    indicator or what was used at install time.

--- cmd/juju/application/utils/origin.go
+++ cmd/juju/application/utils/origin.go
@@ -38,9 +38,10 @@ func DeduceOrigin(url *charm.URL, channel corecharm.Channel, platform corecharm.
 	switch url.Schema {
 	case "cs":
 		return commoncharm.Origin{
-			Source: commoncharm.OriginCharmStore,
-			Risk:   string(channel.Risk),
-			Series: platform.Series,
+			Source:       commoncharm.OriginCharmStore,
+			Risk:         string(channel.Risk),
+			Architecture: architecture,
+			Series:       platform.Series,
 		}, nil
 	case "local":
 		return commoncharm.Origin{
