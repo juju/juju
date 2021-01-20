cb4402a4d1b39ef93fdaf75dfb6f73443914a63d
Oleg Gaidarenko <markelog@gmail.com>
2021-01-20 17:55:09 +0300

    Resume tests execution on macos
    There was four types of issues for running tests on macos.
    
    Makefile
    On macos syntax of the command `mktemp` is a bit different, it doesn't have
    the `--tmpdir` argument on macos it just creates a directory `--tmpdir` in
    the source path and bunch of other temporary directories. This complecates the
    development and confuses the tests.
    
    makefile: remove deadspace
    makefile: make creation of temp dir cross os
    
    Plugin Tests
    Plugin code checks every path on the `$PATH` list in order to
    find addtional plugins. In tests it reduces the amount of paths
    by temporary editing the `$PATH` list leaving  only temp dir and `/bin` folder.
    Assuming that the bash commands used inside the tests are present on the target
    system. Like `touch` for example, but in newer macos systems, some of the
    commands are present inside `/usr/bin` path
    
    tests: carefully edit `$PATH` variable in tests, so it would be cross OS
    
    CI Configuration
    There was minor issues with macos preparation instructions. Like putting
    mongo on incorrect path `/user/...` instead of `/usr/...` and assuming that
    GitHub runners are doesn't have mongodb installed.
    
    ci: correct macos preparation instructions
    
    `go test` Parallization Issues
    
    There is several tests that used mongodb concurrently,
    at least "cmd/application" and "juju/commands". Presumably operationg on the
    same set of data and/or their structure. It makes the tests execution a little
    bit slower on macos environment. Have not invisitigated why this doesn't happen
    on ubuntu. Perhaps amount of CPUs already limited or because Go differently
    works with parallization on Linux machines
    
    ci: limit amount of available CPUs to one for macos setup

--- cmd/juju/commands/plugin_test.go
+++ cmd/juju/commands/plugin_test.go
@@ -37,7 +37,14 @@ func (suite *PluginSuite) SetUpTest(c *gc.C) {
 	}
 	suite.FakeJujuXDGDataHomeSuite.SetUpTest(c)
 	suite.oldPath = os.Getenv("PATH")
-	os.Setenv("PATH", "/bin:"+gitjujutesting.HomePath())
+
+	// We have to be careful to leave the default cmds that we need for tests
+	// like "touch" (which its in /usr/bin on mac but in /bin on linux).
+	// We doings this, because we need to add "binaries" from the tmp dir and reduce
+	// tests exectuion, since we are looking into all paths in $PATH to find juju plugins
+	os.Setenv("PATH", fmt.Sprintf(
+		"/bin:/usr/bin:%s", gitjujutesting.HomePath(),
+	))
 	jujuclienttesting.SetupMinimalFileStore(c)
 }
 
