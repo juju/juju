3dfbb776541cf9c0d6ce8d47713ec2c28c5d0198
Simon Richardson <stickupkid@gmail.com>
2021-02-11 16:26:44 +0000

    Use a better series than xenial

--- tests/suites/unit/unit_series.sh
+++ tests/suites/unit/unit_series.sh
@@ -6,7 +6,7 @@ run_unit_set_series() {
     file="${TEST_DIR}/test-unit-series.log"
     ensure "unit-series" "${file}"
 
-    juju deploy cs:ubuntu --series=xenial
+    juju deploy cs:ubuntu --series=bionic
 
     wait_for "ubuntu" "$(idle_condition "ubuntu")"
 
@@ -16,6 +16,8 @@ run_unit_set_series() {
     wait_for "ubuntu" "$(idle_condition "ubuntu" 0 1)"
 
     juju status --format=json | jq -r ".machines | .[\"1\"] | .series" | grep "focal"
+
+    destroy_model "unit-series"
 }
 
 test_unit_series() {
