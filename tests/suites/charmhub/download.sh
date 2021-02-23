0acd4f81d038690013b16d1c200fbd2b1a4cc45c
Heather Lanigan <heather.lanigan@canonical.com>
2021-02-23 16:22:14 -0500

    Add --series to download of mysql.
    Recent changes to the Refresh response around series, means that by
    default `juju download mysql` will fail with ErrorCodeRevisionNotFound.
    Use --series to avoid the test failing.

--- tests/suites/charmhub/download.sh
+++ tests/suites/charmhub/download.sh
@@ -6,7 +6,7 @@ run_charmhub_download() {
 
     ensure "test-${name}" "${file}"
 
-    output=$(juju download mysql --filepath="${TEST_DIR}/mysql.charm" 2>&1 || true)
+    output=$(juju download mysql --series xenial --filepath="${TEST_DIR}/mysql.charm" 2>&1 || true)
     check_contains "${output}" "Fetching charm \"mysql\""
 
     juju deploy "${TEST_DIR}/mysql.charm" mysql
