5a63ec9e79c3382ee8fa3ac0c6df584449ab21c7
Achilleas Anagnostopoulos <achilleas.anagnostopoulos@canonical.com>
2021-03-01 18:47:02 +0000

    Re-arrange starter commands in help text and rename REPL

--- cmd/juju/commands/main.go
+++ cmd/juju/commands/main.go
@@ -79,7 +79,6 @@ See https://juju.is for getting started tutorials and additional documentation.
 
 Starter commands:
 
-    add-k8s             Adds a k8s endpoint and credential to Juju.
     bootstrap           Initializes a cloud environment.
     add-model           Adds a hosted model.
     deploy              Deploys a new application.
@@ -91,10 +90,11 @@ Starter commands:
     controllers         Lists all controllers.
     whoami              Display the current controller, model and logged in user name. 
     switch              Selects or identifies the current controller and model.
+    add-k8s             Adds a k8s endpoint and credential to Juju.
     add-cloud           Adds a user-defined cloud to Juju.
     add-credential      Adds or replaces credentials for a cloud.
 
-REPL mode:
+Interactive mode:
 
 When run without arguments, Juju will enter an interactive shell which can be
 used to run any Juju command directly.
