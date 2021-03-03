a44ff344b554232683fa1625130bfbaa67f8faa1
Joseph Phillips <joe.phillips@canonical.com>
2021-03-03 13:59:57 +0100

    Adds ConfigSourceNIC implementation for netlink.Link.

--- core/network/source_netlink.go
+++ core/network/source_netlink.go
@@ -6,6 +6,7 @@ package network
 import (
 	"net"
 
+	"github.com/juju/errors"
 	"github.com/vishvananda/netlink"
 )
 
@@ -28,4 +29,67 @@ func (a *netlinkAddr) IPNet() *net.IPNet {
 // String (ConfigSourceAddr) is a simple property accessor.
 func (a *netlinkAddr) String() string {
 	return a.addr.String()
+}
+
+// netlinkNIC implements ConfigSourceNIC by wrapping a netlink Link.
+type netlinkNIC struct {
+	nic      netlink.Link
+	getAddrs func(netlink.Link) ([]netlink.Addr, error)
+}
+
+// Name returns the name of the device.
+func (n netlinkNIC) Name() string {
+	return n.nic.Attrs().Name
+}
+
+// Type returns the interface type of the device.
+func (n netlinkNIC) Type() InterfaceType {
+	switch n.nic.Type() {
+	case "bridge":
+		return BridgeInterface
+	case "vlan":
+		return VLAN_8021QInterface
+	case "bond":
+		return BondInterface
+	}
+
+	if n.nic.Attrs().Flags&net.FlagLoopback > 0 {
+		return LoopbackInterface
+	}
+
+	return EthernetInterface
+}
+
+// Index returns the index of the device.
+func (n netlinkNIC) Index() int {
+	return n.nic.Attrs().Index
+}
+
+// HardwareAddr returns the hardware address of the device.
+func (n netlinkNIC) HardwareAddr() net.HardwareAddr {
+	return n.nic.Attrs().HardwareAddr
+}
+
+// Addresses returns all IP addresses associated with the device.
+func (n netlinkNIC) Addresses() ([]ConfigSourceAddr, error) {
+	rawAddrs, err := n.getAddrs(n.nic)
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+
+	addrs := make([]ConfigSourceAddr, len(rawAddrs))
+	for i := range rawAddrs {
+		addrs[i] = &netlinkAddr{&rawAddrs[i]}
+	}
+	return addrs, nil
+}
+
+// MTU returns the maximum transmission unit for the device.
+func (n netlinkNIC) MTU() int {
+	return n.nic.Attrs().MTU
+}
+
+// IsUp returns true if the interface is in the "up" state.
+func (n netlinkNIC) IsUp() bool {
+	return n.nic.Attrs().Flags&net.FlagUp > 0
 }
--- core/network/source_netlink_test.go
+++ core/network/source_netlink_test.go
@@ -1,6 +1,8 @@
 package network
 
 import (
+	"net"
+
 	"github.com/juju/testing"
 	jc "github.com/juju/testing/checkers"
 	"github.com/vishvananda/netlink"
@@ -33,4 +35,74 @@ func (s *sourceNetlinkSuite) TestNetlinkAddr(c *gc.C) {
 	c.Check(addr.IP().String(), gc.Equals, "fe80::5054:ff:fedd:eef0")
 	c.Assert(addr.IPNet(), gc.NotNil)
 	c.Check(addr.IPNet().String(), gc.Equals, "fe80::5054:ff:fedd:eef0/64")
+}
+
+func (s *sourceNetlinkSuite) TestNetlinkAttrs(c *gc.C) {
+	link := &stubLink{flags: net.FlagUp}
+	nic := &netlinkNIC{nic: link}
+
+	c.Check(nic.MTU(), gc.Equals, 1500)
+	c.Check(nic.Name(), gc.Equals, "eno3")
+	c.Check(nic.IsUp(), jc.IsTrue)
+	c.Check(nic.Index(), gc.Equals, 3)
+	c.Check(nic.HardwareAddr(), gc.DeepEquals, net.HardwareAddr{})
+}
+
+func (s *sourceNetlinkSuite) TestNetlinkNICType(c *gc.C) {
+	link := &stubLink{}
+	nic := &netlinkNIC{nic: link}
+
+	// If we have get value, return it.
+	link.linkType = "bond"
+	c.Check(nic.Type(), gc.Equals, BondInterface)
+
+	// Infer loopback from flags.
+	link.linkType = ""
+	link.flags = net.FlagUp | net.FlagLoopback
+	c.Check(nic.Type(), gc.Equals, LoopbackInterface)
+
+	// Default to ethernet otherwise.
+	link.flags = net.FlagUp | net.FlagBroadcast | net.FlagMulticast
+	c.Check(nic.Type(), gc.Equals, EthernetInterface)
+}
+
+func (s *sourceNetlinkSuite) TestNetlinkNICAddrs(c *gc.C) {
+	raw, err := netlink.ParseAddr("192.168.20.1/24")
+	c.Assert(err, jc.ErrorIsNil)
+
+	getAddrs := func(link netlink.Link) ([]netlink.Addr, error) {
+		// Check that we called correctly passing the inner nic.
+		c.Assert(link.Attrs().Name, gc.Equals, "eno3")
+		return []netlink.Addr{*raw}, nil
+	}
+
+	nic := netlinkNIC{
+		nic:      &stubLink{},
+		getAddrs: getAddrs,
+	}
+
+	addrs, err := nic.Addresses()
+	c.Assert(err, jc.ErrorIsNil)
+	c.Assert(addrs, gc.HasLen, 1)
+	c.Assert(addrs[0].String(), gc.Equals, "192.168.20.1/24")
+}
+
+// stubLink stubs netlink.Link
+type stubLink struct {
+	linkType string
+	flags    net.Flags
+}
+
+func (l *stubLink) Attrs() *netlink.LinkAttrs {
+	return &netlink.LinkAttrs{
+		Index:        3,
+		MTU:          1500,
+		Name:         "eno3",
+		HardwareAddr: net.HardwareAddr{},
+		Flags:        l.flags,
+	}
+}
+
+func (l *stubLink) Type() string {
+	return l.linkType
 }
