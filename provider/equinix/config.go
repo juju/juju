f456ddebf13e728ca89cfdad3fd3ff80974be4b9
root <jasmin.gacic@gmail.com>
2021-05-14 09:08:07 +0000

    Implement Equinix Metal provider configuration and credentials
    This commit contains the struct and functions needed to correctly hook
    Equinix Metal API authentication to Juju
    
    Signed-off-by: root <jasmin.gacic@gmail.com>

---
+++ provider/equinix/config.go
@@ -0,0 +1,17 @@
+// Copyright 2020 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+package equinix
+
+import (
+	"github.com/juju/juju/environs/config"
+)
+
+func validateConfig(cfg, old *config.Config) (*environConfig, error) {
+	validated, err := cfg.ValidateUnknownAttrs(configFields, configDefaults)
+	if err != nil {
+		return nil, err
+	}
+	ecfg := &environConfig{cfg, validated}
+
+	return ecfg, nil
+}
---
+++ provider/equinix/credentials_test.go
@@ -0,0 +1,39 @@
+// Copyright 2021 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+package equinix
+
+import (
+	"os"
+
+	"github.com/juju/testing"
+	jc "github.com/juju/testing/checkers"
+	gc "gopkg.in/check.v1"
+)
+
+type credentialsSuite struct {
+	testing.IsolationSuite
+}
+
+var _ = gc.Suite(&credentialsSuite{})
+
+func (e credentialsSuite) TestDetectCredentials(c *gc.C) {
+	cred := environProviderCredentials{}
+	os.Setenv("METAL_AUTH_TOKEN", "tokenright")
+	os.Setenv("METAL_PROJECT_ID", "project-id")
+	_, err := cred.DetectCredentials("equinix_test")
+	c.Assert(err, jc.ErrorIsNil)
+}
+
+func (e credentialsSuite) TestDetectCredentials_NoMetalToken(c *gc.C) {
+	cred := environProviderCredentials{}
+	os.Setenv("METAL_PROJECT_ID", "project-id")
+	_, err := cred.DetectCredentials("equinix_test")
+	c.Assert(err.Error(), jc.Contains, "equinix metal auth token not present")
+}
+
+func (e credentialsSuite) TestDetectCredentials_NoProject(c *gc.C) {
+	cred := environProviderCredentials{}
+	os.Setenv("METAL_AUTH_TOKEN", "metal")
+	_, err := cred.DetectCredentials("equinix_test")
+	c.Assert(err.Error(), jc.Contains, "equinix metal project ID not present")
+}
