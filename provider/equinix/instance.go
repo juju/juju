a51603a32f9cafba478d8bb6bc4f6c3d19f463ef
root <jasmin.gacic@gmail.com>
2021-05-14 09:38:31 +0000

    Equinix device abstraction (equinixDevice type) and methods
     This commit contains the device implementation for Equinix Metal Device

--- provider/equinix/instance.go
+++ provider/equinix/instance.go
@@ -4,9 +4,12 @@
 package equinix
 
 import (
-	"github.com/juju/errors"
+	"fmt"
+
 	"github.com/juju/juju/core/instance"
+	"github.com/juju/juju/core/network"
 	corenetwork "github.com/juju/juju/core/network"
+	"github.com/juju/juju/core/status"
 	"github.com/juju/juju/environs/context"
 	"github.com/juju/juju/environs/instances"
 
@@ -30,11 +33,53 @@ func (device *equinixDevice) Id() instance.Id {
 }
 
 func (device *equinixDevice) Status(ctx context.ProviderCallContext) instance.Status {
-	panic(errors.NewNotImplemented(nil, "not implemented"))
+	var jujuStatus status.Status
+
+	switch device.State {
+	case "provisioning":
+		jujuStatus = status.Pending
+	case "active":
+		jujuStatus = status.Running
+	case "shutting-down", "terminated", "stopping", "stopped":
+		jujuStatus = status.Empty
+	default:
+		jujuStatus = status.Empty
+	}
+
+	return instance.Status{
+		Status:  jujuStatus,
+		Message: device.State,
+	}
+
 }
 
 // Addresses implements network.Addresses() returning generic address
 // details for the instance, and requerying the equinix api if required.
 func (device *equinixDevice) Addresses(ctx context.ProviderCallContext) (corenetwork.ProviderAddresses, error) {
-	panic(errors.NewNotImplemented(nil, "not implemented"))
+	var addresses []corenetwork.ProviderAddress
+
+	for _, netw := range device.Network {
+		address := corenetwork.ProviderAddress{}
+		address.Value = netw.Address
+		address.CIDR = fmt.Sprintf("%s/%d", netw.Network, netw.CIDR)
+
+		if netw.Public {
+			address.Scope = corenetwork.ScopePublic
+		} else {
+			address.Scope = corenetwork.ScopeCloudLocal
+		}
+
+		if netw.AddressFamily == 4 {
+			address.Type = network.IPv4Address
+		} else {
+			address.Type = network.IPv6Address
+			logger.Infof("skipping IPv6 Address %s", netw.Address)
+
+			continue
+		}
+
+		addresses = append(addresses, address)
+	}
+
+	return addresses, nil
 }
