7b39b7a07914727c917aa94cb240f721257018e6
Joseph Phillips <joe.phillips@canonical.com>
2021-06-24 11:43:06 +0200

    Reorganises test infrastructure for provider/lxd: - package_test.go is now in the lxd package. - mock generation for the local package moved to package_test.go. - mocks package removed. - new generated mocks added to lxd package. - all tests updated for new mock location.

--- provider/lxd/package_test.go
+++ provider/lxd/package_test.go
@@ -1,13 +1,15 @@
 // Copyright 2015 Canonical Ltd.
 // Licensed under the AGPLv3, see LICENCE file for details.
 
-package lxd_test
+package lxd
 
 import (
 	"testing"
 
 	gc "gopkg.in/check.v1"
 )
+
+//go:generate go run github.com/golang/mock/mockgen -package lxd -destination package_mock_test.go github.com/juju/juju/provider/lxd Server,ServerFactory,InterfaceAddress,CertificateReadWriter,CertificateGenerator,NetLookup,LXCConfigReader
 
 func TestPackage(t *testing.T) {
 	gc.TestingT(t)
