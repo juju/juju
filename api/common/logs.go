8ed1170a31cd3bfcfaa480a36aa3344789a59c13
Simon Richardson <stickupkid@gmail.com>
2021-06-29 17:05:25 +0100

    Logs: Ensure we close goroutine on cancel
    The following is a WIP to clean up the StreamDebugLogs goroutine if
    the callee wants to cancel the streaming of the logs.
    
    I noticed the TODO whilst threading through a new debug-log parameter.

--- api/common/logs.go
+++ api/common/logs.go
@@ -4,6 +4,7 @@
 package common
 
 import (
+	"context"
 	"fmt"
 	"net/url"
 	"time"
@@ -104,14 +105,7 @@ type LogMessage struct {
 
 // StreamDebugLog requests the specified debug log records from the
 // server and returns a channel of the messages that come back.
-func StreamDebugLog(source base.StreamConnector, args DebugLogParams) (<-chan LogMessage, error) {
-	// TODO(babbageclunk): this isn't cancellable - if the caller stops
-	// reading from the channel (because it has an error, for example),
-	// the goroutine will be leaked. This is OK when used from the command
-	// line, but is a problem if it happens in jujud. Change it to accept
-	// a stop channel and use a read deadline so that the client can stop
-	// it. https://pad.lv/1644084
-
+func StreamDebugLog(ctx context.Context, source base.StreamConnector, args DebugLogParams) (<-chan LogMessage, error) {
 	// Prepare URL query attributes.
 	attrs := args.URLQuery()
 
@@ -125,6 +119,12 @@ func StreamDebugLog(source base.StreamConnector, args DebugLogParams) (<-chan Lo
 		defer close(messages)
 
 		for {
+			// If the context is done or cancelled, then we can check to ensure
+			// that the goroutine is cleaned up.
+			if ctx.Err() != nil {
+				return
+			}
+
 			var msg params.LogMessage
 			err := connection.ReadJSON(&msg)
 			if err != nil {
