c2c7cd3c603ab19f3bfd7bcf9f46fa4b39185cdd
Achilleas Anagnostopoulos <achilleas.anagnostopoulos@canonical.com>
2021-07-23 11:11:08 +0100

    Refactor and relocate store repositories to core/charm/repositories
    This commit relocates the repository implementations out from the charms
    facade package and into core/charm/repositories. This allows us to use
    them from other facades (e.g. for the upcoming async charm downloader
    work) as well as from the client (e.g. juju download).
    
    Additionally, the repository method signatures have been refactored. In
    particular:
    - All methods accept a macaroon.Slice argument; TODOs have been added to
    the charmhub code at the point where the macaroon should be passed to
    the store client. This will make it easy to support private charmhub charms
    when support for these gets rolled out.
    - FindDownloadURL has been renamed to GetDownloadURL.
    - The charmstore repository now implements FindDownloadURL.
    - The DownloadCharm method of the charmstore implementation will always
    perform a resolution step. This allows any V1 charm.URL to be passed
    through makes the two store implementations behave in exactly the same
    manner.
    
    Finally, the test suites have been cleaned up, extra tests have been
    added to improve coverage and a new suite (original one was empty) has
    been added to test the charmstore implementation.
    
    starting

---
+++ core/charm/repository/sanitizer.go
@@ -0,0 +1,52 @@
+// Copyright 2020 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package repository
+
+import (
+	"strings"
+
+	"github.com/juju/errors"
+	"github.com/juju/os/v2/series"
+
+	corecharm "github.com/juju/juju/core/charm"
+)
+
+// sanitizeCharmOrigin attempts to ensure that any fields we receive from
+// the API are valid and juju knows about them.
+func sanitizeCharmOrigin(received, requested corecharm.Origin) (corecharm.Origin, error) {
+	// Platform is generally the problem at hand. We want to ensure if they
+	// send "all" back for Architecture, OS or Release that we either use the
+	// requested origin using that as the hint or we unset it from the requested
+	// origin.
+	result := received
+
+	if result.Platform.Architecture == "all" {
+		result.Platform.Architecture = ""
+		if requested.Platform.Architecture != "all" {
+			result.Platform.Architecture = requested.Platform.Architecture
+		}
+	}
+
+	if result.Platform.OS == "all" {
+		result.Platform.OS = ""
+	}
+
+	if result.Platform.Series == "all" {
+		result.Platform.Series = ""
+
+		if requested.Platform.Series != "all" {
+			result.Platform.Series = requested.Platform.Series
+		}
+	}
+
+	if result.Platform.Series != "" {
+		os, err := series.GetOSFromSeries(result.Platform.Series)
+		if err != nil {
+			return result, errors.Trace(err)
+		}
+		result.Platform.OS = strings.ToLower(os.String())
+	}
+
+	return result, nil
+}
