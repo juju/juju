0f78da956dcda7fc73da666d3e5ffdcbf9f8c4bd
Simon Richardson <stickupkid@gmail.com>
2021-08-11 16:15:26 +0100

    Add worker tests
    These are unit tests covering the processing of the command, they're not
    full integration tests. Those will come later.

--- worker/raft/raftleaseconsumer/package_test.go
+++ worker/raft/raftleaseconsumer/package_test.go
@@ -13,11 +13,12 @@ import (
 //go:generate go run github.com/golang/mock/mockgen -package raftleaseconsumer -destination worker_mock_test.go github.com/juju/worker/v2 Worker
 //go:generate go run github.com/golang/mock/mockgen -package raftleaseconsumer -destination auth_mock_test.go github.com/juju/juju/apiserver/httpcontext Authenticator
 //go:generate go run github.com/golang/mock/mockgen -package raftleaseconsumer -destination raft_mock_test.go github.com/juju/juju/worker/raft/raftleaseconsumer RaftApplier,State,Logger
-//go:generate go run github.com/golang/mock/mockgen -package raftleaseconsumer -destination target_mock_test.go github.com/juju/juju/core/raftlease NotifyTarget
+//go:generate go run github.com/golang/mock/mockgen -package raftleaseconsumer -destination target_mock_test.go github.com/juju/juju/core/raftlease NotifyTarget,FSMResponse
 //go:generate go run github.com/golang/mock/mockgen -package raftleaseconsumer -destination writer_mock_test.go io Writer
 //go:generate go run github.com/golang/mock/mockgen -package raftleaseconsumer -destination clock_mock_test.go github.com/juju/clock Clock
 //go:generate go run github.com/golang/mock/mockgen -package raftleaseconsumer -destination prometheus_mock_test.go github.com/prometheus/client_golang/prometheus Registerer
 //go:generate go run github.com/golang/mock/mockgen -package raftleaseconsumer -destination state_mock_test.go github.com/juju/juju/worker/state StateTracker
+//go:generate go run github.com/golang/mock/mockgen -package raftleaseconsumer -destination raft_future_mock_test.go github.com/hashicorp/raft ApplyFuture
 
 func TestPackage(t *testing.T) {
 	gc.TestingT(t)
---
+++ worker/raft/raftleaseconsumer/worker_test.go
@@ -0,0 +1,127 @@
+// Copyright 2021 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+package raftleaseconsumer
+
+import (
+	"time"
+
+	gc "gopkg.in/check.v1"
+
+	gomock "github.com/golang/mock/gomock"
+	"github.com/juju/errors"
+	"github.com/juju/juju/apiserver/apiserverhttp"
+	coretesting "github.com/juju/juju/testing"
+	"github.com/juju/testing"
+	jc "github.com/juju/testing/checkers"
+	"github.com/juju/worker/v2/workertest"
+)
+
+type WorkerSuite struct {
+	testing.IsolationSuite
+
+	worker *Worker
+
+	auth        *MockAuthenticator
+	target      *MockNotifyTarget
+	raft        *MockRaftApplier
+	applyFuture *MockApplyFuture
+	fsmResponse *MockFSMResponse
+	logger      *MockLogger
+	clock       *MockClock
+	registerer  *MockRegisterer
+	mux         *apiserverhttp.Mux
+}
+
+var _ = gc.Suite(&WorkerSuite{})
+
+func (s *WorkerSuite) TestWorkerNotify(c *gc.C) {
+	defer s.setupMocks(c).Finish()
+
+	s.fsmResponse.EXPECT().Notify(s.target)
+	s.fsmResponse.EXPECT().Error().Return(nil)
+
+	s.applyFuture.EXPECT().Error().Return(nil)
+	s.applyFuture.EXPECT().Response().Return(s.fsmResponse)
+
+	s.raft.EXPECT().Apply([]byte("claim"), applyTimeout).Return(s.applyFuture)
+
+	done := make(chan struct{})
+	s.worker.operations <- operation{
+		Command: "claim",
+		Callback: func(err error) {
+			c.Assert(err, jc.ErrorIsNil)
+			done <- struct{}{}
+		},
+	}
+
+	select {
+	case <-done:
+	case <-time.After(coretesting.LongWait):
+		c.Fatalf("no first message")
+	}
+
+	// Stopping the worker should cause the state to
+	// eventually be released.
+	workertest.CleanKill(c, s.worker)
+}
+
+func (s *WorkerSuite) TestWorkerNotifyError(c *gc.C) {
+	defer s.setupMocks(c).Finish()
+
+	s.applyFuture.EXPECT().Error().Return(errors.New("boom"))
+	s.raft.EXPECT().Apply([]byte("claim"), applyTimeout).Return(s.applyFuture)
+
+	done := make(chan struct{})
+	s.worker.operations <- operation{
+		Command: "claim",
+		Callback: func(err error) {
+			c.Assert(err, gc.ErrorMatches, `boom`)
+			done <- struct{}{}
+		},
+	}
+
+	select {
+	case <-done:
+	case <-time.After(coretesting.LongWait):
+		c.Fatalf("no first message")
+	}
+
+	// Stopping the worker should cause the state to
+	// eventually be released.
+	workertest.CleanKill(c, s.worker)
+}
+
+func (s *WorkerSuite) setupMocks(c *gc.C) *gomock.Controller {
+	ctrl := gomock.NewController(c)
+
+	s.auth = NewMockAuthenticator(ctrl)
+	s.target = NewMockNotifyTarget(ctrl)
+	s.logger = NewMockLogger(ctrl)
+	s.clock = NewMockClock(ctrl)
+	s.registerer = NewMockRegisterer(ctrl)
+	s.raft = NewMockRaftApplier(ctrl)
+	s.applyFuture = NewMockApplyFuture(ctrl)
+	s.fsmResponse = NewMockFSMResponse(ctrl)
+
+	s.mux = apiserverhttp.NewMux()
+
+	s.clock.EXPECT().Now().Return(time.Now()).AnyTimes()
+	s.registerer.EXPECT().Register(gomock.Any())
+	s.registerer.EXPECT().Unregister(gomock.Any())
+
+	worker, err := NewWorker(Config{
+		Authenticator:        s.auth,
+		Mux:                  s.mux,
+		Path:                 "",
+		Raft:                 s.raft,
+		Target:               s.target,
+		PrometheusRegisterer: s.registerer,
+		Clock:                s.clock,
+		Logger:               s.logger,
+	})
+	c.Assert(err, jc.ErrorIsNil)
+
+	s.worker = worker.(*Worker)
+
+	return ctrl
+}
