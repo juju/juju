4071d3d03f6e12e6276f9712a284a4fb110ef543
Simon Richardson <stickupkid@gmail.com>
2021-09-15 15:43:24 +0100

    Use a test clock for queue tests
    To better test against wall clocks and to get isolated environments, the
    following adds a test clock for tests.

--- worker/raft/queue/blockingqueue_test.go
+++ worker/raft/queue/blockingqueue_test.go
@@ -9,7 +9,7 @@ import (
 	"sync"
 	"time"
 
-	"github.com/juju/clock"
+	"github.com/juju/clock/testclock"
 	"github.com/juju/testing"
 	jc "github.com/juju/testing/checkers"
 	gc "gopkg.in/check.v1"
@@ -22,158 +22,95 @@ type BlockingOpQueueSuite struct {
 var _ = gc.Suite(&BlockingOpQueueSuite{})
 
 func (s *BlockingOpQueueSuite) TestEnqueue(c *gc.C) {
-	queue := NewBlockingOpQueue(clock.WallClock)
+	now := time.Now()
+	queue := NewBlockingOpQueue(testclock.NewClock(now))
 
-	go func() {
-		for op := range queue.Queue() {
-			c.Assert(op.Command, gc.DeepEquals, []byte("abc"))
-			queue.Error() <- nil
-
-			break
-		}
-	}()
+	results := consumeN(c, queue, 1)
 
 	err := queue.Enqueue(Operation{
-		Command:  []byte("abc"),
-		Deadline: clock.WallClock.Now().Add(time.Second),
+		Command:  opName(0),
+		Deadline: now.Add(time.Second),
 	})
 	c.Assert(err, jc.ErrorIsNil)
+
+	var count int
+	for result := range results {
+		c.Assert(result, gc.DeepEquals, opName(count))
+		count++
+	}
+	c.Assert(count, gc.Equals, 1)
 }
 
 func (s *BlockingOpQueueSuite) TestEnqueueWithError(c *gc.C) {
-	queue := NewBlockingOpQueue(clock.WallClock)
+	now := time.Now()
+	queue := NewBlockingOpQueue(testclock.NewClock(now))
 
-	var (
-		mutex   sync.Mutex
-		results []string
-	)
-
-	go func() {
-		for op := range queue.Queue() {
-			mutex.Lock()
-			results = append(results, string(op.Command))
-			mutex.Unlock()
-
-			queue.Error() <- errors.New("boom")
-			break
-		}
-	}()
+	results := consumeNUntilErr(c, queue, 1, errors.New("boom"))
 
 	err := queue.Enqueue(Operation{
-		Command:  []byte("abc"),
-		Deadline: clock.WallClock.Now().Add(time.Second),
+		Command:  opName(0),
+		Deadline: now.Add(time.Second),
 	})
 	c.Assert(err, gc.ErrorMatches, `boom`)
 
-	mutex.Lock()
-	defer mutex.Unlock()
-	c.Assert(results, jc.DeepEquals, []string{"abc"})
+	var count int
+	for result := range results {
+		c.Assert(result, gc.DeepEquals, opName(count))
+		count++
+	}
+	c.Assert(count, gc.Equals, 1)
 }
 
 func (s *BlockingOpQueueSuite) TestEnqueueTimesout(c *gc.C) {
-	queue := NewBlockingOpQueue(clock.WallClock)
-
-	var (
-		mutex   sync.Mutex
-		results []string
-	)
+	now := time.Now()
+	clock := testclock.NewClock(now)
+	queue := NewBlockingOpQueue(clock)
 
 	go func() {
-		var count int
-		for op := range queue.Queue() {
-			mutex.Lock()
-			results = append(results, string(op.Command))
-			mutex.Unlock()
-
-			queue.Error() <- nil
-
-			count++
-			switch count {
-			case 1:
-				time.Sleep(time.Millisecond * 500)
-			case 2:
-				return
-			}
-		}
+		c.Assert(clock.WaitAdvance(time.Millisecond, testing.ShortWait, 1), jc.ErrorIsNil)
 	}()
 
 	err := queue.Enqueue(Operation{
 		Command:  []byte("abc-1"),
-		Deadline: clock.WallClock.Now().Add(time.Second),
-	})
-	c.Assert(err, jc.ErrorIsNil)
-
-	err = queue.Enqueue(Operation{
-		Command:  []byte("abc-2"),
-		Deadline: clock.WallClock.Now().Add(time.Millisecond),
+		Deadline: now.Add(time.Nanosecond),
 	})
 	c.Assert(err, gc.ErrorMatches, `deadline exceeded`)
-
-	mutex.Lock()
-	defer mutex.Unlock()
-	c.Assert(results, jc.DeepEquals, []string{"abc-1"})
 }
 
 func (s *BlockingOpQueueSuite) TestMultipleEnqueue(c *gc.C) {
-	queue := NewBlockingOpQueue(clock.WallClock)
+	now := time.Now()
+	queue := NewBlockingOpQueue(testclock.NewClock(now))
 
-	var (
-		mutex   sync.Mutex
-		results []string
-	)
-
-	cmd := func(i int) []byte {
-		return []byte(fmt.Sprintf("abc-%d", i))
-	}
-
-	go func() {
-		var count int
-		for op := range queue.Queue() {
-			mutex.Lock()
-			results = append(results, string(op.Command))
-			mutex.Unlock()
-			queue.Error() <- nil
-
-			count++
-
-			if count == 2 {
-				break
-			}
-		}
-	}()
+	results := consumeN(c, queue, 2)
 
 	for i := 0; i < 2; i++ {
 		err := queue.Enqueue(Operation{
-			Command:  cmd(i),
-			Deadline: clock.WallClock.Now().Add(time.Second),
+			Command:  opName(i),
+			Deadline: now.Add(time.Second),
 		})
 		c.Assert(err, jc.ErrorIsNil)
 	}
 
-	mutex.Lock()
-	defer mutex.Unlock()
-	c.Assert(results, jc.DeepEquals, []string{"abc-0", "abc-1"})
+	var count int
+	for result := range results {
+		c.Assert(result, gc.DeepEquals, opName(count))
+		count++
+	}
+	c.Assert(count, gc.Equals, 2)
 }
 
 func (s *BlockingOpQueueSuite) TestMultipleEnqueueWithErrors(c *gc.C) {
-	queue := NewBlockingOpQueue(clock.WallClock)
+	now := time.Now()
+	clock := testclock.NewClock(now)
+	queue := NewBlockingOpQueue(clock)
 
-	var (
-		mutex   sync.Mutex
-		results []string
-	)
-
-	cmd := func(i int) []byte {
-		return []byte(fmt.Sprintf("abc-%d", i))
-	}
-
+	results := make(chan []byte, 3)
 	go func() {
+		defer close(results)
+
 		var count int
 		for op := range queue.Queue() {
-			mutex.Lock()
-			results = append(results, string(op.Command))
-			mutex.Unlock()
-
+			results <- op.Command
 			queue.Error() <- nil
 
 			count++
@@ -188,55 +125,50 @@ func (s *BlockingOpQueueSuite) TestMultipleEnqueueWithErrors(c *gc.C) {
 	}()
 
 	err := queue.Enqueue(Operation{
-		Command:  cmd(0),
-		Deadline: clock.WallClock.Now().Add(time.Second),
+		Command:  opName(0),
+		Deadline: now.Add(time.Nanosecond),
 	})
 	c.Assert(err, jc.ErrorIsNil)
 
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		c.Assert(clock.WaitAdvance(time.Millisecond, testing.ShortWait, 2), jc.ErrorIsNil)
+	}()
+
 	// Fail this one
 	err = queue.Enqueue(Operation{
-		Command:  cmd(1),
-		Deadline: clock.WallClock.Now().Add(time.Millisecond),
+		Command:  opName(1),
+		Deadline: now.Add(time.Nanosecond),
 	})
 	c.Assert(err, gc.ErrorMatches, `deadline exceeded`)
 
 	err = queue.Enqueue(Operation{
-		Command:  cmd(2),
-		Deadline: clock.WallClock.Now().Add(time.Second),
+		Command:  opName(2),
+		Deadline: now.Add(time.Millisecond * 100),
 	})
 	c.Assert(err, jc.ErrorIsNil)
 
-	mutex.Lock()
-	defer mutex.Unlock()
-	c.Assert(results, jc.DeepEquals, []string{"abc-0", "abc-2"})
+	var received []string
+	for result := range results {
+		received = append(received, string(result))
+	}
+	c.Assert(len(received), gc.Equals, 2)
+	c.Assert(received, gc.DeepEquals, []string{
+		"abc-0", "abc-2",
+	})
+
+	// Ensure that we actually did advance correctly.
+	wg.Wait()
 }
 
 func (s *BlockingOpQueueSuite) TestMultipleEnqueues(c *gc.C) {
-	queue := NewBlockingOpQueue(clock.WallClock)
+	now := time.Now()
+	queue := NewBlockingOpQueue(testclock.NewClock(now))
 
-	var (
-		mutex   sync.Mutex
-		results []string
-	)
-
-	cmd := func(i int) []byte {
-		return []byte(fmt.Sprintf("abc-%d", i))
-	}
-
-	go func() {
-		for op := range queue.Queue() {
-			mutex.Lock()
-			results = append(results, string(op.Command))
-			num := len(results)
-			mutex.Unlock()
-
-			queue.Error() <- nil
-
-			if num > 10 {
-				break
-			}
-		}
-	}()
+	results := consumeN(c, queue, 10)
 
 	var wg sync.WaitGroup
 	wg.Add(10)
@@ -245,16 +177,63 @@ func (s *BlockingOpQueueSuite) TestMultipleEnqueues(c *gc.C) {
 			defer wg.Done()
 
 			err := queue.Enqueue(Operation{
-				Command:  cmd(i),
-				Deadline: clock.WallClock.Now().Add(time.Second),
+				Command:  opName(i),
+				Deadline: now.Add(time.Second),
 			})
 			c.Assert(err, jc.ErrorIsNil)
 		}(i)
 	}
 	wg.Wait()
 
-	c.Assert(results, jc.SameContents, []string{
+	var received []string
+	for result := range results {
+		received = append(received, string(result))
+	}
+	c.Assert(len(received), gc.Equals, 10)
+	c.Assert(received, jc.SameContents, []string{
 		"abc-0", "abc-1", "abc-2", "abc-3", "abc-4",
 		"abc-5", "abc-6", "abc-7", "abc-8", "abc-9",
 	})
+}
+
+func opName(i int) []byte {
+	return []byte(fmt.Sprintf("abc-%d", i))
+}
+
+func consumeN(c *gc.C, queue *BlockingOpQueue, n int) <-chan []byte {
+	return consumeNUntilErr(c, queue, n, nil)
+}
+
+func consumeNUntilErr(c *gc.C, queue *BlockingOpQueue, n int, err error) <-chan []byte {
+	results := make(chan []byte, n)
+
+	go func() {
+		defer close(results)
+
+		var count int
+		for op := range queue.Queue() {
+			select {
+			case results <- op.Command:
+			case <-time.After(testing.LongWait):
+				c.Fatal("timed out setting results")
+			}
+
+			count++
+			var e error
+			if count == n {
+				e = err
+			}
+			select {
+			case queue.Error() <- e:
+			case <-time.After(testing.LongWait):
+				c.Fatal("timed out setting error")
+			}
+
+			if count == n {
+				break
+			}
+		}
+	}()
+
+	return results
 }
