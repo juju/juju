6b60b7190c00cf9e7d5c15704aaf76a8184da23f
Joseph Phillips <joe.phillips@canonical.com>
2021-09-27 13:56:30 +0200

    Adds generated mock for local Clock indirection.

--- worker/upgradedatabase/package_test.go
+++ worker/upgradedatabase/package_test.go
@@ -9,7 +9,7 @@ import (
 	gc "gopkg.in/check.v1"
 )
 
-//go:generate go run github.com/golang/mock/mockgen -package mocks -destination mocks/package.go github.com/juju/juju/worker/upgradedatabase Logger,Pool,UpgradeInfo
+//go:generate go run github.com/golang/mock/mockgen -package mocks -destination mocks/package.go github.com/juju/juju/worker/upgradedatabase Logger,Pool,UpgradeInfo,Clock
 //go:generate go run github.com/golang/mock/mockgen -package mocks -destination mocks/lock.go github.com/juju/juju/worker/gate Lock
 //go:generate go run github.com/golang/mock/mockgen -package mocks -destination mocks/agent.go github.com/juju/juju/agent Agent,Config,ConfigSetter
 //go:generate go run github.com/golang/mock/mockgen -package mocks -destination mocks/watcher.go github.com/juju/juju/state NotifyWatcher
