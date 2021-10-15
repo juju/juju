4e1cefd18dbfad39ea31c8dbd19d9c4ec3ab7bd9
Achilleas Anagnostopoulos <achilleas.anagnostopoulos@canonical.com>
2021-10-15 15:55:26 +0100

    Populate SupportedFeatures section of the ModelInfo call response
    This codepath is currently behind a controller feature flag.

--- apiserver/facades/client/modelmanager/export_test.go
+++ apiserver/facades/client/modelmanager/export_test.go
@@ -6,9 +6,23 @@ package modelmanager
 import (
 	"github.com/juju/names/v4"
 	gc "gopkg.in/check.v1"
+
+	"github.com/juju/juju/core/assumes"
+	"github.com/juju/juju/environs"
+	"github.com/juju/juju/state/stateenvirons"
 )
 
 func AuthCheck(c *gc.C, mm *ModelManagerAPI, user names.UserTag) bool {
 	mm.authCheck(user)
 	return mm.isAdmin
+}
+
+func MockSupportedFeatures(fs assumes.FeatureSet) {
+	supportedFeaturesGetter = func(stateenvirons.Model, environs.NewEnvironFunc) (assumes.FeatureSet, error) {
+		return fs, nil
+	}
+}
+
+func ResetSupportedFeaturesGetter() {
+	supportedFeaturesGetter = stateenvirons.SupportedFeatures
 }
