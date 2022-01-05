934d0815c69a62c3534fa61372b7b710b7a2ed03
Ian Booth <ian.booth@canonical.com>
2022-01-05 15:34:03 +1000

    Add explicit owner to params when creating an offer

--- cmd/juju/application/deployer/mocks/deployer_mock.go
+++ cmd/juju/application/deployer/mocks/deployer_mock.go
@@ -5,39 +5,40 @@
 package mocks
 
 import (
+	reflect "reflect"
+
 	httpbakery "github.com/go-macaroon-bakery/macaroon-bakery/v3/httpbakery"
 	gomock "github.com/golang/mock/gomock"
 	params "github.com/juju/juju/apiserver/params"
 	modelcmd "github.com/juju/juju/cmd/modelcmd"
 	model "github.com/juju/juju/core/model"
 	jujuclient "github.com/juju/juju/jujuclient"
-	reflect "reflect"
 )
 
-// MockModelCommand is a mock of ModelCommand interface
+// MockModelCommand is a mock of ModelCommand interface.
 type MockModelCommand struct {
 	ctrl     *gomock.Controller
 	recorder *MockModelCommandMockRecorder
 }
 
-// MockModelCommandMockRecorder is the mock recorder for MockModelCommand
+// MockModelCommandMockRecorder is the mock recorder for MockModelCommand.
 type MockModelCommandMockRecorder struct {
 	mock *MockModelCommand
 }
 
-// NewMockModelCommand creates a new mock instance
+// NewMockModelCommand creates a new mock instance.
 func NewMockModelCommand(ctrl *gomock.Controller) *MockModelCommand {
 	mock := &MockModelCommand{ctrl: ctrl}
 	mock.recorder = &MockModelCommandMockRecorder{mock}
 	return mock
 }
 
-// EXPECT returns an object that allows the caller to indicate expected use
+// EXPECT returns an object that allows the caller to indicate expected use.
 func (m *MockModelCommand) EXPECT() *MockModelCommandMockRecorder {
 	return m.recorder
 }
 
-// BakeryClient mocks base method
+// BakeryClient mocks base method.
 func (m *MockModelCommand) BakeryClient() (*httpbakery.Client, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "BakeryClient")
@@ -46,13 +47,13 @@ func (m *MockModelCommand) BakeryClient() (*httpbakery.Client, error) {
 	return ret0, ret1
 }
 
-// BakeryClient indicates an expected call of BakeryClient
+// BakeryClient indicates an expected call of BakeryClient.
 func (mr *MockModelCommandMockRecorder) BakeryClient() *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "BakeryClient", reflect.TypeOf((*MockModelCommand)(nil).BakeryClient))
 }
 
-// ControllerName mocks base method
+// ControllerName mocks base method.
 func (m *MockModelCommand) ControllerName() (string, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "ControllerName")
@@ -61,13 +62,13 @@ func (m *MockModelCommand) ControllerName() (string, error) {
 	return ret0, ret1
 }
 
-// ControllerName indicates an expected call of ControllerName
+// ControllerName indicates an expected call of ControllerName.
 func (mr *MockModelCommandMockRecorder) ControllerName() *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ControllerName", reflect.TypeOf((*MockModelCommand)(nil).ControllerName))
 }
 
-// CurrentAccountDetails mocks base method
+// CurrentAccountDetails mocks base method.
 func (m *MockModelCommand) CurrentAccountDetails() (*jujuclient.AccountDetails, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "CurrentAccountDetails")
@@ -76,13 +77,13 @@ func (m *MockModelCommand) CurrentAccountDetails() (*jujuclient.AccountDetails, 
 	return ret0, ret1
 }
 
-// CurrentAccountDetails indicates an expected call of CurrentAccountDetails
+// CurrentAccountDetails indicates an expected call of CurrentAccountDetails.
 func (mr *MockModelCommandMockRecorder) CurrentAccountDetails() *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "CurrentAccountDetails", reflect.TypeOf((*MockModelCommand)(nil).CurrentAccountDetails))
 }
 
-// Filesystem mocks base method
+// Filesystem mocks base method.
 func (m *MockModelCommand) Filesystem() modelcmd.Filesystem {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "Filesystem")
@@ -90,13 +91,13 @@ func (m *MockModelCommand) Filesystem() modelcmd.Filesystem {
 	return ret0
 }
 
-// Filesystem indicates an expected call of Filesystem
+// Filesystem indicates an expected call of Filesystem.
 func (mr *MockModelCommandMockRecorder) Filesystem() *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Filesystem", reflect.TypeOf((*MockModelCommand)(nil).Filesystem))
 }
 
-// ModelDetails mocks base method
+// ModelDetails mocks base method.
 func (m *MockModelCommand) ModelDetails() (string, *jujuclient.ModelDetails, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "ModelDetails")
@@ -106,13 +107,13 @@ func (m *MockModelCommand) ModelDetails() (string, *jujuclient.ModelDetails, err
 	return ret0, ret1, ret2
 }
 
-// ModelDetails indicates an expected call of ModelDetails
+// ModelDetails indicates an expected call of ModelDetails.
 func (mr *MockModelCommandMockRecorder) ModelDetails() *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ModelDetails", reflect.TypeOf((*MockModelCommand)(nil).ModelDetails))
 }
 
-// ModelType mocks base method
+// ModelType mocks base method.
 func (m *MockModelCommand) ModelType() (model.ModelType, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "ModelType")
@@ -121,36 +122,36 @@ func (m *MockModelCommand) ModelType() (model.ModelType, error) {
 	return ret0, ret1
 }
 
-// ModelType indicates an expected call of ModelType
+// ModelType indicates an expected call of ModelType.
 func (mr *MockModelCommandMockRecorder) ModelType() *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ModelType", reflect.TypeOf((*MockModelCommand)(nil).ModelType))
 }
 
-// MockConsumeDetails is a mock of ConsumeDetails interface
+// MockConsumeDetails is a mock of ConsumeDetails interface.
 type MockConsumeDetails struct {
 	ctrl     *gomock.Controller
 	recorder *MockConsumeDetailsMockRecorder
 }
 
-// MockConsumeDetailsMockRecorder is the mock recorder for MockConsumeDetails
+// MockConsumeDetailsMockRecorder is the mock recorder for MockConsumeDetails.
 type MockConsumeDetailsMockRecorder struct {
 	mock *MockConsumeDetails
 }
 
-// NewMockConsumeDetails creates a new mock instance
+// NewMockConsumeDetails creates a new mock instance.
 func NewMockConsumeDetails(ctrl *gomock.Controller) *MockConsumeDetails {
 	mock := &MockConsumeDetails{ctrl: ctrl}
 	mock.recorder = &MockConsumeDetailsMockRecorder{mock}
 	return mock
 }
 
-// EXPECT returns an object that allows the caller to indicate expected use
+// EXPECT returns an object that allows the caller to indicate expected use.
 func (m *MockConsumeDetails) EXPECT() *MockConsumeDetailsMockRecorder {
 	return m.recorder
 }
 
-// Close mocks base method
+// Close mocks base method.
 func (m *MockConsumeDetails) Close() error {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "Close")
@@ -158,13 +159,13 @@ func (m *MockConsumeDetails) Close() error {
 	return ret0
 }
 
-// Close indicates an expected call of Close
+// Close indicates an expected call of Close.
 func (mr *MockConsumeDetailsMockRecorder) Close() *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Close", reflect.TypeOf((*MockConsumeDetails)(nil).Close))
 }
 
-// GetConsumeDetails mocks base method
+// GetConsumeDetails mocks base method.
 func (m *MockConsumeDetails) GetConsumeDetails(arg0 string) (params.ConsumeOfferDetails, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "GetConsumeDetails", arg0)
@@ -173,36 +174,36 @@ func (m *MockConsumeDetails) GetConsumeDetails(arg0 string) (params.ConsumeOffer
 	return ret0, ret1
 }
 
-// GetConsumeDetails indicates an expected call of GetConsumeDetails
+// GetConsumeDetails indicates an expected call of GetConsumeDetails.
 func (mr *MockConsumeDetailsMockRecorder) GetConsumeDetails(arg0 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetConsumeDetails", reflect.TypeOf((*MockConsumeDetails)(nil).GetConsumeDetails), arg0)
 }
 
-// MockModelConfigGetter is a mock of ModelConfigGetter interface
+// MockModelConfigGetter is a mock of ModelConfigGetter interface.
 type MockModelConfigGetter struct {
 	ctrl     *gomock.Controller
 	recorder *MockModelConfigGetterMockRecorder
 }
 
-// MockModelConfigGetterMockRecorder is the mock recorder for MockModelConfigGetter
+// MockModelConfigGetterMockRecorder is the mock recorder for MockModelConfigGetter.
 type MockModelConfigGetterMockRecorder struct {
 	mock *MockModelConfigGetter
 }
 
-// NewMockModelConfigGetter creates a new mock instance
+// NewMockModelConfigGetter creates a new mock instance.
 func NewMockModelConfigGetter(ctrl *gomock.Controller) *MockModelConfigGetter {
 	mock := &MockModelConfigGetter{ctrl: ctrl}
 	mock.recorder = &MockModelConfigGetterMockRecorder{mock}
 	return mock
 }
 
-// EXPECT returns an object that allows the caller to indicate expected use
+// EXPECT returns an object that allows the caller to indicate expected use.
 func (m *MockModelConfigGetter) EXPECT() *MockModelConfigGetterMockRecorder {
 	return m.recorder
 }
 
-// ModelGet mocks base method
+// ModelGet mocks base method.
 func (m *MockModelConfigGetter) ModelGet() (map[string]interface{}, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "ModelGet")
@@ -211,7 +212,7 @@ func (m *MockModelConfigGetter) ModelGet() (map[string]interface{}, error) {
 	return ret0, ret1
 }
 
-// ModelGet indicates an expected call of ModelGet
+// ModelGet indicates an expected call of ModelGet.
 func (mr *MockModelConfigGetterMockRecorder) ModelGet() *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ModelGet", reflect.TypeOf((*MockModelConfigGetter)(nil).ModelGet))
