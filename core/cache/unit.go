d29b8e460b5414cd22808283795f81ebbd0abf51
Simon Richardson <stickupkid@gmail.com>
2022-01-18 14:22:33 +0000

    Engine Report: Model Cache
    The following improves the reporting for the model cache. Currently,
    it's not possible to know what the current state of the model cache is
    for a given controller.
    
    In an ideal world we would have an API to dump the model cache, except
    that falls down for HA environments. The correct way would be to query
    every controller in the fleet to report back their current information.
    That's a bit of work and something that can be done in the future, but
    for now, we can piggy-back off the juju_engine_report to dump out more
    information.
    
    We don't dump all the information, just enough to help diagnose
    potential problems with in the eventual consistent cache.

--- core/cache/unit.go
+++ core/cache/unit.go
@@ -34,6 +34,19 @@ func newUnit(model *Model, res *Resident) *Unit {
 	}
 }
 
+// Report returns information that is used in the dependency engine report.
+func (u *Unit) Report() map[string]interface{} {
+	details := u.details
+	return map[string]interface{}{
+		"name":            details.Name,
+		"series":          details.Series,
+		"charm-url":       details.CharmURL,
+		"public-address":  details.PublicAddress,
+		"private-address": details.PrivateAddress,
+		"subordinate":     details.Subordinate,
+	}
+}
+
 // Note that these property accessors are not lock-protected.
 // They are intended for calling from external packages that have retrieved a
 // deep copy from the cache.
