393e66c8f68106b34096254ad2e61329c18919e8
Kelvin Liu <kelvin.liu@canonical.com>
2022-01-20 14:50:34 +1100

    Add tests for the action delta change;

--- apiserver/watcher_internal_test.go
+++ apiserver/watcher_internal_test.go
@@ -4,14 +4,16 @@
 package apiserver
 
 import (
+	"github.com/golang/mock/gomock"
+	jc "github.com/juju/testing/checkers"
 	gc "gopkg.in/check.v1"
 
+	"github.com/juju/juju/apiserver/mocks"
 	"github.com/juju/juju/apiserver/params"
 	"github.com/juju/juju/core/life"
 	"github.com/juju/juju/core/multiwatcher"
 	"github.com/juju/juju/core/status"
 	"github.com/juju/juju/testing"
-	jc "github.com/juju/testing/checkers"
 )
 
 type allWatcherSuite struct {
@@ -20,14 +22,8 @@ type allWatcherSuite struct {
 
 var _ = gc.Suite(&allWatcherSuite{})
 
-func (s *allWatcherSuite) watcher() *SrvAllWatcher {
-	// We explicitly don't have a real watcher here as the tests
-	// are for the translation of types.
-	return &SrvAllWatcher{}
-}
-
 func (s *allWatcherSuite) TestTranslateApplicationWithStatus(c *gc.C) {
-	w := s.watcher()
+	t := newAllWatcherDeltaTranslater()
 	input := &multiwatcher.ApplicationInfo{
 		ModelUUID: testing.ModelTag.Id(),
 		Name:      "test-app",
@@ -37,7 +33,7 @@ func (s *allWatcherSuite) TestTranslateApplicationWithStatus(c *gc.C) {
 			Current: status.Active,
 		},
 	}
-	output := w.translateApplication(input)
+	output := t.TranslateApplication(input)
 	c.Assert(output, jc.DeepEquals, &params.ApplicationInfo{
 		ModelUUID: input.ModelUUID,
 		Name:      input.Name,
@@ -47,4 +43,92 @@ func (s *allWatcherSuite) TestTranslateApplicationWithStatus(c *gc.C) {
 			Current: status.Active,
 		},
 	})
+}
+
+func (s *allWatcherSuite) TestTranslateActionV1(c *gc.C) {
+	t := newAllWatcherDeltaTranslaterV1()
+	input := &multiwatcher.ActionInfo{
+		ModelUUID:  testing.ModelTag.Id(),
+		ID:         "1",
+		Parameters: map[string]interface{}{"foo": "bar"},
+		Results:    map[string]interface{}{"done": true},
+	}
+	output := t.TranslateAction(input)
+	c.Assert(output, jc.DeepEquals, &params.ActionInfo{
+		ModelUUID:  input.ModelUUID,
+		Id:         input.ID,
+		Receiver:   input.Receiver,
+		Name:       input.Name,
+		Status:     input.Status,
+		Message:    input.Message,
+		Enqueued:   input.Enqueued,
+		Started:    input.Started,
+		Completed:  input.Completed,
+		Parameters: input.Parameters,
+		Results:    input.Results,
+	})
+}
+
+func (s *allWatcherSuite) TestTranslateActionV2(c *gc.C) {
+	t := newAllWatcherDeltaTranslater()
+	input := &multiwatcher.ActionInfo{
+		ModelUUID:  testing.ModelTag.Id(),
+		ID:         "2",
+		Parameters: map[string]interface{}{"foo": "bar"},
+		Results:    map[string]interface{}{"done": true},
+	}
+	output := t.TranslateAction(input)
+	c.Assert(output, jc.DeepEquals, &params.ActionInfo{
+		ModelUUID: input.ModelUUID,
+		Id:        input.ID,
+		Receiver:  input.Receiver,
+		Name:      input.Name,
+		Status:    input.Status,
+		Message:   input.Message,
+		Enqueued:  input.Enqueued,
+		Started:   input.Started,
+		Completed: input.Completed,
+	})
+}
+
+func newDelta(info multiwatcher.EntityInfo) multiwatcher.Delta {
+	return multiwatcher.Delta{Entity: info}
+}
+
+func (s *allWatcherSuite) TestTranslate(c *gc.C) {
+	ctrl := gomock.NewController(c)
+	defer ctrl.Finish()
+
+	dt := mocks.NewMockDeltaTranslater(ctrl)
+
+	gomock.InOrder(
+		dt.EXPECT().TranslateModel(gomock.Any()).Return(nil),
+		dt.EXPECT().TranslateApplication(gomock.Any()).Return(nil),
+		dt.EXPECT().TranslateRemoteApplication(gomock.Any()).Return(nil),
+		dt.EXPECT().TranslateMachine(gomock.Any()).Return(nil),
+		dt.EXPECT().TranslateUnit(gomock.Any()).Return(nil),
+		dt.EXPECT().TranslateCharm(gomock.Any()).Return(nil),
+		dt.EXPECT().TranslateRelation(gomock.Any()).Return(nil),
+		dt.EXPECT().TranslateBranch(gomock.Any()).Return(nil),
+		dt.EXPECT().TranslateAnnotation(gomock.Any()).Return(nil),
+		dt.EXPECT().TranslateBlock(gomock.Any()).Return(nil),
+		dt.EXPECT().TranslateAction(gomock.Any()).Return(nil),
+		dt.EXPECT().TranslateApplicationOffer(gomock.Any()).Return(nil),
+	)
+
+	deltas := []multiwatcher.Delta{
+		newDelta(&multiwatcher.ModelInfo{}),
+		newDelta(&multiwatcher.ApplicationInfo{}),
+		newDelta(&multiwatcher.RemoteApplicationUpdate{}),
+		newDelta(&multiwatcher.MachineInfo{}),
+		newDelta(&multiwatcher.UnitInfo{}),
+		newDelta(&multiwatcher.CharmInfo{}),
+		newDelta(&multiwatcher.RelationInfo{}),
+		newDelta(&multiwatcher.BranchInfo{}),
+		newDelta(&multiwatcher.AnnotationInfo{}),
+		newDelta(&multiwatcher.BlockInfo{}),
+		newDelta(&multiwatcher.ActionInfo{}),
+		newDelta(&multiwatcher.ApplicationOfferInfo{}),
+	}
+	_ = translate(dt, deltas)
 }
