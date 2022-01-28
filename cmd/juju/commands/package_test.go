3f86f3df777427bde695d6247014567751fc4928
Heather Lanigan <heather.lanigan@canonical.com>
2022-01-28 17:10:19 -0500

    Improve time to run juju ssh application/leader.
    Use a specific API call to get the leader unit name, fall back to using FullStatus
    if the new sshclient facade version is not available.  FullStatus can be very slow
    on large models, juju ssh should not be if possible.
    
    Juju debughooks time to ssh will also improved.
    
    Moved mockgen lines to package_test.go, per convention, easier to find in one place.
    
    Fixed some unhandled error and unused parameter compiler warnings.

--- cmd/juju/commands/package_test.go
+++ cmd/juju/commands/package_test.go
@@ -12,6 +12,13 @@ import (
 
 //go:generate go run github.com/golang/mock/mockgen -package commands -destination mockenvirons_test.go github.com/juju/juju/environs Environ,PrecheckJujuUpgradeStep
 //go:generate go run github.com/golang/mock/mockgen -package commands -destination mockupgradeenvirons_test.go github.com/juju/juju/cmd/juju/commands UpgradePrecheckEnviron
+// For ssh_container:
+//go:generate go run github.com/golang/mock/mockgen -package mocks -destination mocks/ssh_container_mock.go github.com/juju/juju/cmd/juju/commands CloudCredentialAPI,ApplicationAPI,ModelAPI,CharmsAPI
+//go:generate go run github.com/golang/mock/mockgen -package mocks -destination mocks/context_mock.go github.com/juju/juju/cmd/juju/commands Context
+//go:generate go run github.com/golang/mock/mockgen -package mocks -destination mocks/k8s_exec_mock.go github.com/juju/juju/caas/kubernetes/provider/exec Executor
+// For ssh:
+//go:generate go run github.com/golang/mock/mockgen -package mocks -destination mocks/statusapi_mock.go github.com/juju/juju/cmd/juju/commands StatusAPI
+//go:generate go run github.com/golang/mock/mockgen -package mocks -destination mocks/leaderapi_mock.go github.com/juju/juju/cmd/juju/commands LeaderAPI
 
 func init() {
 	if err := all.RegisterForClient(); err != nil {
