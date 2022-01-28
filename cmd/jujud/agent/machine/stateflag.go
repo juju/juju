dc0c4a18b59ed2767cdbe49b6865f1e6c5504003
Joseph Phillips <joe.phillips@canonical.com>
2022-01-28 15:33:06 +0100

    Alters the isControllerFlagManifold to depend on the state config watcher instead of state.
    This means that when a state ping fails, controller-gated workers that
    do not depend on state will not bounce.

--- cmd/jujud/agent/machine/stateflag.go
+++ cmd/jujud/agent/machine/stateflag.go
@@ -4,23 +4,28 @@
 package machine
 
 import (
+	"github.com/juju/errors"
 	"github.com/juju/worker/v3"
 	"github.com/juju/worker/v3/dependency"
 
 	"github.com/juju/juju/cmd/jujud/agent/engine"
 )
 
-// isControllerFlagManifold returns a dependency.Manifold which requires
-// State, and returns a worker implementing engine.Flag, whose Check method
-// always returns true. This is used for flagging that the machine is a
-// controller/model manager.
+// isControllerFlagManifold returns a dependency.Manifold that requires state
+// config.
+// It returns a worker implementing engine.Flag, whose Check method returns
+// whether state config is present on the machine.
 func isControllerFlagManifold() dependency.Manifold {
 	return dependency.Manifold{
-		Inputs: []string{stateName},
+		Inputs: []string{stateConfigWatcherName},
 		Output: engine.FlagOutput,
 		Start: func(context dependency.Context) (worker.Worker, error) {
-			if err := context.Get(stateName, nil); err != nil {
+			var haveStateConfig bool
+			if err := context.Get(stateConfigWatcherName, &haveStateConfig); err != nil {
 				return nil, err
+			}
+			if !haveStateConfig {
+				return nil, errors.Annotate(dependency.ErrMissing, "no state config detected")
 			}
 			return engine.NewStaticFlagWorker(true), nil
 		},
