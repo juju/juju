21c0c51cce0c0d7ce8593080db15fb58b65c8c4c
Heather Lanigan <heather.lanigan@canonical.com>
2022-01-31 11:37:20 -0500

    Update TestSetApplicationSeries.
    To reflect change which added a warning about calling juju refresh next.
    Added to help users install the correct charm with add-unit.

--- featuretests/cmd_juju_setseries_test.go
+++ featuretests/cmd_juju_setseries_test.go
@@ -4,10 +4,12 @@
 package featuretests
 
 import (
+	"github.com/juju/cmd/v3/cmdtesting"
 	jc "github.com/juju/testing/checkers"
 	gc "gopkg.in/check.v1"
 
 	jujutesting "github.com/juju/juju/juju/testing"
+	"github.com/juju/juju/state"
 	"github.com/juju/juju/testing/factory"
 )
 
@@ -17,11 +19,17 @@ type cmdSetSeriesSuite struct {
 
 func (s *cmdSetSeriesSuite) TestSetApplicationSeries(c *gc.C) {
 	charm := s.Factory.MakeCharm(c, &factory.CharmParams{Name: "multi-series", URL: "local:quantal/multi-series-1"})
-	app := s.Factory.MakeApplication(c, &factory.ApplicationParams{Charm: charm})
+	app := s.Factory.MakeApplication(c, &factory.ApplicationParams{
+		Charm:       charm,
+		CharmOrigin: &state.CharmOrigin{},
+	})
 	s.Factory.MakeUnit(c, &factory.UnitParams{Application: app, SetCharmURL: true})
 	c.Assert(app.Series(), gc.Equals, "quantal")
-	runCommandExpectSuccess(c, "set-series", "multi-series", "trusty")
-	err := app.Refresh()
+	context, err := runCommand(c, append([]string{"set-series"}, "multi-series", "trusty")...)
+	c.Assert(err, gc.IsNil)
+	c.Check(cmdtesting.Stderr(context), jc.Contains, "WARNING To ensure the correct charm binaries are installed when add-unit is next called, please first run `juju refresh` for this application and any related subordinates.")
+
+	err = app.Refresh()
 	c.Assert(err, jc.ErrorIsNil)
 	c.Assert(app.Series(), gc.Equals, "trusty")
 }
