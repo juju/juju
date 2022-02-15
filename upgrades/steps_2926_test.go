91df0fe77cb17505fff5af6008e03d28673385ff
Joseph Phillips <joe.phillips@canonical.com>
2022-02-15 09:32:42 +0100

    Adds upgrade step to set container address origins to "machine" if they are currently set to "provider".
    This corrects a prior upgrade step that indisciminately set all address
    origins to "provider".

---
+++ upgrades/steps_2926_test.go
@@ -0,0 +1,26 @@
+// Copyright 2022 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package upgrades_test
+
+import (
+	jc "github.com/juju/testing/checkers"
+	"github.com/juju/version/v2"
+	gc "gopkg.in/check.v1"
+
+	"github.com/juju/juju/testing"
+	"github.com/juju/juju/upgrades"
+)
+
+var v2926 = version.MustParse("2.9.26")
+
+type steps2926Suite struct {
+	testing.BaseSuite
+}
+
+var _ = gc.Suite(&steps2926Suite{})
+
+func (s *steps2926Suite) TestSetContainerAddressOriginToMachine(c *gc.C) {
+	step := findStateStep(c, v2926, `set container address origins to "machine"`)
+	c.Assert(step.Targets(), jc.DeepEquals, []upgrades.Target{upgrades.DatabaseMaster})
+}
