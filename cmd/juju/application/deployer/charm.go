d24ed9ba6a7dc2ec11f213bca376950006d9a529
Jordan Barrett <jordan.barrett@canonical.com>
2022-04-08 12:47:46 +1000

    Improve juju deploy nameclash error msg

--- cmd/juju/application/deployer/charm.go
+++ cmd/juju/application/deployer/charm.go
@@ -258,7 +258,12 @@ func (d *deployCharm) deploy(
 	}
 
 	if errors.IsAlreadyExists(err) {
-		return errors.Wrapf(err, errors.Errorf("\ndeploy application using an alias name, or use remove-application to remove the existing one and try again"), err.Error())
+		// Would be nice to be able to access the app name here
+		return errors.Wrapf(err, errors.Errorf(`
+deploy application using an alias name:
+    juju deploy <application> <alias>
+or use remove-application to remove the existing one and try again.`,
+		), err.Error())
 	}
 	return errors.Trace(err)
 
