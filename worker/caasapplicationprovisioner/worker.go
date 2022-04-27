5ebc199b2de89872ad053f2a82d46b0447a5faf0
Ben Hoyt <ben.hoyt@canonical.com>
2022-04-27 17:47:46 +1200

    Make parent worker not forcefully shut down child application worker
    Trying to forcefully shutdown the application worker meant there was
    a race condition when removing an application: if the parent worker
    got the event first, it would shut down the app worker via
    StopAndRemoveWorker (which would end up causing the app worker to take
    the "case <-a.catacomb.Dying()" in its select and exit before it'd
    had a chance to call a.dead which would ClearApplicationResources).
    
    Just let the child app worker shut itself down gracefully -- it checks
    for dead and handles that already.

--- worker/caasapplicationprovisioner/worker.go
+++ worker/caasapplicationprovisioner/worker.go
@@ -170,7 +170,8 @@ func (p *provisioner) loop() error {
 					return errors.Trace(err)
 				}
 				if errors.IsNotFound(err) || appLife == life.Dead {
-					p.shutDownAppWorker(appName)
+					// Application worker will shut itself down in these cases.
+					p.logger.Debugf("application %q not found or dead, ignoring", appName)
 					continue
 				}
 
@@ -208,16 +209,4 @@ func (p *provisioner) loop() error {
 			}
 		}
 	}
-}
-
-func (p *provisioner) shutDownAppWorker(appName string) {
-	err := p.runner.StopAndRemoveWorker(appName, p.catacomb.Dying())
-	if errors.IsNotFound(err) {
-		return
-	}
-	if err != nil {
-		p.logger.Warningf("stopping app worker %q: %v", appName, err)
-		return
-	}
-	p.logger.Debugf("removed app worker %q", appName)
 }
