19ecc2154babe716db853ee39f639438224d0644
Ben Hoyt <ben.hoyt@canonical.com>
2022-05-16 15:43:19 +1200

    Fix typo in cleanupUnitsForDyingApplication comment

--- state/cleanup.go
+++ state/cleanup.go
@@ -736,7 +736,7 @@ func (st *State) removeOffersForDyingModel() (err error) {
 }
 
 // cleanupUnitsForDyingApplication sets all units with the given prefix to Dying,
-// if they are not already Dying or Dead. It's expected to be used when a
+// if they are not already Dying or Dead. It's expected to be used when an
 // application is destroyed.
 func (st *State) cleanupUnitsForDyingApplication(applicationname string, cleanupArgs []bson.Raw) (err error) {
 	var destroyStorage bool
