2b0289a494e3cadb0e2eb1010c8ebcd16b6f9583
Evanson <evanson.wainaina@canonical.com>
2022-05-17 17:47:20 +0300

    make color mode default

--- cmd/juju/status/status.go
+++ cmd/juju/status/status.go
@@ -421,5 +421,6 @@ func (c *statusCommand) Run(ctx *cmd.Context) error {
 }
 
 func (c *statusCommand) FormatTabular(writer io.Writer, value interface{}) error {
-	return FormatTabular(writer, c.color, value)
+	//enable coloring by default
+	return FormatTabular(writer, true, value)
 }
