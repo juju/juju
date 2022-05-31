470f72fbf0fa3c4e67f8fb9b3da663be78a6e378
Joseph Phillips <joe.phillips@canonical.com>
2022-05-31 11:57:04 +0200

    Updates LXD dependency to the latest 5.2 release. Modifies certificate post payload and logging implementation accordingly.

--- container/lxd/logger.go
+++ container/lxd/logger.go
@@ -11,44 +11,40 @@ import (
 	lxdLogger "github.com/lxc/lxd/shared/logger"
 )
 
-// lxdLogProxy proxies LXD's log calls through the juju logger so we can get
-// more info about what's going on.
+// lxdLogProxy proxies LXD's log calls through the juju logger.
 type lxdLogProxy struct {
 	logger loggo.Logger
 }
 
-func (p *lxdLogProxy) render(msg string, ctx []interface{}) string {
+func (p *lxdLogProxy) render(msg string, ctx []lxdLogger.Ctx) string {
 	var result bytes.Buffer
 	result.WriteString(msg)
 	if len(ctx) > 0 {
 		result.WriteString(": ")
 	}
 
-	// This is sort of a hack, but it's enforced in the LXD code itself as
-	// well. LXD's logging framework forces us to pass things as "string"
-	// for one argument and then a "context object" as the next argument.
-	// So, we do some basic rendering here to make it look slightly less
-	// ugly.
-	var key string
-	for i, entry := range ctx {
-		if i != 0 {
-			result.WriteString(", ")
-		}
+	for _, c := range ctx {
+		var afterFirst bool
+		for k, v := range c {
+			if afterFirst {
+				result.WriteString(", ")
+			}
+			afterFirst = true
 
-		if key == "" {
-			key, _ = entry.(string)
-		} else {
-			result.WriteString(key)
+			result.WriteString(k)
 			result.WriteString(": ")
-			result.WriteString(fmt.Sprintf("%s", entry))
-			key = ""
+			result.WriteString(fmt.Sprintf("%v", v))
 		}
 	}
 
 	return result.String()
 }
 
-func (p *lxdLogProxy) Debug(msg string, ctx ...interface{}) {
+func (p *lxdLogProxy) Trace(msg string, ctx ...lxdLogger.Ctx) {
+	p.logger.Tracef(p.render(msg, ctx))
+}
+
+func (p *lxdLogProxy) Debug(msg string, ctx ...lxdLogger.Ctx) {
 	// NOTE(axw) the LXD client logs a lot of detail at
 	// "debug" level, which is its highest level of logging.
 	// We transform this to Trace, to avoid spamming our
@@ -56,20 +52,32 @@ func (p *lxdLogProxy) Debug(msg string, ctx ...interface{}) {
 	p.logger.Tracef(p.render(msg, ctx))
 }
 
-func (p *lxdLogProxy) Info(msg string, ctx ...interface{}) {
+func (p *lxdLogProxy) Info(msg string, ctx ...lxdLogger.Ctx) {
 	p.logger.Infof(p.render(msg, ctx))
 }
 
-func (p *lxdLogProxy) Warn(msg string, ctx ...interface{}) {
+func (p *lxdLogProxy) Warn(msg string, ctx ...lxdLogger.Ctx) {
 	p.logger.Warningf(p.render(msg, ctx))
 }
 
-func (p *lxdLogProxy) Error(msg string, ctx ...interface{}) {
+func (p *lxdLogProxy) Error(msg string, ctx ...lxdLogger.Ctx) {
 	p.logger.Errorf(p.render(msg, ctx))
 }
 
-func (p *lxdLogProxy) Crit(msg string, ctx ...interface{}) {
+func (p *lxdLogProxy) Crit(msg string, ctx ...lxdLogger.Ctx) {
 	p.logger.Criticalf(p.render(msg, ctx))
+}
+
+func (p *lxdLogProxy) Fatal(msg string, ctx ...lxdLogger.Ctx) {
+	p.logger.Criticalf("Fatal: %s", p.render(msg, ctx))
+}
+
+func (p *lxdLogProxy) Panic(msg string, ctx ...lxdLogger.Ctx) {
+	p.logger.Criticalf("Panic: %s", p.render(msg, ctx))
+}
+
+func (p *lxdLogProxy) AddContext(_ lxdLogger.Ctx) lxdLogger.Logger {
+	return p
 }
 
 func init() {
