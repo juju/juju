90344678463b6e26e9e976331b7732d296d7c252
Vitaly Antonenko <vitaly.antonenko@canonical.com>
2022-06-21 14:37:06 +0300

    init commit for servicelocator Uniter API tests

---
+++ apiserver/facades/agent/uniter/servicelocator_test.go
@@ -0,0 +1,18 @@
+// Copyright 2022 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package uniter_test
+
+import (
+	"github.com/juju/juju/testing"
+)
+
+type serviceLocatorSuite struct {
+	testing.BaseSuite
+
+	//backend *mocks.MockLXDProfileBackendV2
+	//charm   *mocks.MockLXDProfileCharmV2
+	//machine *mocks.MockLXDProfileMachineV2
+	//model   *mocks.MockLXDProfileModelV2
+	//unit    *mocks.MockLXDProfileUnitV2
+}
