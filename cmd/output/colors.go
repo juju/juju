d0ebcae02dd1b567c808acb53072459c9d7b5220
evanson <evanson.wainaina@canonical.com>
2022-06-22 22:40:15 +0300

    clean unused ansiterm colors

--- cmd/output/colors.go
+++ cmd/output/colors.go
@@ -11,8 +11,6 @@ type Colors struct {
 	Null *ansiterm.Context
 	// Bool is the Color for boolean values.
 	Bool *ansiterm.Context
-	// Binary is the color used for yaml binary data type.
-	Binary *ansiterm.Context
 	// Number is the Color for number values.
 	Number *ansiterm.Context
 	// String is the Color for string values.
@@ -21,20 +19,10 @@ type Colors struct {
 	Key *ansiterm.Context
 	// KeyValSep separates key from values.
 	KeyValSep *ansiterm.Context
-	// DocumentStart is the color used to mark the beginning of a valid yaml document
-	DocumentStart *ansiterm.Context
 	// Comment is the color used for yaml comments
 	Comment *ansiterm.Context
 	// Ip is the color for ip addresses
 	Ip *ansiterm.Context
-	// Scalar used to color scalar values
-	Scalar *ansiterm.Context
-	// IndentLine used to color indentation lines
-	IndentLine *ansiterm.Context
-	// Dash colors the beginning of a yaml item list
-	Dash *ansiterm.Context
-	// EmptyStructure colors the empty open and closing brackets '[]'
-	EmptyStructure *ansiterm.Context
-	// NodeAnchor colors a yaml node anchor definition.
-	NodeAnchor *ansiterm.Context
+	// Multiline colors a block of text spanning multiple lines
+	Multiline *ansiterm.Context
 }
