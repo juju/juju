088ed21758ab51e6053971addebe67bdc22959d4
Vitaly Antonenko <vitaly.antonenko@canonical.com>
2022-06-22 13:35:30 +0300

    add assertBackendAPI test to servicelocator in UniterAPI

--- apiserver/facades/agent/uniter/uniter.go
+++ apiserver/facades/agent/uniter/uniter.go
@@ -2892,4 +2892,42 @@ func (u *UniterAPI) CanApplyLXDProfile(args params.Entities) (params.BoolResults
 	return u.lxdProfileAPI.CanApplyLXDProfile(args)
 }
 
+func (u *UniterAPI) getServiceLocatorName(id string) (string, error) {
+	return "TBD", nil
+}
+
+// AddServiceLocator ...
+func (u *UniterAPI) AddServiceLocator(args params.Entities) (params.StringResults, error) {
+	result := params.StringResults{
+		Results: make([]params.StringResult, len(args.Entities)),
+	}
+	canAccess, err := u.accessUnit()
+	if err != nil {
+		return params.StringResults{}, err
+	}
+	for i, entity := range args.Entities {
+		tag, err := names.ParseUnitTag(entity.Tag)
+		if err != nil {
+			result.Results[i].Error = apiservererrors.ServerError(apiservererrors.ErrPerm)
+			continue
+		}
+		if !canAccess(tag) {
+			result.Results[i].Error = apiservererrors.ServerError(apiservererrors.ErrPerm)
+			continue
+		}
+		unit, err := u.getUnit(tag)
+		if err != nil {
+			result.Results[i].Error = apiservererrors.ServerError(err)
+			continue
+		}
+		machineId, err := unit.AssignedMachineId()
+		if err != nil {
+			result.Results[i].Error = apiservererrors.ServerError(err)
+		} else {
+			result.Results[i].Result = names.NewMachineTag(machineId).String()
+		}
+	}
+	return result, nil
+}
+
 func (u *UniterAPIV18) AddServiceLocator(_ struct{}) {}
