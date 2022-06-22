3d4fb400da64fddaf0648915ffe2237d484feb34
Vitaly Antonenko <vitaly.antonenko@canonical.com>
2022-06-22 22:43:16 +0300

    add simple test to serviceLocatorAPI of Uniter

--- apiserver/facades/agent/uniter/servicelocator_test.go
+++ apiserver/facades/agent/uniter/servicelocator_test.go
@@ -6,6 +6,7 @@ package uniter_test
 import (
 	"github.com/golang/mock/gomock"
 	"github.com/juju/loggo"
+	jc "github.com/juju/testing/checkers"
 	gc "gopkg.in/check.v1"
 
 	"github.com/juju/juju/apiserver/facades/agent/uniter"
@@ -28,4 +29,15 @@ func (s *serviceLocatorSuite) assertBackendAPI(c *gc.C) (*uniter.ServiceLocatorA
 	api := uniter.NewServiceLocatorAPI(
 		mockBackend, loggo.GetLogger("juju.apiserver.facades.agent.uniter"))
 	return api, ctrl, mockBackend
+}
+
+func (s *serviceLocatorSuite) TestAddServiceLocator(c *gc.C) {
+	api, ctrl, mockBackend := s.assertBackendAPI(c)
+	defer ctrl.Finish()
+
+	mockBackend.EXPECT().AddServiceLocator("id", "name", "type").Return("id", nil)
+
+	sl, err := api.AddServiceLocator("id", "name", "type")
+	c.Assert(err, jc.ErrorIsNil)
+	c.Assert(sl, gc.Equals, "id")
 }
