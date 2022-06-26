a45e157e13be94f3dacbc5b32d49b92c48ba34ea
Joseph Phillips <joe.phillips@canonical.com>
2022-06-26 18:41:53 +0200

    Removes unused wait-for stand-alone infrastructure and registers the command as a `juju` sub-command.

--- cmd/juju/waitfor/waitfor.go
+++ cmd/juju/waitfor/waitfor.go
@@ -4,14 +4,9 @@
 package waitfor
 
 import (
-	"os"
-
 	"github.com/juju/cmd/v3"
-	"github.com/juju/featureflag"
 	"github.com/juju/loggo"
 
-	"github.com/juju/juju/juju"
-	"github.com/juju/juju/juju/osenv"
 	_ "github.com/juju/juju/provider/all"
 )
 
@@ -21,43 +16,18 @@ var waitForDoc = `
 Juju wait-for attempts to wait for a given entity to reach a goal state.
 `
 
-// Main registers subcommands for the juju-metadata executable, and hands over control
-// to the cmd package. This function is not redundant with main, because it
-// provides an entry point for testing with arbitrary command line arguments.
-func Main(args []string) {
-	ctx, err := cmd.DefaultContext()
-	if err != nil {
-		cmd.WriteError(os.Stderr, err)
-		os.Exit(2)
-	}
-	if err := juju.InitJujuXDGDataHome(); err != nil {
-		cmd.WriteError(ctx.Stderr, err)
-		os.Exit(2)
-	}
-	os.Exit(cmd.Main(NewSuperCommand(), ctx, args[1:]))
-}
-
-// NewSuperCommand creates the metadata plugin supercommand and registers the
+// NewWaitForCommand creates the wait-for supercommand and registers the
 // subcommands that it supports.
-func NewSuperCommand() cmd.Command {
+func NewWaitForCommand() cmd.Command {
 	waitFor := cmd.NewSuperCommand(cmd.SuperCommandParams{
 		Name:        "wait-for",
 		UsagePrefix: "juju",
 		Doc:         waitForDoc,
-		Purpose:     "tools for generating and validating image and tools metadata",
-		Log:         &cmd.Log{}})
+		Purpose:     "tools for generating and validating image and tools metadata"})
 
 	waitFor.Register(newApplicationCommand())
 	waitFor.Register(newMachineCommand())
 	waitFor.Register(newModelCommand())
 	waitFor.Register(newUnitCommand())
 	return waitFor
-}
-
-func init() {
-	featureflag.SetFlagsFromEnvironment(osenv.JujuFeatureFlagEnvKey, osenv.JujuFeatures)
-}
-
-func main() {
-	Main(os.Args)
 }
