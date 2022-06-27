37019bf51ec0a1f69fe693851bd0e8c2b57e6920
Juan Tirado <juan.tirado@canonical.com>
2022-06-27 15:40:27 +0200

    Remove misleading change.

--- state/storage_test.go
+++ state/storage_test.go
@@ -458,7 +458,6 @@ var _ = gc.Suite(&StorageStateSuite{})
 
 func (s *StorageStateSuite) TestBlockStorageNotSupportedOnCAAS(c *gc.C) {
 	st := s.Factory.MakeCAASModel(c, nil)
-	st.Close()
 	defer st.Close()
 	ch := state.AddTestingCharmForSeries(c, st, "kubernetes", "storage-block")
 	_, err := st.AddApplication(state.AddApplicationArgs{
