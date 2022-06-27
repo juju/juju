ac1043118aae33250eae01f2802f19555e520401
Joseph Phillips <joe.phillips@canonical.com>
2022-06-27 11:04:45 +0200

    Updates mock generation directives and generated mocks for relocated wait-for.

--- cmd/juju/waitfor/query/package_test.go
+++ cmd/juju/waitfor/query/package_test.go
@@ -9,7 +9,7 @@ import (
 	gc "gopkg.in/check.v1"
 )
 
-//go:generate go run github.com/golang/mock/mockgen -package query -destination scope_mock_test.go github.com/juju/juju/cmd/plugins/juju-wait-for/query FuncScope,Scope
+//go:generate go run github.com/golang/mock/mockgen -package query -destination scope_mock_test.go github.com/juju/juju/cmd/juju/waitfor/query FuncScope,Scope
 
 func Test(t *testing.T) {
 	gc.TestingT(t)
