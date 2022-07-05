d57b5d4bcf032de14765d56c207bb9ead3770601
evanson <evanson.wainaina@canonical.com>
2022-07-05 16:58:29 +0300

    add color values

--- cmd/output/output.go
+++ cmd/output/output.go
@@ -160,27 +160,29 @@ var InfoHighlight = ansiterm.Foreground(ansiterm.Cyan)
 // EmphasisHighlight is used to show accompanying information, which
 // might be deemed as important by the user.
 var EmphasisHighlight = struct {
-	White         *ansiterm.Context
-	DefaultBold   *ansiterm.Context
-	BoldWhite     *ansiterm.Context
-	Gray          *ansiterm.Context
-	BoldGray      *ansiterm.Context
-	DarkGray      *ansiterm.Context
-	BoldDarkGray  *ansiterm.Context
-	Magenta       *ansiterm.Context
-	BrightMagenta *ansiterm.Context
-	BrightGreen   *ansiterm.Context
+	White             *ansiterm.Context
+	DefaultBold       *ansiterm.Context
+	BoldWhite         *ansiterm.Context
+	Gray              *ansiterm.Context
+	BoldGray          *ansiterm.Context
+	DarkGray          *ansiterm.Context
+	BoldDarkGray      *ansiterm.Context
+	Magenta           *ansiterm.Context
+	BrightMagenta     *ansiterm.Context
+	BoldBrightMagenta *ansiterm.Context
+	BrightGreen       *ansiterm.Context
 }{
-	White:         ansiterm.Foreground(ansiterm.White),
-	DefaultBold:   ansiterm.Foreground(ansiterm.Default).SetStyle(ansiterm.Bold),
-	BoldWhite:     ansiterm.Foreground(ansiterm.White).SetStyle(ansiterm.Bold),
-	Gray:          ansiterm.Foreground(ansiterm.Gray),
-	BoldGray:      ansiterm.Foreground(ansiterm.Gray).SetStyle(ansiterm.Bold),
-	BoldDarkGray:  ansiterm.Foreground(ansiterm.DarkGray).SetStyle(ansiterm.Bold),
-	DarkGray:      ansiterm.Foreground(ansiterm.DarkGray),
-	Magenta:       ansiterm.Foreground(ansiterm.Magenta),
-	BrightMagenta: ansiterm.Foreground(ansiterm.BrightMagenta),
-	BrightGreen:   ansiterm.Foreground(ansiterm.BrightGreen),
+	White:             ansiterm.Foreground(ansiterm.White),
+	DefaultBold:       ansiterm.Foreground(ansiterm.Default).SetStyle(ansiterm.Bold),
+	BoldWhite:         ansiterm.Foreground(ansiterm.White).SetStyle(ansiterm.Bold),
+	Gray:              ansiterm.Foreground(ansiterm.Gray),
+	BoldGray:          ansiterm.Foreground(ansiterm.Gray).SetStyle(ansiterm.Bold),
+	BoldDarkGray:      ansiterm.Foreground(ansiterm.DarkGray).SetStyle(ansiterm.Bold),
+	DarkGray:          ansiterm.Foreground(ansiterm.DarkGray),
+	Magenta:           ansiterm.Foreground(ansiterm.Magenta),
+	BrightMagenta:     ansiterm.Foreground(ansiterm.BrightMagenta),
+	BoldBrightMagenta: ansiterm.Foreground(ansiterm.BrightMagenta).SetStyle(ansiterm.Bold),
+	BrightGreen:       ansiterm.Foreground(ansiterm.BrightGreen),
 }
 
 var statusColors = map[status.Status]*ansiterm.Context{
