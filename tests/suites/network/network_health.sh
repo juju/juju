f2f77dfdf915d0e56230ec33e93a5292c376a17b
Jack Shaw <jack.shaw@canonical.com>
2022-07-06 14:19:24 +0100

    Replace Xenial network tests with Jammy
    Also testign charms from source locally. This makes the tests more clear
    and makes patching much easier
    
    Also implement workaround for Jammy bug that breaks the network-test
    webserver by disabling http server logging

--- tests/suites/network/network_health.sh
+++ tests/suites/network/network_health.sh
@@ -5,30 +5,33 @@ run_network_health() {
 
 	ensure "network-health" "${file}"
 
+	charm build ./tests/suites/network/charms/network-health --output-dir /tmp --force
+
 	# Deploy some applications for different series.
-	juju deploy ubuntu ubuntu-focal --series focal
-	juju deploy ubuntu ubuntu-xenial --series xenial
-	juju deploy ubuntu ubuntu-bionic --series bionic
+	juju deploy ./tests/suites/network/charms/ubuntu ubuntu-bionic --series bionic
+	juju deploy ./tests/suites/network/charms/ubuntu ubuntu-focal --series focal
+	juju deploy ./tests/suites/network/charms/ubuntu ubuntu-jammy --series jammy
 
 	# Now the testing charm for each series.
-	juju deploy 'cs:~juju-qa/network-health' network-health-xenial --series xenial
-	juju deploy 'cs:~juju-qa/network-health' network-health-bionic --series bionic
-	juju deploy 'cs:~juju-qa/network-health' network-health-focal --series focal
+	juju deploy /tmp/network-health network-health-bionic --series bionic
+	juju deploy /tmp/network-health network-health-focal --series focal
+	juju deploy /tmp/network-health network-health-jammy --series jammy
 
-	juju add-relation network-health-focal ubuntu-focal
-	juju add-relation network-health-xenial ubuntu-xenial
 	juju add-relation network-health-bionic ubuntu-bionic
+	juju add-relation network-health-focal ubuntu-focal
+	juju add-relation network-health-jammy ubuntu-jammy
 
 	juju expose network-health-xenial
 	juju expose network-health-bionic
 	juju expose network-health-focal
+	juju expose network-health-jammy
 
-	wait_for "ubuntu-bionic" "$(idle_condition "ubuntu-bionic" 3)"
-	wait_for "ubuntu-focal" "$(idle_condition "ubuntu-focal" 4)"
-	wait_for "ubuntu-xenial" "$(idle_condition "ubuntu-xenial" 5)"
-	wait_for "network-health-focal" "$(idle_subordinate_condition "network-health-focal" "ubuntu-focal")"
+	wait_for "ubuntu-bionic" "$(idle_condition "ubuntu-bionic" 0)"
+	wait_for "ubuntu-focal" "$(idle_condition "ubuntu-focal" 1)"
+	wait_for "ubuntu-jammy" "$(idle_condition "ubuntu-jammy" 2)"
 	wait_for "network-health-bionic" "$(idle_subordinate_condition "network-health-bionic" "ubuntu-bionic")"
-	wait_for "network-health-xenial" "$(idle_subordinate_condition "network-health-xenial" "ubuntu-xenial")"
+	wait_for "network-health-focal" "$(idle_subordinate_condition "network-health-focal" "ubuntu-focal")"
+	wait_for "network-health-jammy" "$(idle_subordinate_condition "network-health-jammy" "ubuntu-jammy")"
 
 	check_default_routes
 	check_accessibility
@@ -51,13 +54,13 @@ check_default_routes() {
 check_accessibility() {
 	echo "[+] checking neighbour connectivity and external access"
 
-	for net_health_unit in "network-health-xenial/0" "network-health-bionic/0" "network-health-focal/0"; do
+	for net_health_unit in "network-health-bionic/0" "network-health-focal/0" "network-health-jammy/0" ; do
 		ip="$(juju show-unit $net_health_unit --format json | jq -r ".[\"$net_health_unit\"] | .[\"public-address\"]")"
 
 		curl_cmd="curl 2>/dev/null ${ip}:8039"
 
 		# Check that each of the principles can access the subordinate.
-		for principle_unit in "ubuntu-focal/0" "ubuntu-xenial/0" "ubuntu-bionic/0"; do
+		for principle_unit in "ubuntu-bionic/0" "ubuntu-focal/0" "ubuntu-jammy/0" ; do
 			check_contains "$(juju run --unit $principle_unit "$curl_cmd")" "pass"
 		done
 
