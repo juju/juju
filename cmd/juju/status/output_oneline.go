2bdb20f69fca308f64d9fa0dda3a5890c6a82742
evanson <evanson.wainaina@canonical.com>
2022-07-18 21:15:49 +0300

    cleanup and remove redundant ansi coloring code

--- cmd/juju/status/output_oneline.go
+++ cmd/juju/status/output_oneline.go
@@ -6,41 +6,18 @@ package status
 import (
 	"bytes"
 	"fmt"
-	"io"
-	"sort"
-	"strconv"
-	"strings"
-
 	"github.com/juju/ansiterm"
 	"github.com/juju/errors"
 	"github.com/juju/juju/cmd/output"
 	"github.com/juju/naturalsort"
+	"io"
 )
 
 // FormatOneline writes a brief list of units and their subordinates.
 // Subordinates will be indented 2 spaces and listed under their
 // superiors. This format works with version 2 of the CLI.
-func FormatOneline(writer io.Writer, value interface{}) error {
-	return formatOneline(writer, value, func(out io.Writer, format, uName string, u unitStatus, level int) {
-		status := fmt.Sprintf(
-			"agent:%s, workload:%s",
-			u.JujuStatusInfo.Current,
-			u.WorkloadStatusInfo.Current,
-		)
-		fmt.Fprintf(out, format,
-			uName,
-			u.PublicAddress,
-			status,
-		)
-	})
-}
-
-// FormatOnelineWithColor appends ansi color codes to a brief list of units and their subordinates.
-// Subordinates will be indented 2 spaces and listed under their
-// superiors. This format works with version 2 of the CLI.
-func FormatOnelineWithColor(writer io.Writer, forceColor bool, value interface{}) error {
-	return formatOnelineWithColor(writer, value, func(out io.Writer, format, uName string, u unitStatus, level int) {
-
+func FormatOneline(writer io.Writer, forceColor bool, value interface{}) error {
+	return formatOneline(writer, forceColor, value, func(out io.Writer, format, uName string, u unitStatus, level int) {
 		agentColored := colorVal(output.EmphasisHighlight.DefaultBold, "agent")
 		statusInfoColored := colorVal(output.StatusColor(u.JujuStatusInfo.Current), u.JujuStatusInfo.Current)
 		workloadColored := colorVal(output.EmphasisHighlight.DefaultBold, "workload")
@@ -54,187 +31,55 @@ func FormatOnelineWithColor(writer io.Writer, forceColor bool, value interface{}
 		if forceColor {
 			statusColored := fmt.Sprintf(
 				"%s:%s, %s:%s", agentColored, statusInfoColored, workloadColored, workloadStatusInfoColored)
-
 			fPrintf(out, format, uName, publicAddressColored, statusColored)
 		} else {
 			status := fmt.Sprintf(
-				"%s:%s, %s:%s",
-				"agent",
+				"agent:%s, workload:%s",
 				u.JujuStatusInfo.Current,
-				"workload",
 				u.WorkloadStatusInfo.Current,
 			)
-
-			fPrintf(out, format, uName, u.PublicAddress, status)
+			fmt.Fprintf(out, format,
+				uName,
+				u.PublicAddress,
+				status,
+			)
 		}
 	})
 }
 
 type onelinePrintf func(out io.Writer, format, uName string, u unitStatus, level int)
 
-func formatOneline(writer io.Writer, value interface{}, printf onelinePrintf) error {
+func formatOneline(writer io.Writer, forceColor bool, value interface{}, printf onelinePrintf) error {
 	fs, valueConverted := value.(formattedStatus)
 	if !valueConverted {
 		return errors.Errorf("expected value of type %T, got %T", fs, value)
 	}
 
+	pw := &output.PrintWriter{Writer: output.Writer(writer)}
+	pw.SetColorCapable(forceColor)
 	pprint := func(uName string, u unitStatus, level int) {
-		var fmtPorts string
-		if len(u.OpenedPorts) > 0 {
-			fmtPorts = fmt.Sprintf(" %s", strings.Join(u.OpenedPorts, ", "))
-		}
-		format := indent("\n", level*2, "- %s: %s (%v)"+fmtPorts)
-		printf(writer, format, uName, u, level)
-	}
-
-	for _, svcName := range naturalsort.Sort(stringKeysFromMap(fs.Applications)) {
-		svc := fs.Applications[svcName]
-		for _, uName := range naturalsort.Sort(stringKeysFromMap(svc.Units)) {
-			unit := svc.Units[uName]
-			pprint(uName, unit, 0)
-			recurseUnits(unit, 1, pprint)
-		}
-	}
-
-	return nil
-}
-
-func formatOnelineWithColor(writer io.Writer, value interface{}, printf onelinePrintf) error {
-	fs, valueConverted := value.(formattedStatus)
-	if !valueConverted {
-		return errors.Errorf("expected value of type %T, got %T", fs, value)
-	}
-
-	pprint := func(uName string, u unitStatus, level int) {
-		var fmtPorts string
-		if len(u.OpenedPorts) > 0 {
-			fmtPorts = fmt.Sprintf(" %s", colorPorts(u.OpenedPorts))
-		}
-		format := indent("\n", level*2, "- %s: %s (%v)"+fmtPorts)
-		printf(writer, format, colorVal(output.GoodHighlight, uName), u, level)
-	}
-
-	for _, svcName := range naturalsort.Sort(stringKeysFromMap(fs.Applications)) {
-		svc := fs.Applications[svcName]
-		for _, uName := range naturalsort.Sort(stringKeysFromMap(svc.Units)) {
-			unit := svc.Units[uName]
-			pprint(uName, unit, 0)
-			recurseUnits(unit, 1, pprint)
-		}
-	}
-
-	return nil
-}
-
-func colorPorts(ps []string) string {
-	buff := &bytes.Buffer{}
-	sorted := append([]string(nil), ps...)
-	sort.Strings(sorted)
-
-	protocols := map[string]*protocol{}
-	proto := func(p string) *protocol {
-		v, ok := protocols[p]
-		if !ok {
-			v = &protocol{
-				group:      map[string]string{},
-				groups:     map[string][]string{},
-				grouped:    map[string]bool{},
-				components: map[string][]string{},
-			}
-			protocols[p] = v
-		}
-		return v
-	}
-
-	for _, port := range sorted {
-		split := strings.Split(port, "/")
-		protocolId := ""
-		if len(split) == 1 {
-			protocolId = split[0]
+		format := indent("\n", level*2, "- %s: %s (%v)")
+		if forceColor {
+			printf(writer, format, colorVal(output.GoodHighlight, uName), u, level)
 		} else {
-			protocolId = strings.Join(append([]string{""}, split[1:]...), "/")
+			printf(writer, format, uName, u, level)
 		}
-		protocol := proto(protocolId)
-		protocol.components[port] = split
-		if len(split) == 1 {
-			continue
+		if len(u.OpenedPorts) > 0 {
+			fmt.Fprint(pw, " (")
+			printPorts(pw, u.OpenedPorts)
+			fmt.Fprint(pw, ") ")
 		}
-		n, err := strconv.Atoi(split[0])
-		if err != nil || n <= 1 {
-			continue
-		}
-		prev := strings.Join(append([]string{strconv.Itoa(n - 1)}, split[1:]...), "/")
-		if _, ok := protocol.components[prev]; !ok {
-			continue
-		}
-		groupName := protocol.group[prev]
-		if groupName == "" {
-			groupName = prev
-		}
-		protocol.group[port] = groupName
-		protocol.groups[groupName] = append(protocol.groups[groupName], port)
-		protocol.grouped[port] = true
 	}
-
-	protocolKeys := []string{}
-	for k := range protocols {
-		protocolKeys = append(protocolKeys, k)
-	}
-	sort.Strings(protocolKeys)
-
-	hasOutput := false
-	for _, pk := range protocolKeys {
-		protocol := protocols[pk]
-
-		portKeys := []string{}
-		for k := range protocol.components {
-			portKeys = append(portKeys, k)
-		}
-		sort.Sort(sortablePorts(portKeys))
-
-		hasPrev := false
-		for _, port := range portKeys {
-			if protocol.grouped[port] {
-				continue
-			}
-			if hasOutput {
-				hasOutput = false
-				buff.WriteString(" ")
-			}
-			if hasPrev {
-				buff.WriteString(",")
-			}
-			hasPrev = true
-			split := protocol.components[port]
-			group := protocol.groups[port]
-			// color grouped ports.
-			if len(group) > 0 {
-				last := group[len(group)-1]
-				lastSplit := protocol.components[last]
-				portRange := fmt.Sprintf("%s-%s", split[0], lastSplit[0])
-				buff.WriteString(colorVal(output.EmphasisHighlight.BrightMagenta, portRange))
-				continue
-			}
-			// color single port with protocol.
-			if len(split) > 1 {
-				buff.WriteString(colorVal(output.EmphasisHighlight.BrightMagenta, split[0]))
-				continue
-			}
-			// Everything else.
-			break
-		}
-		if hasPrev {
-			hasOutput = true
-			if _, err := strconv.Atoi(pk); err == nil {
-				buff.WriteString(colorVal(output.EmphasisHighlight.BrightMagenta, pk))
-			} else {
-				buff.WriteString(pk)
-			}
+	for _, svcName := range naturalsort.Sort(stringKeysFromMap(fs.Applications)) {
+		svc := fs.Applications[svcName]
+		for _, uName := range naturalsort.Sort(stringKeysFromMap(svc.Units)) {
+			unit := svc.Units[uName]
+			pprint(uName, unit, 0)
+			recurseUnits(unit, 1, pprint)
 		}
 	}
 
-	buff.WriteString("")
-	return buff.String()
+	return nil
 }
 
 //colorVal appends ansi color codes to the given value
