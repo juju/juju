480c14d2fa6f0c272ba2fbdef2d3f6dbe4ec2620
Jordan Barrett <jordan.barrett@canonical.com>
2022-07-18 13:56:54 +1000

    [smoke test] add timeout

--- tests/includes/wait-for.sh
+++ tests/includes/wait-for.sh
@@ -1,33 +1,31 @@
-# SHORT_TIMEOUT is the time between polling attempts.
+# SHORT_TIMEOUT creates a consistent short timeout of the wait_for condition.
 SHORT_TIMEOUT=5
 
 # wait_for defines the ability to wait for a given condition to happen in a
 # juju status output. The output is JSON, so everything that the API server
 # knows about should be valid.
 # The query argument is a jq query.
-# The default timeout is 10 minutes. You can change this by providing the
-# timeout argument (an integer number of seconds).
+# The default timeout is 10 minutes (120 attempts @ 5 seconds each).
+# You can change this by providing the max_attempts argument.
 #
 # ```
-# wait_for <model name> <query> [<timeout>]
+# wait_for <model name> <query> [<max_attempts>]
 # ```
 wait_for() {
-	local name query timeout
+	local name query max_attempts
 
 	name=${1}
 	query=${2}
-	timeout=${3:-600} # default timeout: 600s = 10m
+	max_attempts=${3:-120} # default timeout: 120x5s = 10m
 
 	attempt=0
-	start_time="$(date -u +%s)"
 	# shellcheck disable=SC2046,SC2143
 	until [[ "$(juju status --format=json 2>/dev/null | jq -S "${query}" | grep "${name}")" ]]; do
 		echo "[+] (attempt ${attempt}) polling status for" "${query} => ${name}"
 		juju status --relations 2>&1 | sed 's/^/    | /g'
 		sleep "${SHORT_TIMEOUT}"
 
-		elapsed=$(date -u +%s)-$start_time
-		if [[ ${elapsed} -ge ${timeout} ]]; then
+		if [[ "${attempt}" -ge ${max_attempts} ]]; then
 			echo "[-] $(red 'timed out waiting for')" "$(red "${name}")"
 			exit 1
 		fi
