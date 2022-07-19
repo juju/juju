5d8b795364bcd4b72da6f6025c34cd3909b6e4f4
Vitaly Antonenko <vitaly.antonenko@canonical.com>
2022-07-19 11:22:12 +0300

    Dropped the charmstore test which used old mysql

--- tests/suites/agents/key_workers_run.sh
+++ tests/suites/agents/key_workers_run.sh
@@ -1,22 +1,3 @@
-run_charmstore_charmrevisionupdater() {
-	# Echo out to ensure nice output to the test suite.
-	echo
-
-	model_name="test-cs-charmrevisionupdater"
-	file="${TEST_DIR}/${model_name}.log"
-
-	ensure "${model_name}" "${file}"
-
-	# Deploy an old revision of mysql
-	juju deploy cs:mysql-55
-
-	# Wait for revision update worker to update the available revision.
-	# eg can-upgrade-to: cs:mysql-58
-	wait_for "cs:mysql-" '.applications["mysql"] | ."can-upgrade-to"'
-
-	destroy_model "${model_name}"
-}
-
 run_charmhub_charmrevisionupdater() {
 	# Echo out to ensure nice output to the test suite.
 	echo
@@ -48,6 +29,5 @@ test_charmrevisionupdater() {
 		cd .. || exit
 
 		run "run_charmhub_charmrevisionupdater"
-		run "run_charmstore_charmrevisionupdater"
 	)
 }
