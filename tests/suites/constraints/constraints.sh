cee797cc6319fd172901111753146d8450a226bb
Vitaly Antonenko <vitaly.antonenko@canonical.com>
2022-07-21 00:14:02 +0300

    Fixed white spaces

--- tests/suites/constraints/constraints.sh
+++ tests/suites/constraints/constraints.sh
@@ -10,15 +10,15 @@ test_constraints_common() {
   		cd .. || exit
 
   		case "${BOOTSTRAP_PROVIDER:-}" in
-      "lxd" | "lxd-remote" | "localhost")
-        run "run_constraints_lxd"
-        ;;
-      "microk8s")
-        echo "==> TEST SKIPPED: constraints - there are no test for k8s cloud"
-        ;;
-      *)
-        run "run_constraints_vm"
-        ;;
-      esac
+  		"lxd" | "lxd-remote" | "localhost")
+  		  run "run_constraints_lxd"
+  		  ;;
+  		"microk8s")
+  		  echo "==> TEST SKIPPED: constraints - there are no test for k8s cloud"
+  		  ;;
+  		*)
+  		  run "run_constraints_vm"
+  		  ;;
+  		esac
   )
 }
