73010d451059b1b58a59103ca6d737945e2522ed
Vitaly Antonenko <vitaly.antonenko@canonical.com>
2022-08-02 15:06:45 +0300

    Fixed static-analysis.

--- tests/suites/deploy/deploy_charms.sh
+++ tests/suites/deploy/deploy_charms.sh
@@ -235,14 +235,14 @@ test_deploy_charms() {
 
 		run "run_deploy_charm"
 		run "run_deploy_specific_series"
-#		run "run_deploy_lxd_to_container"
-#		run "run_deploy_lxd_profile_charm_container"
+		#		run "run_deploy_lxd_to_container"
+		#		run "run_deploy_lxd_profile_charm_container"
 
 		case "${BOOTSTRAP_PROVIDER:-}" in
 		"lxd" | "localhost")
 			run "run_deploy_lxd_to_machine"
 			run "run_deploy_lxd_profile_charm"
-#			run "run_deploy_local_lxd_profile_charm"
+			#			run "run_deploy_local_lxd_profile_charm"
 			;;
 		*)
 			echo "==> TEST SKIPPED: deploy_lxd_to_machine - tests for LXD only"
