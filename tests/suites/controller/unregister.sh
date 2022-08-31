573b34b7dc804b1b5741d8029455624fc52e8e89
Vitaly Antonenko <vitaly.antonenko@canonical.com>
2022-08-31 10:20:47 +0300

    Added accounts.yaml backup/restore, that helped to avoid using expect-scripts.

--- tests/suites/controller/unregister.sh
+++ tests/suites/controller/unregister.sh
@@ -5,25 +5,25 @@ run_unregister() {
 
 	ensure "unregister" "${file}"
 
+	echo "Get controller name"
 	controller_name=$(juju controllers --format=json | jq -r '."current-controller"')
 
-	echo "Change admin password"
-	./tests/suites/controller/expect-scripts/juju-change-user-password.exp
+	echo "Check controller is known"
+	juju controllers --format=json | jq -r ".\"controllers\" | has(\"${controller_name}\")" | check true
 
 	echo "Backup controller info before unregister"
 	cp ~/.local/share/juju/controllers.yaml ~/.local/share/juju/controllers.yaml.bak
+	cp ~/.local/share/juju/accounts.yaml ~/.local/share/juju/accounts.yaml.bak
 
 	echo "Unregister controller"
 	juju unregister --yes "${controller_name}"
 
+	echo "Check controller is not known"
 	juju controllers --format=json | jq -r ".\"controllers\".\"${controller_name}\"" | check null
 
 	echo "Restore controller info after unregister"
 	mv ~/.local/share/juju/controllers.yaml.bak ~/.local/share/juju/controllers.yaml
-
-	echo "Login to controller info after restoring"
-	./tests/suites/controller/expect-scripts/juju-login.exp "${controller_name}"
-	juju switch unregister
+	mv ~/.local/share/juju/accounts.yaml.bak ~/.local/share/juju/accounts.yaml
 
 	juju controllers --format=json | jq -r '."current-controller"' | check "${controller_name}"
 
