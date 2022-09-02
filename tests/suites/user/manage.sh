378243b27814f81add8900397d51e2c4a041fd69
Vitaly Antonenko <vitaly.antonenko@canonical.com>
2022-09-02 14:42:50 +0300

    Added user-disable-enable test.

--- tests/suites/user/manage.sh
+++ tests/suites/user/manage.sh
@@ -1,4 +1,4 @@
-# Granting and revoke read/write rights for a user.
+# Granting and revoking read/write/admins rights for the users.
 run_user_grant_revoke() {
 	# Echo out to ensure nice output to the test suite.
 	echo
@@ -40,9 +40,40 @@ run_user_grant_revoke() {
 	destroy_model "user-grant-revoke"
 }
 
-test_user_grant_revoke() {
-	if [ -n "$(skip 'test_user_grant_revoke')" ]; then
-		echo "==> SKIP: Asked to skip user grant revoke tests"
+# Disabling and enabling users.
+run_user_disable_enable() {
+	# Echo out to ensure nice output to the test suite.
+	echo
+
+	# The following ensures that a bootstrap juju exists.
+	file="${TEST_DIR}/test-user-disable-enable.log"
+	ensure "user-disable-enable" "${file}"
+
+	echo "Check that current user is admin"
+	juju whoami --format=json | jq -r ".\"user\"" | check "admin"
+
+	echo "Add testuser"
+	juju add-user testuser
+	juju grant testuser read "user-disable-enable"
+
+	echo "Disable testuser"
+	juju disable-user testuser
+
+	echo "Check testuser is disabled"
+	juju show-user testuser --format=json | jq -r ".\"disabled\"" | check true
+
+	echo "Enable testuser"
+	juju enable-user testuser
+
+	echo "Check testuser is enabled"
+	juju show-user testuser --format=json | jq -r ".\"disabled\"" | check null
+
+	destroy_model "user-disable-enable"
+}
+
+test_user_manage() {
+	if [ -n "$(skip 'test_user_manage')" ]; then
+		echo "==> SKIP: Asked to skip user manage tests"
 		return
 	fi
 
@@ -52,5 +83,6 @@ test_user_grant_revoke() {
 		cd .. || exit
 
 		run "run_user_grant_revoke"
+		run "run user_disable_enable"
 	)
 }
