0c8dbd68bb20dd95f3f43f511583b6be0a4495b4
evanson <evanson.wainaina@canonical.com>
2022-09-07 06:55:05 +0300

    reintroduce newline end for empty models status

--- cmd/juju/status/status.go
+++ cmd/juju/status/status.go
@@ -389,6 +389,10 @@ func (c *statusCommand) runStatus(ctx *cmd.Context) error {
 		if err != nil {
 			return err
 		}
+		// A change was made in cmd/v3.0.2 output.go that broke the consistency in output for the
+		// default formatter by removing the newline at the end.
+		// https://github.com/juju/cmd/commit/be22fa661a798055c801f1511aee226db249ef95
+		fmt.Println()
 		ctx.Infof("Model %q is empty.", modelName)
 	} else {
 		plural := func() string {
