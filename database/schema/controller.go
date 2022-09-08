8615747356189a20ac2b490d91f467720b2df564
Joseph Phillips <joe.phillips@canonical.com>
2022-09-08 08:48:26 +0200

    Adds controller database bootstrap DDL with tables for leases.

---
+++ database/schema/controller.go
@@ -0,0 +1,33 @@
+// Copyright 2022 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package schema
+
+// controllerDDL is used to create the controller database schema at bootstrap.
+const controllerDDL = `
+CREATE TABLE lease_type (
+	id		INT PRIMARY KEY,
+	type	TEXT
+);
+
+INSERT INTO lease_type VALUES
+	(0, 'controller'), 
+	(1, 'model' ), 
+	(2, 'application');
+
+CREATE TABLE IF NOT EXISTS lease (
+	uuid			TEXT PRIMARY KEY,
+	lease_type_id	INT NOT NULL,
+	name			TEXT,
+	holder			TEXT,
+	start			TIMESTAMP,
+	duration		INT,
+	pinned			BOOLEAN,
+	CONSTRAINT		fk_lease_lease_type
+		FOREIGN KEY	(lease_type_id)
+		REFERENCES	lease_type(id)
+);
+
+CREATE UNIQUE INDEX idx_lease_type_name
+ON lease (lease_type_id, name);
+`
