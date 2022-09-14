5c59da3ac9ba5d7cad450267a7c0818f5e15fc59
Vitaly Antonenko <vitaly.antonenko@canonical.com>
2022-09-14 11:54:32 +0300

    Removed model creation.

--- tests/suites/credential/add_remove_credential.sh
+++ tests/suites/credential/add_remove_credential.sh
@@ -3,11 +3,6 @@ run_add_remove_credential() {
 	# Echo out to ensure nice output to the test suite.
 	echo
 
-	model_name="test-add-credential"
-	file="${TEST_DIR}/${model_name}.log"
-
-	ensure "${model_name}" "${file}"
-
 	echo "Add fake credential"
 	JUJU_DATA="${TEST_DIR}/juju" juju add-credential aws -f ./tests/suites/credential/credentials-data/fake-credentials.yaml --client
 
@@ -20,7 +15,6 @@ run_add_remove_credential() {
 	echo "Check fake credential is deleted"
 	JUJU_DATA="${TEST_DIR}/juju" juju credentials aws --format=json | jq -r '."client-credentials"."aws"."cloud-credentials"."fake-credential-name"."details"."access-key"' | check null
 
-	destroy_model "${model_name}"
 }
 
 test_add_remove_credential() {
