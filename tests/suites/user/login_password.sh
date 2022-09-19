ef667958a99abcabc34f1e77b383c345b93356a6
Vitaly Antonenko <vitaly.antonenko@canonical.com>
2022-09-19 16:54:06 +0300

    Added test_user_login_password in user suite with expect tool usage.

--- tests/suites/user/login_password.sh
+++ tests/suites/user/login_password.sh
@@ -1,4 +1,36 @@
-# ....
+# expect_command the ability to work with interactive commands with expect tool.
+# The output is "OK", if tests passes successfully.
+# The expect_script argument is a expect script body.
+# The default timeout is 10 seconds.
+#
+# ```
+# wait_for <command> <expect_script>
+# ```
+expect_command() {
+	local command expect_script
+
+	command=${1}
+	filename=$(echo "${command}" | tr ' ' '-')
+	expect_script=${2}
+
+	cat > "${TEST_DIR}/${filename}.exp" <<EOF
+#!/usr/bin/expect
+proc abort { } { send_user \"\rTimeout Error!\" ; exit 2 }
+expect_before timeout abort
+
+set timeout 10
+spawn ${command}
+match_max 100000
+
+${expect_script}
+
+send_user \"OK\"
+expect eof
+EOF
+	expect "${TEST_DIR}/${filename}.exp"
+}
+
+# run_user_change_password changes the admin password, logout and tries to login with new password
 run_user_change_password() {
 	# Echo out to ensure nice output to the test suite.
 	echo
@@ -7,27 +39,26 @@ run_user_change_password() {
 	file="${TEST_DIR}/test-user-change-password.log"
 	ensure "user-change-password" "${file}"
 
-	echo "Add test-user"
-	juju add-user test-user
+	echo "Change admin password"
+	expect_command "juju change-user-password" "
+expect \"new password: \" {
+	send \"test-password\r\"
+	expect \"type new password again: \" {
+		send \"test-password\r\"
+	}
+}" | check "OK"
 
-	echo "Change test-user password"
-	expect -c "
-		proc abort { } { send_user \"\rTimeout Error!\" ; exit 2 }
-		expect_before timeout abort
+	echo "Logout from controller"
+	juju logout
 
-		set timeout 10
-		spawn juju change-user-password test-user
-		match_max 100000
-		expect \"old password: \" {
-			send \"test-password\r\"
-			expect \"\rtype new password again: \" {
-				send \"test-password\r\"
-			}
-		}
-
-		send_user \"passed\"
-		expect eof
-	"
+	echo "Login as admin"
+	expect_command "juju login" "
+expect \"Enter username: \" {
+	send \"admin\r\"
+	expect \"please enter password for admin*\" {
+		send \"test-password\r\"
+	}
+}" | check "OK"
 
 	destroy_model "user-change-password"
 }
