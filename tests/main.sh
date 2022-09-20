d75ace65a365d37730574f36bbefccc343a1aebe
Vitaly Antonenko <vitaly.antonenko@canonical.com>
2022-09-20 12:11:19 +0300

    Added dependency check.

--- tests/main.sh
+++ tests/main.sh
@@ -230,7 +230,7 @@ fi
 echo ""
 
 echo "==> Checking for dependencies"
-check_dependencies curl jq yq shellcheck
+check_dependencies curl jq yq shellcheck expect
 
 if [[ ${USER:-'root'} == "root" ]]; then
 	echo "The testsuite must not be run as root." >&2
