466d4d66b72b08bbb30ab61f3aadf617e5320e81
Caner Derici <caner.derici@canonical.com>
2022-09-21 15:31:12 -0600

    Rewrite api/client/imagemetadatamanager unit tests to use gomock

--- api/client/imagemetadatamanager/package_test.go
+++ api/client/imagemetadatamanager/package_test.go
@@ -1,14 +1,22 @@
 // Copyright 2017 Canonical Ltd.
 // Licensed under the AGPLv3, see LICENCE file for details.
 
-package imagemetadatamanager_test
+package imagemetadatamanager
 
 import (
 	"testing"
 
 	gc "gopkg.in/check.v1"
+
+	"github.com/juju/juju/api/base"
 )
 
 func TestPackage(t *testing.T) {
 	gc.TestingT(t)
+}
+
+func NewClientFromCaller(caller base.FacadeCaller) *Client {
+	return &Client{
+		facade: caller,
+	}
 }
