461d164271cfc968fee5932a38985cec365d3a7b
Joseph Phillips <joe.phillips@canonical.com>
2022-09-21 11:39:04 +0200

    Ensures consistent pointer receivers for methods on the LXD `Server` type.

--- container/lxd/cluster.go
+++ container/lxd/cluster.go
@@ -10,7 +10,7 @@ func (s *Server) ClusterSupported() bool {
 // UseTargetServer returns a new Server based on the input target node name.
 // It is intended for use when operations must target specific nodes in a
 // cluster.
-func (s Server) UseTargetServer(name string) (*Server, error) {
+func (s *Server) UseTargetServer(name string) (*Server, error) {
 	logger.Debugf("creating LXD server for cluster node %q", name)
 	return NewServer(s.UseTarget(name))
 }
