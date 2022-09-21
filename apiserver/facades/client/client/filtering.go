c17495f1d300051ee1d3ac2390ffe32e6bf52f17
Vitaly Antonenko <vitaly.antonenko@canonical.com>
2022-09-21 10:56:46 +0300

    Code simplification.

--- apiserver/facades/client/client/filtering.go
+++ apiserver/facades/client/client/filtering.go
@@ -282,12 +282,10 @@ func buildUnitMatcherShims(u *state.Unit, patterns []string) []closurePredicate 
 
 func matchPortRanges(patterns []string, portRanges ...network.PortRange) (bool, bool, error) {
 	for _, p := range portRanges {
-		splitPortStr := strings.Split(p.String(), "/")
-		pNum := splitPortStr[0]
+		pNum, _, _ := strings.Cut(p.String(), "/")
 		for _, patt := range patterns {
-			splitPattStr := strings.Split(patt, "/")
-			pattNum := splitPattStr[0]
-			if strings.EqualFold(pNum, pattNum) && strings.HasPrefix(p.String(), patt) {
+			pattNum, _, _ := strings.Cut(patt, "/")
+			if pNum == pattNum && strings.HasPrefix(p.String(), patt) {
 				return true, true, nil
 			}
 		}
