3c4a996fdb4939bf4d366c7a56e4af263ac16c54
evanson <evanson.wainaina@canonical.com>
2022-10-03 14:12:20 +0300

    setup cos lite intgration test

---
+++ tests/suites/coslite/task.sh
@@ -0,0 +1,35 @@
+test_coslite() {
+	if [ "$(skip 'test_coslite')" ]; then
+		echo "==> TEST SKIPPED: COS Lite tests"
+		return
+	fi
+
+	set_verbosity
+
+	echo "==> Checking for dependencies"
+	check_dependencies juju
+
+	file="${TEST_DIR}/test_coslite.log"
+
+	bootstrap "test-coslite" "${file}"
+
+	case "${BOOTSTRAP_PROVIDER:-}" in
+	"k8s")
+		# disable metallb then enable it with a new set of out ipaddr
+		microk8s disable metallb
+		IPADDR=$(ip -4 -j route get 2.2.2.2 | jq -r '.[] | .prefsrc')
+		microk8s enable metallb:"$IPADDR"-"$IPADDR"
+		microk8s kubectl rollout status deployments/hostpath-provisioner -n kube-system -w
+		microk8s kubectl rollout status deployments/coredns -n kube-system -w
+		microk8s kubectl rollout status daemonset.apps/speaker -n metallb-system -w
+
+		test_deploy_coslite
+		;;
+	*)
+		echo "==> TEST SKIPPED: test_deploy_coslite test runs on k8s only"
+		;;
+
+	esac
+
+	destroy_controller "test-coslite"
+}
