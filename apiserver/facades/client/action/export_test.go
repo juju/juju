e460617055a05efdb01d21f7c2de3b19a8c6fcab
Joseph Phillips <joe.phillips@canonical.com>
2022-10-04 12:16:39 +0200

    Extracts a base suite for mock-based tests into package_test.go, and uses the leadership reader mock instead of state to determine application leaders.

--- apiserver/facades/client/action/export_test.go
+++ apiserver/facades/client/action/export_test.go
@@ -11,16 +11,3 @@ import (
 func NewActionAPI(st *state.State, resources facade.Resources, authorizer facade.Authorizer) (*ActionAPI, error) {
 	return newActionAPI(&stateShim{st: st}, resources, authorizer)
 }
-
-func NewActionAPIForMockTest(st State, resources facade.Resources, authorizer facade.Authorizer, tagToActionReceiverFn TagToActionReceiverFunc) (*ActionAPI, error) {
-	api, err := newActionAPI(st, resources, authorizer)
-	if err != nil {
-		return api, err
-	}
-	api.tagToActionReceiverFn = tagToActionReceiverFn
-	return api, nil
-}
-
-func StateShimForTest(st *state.State) *stateShim {
-	return &stateShim{st: st}
-}
--- apiserver/facades/client/action/package_test.go
+++ apiserver/facades/client/action/package_test.go
@@ -3,6 +3,38 @@
 
 package action
 
+import (
+	facademocks "github.com/juju/juju/apiserver/facade/mocks"
+	"github.com/juju/juju/apiserver/facades/client/action/mocks"
+	"github.com/juju/juju/state"
+	"github.com/juju/names/v4"
+	jc "github.com/juju/testing/checkers"
+	gc "gopkg.in/check.v1"
+)
+
 //go:generate go run github.com/golang/mock/mockgen -package mocks -destination mocks/local_mock.go github.com/juju/juju/apiserver/facades/client/action State,Model
 //go:generate go run github.com/golang/mock/mockgen -package mocks -destination mocks/state_mock.go github.com/juju/juju/state Action,ActionReceiver
 //go:generate go run github.com/golang/mock/mockgen -package mocks -destination mocks/leader_mock.go github.com/juju/juju/core/leadership Reader
+
+type MockBaseSuite struct {
+	State          *mocks.MockState
+	Authorizer     *facademocks.MockAuthorizer
+	ActionReceiver *mocks.MockActionReceiver
+	Leadership     *mocks.MockReader
+}
+
+func (s *MockBaseSuite) NewActionAPI(c *gc.C) *ActionAPI {
+	api, err := newActionAPI(s.State, nil, s.Authorizer, s.Leadership)
+	c.Assert(err, jc.ErrorIsNil)
+
+	api.tagToActionReceiverFn = s.tagToActionReceiverFn
+	return api
+}
+
+func (s *MockBaseSuite) tagToActionReceiverFn(
+	_ func(names.Tag) (state.Entity, error),
+) func(tag string) (state.ActionReceiver, error) {
+	return func(tag string) (state.ActionReceiver, error) {
+		return s.ActionReceiver, nil
+	}
+}
