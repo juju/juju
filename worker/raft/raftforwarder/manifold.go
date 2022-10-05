23d23834dc7a96776e06585b499e2d8dc8045923
Joseph Phillips <joe.phillips@canonical.com>
2022-10-05 13:09:06 +0200

    Removes notify functionality and state dependency from Raft forwarder worker.

--- worker/raft/raftforwarder/manifold.go
+++ worker/raft/raftforwarder/manifold.go
@@ -10,33 +10,22 @@ import (
 	"github.com/juju/worker/v3"
 	"github.com/juju/worker/v3/dependency"
 	"github.com/prometheus/client_golang/prometheus"
-
-	"github.com/juju/juju/core/raftlease"
-	"github.com/juju/juju/state"
-	raftleasestore "github.com/juju/juju/state/raftlease"
-	"github.com/juju/juju/worker/common"
-	workerstate "github.com/juju/juju/worker/state"
 )
 
 // ManifoldConfig holds the resources needed to start a raft forwarder
 // worker in a dependency engine.
 type ManifoldConfig struct {
 	RaftName       string
-	StateName      string
 	CentralHubName string
 
 	RequestTopic         string
 	PrometheusRegisterer prometheus.Registerer
 	Logger               Logger
 	NewWorker            func(Config) (worker.Worker, error)
-	NewTarget            func(*state.State, raftleasestore.Logger) raftlease.NotifyTarget
 }
 
 // Validate checks that the config has all the required values.
 func (config ManifoldConfig) Validate() error {
-	if config.StateName == "" {
-		return errors.NotValidf("empty StateName")
-	}
 	if config.CentralHubName == "" {
 		return errors.NotValidf("empty CentralHubName")
 	}
@@ -51,9 +40,6 @@ func (config ManifoldConfig) Validate() error {
 	}
 	if config.NewWorker == nil {
 		return errors.NotValidf("nil NewWorker")
-	}
-	if config.NewTarget == nil {
-		return errors.NotValidf("nil NewTarget")
 	}
 	return nil
 }
@@ -72,34 +58,17 @@ func (config ManifoldConfig) start(context dependency.Context) (worker.Worker, e
 		return nil, errors.Trace(err)
 	}
 
-	var stTracker workerstate.StateTracker
-	if err := context.Get(config.StateName, &stTracker); err != nil {
-		return nil, errors.Trace(err)
-	}
-	statePool, err := stTracker.Use()
-	if err != nil {
-		return nil, errors.Trace(err)
-	}
-
-	st, err := statePool.SystemState()
-	if err != nil {
-		return nil, errors.Trace(err)
-	}
-
-	notifyTarget := config.NewTarget(st, config.Logger)
 	w, err := config.NewWorker(Config{
 		Raft:                 r,
 		Hub:                  hub,
 		Logger:               config.Logger,
 		Topic:                config.RequestTopic,
-		Target:               notifyTarget,
 		PrometheusRegisterer: config.PrometheusRegisterer,
 	})
 	if err != nil {
-		_ = stTracker.Done()
 		return nil, errors.Trace(err)
 	}
-	return common.NewCleanupWorker(w, func() { _ = stTracker.Done() }), nil
+	return w, nil
 }
 
 // Manifold builds a dependency.Manifold for running a raftforwarder
@@ -108,14 +77,8 @@ func Manifold(config ManifoldConfig) dependency.Manifold {
 	return dependency.Manifold{
 		Inputs: []string{
 			config.RaftName,
-			config.StateName,
 			config.CentralHubName,
 		},
 		Start: config.start,
 	}
-}
-
-// NewTarget is a shim to construct a raftlease.NotifyTarget for testability.
-func NewTarget(st *state.State, logger raftleasestore.Logger) raftlease.NotifyTarget {
-	return st.LeaseNotifyTarget(logger)
 }
