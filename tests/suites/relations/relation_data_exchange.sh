69beeee0fc6d908bed82d24a84f0e19a630699a3
Vitaly Antonenko <vitaly.antonenko@canonical.com>
2022-10-18 17:43:13 +0300

    Added echo comments for test stages.

--- tests/suites/relations/relation_data_exchange.sh
+++ tests/suites/relations/relation_data_exchange.sh
@@ -6,7 +6,7 @@ run_relation_data_exchange() {
 
 	ensure "${model_name}" "${file}"
 
-	# Deploy 2 wordpress instances and one mysql instance
+	echo "Deploy 2 wordpress instances and one mysql instance"
 	juju deploy wordpress -n 2 --force --series bionic
 	wait_for "wordpress" "$(idle_condition "wordpress" 0 0)"
 	wait_for "wordpress" "$(idle_condition "wordpress" 0 1)"
@@ -14,15 +14,14 @@ run_relation_data_exchange() {
 	juju deploy mysql --channel=edge --force --series focal
 	wait_for "mysql" "$(idle_condition "mysql")"
 
-	# Establish relation
+	echo "Establish relation"
 	juju relate wordpress mysql
 
-	# Get the leader unit name
+	echo "Get the leader unit name"
 	leader_wordpress_unit=$(juju status wordpress --format json | jq -r  ".applications.wordpress.units | to_entries[] | select(.value.leader==true) | .key")
 	non_leader_wordpress_unit=$(juju status wordpress --format json | jq -r  ".applications.wordpress.units | to_entries[] | select(.value.leader!=true) | .key")
 
-	# Block until the relation is joined; otherwise, the relation-set commands
-	# below will fail
+	echo "Block until the relation is joined; otherwise, the relation-set commands below will fail"
 	attempt=0
 	while true; do
 		got=$(juju exec --unit "${leader_wordpress_unit}" 'relation-get --app -r db:2 origin wordpress' || echo 'NOT FOUND')
@@ -52,18 +51,17 @@ run_relation_data_exchange() {
 		sleep 1
 	done
 
+	echo "Exchange relation data"
 	juju exec --unit 'mysql/0' 'relation-set --app -r db:2 origin=mysql'
 
-	# As the leader units, set some *application* data for both sides of a
-	# non-peer relation
+	echo "As the leader units, set some *application* data for both sides of a non-peer relation"
 	juju exec --unit "${leader_wordpress_unit}" 'relation-set --app -r db:2 origin=wordpress'
 	juju exec --unit 'mysql/0' 'relation-set --app -r db:2 origin=mysql'
 
-	# As the leader wordpress unit, also set *application* data for a peer relation
+	echo "As the leader wordpress unit, also set *application* data for a peer relation"
 	juju exec --unit "${leader_wordpress_unit}" 'relation-set --app -r loadbalancer:0 visible=to-peers'
 
-	# Check 1: ensure that leaders can read the application databag for their
-	# own application (LP1854348)
+	echo "Check 1: ensure that leaders can read the application databag for their own application (LP1854348)"
 	got=$(juju exec --unit "${leader_wordpress_unit}" 'relation-get --app -r db:2 origin wordpress')
 	if [ "${got}" != "wordpress" ]; then
 		# shellcheck disable=SC2046
@@ -77,8 +75,7 @@ run_relation_data_exchange() {
 		exit 1
 	fi
 
-	# Check 2: ensure that any unit can read its own application databag for
-	# *peer* relations LP1865229)
+	echo "Check 2: ensure that any unit can read its own application databag for *peer* relations LP1865229)"
 	got=$(juju exec --unit "${leader_wordpress_unit}" 'relation-get --app -r loadbalancer:0 visible wordpress')
 	if [ "${got}" != "to-peers" ]; then
 		# shellcheck disable=SC2046
@@ -92,8 +89,7 @@ run_relation_data_exchange() {
 		exit 1
 	fi
 
-	# Check 3: ensure that non-leader units are not allowed to read their own
-	# application databag for non-peer relations
+#	echo "Check 3: ensure that non-leader units are not allowed to read their own application databag for non-peer relations"
 #	got=$(juju exec --unit "${non_leader_wordpress_unit}" 'relation-get --app -r db:2 origin wordpress')
 #	if [ "${got}" != "ERROR permission denied" ]; then
 #		# shellcheck disable=SC2046
