4b34d505b748fcad6242642e0a5dfa58744e1701
Heather Lanigan <heather.lanigan@canonical.com>
2022-10-19 13:27:08 -0400

    return fix for refresh test lost in merge.
    Otherwise the test fail inappropriately. See note for more detail.

--- tests/suites/deploy/deploy_revision.sh
+++ tests/suites/deploy/deploy_revision.sh
@@ -79,7 +79,27 @@ run_deploy_revision_refresh() {
 	juju deploy juju-qa-test --revision 23 --channel latest/edge
 	wait_for "juju-qa-test" "$(charm_rev "juju-qa-test" 23)"
 
-	juju refresh juju-qa-test
+  # NOTE:
+  # The following loop is specific to juju 3.0+ due to
+  # async charm download and should NOT be removed in
+  # a merge from 2.9.
+	attempt=0
+	while true; do
+		# Ensure that refresh gets the revision from the channel
+		# listed at deploy.
+		# revision 15 is in channel latest/edge
+		OUT=$(juju refresh juju-qa-test 2>&1 || true)
+		if echo "${OUT}" | grep -E -q "Added"; then
+			break
+		fi
+		attempt=$((attempt + 1))
+		if [ $attempt -eq 10 ]; then
+			# shellcheck disable=SC2046
+			echo $(red "timeout: waiting for charm download to complete 50sec")
+			exit 5
+		fi
+		sleep 5
+	done
 
 	# revision 21 is in channel latest/edge
 	wait_for "juju-qa-test" "$(charm_rev "juju-qa-test" 21)"
