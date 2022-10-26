a5be096043b3cf4433d2ded583bf285ec3b783e6
Vitaly Antonenko <vitaly.antonenko@canonical.com>
2022-10-26 17:44:26 +0300

    Switched to <app-name>/leader. Added env_var for relation ids. Changed the order of 'juju relate' and 'wait_for'.

--- tests/suites/relations/relation_data_exchange.sh
+++ tests/suites/relations/relation_data_exchange.sh
@@ -11,21 +11,23 @@ run_relation_data_exchange() {
 	juju deploy wordpress -n 2 --force --series bionic
 	# mysql charm does not have stable channel, so we use edge channel
 	juju deploy mysql --channel=edge --force --series focal
+
+	echo "Establish relation"
+  	juju relate wordpress mysql
+
 	wait_for "wordpress" "$(idle_condition "wordpress" 1 0)"
 	wait_for "wordpress" "$(idle_condition "wordpress" 1 1)"
 	wait_for "mysql" "$(idle_condition "mysql")"
 
-	echo "Establish relation"
-	juju relate wordpress mysql
-
 	echo "Get the leader unit name"
-	leader_wordpress_unit=$(juju status wordpress --format json | jq -r ".applications.wordpress.units | to_entries[] | select(.value.leader==true) | .key")
 	non_leader_wordpress_unit=$(juju status wordpress --format json | jq -r ".applications.wordpress.units | to_entries[] | select(.value.leader!=true) | .key")
+    wordpress_relation_id=$(juju exec --unit "wordpress/leader" 'relation-ids db')
+    mysql_relation_id=$(juju exec --unit "mysql/leader" 'relation-ids mysql')
 
 	echo "Block until the relation is joined; otherwise, the relation-set commands below will fail"
 	attempt=0
 	while true; do
-		got=$(juju exec --unit "${leader_wordpress_unit}" 'relation-get --app -r db:2 origin wordpress' || echo 'NOT FOUND')
+		got=$(juju exec --unit "wordpress/leader" "relation-get --app -r ${wordpress_relation_id} origin wordpress" || echo 'NOT FOUND')
 		if [ "${got}" != "NOT FOUND" ]; then
 			break
 		fi
@@ -39,7 +41,7 @@ run_relation_data_exchange() {
 	done
 	attempt=0
 	while true; do
-		got=$(juju exec --unit 'mysql/0' 'relation-get --app -r db:2 origin mysql' || echo 'NOT FOUND')
+		got=$(juju exec --unit 'mysql/0' "relation-get --app -r ${wordpress_relation_id} origin mysql" || echo 'NOT FOUND')
 		if [ "${got}" != "NOT FOUND" ]; then
 			break
 		fi
@@ -53,23 +55,23 @@ run_relation_data_exchange() {
 	done
 
 	echo "Exchange relation data"
-	juju exec --unit 'mysql/0' 'relation-set --app -r db:2 origin=mysql'
+	juju exec --unit 'mysql/0' "relation-set --app -r ${mysql_relation_id} origin=mysql"
 	# As the leader units, set some *application* data for both sides of a non-peer relation
-	juju exec --unit "${leader_wordpress_unit}" 'relation-set --app -r db:2 origin=wordpress'
-	juju exec --unit 'mysql/0' 'relation-set --app -r db:2 origin=mysql'
+	juju exec --unit "wordpress/leader" "relation-set --app -r ${wordpress_relation_id} origin=wordpress"
+	juju exec --unit 'mysql/0' "relation-set --app -r ${mysql_relation_id} origin=mysql"
 	# As the leader wordpress unit, also set *application* data for a peer relation
-	juju exec --unit "${leader_wordpress_unit}" 'relation-set --app -r loadbalancer:0 visible=to-peers'
+	juju exec --unit "wordpress/leader" 'relation-set --app -r loadbalancer:0 visible=to-peers'
 
 	echo "Check 1: ensure that leaders can read the application databag for their own application"
-	juju exec --unit "${leader_wordpress_unit}" 'relation-get --app -r db:2 origin wordpress' | check "wordpress"
-	juju exec --unit 'mysql/0' 'relation-get --app -r db:2 origin mysql' | check "mysql"
+	juju exec --unit "wordpress/leader" "relation-get --app -r ${wordpress_relation_id} origin wordpress" | check "wordpress"
+	juju exec --unit 'mysql/0' "relation-get --app -r ${mysql_relation_id} origin mysql" | check "mysql"
 
 	echo "Check 2: ensure that any unit can read its own application databag for *peer* relations"
-	juju exec --unit "${leader_wordpress_unit}" 'relation-get --app -r loadbalancer:0 visible wordpress' | check "to-peers"
+	juju exec --unit "wordpress/leader" 'relation-get --app -r loadbalancer:0 visible wordpress' | check "to-peers"
 	juju exec --unit "${non_leader_wordpress_unit}" 'relation-get --app -r loadbalancer:0 visible wordpress' | check "to-peers"
 
 	echo "Check 3: ensure that non-leader units are not allowed to read their own application databag for non-peer relations"
-	juju exec --unit "${non_leader_wordpress_unit}" 'relation-get --app -r db:2 origin wordpress' 2>&1 || echo "PERMISSION DENIED" | check "PERMISSION DENIED"
+	juju exec --unit "${non_leader_wordpress_unit}" "relation-get --app -r ${wordpress_relation_id} origin wordpress" 2>&1 || echo "PERMISSION DENIED" | check "PERMISSION DENIED"
 
 	destroy_model "${model_name}"
 }
