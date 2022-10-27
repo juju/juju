c0a9a21c2cae0d84377841e1bf0334d94e990ede
Jack Shaw <jack.shaw@canonical.com>
2022-10-27 14:58:59 +0100

    Fix bugs in manual spaces integrations tests

--- tests/suites/manual/deploy_manual_aws.sh
+++ tests/suites/manual/deploy_manual_aws.sh
@@ -85,7 +85,7 @@ run_deploy_manual_aws() {
 
 	local addr_c addr_m1 addr_m2
 
-	echo "==> Creating machines in aws"
+	echo "===> Creating machines in aws"
 	launch_and_wait_addr_ec2 "${name}" "${controller}" "${instance_image_id}" "${subnet_id}" "${sg_id}" addr_c
 	launch_and_wait_addr_ec2 "${name}" "${model1}" "${instance_image_id}" "${subnet_id}" "${sg_id}" addr_m1
 	launch_and_wait_addr_ec2 "${name}" "${model2}" "${instance_image_id}" "${subnet_id}" "${sg_id}" addr_m2
