f2ab2c6f1c27a9a20fb679cd10fd9f6d9ce45b24
Vitaly Antonenko <vitaly.antonenko@canonical.com>
2022-11-08 12:40:26 +0300

    Changed magma channel on beta.

--- tests/suites/magma/magma.sh
+++ tests/suites/magma/magma.sh
@@ -11,9 +11,10 @@ run_deploy_magma() {
 
 	echo "Deploy Magma project"
 	overlay_path="./tests/suites/magma/overlay/overlay.yaml"
-	juju deploy magma-orc8r --overlay "${overlay_path}" --trust --channel=edge
+	juju deploy magma-orc8r --overlay "${overlay_path}" --trust --channel=beta
 
 	echo "Check all Magma project components have ACTIVE status"
+	# Magical number 34 means that all 34 apps from the magma bundle has the same status
 	wait_for 34 '[.applications[] | select(."application-status".current == "active")] | length' 3600
 
 	echo "Get cert file and request password for it"
