ab283c43c0b26e7036560ff7adf53b5be733ebdc
Kelvin Liu <kelvin.liu@canonical.com>
2022-11-15 20:10:57 +1100

    Add tests for caas Proxier.Insecure();

--- apiserver/package_test.go
+++ apiserver/package_test.go
@@ -10,8 +10,8 @@ import (
 )
 
 //go:generate go run github.com/golang/mock/mockgen -package mocks -destination mocks/deltatranslater_mock.go github.com/juju/juju/apiserver DeltaTranslater
-//go:generate go run github.com/golang/mock/mockgen -package apiserver -destination registration_environs_mock.go github.com/juju/juju/environs ConnectorInfo
-//go:generate go run github.com/golang/mock/mockgen -package apiserver -destination registration_proxy_mock.go github.com/juju/juju/proxy Proxier
+//go:generate go run github.com/golang/mock/mockgen -package apiserver_test -destination registration_environs_mock_test.go github.com/juju/juju/environs ConnectorInfo
+//go:generate go run github.com/golang/mock/mockgen -package apiserver_test -destination registration_proxy_mock_test.go github.com/juju/juju/proxy Proxier
 
 func TestPackage(t *testing.T) {
 	coretesting.MgoTestPackage(t)
