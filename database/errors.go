82d7fad548fb2195dc419e55a7402e6db5721eeb
Joseph Phillips <joe.phillips@canonical.com>
2022-12-16 17:28:57 +0100

    Adds a fallback for unique constraint violation errors by interrogating the error string.
    This is intended as a temporary measure which we work out how to ensure
    detection with Dqlite codes.

--- database/errors.go
+++ database/errors.go
@@ -11,10 +11,23 @@ import (
 // IsErrConstraintUnique returns true if the input error was
 // returned by SQLite due to violation of a unique constraint.
 func IsErrConstraintUnique(err error) bool {
+	if err == nil {
+		return false
+	}
+
 	var sqliteErr sqlite3.Error
 	if errors.As(err, &sqliteErr) && sqliteErr.ExtendedCode == sqlite3.ErrConstraintUnique {
 		return true
 	}
+
+	// TODO (manadart 2022-12-16): The logic above works in unit tests using an
+	// in-memory SQLite DB, but appears to fail when running with Dqlite.
+	// Extended error codes can be enabled via PRAGMA, but we need to
+	// investigate further.
+	if strings.Contains(strings.ToLower(err.Error()), "unique constraint failed") {
+		return true
+	}
+
 	return false
 }
 
