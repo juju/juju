d2d03a331c7551ed3a4ce95ef1aba3a1b7c58728
Heather Lanigan <heather.lanigan@canonical.com>
2023-02-06 15:03:04 -0500

    Add two new tests related to refresh without revision change.
    Test cover scenarios in LP1988587, where refresh failed due to a lack of new
    available charm revision, however the switch or channel flags were used.

--- tests/suites/refresh/switch.sh
+++ tests/suites/refresh/switch.sh
@@ -19,10 +19,43 @@ run_refresh_switch_cs_to_ch() {
 	# shellcheck disable=SC2059
 	printf "${OUT}\n"
 
-	# Added local charm "ubuntu", revision 2, to the model
+	# format: Added local charm "ubuntu", revision 2, to the model
 	revision=$(echo "${OUT}" | awk 'BEGIN{FS=","} {print $2}' | awk 'BEGIN{FS=" "} {print $2}')
 
 	wait_for "ubuntu" "$(charm_rev "ubuntu" "${revision}")"
+	wait_for "ubuntu" "$(idle_condition "ubuntu")"
+
+	destroy_model "${model_name}"
+}
+
+run_refresh_switch_cs_to_ch_no_new_revision() {
+	# Test juju refresh from a charm store charm to a charm hub charm, with no new
+	# charm revision.
+	echo
+
+	model_name="test-refresh-switch-ch"
+	file="${TEST_DIR}/${model_name}.log"
+
+	ensure "${model_name}" "${file}"
+
+	OUT=$(juju deploy cs:ubuntu2 >&1 || true)
+	# shellcheck disable=SC2059
+	printf "${OUT}\n"
+	# format: Added local charm "ubuntu", revision 2, to the model
+	cs_revision=$(echo "${OUT}" | awk 'BEGIN{FS=","} {print $2}' | awk 'BEGIN{FS=" "} {print $2}')
+
+	wait_for "ubuntu" "$(idle_condition "ubuntu")"
+
+	OUT=$(juju refresh ubuntu --switch ch:ubuntu 2>&1 || true)
+	if echo "${OUT}" | grep -E -vq "Added charm-hub charm"; then
+		# shellcheck disable=SC2046
+		echo $(red "failed refreshing charm: ${OUT}")
+		exit 5
+	fi
+	# shellcheck disable=SC2059
+	printf "${OUT}\n"
+
+	wait_for "ubuntu" "$(charm_rev "ubuntu" "${cs_revision}")"
 	wait_for "ubuntu" "$(idle_condition "ubuntu")"
 
 	destroy_model "${model_name}"
@@ -49,7 +82,7 @@ run_refresh_switch_cs_to_ch_channel() {
 	# shellcheck disable=SC2059
 	printf "${OUT}\n"
 
-	# Added local charm "ubuntu", revision 2, to the model
+	# format: Added local charm "ubuntu", revision 2, to the model
 	revision=$(echo "${OUT}" | awk 'BEGIN{FS=","} {print $2}' | awk 'BEGIN{FS=" "} {print $2}')
 
 	wait_for "ubuntu" "$(charm_rev "ubuntu" "${revision}")"
@@ -82,7 +115,7 @@ run_refresh_switch_local_to_ch_channel() {
 	# shellcheck disable=SC2059
 	printf "${OUT}\n"
 
-	# Added local charm "ubuntu", revision 2, to the model
+	# format: Added local charm "ubuntu", revision 2, to the model
 	revision=$(echo "${OUT}" | awk 'BEGIN{FS=","} {print $2}' | awk 'BEGIN{FS=" "} {print $2}')
 
 	wait_for "ubuntu" "$(charm_rev "ubuntu" "${revision}")"
@@ -108,12 +141,43 @@ run_refresh_switch_channel() {
 	# shellcheck disable=SC2059
 	printf "${OUT}\n"
 
-	# Added local charm "ubuntu", revision 2, to the model
+	# format: Added local charm "ubuntu", revision 2, to the model
 	revision=$(echo "${OUT}" | awk 'BEGIN{FS=","} {print $2}' | awk 'BEGIN{FS=" "} {print $2}')
 
 	wait_for "juju-qa-test" "$(charm_rev "juju-qa-test" "${revision}")"
 	wait_for "juju-qa-test" "$(charm_channel "juju-qa-test" "2.0/edge")"
 	wait_for "juju-qa-test" "$(idle_condition "juju-qa-test")"
+
+	destroy_model "${model_name}"
+}
+
+run_refresh_switch_channel_no_new_revision() {
+	# Test juju refresh switching from one channel to another, with no new
+	# charm revision.
+	echo
+
+	model_name="test-refresh-switch-channel-no-new-revision"
+	file="${TEST_DIR}/${model_name}.log"
+
+	ensure "${model_name}" "${file}"
+
+	juju deploy ubuntu
+	wait_for "ubuntu" "$(idle_condition "ubuntu")"
+
+	OUT=$(juju refresh ubuntu --channel edge 2>&1 || true)
+	# shellcheck disable=SC2059
+	printf "${OUT}\n"
+
+	if echo "${OUT}" | grep -E -vq "all future refreshes will now use channel"; then
+		# shellcheck disable=SC2046
+		echo $(red "failed refreshing charm: ${OUT}")
+		exit 5
+	fi
+	# shellcheck disable=SC2059
+	printf "${OUT}\n"
+
+	wait_for "ubuntu" "$(charm_channel "ubuntu" "edge")"
+	wait_for "ubuntu" "$(idle_condition "ubuntu")"
 
 	destroy_model "${model_name}"
 }
@@ -130,8 +194,10 @@ test_switch() {
 		cd .. || exit
 
 		run "run_refresh_switch_cs_to_ch"
+		run "run_refresh_switch_cs_to_ch_no_new_revision"
 		run "run_refresh_switch_cs_to_ch_channel"
 		run "run_refresh_switch_local_to_ch_channel"
 		run "run_refresh_switch_channel"
+		run "run_refresh_switch_channel_no_new_revision"
 	)
 }
