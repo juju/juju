ca281368092a9411756f8a3855e0022d76b40c56
Joseph Phillips <joe.phillips@canonical.com>
2023-02-15 14:20:42 +0100

    Fixes agent test suite for renaming of OptionFactory to NodeManager.

--- cmd/jujud/agent/agenttest/agent.go
+++ cmd/jujud/agent/agenttest/agent.go
@@ -212,7 +212,7 @@ func (s *AgentSuite) PrimeStateAgentVersion(c *gc.C, tag names.Tag, password str
 	conf := s.WriteStateAgentConfig(c, tag, password, vers, model.ModelTag())
 	s.primeAPIHostPorts(c)
 
-	err = database.BootstrapDqlite(context.TODO(), database.NewOptionFactory(conf, logger), logger, s.InitialDBOps...)
+	err = database.BootstrapDqlite(context.TODO(), database.NewNodeManager(conf, logger), logger, s.InitialDBOps...)
 	c.Assert(err, jc.ErrorIsNil)
 
 	return conf, agentTools
