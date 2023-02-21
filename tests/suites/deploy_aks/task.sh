8676dae47280bc7fd0cc8cd0ecbf66642e540d94
Vitaly Antonenko <vitaly.antonenko@canonical.com>
2023-02-21 13:47:29 +0300

    Added TODO comment about AKS provider.

--- tests/suites/deploy_aks/task.sh
+++ tests/suites/deploy_aks/task.sh
@@ -9,8 +9,9 @@ test_deploy_aks() {
 	echo "==> Checking for dependencies"
 	check_dependencies juju
 
+  # TODO(anvial): we need to add separate provider for such tests (for example, "aks") and move all this code to
+  # create/delete to code from here.
 	resource_group_name="test-aks-resource-group-$(rnd_str)"
-	echo "rname = ${resource_group_name}"
 	az group create -l eastus -n "${resource_group_name}"
 	az aks create -g "${resource_group_name}" -n aks-cluster --generate-ssh-keys
 	juju add-k8s --aks --client --resource-group "${resource_group_name}" --storage test-aks-storage --cluster-name aks-cluster aks-k8s-cloud
