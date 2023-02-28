6baaeba490a7d76b1477d35fb68263fd7ea2f53b
Simon Richardson <stickupkid@gmail.com>
2023-02-28 11:23:51 +0000

    Rename receiver type to make code more readable

--- worker/changestream/eventqueue/eventqueue.go
+++ worker/changestream/eventqueue/eventqueue.go
@@ -81,28 +81,28 @@ func New(stream Stream, logger Logger) *EventQueue {
 }
 
 // Subscribe creates a new subscription to the event queue.
-func (s *EventQueue) Subscribe(opts ...changestream.SubscriptionOption) (changestream.Subscription, error) {
+func (q *EventQueue) Subscribe(opts ...changestream.SubscriptionOption) (changestream.Subscription, error) {
 	if len(opts) == 0 {
 		return nil, errors.Errorf("no subscription options specified")
 	}
 
-	s.mutex.Lock()
-	defer s.mutex.Unlock()
+	q.mutex.Lock()
+	defer q.mutex.Unlock()
 
 	// Create a new subscription and assign a unique ID to it.
-	s.subscriptionsCount++
+	q.subscriptionsCount++
 	sub := &subscription{
-		id:           s.subscriptionsCount,
+		id:           q.subscriptionsCount,
 		changes:      make(chan changestream.ChangeEvent),
 		topics:       make(map[string]struct{}),
-		unsubscriber: func() { s.unsubscribe(s.subscriptionsCount) },
+		unsubscriber: func() { q.unsubscribe(q.subscriptionsCount) },
 	}
-	s.subscriptions[sub.id] = sub
+	q.subscriptions[sub.id] = sub
 
 	// Register filters to route changes matching the subscription criteria to the newly crated subscription.
 	for _, opt := range opts {
 		namespace := opt.Namespace()
-		s.subscriptionsByNS[namespace] = append(s.subscriptionsByNS[namespace], &eventFilter{
+		q.subscriptionsByNS[namespace] = append(q.subscriptionsByNS[namespace], &eventFilter{
 			subscriptionID: sub.id,
 			changeMask:     opt.ChangeMask(),
 			filter:         opt.Filter(),
@@ -114,49 +114,49 @@ func (s *EventQueue) Subscribe(opts ...changestream.SubscriptionOption) (changes
 }
 
 // Kill stops the event queue.
-func (s *EventQueue) Kill() {
-	s.tomb.Kill(nil)
+func (q *EventQueue) Kill() {
+	q.tomb.Kill(nil)
 }
 
 // Wait waits for the event queue to stop.
-func (s *EventQueue) Wait() error {
-	return s.tomb.Wait()
+func (q *EventQueue) Wait() error {
+	return q.tomb.Wait()
 }
 
-func (s *EventQueue) unsubscribe(subscriptionID int) {
-	s.mutex.Lock()
-	defer s.mutex.Unlock()
+func (q *EventQueue) unsubscribe(subscriptionID int) {
+	q.mutex.Lock()
+	defer q.mutex.Unlock()
 
-	sub, found := s.subscriptions[subscriptionID]
+	sub, found := q.subscriptions[subscriptionID]
 	if !found {
 		return
 	}
 
 	for topic := range sub.topics {
 		var updatedFilters []*eventFilter
-		for _, filter := range s.subscriptionsByNS[topic] {
+		for _, filter := range q.subscriptionsByNS[topic] {
 			if filter.subscriptionID == subscriptionID {
 				continue
 			}
 			updatedFilters = append(updatedFilters, filter)
 		}
-		s.subscriptionsByNS[topic] = updatedFilters
+		q.subscriptionsByNS[topic] = updatedFilters
 	}
 
-	delete(s.subscriptions, subscriptionID)
+	delete(q.subscriptions, subscriptionID)
 
 	// Close the subscription channel after a dispatch. If a unsubscription
-	// happens during a dispatch, then it clauses the event queue to potentially
+	// happens during a dispatch, then it causes the event queue to potentially
 	// panic. Removal of the subscription from the queue is synchronous, so
 	// the subscription can no longer be used when another dispatch occurs. The
 	// closing of the channel then becomes asynchronous and can be closed during
 	// the next selection.
-	s.tomb.Go(func() error {
+	q.tomb.Go(func() error {
 		// Ensure that we give up if the tomb is dying.
 		select {
-		case <-s.tomb.Dying():
+		case <-q.tomb.Dying():
 			return tomb.ErrDying
-		case s.actions <- func() {
+		case q.actions <- func() {
 			close(sub.changes)
 		}:
 		}
@@ -164,15 +164,15 @@ func (s *EventQueue) unsubscribe(subscriptionID int) {
 	})
 }
 
-func (s *EventQueue) loop() error {
+func (q *EventQueue) loop() error {
 	defer func() {
-		s.mutex.Lock()
-		defer s.mutex.Unlock()
+		q.mutex.Lock()
+		defer q.mutex.Unlock()
 
-		for _, sub := range s.subscriptions {
+		for _, sub := range q.subscriptions {
 			close(sub.changes)
 		}
-		s.subscriptions = make(map[int]*subscription)
+		q.subscriptions = make(map[int]*subscription)
 	}()
 
 	for {
@@ -181,63 +181,63 @@ func (s *EventQueue) loop() error {
 			ok bool
 		)
 		select {
-		case <-s.tomb.Dying():
+		case <-q.tomb.Dying():
 			return tomb.ErrDying
-		case action := <-s.actions:
+		case action := <-q.actions:
 			action()
 			continue
-		case ch, ok = <-s.stream.Changes():
+		case ch, ok = <-q.stream.Changes():
 			// If the stream is closed, we expect that a new worker will come
 			// again using the change stream worker infrastructure. In this case
 			// just ignore and close out.
 			if !ok {
-				s.logger.Infof("change stream change channel is closed")
+				q.logger.Infof("change stream change channel is closed")
 				return nil
 			}
 		}
 
 		// The dispatch is done in two stages to avoid holding the lock for the
 		// duration of the dispatch.
-		subs := s.gatherSubscriptions(ch)
-		if err := s.dispatchChange(subs, ch); err != nil {
+		subs := q.gatherSubscriptions(ch)
+		if err := q.dispatchChange(subs, ch); err != nil {
 			return errors.Trace(err)
 		}
 	}
 }
 
-func (s *EventQueue) gatherSubscriptions(ch changestream.ChangeEvent) []*subscription {
-	s.mutex.Lock()
-	defer s.mutex.Unlock()
+func (q *EventQueue) gatherSubscriptions(ch changestream.ChangeEvent) []*subscription {
+	q.mutex.Lock()
+	defer q.mutex.Unlock()
 
 	var subs []*subscription
-	for _, subOpt := range s.subscriptionsByNS[ch.Namespace()] {
+	for _, subOpt := range q.subscriptionsByNS[ch.Namespace()] {
 		if (ch.Type() & subOpt.changeMask) == 0 {
-			if s.logger.IsTraceEnabled() {
-				s.logger.Tracef("ignoring change: %v", ch)
+			if q.logger.IsTraceEnabled() {
+				q.logger.Tracef("ignoring change: %v", ch)
 			}
 			continue
 		}
 
 		if !subOpt.filter(ch) {
-			if s.logger.IsTraceEnabled() {
-				s.logger.Tracef("filtering out change: %v", ch)
+			if q.logger.IsTraceEnabled() {
+				q.logger.Tracef("filtering out change: %v", ch)
 			}
 			continue
 		}
 
-		if s.logger.IsTraceEnabled() {
-			s.logger.Tracef("dispatching change: %v", ch)
+		if q.logger.IsTraceEnabled() {
+			q.logger.Tracef("dispatching change: %v", ch)
 		}
 
-		subs = append(subs, s.subscriptions[subOpt.subscriptionID])
+		subs = append(subs, q.subscriptions[subOpt.subscriptionID])
 	}
 	return subs
 }
 
-func (s *EventQueue) dispatchChange(subs []*subscription, ch changestream.ChangeEvent) error {
+func (q *EventQueue) dispatchChange(subs []*subscription, ch changestream.ChangeEvent) error {
 	for _, sub := range subs {
 		select {
-		case <-s.tomb.Dying():
+		case <-q.tomb.Dying():
 			return tomb.ErrDying
 		case sub.changes <- ch:
 			// pushed change.
