396de6fb5b178c354f9c444d71f34e2d28412717
Simon Richardson <stickupkid@gmail.com>
2023-03-20 20:09:48 +0000

    Ensure we symlink on post install
    The following just ensures that we symlink the first time around on post
    install.

--- scripts/dqlite/scripts/musl/install-if-missing.sh
+++ scripts/dqlite/scripts/musl/install-if-missing.sh
@@ -4,20 +4,12 @@ set -e
 
 source "$(dirname $0)/musl-install.sh"
 
-PATH=${PATH}:${MUSL_BIN_PATH} which musl-gcc >/dev/null || { echo "Installing required musl dependencies"; install; exit 0; }
+PATH=${MUSL_BIN_PATH}:${PATH} which musl-gcc >/dev/null || { echo "Installing required musl dependencies"; install; exit 0; }
 
 echo "musl-gcc already installed"
 
 if [ $(is_darwin) = true ] && [ ! -f "${MUSL_LOCAL_PATH}/output/bin/musl-gcc" ]; then
-    echo "Symlinking darwin musl-gcc to ${BUILD_ARCH}"
-    mkdir -p ${MUSL_LOCAL_PATH}/output/bin || { echo "Failed to create ${MUSL_LOCAL_PATH}/output/bin"; exit 1; }
-    BREW_PATH=$(brew --prefix)
-    BREW_BIN_PATH=${BREW_PATH}/bin
-    case ${BUILD_ARCH} in
-		amd64) ln -s "${BREW_BIN_PATH}/x86_64-linux-musl-gcc" ${MUSL_LOCAL_PATH}/output/bin/musl-gcc || { echo "Failed to link musl-gcc"; exit 1; } ;;
-		arm64) ln -s "${BREW_BIN_PATH}/aarch64-linux-musl-gcc" ${MUSL_LOCAL_PATH}/output/bin/musl-gcc || { echo "Failed to link musl-gcc"; exit 1; } ;;
-		*) { echo "Unsupported arch ${BUILD_ARCH}."; exit 1; } ;;
-	esac
+    post_musl_install_cross_darwin
     exit 0
 fi
 
