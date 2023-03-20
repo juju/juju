7bc22ed66b08f38df5f45ef701888f8460191a19
Simon Richardson <stickupkid@gmail.com>
2023-03-20 09:27:31 +0000

    Update comment about return TrackedDB
    The DBGetter now returns a TrackedDB and not a sql.DB. The idea being
    that you can late bind to the sql.DB in case there is a transient fault.

--- core/database/interface.go
+++ core/database/interface.go
@@ -3,12 +3,13 @@
 
 package database
 
-// DBGetter describes the ability to supply a sql.DB
-// reference for a particular database.
+// DBGetter describes the ability to supply a TrackedDB reference for a
+// particular database.
 type DBGetter interface {
-	// GetDB returns a sql.DB reference for the dqlite-backed database that
+	// GetDB returns a TrackedDB reference for the dqlite-backed database that
 	// contains the data for the specified namespace.
-	// A NotFound error is returned if the worker is unaware of the requested DB.
+	// A NotFound error is returned if the worker is unaware of the requested
+	// DB.
 	GetDB(namespace string) (TrackedDB, error)
 }
 
