438d1e36ef140d9d02d471a67b6c81c7bfba8894
Teodora Mihoc <te.mihoc@gmail.com>
2023-03-29 21:01:39 +0200

    Moved Examples and See also to fields inside Info for the actions command.

--- cmd/juju/commands/enableha.go
+++ cmd/juju/commands/enableha.go
@@ -72,24 +72,28 @@ that the specified number of controller machines are used to make up the
 controller.
 
 An odd number of controllers is required.
+`
 
-Examples:
-    # Ensure that the controller is still in highly available mode. If
-    # there is only 1 controller running, this will ensure there
-    # are 3 running. If you have previously requested more than 3,
-    # then that number will be ensured.
+const enableHAExamples = `
+Ensure that the controller is still in highly available mode. If there is only 1 controller running, this will ensure there
+are 3 running. If you have previously requested more than 3,
+then that number will be ensured.
+
     juju enable-ha
 
-    # Ensure that 5 controllers are available.
+Ensure that 5 controllers are available:
+
     juju enable-ha -n 5 
 
-    # Ensure that 7 controllers are available, with newly created
-    # controller machines having at least 8GB RAM.
+Ensure that 7 controllers are available, with newly created
+controller machines having at least 8GB RAM.
+
     juju enable-ha -n 7 --constraints mem=8G
 
-    # Ensure that 7 controllers are available, with machines server1 and
-    # server2 used first, and if necessary, newly created controller
-    # machines having at least 8GB RAM.
+Ensure that 7 controllers are available, with machines server1 and
+server2 used first, and if necessary, newly created controller
+machines having at least 8GB RAM.
+
     juju enable-ha -n 7 --to server1,server2 --constraints mem=8G
 `
 
@@ -135,9 +139,10 @@ func formatSimple(writer io.Writer, value interface{}) error {
 
 func (c *enableHACommand) Info() *cmd.Info {
 	return jujucmd.Info(&cmd.Info{
-		Name:    "enable-ha",
-		Purpose: "Ensure that sufficient controllers exist to provide redundancy.",
-		Doc:     enableHADoc,
+		Name:     "enable-ha",
+		Purpose:  "Ensure that sufficient controllers exist to provide redundancy.",
+		Doc:      enableHADoc,
+		Examples: enableHAExamples,
 	})
 }
 
