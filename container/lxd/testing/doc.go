a82abe37b92b4cab91704b68db3756b512510caf
Simon Richardson <stickupkid@gmail.com>
2023-04-11 10:47:02 +0100

    LXD dependency update
    The following brings in the latest lxd dependency update. This update
    should alleviate any potential issues we have when trying to build a
    jujud on a darwin system (macOS). This is split out from PR#15183. By
    splitting this out, it's possible to see what is required for updating
    just the LXD part.
    
    The part which will unlock the build process on mac is the following
    PR[1] I submitted prior to the LXD project.
    
    This will also lock sync the dqlite dependency for both juju and lxd for
    now. We'll have to find a way to untaggle that in the future.
    
      1. https://github.com/lxc/lxd/pull/11376

--- container/lxd/testing/doc.go
+++ container/lxd/testing/doc.go
@@ -4,5 +4,3 @@
 // Package testing contains the testing infrastructure to mock out the LXD API.
 // Run 'go generate' to regenerate the mock interfaces.
 package testing
-
-//go:generate go run github.com/golang/mock/mockgen -package testing -destination lxd_mock.go -write_package_comment=false github.com/lxc/lxd/client Operation,RemoteOperation,Server,ImageServer,InstanceServer
