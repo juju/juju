97c1586747bb1817189717f71cfa615f5392aef9
Juan Tirado <juan.tirado@canonical.com>
2023-04-21 12:59:15 +0200

    Fix redundant configure_multi_mic_netplan function.

--- tests/suites/spaces_ec2/util.sh
+++ tests/suites/spaces_ec2/util.sh
@@ -22,16 +22,6 @@ add_multi_nic_machine() {
 		--instance-id $(juju show-machine --format json | jq -r ".[\"machines\"] | .[\"${juju_machine_id}\"] | .[\"instance-id\"]")
 }
 
-# configure_multi_mic_netplan()
-#
-# Patch the netplan settings for the new interface, apply the new plan,
-# restart the machine agent and wait for juju to detect the new interface
-# before returning.
-configure_multi_nic_netplan() {
-	local juju_machine_id hotplug_iface
-	juju_machine_id=$1
-	hotplug_iface=$2
-}
 
 # configure_multi_mic_netplan()
 #
