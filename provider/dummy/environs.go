495c2d9e82109c69c7727363924362053c0f3b6d
Simon Richardson <stickupkid@gmail.com>
2023-05-09 20:18:23 +0100

    Force through tracked db in dummy provider
    For some reason in the feature tests we require the dummy provider to
    be more than a provider. We actually expect it to be also a jujud.
    This is stupid.
    
    Anyway, to get the feature tests running, I push through a trackedDB
    through the many levels of turd so that they pass.

--- provider/dummy/environs.go
+++ provider/dummy/environs.go
@@ -6,6 +6,7 @@ package dummy
 import (
 	stdcontext "context"
 	"crypto/tls"
+	"database/sql"
 	"fmt"
 	"net"
 	"net/http/httptest"
@@ -58,6 +59,8 @@ import (
 	"github.com/juju/juju/core/presence"
 	"github.com/juju/juju/core/series"
 	"github.com/juju/juju/core/status"
+	databaseschema "github.com/juju/juju/database/schema"
+	"github.com/juju/juju/database/txn"
 	"github.com/juju/juju/environs"
 	environscloudspec "github.com/juju/juju/environs/cloudspec"
 	"github.com/juju/juju/environs/config"
@@ -234,6 +237,7 @@ type environProvider struct {
 	apiPort                    int
 	controllerState            *environState
 	state                      map[string]*environState
+	db                         coredatabase.TrackedDB
 }
 
 // APIPort returns the random api port used by the given provider instance.
@@ -790,6 +794,14 @@ func (e *environ) PrepareForBootstrap(ctx environs.BootstrapContext, controllerN
 		dummy.controllerState = envState
 	}
 	dummy.state[e.modelUUID] = envState
+
+	// Create a new sqlite3 database for the environment.
+	db, err := e.newCleanDB()
+	if err != nil {
+		return errors.Trace(err)
+	}
+	dummy.db = db
+
 	return nil
 }
 
@@ -991,6 +1003,10 @@ func (e *environ) Bootstrap(ctx environs.BootstrapContext, callCtx context.Provi
 				return errors.Trace(err)
 			}
 
+			dummy.mu.Lock()
+			db := dummy.db
+			dummy.mu.Unlock()
+
 			estate.apiServer, err = apiserver.NewServer(apiserver.ServerConfig{
 				StatePool:           statePool,
 				Controller:          estate.controller,
@@ -1020,7 +1036,7 @@ func (e *environ) Bootstrap(ctx environs.BootstrapContext, callCtx context.Provi
 				},
 				MetricsCollector: apiserver.NewMetricsCollector(),
 				SysLogger:        noopSysLogger{},
-				DBGetter:         stubDBGetter{},
+				DBGetter:         stubDBGetter{db: db},
 			})
 			if err != nil {
 				panic(err)
@@ -1054,13 +1070,15 @@ type noopSysLogger struct{}
 
 func (noopSysLogger) Log([]corelogger.LogRecord) error { return nil }
 
-type stubDBGetter struct{}
+type stubDBGetter struct {
+	db coredatabase.TrackedDB
+}
 
 func (s stubDBGetter) GetDB(namespace string) (coredatabase.TrackedDB, error) {
 	if namespace != "controller" {
 		return nil, errors.Errorf(`expected a request for "controller" DB; got %q`, namespace)
 	}
-	return nil, nil
+	return s.db, nil
 }
 
 func leaseManager(controllerUUID string, st *state.State) (*lease.Manager, error) {
@@ -1164,6 +1182,9 @@ func (e *environ) DestroyController(ctx context.ProviderCallContext, controllerU
 	}
 	dummy.mu.Lock()
 	dummy.controllerState = nil
+	if dummy.db != nil {
+		dummy.db.(*trackedDB).db.Close()
+	}
 	dummy.mu.Unlock()
 	return nil
 }
@@ -2088,6 +2109,60 @@ func (e *environ) SetAgentStatus(agent string, status presence.Status) {
 		panic(err)
 	}
 	estate.presence.agent[agent] = status
+}
+
+// NewCleanDB returns a new sql.DB reference.
+func (e *environ) newCleanDB() (coredatabase.TrackedDB, error) {
+	dir, err := os.MkdirTemp("", "dummy")
+	if err != nil {
+		return nil, err
+	}
+
+	url := fmt.Sprintf("file:%s/db.sqlite3?_foreign_keys=1", dir)
+
+	db, err := sql.Open("sqlite3", url)
+	if err != nil {
+		return nil, err
+	}
+
+	tx, err := db.Begin()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, stmt := range databaseschema.ControllerDDL() {
+		_, err := tx.Exec(stmt)
+		if err != nil {
+			_ = tx.Rollback()
+			return nil, err
+		}
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return nil, err
+	}
+	return &trackedDB{db: db}, nil
+}
+
+var defaultTransactionRunner = txn.NewTransactionRunner()
+
+// trackedDB is used for testing purposes.
+type trackedDB struct {
+	db *sql.DB
+}
+
+func (t *trackedDB) Txn(ctx stdcontext.Context, fn func(stdcontext.Context, *sql.Tx) error) error {
+	return defaultTransactionRunner.Retry(ctx, func() error {
+		return errors.Trace(t.TxnNoRetry(ctx, fn))
+	})
+}
+func (t *trackedDB) TxnNoRetry(ctx stdcontext.Context, fn func(stdcontext.Context, *sql.Tx) error) error {
+	return errors.Trace(defaultTransactionRunner.Txn(ctx, t.db, fn))
+}
+
+func (t *trackedDB) Err() error {
+	return nil
 }
 
 // fakePresence returns alive for all agent alive requests.
