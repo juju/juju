b60ba6a9b6e8dffe6b068e13c6f8e944584ad5cd
Joseph Phillips <joe.phillips@canonical.com>
2023-05-10 15:17:49 +0200

    Adds mock generation for Subscription, and a matcher that can be used to check that subscription options match.
    We can't compare structs directly that have function pointer members.

--- core/watcher/eventqueue/package_test.go
+++ core/watcher/eventqueue/package_test.go
@@ -4,15 +4,18 @@
 package eventqueue
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/golang/mock/gomock"
 	gc "gopkg.in/check.v1"
 
+	"github.com/juju/juju/core/changestream"
 	dbtesting "github.com/juju/juju/database/testing"
 )
 
 //go:generate go run github.com/golang/mock/mockgen -package eventqueue -destination package_mock_test.go github.com/juju/juju/core/watcher/eventqueue EventQueue,Logger
+//go:generate go run github.com/golang/mock/mockgen -package eventqueue -destination changestream_mock_test.go github.com/juju/juju/core/changestream Subscription
 
 func TestPackage(t *testing.T) {
 	gc.TestingT(t)
@@ -23,6 +26,7 @@ type baseSuite struct {
 
 	queue  *MockEventQueue
 	logger *MockLogger
+	sub    *MockSubscription
 }
 
 func (s *baseSuite) setUpMocks(c *gc.C) *gomock.Controller {
@@ -30,10 +34,34 @@ func (s *baseSuite) setUpMocks(c *gc.C) *gomock.Controller {
 
 	s.queue = NewMockEventQueue(ctrl)
 	s.logger = NewMockLogger(ctrl)
+	s.sub = NewMockSubscription(ctrl)
 
 	return ctrl
 }
 
 func (s *baseSuite) makeBaseWatcher() BaseWatcher {
 	return MakeBaseWatcher(s.queue, s.TrackedDB(), s.logger)
+}
+
+// subscriptionOptionMatcher is a gomock.Matcher that can be used to check
+// that subscription options match, by comparing their namespaces and masks.
+// The filter func is omitted from comparison.
+type subscriptionOptionMatcher struct {
+	opt changestream.SubscriptionOption
+}
+
+// Matches returns true if the argument is a changestream.SubscriptionOption,
+// and its namespace and mask match those of our member.
+func (m subscriptionOptionMatcher) Matches(arg interface{}) bool {
+	optArg, ok := arg.(changestream.SubscriptionOption)
+	if !ok {
+		return false
+	}
+
+	return optArg.Namespace() == m.opt.Namespace() && optArg.ChangeMask() == m.opt.ChangeMask()
+}
+
+// String exists to satisfy the gomock.Matcher interface.
+func (m subscriptionOptionMatcher) String() string {
+	return fmt.Sprintf("%s %d", m.opt.Namespace(), m.opt.ChangeMask())
 }
