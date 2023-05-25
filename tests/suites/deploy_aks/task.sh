682a4b03c5c6bd481906deb38009fa0aaee4ddb0
Vitaly Antonenko <vitaly.antonenko@canonical.com>
2023-05-25 15:39:48 +0300

    Adds BOOTSTRAP_PROVIDER=k8s before aks-controller boostraping.
    Fixes spaces and tabs issues.
    
    Adds kill-controller for more relable removal.

--- tests/suites/deploy_aks/task.sh
+++ tests/suites/deploy_aks/task.sh
@@ -16,10 +16,12 @@ test_deploy_aks() {
 	az aks create -g "${resource_group_name}" -n aks-cluster --generate-ssh-keys
 	juju add-k8s --aks --client --resource-group "${resource_group_name}" --storage test-aks-storage --cluster-name aks-cluster aks-k8s-cloud
 
+	export BOOTSTRAP_PROVIDER=k8s
 	bootstrap_custom_controller "test-deploy-aks" "aks-k8s-cloud"
 
 	test_deploy_aks_charms
 
+	export KILL_CONTROLLER=true
 	destroy_controller "test-deploy-aks"
 
 	juju remove-k8s --client aks-k8s-cloud
