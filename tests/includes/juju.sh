870f6c76ad7e975de3df5c6d5fa42cab02e7392c
Jordan Barrett <jordan.barrett@canonical.com>
2023-05-25 14:25:54 +1000

    [bash tests] print controller teardown logs
    We are having intermittent teardown failures in many tests.
    This should help us diagnose the issue(s).

--- tests/includes/juju.sh
+++ tests/includes/juju.sh
@@ -401,9 +401,9 @@ destroy_controller() {
 
 	echo "====> Destroying juju ($(green "${name}"))"
 	if [[ ${KILL_CONTROLLER:-} != "true" ]]; then
-		echo "${name}" | xargs -I % juju destroy-controller --destroy-all-models -y % >"${output}" 2>&1
+		echo "${name}" | xargs -I % juju destroy-controller --destroy-all-models -y % 2>&1 | OUTPUT "${output}"
 	else
-		echo "${name}" | xargs -I % juju kill-controller -t 0 -y % >"${output}" 2>&1
+		echo "${name}" | xargs -I % juju kill-controller -t 0 -y % 2>&1 | OUTPUT "${output}"
 	fi
 
 	set +e
