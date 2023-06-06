10405d7c5cf0fe0588d98ca48cde7e862d7c8d56
Simon Richardson <stickupkid@gmail.com>
2023-06-06 17:03:26 +0100

    Implement tests for the core/pinger package
    The following ensures that we have at least some coverage on the
    core/pinger package.

---
+++ core/pinger/package_test.go
@@ -0,0 +1,29 @@
+// Copyright 2023 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package pinger_test
+
+import (
+	"testing"
+
+	gc "gopkg.in/check.v1"
+
+	coretesting "github.com/juju/juju/testing"
+)
+
+//go:generate go run github.com/golang/mock/mockgen -package pinger -destination clock_mock_test.go github.com/juju/clock Clock,Timer
+
+func TestPackage(t *testing.T) {
+	gc.TestingT(t)
+}
+
+type ImportTest struct{}
+
+var _ = gc.Suite(&ImportTest{})
+
+func (*ImportTest) TestImports(c *gc.C) {
+	found := coretesting.FindJujuCoreImports(c, "github.com/juju/juju/core/pinger")
+
+	// This package brings in nothing else from juju/juju
+	c.Assert(found, gc.HasLen, 0)
+}
---
+++ core/pinger/pinger_test.go
@@ -0,0 +1,100 @@
+// Copyright 2023 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package pinger
+
+import (
+	"time"
+
+	"github.com/golang/mock/gomock"
+	"github.com/juju/testing"
+	"github.com/juju/worker/v3/workertest"
+	gc "gopkg.in/check.v1"
+)
+
+type suite struct {
+	testing.IsolationSuite
+
+	clock *MockClock
+}
+
+var _ = gc.Suite(&suite{})
+
+func (s *suite) TestPing(c *gc.C) {
+	defer s.setupMocks(c).Finish()
+
+	s.clock.EXPECT().After(time.Second).DoAndReturn(func(d time.Duration) <-chan time.Time {
+		return make(chan time.Time)
+	}).MinTimes(1)
+
+	action := func() {
+		c.Fatal("action should not be called")
+	}
+
+	p := NewPinger(action, s.clock, time.Second)
+	defer workertest.DirtyKill(c, p)
+
+	for i := 0; i < 10; i++ {
+		p.Ping()
+		// Ensure that the ping timer has been at least called
+		<-time.After(testing.ShortWait)
+	}
+
+	workertest.CheckKill(c, p)
+}
+
+func (s *suite) TestPingAfterKilled(c *gc.C) {
+	defer s.setupMocks(c).Finish()
+
+	s.clock.EXPECT().After(time.Second).DoAndReturn(func(d time.Duration) <-chan time.Time {
+		return make(chan time.Time)
+	}).AnyTimes()
+
+	action := func() {
+		c.Fatal("action should not be called")
+	}
+
+	p := NewPinger(action, s.clock, time.Second)
+	workertest.CheckKill(c, p)
+
+	p.Ping()
+}
+
+func (s *suite) TestPingTimeout(c *gc.C) {
+	defer s.setupMocks(c).Finish()
+
+	s.clock.EXPECT().After(time.Second).DoAndReturn(func(d time.Duration) <-chan time.Time {
+		ch := make(chan time.Time)
+		go func() {
+			ch <- time.Now()
+		}()
+		return ch
+	}).MinTimes(1)
+
+	sync := make(chan struct{})
+	action := func() {
+		close(sync)
+	}
+
+	p := NewPinger(action, s.clock, time.Second)
+	defer workertest.DirtyKill(c, p)
+
+	p.Ping()
+
+	select {
+	case <-sync:
+	case <-time.After(testing.ShortWait):
+		c.Fatal("timed out waiting for ping")
+	}
+
+	err := p.Wait()
+	c.Assert(err, gc.ErrorMatches, "ping timeout")
+}
+
+func (s *suite) setupMocks(c *gc.C) *gomock.Controller {
+	ctrl := gomock.NewController(c)
+
+	s.clock = NewMockClock(ctrl)
+
+	return ctrl
+}
