c0bf332d27488adfa227776c435bbedb6a43414e
Joseph Phillips <joe.phillips@canonical.com>
2023-06-12 14:56:01 +0200

    Adds controller node service, using the new state, with a method for adding and removing controller nodes.

---
+++ domain/controllernode/service/service_test.go
@@ -0,0 +1,35 @@
+package service
+
+import (
+	"context"
+
+	"github.com/golang/mock/gomock"
+	"github.com/juju/testing"
+	jc "github.com/juju/testing/checkers"
+	gc "gopkg.in/check.v1"
+)
+
+type serviceSuite struct {
+	testing.IsolationSuite
+
+	state *MockState
+}
+
+var _ = gc.Suite(&serviceSuite{})
+
+func (s *serviceSuite) TestUpdateExternalControllerSuccess(c *gc.C) {
+	defer s.setupMocks(c).Finish()
+
+	s.state.EXPECT().CurateNodes(gomock.Any(), []string{"3", "4"}, []string{"1"})
+
+	err := NewService(s.state).CurateNodes(context.Background(), []string{"3", "4"}, []string{"1"})
+	c.Assert(err, jc.ErrorIsNil)
+}
+
+func (s *serviceSuite) setupMocks(c *gc.C) *gomock.Controller {
+	ctrl := gomock.NewController(c)
+
+	s.state = NewMockState(ctrl)
+
+	return ctrl
+}
