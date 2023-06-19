d6c2a203231407c9bd6ac5c5887d240dbe1249d6
Simon Richardson <stickupkid@gmail.com>
2023-06-19 11:34:27 +0100

    Add a default package test for imports.

---
+++ api/package_test.go
@@ -0,0 +1,55 @@
+package api
+
+import (
+	"testing"
+
+	jc "github.com/juju/testing/checkers"
+	gc "gopkg.in/check.v1"
+
+	coretesting "github.com/juju/juju/testing"
+)
+
+func TestPackage(t *testing.T) {
+	coretesting.MgoTestPackage(t)
+}
+
+type ImportSuite struct{}
+
+var _ = gc.Suite(&ImportSuite{})
+
+func (*ImportSuite) TestImports(c *gc.C) {
+	found := coretesting.FindJujuCoreImports(c, "github.com/juju/juju/api")
+
+	c.Assert(found, jc.SameContents, []string{
+		"api/agent/keyupdater",
+		"api/base",
+		"api/watcher",
+		"core/constraints",
+		"core/devices",
+		"core/instance",
+		"core/life",
+		"core/macaroon",
+		"core/migration",
+		"core/model",
+		"core/network",
+		"core/os",
+		"core/paths",
+		"core/relation",
+		"core/resources",
+		"core/secrets",
+		"core/series",
+		"core/status",
+		"core/watcher",
+		"docker",
+		"environs/context",
+		"feature",
+		"proxy",
+		"rpc",
+		"rpc/jsoncodec",
+		"rpc/params",
+		"storage",
+		"tools",
+		"utils/proxy",
+		"version",
+	})
+}
