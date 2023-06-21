109b2a32dc33956d31ca36f3b7d184310d7f935d
Simon Richardson <stickupkid@gmail.com>
2023-06-21 09:18:14 +0100

    Remove redundant errors.Cause
    As a driveby make const error a real const.

--- worker/dbaccessor/worker.go
+++ worker/dbaccessor/worker.go
@@ -22,7 +22,7 @@ import (
 	"github.com/juju/juju/pubsub/apiserver"
 )
 
-var (
+const (
 	// ErrTryAgain indicates that the worker should try again to start the
 	// worker.
 	ErrTryAgain = errors.ConstError("DB node is nil, but worker is not dying; rescheduling TrackedDB start attempt")
@@ -205,7 +205,7 @@ func NewWorker(cfg WorkerConfig) (*dbWorker, error) {
 				// will be nil. In this case, we'll return ErrTryAgain. In this
 				// case we don't want to kill the worker. We'll force the
 				// worker to try again.
-				return !errors.Is(errors.Cause(err), ErrTryAgain)
+				return !errors.Is(err, ErrTryAgain)
 			},
 			RestartDelay: time.Second * 10,
 			Logger:       cfg.Logger,
