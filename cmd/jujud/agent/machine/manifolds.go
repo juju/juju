1dd578b74f6d627de27d1a1c7353efca60b84f60
Vitaly Antonenko <vitaly.antonenko@canonical.com>
2023-07-21 17:54:55 +0300

    Adds ChangeStreamName to manifolds.go

--- cmd/jujud/agent/machine/manifolds.go
+++ cmd/jujud/agent/machine/manifolds.go
@@ -506,10 +506,11 @@ func commonManifolds(config ManifoldsConfig) dependency.Manifolds {
 		// The agent-config-updater manifold sets the state serving info from
 		// the API connection and writes it to the agent config.
 		agentConfigUpdaterName: ifNotMigrating(agentconfigupdater.Manifold(agentconfigupdater.ManifoldConfig{
-			AgentName:      agentName,
-			APICallerName:  apiCallerName,
-			CentralHubName: centralHubName,
-			Logger:         loggo.GetLogger("juju.worker.agentconfigupdater"),
+			AgentName:        agentName,
+			APICallerName:    apiCallerName,
+			CentralHubName:   centralHubName,
+			ChangeStreamName: changeStreamName,
+			Logger:           loggo.GetLogger("juju.worker.agentconfigupdater"),
 		})),
 
 		// The logging config updater is a leaf worker that indirectly
--- worker/agentconfigupdater/manifold_test.go
+++ worker/agentconfigupdater/manifold_test.go
@@ -4,7 +4,6 @@
 package agentconfigupdater_test
 
 import (
-	"go.uber.org/mock/gomock"
 	"time"
 
 	"github.com/juju/loggo"
@@ -15,6 +14,7 @@ import (
 	"github.com/juju/worker/v3/dependency"
 	dt "github.com/juju/worker/v3/dependency/testing"
 	"github.com/juju/worker/v3/workertest"
+	"go.uber.org/mock/gomock"
 	gc "gopkg.in/check.v1"
 
 	"github.com/juju/juju/agent"
