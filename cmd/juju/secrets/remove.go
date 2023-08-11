1a597e3b519834482fe782759cb14ca1a24053d0
Kelvin Liu <kelvin.liu@canonical.com>
2023-08-11 19:32:53 +1000

    Implement update-secret and remove-secret commands;

---
+++ cmd/juju/secrets/remove.go
@@ -0,0 +1,93 @@
+// Copyright 2023 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package secrets
+
+import (
+	"github.com/juju/cmd/v3"
+	"github.com/juju/errors"
+	"github.com/juju/gnuflag"
+
+	apisecrets "github.com/juju/juju/api/client/secrets"
+	jujucmd "github.com/juju/juju/cmd"
+	"github.com/juju/juju/cmd/modelcmd"
+	"github.com/juju/juju/core/secrets"
+)
+
+type removeSecretCommand struct {
+	modelcmd.ModelCommandBase
+
+	secretsAPIFunc func() (RemoveSecretsAPI, error)
+
+	secretURI *secrets.URI
+	revision  int
+}
+
+// RemoveSecretsAPI is the secrets client API.
+type RemoveSecretsAPI interface {
+	RemoveSecret(uri *secrets.URI, revision *int) error
+	Close() error
+}
+
+// NewRemoveSecretCommand returns a command to remove a secret.
+func NewRemoveSecretCommand() cmd.Command {
+	c := &removeSecretCommand{}
+	c.secretsAPIFunc = c.secretsAPI
+	return modelcmd.Wrap(c)
+}
+
+func (c *removeSecretCommand) secretsAPI() (RemoveSecretsAPI, error) {
+	root, err := c.NewAPIRoot()
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+	return apisecrets.NewClient(root), nil
+}
+
+// Info implements cmd.Command.
+func (c *removeSecretCommand) Info() *cmd.Info {
+	doc := `
+Remove all the revisions of a secret with the specified URI or remove the provided revision only.
+
+Examples:
+    remove-secret secret:9m4e2mr0ui3e8a215n4g
+	remove-secret secret:9m4e2mr0ui3e8a215n4g --revision 4
+`
+	return jujucmd.Info(&cmd.Info{
+		Name:    "remove-secret",
+		Args:    "<ID>",
+		Purpose: "Remove a existing secret.",
+		Doc:     doc,
+	})
+}
+
+// SetFlags implements cmd.Command.
+func (c *removeSecretCommand) SetFlags(f *gnuflag.FlagSet) {
+	f.IntVar(&c.revision, "revision", 0, "remove the specified revision")
+}
+
+// Init implements cmd.Command.
+func (c *removeSecretCommand) Init(args []string) error {
+	if len(args) < 1 {
+		return errors.New("missing secret URI")
+	}
+	var err error
+	if c.secretURI, err = secrets.ParseURI(args[0]); err != nil {
+		return errors.Trace(err)
+	}
+	return cmd.CheckEmpty(args[1:])
+}
+
+// Run implements cmd.Command.
+func (c *removeSecretCommand) Run(ctx *cmd.Context) error {
+	var rev *int
+	if c.revision > 0 {
+		rev = &c.revision
+	}
+	secretsAPI, err := c.secretsAPIFunc()
+	if err != nil {
+		return errors.Trace(err)
+	}
+	defer secretsAPI.Close()
+	return secretsAPI.RemoveSecret(c.secretURI, rev)
+}
---
+++ cmd/juju/secrets/update_test.go
@@ -0,0 +1,84 @@
+// Copyright 2023 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package secrets_test
+
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/juju/cmd/v3/cmdtesting"
+	jujutesting "github.com/juju/testing"
+	jc "github.com/juju/testing/checkers"
+	"go.uber.org/mock/gomock"
+	gc "gopkg.in/check.v1"
+
+	"github.com/juju/juju/cmd/juju/secrets"
+	"github.com/juju/juju/cmd/juju/secrets/mocks"
+	coresecrets "github.com/juju/juju/core/secrets"
+	"github.com/juju/juju/jujuclient"
+)
+
+type updateSuite struct {
+	jujutesting.IsolationSuite
+	store      *jujuclient.MemStore
+	secretsAPI *mocks.MockUpdateSecretsAPI
+}
+
+var _ = gc.Suite(&updateSuite{})
+
+func (s *updateSuite) SetUpTest(c *gc.C) {
+	s.IsolationSuite.SetUpTest(c)
+	store := jujuclient.NewMemStore()
+	store.Controllers["mycontroller"] = jujuclient.ControllerDetails{}
+	store.CurrentControllerName = "mycontroller"
+	s.store = store
+}
+
+func (s *updateSuite) setup(c *gc.C) *gomock.Controller {
+	ctrl := gomock.NewController(c)
+	s.secretsAPI = mocks.NewMockUpdateSecretsAPI(ctrl)
+	return ctrl
+}
+
+func (s *updateSuite) TestUpdateMissArg(c *gc.C) {
+	defer s.setup(c).Finish()
+
+	_, err := cmdtesting.RunCommand(c, secrets.NewUpdateCommandForTest(s.store, s.secretsAPI), "--label", "label", "--info", "this is a secret.")
+	c.Assert(err, gc.ErrorMatches, `missing secret URI`)
+}
+
+func (s *updateSuite) TestUpdateFromArg(c *gc.C) {
+	defer s.setup(c).Finish()
+
+	uri := coresecrets.NewURI()
+	s.secretsAPI.EXPECT().UpdateSecret(uri, ptr(true), "label", "this is a secret.", map[string]string{"foo": "YmFy"}).Return(nil)
+	s.secretsAPI.EXPECT().Close().Return(nil)
+
+	_, err := cmdtesting.RunCommand(c, secrets.NewUpdateCommandForTest(
+		s.store, s.secretsAPI), uri.String(), "foo=bar",
+		"--auto-prune", "--label", "label", "--info", "this is a secret.",
+	)
+	c.Assert(err, jc.ErrorIsNil)
+}
+
+func (s *updateSuite) TestUpdateFromFile(c *gc.C) {
+	defer s.setup(c).Finish()
+
+	uri := coresecrets.NewURI()
+	s.secretsAPI.EXPECT().UpdateSecret(uri, ptr(true), "label", "this is a secret.", map[string]string{"foo": "YmFy"}).Return(nil)
+	s.secretsAPI.EXPECT().Close().Return(nil)
+
+	dir := c.MkDir()
+	path := filepath.Join(dir, "data.txt")
+	data := `
+foo: bar
+    `
+	err := os.WriteFile(path, []byte(data), 0644)
+	c.Assert(err, jc.ErrorIsNil)
+	_, err = cmdtesting.RunCommand(c, secrets.NewUpdateCommandForTest(
+		s.store, s.secretsAPI), uri.String(), "--file", path,
+		"--auto-prune", "--label", "label", "--info", "this is a secret.",
+	)
+	c.Assert(err, jc.ErrorIsNil)
+}
