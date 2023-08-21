067dc72d8bffb5345c1a4803128f94fd6aa8a732
Vitaly Antonenko <vitaly.antonenko@canonical.com>
2023-08-21 11:23:40 +0300

    Adds context to FindTools function in client/client facade unit-tests.

--- apiserver/facades/client/client/package_test.go
+++ apiserver/facades/client/client/package_test.go
@@ -9,10 +9,10 @@ import (
 	coretesting "github.com/juju/juju/testing"
 )
 
-func TestPackage(t *stdtesting.T) {
-	coretesting.MgoTestPackage(t)
-}
-
 //go:generate go run go.uber.org/mock/mockgen -package mocks -destination mocks/client_mock.go github.com/juju/juju/apiserver/facades/client/client Backend,Model
 //go:generate go run go.uber.org/mock/mockgen -package mocks -destination mocks/facade_mock.go github.com/juju/juju/apiserver/facade Authorizer
 //go:generate go run go.uber.org/mock/mockgen -package mocks -destination mocks/common_mock.go github.com/juju/juju/apiserver/common ToolsFinder
+
+func TestPackage(t *stdtesting.T) {
+	coretesting.MgoTestPackage(t)
+}
