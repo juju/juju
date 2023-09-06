c050b1b6b94e1e6b312c9b4da56d4f9a64fdd15a
Jordan Barrett <jordan.barrett@canonical.com>
2023-09-06 12:29:51 +0700

    add run_prometheus_multi test

--- tests/suites/controllercharm/prometheus.sh
+++ tests/suites/controllercharm/prometheus.sh
@@ -1,17 +1,49 @@
 run_prometheus() {
 	echo
 
-	file="${TEST_DIR}/test-prometheus.log"
-	bootstrap "test-prometheus" "${file}"
+	MODEL_NAME="test-prometheus"
+	file="${TEST_DIR}/${MODEL_NAME}.log"
+	bootstrap "${MODEL_NAME}" "${file}"
 
 	juju offer controller.controller:metrics-endpoint
+
 	juju deploy prometheus-k8s --trust
 	juju relate prometheus-k8s controller.controller
 	wait_for "prometheus-k8s" "$(active_condition "prometheus-k8s")"
-
 	retry check_prometheus_targets 10
 
-	destroy_controller "test-prometheus"
+	# TODO: need to destroy persistent storage volume here
+  destroy_model "${MODEL_NAME}"
+	destroy_controller "${MODEL_NAME}"
+}
+
+# Check the controller charm can handle multiple Prometheus relations.
+run_prometheus_multi() {
+	echo
+
+	MODEL_NAME="test-prometheus-multi"
+	file="${TEST_DIR}/${MODEL_NAME}.log"
+	bootstrap "${MODEL_NAME}" "${file}"
+
+	juju offer controller.controller:metrics-endpoint
+
+	juju deploy prometheus-k8s p1 --trust
+	juju relate prometheus-k8s controller.controller
+	wait_for "p1" "$(active_condition "p1")"
+  retry check_prometheus_targets 10
+
+	juju deploy prometheus-k8s p2 --trust
+	juju relate prometheus-k8s controller.controller
+
+	wait_for "p1" "$(active_condition "p1")"
+	retry check_prometheus_targets 10
+
+	juju add-unit p1
+	retry check_prometheus_targets 10
+
+	# TODO: need to destroy persistent storage volume here
+  destroy_model "${MODEL_NAME}"
+	destroy_controller "${MODEL_NAME}"
 }
 
 # Check the Juju controller is in the list of Prometheus targets.
@@ -27,11 +59,13 @@ check_prometheus_targets() {
 		return 1
 	fi
 
-	TARGET_STATUS=$(echo $TARGET | jq '.health')
+	TARGET_STATUS=$(echo $TARGET | jq -r '.health')
 	if [[ $TARGET_STATUS != "up" ]]; then
-		echo "Controller metrics endpoint status: $TARGET_STATUS: $(echo $TARGET | jq '.lastError')"
+		echo "Controller metrics endpoint status: $TARGET_STATUS: $(echo $TARGET | jq -r '.lastError')"
 		return 1
 	fi
+
+	echo "Controller metrics endpoint is up"
 }
 
 test_prometheus() {
@@ -48,6 +82,7 @@ test_prometheus() {
 		case "${BOOTSTRAP_PROVIDER:-}" in
 		"k8s")
 			run "run_prometheus"
+			#run "run_prometheus_multi"
 			;;
 		*)
 			echo "==> TEST SKIPPED: run_prometheus test runs on k8s only"
