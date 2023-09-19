682ec951c9b24847341bbf342d048335e32c1397
Simon Richardson <stickupkid@gmail.com>
2023-09-19 14:13:15 +0100

    Remove dead/unused code
    As we no longer support client side checks, we can get rid of the
    min checks for clients.

--- upgrades/upgradevalidation/version.go
+++ upgrades/upgradevalidation/version.go
@@ -17,28 +17,14 @@ var MinAgentVersions = map[int]version.Number{
 	3: version.MustParse("2.9.43"),
 }
 
-// MinClientVersions defines the minimum user client version
-// allowed to make a call to a controller with the major version,
-// or the minimum controller version needed to accept a call from a
-// client with the major version.
-var MinClientVersions = map[int]version.Number{
-	3: version.MustParse("2.9.42"),
-}
-
-// MinMajorMigrateVersions defines the minimum version the model
-// must be running before migrating to the target controller.
-var MinMajorMigrateVersions = MinAgentVersions
-
 // UpgradeControllerAllowed returns true if a controller upgrade is allowed
 // when it hosts a model with the specified version.
 func UpgradeControllerAllowed(modelVersion, targetControllerVersion version.Number) (bool, version.Number, error) {
-	return versionCheck(
-		modelVersion, targetControllerVersion, MinAgentVersions, "upgrading controller",
-	)
+	return versionCheck(modelVersion, targetControllerVersion, MinAgentVersions)
 }
 
 func versionCheck(
-	from, to version.Number, versionMap map[int]version.Number, operation string,
+	from, to version.Number, versionMap map[int]version.Number,
 ) (bool, version.Number, error) {
 	// If the major version is the same then we will allow the upgrade.
 	if from.Major == to.Major {
@@ -53,7 +39,7 @@ func versionCheck(
 	minVer, ok := versionMap[to.Major]
 	logger.Debugf("from %q, to %q, versionMap %#v", from, to, versionMap)
 	if !ok {
-		return false, version.Number{}, errors.Errorf("%s to %q is not supported from %q", operation, to, from)
+		return false, version.Number{}, errors.Errorf("%s to %q is not supported from %q", "upgrading controller", to, from)
 	}
 	// Allow upgrades from rc etc.
 	from.Tag = ""
