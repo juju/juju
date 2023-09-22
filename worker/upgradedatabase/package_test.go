01aff7bd0f2989822b4efa78cf52bac32e2c62e0
Vitaly Antonenko <vitaly.antonenko@canonical.com>
2023-09-22 16:23:41 +0300

    Renames the mock test files.

--- worker/upgradedatabase/package_test.go
+++ worker/upgradedatabase/package_test.go
@@ -9,8 +9,8 @@ import (
 	gc "gopkg.in/check.v1"
 )
 
-//go:generate go run go.uber.org/mock/mockgen -package upgradedatabase -destination worker_test.go github.com/juju/juju/worker/upgradedatabase Logger
-//go:generate go run go.uber.org/mock/mockgen -package upgradedatabase -destination lock_test.go github.com/juju/juju/worker/gate Lock
+//go:generate go run go.uber.org/mock/mockgen -package upgradedatabase -destination package_mock_test.go github.com/juju/juju/worker/upgradedatabase Logger
+//go:generate go run go.uber.org/mock/mockgen -package upgradedatabase -destination lock_mock_test.go github.com/juju/juju/worker/gate Lock
 
 func TestPackage(t *testing.T) {
 	gc.TestingT(t)
