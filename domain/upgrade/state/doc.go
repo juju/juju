81812134e92a5f0980fcc39fe2db7b1f18a337e8
Simon Richardson <stickupkid@gmail.com>
2023-10-11 12:03:59 +0100

    Improve the documentation for the state machine
    The digram laying out the statemachine didn't have the error state,
    now it does.

--- domain/upgrade/state/doc.go
+++ domain/upgrade/state/doc.go
@@ -7,7 +7,24 @@
 // The states are represented as a finite state machine, with the following
 // transitions:
 //
-//	Created -> Started -> DBCompleted -> StepsCompleted
+//	┌──────────────────┐
+//	│      Created     ├─────┐
+//	└─────────┬────────┘     │
+//	          │              │
+//	          │              │
+//	┌─────────▼─────────┐    │
+//	│      Started      ├────┤
+//	└─────────┬─────────┘    │
+//	          │              │    ┌───────────┐
+//	          │              ├────►   Error   │
+//	┌─────────▼─────────┐    │    └───────────┘
+//	│    DBCompleted    ├────┤
+//	└─────────┬─────────┘    │
+//	          │              │
+//	          │              │
+//	┌─────────▼─────────┐    │
+//	│  StepsCompleted   ├────┘
+//	└───────────────────┘
 //
 // The state machine is represented by the State type, which is an integer
 // type. The constants for the states are exported, and the type is exported
