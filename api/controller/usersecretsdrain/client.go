2c7fd59480134bbd40536ede01645bc967a550fc
Kelvin Liu <kelvin.liu@canonical.com>
2023-10-18 19:40:08 +1100

    Add UserSecretsDrain facade client;

--- api/controller/usersecretsdrain/client.go
+++ api/controller/usersecretsdrain/client.go
@@ -5,10 +5,23 @@ package usersecretsdrain
 
 import (
 	"github.com/juju/juju/api/base"
+	commonsecretbackends "github.com/juju/juju/api/common/secretbackends"
 	commonsecretdrain "github.com/juju/juju/api/common/secretsdrain"
 )
 
+type backendsClient = commonsecretbackends.Client
+
+// Client is the api client for the UserSecretsDrain facade.
+type Client struct {
+	*commonsecretdrain.Client
+	*backendsClient
+}
+
 // NewClient creates a secrets api client.
-func NewClient(caller base.APICaller) *commonsecretdrain.Client {
-	return commonsecretdrain.NewClient(base.NewFacadeCaller(caller, "UserSecretsDrain"))
+func NewClient(caller base.APICaller) *Client {
+	facade := base.NewFacadeCaller(caller, "UserSecretsDrain")
+	return &Client{
+		commonsecretdrain.NewClient(facade),
+		commonsecretbackends.NewClient(facade),
+	}
 }
