a4f08a0dfcf69e7f6601770842d568389fa1218a
Teodora Mihoc <te.mihoc@gmail.com>
2023-10-19 15:08:06 +0200

    There is a difference between 2.9 and 3.x in Usage -- in 2.9 one could pass cloud/region as an optional positional argument but in 3.x it must be passed via the --region flag.

--- cmd/juju/model/defaults.go
+++ cmd/juju/model/defaults.go
@@ -261,7 +261,7 @@ type defaultsCommandAPI interface {
 // Info implements part of the cmd.Command interface.
 func (c *defaultsCommand) Info() *cmd.Info {
 	return jujucmd.Info(&cmd.Info{
-		Args:     "[[<cloud>/]<region> ]<model-key>[<=value>] ...]",
+		Args:     "[<model-key>[<=value>] ...]",
 		Doc:      modelDefaultsHelpDoc,
 		Name:     "model-defaults",
 		Purpose:  modelDefaultsSummary,
