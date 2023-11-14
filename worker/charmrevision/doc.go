4cfd27529a74404aa812db1fb15af76a9cb428e5
hmlanigan <heather.lanigan@canonical.com>
2023-11-14 11:26:49 -0500

    Only repository charms are queried for updates.

--- worker/charmrevision/doc.go
+++ worker/charmrevision/doc.go
@@ -3,8 +3,8 @@
 
 // Package charmrevision defines the charm revision updater worker. This worker
 // is responsible for polling Charmhub every 24 hours to check if there are new
-// revisions available of any charm deployed in the model. If so, it will put a
-// document in the Juju database, so that the next time the user runs
+// revisions available of any repository charm deployed in the model. If so, it
+// will put a document in the Juju database, so that the next time the user runs
 // `juju status`, they can see that there is an update available. This worker
 // also sends anonymised usage metrics to Charmhub when it polls.
 //
