d4a989b65167a5c44f7506f7fb22543409c4d22a
Ian Booth <ian.booth@canonical.com>
2023-11-16 11:45:42 +1000

    Incorprate suggested feedback

--- worker/remoterelations/doc.go
+++ worker/remoterelations/doc.go
@@ -1,8 +1,14 @@
 // Copyright 2023 Canonical Ltd.
 // Licensed under the AGPLv3, see LICENCE file for details.
 
-// Package remoterelations contains workers which manage the operation of cross model relations.
-// The workers run on both the consuming and offering models.
+// Package remoterelations defines workers which manage the operation of cross model relations.
+//   - `Worker`: top level worker, watches saas applications/proxies and creates a worker for each.
+//   - `remoteApplicationWorker`: manages operations for a consumer or offer proxy;
+//     consume and publish relation data and status changes.
+//   - `remoteRelationsWorker`: runs on the consuming model to manages relations to the offer.
+//   - `relationUnitsWorker`: runs on the consuming model to receive and publish on changes to
+//     each relation unit data bag.
+//
 // The consuming side pushes relation updates from the consumer application to the model containing
 // the offer. It also polls the offered application to record relation changes from the offer into
 // the consuming model.
