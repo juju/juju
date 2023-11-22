1b6335bd1857f2d651b0b537288bbc7ca6ba5c44
Thomas Miller <thomas.miller@canonical.com>
2023-11-22 15:25:16 +1000

    Refactor of DefaultAttributeValue.
    In previous work around ModelDefaults we took the approach of modeling
    DefaultAttributeValue to include the 3 possible sources of defaults.
    After investigating the code some more we have a lot more places where
    default values come from that are acting like model defaults but are not
    described as such.
    
    Think of these extra sources as ad hoc places in the code base where we
    have decided to start acting with opinionated defaults about model config
    and skipped the established patterns around how model defaults work.
    
    In order to move Juju over to the services defined in both modeldefaults
    and modelconfig we need to gather all of these opinions and sources of
    defaults into one location and represent the values in one coherent way.
    
    This change tackles the above problems by defining a single default
    value, the source of the value and a way for the source to inject a set
    of opinions on how the default value should be applied.
    
    Overall it would be better if we could make model defaults much more
    coherent and standardised but for the time being this allows use to take
    what we already have move across to DQlite and the services layer.

--- domain/modeldefaults/types_test.go
+++ domain/modeldefaults/types_test.go
@@ -12,73 +12,120 @@ type typesSuite struct{}
 
 var _ = gc.Suite(&typesSuite{})
 
+// TestZeroDefaultsValue is here to test what the zero value of a
+// DefaultAttributeValue does. Specifically that Has returns false and the apply
+// strategy just returns whatever is passed to it.
+//
+// We want to make sure that if a zero value escapes by accident it will not
+// cause damage to a models config.
 func (s *typesSuite) TestZeroDefaultsValue(c *gc.C) {
 	val := DefaultAttributeValue{}
-	c.Assert(val.Value(), gc.IsNil)
-	c.Assert(val.ValueSource(), gc.Equals, "")
 
 	has, source := val.Has("someval")
-	c.Assert(has, jc.IsFalse)
-	c.Assert(source, gc.Equals, "")
+	c.Check(has, jc.IsFalse)
+	c.Check(source, gc.Equals, "")
+
+	applied, is := val.ApplyStrategy("teststring").(string)
+	c.Assert(is, jc.IsTrue, gc.Commentf("expected zero value apply strategy to return what was passed to it verbatim"))
+	c.Check(applied, gc.Equals, "teststring")
 }
 
-func (s *typesSuite) TestDefaultDefaultsValue(c *gc.C) {
-	val := DefaultAttributeValue{Default: "test"}
-	c.Assert(val.Value(), gc.DeepEquals, "test")
-	c.Assert(val.ValueSource(), gc.Equals, "default")
-
-	has, source := val.Has("test")
-	c.Assert(has, jc.IsTrue)
-	c.Assert(source, gc.Equals, "default")
-
-	has, source = val.Has("noexist")
-	c.Assert(has, jc.IsFalse)
-	c.Assert(source, gc.Equals, "")
-}
-
-func (s *typesSuite) TestControllerDefaultsValue(c *gc.C) {
+// TestHasSupportForNil is testing for nil values to Has() we always return
+// false and no source information as per the contract of the function.
+func (s *typesSuite) TestHasSupportForNil(c *gc.C) {
 	val := DefaultAttributeValue{
-		Default:    "default",
-		Controller: "test",
+		Source: "testsource",
+		V:      "someval",
 	}
-	c.Assert(val.Value(), gc.DeepEquals, "test")
-	c.Assert(val.ValueSource(), gc.Equals, "controller")
 
-	has, source := val.Has("test")
-	c.Assert(has, jc.IsTrue)
-	c.Assert(source, gc.Equals, "controller")
+	has, source := val.Has(nil)
+	c.Check(has, jc.IsFalse)
+	c.Check(source, gc.Equals, "")
 
-	has, source = val.Has("noexist")
-	c.Assert(has, jc.IsFalse)
-	c.Assert(source, gc.Equals, "")
+	val = DefaultAttributeValue{
+		Source: "testsource",
+		V:      nil,
+	}
 
-	has, source = val.Has("default")
-	c.Assert(has, jc.IsFalse)
-	c.Assert(source, gc.Equals, "")
+	has, source = val.Has("myval")
+	c.Check(has, jc.IsFalse)
+	c.Check(source, gc.Equals, "")
 }
 
-func (s *typesSuite) TestRegionDefaultsValue(c *gc.C) {
+// TestHasSupport is testing Has for DefaultAttributeValue and that we can ask
+// the question correctly. We are only checking basic comparison here as that is
+// all Has supports.
+func (s *typesSuite) TestHasSupport(c *gc.C) {
 	val := DefaultAttributeValue{
-		Default:    "default",
-		Controller: "controller",
-		Region:     "test",
+		Source: "testsource",
+		V:      "someval",
 	}
-	c.Assert(val.Value(), gc.DeepEquals, "test")
-	c.Assert(val.ValueSource(), gc.Equals, "region")
 
-	has, source := val.Has("test")
-	c.Assert(has, jc.IsTrue)
-	c.Assert(source, gc.Equals, "region")
+	has, source := val.Has("someval")
+	c.Check(has, jc.IsTrue)
+	c.Check(source, gc.Equals, "testsource")
 
-	has, source = val.Has("noexist")
-	c.Assert(has, jc.IsFalse)
-	c.Assert(source, gc.Equals, "")
+	i := int32(10)
+	val = DefaultAttributeValue{
+		Source: "testsource",
+		V:      &i,
+	}
 
-	has, source = val.Has("default")
-	c.Assert(has, jc.IsFalse)
-	c.Assert(source, gc.Equals, "")
+	has, source = val.Has(&i)
+	c.Check(has, jc.IsTrue)
+	c.Check(source, gc.Equals, "testsource")
 
-	has, source = val.Has("controller")
-	c.Assert(has, jc.IsFalse)
-	c.Assert(source, gc.Equals, "")
+	val = DefaultAttributeValue{
+		Source: "testsource",
+		V: []any{
+			"one",
+			"two",
+			"three",
+		},
+	}
+
+	has, source = val.Has([]any{
+		"one", "two", "three",
+	})
+	c.Check(has, jc.IsTrue)
+	c.Check(source, gc.Equals, "testsource")
+
+	structVal := struct{ name string }{"test"}
+	val = DefaultAttributeValue{
+		Source: "testsource",
+		V:      &structVal,
+	}
+
+	has, source = val.Has(&struct{ name string }{"test"})
+	c.Check(has, jc.IsFalse)
+	c.Check(source, gc.Equals, "")
+}
+
+// testApplyStrategy is a test implementation of ApplyStrategy that is here to
+// just indicate that the Apply method of the strategy has been called.
+type testApplyStrategy struct {
+	// Called indicates that the Apply func of this struct has been called.
+	Called bool
+}
+
+// Apply implements the ApplyStrategy interface.
+func (t *testApplyStrategy) Apply(d, s any) any {
+	t.Called = true
+	return s
+}
+
+// TestApplyStrategy is checking to make sure that if we set an apply strategy
+// on the DefaultAttributeValue. This test isn't concerned about testing the
+// logic of strategies just that the strategy is asked to make a decision.
+func (s *typesSuite) TestApplyStrategy(c *gc.C) {
+	strategy := &testApplyStrategy{}
+	val := DefaultAttributeValue{
+		Source:   "source",
+		Strategy: strategy,
+		V:        "someval",
+	}
+
+	out := val.ApplyStrategy("someval1")
+	c.Check(strategy.Called, jc.IsTrue)
+	c.Check(out, gc.Equals, "someval1")
 }
