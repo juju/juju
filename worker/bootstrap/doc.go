33b578a0a985ae29f10cfe34b12a34258c8e1839
Simon Richardson <stickupkid@gmail.com>
2023-12-04 12:03:47 +0000

    Improve bootstrap worker doc.go

--- worker/bootstrap/doc.go
+++ worker/bootstrap/doc.go
@@ -2,15 +2,14 @@
 // Licensed under the AGPLv3, see LICENCE file for details.
 
 // Package bootstrap ensures that when the initial bootstrap process
-// has started that the agent binary and the controller application along with
-// the charm is correctly seeded into the object store.
+// has started that we seed the following:
+//
+//  1. The agent binary is seeded into the objectstore.
+//  2. The controller application has been created, along with the supporting
+//     machine and spaces.
+//  3. The controller charm is seeded into the objectstore.
 //
 // The intention is that the worker is started as soon as possible during
 // bootstrap and that it will uninstall itself once the bootstrap process
 // has completed.
-//
-// Underlying all these changes is to ensure that we do not expose the
-// database outside of the dependency engine. This should prevent any side
-// loading of the database, which makes it easier to reason about and prevents
-// flaky tests (see machine legacy tests as an example).
 package bootstrap
