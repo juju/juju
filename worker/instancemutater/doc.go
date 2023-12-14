78473ba0f2adcc98ec8ddf6c1422e1baa18c2c54
Caner Derici <caner.derici@canonical.com>
2023-12-14 15:28:45 -0700

    Fix formatting in instancemutator worker doc

--- worker/instancemutater/doc.go
+++ worker/instancemutater/doc.go
@@ -1,7 +1,6 @@
 // Copyright 2023 Canonical Ltd.
 // Licensed under the AGPLv3, see LICENCE file for details.
 
-
 // Package instancemutater defines workers that compares the list of lxd profiles
 // applied to a machine with the list of expected profiles based on the
 // application versions which should be running on the machine. In particular, it
@@ -11,13 +10,13 @@
 // The ContainerWorker runs on a machine and watches for containers to be
 // created on it.
 //
-//      ┌───────────────────────────────┐
-//      │       MACHINE                 │
-//      │                               │
-//      │                               │
-//      │   ┌──────────────────────┐    │
-//      │   │                      │    │
-//      │   │     CONTAINER        │    │
+// //	┌───────────────────────────────┐
+// //	│       MACHINE                 │
+// //	│                               │
+// //	│                               │
+// //	│   ┌──────────────────────┐    │
+// //	│   │                      │    │
+// //	│   │     CONTAINER        │    │
 // ┌────┼───►                      │    │
 // │    │   │                      │    │
 // │    │   │  ┌────────────────┐  │    │
@@ -36,13 +35,14 @@
 // │    └──────────────┼────────────────┘
 // │                   │
 // └───────────────────┘
-//      LXD PROFILE
+//
+//	LXD PROFILE
 //
 // The EnvironWorker watches for machines in the model to be created.
-// 
-//      ┌───────────────────────────────┐
-//      │       MACHINE                 │
-//      │                               │
+//
+// //	┌───────────────────────────────┐
+// //	│       MACHINE                 │
+// //	│                               │
 // ┌────►                               │
 // │    │   ┌──────────────────────┐    │
 // │    │   │                      │    │
