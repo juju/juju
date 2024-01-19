b05772cda3cd55601cf305bcc4d0bc5f2c2973d2
Simon Richardson <stickupkid@gmail.com>
2024-01-19 17:08:10 +0000

    Ensure we import models
    The migration target tests rely on the importing logic of the
    underlying state. To work around this for now, we can just pretend
    to import the new state, whislt passing through to the legacy
    import logic.
    
    As more logic is moved over, it might be wise to re-write these
    tests again.

--- apiserver/facades/controller/migrationtarget/package_test.go
+++ apiserver/facades/controller/migrationtarget/package_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/juju/juju/testing"
 )
 
-//go:generate go run go.uber.org/mock/mockgen -package migrationtarget_test -destination domain_mock_test.go github.com/juju/juju/apiserver/facades/controller/migrationtarget ControllerConfigService,ExternalControllerService,UpgradeService
+//go:generate go run go.uber.org/mock/mockgen -package migrationtarget_test -destination domain_mock_test.go github.com/juju/juju/apiserver/facades/controller/migrationtarget ControllerConfigService,ExternalControllerService,UpgradeService,ModelImporter
 //go:generate go run go.uber.org/mock/mockgen -package migrationtarget_test -destination credential_mock_test.go github.com/juju/juju/domain/credential/service CredentialValidator
 
 func TestAll(t *stdtesting.T) {
