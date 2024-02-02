62f56f53d30c713127c8fa11dfaedaf4e2df35f5
Heather Lanigan <heather.lanigan@canonical.com>
2024-02-02 15:44:15 -0500

    Replace fake state with MockState implementation.
    The goal of these unit tests are to ensure the service is working as
    expected. The validation occurs where needed and the state methods are
    called appropriately at the correct times. The response of the state
    methods has no impact on how this service layer responds.

--- domain/permission/service/package_test.go
+++ domain/permission/service/package_test.go
@@ -9,6 +9,8 @@ import (
 	gc "gopkg.in/check.v1"
 )
 
+//go:generate go run go.uber.org/mock/mockgen -package service -destination state_mock_test.go github.com/juju/juju/domain/permission/service State
+
 func TestPackage(t *testing.T) {
 	gc.TestingT(t)
 }
--- domain/permission/service/service_test.go
+++ domain/permission/service/service_test.go
@@ -10,28 +10,29 @@ import (
 	"github.com/juju/names/v5"
 	"github.com/juju/testing"
 	jc "github.com/juju/testing/checkers"
+	"go.uber.org/mock/gomock"
 	gc "gopkg.in/check.v1"
 
 	"github.com/juju/juju/core/permission"
-	"github.com/juju/juju/core/user"
-	"github.com/juju/juju/domain/model"
 )
 
 type serviceSuite struct {
 	testing.IsolationSuite
 
-	modelUUID model.UUID
-	userUUID  user.UUID
-	service   *Service
+	state *MockState
 }
 
 var _ = gc.Suite(&serviceSuite{})
 
-func (s *serviceSuite) SetUpSuite(c *gc.C) {
-	s.service = NewService(&fakeState{})
+func (s *serviceSuite) setupMocks(c *gc.C) *gomock.Controller {
+	ctrl := gomock.NewController(c)
+	s.state = NewMockState(ctrl)
+	return ctrl
 }
 
 func (s *serviceSuite) TestCreatePermission(c *gc.C) {
+	defer s.setupMocks(c).Finish()
+	s.state.EXPECT().CreatePermission(gomock.Any(), gomock.AssignableToTypeOf(UserAccessSpec{})).Return(permission.UserAccess{}, nil)
 
 	spec := UserAccessSpec{
 		User:       names.NewUserTag("testme"),
@@ -39,33 +40,51 @@ func (s *serviceSuite) TestCreatePermission(c *gc.C) {
 		Access:     permission.AddModelAccess,
 		AccessType: permission.Cloud,
 	}
-	_, err := s.service.CreatePermission(context.Background(), spec)
+	_, err := NewService(s.state).CreatePermission(context.Background(), spec)
 	c.Assert(err, jc.ErrorIsNil)
 }
 
 func (s *serviceSuite) TestCreatePermissionError(c *gc.C) {
+	defer s.setupMocks(c).Finish()
+
 	spec := UserAccessSpec{
 		User:       names.NewUserTag("testme"),
 		Target:     names.NewCloudTag("aws"),
 		Access:     permission.ReadAccess,
 		AccessType: permission.Cloud,
 	}
-	_, err := s.service.CreatePermission(context.Background(), spec)
+	_, err := NewService(s.state).CreatePermission(context.Background(), spec)
 	c.Assert(err, jc.ErrorIs, errors.NotValid)
 }
 
+var targetTagCond = func(obtained any) bool {
+	_, ok1 := obtained.(names.CloudTag)
+	_, ok2 := obtained.(names.ControllerTag)
+	_, ok3 := obtained.(names.ModelTag)
+	_, ok4 := obtained.(names.ApplicationOfferTag)
+	return ok1 || ok2 || ok3 || ok4
+}
+
 func (s *serviceSuite) TestDeletePermission(c *gc.C) {
-	err := s.service.DeletePermission(context.Background(), names.NewUserTag("testme"), names.NewCloudTag("aws"))
+	defer s.setupMocks(c).Finish()
+	s.state.EXPECT().DeletePermission(gomock.Any(), gomock.AssignableToTypeOf(names.UserTag{}), gomock.Cond(targetTagCond)).Return(nil)
+
+	err := NewService(s.state).DeletePermission(context.Background(), names.NewUserTag("testme"), names.NewCloudTag("aws"))
 	c.Assert(err, jc.ErrorIsNil)
 }
 
 func (s *serviceSuite) TestDeletePermissionError(c *gc.C) {
-	err := s.service.DeletePermission(context.Background(), names.NewUserTag("testme"), names.NewMachineTag("7"))
+	defer s.setupMocks(c).Finish()
+
+	err := NewService(s.state).DeletePermission(context.Background(), names.NewUserTag("testme"), names.NewMachineTag("7"))
 	c.Assert(errors.Is(err, errors.NotValid), jc.IsTrue, gc.Commentf("%+v", err))
 }
 
 func (s *serviceSuite) TestUpdatePermission(c *gc.C) {
-	err := s.service.UpdatePermission(
+	defer s.setupMocks(c).Finish()
+	s.state.EXPECT().UpdatePermission(gomock.Any(), gomock.AssignableToTypeOf(names.UserTag{}), gomock.Cond(targetTagCond), permission.AddModelAccess).Return(nil)
+
+	err := NewService(s.state).UpdatePermission(
 		context.Background(),
 		names.NewUserTag("testme"),
 		names.NewCloudTag("aws"),
@@ -74,7 +93,9 @@ func (s *serviceSuite) TestUpdatePermission(c *gc.C) {
 }
 
 func (s *serviceSuite) TestUpdatePermissionError(c *gc.C) {
-	err := s.service.UpdatePermission(
+	defer s.setupMocks(c).Finish()
+
+	err := NewService(s.state).UpdatePermission(
 		context.Background(),
 		names.NewUserTag("testme"),
 		names.NewUnitTag("aws/0"),
@@ -83,7 +104,10 @@ func (s *serviceSuite) TestUpdatePermissionError(c *gc.C) {
 }
 
 func (s *serviceSuite) TestReadUserAccessForTarget(c *gc.C) {
-	_, err := s.service.ReadUserAccessForTarget(
+	defer s.setupMocks(c).Finish()
+	s.state.EXPECT().ReadUserAccessForTarget(gomock.Any(), gomock.AssignableToTypeOf(names.UserTag{}), gomock.Cond(targetTagCond)).Return(permission.UserAccess{}, nil)
+
+	_, err := NewService(s.state).ReadUserAccessForTarget(
 		context.Background(),
 		names.NewUserTag("testme"),
 		names.NewCloudTag("aws"))
@@ -91,7 +115,9 @@ func (s *serviceSuite) TestReadUserAccessForTarget(c *gc.C) {
 }
 
 func (s *serviceSuite) TestReadUserAccessForTargetError(c *gc.C) {
-	_, err := s.service.ReadUserAccessForTarget(
+	defer s.setupMocks(c).Finish()
+
+	_, err := NewService(s.state).ReadUserAccessForTarget(
 		context.Background(),
 		names.NewUserTag("testme"),
 		names.NewUnitTag("aws/0"))
@@ -99,7 +125,10 @@ func (s *serviceSuite) TestReadUserAccessForTargetError(c *gc.C) {
 }
 
 func (s *serviceSuite) TestReadUserAccessLevelForTarget(c *gc.C) {
-	_, err := s.service.ReadUserAccessLevelForTarget(
+	defer s.setupMocks(c).Finish()
+	s.state.EXPECT().ReadUserAccessLevelForTarget(gomock.Any(), gomock.AssignableToTypeOf(names.UserTag{}), gomock.Cond(targetTagCond)).Return(permission.NoAccess, nil)
+
+	_, err := NewService(s.state).ReadUserAccessLevelForTarget(
 		context.Background(),
 		names.NewUserTag("testme"),
 		names.NewCloudTag("aws"))
@@ -107,7 +136,9 @@ func (s *serviceSuite) TestReadUserAccessLevelForTarget(c *gc.C) {
 }
 
 func (s *serviceSuite) TestReadUserAccessLevelForTargetError(c *gc.C) {
-	_, err := s.service.ReadUserAccessForTarget(
+	defer s.setupMocks(c).Finish()
+
+	_, err := NewService(s.state).ReadUserAccessForTarget(
 		context.Background(),
 		names.NewUserTag("testme"),
 		names.NewUnitTag("aws/0"))
@@ -115,57 +146,30 @@ func (s *serviceSuite) TestReadUserAccessLevelForTargetError(c *gc.C) {
 }
 
 func (s *serviceSuite) TestReadAllUserAccessForTarget(c *gc.C) {
-	_, err := s.service.ReadAllUserAccessForTarget(
+	defer s.setupMocks(c).Finish()
+	s.state.EXPECT().ReadAllUserAccessForTarget(gomock.Any(), gomock.Cond(targetTagCond)).Return(nil, nil)
+
+	_, err := NewService(s.state).ReadAllUserAccessForTarget(
 		context.Background(),
 		names.NewCloudTag("aws"))
 	c.Assert(err, jc.ErrorIsNil)
 }
 
 func (s *serviceSuite) TestReadAllUserAccessForTargetError(c *gc.C) {
-	_, err := s.service.ReadAllUserAccessForTarget(
+	defer s.setupMocks(c).Finish()
+
+	_, err := NewService(s.state).ReadAllUserAccessForTarget(
 		context.Background(),
 		names.NewUnitTag("aws/0"))
 	c.Assert(errors.Is(err, errors.NotValid), jc.IsTrue, gc.Commentf("%+v", err))
 }
 
 func (s *serviceSuite) TestReadAllUserAccessForUser(c *gc.C) {
-	_, err := s.service.ReadAllUserAccessForUser(
+	defer s.setupMocks(c).Finish()
+	s.state.EXPECT().ReadAllUserAccessForUser(gomock.Any(), gomock.AssignableToTypeOf(names.UserTag{})).Return(nil, nil)
+
+	_, err := NewService(s.state).ReadAllUserAccessForUser(
 		context.Background(),
 		names.NewUserTag("testme"))
 	c.Assert(err, jc.ErrorIsNil)
-}
-
-type fakeState struct {
-}
-
-func (f *fakeState) CreatePermission(_ context.Context, _ UserAccessSpec) (permission.UserAccess, error) {
-	return permission.UserAccess{}, nil
-}
-
-func (f *fakeState) DeletePermission(_ context.Context, _ names.UserTag, _ names.Tag) error {
-	return nil
-}
-
-func (f *fakeState) UpdatePermission(_ context.Context, _ names.UserTag, _ names.Tag, _ permission.Access) error {
-	return nil
-}
-
-func (f *fakeState) ReadUserAccessForTarget(_ context.Context, _ names.UserTag, _ names.Tag) (permission.UserAccess, error) {
-	return permission.UserAccess{}, nil
-}
-
-func (f *fakeState) ReadAllUserAccessForUser(_ context.Context, _ names.UserTag) ([]permission.UserAccess, error) {
-	return nil, nil
-}
-
-func (f *fakeState) ReadAllUserAccessForTarget(_ context.Context, _ names.Tag) ([]permission.UserAccess, error) {
-	return nil, nil
-}
-
-func (f *fakeState) ReadAllAccessTypeForUser(_ context.Context, _ names.UserTag, _ permission.AccessType) ([]permission.UserAccess, error) {
-	return nil, nil
-}
-
-func (f *fakeState) ReadUserAccessLevelForTarget(_ context.Context, _ names.UserTag, _ names.Tag) (permission.Access, error) {
-	return permission.ReadAccess, nil
 }
