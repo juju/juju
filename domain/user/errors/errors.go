3091823823664f14104be23138adfee5fda1850d
Joseph Phillips <joe.phillips@canonical.com>
2024-02-13 11:17:01 +0100

    Adds a new AddUserArg type for supply to user creation. Depending on wihch fields are populated, creating with a password or activation key will be invoked.

--- domain/user/errors/errors.go
+++ domain/user/errors/errors.go
@@ -12,27 +12,27 @@ const (
 	// not exist.
 	NotFound = errors.ConstError("user not found")
 
-	// UserCreatorUUIDNotFound describes an error that occurs when a user's creator UUID,
+	// CreatorUUIDNotFound describes an error that occurs when a user's creator UUID,
 	// the user that created the user in question, does not exist.
-	UserCreatorUUIDNotFound = errors.ConstError("user creator UUID not found")
+	CreatorUUIDNotFound = errors.ConstError("user creator UUID not found")
 
-	// UsernameNotValid describes an error that occurs when a supplied username
+	// UserNameNotValid describes an error that occurs when a supplied username
 	// is not valid.
 	// Examples of this include illegal characters or usernames that are not of
 	// sufficient length.
-	UsernameNotValid = errors.ConstError("username not valid")
+	UserNameNotValid = errors.ConstError("username not valid")
 
-	// UserUUIDNotValid describes an error that occurs when a supplied UUID is not
+	// UUIDNotValid describes an error that occurs when a supplied UUID is not
 	// valid.
-	UserUUIDNotValid = errors.ConstError("User UUID not valid")
+	UUIDNotValid = errors.ConstError("User UUID not valid")
 
 	// AlreadyExists describes an error that occurs when the user being
 	// created already exists.
 	AlreadyExists = errors.ConstError("user already exists")
 
-	// UserAuthenticationDisabled describes an error that occurs when the users
+	// AuthenticationDisabled describes an error that occurs when the user's
 	// authentication mechanisms are disabled.
-	UserAuthenticationDisabled = errors.ConstError("user authentication disabled")
+	AuthenticationDisabled = errors.ConstError("user authentication disabled")
 
 	// Unauthorized describes an error that occurs when the user does not have
 	// the required permissions to perform an action.
