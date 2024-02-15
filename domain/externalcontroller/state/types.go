765e72bedba01fc5689dbeedba3eda0bcbd57a08
Alastair Flynn <af@aflynn.uk>
2024-02-15 10:21:24 +0000

    Move to using SQLair in externalController
    Statements are prepared outside transactions.

--- domain/externalcontroller/state/types.go
+++ domain/externalcontroller/state/types.go
@@ -6,15 +6,17 @@ package state
 import (
 	"database/sql"
 
+	"github.com/canonical/sqlair"
+	"github.com/juju/errors"
 	"github.com/juju/names/v5"
 
 	"github.com/juju/juju/core/crossmodel"
 )
 
-// ExternalController represents a single row from the database when
+// Controller represents a single row from the database when
 // external_controller is joined with external_controller_address and
 // external_model.
-type ExternalController struct {
+type Controller struct {
 	// ID is the controller UUID.
 	ID string `db:"uuid"`
 
@@ -33,14 +35,110 @@ type ExternalController struct {
 	ModelUUID sql.NullString `db:"model"`
 }
 
-type ExternalControllers []ExternalController
+// Address represents a single row from external_controller_address.
+type Address struct {
+	// ID is the address UUID.
+	ID string `db:"uuid"`
 
-// ToControllerInfo ExternalControllers to a slice of crossmodel.ControllerInfo
+	// Addr holds a single host:port value for
+	// the external controller's API server.
+	Addr string `db:"address"`
+
+	// ControllerUUID holds the controller uuid.
+	ControllerUUID string `db:"controller_uuid"`
+}
+
+// Model represents a single row from external_model.
+type Model struct {
+	// ID is the model UUID.
+	ID string `db:"uuid"`
+
+	// ControllerUUID holds the controller uuid.
+	ControllerUUID string `db:"controller_uuid"`
+}
+
+type Controllers []Controller
+
+// uuids is a slice of controller uuids from the database.
+type uuids []string
+
+// updateStatements contains the prepared statements used by
+// updateExternalControllerTx.
+type updateStatements struct {
+	// upsertController upserts the controller alias and certificate in
+	// external_controller.
+	upsertController *sqlair.Statement
+	// deleteUnusedAddresses removes addresses not passed in the uuids slice
+	// from external_controller_addresses.
+	deleteUnusedAddresses *sqlair.Statement
+	// insertNewAddresses adds new addresses to external_controller_addresses,
+	// leaving existing ones untouched.
+	insertNewAddresses *sqlair.Statement
+	// upsertModel upserts the model uuids associated with the controller in
+	// external_model.
+	upsertModel *sqlair.Statement
+}
+
+// NewUpdateStatements prepares the SQLair statements used by
+// updateExternalControllerTx.
+func NewUpdateStatements() (*updateStatements, error) {
+	upsertControllerQuery := `
+INSERT INTO external_controller (uuid, alias, ca_cert)
+VALUES ($Controller.uuid, $Controller.alias, $Controller.ca_cert)
+  ON CONFLICT(uuid) DO UPDATE SET alias=excluded.alias, ca_cert=excluded.ca_cert
+`
+	upsertControllerStmt, err := sqlair.Prepare(upsertControllerQuery, Controller{})
+	if err != nil {
+		return nil, errors.Annotatef(err, "preparing %q:", upsertControllerQuery)
+	}
+
+	deleteUnusedAddressesQuery := `
+DELETE FROM external_controller_address
+WHERE  controller_uuid = $Controller.uuid
+AND    address NOT IN ($uuids[:])
+`
+	deleteUnusedAddressesStmt, err := sqlair.Prepare(deleteUnusedAddressesQuery, Controller{}, uuids{})
+	if err != nil {
+		return nil, errors.Annotatef(err, "preparing %q:", deleteUnusedAddressesQuery)
+	}
+
+	insertNewAddressesQuery := `
+INSERT INTO external_controller_address (uuid, controller_uuid, address)
+VALUES ($Address.uuid, $Address.controller_uuid, $Address.address)
+  ON CONFLICT(controller_uuid, address) DO NOTHING
+`
+	insertNewAddressesStmt, err := sqlair.Prepare(insertNewAddressesQuery, Address{})
+	if err != nil {
+		return nil, errors.Annotatef(err, "preparing %q:", insertNewAddressesQuery)
+	}
+
+	// TODO (manadart 2023-05-13): Check current implementation and see if
+	// we need to delete models as we do for addresses, or whether this
+	// (additive) approach is what we have now.
+	upsertModelQuery := `
+INSERT INTO external_model (uuid, controller_uuid)
+VALUES ($Model.uuid, $Model.controller_uuid)
+  ON CONFLICT(uuid) DO UPDATE SET controller_uuid=excluded.controller_uuid
+`
+	upsertModelStmt, err := sqlair.Prepare(upsertModelQuery, Model{})
+	if err != nil {
+		return nil, errors.Annotatef(err, "preparing %q:", upsertModelQuery)
+	}
+
+	return &updateStatements{
+		upsertController:      upsertControllerStmt,
+		deleteUnusedAddresses: deleteUnusedAddressesStmt,
+		insertNewAddresses:    insertNewAddressesStmt,
+		upsertModel:           upsertModelStmt,
+	}, nil
+}
+
+// ToControllerInfo Controllers to a slice of crossmodel.ControllerInfo
 // structs. This method makes sure only unique models and addresses are mapped
 // and flattens them into each controller.
 // Order of addresses, models and the resulting crossmodel.ControllerInfo
 // elements are not guaranteed, no sorting is applied.
-func (e ExternalControllers) ToControllerInfo() []crossmodel.ControllerInfo {
+func (e Controllers) ToControllerInfo() []crossmodel.ControllerInfo {
 	var resultControllers []crossmodel.ControllerInfo
 	// Prepare structs for unique models and addresses for each
 	// controller.
