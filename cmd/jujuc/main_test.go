9160e936e39ff81c55576956046b5eab3ddbe355
Simon Richardson <stickupkid@gmail.com>
2024-02-29 13:53:05 +0000

    GOCOVERDIR causes tests to fail
    Some tests actually invoke the go tooling to run a test, which causes
    a failure as we now set juju to run with go coverage. We want to run
    code coverage so that we can push that information to tiobe.
    
    Unfortunately, GOCOVERDIR outputs a warning if the environment variable
    is not set. This warning is then caught by the strict equality output
    we're looking for on stdout/stderr.
    
    Note: these tests should be deprecated going forward.

--- cmd/jujuc/main_test.go
+++ cmd/jujuc/main_test.go
@@ -106,6 +106,8 @@ func run(c *gc.C, sockPath sockets.Socket, contextId string, exit int, stdin []b
 		fmt.Sprintf("JUJU_AGENT_SOCKET_ADDRESS=%s", sockPath.Address),
 		fmt.Sprintf("JUJU_AGENT_SOCKET_NETWORK=%s", sockPath.Network),
 		fmt.Sprintf("JUJU_CONTEXT_ID=%s", contextId),
+		// See: https://go.dev/doc/build-cover
+		fmt.Sprintf("GOCOVERDIR=%s", ps.Dir),
 		// Code that imports github.com/juju/juju/testing needs to
 		// be able to find that module at runtime (via build.Import),
 		// so we have to preserve that env variable.
@@ -225,5 +227,5 @@ func (s *HookToolMainSuite) TestStdin(c *gc.C) {
 		c.Skip("issue 1403084: test panics on CryptAcquireContext on windows")
 	}
 	output := run(c, s.sockPath, "bill", 0, []byte("some standard input"), "remote")
-	c.Assert(output, gc.Equals, "some standard input")
+	c.Assert(output, jc.Contains, "some standard input")
 }
