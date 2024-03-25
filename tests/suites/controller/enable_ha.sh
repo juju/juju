7b3f99f0b8b41f4c9aac42c6052a765b2d9ce03a
Jack Shaw <jack.shaw@canonical.com>
2024-03-25 09:42:11 +0000

    Back enable-ha integration test with microceph storage
    Use a (currently edge) microceph charm, integrated with the ceph-radosgw
    charm to provide an s3 endpoint the controller can use for distrbuted
    block storage

--- tests/suites/controller/enable_ha.sh
+++ tests/suites/controller/enable_ha.sh
@@ -85,6 +85,8 @@ run_enable_ha() {
 
 	juju deploy jameinel-ubuntu-lite
 
+	enable_microceph_backed_storage
+
 	juju enable-ha
 
 	wait_for_controller_machines 3
@@ -96,13 +98,15 @@ run_enable_ha() {
 	# remove-machine will fail. Wait for the config to be
 	# settled before trying to tear down.
 	juju switch controller
-	wait_for "controller" "$(idle_condition "controller" 0 0)"
-	wait_for "controller" "$(idle_condition "controller" 0 1)"
-	wait_for "controller" "$(idle_condition "controller" 0 2)"
+	wait_for "controller" "$(idle_condition "controller" 1 0)"
+	wait_for "controller" "$(idle_condition "controller" 1 1)"
+	wait_for "controller" "$(idle_condition "controller" 1 2)"
 
 	juju switch enable-ha
-	juju remove-machine -m controller 1
-	juju remove-machine -m controller 2
+	controller_1=$(juju status -m controller --format json | jq -r '.applications.controller.units["controller/1"].machine')
+	juju remove-machine -m controller "${controller_1}"
+	controller_2=$(juju status -m controller --format json | jq -r '.applications.controller.units["controller/2"].machine')
+	juju remove-machine -m controller "${controller_2}"
 
 	wait_for_controller_no_leader
 	wait_for_controller_leader
