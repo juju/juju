318c094a25dedf7d10767131e269df6e2dbcf27a
jujubot <juju-qa-aws@canonical.com>
2024-04-08 07:47:33 +0000

    Increment juju to 2.9.50

--- scripts/win-installer/setup.iss
+++ scripts/win-installer/setup.iss
@@ -4,7 +4,7 @@
 #if GetEnv('JUJU_VERSION') != ""
 #define MyAppVersion=GetEnv('JUJU_VERSION')
 #else
-#define MyAppVersion="2.9.49"
+#define MyAppVersion="2.9.50"
 #endif
 
 #define MyAppName "Juju"
--- version/version.go
+++ version/version.go
@@ -19,7 +19,7 @@ import (
 // The presence and format of this constant is very important.
 // The debian/rules build recipe uses this value for the version
 // number of the release package.
-const version = "2.9.49"
+const version = "2.9.50"
 
 // UserAgentVersion defines a user agent version used for communication for
 // outside resources.
