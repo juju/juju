2a269e33a6b6a1edfe4827f4a900a6ef958ba7a0
Heather Lanigan <heather.lanigan@canonical.com>
2024-04-23 15:30:07 -0400

    Add TestRevokeAccess.
    Test the different permutations of revoking access to an object for the
    access. Replaces some tests in the controller, model and offer facades
    around revoking access there. These tests use to do a full round trip to
    state. Now they are mocked and the actual functionality is in the Access
    domain.

--- core/permission/user_test.go
+++ core/permission/user_test.go
@@ -18,3 +18,50 @@ func (s *userSuite) TestControllerForAccess(c *gc.C) {
 	spec := permission.ControllerForAccess(permission.ReadAccess)
 	c.Assert(spec.Target.Key, gc.Equals, database.ControllerNS)
 }
+
+var validateRevokeAccessTest = []struct {
+	spec     permission.AccessSpec
+	expected permission.Access
+}{
+	{
+		spec:     permission.AccessSpec{Target: permission.ID{ObjectType: permission.Model}, Access: permission.AddModelAccess},
+		expected: permission.WriteAccess,
+	}, {
+		spec:     permission.AccessSpec{Target: permission.ID{ObjectType: permission.Model}, Access: permission.WriteAccess},
+		expected: permission.ReadAccess,
+	}, {
+		spec:     permission.AccessSpec{Target: permission.ID{ObjectType: permission.Model}, Access: permission.ReadAccess},
+		expected: permission.NoAccess,
+	}, {
+		spec:     permission.AccessSpec{Target: permission.ID{ObjectType: permission.Offer}, Access: permission.AdminAccess},
+		expected: permission.ConsumeAccess,
+	}, {
+		spec:     permission.AccessSpec{Target: permission.ID{ObjectType: permission.Offer}, Access: permission.ConsumeAccess},
+		expected: permission.ReadAccess,
+	}, {
+		spec:     permission.AccessSpec{Target: permission.ID{ObjectType: permission.Offer}, Access: permission.ReadAccess},
+		expected: permission.NoAccess,
+	}, {
+		spec:     permission.AccessSpec{Target: permission.ID{ObjectType: permission.Controller}, Access: permission.SuperuserAccess},
+		expected: permission.LoginAccess,
+	}, {
+		spec:     permission.AccessSpec{Target: permission.ID{ObjectType: permission.Controller}, Access: permission.LoginAccess},
+		expected: permission.NoAccess,
+	}, {
+		spec:     permission.AccessSpec{Target: permission.ID{ObjectType: permission.Cloud}, Access: permission.AdminAccess},
+		expected: permission.AddModelAccess,
+	}, {
+		spec:     permission.AccessSpec{Target: permission.ID{ObjectType: permission.Cloud}, Access: permission.AddModelAccess},
+		expected: permission.NoAccess,
+	},
+}
+
+func (*userSuite) TestRevokeAccess(c *gc.C) {
+	size := len(validateRevokeAccessTest)
+	for i, test := range validateRevokeAccessTest {
+		c.Logf("Running test %d of %d", i, size)
+		obtained := test.spec.RevokeAccess()
+		c.Check(obtained, gc.Equals, test.expected,
+			gc.Commentf("revoke %q on %q, expect %q", test.spec.Access, test.spec.Target.ObjectType, test.expected))
+	}
+}
