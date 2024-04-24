6e89577f7798f834fa5ac328ba5bc0e8ee41b08b
Simon Richardson <stickupkid@gmail.com>
2024-04-24 17:13:43 +0100

    Create the basic Logger interface
    For better or worse we just want a centeralised logging interface.
    Without re-writing the whole of Juju we should just standardise on
    what we have. We can then move to a better one at a later date.
    
    I suspect we could even still use this interface and replace loggo
    with slog at a later date.

--- core/logger/interface.go
+++ core/logger/interface.go
@@ -11,6 +11,55 @@ import (
 	"github.com/juju/names/v5"
 )
 
+// Level represents the log level.
+type Level uint32
+
+// Logger is an interface that provides logging methods.
+type Logger interface {
+	// Critical logs a message at the critical level.
+	Criticalf(msg string, args ...any)
+
+	// Error logs a message at the error level.
+	Errorf(msg string, args ...any)
+
+	// Warning logs a message at the warning level.
+	Warningf(msg string, args ...any)
+
+	// Info logs a message at the info level.
+	Infof(msg string, args ...any)
+
+	// Debug logs a message at the debug level.
+	Debugf(msg string, args ...any)
+
+	// Trace logs a message at the trace level.
+	Tracef(msg string, args ...any)
+
+	// Child returns a new logger with the given name.
+	Child(name string) Logger
+
+	// ChildWithTags returns a new logger with the given name and tags.
+	ChildWithTags(string, ...string) Logger
+
+	// IsEnabled returns true if the given level is enabled for the logger.
+	IsErrorEnabled() bool
+
+	// IsWarningEnabled returns true if the warning level is enabled for the
+	// logger.
+	IsWarningEnabled() bool
+
+	// IsInfoEnabled returns true if the info level is enabled for the logger.
+	IsInfoEnabled() bool
+
+	// IsDebugEnabled returns true if the debug level is enabled for the logger.
+	IsDebugEnabled() bool
+
+	// IsTraceEnabled returns true if the trace level is enabled for the logger.
+	IsTraceEnabled() bool
+
+	// IsLevelEnabled returns true if the given level is enabled for the logger.
+	IsLevelEnabled(Level) bool
+}
+
 // LogWriter provides an interface for writing log records.
 type LogWriter interface {
 	// Log writes the given log records to the logger's storage.
