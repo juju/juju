a5a80b944fcec2fec32d847278be6b5d9b6716e2
Thomas Miller <thomas.miller@canonical.com>
2024-05-02 11:51:21 +1000

    Introduces an easier model config set for bootstrap.
    This change now makes it so bootstrap model config takes raw values and
    not a config object that requires specific values to be pre set. What
    this allows us to do is have model config bootstrap correctly construct
    a models config without the user having to understand what makes a valid
    model config.
    
    This is important for facade testing to work correctly as well.

--- domain/modelconfig/bootstrap/bootstrap_test.go
+++ domain/modelconfig/bootstrap/bootstrap_test.go
@@ -9,13 +9,28 @@ import (
 	jc "github.com/juju/testing/checkers"
 	gc "gopkg.in/check.v1"
 
+	"github.com/juju/juju/cloud"
+	"github.com/juju/juju/core/credential"
+	coremodel "github.com/juju/juju/core/model"
+	"github.com/juju/juju/core/permission"
+	coreuser "github.com/juju/juju/core/user"
+	userbootstrap "github.com/juju/juju/domain/access/bootstrap"
+	cloudbootstrap "github.com/juju/juju/domain/cloud/bootstrap"
+	credentialbootstrap "github.com/juju/juju/domain/credential/bootstrap"
+	"github.com/juju/juju/domain/model"
+	modelbootstrap "github.com/juju/juju/domain/model/bootstrap"
+	"github.com/juju/juju/domain/model/state/testing"
 	"github.com/juju/juju/domain/modeldefaults"
 	schematesting "github.com/juju/juju/domain/schema/testing"
 	"github.com/juju/juju/environs/config"
+	jujuversion "github.com/juju/juju/version"
 )
 
 type bootstrapSuite struct {
+	schematesting.ControllerSuite
 	schematesting.ModelSuite
+
+	modelID coremodel.UUID
 }
 
 type ModelDefaultsProviderFunc func(context.Context) (modeldefaults.Defaults, error)
@@ -28,6 +43,55 @@ func (f ModelDefaultsProviderFunc) ModelDefaults(
 	return f(c)
 }
 
+func (s *bootstrapSuite) SetUpTest(c *gc.C) {
+	s.ControllerSuite.SetUpTest(c)
+	s.ModelSuite.SetUpTest(c)
+
+	userID, fn := userbootstrap.AddUser(coreuser.AdminUserName, permission.ControllerForAccess(permission.SuperuserAccess))
+	err := fn(context.Background(), s.ControllerTxnRunner(), s.ControllerSuite.NoopTxnRunner())
+	c.Assert(err, jc.ErrorIsNil)
+
+	cloudName := "test"
+	fn = cloudbootstrap.InsertCloud(coreuser.AdminUserName, cloud.Cloud{
+		Name:      cloudName,
+		Type:      "ec2",
+		AuthTypes: cloud.AuthTypes{cloud.EmptyAuthType},
+	})
+
+	err = fn(context.Background(), s.ControllerTxnRunner(), s.ControllerSuite.NoopTxnRunner())
+	c.Assert(err, jc.ErrorIsNil)
+
+	credentialName := "test"
+	fn = credentialbootstrap.InsertCredential(credential.Key{
+		Cloud: cloudName,
+		Name:  credentialName,
+		Owner: coreuser.AdminUserName,
+	},
+		cloud.NewCredential(cloud.EmptyAuthType, nil),
+	)
+
+	err = fn(context.Background(), s.ControllerTxnRunner(), s.ControllerSuite.NoopTxnRunner())
+	c.Assert(err, jc.ErrorIsNil)
+
+	testing.CreateInternalSecretBackend(c, s.ControllerTxnRunner())
+
+	id, fn := modelbootstrap.CreateModel(model.ModelCreationArgs{
+		AgentVersion: jujuversion.Current,
+		Cloud:        cloudName,
+		Credential: credential.Key{
+			Cloud: cloudName,
+			Name:  credentialName,
+			Owner: coreuser.AdminUserName,
+		},
+		Name:  "test",
+		Owner: userID,
+	})
+	s.modelID = id
+
+	err = fn(context.Background(), s.ControllerTxnRunner(), s.ControllerSuite.NoopTxnRunner())
+	c.Assert(err, jc.ErrorIsNil)
+}
+
 func (s *bootstrapSuite) TestSetModelConfig(c *gc.C) {
 	var defaults ModelDefaultsProviderFunc = func(_ context.Context) (modeldefaults.Defaults, error) {
 		return modeldefaults.Defaults{
@@ -38,17 +102,17 @@ func (s *bootstrapSuite) TestSetModelConfig(c *gc.C) {
 		}, nil
 	}
 
-	cfg, err := config.New(config.NoDefaults, map[string]any{
-		"name": "wallyworld",
-		"uuid": "a677bdfd-3c96-46b2-912f-38e25faceaf7",
-		"type": "sometype",
-	})
+	//cfg, err := config.New(config.NoDefaults, map[string]any{
+	//	"name": "wallyworld",
+	//	"uuid": "a677bdfd-3c96-46b2-912f-38e25faceaf7",
+	//	"type": "sometype",
+	//})
+	//c.Assert(err, jc.ErrorIsNil)
+
+	err := SetModelConfig(s.modelID, nil, defaults)(context.Background(), s.ControllerTxnRunner(), s.ModelTxnRunner())
 	c.Assert(err, jc.ErrorIsNil)
 
-	err = SetModelConfig(cfg, defaults)(context.Background(), s.NoopTxnRunner(), s.TxnRunner())
-	c.Assert(err, jc.ErrorIsNil)
-
-	rows, err := s.DB().Query("SELECT * FROM model_config")
+	rows, err := s.ModelSuite.DB().Query("SELECT * FROM model_config")
 	c.Assert(err, jc.ErrorIsNil)
 	defer rows.Close()
 
@@ -62,9 +126,9 @@ func (s *bootstrapSuite) TestSetModelConfig(c *gc.C) {
 
 	c.Assert(rows.Err(), jc.ErrorIsNil)
 	c.Assert(configVals, jc.DeepEquals, map[string]string{
-		"name":           "wallyworld",
-		"uuid":           "a677bdfd-3c96-46b2-912f-38e25faceaf7",
-		"type":           "sometype",
+		"name":           "test",
+		"uuid":           s.modelID.String(),
+		"type":           "iaas",
 		"foo":            "bar",
 		"logging-config": "<root>=INFO",
 		"secret-backend": "auto",
