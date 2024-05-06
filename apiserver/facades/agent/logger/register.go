8fd21f051e1a96f0af8066e7c1eaee2bfafd9d28
Harry Pidcock <harry.pidcock@canonical.com>
2024-05-07 00:08:32 +1000

    Move agent/logger facades to use model config service.

--- apiserver/facades/agent/logger/register.go
+++ apiserver/facades/agent/logger/register.go
@@ -7,38 +7,19 @@ import (
 	"context"
 	"reflect"
 
-	"github.com/juju/errors"
-
-	apiservererrors "github.com/juju/juju/apiserver/errors"
 	"github.com/juju/juju/apiserver/facade"
 )
 
 // Register is called to expose a package of facades onto a given registry.
 func Register(registry facade.FacadeRegistry) {
 	registry.MustRegister("Logger", 1, func(stdCtx context.Context, ctx facade.ModelContext) (facade.Facade, error) {
-		return newLoggerAPI(ctx)
+		return newLoggerAPIV1(ctx)
 	}, reflect.TypeOf((*LoggerAPI)(nil)))
 }
 
-// newLoggerAPI creates a new server-side logger API end point.
-func newLoggerAPI(ctx facade.ModelContext) (*LoggerAPI, error) {
-	model, err := ctx.State().Model()
-	if err != nil {
-		return nil, errors.Trace(err)
-	}
-	resources := ctx.Resources()
-	authorizer := ctx.Auth()
-
-	if !authorizer.AuthMachineAgent() &&
-		!authorizer.AuthUnitAgent() &&
-		!authorizer.AuthApplicationAgent() &&
-		!authorizer.AuthModelAgent() {
-		return nil, apiservererrors.ErrPerm
-	}
-
-	return &LoggerAPI{
-		model:      model,
-		resources:  resources,
-		authorizer: authorizer,
-	}, nil
+// newLoggerAPIV1 creates a new server-side logger API end point.
+func newLoggerAPIV1(ctx facade.ModelContext) (*LoggerAPI, error) {
+	return NewLoggerAPI(ctx.Auth(),
+		ctx.WatcherRegistry(),
+		ctx.ServiceFactory().Config())
 }
