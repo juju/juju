f0a9513635a5bee4727bd650c4e88e486a7d5705
Simon Richardson <stickupkid@gmail.com>
2024-05-21 11:11:48 +0100

    Remove the local logger interface
    We no longer need the local logger interface, instead we have the
    central core/logger interface.

--- internal/http/package_test.go
+++ internal/http/package_test.go
@@ -9,7 +9,7 @@ import (
 	gc "gopkg.in/check.v1"
 )
 
-//go:generate go run go.uber.org/mock/mockgen -typed -package http -destination client_mock_test.go github.com/juju/juju/internal/http HTTPClient,RequestRecorder,Logger
+//go:generate go run go.uber.org/mock/mockgen -typed -package http -destination client_mock_test.go github.com/juju/juju/internal/http HTTPClient,RequestRecorder
 //go:generate go run go.uber.org/mock/mockgen -typed -package http -destination http_mock_test.go github.com/juju/juju/internal/http RoundTripper
 //go:generate go run go.uber.org/mock/mockgen -typed -package http -destination clock_mock_test.go github.com/juju/clock Clock
 
