dfbe3c5e53ef6033a691f97f0c65634c2714f503
Harry Pidcock <harry.pidcock@canonical.com>
2024-05-24 10:31:22 +1000

    Fix non-rootless sidecar charms by optionally setting SecurityContext.

--- worker/caasapplicationprovisioner/ops_test.go
+++ worker/caasapplicationprovisioner/ops_test.go
@@ -573,8 +573,8 @@ func (s *OpsSuite) TestAppAlive(c *gc.C) {
 				},
 				"rootless": {
 					Resource: "rootless-image",
-					Uid:      5000,
-					Gid:      5001,
+					Uid:      intPtr(5000),
+					Gid:      intPtr(5001),
 				},
 			},
 		},
@@ -612,8 +612,8 @@ func (s *OpsSuite) TestAppAlive(c *gc.C) {
 				Image: resources.DockerImageDetails{
 					RegistryPath: "rootless:foo-bar",
 				},
-				Uid: 5000,
-				Gid: 5001,
+				Uid: intPtr(5000),
+				Gid: intPtr(5001),
 			},
 		},
 		IntroductionSecret:   "123456789",
@@ -630,7 +630,7 @@ func (s *OpsSuite) TestAppAlive(c *gc.C) {
 		Devices:      []devices.KubernetesDeviceParams{},
 		Trust:        true,
 		InitialScale: 10,
-		CharmUser:    caas.RunAsRoot,
+		CharmUser:    caas.RunAsDefault,
 	}
 	gomock.InOrder(
 		facade.EXPECT().ProvisioningInfo("test").Return(pi, nil),
@@ -638,7 +638,10 @@ func (s *OpsSuite) TestAppAlive(c *gc.C) {
 		app.EXPECT().Exists().Return(ds, nil),
 		app.EXPECT().Exists().Return(caas.DeploymentState{}, nil),
 		facade.EXPECT().ApplicationOCIResources("test").Return(oci, nil),
-		app.EXPECT().Ensure(ensureParams).Return(nil),
+		app.EXPECT().Ensure(gomock.Any()).DoAndReturn(func(config caas.ApplicationConfig) error {
+			c.Check(config, gc.DeepEquals, ensureParams)
+			return nil
+		}),
 	)
 
 	err := caasapplicationprovisioner.AppOps.AppAlive("test", app, password, &lastApplied, facade, clk, s.logger)
@@ -698,4 +701,8 @@ func (s *OpsSuite) TestAppDead(c *gc.C) {
 
 	err := caasapplicationprovisioner.AppOps.AppDead("test", app, broker, facade, unitFacade, clk, s.logger)
 	c.Assert(err, jc.ErrorIsNil)
+}
+
+func intPtr(i int) *int {
+	return &i
 }
