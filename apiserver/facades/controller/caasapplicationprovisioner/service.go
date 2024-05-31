036f97069afeadfdafc51a04ae36c59b99b5e525
Jordan Barrett <90195985+barrettj12@users.noreply.github.com.>
2024-05-31 12:14:49 +1200

    caasapplicationprovisioner: replace state.Model with ModelConfigService and ModelInfoService

---
+++ apiserver/facades/controller/caasapplicationprovisioner/service.go
@@ -0,0 +1,39 @@
+// Copyright 2024 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package caasapplicationprovisioner
+
+import (
+	"context"
+
+	"github.com/juju/juju/controller"
+	"github.com/juju/juju/core/model"
+	"github.com/juju/juju/core/watcher"
+	"github.com/juju/juju/environs/config"
+)
+
+// ControllerConfigService provides the controller configuration.
+type ControllerConfigService interface {
+	// ControllerConfig returns the config values for the controller.
+	ControllerConfig(ctx context.Context) (controller.Config, error)
+	// WatchControllerConfig returns a watcher that returns keys for any
+	// changes to controller config.
+	WatchControllerConfig() (watcher.StringsWatcher, error)
+}
+
+// ModelConfigService provides access to the model configuration.
+type ModelConfigService interface {
+	// ModelConfig returns the current config for the model.
+	ModelConfig(context.Context) (*config.Config, error)
+	// Watch returns a watcher that returns keys for any changes to model
+	// config.
+	Watch() (watcher.StringsWatcher, error)
+}
+
+// ModelInfoService describe the service for interacting and reading the underlying
+// model information.
+type ModelInfoService interface {
+	// GetModelInfo returns the readonly model information for the model in
+	// question.
+	GetModelInfo(context.Context) (model.ReadOnlyModel, error)
+}
