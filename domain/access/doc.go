55bcb8cf3d71383c6ffbd4c9a1eb0e12f50807ab
Alastair Flynn <af@aflynn.uk>
2024-07-01 15:14:11 +0200

    docs: add doc.go to the access domain

---
+++ domain/access/doc.go
@@ -0,0 +1,7 @@
+// Copyright 2024 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+// Package access provides the services for managing users and permissions in
+// Juju. The users side is mainly concerned with authentication and the
+// permissions side with authorization.
+package access
