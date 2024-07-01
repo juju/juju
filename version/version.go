22f662b7d58886bae6a5e6b6b0f9801fc6d02865
jujubot <juju-qa-aws@canonical.com>
2024-07-01 14:32:41 +0000

    chore: increment juju to 3.5.3

--- version/version.go
+++ version/version.go
@@ -18,7 +18,7 @@ import (
 // The presence and format of this constant is very important.
 // The debian/rules build recipe uses this value for the version
 // number of the release package.
-const version = "3.5.2"
+const version = "3.5.3"
 
 // UserAgentVersion defines a user agent version used for communication for
 // outside resources.
