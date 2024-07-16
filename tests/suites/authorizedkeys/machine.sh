f5acf83b7de85d5235913ae510dae13649d4257c
Thomas Miller <thomas.miller@canonical.com>
2024-07-17 09:45:21 +1000

    style: apply shfmt feedback
    Apply shell format changes.

--- tests/suites/authorizedkeys/machine.sh
+++ tests/suites/authorizedkeys/machine.sh
@@ -1,27 +1,26 @@
-
 run_machine_ssh() {
 	# Echo out to ensure nice output to the test suite.
-    echo
+	echo
 
-    # Add a new machine for the test to assert sshing into.
-    juju add-machine -n 1
-    wait_for_machine_agent_status "0" "started"
+	# Add a new machine for the test to assert sshing into.
+	juju add-machine -n 1
+	wait_for_machine_agent_status "0" "started"
 
-    # Generate a new ssh key pair for this test.
-    ssh_key_file="${TEST_DIR}/machine-ssh-key"
-    ssh_key_file_pub="${ssh_key_file}.pub"
-    ssh-keygen -t ed25519 -f "$ssh_key_file" -C "isgreat@juju.is" -P ""
+	# Generate a new ssh key pair for this test.
+	ssh_key_file="${TEST_DIR}/machine-ssh-key"
+	ssh_key_file_pub="${ssh_key_file}.pub"
+	ssh-keygen -t ed25519 -f "$ssh_key_file" -C "isgreat@juju.is" -P ""
 
-    # Add the SSH key and see that it comes back out in the list of user keys.
-    juju add-ssh-key "$(cat $ssh_key_file_pub)"
+	# Add the SSH key and see that it comes back out in the list of user keys.
+	juju add-ssh-key "$(cat $ssh_key_file_pub)"
 
-    # Watch the debug log for the agent to assert that a key has been added to
-    # the machine.
-    timeout 5m juju debug-log --tail | grep -m 1 'adding ssh keys to authorised keys' || true
+	# Watch the debug log for the agent to assert that a key has been added to
+	# the machine.
+	timeout 5m juju debug-log --tail | grep -m 1 'adding ssh keys to authorised keys' || true
 
-    # Check that the test can ssh to the machine with the new keypair and run a
-    # command.
-    check_contains "$(juju ssh 0 -i \"${ssh_key_file}\" echo foobar)" "foobar"
+	# Check that the test can ssh to the machine with the new keypair and run a
+	# command.
+	check_contains "$(juju ssh 0 -i \"${ssh_key_file}\" echo foobar)" "foobar"
 }
 
 # test_machine_ssh is responsible for testing that adding authorised keys to a
