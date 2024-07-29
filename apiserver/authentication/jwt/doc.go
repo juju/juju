b0a5083c9a2570e7dfba41c650cc7710c1e73998
wallyworld <ian.booth@canonical.com>
2024-07-30 02:20:18 +1000

    chore: tweak doc as per code review suggestions

--- apiserver/authentication/jwt/doc.go
+++ apiserver/authentication/jwt/doc.go
@@ -1,27 +1,24 @@
 // Copyright 2024 Canonical Ltd.
 // Licensed under the AGPLv3, see LICENCE file for details.
 
-// Package jwt provides an authenticator which accepts a jwt as
-// the means to declare the user wanting access and the permissions
-// they have. It is used to authenticate Juju login requests against
-// the api endpoint, and raw http requests.
+// Package jwt provides an authentication mechanism whereby a JAAS-registered
+// Juju controller can accept a JAAS JWT (JSON Web Token) as the means to
+// declare a user and their permissions. The JWT is used to authenticate Juju
+// login requests against the api endpoint, and raw http requests.
 //
-// This mechanism is used when a Juju Controller is registered with JAAS.
-// JAAS has its own permissions model with finer grained RBAC permissions
-// than Juju supports. JAAS will perform the authentication step and present
-// to Juju a jwt with the username and permissions matching what Juju supports.
+// This mechanism kicks in if, after setting up the JWT in JAAS, you bootstrap your
+// controller with the 'login-token-refresh-url' controller config set to
+// the JAAS JWT refresh endpoint.
 //
 // Juju uses a [github.com/juju/juju/apiserver/authentication.PermissionDelegator]
-// instance to look up what permissions a user has. For non jwt scenarios, these
-// come from the Juju permissions model; when a jwt is used these come from the jwt
-// and the Juju permission model is empty.
+// instance to look up what permissions a user has. For non-JWT scenarios, these
+// come from the Juju permissions model. However, when a JWT is used, the Juju
+// permission model is empty, and the permissions are retrieved from the JWT.
+// JAAS provides a finer-grained permission model than Juju, and so constructs the
+// JWT to contain permissions matching those which Juju understands.
 //
-// To use this authentication mechanism, the controller is bootstrapped with the
-// 'login-token-refresh-url' attribute set to the JAAS jwt refresh endpoint.
-//
-// The other implication of configuring a controller to use this authentication
-// mechanism is that the discharge endpoint for cross model relation macaroons
-// is also set to point to JAAS, so that JAAS is the service that also validates
-// permissions for consuming offers.
+// Configuring a Juju controller to use a JWT for authentication means that the
+// discharge of cross model relation macaroons is performed by JAAS, which uses
+// its permissions model to validate access for consuming offers.
 
 package jwt
