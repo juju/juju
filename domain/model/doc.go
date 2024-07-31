f620acd3ff6c548ac7821b498732cfef1b429040
Alastair Flynn <af@aflynn.uk>
2024-07-31 11:19:00 +0100

    docs: add doc.go to model domain

---
+++ domain/model/doc.go
@@ -0,0 +1,8 @@
+// Copyright 2024 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+// Package model contains the controller model service. The controller stores
+// information about all the models it manages. This service exposes methods to
+// perform management operations such as creating, destroying and gathering
+// information about models the controller manages.
+package model
