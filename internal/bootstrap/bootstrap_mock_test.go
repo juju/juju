7f836e4d769f5de25d86291dae29f4be09ed2ce7
Harry Pidcock <harry.pidcock@canonical.com>
2024-08-02 02:43:40 +1000

    chore: regenerate mocks for internal/bootstrap

--- internal/bootstrap/bootstrap_mock_test.go
+++ internal/bootstrap/bootstrap_mock_test.go
@@ -1237,45 +1237,6 @@ func (c *MockStateBackendCharmCall) DoAndReturn(f func(string) (Charm, error)) *
 	return c
 }
 
-// Model mocks base method.
-func (m *MockStateBackend) Model() (Model, error) {
-	m.ctrl.T.Helper()
-	ret := m.ctrl.Call(m, "Model")
-	ret0, _ := ret[0].(Model)
-	ret1, _ := ret[1].(error)
-	return ret0, ret1
-}
-
-// Model indicates an expected call of Model.
-func (mr *MockStateBackendMockRecorder) Model() *MockStateBackendModelCall {
-	mr.mock.ctrl.T.Helper()
-	call := mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Model", reflect.TypeOf((*MockStateBackend)(nil).Model))
-	return &MockStateBackendModelCall{Call: call}
-}
-
-// MockStateBackendModelCall wrap *gomock.Call
-type MockStateBackendModelCall struct {
-	*gomock.Call
-}
-
-// Return rewrite *gomock.Call.Return
-func (c *MockStateBackendModelCall) Return(arg0 Model, arg1 error) *MockStateBackendModelCall {
-	c.Call = c.Call.Return(arg0, arg1)
-	return c
-}
-
-// Do rewrite *gomock.Call.Do
-func (c *MockStateBackendModelCall) Do(f func() (Model, error)) *MockStateBackendModelCall {
-	c.Call = c.Call.Do(f)
-	return c
-}
-
-// DoAndReturn rewrite *gomock.Call.DoAndReturn
-func (c *MockStateBackendModelCall) DoAndReturn(f func() (Model, error)) *MockStateBackendModelCall {
-	c.Call = c.Call.DoAndReturn(f)
-	return c
-}
-
 // Unit mocks base method.
 func (m *MockStateBackend) Unit(arg0 string) (Unit, error) {
 	m.ctrl.T.Helper()
