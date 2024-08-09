fae1faf06b11bcf06743dbbd380b3b8111862866
Heather Lanigan <heather.lanigan@canonical.com>
2024-08-09 13:15:54 -0400

    docs: distribute details of simplestreams-metadata txt and delete
    simplestreams-metadata.txt contained user facing information already
    written in the juju docs. Any delevoper needed detail has been moved to
    the related location. Then the file was deleted.

--- environs/imagemetadata/doc.go
+++ environs/imagemetadata/doc.go
@@ -1,9 +1,12 @@
 // Copyright 2022 Canonical Ltd.
 // Licensed under the AGPLv3, see LICENCE file for details.
 
-// Package imagemetadata supports locating, parsing, and filtering Ubuntu
+// Package imagemetadata supports locating, parsing, and filtering juju
 // image metadata in simplestreams format.
 //
 // See http://launchpad.net/simplestreams and in particular the doc/README
 // file in that project for more information about the file formats.
+//
+// Generally, agent binaries and related metadata are mirrored from https://streams.canonical.com/juju/tools.
+
 package imagemetadata
