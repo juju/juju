52bcecfc5e1fa34c50497cd39fe70465e437781d
Simon Richardson <stickupkid@gmail.com>
2024-08-12 17:12:58 +0100

    chore: remove remote application icon lookup
    The apiserver has the ability of serving application charm icons,
    when requested. This was all well a good, except it didn't work
    for cross-models aka remote applications on different controllers.
    There is no redirect to point the request over to another controller
    and the remote application might have been moved from there as well.
    
    To read the icon from the charm, it has to contact the underlying
    objectstore and open the charm on the controller. Then select the
    charm icon (which might not even be there!) and return that back.
    Not only does this feel like a waste of resources, but it would have
    probably been better to just pass the icon blob with the remote
    application integration.
    
    To cement the nail in the coffin, the juju-dashboard doesn't use
    this path either. Instead is favours reading the icon directly from
    charmhub[1].
    
     1. https://github.com/canonical/juju-dashboard/blob/b3fb186bef033
        f07526dd42e5249737488fb6cd9/src/components/CharmIcon/CharmIcon
        .tsx#L12-L32

--- apiserver/apiserver.go
+++ apiserver/apiserver.go
@@ -775,14 +775,6 @@ func (srv *Server) endpoints() ([]apihttp.Endpoint, error) {
 		logsinkMetricsCollectorWrapper{collector: srv.metricsCollector},
 		controllerModelUUID,
 	)
-	modelRestHandler := &modelRestHandler{
-		ctxt:              httpCtxt,
-		dataDir:           srv.dataDir,
-		objectStoreGetter: srv.shared.objectStoreGetter,
-	}
-	modelRestServer := &restHTTPHandler{
-		getHandler: modelRestHandler.ServeGet,
-	}
 	modelCharmsHandler := &charmsHandler{
 		ctxt:              httpCtxt,
 		dataDir:           srv.dataDir,
@@ -927,9 +919,6 @@ func (srv *Server) endpoints() ([]apihttp.Endpoint, error) {
 		handler:         embeddedCLIHandler,
 		tracked:         true,
 		unauthenticated: true,
-	}, {
-		pattern: modelRoutePrefix + "/rest/1.0/:entity/:name/:attribute",
-		handler: modelRestServer,
 	}, {
 		// GET /charms has no authorizer
 		pattern: modelRoutePrefix + "/charms",
