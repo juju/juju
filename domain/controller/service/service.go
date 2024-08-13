f0de80e118a25621f90d5104cd1007a0f7c1df6d
Alastair Flynn <af@aflynn.uk>
2024-08-13 17:11:17 +0100

    feat: add a controller serivce with ControllerModelUUID method
    Add a controller service to handle retrival of core controller values
    that are related to the controller but do not fall into the category of
    controller config
    
    Add a method to retrive the controllerModelUUID. Primarily to be used by
    the controlsocket worker in a future change.

---
+++ domain/controller/service/service.go
@@ -0,0 +1,32 @@
+// Copyright 2024 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package service
+
+import (
+	"context"
+
+	"github.com/juju/juju/core/model"
+)
+
+// State defines an interface for interacting with the underlying state.
+type State interface {
+	ControllerModelUUID(ctx context.Context) (model.UUID, error)
+}
+
+// Service defines a service for interacting with the underlying state.
+type Service struct {
+	st State
+}
+
+// NewService returns a new Service for interacting with the underlying state.
+func NewService(st State) *Service {
+	return &Service{
+		st: st,
+	}
+}
+
+// ControllerModelUUID returns the model UUID of the controller model.
+func (s *Service) ControllerModelUUID(ctx context.Context) (model.UUID, error) {
+	return s.st.ControllerModelUUID(ctx)
+}
