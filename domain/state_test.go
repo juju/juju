7c32daa92b132b07af6a8da4b31a91fd004a886c
Simon Richardson <stickupkid@gmail.com>
2024-08-14 17:18:59 +0100

    feat: state base run transaction
    Run starts to add the ability to call discrete state methods within
    a transaction scope, without exposing the underlying transaction
    implementation at the service layer.
    
    It solves the problem of trying to execute business logic at the
    service layer without exposing sql.Tx or sqlair.TX types. The
    DBContext interface type is a typed context.Context that can be
    provided as paramter to the Run closure.
    
    The Run method gets a DB and then creates a transaction. Within
    that transaction a dbContext is then created which captures both
    the context and the sqlair.TX. This tuple can then be passed as
    an argument to the closure. The dbContext ensures that the sqlair.TX
    can only be within the closure for the lifetime of the transaction.
    This is to prevent capturing the DBContext and using it outside of
    the Run method. This will cause a error later on in the state
    code. The tuple was deinfed in this way, as it's not possible to
    modify a generic context.Value without creating a new context, or
    creating a bespoke impelementation. Thus the tuple presents the
    best approach of ensuring safety and easy access.
    
    It should be noted that because of go invariance it's not possible
    to swap a Run closure for the following type:
    
        st.Run(ctx, func(ctx context.Context) error { ... })
    
    This will simply not compile. The Run function must be of instance
    type:
    
       st.Run(ctx, func(dbCtx DBContext) error { ... })
    
    Lastly, I've added a note that the DBContext implementation (dbContext)
    can be pooled[1] to keep the allocations of the dbContext low. That
    is not part of this changeset and can be done in future work.
    
      1. https://pkg.go.dev/sync#Pool

--- domain/state_test.go
+++ domain/state_test.go
@@ -7,6 +7,7 @@ import (
 	"context"
 
 	"github.com/canonical/sqlair"
+	jc "github.com/juju/testing/checkers"
 	gc "gopkg.in/check.v1"
 
 	schematesting "github.com/juju/juju/domain/schema/testing"
@@ -95,4 +96,93 @@ func (s *stateSuite) TestStateBasePrepareKeyClash(c *gc.C) {
 		return nil
 	})
 	c.Assert(err, gc.ErrorMatches, `cannot get result: parameter with type "domain.TestType" missing, have type with same name: "domain.TestType"`)
+}
+
+func (s *stateSuite) TestStateBaseRunTransactionExists(c *gc.C) {
+	f := s.TxnRunnerFactory()
+	base := NewStateBase(f)
+	db, err := base.DB()
+	c.Assert(err, gc.IsNil)
+	c.Assert(db, gc.NotNil)
+
+	// Ensure that the transaction is sent via the DBContext.
+
+	var tx *sqlair.TX
+	err = base.Run(context.Background(), func(c DBContext) error {
+		tx = c.(*dbContext).tx()
+		return err
+	})
+	c.Assert(err, jc.ErrorIsNil)
+
+	c.Assert(tx, gc.NotNil)
+}
+
+func (s *stateSuite) TestStateBaseRunPreventDBContextStoring(c *gc.C) {
+	f := s.TxnRunnerFactory()
+	base := NewStateBase(f)
+	db, err := base.DB()
+	c.Assert(err, gc.IsNil)
+	c.Assert(db, gc.NotNil)
+
+	// If the DBContext is stored outside of the transaction, it should
+	// not be possible to use it to perform state changes, as the sqlair.TX
+	// should be removed upon completion of the transaction.
+
+	var dbCtx DBContext
+	err = base.Run(context.Background(), func(c DBContext) error {
+		dbCtx = c
+		return err
+	})
+	c.Assert(err, jc.ErrorIsNil)
+
+	c.Assert(dbCtx, gc.NotNil)
+
+	// Convert the DBContext to the underlying type.
+	c.Check(dbCtx.(*dbContext).tx(), gc.IsNil)
+}
+
+func (s *stateSuite) TestStateBaseRunContextValue(c *gc.C) {
+	f := s.TxnRunnerFactory()
+	base := NewStateBase(f)
+	db, err := base.DB()
+	c.Assert(err, gc.IsNil)
+	c.Assert(db, gc.NotNil)
+
+	// Ensure that the context is passed through to the DBContext.
+
+	type contextKey string
+	var key contextKey = "key"
+
+	ctx := context.WithValue(context.Background(), key, "hello")
+
+	var dbCtx DBContext
+	err = base.Run(ctx, func(c DBContext) error {
+		dbCtx = c
+		return err
+	})
+	c.Assert(err, jc.ErrorIsNil)
+
+	c.Assert(dbCtx, gc.NotNil)
+	c.Check(dbCtx.Value(key), gc.Equals, "hello")
+}
+
+func (s *stateSuite) TestStateBaseRunCancel(c *gc.C) {
+	f := s.TxnRunnerFactory()
+	base := NewStateBase(f)
+	db, err := base.DB()
+	c.Assert(err, gc.IsNil)
+	c.Assert(db, gc.NotNil)
+
+	// Make sure that the context symantics are respected in terms of
+	// cancellation.
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	cancel()
+
+	err = base.Run(ctx, func(dbCtx DBContext) error {
+		c.Fatalf("should not be called")
+		return err
+	})
+	c.Assert(err, jc.ErrorIs, context.Canceled)
 }
