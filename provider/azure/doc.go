e81f92947e86d712be6509a12e69d44a8aadeb9b
wallyworld <ian.booth@canonical.com>
2024-08-16 08:51:45 +1000

    chore: tweaks as per code review

--- provider/azure/doc.go
+++ provider/azure/doc.go
@@ -6,7 +6,7 @@
 // the [github.com/juju/juju/environs.Environ] interface, which defines
 // methods for provisioning compute, network, and storage resources.
 //
-// Here we describe some key implementation details specific to the Azure provider.
+// This document describes some key implementation details specific to the Azure provider.
 //
 // # SDK
 //
@@ -14,7 +14,7 @@
 //
 // # Models
 //
-// All models, including the controller model, are created in a resource group.
+// The provider creates all models, including the controller model, in a resource group.
 // The resource group contains all artefacts for the model, including:
 //   - virtual machines
 //   - disks
@@ -24,43 +24,43 @@
 //   - availability sets
 //   - key vaults
 //
-// # Provisioning Resources
+// # Provisioning resources
 //
-// During bootstrap, a deployment API client is used to deploy an [Azure Resource Manager]
+// During bootstrap, a deployment API client is used to deploy an [Azure resource manager]
 // template which contains all compute, network, and storage resources for the controller.
-// After bootstrap, API clients are created for particular resource types defined in the SDK.
+// After bootstrap, the provider creates API clients for particular resource types defined in the SDK.
 // All API clients use the same options which define the retry and logging behaviour.
 //
 // # Resiliency
 //
 // Unlike most other providers, the Azure provider does not currently support availability zones.
-// Instead, each application has created for it an [Azure Availability Set] named after the application.
+// Instead, the provider creates for each application an [Azure availability set] named after the application.
 // Availability sets are scoped to a single Azure region. They are designed to protect against failures
-// within that region but do not provide protection against a regional outage
+// within that region but do not provide protection against a regional outage.
 //
-// # Machine Addresses
+// # Machine addresses
 //
-// Each virtual machine is assigned an [Azure Public IP Address] by default, unless the
-// "allocate-public-ip" constraint is set to false.
+// Unless the "allocate-public-ip" constraint is set to false, each virtual machine
+// is assigned an [Azure public IP address],
 //
 // # Encrypted Disks
 //
-// Where an encrypted disk is required for workload storage, an [Azure Disk Encryption Set] and
-// [Azure Key Vault] is created according to the requirements of the Juju storage pool created
-// to define the encrypted disk configuration.
+// Where an encrypted disk is required for workload storage, the provider creates an
+// [Azure disk encryption set] and [Azure key vault] according to the requirements
+// of the Juju storage pool created to define the encrypted disk configuration.
 //
-// # Exposing Applications
+// # Exposing applications
 //
-// The [Azure Network Security Group] attached to the model's [Azure Virtual Network] has rules added as
-// required to provide the allowed ingress to model applications according to what ports should
-// be opened once the application is exposed.
+// The provider adds rules to the [Azure network security group] attached to the model's
+// [Azure virtual network]. The rules provide the allowed ingress to model applications
+// according to what ports should be opened once the application is exposed.
 //
 // [Azure SDK]: https://github.com/Azure/azure-sdk-for-go
-// [Azure Resource Manager]: https://learn.microsoft.com/en-us/azure/azure-resource-manager/management/overview
-// [Azure Availability Set]: https://learn.microsoft.com/en-us/azure/virtual-machines/availability-set-overview
-// [Azure Disk Encryption Set]: https://learn.microsoft.com/en-us/azure/virtual-machines/disk-encryption
-// [Azure Key Vault]: https://learn.microsoft.com/en-us/azure/key-vault/general/overview
-// [Azure Virtual Network]: https://learn.microsoft.com/en-us/azure/virtual-network/virtual-networks-overview
-// [Azure Network Security Group]: https://learn.microsoft.com/en-us/azure/virtual-network/network-security-groups-overview
-// [Azure Public IP Address]: https://learn.microsoft.com/en-us/azure/virtual-network/ip-services/public-ip-addresses
+// [Azure resource manager]: https://learn.microsoft.com/en-us/azure/azure-resource-manager/management/overview
+// [Azure availability set]: https://learn.microsoft.com/en-us/azure/virtual-machines/availability-set-overview
+// [Azure disk encryption set]: https://learn.microsoft.com/en-us/azure/virtual-machines/disk-encryption
+// [Azure key vault]: https://learn.microsoft.com/en-us/azure/key-vault/general/overview
+// [Azure virtual network]: https://learn.microsoft.com/en-us/azure/virtual-network/virtual-networks-overview
+// [Azure network security group]: https://learn.microsoft.com/en-us/azure/virtual-network/network-security-groups-overview
+// [Azure public IP address]: https://learn.microsoft.com/en-us/azure/virtual-network/ip-services/public-ip-addresses
 package azure
