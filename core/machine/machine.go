dfc677a9a871f30f34cfe06dd7fb05395b953c36
Guillaume Fouillet <guillaume.fouillet@canonical.com>
2024-08-22 10:44:59 +0200

    refactor: move RebootAction to core/machine package
    - Before this commit, RebootAction was defined in the domain/machine package.
    - After this commit, RebootAction is moved to the core/machine package and references
      to it are updated accordingly.
    
    Signed-off-by: Guillaume Fouillet <guillaume.fouillet@canonical.com>

--- core/machine/machine.go
+++ core/machine/machine.go
@@ -25,3 +25,21 @@ func (n Name) Validate() error {
 func (i Name) String() string {
 	return string(i)
 }
+
+// RebootAction defines the action a machine should
+// take when a hook needs to reboot
+type RebootAction string
+
+const (
+	// ShouldDoNothing instructs a machine agent that no action
+	// is required on its part
+	ShouldDoNothing RebootAction = "noop"
+	// ShouldReboot instructs a machine to reboot
+	// this happens when a hook running on a machine, requests
+	// a reboot
+	ShouldReboot RebootAction = "reboot"
+	// ShouldShutdown instructs a machine to shut down. This usually
+	// happens when running inside a container, and a hook on the parent
+	// machine requests a reboot
+	ShouldShutdown RebootAction = "shutdown"
+)
