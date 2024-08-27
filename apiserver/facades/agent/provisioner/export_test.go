42467064daba72ca6a8865d5da3b74d68d152e1a
Thomas Miller <thomas.miller@canonical.com>
2024-08-27 11:48:12 +1000

    refactor: remove common model watcher tests
    This commit removes the common model watcher test suite from the
    provisioner facade. This allows us to remove the defunct test suite from
    common and another usage of state based model config.
    
    The provisioner has been swapped over to using the new service backed
    common model config methods. Because of this we have removed the one
    test we had in favour of the tests on the common package.
    
    Also performed several clean up tasks to satisfy some old todo's in the
    package.

--- apiserver/facades/agent/provisioner/export_test.go
+++ apiserver/facades/agent/provisioner/export_test.go
@@ -3,24 +3,4 @@
 
 package provisioner
 
-import (
-	"github.com/juju/juju/rpc/params"
-)
-
-// TODO (manadart 2019-09-26): This file should be deleted via these steps:
-// - Rename provisioner_test.go to provisioner_integration_test.go
-// - Relocate the provisionerMockSuite tests to provisioner_test *inside*
-//   the provisioner package.
-// - Instantiate these contexts directly instead of requiring these methods.
-
-func NewPrepareOrGetContext(result params.MachineNetworkConfigResults, maintain bool) *prepareOrGetContext {
-	return &prepareOrGetContext{result: result, maintain: maintain}
-}
-
-func NewContainerProfileContext(result params.ContainerProfileResults, modelName string) *containerProfileContext {
-	return &containerProfileContext{result: result, modelName: modelName}
-}
-
-var (
-	NewProvisionerAPIV11 = newProvisionerAPIV11
-)
+var NewProvisionerAPIV11 = newProvisionerAPIV11
--- apiserver/facades/agent/provisioner/package_test.go
+++ apiserver/facades/agent/provisioner/package_test.go
@@ -10,6 +10,8 @@ import (
 )
 
 //go:generate go run go.uber.org/mock/mockgen -typed -package provisioner -destination service_mock_test.go github.com/juju/juju/apiserver/facades/agent/provisioner AgentProvisionerService,KeyUpdaterService
+//go:generate go run go.uber.org/mock/mockgen -typed -package provisioner -destination interface_mock_test.go github.com/juju/juju/apiserver/facades/agent/provisioner Machine,BridgePolicy,Unit,Application,Charm
+//go:generate go run go.uber.org/mock/mockgen -typed -package provisioner -destination containerizer_mock_test.go github.com/juju/juju/internal/network/containerizer LinkLayerDevice
 
 func TestPackage(t *testing.T) {
 	coretesting.MgoTestPackage(t)
