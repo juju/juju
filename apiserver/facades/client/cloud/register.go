64bb21094da23ae9e9ddf676071b3d5253601ee7
Thomas Miller <thomas.miller@canonical.com>
2024-09-04 14:34:20 +1000

    refactor: remove credential validator
    Removes the creation and usage of a credential validator from the
    credential service on the cloud facade.
    
    We are removing this validation as it relies heavily on existing mongo
    implementations and patterns that are not present in the DQlite world
    anymore.
    
    Also now that we have the provider tracker we want to use this work for
    establishing providers and not that of environs.New. This is a
    regression in Juju functionality and new credential validation will need
    to be added back in at another stage.

--- apiserver/facades/client/cloud/register.go
+++ apiserver/facades/client/cloud/register.go
@@ -9,10 +9,7 @@ import (
 
 	"github.com/juju/errors"
 
-	"github.com/juju/juju/apiserver/common/credentialcommon"
 	"github.com/juju/juju/apiserver/facade"
-	coremodel "github.com/juju/juju/core/model"
-	"github.com/juju/juju/domain/credential/service"
 )
 
 // Register is called to expose a package of facades onto a given registry.
@@ -32,39 +29,6 @@ func newFacadeV7(stdCtx stdcontext.Context, context facade.ModelContext) (*Cloud
 	credentialService := serviceFactory.Credential().
 		WithLegacyUpdater(systemState.CloudCredentialUpdated).
 		WithLegacyRemover(systemState.RemoveModelsCredential)
-
-	credentialCallContextGetter := func(ctx stdcontext.Context, modelUUID coremodel.UUID) (service.CredentialValidationContext, error) {
-		modelState, err := context.StatePool().Get(modelUUID.String())
-		if err != nil {
-			return service.CredentialValidationContext{}, err
-		}
-		defer modelState.Release()
-
-		m, err := modelState.Model()
-		if err != nil {
-			return service.CredentialValidationContext{}, err
-		}
-		cfg, err := m.Config()
-		if err != nil {
-			return service.CredentialValidationContext{}, err
-		}
-
-		cld, err := context.ServiceFactory().Cloud().Cloud(ctx, m.CloudName())
-		if err != nil {
-			return service.CredentialValidationContext{}, err
-		}
-
-		return service.CredentialValidationContext{
-			ControllerUUID: context.ControllerUUID(),
-			Config:         cfg,
-			MachineService: credentialcommon.NewMachineService(modelState.State),
-			ModelType:      coremodel.ModelType(m.Type()),
-			Cloud:          *cld,
-			Region:         m.CloudRegion(),
-		}, nil
-	}
-
-	credentialService = credentialService.WithValidationContextGetter(credentialCallContextGetter)
 
 	controllerInfo, err := systemState.ControllerInfo()
 	if err != nil {
