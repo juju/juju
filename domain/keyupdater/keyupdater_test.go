a55eb58605e86018b13cee7384e0d040e84b5099
Thomas Miller <thomas.miller@canonical.com>
2024-09-11 15:41:31 +1000

    test: add integration tests for key updater
    Adds integration tests for the key updater domain to assert watchers.

---
+++ domain/keyupdater/keyupdater_test.go
@@ -0,0 +1,217 @@
+// Copyright 2024 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package keyupdater
+
+import (
+	"context"
+
+	jc "github.com/juju/testing/checkers"
+	gc "gopkg.in/check.v1"
+
+	"github.com/juju/juju/cloud"
+	"github.com/juju/juju/core/changestream"
+	"github.com/juju/juju/core/credential"
+	"github.com/juju/juju/core/machine"
+	"github.com/juju/juju/core/model"
+	modeltesting "github.com/juju/juju/core/model/testing"
+	"github.com/juju/juju/core/permission"
+	"github.com/juju/juju/core/user"
+	coreuser "github.com/juju/juju/core/user"
+	usertesting "github.com/juju/juju/core/user/testing"
+	jujuversion "github.com/juju/juju/core/version"
+	"github.com/juju/juju/core/watcher/watchertest"
+	"github.com/juju/juju/domain"
+	userservice "github.com/juju/juju/domain/access/service"
+	accessstate "github.com/juju/juju/domain/access/state"
+	userstate "github.com/juju/juju/domain/access/state"
+	cloudbootstrap "github.com/juju/juju/domain/cloud/bootstrap"
+	credentialbootstrap "github.com/juju/juju/domain/credential/bootstrap"
+	keymanagerservice "github.com/juju/juju/domain/keymanager/service"
+	keymanagerstate "github.com/juju/juju/domain/keymanager/state"
+	"github.com/juju/juju/domain/keyupdater/service"
+	"github.com/juju/juju/domain/keyupdater/state"
+	machinebootstrap "github.com/juju/juju/domain/machine/bootstrap"
+	domainmodel "github.com/juju/juju/domain/model"
+	modelbootstrap "github.com/juju/juju/domain/model/bootstrap"
+	"github.com/juju/juju/domain/model/state/testing"
+	changestreamtesting "github.com/juju/juju/internal/changestream/testing"
+	loggertesting "github.com/juju/juju/internal/logger/testing"
+	jujutesting "github.com/juju/juju/internal/testing"
+	"github.com/juju/juju/internal/uuid"
+)
+
+type keyUpdaterSuite struct {
+	changestreamtesting.ControllerSuite
+	changestreamtesting.ModelSuite
+
+	modelID model.UUID
+	userID  user.UUID
+}
+
+var _ = gc.Suite(&keyUpdaterSuite{})
+
+func (s *keyUpdaterSuite) SetUpTest(c *gc.C) {
+	s.ControllerSuite.SetUpTest(c)
+	s.ModelSuite.SetUpTest(c)
+
+	controllerUUID := s.SeedControllerUUID(c)
+	s.userID = usertesting.GenUserUUID(c)
+
+	accessState := accessstate.NewState(s.ControllerSuite.TxnRunnerFactory(), loggertesting.WrapCheckLog(c))
+	err := accessState.AddUser(
+		context.Background(), s.userID,
+		coreuser.AdminUserName,
+		coreuser.AdminUserName.Name(),
+		false,
+		s.userID,
+		permission.AccessSpec{
+			Access: permission.SuperuserAccess,
+			Target: permission.ID{
+				ObjectType: permission.Controller,
+				Key:        controllerUUID,
+			},
+		},
+	)
+	c.Assert(err, jc.ErrorIsNil)
+
+	cloudName := "test"
+	fn := cloudbootstrap.InsertCloud(coreuser.AdminUserName, cloud.Cloud{
+		Name:      cloudName,
+		Type:      "ec2",
+		AuthTypes: cloud.AuthTypes{cloud.EmptyAuthType},
+	})
+
+	err = fn(context.Background(), s.ControllerTxnRunner(), s.ControllerSuite.NoopTxnRunner())
+	c.Assert(err, jc.ErrorIsNil)
+
+	credentialName := "test"
+	fn = credentialbootstrap.InsertCredential(credential.Key{
+		Cloud: cloudName,
+		Name:  credentialName,
+		Owner: coreuser.AdminUserName,
+	},
+		cloud.NewCredential(cloud.EmptyAuthType, nil),
+	)
+
+	err = fn(context.Background(), s.ControllerTxnRunner(), s.ControllerSuite.NoopTxnRunner())
+	c.Assert(err, jc.ErrorIsNil)
+
+	testing.CreateInternalSecretBackend(c, s.ControllerTxnRunner())
+
+	modelUUID := modeltesting.GenModelUUID(c)
+	modelFn := modelbootstrap.CreateModel(modelUUID, domainmodel.ModelCreationArgs{
+		AgentVersion: jujuversion.Current,
+		Cloud:        cloudName,
+		Credential: credential.Key{
+			Cloud: cloudName,
+			Name:  credentialName,
+			Owner: coreuser.AdminUserName,
+		},
+		Name:  "test",
+		Owner: s.userID,
+	})
+	s.modelID = modelUUID
+
+	err = modelFn(context.Background(), s.ControllerTxnRunner(), s.ControllerSuite.NoopTxnRunner())
+	c.Assert(err, jc.ErrorIsNil)
+
+	err = modelbootstrap.CreateReadOnlyModel(modelUUID, uuid.MustNewUUID())(context.Background(), s.ControllerTxnRunner(), s.ModelTxnRunner())
+	c.Assert(err, jc.ErrorIsNil)
+
+	err = machinebootstrap.InsertMachine("0")(
+		context.Background(),
+		s.ControllerTxnRunner(),
+		s.ModelTxnRunner(),
+	)
+	c.Assert(err, jc.ErrorIsNil)
+}
+
+// TestWatchAuthorizedKeysForMachine is here to assert an integration test
+// between all the components of key updating. Specifically we want to see that
+// as users come and go from the system and also their private keys we get
+// watcher events and the authorized keys reported for the machine in question
+// is correct.
+func (s *keyUpdaterSuite) TestWatchAuthorizedKeysForMachine(c *gc.C) {
+	ctx, cancel := jujutesting.LongWaitContext()
+	defer cancel()
+
+	controllerSt := state.NewControllerState(s.ControllerSuite.TxnRunnerFactory())
+	st := state.NewState(s.ModelSuite.TxnRunnerFactory())
+	factory := domain.NewWatcherFactory(
+		changestream.NewWatchableDBFactoryForNamespace(s.ControllerSuite.GetWatchableDB, "model_authorized_keys"),
+		loggertesting.WrapCheckLog(c),
+	)
+
+	svc := service.NewWatchableService(
+		service.NewControllerKeyService(controllerSt),
+		controllerSt,
+		st,
+		factory,
+	)
+
+	watcher, err := svc.WatchAuthorisedKeysForMachine(ctx, machine.Name("0"))
+	c.Assert(err, jc.ErrorIsNil)
+
+	keyManagerSt := keymanagerstate.NewState(s.ControllerSuite.TxnRunnerFactory())
+	keyManagerSvc := keymanagerservice.NewService(s.modelID, keyManagerSt)
+
+	err = keyManagerSvc.AddPublicKeysForUser(
+		ctx,
+		s.userID,
+		"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII4GpCvqUUYUJlx6d1kpUO9k/t4VhSYsf0yE0/QTqDzC one@juju.is",
+		"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJQJ9wv0uC3yytXM3d2sJJWvZLuISKo7ZHwafHVviwVe two@juju.is",
+	)
+	c.Assert(err, jc.ErrorIsNil)
+
+	w := watchertest.NewNotifyWatcherC(c, watcher)
+	w.AssertOneChange()
+	s.ControllerSuite.AssertChangeStreamIdle(c)
+
+	err = keyManagerSvc.DeleteKeysForUser(
+		ctx,
+		s.userID,
+		"one@juju.is",
+	)
+	c.Assert(err, jc.ErrorIsNil)
+	w.AssertOneChange()
+	s.ControllerSuite.AssertChangeStreamIdle(c)
+
+	keys, err := svc.GetAuthorisedKeysForMachine(ctx, machine.Name("0"))
+	c.Assert(err, jc.ErrorIsNil)
+	c.Assert(keys, jc.DeepEquals, []string{
+		"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJQJ9wv0uC3yytXM3d2sJJWvZLuISKo7ZHwafHVviwVe two@juju.is",
+	})
+
+	userSvc := userservice.NewUserService(
+		userstate.NewUserState(s.ControllerSuite.TxnRunnerFactory()),
+	)
+
+	err = userSvc.DisableUserAuthentication(ctx, coreuser.AdminUserName)
+	c.Assert(err, jc.ErrorIsNil)
+	w.AssertOneChange()
+	s.ControllerSuite.AssertChangeStreamIdle(c)
+
+	keys, err = svc.GetAuthorisedKeysForMachine(ctx, machine.Name("0"))
+	c.Assert(err, jc.ErrorIsNil)
+	c.Assert(len(keys), gc.Equals, 0)
+
+	err = userSvc.EnableUserAuthentication(ctx, coreuser.AdminUserName)
+	c.Assert(err, jc.ErrorIsNil)
+	w.AssertOneChange()
+	s.ControllerSuite.AssertChangeStreamIdle(c)
+
+	keys, err = svc.GetAuthorisedKeysForMachine(ctx, machine.Name("0"))
+	c.Assert(err, jc.ErrorIsNil)
+	c.Assert(keys, jc.DeepEquals, []string{
+		"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJQJ9wv0uC3yytXM3d2sJJWvZLuISKo7ZHwafHVviwVe two@juju.is",
+	})
+
+	err = userSvc.RemoveUser(ctx, coreuser.AdminUserName)
+	c.Assert(err, jc.ErrorIsNil)
+	w.AssertOneChange()
+
+	keys, err = svc.GetAuthorisedKeysForMachine(ctx, machine.Name("0"))
+	c.Assert(err, jc.ErrorIsNil)
+	c.Assert(len(keys), gc.Equals, 0)
+}
