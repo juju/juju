007554e5719e08d3c224ced42dbce2aba79fda9c
Thomas Miller <thomas.miller@canonical.com>
2024-09-19 12:29:38 +1000

    feat: adds model config defaults to manual provider

--- internal/provider/manual/config.go
+++ internal/provider/manual/config.go
@@ -4,6 +4,8 @@
 package manual
 
 import (
+	"context"
+
 	"github.com/juju/schema"
 
 	"github.com/juju/juju/environs/config"
@@ -21,4 +23,24 @@ type environConfig struct {
 
 func newModelConfig(config *config.Config, attrs map[string]interface{}) *environConfig {
 	return &environConfig{Config: config, attrs: attrs}
+}
+
+// ConfigDefaults returns the default values for this providers specific config
+// attributes.
+func (p ManualProvider) ConfigDefaults() schema.Defaults {
+	return schema.Defaults{}
+}
+
+// ConfigSchema returns the extra config attributes specific to this provider.
+func (p ManualProvider) ConfigSchema() schema.Fields {
+	return schema.Fields{}
+}
+
+// ModelConfigDefaults provides a set of default model config attributes that
+// should be set on a models config if they have not been specified by the user.
+func (p ManualProvider) ModelConfigDefaults(_ context.Context) (map[string]any, error) {
+	return map[string]any{
+		config.EnableOSRefreshUpdateKey: true,
+		config.EnableOSUpgradeKey:       false,
+	}, nil
 }
