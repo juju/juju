7bb79fdc99203be134d1c427a875caa4dbecd10f
Simon Richardson <stickupkid@gmail.com>
2024-09-23 12:00:54 +0100

    refactor: allow multiple manifold selection
    It's useful to have multiple manifold selection, this can be done
    using the --manifold flag.
    
    Also, you can now get the model manifolds for the controller.
    
    We probably want the jujud version of this, but for now I'm only
    using it against the controller.

--- scripts/engine-dag/main.go
+++ scripts/engine-dag/main.go
@@ -7,69 +7,108 @@ import (
 	"bytes"
 	"flag"
 	"fmt"
+	"os"
 	"strings"
+	"text/tabwriter"
 
 	"github.com/juju/collections/set"
 	"github.com/juju/names/v5"
 	"github.com/juju/worker/v4/dependency"
 
 	"github.com/juju/juju/agent"
+	"github.com/juju/juju/api"
 	"github.com/juju/juju/cmd/jujud-controller/agent/machine"
+	"github.com/juju/juju/cmd/jujud-controller/agent/model"
 	"github.com/juju/juju/controller"
+	internallogger "github.com/juju/juju/internal/logger"
 	"github.com/juju/juju/internal/testing"
 	"github.com/juju/juju/internal/upgrades"
 	"github.com/juju/juju/state"
 )
 
-var (
-	modelTypeFlag      = flag.String("model-type", "iaas", "model type to use (iaas|caas)")
-	transitiveDepsFlag = flag.Int("transitive-deps", 0, "include transitive dependencies and how many levels to include")
-	manifoldFlag       = flag.String("manifold", "", "manifold to select (empty indicates all)")
-)
-
 func main() {
-	flag.Parse()
+	var (
+		flags = flag.NewFlagSet("dag", flag.ExitOnError)
 
-	var manifolds dependency.Manifolds
-	switch *modelTypeFlag {
-	case "iaas":
+		modelTypeFlag      = flags.String("model-type", "iaas", "model type to use (iaas|caas)")
+		useModelFlag       = flags.Bool("model", false, "use model manifolds")
+		transitiveDepsFlag = flags.Int("dependency-depth", 0, "include transitive dependencies and how many levels to include")
 
-		manifolds = machine.IAASManifolds(machine.ManifoldsConfig{
-			Agent:           &mockAgent{},
-			PreUpgradeSteps: preUpgradeSteps,
-		})
-	case "caas":
-		manifolds = machine.CAASManifolds(machine.ManifoldsConfig{
-			Agent:           &mockAgent{},
-			PreUpgradeSteps: preUpgradeSteps,
-		})
-	default:
-		panic("unknown model type")
+		manifoldsFlag = stringslice{}
+	)
+
+	flags.Var(&manifoldsFlag, "manifold", "manifold to select (empty indicates all)")
+	flags.Usage = usageFor(flags, "dag")
+
+	if err := flags.Parse(os.Args[1:]); err != nil {
+		fmt.Fprintf(os.Stderr, "error parsing flags: %v\n", err)
+		os.Exit(1)
 	}
 
 	root := NewDAG()
 
+	manifolds := getManifolds(*useModelFlag, *modelTypeFlag)
+
+	selectedManifolds := set.NewStrings(manifoldsFlag.Slice()...)
+	if selectedManifolds.IsEmpty() {
+		// Spit out all the manifolds, with or without transitive dependencies,
+		// depending on the flag.
+		for name, manifold := range manifolds {
+			node := root.AddVertex(name)
+			dependencies := manifoldDependencies(manifolds, manifold, *transitiveDepsFlag)
+			for _, dep := range dependencies.Values() {
+				node.AddEdge(dep)
+			}
+		}
+
+		// Render the graph and exit.
+		fmt.Fprintln(os.Stdout, root.Render())
+		os.Exit(0)
+	}
+
+	// Transitive dependencies are not supported with manifold selection.
 	transativeDepth := *transitiveDepsFlag
-	selectedManifold := *manifoldFlag
+	if transativeDepth > 0 {
+		fmt.Fprintf(os.Stderr, "cannot include transitive dependencies with manifold selection\n")
+		os.Exit(1)
+	}
+
+	selection := make(map[string]dependency.Manifold)
 	for name, manifold := range manifolds {
-		if selectedManifold != "" && name != selectedManifold {
+		if !selectedManifolds.Contains(name) {
 			continue
 		}
+		selection[name] = manifold
+	}
+
+	if num := len(selection); num == 0 {
+		fmt.Fprintf(os.Stderr, "manifold(s) not found: %q\n", selectedManifolds.SortedValues())
+		os.Exit(1)
+	} else if num < selectedManifolds.Size() {
+		a := set.NewStrings()
+		for name := range selection {
+			a.Add(name)
+		}
+		fmt.Fprintf(os.Stderr, "not all manifolds found: %q\n", selectedManifolds.Difference(a).SortedValues())
+	}
+
+	for name, manifold := range selection {
 		node := root.AddVertex(name)
-		dependencies := manifoldDependencies(manifolds, manifold, transativeDepth)
+		dependencies := manifoldDependencies(manifolds, manifold, 0)
 		for _, dep := range dependencies.Values() {
 			node.AddEdge(dep)
 		}
 	}
 
-	fmt.Println(root.Render())
+	fmt.Fprintln(os.Stdout, root.Render())
+	os.Exit(0)
 }
 
 func manifoldDependencies(all dependency.Manifolds, manifold dependency.Manifold, transativeDepth int) set.Strings {
 	result := set.NewStrings()
 	for _, input := range manifold.Inputs {
 		result.Add(input)
-		if transativeDepth == 0 {
+		if transativeDepth <= 0 {
 			continue
 		}
 
@@ -136,6 +175,40 @@ func (n *DagNode) Render(b Writer) {
 	}
 }
 
+func getManifolds(useModel bool, modelType string) dependency.Manifolds {
+	if useModel {
+		switch modelType {
+		case "iaas":
+			return model.IAASManifolds(model.ManifoldsConfig{
+				Agent:          &mockAgent{},
+				LoggingContext: internallogger.DefaultContext(),
+			})
+		case "caas":
+			return model.CAASManifolds(model.ManifoldsConfig{
+				Agent:          &mockAgent{},
+				LoggingContext: internallogger.DefaultContext(),
+			})
+		default:
+			panic("unknown model type for model manifolds")
+		}
+	}
+
+	switch modelType {
+	case "iaas":
+		return machine.IAASManifolds(machine.ManifoldsConfig{
+			Agent:           &mockAgent{},
+			PreUpgradeSteps: preUpgradeSteps,
+		})
+	case "caas":
+		return machine.CAASManifolds(machine.ManifoldsConfig{
+			Agent:           &mockAgent{},
+			PreUpgradeSteps: preUpgradeSteps,
+		})
+	default:
+		panic("unknown model type for machine manifolds")
+	}
+}
+
 type mockAgent struct {
 	agent.Agent
 	conf mockConfig
@@ -164,6 +237,10 @@ func (mc *mockConfig) Tag() names.Tag {
 	return mc.tag
 }
 
+func (mock *mockConfig) Model() names.ModelTag {
+	return names.NewModelTag("mock-model-uuid")
+}
+
 func (mc *mockConfig) Controller() names.ControllerTag {
 	return testing.ControllerTag
 }
@@ -188,4 +265,54 @@ func (mc *mockConfig) DataDir() string {
 	return "data-dir"
 }
 
+func (mc *mockConfig) APIInfo() (*api.Info, bool) {
+	return &api.Info{
+		Addrs:    []string{"here", "there"},
+		CACert:   "trust-me",
+		ModelTag: names.NewModelTag("mock-model-uuid"),
+		Tag:      names.NewMachineTag("123"),
+		Password: "12345",
+		Nonce:    "11111",
+	}, true
+}
+
+func (mc *mockConfig) OldPassword() string {
+	return "do-not-use"
+}
+
 func preUpgradeSteps(state.ModelType) upgrades.PreUpgradeStepsFunc { return nil }
+
+type stringslice []string
+
+func (ss *stringslice) Set(s string) error {
+	(*ss) = append(*ss, s)
+	return nil
+}
+
+func (ss *stringslice) Slice() []string {
+	return []string(*ss)
+}
+
+func (ss *stringslice) String() string {
+	if len(*ss) <= 0 {
+		return "..."
+	}
+	return strings.Join(*ss, ", ")
+}
+
+func usageFor(fs *flag.FlagSet, name string) func() {
+	return func() {
+		fmt.Fprintf(os.Stderr, "USAGE\n")
+		fmt.Fprintf(os.Stderr, "  %s\n", name)
+		fmt.Fprintf(os.Stderr, "\n")
+		fmt.Fprintf(os.Stderr, "FLAGS\n")
+
+		writer := tabwriter.NewWriter(os.Stderr, 0, 2, 2, ' ', 0)
+		fs.VisitAll(func(f *flag.Flag) {
+			fmt.Fprintf(writer, "\t-%s %s\t%s\n", f.Name, f.DefValue, f.Usage)
+		})
+		writer.Flush()
+
+		fmt.Fprintf(os.Stderr, "\n")
+	}
+}
