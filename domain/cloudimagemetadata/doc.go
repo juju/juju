fe48cd098dd1ec2b870e1b518018473baa23e3f8
Guillaume Fouillet <guillaume.fouillet@canonical.com>
2024-09-26 08:51:44 +0200

    docs(dqlite): fix typo
    Signed-off-by: Guillaume Fouillet <guillaume.fouillet@canonical.com>

--- domain/cloudimagemetadata/doc.go
+++ domain/cloudimagemetadata/doc.go
@@ -1,5 +1,5 @@
 // Copyright 2024 Canonical Ltd.
 // Licensed under the AGPLv3, see LICENCE file for details.
 
-// Package cloudimagemetadata provides a service to  store and retrie cloud image metadata.
+// Package cloudimagemetadata provides a service to store and retrieve cloud image metadata.
 package cloudimagemetadata
