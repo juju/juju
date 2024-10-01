dccaba9471f04564a47b32597a26e7c1f7ae62f0
Guillaume Fouillet <guillaume.fouillet@canonical.com>
2024-10-01 11:25:19 +0200

    fix(lint): handle unexpected TERM return as not supporting colors
    - Before this commit, a failed tput command would cause an error if TERM has an unexpected value.
    - After this commit, a failed tput causes the script indicates that the terminal doesn't support
      colors, avoiding unexpected lint failures
    
    Signed-off-by: Guillaume Fouillet <guillaume.fouillet@canonical.com>

--- tests/includes/colors.sh
+++ tests/includes/colors.sh
@@ -1,11 +1,7 @@
 supports_colors() {
-	if [[ -z ${TERM} ]] || [[ ${TERM} == "" ]] || [[ ${TERM} == "dumb" ]]; then
-		echo "NO"
-		return
-	fi
 	if which tput >/dev/null 2>&1; then
 		# shellcheck disable=SC2046
-		if [[ $(tput colors) -gt 1 ]]; then
+		if [[ $(tput colors 2> /dev/null || echo 0) -gt 1 ]]; then
 			echo "YES"
 			return
 		fi
