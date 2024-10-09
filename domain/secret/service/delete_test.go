4b0dade7d16caa0bde62377df8f89ec4b0a4d0f4
Kelvin Liu <kelvin.liu@canonical.com>
2024-10-09 17:40:38 +1100

    feat: add missing tests for secret removal code;

--- domain/secret/service/delete_test.go
+++ domain/secret/service/delete_test.go
@@ -10,6 +10,11 @@ import (
 	"go.uber.org/mock/gomock"
 	gc "gopkg.in/check.v1"
 
+	coresecrets "github.com/juju/juju/core/secrets"
+	"github.com/juju/juju/domain"
+	domainsecret "github.com/juju/juju/domain/secret"
+	domaintesting "github.com/juju/juju/domain/testing"
+	"github.com/juju/juju/internal/secrets/provider"
 	"github.com/juju/juju/internal/uuid"
 )
 
@@ -27,4 +32,94 @@ func (s *serviceSuite) TestDeleteObsoleteUserSecretRevisions(c *gc.C) {
 
 	err = s.service.DeleteObsoleteUserSecretRevisions(context.Background())
 	c.Assert(err, jc.ErrorIsNil)
+}
+
+func (s *serviceSuite) TestDeleteSecret(c *gc.C) {
+	ctrl := s.setupMocks(c)
+	defer ctrl.Finish()
+
+	uri := coresecrets.NewURI()
+
+	s.state.EXPECT().GetSecretAccess(gomock.Any(), uri, domainsecret.AccessParams{
+		SubjectTypeID: domainsecret.SubjectUnit,
+		SubjectID:     "mariadb/0",
+	}).Return("manage", nil)
+	s.state.EXPECT().ListExternalSecretRevisions(domaintesting.IsAtomicContextChecker, uri, 1, 2).Return([]coresecrets.ValueRef{
+		{BackendID: "backend-id", RevisionID: "rev-id1"},
+		{BackendID: "backend-id", RevisionID: "rev-id2"},
+	}, nil)
+	s.state.EXPECT().DeleteSecret(domaintesting.IsAtomicContextChecker, uri, []int{1, 2}).Return([]string{
+		"revision-uuid-1",
+		"revision-uuid-2",
+	}, nil)
+
+	s.secretsBackendProvider.EXPECT().NewBackend(ptr(backendConfigs.Configs["backend-id"])).DoAndReturn(
+		func(cfg *provider.ModelBackendConfig) (provider.SecretsBackend, error) {
+			return s.secretsBackend, nil
+		},
+	)
+	s.secretsBackend.EXPECT().DeleteContent(gomock.Any(), "rev-id1").Return(nil)
+	s.secretsBackend.EXPECT().DeleteContent(gomock.Any(), "rev-id2").Return(nil)
+
+	revs := provider.SecretRevisions{}
+	revs.Add(uri, "rev-id1")
+	revs.Add(uri, "rev-id2")
+	s.secretsBackendProvider.EXPECT().CleanupSecrets(gomock.Any(), ptr(backendConfigs.Configs["backend-id"]), "mariadb/0", revs).Return(nil)
+	s.secretBackendReferenceMutator.EXPECT().RemoveSecretBackendReference(gomock.Any(), "revision-uuid-1", "revision-uuid-2").Return(nil)
+
+	err := s.service.DeleteSecret(context.Background(), uri, DeleteSecretParams{
+		LeaderToken: successfulToken{},
+		Accessor: SecretAccessor{
+			Kind: UnitAccessor,
+			ID:   "mariadb/0",
+		},
+		Revisions: []int{1, 2},
+	})
+	c.Assert(err, jc.ErrorIsNil)
+}
+
+func (s *serviceSuite) TestInternalDeleteSecrett(c *gc.C) {
+	ctrl := s.setupMocks(c)
+	defer ctrl.Finish()
+
+	uri := coresecrets.NewURI()
+
+	s.state.EXPECT().ListExternalSecretRevisions(domaintesting.IsAtomicContextChecker, uri, 1, 2).Return([]coresecrets.ValueRef{
+		{BackendID: "backend-id", RevisionID: "rev-id1"},
+		{BackendID: "backend-id", RevisionID: "rev-id2"},
+	}, nil)
+	s.state.EXPECT().DeleteSecret(domaintesting.IsAtomicContextChecker, uri, []int{1, 2}).Return([]string{
+		"revision-uuid-1",
+		"revision-uuid-2",
+	}, nil)
+
+	s.secretsBackendProvider.EXPECT().NewBackend(ptr(backendConfigs.Configs["backend-id"])).DoAndReturn(
+		func(cfg *provider.ModelBackendConfig) (provider.SecretsBackend, error) {
+			return s.secretsBackend, nil
+		},
+	)
+	s.secretsBackend.EXPECT().DeleteContent(gomock.Any(), "rev-id1").Return(nil)
+	s.secretsBackend.EXPECT().DeleteContent(gomock.Any(), "rev-id2").Return(nil)
+
+	revs := provider.SecretRevisions{}
+	revs.Add(uri, "rev-id1")
+	revs.Add(uri, "rev-id2")
+	s.secretsBackendProvider.EXPECT().CleanupSecrets(gomock.Any(), ptr(backendConfigs.Configs["backend-id"]), "mariadb/0", revs).Return(nil)
+	s.secretBackendReferenceMutator.EXPECT().RemoveSecretBackendReference(gomock.Any(), "revision-uuid-1", "revision-uuid-2").Return(nil)
+
+	var cleanExternal func(context.Context)
+	err := s.state.RunAtomic(context.Background(), func(ctx domain.AtomicContext) error {
+		var err error
+		cleanExternal, err = s.service.InternalDeleteSecret(ctx, uri, DeleteSecretParams{
+			LeaderToken: successfulToken{},
+			Accessor: SecretAccessor{
+				Kind: UnitAccessor,
+				ID:   "mariadb/0",
+			},
+			Revisions: []int{1, 2},
+		})
+		return err
+	})
+	c.Assert(err, jc.ErrorIsNil)
+	cleanExternal(context.Background())
 }
