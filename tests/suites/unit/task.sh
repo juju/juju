9bdabcddd95cfc813dbaa26206e7c73692290815
Jordan Barrett <90195985+barrettj12@users.noreply.github.com.>
2024-10-17 12:29:27 -0500

    test: delete 'unit' Bash test suite
    The only purpose of this test suite was to test the
    set-application-base command, which was removed in #17682. We already
    have a test (run_deploy_specific_series in the deploy suite) that
    ensures that charm bases can be specified on deploy. Hence, we can
    remove the test suite (or replace it with a no-op).

--- tests/suites/unit/task.sh
+++ tests/suites/unit/task.sh
@@ -1,20 +1,3 @@
 test_unit() {
-	if [ "$(skip 'test_unit')" ]; then
-		echo "==> TEST SKIPPED: unit tests"
-		return
-	fi
-
-	set_verbosity
-
-	echo "==> Checking for dependencies"
-	check_dependencies juju
-
-	file="${TEST_DIR}/test-units.log"
-
-	bootstrap "test-units" "${file}"
-
-	# Tests that need to be run are added here.
-	test_unit_series
-
-	destroy_controller "test-units"
+  echo "The 'unit' test suite does not exist in Juju 4.0+"
 }
