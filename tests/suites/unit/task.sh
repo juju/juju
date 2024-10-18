4d64267c92647cc205fc29a09d3012c0167025d0
Jordan Barrett <90195985+barrettj12@users.noreply.github.com.>
2024-10-18 09:19:22 -0500

    chore: shell formatting

--- tests/suites/unit/task.sh
+++ tests/suites/unit/task.sh
@@ -1,3 +1,3 @@
 test_unit() {
-  echo "The 'unit' test suite does not exist in Juju 4.0+"
+	echo "The 'unit' test suite does not exist in Juju 4.0+"
 }
--- tests/suites/upgrade_series/task.sh
+++ tests/suites/upgrade_series/task.sh
@@ -1,3 +1,3 @@
 test_upgrade_series() {
-  echo "The 'upgrade_series' test suite does not exist in Juju 4.0+"
+	echo "The 'upgrade_series' test suite does not exist in Juju 4.0+"
 }
