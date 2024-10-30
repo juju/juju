501b6ba74b501e18cd7e97762e4c1b2fb255523e
Simon Richardson <stickupkid@gmail.com>
2024-10-30 09:38:34 +0000

    test: use mock for caas storage provisioner tests
    The caas storage provisioner are full end to end integration tests
    from the apiserver down. There is a roadblock atm were the state
    requires domain services and the domain services require state.
    This fine because of lazy evaluation at runtime, but is not fine
    when for tests.
    
    Moving to mocks removes this problem.

--- apiserver/facades/agent/storageprovisioner/package_test.go
+++ apiserver/facades/agent/storageprovisioner/package_test.go
@@ -14,6 +14,9 @@ import (
 )
 
 //go:generate go run go.uber.org/mock/mockgen -typed -package storageprovisioner_test -destination blockdevice_mock_test.go github.com/juju/juju/apiserver/facades/agent/storageprovisioner BlockDeviceService
+//go:generate go run go.uber.org/mock/mockgen -typed -package storageprovisioner -destination storage_mock_test.go github.com/juju/juju/apiserver/facades/agent/storageprovisioner StorageBackend,Backend
+//go:generate go run go.uber.org/mock/mockgen -typed -package storageprovisioner -destination state_mock_test.go github.com/juju/juju/state FilesystemAttachment,VolumeAttachment,EntityFinder,Lifer
+//go:generate go run go.uber.org/mock/mockgen -typed -package storageprovisioner -destination facade_mock_test.go github.com/juju/juju/apiserver/facade Resources
 
 func TestAll(t *stdtesting.T) {
 	testing.MgoTestPackage(t)
