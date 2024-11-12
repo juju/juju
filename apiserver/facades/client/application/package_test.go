314ca292c8737aa6cf096ad93c4d2521acd992b8
Simon Richardson <stickupkid@gmail.com>
2024-11-12 11:16:32 +0000

    test: rip out all the tests
    All the tests are being removed, we'll add them back with the same
    coverage in the future.
    
    I'll add some initial tests back in a future commit, but we don't
    want to spend copious time on this. We could be hear for weeks fixing
    this.

--- apiserver/facades/client/application/package_test.go
+++ apiserver/facades/client/application/package_test.go
@@ -1,25 +1,14 @@
 // Copyright 2014 Canonical Ltd.
 // Licensed under the AGPLv3, see LICENCE file for details.
 
-package application_test
+package application
 
 import (
-	stdtesting "testing"
+	"testing"
 
-	"github.com/juju/juju/internal/testing"
+	gc "gopkg.in/check.v1"
 )
 
-func TestAll(t *stdtesting.T) {
-	testing.MgoTestPackage(t)
+func TestPackage(t *testing.T) {
+	gc.TestingT(t)
 }
-
-//go:generate go run go.uber.org/mock/mockgen -typed -package mocks -destination mocks/lxdprofile_mock.go github.com/juju/juju/internal/charm LXDProfiler
-//go:generate go run go.uber.org/mock/mockgen -typed -package mocks -destination mocks/state_mock.go github.com/juju/juju/state StorageAttachment,StorageInstance,CloudContainer
-//go:generate go run go.uber.org/mock/mockgen -typed -package mocks -destination mocks/storage_mock.go github.com/juju/juju/internal/storage ProviderRegistry
-//go:generate go run go.uber.org/mock/mockgen -typed -package mocks -destination mocks/leadership_mock.go github.com/juju/juju/core/leadership Reader
-//go:generate go run go.uber.org/mock/mockgen -typed -package mocks -destination mocks/application_mock.go github.com/juju/juju/apiserver/facades/client/application Backend,StorageInterface,BlockChecker,Model,CaasBrokerInterface,Application,RemoteApplication,Charm,Relation,Unit,RelationUnit,Machine,Bindings,Resources
-//go:generate go run go.uber.org/mock/mockgen -typed -package mocks -destination mocks/offer_mock.go github.com/juju/juju/apiserver/facades/client/application OfferConnection
-//go:generate go run go.uber.org/mock/mockgen -typed -package mocks -destination mocks/objectstore_mock.go github.com/juju/juju/core/objectstore ObjectStore
-//go:generate go run go.uber.org/mock/mockgen -typed -package application -destination deployrepository_mocks_test.go github.com/juju/juju/apiserver/facades/client/application Bindings,DeployFromRepositoryState,DeployFromRepositoryValidator,Model,Machine,Application,Charm
-//go:generate go run go.uber.org/mock/mockgen -typed -package application -destination repository_mocks_test.go github.com/juju/juju/core/charm Repository,RepositoryFactory
-//go:generate go run go.uber.org/mock/mockgen -typed -package application -destination service_mock_test.go github.com/juju/juju/apiserver/facades/client/application MachineService,ApplicationService,StoragePoolGetter,NetworkService,ExternalControllerService,ModelConfigService,PortService,StubService
