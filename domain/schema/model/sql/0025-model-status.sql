2e8abd3ad1490d3238c013a060dfb8758125409a
Kelvin Liu <kelvin.liu@canonical.com>
2024-11-18 19:29:20 +1100

    feat: set default value for the updated_at timestamp;

--- domain/schema/model/sql/0025-model-status.sql
+++ domain/schema/model/sql/0025-model-status.sql
@@ -7,16 +7,15 @@ CREATE TABLE model_status_value (
 INSERT INTO model_status_value VALUES
 (0, 'error'),
 (1, 'available'),
--- We set the model status to busy when the model is being upgrading.
+-- We set the model status to busy when the model is being upgraded.
 (2, 'busy'),
--- We set the model status to suspended when the model has invlid cloud credentials.
 (3, 'suspended');
 
 CREATE TABLE model_status (
     model_uuid TEXT NOT NULL PRIMARY KEY,
     status_id INT NOT NULL,
     message TEXT,
-    updated_at DATETIME,
+    updated_at DATETIME NOT NULL DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW', 'utc')),
     CONSTRAINT fk_model_status_model
     FOREIGN KEY (model_uuid)
     REFERENCES model (uuid),
