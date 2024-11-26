b698d38529fd9cb8e27ca6be5b8d4fb6f4981167
Joseph Phillips <joe.phillips@canonical.com>
2024-11-26 12:23:40 +0100

    fix: removes GetModelUUID from model agent state
    This is no longer required, as the model is implicit when getting target
    agent version from the model state instead of the controller.

--- domain/modelagent/state/modelstate.go
+++ domain/modelagent/state/modelstate.go
@@ -13,7 +13,6 @@ import (
 
 	"github.com/juju/juju/core/database"
 	"github.com/juju/juju/core/machine"
-	"github.com/juju/juju/core/model"
 	"github.com/juju/juju/domain"
 	applicationerrors "github.com/juju/juju/domain/application/errors"
 	machineerrors "github.com/juju/juju/domain/machine/errors"
@@ -76,48 +75,6 @@ WHERE name = $machineName.name
 	})
 
 	return err
-}
-
-// GetModelUUID returns the uuid for the model represented by this state.
-func (m *ModelState) GetModelUUID(ctx context.Context) (model.UUID, error) {
-	db, err := m.DB()
-	if err != nil {
-		return model.UUID(""), errors.Errorf(
-			"getting database to get the model uuid: %w", err,
-		)
-	}
-
-	modelUUIDVal := modelUUIDValue{}
-
-	stmt, err := m.Prepare(`
-SELECT (uuid) AS (&modelUUIDValue.model_uuid)
-FROM model
-`, modelUUIDVal)
-	if err != nil {
-		return model.UUID(""), errors.Errorf(
-			"preparing model uuid selection statement: %w", err,
-		)
-	}
-
-	err = db.Txn(ctx, func(ctx context.Context, tx *sqlair.TX) error {
-		err := tx.Query(ctx, stmt).Get(&modelUUIDVal)
-		if errors.Is(err, sqlair.ErrNoRows) {
-			return errors.New(
-				"getting model uuid from database, read only model records don't exist",
-			)
-		} else if err != nil {
-			return errors.Errorf(
-				"getting model uuid from database: %w", err,
-			)
-		}
-		return nil
-	})
-
-	if err != nil {
-		return model.UUID(""), err
-	}
-
-	return model.UUID(modelUUIDVal.UUID), nil
 }
 
 // CheckUnitExists checks to see if the given unit exists in the model. If
