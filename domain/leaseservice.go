6cc50579013efb939a3813d9c003c86f86ee81e9
Joseph Phillips <joe.phillips@canonical.com>
2024-12-06 14:12:06 +0100

    fix: comments and errors for leadership as opposed to lease

--- domain/leaseservice.go
+++ domain/leaseservice.go
@@ -34,7 +34,7 @@ func (s *LeaseService) WithLeader(
 	// Holding the lease is quite a complex operation, so we need to ensure that
 	// the context is not cancelled before we start the operation.
 	if err := ctx.Err(); err != nil {
-		return internalerrors.Errorf("lease pre-checking").Add(ctx.Err())
+		return internalerrors.Errorf("leader pre-checking").Add(ctx.Err())
 	}
 
 	leaseChecker, err := s.leaseChecker.GetLeaseManager()
@@ -43,7 +43,7 @@ func (s *LeaseService) WithLeader(
 	}
 
 	// The leaseCtx will be cancelled when the lease is no longer held by the
-	// lease holder. This may or may not be the same as the holderName for the
+	// leaseholder. This may or may not be the same as the holderName for the
 	// lease. That check is done by the Token checker.
 	leaseCtx, leaseCancel := context.WithCancel(ctx)
 	defer leaseCancel()
@@ -59,7 +59,7 @@ func (s *LeaseService) WithLeader(
 	// is held by the holder for the duration of the function. Although
 	// convoluted this is necessary to ensure that the lease is held by the
 	// holder for the duration of the function. The context will be cancelled
-	// when the lease is no longer held by the lease holder for the lease name.
+	// when the lease is no longer held by the leaseholder for the lease name.
 
 	waitCtx, waitCancel := context.WithCancel(ctx)
 	defer waitCancel()
@@ -81,7 +81,7 @@ func (s *LeaseService) WithLeader(
 		select {
 		case <-waitCtx.Done():
 			return
-		case waitErr <- internalerrors.Errorf("waiting for lease to expire: %w", err):
+		case waitErr <- internalerrors.Errorf("waiting for leadership to expire: %w", err):
 		}
 	}()
 
@@ -89,14 +89,14 @@ func (s *LeaseService) WithLeader(
 	case <-leaseCtx.Done():
 		// If the leaseCtx is cancelled, then the waiting for the lease to
 		// expire finished unexpectedly. Return the context error.
-		return internalerrors.Errorf("waiting for lease finished before execution").Add(leaseCtx.Err())
+		return internalerrors.Errorf("waiting for leadership finished before execution").Add(leaseCtx.Err())
 	case err := <-waitErr:
 		if err == nil {
 			// This shouldn't happen, but if it does, we need to return an
 			// error. If we're attempting to wait whilst holding the lease,
 			// before running the function and then wait return nil, we don't
 			// know if the lease is held by the holder or what state we're in.
-			return internalerrors.Errorf("unable to wait for lease to expire whilst holding lease")
+			return internalerrors.Errorf("unable to wait for leadership to expire whilst holding lease")
 		}
 		return err
 	case <-start:
@@ -105,8 +105,7 @@ func (s *LeaseService) WithLeader(
 	// Ensure that the lease is held by the holder before proceeding.
 	// We're guaranteed that the lease is held by the holder, otherwise the
 	// context will have been cancelled.
-	token := leaseChecker.Token(appName, unitName)
-	if err := token.Check(); err != nil {
+	if err := leaseChecker.Token(appName, unitName).Check(); err != nil {
 		return internalerrors.Errorf("checking lease token: %w", err)
 	}
 
@@ -114,7 +113,7 @@ func (s *LeaseService) WithLeader(
 	// will ensure that the function is cancelled when the lease is no longer
 	// held.
 	if err := fn(leaseCtx); err != nil {
-		return internalerrors.Errorf("executing lease func: %w", err)
+		return internalerrors.Errorf("executing leadership func: %w", err)
 	}
 	return nil
 }
