7f9402301123ba9e5e64d66746d970fbcea6a549
Simon Richardson <stickupkid@gmail.com>
2024-12-12 10:28:06 +0000

    chore: remove charm revision updater
    Remove the charm revision updater API, we're going direct to the
    domain services.

--- api/facadeversions.go
+++ api/facadeversions.go
@@ -41,7 +41,6 @@ var facadeVersions = facades.FacadeVersions{
 	"CAASModelOperator":            {1},
 	"CAASOperatorUpgrader":         {1},
 	"CAASUnitProvisioner":          {2},
-	"CharmRevisionUpdater":         {2},
 	"Charms":                       {7},
 	"Cleaner":                      {2},
 	"Client":                       {8},
