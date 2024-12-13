707fccc0ed472d155cc48bcb5a6aaaa3981841aa
Simon Richardson <stickupkid@gmail.com>
2024-12-13 16:18:16 +0000

    test: add tests for the objects handler
    The objects handler is now fronted by interfaces, so we could if
    we wanted move this out to a handlers package, rather dumped in
    the root of the apiserver, which makes things impossible to find.

--- apiserver/objects_test.go
+++ apiserver/objects_test.go
@@ -4,6 +4,7 @@
 package apiserver
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"net/http"
@@ -15,13 +16,22 @@ import (
 	gc "gopkg.in/check.v1"
 
 	"github.com/juju/juju/apiserver/apiserverhttp"
+	"github.com/juju/juju/domain/application/architecture"
+	applicationcharm "github.com/juju/juju/domain/application/charm"
 	applicationerrors "github.com/juju/juju/domain/application/errors"
 	"github.com/juju/juju/internal/testing"
+	"github.com/juju/juju/rpc/params"
+	"github.com/juju/juju/state"
 )
 
 type objectsCharmHandlerSuite struct {
 	applicationsServiceGetter *MockApplicationServiceGetter
 	applicationsService       *MockApplicationService
+
+	// These will move to the model service.
+	stateGetter *MockStateGetter
+	modelState  *MockModelState
+	model       *MockModel
 
 	mux *apiserverhttp.Mux
 	srv *httptest.Server
@@ -38,18 +48,32 @@ func (s *objectsCharmHandlerSuite) TearDownTest(c *gc.C) {
 	s.srv.Close()
 }
 
-func (s *objectsCharmHandlerSuite) setupMocks(c *gc.C) *gomock.Controller {
-	ctrl := gomock.NewController(c)
+func (s *objectsCharmHandlerSuite) TestServeMethodNotSupported(c *gc.C) {
+	defer s.setupMocks(c).Finish()
 
-	s.applicationsServiceGetter = NewMockApplicationServiceGetter(ctrl)
-	s.applicationsService = NewMockApplicationService(ctrl)
-	s.applicationsServiceGetter.EXPECT().Application(gomock.Any()).Return(s.applicationsService, nil)
+	handlers := &objectsCharmHTTPHandler{
+		applicationServiceGetter: s.applicationsServiceGetter,
+	}
 
-	return ctrl
+	// This is a bit pathological, but we want to make sure that the handler
+	// logic only actions on POST requests.
+	s.mux.AddHandler("POST", charmsObjectsRoutePrefix, handlers)
+	defer s.mux.RemoveHandler("POST", charmsObjectsRoutePrefix)
+
+	modelUUID := testing.ModelTag.Id()
+	hashPrefix := "0abcdef"
+
+	url := fmt.Sprintf("%s/model-%s/charms/testcharm-%s", s.srv.URL, modelUUID, hashPrefix)
+	resp, err := http.Post(url, "application/octet-stream", strings.NewReader("charm-content"))
+	c.Assert(err, jc.ErrorIsNil)
+
+	c.Assert(resp.StatusCode, gc.Equals, http.StatusNotImplemented)
 }
 
 func (s *objectsCharmHandlerSuite) TestServeGet(c *gc.C) {
 	defer s.setupMocks(c).Finish()
+
+	s.expectApplicationService()
 
 	s.applicationsService.EXPECT().GetCharmArchiveBySHA256Prefix(gomock.Any(), "0abcdef").Return(io.NopCloser(strings.NewReader("charm-content")), nil)
 
@@ -63,7 +87,8 @@ func (s *objectsCharmHandlerSuite) TestServeGet(c *gc.C) {
 	modelUUID := testing.ModelTag.Id()
 	hashPrefix := "0abcdef"
 
-	resp, err := http.Get(fmt.Sprintf("%s/model-%s/charms/testcharm-%s", s.srv.URL, modelUUID, hashPrefix))
+	url := fmt.Sprintf("%s/model-%s/charms/testcharm-%s", s.srv.URL, modelUUID, hashPrefix)
+	resp, err := http.Get(url)
 	c.Assert(err, jc.ErrorIsNil)
 
 	c.Assert(resp.StatusCode, gc.Equals, http.StatusOK)
@@ -74,6 +99,8 @@ func (s *objectsCharmHandlerSuite) TestServeGet(c *gc.C) {
 
 func (s *objectsCharmHandlerSuite) TestServeGetNotFound(c *gc.C) {
 	defer s.setupMocks(c).Finish()
+
+	s.expectApplicationService()
 
 	s.applicationsService.EXPECT().GetCharmArchiveBySHA256Prefix(gomock.Any(), "0abcdef").Return(nil, applicationerrors.CharmNotFound)
 
@@ -87,8 +114,198 @@ func (s *objectsCharmHandlerSuite) TestServeGetNotFound(c *gc.C) {
 	modelUUID := testing.ModelTag.Id()
 	hashPrefix := "0abcdef"
 
-	resp, err := http.Get(fmt.Sprintf("%s/model-%s/charms/testcharm-%s", s.srv.URL, modelUUID, hashPrefix))
+	url := fmt.Sprintf("%s/model-%s/charms/testcharm-%s", s.srv.URL, modelUUID, hashPrefix)
+	resp, err := http.Get(url)
 	c.Assert(err, jc.ErrorIsNil)
 
 	c.Assert(resp.StatusCode, gc.Equals, http.StatusNotFound)
+}
+
+func (s *objectsCharmHandlerSuite) TestServePutIncorrectEncoding(c *gc.C) {
+	defer s.setupMocks(c).Finish()
+
+	handlers := &objectsCharmHTTPHandler{
+		applicationServiceGetter: s.applicationsServiceGetter,
+	}
+
+	s.mux.AddHandler("PUT", charmsObjectsRoutePrefix, handlers)
+	defer s.mux.RemoveHandler("PUT", charmsObjectsRoutePrefix)
+
+	modelUUID := testing.ModelTag.Id()
+	hashPrefix := "0abcdef"
+
+	url := fmt.Sprintf("%s/model-%s/charms/testcharm-%s", s.srv.URL, modelUUID, hashPrefix)
+	req, err := http.NewRequest("PUT", url, strings.NewReader("charm-content"))
+	c.Assert(err, jc.ErrorIsNil)
+
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	c.Assert(err, jc.ErrorIsNil)
+
+	c.Assert(resp.StatusCode, gc.Equals, http.StatusBadRequest)
+}
+
+func (s *objectsCharmHandlerSuite) TestServePutNoJujuCharmURL(c *gc.C) {
+	defer s.setupMocks(c).Finish()
+
+	handlers := &objectsCharmHTTPHandler{
+		stateGetter:              s.stateGetter,
+		applicationServiceGetter: s.applicationsServiceGetter,
+	}
+
+	s.expectApplicationService()
+	s.expectModelState()
+
+	s.mux.AddHandler("PUT", charmsObjectsRoutePrefix, handlers)
+	defer s.mux.RemoveHandler("PUT", charmsObjectsRoutePrefix)
+
+	modelUUID := testing.ModelTag.Id()
+	hashPrefix := "0abcdef"
+
+	url := fmt.Sprintf("%s/model-%s/charms/testcharm-%s", s.srv.URL, modelUUID, hashPrefix)
+	req, err := http.NewRequest("PUT", url, strings.NewReader("charm-content"))
+	c.Assert(err, jc.ErrorIsNil)
+
+	req.Header.Set("Content-Type", "application/zip")
+
+	resp, err := http.DefaultClient.Do(req)
+	c.Assert(err, jc.ErrorIsNil)
+
+	c.Check(resp.StatusCode, gc.Equals, http.StatusBadRequest)
+}
+
+func (s *objectsCharmHandlerSuite) TestServePutInvalidSHA256Prefix(c *gc.C) {
+	defer s.setupMocks(c).Finish()
+
+	handlers := &objectsCharmHTTPHandler{
+		stateGetter:              s.stateGetter,
+		applicationServiceGetter: s.applicationsServiceGetter,
+	}
+
+	s.expectApplicationService()
+	s.expectModelState()
+
+	s.mux.AddHandler("PUT", charmsObjectsRoutePrefix, handlers)
+	defer s.mux.RemoveHandler("PUT", charmsObjectsRoutePrefix)
+
+	modelUUID := testing.ModelTag.Id()
+	hashPrefix := "cdef"
+
+	url := fmt.Sprintf("%s/model-%s/charms/testcharm-%s", s.srv.URL, modelUUID, hashPrefix)
+	req, err := http.NewRequest("PUT", url, strings.NewReader("charm-content"))
+	c.Assert(err, jc.ErrorIsNil)
+
+	req.Header.Set("Content-Type", "application/zip")
+	req.Header.Set(params.JujuCharmURLHeader, "ch:testcharm-1")
+
+	resp, err := http.DefaultClient.Do(req)
+	c.Assert(err, jc.ErrorIsNil)
+
+	c.Check(resp.StatusCode, gc.Equals, http.StatusBadRequest)
+}
+
+func (s *objectsCharmHandlerSuite) TestServePutInvalidCharmURL(c *gc.C) {
+	defer s.setupMocks(c).Finish()
+
+	handlers := &objectsCharmHTTPHandler{
+		stateGetter:              s.stateGetter,
+		applicationServiceGetter: s.applicationsServiceGetter,
+	}
+
+	s.expectApplicationService()
+	s.expectModelState()
+
+	s.mux.AddHandler("PUT", charmsObjectsRoutePrefix, handlers)
+	defer s.mux.RemoveHandler("PUT", charmsObjectsRoutePrefix)
+
+	modelUUID := testing.ModelTag.Id()
+	hashPrefix := "0abcdef"
+
+	url := fmt.Sprintf("%s/model-%s/charms/testcharm_%s", s.srv.URL, modelUUID, hashPrefix)
+	req, err := http.NewRequest("PUT", url, strings.NewReader("charm-content"))
+	c.Assert(err, jc.ErrorIsNil)
+
+	req.Header.Set("Content-Type", "application/zip")
+	req.Header.Set(params.JujuCharmURLHeader, "ch:testcharm-1")
+
+	resp, err := http.DefaultClient.Do(req)
+	c.Assert(err, jc.ErrorIsNil)
+
+	c.Check(resp.StatusCode, gc.Equals, http.StatusBadRequest)
+}
+
+func (s *objectsCharmHandlerSuite) TestServePut(c *gc.C) {
+	defer s.setupMocks(c).Finish()
+
+	handlers := &objectsCharmHTTPHandler{
+		stateGetter:              s.stateGetter,
+		applicationServiceGetter: s.applicationsServiceGetter,
+	}
+
+	s.expectApplicationService()
+	s.expectModelState()
+	s.expectModel()
+
+	s.mux.AddHandler("PUT", charmsObjectsRoutePrefix, handlers)
+	defer s.mux.RemoveHandler("PUT", charmsObjectsRoutePrefix)
+
+	modelUUID := testing.ModelTag.Id()
+	hashPrefix := "0abcdef"
+
+	url := fmt.Sprintf("%s/model-%s/charms/testcharm-%s", s.srv.URL, modelUUID, hashPrefix)
+	req, err := http.NewRequest("PUT", url, strings.NewReader("charm-content"))
+	c.Assert(err, jc.ErrorIsNil)
+
+	req.Header.Set("Content-Type", "application/zip")
+	req.Header.Set(params.JujuCharmURLHeader, "ch:testcharm-1")
+
+	s.applicationsService.EXPECT().ResolveUploadCharm(gomock.Any(), gomock.Any()).DoAndReturn(func(_ context.Context, args applicationcharm.ResolveUploadCharm) (applicationcharm.CharmLocator, error) {
+		c.Check(args.Name, gc.Equals, "testcharm")
+		c.Check(args.Revision, gc.Equals, 1)
+		c.Check(args.Architecture, gc.Equals, "")
+		c.Check(args.Size, gc.Equals, int64(13))
+
+		return applicationcharm.CharmLocator{
+			Name:         "testcharm",
+			Revision:     2,
+			Source:       applicationcharm.CharmHubSource,
+			Architecture: architecture.AMD64,
+		}, nil
+
+	})
+
+	resp, err := http.DefaultClient.Do(req)
+	c.Assert(err, jc.ErrorIsNil)
+
+	c.Check(resp.StatusCode, gc.Equals, http.StatusOK)
+	c.Check(resp.Header.Get(params.JujuCharmURLHeader), gc.Equals, "ch:amd64/testcharm-2")
+}
+
+func (s *objectsCharmHandlerSuite) expectApplicationService() {
+	s.applicationsServiceGetter.EXPECT().Application(gomock.Any()).Return(s.applicationsService, nil)
+}
+
+func (s *objectsCharmHandlerSuite) expectModelState() {
+	s.stateGetter.EXPECT().GetState(gomock.Any()).Return(s.modelState, nil)
+	s.modelState.EXPECT().Release().Return(true)
+}
+
+func (s *objectsCharmHandlerSuite) expectModel() {
+	s.modelState.EXPECT().Model().Return(s.model, nil)
+	s.model.EXPECT().MigrationMode().Return(state.MigrationModeNone)
+}
+
+func (s *objectsCharmHandlerSuite) setupMocks(c *gc.C) *gomock.Controller {
+	ctrl := gomock.NewController(c)
+
+	s.applicationsServiceGetter = NewMockApplicationServiceGetter(ctrl)
+	s.applicationsService = NewMockApplicationService(ctrl)
+
+	// These should be on the model service!
+	s.stateGetter = NewMockStateGetter(ctrl)
+	s.modelState = NewMockModelState(ctrl)
+	s.model = NewMockModel(ctrl)
+
+	return ctrl
 }
