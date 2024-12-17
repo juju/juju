6d00c721113169bd16caefe0b36e9ad079e0e8cf
Simon Richardson <stickupkid@gmail.com>
2024-12-17 13:11:13 +0000

    test: add manifold test for the worker
    This scaffolds out the mocks and the manifold tests.

---
+++ internal/worker/dbreplaccessor/manifold_test.go
@@ -0,0 +1,65 @@
+// Copyright 2022 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package dbreplaccessor
+
+import (
+	"context"
+
+	"github.com/juju/errors"
+	jc "github.com/juju/testing/checkers"
+	gc "gopkg.in/check.v1"
+
+	"github.com/juju/juju/agent"
+	coredatabase "github.com/juju/juju/core/database"
+	"github.com/juju/juju/core/logger"
+)
+
+type manifoldSuite struct {
+	baseSuite
+}
+
+var _ = gc.Suite(&manifoldSuite{})
+
+func (s *manifoldSuite) TestValidateConfig(c *gc.C) {
+	defer s.setupMocks(c).Finish()
+
+	cfg := s.getConfig()
+	c.Check(cfg.Validate(), jc.ErrorIsNil)
+
+	cfg.AgentName = ""
+	c.Check(cfg.Validate(), jc.ErrorIs, errors.NotValid)
+
+	cfg = s.getConfig()
+	cfg.Clock = nil
+	c.Check(cfg.Validate(), jc.ErrorIs, errors.NotValid)
+
+	cfg = s.getConfig()
+	cfg.Logger = nil
+	c.Check(cfg.Validate(), jc.ErrorIs, errors.NotValid)
+
+	cfg = s.getConfig()
+	cfg.NewApp = nil
+	c.Check(cfg.Validate(), jc.ErrorIs, errors.NotValid)
+
+	cfg = s.getConfig()
+	cfg.NewNodeManager = nil
+	c.Check(cfg.Validate(), jc.ErrorIs, errors.NotValid)
+}
+
+func (s *manifoldSuite) getConfig() ManifoldConfig {
+	return ManifoldConfig{
+		AgentName: "agent",
+		Clock:     s.clock,
+		Logger:    s.logger,
+		NewApp: func(string) (DBApp, error) {
+			return s.dbApp, nil
+		},
+		NewDBReplWorker: func(context.Context, DBApp, string, ...TrackedDBWorkerOption) (TrackedDB, error) {
+			return nil, nil
+		},
+		NewNodeManager: func(agent.Config, logger.Logger, coredatabase.SlowQueryLogger) NodeManager {
+			return s.nodeManager
+		},
+	}
+}
---
+++ internal/worker/dbreplaccessor/package_test.go
@@ -0,0 +1,111 @@
+// Copyright 2022 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package dbreplaccessor
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	jujutesting "github.com/juju/testing"
+	jc "github.com/juju/testing/checkers"
+	"github.com/juju/worker/v4"
+	"go.uber.org/goleak"
+	"go.uber.org/mock/gomock"
+	gc "gopkg.in/check.v1"
+
+	"github.com/juju/juju/core/logger"
+	domaintesting "github.com/juju/juju/domain/schema/testing"
+	loggertesting "github.com/juju/juju/internal/logger/testing"
+)
+
+//go:generate go run go.uber.org/mock/mockgen -typed -package dbreplaccessor -destination package_mock_test.go github.com/juju/juju/internal/worker/dbreplaccessor DBApp,NodeManager,TrackedDB,Client
+//go:generate go run go.uber.org/mock/mockgen -typed -package dbreplaccessor -destination clock_mock_test.go github.com/juju/clock Clock,Timer
+
+func TestPackage(t *testing.T) {
+	defer goleak.VerifyNone(t)
+
+	gc.TestingT(t)
+}
+
+type baseSuite struct {
+	jujutesting.IsolationSuite
+
+	logger logger.Logger
+
+	clock       *MockClock
+	timer       *MockTimer
+	dbApp       *MockDBApp
+	client      *MockClient
+	nodeManager *MockNodeManager
+}
+
+func (s *baseSuite) setupMocks(c *gc.C) *gomock.Controller {
+	ctrl := gomock.NewController(c)
+
+	s.clock = NewMockClock(ctrl)
+	s.timer = NewMockTimer(ctrl)
+	s.dbApp = NewMockDBApp(ctrl)
+	s.client = NewMockClient(ctrl)
+	s.nodeManager = NewMockNodeManager(ctrl)
+
+	s.logger = loggertesting.WrapCheckLog(c)
+
+	return ctrl
+}
+
+func (s *baseSuite) expectClock() {
+	s.clock.EXPECT().Now().Return(time.Now()).AnyTimes()
+	s.clock.EXPECT().After(gomock.Any()).AnyTimes()
+}
+
+func (s *baseSuite) setupTimer(interval time.Duration) chan time.Time {
+	s.timer.EXPECT().Stop().MinTimes(1)
+	s.clock.EXPECT().NewTimer(interval).Return(s.timer)
+
+	ch := make(chan time.Time)
+	s.timer.EXPECT().Chan().Return(ch).AnyTimes()
+	return ch
+}
+
+func (s *baseSuite) newWorkerWithDB(c *gc.C, db TrackedDB) worker.Worker {
+	cfg := WorkerConfig{
+		NodeManager: s.nodeManager,
+		Clock:       s.clock,
+		Logger:      s.logger,
+		NewApp: func(driverName string) (DBApp, error) {
+			return s.dbApp, nil
+		},
+		NewDBReplWorker: func(context.Context, DBApp, string, ...TrackedDBWorkerOption) (TrackedDB, error) {
+			return db, nil
+		},
+	}
+
+	w, err := NewWorker(cfg)
+	c.Assert(err, jc.ErrorIsNil)
+	return w
+}
+
+type dbBaseSuite struct {
+	domaintesting.ControllerSuite
+	baseSuite
+}
+
+func (s *dbBaseSuite) SetUpTest(c *gc.C) {
+	s.ControllerSuite.SetUpTest(c)
+	s.baseSuite.SetUpTest(c)
+}
+
+func (s *dbBaseSuite) TearDownTest(c *gc.C) {
+	s.ControllerSuite.TearDownTest(c)
+	s.baseSuite.TearDownTest(c)
+}
+
+func ensureStartup(c *gc.C, w *dbReplWorker) {
+	select {
+	case <-w.dbReplReady:
+	case <-time.After(jujutesting.LongWait):
+		c.Fatal("timed out waiting for Dqlite node start")
+	}
+}
