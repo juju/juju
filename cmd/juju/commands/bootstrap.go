juju/juju

commit 4a9a7443613cedbd77817d9e606528a8d931978b

feat: set authorized keys during bootstrap
This PR introduces the concept of setting authorized key for the
controller model during bootstrap. We are now removing authorized keys
from model config during bootstrap and carrying them all the way through
directly to the state init params and the bootstrap machine.

This change will also affect how authorized keys are applied to machines
started through the provisioner.

cmd/juju/commands/bootstrap.go
@@ -53,6 +53,7 @@ import (
 	_ "github.com/juju/juju/internal/provider/all" // Import all the providers for bootstrap.
 	"github.com/juju/juju/internal/provider/lxd/lxdnames"
 	"github.com/juju/juju/internal/proxy"
+	"github.com/juju/juju/internal/ssh"
 	"github.com/juju/juju/internal/storage"
 	"github.com/juju/juju/internal/uuid"
 	"github.com/juju/juju/juju"
@@ -165,6 +166,15 @@ other controllers for cross-model (cross-controller, actually) relations to work
 
 If a storage pool is specified using --storage-pool, this will be created
 in the controller model.
+
+By default the bootstrap command will add the users ssh public keys as
+authorized for ssh onto the controller machine and also the controller model.
+Bootstrap will read common public keys from the users .ssh directory and also
+create a default ssh key pair in the juju home directory. These keys will be
+added and authorized during bootstrap.
+
+Authorized keys can be set by using --config authorized-keys and or
+--config authorized-keys-path.
 `
 
 var usageBootstrapConfigTxt = `
@@ -330,8 +340,8 @@ func (c *bootstrapCommand) configDetails() ConfigCategoryKeys {
 		categoryKeys.ControllerKeys = controllerCgf
 	}
 
-	categoryKeys.BootstrapKeys = make(map[string]common.PrintConfigSchema, len(bootstrap.BootstrapConfigSchema))
-	for key, attr := range bootstrap.BootstrapConfigSchema {
+	categoryKeys.BootstrapKeys = make(map[string]common.PrintConfigSchema, len(bootstrap.BootstrapConfigSchema()))
+	for key, attr := range bootstrap.BootstrapConfigSchema() {
 		categoryKeys.BootstrapKeys[key] = common.PrintConfigSchema{
 			Description: attr.Description,
 			Type:        string(attr.Type),
@@ -850,29 +860,30 @@ to create a new model to deploy %sworkloads.
 	logger.Tracef("supported bootstrap bases %v", supportedBootstrapBases)
 
 	bootstrapParams := bootstrap.BootstrapParams{
-		ControllerName:            c.controllerName,
-		BootstrapBase:             bootstrapBase,
-		SupportedBootstrapBases:   supportedBootstrapBases,
-		BootstrapImage:            c.BootstrapImage,
-		Placement:                 c.Placement,
-		BuildAgent:                c.BuildAgent,
-		BuildAgentTarball:         sync.BuildAgentTarball,
-		AgentVersion:              c.AgentVersion,
-		Cloud:                     cloud,
-		CloudRegion:               region.Name,
-		ControllerConfig:          bootstrapCfg.controller,
-		ControllerInheritedConfig: bootstrapCfg.inheritedControllerAttrs,
-		RegionInheritedConfig:     cloud.RegionConfig,
-		AdminSecret:               bootstrapCfg.bootstrap.AdminSecret,
-		CAPrivateKey:              bootstrapCfg.bootstrap.CAPrivateKey,
-		ControllerServiceType:     bootstrapCfg.bootstrap.ControllerServiceType,
-		ControllerExternalName:    bootstrapCfg.bootstrap.ControllerExternalName,
-		ControllerExternalIPs:     append([]string(nil), bootstrapCfg.bootstrap.ControllerExternalIPs...),
-		JujuDbSnapPath:            c.JujuDbSnapPath,
-		JujuDbSnapAssertionsPath:  c.JujuDbSnapAssertionsPath,
-		StoragePools:              bootstrapCfg.storagePools,
-		ControllerCharmPath:       c.ControllerCharmPath,
-		ControllerCharmChannel:    c.ControllerCharmChannel,
+		ControllerName:                c.controllerName,
+		BootstrapBase:                 bootstrapBase,
+		SupportedBootstrapBases:       supportedBootstrapBases,
+		BootstrapImage:                c.BootstrapImage,
+		Placement:                     c.Placement,
+		BuildAgent:                    c.BuildAgent,
+		BuildAgentTarball:             sync.BuildAgentTarball,
+		AgentVersion:                  c.AgentVersion,
+		Cloud:                         cloud,
+		CloudRegion:                   region.Name,
+		ControllerConfig:              bootstrapCfg.controller,
+		ControllerInheritedConfig:     bootstrapCfg.inheritedControllerAttrs,
+		ControllerModelAuthorizedKeys: bootstrapCfg.bootstrap.AuthorizedKeys,
+		RegionInheritedConfig:         cloud.RegionConfig,
+		AdminSecret:                   bootstrapCfg.bootstrap.AdminSecret,
+		CAPrivateKey:                  bootstrapCfg.bootstrap.CAPrivateKey,
+		ControllerServiceType:         bootstrapCfg.bootstrap.ControllerServiceType,
+		ControllerExternalName:        bootstrapCfg.bootstrap.ControllerExternalName,
+		ControllerExternalIPs:         append([]string(nil), bootstrapCfg.bootstrap.ControllerExternalIPs...),
+		JujuDbSnapPath:                c.JujuDbSnapPath,
+		JujuDbSnapAssertionsPath:      c.JujuDbSnapAssertionsPath,
+		StoragePools:                  bootstrapCfg.storagePools,
+		ControllerCharmPath:           c.ControllerCharmPath,
+		ControllerCharmChannel:        c.ControllerCharmChannel,
 		DialOpts: environs.BootstrapDialOpts{
 			Timeout:        bootstrapCfg.bootstrap.BootstrapTimeout,
 			RetryDelay:     bootstrapCfg.bootstrap.BootstrapRetryDelay,
@@ -1357,6 +1368,8 @@ func (c *bootstrapCommand) credentialsAndRegionName(
 	return creds, regionName, nil
 }
 
+// bootstrapConfigs is a deconstructed representation of all of the config
+// options supplied by a user at bootstrap time into their various buckets.
 type bootstrapConfigs struct {
 	bootstrapModel           map[string]interface{}
 	controller               controller.Config
@@ -1535,6 +1548,23 @@ func (c *bootstrapCommand) bootstrapConfigs(
 		return bootstrapConfigs{}, errors.Annotate(err, "constructing bootstrap config")
 	}
 
+	// If the user has not specified any additional authorized keys at bootstrap
+	// time we will try and add their default keys.
+	if len(bootstrapConfig.AuthorizedKeys) == 0 {
+		userDefaultKeys, err := ssh.GetCommonUserPublicKeys(ctx, ssh.LocalUserSSHFileSystem())
+		if err != nil {
+			return bootstrapConfigs{}, errors.Annotate(err, "reading user ssh keys")
+		}
+		bootstrapConfig.AuthorizedKeys = append(bootstrapConfig.AuthorizedKeys, userDefaultKeys...)
+	}
+
+	// We need to slurp up all of the Juju SSH Keys in the Juju directory.
+	jujuSSHKeys, err := ssh.GetFileSystemPublicKeys(ctx, osenv.JujuXDGDataSSHFS())
+	if err != nil {
+		return bootstrapConfigs{}, errors.Annotate(err, "reading juju home ssh keys")
+	}
+	bootstrapConfig.AuthorizedKeys = append(bootstrapConfig.AuthorizedKeys, jujuSSHKeys...)
+
 	// Pre-process controller attributes.
 	if _, ok := controllerConfigAttrs[controller.CAASOperatorImagePath]; ok {
 		return bootstrapConfigs{}, fmt.Errorf("%q is no longer supported controller configuration",
@@ -1568,10 +1598,6 @@ func (c *bootstrapCommand) bootstrapConfigs(
 			// obtain autocert certificates without listening on port 443.
 			controllerConfig[controller.APIPort] = 443
 		}
-	}
-
-	if err := common.FinalizeAuthorizedKeys(ctx, bootstrapModelConfig); err != nil {
-		return bootstrapConfigs{}, errors.Annotate(err, "finalizing authorized-keys")
 	}
 
 	logger.Debugf("preparing controller with config: %v", bootstrapModelConfig)

environs/config/schema.go
@@ -85,11 +85,6 @@ var configSchema = environschema.Fields{
 		Type:        environschema.Tstring,
 		Group:       environschema.EnvironGroup,
 	},
-	AuthorizedKeysKey: {
-		Description: "Any authorized SSH public keys for the model, as found in a ~/.ssh/authorized_keys file",
-		Type:        environschema.Tstring,
-		Group:       environschema.EnvironGroup,
-	},
 	DefaultBaseKey: {
 		Description: "The default base image to use for deploying charms, will act like --base when deploying charms",
 		Type:        environschema.Tstring,

environs/bootstrap.go
@@ -21,6 +21,12 @@ import (
 
 // BootstrapParams holds the parameters for bootstrapping an environment.
 type BootstrapParams struct {
+	// AuthorizedKeys is the set of authorized keys to be allowed to ssh to the
+	// bootstrap instance during bootstrap. This may not be the same set of keys
+	// that are allowed after the controller takes over management of the
+	// instance.
+	AuthorizedKeys []string
+
 	// Cloud contains the name of the cloud that Juju will be
 	// bootstrapped in. Used for printing feedback during bootstrap.
 	CloudName string

environs/bootstrap/bootstrap.go
@@ -14,7 +14,6 @@ import (
 	"github.com/juju/errors"
 	"github.com/juju/names/v5"
 	"github.com/juju/utils/v4"
-	"github.com/juju/utils/v4/ssh"
 	"github.com/juju/version/v2"
 
 	"github.com/juju/juju/api"
@@ -100,6 +99,10 @@ type BootstrapParams struct {
 	// ControllerInheritedConfig is the set of config attributes to be shared
 	// across all models in the same controller.
 	ControllerInheritedConfig map[string]interface{}
+
+	// ControllerModelAuthorizedKeys is a set of pre-allowed authorized keys
+	// for the initial controller model.
+	ControllerModelAuthorizedKeys []string
 
 	// RegionInheritedConfig holds region specific configuration attributes to
 	// be shared across all models in the same controller on a particular
@@ -309,14 +312,6 @@ func bootstrapIAAS(
 	bootstrapParams environs.BootstrapParams,
 ) error {
 	cfg := environ.Config()
-	if authKeys := ssh.SplitAuthorisedKeys(cfg.AuthorizedKeys()); len(authKeys) == 0 {
-		// Apparently this can never happen, so it's not tested. But, one day,
-		// Config will act differently (it's pretty crazy that, AFAICT, the
-		// authorized-keys are optional config settings... but it's impossible
-		// to actually *create* a config without them)... and when it does,
-		// we'll be here to catch this problem early.
-		return errors.Errorf("model configuration has no authorized-keys")
-	}
 
 	_, supportsNetworking := environs.SupportsNetworking(environ)
 	logger.Debugf("model %q supports application/machine networks: %v", cfg.Name(), supportsNetworking)
@@ -699,6 +694,9 @@ func Bootstrap(
 	}
 
 	bootstrapParams := environs.BootstrapParams{
+		// We set the authorized keys allowed to bootstrap to the controller
+		// instance during bootstrap.
+		AuthorizedKeys:             args.ControllerModelAuthorizedKeys,
 		CloudName:                  args.Cloud.Name,
 		CloudRegion:                args.CloudRegion,
 		ControllerConfig:           args.ControllerConfig,
@@ -781,6 +779,7 @@ func finalizeInstanceBootstrapConfig(
 		CAPrivateKey: args.CAPrivateKey,
 	}
 	icfg.Bootstrap.StateInitializationParams.AgentVersion = agentVersion
+	icfg.Bootstrap.StateInitializationParams.ControllerModelAuthorizedKeys = args.ControllerModelAuthorizedKeys
 	icfg.Bootstrap.ControllerModelConfig = cfg
 	icfg.Bootstrap.ControllerModelEnvironVersion = environVersion
 	icfg.Bootstrap.CustomImageMetadata = customImageMetadata
@@ -859,6 +858,7 @@ func finalizePodBootstrapConfig(
 		pcfg.AgentEnvironment[k] = v
 	}
 
+	pcfg.Bootstrap.ControllerModelAuthorizedKeys = args.ControllerModelAuthorizedKeys
 	pcfg.Bootstrap.ControllerModelConfig = cfg
 	pcfg.Bootstrap.ControllerCloud = args.Cloud
 	pcfg.Bootstrap.ControllerCloudRegion = args.CloudRegion

internal/provider/common/bootstrap.go
@@ -43,6 +43,7 @@ import (
 	"github.com/juju/juju/internal/cloudconfig/sshinit"
 	internallogger "github.com/juju/juju/internal/logger"
 	pkissh "github.com/juju/juju/internal/pki/ssh"
+	jujussh "github.com/juju/juju/internal/ssh"
 	"github.com/juju/juju/internal/storage"
 	coretools "github.com/juju/juju/internal/tools"
 )
@@ -145,6 +146,9 @@ func BootstrapInstance(
 	if err != nil {
 		return nil, nil, nil, err
 	}
+
+	ak := jujussh.MakeAuthorizedKeysString(args.AuthorizedKeys)
+	instanceConfig.AuthorizedKeys = ak
 
 	envCfg := env.Config()
 	instanceConfig.EnableOSRefreshUpdate = envCfg.EnableOSRefreshUpdate()

cmd/juju/commands/bootstrap_test.go
@@ -317,10 +317,9 @@ func (s *BootstrapSuite) run(c *gc.C, test bootstrapTest) testing.Restorer {
 	c.Assert(bootstrapConfig.Cloud, gc.Equals, "dummy")
 	c.Assert(bootstrapConfig.Credential, gc.Equals, "")
 	expected := map[string]interface{}{
-		"name":            bootstrap.ControllerModelName,
-		"type":            "dummy",
-		"default-base":    "ubuntu@22.04",
-		"authorized-keys": "public auth key\n",
+		"name":         bootstrap.ControllerModelName,
+		"type":         "dummy",
+		"default-base": "ubuntu@22.04",
 		// Dummy provider defaults
 		"broken":   "",
 		"secret":   "pork",

environs/bootstrap/bootstrap_test.go
@@ -207,6 +207,31 @@ func (s *bootstrapSuite) TestBootstrapEmptyConstraints(c *gc.C) {
 	})
 }
 
+// TestBootstrapControllerModelAuthorizedKeys is asserting that the authorized
+// keys for the controller model are being populated as authorized keys for the
+// controller machine during bootstrap.
+func (s *bootstrapSuite) TestBootstrapControllerModelAuthorizedKeys(c *gc.C) {
+	env := newEnviron("foo", useDefaultKeys, nil)
+	s.setDummyStorage(c, env)
+	err := bootstrap.Bootstrap(envtesting.BootstrapTestContext(c), env,
+		s.callContext, bootstrap.BootstrapParams{
+			AdminSecret:                   "admin-secret",
+			ControllerConfig:              coretesting.FakeControllerConfig(),
+			CAPrivateKey:                  coretesting.CAKey,
+			ControllerModelAuthorizedKeys: []string{"key1"},
+			SupportedBootstrapBases:       supportedJujuBases,
+		})
+	c.Assert(err, jc.ErrorIsNil)
+	c.Assert(env.bootstrapCount, gc.Equals, 1)
+	env.args.AvailableTools = nil
+	env.args.SupportedBootstrapBases = nil
+	c.Assert(env.args, gc.DeepEquals, environs.BootstrapParams{
+		ControllerConfig:     coretesting.FakeControllerConfig(),
+		AuthorizedKeys:       []string{"key1"},
+		BootstrapConstraints: constraints.MustParse("mem=3.5G"),
+	})
+}
+
 func (s *bootstrapSuite) TestBootstrapSpecifiedConstraints(c *gc.C) {
 	env := newEnviron("foo", useDefaultKeys, nil)
 	s.setDummyStorage(c, env)

environs/bootstrap/config.go
@@ -20,12 +20,26 @@ import (
 	"github.com/juju/juju/caas"
 	coreconfig "github.com/juju/juju/core/config"
 	"github.com/juju/juju/internal/pki"
+	"github.com/juju/juju/internal/ssh"
 	"github.com/juju/juju/juju/osenv"
 )
 
 const (
 	// AdminSecretKey is the attribute key for the administrator password.
 	AdminSecretKey = "admin-secret"
+
+	// authorizedKeysDelimiter denotes the delimiter used when reading the value
+	// for [AuthorizedKeysKey] to seperate multiple ssh public keys.
+	authorizedKeysDelimiter = ';'
+
+	// AuthorizedKeysKey is the key used for supplying additional authorized
+	// keys to be allowed to the controller model during bootstrap.
+	AuthorizedKeysKey = "authorized-keys"
+
+	// AuthorizedKeysPathKey is the key used for supplying a path to an
+	// authorized key file that will be used for adding additional authorized
+	// keys to the controller model during bootstrap.
+	AuthorizedKeysPathKey = AuthorizedKeysKey + "-path"
 
 	// CACertKey is the attribute key for the controller's CA certificate.
 	CACertKey = "ca-cert"
@@ -77,9 +91,11 @@ const (
 
 // BootstrapConfigAttributes are attributes which may be defined by the
 // user at bootstrap time, but should not be present in general controller
-// config.
+// config or model config.
 var BootstrapConfigAttributes = []string{
 	AdminSecretKey,
+	AuthorizedKeysKey,
+	AuthorizedKeysPathKey,
 	CACertKey,
 	CAPrivateKeyKey,
 	BootstrapTimeoutKey,
@@ -90,59 +106,83 @@ var BootstrapConfigAttributes = []string{
 	ControllerExternalIPs,
 }
 
-// BootstrapConfigSchema defines the schema used for config items during
+// BootstrapConfigSchema returns the schema used for config items during
 // bootstrap.
-var BootstrapConfigSchema = environschema.Fields{
-	AdminSecretKey: {
-		Description: "Sets the Juju administrator password",
-		Type:        environschema.Tstring,
-	},
-	CACertKey: {
-		Description: fmt.Sprintf(
-			"Sets the bootstrapped controllers CA cert to use and issue "+
-				"certificates from, used in conjunction with %s",
-			CAPrivateKeyKey),
-		Type: environschema.Tstring,
-	},
-	CAPrivateKeyKey: {
-		Description: fmt.Sprintf(
-			"Sets the bootstrapped controllers CA cert private key to sign "+
-				"certificates with, used in conjunction with %s",
-			CACertKey),
-		Type: environschema.Tstring,
-	},
-	BootstrapTimeoutKey: {
-		Description: "Controls how long Juju will wait for a bootstrap to " +
-			"complete before considering it failed in seconds",
-		Type: environschema.Tint,
-	},
-	BootstrapRetryDelayKey: {
-		Description: "Controls the amount of time in seconds between attempts " +
-			"to connect to a bootstrap machine address",
-		Type: environschema.Tint,
-	},
-	BootstrapAddressesDelayKey: {
-		Description: "Controls the amount of time in seconds in between " +
-			"refreshing the bootstrap machine addresses",
-		Type: environschema.Tint,
-	},
-	ControllerServiceType: {
-		Description: "Controls the kubernetes service type for Juju " +
-			"controllers, see\n" +
-			"https://kubernetes.io/docs/reference/kubernetes-api/service-resources/service-v1/#ServiceSpec\n" +
-			"valid values are one of cluster, loadbalancer, external",
-		Type: environschema.Tstring,
-	},
-	ControllerExternalName: {
-		Description: "Sets the external name for a k8s controller of type " +
-			"external",
-		Type: environschema.Tstring,
-	},
-	ControllerExternalIPs: {
-		Description: "Specifies a comma separated list of external IPs for a " +
-			"k8s controller of type external",
-		Type: environschema.Tlist,
-	},
+func BootstrapConfigSchema() environschema.Fields {
+	return environschema.Fields{
+		// TODO (tlm): It is unclear why we define this schema twice in this file.
+		// Take a look at [configSchema] that repeats this information again and is
+		// what is actually used by this file. This information is purely used for
+		// display purposed in help for the bootstrap command.
+		//
+		// Ideally we can just merge these two schemas together and stop repeating
+		// ourselves.
+		AdminSecretKey: {
+			Description: "Sets the Juju administrator password",
+			Type:        environschema.Tstring,
+		},
+		AuthorizedKeysKey: {
+			Description: "Additional authorized SSH public keys for the " +
+				"initial controller model, as found in a " +
+				"~/.ssh/authorized_keys file. Multiple keys are delimited by ';'",
+			Type: environschema.Tstring,
+		},
+		AuthorizedKeysPathKey: {
+			Description: fmt.Sprintf(
+				"Additional authorized SSH public keys to be read "+
+					"from a ~/.ssh/authorized_keys file. Keys defined in this "+
+					"file are appended to those already defined in %s",
+				AuthorizedKeysKey,
+			),
+			Type: environschema.Tstring,
+		},
+		CACertKey: {
+			Description: fmt.Sprintf(
+				"Sets the bootstrapped controllers CA cert to use and issue "+
+					"certificates from, used in conjunction with %s",
+				CAPrivateKeyKey),
+			Type: environschema.Tstring,
+		},
+		CAPrivateKeyKey: {
+			Description: fmt.Sprintf(
+				"Sets the bootstrapped controllers CA cert private key to sign "+
+					"certificates with, used in conjunction with %s",
+				CACertKey),
+			Type: environschema.Tstring,
+		},
+		BootstrapTimeoutKey: {
+			Description: "Controls how long Juju will wait for a bootstrap to " +
+				"complete before considering it failed in seconds",
+			Type: environschema.Tint,
+		},
+		BootstrapRetryDelayKey: {
+			Description: "Controls the amount of time in seconds between attempts " +
+				"to connect to a bootstrap machine address",
+			Type: environschema.Tint,
+		},
+		BootstrapAddressesDelayKey: {
+			Description: "Controls the amount of time in seconds in between " +
+				"refreshing the bootstrap machine addresses",
+			Type: environschema.Tint,
+		},
+		ControllerServiceType: {
+			Description: "Controls the kubernetes service type for Juju " +
+				"controllers, see\n" +
+				"https://kubernetes.io/docs/reference/kubernetes-api/service-resources/service-v1/#ServiceSpec\n" +
+				"valid values are one of cluster, loadbalancer, external",
+			Type: environschema.Tstring,
+		},
+		ControllerExternalName: {
+			Description: "Sets the external name for a k8s controller of type " +
+				"external",
+			Type: environschema.Tstring,
+		},
+		ControllerExternalIPs: {
+			Description: "Specifies a comma separated list of external IPs for a " +
+				"k8s controller of type external",
+			Type: environschema.Tlist,
+		},
+	}
 }
 
 // IsBootstrapAttribute reports whether or not the specified
@@ -158,7 +198,10 @@ func IsBootstrapAttribute(attr string) bool {
 
 // Config contains bootstrap-specific configuration.
 type Config struct {
-	AdminSecret             string
+	AdminSecret string
+	// AuthorizedKeys is a set of additional authorized keys to be used during
+	// bootstrap.
+	AuthorizedKeys          []string
 	CACert                  string
 	CAPrivateKey            string
 	ControllerServiceType   string
@@ -288,6 +331,35 @@ func NewConfig(attrs map[string]interface{}) (Config, error) {
 		config.CAPrivateKey = caKeyPem
 	}
 
+	// If authorized keys is not returned we will just get back the zero value
+	// of a string which is safe to parse.
+	authorizedKeys, _ := attrs[AuthorizedKeysKey].(string)
+	config.AuthorizedKeys, err = ssh.SplitAuthorizedKeysByDelimiter(authorizedKeysDelimiter, authorizedKeys)
+	if err != nil {
+		return Config{}, fmt.Errorf("cannot parse and split authorized keys: %w", err)
+	}
+
+	if authorizedKeysFilePath, ok := attrs[AuthorizedKeysPathKey].(string); ok {
+		file, err := os.Open(authorizedKeysFilePath)
+		if err != nil {
+			return Config{}, fmt.Errorf(
+				"cannot open authorised key file %q: %w",
+				authorizedKeysFilePath, err,
+			)
+		}
+		defer file.Close()
+
+		keys, err := ssh.SplitAuthorizedKeysReader(file)
+		if err != nil {
+			return Config{}, fmt.Errorf(
+				"cannot split authorized key file %q: %w",
+				authorizedKeysFilePath, err,
+			)
+		}
+
+		config.AuthorizedKeys = append(config.AuthorizedKeys, keys...)
+	}
+
 	return config, config.Validate()
 }
 
@@ -320,6 +392,14 @@ func readFileAttr(attrs map[string]interface{}, key, defaultPath string) (conten
 
 var configSchema = environschema.Fields{
 	AdminSecretKey: {
+		Type:  environschema.Tstring,
+		Group: environschema.JujuGroup,
+	},
+	AuthorizedKeysKey: {
+		Type:  environschema.Tstring,
+		Group: environschema.JujuGroup,
+	},
+	AuthorizedKeysPathKey: {
 		Type:  environschema.Tstring,
 		Group: environschema.JujuGroup,
 	},
@@ -372,6 +452,8 @@ var configSchema = environschema.Fields{
 
 var configDefaults = schema.Defaults{
 	AdminSecretKey:             schema.Omit,
+	AuthorizedKeysKey:          schema.Omit,
+	AuthorizedKeysPathKey:      schema.Omit,
 	CACertKey:                  schema.Omit,
 	CACertKey + "-path":        schema.Omit,
 	CAPrivateKeyKey:            schema.Omit,

commit f02d87b991a8e8a7fafd84d2406c2a51bb43ee14

fix(bootstrap): add support for authorized SSH keys for manual cloud
Fixup a regression in commit:
https://github.com/juju/juju/commit/4a9a7443613cedbd77817d9e606528a8d931978b

- Before this commit, trying to bootstrap a manual cloud will fails with a permission error,
  since authorization keys weren't not added to the machine.
- After this commit, authorization keys are correctly pushed to the target machine and the bootstrap
  works.

cmd/juju/commands/bootstrap.go
@@ -808,6 +808,7 @@ to create a new model to deploy %sworkloads.
 
 	bootstrapCtx := environscmd.BootstrapContext(stdCtx, ctx)
 	bootstrapPrepareParams := bootstrap.PrepareParams{
+		AuthorizedKeys:   bootstrapCfg.bootstrap.AuthorizedKeys,
 		ModelConfig:      bootstrapCfg.bootstrapModel,
 		ControllerConfig: bootstrapCfg.controller,
 		ControllerName:   c.controllerName,

environs/bootstrap/prepare.go
@@ -4,6 +4,9 @@
 package bootstrap
 
 import (
+	"maps"
+	"strings"
+
 	"github.com/juju/errors"
 	"github.com/juju/names/v5"
 
@@ -34,6 +37,9 @@ const (
 // PrepareParams contains the parameters for preparing a controller Environ
 // for bootstrapping.
 type PrepareParams struct {
+	// AuthorizedKeys is a list of SSH authorized keys to add to the controller environment.
+	AuthorizedKeys []string
+
 	// ModelConfig contains the base configuration for the controller model.
 	//
 	// This includes the model name, cloud type, any user-supplied
@@ -184,7 +190,12 @@ func prepare(
 ) (*config.Config, prepareDetails, error) {
 	var details prepareDetails
 
-	cfg, err := config.New(config.NoDefaults, args.ModelConfig)
+	completeConfig := map[string]any{
+		config.AuthorizedKeysKey: strings.Join(args.AuthorizedKeys, "\n"),
+	}
+	maps.Copy(completeConfig, args.ModelConfig)
+
+	cfg, err := config.New(config.NoDefaults, completeConfig)
 	if err != nil {
 		return cfg, details, errors.Trace(err)
 	}

environs/config/schema.go
@@ -85,6 +85,11 @@ var configSchema = environschema.Fields{
 		Type:        environschema.Tstring,
 		Group:       environschema.EnvironGroup,
 	},
+	AuthorizedKeysKey: {
+		Description: "Any authorized SSH public keys for the model, as found in a ~/.ssh/authorized_keys file",
+		Type:        environschema.Tstring,
+		Group:       environschema.EnvironGroup,
+	},
 	DefaultBaseKey: {
 		Description: "The default base image to use for deploying charms, will act like --base when deploying charms",
 		Type:        environschema.Tstring,