cad2011bcff23eef97d212d906da89ba51f498ac
Simon Richardson <stickupkid@gmail.com>
2025-01-07 20:17:50 +0000

    refactor: removes the old repl
    This removes the old REPL and instead favours the new one. If there
    are any missing features, we should port them to the new REPL.
    
    > The King is dead, long live the King!

--- scripts/dqlite/scripts/repl/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-MACHINE=${MACHINE:-0}
-
-juju exec -m controller --machine=${MACHINE} 'which dqlite &>/dev/null || sudo DEBIAN_FRONTEND=noninteractive add-apt-repository -y ppa:dqlite/dev && sudo DEBIAN_FRONTEND=noninteractive apt install -y dqlite-tools'
