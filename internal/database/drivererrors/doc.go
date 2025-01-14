2a04aeb697ed2af2a850aaebe018e26381f78536
Simon Richardson <stickupkid@gmail.com>
2025-01-14 11:02:13 +0000

    chore: add missing copyright header

--- internal/database/drivererrors/doc.go
+++ internal/database/drivererrors/doc.go
@@ -1,3 +1,6 @@
+// Copyright 2023 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
 // DriverErrors package is the internal errors package, which provides extensive
 // error handling for the dqlite database. It is not expected that the domain
 // package will need to interact with this package directly, instead it should
