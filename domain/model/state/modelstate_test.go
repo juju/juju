99e00b65ee4a7aef3567ebdd121950f349976543
Kelvin Liu <kelvin.liu@canonical.com>
2025-01-16 17:30:42 +1100

    feat: add tests for model constraints getter and setter state methods;

--- domain/model/state/modelstate_test.go
+++ domain/model/state/modelstate_test.go
@@ -9,6 +9,7 @@ import (
 	jc "github.com/juju/testing/checkers"
 	gc "gopkg.in/check.v1"
 
+	"github.com/juju/juju/core/constraints"
 	coremodel "github.com/juju/juju/core/model"
 	modeltesting "github.com/juju/juju/core/model/testing"
 	usertesting "github.com/juju/juju/core/user/testing"
@@ -266,6 +267,254 @@ func (s *modelSuite) TestGetModelMetricsNotFound(c *gc.C) {
 
 	_, err := state.GetModelMetrics(context.Background())
 	c.Assert(err, jc.ErrorIs, modelerrors.NotFound)
+}
+
+func ptr[T any](s T) *T {
+	return &s
+}
+
+func (s *modelSuite) TestSetModelConstraints(c *gc.C) {
+	runner := s.TxnRunnerFactory()
+	state := NewModelState(runner, loggertesting.WrapCheckLog(c))
+
+	id := modeltesting.GenModelUUID(c)
+	args := model.ReadOnlyModelCreationArgs{
+		UUID:            id,
+		AgentVersion:    jujuversion.Current,
+		ControllerUUID:  s.controllerUUID,
+		Name:            "my-awesome-model",
+		Type:            coremodel.IAAS,
+		Cloud:           "aws",
+		CloudType:       "ec2",
+		CloudRegion:     "myregion",
+		CredentialOwner: usertesting.GenNewName(c, "myowner"),
+		CredentialName:  "mycredential",
+	}
+	err := state.Create(context.Background(), args)
+	c.Assert(err, jc.ErrorIsNil)
+
+	_, err = s.DB().ExecContext(context.Background(),
+		`INSERT INTO space (uuid, name) VALUES (?, ?)`, uuid.MustNewUUID().String(), "space1")
+	c.Assert(err, jc.ErrorIsNil)
+
+	err = state.SetModelConstraints(context.Background(), constraints.Value{
+		Arch:             ptr("amd64"),
+		CpuCores:         ptr(uint64(4)),
+		Mem:              ptr(uint64(1024)),
+		RootDisk:         ptr(uint64(1024)),
+		RootDiskSource:   ptr("root-disk-source"),
+		Tags:             ptr([]string{"tag1", "tag2"}),
+		InstanceRole:     ptr("instance-role"),
+		InstanceType:     ptr("instance-type"),
+		Spaces:           ptr([]string{"space1"}),
+		VirtType:         ptr("virt-type"),
+		Zones:            ptr([]string{"zone1", "zone2"}),
+		AllocatePublicIP: ptr(true),
+		ImageID:          ptr("image-id"),
+	})
+	c.Assert(err, jc.ErrorIsNil)
+
+	var consData dbConstraint
+	err = s.DB().QueryRowContext(context.Background(), `
+SELECT arch, cpu_cores, mem, root_disk, root_disk_source, 
+	instance_role, instance_type, 
+	virt_type, allocate_public_ip, image_id
+FROM "constraint"`).Scan(
+		&consData.Arch, &consData.CPUCores, &consData.Mem,
+		&consData.RootDisk, &consData.RootDiskSource,
+		&consData.InstanceRole, &consData.InstanceType, &consData.VirtType,
+		&consData.AllocatePublicIP, &consData.ImageID,
+	)
+	c.Assert(err, jc.ErrorIsNil)
+
+	c.Assert(consData, jc.DeepEquals, dbConstraint{
+		Arch:             ptr("amd64"),
+		CPUCores:         ptr(uint64(4)),
+		Mem:              ptr(uint64(1024)),
+		RootDisk:         ptr(uint64(1024)),
+		RootDiskSource:   ptr("root-disk-source"),
+		InstanceRole:     ptr("instance-role"),
+		InstanceType:     ptr("instance-type"),
+		VirtType:         ptr("virt-type"),
+		AllocatePublicIP: ptr(true),
+		ImageID:          ptr("image-id"),
+	})
+
+	var tags []string
+	row, err := s.DB().QueryContext(context.Background(), `
+SELECT tag FROM constraint_tag`)
+	c.Assert(err, jc.ErrorIsNil)
+	for row.Next() {
+		var tag string
+		err := row.Scan(&tag)
+		c.Assert(err, jc.ErrorIsNil)
+		tags = append(tags, tag)
+	}
+	c.Assert(tags, jc.DeepEquals, []string{"tag1", "tag2"})
+
+	var spaces []string
+	row, err = s.DB().QueryContext(context.Background(), `
+SELECT space FROM constraint_space`)
+	for row.Next() {
+		var space string
+		err := row.Scan(&space)
+		c.Assert(err, jc.ErrorIsNil)
+		spaces = append(spaces, space)
+	}
+	c.Assert(spaces, jc.DeepEquals, []string{"space1"})
+
+	var zones []string
+	row, err = s.DB().QueryContext(context.Background(), `
+SELECT zone FROM constraint_zone`)
+	for row.Next() {
+		var zone string
+		err := row.Scan(&zone)
+		c.Assert(err, jc.ErrorIsNil)
+		zones = append(zones, zone)
+	}
+	c.Assert(zones, jc.DeepEquals, []string{"zone1", "zone2"})
+}
+
+func (s *modelSuite) TestSetModelConstraintFailedModelNotFound(c *gc.C) {
+	runner := s.TxnRunnerFactory()
+	state := NewModelState(runner, loggertesting.WrapCheckLog(c))
+
+	err := state.SetModelConstraints(context.Background(), constraints.Value{
+		Arch: ptr("amd64"),
+	})
+	c.Assert(err, jc.ErrorIs, modelerrors.NotFound)
+}
+
+func (s *modelSuite) TestSetModelConstraintFailedSpaceDoesNotExist(c *gc.C) {
+	runner := s.TxnRunnerFactory()
+	state := NewModelState(runner, loggertesting.WrapCheckLog(c))
+
+	id := modeltesting.GenModelUUID(c)
+	args := model.ReadOnlyModelCreationArgs{
+		UUID:            id,
+		AgentVersion:    jujuversion.Current,
+		ControllerUUID:  s.controllerUUID,
+		Name:            "my-awesome-model",
+		Type:            coremodel.IAAS,
+		Cloud:           "aws",
+		CloudType:       "ec2",
+		CloudRegion:     "myregion",
+		CredentialOwner: usertesting.GenNewName(c, "myowner"),
+		CredentialName:  "mycredential",
+	}
+	err := state.Create(context.Background(), args)
+	c.Assert(err, jc.ErrorIsNil)
+
+	err = state.SetModelConstraints(context.Background(), constraints.Value{
+		Spaces: ptr([]string{"space1"}),
+	})
+	c.Assert(err, jc.ErrorIs, modelerrors.ModelConstraintSpaceDoesNotExist)
+}
+
+func (s *modelSuite) TestGetModelConstraints(c *gc.C) {
+	runner := s.TxnRunnerFactory()
+	state := NewModelState(runner, loggertesting.WrapCheckLog(c))
+
+	id := modeltesting.GenModelUUID(c)
+	args := model.ReadOnlyModelCreationArgs{
+		UUID:            id,
+		AgentVersion:    jujuversion.Current,
+		ControllerUUID:  s.controllerUUID,
+		Name:            "my-awesome-model",
+		Type:            coremodel.IAAS,
+		Cloud:           "aws",
+		CloudType:       "ec2",
+		CloudRegion:     "myregion",
+		CredentialOwner: usertesting.GenNewName(c, "myowner"),
+		CredentialName:  "mycredential",
+	}
+	err := state.Create(context.Background(), args)
+	c.Assert(err, jc.ErrorIsNil)
+
+	_, err = s.DB().ExecContext(context.Background(),
+		`INSERT INTO space (uuid, name) VALUES (?, ?)`, uuid.MustNewUUID().String(), "space1")
+	c.Assert(err, jc.ErrorIsNil)
+
+	err = state.SetModelConstraints(context.Background(), constraints.Value{
+		Arch:             ptr("amd64"),
+		CpuCores:         ptr(uint64(4)),
+		Mem:              ptr(uint64(1024)),
+		RootDisk:         ptr(uint64(1024)),
+		RootDiskSource:   ptr("root-disk-source"),
+		Tags:             ptr([]string{"tag1", "tag2"}),
+		InstanceRole:     ptr("instance-role"),
+		InstanceType:     ptr("instance-type"),
+		Spaces:           ptr([]string{"space1"}),
+		VirtType:         ptr("virt-type"),
+		Zones:            ptr([]string{"zone1", "zone2"}),
+		AllocatePublicIP: ptr(true),
+		ImageID:          ptr("image-id"),
+	})
+	c.Assert(err, jc.ErrorIsNil)
+
+	cons, err := state.GetModelConstraints(context.Background())
+	c.Assert(err, jc.ErrorIsNil)
+	c.Assert(cons, jc.DeepEquals, constraints.Value{
+		Arch:             ptr("amd64"),
+		CpuCores:         ptr(uint64(4)),
+		Mem:              ptr(uint64(1024)),
+		RootDisk:         ptr(uint64(1024)),
+		RootDiskSource:   ptr("root-disk-source"),
+		Tags:             ptr([]string{"tag1", "tag2"}),
+		InstanceRole:     ptr("instance-role"),
+		InstanceType:     ptr("instance-type"),
+		Spaces:           ptr([]string{"space1"}),
+		VirtType:         ptr("virt-type"),
+		Zones:            ptr([]string{"zone1", "zone2"}),
+		AllocatePublicIP: ptr(true),
+		ImageID:          ptr("image-id"),
+	})
+}
+
+func (s *modelSuite) TestGetModelConstraintsFailedModelNotFound(c *gc.C) {
+	runner := s.TxnRunnerFactory()
+	state := NewModelState(runner, loggertesting.WrapCheckLog(c))
+
+	err := state.SetModelConstraints(context.Background(), constraints.Value{
+		Arch:             ptr("amd64"),
+		CpuCores:         ptr(uint64(4)),
+		Mem:              ptr(uint64(1024)),
+		RootDisk:         ptr(uint64(1024)),
+		RootDiskSource:   ptr("root-disk-source"),
+		Tags:             ptr([]string{"tag1", "tag2"}),
+		InstanceRole:     ptr("instance-role"),
+		InstanceType:     ptr("instance-type"),
+		Spaces:           ptr([]string{"space1"}),
+		VirtType:         ptr("virt-type"),
+		Zones:            ptr([]string{"zone1", "zone2"}),
+		AllocatePublicIP: ptr(true),
+		ImageID:          ptr("image-id"),
+	})
+	c.Assert(err, jc.ErrorIs, modelerrors.NotFound)
+}
+
+func (s *modelSuite) TestGetModelConstraintsFailedModelConstraintNotFound(c *gc.C) {
+	runner := s.TxnRunnerFactory()
+	state := NewModelState(runner, loggertesting.WrapCheckLog(c))
+
+	id := modeltesting.GenModelUUID(c)
+	args := model.ReadOnlyModelCreationArgs{
+		UUID:            id,
+		AgentVersion:    jujuversion.Current,
+		ControllerUUID:  s.controllerUUID,
+		Name:            "my-awesome-model",
+		Type:            coremodel.IAAS,
+		Cloud:           "aws",
+		CloudType:       "ec2",
+		CloudRegion:     "myregion",
+		CredentialOwner: usertesting.GenNewName(c, "myowner"),
+		CredentialName:  "mycredential",
+	}
+	err := state.Create(context.Background(), args)
+	c.Assert(err, jc.ErrorIsNil)
+
+	_, err = state.GetModelConstraints(context.Background())
+	c.Assert(err, jc.ErrorIs, modelerrors.ModelConstraintNotFound)
 }
 
 func (s *modelSuite) TestGetModelCloudType(c *gc.C) {
