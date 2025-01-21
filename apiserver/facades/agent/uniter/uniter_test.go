ef6386fd3c57973d299eb239823f18e408c249d1
Simon Richardson <stickupkid@gmail.com>
2025-01-21 17:46:18 +0000

    test: add uniter tests for leader settings
    Ensure that the uniter versions correctly return the right values
    for the uniter.

--- apiserver/facades/agent/uniter/uniter_test.go
+++ apiserver/facades/agent/uniter/uniter_test.go
@@ -119,6 +119,14 @@ func (s *uniterSuite) TestCharmArchiveSha256Errors(c *gc.C) {
 	})
 }
 
+func (s *uniterSuite) TestLeadershipSettings(c *gc.C) {
+	defer s.setupMocks(c).Finish()
+
+	s.uniter.Merge(context.Background(), struct{}{}, struct{}{})
+	s.uniter.Read(context.Background(), struct{}{}, struct{}{})
+	s.uniter.WatchLeadershipSettings(context.Background(), struct{}{}, struct{}{})
+}
+
 func (s *uniterSuite) setupMocks(c *gc.C) *gomock.Controller {
 	ctrl := gomock.NewController(c)
 
@@ -129,4 +137,129 @@ func (s *uniterSuite) setupMocks(c *gc.C) *gomock.Controller {
 	}
 
 	return ctrl
+}
+
+type leadershipSettings interface {
+	// Merge merges in the provided leadership settings. Only leaders for
+	// the given service may perform this operation.
+	Merge(ctx context.Context, bulkArgs params.MergeLeadershipSettingsBulkParams) (params.ErrorResults, error)
+
+	// Read reads leadership settings for the provided service ID. Any
+	// unit of the service may perform this operation.
+	Read(ctx context.Context, bulkArgs params.Entities) (params.GetLeadershipSettingsBulkResults, error)
+
+	// WatchLeadershipSettings will block the caller until leadership settings
+	// for the given service ID change.
+	WatchLeadershipSettings(ctx context.Context, bulkArgs params.Entities) (params.NotifyWatchResults, error)
+}
+
+type leadershipUniterSuite struct {
+	testing.IsolationSuite
+
+	watcherRegistry *MockWatcherRegistry
+
+	uniter leadershipSettings
+
+	setupMocks func(c *gc.C) *gomock.Controller
+}
+
+func (s *leadershipUniterSuite) TestLeadershipSettingsMerge(c *gc.C) {
+	defer s.setupMocks(c).Finish()
+
+	results, err := s.uniter.Merge(context.Background(), params.MergeLeadershipSettingsBulkParams{
+		Params: []params.MergeLeadershipSettingsParam{
+			{
+				ApplicationTag: "app1",
+				Settings: params.Settings{
+					"key1": "value1",
+				},
+			},
+		},
+	})
+	c.Assert(err, jc.ErrorIsNil)
+	c.Assert(results, jc.DeepEquals, params.ErrorResults{
+		Results: []params.ErrorResult{{}},
+	})
+}
+
+func (s *leadershipUniterSuite) TestLeadershipSettingsRead(c *gc.C) {
+	defer s.setupMocks(c).Finish()
+
+	results, err := s.uniter.Read(context.Background(), params.Entities{
+		Entities: []params.Entity{
+			{
+				Tag: "app1",
+			},
+		},
+	})
+	c.Assert(err, jc.ErrorIsNil)
+	c.Assert(results, jc.DeepEquals, params.GetLeadershipSettingsBulkResults{
+		Results: []params.GetLeadershipSettingsResult{{}},
+	})
+}
+
+func (s *leadershipUniterSuite) TestLeadershipSettingsWatchLeadershipSettings(c *gc.C) {
+	defer s.setupMocks(c).Finish()
+
+	results, err := s.uniter.WatchLeadershipSettings(context.Background(), params.Entities{
+		Entities: []params.Entity{
+			{
+				Tag: "app1",
+			},
+		},
+	})
+	c.Assert(err, jc.ErrorIsNil)
+	c.Assert(results, jc.DeepEquals, params.NotifyWatchResults{
+		Results: []params.NotifyWatchResult{{
+			NotifyWatcherId: "watcher1",
+		}},
+	})
+}
+
+type uniterv19Suite struct {
+	leadershipUniterSuite
+}
+
+var _ = gc.Suite(&uniterv19Suite{})
+
+func (s *uniterv19Suite) SetUpTest(c *gc.C) {
+	s.setupMocks = func(c *gc.C) *gomock.Controller {
+		ctrl := gomock.NewController(c)
+
+		s.watcherRegistry = NewMockWatcherRegistry(ctrl)
+		s.watcherRegistry.EXPECT().Register(gomock.Any()).Return("watcher1", nil).AnyTimes()
+
+		s.uniter = &UniterAPIv19{
+			UniterAPIv20: &UniterAPIv20{
+				UniterAPI: &UniterAPI{
+					watcherRegistry: s.watcherRegistry,
+				},
+			},
+		}
+
+		return ctrl
+	}
+}
+
+type uniterv20Suite struct {
+	leadershipUniterSuite
+}
+
+var _ = gc.Suite(&uniterv20Suite{})
+
+func (s *uniterv20Suite) SetUpTest(c *gc.C) {
+	s.setupMocks = func(c *gc.C) *gomock.Controller {
+		ctrl := gomock.NewController(c)
+
+		s.watcherRegistry = NewMockWatcherRegistry(ctrl)
+		s.watcherRegistry.EXPECT().Register(gomock.Any()).Return("watcher1", nil).AnyTimes()
+
+		s.uniter = &UniterAPIv20{
+			UniterAPI: &UniterAPI{
+				watcherRegistry: s.watcherRegistry,
+			},
+		}
+
+		return ctrl
+	}
 }
