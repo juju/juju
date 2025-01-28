eb21bb39c38a863dca22f5b71ebc74ea999324ba
Thomas Miller <thomas.miller@canonical.com>
2025-01-28 15:11:25 +1000

    style: fix indentation of command
    Fixes indentation of a command in the model constraint test that was
    using a combination of tabs and spaces.

--- tests/suites/constraints/constraints_model.sh
+++ tests/suites/constraints/constraints_model.sh
@@ -7,9 +7,9 @@ run_constraints_model_bootstrap() {
         log_file="${TEST_DIR}/bootstrap.log"
 
         bootstrap_additional_args=(--constraints "mem=1024M")
-		BOOTSTRAP_ADDITIONAL_ARGS="${bootstrap_additional_args[*]}" \
+        BOOTSTRAP_ADDITIONAL_ARGS="${bootstrap_additional_args[*]}" \
         BOOTSTRAP_REUSE=false \
-			bootstrap "model-constraints" "$log_file"
+            bootstrap "model-constraints" "$log_file"
 
         juju switch controller
         check_contains "$(juju model-constraints)" "mem=1024M"
