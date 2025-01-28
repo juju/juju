f351680c9a84b1e5e0c4becc300a7b34f9ba4ef4
Thomas Miller <thomas.miller@canonical.com>
2025-01-28 15:17:10 +1000

    test: generating updated mocks
    Generates new mocks after rebasing on to latest main.

--- domain/model/service/package_mock_test.go
+++ domain/model/service/package_mock_test.go
@@ -157,6 +157,45 @@ func (c *MockModelStateGetModelCall) DoAndReturn(f func(context.Context) (model.
 	return c
 }
 
+// GetModelCloudType mocks base method.
+func (m *MockModelState) GetModelCloudType(arg0 context.Context) (string, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "GetModelCloudType", arg0)
+	ret0, _ := ret[0].(string)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// GetModelCloudType indicates an expected call of GetModelCloudType.
+func (mr *MockModelStateMockRecorder) GetModelCloudType(arg0 any) *MockModelStateGetModelCloudTypeCall {
+	mr.mock.ctrl.T.Helper()
+	call := mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetModelCloudType", reflect.TypeOf((*MockModelState)(nil).GetModelCloudType), arg0)
+	return &MockModelStateGetModelCloudTypeCall{Call: call}
+}
+
+// MockModelStateGetModelCloudTypeCall wrap *gomock.Call
+type MockModelStateGetModelCloudTypeCall struct {
+	*gomock.Call
+}
+
+// Return rewrite *gomock.Call.Return
+func (c *MockModelStateGetModelCloudTypeCall) Return(arg0 string, arg1 error) *MockModelStateGetModelCloudTypeCall {
+	c.Call = c.Call.Return(arg0, arg1)
+	return c
+}
+
+// Do rewrite *gomock.Call.Do
+func (c *MockModelStateGetModelCloudTypeCall) Do(f func(context.Context) (string, error)) *MockModelStateGetModelCloudTypeCall {
+	c.Call = c.Call.Do(f)
+	return c
+}
+
+// DoAndReturn rewrite *gomock.Call.DoAndReturn
+func (c *MockModelStateGetModelCloudTypeCall) DoAndReturn(f func(context.Context) (string, error)) *MockModelStateGetModelCloudTypeCall {
+	c.Call = c.Call.DoAndReturn(f)
+	return c
+}
+
 // GetModelConstraints mocks base method.
 func (m *MockModelState) GetModelConstraints(arg0 context.Context) (constraints.Value, error) {
 	m.ctrl.T.Helper()
