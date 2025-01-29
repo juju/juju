7ff1d5c3713d796d92a960625c74947bb936dca4
Guillaume Fouillet <guillaume.fouillet@canonical.com>
2025-01-29 15:48:42 +0100

    feat(charmrevisioner): wire up charm revisioner to update potential resources
    This commits implements the logic which allows to update available version of
    resources for deployed application. CharmRevisioner fetch the new version data
    from charmhub and update potential resources for every deployed application
    with potential updates.

--- internal/worker/charmrevisioner/package_test.go
+++ internal/worker/charmrevisioner/package_test.go
@@ -10,7 +10,7 @@ import (
 	gc "gopkg.in/check.v1"
 )
 
-//go:generate go run go.uber.org/mock/mockgen -typed -package charmrevisioner -destination package_mocks_test.go github.com/juju/juju/internal/worker/charmrevisioner CharmhubClient,ModelConfigService,ApplicationService,ModelService
+//go:generate go run go.uber.org/mock/mockgen -typed -package charmrevisioner -destination package_mocks_test.go github.com/juju/juju/internal/worker/charmrevisioner CharmhubClient,ModelConfigService,ApplicationService,ModelService,ResourceService
 //go:generate go run go.uber.org/mock/mockgen -typed -package charmrevisioner -destination clock_mocks_test.go github.com/juju/clock Clock
 //go:generate go run go.uber.org/mock/mockgen -typed -package charmrevisioner -destination http_mocks_test.go github.com/juju/juju/core/http HTTPClientGetter,HTTPClient
 
