8e8bb58235ec3c72138560d2cfe21ac50108f79d
Thomas Miller <thomas.miller@canonical.com>
2025-02-04 09:15:17 +1000

    refactor: explicit constraint columns
    Updates the model constraint views to explicitly call out the columns
    they require.

--- domain/schema/model/sql/0005-model.sql
+++ domain/schema/model/sql/0005-model.sql
@@ -26,14 +26,29 @@ CREATE TABLE model_constraint (
 -- no constraints have been set then expect this view to be empty. There will
 -- also only ever be a maximum of 1 record in this view.
 CREATE VIEW v_model_constraint AS
-SELECT c.*
+SELECT
+    c.uuid,
+    c.arch,
+    c.cpu_cores,
+    c.cpu_power,
+    c.mem,
+    c.root_disk,
+    c.root_disk_source,
+    c.instance_role,
+    c.instance_type,
+    c.container_type,
+    c.virt_type,
+    c.allocate_public_ip,
+    c.image_id
 FROM model_constraint mc
 INNER JOIN v_constraint c ON mc.constraint_uuid = c.uuid;
 
 -- v_model_constraint_tag is a view of all the constraint tags set for the
 -- current model. It is expected that this view can be empty.
 CREATE VIEW v_model_constraint_tag AS
-SELECT ct.*
+SELECT
+    ct.constraint_uuid,
+    ct.tag
 FROM constraint_tag ct
 JOIN "constraint" c on ct.constraint_uuid = c.uuid
 JOIN model_constraint mc on mc.constraint_uuid = c.uuid;
@@ -41,7 +56,10 @@ JOIN model_constraint mc on mc.constraint_uuid = c.uuid;
 -- v_model_constraint_space is a view of all the constraint spaces set for the
 -- current model. It is expected that this view can be empty.
 CREATE VIEW v_model_constraint_space AS
-SELECT cs.*
+SELECT
+    cs.constraint_uuid,
+    cs.space,
+    cs.exclude
 FROM constraint_space cs
 JOIN "constraint" c on cs.constraint_uuid = c.uuid
 JOIN model_constraint mc on mc.constraint_uuid = c.uuid;
@@ -49,7 +67,9 @@ JOIN model_constraint mc on mc.constraint_uuid = c.uuid;
 -- v_model_constraint_zone is a view of all the constraint zones set for the
 -- current model. It is expected that this view can be empty.
 CREATE VIEW v_model_constraint_zone AS
-SELECT cz.*
+SELECT
+    cz.constraint_uuid,
+    cz.zone
 FROM constraint_zone cz
 JOIN "constraint" c on cz.constraint_uuid = c.uuid
 JOIN model_constraint mc on mc.constraint_uuid = c.uuid;
