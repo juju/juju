9d9815c8b539dee77bd3f9fd9677e78dd990dba2
Guillaume Fouillet <guillaume.fouillet@canonical.com>
2025-02-06 10:47:46 +0100

    feat(relation): add support for retrieving relation endpoint UUID
    This commit introduces several function through state and service layer to retrieve
    the RelationEndpointUUID. It is necessary to implements a watcher on relation_application_setting
    table, which rely on a relationEndpoint.
    
    It also inits tests files structure in relation domain.

--- core/relation/uuid.go
+++ core/relation/uuid.go
@@ -14,9 +14,6 @@ import (
 // UUID represents a relation unique identifier.
 type UUID string
 
-// UnitUUID represents a relation unit unique identifier.
-type UnitUUID string
-
 // NewUUID is a convince function for generating a new relation uuid.
 func NewUUID() (UUID, error) {
 	id, err := uuid.NewUUID()
@@ -52,6 +49,9 @@ func (u UUID) Validate() error {
 	return nil
 }
 
+// UnitUUID represents a relation unit unique identifier.
+type UnitUUID string
+
 // NewUnitUUID is a convince function for generating a new relation unit uuid.
 func NewUnitUUID() (UnitUUID, error) {
 	id, err := uuid.NewUUID()
@@ -61,7 +61,7 @@ func NewUnitUUID() (UnitUUID, error) {
 	return UnitUUID(id.String()), nil
 }
 
-// UnitUUID returns a new UUID from the given string. If the string is not a
+// ParseUnitUUID returns a new UUID from the given string. If the string is not a
 // valid uuid an error satisfying [errors.NotValid] will be returned.
 func ParseUnitUUID(value string) (UnitUUID, error) {
 	if !uuid.IsValidUUIDString(value) {
@@ -78,6 +78,44 @@ func (u UnitUUID) String() string {
 // Validate ensures the consistency of the UUID. If the uuid is invalid an error
 // satisfying [errors.NotValid] will be returned.
 func (u UnitUUID) Validate() error {
+	if u == "" {
+		return fmt.Errorf("%wuuid cannot be empty", errors.Hide(errors.NotValid))
+	}
+	if !uuid.IsValidUUIDString(string(u)) {
+		return fmt.Errorf("uuid %q %w", u, errors.NotValid)
+	}
+	return nil
+}
+
+// EndpointUUID represents a relation endpoint unique identifier.
+type EndpointUUID string
+
+// NewEndpointUUID is a convenience function for generating a new relation endpoint uuid.
+func NewEndpointUUID() (EndpointUUID, error) {
+	id, err := uuid.NewUUID()
+	if err != nil {
+		return EndpointUUID(""), err
+	}
+	return EndpointUUID(id.String()), nil
+}
+
+// ParseEndpointUUID returns a new UUID from the given string. If the string is not a
+// valid uuid an error satisfying [errors.NotValid] will be returned.
+func ParseEndpointUUID(value string) (EndpointUUID, error) {
+	if !uuid.IsValidUUIDString(value) {
+		return "", fmt.Errorf("id %q %w", value, errors.NotValid)
+	}
+	return EndpointUUID(value), nil
+}
+
+// String implements the stringer interface for UUID.
+func (u EndpointUUID) String() string {
+	return string(u)
+}
+
+// Validate ensures the consistency of the UUID. If the uuid is invalid an error
+// satisfying [errors.NotValid] will be returned.
+func (u EndpointUUID) Validate() error {
 	if u == "" {
 		return fmt.Errorf("%wuuid cannot be empty", errors.Hide(errors.NotValid))
 	}
---
+++ domain/relation/errors/doc.go
@@ -0,0 +1,5 @@
+// Copyright 2025 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+// Package errors provides domain specific errors for relations.
+package errors
