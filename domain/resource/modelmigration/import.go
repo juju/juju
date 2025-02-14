22e7da686c64823a22babc2ad4774c820f594e4c
Alastair Flynn <af@aflynn.uk>
2025-02-14 14:56:41 +0000

    fix: always set -1 revision for origin upload imported resources

--- domain/resource/modelmigration/import.go
+++ domain/resource/modelmigration/import.go
@@ -162,13 +162,9 @@ func importResourceRevision(name string, rev description.ResourceRevision) (reso
 				charmresource.OriginUpload, revision, resourceerrors.ResourceRevisionNotValid,
 			)
 		}
+		importInfo.Revision = revision
 	case charmresource.OriginUpload:
-		if revision > 0 {
-			return resource.ImportResourceInfo{}, errors.Errorf(
-				"expected resource with origin %q to have revision unset, got %d: %w",
-				charmresource.OriginUpload, revision, resourceerrors.ResourceRevisionNotValid,
-			)
-		}
+		importInfo.Revision = -1
 	default:
 		return resource.ImportResourceInfo{}, errors.Errorf(
 			"unexpected origin %s: %w", origin, resourceerrors.OriginNotValid,
