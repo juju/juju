7e593dcd195585f88a46f2edfb8cc81ede08559a
ale8k <alexjkilroy@gmail.com>
2025-02-21 10:44:53 +0000

    refactor: remove unused mocks

--- worker/sshserver/package_test.go
+++ worker/sshserver/package_test.go
@@ -9,9 +9,6 @@ import (
 	"github.com/juju/juju/testing"
 )
 
-//go:generate go run go.uber.org/mock/mockgen -package mocks -destination mocks/agent_config_mock.go github.com/juju/juju/agent Config
-//go:generate go run go.uber.org/mock/mockgen -package mocks -destination mocks/agent_mock.go github.com/juju/juju/agent Agent
-//go:generate go run go.uber.org/mock/mockgen -package mocks -destination mocks/ctrl_cfg_notify_watcher_mock.go github.com/juju/juju/state NotifyWatcher
 //go:generate go run go.uber.org/mock/mockgen -package mocks -destination mocks/state_tracker_mock.go github.com/juju/juju/worker/state StateTracker
 //go:generate go run go.uber.org/mock/mockgen -package mocks -destination mocks/system_state_mock.go github.com/juju/juju/worker/sshserver SystemState
 
