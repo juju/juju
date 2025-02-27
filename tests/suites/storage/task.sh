cbd7c32ed5b7eee578e4342dc462d69671e4878e
Lye Yao <lye.yao@canonical.com>
2025-02-27 09:14:10 +0000

    chore(test): fix the block device test

--- tests/suites/storage/task.sh
+++ tests/suites/storage/task.sh
@@ -14,8 +14,9 @@ test_storage() {
 	bootstrap "test-storage" "${file}"
 
 	test_charm_storage
+	test_model_storage_block
+	test_model_storage_filesystem
 	test_persistent_storage
-	test_model_storage
 
 	destroy_controller "test-storage"
 }
