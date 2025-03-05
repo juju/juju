ecffe9cf8264d7dd945091ae0c3b10a9953ea8a7
Heather Lanigan <heather.lanigan@canonical.com>
2025-03-05 11:35:37 -0500

    feat(relations): add generate uuid methods for relation domain testing

---
+++ core/relation/testing/relation.go
@@ -0,0 +1,29 @@
+// Copyright 2025 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package testing
+
+import (
+	jc "github.com/juju/testing/checkers"
+	gc "gopkg.in/check.v1"
+
+	"github.com/juju/juju/core/relation"
+)
+
+// GenRelationUUID can be used in testing for generating a relation UUID
+// that is checked for subsequent errors using the test suite's go check
+// instance.
+func GenRelationUUID(c *gc.C) relation.UUID {
+	id, err := relation.NewUUID()
+	c.Assert(err, jc.ErrorIsNil)
+	return id
+}
+
+// GenRelationUnitUUID can be used in testing for generating a relation
+// Unit UUID that is checked for subsequent errors using the test suite's
+// go check instance.
+func GenRelationUnitUUID(c *gc.C) relation.UnitUUID {
+	id, err := relation.NewUnitUUID()
+	c.Assert(err, jc.ErrorIsNil)
+	return id
+}
