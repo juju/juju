49c717d250fe03fc8bff0557cc64798785664e40
Simon Richardson <stickupkid@gmail.com>
2025-03-05 13:35:38 +0000

    chore: fix sshserver flakey test

--- internal/worker/sshserver/manifold_test.go
+++ internal/worker/sshserver/manifold_test.go
@@ -7,6 +7,7 @@ import (
 	"context"
 
 	"github.com/juju/errors"
+	"github.com/juju/testing"
 	jc "github.com/juju/testing/checkers"
 	"github.com/juju/worker/v4"
 	"github.com/juju/worker/v4/dependency"
@@ -15,42 +16,29 @@ import (
 	"go.uber.org/mock/gomock"
 	gc "gopkg.in/check.v1"
 
+	"github.com/juju/juju/core/watcher/watchertest"
 	loggertesting "github.com/juju/juju/internal/logger/testing"
 	"github.com/juju/juju/internal/worker/sshserver"
 )
 
 type manifoldSuite struct {
+	testing.IsolationSuite
+
+	controllerConfigService *sshserver.MockControllerConfigService
 }
 
 var _ = gc.Suite(&manifoldSuite{})
 
-func newManifoldConfig(c *gc.C, modifier func(cfg *sshserver.ManifoldConfig)) *sshserver.ManifoldConfig {
-	cfg := &sshserver.ManifoldConfig{
-		DomainServicesName:     "domain-services",
-		NewServerWrapperWorker: func(sshserver.ServerWrapperWorkerConfig) (worker.Worker, error) { return nil, nil },
-		NewServerWorker:        func(sshserver.ServerWorkerConfig) (worker.Worker, error) { return nil, nil },
-		GetControllerConfigService: func(getter dependency.Getter, name string) (sshserver.ControllerConfigService, error) {
-			return nil, nil
-		},
-		Logger: loggertesting.WrapCheckLog(c),
-	}
-
-	modifier(cfg)
-
-	return cfg
-}
-
 func (s *manifoldSuite) TestConfigValidate(c *gc.C) {
-	ctrl := gomock.NewController(c)
-	defer ctrl.Finish()
+	defer s.setupMocks(c).Finish()
 
 	// Check config as expected.
 
-	cfg := newManifoldConfig(c, func(cfg *sshserver.ManifoldConfig) {})
+	cfg := s.newManifoldConfig(c, func(cfg *sshserver.ManifoldConfig) {})
 	c.Assert(cfg.Validate(), gc.IsNil)
 
 	// Entirely missing.
-	cfg = newManifoldConfig(c, func(cfg *sshserver.ManifoldConfig) {
+	cfg = s.newManifoldConfig(c, func(cfg *sshserver.ManifoldConfig) {
 		cfg.DomainServicesName = ""
 		cfg.NewServerWrapperWorker = nil
 		cfg.NewServerWorker = nil
@@ -60,31 +48,31 @@ func (s *manifoldSuite) TestConfigValidate(c *gc.C) {
 	c.Check(errors.Is(cfg.Validate(), errors.NotValid), jc.IsTrue)
 
 	// Missing domain services name.
-	cfg = newManifoldConfig(c, func(cfg *sshserver.ManifoldConfig) {
+	cfg = s.newManifoldConfig(c, func(cfg *sshserver.ManifoldConfig) {
 		cfg.DomainServicesName = ""
 	})
 	c.Check(errors.Is(cfg.Validate(), errors.NotValid), jc.IsTrue)
 
 	// Missing NewServerWrapperWorker.
-	cfg = newManifoldConfig(c, func(cfg *sshserver.ManifoldConfig) {
+	cfg = s.newManifoldConfig(c, func(cfg *sshserver.ManifoldConfig) {
 		cfg.NewServerWrapperWorker = nil
 	})
 	c.Check(errors.Is(cfg.Validate(), errors.NotValid), jc.IsTrue)
 
 	// Missing NewServerWorker.
-	cfg = newManifoldConfig(c, func(cfg *sshserver.ManifoldConfig) {
+	cfg = s.newManifoldConfig(c, func(cfg *sshserver.ManifoldConfig) {
 		cfg.NewServerWorker = nil
 	})
 	c.Check(errors.Is(cfg.Validate(), errors.NotValid), jc.IsTrue)
 
 	// Missing GetControllerConfigService.
-	cfg = newManifoldConfig(c, func(cfg *sshserver.ManifoldConfig) {
+	cfg = s.newManifoldConfig(c, func(cfg *sshserver.ManifoldConfig) {
 		cfg.GetControllerConfigService = nil
 	})
 	c.Check(errors.Is(cfg.Validate(), errors.NotValid), jc.IsTrue)
 
 	// Missing Logger.
-	cfg = newManifoldConfig(c, func(cfg *sshserver.ManifoldConfig) {
+	cfg = s.newManifoldConfig(c, func(cfg *sshserver.ManifoldConfig) {
 		cfg.Logger = nil
 	})
 	c.Check(errors.Is(cfg.Validate(), errors.NotValid), jc.IsTrue)
@@ -92,10 +80,8 @@ func (s *manifoldSuite) TestConfigValidate(c *gc.C) {
 }
 
 func (s *manifoldSuite) TestManifoldStart(c *gc.C) {
-	ctrl := gomock.NewController(c)
-	defer ctrl.Finish()
+	defer s.setupMocks(c).Finish()
 
-	controllerConfigService := sshserver.NewMockControllerConfigService(ctrl)
 	w := workertest.NewErrorWorker(nil)
 	// Setup the manifold
 	manifold := sshserver.Manifold(sshserver.ManifoldConfig{
@@ -103,7 +89,7 @@ func (s *manifoldSuite) TestManifoldStart(c *gc.C) {
 		NewServerWrapperWorker: sshserver.NewServerWrapperWorker,
 		NewServerWorker:        func(sshserver.ServerWorkerConfig) (worker.Worker, error) { return w, nil },
 		GetControllerConfigService: func(getter dependency.Getter, name string) (sshserver.ControllerConfigService, error) {
-			return controllerConfigService, nil
+			return s.controllerConfigService, nil
 		},
 		Logger: loggertesting.WrapCheckLog(c),
 	})
@@ -119,4 +105,35 @@ func (s *manifoldSuite) TestManifoldStart(c *gc.C) {
 	c.Assert(err, jc.ErrorIsNil)
 	c.Assert(result, gc.NotNil)
 	workertest.CleanKill(c, w)
+}
+
+func (s *manifoldSuite) setupMocks(c *gc.C) *gomock.Controller {
+	ctrl := gomock.NewController(c)
+
+	s.controllerConfigService = sshserver.NewMockControllerConfigService(ctrl)
+
+	watcher := watchertest.NewMockStringsWatcher(make(<-chan []string))
+	s.controllerConfigService.EXPECT().WatchControllerConfig().Return(watcher, nil).AnyTimes()
+
+	return ctrl
+}
+
+func (s *manifoldSuite) newManifoldConfig(c *gc.C, modifier func(cfg *sshserver.ManifoldConfig)) *sshserver.ManifoldConfig {
+	cfg := &sshserver.ManifoldConfig{
+		DomainServicesName: "domain-services",
+		NewServerWrapperWorker: func(sshserver.ServerWrapperWorkerConfig) (worker.Worker, error) {
+			return nil, nil
+		},
+		NewServerWorker: func(sshserver.ServerWorkerConfig) (worker.Worker, error) {
+			return nil, nil
+		},
+		GetControllerConfigService: func(getter dependency.Getter, name string) (sshserver.ControllerConfigService, error) {
+			return s.controllerConfigService, nil
+		},
+		Logger: loggertesting.WrapCheckLog(c),
+	}
+
+	modifier(cfg)
+
+	return cfg
 }
