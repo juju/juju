1888b8aad42fca096ef87b7a9f27298ac0cebaca
Thomas Miller <thomas.miller@canonical.com>
2025-03-13 21:12:53 +1000

    feat: version type to track agent versions
    This commit introduces a new core package and type that is capable of
    providing information about an agents version. This information is
    needed to track what version an agent is currently running.

---
+++ core/agentbinary/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2024 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+// Package agentbinary provides types for use through out Juju to represent the
+// Juju agent binaries.
+package agentbinary
