0b26929f0a97753c9a9a99316b0f1fa3cdb22a4a
Joseph Phillips <joe.phillips@canonical.com>
2025-03-18 14:45:42 +0100

    feat: adds removal job UUID generation and tests
    The removal service generates a new UUID for the relation removal job,
    which is verified as correct by new service tests.

--- domain/removal/service/service.go
+++ domain/removal/service/service.go
@@ -12,6 +12,7 @@ import (
 	"github.com/juju/juju/core/watcher"
 	"github.com/juju/juju/core/watcher/eventsource"
 	relationerrors "github.com/juju/juju/domain/relation/errors"
+	"github.com/juju/juju/domain/removal"
 	"github.com/juju/juju/internal/errors"
 )
 
@@ -44,20 +45,28 @@ type Service struct {
 // If it does, the relation is guaranteed after this call to be:
 // - No longer alive.
 // - Removed or scheduled to be removed with the input force qualification.
+// The UUID for the scheduled removal job is returned.
 // [relationerrors.RelationNotFound] is returned if no such relation exists.
-func (s *Service) RemoveRelation(ctx context.Context, relUUID corerelation.UUID, force bool) error {
+func (s *Service) RemoveRelation(ctx context.Context, relUUID corerelation.UUID, force bool) (removal.UUID, error) {
 	exists, err := s.st.RelationExists(ctx, relUUID.String())
 	if err != nil {
-		return errors.Errorf("checking if relation %q exists: %w", relUUID, err)
+		return "", errors.Errorf("checking if relation %q exists: %w", relUUID, err)
 	}
 	if !exists {
-		return errors.Errorf("relation %q does not exist", relUUID).Add(relationerrors.RelationNotFound)
+		return "", errors.Errorf("relation %q does not exist", relUUID).Add(relationerrors.RelationNotFound)
 	}
 
-	if err := s.st.RelationAdvanceLifeAndScheduleRemoval(ctx, "", relUUID.String(), force); err != nil {
-		return errors.Errorf("removing relation %q: %w", relUUID, err)
+	jobUUID, err := removal.NewUUID()
+	if err != nil {
+		return "", errors.Capture(err)
 	}
-	return nil
+
+	if err := s.st.RelationAdvanceLifeAndScheduleRemoval(ctx, jobUUID.String(), relUUID.String(), force); err != nil {
+		return "", errors.Errorf("removing relation %q: %w", relUUID, err)
+	}
+
+	s.logger.Infof(ctx, "sheduled cleanup job %q for relation %q", jobUUID, relUUID)
+	return jobUUID, nil
 }
 
 // WatchableService provides the API for working with entity removal,
---
+++ domain/removal/service/service_test.go
@@ -0,0 +1,83 @@
+// Copyright 2025 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package service
+
+import (
+	"context"
+
+	jujutesting "github.com/juju/testing"
+	jc "github.com/juju/testing/checkers"
+	"go.uber.org/mock/gomock"
+	gc "gopkg.in/check.v1"
+
+	corerelation "github.com/juju/juju/core/relation"
+	relationerrors "github.com/juju/juju/domain/relation/errors"
+	loggertesting "github.com/juju/juju/internal/logger/testing"
+)
+
+type serviceSuite struct {
+	jujutesting.IsolationSuite
+
+	state *MockState
+}
+
+var _ = gc.Suite(&serviceSuite{})
+
+func (s *serviceSuite) TestRemoveRelationNoForceSuccess(c *gc.C) {
+	defer s.setupMocks(c).Finish()
+
+	rUUID := newRelUUID(c)
+
+	exp := s.state.EXPECT()
+	exp.RelationExists(gomock.Any(), rUUID.String()).Return(true, nil)
+	exp.RelationAdvanceLifeAndScheduleRemoval(gomock.Any(), gomock.Any(), rUUID.String(), false).Return(nil)
+
+	jobUUID, err := s.newService(c).RemoveRelation(context.Background(), rUUID, false)
+	c.Assert(err, jc.ErrorIsNil)
+	c.Assert(jobUUID.Validate(), jc.ErrorIsNil)
+}
+
+func (s *serviceSuite) TestRemoveRelationForceSuccess(c *gc.C) {
+	defer s.setupMocks(c).Finish()
+
+	rUUID := newRelUUID(c)
+
+	exp := s.state.EXPECT()
+	exp.RelationExists(gomock.Any(), rUUID.String()).Return(true, nil)
+	exp.RelationAdvanceLifeAndScheduleRemoval(gomock.Any(), gomock.Any(), rUUID.String(), true).Return(nil)
+
+	jobUUID, err := s.newService(c).RemoveRelation(context.Background(), rUUID, true)
+	c.Assert(err, jc.ErrorIsNil)
+	c.Assert(jobUUID.Validate(), jc.ErrorIsNil)
+}
+
+func (s *serviceSuite) TestRemoveRelationNotFound(c *gc.C) {
+	defer s.setupMocks(c).Finish()
+
+	rUUID := newRelUUID(c)
+
+	s.state.EXPECT().RelationExists(gomock.Any(), rUUID.String()).Return(false, nil)
+
+	_, err := s.newService(c).RemoveRelation(context.Background(), rUUID, true)
+	c.Assert(err, jc.ErrorIs, relationerrors.RelationNotFound)
+}
+
+func (s *serviceSuite) setupMocks(c *gc.C) *gomock.Controller {
+	ctrl := gomock.NewController(c)
+	s.state = NewMockState(ctrl)
+	return ctrl
+}
+
+func (s *serviceSuite) newService(c *gc.C) *Service {
+	return &Service{
+		st:     s.state,
+		logger: loggertesting.WrapCheckLog(c),
+	}
+}
+
+func newRelUUID(c *gc.C) corerelation.UUID {
+	rUUID, err := corerelation.NewUUID()
+	c.Assert(err, jc.ErrorIsNil)
+	return rUUID
+}
