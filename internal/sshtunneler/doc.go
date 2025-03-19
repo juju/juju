afa3675754987d8cc9c751ad517d5b2e44da3068
Kian Parvin <kian.parvin@canonical.com>
2025-03-19 21:49:14 +0200

    feat: sshtunneler package
    The sshtunneler packages provides logic to create SSH tunnels to machine units using reverse SSH.

---
+++ internal/sshtunneler/doc.go
@@ -0,0 +1,7 @@
+// Copyright 2025 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+// Package sshtunneler provides a way to create
+// SSH connections to machine units using a reverse
+// SSH tunnel approach.
+package sshtunneler
---
+++ internal/sshtunneler/package_test.go
@@ -0,0 +1,16 @@
+// Copyright 2025 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package sshtunneler
+
+import (
+	. "testing"
+
+	gc "gopkg.in/check.v1"
+)
+
+//go:generate go run go.uber.org/mock/mockgen -typed -package sshtunneler -destination ./service_mock_test.go github.com/juju/juju/internal/sshtunneler State,ControllerInfo,SSHDial
+
+func TestPackage(t *T) {
+	gc.TestingT(t)
+}
