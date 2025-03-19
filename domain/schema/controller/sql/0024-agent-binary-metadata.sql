d728efb18ed7291d99f8472cc354c01b1d9e696f
Kelvin Liu <kelvin.liu@canonical.com>
2025-03-19 15:48:39 +1100

    feat: add architecture field in the agent binary table;

--- domain/schema/controller/sql/0024-agent-binary-metadata.sql
+++ domain/schema/controller/sql/0024-agent-binary-metadata.sql
@@ -1,15 +1,20 @@
 -- The agent_binary_metadata table in the controller database records information about
--- cached agent binaries, including their version, SHA, and object store location.
--- This table primarily tracks custom-built agent binaries, while SimpleStream agent
+-- the agent binaries stored in the controller's object store, including their version, SHA,
+-- architecture, and the object store information.
+-- This table primarily tracks custom-built agent binaries, while the simple stream agent
 -- binaries are tracked in the corresponding table in the model database.
 CREATE TABLE agent_binary_metadata (
     uuid TEXT NOT NULL PRIMARY KEY,
     version TEXT NOT NULL,
     object_store_uuid TEXT NOT NULL,
+    architecture_id INT NOT NULL,
     CONSTRAINT fk_agent_binary_metadata_object_store_metadata
     FOREIGN KEY (object_store_uuid)
-    REFERENCES object_store_metadata (uuid)
+    REFERENCES object_store_metadata (uuid),
+    CONSTRAINT fk_agent_binary_metadata_architecture
+    FOREIGN KEY (architecture_id)
+    REFERENCES architecture (id)
 );
 
-CREATE UNIQUE INDEX idx_agent_binary_metadata_version
-ON agent_binary_metadata (version);
+CREATE UNIQUE INDEX idx_agent_binary_metadata_version_architecture
+ON agent_binary_metadata (version, architecture_id);
--- domain/schema/model/sql/0030-agent-binary-metadata.sql
+++ domain/schema/model/sql/0030-agent-binary-metadata.sql
@@ -1,14 +1,18 @@
--- The agent_binary_metadata table stores information about cached agent binaries,
--- including their version, SHA, and object store location, sourced from the
+-- The agent_binary_metadata table stores information about agent binaries stored in the model's object store,
+-- including their version, SHA, architecture, and object store information, sourced from the
 -- simple stream in the model database.
 CREATE TABLE agent_binary_metadata (
     uuid TEXT NOT NULL PRIMARY KEY,
     version TEXT NOT NULL,
     object_store_uuid TEXT NOT NULL,
+    architecture_id INT NOT NULL,
     CONSTRAINT fk_agent_binary_metadata_object_store_metadata
     FOREIGN KEY (object_store_uuid)
-    REFERENCES object_store_metadata (uuid)
+    REFERENCES object_store_metadata (uuid),
+    CONSTRAINT fk_agent_binary_metadata_architecture
+    FOREIGN KEY (architecture_id)
+    REFERENCES architecture (id)
 );
 
-CREATE UNIQUE INDEX idx_agent_binary_metadata_version
-ON agent_binary_metadata (version);
+CREATE UNIQUE INDEX idx_agent_binary_metadata_version_architecture
+ON agent_binary_metadata (version, architecture_id);
