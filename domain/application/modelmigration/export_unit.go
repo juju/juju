e42614bfafefa248eeb4f90d24c87b1fab3eb1f2
Simon Richardson <stickupkid@gmail.com>
2025-03-26 18:37:49 +0000

    feat: ensure we don't panic

--- domain/application/modelmigration/export_unit.go
+++ domain/application/modelmigration/export_unit.go
@@ -7,6 +7,7 @@ import (
 	"context"
 
 	"github.com/juju/description/v9"
+	"github.com/juju/version/v2"
 
 	"github.com/juju/juju/domain/application"
 	"github.com/juju/juju/internal/errors"
@@ -25,15 +26,35 @@ func (e *exportOperation) exportApplicationUnits(ctx context.Context, app descri
 		if err != nil {
 			return errors.Capture(err)
 		}
-		app.AddUnit(arg)
+		descriptionUnit := app.AddUnit(arg)
+
+		// This is a hack to get the tools for the unit.
+		descriptionUnit.SetTools(description.AgentToolsArgs{
+			Version: version.Binary{
+				Number: version.Number{
+					Major: 4,
+					Minor: 0,
+					Tag:   "beta",
+					Patch: 6,
+					Build: 1,
+				},
+				Release: "ubuntu",
+				Arch:    "amd64",
+			},
+			URL:    "tools/4.0-beta6.1-ubuntu-amd64-eb9e6949fd2fb1c4c6953619a4807ae1a3f54e355be1d37d14f5ba918fb07165",
+			Size:   52806385,
+			SHA256: "eb9e6949fd2fb1c4c6953619a4807ae1a3f54e355be1d37d14f5ba918fb07165",
+		})
 	}
 	return nil
 }
 
 func (e *exportOperation) exportUnit(ctx context.Context, modelType string, unit application.ExportUnit) (description.UnitArgs, error) {
-	return description.UnitArgs{
+	args := description.UnitArgs{
 		Name:         unit.Name.String(),
 		Type:         modelType,
 		PasswordHash: unit.PasswordHash,
-	}, nil
+	}
+
+	return args, nil
 }
