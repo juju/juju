f08bf90b61c2a7728b089e76f78c2c8ebbe0eb81
Harry Pidcock <harry.pidcock@canonical.com>
2025-03-27 11:55:39 +1000

    feat: caasapplicationprovisioner watching domain apps

--- internal/worker/caasapplicationprovisioner/package_test.go
+++ internal/worker/caasapplicationprovisioner/package_test.go
@@ -14,6 +14,8 @@ import (
 //go:generate go run go.uber.org/mock/mockgen -typed -package mocks -destination mocks/unitfacade_mock.go github.com/juju/juju/internal/worker/caasapplicationprovisioner CAASUnitProvisionerFacade
 //go:generate go run go.uber.org/mock/mockgen -typed -package mocks -destination mocks/runner_mock.go github.com/juju/juju/internal/worker/caasapplicationprovisioner Runner
 //go:generate go run go.uber.org/mock/mockgen -typed -package mocks -destination mocks/ops_mock.go github.com/juju/juju/internal/worker/caasapplicationprovisioner ApplicationOps
+//go:generate go run go.uber.org/mock/mockgen -typed -package mocks -destination mocks/service_mock.go github.com/juju/juju/internal/worker/caasapplicationprovisioner ApplicationService,StatusService
+//go:generate go run go.uber.org/mock/mockgen -typed -package mocks -destination mocks/services_mocks.go github.com/juju/juju/internal/services ModelDomainServices
 
 func TestPackage(t *stdtesting.T) {
 	gc.TestingT(t)
