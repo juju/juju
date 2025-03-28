c777b09b46c1742b2581eb70e633d96a39c85823
Kian Parvin <kian.parvin@canonical.com>
2025-03-28 12:41:00 +0200

    chore: remove use of _test package for sshserver

--- internal/worker/sshserver/server_test.go
+++ internal/worker/sshserver/server_test.go
@@ -1,7 +1,7 @@
 // Copyright 2025 Canonical Ltd.
 // Licensed under the AGPLv3, see LICENCE file for details.
 
-package sshserver_test
+package sshserver
 
 import (
 	"crypto/rand"
@@ -19,7 +19,6 @@ import (
 	"google.golang.org/grpc/test/bufconn"
 	gc "gopkg.in/check.v1"
 
-	"github.com/juju/juju/internal/worker/sshserver"
 	pkitest "github.com/juju/juju/pki/test"
 	jujutesting "github.com/juju/juju/testing"
 )
@@ -73,11 +72,11 @@ func (s *sshServerSuite) setupMocks(c *gc.C) *gomock.Controller {
 }
 
 func newServerWorkerConfig(
-	l sshserver.Logger,
+	l Logger,
 	j string,
-	modifier func(*sshserver.ServerWorkerConfig),
-) *sshserver.ServerWorkerConfig {
-	cfg := &sshserver.ServerWorkerConfig{
+	modifier func(*ServerWorkerConfig),
+) *ServerWorkerConfig {
+	cfg := &ServerWorkerConfig{
 		Logger:               l,
 		JumpHostKey:          j,
 		NewSSHServerListener: newTestingSSHServerListener,
@@ -89,37 +88,37 @@ func newServerWorkerConfig(
 }
 
 func (s *sshServerSuite) TestValidate(c *gc.C) {
-	cfg := &sshserver.ServerWorkerConfig{}
+	cfg := &ServerWorkerConfig{}
 	l := loggo.GetLogger("test")
 
 	c.Assert(cfg.Validate(), jc.ErrorIs, errors.NotValid)
 
 	// Test no Logger.
-	cfg = newServerWorkerConfig(l, "Logger", func(cfg *sshserver.ServerWorkerConfig) {
+	cfg = newServerWorkerConfig(l, "Logger", func(cfg *ServerWorkerConfig) {
 		cfg.Logger = nil
 	})
 	c.Assert(cfg.Validate(), jc.ErrorIs, errors.NotValid)
 
 	// Test no JumpHostKey.
-	cfg = newServerWorkerConfig(l, "jumpHostKey", func(cfg *sshserver.ServerWorkerConfig) {
+	cfg = newServerWorkerConfig(l, "jumpHostKey", func(cfg *ServerWorkerConfig) {
 		cfg.JumpHostKey = ""
 	})
 	c.Assert(cfg.Validate(), jc.ErrorIs, errors.NotValid)
 
 	// Test no NewSSHServerListener.
-	cfg = newServerWorkerConfig(l, "NewSSHServerListener", func(cfg *sshserver.ServerWorkerConfig) {
+	cfg = newServerWorkerConfig(l, "NewSSHServerListener", func(cfg *ServerWorkerConfig) {
 		cfg.NewSSHServerListener = nil
 	})
 	c.Assert(cfg.Validate(), jc.ErrorIs, errors.NotValid)
 
 	// Test no FacadeClient.
-	cfg = newServerWorkerConfig(l, "NewSSHServerListener", func(cfg *sshserver.ServerWorkerConfig) {
+	cfg = newServerWorkerConfig(l, "NewSSHServerListener", func(cfg *ServerWorkerConfig) {
 		cfg.FacadeClient = nil
 	})
 	c.Assert(cfg.Validate(), jc.ErrorIs, errors.NotValid)
 }
 
-func (s *sshServerSuite) TestSSHServer(c *gc.C) {
+func (s *sshServerSuite) TestSSHServerNoAuth(c *gc.C) {
 	defer s.setupMocks(c).Finish()
 
 	s.facadeClient.EXPECT().HostKeyForTarget(gomock.Any()).Return(s.hostKey, nil)
@@ -127,7 +126,7 @@ func (s *sshServerSuite) TestSSHServer(c *gc.C) {
 	// Start the server on an in-memory listener
 	listener := bufconn.Listen(8 * 1024)
 
-	server, err := sshserver.NewServerWorker(sshserver.ServerWorkerConfig{
+	server, err := NewServerWorker(ServerWorkerConfig{
 		Logger:                   loggo.GetLogger("test"),
 		Listener:                 listener,
 		MaxConcurrentConnections: maxConcurrentConnections,
@@ -192,7 +191,7 @@ func (s *sshServerSuite) TestHostKeyForTarget(c *gc.C) {
 	// Firstly, start the server on an in-memory listener
 	listener := bufconn.Listen(8 * 1024)
 	s.facadeClient.EXPECT().HostKeyForTarget(gomock.Any()).Return(s.hostKey, nil)
-	_, err := sshserver.NewServerWorker(sshserver.ServerWorkerConfig{
+	_, err := NewServerWorker(ServerWorkerConfig{
 		Logger:                   loggo.GetLogger("test"),
 		Listener:                 listener,
 		JumpHostKey:              jujutesting.SSHServerHostKey,
@@ -244,7 +243,7 @@ func (s *sshServerSuite) TestSSHServerMaxConnections(c *gc.C) {
 	s.facadeClient.EXPECT().HostKeyForTarget(gomock.Any()).Return(s.hostKey, nil).AnyTimes()
 	// Firstly, start the server on an in-memory listener
 	listener := bufconn.Listen(8 * 1024)
-	_, err := sshserver.NewServerWorker(sshserver.ServerWorkerConfig{
+	_, err := NewServerWorker(ServerWorkerConfig{
 		Logger:                   loggo.GetLogger("test"),
 		Listener:                 listener,
 		MaxConcurrentConnections: maxConcurrentConnections,
@@ -291,7 +290,7 @@ func (s *sshServerSuite) TestSSHWorkerReport(c *gc.C) {
 	defer s.setupMocks(c).Finish()
 	// Firstly, start the server on an in-memory listener
 	listener := bufconn.Listen(8 * 1024)
-	worker, err := sshserver.NewServerWorker(sshserver.ServerWorkerConfig{
+	worker, err := NewServerWorker(ServerWorkerConfig{
 		Logger:                   loggo.GetLogger("test"),
 		Listener:                 listener,
 		MaxConcurrentConnections: maxConcurrentConnections,
@@ -301,7 +300,7 @@ func (s *sshServerSuite) TestSSHWorkerReport(c *gc.C) {
 	})
 	c.Assert(err, jc.ErrorIsNil)
 
-	report := worker.(*sshserver.ServerWorker).Report()
+	report := worker.(*ServerWorker).Report()
 	c.Assert(report, gc.DeepEquals, map[string]interface{}{
 		"concurrent_connections": int32(0),
 	})
@@ -315,7 +314,7 @@ func (s *sshServerSuite) TestSSHWorkerReport(c *gc.C) {
 		},
 	})
 
-	report = worker.(*sshserver.ServerWorker).Report()
+	report = worker.(*ServerWorker).Report()
 	c.Assert(report, gc.DeepEquals, map[string]interface{}{
 		"concurrent_connections": int32(1),
 	})
