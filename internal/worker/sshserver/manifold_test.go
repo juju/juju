b98ee674cd7ca87457c205b60fc9e1b29cb25f68
SimoneDutto <simone.dutto@canonical.com>
2025-03-31 11:17:17 +0200

    feat(ssh-server-worker): add feature flag for ssh jump server
    Add a feature flag to enable the ssh jump server. To avoid exposing the ssh jump server while it's
    in development, we've added a feature flag to enable the ssh jump server, otherwise it is disabled
    by default.

--- internal/worker/sshserver/manifold_test.go
+++ internal/worker/sshserver/manifold_test.go
@@ -4,16 +4,22 @@
 package sshserver
 
 import (
+	"os"
+
 	"github.com/juju/errors"
+	"github.com/juju/featureflag"
 	"github.com/juju/loggo"
 	"github.com/juju/testing"
 	jc "github.com/juju/testing/checkers"
 	"github.com/juju/worker/v3"
+	"github.com/juju/worker/v3/dependency"
 	dt "github.com/juju/worker/v3/dependency/testing"
 	"github.com/juju/worker/v3/workertest"
 	gc "gopkg.in/check.v1"
 
 	"github.com/juju/juju/api/base"
+	"github.com/juju/juju/feature"
+	"github.com/juju/juju/juju/osenv"
 )
 
 type manifoldSuite struct {
@@ -21,6 +27,12 @@ type manifoldSuite struct {
 }
 
 var _ = gc.Suite(&manifoldSuite{})
+
+func (s *manifoldSuite) SetUpTest(c *gc.C) {
+	err := os.Setenv(osenv.JujuFeatureFlagEnvKey, feature.SSHJump)
+	c.Assert(err, jc.ErrorIsNil)
+	featureflag.SetFlagsFromEnvironment(osenv.JujuFeatureFlagEnvKey)
+}
 
 func newManifoldConfig(l loggo.Logger, modifier func(cfg *ManifoldConfig)) *ManifoldConfig {
 	cfg := &ManifoldConfig{
@@ -116,4 +128,28 @@ type mockAPICaller struct {
 
 func (a mockAPICaller) BestFacadeVersion(facade string) int {
 	return 0
+}
+
+func (s *manifoldSuite) TestManifolUninstall(c *gc.C) {
+	// Unset feature flag
+	os.Unsetenv(osenv.JujuFeatureFlagEnvKey)
+	featureflag.SetFlagsFromEnvironment(osenv.JujuFeatureFlagEnvKey)
+
+	manifold := Manifold(ManifoldConfig{
+		APICallerName: "api-caller",
+		NewServerWrapperWorker: func(ServerWrapperWorkerConfig) (worker.Worker, error) {
+			return workertest.NewDeadWorker(nil), nil
+		},
+		NewServerWorker:      func(ServerWorkerConfig) (worker.Worker, error) { return nil, nil },
+		Logger:               loggo.GetLogger("test"),
+		NewSSHServerListener: newTestingSSHServerListener,
+	})
+	// Start the worker
+	_, err := manifold.Start(
+		dt.StubContext(nil, map[string]interface{}{
+			"api-caller": mockAPICaller{},
+		}),
+	)
+	c.Assert(err, jc.ErrorIs, dependency.ErrUninstall)
+
 }
