abedf6ad743f54ca4a39edfbf1449cf184f147e0
Joseph Phillips <joe.phillips@canonical.com>
2025-04-01 14:17:07 +0200

    feat: add timer to removal worker
    Passes the clock through to the worker, and uses it to create a timer
    based on a maximum interval between check for jobs.
    
    This is useful, because some jobs will simply check for certain
    conditions, and do nothing if those are not met. They need to be woken
    up to look again.

--- internal/worker/removal/worker_test.go
+++ internal/worker/removal/worker_test.go
@@ -4,6 +4,9 @@
 package removal
 
 import (
+	"time"
+
+	"github.com/juju/clock"
 	"github.com/juju/testing"
 	jc "github.com/juju/testing/checkers"
 	"github.com/juju/worker/v4/workertest"
@@ -18,6 +21,7 @@ type workerSuite struct {
 	testing.IsolationSuite
 
 	svc *MockRemovalService
+	clk *MockClock
 }
 
 var _ = gc.Suite(&workerSuite{})
@@ -29,19 +33,37 @@ func (s *workerSuite) TestWorkerStartStop(c *gc.C) {
 	watch := watchertest.NewMockStringsWatcher(ch)
 	s.svc.EXPECT().WatchRemovals().Return(watch, nil)
 
+	// Use the timer creation as a synchronisation point below.
+	// so that we know we are entrant into the worker's loop.
+	sync := make(chan struct{})
+	s.clk.EXPECT().NewTimer(jobCheckMaxInterval).DoAndReturn(func(d time.Duration) clock.Timer {
+		sync <- struct{}{}
+		return clock.WallClock.NewTimer(d)
+	})
+
 	cfg := Config{
 		RemovalService: s.svc,
+		Clock:          s.clk,
 		Logger:         loggertesting.WrapCheckLog(c),
 	}
 
 	w, err := NewWorker(cfg)
 	c.Assert(err, jc.ErrorIsNil)
 
+	select {
+	case <-sync:
+	case <-time.After(testing.ShortWait):
+		c.Fatalf("timed out waiting for worker to start")
+	}
+
 	workertest.CleanKill(c, w)
 }
 
 func (s *workerSuite) setUpMocks(c *gc.C) *gomock.Controller {
 	ctrl := gomock.NewController(c)
+
 	s.svc = NewMockRemovalService(ctrl)
+	s.clk = NewMockClock(ctrl)
+
 	return ctrl
 }
