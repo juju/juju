c9b080ea4079774f8c417389255fa95c126a6577
Simon Richardson <stickupkid@gmail.com>
2025-04-02 16:59:23 +0100

    refactor: remove file writer
    We're using the model logs and only the model logs, stop writing
    to a file you don't own.

--- apiserver/logsink/logsink.go
+++ apiserver/logsink/logsink.go
@@ -213,7 +213,7 @@ func (h *logSinkHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 		if endpointVersion > 0 {
 			_ = socket.SetReadDeadline(time.Now().Add(websocket.PongDelay))
 			socket.SetPongHandler(func(string) error {
-				logger.Tracef(context.TODO(), "pong logsink %p", socket)
+				logger.Tracef(req.Context(), "pong logsink %p", socket)
 				_ = socket.SetReadDeadline(time.Now().Add(websocket.PongDelay))
 				return nil
 			})
@@ -226,19 +226,19 @@ func (h *logSinkHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 
 		stopReceiving, closer := h.newStopChannel()
 		defer closer()
-		logCh := h.receiveLogs(socket, endpointVersion, resolvedModelUUID, stopReceiving)
+		logCh := h.receiveLogs(req.Context(), socket, endpointVersion, resolvedModelUUID, stopReceiving)
 		for {
 			select {
 			case <-h.abort:
 				return
 			case <-tickChannel:
 				deadline := time.Now().Add(websocket.WriteWait)
-				logger.Tracef(context.TODO(), "ping logsink %p", socket)
+				logger.Tracef(req.Context(), "ping logsink %p", socket)
 				if err := socket.WriteControl(gorillaws.PingMessage, []byte{}, deadline); err != nil {
 					// This error is expected if the other end goes away. By
 					// returning we clean up the strategy and close the socket
 					// through the defer calls.
-					logger.Debugf(context.TODO(), "failed to write ping: %s", err)
+					logger.Debugf(req.Context(), "failed to write ping: %s", err)
 					// Bump the ping failure count.
 					h.metrics.PingFailureCount(resolvedModelUUID).Inc()
 					return
@@ -283,7 +283,9 @@ func (h *logSinkHandler) getVersion(req *http.Request) (int, error) {
 	}
 }
 
-func (h *logSinkHandler) receiveLogs(socket *websocket.Conn,
+func (h *logSinkHandler) receiveLogs(
+	ctx context.Context,
+	socket *websocket.Conn,
 	endpointVersion int,
 	resolvedModelUUID string,
 	stop <-chan struct{},
@@ -295,7 +297,7 @@ func (h *logSinkHandler) receiveLogs(socket *websocket.Conn,
 		tokenBucket = ratelimit.NewBucketWithClock(
 			h.ratelimit.Refill,
 			h.ratelimit.Burst,
-			ratelimitClock{h.ratelimit.Clock},
+			ratelimitClock{Clock: h.ratelimit.Clock},
 		)
 	}
 
@@ -317,13 +319,13 @@ func (h *logSinkHandler) receiveLogs(socket *websocket.Conn,
 			var m params.LogRecord
 			if err := socket.ReadJSON(&m); err != nil {
 				if gorillaws.IsCloseError(err, gorillaws.CloseNormalClosure, gorillaws.CloseGoingAway) {
-					logger.Tracef(context.TODO(), "logsink closed: %v", err)
+					logger.Tracef(ctx, "logsink closed: %v", err)
 					h.metrics.LogReadCount(resolvedModelUUID, metricLogReadLabelDisconnect).Inc()
 				} else if gorillaws.IsUnexpectedCloseError(err, gorillaws.CloseNormalClosure, gorillaws.CloseGoingAway) {
-					logger.Debugf(context.TODO(), "logsink unexpected close error: %v", err)
+					logger.Debugf(ctx, "logsink unexpected close error: %v", err)
 					h.metrics.LogReadCount(resolvedModelUUID, metricLogReadLabelError).Inc()
 				} else {
-					logger.Debugf(context.TODO(), "logsink error: %v", err)
+					logger.Debugf(ctx, "logsink error: %v", err)
 					h.metrics.LogReadCount(resolvedModelUUID, metricLogReadLabelError).Inc()
 				}
 				// Try to tell the other end we are closing. If the other end
@@ -375,12 +377,12 @@ func (h *logSinkHandler) sendError(ws *websocket.Conn, req *http.Request, err er
 	// There is no need to log the error for normal operators as there is nothing
 	// they can action. This is for developers.
 	if err != nil && featureflag.Enabled(featureflag.DeveloperMode) {
-		logger.Errorf(context.TODO(), "returning error from %s %s: %s", req.Method, req.URL.Path, errors.Details(err))
+		logger.Errorf(req.Context(), "returning error from %s %s: %s", req.Method, req.URL.Path, errors.Details(err))
 	}
 	h.mu.Lock()
 	defer h.mu.Unlock()
 	if sendErr := ws.SendInitialErrorV0(err); sendErr != nil {
-		logger.Errorf(context.TODO(), "closing websocket, %v", err)
+		logger.Errorf(req.Context(), "closing websocket, %v", err)
 		ws.Close()
 	}
 }
