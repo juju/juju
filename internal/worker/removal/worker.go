3bd10a2151f994c349e5f381c139b7fdf8256948
Joseph Phillips <joe.phillips@canonical.com>
2025-04-04 17:20:19 +0200

    feat: removal worker job scheduling
    Adds functionality to the worker for scheduling jobs via the runner.
    All jobs are interrogated when either the watcher or timer fire.
    If any are not currently being run by the runner, they are scheduled.

--- internal/worker/removal/worker.go
+++ internal/worker/removal/worker.go
@@ -7,11 +7,14 @@ import (
 	"context"
 	"time"
 
+	"github.com/juju/collections/set"
 	"github.com/juju/worker/v4"
 	"github.com/juju/worker/v4/catacomb"
+	"gopkg.in/tomb.v2"
 
 	coreerrors "github.com/juju/juju/core/errors"
 	"github.com/juju/juju/core/logger"
+	"github.com/juju/juju/domain/removal"
 	"github.com/juju/juju/internal/errors"
 	internalworker "github.com/juju/juju/internal/worker"
 )
@@ -105,12 +108,12 @@ func (w *removalWorker) loop() (err error) {
 			return w.catacomb.ErrDying()
 		case jobIDs := <-watch.Changes():
 			w.cfg.Logger.Infof(ctx, "got removal job changes: %v", jobIDs)
-			if err := w.processRemovalJobs(); err != nil {
+			if err := w.processRemovalJobs(ctx); err != nil {
 				return errors.Capture(err)
 			}
 			timer.Reset(jobCheckMaxInterval)
 		case <-timer.Chan():
-			if err := w.processRemovalJobs(); err != nil {
+			if err := w.processRemovalJobs(ctx); err != nil {
 				return errors.Capture(err)
 			}
 			timer.Reset(jobCheckMaxInterval)
@@ -125,7 +128,34 @@ func (w *removalWorker) loop() (err error) {
 // This is safe due to the following conditions:
 // - This is the only method interrogating/adding workers to the runner.
 // - It is only invoked from cases in the main event loop, so is Goroutine safe.
-func (w *removalWorker) processRemovalJobs() error {
+func (w *removalWorker) processRemovalJobs(ctx context.Context) error {
+	jobs, err := w.cfg.RemovalService.GetAllJobs(ctx)
+	if err != nil {
+		return errors.Capture(err)
+	}
+
+	running := set.NewStrings(w.runner.WorkerNames()...)
+	log := w.cfg.Logger
+
+	for _, j := range jobs {
+		id := j.UUID.String()
+
+		if running.Contains(id) {
+			log.Debugf(ctx, "removal job %q already running", id)
+			continue
+		}
+
+		if j.ScheduledFor.After(w.cfg.Clock.Now().UTC()) {
+			log.Debugf(ctx, "removal job %q not due until %s", id, j.ScheduledFor.Format(time.RFC3339))
+			continue
+		}
+
+		w.cfg.Logger.Infof(ctx, "scheduling job %q", id)
+		if err := w.runner.StartWorker(id, newJobWorker(w.cfg.RemovalService, j)); err != nil {
+			return errors.Capture(err)
+		}
+	}
+
 	return nil
 }
 
@@ -138,4 +168,33 @@ func (w *removalWorker) Kill() {
 // and returns the error with which it exited.
 func (w *removalWorker) Wait() error {
 	return w.catacomb.Wait()
+}
+
+// jobWorker exists only to expose enough of a tomb to satisfy [worker.Worker]
+type jobWorker struct {
+	tomb tomb.Tomb
+}
+
+// newJobWorker returns a closure suitable for passing to
+// a runner's StartWorker method.
+// It uses the input service to run the input removal job.
+func newJobWorker(svc RemovalService, job removal.Job) func() (worker.Worker, error) {
+	return func() (worker.Worker, error) {
+		w := jobWorker{}
+		w.tomb.Go(func() error {
+			return svc.ExecuteJob(w.tomb.Context(context.Background()), job)
+		})
+		return &w, nil
+	}
+}
+
+// Kill (worker.Worker) tells the worker to stop running the job and return.
+func (w *jobWorker) Kill() {
+	w.tomb.Kill(nil)
+}
+
+// Wait (worker.Worker) waits for the worker to stop,
+// and returns the error with which it exited.
+func (w *jobWorker) Wait() error {
+	return w.tomb.Wait()
 }
--- internal/worker/removal/worker_test.go
+++ internal/worker/removal/worker_test.go
@@ -4,16 +4,20 @@
 package removal
 
 import (
+	"context"
+	"reflect"
 	"time"
 
 	"github.com/juju/clock"
 	"github.com/juju/testing"
 	jc "github.com/juju/testing/checkers"
+	"github.com/juju/worker/v4"
 	"github.com/juju/worker/v4/workertest"
 	"go.uber.org/mock/gomock"
 	gc "gopkg.in/check.v1"
 
 	"github.com/juju/juju/core/watcher/watchertest"
+	"github.com/juju/juju/domain/removal"
 	loggertesting "github.com/juju/juju/internal/logger/testing"
 )
 
@@ -46,7 +50,6 @@ func (s *workerSuite) TestWorkerStartStop(c *gc.C) {
 		Clock:          s.clk,
 		Logger:         loggertesting.WrapCheckLog(c),
 	}
-
 	w, err := NewWorker(cfg)
 	c.Assert(err, jc.ErrorIsNil)
 
@@ -54,6 +57,165 @@ func (s *workerSuite) TestWorkerStartStop(c *gc.C) {
 	case <-sync:
 	case <-time.After(testing.ShortWait):
 		c.Fatalf("timed out waiting for worker to start")
+	}
+
+	workertest.CleanKill(c, w)
+}
+
+// TestWorkerNotifiedSchedulesDueJob tests the following sequence of events:
+// - The watcher fires.
+// - We query for jobs, receive two, but only one is due for execution,
+// - Only the due job is scheduled with the runner.
+func (s *workerSuite) TestWorkerNotifiedSchedulesDueJob(c *gc.C) {
+	defer s.setUpMocks(c).Finish()
+
+	ch := make(chan []string)
+	watch := watchertest.NewMockStringsWatcher(ch)
+	s.svc.EXPECT().WatchRemovals().Return(watch, nil)
+
+	s.clk.EXPECT().NewTimer(jobCheckMaxInterval).DoAndReturn(func(d time.Duration) clock.Timer {
+		return clock.WallClock.NewTimer(d)
+	})
+
+	now := time.Now().UTC()
+	s.clk.EXPECT().Now().Return(now).Times(2)
+
+	dueJob := removal.Job{
+		UUID:         "due-job-uuid",
+		RemovalType:  0,
+		EntityUUID:   "due-relation-uuid",
+		Force:        false,
+		ScheduledFor: now.Add(-time.Hour),
+	}
+	laterJob := removal.Job{
+		UUID:         "later-job-uuid",
+		RemovalType:  0,
+		EntityUUID:   "later-relation-uuid",
+		Force:        false,
+		ScheduledFor: now.Add(time.Hour),
+	}
+	s.svc.EXPECT().GetAllJobs(gomock.Any()).Return([]removal.Job{dueJob, laterJob}, nil)
+
+	// Use job execution as a synchronisation point below.
+	// so that we know we can kill the worker.
+	sync := make(chan struct{})
+	s.svc.EXPECT().ExecuteJob(gomock.Any(), dueJob).DoAndReturn(func(_ context.Context, job removal.Job) error {
+		sync <- struct{}{}
+		return nil
+	})
+
+	cfg := Config{
+		RemovalService: s.svc,
+		Clock:          s.clk,
+		Logger:         loggertesting.WrapCheckLog(c),
+	}
+	w, err := NewWorker(cfg)
+	c.Assert(err, jc.ErrorIsNil)
+	defer workertest.DirtyKill(c, w)
+
+	select {
+	case ch <- []string{"some-job-uuid"}:
+	case <-time.After(testing.ShortWait):
+		c.Fatalf("timed out waiting for watcher event consumption")
+	}
+
+	select {
+	case <-sync:
+	case <-time.After(testing.ShortWait):
+		c.Fatalf("timed out waiting for job execution")
+	}
+
+	workertest.CleanKill(c, w)
+}
+
+// TestWorkerTimerSchedulesOnlyRequiredJob tests the following sequence of events:
+// - The timer fires.
+// - We query for jobs, receive two, but one has already been scheduled.
+// - Only the unscheduled job is scheduled with the runner.
+func (s *workerSuite) TestWorkerTimerSchedulesOnlyRequiredJob(c *gc.C) {
+	defer s.setUpMocks(c).Finish()
+
+	ch := make(chan []string)
+	watch := watchertest.NewMockStringsWatcher(ch)
+	s.svc.EXPECT().WatchRemovals().Return(watch, nil)
+
+	// Fire it straight away.
+	s.clk.EXPECT().NewTimer(jobCheckMaxInterval).DoAndReturn(func(d time.Duration) clock.Timer {
+		return clock.WallClock.NewTimer(time.Millisecond)
+	})
+
+	now := time.Now().UTC()
+	s.clk.EXPECT().Now().Return(now)
+
+	dueJob := removal.Job{
+		UUID:         "due-job-uuid",
+		RemovalType:  0,
+		EntityUUID:   "due-relation-uuid",
+		Force:        false,
+		ScheduledFor: now.Add(-time.Hour),
+	}
+	scheduledJob := removal.Job{
+		UUID:         "scheduled-job-uuid",
+		RemovalType:  0,
+		EntityUUID:   "scheduled-relation-uuid",
+		Force:        false,
+		ScheduledFor: now.Add(-time.Hour),
+	}
+	s.svc.EXPECT().GetAllJobs(gomock.Any()).Return([]removal.Job{dueJob, scheduledJob}, nil)
+
+	// Use job execution as a synchronisation point below.
+	// so that we know we can kill the worker.
+	sync := make(chan struct{})
+	s.svc.EXPECT().ExecuteJob(gomock.Any(), dueJob).DoAndReturn(func(_ context.Context, job removal.Job) error {
+		sync <- struct{}{}
+		return nil
+	})
+
+	cfg := Config{
+		RemovalService: s.svc,
+		Clock:          s.clk,
+		Logger:         loggertesting.WrapCheckLog(c),
+	}
+	w, err := NewWorker(cfg)
+	c.Assert(err, jc.ErrorIsNil)
+	defer workertest.DirtyKill(c, w)
+
+	// Imitate a worker already scheduled in the runner.
+	rw := w.(*removalWorker)
+	err = rw.runner.StartWorker("scheduled-job-uuid", func() (worker.Worker, error) {
+		w := jobWorker{}
+		w.tomb.Go(func() error {
+			<-w.tomb.Dying()
+			return nil
+		})
+		return &w, nil
+	})
+	c.Assert(err, jc.ErrorIsNil)
+
+	// We need to wait until it is actually reported starting.
+	// This is because StartWorker above is not synchronous.
+	var count int
+	for {
+		if reflect.DeepEqual(rw.runner.WorkerNames(), []string{"scheduled-job-uuid"}) {
+			break
+		}
+		count++
+		if count > 200 {
+			c.Fatalf("timed out waiting for runner to schedule job")
+		}
+		time.Sleep(10 * time.Millisecond)
+	}
+
+	select {
+	case ch <- []string{"due-job-uuid"}:
+	case <-time.After(testing.ShortWait):
+		c.Fatalf("timed out waiting for watcher event consumption")
+	}
+
+	select {
+	case <-sync:
+	case <-time.After(testing.ShortWait):
+		c.Fatalf("timed out waiting for job execution")
 	}
 
 	workertest.CleanKill(c, w)
