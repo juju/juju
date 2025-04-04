2e56528c6eebe9310174bf35add4e808bc71918c
Nicolas Vinuesa <vinuesa.nico@gmail.com>
2025-04-04 11:20:57 +0200

    chore: unset all exposed endpoints if the input is empty
    Small patch to add the possibility to remove all exposed endpoints of a
    given application if the provided set of endpoints is empty.

--- domain/application/service/exposed.go
+++ domain/application/service/exposed.go
@@ -47,8 +47,6 @@ func (s *Service) GetExposedEndpoints(ctx context.Context, appName string) (map[
 // endpoint names. If the resulting exposed endpoints map for the application
 // becomes empty after the settings are removed, the application will be
 // automatically unexposed.
-// If the provided set of endpoints is empty, all exposed endpoints of the
-// application will be removed.
 //
 // If no application is found, an error satisfying
 // [applicationerrors.ApplicationNotFound] is returned.
@@ -58,10 +56,6 @@ func (s *Service) UnsetExposeSettings(ctx context.Context, appName string, expos
 		return errors.Capture(err)
 	}
 
-	if exposedEndpoints.IsEmpty() {
-		// TODO: Implement me.
-		return nil
-	}
 	return s.st.UnsetExposeSettings(ctx, appID, exposedEndpoints)
 }
 
