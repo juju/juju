35de397688523f6f4f08117d6e60681199b1ea1f
Heather Lanigan <heather.lanigan@canonical.com>
2025-04-04 10:32:00 -0400

    refactor: move watchers to a new file
    Anticipating the implemention of WatchLifeSuspendedStatus watchers to
    a new file. It will be large and complex.
    
    Updated the signature of WatchLifeSuspendedStatus to take a unit name
    rather than an application ID. The service method will determine if the
    unit is a subordinate or not and watch different applications based on
    the answer. All of the logic from the SubordinateRelationWatcher in the
    uniter facade will be pushed into the domain.

---
+++ domain/relation/service/watcher.go
@@ -0,0 +1,102 @@
+// Copyright 2025 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package service
+
+import (
+	"context"
+
+	"github.com/juju/juju/core/application"
+	"github.com/juju/juju/core/changestream"
+	coreerrors "github.com/juju/juju/core/errors"
+	"github.com/juju/juju/core/logger"
+	corerelation "github.com/juju/juju/core/relation"
+	"github.com/juju/juju/core/unit"
+	"github.com/juju/juju/core/watcher"
+	"github.com/juju/juju/core/watcher/eventsource"
+	"github.com/juju/juju/domain/relation"
+	"github.com/juju/juju/internal/errors"
+)
+
+// WatcherFactory instances return watchers for a given namespace and UUID.
+type WatcherFactory interface {
+	// NewNotifyWatcher returns a new watcher that filters changes from the
+	// input base watcher's db/queue. A single filter option is required, though
+	// additional filter options can be provided.
+	NewNotifyWatcher(
+		filter eventsource.FilterOption,
+		filterOpts ...eventsource.FilterOption,
+	) (watcher.NotifyWatcher, error)
+}
+
+// WatchableService provides the API for working with applications and the
+// ability to create watchers.
+type WatchableService struct {
+	*Service
+	watcherFactory WatcherFactory
+}
+
+// NewWatchableService returns a new watchable service reference wrapping the input state.
+func NewWatchableService(
+	st State,
+	watcherFactory WatcherFactory,
+	logger logger.Logger,
+) *WatchableService {
+	return &WatchableService{
+		Service:        NewService(st, logger),
+		watcherFactory: watcherFactory,
+	}
+}
+
+// WatchApplicationSettings returns a notify watcher that will signal
+// whenever the specified application's relation settings are changed.
+func (s *WatchableService) WatchApplicationSettings(
+	ctx context.Context,
+	relationUUID corerelation.UUID,
+	applicationID application.ID,
+) (watcher.NotifyWatcher, error) {
+	relationEndpointUUID, err := s.getRelationEndpointUUID(ctx, relation.GetRelationEndpointUUIDArgs{
+		RelationUUID:  relationUUID,
+		ApplicationID: applicationID,
+	})
+	if err != nil {
+		return nil, errors.Capture(errors.Errorf("watch application settings: %w", err))
+	}
+	return s.watcherFactory.NewNotifyWatcher(
+		eventsource.PredicateFilter(
+			s.st.WatcherApplicationSettingsNamespace(),
+			changestream.All,
+			func(s string) bool {
+				return s == relationEndpointUUID.String()
+			},
+		),
+	)
+}
+
+// WatchLifeSuspendedStatus returns a watcher that notifies of changes to
+// the life or suspended status any relation the unit's application is part
+// of. If the unit is a subordinate, its principal application is watched.
+func (s *WatchableService) WatchLifeSuspendedStatus(
+	ctx context.Context,
+	unitUUID unit.UUID,
+) (watcher.StringsWatcher, error) {
+	return nil, coreerrors.NotImplemented
+}
+
+// WatchUnitScopes returns a watcher which notifies of counterpart units
+// entering and leaving the unit's scope.
+func (s *WatchableService) WatchUnitScopes(
+	ctx context.Context,
+	relationUnit corerelation.UnitUUID,
+) (relation.RelationScopeWatcher, error) {
+	return relation.RelationScopeWatcher{}, coreerrors.NotImplemented
+}
+
+// WatchUnitRelations returns a watcher that notifies of changes to counterpart units in
+// the relation.
+func (s *WatchableService) WatchUnitRelations(
+	ctx context.Context,
+	relationUnit corerelation.UnitUUID,
+) (relation.RelationUnitsWatcher, error) {
+	return nil, coreerrors.NotImplemented
+}
