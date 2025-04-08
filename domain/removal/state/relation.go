9eb0cefba28b620ad0399e03b55006e8dff3cc7e
Joseph Phillips <joe.phillips@canonical.com>
2025-04-08 10:59:30 +0200

    chore: relocate removal state concerns for relations to separate file
    This mirrors what was done in the service layer to ease work on the
    removal logic for specific entities.

---
+++ domain/removal/state/relation.go
@@ -0,0 +1,113 @@
+// Copyright 2025 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package state
+
+import (
+	"context"
+	"time"
+
+	"github.com/canonical/sqlair"
+
+	"github.com/juju/juju/internal/errors"
+)
+
+// RelationExists returns true if a relation exists with the input UUID.
+func (st *State) RelationExists(ctx context.Context, rUUID string) (bool, error) {
+	db, err := st.DB()
+	if err != nil {
+		return false, errors.Capture(err)
+	}
+
+	relationUUID := entityUUID{UUID: rUUID}
+	existsStmt, err := st.Prepare(`
+SELECT uuid AS &entityUUID.uuid
+FROM   relation
+WHERE  uuid = $entityUUID.uuid`, relationUUID)
+	if err != nil {
+		return false, errors.Errorf("preparing relation exists query: %w", err)
+	}
+
+	var relationExists bool
+	err = db.Txn(ctx, func(ctx context.Context, tx *sqlair.TX) error {
+		err = tx.Query(ctx, existsStmt, relationUUID).Get(&relationUUID)
+		if errors.Is(err, sqlair.ErrNoRows) {
+			return nil
+		} else if err != nil {
+			return errors.Errorf("running relation exists query: %w", err)
+		}
+
+		relationExists = true
+		return nil
+	})
+
+	return relationExists, err
+}
+
+// RelationAdvanceLife ensures that there is no relation
+// identified by the input UUID, that is still alive.
+func (st *State) RelationAdvanceLife(ctx context.Context, rUUID string) error {
+	db, err := st.DB()
+	if err != nil {
+		return errors.Capture(err)
+	}
+
+	relationUUID := entityUUID{UUID: rUUID}
+	stmt, err := st.Prepare(`
+UPDATE relation
+SET    life_id = 1
+WHERE  uuid = $entityUUID.uuid
+AND    life_id = 0`, relationUUID)
+	if err != nil {
+		return errors.Errorf("preparing relation life update: %w", err)
+	}
+
+	return errors.Capture(db.Txn(ctx, func(ctx context.Context, tx *sqlair.TX) error {
+		err = tx.Query(ctx, stmt, relationUUID).Run()
+		if err != nil {
+			return errors.Errorf("advancing relation life: %w", err)
+		}
+		return nil
+	}))
+}
+
+// RelationScheduleRemoval schedules a removal job for the relation with the
+// input UUID, qualified with the input force boolean.
+// We don't care if the relation does not exist at this point because:
+// - it should have been validated prior to calling this method,
+// - the removal job executor will handle that fact.
+func (st *State) RelationScheduleRemoval(
+	ctx context.Context, removalUUID, relUUID string, force bool, when time.Time,
+) error {
+	db, err := st.DB()
+	if err != nil {
+		return errors.Capture(err)
+	}
+
+	removalRec := removalJob{
+		UUID:          removalUUID,
+		RemovalTypeID: 0,
+		EntityUUID:    relUUID,
+		Force:         force,
+		ScheduledFor:  when,
+	}
+
+	removalStmt, err := st.Prepare("INSERT INTO removal (*) VALUES ($removalJob.*)", removalRec)
+	if err != nil {
+		return errors.Errorf("preparing relation removal: %w", err)
+	}
+
+	return errors.Capture(db.Txn(ctx, func(ctx context.Context, tx *sqlair.TX) error {
+		err = tx.Query(ctx, removalStmt, removalRec).Run()
+		if err != nil {
+			return errors.Errorf("scheduling relation removal: %w", err)
+		}
+		return nil
+	}))
+}
+
+// NamespaceForWatchRemovals returns the table name whose UUIDs we
+// are watching in order to be notified of new removal jobs.
+func (st *State) NamespaceForWatchRemovals() string {
+	return "removal"
+}
