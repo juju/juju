b2a2ef8bf62ecabae4cc06f1f9d6458fc251c226
Guillaume Fouillet <guillaume.fouillet@canonical.com>
2025-04-14 15:35:22 +0200

    feat(relation): add encoding and decoding for watch events
    This patch is required to be able to smuggle different type of UUIDs from the relation domain.
    Uniter will require  this kind of functionality to be able to take decision over what event are
    returned from the RelatedUnitWatcher
    
    - `domain/relation/encode_watch_event.go`: Implement functions to encode and decode application and
      unit UUIDs for event transmission, ensuring type specificity and error handling.
    - `domain/relation/encode_watch_event_test.go`: Add unit tests covering encoding and decoding
      functionality, including error cases for invalid inputs.

---
+++ domain/relation/encode_watch_event.go
@@ -0,0 +1,60 @@
+// Copyright 2025 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package relation
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/juju/collections/set"
+
+	coreapplication "github.com/juju/juju/core/application"
+	coreunit "github.com/juju/juju/core/unit"
+)
+
+const (
+	separator = "#"
+)
+
+// EncodedUUID represents a string-encoded type for categorizing UUIDs
+// with predefined values like "app" or "unit".
+type EncodedUUID string
+
+const (
+
+	// ApplicationUUID defines a constant EncodedUUID value used to categorize
+	// application UUIDs.
+	ApplicationUUID EncodedUUID = "app"
+
+	// UnitUUID is a constant of type EncodedUUID used to categorize unit UUIDs.
+	UnitUUID EncodedUUID = "unit"
+)
+
+// EncodeApplicationUUID encodes an application UUID with a marker which specify
+// the fact that it is an application uuid. It helps to ship multiple uuid kind
+// through watchers and interprets them.
+func EncodeApplicationUUID(uuid coreapplication.ID) string {
+	return fmt.Sprintf("%s%s%s", ApplicationUUID, separator, uuid)
+}
+
+// EncodeUnitUUID encodes a unit UUID with a marker which specify
+// the fact that it is a unit uuid. It helps to ship multiple uuid kind
+// through watchers and interprets them.
+func EncodeUnitUUID(uuid coreunit.UUID) string {
+	return fmt.Sprintf("%s%s%s", UnitUUID, separator, uuid)
+}
+
+// Decode parses an event string into its respective EncodedUUID
+// and associated value, returning an error for invalid input.
+func Decode(event string) (EncodedUUID, string, error) {
+	values := strings.Split(event, separator)
+	allowedTypes := set.NewStrings(
+		string(ApplicationUUID),
+		string(UnitUUID),
+	)
+	if len(values) != 2 || !allowedTypes.Contains(values[0]) {
+		return "", "", fmt.Errorf("invalid event with uuid: %s", event)
+	}
+	return EncodedUUID(values[0]), values[1], nil
+}
