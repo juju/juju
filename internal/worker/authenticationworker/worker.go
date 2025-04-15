7879ff043836d6f64883689f12c357bfd244d557
SimoneDutto <simone.dutto@canonical.com>
2025-04-15 16:47:00 +0200

    feat(authentication-worker): refactor authentication worker and add methods for ephemeral keys
    In this PR:
    - refactor the authentication worker to work in a different way. Before it was replacing the
      content of the whole authorized_keys on each update. In this way we needed a way to track
      non-juju keys to make sure not to delete them. Plus, since we were not refreshing the
      nonJujuKeys array, on each restarted the keys manually added by the user were automatically
      delete. In this new implementation since we were already calculating the diff,
      I am adding/deleting just the affected juju keys.
    - added some tests to make sure they don't interact in some strange way
    - the change mentioned above enabled a way easier implementation to add/remove
      ephemeral keys, because we can just add/delete them without the other code
      inadvertently deleting them.
    - change the manifold config to permit in the future to consume the ephemeral
      keys methods from outside this worker.

--- internal/worker/authenticationworker/worker.go
+++ internal/worker/authenticationworker/worker.go
@@ -24,14 +24,18 @@ var SSHUser = "ubuntu"
 
 var logger = loggo.GetLogger("juju.worker.authenticationworker")
 
-type keyupdaterWorker struct {
+type keyUpdater struct {
 	st  *keyupdater.State
 	tag names.MachineTag
 	// jujuKeys are the most recently retrieved keys from state.
 	jujuKeys set.Strings
-	// nonJujuKeys are those added externally to auth keys file
-	// such keys do not have comments with the Juju: prefix.
-	nonJujuKeys []string
+}
+
+// AuthWorker is a worker that keeps track of
+// the machine's authorised ssh keys and ensures the
+// ~/.ssh/authorized_keys file is up to date.
+type AuthWorker struct {
+	*watcher.NotifyWorker
 }
 
 // NewWorker returns a worker that keeps track of
@@ -42,51 +46,76 @@ func NewWorker(st *keyupdater.State, agentConfig agent.Config) (worker.Worker, e
 	if !ok {
 		return nil, errors.NotValidf("machine tag %v", agentConfig.Tag())
 	}
+	keyUpdater := &keyUpdater{
+		st:  st,
+		tag: machineTag,
+	}
+	err := keyUpdater.initializeAuthorizedKeys()
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
 	w, err := watcher.NewNotifyWorker(watcher.NotifyConfig{
-		Handler: &keyupdaterWorker{
-			st:  st,
-			tag: machineTag,
-		},
+		Handler: keyUpdater,
 	})
 	if err != nil {
 		return nil, errors.Trace(err)
 	}
-	return w, nil
+
+	return AuthWorker{
+		NotifyWorker: w,
+	}, nil
 }
 
-// SetUp is defined on the worker.NotifyWatchHandler interface.
-func (kw *keyupdaterWorker) SetUp() (watcher.NotifyWatcher, error) {
+// initializeAuthorizedKeys reads the current ssh keys from the state
+// and the current ssh keys from ~/.ssh/authorized_keys. It removes any
+// ephemeral keys.
+func (kw *keyUpdater) initializeAuthorizedKeys() error {
 	// Record the keys Juju knows about.
 	jujuKeys, err := kw.st.AuthorisedKeys(kw.tag)
 	if err != nil {
 		err = errors.Annotatef(err, "reading Juju ssh keys for %q", kw.tag)
 		logger.Infof(err.Error())
-		return nil, err
+		return err
 	}
-	kw.jujuKeys = set.NewStrings(jujuKeys...)
-
+	kw.jujuKeys = set.NewStrings(ensureJujuCommentForKeys(jujuKeys)...)
+	allKeys := kw.jujuKeys.Values()
 	// Read the keys currently in ~/.ssh/authorised_keys.
 	sshKeys, err := ssh.ListKeys(SSHUser, ssh.FullKeys)
 	if err != nil {
 		err = errors.Annotatef(err, "reading ssh authorized keys for %q", kw.tag)
 		logger.Infof(err.Error())
-		return nil, err
+		return err
 	}
-	// Record any keys not added by Juju.
+	// Preserve any non-juju keys, including malformed ones.
 	for _, key := range sshKeys {
 		_, comment, err := ssh.KeyFingerprint(key)
-		// Also record keys which we cannot parse.
-		if err != nil || !strings.HasPrefix(comment, ssh.JujuCommentPrefix) {
-			kw.nonJujuKeys = append(kw.nonJujuKeys, key)
+		if err != nil {
+			logger.Warningf("preserved malformed ssh key %q: %v", key, err)
+			allKeys = append(allKeys, key)
+			continue
 		}
+		if strings.HasPrefix(comment, ssh.JujuCommentPrefix) {
+			logger.Infof("filtered out juju ssh key %q", key)
+			continue
+		}
+		if strings.HasPrefix(comment, jujuEphemeralCommentPrefix) {
+			logger.Infof("filtered out juju ephemeral ssh key %q", key)
+			continue
+		}
+		logger.Infof("preserved ssh key %q", key)
+		allKeys = append(allKeys, key)
 	}
 	// Write out the ssh authorised keys file to match the current state of the world.
-	if err := kw.writeSSHKeys(jujuKeys); err != nil {
+	if err := ssh.ReplaceKeys(SSHUser, allKeys...); err != nil {
 		err = errors.Annotate(err, "adding current Juju keys to ssh authorised keys")
 		logger.Infof(err.Error())
-		return nil, err
+		return err
 	}
+	return nil
+}
 
+// SetUp is defined on the worker.NotifyWatchHandler interface.
+func (kw *keyUpdater) SetUp() (watcher.NotifyWatcher, error) {
 	w, err := kw.st.WatchAuthorisedKeys(kw.tag)
 	if err != nil {
 		err = errors.Annotate(err, "starting key updater worker")
@@ -97,20 +126,8 @@ func (kw *keyupdaterWorker) SetUp() (watcher.NotifyWatcher, error) {
 	return w, nil
 }
 
-// writeSSHKeys writes out a new ~/.ssh/authorised_keys file, retaining any non Juju keys
-// and adding the specified set of Juju keys.
-func (kw *keyupdaterWorker) writeSSHKeys(jujuKeys []string) error {
-	allKeys := kw.nonJujuKeys
-	// Ensure any Juju keys have the required prefix in their comment.
-	for i, key := range jujuKeys {
-		jujuKeys[i] = ssh.EnsureJujuComment(key)
-	}
-	allKeys = append(allKeys, jujuKeys...)
-	return ssh.ReplaceKeys(SSHUser, allKeys...)
-}
-
 // Handle is defined on the worker.NotifyWatchHandler interface.
-func (kw *keyupdaterWorker) Handle(_ <-chan struct{}) error {
+func (kw *keyUpdater) Handle(_ <-chan struct{}) error {
 	// Read the keys that Juju has.
 	newKeys, err := kw.st.AuthorisedKeys(kw.tag)
 	if err != nil {
@@ -119,24 +136,60 @@ func (kw *keyupdaterWorker) Handle(_ <-chan struct{}) error {
 		return err
 	}
 	// Figure out if any keys have been added or deleted.
-	newJujuKeys := set.NewStrings(newKeys...)
+	newJujuKeys := set.NewStrings(ensureJujuCommentForKeys(newKeys)...)
 	deleted := kw.jujuKeys.Difference(newJujuKeys)
 	added := newJujuKeys.Difference(kw.jujuKeys)
-	if added.Size() > 0 || deleted.Size() > 0 {
-		logger.Infof("adding ssh keys to authorised keys: %v", added)
-		logger.Infof("deleting ssh keys from authorised keys: %v", deleted)
-		if err = kw.writeSSHKeys(newKeys); err != nil {
-			err = errors.Annotate(err, "updating ssh keys")
-			logger.Infof(err.Error())
-			return err
+	logger.Infof("adding ssh keys to authorised keys: %v", added)
+	logger.Infof("deleting ssh keys from authorised keys: %v", deleted)
+	err = ssh.AddKeys(SSHUser, added.Values()...)
+	if err != nil {
+		err = errors.Annotatef(err, "adding ssh keys to authorised keys: %v", added)
+		logger.Infof(err.Error())
+		return err
+	}
+	keyFingerprintsToDelete := []string{}
+	for _, key := range deleted.Values() {
+		fingerprint, _, err := ssh.KeyFingerprint(key)
+		if err != nil {
+			continue
 		}
+		keyFingerprintsToDelete = append(keyFingerprintsToDelete, fingerprint)
+	}
+
+	err = ssh.DeleteKeys(SSHUser, keyFingerprintsToDelete...)
+	if err != nil {
+		err = errors.Annotatef(err, "removing ssh keys from authorised keys: %v", deleted)
+		return err
 	}
 	kw.jujuKeys = newJujuKeys
 	return nil
 }
 
 // TearDown is defined on the worker.NotifyWatchHandler interface.
-func (kw *keyupdaterWorker) TearDown() error {
+func (kw *keyUpdater) TearDown() error {
 	// Nothing to do here.
+	return nil
+}
+
+// AddEphemeralKey adds an ephemeral key to authorized_keys file.
+func (a *AuthWorker) AddEphemeralKey(ephemeralKey string) error {
+	ephemeralKey = ensureJujuEphemeralComment(ephemeralKey)
+	err := ssh.AddKeys(SSHUser, ephemeralKey)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// RemoveEphemeralKey removes an ephemeral key from authorized_keys file.
+func (a *AuthWorker) RemoveEphemeralKey(ephemeralKey string) error {
+	fingerprint, _, err := ssh.KeyFingerprint(ephemeralKey)
+	if err != nil {
+		return errors.Annotatef(err, "removing ephemeral key %q", ephemeralKey)
+	}
+	err = ssh.DeleteKeys(SSHUser, fingerprint)
+	if err != nil {
+		return err
+	}
 	return nil
 }
