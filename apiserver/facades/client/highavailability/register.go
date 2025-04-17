93884b19da0fdaf70133cc08215183606a8d1d1c
Lye Yao <lye.yao@canonical.com>
2025-04-17 06:04:33 +0000

    refactor: remove basic model information from client/highavailability

--- apiserver/facades/client/highavailability/register.go
+++ apiserver/facades/client/highavailability/register.go
@@ -11,22 +11,22 @@ import (
 
 	apiservererrors "github.com/juju/juju/apiserver/errors"
 	"github.com/juju/juju/apiserver/facade"
-	oldstate "github.com/juju/juju/state"
+	"github.com/juju/juju/core/model"
 )
 
 // Register is called to expose a package of facades onto a given registry.
 func Register(registry facade.FacadeRegistry) {
 	registry.MustRegister("HighAvailability", 2, func(stdCtx context.Context, ctx facade.ModelContext) (facade.Facade, error) {
-		return newHighAvailabilityAPIV2(ctx)
+		return newHighAvailabilityAPIV2(stdCtx, ctx)
 	}, reflect.TypeOf((*HighAvailabilityAPIV2)(nil)))
 	registry.MustRegister("HighAvailability", 3, func(stdCtx context.Context, ctx facade.ModelContext) (facade.Facade, error) {
-		return newHighAvailabilityAPI(ctx)
+		return newHighAvailabilityAPI(stdCtx, ctx)
 	}, reflect.TypeOf((*HighAvailabilityAPI)(nil)))
 }
 
 // newHighAvailabilityAPI creates a new server-side highavailability API end point.
-func newHighAvailabilityAPIV2(ctx facade.ModelContext) (*HighAvailabilityAPIV2, error) {
-	v3, err := newHighAvailabilityAPI(ctx)
+func newHighAvailabilityAPIV2(stdCtx context.Context, ctx facade.ModelContext) (*HighAvailabilityAPIV2, error) {
+	v3, err := newHighAvailabilityAPI(stdCtx, ctx)
 	if err != nil {
 		return nil, errors.Trace(err)
 	}
@@ -34,29 +34,29 @@ func newHighAvailabilityAPIV2(ctx facade.ModelContext) (*HighAvailabilityAPIV2, 
 }
 
 // newHighAvailabilityAPI creates a new server-side highavailability API end point.
-func newHighAvailabilityAPI(ctx facade.ModelContext) (*HighAvailabilityAPI, error) {
+func newHighAvailabilityAPI(stdCtx context.Context, ctx facade.ModelContext) (*HighAvailabilityAPI, error) {
 	// Only clients can access the high availability facade.
 	authorizer := ctx.Auth()
 	if !authorizer.AuthClient() {
 		return nil, apiservererrors.ErrPerm
 	}
 
-	st := ctx.State()
-	model, err := st.Model()
+	domainServices := ctx.DomainServices()
+
+	modelInfoService := domainServices.ModelInfo()
+	modelInfo, err := modelInfoService.GetModelInfo(stdCtx)
 	if err != nil {
 		return nil, errors.Trace(err)
 	}
-	if model.Type() == oldstate.ModelTypeCAAS {
+	if modelInfo.Type == model.CAAS {
 		return nil, errors.NotSupportedf("high availability on kubernetes controllers")
 	}
-
-	domainServices := ctx.DomainServices()
 
 	// For adding additional controller units, we don't need a storage registry.
 	applicationService := domainServices.Application()
 
 	return &HighAvailabilityAPI{
-		st:                      st,
+		st:                      ctx.State(),
 		nodeService:             domainServices.ControllerNode(),
 		machineService:          domainServices.Machine(),
 		applicationService:      applicationService,
