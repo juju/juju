978ed5be3ce6ad677f58e2f82d413e6fbe312209
Thomas Miller <thomas.miller@canonical.com>
2025-05-01 14:00:12 +1000

    feat: model config agent stream validator
    This commit adds a new validator to the model config domain for making
    sure that the user cannot modify the agent stream value in model config
    as it is now considered a read only value.
    
    The validator will also remove the value from model config so that it c
    an't be persisted into state.
    
    As part of this commit I also removed the defaulting of the agent stream
    value in model config. This is business logic and does not belong in
    environ config.

--- environs/config/config.go
+++ environs/config/config.go
@@ -1430,10 +1430,7 @@ func (c *Config) ImageStream() string {
 // bootstrapping or upgrading an environment.
 func (c *Config) AgentStream() string {
 	v, _ := c.defined[AgentStreamKey].(string)
-	if v != "" {
-		return v
-	}
-	return "released"
+	return v
 }
 
 // ContainerImageStream returns the simplestreams stream used to identify which
