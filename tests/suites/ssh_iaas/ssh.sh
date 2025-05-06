db5ef7899608682ab1c1604564539f89b740fad5
Kian Parvin <kian.parvin@canonical.com>
2025-05-06 13:27:13 +0200

    chore: tweaks to make test non-interactive

--- tests/suites/ssh_iaas/ssh.sh
+++ tests/suites/ssh_iaas/ssh.sh
@@ -32,8 +32,9 @@ check_ssh_using_openssh() {
 	test_file="test-ssh.txt"
 
 	# Check that we can write a file on the remote host then read it back.
-	ssh_no_hostkey_check -J admin@"$controller_address":17022 ubuntu@"$virtual_hostname" "echo hello > $test_file"
-	output=$(ssh_no_hostkey_check -J admin@"$controller_address":17022 ubuntu@"$virtual_hostname" "cat $test_file")
+	jump_host=admin@"$controller_address"
+	ssh_no_hostkey_check "$jump_host" ubuntu@"$virtual_hostname" "echo hello > $test_file"
+	output=$(ssh_no_hostkey_check "$jump_host" ubuntu@"$virtual_hostname" "cat $test_file")
 	check_contains "$output" hello
 }
 
@@ -44,18 +45,36 @@ check_scp_using_openssh() {
 
 	# Check that we can copy a file to the remote host then copy it back.
 	echo hello > $scp_file
-	scp_no_hostkey_check -J admin@"$controller_address":17022 test-scp.txt ubuntu@"$virtual_hostname":$scp_file
-	scp_no_hostkey_check -J admin@"$controller_address":17022 ubuntu@"$virtual_hostname":$scp_file $scp_file
+	jump_host=admin@"$controller_address"
+	scp_no_hostkey_check "$jump_host" test-scp.txt ubuntu@"$virtual_hostname":$scp_file
+	scp_no_hostkey_check "$jump_host" ubuntu@"$virtual_hostname":$scp_file $scp_file
 	check_contains "$(cat $scp_file)" hello
 }
 
 ssh_no_hostkey_check() {
-	ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "$@"
+	jump_host=${1}
+	ssh_port=17022
+
+	# Note the use of ProxyCommand instead of the simpler -J flag.
+	# Needed because -J issues another ssh process which doesn't
+	# inherit the arguements from the parent process. We need both ssh
+	# processes to ignore the host key check to ensure the process is not interactive.
+	proxy_command="ssh ${ssh_flags[*]} -p $ssh_port $jump_host -W %h:%p"
+	ssh "${ssh_flags[@]}" -o ProxyCommand="$proxy_command" "${@:2}"
 }
 
 scp_no_hostkey_check() {
-	scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "$@"
+	jump_host=${1}
+	ssh_port=17022
+
+	proxy_command="ssh ${ssh_flags[*]} -p $ssh_port $jump_host -W %h:%p"
+	scp "${ssh_flags[@]}" -o ProxyCommand="$proxy_command" "${@:2}"
 }
+
+ssh_flags=(
+	-o StrictHostKeyChecking=no
+	-o UserKnownHostsFile=/dev/null
+)
 
 test_ssh() {
 	if [ "$(skip 'test_ssh')" ]; then
