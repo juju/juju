407be0bf50cc56c1c256749644a968462d558f37
Guillaume Fouillet <guillaume.fouillet@canonical.com>
2025-05-20 11:17:37 +0200

    feat(uuidgen): add UUID generation and testing utilities
    This patch is to address the fact that we create a lot of UUID for a lot of things.
    It allows to one-line the creation of utility function for strong typed UUIDs for usage in code and
    in tests.
    
    Generation provides, for each UUID types generated:
    - method NewUUID,
    - method ParseUUID,
    - implementation for Stringer interface
    - Validate method which return "errors.NotValid" if not valid
    
    Also, in a subpackage "testing", it provides method for generating uuid in tests.

---
+++ generate/uuidgen/main.go
@@ -0,0 +1,41 @@
+// Copyright 2025 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 4 || (len(os.Args)-3)%2 != 0 {
+		fmt.Println("Usage: go run generate/uuidgen <output-file> <package-name> <type-name1> <description1> [<type-name2> <description2> ...]")
+		fmt.Println("Example: go run generate/uuidgen uuid.go relation UUID \"relation\" UnitUUID \"relation unit\"")
+		os.Exit(1)
+	}
+
+	fmt.Println(os.Args)
+
+	outputFile := os.Args[1]
+	packageName := os.Args[2]
+
+	// Parse type/description pairs
+	var types []UUIDType
+	for i := 3; i < len(os.Args); i += 2 {
+		if i+1 < len(os.Args) {
+			types = append(types, UUIDType{
+				TypeName:    os.Args[i],
+				Description: os.Args[i+1],
+			})
+		}
+	}
+
+	params := FileParams{
+		Package: packageName,
+		Types:   types,
+	}
+
+	generateTypeFile(outputFile, params)
+	generateTestGenFile(outputFile, params)
+}
