47066a8dd2b4964aed4c2822d65b63e3f7ea8e9f
Harry Pidcock <harry.pidcock@canonical.com>
2025-05-24 13:32:37 +1000

    test: fix externalcontrollerupdater flakiness

--- internal/worker/externalcontrollerupdater/package_test.go
+++ internal/worker/externalcontrollerupdater/package_test.go
@@ -3,4 +3,4 @@
 
 package externalcontrollerupdater_test
 
-//go:generate go run go.uber.org/mock/mockgen -package externalcontrollerupdater_test -destination package_mock_test.go github.com/juju/juju/internal/worker/externalcontrollerupdater ExternalControllerWatcherClientCloser,ExternalControllerUpdaterClient
+//go:generate go run go.uber.org/mock/mockgen -typed -package externalcontrollerupdater_test -destination package_mock_test.go github.com/juju/juju/internal/worker/externalcontrollerupdater ExternalControllerWatcherClientCloser,ExternalControllerUpdaterClient
