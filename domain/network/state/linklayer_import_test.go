aaeccc9b3ba3313fcce8b318344c9361b60b1e95
Guillaume Fouillet <guillaume.fouillet@canonical.com>
2025-05-27 11:09:27 +0200

    refactor(network): rename linklayer.go to linklayer_import.go to be able to restore link_layer.go
    Signed-off-by: Guillaume Fouillet <guillaume.fouillet@canonical.com>

--- domain/network/state/linklayer_import_test.go
+++ domain/network/state/linklayer_import_test.go
@@ -5,7 +5,6 @@ package state
 
 import (
 	"context"
-	"database/sql"
 	"testing"
 
 	"github.com/canonical/sqlair"
@@ -14,89 +13,8 @@ import (
 
 	corenetwork "github.com/juju/juju/core/network"
 	"github.com/juju/juju/domain/network/internal"
-	schematesting "github.com/juju/juju/domain/schema/testing"
 	"github.com/juju/juju/internal/uuid"
 )
-
-type linkLayerSuite struct {
-	schematesting.ModelSuite
-}
-
-func TestLinkLayerSuite(t *testing.T) {
-	tc.Run(t, &linkLayerSuite{})
-}
-
-func (s *linkLayerSuite) TestMachineInterfaceViewFitsType(c *tc.C) {
-	db, err := s.TxnRunnerFactory()()
-	c.Assert(err, tc.ErrorIsNil)
-
-	nodeUUID := "net-node-uuid"
-	machineUUID := "machine-uuid"
-	machineName := "0"
-	devUUID := "dev-uuid"
-	devName := "eth0"
-	subUUID := "sub-uuid"
-	addrUUID := "addr-uuid"
-
-	ctx := c.Context()
-
-	err = db.StdTxn(ctx, func(ctx context.Context, tx *sql.Tx) error {
-		if _, err := tx.ExecContext(ctx, "INSERT INTO net_node (uuid) VALUES (?)", nodeUUID); err != nil {
-			return err
-		}
-
-		if _, err := tx.ExecContext(ctx, "INSERT INTO machine (uuid, net_node_uuid, name, life_id) VALUES (?, ?, ? ,?)",
-			machineUUID, nodeUUID, machineName, 0,
-		); err != nil {
-			return err
-		}
-
-		insertLLD := `
-INSERT INTO link_layer_device (uuid, net_node_uuid, name, mtu, mac_address, device_type_id, virtual_port_type_id) 
-VALUES (?, ?, ?, ?, ?, ?, ?)`
-
-		if _, err = tx.ExecContext(ctx, insertLLD, devUUID, nodeUUID, devName, 1500, "00:11:22:33:44:55", 0, 0); err != nil {
-			return err
-		}
-
-		if _, err = tx.ExecContext(ctx, "INSERT INTO subnet (uuid, cidr, space_uuid) VALUES (?, ?, ?)",
-			subUUID, "10.0.0.0/24", corenetwork.AlphaSpaceId,
-		); err != nil {
-			return err
-		}
-
-		insertIPAddress := `
-INSERT INTO ip_address (uuid, device_uuid, address_value, type_id, scope_id, origin_id, config_type_id, subnet_uuid, net_node_uuid) 
-VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`
-
-		_, err = tx.ExecContext(ctx, insertIPAddress, addrUUID, devUUID, "10.0.0.1", 0, 0, 0, 0, subUUID, nodeUUID)
-		if err != nil {
-			return err
-		}
-
-		return nil
-	})
-	c.Assert(err, tc.ErrorIsNil)
-
-	stmt, err := sqlair.Prepare("SELECT &machineInterfaceRow.* FROM v_machine_interface", machineInterfaceRow{})
-	c.Assert(err, tc.ErrorIsNil)
-
-	var rows []machineInterfaceRow
-	err = db.Txn(ctx, func(ctx context.Context, txn *sqlair.TX) error {
-		return txn.Query(ctx, stmt).GetAll(&rows)
-	})
-	c.Assert(err, tc.ErrorIsNil)
-
-	c.Assert(rows, tc.HasLen, 1)
-
-	r := rows[0]
-	c.Check(r.MachineUUID, tc.Equals, machineUUID)
-	c.Check(r.MachineName, tc.Equals, machineName)
-	c.Check(r.DeviceUUID, tc.Equals, devUUID)
-	c.Check(r.DeviceName, tc.Equals, devName)
-	c.Check(r.AddressUUID.String, tc.Equals, addrUUID)
-	c.Check(r.SubnetUUID.String, tc.Equals, subUUID)
-}
 
 type linkLayerImportSuite struct {
 	linkLayerBaseSuite
@@ -160,7 +78,8 @@ func (s *linkLayerImportSuite) TestImportLinkLayerDevices(c *tc.C) {
 			Type:            corenetwork.EthernetDevice,
 			VirtualPortType: corenetwork.NonVirtualPort,
 			MachineID:       machineName,
-		}, { // This LLD should not be matched as the parent LLD of test.
+		}, {
+			// This LLD should not be matched as the parent LLD of test.
 			UUID:            uuid.MustNewUUID().String(),
 			NetNodeUUID:     netNodeUUID2,
 			Name:            "parent",
@@ -314,26 +233,29 @@ FROM provider_link_layer_device
 	return rows
 }
 
-func transformImportArgToResult(c *tc.C, importData []internal.ImportLinkLayerDevice) []linkLayerDeviceDML {
-	return transform.Slice[internal.ImportLinkLayerDevice, linkLayerDeviceDML](importData, func(in internal.ImportLinkLayerDevice) linkLayerDeviceDML {
-		typeID, err := encodeDeviceType(in.Type)
-		c.Check(err, tc.ErrorIsNil)
-		portTypeID, err := encodeVirtualPortType(in.VirtualPortType)
-		c.Check(err, tc.ErrorIsNil)
-		return linkLayerDeviceDML{
-			UUID:              in.UUID,
-			NetNodeUUID:       in.NetNodeUUID,
-			Name:              in.Name,
-			MTU:               in.MTU,
-			MACAddress:        in.MACAddress,
-			DeviceTypeID:      typeID,
-			VirtualPortTypeID: portTypeID,
-			IsAutoStart:       in.IsAutoStart,
-			IsEnabled:         in.IsEnabled,
-			IsDefaultGateway:  false,
-			GatewayAddress:    nil,
-			VlanTag:           0,
-		}
-	})
+func transformImportArgToResult(
+	c *tc.C, importData []internal.ImportLinkLayerDevice,
+) []linkLayerDeviceDML {
+	return transform.Slice[internal.ImportLinkLayerDevice, linkLayerDeviceDML](importData,
+		func(in internal.ImportLinkLayerDevice) linkLayerDeviceDML {
+			typeID, err := encodeDeviceType(in.Type)
+			c.Check(err, tc.ErrorIsNil)
+			portTypeID, err := encodeVirtualPortType(in.VirtualPortType)
+			c.Check(err, tc.ErrorIsNil)
+			return linkLayerDeviceDML{
+				UUID:              in.UUID,
+				NetNodeUUID:       in.NetNodeUUID,
+				Name:              in.Name,
+				MTU:               in.MTU,
+				MACAddress:        in.MACAddress,
+				DeviceTypeID:      typeID,
+				VirtualPortTypeID: portTypeID,
+				IsAutoStart:       in.IsAutoStart,
+				IsEnabled:         in.IsEnabled,
+				IsDefaultGateway:  false,
+				GatewayAddress:    nil,
+				VlanTag:           0,
+			}
+		})
 
 }
