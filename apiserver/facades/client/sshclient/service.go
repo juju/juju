de460581f7c9251d3595c96b4cf42b9952cc01f4
Jack Shaw <jack@jackshaw.me>
2025-06-02 16:28:16 +0100

    refactor(sshclient): sshclient facade now uses application service
    The sshclient facades used to use Mongo to get the assigned machine for
    a unit. Swap over to DQLite

--- apiserver/facades/client/sshclient/service.go
+++ apiserver/facades/client/sshclient/service.go
@@ -6,9 +6,29 @@ package sshclient
 import (
 	"context"
 
+	"github.com/juju/juju/core/machine"
+	"github.com/juju/juju/core/network"
+	"github.com/juju/juju/core/unit"
 	"github.com/juju/juju/environs/cloudspec"
 	"github.com/juju/juju/environs/config"
 )
+
+// NetworkService is the interface that is used to interact with the
+// network spaces/subnets.
+type NetworkService interface {
+	// GetAllSubnets returns all the subnets for the model.
+	GetAllSubnets(ctx context.Context) (network.SubnetInfos, error)
+}
+
+// ApplicationService is the interface that is used to interact with the
+// applications.
+type ApplicationService interface {
+	// GetUnitMachineName gets the name of the unit's machine.
+	//
+	// The following errors may be returned:
+	//   - [applicationerrors.UnitNotFound] if the unit cannot be found.
+	GetUnitMachineName(context.Context, unit.Name) (machine.Name, error)
+}
 
 // ModelConfigService is an interface that provides access to the
 // model configuration.
