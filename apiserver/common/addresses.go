9305bfe656f3277fb817b26e2bb4c57eb7fcceec
Heather Lanigan <heather.lanigan@canonical.com>
2025-06-04 16:20:21 -0400

    feat: move APIAddresser to use controller node domain methods
    APIAddresser now uses methods from the controller node domain. This
    means that APIHostPorts now returns significantly less detail and
    APIAddresses does not prioritize the list of addresses base on
    scope.
    
    The apiAddresses method is still used by two other common apiserver
    structures and should be transitioned over in the future. Limiting the
    scope of this change today.

--- apiserver/common/addresses.go
+++ apiserver/common/addresses.go
@@ -5,80 +5,123 @@ package common
 
 import (
 	"context"
+	"net/netip"
+
+	"github.com/juju/errors"
 
 	"github.com/juju/juju/apiserver/facade"
+	"github.com/juju/juju/apiserver/internal"
 	"github.com/juju/juju/controller"
 	"github.com/juju/juju/core/network"
+	"github.com/juju/juju/core/watcher"
 	"github.com/juju/juju/rpc/params"
-	"github.com/juju/juju/state"
-	"github.com/juju/juju/state/watcher"
 )
 
 // APIAddressAccessor describes methods that allow agents to maintain
 // up-to-date information on how to connect to the Juju API server.
 type APIAddressAccessor interface {
-	APIHostPortsForAgents(controller.Config) ([]network.SpaceHostPorts, error)
-	WatchAPIHostPortsForAgents() state.NotifyWatcher
+	// GetAllAPIAddressesForAgents returns a map of controller IDs to their API
+	// addresses that are available for agents. The map is keyed by controller
+	// ID, and the values are slices of strings representing the API addresses
+	// for each controller node.
+	GetAllAPIAddressesForAgents(ctx context.Context) (map[string][]string, error)
+	// WatchControllerAPIAddresses returns a watcher that observes changes to the
+	// controller ip addresses.
+	WatchControllerAPIAddresses(context.Context) (watcher.NotifyWatcher, error)
 }
 
 // APIAddresser implements the APIAddresses method.
-// Note that the getter backing for this implies that it is suitable for use by
-// agents, which are bound by the configured controller management space.
-// It is not suitable for callers requiring *all* available API addresses.
+// Note that the apiAddressAccessor backing for this implies that it is
+// suitable for use by agents, which are bound by the configured controller
+// management space. It is not suitable for callers requiring *all* available
+// API addresses.
 type APIAddresser struct {
-	resources facade.Resources
-	getter    APIAddressAccessor
+	apiAddressAccessor APIAddressAccessor
+	watcherRegistry    facade.WatcherRegistry
 }
 
-// NewAPIAddresser returns a new APIAddresser that uses the given getter to
+// NewAPIAddresser returns a new APIAddresser that uses the given apiAddressAccessor to
 // fetch its addresses.
-func NewAPIAddresser(getter APIAddressAccessor, resources facade.Resources) *APIAddresser {
+func NewAPIAddresser(getter APIAddressAccessor, watcherRegistry facade.WatcherRegistry) *APIAddresser {
 	return &APIAddresser{
-		getter:    getter,
-		resources: resources,
+		apiAddressAccessor: getter,
+		watcherRegistry:    watcherRegistry,
 	}
 }
 
 // APIHostPorts returns the API server addresses.
-func (a *APIAddresser) APIHostPorts(ctx context.Context, controllerConfig controller.Config) (params.APIHostPortsResult, error) {
-	sSvrs, err := a.getter.APIHostPortsForAgents(controllerConfig)
+func (a *APIAddresser) APIHostPorts(ctx context.Context) (params.APIHostPortsResult, error) {
+	srvs, err := a.apiAddressAccessor.GetAllAPIAddressesForAgents(ctx)
 	if err != nil {
 		return params.APIHostPortsResult{}, err
 	}
 
-	// Convert the SpaceHostPorts to the HostPorts indirection.
-	pSvrs := make([]network.HostPorts, len(sSvrs))
-	for i, sHPs := range sSvrs {
-		pSvrs[i] = sHPs.HostPorts()
+	// Convert the strings to the HostPorts.
+	serverResults := make([][]params.HostPort, 0)
+	for _, addrs := range srvs {
+		out, err := transformToHostPorts(addrs)
+		if err != nil {
+			return params.APIHostPortsResult{}, err
+		}
+		serverResults = append(serverResults, out)
 	}
 
 	return params.APIHostPortsResult{
-		Servers: params.FromHostsPorts(pSvrs),
+		Servers: serverResults,
 	}, nil
+}
+
+func transformToHostPorts(input []string) ([]params.HostPort, error) {
+	results := make([]params.HostPort, len(input))
+	for i, in := range input {
+		addrPort, err := netip.ParseAddrPort(in)
+		if err != nil {
+			return nil, err
+		}
+		results[i] = params.HostPort{
+			Address: params.Address{Value: addrPort.Addr().String()},
+			Port:    int(addrPort.Port()),
+		}
+	}
+	return results, nil
 }
 
 // WatchAPIHostPorts watches the API server addresses.
 func (a *APIAddresser) WatchAPIHostPorts(ctx context.Context) (params.NotifyWatchResult, error) {
-	watch := a.getter.WatchAPIHostPortsForAgents()
-	if _, ok := <-watch.Changes(); ok {
-		return params.NotifyWatchResult{
-			NotifyWatcherId: a.resources.Register(watch),
-		}, nil
+	var result params.NotifyWatchResult
+	notifyWatcher, err := a.apiAddressAccessor.WatchControllerAPIAddresses(ctx)
+	if err != nil {
+		return result, errors.Trace(err)
 	}
-	return params.NotifyWatchResult{}, watcher.EnsureErr(watch)
+	result.NotifyWatcherId, _, err = internal.EnsureRegisterWatcher(ctx, a.watcherRegistry, notifyWatcher)
+	return result, err
 }
 
 // APIAddresses returns the list of addresses used to connect to the API.
-func (a *APIAddresser) APIAddresses(ctx context.Context, controllerConfig controller.Config) (params.StringsResult, error) {
-	addrs, err := apiAddresses(controllerConfig, a.getter)
+//
+// Note:
+// The slice of addresses is not prioritized for cloud local addresses
+// at this time.
+func (a *APIAddresser) APIAddresses(ctx context.Context) (params.StringsResult, error) {
+	// TODO: 2025-Jun-04 hml
+	// Update to prioritize network.ScopeMatchCloudLocal addresses
+	// only.
+	srvs, err := a.apiAddressAccessor.GetAllAPIAddressesForAgents(ctx)
 	if err != nil {
 		return params.StringsResult{}, err
 	}
-	return params.StringsResult{
-		Result: addrs,
-	}, nil
+
+	result := make([]string, 0, len(srvs))
+	for _, addrs := range srvs {
+		result = append(result, addrs...)
+	}
+
+	return params.StringsResult{Result: result}, nil
 }
 
+// TODO - 2025-Jun-04 hml
+// Convert this method over to using the ControllerNode domain.
+// Previously used by APIAddresses too.
 func apiAddresses(controllerConfig controller.Config, getter APIHostPortsForAgentsGetter) ([]string, error) {
 	apiHostPorts, err := getter.APIHostPortsForAgents(controllerConfig)
 	if err != nil {
