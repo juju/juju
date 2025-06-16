6a177b1ac0076e68470d19ad059a0816332658d6
Thomas Miller <thomas.miller@canonical.com>
2025-06-16 16:30:07 +1000

    feat: create unit storage directives with unit
    This commit wires up support for the two state functions that exists on
    units to set the storage directives. This will now result in the unit
    storage directive table having been populated.

--- domain/application/state/unit.go
+++ domain/application/state/unit.go
@@ -1177,6 +1177,15 @@ func (st *State) insertCAASUnit(
 		return nil
 	}
 
+	_, err = st.createUnitStorageDirectives(
+		ctx, tx, unitUUID, charmUUID, args.StorageDirectives,
+	)
+	if err != nil {
+		return errors.Errorf(
+			"creating storage directives for unit %q: %w", args.UnitName, err,
+		)
+	}
+
 	attachArgs, err := st.insertUnitStorage(ctx, tx, appUUID, unitUUID, args.Storage, args.StoragePoolKind)
 	if err != nil {
 		return errors.Errorf("creating storage for unit %q: %w", args.UnitName, err)
@@ -1233,6 +1242,16 @@ func (st *State) insertIAASUnit(
 	}); err != nil {
 		return nil, errors.Errorf("inserting unit for application %q: %w", appUUID, err)
 	}
+
+	_, err = st.createUnitStorageDirectives(
+		ctx, tx, unitUUID, charmUUID, args.StorageDirectives,
+	)
+	if err != nil {
+		return nil, errors.Errorf(
+			"creating storage directives for unit %q: %w", args.UnitName, err,
+		)
+	}
+
 	if _, err := st.insertUnitStorage(ctx, tx, appUUID, unitUUID, args.Storage, args.StoragePoolKind); err != nil {
 		return nil, errors.Errorf("creating storage for unit %q: %w", args.UnitName, err)
 	}
