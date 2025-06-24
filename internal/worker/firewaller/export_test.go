73eac4ee6ce5c0172d1f1f2e8f99f0ad02265039
Adis Azhar <adis.azhar@canonical.com>
2025-06-24 15:51:46 +0700

    chore: delete unused export test

--- internal/worker/firewaller/export_test.go
+++
@@ -1,9 +0,0 @@
-package firewaller
-
-func FlushMachine(fw *Firewaller) error {
-	return fw.flushMachine(&machineData{})
-}
-
-func SetNeedsToFlushModel(fw *Firewaller, needsToFlushModel bool) {
-	fw.needsToFlushModel = needsToFlushModel
-}
