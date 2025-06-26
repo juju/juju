fb839e63195b0d938ce3298ccd3bc1de89348945
Simon Richardson <stickupkid@gmail.com>
2025-06-26 09:58:21 +0100

    feat: todo machine actions runner
    The machine actions runner expects to be able to get all the machine
    actions. Actions haven't been done yet, so for now, just have a TODO
    watcher.

--- apiserver/facades/agent/machineactions/machineactions.go
+++ apiserver/facades/agent/machineactions/machineactions.go
@@ -12,6 +12,8 @@ import (
 	"github.com/juju/juju/apiserver/common"
 	apiservererrors "github.com/juju/juju/apiserver/errors"
 	"github.com/juju/juju/apiserver/facade"
+	"github.com/juju/juju/apiserver/internal"
+	"github.com/juju/juju/core/watcher"
 	"github.com/juju/juju/rpc/params"
 	"github.com/juju/juju/state"
 )
@@ -26,24 +28,24 @@ type Backend interface {
 // Facade implements the machineactions interface and is the concrete
 // implementation of the api end point.
 type Facade struct {
-	backend       Backend
-	resources     facade.Resources
-	accessMachine common.AuthFunc
+	backend         Backend
+	watcherRegistry facade.WatcherRegistry
+	accessMachine   common.AuthFunc
 }
 
 // NewFacade creates a new server-side machineactions API end point.
 func NewFacade(
 	backend Backend,
-	resources facade.Resources,
+	watcherRegistry facade.WatcherRegistry,
 	authorizer facade.Authorizer,
 ) (*Facade, error) {
 	if !authorizer.AuthMachineAgent() {
 		return nil, apiservererrors.ErrPerm
 	}
 	return &Facade{
-		backend:       backend,
-		resources:     resources,
-		accessMachine: authorizer.AuthOwner,
+		backend:         backend,
+		watcherRegistry: watcherRegistry,
+		accessMachine:   authorizer.AuthOwner,
 	}, nil
 }
 
@@ -69,9 +71,24 @@ func (f *Facade) FinishActions(ctx context.Context, args params.ActionExecutionR
 // WatchActionNotifications returns a StringsWatcher for observing
 // incoming action calls to a machine.
 func (f *Facade) WatchActionNotifications(ctx context.Context, args params.Entities) params.StringsWatchResults {
-	tagToActionReceiver := f.backend.TagToActionReceiverFn(f.backend.FindEntity)
-	watchOne := common.WatchPendingActionsForReceiver(tagToActionReceiver, f.resources.Register)
-	return common.WatchActionNotifications(args, f.accessMachine, watchOne)
+	results := make([]params.StringsWatchResult, len(args.Entities))
+
+	for i := range args.Entities {
+		result := &results[i]
+
+		// We need a notify watcher for each item, otherwise during a migration
+		// a 3.x agent will bounce and will not be able to continue. By
+		// providing a watcher which does nothing, we can ensure that the 3.x
+		// agent will continue to work.
+		watcher := watcher.TODO[[]string]()
+		id, _, err := internal.EnsureRegisterWatcher(ctx, f.watcherRegistry, watcher)
+		if err != nil {
+			result.Error = apiservererrors.ServerError(err)
+			continue
+		}
+		result.StringsWatcherId = id
+	}
+	return params.StringsWatchResults{Results: results}
 }
 
 // RunningActions lists the actions running for the entities passed in.
