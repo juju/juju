e664bf06171c71f3720fc6b90b79a727b7dff98e
wallyworld <ian.booth@canonical.com>
2025-06-27 10:40:25 +1000

    chore: add backwards compatibility for model qualifiers Older Juju clients still expect to send and receive api params with model owner rather than model qualifier. Here we add the necessary compatibility shims.

---
+++ apiserver/facades/controller/migrationtarget/legacy.go
@@ -0,0 +1,33 @@
+// Copyright 2025 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package migrationtarget
+
+import (
+	"context"
+
+	"github.com/juju/names/v6"
+
+	coremodel "github.com/juju/juju/core/model"
+	"github.com/juju/juju/internal/errors"
+	"github.com/juju/juju/rpc/params"
+)
+
+// Prechecks ensure that the target controller is ready to accept a
+// model migration.
+func (api *APIV5) Prechecks(ctx context.Context, model params.MigrationModelInfoLegacy) error {
+	ownerTag, err := names.ParseUserTag(model.OwnerTag)
+	if err != nil {
+		return errors.Errorf("cannot parse model %q owner during prechecks: %w", model.UUID, err)
+	}
+	info := params.MigrationModelInfo{
+		UUID:                   model.UUID,
+		Name:                   model.Name,
+		Qualifier:              coremodel.QualifierFromUserTag(ownerTag).String(),
+		AgentVersion:           model.AgentVersion,
+		ControllerAgentVersion: model.ControllerAgentVersion,
+		FacadeVersions:         model.FacadeVersions,
+		ModelDescription:       model.ModelDescription,
+	}
+	return api.API.Prechecks(ctx, info)
+}
