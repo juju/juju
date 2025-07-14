e5c8dec78f55b684105654dcfdd062e2d3820eea
Thomas Miller <thomas.miller@canonical.com>
2025-07-14 12:17:00 +1000

    feat: introduce a new method for getting n sequences
    Allows the caller to get n sequence numbers out of the database instead
    of having to go back to the database n times. This will help build bulk
    storage inserts.

--- domain/sequence/state/sequence.go
+++ domain/sequence/state/sequence.go
@@ -6,6 +6,7 @@ package state
 import (
 	"context"
 	"database/sql"
+	"slices"
 
 	"github.com/canonical/sqlair"
 
@@ -14,7 +15,13 @@ import (
 	"github.com/juju/juju/internal/errors"
 )
 
-// NextValue returns a monotonically incrementing int value for the given namespace.
+type sequence struct {
+	Namespace string `db:"namespace"`
+	Value     uint64 `db:"value"`
+}
+
+// NextValue returns a monotonically incrementing int value for the given
+// namespace.
 // The first such value starts at 0.
 func NextValue(ctx context.Context, preparer domain.Preparer, tx *sqlair.TX, namespace domainsequence.Namespace) (uint64, error) {
 	seq := sequence{
@@ -57,7 +64,67 @@ SELECT &sequence.value FROM sequence WHERE namespace = $sequence.namespace
 	return result, nil
 }
 
-type sequence struct {
-	Namespace string `db:"namespace"`
-	Value     uint64 `db:"value"`
+// NextNValues returns the next n monotonically incrementing uint64 values for
+// the given namespace. The values returned by this function will never be
+// available for use again.
+//
+// If zero is supplied to this func no values will be created with no error
+// returned.
+func NextNValues(
+	ctx context.Context,
+	preparer domain.Preparer,
+	tx *sqlair.TX,
+	n uint64,
+	namespace domainsequence.Namespace,
+) ([]uint64, error) {
+	if n == 0 {
+		return nil, nil
+	}
+
+	seq := sequence{
+		Namespace: namespace.String(),
+		Value:     0,
+	}
+
+	updateStmt, err := preparer.Prepare(`
+INSERT INTO sequence (*) VALUES ($sequence.*)
+ON CONFLICT DO UPDATE SET value = value + 1
+`, sequence{})
+	if err != nil {
+		return nil, errors.Capture(err)
+	}
+
+	latestValStmt, err := preparer.Prepare(`
+SELECT &sequence.value FROM sequence WHERE namespace = $sequence.namespace
+`, seq)
+	if err != nil {
+		return nil, errors.Capture(err)
+	}
+
+	updateVals := slices.Repeat([]sequence{seq}, int(n))
+
+	// Increment the sequence number, we need to ensure that we affected the
+	// sequence, otherwise we can end up with values that aren't unique.
+	var outcome sqlair.Outcome
+	err = tx.Query(ctx, updateStmt, updateVals).Get(&outcome)
+	if err != nil {
+		return nil, errors.Errorf("updating sequence number for namespace %q: %w", namespace, err)
+	}
+	if affected, err := outcome.Result().RowsAffected(); err != nil {
+		return nil, errors.Errorf("getting affected rows for sequence number for namespace %q: %w", namespace, err)
+	} else if affected != int64(n) {
+		return nil, errors.Errorf("updating sequence number for namespace %q", namespace)
+	}
+
+	// Read the new value to return.
+	err = tx.Query(ctx, latestValStmt, seq).Get(&seq)
+	if err != nil && !errors.Is(err, sql.ErrNoRows) {
+		return nil, errors.Errorf("reading sequence number for namespace %q: %w", namespace, err)
+	}
+
+	rval := make([]uint64, 0, n)
+	for i := seq.Value - n + 1; i <= seq.Value; i++ {
+		rval = append(rval, i)
+	}
+	return rval, nil
 }
