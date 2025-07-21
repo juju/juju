e3f2c5402b0a4e96a801113fe47fff1343976caa
Joseph Phillips <joe.phillips@canonical.com>
2025-07-21 17:54:38 +0200

    feat: delete machine devices and addresses not observed
    For addresses not present in observed machine network config, we delete
    them if they have `machine` as the origin.
    
    For devices not present, we delete them if they have no addresses and no
    associated provider ID.

--- domain/network/state/netconfig.go
+++ domain/network/state/netconfig.go
@@ -8,6 +8,7 @@ import (
 	"net"
 
 	"github.com/canonical/sqlair"
+	"github.com/juju/collections/set"
 	"github.com/juju/collections/transform"
 
 	corenetwork "github.com/juju/juju/core/network"
@@ -40,15 +41,13 @@ func (st *State) SetMachineNetConfig(ctx context.Context, nodeUUID string, nics 
 	}
 
 	err = db.Txn(ctx, func(ctx context.Context, tx *sqlair.TX) error {
-		// Determine the type name to ID conversions as defined
-		// in the database.
-		namesToIDs, err := st.getNetConfigLookups(ctx, tx)
+		lookups, err := st.getNetConfigLookups(ctx, tx)
 		if err != nil {
 			return errors.Capture(err)
 		}
 
 		newNics, dnsDoms, dnsAddrs, parents, nicNameToUUID, err :=
-			st.reconcileNetConfigDevices(ctx, tx, nodeUUID, nics, namesToIDs)
+			st.reconcileNetConfigDevices(ctx, tx, nodeUUID, nics, lookups)
 		if err != nil {
 			return errors.Errorf("reconciling incoming network devices: %w", err)
 		}
@@ -58,20 +57,8 @@ func (st *State) SetMachineNetConfig(ctx context.Context, nodeUUID string, nics 
 		// I.e. those inserted and updated.
 		retainedDeviceUUIDs := transform.MapToSlice(nicNameToUUID, func(k, v string) []string { return []string{v} })
 
-		if err = st.upsertLinkLayerDevices(ctx, tx, newNics); err != nil {
-			return errors.Errorf("inserting link layer devices: %w", err)
-		}
-
-		if err = st.updateDNSSearchDomains(ctx, tx, dnsDoms, retainedDeviceUUIDs); err != nil {
-			return errors.Errorf("updating DNS search domains: %w", err)
-		}
-
-		if err = st.updateDNSAddresses(ctx, tx, dnsAddrs, retainedDeviceUUIDs); err != nil {
-			return errors.Errorf("updating DNS addresses: %w", err)
-		}
-
-		if err = st.updateDeviceParents(ctx, tx, parents, retainedDeviceUUIDs); err != nil {
-			return errors.Errorf("inserting device parents: %w", err)
+		if err := st.updateNetworkDevices(ctx, tx, newNics, dnsDoms, dnsAddrs, parents, retainedDeviceUUIDs); err != nil {
+			return errors.Capture(err)
 		}
 
 		subs, err := st.getSubnetGroups(ctx, tx)
@@ -81,21 +68,27 @@ func (st *State) SetMachineNetConfig(ctx context.Context, nodeUUID string, nics 
 		st.logger.Debugf(ctx, "matching with subnet groups: %#v", subs)
 
 		addrsToInsert, newSubs, err :=
-			st.reconcileNetConfigAddresses(ctx, tx, nodeUUID, nics, nicNameToUUID, subs, namesToIDs)
+			st.reconcileNetConfigAddresses(ctx, tx, nodeUUID, nics, nicNameToUUID, subs, lookups)
 		if err != nil {
 			return errors.Errorf("reconciling incoming ip addresses: %w", err)
 		}
 
-		if len(addrsToInsert) == 0 {
-			return nil
+		if len(addrsToInsert) != 0 {
+			if err := st.insertSubnets(ctx, tx, newSubs); err != nil {
+				return errors.Errorf("inserting subnets: %w", err)
+			}
+
+			if err = st.upsertIPAddresses(ctx, tx, addrsToInsert); err != nil {
+				return errors.Errorf("inserting IP addresses: %w", err)
+			}
 		}
 
-		if err := st.insertSubnets(ctx, tx, newSubs); err != nil {
-			return errors.Errorf("inserting subnets: %w", err)
+		if err := st.deleteAddressesNotObserved(ctx, tx, nodeUUID, addrsToInsert); err != nil {
+			return errors.Errorf("deleting addresses: %w", err)
 		}
 
-		if err = st.upsertIPAddresses(ctx, tx, addrsToInsert); err != nil {
-			return errors.Errorf("inserting IP addresses: %w", err)
+		if err := st.deleteDevicesNotObserved(ctx, tx, nodeUUID, retainedDeviceUUIDs); err != nil {
+			return errors.Errorf("deleting devices: %w", err)
 		}
 
 		return nil
@@ -157,13 +150,46 @@ func (st *State) reconcileNetConfigDevices(
 				DeviceUUID: nameToUUID[n.Name],
 				ParentUUID: parentUUID,
 			})
+		} else if parentUUID, ok := existing[n.ParentDeviceName]; ok {
+			parentDMLs = append(parentDMLs, linkLayerDeviceParent{
+				DeviceUUID: nameToUUID[n.Name],
+				ParentUUID: parentUUID,
+			})
 		} else {
-			st.logger.Warningf(context.TODO(), "parent device %q for %q not found in incoming data",
+			st.logger.Warningf(context.TODO(), "parent device %q for %q not found in incoming or existing data",
 				n.ParentDeviceName, n.Name)
 		}
 	}
 
 	return nicsDML, dnsSearchDMLs, dnsAddrDMLs, parentDMLs, nameToUUID, nil
+}
+
+func (st *State) updateNetworkDevices(
+	ctx context.Context,
+	tx *sqlair.TX,
+	newNics []linkLayerDeviceDML,
+	dnsDoms []dnsSearchDomainRow,
+	dnsAddrs []dnsAddressRow,
+	parents []linkLayerDeviceParent,
+	retainedDeviceUUIDs []string,
+) error {
+	if err := st.upsertLinkLayerDevices(ctx, tx, newNics); err != nil {
+		return errors.Errorf("inserting link layer devices: %w", err)
+	}
+
+	if err := st.updateDNSSearchDomains(ctx, tx, dnsDoms, retainedDeviceUUIDs); err != nil {
+		return errors.Errorf("updating DNS search domains: %w", err)
+	}
+
+	if err := st.updateDNSAddresses(ctx, tx, dnsAddrs, retainedDeviceUUIDs); err != nil {
+		return errors.Errorf("updating DNS addresses: %w", err)
+	}
+
+	if err := st.updateDeviceParents(ctx, tx, parents, retainedDeviceUUIDs); err != nil {
+		return errors.Errorf("inserting device parents: %w", err)
+	}
+
+	return nil
 }
 
 // updateDNSSearchDomains replaces all the data in link_layer_device_dns_domain
@@ -474,6 +500,124 @@ ON CONFLICT (uuid) DO UPDATE SET
 	err = tx.Query(ctx, stmt, addrs).Run()
 	if err != nil {
 		return errors.Errorf("running address insert statement: %w", err)
+	}
+
+	return nil
+}
+
+// deleteAddressesNotObserved deletes addresses under the remit
+// of the machine that were not observed in the incoming data.
+func (st *State) deleteAddressesNotObserved(
+	ctx context.Context, tx *sqlair.TX, nodeUUID string, retainedAddresses []ipAddressDML,
+) error {
+	nUUID := entityUUID{UUID: nodeUUID}
+	retainedAddressUUIDs := uuids(transform.Slice(retainedAddresses, func(a ipAddressDML) string { return a.UUID }))
+
+	addrDML := `
+DELETE FROM ip_address 
+WHERE  net_node_uuid = $entityUUID.uuid
+AND    uuid NOT IN ($uuids[:])
+AND    origin_id = 0`
+
+	addrStmt, err := st.Prepare(addrDML, nUUID, retainedAddressUUIDs)
+	if err != nil {
+		return errors.Errorf("preparing address deletion statement: %w", err)
+	}
+
+	if err := tx.Query(ctx, addrStmt, nUUID, retainedAddressUUIDs).Run(); err != nil {
+		return errors.Errorf("running address deletion statement: %w", err)
+	}
+
+	return nil
+}
+
+// deleteDevicesNotObserved deletes devices not observed in the incoming data,
+// provided they have no addresses and no provider ID, and are not in
+// parent-child relationships that would result in orphans.
+func (st *State) deleteDevicesNotObserved(
+	ctx context.Context, tx *sqlair.TX, nodeUUID string, retainedDeviceUUIDs []string,
+) error {
+	nUUID := entityUUID{UUID: nodeUUID}
+	retainedDevices := uuids(retainedDeviceUUIDs)
+
+	devSQL := `
+SELECT d.uuid AS &entityUUID.uuid
+FROM   link_layer_device d
+       LEFT JOIN provider_link_layer_device p ON d.uuid = p.device_uuid
+       LEFT JOIN ip_address a ON d.uuid = a.device_uuid
+WHERE  d.net_node_uuid = $entityUUID.uuid
+AND    p.device_uuid IS NULL
+AND    a.device_uuid IS NULL
+AND    d.uuid NOT IN ($uuids[:])`
+
+	devStmt, err := st.Prepare(devSQL, nUUID, retainedDevices)
+	if err != nil {
+		return errors.Errorf("preparing devices for deletion statement: %w", err)
+	}
+
+	var devsToDelete []entityUUID
+	if err := tx.Query(ctx, devStmt, nUUID, retainedDevices).GetAll(&devsToDelete); err != nil {
+		if !errors.Is(err, sqlair.ErrNoRows) {
+			return errors.Errorf("running devices for deletion query: %w", err)
+		}
+	}
+
+	if len(devsToDelete) == 0 {
+		return nil
+	}
+
+	deletionCandidates := transform.Slice(devsToDelete, func(d entityUUID) string { return d.UUID })
+
+	// We need to ensure that we delete parent devices only if:
+	// - they have no children or;
+	// - their child devices are also being deleted.
+	parentSQL := `
+SELECT &linkLayerDeviceParent.*
+FROM   link_layer_device_parent
+WHERE  parent_uuid IN ($uuids[:])`
+
+	parentStmt, err := st.Prepare(parentSQL, linkLayerDeviceParent{}, uuids(deletionCandidates))
+	if err != nil {
+		return errors.Errorf("preparing child devices statement: %w", err)
+	}
+
+	var parents []linkLayerDeviceParent
+	if err := tx.Query(ctx, parentStmt, uuids(deletionCandidates)).GetAll(&parents); err != nil {
+		if !errors.Is(err, sqlair.ErrNoRows) {
+			return errors.Errorf("running child devices query: %w", err)
+		}
+	}
+
+	deletions := set.NewStrings(deletionCandidates...)
+	for _, p := range parents {
+		if !deletions.Contains(p.DeviceUUID) {
+			st.logger.Debugf(ctx, "not removing device %q as it is a parent of existing device %q",
+				p.ParentUUID, p.DeviceUUID)
+
+			deletions.Remove(p.ParentUUID)
+		}
+	}
+
+	if len(deletions) == 0 {
+		return nil
+	}
+
+	delArg := uuids(deletions.Values())
+	for _, delSQL := range []string{
+		"DELETE FROM link_layer_device_parent WHERE device_uuid IN ($uuids[:])",
+		"DELETE FROM link_layer_device_parent WHERE parent_uuid IN ($uuids[:])",
+		"DELETE FROM link_layer_device_dns_domain WHERE device_uuid IN ($uuids[:])",
+		"DELETE FROM link_layer_device_dns_address WHERE device_uuid IN ($uuids[:])",
+		"DELETE FROM link_layer_device WHERE uuid IN ($uuids[:])",
+	} {
+		delStmt, err := st.Prepare(delSQL, delArg)
+		if err != nil {
+			return errors.Errorf("preparing device deletion statement: %w", err)
+		}
+
+		if err := tx.Query(ctx, delStmt, delArg).Run(); err != nil {
+			return errors.Errorf("running device deletion statement: %w", err)
+		}
 	}
 
 	return nil
