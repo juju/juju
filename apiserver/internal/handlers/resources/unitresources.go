e31e6cd9529865a54c02625428d5b88f9971ebb8
Harry Pidcock <harry.pidcock@canonical.com>
2025-07-23 09:44:57 +1000

    refactor: simplify resource opener

--- apiserver/internal/handlers/resources/unitresources.go
+++ apiserver/internal/handlers/resources/unitresources.go
@@ -94,7 +94,7 @@ func (h *UnitResourcesHandler) serveGet(resp http.ResponseWriter, req *http.Requ
 	}
 
 	// Mark the downloaded resource as in use on the unit.
-	err = opener.SetResourceUsed(req.Context(), name)
+	err = opener.SetResourceUsed(req.Context(), opened.UUID)
 	if err != nil {
 		h.logger.Warningf(req.Context(), "setting resource %s as in use: %w", name, err)
 	}
