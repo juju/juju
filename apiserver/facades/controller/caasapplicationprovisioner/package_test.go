fd74b5e47a1816b0890d1b96bb7871c4dd77e32c
Harry Pidcock <harry.pidcock@canonical.com>
2025-07-24 16:52:05 +1000

    chore: cleanup unused mocks in caasapplicationprovisioner facade

--- apiserver/facades/controller/caasapplicationprovisioner/package_test.go
+++ apiserver/facades/controller/caasapplicationprovisioner/package_test.go
@@ -4,5 +4,3 @@
 package caasapplicationprovisioner_test
 
 //go:generate go run go.uber.org/mock/mockgen -typed -package caasapplicationprovisioner_test -destination service_mock_test.go github.com/juju/juju/apiserver/facades/controller/caasapplicationprovisioner ControllerConfigService,ModelConfigService,ModelInfoService,ApplicationService,StatusService,ControllerNodeService,RemovalService
-//go:generate go run go.uber.org/mock/mockgen -typed -package caasapplicationprovisioner_test -destination resource_opener_mock_test.go github.com/juju/juju/core/resource Opener
-//go:generate go run go.uber.org/mock/mockgen -typed -package caasapplicationprovisioner_test -destination objectstore_mock_test.go github.com/juju/juju/core/objectstore ObjectStore
