7cb97303b070f505da9d946a49e395d65572c87c
Simon Richardson <stickupkid@gmail.com>
2025-07-29 21:59:10 +0100

    chore: remove dead code

--- domain/removal/state/controller/types.go
+++ domain/removal/state/controller/types.go
@@ -5,26 +5,10 @@ package controller
 
 import "github.com/juju/juju/domain/life"
 
-// entityUUIDs is a slice of entityUUID, used to hold multiple UUIDs.
-type uuids []string
-
 // entityUUID holds a UUID in string form.
 type entityUUID struct {
 	// UUID uniquely identifies a domain entity.
 	UUID string `db:"uuid"`
-}
-
-// entityAssociationCount holds a Count in int form and the UUID in string form
-// for the associated entity.
-type entityAssociationCount struct {
-	// UUID uniquely identifies a associated domain entity.
-	UUID string `db:"uuid"`
-	// Count counts the number of entities.
-	Count int `db:"count"`
-}
-
-type count struct {
-	Count int `db:"count"`
 }
 
 // entityLife holds an entity's life in integer
