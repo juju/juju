43da93d7068d70682d35f4e6d24316f483dbd9a4
Simon Richardson <stickupkid@gmail.com>
2025-07-29 11:47:15 +0100

    chore: remove unused mocks

--- domain/model/service/objectstoreservice_test.go
+++ domain/model/service/objectstoreservice_test.go
@@ -30,9 +30,6 @@ type objectStoreServiceSuite struct {
 	testhelpers.IsolationSuite
 
 	state *dummyObjectStoreState
-
-	mockControllerState *MockState
-	mockWatcherFactory  *MockWatcherFactory
 }
 
 func TestObjectStoreServiceSuite(t *testing.T) {
