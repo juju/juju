c0f1de86a3b57d6e05bbfce741eac5a00f06c191
jujubot <juju-qa-aws@canonical.com>
2025-08-05 08:01:43 +0000

    chore: increment juju to 4.0-beta7

--- core/version/version.go
+++ core/version/version.go
@@ -19,7 +19,7 @@ import (
 // The presence and format of this constant is very important.
 // The debian/rules build recipe uses this value for the version
 // number of the release package.
-const version = "4.0-beta6"
+const version = "4.0-beta7"
 
 // UserAgentVersion defines a user agent version used for communication for
 // outside resources.
