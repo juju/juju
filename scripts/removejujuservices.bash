49cbd6946da3fdcc04e0ba27d30707368f7a9ead
Simon Richardson <stickupkid@gmail.com>
2025-08-07 12:43:54 +0100

    feat: cleanup makefile
    The follow items in the Makefile are used or required or are obsolete.
    
    We'll need to sweep around to create a sanitise-db.py, but we shouldn't
    lie to ourselves thinking we have one, when we don't.

--- scripts/removejujuservices.bash
+++ scripts/removejujuservices.bash
@@ -16,20 +16,11 @@ done
 echo "removing /var/lib/juju/tools/*"
 rm -rf /var/lib/juju/tools/*
 
-echo "removing /var/lib/juju/db/*"
-rm -rf /var/lib/juju/db/*
-
 echo "removing /var/lib/juju/dqlite/*"
 rm -rf /var/lib/juju/dqlite/*
 
-echo "removing /var/lib/juju/raft/*"
-rm -rf /var/lib/juju/raft/*
+echo "removing /var/lib/juju/objecstore/*"
+rm -rf /var/lib/juju/objecstore/*
 
 echo "removing /var/run/juju/*"
 rm -rf /var/run/juju/*
-
-has_juju_db_snap=$(snap info juju-db | grep installed:)
-if [ ! -z "$has_juju_db_snap" ]; then
-  echo "removing juju-db snap and any persisted database data"
-  snap remove --purge juju-db
-fi
