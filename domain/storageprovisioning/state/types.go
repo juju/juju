b06a1ea7d4af4dc51929d18122008b6059ad0542
Harry Pidcock <harry.pidcock@canonical.com>
2025-08-25 21:08:02 +1000

    feat: implement volume attachment params in storageprovisioning state

--- domain/storageprovisioning/state/types.go
+++ domain/storageprovisioning/state/types.go
@@ -239,6 +239,15 @@ type volumeParams struct {
 	RequestedSizeMiB uint64 `db:"requested_size_mib"`
 }
 
+// volumeAttachmentParams represents the attachment params for a volume
+// attachment from the model database.
+type volumeAttachmentParams struct {
+	Type       string `db:"type"`
+	InstanceID string `db:"instance_id"`
+	ProviderID string `db:"provider_id"`
+	ReadOnly   bool   `db:"read_only"`
+}
+
 // storageNameAttributes represents each key/value attribute for a given storage
 // derived from the provider/pool used to provisioner the storage.
 type storageNameAttributes struct {
