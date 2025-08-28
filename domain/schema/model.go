1ad9947834655f9b6751004bb2596954ef85ac11
Kelvin Liu <kelvin.liu@canonical.com>
2025-08-28 19:17:35 +1000

    feat: add custom triggers for storage attachment related entity changes;

--- domain/schema/model.go
+++ domain/schema/model.go
@@ -47,6 +47,7 @@ const (
 	customNamespaceApplicationRemovalLifecycle
 	customNamespaceRelationRemovalLifecycle
 	customNamespaceModelLifeRemovalLifecycle
+	customNamespaceStorageAttachmentRelatedEntities
 )
 
 const (
