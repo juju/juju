9bdc7a44c730a315974ad09c9b9cd2ad1175cdc7
Adis Azhar <adis.azhar@canonical.com>
2025-08-29 11:21:44 +0700

    feat: remove meterstatus client

--- api/agent/meterstatus/doc.go
+++
@@ -1,6 +0,0 @@
-// Copyright 2022 Canonical Ltd.
-// Licensed under the AGPLv3, see LICENCE file for details.
-
-// Package meterstatus contains an implementation of the API facade to
-// watch the meter status of a unit for changes and return the current meter status.
-package meterstatus
