f7ffd5d8151769b87fc73f0934d0ef9b9ee90975
Joseph Phillips <joe.phillips@canonical.com>
2025-09-04 11:28:06 +0200

    feat: struct for passing related dying entities for units
    This will be used to pass cascaded life advancements back to the service
    layer.

---
+++ domain/removal/internal/types.go
@@ -0,0 +1,17 @@
+// Copyright 2025 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package internal
+
+// CascadedUnitLives contains identifiers for entities that were ensured to be
+// "dying" along with a unit. It is intended to inform the service layer which
+// entities should have removal jobs scheduled for them.
+type CascadedUnitLives struct {
+	// MachineUUID if not nil indicates that the unit's
+	// machine is no longer alive.
+	MachineUUID *string
+
+	// StorageInstanceUUIDs identify any of the unit's storage
+	// instances that are not longer alive.
+	StorageInstanceUUIDs []string
+}
