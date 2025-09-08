61b01f70826812682b14c9b57ee12f0355f9854f
Guillaume Fouillet <guillaume.fouillet@canonical.com>
2025-09-08 18:59:12 +0200

    refactor(apiserver/action): consolidate `get_operation.go` and `run.go` into `operation.go`
    Reorganize files by merging logic from `get_operation.go` and `run.go` into `operation.go` for
    better cohesiveness within the `ActionAPI` facade.
    
    Tests have been kept separated to avoid too big files.

--- apiserver/facades/client/action/operation.go
+++ apiserver/facades/client/action/operation.go
@@ -15,6 +15,7 @@ import (
 	coreerrors "github.com/juju/juju/core/errors"
 	"github.com/juju/juju/core/model"
 	coreoperation "github.com/juju/juju/core/operation"
+	corestatus "github.com/juju/juju/core/status"
 	"github.com/juju/juju/core/unit"
 	"github.com/juju/juju/domain/operation"
 	"github.com/juju/juju/internal/errors"
@@ -132,6 +133,57 @@ func (h *taskParamHolder) Set(arg params.Action) error {
 
 func (h *taskParamHolder) Get() operation.TaskArgs {
 	return *h.args
+}
+
+// ListOperations fetches the called operations for specified apps/units.
+func (a *ActionAPI) ListOperations(ctx context.Context, arg params.OperationQueryArgs) (params.OperationResults, error) {
+	if err := a.checkCanRead(ctx); err != nil {
+		return params.OperationResults{}, errors.Capture(err)
+	}
+
+	var status []corestatus.Status
+	if arg.Status != nil {
+		status = transform.Slice(arg.Status, func(f string) corestatus.Status {
+			return corestatus.Status(f)
+		})
+	}
+	args := operation.QueryArgs{
+		Receivers:   makeOperationReceivers(arg.Applications, arg.Machines, arg.Units),
+		ActionNames: arg.ActionNames,
+		Status:      status,
+		Limit:       arg.Limit,
+		Offset:      arg.Offset,
+	}
+	result, err := a.operationService.GetOperations(ctx, args)
+	if err != nil {
+		return params.OperationResults{}, errors.Capture(err)
+	}
+
+	return toOperationResults(result), errors.Capture(err)
+}
+
+// Operations fetches the specified operation ids.
+func (a *ActionAPI) Operations(ctx context.Context, arg params.Entities) (params.OperationResults, error) {
+	if err := a.checkCanRead(ctx); err != nil {
+		return params.OperationResults{}, errors.Capture(err)
+	}
+
+	results := params.OperationResults{Results: make([]params.OperationResult, len(arg.Entities))}
+
+	for i, entity := range arg.Entities {
+		operationTag, err := names.ParseOperationTag(entity.Tag)
+		if err != nil {
+			results.Results[i].Error = apiservererrors.ServerError(err)
+			continue
+		}
+		result, err := a.operationService.GetOperationByID(ctx, operationTag.Id())
+		if err != nil {
+			results.Results[i].Error = apiservererrors.ServerError(err)
+			continue
+		}
+		results.Results[i] = toOperationResult(result)
+	}
+	return results, nil
 }
 
 // Run the commands specified on the machines identified through the
