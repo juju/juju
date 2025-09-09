eb21f9c52d103c007efc97b368e7ffe4793d3223
Harry Pidcock <harry.pidcock@canonical.com>
2025-09-09 13:14:40 +1000

    refactor: drop fs type in block device ddl

--- domain/schema/model/sql/0010-blockdevice.sql
+++ domain/schema/model/sql/0010-blockdevice.sql
@@ -2,48 +2,24 @@ CREATE TABLE block_device (
     uuid TEXT NOT NULL PRIMARY KEY,
     machine_uuid TEXT NOT NULL,
     name TEXT,
-    label TEXT,
-    device_uuid TEXT,
     hardware_id TEXT,
     wwn TEXT,
-    bus_address TEXT,
     serial_id TEXT,
-    filesystem_type_id INT,
+    bus_address TEXT,
     size_mib INT,
     mount_point TEXT,
     in_use BOOLEAN,
-    CONSTRAINT fk_filesystem_type
-    FOREIGN KEY (filesystem_type_id)
-    REFERENCES filesystem_type (id),
+    filesystem_label TEXT,
+    filesystem_uuid TEXT, -- not a key, value from the partition table
+    filesystem_type TEXT,
     CONSTRAINT fk_block_device_machine
     FOREIGN KEY (machine_uuid)
     REFERENCES machine (uuid)
 );
 
+-- name can be NULL. In Sqlite all NULLs are distinct.
 CREATE UNIQUE INDEX idx_block_device_name
 ON block_device (machine_uuid, name);
-
-CREATE UNIQUE INDEX idx_block_device_wwn
-ON block_device (machine_uuid, wwn);
-
-CREATE TABLE filesystem_type (
-    id INT PRIMARY KEY,
-    name TEXT NOT NULL
-);
-
-CREATE UNIQUE INDEX idx_filesystem_type_name
-ON filesystem_type (name);
-
-INSERT INTO filesystem_type VALUES
-(0, 'unspecified'),
-(1, 'vfat'),
-(2, 'ext4'),
-(3, 'xfs'),
-(4, 'btrfs'),
-(5, 'zfs'),
-(6, 'jfs'),
-(7, 'squashfs'),
-(8, 'bcachefs');
 
 CREATE TABLE block_device_link_device (
     block_device_uuid TEXT NOT NULL,
