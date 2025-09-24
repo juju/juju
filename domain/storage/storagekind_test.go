afa704c78934c8687765b28dc6c66540c69b4469
Thomas Miller <thomas.miller@canonical.com>
2025-09-24 10:38:56 +1000

    refactor: storage kind in storage domain
    I made the mistake of introducing another storage kind enum into the
    storage provisioning domain when one already existed in the storage
    domain.
    
    This change fixes up the one in the storage domain so that it is better
    aligned to it's purpose.
    
    A follow PR will remove the storageprovisioning domain one.
    
    Signed-off-by: Thomas Miller <thomas.miller@canonical.com>

--- domain/storage/storagekind_test.go
+++ domain/storage/storagekind_test.go
@@ -11,34 +11,46 @@ import (
 	schematesting "github.com/juju/juju/domain/schema/testing"
 )
 
+// storageKindSuite is a test suite for asserting assumptions about
+// [StorageKind] and making sure that it is aligned with the Juju model
+// database.
 type storageKindSuite struct {
 	schematesting.ModelSuite
 }
 
+// TestStorageKindSuite runs all of the tests in the [storageKindSuite].
 func TestStorageKindSuite(t *testing.T) {
 	tc.Run(t, &storageKindSuite{})
 }
 
-// TestStorageKindDBValues ensures there's no skew between what's in the
-// database table for charm_storage_kind and the typed consts used in the state packages.
-func (s *storageKindSuite) TestStorageKindDBValues(c *tc.C) {
-	db := s.DB()
-	rows, err := db.Query("SELECT id, kind FROM charm_storage_kind")
+// TestStorageKindValuesAlignedToDB asserts that the storage kind values that
+// exist in the database schema align with the enum values defined in this
+// package.
+//
+// If this test fails it indicates that either a new value has been added to the
+// schema and a new enum needs to be created or a value has been modified or
+// removed that will result in a breaking change.
+func (s *storageKindSuite) TestStorageKindValuesAlignedToDB(c *tc.C) {
+	rows, err := s.DB().QueryContext(
+		c.Context(),
+		"SELECT id, kind FROM storage_kind",
+	)
 	c.Assert(err, tc.ErrorIsNil)
 	defer func() { _ = rows.Close() }()
 
-	dbValues := make(map[StorageKind]string)
+	dbValues := map[StorageKind]string{}
 	for rows.Next() {
 		var (
-			id    int
-			value string
+			id   int
+			kind string
 		)
 
-		c.Assert(rows.Scan(&id, &value), tc.ErrorIsNil)
-		dbValues[StorageKind(id)] = value
+		c.Assert(rows.Scan(&id, &kind), tc.ErrorIsNil)
+		dbValues[StorageKind(id)] = kind
 	}
-	c.Assert(dbValues, tc.DeepEquals, map[StorageKind]string{
-		StorageKindFilesystem: "filesystem",
+
+	c.Check(dbValues, tc.DeepEquals, map[StorageKind]string{
 		StorageKindBlock:      "block",
+		StorageKindFilesystem: "filesystem",
 	})
 }
