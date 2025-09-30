b3e7948049a8ec1c224b6ddad0a26caf1278bd95
Simon Richardson <stickupkid@gmail.com>
2025-09-30 10:18:16 +0100

    feat: add owned worker for dependency engine
    The owned worker allows the dependency engine to take ownership of
    workers that might end up dangling and not be stopped when the engine
    bounces. Introduction of this ensures that the worker won't leak.

--- core/flightrecorder/flightrecorder.go
+++ core/flightrecorder/flightrecorder.go
@@ -1,7 +1,11 @@
 package flightrecorder
 
+import "github.com/juju/worker/v4"
+
 // FlightRecorder is the interface for controlling the flight recorder.
 type FlightRecorder interface {
+	worker.Worker
+
 	// Start starts the flight recorder.
 	Start() error
 
