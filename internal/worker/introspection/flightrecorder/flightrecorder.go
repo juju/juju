3f99381755b95fe568e727a4d70b62632864995e
Simon Richardson <stickupkid@gmail.com>
2025-10-10 17:58:14 +0100

    feat: allow different capture kinds
    This allows us to run captures with different types of kinds

--- internal/worker/introspection/flightrecorder/flightrecorder.go
+++ internal/worker/introspection/flightrecorder/flightrecorder.go
@@ -4,6 +4,7 @@
 package flightrecorder
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/juju/juju/core/flightrecorder"
@@ -12,7 +13,12 @@ import (
 // StartHandler returns an http handler for starting the flight recorder.
 func StartHandler(w flightrecorder.FlightRecorder) http.HandlerFunc {
 	return func(wr http.ResponseWriter, req *http.Request) {
-		if err := w.Start(); err != nil {
+		kind, err := flightrecorder.ParseKind(req.URL.Query().Get("kind"))
+		if err != nil {
+			http.Error(wr, fmt.Sprintf("invalid kind: %v", err), http.StatusBadRequest)
+			return
+		}
+		if err := w.Start(kind); err != nil {
 			http.Error(wr, err.Error(), http.StatusInternalServerError)
 			return
 		}
@@ -34,7 +40,13 @@ func StopHandler(w flightrecorder.FlightRecorder) http.HandlerFunc {
 // CaptureHandler returns an http handler for capturing a flight recording.
 func CaptureHandler(w flightrecorder.FlightRecorder) http.HandlerFunc {
 	return func(wr http.ResponseWriter, req *http.Request) {
-		if err := w.Capture(); err != nil {
+		kind, err := flightrecorder.ParseKind(req.URL.Query().Get("kind"))
+		if err != nil {
+			http.Error(wr, fmt.Sprintf("invalid kind: %v", err), http.StatusBadRequest)
+			return
+		}
+
+		if err := w.Capture(kind); err != nil {
 			http.Error(wr, err.Error(), http.StatusInternalServerError)
 			return
 		}
