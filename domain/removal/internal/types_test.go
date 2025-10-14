de76056042b449b7be129d34b8884401a1a4f47f
Joseph Phillips <joe.phillips@canonical.com>
2025-10-14 14:24:25 +0200

    refactor: add CascadedStorageLives
    VolumeAttachmentPlanUUIDs is now included, and the storage type is
    empeded in the cascade types for unit, machine and application.

--- domain/removal/internal/types_test.go
+++ domain/removal/internal/types_test.go
@@ -19,6 +19,34 @@ func TestTypesSuite(t *testing.T) {
 	tc.Run(t, &typesSuite{})
 }
 
+func (s *typesSuite) TestCascadedStorageLivesEmpty(c *tc.C) {
+	csl := CascadedStorageLives{}
+
+	csl.StorageAttachmentUUIDs = nil
+	csl.StorageInstanceUUIDs = []string{"burp"}
+	c.Check(csl.IsEmpty(), tc.IsFalse)
+
+	csl.StorageInstanceUUIDs = nil
+	csl.FileSystemUUIDs = []string{"burp"}
+	c.Check(csl.IsEmpty(), tc.IsFalse)
+
+	csl.FileSystemUUIDs = nil
+	csl.FileSystemAttachmentUUIDs = []string{"burp"}
+	c.Check(csl.IsEmpty(), tc.IsFalse)
+
+	csl.FileSystemAttachmentUUIDs = nil
+	csl.VolumeUUIDs = []string{"burp"}
+	c.Check(csl.IsEmpty(), tc.IsFalse)
+
+	csl.VolumeUUIDs = nil
+	csl.VolumeAttachmentUUIDs = []string{"burp"}
+	c.Check(csl.IsEmpty(), tc.IsFalse)
+
+	csl.VolumeAttachmentUUIDs = nil
+	csl.VolumeAttachmentPlanUUIDs = []string{"burp"}
+	c.Check(csl.IsEmpty(), tc.IsFalse)
+}
+
 func (s *typesSuite) TestCascadedUnitLivesEmpty(c *tc.C) {
 	cul := CascadedUnitLives{}
 	c.Check(cul.IsEmpty(), tc.IsTrue)
@@ -51,25 +79,6 @@ func (s *typesSuite) TestCascadedMachineLivesEmpty(c *tc.C) {
 	cml.StorageAttachmentUUIDs = []string{"burp"}
 	c.Check(cml.IsEmpty(), tc.IsFalse)
 
-	cml.StorageAttachmentUUIDs = nil
-	cml.StorageInstanceUUIDs = []string{"burp"}
-	c.Check(cml.IsEmpty(), tc.IsFalse)
-
-	cml.StorageInstanceUUIDs = nil
-	cml.FileSystemUUIDs = []string{"burp"}
-	c.Check(cml.IsEmpty(), tc.IsFalse)
-
-	cml.FileSystemUUIDs = nil
-	cml.FileSystemAttachmentUUIDs = []string{"burp"}
-	c.Check(cml.IsEmpty(), tc.IsFalse)
-
-	cml.FileSystemAttachmentUUIDs = nil
-	cml.VolumeUUIDs = []string{"burp"}
-	c.Check(cml.IsEmpty(), tc.IsFalse)
-
-	cml.VolumeUUIDs = nil
-	cml.VolumeAttachmentUUIDs = []string{"burp"}
-	c.Check(cml.IsEmpty(), tc.IsFalse)
 }
 
 func (s *typesSuite) TestCascadedApplicationLivesEmpty(c *tc.C) {
