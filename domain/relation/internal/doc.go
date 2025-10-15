7c0e95c8e2448c6b626af393bccdb39c1c200665
Heather Lanigan <heather.lanigan@canonical.com>
2025-10-15 16:43:57 -0400

    feat: implement WatchRelationUnits
    It notifies if the application settings change, any unit relation
    settings change, a relation unit is added or removed.
    
    Relation units and the application settings can be watched based on the
    relation endpoint uuid. However relation unit settings cannot. The
    trigger for the relation unit settings is based on the relation endpoint
    uuid. Each time the relation units are triggered, get all of the
    relation unit uuids for the relation endpoing uuid. This list can be
    checked when a relation unit setting changes to determine whether to
    notify.

---
+++ domain/relation/internal/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2025 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+// Package internal provides structures used in the relation domain
+// not to be used outside of it.
+package internal
