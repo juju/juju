1632611c38958d14bcb763aec85d3473833d0077
Thomas Miller <thomas.miller@canonical.com>
2025-10-16 04:21:42 +0000

    refactor: remove unused error type from storage domain
    This commit removes unused error types from storage domain and adds some
    spacing between the errors for readability.
    
    Signed-off-by: Thomas Miller <thomas.miller@canonical.com>

--- domain/storage/errors/errors.go
+++ domain/storage/errors/errors.go
@@ -9,26 +9,24 @@ import "github.com/juju/juju/internal/errors"
 const (
 	// MissingPoolTypeError is used when a provider type is empty.
 	MissingPoolTypeError = errors.ConstError("pool provider type is empty")
+
 	// MissingPoolNameError is used when a name is empty.
 	MissingPoolNameError = errors.ConstError("pool name is empty")
+
 	// InvalidPoolNameError is used when a storage pool name is invalid.
 	InvalidPoolNameError = errors.ConstError("pool name is not valid")
+
 	// InvalidStorageName represents an invalid storage name.
 	InvalidStorageName = errors.ConstError("invalid storage name")
+
 	// PoolNotFoundError is used when a storage pool is not found.
 	PoolNotFoundError = errors.ConstError("storage pool is not found")
+
 	// PoolAlreadyExists is used when a storage pool already exists.
 	PoolAlreadyExists = errors.ConstError("storage pool already exists")
+
 	// ProviderTypeNotFound is used when a storage provider type is not found.
 	ProviderTypeNotFound = errors.ConstError("storage provider type not found")
-	// ErrNoDefaultStoragePool is returned when a storage pool is required but none is specified nor available as a default.
-	ErrNoDefaultStoragePool = errors.ConstError("no storage pool specified and no default available")
-)
-
-// These errors are used for storage directives operations.
-const (
-	// MissingSharedStorageDirectiveError is used when a storage directive for shared storage is not provided.
-	MissingSharedStorageDirectiveError = errors.ConstError("no storage directive specified")
 )
 
 const (
