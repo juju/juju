e64c0f033c44614a06b96fbe5cab3e0bf70dddf4
Thomas Miller <thomas.miller@canonical.com>
2025-10-16 04:24:19 +0000

    refactor: storage domain error consts together
    This places all the storage domain error consts together. There was
    previously some attempt at trying to group errors types but the reality
    is most of the error types break the grouping in one way or another.
    
    The better way to group them is to document their purpose effectively so
    a reasonable person can make the determination.
    
    Signed-off-by: Thomas Miller <thomas.miller@canonical.com>

--- domain/storage/errors/errors.go
+++ domain/storage/errors/errors.go
@@ -5,19 +5,22 @@ package errors
 
 import "github.com/juju/juju/internal/errors"
 
-// These errors are used for storage pool operations.
 const (
-	// MissingPoolTypeError is used when a provider type is empty.
-	MissingPoolTypeError = errors.ConstError("pool provider type is empty")
-
-	// MissingPoolNameError is used when a name is empty.
-	MissingPoolNameError = errors.ConstError("pool name is empty")
+	// FilesystemNotFound describes an error that occurs when the filesystem being operated
+	// on does not exist.
+	FilesystemNotFound = errors.ConstError("filesystem not found")
 
 	// InvalidPoolNameError is used when a storage pool name is invalid.
 	InvalidPoolNameError = errors.ConstError("pool name is not valid")
 
 	// InvalidStorageName represents an invalid storage name.
 	InvalidStorageName = errors.ConstError("invalid storage name")
+
+	// MissingPoolTypeError is used when a provider type is empty.
+	MissingPoolTypeError = errors.ConstError("pool provider type is empty")
+
+	// MissingPoolNameError is used when a name is empty.
+	MissingPoolNameError = errors.ConstError("pool name is empty")
 
 	// PoolNotFoundError is used when a storage pool is not found.
 	PoolNotFoundError = errors.ConstError("storage pool is not found")
@@ -27,16 +30,10 @@ const (
 
 	// ProviderTypeNotFound is used when a storage provider type is not found.
 	ProviderTypeNotFound = errors.ConstError("storage provider type not found")
-)
 
-const (
 	// StorageNotFound describes an error that occurs when the storage being operated
 	// on does not exist.
 	StorageNotFound = errors.ConstError("storage not found")
-
-	// FilesystemNotFound describes an error that occurs when the filesystem being operated
-	// on does not exist.
-	FilesystemNotFound = errors.ConstError("filesystem not found")
 
 	// VolumeNotFound describes an error that occurs when the volume being operated
 	// on does not exist.
