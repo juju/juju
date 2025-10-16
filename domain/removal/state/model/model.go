7d3fe7a1e9097e5f127acc92640ca49f3bccef3b
Joseph Phillips <joe.phillips@canonical.com>
2025-10-16 18:37:21 +0200

    fix: logging for waiting on dead entities

--- domain/removal/state/model/model.go
+++ domain/removal/state/model/model.go
@@ -320,7 +320,7 @@ WHERE uuid = $entityUUID.uuid;
 			return errors.Errorf("cannot delete model %q, model is still alive", modelUUIDParam.UUID).
 				Add(removalerrors.EntityStillAlive)
 		} else if mLife == life.Dying {
-			return errors.Errorf("waiting for model to be removed before deletion").
+			return errors.Errorf("waiting for model to be dead before deletion").
 				Add(removalerrors.RemovalJobIncomplete)
 		}
 
