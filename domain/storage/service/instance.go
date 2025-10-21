2a41288c5b0b6c7166e08ae6a3a1216c63b060bf
Thomas Miller <thomas.miller@canonical.com>
2025-10-21 03:40:06 +0000

    feat: implement a new storage id to uuid method
    Adds a new method to the storage domain for converting from a storage id
    to a storage uuid. This is a common pattern we need in the facade layer
    to go from tags to uuids.
    
    Signed-off-by: Thomas Miller <thomas.miller@canonical.com>

---
+++ domain/storage/service/instance.go
@@ -0,0 +1,27 @@
+// Copyright 2025 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package service
+
+import (
+	"context"
+
+	"github.com/juju/juju/core/trace"
+	domainstorage "github.com/juju/juju/domain/storage"
+	"github.com/juju/juju/internal/errors"
+)
+
+// GetStorageInstanceUUIDForID returns the StorageInstanceUUID for the given
+// storage ID.
+//
+// The following errors may be returned:
+// - [github.com/juju/juju/domain/storage/errors.StorageNotFound] if no storage
+// instance exists for the provided storage id.
+func (s *Service) GetStorageInstanceUUIDForID(
+	ctx context.Context, storageID string,
+) (domainstorage.StorageInstanceUUID, error) {
+	ctx, span := trace.Start(ctx, trace.NameFromFunc())
+	defer span.End()
+
+	return "", errors.New("not implemented: coming soon")
+}
