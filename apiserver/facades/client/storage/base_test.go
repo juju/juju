3bf0542d23d25f504dd50aa8629e43d42ec27813
Thomas Miller <thomas.miller@canonical.com>
2025-10-21 07:53:32 +0000

    test: move storage facade tests into same package
    Removes test package for storage facade.
    
    Signed-off-by: Thomas Miller <thomas.miller@canonical.com>

--- apiserver/facades/client/storage/base_test.go
+++ apiserver/facades/client/storage/base_test.go
@@ -1,7 +1,7 @@
 // Copyright 2015 Canonical Ltd.
 // Licensed under the AGPLv3, see LICENCE file for details.
 
-package storage_test
+package storage
 
 import (
 	"context"
@@ -10,7 +10,6 @@ import (
 	"github.com/juju/tc"
 	"go.uber.org/mock/gomock"
 
-	"github.com/juju/juju/apiserver/facades/client/storage"
 	apiservertesting "github.com/juju/juju/apiserver/testing"
 	"github.com/juju/juju/core/machine"
 	coremodel "github.com/juju/juju/core/model"
@@ -25,15 +24,15 @@ type baseStorageSuite struct {
 	controllerUUID string
 	modelUUID      coremodel.UUID
 
-	api *storage.StorageAPI
+	api *StorageAPI
 
 	unitTag    names.UnitTag
 	machineTag names.MachineTag
 
-	applicationService *storage.MockApplicationService
-	blockDeviceService *storage.MockBlockDeviceService
-	removalService     *storage.MockRemovalService
-	storageService     *storage.MockStorageService
+	applicationService *MockApplicationService
+	blockDeviceService *MockBlockDeviceService
+	removalService     *MockRemovalService
+	storageService     *MockStorageService
 
 	poolsInUse []string
 }
@@ -46,22 +45,22 @@ func (s *baseStorageSuite) setupMocks(c *tc.C) *gomock.Controller {
 
 	s.authorizer = apiservertesting.FakeAuthorizer{Tag: names.NewUserTag("admin"), Controller: true}
 
-	s.applicationService = storage.NewMockApplicationService(ctrl)
+	s.applicationService = NewMockApplicationService(ctrl)
 	s.applicationService.EXPECT().GetUnitMachineName(gomock.Any(), unit.Name("mysql/0")).DoAndReturn(func(ctx context.Context, u unit.Name) (machine.Name, error) {
 		c.Assert(u.String(), tc.Equals, s.unitTag.Id())
 		return machine.Name(s.machineTag.Id()), nil
 	}).AnyTimes()
 
-	s.blockDeviceService = storage.NewMockBlockDeviceService(ctrl)
-	s.removalService = storage.NewMockRemovalService(ctrl)
-	s.storageService = storage.NewMockStorageService(ctrl)
+	s.blockDeviceService = NewMockBlockDeviceService(ctrl)
+	s.removalService = NewMockRemovalService(ctrl)
+	s.storageService = NewMockStorageService(ctrl)
 
 	s.poolsInUse = []string{}
 
 	s.controllerUUID = uuid.MustNewUUID().String()
 	s.modelUUID = modeltesting.GenModelUUID(c)
 
-	s.api = storage.NewStorageAPI(
+	s.api = NewStorageAPI(
 		s.controllerUUID,
 		s.modelUUID,
 		s.authorizer,
