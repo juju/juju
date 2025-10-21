5e3da7de1eec777f17086887174bcdaf952d0b4b
Thomas Miller <thomas.miller@canonical.com>
2025-10-21 03:47:24 +0000

    feat: implement new storage domain method for getting attachment uuid
    This commit implements a new storage domain method for getting the
    attachment uuid of a storage instance and unit uuid.
    
    This is a dummy implenentation to satisfy the facade for now.
    
    Signed-off-by: Thomas Miller <thomas.miller@canonical.com>

---
+++ domain/storage/service/attachment.go
@@ -0,0 +1,48 @@
+// Copyright 2025 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package service
+
+import (
+	"context"
+
+	coreerrors "github.com/juju/juju/core/errors"
+	"github.com/juju/juju/core/trace"
+	coreunit "github.com/juju/juju/core/unit"
+	domainstorage "github.com/juju/juju/domain/storage"
+	domainstorageprovisioning "github.com/juju/juju/domain/storageprovisioning"
+	"github.com/juju/juju/internal/errors"
+)
+
+// GetStorageAttachmentUUIDForStorageIDAndUnit returns the
+// [domainstorageprovisioning.StorageAttachmentUUID] associated with the given
+// storage instance id and unit name.
+//
+// The following errors may be returned:
+// - [coreerrors.NotValid] when either of the supplied uuids did not pass
+// validation.
+// - [github.com/juju/juju/domain/storage/errors.StorageNotFound] if the storage
+// instance for the supplied uuid no longer exists.
+// - [github.com/juju/juju/domain/application/errors.UnitNotFound] if the unit
+// no longer exists for the supplied uuid.
+func (*Service) GetStorageAttachmentUUIDForStorageInstanceAndUnit(
+	ctx context.Context,
+	uuid domainstorage.StorageInstanceUUID,
+	unitUUID coreunit.UUID,
+) (domainstorageprovisioning.StorageAttachmentUUID, error) {
+	ctx, span := trace.Start(ctx, trace.NameFromFunc())
+	defer span.End()
+
+	if uuid.Validate() != nil {
+		return "", errors.New("invalid storage instance uuid").Add(
+			coreerrors.NotValid,
+		)
+	}
+	if unitUUID.Validate() != nil {
+		return "", errors.New("invalid unit uuid").Add(
+			coreerrors.NotValid,
+		)
+	}
+
+	return "", errors.New("not implemented: coming soon")
+}
