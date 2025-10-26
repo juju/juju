2a32325f8f55d44101c94d8c371914ba17e26a60
Thomas Miller <thomas.miller@canonical.com>
2025-10-26 15:38:48 +0000

    refactor: remove storage mount calculation code from storage domain
    We had maintained commented out code in the storage domain on what the
    implementation for calculating the mount point for a filsystem
    attachment needed to follow as a reminder. Now that this has been
    implemented in the storageprovisioning domain the code can be removed.

--- domain/storage/mount.go
+++ domain/storage/mount.go
@@ -15,36 +15,6 @@ import (
 // storageParentDir is the parent directory for mounting charm storage.
 var storageParentDir = paths.StorageDir(paths.OSUnixLike)
 
-//
-//// FilesystemMountPoint returns a mount point to use for the given charm
-//// storage. For stores with potentially multiple instances, the
-//// instance ID is appended to the location.
-//func FilesystemMountPoint(
-//	location string,
-//	maxCount int,
-//	storageID corestorage.ID,
-//) (string, error) {
-//	parentDir := storageParentDir
-//	if strings.HasPrefix(location, path.Join(storageParentDir, "/")) {
-//		return "", errors.Errorf(
-//			"invalid location %q: must not fall within %q",
-//			location, parentDir,
-//		)
-//	}
-//	if location != "" && maxCount == 1 {
-//		// The location is specified and it's a singleton
-//		// store, so just use the location as-is.
-//		return location, nil
-//	}
-//	// If the location is unspecified then we use
-//	// <parentDir>/<storage-id> as the location.
-//	// Otherwise, we use <location>/<storage-id>.
-//	if location != "" {
-//		parentDir = location
-//	}
-//	return path.Join(parentDir, storageID.String()), nil
-//}
-
 // FilesystemMountPointK8s returns a mount point to use for a given filesystem
 // to be mounted in homogenous k8s units.
 func FilesystemMountPointK8s(
