ac604453069c8f7ebba6f4d573dfdb650e2c2b75
Thomas Miller <thomas.miller@canonical.com>
2025-11-04 21:45:25 +0000

    refactor: defines the missing methods on the agent stores
    Defines the functions for placing binaries into a store.

--- domain/agentbinary/service/store.go
+++ domain/agentbinary/service/store.go
@@ -14,13 +14,16 @@ import (
 // AgentBinaryGetterStore defines a agent binary store that can be used to
 // retrieve agent binaries.
 type AgentBinaryGetterStore interface {
-	// GetAgentBinaryWithSHA256 retrieves the agent binary corresponding to the given version
-	// and stream from an external store.
-	// The caller is responsible for closing the returned reader.
+	// GetAgentBinaryForVersionStream retrieves the agent binary
+	// corresponding to the given version and stream. If sucessfully found the
+	// the agent binary stream is returned along with its size and sha256 sum.
+	// It is the caller's responsibility to close the returned stream when no
+	// error condition exists.
 	//
 	// The following errors may be returned:
-	// - [domainagenterrors.NotFound] if the agent binary metadata does not exist.
-	GetAgentBinaryWithSHA256(
+	// - [github.com/juju/juju/domain/agentbinary/errors.NotFound] if the agent
+	// binary does not exist.
+	GetAgentBinaryForVersionStreamSHA256(
 		context.Context,
 		coreagentbinary.Version,
 		agentbinary.Stream,
@@ -30,6 +33,31 @@ type AgentBinaryGetterStore interface {
 // AgentBinaryPutterStore defines a agent binary store that can be used to
 // put agent binaries.
 type AgentBinaryPutterStore interface {
+	// AddAgentBinaryWithSHA256 adds a new agent binary to the store.
+	// The following errors can be returned:
+	// - [coreerrors.NotSupported] if the architecture is not supported.
+	// - [agentbinaryerrors.AlreadyExists] if an agent binary already exists for
+	// this version and architecture.
+	// - [coreerrors.NotValid] if the agent version is not valid.
+	// - [agentbinaryerrors.HashMismatch] when the expected sha does not match
+	// that which was computed against the binary data.
+	AddAgentBinaryWithSHA256(
+		context.Context, io.Reader, coreagentbinary.Version, int64, string,
+	) error
+
+	// AddAgentBinaryWithSHA384 adds a new agent binary to the store.
+	//
+	// The following errors can be returned:
+	// - [github.com/juju/juju/core/errors.NotSupported] if the architecture is
+	// not supported.
+	// - [github.com/juju/juju/domain/agentbinary/errors.AlreadyExists] if an
+	// agent binary already exists for this version architecture and stream.
+	// - [coreerrors.NotValid] when the agent version is not considered valid.
+	// - [agentbinaryerrors.HashMismatch] when the expected sha does not match
+	// that which was computed against the binary data.
+	AddAgentBinaryWithSHA384(
+		context.Context, io.Reader, coreagentbinary.Version, int64, string,
+	) error
 }
 
 // AgentBinaryQuerierStore defines a agent binary store that can be queried for
