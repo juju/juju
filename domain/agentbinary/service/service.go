657f02bbda51ca827f929494b3406f5f538a1a69
Thomas Miller <thomas.miller@canonical.com>
2025-11-05 10:04:37 +0000

    feat: creates a new interface for the agent binary service
    Sets up a more consistent agent binary store interface for callers. This
    change provides a consitent implementation based on stores and stops
    exposing the environ outwards.

--- domain/agentbinary/service/service.go
+++ domain/agentbinary/service/service.go
@@ -6,98 +6,22 @@ package service
 import (
 	"context"
 	"io"
+	"slices"
 
-	coreagentbinary "github.com/juju/juju/core/agentbinary"
 	coreerrors "github.com/juju/juju/core/errors"
-	"github.com/juju/juju/core/providertracker"
+	"github.com/juju/juju/core/logger"
 	"github.com/juju/juju/core/semversion"
 	"github.com/juju/juju/core/trace"
 	"github.com/juju/juju/domain/agentbinary"
 	domainagenterrors "github.com/juju/juju/domain/agentbinary/errors"
-	"github.com/juju/juju/environs/simplestreams"
 	"github.com/juju/juju/internal/errors"
-	coretools "github.com/juju/juju/internal/tools"
 )
 
-type AgentBinaryLocalStore interface {
-	// AddAgentBinaryWithSHA256 adds a new agent binary to the object store and saves its metadata to the database.
-	// The following errors can be returned:
-	// - [coreerrors.NotSupported] if the architecture is not supported.
-	// - [agentbinaryerrors.AlreadyExists] if an agent binary already exists for
-	// this version and architecture.
-	// - [agentbinaryerrors.ObjectNotFound] if there was a problem referencing the
-	// agent binary metadata with the previously saved binary object. This error
-	// should be considered an internal problem. It is discussed here to make the
-	// caller aware of future problems.
-	// - [coreerrors.NotValid] if the agent version is not valid.
-	// - [agentbinaryerrors.HashMismatch] when the expected sha does not match that
-	// which was computed against the binary data.
-	AddAgentBinaryWithSHA256(
-		ctx context.Context, r io.Reader,
-		version coreagentbinary.Version,
-		size int64, sha256 string,
-	) error
-
-	// GetAgentBinaryWithSHA256 retrieves the agent binary corresponding to the given version
-	// and stream from an external store.
-	// The caller is responsible for closing the returned reader.
-	//
-	// The following errors may be returned:
-	// - [domainagenterrors.NotFound] if the agent binary metadata does not exist.
-	GetAgentBinaryWithSHA256(
-		context.Context,
-		coreagentbinary.Version,
-		agentbinary.Stream,
-	) (io.ReadCloser, int64, string, error)
-
-	// AddAgentBinaryWithSHA384 adds a new agent binary to the store and saves its
-	// metadata to the database.
-	//
-	// The following errors can be returned:
-	// - [github.com/juju/juju/core/errors.NotSupported] if the architecture is
-	// not supported.
-	// - [github.com/juju/juju/domain/agentbinary/errors.AlreadyExists] if an
-	// agent binary already exists for this version architecture and stream.
-	// - [agentbinaryerrors.ObjectNotFound] if there was a problem referencing
-	// the agent binary metadata with the previously saved binary object. This
-	// error should be considered an internal problem. It is discussed here to
-	// make the caller aware of future problems.
-	// - [coreerrors.NotValid] when the agent version is not considered valid.
-	// - [agentbinaryerrors.HashMismatch] when the expected sha does not match
-	// that which was computed against the binary data.
-	AddAgentBinaryWithSHA384(
-		ctx context.Context,
-		r io.Reader,
-		version coreagentbinary.Version,
-		size int64,
-		sha384 string,
-	) error
-
-	// GetAgentBinaryUsingSHA256 returns the agent binary associated with the given
-	// SHA256 sum. The following errors can be expected:
-	// - [agentbinaryerrors.NotFound] when no agent binaries exist for the provided
-	// sha.
-	GetAgentBinaryUsingSHA256(
-		ctx context.Context,
-		sha256Sum string,
-	) (io.ReadCloser, int64, error)
-}
-
+// ModelState defines the interface required to get model specific agent binary
+// information.
 type ModelState interface {
 	// GetAgentStream returns the stream currently in use by the model.
 	GetAgentStream(ctx context.Context) (agentbinary.Stream, error)
-
-	// ListAgentBinaries lists all agent binaries in the state.
-	// It returns a slice of agent binary metadata.
-	// An empty slice is returned if no agent binaries are found.
-	ListAgentBinaries(ctx context.Context) ([]agentbinary.Metadata, error)
-}
-
-type ControllerState interface {
-	// ListAgentBinaries lists all agent binaries in the state.
-	// It returns a slice of agent binary metadata.
-	// An empty slice is returned if no agent binaries are found.
-	ListAgentBinaries(ctx context.Context) ([]agentbinary.Metadata, error)
 }
 
 // AgentBinaryService provides the API for working with agent binaries.
@@ -105,226 +29,281 @@ type ControllerState interface {
 // The service is used to provide a unified view of the agent binaries
 // across the controller and model states.
 type AgentBinaryService struct {
-	providerForAgentBinaryFinder providertracker.ProviderGetter[ProviderForAgentBinaryFinder]
-	getPreferredSimpleStreams    PreferredSimpleStreamsFunc
-	agentBinaryFilter            AgentBinaryFilter
-	controllerState              ControllerState
-	externalStores               []AgentBinaryGetterStore
-	modelState                   ModelState
-	store                        AgentBinaryLocalStore
+	// externalStores defines in order the set of external stores to use get
+	// getting agent binaries that don't exist in the
+	// [AgentBinaryService.putableAgentStore].
+	externalStores []AgentBinaryGetterStore
+
+	// logger is used to high light warnings around fetching and caching agent
+	// binaries that can not be surfaced to a caller.
+	logger logger.Logger
+
+	// putableAgentStore defines the store attached to this service where agent
+	// binary caching and storing can occur.
+	putableAgentStore AgentBinaryStore
+
+	// state provides access to the underlying model's state.
+	state ModelState
 }
 
-// NewAgentBinaryService returns a new instance of AgentBinaryService.
+// NewService returns a new instance of AgentBinaryService.
 // It takes two states: the controller state and the model state to aggregate the
 // agent binaries from both states.
-func NewAgentBinaryService(
-	providerForAgentBinaryFinder providertracker.ProviderGetter[ProviderForAgentBinaryFinder],
-	getPreferredSimpleStreams PreferredSimpleStreamsFunc,
-	agentBinaryFilter AgentBinaryFilter,
-	controllerState ControllerState,
-	modelState ModelState,
-	store AgentBinaryLocalStore,
+func NewService(
+	state ModelState,
+	logger logger.Logger,
+	putableAgentStore AgentBinaryStore,
 	externalStores ...AgentBinaryGetterStore,
 ) *AgentBinaryService {
 	return &AgentBinaryService{
-		providerForAgentBinaryFinder: providerForAgentBinaryFinder,
-		getPreferredSimpleStreams:    getPreferredSimpleStreams,
-		agentBinaryFilter:            agentBinaryFilter,
-		externalStores:               externalStores,
-		controllerState:              controllerState,
-		modelState:                   modelState,
-		store:                        store,
+		externalStores:    externalStores,
+		logger:            logger,
+		putableAgentStore: putableAgentStore,
+		state:             state,
 	}
 }
 
 // GetAgentBinary retrieves the agent binary for the specified version from the
-// model's configured store. The returned reader provides access to the
-// verified binary contents.
-func (s *AgentBinaryService) GetAgentBinary(ctx context.Context, ver coreagentbinary.Version) (io.ReadCloser, int64, error) {
-	stream, err := s.modelState.GetAgentStream(ctx)
+// underlying agent store. The returned reader provides access to the
+// verified binary contents. The caller MUST close the provided stream under non
+// error conditions.
+//
+// The following errors may be returned:
+// - [coreerrors.NotValid] when the supplied version is not valid.
+// - [domainagenterrors.NotFound] if the agent binary does not exist in the
+// store.
+func (s *AgentBinaryService) GetAgentBinaryForVersion(
+	ctx context.Context, ver agentbinary.Version,
+) (io.ReadCloser, int64, error) {
+	ctx, span := trace.Start(ctx, trace.NameFromFunc())
+	defer span.End()
+
+	if err := ver.Validate(); err != nil {
+		return nil, 0, errors.Capture(err)
+	}
+
+	stream, err := s.state.GetAgentStream(ctx)
 	if err != nil {
 		return nil, 0, errors.Errorf(
-			"getting agent stream from model state: %w",
+			"getting agent stream for model: %w",
 			err,
 		)
 	}
 
-	reader, size, _, err := s.store.GetAgentBinaryWithSHA256(ctx, ver, stream)
+	reader, size, _, err := s.putableAgentStore.GetAgentBinaryForVersionStreamSHA256(
+		ctx, ver, stream,
+	)
 	if err != nil {
-		return nil, 0, errors.Capture(err)
-	}
-	if reader == nil {
-		return nil, 0, domainagenterrors.NotFound
+		return nil, 0, errors.Errorf("getting agent binary from store: %w", err)
 	}
 
 	return reader, size, nil
 }
 
-// GetExternalAgentBinary attempts to retrieve the specified agent binary from one
+// GetAndCacheExternalAgentBinary attempts to retrieve the specified agent
+// binary from one of the available external agent binary stores.
+//
+// The following errors may be returned:
+// - [coreerrors.NotValid] when the supplied version is not valid.
+// - [domainagenterrors.NotFound] if the agent binary does not exist in any of
+// the available external stores.
+// - [domainagenterrors.HashMismatch] if the external agent binary store=
+// supplied data that did not meet the expected hash.
+//
 // or more configured external stores. It validates the integrity of the fetched
 // binary via SHA256 and SHA384 comparison, then caches and persists it into the
 // local store for subsequent faster retrieval. If the binary cannot be found in
 // any external store or fails hash verification, an appropriate error is
 // returned. The returned reader provides the verified binary content along with
 // its size and SHA384 checksum.
-func (s *AgentBinaryService) GetExternalAgentBinary(ctx context.Context, ver coreagentbinary.Version) (io.ReadCloser, int64, string, error) {
-	var extReader io.ReadCloser
-	var extSize int64
-	var extSHA256 string
-	var err error
+func (s *AgentBinaryService) GetAndCacheExternalAgentBinary(
+	ctx context.Context, ver agentbinary.Version,
+) (io.ReadCloser, int64, error) {
+	ctx, span := trace.Start(ctx, trace.NameFromFunc())
+	defer span.End()
 
-	stream, err := s.modelState.GetAgentStream(ctx)
+	if err := ver.Validate(); err != nil {
+		return nil, 0, errors.Capture(err)
+	}
+
+	stream, err := s.state.GetAgentStream(ctx)
 	if err != nil {
-		return nil, 0, "", errors.Errorf(
-			"getting agent stream from model state: %w",
+		return nil, 0, errors.Errorf(
+			"getting agent stream for model: %w",
 			err,
 		)
 	}
 
+	var (
+		extReader io.ReadCloser
+		extSize   int64
+		extSHA256 string
+	)
 	for i, store := range s.externalStores {
-		extReader, extSize, extSHA256, err = store.GetAgentBinaryWithSHA256(
+		var err error
+		extReader, extSize, extSHA256, err = store.GetAgentBinaryForVersionStreamSHA256(
 			ctx, ver, stream,
 		)
 		if errors.Is(err, domainagenterrors.NotFound) {
+			// External store does not have the requested agent binary, try
+			// the next one.
 			continue
 		} else if err != nil {
-			// Return any unknown err early since we are not sure if proceeding is safe.
-			return nil, 0, "", errors.Errorf(
-				"attempted fetching agent binary %q from external store %d: %w",
+			// Return any unknown err early since we are not sure if continuing
+			// is safe.
+			return nil, 0, errors.Errorf(
+				"fetching agent binary %q from external store %d: %w",
 				ver, i, err,
 			)
 		}
+
+		// External store has what we are looking for, let's get out of here!
 		break
 	}
 
+	// if extReader is nil then no external agent binary was found
 	if extReader == nil {
-		return nil, 0, "", errors.Errorf(
-			"agent binary %q does not exist in external stores: %w",
-			ver, err,
-		)
+		return nil, 0, errors.Errorf(
+			"agent binary %q not available in external stores", ver,
+		).Add(domainagenterrors.NotFound)
 	}
 
+	// Make sure we close the external reader.
 	defer func(extReader io.ReadCloser) {
-		_ = extReader.Close()
+		err := extReader.Close()
+		if err != nil {
+			s.logger.Errorf(
+				ctx, "closing external agent binary store stream: %s", err.Error(),
+			)
+		}
 	}(extReader)
+
 	rSHA, shaCalc := computeSHA256andSHA384(extReader)
 	cacheR, err := newStrictCacher(rSHA, extSize)
-
 	if errors.Is(err, ErrorReaderDesync) {
-		return nil, 0, "", errors.Errorf(
+		// This error happens when the number of bytes reported by the external
+		// store does not match what was provided.
+		return nil, 0, errors.Errorf(
 			"agent binary received from external store did not match the expected number of bytes %d",
 			extSize,
 		)
 	} else if err != nil {
-		return nil, 0, "", errors.Errorf(
+		return nil, 0, errors.Errorf(
 			"caching agent binary from external store for processing: %w", err,
 		)
 	}
-	defer func() { _ = cacheR.Close() }()
+
+	defer func() {
+		err := cacheR.Close()
+		if err != nil {
+			s.logger.Errorf(
+				ctx, "closing external agent binary store cache: %s", err.Error(),
+			)
+		}
+	}()
 
 	sha256Calc, sha384Calc := shaCalc()
 	if sha256Calc != extSHA256 {
-		return nil, 0, "", errors.Errorf(
+		return nil, 0, errors.Errorf(
 			"computed sha from external store does not match expected value",
 		).Add(domainagenterrors.HashMismatch)
 	}
 
 	// Add the external agent binary to our store for faster access next time.
-	err = s.store.AddAgentBinaryWithSHA384(ctx, cacheR, ver, extSize, sha384Calc)
-	if err != nil {
-		return nil, 0, "", errors.Errorf(
-			"saving found agent binary from external store: %w", err,
+	err = s.putableAgentStore.AddAgentBinaryWithSHA384(
+		ctx, cacheR, ver, extSize, sha384Calc,
+	)
+	if errors.Is(err, domainagenterrors.AlreadyExists) {
+		// This is fine, it means the effort was wasted but the end result has
+		// been achieved.
+		s.logger.Debugf(ctx, "external agent binary has already been cached in store")
+	} else if err != nil {
+		return nil, 0, errors.Errorf(
+			"storing external agent binary in store for caching: %w", err,
 		)
 	}
 
-	r, size, err := s.store.GetAgentBinaryUsingSHA256(ctx, sha256Calc)
-	return r, size, sha384Calc, err
+	r, size, err := s.putableAgentStore.GetAgentBinaryForSHA384(ctx, sha384Calc)
+	if err != nil {
+		return nil, 0, errors.Errorf(
+			"retrieving agent binary from store after caching: %w", err,
+		)
+	}
+
+	return r, size, nil
 }
 
-// TODO(Alvin): Remove these following after subsequent callers do not call this
-
-// ListAgentBinaries lists all agent binaries in the controller and model stores.
-// It merges the two lists of agent binaries, with the model agent binaries
-// taking precedence over the controller agent binaries.
-// It returns a slice of agent binary metadata. The order of the metadata is not guaranteed.
-// An empty slice is returned if no agent binaries are found.
-func (s *AgentBinaryService) ListAgentBinaries(ctx context.Context) ([]agentbinary.Metadata, error) {
-	// Merge the two lists of agent binaries. The model agent binaries
-	// take precedence over the controller agent binaries.
-
-	allAgentBinaries := make(map[string]agentbinary.Metadata)
-
-	modelAgentBinaries, err := s.modelState.ListAgentBinaries(ctx)
-	if err != nil && !errors.Is(err, domainagenterrors.NotFound) {
-		return nil, errors.Errorf("listing agent binaries from model state: %w", err)
-	}
-	controllerAgentBinaries, err := s.controllerState.ListAgentBinaries(ctx)
-	if err != nil && !errors.Is(err, domainagenterrors.NotFound) {
-		return nil, errors.Errorf("listing agent binaries from controller state: %w", err)
+// GetAvailableAgentBinaryiesForVersion returns a list of all agent binaries
+// available for the specified version across all architectures supported.
+//
+// The following errors may be returned:
+// - [coreerrors.NotValid] when the supplied version is not valid.
+func (s *AgentBinaryService) GetAvailableAgentBinaryiesForVersion(
+	ctx context.Context, ver semversion.Number,
+) ([]agentbinary.AgentBinary, error) {
+	if ver == semversion.Zero {
+		return nil, errors.New("version cannot be zero").Add(coreerrors.NotValid)
 	}
 
-	for _, ab := range controllerAgentBinaries {
-		allAgentBinaries[ab.SHA256] = ab
+	stream, err := s.state.GetAgentStream(ctx)
+	if err != nil {
+		return nil, errors.Errorf(
+			"getting model's current agent stream: %w", err,
+		)
 	}
 
-	for _, ab := range modelAgentBinaries {
-		allAgentBinaries[ab.SHA256] = ab
+	archsToFind := agentbinary.SupportedArchitectures()
+	retVal := make([]agentbinary.AgentBinary, 0, len(archsToFind))
+
+	available, err := s.putableAgentStore.GetAvailableForVersionInStream(
+		ctx, ver, stream,
+	)
+	if err != nil {
+		return nil, errors.Errorf(
+			"getting available agent binaries in store for version %q in stream %q: %w",
+			ver, stream, err,
+		)
 	}
 
-	allAgentBinariesSlice := make([]agentbinary.Metadata, 0, len(allAgentBinaries))
-	for _, ab := range allAgentBinaries {
-		allAgentBinariesSlice = append(allAgentBinariesSlice, ab)
+	available = slices.DeleteFunc(
+		available, agentbinary.AgentBinaryNotMatchingArchitectures(archsToFind),
+	)
+	archsToFind = agentbinary.ArchitectureNotIn(
+		archsToFind,
+		slices.Collect(agentbinary.AgentBinaryArchitectures(available)),
+	)
+	retVal = append(retVal, available...)
+
+	if len(archsToFind) == 0 {
+		// Found all agent binaries nothing more to do.
+		return retVal, nil
 	}
-	return allAgentBinariesSlice, nil
-}
 
-// PreferredSimpleStreamsFunc is a function that returns the preferred streams
-// for the given version and stream.
-type PreferredSimpleStreamsFunc func(
-	vers *semversion.Number,
-	forceDevel bool,
-	stream string,
-) []string
-
-// EnvironAgentBinariesFinderFunc is a function that can be used to find agent binaries
-// from the simplestreams data sources.
-type EnvironAgentBinariesFinderFunc func(
-	ctx context.Context,
-	major,
-	minor int,
-	version semversion.Number,
-	requestedStream string,
-	filter coretools.Filter,
-) (coretools.List, error)
-
-// GetEnvironAgentBinariesFinder returns a function that can be used to find
-// agent binaries from the simplestreams data sources.
-func (s *AgentBinaryService) GetEnvironAgentBinariesFinder() EnvironAgentBinariesFinderFunc {
-	return func(
-		ctx context.Context,
-		major,
-		minor int,
-		version semversion.Number,
-		requestedStream string,
-		filter coretools.Filter,
-	) (_ coretools.List, err error) {
-		ctx, span := trace.Start(ctx, trace.NameFromFunc())
-		defer span.End()
-
-		provider, err := s.providerForAgentBinaryFinder(ctx)
-		if errors.Is(err, coreerrors.NotSupported) {
-			return nil, errors.Errorf("getting provider for agent binary finder %w", coreerrors.NotSupported)
-		}
+	for i, store := range s.externalStores {
+		available, err := store.GetAvailableForVersionInStream(
+			ctx, ver, stream,
+		)
 		if err != nil {
-			return nil, errors.Capture(err)
-		}
-		cfg := provider.Config()
-		if requestedStream == "" {
-			requestedStream = cfg.AgentStream()
+			return nil, errors.Errorf(
+				"getting available agent binaries in external store %d for version %q in stream %q: %w",
+				i, ver, stream, err,
+			)
 		}
 
-		streams := s.getPreferredSimpleStreams(&version, cfg.Development(), requestedStream)
-		ssFetcher := simplestreams.NewSimpleStreams(simplestreams.DefaultDataSourceFactory())
-		return s.agentBinaryFilter(ctx, ssFetcher, provider, major, minor, streams, filter)
+		available = slices.DeleteFunc(
+			available, agentbinary.AgentBinaryNotMatchingArchitectures(archsToFind),
+		)
+		archsToFind = agentbinary.ArchitectureNotIn(
+			archsToFind,
+			slices.Collect(agentbinary.AgentBinaryArchitectures(available)),
+		)
+		retVal = append(retVal, available...)
+
+		if len(archsToFind) == 0 {
+			// Found all agent binaries nothing more to do.
+			return retVal, nil
+		}
 	}
+
+	return retVal, nil
 }
