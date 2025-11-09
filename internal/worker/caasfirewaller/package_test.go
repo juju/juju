bfbc3b638268ac040ae64514432228690315cd48
Thomas Miller <thomas.miller@canonical.com>
2025-11-09 11:05:36 +0200

    test: update mock requirements for caas firewaller
    Updates all the package mock requirements

--- internal/worker/caasfirewaller/package_test.go
+++ internal/worker/caasfirewaller/package_test.go
@@ -3,7 +3,8 @@
 
 package caasfirewaller
 
-//go:generate go run go.uber.org/mock/mockgen -typed -package mocks -destination mocks/broker_mock.go github.com/juju/juju/internal/worker/caasfirewaller CAASBroker,PortMutator,ServiceUpdater
+//go:generate go run go.uber.org/mock/mockgen -typed -package mocks -mock_names=Broker=MockExtCAASBroker -destination mocks/caasbroker_mock.go github.com/juju/juju/caas Broker
+//go:generate go run go.uber.org/mock/mockgen -typed -package mocks -destination mocks/broker_mock.go github.com/juju/juju/internal/worker/caasfirewaller CAASBroker,PortMutator
 //go:generate go run go.uber.org/mock/mockgen -typed -package mocks -destination mocks/worker_mock.go github.com/juju/worker/v4 Worker
 //go:generate go run go.uber.org/mock/mockgen -typed -package mocks -destination mocks/domain_mocks.go github.com/juju/juju/internal/worker/caasfirewaller ApplicationService,PortService
 //go:generate go run go.uber.org/mock/mockgen -typed -package mocks -destination mocks/services_mocks.go github.com/juju/juju/internal/services ModelDomainServices
