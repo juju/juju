9dca48cb675db0b68bcbaa0e809bee1fd462e846
Alvin Chee <alvin.chee@canonical.com>
2025-11-21 11:50:18 +0800

    refactor: test cases

--- internal/worker/uniter/runner/jujuc/storage-add_test.go
+++ internal/worker/uniter/runner/jujuc/storage-add_test.go
@@ -39,7 +39,7 @@ type tstData struct {
 func (s *storageAddSuite) TestStorageAddInit(c *gc.C) {
 	tests := []tstData{
 		{[]string{}, 1, "storage add requires a storage directive"},
-		{[]string{"data=-676"}, 1, `.*cannot parse count: count must be gre.*`},
+		{[]string{"data=-676"}, 1, `.*parsing storage count:  count must be gre.*`},
 		{[]string{"data="}, 1, ".*storage constraints require at least one.*"},
 		{[]string{"data=pool"}, 1, `.*only count can be specified for "data".*`},
 		{[]string{"data=pool,1M"}, 1, `.*only count can be specified for "data".*`},
