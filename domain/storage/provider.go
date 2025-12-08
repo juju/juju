f206bb7b6b7a73617e232201389b293abbc7bf80
Thomas Miller <thomas.miller@canonical.com>
2025-12-08 04:30:48 +0000

    feat: support validation of provider types
    Introduces a new func on storage provider type for validating provider
    type values. This allows for user input values to be asserted for
    correctness before use.
    
    Includes tests to assert the logic of valid for a provider type.

--- domain/storage/provider.go
+++ domain/storage/provider.go
@@ -3,9 +3,42 @@
 
 package storage
 
+import (
+	"regexp"
+)
+
 // ProviderType is a unique identification type to represent individual storage
 // providers within a controller by common value.
 type ProviderType string
+
+var (
+	// validProviderTypeRegex is a regular expression that checks if a provider
+	// type value is valid. This may seem odd as provider types are controlled
+	// within source code. However we ask users to supply these values via
+	// multiple interfaces. We need a safe way of validating what they have
+	// supplied before using it any further.
+	//
+	// The rules for a valid provider type are:
+	// - MUST start with an upper case or lower case ASCII character.
+	// - MUST be at least one character long.
+	// - After the first character, ASCII alphasnumeric characters are allowed
+	// including hyphens.
+	// - MUST not be greater than 128 characters.
+	validProviderTypeRegex = regexp.MustCompile("^[[:alpha:]][-[:alpha:][:digit:]]{1,127}$")
+)
+
+// IsValid exists to validate a supplied provider type value from an untrusted
+// source before using any further.
+//
+// The rules for a valid provider type are:
+// - MUST start with an upper case or lower case ASCII character.
+// - MUST be at least one character long.
+// - After the first character, ASCII alphasnumeric characters are allowed
+// including hyphens.
+// - MUST not be greater than 128 characters.
+func (p ProviderType) IsValid() bool {
+	return validProviderTypeRegex.MatchString(p.String())
+}
 
 // String returns the string representation of [ProviderType]. String implements
 // the [fmt.Stringer] interface.
