24be73800e9b5e6f7dd771dc7f6af6827ad57d38
Thomas Miller <thomas.miller@canonical.com>
2025-12-11 05:12:59 +0000

    test: add missing authz tests for detach storage
    Adds missing authz tests for detatch storage.

--- apiserver/facades/client/storage/detach_test.go
+++ apiserver/facades/client/storage/detach_test.go
@@ -7,9 +7,11 @@ import (
 	"testing"
 	time "time"
 
+	"github.com/juju/names/v6"
 	"github.com/juju/tc"
 	"go.uber.org/mock/gomock"
 
+	apiservertesting "github.com/juju/juju/apiserver/testing"
 	coreunit "github.com/juju/juju/core/unit"
 	applicationerrors "github.com/juju/juju/domain/application/errors"
 	domainstorage "github.com/juju/juju/domain/storage"
@@ -29,6 +31,157 @@ type storageDetachSuite struct {
 // [storageDetachSuite].
 func TestStorageDetachSuite(t *testing.T) {
 	tc.Run(t, &storageDetachSuite{})
+}
+
+// TestWithModelWritePermission tests that a user with write permission on the
+// model is allowed to detach storage.
+func (s *storageDetachSuite) TestWithModelWritePermission(c *tc.C) {
+	defer s.setupMocks(c).Finish()
+	userTag := tc.Must1(c, names.ParseUserTag, "user-tlm")
+	s.authorizer = apiservertesting.FakeAuthorizer{
+		HasWriteTag: userTag,
+		Tag:         userTag,
+	}
+
+	storageAttachmentUUID := tc.Must(c, domainstorageprovisioning.NewStorageAttachmentUUID)
+	storageInstUUID := tc.Must(c, domainstorage.NewStorageInstanceUUID)
+	unitUUID := tc.Must(c, coreunit.NewUUID)
+
+	appExp := s.applicationService.EXPECT()
+	appExp.GetUnitUUID(gomock.Any(), coreunit.Name("myapp/0")).Return(
+		unitUUID, nil,
+	).AnyTimes()
+
+	storageExp := s.storageService.EXPECT()
+	storageExp.GetStorageInstanceUUIDForID(gomock.Any(), "data/1").Return(
+		storageInstUUID, nil,
+	).AnyTimes()
+	storageExp.GetStorageAttachmentUUIDForStorageInstanceAndUnit(
+		gomock.Any(), storageInstUUID, unitUUID,
+	).Return(storageAttachmentUUID, nil).AnyTimes()
+
+	removalEXP := s.removalService.EXPECT()
+	removalEXP.RemoveStorageAttachment(
+		gomock.Any(), storageAttachmentUUID, false, time.Duration(0),
+	).Return("123", nil)
+
+	api := s.makeTestAPI(c)
+	result, err := api.DetachStorage(c.Context(), params.StorageDetachmentParams{
+		StorageIds: params.StorageAttachmentIds{
+			Ids: []params.StorageAttachmentId{
+				{
+					StorageTag: "storage-data/1",
+					UnitTag:    "unit-myapp/0",
+				},
+			},
+		},
+	})
+	c.Assert(err, tc.ErrorIsNil)
+	c.Assert(result.Results, tc.HasLen, 1)
+	c.Check(result.Results[0].Error, tc.IsNil)
+}
+
+// TestWithModelAdminPermission tests that a user with admin permission on the
+// model is allowed to detach storage.
+func (s *storageDetachSuite) TestWithModelAdminPermission(c *tc.C) {
+	defer s.setupMocks(c).Finish()
+	userTag := tc.Must1(c, names.ParseUserTag, "user-tlm")
+	s.authorizer = apiservertesting.FakeAuthorizer{
+		AdminTag: userTag,
+		Tag:      userTag,
+	}
+
+	storageAttachmentUUID := tc.Must(c, domainstorageprovisioning.NewStorageAttachmentUUID)
+	storageInstUUID := tc.Must(c, domainstorage.NewStorageInstanceUUID)
+	unitUUID := tc.Must(c, coreunit.NewUUID)
+
+	appExp := s.applicationService.EXPECT()
+	appExp.GetUnitUUID(gomock.Any(), coreunit.Name("myapp/0")).Return(
+		unitUUID, nil,
+	).AnyTimes()
+
+	storageExp := s.storageService.EXPECT()
+	storageExp.GetStorageInstanceUUIDForID(gomock.Any(), "data/1").Return(
+		storageInstUUID, nil,
+	).AnyTimes()
+	storageExp.GetStorageAttachmentUUIDForStorageInstanceAndUnit(
+		gomock.Any(), storageInstUUID, unitUUID,
+	).Return(storageAttachmentUUID, nil).AnyTimes()
+
+	removalEXP := s.removalService.EXPECT()
+	removalEXP.RemoveStorageAttachment(
+		gomock.Any(), storageAttachmentUUID, false, time.Duration(0),
+	).Return("123", nil)
+
+	api := s.makeTestAPI(c)
+	result, err := api.DetachStorage(c.Context(), params.StorageDetachmentParams{
+		StorageIds: params.StorageAttachmentIds{
+			Ids: []params.StorageAttachmentId{
+				{
+					StorageTag: "storage-data/1",
+					UnitTag:    "unit-myapp/0",
+				},
+			},
+		},
+	})
+	c.Assert(err, tc.ErrorIsNil)
+	c.Assert(result.Results, tc.HasLen, 1)
+	c.Check(result.Results[0].Error, tc.IsNil)
+}
+
+// TestWithReadPermissionFails tests that if the caller has model read
+// permission they are unable to detach storage. The caller MUST get back an
+// error with [params.CodeUnauthorized] set.
+func (s *storageDetachSuite) TestWithReadPermissionFails(c *tc.C) {
+	defer s.setupMocks(c).Finish()
+	userTag := tc.Must1(c, names.ParseUserTag, "user-tlm")
+	s.authorizer = apiservertesting.FakeAuthorizer{
+		HasReadTag: userTag,
+		Tag:        userTag,
+	}
+
+	api := s.makeTestAPI(c)
+	result, err := api.DetachStorage(c.Context(), params.StorageDetachmentParams{
+		StorageIds: params.StorageAttachmentIds{
+			Ids: []params.StorageAttachmentId{
+				{
+					StorageTag: "storage-data/1",
+					UnitTag:    "unit-myapp/0",
+				},
+			},
+		},
+	})
+	paramsErr, is := errors.AsType[*params.Error](err)
+	c.Assert(is, tc.IsTrue)
+	c.Check(paramsErr.Code, tc.Equals, params.CodeUnauthorized)
+	c.Check(result.Results, tc.HasLen, 0)
+}
+
+// TestWithNoPermissionFails tests that if the caller has no model permissions
+// they are unable to detach storage. The caller MUST get back an error with
+// [params.CodeUnauthorized] set.
+func (s *storageDetachSuite) TestWithNoPermissionFails(c *tc.C) {
+	defer s.setupMocks(c).Finish()
+	userTag := tc.Must1(c, names.ParseUserTag, "user-tlm")
+	s.authorizer = apiservertesting.FakeAuthorizer{
+		Tag: userTag,
+	}
+
+	api := s.makeTestAPI(c)
+	result, err := api.DetachStorage(c.Context(), params.StorageDetachmentParams{
+		StorageIds: params.StorageAttachmentIds{
+			Ids: []params.StorageAttachmentId{
+				{
+					StorageTag: "storage-data/1",
+					UnitTag:    "unit-myapp/0",
+				},
+			},
+		},
+	})
+	paramsErr, is := errors.AsType[*params.Error](err)
+	c.Assert(is, tc.IsTrue)
+	c.Check(paramsErr.Code, tc.Equals, params.CodeUnauthorized)
+	c.Check(result.Results, tc.HasLen, 0)
 }
 
 // TestNegativeMaxWaitTime asserts that if the user supplies a negative max
