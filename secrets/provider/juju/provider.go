249e54277b35f0371b57ca8e7fc4c51419e1715d
Harry Pidcock <harry.pidcock@canonical.com>
2026-01-14 15:37:23 +1000

    chore: update juju secrets provider no-op

--- secrets/provider/juju/provider.go
+++ secrets/provider/juju/provider.go
@@ -57,7 +57,9 @@ func (p jujuProvider) IssuesTokens() bool {
 // secrets backend client restricted to manage the specified
 // owned secrets and read shared secrets for the given entity tag.
 func (p jujuProvider) RestrictedConfig(
-	adminCfg *provider.ModelBackendConfig, sameController, forDrain bool, tag names.Tag, owned provider.SecretRevisions, read provider.SecretRevisions,
+	*provider.ModelBackendConfig,
+	bool, bool, string, names.Tag,
+	[]string, provider.SecretRevisions, provider.SecretRevisions,
 ) (*provider.BackendConfig, error) {
 	return &provider.BackendConfig{
 		BackendType: BackendType,
