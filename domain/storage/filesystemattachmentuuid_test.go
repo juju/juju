c2eb915d2a56a6b06bdce0eddd45eb3639ef675a
Thomas Miller <thomas.miller@canonical.com>
2026-01-21 23:14:34 +0000

    test: add tests for filesystemattachment uuid type
    Adds missing tests for filesystemattachment uuid type.

---
+++ domain/storage/filesystemattachmentuuid_test.go
@@ -0,0 +1,47 @@
+// Copyright 2026 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package storage
+
+import (
+	"testing"
+
+	"github.com/juju/tc"
+)
+
+// filesystemAttachmentUUIDSuite is a suite of tests for asserting the behaviour of
+// [VolumeAttachmentUUID].
+type filesystemAttachmentUUIDSuite struct{}
+
+// TestFilesystemAttachmentUUIDSuite runs all of the tests contained within
+// [filesystemAttachmentUUIDSuite].
+func TestFilesystemAttachmentUUIDSuite(t *testing.T) {
+	tc.Run(t, filesystemAttachmentUUIDSuite{})
+}
+
+// TestNew tests that constructing a new volumeattachment uuid suceeds with no
+// errors and the end result is valid.
+func (filesystemAttachmentUUIDSuite) TestNew(c *tc.C) {
+	u, err := NewFilesystemAttachmentUUID()
+	c.Check(err, tc.ErrorIsNil)
+	c.Check(u.Validate(), tc.ErrorIsNil)
+}
+
+// TestStringer asserts the [fmt.Stringer] interface of [VolumeAttachmentUUID]
+// by making sure the correct string representation of the uuid is returned to
+// the caller.
+func (filesystemAttachmentUUIDSuite) TestStringer(c *tc.C) {
+	const validUUID = "0de7ed80-bfcf-49b1-876a-31462e940ca1"
+	c.Check(FilesystemAttachmentUUID(validUUID).String(), tc.Equals, validUUID)
+}
+
+// TestValidate asserts that a valid uuid passes validation with no errors.
+func (filesystemAttachmentUUIDSuite) TestValidate(c *tc.C) {
+	const validUUID = "0de7ed80-bfcf-49b1-876a-31462e940ca1"
+	c.Check(FilesystemAttachmentUUID(validUUID).Validate(), tc.ErrorIsNil)
+}
+
+// TestValidateFail asserts that a bad uuid fails validation.
+func (filesystemAttachmentUUIDSuite) TestValidateFail(c *tc.C) {
+	c.Check(FilesystemAttachmentUUID("invalid").Validate(), tc.NotNil)
+}
