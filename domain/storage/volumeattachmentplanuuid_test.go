f0661f99fdb3be5747ad054813d33fe103a0cde4
Thomas Miller <thomas.miller@canonical.com>
2026-01-21 23:27:20 +0000

    test: add missing tests for volumeattachmentplan uuid type
    Adds missing test coverage for VolumeAttachmentPlanUUID type in the
    storage domain.

---
+++ domain/storage/volumeattachmentplanuuid_test.go
@@ -0,0 +1,47 @@
+// Copyright 2026 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package storage
+
+import (
+	"testing"
+
+	"github.com/juju/tc"
+)
+
+// volumeAttachmentPlanUUIDSuite is a suite of tests for asserting the behaviour
+// of [volumeAttachmentPlanUUIDSuite].
+type volumeAttachmentPlanUUIDSuite struct{}
+
+// TestVolumeAttachmentPlanUUIDSuite runs all of the tests contained within
+// [volumeAttachmentPlanUUIDSuite].
+func TestVolumeAttachmentPlanUUIDSuite(t *testing.T) {
+	tc.Run(t, volumeAttachmentPlanUUIDSuite{})
+}
+
+// TestNew tests that constructing a new VolumeAttachmentPlanUUID suceeds with
+// no errors and the end result is valid.
+func (volumeAttachmentPlanUUIDSuite) TestNew(c *tc.C) {
+	u, err := NewVolumeAttachmentPlanUUID()
+	c.Check(err, tc.ErrorIsNil)
+	c.Check(u.Validate(), tc.ErrorIsNil)
+}
+
+// TestStringer asserts the [fmt.Stringer] interface of
+// [VolumeAttachmentPlanUUID] by making sure the correct string representation
+// of the uuid is returned to the caller.
+func (volumeAttachmentPlanUUIDSuite) TestStringer(c *tc.C) {
+	const validUUID = "0de7ed80-bfcf-49b1-876a-31462e940ca1"
+	c.Check(VolumeAttachmentPlanUUID(validUUID).String(), tc.Equals, validUUID)
+}
+
+// TestValidate asserts that a valid uuid passes validation with no errors.
+func (volumeAttachmentPlanUUIDSuite) TestValidate(c *tc.C) {
+	const validUUID = "0de7ed80-bfcf-49b1-876a-31462e940ca1"
+	c.Check(VolumeAttachmentPlanUUID(validUUID).Validate(), tc.ErrorIsNil)
+}
+
+// TestValidateFail asserts that a bad uuid fails validation.
+func (volumeAttachmentPlanUUIDSuite) TestValidateFail(c *tc.C) {
+	c.Check(VolumeAttachmentPlanUUID("invalid").Validate(), tc.NotNil)
+}
