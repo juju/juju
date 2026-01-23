af5c41d75f127cf0ae364965a5a1ca2883606d76
Adis Azhar <adis.azhar@canonical.com>
2026-01-23 15:34:13 +0700

    fix: adjust logic to skip duplicates

--- domain/storage/modelmigration/import.go
+++ domain/storage/modelmigration/import.go
@@ -5,9 +5,12 @@ package modelmigration
 
 import (
 	"context"
+	"fmt"
+	"slices"
 
 	"github.com/juju/description/v11"
 
+	coreerrors "github.com/juju/juju/core/errors"
 	"github.com/juju/juju/core/logger"
 	"github.com/juju/juju/core/modelmigration"
 	corestorage "github.com/juju/juju/core/storage"
@@ -15,12 +18,26 @@ import (
 	"github.com/juju/juju/domain/storage/service"
 	"github.com/juju/juju/domain/storage/state"
 	"github.com/juju/juju/internal/errors"
+	"github.com/juju/juju/internal/storage"
 )
 
 // Coordinator is the interface that is used to add operations to a migration.
 type Coordinator interface {
 	// Add adds the given operation to the migration.
 	Add(modelmigration.Operation)
+}
+
+type DefaultStoragePool struct {
+	UUID   domainstorage.StoragePoolUUID
+	Name   string
+	Origin domainstorage.StoragePoolOrigin
+	Type   string
+	Attrs  map[string]any
+}
+
+type ModelStoragePoolArg struct {
+	StorageKind     domainstorage.StorageKind
+	StoragePoolUUID domainstorage.StoragePoolUUID
 }
 
 // RegisterImport registers the import operations with the given coordinator.
@@ -34,12 +51,9 @@ func RegisterImport(coordinator Coordinator, storageRegistryGetter corestorage.M
 // ImportService provides a subset of the storage domain
 // service methods needed for storage pool import.
 type ImportService interface {
-	CreateStoragePool(
-		ctx context.Context,
-		name string,
-		providerType domainstorage.ProviderType,
-		attrs map[string]any,
-	) (domainstorage.StoragePoolUUID, error)
+	CreateStoragePool(ctx context.Context, UUID domainstorage.StoragePoolUUID,
+		name string, providerType storage.ProviderType, attrs service.PoolAttrs,
+		originID domainstorage.StoragePoolOrigin) error
 }
 
 type importOperation struct {
@@ -64,16 +78,182 @@ func (i *importOperation) Setup(scope modelmigration.Scope) error {
 
 // Execute the import on the storage pools contained in the model.
 func (i *importOperation) Execute(ctx context.Context, model description.Model) error {
-	for _, pool := range model.StoragePools() {
-		_, err := i.service.CreateStoragePool(
-			ctx,
-			pool.Name(),
-			domainstorage.ProviderType(pool.Provider()),
-			pool.Attributes(),
+	poolsToCreate := make([]DefaultStoragePool, 0)
+	// We first create the list of pools from the migrated models.
+	// This is to ensure that the user-defined pools from the import are chosen
+	// should the name conflicts with built-in pools.
+	for _, v := range model.StoragePools() {
+		poolsToCreate = append(poolsToCreate, DefaultStoragePool{
+			Name:   v.Name(),
+			Origin: domainstorage.StoragePoolOriginUser,
+			Type:   v.Provider(),
+			Attrs:  v.Attributes(),
+		})
+	}
+
+	modelStorageRegistry, err := i.storageRegistryGetter.GetStorageRegistry(ctx)
+	if err != nil {
+		return errors.Errorf(
+			"getting storage provider registry for model: %w", err,
 		)
+	}
+
+	providerTypes, err := modelStorageRegistry.StorageProviderTypes()
+	if err != nil {
+		return errors.Errorf(
+			"getting storage provider types for model storage registry: %w", err,
+		)
+	}
+
+	for _, providerType := range providerTypes {
+		registry, err := modelStorageRegistry.StorageProvider(providerType)
 		if err != nil {
-			return errors.Errorf("creating storage pool %q: %w", pool.Name(), err)
+			return errors.Errorf(
+				"getting storage provider %q from registry: %w",
+				providerType, err,
+			)
+		}
+
+		providerDefaultPools := registry.DefaultPools()
+		for _, providerDefaultPool := range providerDefaultPools {
+			err := i.appendToPoolsForCreation(ctx, &poolsToCreate, providerDefaultPool)
+			if err != nil {
+				return err
+			}
 		}
 	}
+
+	for _, pool := range poolsToCreate {
+		err := i.service.CreateStoragePool(ctx, pool.UUID, pool.Name, storage.ProviderType(pool.Type),
+			pool.Attrs, pool.Origin)
+		if err != nil {
+			return errors.Errorf("creating storage pool %q: %w", pool.Name, err)
+		}
+	}
+
 	return nil
+}
+
+func (i *importOperation) appendToPoolsForCreation(
+	ctx context.Context,
+	existingPools *[]DefaultStoragePool,
+	config *storage.Config) error {
+	// A storage pool with a duplicate provider and name already exists.
+	// We skip adding it to the slice.
+	if slices.ContainsFunc(*existingPools, func(pool DefaultStoragePool) bool {
+		return pool.Name == config.Name() && pool.Type == config.Provider().String()
+	}) {
+		return nil
+	}
+	name := config.Name()
+	provider := config.Provider().String()
+	uuid, err := domainstorage.GetProviderDefaultStoragePoolUUID(
+		name, provider)
+
+	// Logic carried over from [SeedDefaultStoragePools] func
+	// in [github.com/juju/juju/domain/model/service.ProviderModelService].
+	if errors.Is(err, coreerrors.NotFound) {
+		// This happens when the default pool is not supported yet by the
+		// storage domain. This shouldn't stop the model from being created.
+		// Instead we log the problem.
+		i.logger.Warningf(
+			ctx,
+			"storage provider %q default pool %q is not recognised, adding to model with generated uuid.",
+			provider,
+			name,
+		)
+		return nil
+	} else if err != nil {
+		return fmt.Errorf(
+			"getting storage pool uuid for default provider %q pool %q",
+			provider,
+			name,
+		)
+	}
+	// The built-in pool doesn't conflict with the user-defined pools, it's save
+	// to append for later creation.
+	*existingPools = append(*existingPools, DefaultStoragePool{
+		UUID:   uuid,
+		Name:   name,
+		Origin: domainstorage.StoragePoolOriginProviderDefault,
+		Type:   provider,
+		Attrs:  config.Attrs(),
+	})
+	return nil
+}
+
+func getRecommendedStoragePools(existingPools *[]DefaultStoragePool, reg storage.ProviderRegistry) ([]ModelStoragePoolArg, error) {
+	recommendedVals := make([]ModelStoragePoolArg, 0)
+	appendPool := func(cfg *storage.Config) (domainstorage.StoragePoolUUID, error) {
+		// Get the UUID of the given pool so that we can later
+		// check for duplication within [existingPools].
+		uuid, err := domainstorage.GenerateProviderDefaultStoragePoolUUIDWithDefaults(
+			cfg.Name(),
+			cfg.Provider().String(),
+		)
+		if err != nil {
+			return "", errors.Capture(err)
+		}
+
+		// Duplication checking is performed on uuid and then name and provider
+		// type.
+		index := slices.IndexFunc(*existingPools, func(e DefaultStoragePool) bool {
+			return e.UUID == uuid
+		},
+		)
+		// The given pool exists in [existingPools]. We don't want to add a duplicate
+		// so return early.
+		if index != -1 {
+			return (*existingPools)[index].UUID, nil
+		}
+		// We don't want to add it for creation if there exists an existing pool
+		// with duplicate name and provider. This may have been a user-defined
+		// pool from the source controller.
+		if slices.ContainsFunc(*existingPools, func(pool DefaultStoragePool) bool {
+			return pool.Name == cfg.Name() && pool.Type == cfg.Provider().String()
+		}) {
+			return "", nil
+		}
+		*existingPools = append(*existingPools, DefaultStoragePool{
+			UUID:   uuid,
+			Name:   cfg.Name(),
+			Origin: domainstorage.StoragePoolOriginProviderDefault,
+			Type:   cfg.Provider().String(),
+			Attrs:  cfg.Attrs(),
+		})
+		return uuid, nil
+	}
+
+	// Get filesystem recommendation.
+	poolCfg := reg.RecommendedPoolForKind(storage.StorageKindFilesystem)
+	if poolCfg != nil {
+
+		uuid, err := appendPool(poolCfg)
+		if err != nil {
+			return nil, errors.Capture(err)
+		}
+		if uuid != "" {
+			recommendedVals = append(recommendedVals, ModelStoragePoolArg{
+				StorageKind:     domainstorage.StorageKindFilesystem,
+				StoragePoolUUID: uuid,
+			})
+		}
+	}
+
+	// Get block device recommendation.
+	poolCfg = reg.RecommendedPoolForKind(storage.StorageKindBlock)
+	if poolCfg != nil {
+		uuid, err := appendPool(poolCfg)
+		if err != nil {
+			return nil, errors.Capture(err)
+		}
+		if uuid != "" {
+			recommendedVals = append(recommendedVals, ModelStoragePoolArg{
+				StorageKind:     domainstorage.StorageKindBlock,
+				StoragePoolUUID: uuid,
+			})
+		}
+	}
+
+	return recommendedVals, nil
 }
