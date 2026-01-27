f536bcdd15e4c4fda472ff41e9c2a86e4631aa94
Guillaume Fouillet <guillaume.fouillet@canonical.com>
2026-01-27 11:27:31 +0100

    refactor(schema): prevent changing triggers from older models
    Replaced the auto-generated `ChangeLogTriggersForSecretRevision` function with a custom
    implementation. Removed redundancy and updated the schema to streamline secret revision trigger
    management.

--- domain/schema/model.go
+++ domain/schema/model.go
@@ -16,7 +16,7 @@ import (
 //go:generate go run ./../../generate/triggergen -db=model -destination=./model/triggers/blockdevice-triggers.gen.go -package=triggers -tables=block_device
 //go:generate go run ./../../generate/triggergen -db=model -destination=./model/triggers/model-triggers.gen.go -package=triggers -tables=model_config
 //go:generate go run ./../../generate/triggergen -db=model -destination=./model/triggers/objectstore-triggers.gen.go -package=triggers -tables=object_store_metadata_path
-//go:generate go run ./../../generate/triggergen -db=model -destination=./model/triggers/secret-triggers.gen.go -package=triggers -tables=secret_metadata,secret_rotation,secret_revision,secret_revision_expire,secret_revision_obsolete,secret_reference,secret_deleted_value_ref
+//go:generate go run ./../../generate/triggergen -db=model -destination=./model/triggers/secret-triggers.gen.go -package=triggers -tables=secret_metadata,secret_rotation,secret_revision_expire,secret_revision_obsolete,secret_reference,secret_deleted_value_ref
 //go:generate go run ./../../generate/triggergen -db=model -destination=./model/triggers/network-triggers.gen.go -package=triggers -tables=subnet,ip_address
 //go:generate go run ./../../generate/triggergen -db=model -destination=./model/triggers/machine-triggers.gen.go -package=triggers -tables=machine,machine_lxd_profile
 //go:generate go run ./../../generate/triggergen -db=model -destination=./model/triggers/machine-cloud-instance-triggers.gen.go -package=triggers -tables=machine_cloud_instance
@@ -174,7 +174,7 @@ func ModelDDLForVersion(version semversion.Number) *schema.Schema {
 		triggers.ChangeLogTriggersForSecretRotation("secret_id", tableSecretRotation),
 		triggers.ChangeLogTriggersForSecretRevisionObsolete("revision_uuid", tableSecretRevisionObsolete),
 		triggers.ChangeLogTriggersForSecretRevisionExpire("revision_uuid", tableSecretRevisionExpire),
-		triggers.ChangeLogTriggersForSecretRevision("uuid", tableSecretRevision),
+		changeLogTriggersForSecretRevision("uuid", tableSecretRevision),
 		triggers.ChangeLogTriggersForSecretReference("secret_id", tableSecretReference),
 		triggers.ChangeLogTriggersForSubnet("uuid", tableSubnet),
 		triggers.ChangeLogTriggersForMachine("uuid", tableMachine),
--- domain/schema/model/sql/0044-secret.PATCH.sql
+++ domain/schema/model/sql/0044-secret.PATCH.sql
@@ -12,3 +12,25 @@
 -- Secret revisions have update time too. A revision is updated when one of its fields is updated, even
 -- if the revision content is not changed.
 ALTER TABLE secret_revision ADD COLUMN update_time DATETIME; -- NOT NULL should be added on merge
+
+-- TODO on merge: reintroduce the generated trigger on secret_revision (check what has been removed in the commit
+--  which introduce comment)
+
+DROP TRIGGER trg_log_secret_revision_update;
+
+-- sqlfluff doesn't support TRIGGER statements
+-- noqa: disable=all
+CREATE TRIGGER trg_log_secret_revision_update
+AFTER UPDATE ON secret_revision FOR EACH ROW
+    WHEN
+        NEW.uuid != OLD.uuid OR
+        NEW.secret_id != OLD.secret_id OR
+        NEW.revision != OLD.revision OR
+        NEW.create_time != OLD.create_time OR
+        (NEW.update_time != OLD.update_time OR
+            (NEW.update_time IS NOT NULL AND OLD.update_time IS NULL) OR
+            (NEW.update_time IS NULL AND OLD.update_time IS NOT NULL))
+BEGIN
+    INSERT INTO change_log (edit_type_id, namespace_id, changed, created_at)
+    VALUES (2, 10007, "uuid", DATETIME('now', 'utc'));
+END;
