aa4f20da9a0f18fb181120cb9c9a703b11f1c19d
Adis Azhar <adis.azhar@canonical.com>
2026-02-02 22:15:01 +0700

    fix: rename table and delete column

--- domain/schema/model/sql/0046-application-storage-suffix.PATCH.sql
+++ domain/schema/model/sql/0046-application-storage-suffix.PATCH.sql
@@ -1,12 +1,11 @@
--- The application_provider_storage_id table stores information about a
+-- The application_storage_suffix table stores information about a
 -- migrated CAAS application's storage unique ID from prior Juju 4.0 versions.
 -- For new applications deployed on 4.0, the application will have no entry here.
-CREATE TABLE application_provider_storage_id (
+CREATE TABLE application_storage_suffix (
     application_uuid TEXT NOT NULL,
-    storage_name TEXT NOT NULL,
     storage_unique_id TEXT NOT NULL,
-    PRIMARY KEY (storage_name, storage_unique_id),
-    CONSTRAINT fk_application_provider_storage_id_application
+    PRIMARY KEY (application_uuid, storage_unique_id),
+    CONSTRAINT fk_application_storage_suffix_application
     FOREIGN KEY (application_uuid)
     REFERENCES application (uuid)
 )
