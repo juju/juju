86fd91ba3b19b2ea7055ac60d2ffb67662ce8a05
Joseph Phillips <joe.phillips@canonical.com>
2026-02-02 17:58:32 +0100

    feat: add supportsNetworking to network service
    This returns whether a provider implements the EnvironsNetworking
    interface.
    
    A locking strategy ensures we only ask the provider once. There's no
    need to ask again, as this fact does not change at runtime.

--- domain/network/service/service.go
+++ domain/network/service/service.go
@@ -4,8 +4,13 @@
 package service
 
 import (
+	"context"
+	"sync"
+
+	coreerrors "github.com/juju/juju/core/errors"
 	"github.com/juju/juju/core/logger"
 	"github.com/juju/juju/core/providertracker"
+	"github.com/juju/juju/internal/errors"
 )
 
 // Service provides the API for working with the network domain.
@@ -27,6 +32,9 @@ type ProviderService struct {
 	Service
 	providerWithNetworking providertracker.ProviderGetter[ProviderWithNetworking]
 	providerWithZones      providertracker.ProviderGetter[ProviderWithZones]
+
+	supportsNetworkingMu     sync.RWMutex
+	cachedSupportsNetworking *bool
 }
 
 // NewProviderService returns a new service reference wrapping the input state.
@@ -44,4 +52,37 @@ func NewProviderService(
 		providerWithNetworking: providerWithNetworking,
 		providerWithZones:      providerWithZones,
 	}
+}
+
+// supportsNetworking reports if the provider supports networking.
+// We effectively ask this question of the provider only once,
+// caching the result - it can never change during runtime.
+func (s *ProviderService) supportsNetworking(ctx context.Context) (bool, error) {
+	s.supportsNetworkingMu.RLock()
+	if s.cachedSupportsNetworking != nil {
+		defer s.supportsNetworkingMu.RUnlock()
+		return *s.cachedSupportsNetworking, nil
+	}
+	s.supportsNetworkingMu.RUnlock()
+
+	s.supportsNetworkingMu.Lock()
+	defer s.supportsNetworkingMu.Unlock()
+
+	// Re-check after taking the write lock in case another goroutine won the race.
+	if s.cachedSupportsNetworking != nil {
+		return *s.cachedSupportsNetworking, nil
+	}
+
+	provider, err := s.providerWithNetworking(ctx)
+	if errors.Is(err, coreerrors.NotSupported) {
+		supported := false
+		s.cachedSupportsNetworking = &supported
+		return false, nil
+	} else if err != nil {
+		return false, errors.Capture(err)
+	}
+
+	supported := provider != nil
+	s.cachedSupportsNetworking = &supported
+	return supported, nil
 }
