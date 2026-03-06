2406b65116c525dbf8c33c7272c0365d9ddfc345
Nicolas Vinuesa <vinuesa.nico@gmail.com>
2026-03-06 11:35:26 +0100

    fix: inject JWTAuthenticator test to use explicit authenticator instance

--- internal/worker/apiserver/worker_state_test.go
+++ internal/worker/apiserver/worker_state_test.go
@@ -12,10 +12,12 @@ import (
 	"go.uber.org/mock/gomock"
 
 	coreapiserver "github.com/juju/juju/apiserver"
+	"github.com/juju/juju/apiserver/authentication/jwt"
 	apitesting "github.com/juju/juju/apiserver/testing"
 	"github.com/juju/juju/core/auditlog"
 	"github.com/juju/juju/core/flightrecorder"
 	"github.com/juju/juju/core/model"
+	"github.com/juju/juju/internal/jwtparser"
 	coretesting "github.com/juju/juju/internal/testing"
 	"github.com/juju/juju/internal/worker/apiserver"
 )
@@ -109,9 +111,7 @@ func (s *WorkerStateSuite) TestStart(c *tc.C) {
 	// compare it.
 	config.GetAuditConfig = nil
 
-	c.Assert(config.JWTAuthenticator, tc.NotNil)
-	config.JWTAuthenticator = nil
-
+	jwtAuthenticator := jwt.NewAuthenticator(&jwtparser.Parser{})
 	logSinkConfig := coreapiserver.DefaultLogSinkConfig()
 
 	c.Assert(config, tc.DeepEquals, coreapiserver.ServerConfig{
@@ -137,6 +137,7 @@ func (s *WorkerStateSuite) TestStart(c *tc.C) {
 		ObjectStoreGetter:          s.objectStoreGetter,
 		ControllerUUID:             s.controllerUUID,
 		ControllerModelUUID:        s.controllerModelUUID,
+		JWTAuthenticator:           jwtAuthenticator,
 		WatcherRegistryGetter:      s.watcherRegistryGetter,
 	})
 }
