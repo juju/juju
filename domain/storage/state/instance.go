bcac49676a71c6d4e21c313891235f67c89bd2bd
Thomas Miller <thomas.miller@canonical.com>
2026-03-12 04:15:48 +0000

    refactor: split fetching and packing of storage instance info data
    Separates fetch and packing for Storage Instance info data. This was
    done to make the functions a little less busy and easier to reason
    about.

--- domain/storage/state/instance.go
+++ domain/storage/state/instance.go
@@ -237,6 +237,118 @@ FROM (
 		return internal.StorageInstanceInfo{}, err
 	}
 
+	return makeStorageInstanceInfoFromDBData(
+		siInfo, attachments, attachmentBlockDeviceLinks,
+	), nil
+}
+
+// GetStorageInstanceUUIDByID retrieves the UUID of a storage instance by
+// its ID.
+//
+// The following errors may be returned:
+// - [domainstorageerrors.StorageInstanceNotFound] when no storage
+// instance exists for the provided ID.
+func (s *State) GetStorageInstanceUUIDByID(
+	ctx context.Context, storageID string,
+) (domainstorage.StorageInstanceUUID, error) {
+	db, err := s.DB(ctx)
+	if err != nil {
+		return "", errors.Capture(err)
+	}
+
+	var (
+		input = storageInstanceID{ID: storageID}
+		dbVal entityUUID
+	)
+
+	stmt, err := s.Prepare(`
+SELECT &entityUUID.*
+FROM   storage_instance
+WHERE  storage_id = $storageInstanceID.storage_id`,
+		input, dbVal,
+	)
+	if err != nil {
+		return "", errors.Capture(err)
+	}
+
+	err = db.Txn(ctx, func(ctx context.Context, tx *sqlair.TX) error {
+		err := tx.Query(ctx, stmt, input).Get(&dbVal)
+		if errors.Is(err, sqlair.ErrNoRows) {
+			return errors.Errorf(
+				"storage instance with ID %q does not exist", storageID,
+			).Add(domainstorageerrors.StorageInstanceNotFound)
+		}
+		return err
+	})
+	if err != nil {
+		return "", errors.Capture(err)
+	}
+
+	return domainstorage.StorageInstanceUUID(dbVal.UUID), nil
+}
+
+// GetStorageInstanceUUIDsByIDs retrieves the UUIDs of storage instances by
+// their IDs.
+func (s *State) GetStorageInstanceUUIDsByIDs(
+	ctx context.Context,
+	storageIDs []string,
+) (map[string]domainstorage.StorageInstanceUUID, error) {
+	if len(storageIDs) == 0 {
+		return map[string]domainstorage.StorageInstanceUUID{}, nil
+	}
+
+	db, err := s.DB(ctx)
+	if err != nil {
+		return nil, errors.Capture(err)
+	}
+
+	// De-dupe input.
+	storageInstanceIDs := storageInstanceIDs(
+		slices.Compact(slices.Sorted(slices.Values(storageIDs))),
+	)
+
+	stmt, err := s.Prepare(`
+SELECT &storageInstanceUUIDAndID.*
+FROM   storage_instance
+WHERE  storage_id IN ($storageInstanceIDs[:])`,
+		storageInstanceUUIDAndID{}, storageInstanceIDs,
+	)
+	if err != nil {
+		return nil, errors.Capture(err)
+	}
+
+	var dbVals []storageInstanceUUIDAndID
+	err = db.Txn(ctx, func(ctx context.Context, tx *sqlair.TX) error {
+		err := tx.Query(ctx, stmt, storageInstanceIDs).GetAll(&dbVals)
+		if errors.Is(err, sqlair.ErrNoRows) {
+			return nil
+		}
+		return err
+	})
+	if err != nil {
+		return nil, errors.Capture(err)
+	}
+
+	result := make(map[string]domainstorage.StorageInstanceUUID, len(dbVals))
+	for _, val := range dbVals {
+		result[val.ID] = domainstorage.StorageInstanceUUID(val.UUID)
+	}
+
+	return result, nil
+}
+
+// makeStorageInstanceInfoFromDBData is responsible for taking the returned
+// database data from [state.GetStorageInstanceInfo] and constructing a
+// [internal.StorageInstanceInfo] result.
+//
+// This function is only designed to work with the data returned from
+// [state.GetStorageInstanceInfo] and expects that attachments and
+// attachmentBlockDeviceLinks have been ordered by Storage Attachment UUID.
+func makeStorageInstanceInfoFromDBData(
+	siInfo storageInstanceInfo,
+	attachments []storageInstanceInfoAttachment,
+	attachmentBlockDeviceLinks []storageInstanceInfoAttachmentBlockDeviceLink,
+) internal.StorageInstanceInfo {
 	retVal := internal.StorageInstanceInfo{
 		Attachments: make([]internal.StorageInstanceInfoAttachment, 0, len(attachments)),
 		Life:        domainlife.Life(siInfo.LifeID),
@@ -334,100 +446,5 @@ FROM (
 		retVal.Attachments = append(retVal.Attachments, val)
 	}
 
-	return retVal, nil
-}
-
-// GetStorageInstanceUUIDByID retrieves the UUID of a storage instance by
-// its ID.
-//
-// The following errors may be returned:
-// - [domainstorageerrors.StorageInstanceNotFound] when no storage
-// instance exists for the provided ID.
-func (s *State) GetStorageInstanceUUIDByID(
-	ctx context.Context, storageID string,
-) (domainstorage.StorageInstanceUUID, error) {
-	db, err := s.DB(ctx)
-	if err != nil {
-		return "", errors.Capture(err)
-	}
-
-	var (
-		input = storageInstanceID{ID: storageID}
-		dbVal entityUUID
-	)
-
-	stmt, err := s.Prepare(`
-SELECT &entityUUID.*
-FROM   storage_instance
-WHERE  storage_id = $storageInstanceID.storage_id`,
-		input, dbVal,
-	)
-	if err != nil {
-		return "", errors.Capture(err)
-	}
-
-	err = db.Txn(ctx, func(ctx context.Context, tx *sqlair.TX) error {
-		err := tx.Query(ctx, stmt, input).Get(&dbVal)
-		if errors.Is(err, sqlair.ErrNoRows) {
-			return errors.Errorf(
-				"storage instance with ID %q does not exist", storageID,
-			).Add(domainstorageerrors.StorageInstanceNotFound)
-		}
-		return err
-	})
-	if err != nil {
-		return "", errors.Capture(err)
-	}
-
-	return domainstorage.StorageInstanceUUID(dbVal.UUID), nil
-}
-
-// GetStorageInstanceUUIDsByIDs retrieves the UUIDs of storage instances by
-// their IDs.
-func (s *State) GetStorageInstanceUUIDsByIDs(
-	ctx context.Context,
-	storageIDs []string,
-) (map[string]domainstorage.StorageInstanceUUID, error) {
-	if len(storageIDs) == 0 {
-		return map[string]domainstorage.StorageInstanceUUID{}, nil
-	}
-
-	db, err := s.DB(ctx)
-	if err != nil {
-		return nil, errors.Capture(err)
-	}
-
-	// De-dupe input.
-	storageInstanceIDs := storageInstanceIDs(
-		slices.Compact(slices.Sorted(slices.Values(storageIDs))),
-	)
-
-	stmt, err := s.Prepare(`
-SELECT &storageInstanceUUIDAndID.*
-FROM   storage_instance
-WHERE  storage_id IN ($storageInstanceIDs[:])`,
-		storageInstanceUUIDAndID{}, storageInstanceIDs,
-	)
-	if err != nil {
-		return nil, errors.Capture(err)
-	}
-
-	var dbVals []storageInstanceUUIDAndID
-	err = db.Txn(ctx, func(ctx context.Context, tx *sqlair.TX) error {
-		err := tx.Query(ctx, stmt, storageInstanceIDs).GetAll(&dbVals)
-		if errors.Is(err, sqlair.ErrNoRows) {
-			return nil
-		}
-		return err
-	})
-	if err != nil {
-		return nil, errors.Capture(err)
-	}
-
-	result := make(map[string]domainstorage.StorageInstanceUUID, len(dbVals))
-	for _, val := range dbVals {
-		result[val.ID] = domainstorage.StorageInstanceUUID(val.UUID)
-	}
-
-	return result, nil
+	return retVal
 }
