6e5c0424b00c82d132367327324daff1a890e375
Joseph Phillips <joe.phillips@canonical.com>
2026-03-12 14:02:15 +0100

    refactor: move machiner tests into machiner package
    This allows us to drop export_test.go.

--- internal/worker/machiner/export_test.go
+++
@@ -1,8 +0,0 @@
-// Copyright 2026 Canonical Ltd.
-// Licensed under the AGPLv3, see LICENCE file for details.
-
-package machiner
-
-// NewAddressChangeWatcherForTest exposes the local address watcher factory for
-// patching in external tests.
-var NewAddressChangeWatcherForTest = &newAddressChangeWatcher
