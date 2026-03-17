61a60d33ae92c48d8f9c35ad2dd171935e19d98c
Heather Lanigan <heather.lanigan@canonical.com>
2026-03-17 17:46:01 +0000

    fix: update v_application_resource to its name signifies
    Based on the name, the view should return resources which have
    applications. With the left join on applications it does not
    necessarily do that. Update the view to be a join instead.

--- domain/schema/model.go
+++ domain/schema/model.go
@@ -156,6 +156,7 @@ var modelPostPatchFilesByVersion = []struct {
 		"0049-object-store-node-id.PATCH.sql",
 		"0050-secret.PATCH.sql",
 		"0051-secret-removal.PATCH.sql",
+		"0052-resource.PATCH.sql",
 	},
 }}
 
