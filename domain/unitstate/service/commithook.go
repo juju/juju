3b4ccf1e90966d00a263599f06ebd8abad2d1a24
Heather Lanigan <heather.lanigan@canonical.com>
2026-03-18 13:52:06 +0000

    refactor: split commit hook and unit state functionality
    The two sets of functionality are now split into different files and
    interfaces. Any overlap in functionality happens at the state layer.
    
    The commit hook pieces will grow over time, start with different files
    now.

---
+++ domain/unitstate/service/commithook.go
@@ -0,0 +1,50 @@
+// Copyright 2026 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package service
+
+import (
+	"context"
+
+	"github.com/juju/juju/core/trace"
+	"github.com/juju/juju/domain/unitstate"
+	"github.com/juju/juju/domain/unitstate/internal"
+)
+
+// CommitHookChanges persists a set of changes after a hook successfully
+// completes and executes them in a single transaction.
+func (s *LeadershipService) CommitHookChanges(ctx context.Context, arg unitstate.CommitHookChangesArg) error {
+	ctx, span := trace.Start(ctx, trace.NameFromFunc())
+	defer span.End()
+
+	hasChanges, err := arg.ValidateAndHasChanges()
+	if err != nil {
+		return err
+	}
+	if !hasChanges {
+		return nil
+	}
+
+	withCaveat, err := s.getManagementCaveat(arg)
+	if err != nil {
+		return err
+	}
+	return withCaveat(ctx, func(innerCtx context.Context) error {
+		return s.st.CommitHookChanges(innerCtx, internal.TransformCommitHookChangesArg(arg))
+	})
+}
+
+func (s *LeadershipService) getManagementCaveat(arg unitstate.CommitHookChangesArg) (func(context.Context, func(context.Context) error) error, error) {
+	if arg.RequiresLeadership() {
+		return func(ctx context.Context, fn func(context.Context) error) error {
+			return s.leaderEnsurer.WithLeader(ctx, arg.UnitName.Application(), arg.UnitName.String(),
+				func(ctx context.Context) error {
+					return fn(ctx)
+				},
+			)
+		}, nil
+	}
+	return func(ctx context.Context, fn func(context.Context) error) error {
+		return fn(ctx)
+	}, nil
+}
--- domain/unitstate/service/unitstate_test.go
+++ domain/unitstate/service/unitstate_test.go
@@ -9,12 +9,10 @@ import (
 	"github.com/juju/tc"
 	"go.uber.org/mock/gomock"
 
-	corerelation "github.com/juju/juju/core/relation"
 	unittesting "github.com/juju/juju/core/unit/testing"
 	"github.com/juju/juju/domain/application/errors"
 	"github.com/juju/juju/domain/unitstate"
 	unitstateerrors "github.com/juju/juju/domain/unitstate/errors"
-	"github.com/juju/juju/domain/unitstate/internal"
 	loggertesting "github.com/juju/juju/internal/logger/testing"
 )
 
@@ -90,84 +88,6 @@ func (s *serviceSuite) setupMocks(c *tc.C) *gomock.Controller {
 	s.st = NewMockState(ctrl)
 
 	c.Cleanup(func() { s.st = nil })
-
-	return ctrl
-}
-
-type commitHookSuite struct {
-	st                *MockState
-	leadershipEnsurer *MockEnsurer
-}
-
-func TestCommitHookSuite(t *testing.T) {
-	tc.Run(t, &commitHookSuite{})
-}
-
-func (s *commitHookSuite) TestCommitHookChangesNoChanges(c *tc.C) {
-	defer s.setupMocks(c).Finish()
-	// Arrange: args which no changes are needed
-	arg := unitstate.CommitHookChangesArg{
-		UnitName: unittesting.GenNewName(c, "test/0"),
-	}
-
-	// Act
-	svc := NewLeadershipService(s.st, s.leadershipEnsurer, loggertesting.WrapCheckLog(c))
-	err := svc.CommitHookChanges(c.Context(), arg)
-
-	// Assert
-	c.Assert(err, tc.ErrorIsNil)
-}
-
-func (s *commitHookSuite) TestCommitHookChangesNoLeadership(c *tc.C) {
-	defer s.setupMocks(c).Finish()
-	// Arrange: args which indicate leadership is not required
-	arg := unitstate.CommitHookChangesArg{
-		UnitName: unittesting.GenNewName(c, "test/0"),
-		RelationSettings: []unitstate.RelationSettings{{
-			RelationUUID: tc.Must(c, corerelation.NewUUID),
-			Settings:     map[string]string{"key": "value"},
-		}},
-	}
-	s.st.EXPECT().CommitHookChanges(c.Context(), internal.TransformCommitHookChangesArg(arg)).Return(nil)
-
-	// Act
-	svc := NewLeadershipService(s.st, s.leadershipEnsurer, loggertesting.WrapCheckLog(c))
-	err := svc.CommitHookChanges(c.Context(), arg)
-
-	// Assert
-	c.Assert(err, tc.ErrorIsNil)
-}
-
-func (s *commitHookSuite) TestCommitHookChangesLeadership(c *tc.C) {
-	defer s.setupMocks(c).Finish()
-	// Arrange: args which indicate leadership is required
-	arg := unitstate.CommitHookChangesArg{
-		UnitName: unittesting.GenNewName(c, "test/0"),
-		RelationSettings: []unitstate.RelationSettings{{
-			RelationUUID:        tc.Must(c, corerelation.NewUUID),
-			ApplicationSettings: map[string]string{"key": "value"},
-		}},
-	}
-	s.leadershipEnsurer.EXPECT().WithLeader(c.Context(), "test", "test/0", gomock.Any()).Return(nil)
-
-	// Act
-	svc := NewLeadershipService(s.st, s.leadershipEnsurer, loggertesting.WrapCheckLog(c))
-	err := svc.CommitHookChanges(c.Context(), arg)
-
-	// Assert
-	c.Assert(err, tc.ErrorIsNil)
-}
-
-func (s *commitHookSuite) setupMocks(c *tc.C) *gomock.Controller {
-	ctrl := gomock.NewController(c)
-
-	s.st = NewMockState(ctrl)
-	s.leadershipEnsurer = NewMockEnsurer(ctrl)
-
-	c.Cleanup(func() {
-		s.st = nil
-		s.leadershipEnsurer = nil
-	})
 
 	return ctrl
 }
