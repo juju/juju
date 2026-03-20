f94e721da2a79bd580aa6925f7cadcc5c7a2caff
Simon Richardson <stickupkid@gmail.com>
2026-03-20 15:02:38 +0000

    chore: remove deadcode and fix doc.go

--- domain/tracing/doc.go
+++ domain/tracing/doc.go
@@ -1,6 +1,6 @@
 // Copyright 2026 Canonical Ltd.
 // Licensed under the AGPLv3, see LICENCE file for details.
-//
+
 // Package tracing holds information about the tracing state and service. The
 // tracing domain contains methods to handle the core tracing values that do not
 // fall into the category of tracing config.
