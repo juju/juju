79b5f51014c7032e34bcf6facaa6583f144e4c7c
Harry Pidcock <harry.pidcock@canonical.com>
2026-03-23 20:38:27 +1000

    fix: retry k8s secret calls on expected errors

--- secrets/provider/kubernetes/backend.go
+++ secrets/provider/kubernetes/backend.go
@@ -6,8 +6,12 @@ package kubernetes
 import (
 	"context"
 	"encoding/base64"
+	"math"
+	"time"
 
+	"github.com/juju/clock"
 	"github.com/juju/errors"
+	"github.com/juju/retry"
 	core "k8s.io/api/core/v1"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -24,6 +28,7 @@ type k8sBackend struct {
 	modelUUID      string
 
 	client kubernetes.Interface
+	clock  clock.Clock
 }
 
 // Ping implements SecretsBackend.
@@ -49,15 +54,37 @@ func (k *k8sBackend) Ping() (err error) {
 }
 
 // getSecret returns a secret resource.
-func (k *k8sBackend) getSecret(ctx context.Context, secretName string) (*core.Secret, error) {
+func (k *k8sBackend) getSecret(ctx context.Context, secretName string) (*resources.Secret, error) {
 	if k.namespace == "" {
 		return nil, errNoNamespace
 	}
-	secret, err := k.client.CoreV1().Secrets(k.namespace).Get(ctx, secretName, v1.GetOptions{})
-	if err != nil {
-		if k8serrors.IsNotFound(err) {
-			return nil, errors.NotFoundf("secret %q", secretName)
-		}
+	secret := resources.NewSecret(k.client.CoreV1().Secrets(k.namespace), k.namespace, secretName, nil)
+	err := retry.Call(retry.CallArgs{
+		Func: func() error {
+			return secret.Get(ctx)
+		},
+		IsFatalError: isFatalError,
+		NotifyFunc: func(lastError error, attempt int) {
+			logFn := logger.Warningf
+			if attempt <= 1 {
+				// It is expected that this may fail at least once due to etcd
+				// eventual consistency, silence the first warning.
+				logFn = logger.Debugf
+			}
+			logFn(
+				"error getting secret (attempt %d): %s",
+				attempt, lastError.Error(),
+			)
+		},
+		Attempts:    10,
+		Delay:       time.Second,
+		BackoffFunc: retry.ExpBackoff(time.Second, time.Minute, math.Phi, true),
+		Clock:       k.clock,
+		Stop:        ctx.Done(),
+	})
+	if k8serrors.IsNotFound(err) {
+		return nil, errors.NotFoundf("secret %q", secretName)
+	} else if err != nil {
 		return nil, errors.Trace(err)
 	}
 	return secret, nil
@@ -100,7 +127,30 @@ func (k *k8sBackend) SaveContent(ctx context.Context, uri *coresecrets.URI, revi
 		return "", errors.Trace(err)
 	}
 	secret := resources.NewSecret(k.client.CoreV1().Secrets(k.namespace), k.namespace, name, in)
-	if err = secret.Apply(ctx); err != nil {
+	err = retry.Call(retry.CallArgs{
+		Func: func() error {
+			return secret.Apply(ctx)
+		},
+		IsFatalError: isFatalError,
+		NotifyFunc: func(lastError error, attempt int) {
+			logFn := logger.Warningf
+			if attempt <= 1 {
+				// It is expected that this may fail at least once due to etcd
+				// eventual consistency, silence the first warning.
+				logFn = logger.Debugf
+			}
+			logFn(
+				"error saving secret content (attempt %d): %s",
+				attempt, lastError.Error(),
+			)
+		},
+		Attempts:    10,
+		Delay:       time.Second,
+		BackoffFunc: retry.ExpBackoff(time.Second, time.Minute, math.Phi, true),
+		Clock:       k.clock,
+		Stop:        ctx.Done(),
+	})
+	if err != nil {
 		return "", errors.Trace(err)
 	}
 	return name, nil
@@ -118,5 +168,43 @@ func (k *k8sBackend) DeleteContent(ctx context.Context, revisionId string) (err 
 		logger.Tracef("deleting secret %q: %v", revisionId, err)
 		return errors.Trace(err)
 	}
-	return resources.NewSecret(k.client.CoreV1().Secrets(k.namespace), k.namespace, secret.Name, secret).Delete(ctx)
+
+	err = retry.Call(retry.CallArgs{
+		Func: func() error {
+			return secret.Delete(ctx)
+		},
+		IsFatalError: isFatalError,
+		NotifyFunc: func(lastError error, attempt int) {
+			logFn := logger.Warningf
+			if attempt <= 1 {
+				// It is expected that this may fail at least once due to etcd
+				// eventual consistency, silence the first warning.
+				logFn = logger.Debugf
+			}
+			logFn(
+				"error deleting secret content (attempt %d): %s",
+				attempt, lastError.Error(),
+			)
+		},
+		Attempts:    10,
+		Delay:       time.Second,
+		BackoffFunc: retry.ExpBackoff(time.Second, time.Minute, math.Phi, true),
+		Clock:       k.clock,
+		Stop:        ctx.Done(),
+	})
+	if err != nil {
+		return errors.Trace(err)
+	}
+
+	return nil
+}
+
+func isFatalError(err error) bool {
+	temporary := k8serrors.IsForbidden(err) ||
+		k8serrors.IsInternalError(err) ||
+		k8serrors.IsServiceUnavailable(err) ||
+		k8serrors.IsConflict(err) ||
+		k8serrors.IsTooManyRequests(err) ||
+		k8serrors.IsServerTimeout(err)
+	return !temporary
 }
