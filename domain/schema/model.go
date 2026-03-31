0113f2835fabf50843efb16c3424a6ae7c349eb3
Joseph Phillips <joe.phillips@canonical.com>
2026-03-31 13:59:32 +0200

    feat: add common view for unit relation networks
    This selects address, device and space info along with a ranking
    appropriate for returning determining unit relation ingress addresses.

--- domain/schema/model.go
+++ domain/schema/model.go
@@ -157,6 +157,7 @@ var modelPostPatchFilesByVersion = []struct {
 		"0050-secret.PATCH.sql",
 		"0051-secret-removal.PATCH.sql",
 		"0052-resource.PATCH.sql",
+		"0053-network.PATCH.sql",
 	},
 }}
 
