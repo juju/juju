a92d8ea5d308a62b00a24ca2876a67d50b0d38ef
Iyigun Cevik <iyigun.cevik@canonical.com>
2026-05-28 15:38:44 +0200

    chore: switch controller manifolds to explicit local values

--- internal/worker/querylogger/manifold_test.go
+++ internal/worker/querylogger/manifold_test.go
@@ -6,8 +6,10 @@ package querylogger
 import (
 	"testing"
 
+	"github.com/juju/clock"
 	"github.com/juju/errors"
 	"github.com/juju/tc"
+	"github.com/juju/worker/v5/workertest"
 )
 
 type manifoldSuite struct {
@@ -27,18 +29,28 @@ func (s *manifoldSuite) TestValidateConfig(c *tc.C) {
 	cfg.LogDir = ""
 	c.Check(cfg.Validate(), tc.ErrorIs, errors.NotValid)
 
-	cfg.Clock = nil
-	c.Check(cfg.Validate(), tc.ErrorIs, errors.NotValid)
-
 	cfg = s.getConfig()
 	cfg.Logger = nil
 	c.Check(cfg.Validate(), tc.ErrorIs, errors.NotValid)
 }
 
+func (s *manifoldSuite) TestStartConfig(c *tc.C) {
+	defer s.setupMocks(c).Finish()
+
+	cfg := s.getConfig()
+	cfg.LogDir = c.MkDir()
+	w, err := Manifold(cfg).Start(c.Context(), nil)
+	c.Assert(err, tc.ErrorIsNil)
+	defer workertest.CleanKill(c, w)
+
+	lw, ok := w.(*loggerWorker)
+	c.Assert(ok, tc.IsTrue)
+	c.Check(lw.clock, tc.Equals, clock.WallClock)
+}
+
 func (s *manifoldSuite) getConfig() ManifoldConfig {
 	return ManifoldConfig{
 		LogDir: "log dir",
-		Clock:  s.clock,
 		Logger: s.logger,
 	}
 }
