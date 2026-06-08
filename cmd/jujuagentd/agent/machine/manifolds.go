629dcbc0635c1392fc08598f5859ccffbe31ccc3
Iyigun Cevik <iyigun.cevik@canonical.com>
2026-06-08 18:41:39 +0200

    refactor(httpserverargs): replace ClockName with direct Clock field, wire clock.WallClock

--- cmd/jujuagentd/agent/machine/manifolds.go
+++ cmd/jujuagentd/agent/machine/manifolds.go
@@ -636,7 +636,7 @@ func commonManifolds(config ManifoldsConfig) dependency.Manifolds {
 		}),
 
 		httpServerArgsName: ifBootstrapComplete(httpserverargs.Manifold(httpserverargs.ManifoldConfig{
-			ClockName:             clockName,
+			Clock:                 clock.WallClock,
 			DomainServicesName:    domainServicesName,
 			NewStateAuthenticator: httpserverargs.NewStateAuthenticator,
 		})),
