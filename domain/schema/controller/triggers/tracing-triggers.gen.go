7eeda4fa41f6aeefdc4b4eb6698e00ad1f59772b
Nicolas Vinuesa <vinuesa.nico@gmail.com>
2026-06-09 20:47:56 +0200

    chore: regenerate triggers after forward merge

--- domain/schema/controller/triggers/tracing-triggers.gen.go
+++ domain/schema/controller/triggers/tracing-triggers.gen.go
@@ -30,7 +30,7 @@ CREATE TRIGGER trg_log_workload_tracing_config_update
 AFTER UPDATE ON workload_tracing_config FOR EACH ROW
 WHEN 
 	NEW.key != OLD.key OR
-	NEW.value != OLD.value 
+	NEW.value != OLD.value
 BEGIN
     INSERT INTO change_log (edit_type_id, namespace_id, changed, created_at)
     VALUES (2, %[2]d, OLD.%[1]s, DATETIME('now', 'utc'));
