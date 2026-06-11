21e2eedd3f47d3b4fd3d2e2a19a4cdd8b9dcd5b8
Nicolas Vinuesa <vinuesa.nico@gmail.com>
2026-06-11 16:21:02 +0200

    test: restore canonical gomock in merge-forward resolution

--- cmd/jujud/agent/package_test.go
+++ cmd/jujud/agent/package_test.go
@@ -8,7 +8,7 @@ import (
 	"testing"
 )
 
-//go:generate go run go.uber.org/mock/mockgen -typed -package mocks -destination mocks/machine_mock.go github.com/juju/juju/cmd/jujud/agent CommandRunner
+//go:generate go run github.com/canonical/gomock/mockgen -package mocks -destination mocks/machine_mock.go github.com/juju/juju/cmd/jujud/agent CommandRunner
 
 func TestMain(m *testing.M) {
 	os.Exit(m.Run())
--- internal/worker/controlsocket/package_test.go
+++ internal/worker/controlsocket/package_test.go
@@ -3,4 +3,4 @@
 
 package controlsocket
 
-//go:generate go run go.uber.org/mock/mockgen -typed -package controlsocket -destination services_mock_test.go github.com/juju/juju/internal/worker/controlsocket AccessService,TracingService,LoggingService,ControllerObjectStoreService
+//go:generate go run github.com/canonical/gomock/mockgen -package controlsocket -destination services_mock_test.go github.com/juju/juju/internal/worker/controlsocket AccessService,TracingService,LoggingService,ControllerObjectStoreService
