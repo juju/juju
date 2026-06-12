e38f588516b66ef3ea81db73e5f3f3ed34543ed9
Simon Richardson <stickupkid@gmail.com>
2026-06-12 10:37:56 +0100

    refactor: satisfy the doc.go format

--- internal/worker/lokiendpointupdater/worker.go
+++ internal/worker/lokiendpointupdater/worker.go
@@ -1,8 +1,6 @@
 // Copyright 2026 Canonical Ltd.
 // Licensed under the AGPLv3, see LICENCE file for details.
 
-// Package lokiendpointupdater updates the local agent config with the
-// controller-wide Loki endpoint configuration.
 package lokiendpointupdater
 
 import (
