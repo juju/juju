ba9e54f413371d80bdfa98dcd8b01b1bb81eebce
Iyigun Cevik <iyigun.cevik@canonical.com>
2026-06-12 14:19:04 +0200

    chore: removed unused facade

--- apiserver/facades/controller/externalcontrollerupdater/package_test.go
+++
@@ -1,7 +0,0 @@
-// Copyright 2017 Canonical Ltd.
-// Licensed under the AGPLv3, see LICENCE file for details.
-
-package externalcontrollerupdater_test
-
-//go:generate go run go.uber.org/mock/mockgen -typed -package externalcontrollerupdater_test -destination domain_mock_test.go github.com/juju/juju/apiserver/facades/controller/externalcontrollerupdater ExternalControllerService
-//go:generate go run go.uber.org/mock/mockgen -typed -package externalcontrollerupdater_test -destination watcher_mock_test.go github.com/juju/juju/core/watcher StringsWatcher
