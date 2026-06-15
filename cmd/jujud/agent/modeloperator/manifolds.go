763b72359e2725cd6e35268a784c38227d6695d9
Simon Richardson <stickupkid@gmail.com>
2026-06-15 18:01:59 +0100

    feat: wire up unit manifolds

--- cmd/jujud/agent/modeloperator/manifolds.go
+++ cmd/jujud/agent/modeloperator/manifolds.go
@@ -4,6 +4,9 @@
 package modeloperator
 
 import (
+	"net/http"
+
+	"github.com/juju/clock"
 	"github.com/juju/utils/v4/voyeur"
 	"github.com/juju/worker/v5/dependency"
 
@@ -22,6 +25,7 @@ import (
 	"github.com/juju/juju/internal/worker/caasupgrader"
 	"github.com/juju/juju/internal/worker/gate"
 	"github.com/juju/juju/internal/worker/logger"
+	"github.com/juju/juju/internal/worker/logrouter"
 	"github.com/juju/juju/internal/worker/logsender"
 	"github.com/juju/juju/internal/worker/muxhttpserver"
 )
@@ -87,12 +91,15 @@ func Manifolds(config ManifoldConfig) dependency.Manifolds {
 			Logger:               internallogger.GetLogger("juju.worker.apicaller"),
 		}),
 
-		// The log sender is a leaf worker that sends log messages to some
-		// API server, when configured so to do. We should only need one of
-		// these in a consolidated agent.
-		logSenderName: logsender.Manifold(logsender.ManifoldConfig{
-			APICallerName: apiCallerName,
-			LogSource:     config.LogSource,
+		// The log router owns the buffered log stream and forwards records to
+		// one active backend at a time.
+		logSenderName: logrouter.Manifold(logrouter.ManifoldConfig{
+			AgentName:          agentName,
+			LogSource:          config.LogSource,
+			AgentConfigChanged: config.AgentConfigChanged,
+			Logger:             internallogger.GetLogger("juju.worker.logrouter"),
+			Clock:              clock.WallClock,
+			HTTPClient:         http.DefaultClient,
 		}),
 
 		caasAdmissionName: caasadmission.Manifold(caasadmission.ManifoldConfig{
