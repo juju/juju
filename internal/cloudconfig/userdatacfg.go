6b28103397cf421c8778e9ecf8da6d162498dfc6
Iyigun Cevik <iyigun.cevik@canonical.com>
2026-06-18 16:58:57 +0200

    chore: add apiport and password to controllerruntimeconfig

--- internal/cloudconfig/userdatacfg.go
+++ internal/cloudconfig/userdatacfg.go
@@ -502,6 +502,8 @@ func (w *userdataConfig) configureBootstrap() error {
 		DataDir:                w.icfg.DataDir,
 		LoopbackPreferred:      false,
 		LogDir:                 w.icfg.LogDir,
+		APIPort:                w.icfg.Bootstrap.ControllerAgentInfo.APIPort,
+		AgentPassword:          w.icfg.APIInfo.Password,
 		LoggingConfig:          w.icfg.Bootstrap.ControllerModelConfig.LoggingConfig(),
 		LoggingOverride:        w.icfg.AgentEnvironment[agent.LoggingOverride],
 		QueryTracingEnabled:    w.icfg.ControllerConfig.QueryTracingEnabled(),
