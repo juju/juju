6a74b7f3238b12f1b4fd5b388fe0fbece4d09e74
Luci Brănescu <luci.branescu@canonical.com>
2026-07-16 11:26:20 +0100

    docs: revert some of the reword

--- cmd/juju/application/consume.go
+++ cmd/juju/application/consume.go
@@ -33,9 +33,8 @@ The path to the remote offer is formatted as follows:
 
 If the model qualifier is omitted, Juju will use the user that is currently
 logged in to the controller providing the offer.
-
-If the controller name is omitted, Juju looks for the offer on the currently
-active controller.
+If the controller name is omitted, Juju will use the currently active
+controller.
 Pass ` + "`--all-controllers`" + ` or ` + "`-a`" + ` to also search the other
 controllers registered locally.
 To target a specific controller directly, include the controller name in the offer path.
