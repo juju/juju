284f40cf45b003e0cedefbd599f50b515b714ffe
Joseph Phillips <joe.phillips@canonical.com>
2026-07-17 13:53:42 +0200

    chore: update source/dest package for generated mock

--- internal/worker/unitless/package_test.go
+++ internal/worker/unitless/package_test.go
@@ -3,4 +3,4 @@
 
 package unitless
 
-//go:generate go run go.uber.org/mock/mockgen -typed -package unitless -self_package github.com/juju/juju/internal/worker/scriptlet -destination service_mock_test.go github.com/juju/juju/internal/worker/scriptlet ScriptletService
+//go:generate go run go.uber.org/mock/mockgen -typed -package unitless -self_package github.com/juju/juju/internal/worker/unitless -destination service_mock_test.go github.com/juju/juju/internal/worker/unitless ScriptletService
