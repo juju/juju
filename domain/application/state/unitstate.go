a1422399c5c8f686b32148da33b6a823d9b897cb
Simon Richardson <stickupkid@gmail.com>
2026-07-17 16:59:00 +0100

    refactor: finish unitstate

--- domain/application/state/unitstate.go
+++ domain/application/state/unitstate.go
@@ -646,7 +646,7 @@ DO NOTHING
 
 func (st *State) k8sSubnetUUIDsByAddressType(ctx context.Context, tx *sqlair.TX) (map[network.AddressType]string, error) {
 	result := make(map[network.AddressType]string)
-	subnetStmt, err := st.Prepare(`SELECT &subnet.* FROM subnet`, subnet{})
+	subnetStmt, err := st.Prepare(`SELECT &subnet.* FROM subnet WHERE cidr >= ''`, subnet{})
 	if err != nil {
 		return nil, errors.Capture(err)
 	}
