80c1d215776e125d90bd9e8f1ad119337024d339
ale8k <alexjkilroy@gmail.com>
2026-07-17 12:02:08 +0100

    chore: fix static analysis

--- core/machineauthentication/machineauthentication.go
+++ core/machineauthentication/machineauthentication.go
@@ -1,8 +1,6 @@
 // Copyright 2023 Canonical Ltd.
 // Licensed under the AGPLv3, see LICENCE file for details.
 
-// Package machineauthentication holds core types needed for Juju for
-// representing machine authentication information.
 package machineauthentication
 
 import (
