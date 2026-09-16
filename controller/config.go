e9892f2adcfb69257de74916b251a4d73979456b
ale8k <alexjkilroy@gmail.com>
2026-09-16 16:32:30 +0100

    feat: update ssh server port from controlsocket via controller config update
    Updates the ssh server ssh port after receiving the new value over the control socket.

--- controller/config.go
+++ controller/config.go
@@ -518,6 +518,10 @@ var (
 		DqliteBusyTimeout,
 
 		SSHMaxConcurrentConnections,
+		// SSHServerPort is updatable after bootstrap because the controller
+		// charm owns the SSH server port and pushes changes to the controller
+		// agent via the control socket.
+		SSHServerPort,
 	)
 
 	methodNameRE = regexp.MustCompile(`[[:alpha:]][[:alnum:]]*\.[[:alpha:]][[:alnum:]]*`)
